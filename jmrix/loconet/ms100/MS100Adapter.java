6ab929468fa17ca1bb8bfe6046c6cc7f5c96db5a
Bob Jacobsen <jacobsen@mac.com>
2001-06-04 03:01:55 +0000

    Installing initial files as we build the tree

---
+++ jmrix/loconet/ms100/MS100Adapter.java
@@ -0,0 +1,105 @@
+/** 
+ * MS100Adapter.java
+ *
+ * Title:			MS100Adapter
+ * Description:		Provide access to LocoNet via a MS100 attached to a serial comm port.
+ *					Normally controlled by the MS100Frame class.
+ * @author			Bob Jacobsen
+ * @version			
+ */
+
+package LocoMon;
+
+import javax.comm.CommPortIdentifier;
+import javax.comm.SerialPort;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.io.DataOutputStream;
+import java.io.DataInputStream;
+
+import LocoNet.LnPortController;
+
+import ErrLoggerJ.ErrLog;
+
+public class MS100Adapter extends LocoNet.LnPortController  {
+
+	Vector portNameVector = null;
+	SerialPort activeSerialPort = null;
+	
+	public Vector getPortNames() {
+		// first, check that the comm package can be opened and ports seen
+		portNameVector = new Vector();
+		Enumeration portIDs = CommPortIdentifier.getPortIdentifiers();
+		// find the names of suitable ports
+		while (portIDs.hasMoreElements()) {
+		  CommPortIdentifier id = (CommPortIdentifier) portIDs.nextElement();
+		  // accumulate the names in a vector
+		  portNameVector.addElement(id.getName());
+		  }
+		return portNameVector;
+	}
+	
+	public void openPort(String portName, String appName)  {
+		// open the primary and secondary ports in LocoNet mode, check ability to set moderators
+		try {
+			// get and open the primary port
+			CommPortIdentifier portID = CommPortIdentifier.getPortIdentifier(portName);
+  			activeSerialPort = (SerialPort) portID.open(appName, 0);  // name of program, msec to wait
+
+			// try to set it for LocoNet direct (e.g. via MS100)
+			// spec is 16600, says 16457 is OK also. Try that as a second choice
+			try {
+				activeSerialPort.setSerialPortParams(16600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			} catch (javax.comm.UnsupportedCommOperationException e) {
+				// assume that's a baudrate problem, fall back. Error here goes out to terminate function
+				ErrLog.msg(ErrLog.warning, "MS100Adapter", "openPort", "attempting to fall back to 16457 baud after 16600 failed");
+				activeSerialPort.setSerialPortParams(16457, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+			}
+			
+			// disable flow control; hardware lines used for signalling, XON/XOFF might appear in data
+			activeSerialPort.setFlowControlMode(0);
+			// activeSerialPort.setFlowControlMode(SerialPort.FLOWCONTROL_RTSCTS_IN | SerialPort.FLOWCONTROL_RTSCTS_OUT);
+
+			// set RTS high, DTR low to power the MS100
+			activeSerialPort.setRTS(true);		// not connected in some serial ports and adapters
+			activeSerialPort.setDTR(false);		// pin 1 in DIN8; on main connector, this is DTR
+						
+			opened = true;
+			
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		
+	}
+
+// base class methods for the LnPortController interface
+	public DataInputStream getInputStream() {
+		if (!opened) ErrLog.msg(ErrLog.error, "MS100Adapter", "getInputStream", "called before load(), stream not available");
+		try {
+			return new DataInputStream(activeSerialPort.getInputStream());
+     		}
+     	catch (java.io.IOException e) {
+     		ErrLog.msg(ErrLog.error, "MS100Adapter", "getInputStream", "exception: "+e);
+     	}
+     	return null;
+	}
+	
+	public DataOutputStream getOutputStream() {
+		if (!opened) ErrLog.msg(ErrLog.error, "MS100Adapter", "getOutputStream", "called before load(), stream not available");
+		try {
+     		return new DataOutputStream(activeSerialPort.getOutputStream());
+     		}
+     	catch (java.io.IOException e) {
+     		ErrLog.msg(ErrLog.error, "MS100Adapter", "getOutputStream", "exception: "+e);
+     	}
+     	return null;
+	}
+	
+	public boolean status() {return opened;}
+	
+// private control members
+	private boolean opened = false;
+	
+}
---
+++ jmrix/loconet/ms100/MS100Frame.java
@@ -0,0 +1,158 @@
+package LocoMon;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import LocoMon.LocoMonFrame;
+import LocoMon.LocoMonGen;
+import LocoNet.LnTrafficController;
+import ErrLoggerJ.ErrLog;
+
+
+public class MS100Frame extends javax.swing.JFrame {
+
+// IMPORTANT: Source code between BEGIN/END comment pair will be regenerated
+// every time the form is saved. All manual changes will be overwritten.
+// BEGIN GENERATED CODE
+	// member declarations
+	javax.swing.JButton getNamesButton = new javax.swing.JButton();
+	javax.swing.JList portList = new javax.swing.JList();
+	javax.swing.JButton openPortButton = new javax.swing.JButton();
+// END GENERATED CODE
+
+	public MS100Frame() {
+	}
+
+	public void initComponents() throws Exception {
+// IMPORTANT: Source code between BEGIN/END comment pair will be regenerated
+// every time the form is saved. All manual changes will be overwritten.
+// BEGIN GENERATED CODE
+		// the following code sets the frame's initial state
+
+		getNamesButton.setText("Get port names");
+		getNamesButton.setLocation(new java.awt.Point(10, 10));
+		getNamesButton.setVisible(true);
+		getNamesButton.setSize(new java.awt.Dimension(180, 40));
+
+		portList.setLocation(new java.awt.Point(10, 60));
+		portList.setVisible(true);
+		portList.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+		portList.setToolTipText("Select the primary serial port");
+		portList.setSize(new java.awt.Dimension(180, 80));
+
+		openPortButton.setText("Open LocoNet port");
+		openPortButton.setLocation(new java.awt.Point(10, 150));
+		openPortButton.setVisible(true);
+		openPortButton.setToolTipText("Use this open button if your MS100 is connected to one port");
+		openPortButton.setSize(new java.awt.Dimension(180, 40));
+
+		setLocation(new java.awt.Point(5, 40));
+		setTitle("LocoNet Monitor startup");
+		getContentPane().setLayout(null);
+		setSize(new java.awt.Dimension(442, 257));
+		getContentPane().add(getNamesButton);
+		getContentPane().add(portList);
+		getContentPane().add(openPortButton);
+
+
+		getNamesButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				getNamesButtonActionPerformed(e);
+			}
+		});
+		openPortButton.addActionListener(new java.awt.event.ActionListener() {
+			public void actionPerformed(java.awt.event.ActionEvent e) {
+				openPortButtonActionPerformed(e);
+			}
+		});
+		addWindowListener(new java.awt.event.WindowAdapter() {
+			public void windowClosing(java.awt.event.WindowEvent e) {
+				thisWindowClosing(e);
+			}
+		});
+
+// END GENERATED CODE
+	}
+  
+  	private boolean mShown = false;
+  	
+	public void addNotify() {
+		super.addNotify();
+		
+		if (mShown)
+			return;
+			
+		// resize frame to account for menubar
+		JMenuBar jMenuBar = getJMenuBar();
+		if (jMenuBar != null) {
+			int jMenuBarHeight = jMenuBar.getPreferredSize().height;
+			Dimension dimension = getSize();
+			dimension.height += jMenuBarHeight;
+			setSize(dimension);
+		}
+
+		mShown = true;
+	}
+
+	// Close the window when the close box is clicked
+	void thisWindowClosing(java.awt.event.WindowEvent e) {
+		setVisible(false);
+		dispose();
+		System.exit(0);
+	}
+		
+	public void getNamesButtonActionPerformed(java.awt.event.ActionEvent e) {
+		portList.setListData(adapter.getPortNames());
+
+	}
+	
+	public void openPortButtonActionPerformed(java.awt.event.ActionEvent e) {
+		adapter.openPort((String) portList.getSelectedValue(),"MS100SerialFrame");
+		
+		// now continue firing up the rest of the app
+		// add the LocoMonFrame frame
+		lmFrame = new LocoMonFrame();
+		try {
+			lmFrame.initComponents();
+			}
+		catch (Exception ex) {
+			ErrLog.msg(ErrLog.error, "LocoMonAppl.Frame1 starting LocoMonFrame", "", "Exception: "+ex.toString());
+			}
+		lmFrame.show();
+		// add the LocoMonGen frame
+		lmGen = new LocoMonGen();
+		try {
+			lmGen.initComponents();
+			}
+		catch (Exception ex) {
+			ErrLog.msg(ErrLog.error, "LocoMonAppl.Frame1 starting LocoMonGen", "", "Exception: "+ex.toString());
+			}
+		lmGen.show();
+		//create a LnTrafficController object and a LocoMonFrame,
+		LnTrafficController tc = new LnTrafficController();
+
+		// connect them all together
+		tc.notifyLocoNetEvents(~0, lmFrame);
+		tc.connectToPort(adapter);
+		lmGen.connect(tc);
+		
+		// start operation
+		// sourceThread = new Thread(p);
+		// sourceThread.start();
+		sinkThread = new Thread(tc);
+		sinkThread.start();
+		
+		// hide this frame, since we're done
+		hide();
+
+	}
+	
+// Data members
+	private MS100Adapter adapter = new MS100Adapter();
+	// private Thread sourceThread;
+	private Thread sinkThread;
+	private LocoMonFrame lmFrame = null;
+	private LocoMonGen lmGen = null;
+
+}
