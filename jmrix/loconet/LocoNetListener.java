5169e662e92a733db5f2ee90e5a19bc46f74cd76
Bob Jacobsen <jacobsen@mac.com>
2001-06-06 05:40:58 +0000

    Initial install

---
+++ jmrix/loconet/LocoNetListener.java
@@ -0,0 +1,17 @@
+/** 
+ * LocoNetListener.java
+ *
+ * Description:		<describe the LocoNetListener class here>
+ * @author			Bob Jacobsen  Copyright (C) 2001
+ * @version			
+ */
+
+package LocoNet;
+
+
+public interface LocoNetListener extends java.util.EventListener{
+	public void message(LocoNetMessage m);
+}
+
+
+/* @(#)LocoNetListener.java */
