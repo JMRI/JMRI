a14f9128202a008d0958fed1c00b01e2f9f05476
Bob Jacobsen <jacobsen@mac.com>
2001-07-27 08:27:22 +0000

    Initial install

---
+++ jmri/progdebugger/ProgDebugger.java
@@ -0,0 +1,45 @@
+/** 
+ * ProgDebugger.java
+ *
+ * Description:		debugging implementation of Programmer
+ * @author			Bob Jacobsen Copyright (C) 2001
+ * @version			
+ */
+
+package jmri;
+
+import ErrLoggerJ.ErrLog;
+import jmri.ProgListener;
+
+public class ProgDebugger implements Programmer  {
+
+	// write CV
+	public void writeCV(int CV, int val, int mode, ProgListener p) throws ProgrammerException
+	{
+		final ProgListener m = p;
+		// log out the request
+		ErrLog.msg(ErrLog.routine,"ProgDebugger", "writeCV", "write CV: "+CV+" to: "+val+" mode: "+mode);
+		// return a notification via the queue to ensure end
+		Runnable r = new Runnable() {
+			ProgListener l = m;
+			public void run() { l.programmingOpReply(-1, 0); }  // 0 is OK status
+			};
+		javax.swing.SwingUtilities.invokeLater(r);
+	}
+	
+	// read CV
+	public void readCV(int CV, int mode, ProgListener p) throws ProgrammerException {
+		final ProgListener m = p;
+		ErrLog.msg(ErrLog.routine,"ProgDebugger", "readCV", "read CV: "+CV+" mode: "+mode);
+		// return a notification via the queue to ensure end
+		Runnable r = new Runnable() {
+			ProgListener l = m;
+			public void run() { l.programmingOpReply(123, 0); }  // 0 is OK status
+			};
+		javax.swing.SwingUtilities.invokeLater(r);
+
+	}
+}
+
+
+/* @(#)ProgDebugger.java */
