266b6133a5fa1253a011a8e2ea80bd0958178b07
Bob Jacobsen <jacobsen@mac.com>
2001-08-07 08:34:43 +0000

    This time, spell the PowerManager filename right. Add it to the InstanceManager.

--- jmri/InstanceManager.java
+++ jmri/InstanceManager.java
@@ -8,15 +8,20 @@
 
 package jmri;
 import jmri.Programmer;
+import jmri.PowerManager;
 
 public class InstanceManager {
+
+	static public PowerManager powerManagerInstance()  { return _powerManager; }
 
 	static public Programmer programmerInstance()  { return _programmer; }
 	
 	
+	static private PowerManager _powerManager = null;
+	static public void setPowerManager(PowerManager p) { _powerManager = p; }
+
 	static private Programmer _programmer = null;
 	static public void setProgrammer(Programmer p) { _programmer = p; }
-
 }
 
 
