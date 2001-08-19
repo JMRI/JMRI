abce61ce93ae35652a900b269d5081733e2a9edf
Bob Jacobsen <jacobsen@mac.com>
2001-08-19 18:00:06 +0000

    More Sensor install; update comments in turnout

--- jmrix/loconet/LnSensor.java
+++ jmrix/loconet/LnSensor.java
@@ -1,18 +1,18 @@
 /** 
- * LnTurnout.java
+ * LnSensor.java
  *
- * Description:		extend jmri.AbstractTurnout for LocoNet layouts
+ * Description:		extend jmri.AbstractSensor for LocoNet layouts
  * @author			Bob Jacobsen Copyright (C) 2001
  * @version			
  */
 
 package jmri.jmrix.loconet;
 
-import jmri.AbstractTurnout;
+import jmri.AbstractSensor;
 
-public class LnTurnout extends AbstractTurnout implements LocoNetListener {
+public class LnSensor extends AbstractSensor implements LocoNetListener {
 
-	public LnTurnout(int number) {  // a human-readable turnout number must be specified!
+	public LnSensor(int number) {  // a human-readable sensor number must be specified!
 		_number = number;
 		// At construction, register for messages
 		LnTrafficController.instance().addLocoNetListener(~0, this);	
@@ -29,16 +29,7 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 		// compute address fields
 		int hiadr = (_number-1)/128;
 		int loadr = (_number-1)-hiadr*128;
-		
-		// set closed (note that this can't handle both!  Not sure how to
-		// say that in LocoNet.
-		if ((s & CLOSED) != 0) {
-			hiadr |= 0x20;
-			// thrown exception if also THROWN
-			if ((s & THROWN) != 0)
-				throw new jmri.JmriException("LocoNet turnout logic can't handle both THROWN and CLOSED yet");
-			}
-			
+					
 		// load On/Off with on
 		hiadr |= 0x10;
 		
@@ -57,37 +48,25 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 	public void message(LocoNetMessage l) {
 		// parse message type
 		switch (l.getOpCode()) {
-        	case LnConstants.OPC_SW_REQ: {               /* page 9 of Loconet PE */
-	            int sw1 = l.getElement(1);
-	            int sw2 = l.getElement(2);
-				if (myAddress(sw1, sw2)) {
-					if (log.isDebugEnabled()) log.debug("SW_REQ received with valid address");
-					if ((sw2 & LnConstants.OPC_SW_REQ_DIR)!=0)
-						newCommandedState(CLOSED);
-					else
-						newCommandedState(THROWN);
-				}
-				break;
-				}
 	        case LnConstants.OPC_SW_REP: {               /* page 9 of Loconet PE */
             	int sw1 = l.getElement(1);
             	int sw2 = l.getElement(2);
 				if (myAddress(sw1, sw2)) {
 					if (log.isDebugEnabled()) log.debug("SW_REP received with valid address");
-					// see if its a turnout state report
+					// see if its a sensor state report
     	        	if ((sw2 & LnConstants.OPC_SW_REP_INPUTS)==0) {
     	        		// sort out states
     	        		switch (sw2 & 
     	        			(LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN)) {
     	        			
     	        			case LnConstants.OPC_SW_REP_CLOSED:	
-    	        				setKnownState(CLOSED);
+    	        				setKnownState(ACTIVE);
     	        				break;
     	        			case LnConstants.OPC_SW_REP_THROWN:	
-    	        				setKnownState(THROWN);
+    	        				setKnownState(INACTIVE);
     	        				break;
     	        			case LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN:	
-    	        				setKnownState(CLOSED+THROWN);
+    	        				setKnownState(ACTIVE+INACTIVE);
     	        				break;
     	        			default:	
     	        				setKnownState(0);
@@ -103,15 +82,15 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 	}
 		
 	// data members
-	int _number;   // loconet turnout number
+	int _number;   // loconet sensor number
 	
 	private boolean myAddress(int a1, int a2) { 
 		// the "+ 1" in the following converts to throttle-visible numbering
 		return (((a2 & 0x0f) * 128) + (a1 & 0x7f) + 1) == _number; 
 		}
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnTurnout.class.getName());
+	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnSensor.class.getName());
 
 }
 
 
-/* @(#)LnTurnout.java */
+/* @(#)LnSensor.java */
