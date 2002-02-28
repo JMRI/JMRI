bf4bf427e55b01f1686d73572e0d320f5c255a62
Bob Jacobsen <jacobsen@mac.com>
2002-02-28 20:20:47 +0000

    Part of the move to consistent directory and package names

---
+++ jmri/util/oreilly/BasicWindowMonitor.java
@@ -0,0 +1,20 @@
+
+/*   BasicWindowMonitor.java
+ *           cribbed from the O'Reilly Swing book chapter 2
+ *
+ */
+
+package jmri.util.oreilly;
+
+import java.awt.event.*;
+import java.awt.Window;
+
+public class BasicWindowMonitor extends WindowAdapter {
+
+  public void windowClosing(WindowEvent e) {
+    Window w = e.getWindow();
+    w.setVisible(false);
+    w.dispose();
+    System.exit(0);
+  }
+}
