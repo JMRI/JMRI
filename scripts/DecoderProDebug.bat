9a57caa3ea20f9526fdcf4b19322c8cf768b132f
Bob Jacobsen <jacobsen@mac.com>
2002-03-09 20:17:43 +0000

    remove DecoderPro.jar from project

--- scripts/DecoderProDebug.bat
+++ scripts/DecoderProDebug.bat
@@ -1,2 +1,2 @@
-java -Djava.class.path=".;DecoderPro.jar;lib\collections.jar;lib\log4j.jar;lib\jdom-jdk11.jar;lib\crimson.jar;lib\comm.jar;$VFS" apps.DecoderPro.DecoderPro
+java -Djava.class.path=".;jmri.jar;lib\collections.jar;lib\log4j.jar;lib\jdom-jdk11.jar;lib\crimson.jar;lib\comm.jar;$VFS" apps.DecoderPro.DecoderPro
 
