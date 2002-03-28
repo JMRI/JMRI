222b8002b72fb534db1fefe4b149a9dcebbac1ca
Bob Jacobsen <jacobsen@mac.com>
2002-03-28 04:19:49 +0000

    initial add

---
+++ jmri/configurexml/AbstractTurnoutManagerConfigXML.java
@@ -0,0 +1,15 @@
+package jmri.configurexml;
+
+/**
+ * Provides the abstract base and much of the functionality for
+ * configuring TurnoutManagers, in particularly working with
+ * AbstractTurnoutManagers
+ * <p>Copyright: Copyright (c) 2002</p>
+ * @author Bob Jacobsen
+ * @version $Id: AbstractTurnoutManagerConfigXML.java,v 1.1 2002-03-28 04:19:49 jacobsen Exp $
+ */
+public abstract class AbstractTurnoutManagerConfigXML {
+
+    public AbstractTurnoutManagerConfigXML() {
+    }
+}
