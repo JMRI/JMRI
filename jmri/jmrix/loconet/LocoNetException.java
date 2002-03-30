62793a5d31eff62cca52648e44bc871db0831f8b
Alex Shepherd <kiwi64ajs@gmail.com>
2002-03-30 00:36:10 +0000

    added constructor that takes a string

--- jmri/jmrix/loconet/LocoNetException.java
+++ jmri/jmrix/loconet/LocoNetException.java
@@ -1,9 +1,9 @@
-/** 
+/**
  * LocoNetException.java
  *
  * Description:		<describe the LocoNetException class here>
  * @author			Bob Jacobsen  Copyright (C) 2001
- * @version			
+ * @version
  */
 
 package jmri.jmrix.loconet;
@@ -11,6 +11,10 @@ package jmri.jmrix.loconet;
 import jmri.JmriException;
 
 public class LocoNetException extends JmriException {
+
+    public LocoNetException( String m ){
+        super( m ) ;
+    }
 
 }
 
