f6fa29d93335cceb9c2f85fdcdc789c5a10bc0e6
Bob Jacobsen <jacobsen@mac.com>
2002-07-05 03:22:41 +0000

    new file, a central place to find the version string

---
+++ jmri/Version.java
@@ -0,0 +1,11 @@
+package jmri;
+
+/**
+ * Defines a simple place to get the JMRI version string
+ * @author			Bob Jacobsen   Copyright 2002
+ * @version $Revision: 1.1 $
+ */
+
+public class Version {
+    static public String name() { return "9.3.3.4"; }
+}
