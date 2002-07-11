289df33b9d6114d9f781e444df9ff9788ccff8ec
Bob Jacobsen <jacobsen@mac.com>
2002-07-11 14:39:47 +0000

    use option2 for command station id

--- jmri/jmrix/loconet/locobuffer/LocoBufferFrame.java
+++ jmri/jmrix/loconet/locobuffer/LocoBufferFrame.java
@@ -1,9 +1,9 @@
-/** 
+/**
  * LocoBufferFrame.java
  *
  * Description:		Frame to control and connect LocoNet via LocoBuffer interface and comm port
  * @author			Bob Jacobsen   Copyright (C) 2001
- * @version			
+ * @version			$Revision: 1.2 $
  */
 
 package jmri.jmrix.loconet.locobuffer;
@@ -27,20 +27,21 @@ public class LocoBufferFrame extends jmri.jmrix.SerialPortFrame {
 			// connect to the port
 			adapter.configureBaudRate((String)baudBox.getSelectedItem());
 			adapter.configureOption1((String)opt1Box.getSelectedItem());
+			adapter.configureOption2((String)opt2Box.getSelectedItem());
 			String errCode = adapter.openPort((String) portBox.getSelectedItem(),"LocoBufferFrame");
-			
+
 			if (errCode == null)	{
-				adapter.configure();						
+				adapter.configure();
 				// check for port in OK state
 				if (!((LocoBufferAdapter)adapter).okToSend()) {
 					log.info("LocoBuffer port not ready to send");
-					JOptionPane.showMessageDialog(null, 
+					JOptionPane.showMessageDialog(null,
 				   		"The LocoBuffer is unable to accept data.\n"
 				   		+"Make sure its power is on, it is connected\n"
 				   		+"to a working LocoNet, and the command station is on.\n"
 				   		+"The LocoNet LED on the LocoBuffer should be off.\n"
 				   		+"Reset the LocoBuffer by cycling its power.\n"
-				   		+"Then restart this program.", 
+				   		+"Then restart this program.",
 				   		"LocoBuffer not ready", JOptionPane.ERROR_MESSAGE);
 				}
 				// hide this frame, since we're done
@@ -53,7 +54,7 @@ public class LocoBufferFrame extends jmri.jmrix.SerialPortFrame {
 			JOptionPane.showMessageDialog(this, "Please select a port name first");
 		}
 	}
-	
+
    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LocoBufferFrame.class.getName());
 
 }
