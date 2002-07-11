f2523187fd6fb5ff0aa5b2a66bb79ecc674ad7f0
Bob Jacobsen <jacobsen@mac.com>
2002-07-11 16:16:06 +0000

    update to 0.9.4

--- jmri/Version.java
+++ jmri/Version.java
@@ -3,9 +3,9 @@ package jmri;
 /**
  * Defines a simple place to get the JMRI version string
  * @author			Bob Jacobsen   Copyright 2002
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 
 public class Version {
-    static public String name() { return "9.3.3.4"; }
+    static public String name() { return "0.9.4"; }
 }
