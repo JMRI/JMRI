8def3c0ebc9f6f18f747b96dbfcc39f92e25abf9
Bob Jacobsen <jacobsen@mac.com>
2002-07-24 05:20:40 +0000

    add bean accessors

--- jmri/AbstractSensor.java
+++ jmri/AbstractSensor.java
@@ -1,56 +1,56 @@
-/** 
+/**
  * AbstractSensor.java
  *
  * Description:		Abstract class providing the basic logic of the Sensor interface
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			
+ * @version
  */
 
 package jmri;
 import jmri.Sensor;
 
 public abstract class AbstractSensor implements Sensor, java.io.Serializable {
-	
+
 	// ctor takes a system-name string for initialization
 	public AbstractSensor(String s) { _id = s; }
 
 	// implementing classes will typically have a function/listener to get
-	// updates from the layout, which will then call 
+	// updates from the layout, which will then call
 	//		public void firePropertyChange(String propertyName,
 	//										Object oldValue,
-	//										Object newValue)	 
+	//										Object newValue)
 	// _once_ if anything has changed state
-		
+
 	// interface function implementations
 
 	public String getID() {return _id;}
 
 	public int getKnownState() {return _knownState;}
-	
+
 	// add a protected setKnownState() for implementations. Not intended for general use
 	protected void setKnownState(int s) {
 		if (_knownState != s) {
 			int oldState = _knownState;
 			_knownState = s;
 			firePropertyChange("KnownState", new Integer(oldState), new Integer(_knownState));
-			}
+        }
 	}
 
 	// internal data members
 	private String _id;
 	private int _knownState     = UNKNOWN;
-	
+
 	// since we can't do a "super(this)" in the ctor to inherit from PropertyChangeSupport, we'll
 	// reflect to it
 	java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
-	public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) { 
+	public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
 		pcs.addPropertyChangeListener(l);
 		}
 	public void firePropertyChange(String p, Object old, Object n) { pcs.firePropertyChange(p,old,n);}
-	public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) { 
+	public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
 		pcs.removePropertyChangeListener(l);
 		}
-	
+
 }
 
 
--- jmri/Sensor.java
+++ jmri/Sensor.java
@@ -1,19 +1,19 @@
-/** 
- * Sensor.java
- *
- * Description:		<describe the Sensor interface here>
- * @author			Bob Jacobsen Copyright (C) 2001
- * @version			
- */
+// Sensor.java
 
 package jmri;
 
-
+/**
+ * General input device representation.  Often subclassed for specific
+ * types of sensors.
+ *
+ * @author			Bob Jacobsen Copyright (C) 2001
+ * @version			$Revision: 1.3 $
+ */
 public interface Sensor {
-	
+
 	// user identification, unbound parameter
 	public String getID();
-	
+
 	// states are parameters; both closed and thrown is possible!
 	public static final int UNKNOWN      = 0x01;
 	public static final int ACTIVE       = 0x02;
@@ -23,13 +23,27 @@ public interface Sensor {
 	// known state on layout is a bound parameter -
 	// always returns a answer, if need be the commanded state
 	public int getKnownState();
-	
+
 	// request an update from the layout soft/hardware.  May not even
 	// happen, and if it does it will happen later; listen for the result.
 	public void requestUpdateFromLayout();
-	
-	// implementing classes will generally provide PropertyChangeListener
-	// calls for KnownState
+
+	/**
+	 * Request a call-back when the bound KnownState property changes.
+	 */
+	public void addPropertyChangeListener(java.beans.PropertyChangeListener l);
+
+	/**
+	 * Remove a request for a call-back when a bound property changes.
+	 */
+	public void removePropertyChangeListener(java.beans.PropertyChangeListener l);
+
+	/**
+	 * Remove references to and from this object, so that it can
+	 * eventually be garbage-collected.
+	 */
+	public void dispose();  // remove _all_ connections!
+
 }
 
 
