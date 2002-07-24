ddbbf93455575c6c68ff4f97f4fc849857d544d0
Bob Jacobsen <jacobsen@mac.com>
2002-07-24 05:02:06 +0000

    adjust to configxml signature changes

--- jmri/jmrix/loconet/configurexml/LnTurnoutManagerXml.java
+++ jmri/jmrix/loconet/configurexml/LnTurnoutManagerXml.java
@@ -12,7 +12,7 @@ import jmri.jmrix.loconet.LnTurnoutManager;
  * provides a load method here.
  *
  * @author Bob Jacobsen Copyright: Copyright (c) 2002
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class LnTurnoutManagerXml extends jmri.configurexml.AbstractTurnoutManagerConfigXML {
 
@@ -22,6 +22,10 @@ public class LnTurnoutManagerXml extends jmri.configurexml.AbstractTurnoutManage
 
     public void setStoreElementClass(Element turnouts) {
         turnouts.addAttribute("class","jmri.jmrix.loconet.configurexml.LnTurnoutManagerXml");
+    }
+
+    public void load(Element element, Object o) {
+        log.error("Invalid method called");
     }
 
     public void load(Element turnouts) {
@@ -34,5 +38,7 @@ public class LnTurnoutManagerXml extends jmri.configurexml.AbstractTurnoutManage
         // load individual turnouts
         loadTurnouts(turnouts);
     }
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnTurnoutManagerXml.class.getName());
 
 }
