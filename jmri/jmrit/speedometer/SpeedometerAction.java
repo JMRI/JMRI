514dccbed2a26011e032987496b46126469cfcd5
Bob Jacobsen <jacobsen@mac.com>
2002-07-26 23:02:24 +0000

    basic functionality working

---
+++ jmri/jmrit/speedometer/SpeedometerAction.java
@@ -0,0 +1,32 @@
+// SpeedometerAction.java
+
+package jmri.jmrit.speedometer;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+/**
+ * Swing action to create and register a
+ * SpeedometerFrame
+ *
+ * @author			Bob Jacobsen    Copyright (C) 2001
+ * @version			$Revision: 1.1 $
+ */
+
+public class SpeedometerAction 			extends AbstractAction {
+
+	public SpeedometerAction(String s) {
+		super(s);
+	}
+
+    public void actionPerformed(ActionEvent e) {
+
+		// create a SimpleProgFrame
+		SpeedometerFrame f = new SpeedometerFrame();
+		f.show();
+
+	}
+
+}
+
+/* @(#)SpeedometerAction.java */
