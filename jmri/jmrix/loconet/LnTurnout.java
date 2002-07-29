cbbc70005aa5ec488ff4f670260989f16e34aa2b
Bob Jacobsen <jacobsen@mac.com>
2002-07-29 06:12:02 +0000

    explicit handling of SENT feedback

--- jmri/jmrix/loconet/LnTurnout.java
+++ jmri/jmrix/loconet/LnTurnout.java
@@ -1,36 +1,42 @@
-/** 
- * LnTurnout.java
- *
- * Description:		extend jmri.AbstractTurnout for LocoNet layouts
- * @author			Bob Jacobsen Copyright (C) 2001
- * @version			
- */
+// LnTurnout.java
 
 package jmri.jmrix.loconet;
 
 import jmri.AbstractTurnout;
 
-public class LnTurnout extends AbstractTurnout implements LocoNetListener {
+/**
+ * Extend jmri.AbstractTurnout for LocoNet layouts
+ * <P>
+ * This implementation implements the "SENT" feedback,
+ * where LocoNet messages originating on the layout can change both KnownState
+ * and CommandedState.  We change both because we consider a LocoNet message to
+ * reflect how the turnout should be, even if its a readback status message.
+ * E.g. if you use a DS54 local input to change the state, resulting in a
+ * status message, we still consider that to be a commanded state change.
+ *
+ * @author			Bob Jacobsen Copyright (C) 2001
+ * @version			$Revision: 1.2 $
+ */public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 
 	public LnTurnout(int number) {  // a human-readable turnout number must be specified!
 		_number = number;
 		// At construction, register for messages
-		LnTrafficController.instance().addLocoNetListener(~0, this);	
+		LnTrafficController.instance().addLocoNetListener(~0, this);
 	}
 
 	public int getNumber() { return _number; }
 	public String getSystemName() { return "LT"+getNumber(); }
-	
+
 	// Handle a request to change state by sending a LocoNet command
 	protected void forwardCommandChangeToLayout(int s) throws jmri.JmriException {
 		// send SWREQ for close
 		LocoNetMessage l = new LocoNetMessage(4);
 		l.setOpCode(LnConstants.OPC_SW_REQ);
-		
+
 		// compute address fields
 		int hiadr = (_number-1)/128;
 		int loadr = (_number-1)-hiadr*128;
-		
+
 		// set closed (note that this can't handle both!  Not sure how to
 		// say that in LocoNet.
 		if ((s & CLOSED) != 0) {
@@ -39,21 +45,21 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 			if ((s & THROWN) != 0)
 				throw new jmri.JmriException("LocoNet turnout logic can't handle both THROWN and CLOSED yet");
 			}
-			
+
 		// load On/Off with on
 		hiadr |= 0x10;
-		
+
 		// store and send
 		l.setElement(1,loadr);
 		l.setElement(2,hiadr);
 		LnTrafficController.instance().sendLocoNetMessage(l);
 	}
-	
+
 	// implementing classes will typically have a function/listener to get
-	// updates from the layout, which will then call 
+	// updates from the layout, which will then call
 	//		public void firePropertyChange(String propertyName,
 	//										Object oldValue,
-	//										Object newValue)	 
+	//										Object newValue)
 	// _once_ if anything has changed state (or set the commanded state directly)
 	public void message(LocoNetMessage l) {
 		// parse message type
@@ -63,10 +69,13 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 	            int sw2 = l.getElement(2);
 				if (myAddress(sw1, sw2)) {
 					if (log.isDebugEnabled()) log.debug("SW_REQ received with valid address");
-					if ((sw2 & LnConstants.OPC_SW_REQ_DIR)!=0)
+					if ((sw2 & LnConstants.OPC_SW_REQ_DIR)!=0) {
 						newCommandedState(CLOSED);
-					else
+                        newKnownState(CLOSED);
+					} else {
 						newCommandedState(THROWN);
+                        newKnownState(THROWN);
+                    }
 				}
 				break;
 				}
@@ -78,20 +87,24 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 					// see if its a turnout state report
     	        	if ((sw2 & LnConstants.OPC_SW_REP_INPUTS)==0) {
     	        		// sort out states
-    	        		switch (sw2 & 
+    	        		switch (sw2 &
     	        			(LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN)) {
-    	        			
-    	        			case LnConstants.OPC_SW_REP_CLOSED:	
-    	        				setKnownState(CLOSED);
+
+    	        			case LnConstants.OPC_SW_REP_CLOSED:
+                                newCommandedState(CLOSED);
+    	        				newKnownState(CLOSED);
     	        				break;
-    	        			case LnConstants.OPC_SW_REP_THROWN:	
-    	        				setKnownState(THROWN);
+    	        			case LnConstants.OPC_SW_REP_THROWN:
+                                newCommandedState(THROWN);
+                                newKnownState(THROWN);
     	        				break;
-    	        			case LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN:	
-    	        				setKnownState(CLOSED+THROWN);
+    	        			case LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN:
+                                newCommandedState(CLOSED+THROWN);
+    	        				newKnownState(CLOSED+THROWN);
     	        				break;
-    	        			default:	
-    	        				setKnownState(0);
+    	        			default:
+                                newCommandedState(0);
+    	        				newKnownState(0);
     	        				break;
 							}
     	        		}
@@ -102,17 +115,17 @@ public class LnTurnout extends AbstractTurnout implements LocoNetListener {
 			}
 		// reach here only in error
 	}
-	
+
 	public void dispose() {
 		LnTrafficController.instance().removeLocoNetListener(~0, this);
 	}
-		
+
 	// data members
 	int _number;   // loconet turnout number
-	
-	private boolean myAddress(int a1, int a2) { 
+
+	private boolean myAddress(int a1, int a2) {
 		// the "+ 1" in the following converts to throttle-visible numbering
-		return (((a2 & 0x0f) * 128) + (a1 & 0x7f) + 1) == _number; 
+		return (((a2 & 0x0f) * 128) + (a1 & 0x7f) + 1) == _number;
 		}
 	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnTurnout.class.getName());
 
