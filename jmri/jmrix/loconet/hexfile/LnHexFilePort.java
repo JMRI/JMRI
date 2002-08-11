3eef26cc5424216a977d4b23a3e58a1cd77fc15d
Bob Jacobsen <jacobsen@mac.com>
2002-08-11 09:01:30 +0000

    trying to remove delay in threading. Fixed tabs

--- jmri/jmrix/loconet/hexfile/LnHexFilePort.java
+++ jmri/jmrix/loconet/hexfile/LnHexFilePort.java
@@ -1,25 +1,4 @@
-/**
- * LnHexFilePort.java
- *
- * Description:		LnHexFilePort implements a LnPortController via a
- *					ASCII-hex input file. See below for the file format
- *					There are user-level controls for
- *						send next message
- *						how long to wait between messages
- *
- *					An object of this class should run in a thread
- *					of its own so that it can fill the output pipe as
- *					needed.
- *
- * @author			Bob Jacobsen    Copyright (C) 2001
- * @version			$Revision: 1.2 $
- */
-
-/**
-	The input file is expected to have one message per line. Each line
-	can contain as many bytes as needed, each represented by two Hex characters
-	and separated by a space. Variable whitespace is not (yet) supported
-*/
+// LnHexFilePort.java
 
 package jmri.jmrix.loconet.hexfile;
 
@@ -34,66 +13,82 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.File;
 
+/**
+ * LnHexFilePort implements a LnPortController via a
+ * ASCII-hex input file. See below for the file format
+ * There are user-level controls for
+ *      send next message
+ *	how long to wait between messages
+ *
+ * An object of this class should run in a thread
+ * of its own so that it can fill the output pipe as
+ * needed.
+ *
+ *	The input file is expected to have one message per line. Each line
+ *	can contain as many bytes as needed, each represented by two Hex characters
+ *	and separated by a space. Variable whitespace is not (yet) supported
+ *
+ * @author			Bob Jacobsen    Copyright (C) 2001
+ * @version			$Revision: 1.3 $
+ */
 public class LnHexFilePort 			extends LnPortController implements Runnable {
 
     public LnHexFilePort() {
-		try {
+        try {
             PipedInputStream tempPipe = new PipedInputStream();
             pin = new DataInputStream(tempPipe);
             outpipe = new DataOutputStream(new PipedOutputStream(tempPipe));
             pout = outpipe;
         }
-		catch (java.io.IOException e) {
-			log.error("init (pipe): Exception: "+e.toString());
+        catch (java.io.IOException e) {
+            log.error("init (pipe): Exception: "+e.toString());
         }
     }
 
     /* load(File) fills the contents from a file */
     public void load(File file) {
-		if (log.isDebugEnabled()) log.debug("file: "+file);
+        if (log.isDebugEnabled()) log.debug("file: "+file);
 
-		// create the pipe stream for output, also store as the input stream if somebody wants to send
-		// (This will emulate the LocoNet echo)
-		try {
-			sFile = new DataInputStream(new FileInputStream(file));
+        // create the pipe stream for output, also store as the input stream if somebody wants to send
+        // (This will emulate the LocoNet echo)
+        try {
+            sFile = new DataInputStream(new FileInputStream(file));
+        } catch (Exception e) {
+            log.error("load (pipe): Exception: "+e.toString());
         }
-		catch (Exception e) {
-			log.error("load (pipe): Exception: "+e.toString());
-        }
-	}
+    }
 
     public void run() { // invoked in a new thread
         while (true) {
             if (sFile!= null) {
-        		_running = true;
-        		// process the input file into the output side of pipe
-		        try {
-			        String s;
-			        byte bval;
-			        int ival;
-			        int len;
-			        while (sFile.available() > 3) {
-				        // this loop reads one line per turn
-				        s = sFile.readLine();
-				        // ErrLog.msg(ErrLog.debugging,"LnHexFilePort","run","string=<"+s+">");
-				        len = s.length();
-				        for (int i=0; i<len; i+=3) {
-					        // parse as hex into integer, then convert to byte
-					        ival = Integer.valueOf(s.substring(i,i+2),16).intValue();
-					        // send each byte to the output pipe (input to consumer)
-					        bval = (byte) ival;
-					        outpipe.writeByte(bval);
-					    }
-				        // finished that line, wait
-				        Thread.sleep(delay);
-				    }
-			    }
-		        catch (Exception e) {
-			        log.error("run: Exception: "+e.toString());
-			    }
-		        // here we're done processing the file
+                _running = true;
+                // process the input file into the output side of pipe
+                try {
+                    String s;
+                    byte bval;
+                    int ival;
+                    int len;
+                    while (sFile.available() > 3) {
+                        // this loop reads one line per turn
+                        s = sFile.readLine();
+                        // ErrLog.msg(ErrLog.debugging,"LnHexFilePort","run","string=<"+s+">");
+                        len = s.length();
+                        for (int i=0; i<len; i+=3) {
+                            // parse as hex into integer, then convert to byte
+                            ival = Integer.valueOf(s.substring(i,i+2),16).intValue();
+                            // send each byte to the output pipe (input to consumer)
+                            bval = (byte) ival;
+                            outpipe.writeByte(bval);
+                        }
+                        // finished that line, wait
+                        Thread.sleep(delay);
+                    }
+                } catch (Exception e) {
+                    log.error("run: Exception: "+e.toString());
+                }
+                // here we're done processing the file
                 log.info("normal finish to file");
-		        sFile = null;
+                sFile = null;
                 _running = false;
             }
             // wait to be told there's more coming
@@ -103,49 +98,48 @@ public class LnHexFilePort 			extends LnPortController implements Runnable {
                 log.debug("woken from sleep");
             }
         }
-	}
+    }
 
     public void setDelay(int newDelay) {
-	    delay = newDelay;
-	}
+        delay = newDelay;
+    }
 
     // base class methods
-	public DataInputStream getInputStream() {
-		if (pin == null)
-			log.error("getInputStream: called before load(), stream not available");
-		return pin;
+    public DataInputStream getInputStream() {
+        if (pin == null)
+            log.error("getInputStream: called before load(), stream not available");
+        return pin;
     }
 
-	public DataOutputStream getOutputStream(){
-		if (pout == null) log.error("getOutputStream: called before load(), stream not available");
-		return pout;
+    public DataOutputStream getOutputStream(){
+        if (pout == null) log.error("getOutputStream: called before load(), stream not available");
+        return pout;
     }
 
-	public boolean status() {return (pout!=null)&(pin!=null);}
+    public boolean status() {return (pout!=null)&(pin!=null);}
 
-	// to tell if we're currently putting out data
-	public boolean running() { return _running; }
+    // to tell if we're currently putting out data
+    public boolean running() { return _running; }
 
-	// private data
-	private boolean _running = false;
+    // private data
+    private boolean _running = false;
 
-	// streams to share with user class
-	private DataOutputStream pout = null; // this is provided to classes who want to write to us
-	private DataInputStream pin = null;  // this is provided to class who want data from us
+    // streams to share with user class
+    private DataOutputStream pout = null; // this is provided to classes who want to write to us
+    private DataInputStream pin = null;  // this is provided to class who want data from us
 
-	// internal ends of the pipes
-	private DataOutputStream outpipe = null;  // feed pin
-	private DataInputStream inpipe = null; // feed pout
+    // internal ends of the pipes
+    private DataOutputStream outpipe = null;  // feed pin
+    private DataInputStream inpipe = null; // feed pout
 
-	// internal access to the input file
-	DataInputStream sFile = null;
+    // internal access to the input file
+    DataInputStream sFile = null;
 
-	public boolean okToSend() { return true; }
-	// define operation
-	private int delay=100;  				// units are milliseconds; default is quiet a busy LocoNet
+    public boolean okToSend() { return true; }
+    // define operation
+    private int delay=100;  				// units are milliseconds; default is quiet a busy LocoNet
 
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnHexFilePort.class.getName());
-
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnHexFilePort.class.getName());
 }
 
 /* @(#)LnHexFilePort.java */
