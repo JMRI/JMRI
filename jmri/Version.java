3eb3e362dcfdbcf05c8342486ed519621557488f
Bob Jacobsen <jacobsen@mac.com>
2002-08-13 16:41:49 +0000

    update for next dist

--- jmri/Version.java
+++ jmri/Version.java
@@ -3,9 +3,9 @@ package jmri;
 /**
  * Defines a simple place to get the JMRI version string
  * @author			Bob Jacobsen   Copyright 2002
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 
 public class Version {
-    static public String name() { return "1.0.4"; }
+    static public String name() { return "1.0.5"; }
 }
