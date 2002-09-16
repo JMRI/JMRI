6067dde0a7200a58d04cebd66ecea79b78c6a1ca
Bob Jacobsen <jacobsen@mac.com>
2002-09-16 03:47:34 +0000

    redo tabs with emacs

--- test/jmri/jmrix/loconet/LnOpsModeProgrammerTest.java
+++ test/jmri/jmrix/loconet/LnOpsModeProgrammerTest.java
@@ -7,80 +7,80 @@ import jmri.*;
 
 
 public class LnOpsModeProgrammerTest extends TestCase {
-
-  public LnOpsModeProgrammerTest(String s) {
-    super(s);
-  }
-
-  public void testSetMode() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    int mode1=  0;
-    lnopsmodeprogrammer.setMode(mode1);
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
-  }
-  public void testWriteCV() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    int CV1=  0;
-    int val2=  0;
-    ProgListener p3=  null  /** @todo fill in non-null value */;
-    try {
-      lnopsmodeprogrammer.writeCV(CV1, val2, p3);
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
+    
+    public LnOpsModeProgrammerTest(String s) {
+	super(s);
     }
-    catch(Exception e) {
-      System.err.println("Exception thrown:  "+e);
+    
+    public void testSetMode() {
+	SlotManager val1=  new SlotManager();
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	int mode1=  0;
+	lnopsmodeprogrammer.setMode(mode1);
+	/** @todo:  Insert test code here.  Use assertEquals(), for example. */
     }
-  }
-  public void testReadCV() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    int CV1=  0;
-    ProgListener p2=  null  /** @todo fill in non-null value */;
-    try {
-      lnopsmodeprogrammer.readCV(CV1, p2);
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
+    public void testWriteCV() {
+	SlotManager val1=  null  /** @todo fill in non-null value */;
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	int CV1=  0;
+	int val2=  0;
+	ProgListener p3=  null  /** @todo fill in non-null value */;
+	try {
+	    lnopsmodeprogrammer.writeCV(CV1, val2, p3);
+	    /** @todo:  Insert test code here.  Use assertEquals(), for example. */
+	}
+	catch(Exception e) {
+	    System.err.println("Exception thrown:  "+e);
+	}
     }
-    catch(Exception e) {
-      System.err.println("Exception thrown:  "+e);
+    public void testReadCV() {
+	SlotManager val1=  null  /** @todo fill in non-null value */;
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	int CV1=  0;
+	ProgListener p2=  null  /** @todo fill in non-null value */;
+	try {
+	    lnopsmodeprogrammer.readCV(CV1, p2);
+	    /** @todo:  Insert test code here.  Use assertEquals(), for example. */
+	}
+	catch(Exception e) {
+	    System.err.println("Exception thrown:  "+e);
+	}
     }
-  }
-  public void testGetMode() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    int intRet = lnopsmodeprogrammer.getMode();
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
-  }
-  public void testGetCanRead() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    boolean booleanRet = lnopsmodeprogrammer.getCanRead();
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
-  }
-  public void testConfirmCV() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    int CV1=  0;
-    int val2=  0;
-    ProgListener p3=  null  /** @todo fill in non-null value */;
-    try {
-      lnopsmodeprogrammer.confirmCV(CV1, val2, p3);
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
+    public void testGetMode() {
+	SlotManager val1=  null  /** @todo fill in non-null value */;
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	int intRet = lnopsmodeprogrammer.getMode();
+	/** @todo:  Insert test code here.  Use assertEquals(), for example. */
     }
-    catch(Exception e) {
-      System.err.println("Exception thrown:  "+e);
+    public void testGetCanRead() {
+	SlotManager val1=  null  /** @todo fill in non-null value */;
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	boolean booleanRet = lnopsmodeprogrammer.getCanRead();
+	/** @todo:  Insert test code here.  Use assertEquals(), for example. */
     }
-  }
-  public void testHasMode() {
-    SlotManager val1=  null  /** @todo fill in non-null value */;
-    LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
-    int mode1=  0;
-    boolean booleanRet = lnopsmodeprogrammer.hasMode(mode1);
-  /** @todo:  Insert test code here.  Use assertEquals(), for example. */
-  }
-
-  // The minimal setup for log4J
+    public void testConfirmCV() {
+	SlotManager val1=  null  /** @todo fill in non-null value */;
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	int CV1=  0;
+	int val2=  0;
+	ProgListener p3=  null  /** @todo fill in non-null value */;
+	try {
+	    lnopsmodeprogrammer.confirmCV(CV1, val2, p3);
+	    /** @todo:  Insert test code here.  Use assertEquals(), for example. */
+	}
+	catch(Exception e) {
+	    System.err.println("Exception thrown:  "+e);
+	}
+    }
+    public void testHasMode() {
+	SlotManager val1=  null  /** @todo fill in non-null value */;
+	LnOpsModeProgrammer lnopsmodeprogrammer = new LnOpsModeProgrammer(val1);
+	int mode1=  0;
+	boolean booleanRet = lnopsmodeprogrammer.hasMode(mode1);
+	/** @todo:  Insert test code here.  Use assertEquals(), for example. */
+    }
+    
+    // The minimal setup for log4J
     apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
     protected void setUp() { log4jfixtureInst.setUp(); }
     protected void tearDown() { log4jfixtureInst.tearDown(); }
