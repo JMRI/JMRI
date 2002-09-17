6170399038476c21b741709b1b95e66fb54c81e3
Bob Jacobsen <jacobsen@mac.com>
2002-09-17 08:57:50 +0000

    clean up comments and retab

--- jmri/Programmer.java
+++ jmri/Programmer.java
@@ -11,61 +11,64 @@ import java.beans.PropertyChangeListener;
  * <LI>Service Mode, e.g. on a programming track
  * <LI>Ops Mode, e.g. "programming on the main"
  * </UL>
- * You get a Programmer object from a ProgrammerManager, which in turn can be located
- * from the InstanceManager.
+ * depending on which type you have, only certain modes can
+ * be set.  
+ * <P>
+ * You get a Programmer object from a ProgrammerManager, 
+ * which in turn can be located from the InstanceManager.
  * @see             jmri.ProgrammerManager
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.11 $
+ * @version			$Revision: 1.12 $
  */
 public interface Programmer  {
 
-	// mode e.g. register, direct, paged
-	public static final int NONE			=  0;
-	public static final int REGISTERMODE    = 11;
-	public static final int PAGEMODE        = 21;
-	public static final int DIRECTBITMODE   = 31;
-	public static final int DIRECTBYTEMODE  = 32;
-	public static final int ADDRESSMODE     = 41;
+    // mode e.g. register, direct, paged
+    public static final int NONE			=  0;
+    public static final int REGISTERMODE    = 11;
+    public static final int PAGEMODE        = 21;
+    public static final int DIRECTBITMODE   = 31;
+    public static final int DIRECTBYTEMODE  = 32;
 
-	/**
-	 * Perform a CV write in the system-specific manner,
-	 * and using the specified programming mode.
-	 * Note that this returns before the write
-	 * is complete; you have to provide a ProgListener to hear about
-	 * completion. The exceptions will only be thrown at the start, not
-	 * during the actual programming sequence. A typical exception would be
-	 * due to an invalid mode (though that should be prevented earlier)
-	 */
-	public void writeCV(int CV, int val, ProgListener p) throws ProgrammerException;
+    /**
+     * Perform a CV write in the system-specific manner,
+     * and using the specified programming mode.
+     * Note that this returns before the write
+     * is complete; you have to provide a ProgListener to hear about
+     * completion. The exceptions will only be thrown at the start, not
+     * during the actual programming sequence. A typical exception would be
+     * due to an invalid mode (though that should be prevented earlier)
+     */
+    public void writeCV(int CV, int val, ProgListener p) throws ProgrammerException;
 
-	/**
-	 * Perform a CV read in the system-specific manner,
-	 * and using the specified programming mode.
-	 * Note that this returns before the read
-	 * is complete; you have to provide a ProgListener to hear about
-	 * completion. The exceptions will only be thrown at the start, not
-	 * during the actual programming sequence. A typical exception would be
-	 * due to an invalid mode (though that should be prevented earlier)
-	 */
-	public void readCV(int CV, ProgListener p) throws ProgrammerException;
+    /**
+     * Perform a CV read in the system-specific manner,
+     * and using the specified programming mode.
+     * Note that this returns before the read
+     * is complete; you have to provide a ProgListener to hear about
+     * completion. The exceptions will only be thrown at the start, not
+     * during the actual programming sequence. A typical exception would be
+     * due to an invalid mode (though that should be prevented earlier)
+     */
+    public void readCV(int CV, ProgListener p) throws ProgrammerException;
 
-	/**
-	 * Confirm the value of a CV using the specified programming mode.
-	 * On some systems, this is faster than a read.
-	 * Note that this returns before the confirm
-	 * is complete; you have to provide a ProgListener to hear about
-	 * completion. The exceptions will only be thrown at the start, not
-	 * during the actual programming sequence. A typical exception would be
-	 * due to an invalid mode (though that should be prevented earlier)
-	 */
-	public void confirmCV(int CV, int val, ProgListener p) throws ProgrammerException;
+    /**
+     * Confirm the value of a CV using the specified programming mode.
+     * On some systems, this is faster than a read.
+     * Note that this returns before the confirm
+     * is complete; you have to provide a ProgListener to hear about
+     * completion. The exceptions will only be thrown at the start, not
+     * during the actual programming sequence. A typical exception would be
+     * due to an invalid mode (though that should be prevented earlier)
+     */
+    public void confirmCV(int CV, int val, ProgListener p) throws ProgrammerException;
 
-	/*
-	 * Mode is a property that can be set and queried for the
-	 * programmer.  Notification is also possible...
-	 */
-	public void setMode(int mode);
-	public int  getMode();
+    /*
+     * Mode is a property that can be set and queried for the
+     * programmer.  Notification is also possible...
+     */
+    public void setMode(int mode);
+    public int  getMode();
+
     /**
      * Check if a given mode is available
      * @param mode Availability of this mode is returned
@@ -75,22 +78,13 @@ public interface Programmer  {
 
     public boolean getCanRead();
 
-	public void addPropertyChangeListener(PropertyChangeListener p);
-	public void removePropertyChangeListener(PropertyChangeListener p);
+    public void addPropertyChangeListener(PropertyChangeListener p);
+    public void removePropertyChangeListener(PropertyChangeListener p);
 
-	// error handling on request is via exceptions
-	// results are returned via the ProgListener callback
+    // error handling on request is via exceptions
+    // results are returned via the ProgListener callback
 
-	public String decodeErrorCode(int i);
-
-	// special case for CV18/19 double write?
-	// access to direct mode bit operations?
-	// programming on the main / ops mode?
-
-	// in use? By who?
-
-	// support for more than one programmer? For getting one?
-
+    public String decodeErrorCode(int i);
 
 }
 
