7036a440164b4b873a7f876fa92644a7efcbcd5d
Bob Jacobsen <jacobsen@mac.com>
2002-09-25 16:43:40 +0000

    clean up

--- jmri/jmrit/symbolicprog/KnownLocoSelPane.java
+++ jmri/jmrit/symbolicprog/KnownLocoSelPane.java
@@ -20,105 +20,105 @@ import com.sun.java.util.collections.List;
  * you're interested in.
  *
  * @author			Bob Jacobsen   Copyright (C) 2001, 2002
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  */
 public class KnownLocoSelPane extends javax.swing.JPanel  {
-
-	public KnownLocoSelPane(JLabel s) {
-		_statusLabel = s;
-		init();
-	}
-
-	public KnownLocoSelPane() {
-		init();
-	}
-
-	protected void init() {
-		JLabel last;
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		JLabel l2 = new JLabel("Known locomotive on programming track");
-		l2.setBorder(new EmptyBorder(6,0,6,0));
-		add(l2);
-			JPanel pane2a = new JPanel();
-			pane2a.setLayout(new BoxLayout(pane2a, BoxLayout.X_AXIS));
-			pane2a.add(new JLabel("Select from roster:"));
-			JButton idloco = new JButton("Identify locomotive");
-			idloco.addActionListener( new ActionListener() {
-				public void actionPerformed(java.awt.event.ActionEvent e) {
-					if (log.isInfoEnabled()) log.info("Identify locomotive pressed");
-					startIdentify();
-				}
-			});
-			pane2a.add(idloco);
-			pane2a.setAlignmentX(JLabel.LEFT_ALIGNMENT);
-		add(pane2a);
-
-		locoBox = Roster.instance().matchingComboBox(null, null, null, null, null, null, null);
-		add(locoBox);
-
-		JButton go2 = new JButton("Open programmer");
-		go2.addActionListener( new ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent e) {
-				if (log.isInfoEnabled()) log.info("Open programmer pressed");
-				openButton();
-			}
-		});
-		add(go2);
-		setBorder(new EmptyBorder(6,6,6,6));
-	}
-
-	JLabel _statusLabel = null;
-
-	private void startIdentify() {
-		// start identifying a loco
-		final KnownLocoSelPane me = this;
-		IdentifyLoco id = new IdentifyLoco() {
-			private KnownLocoSelPane who = me;
-			protected void done(int dccAddress) {
+    
+    public KnownLocoSelPane(JLabel s) {
+        _statusLabel = s;
+        init();
+    }
+    
+    public KnownLocoSelPane() {
+        init();
+    }
+    
+    protected void init() {
+        JLabel last;
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JLabel l2 = new JLabel("Known locomotive on programming track");
+        l2.setBorder(new EmptyBorder(6,0,6,0));
+        add(l2);
+        JPanel pane2a = new JPanel();
+        pane2a.setLayout(new BoxLayout(pane2a, BoxLayout.X_AXIS));
+        pane2a.add(new JLabel("Select from roster:"));
+        JButton idloco = new JButton("Identify locomotive");
+        idloco.addActionListener( new ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    if (log.isInfoEnabled()) log.info("Identify locomotive pressed");
+                    startIdentify();
+                }
+            });
+        pane2a.add(idloco);
+        pane2a.setAlignmentX(JLabel.LEFT_ALIGNMENT);
+        add(pane2a);
+        
+        locoBox = Roster.instance().matchingComboBox(null, null, null, null, null, null, null);
+        add(locoBox);
+        
+        JButton go2 = new JButton("Open programmer");
+        go2.addActionListener( new ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    if (log.isInfoEnabled()) log.info("Open programmer pressed");
+                    openButton();
+                }
+            });
+        add(go2);
+        setBorder(new EmptyBorder(6,6,6,6));
+    }
+    
+    JLabel _statusLabel = null;
+    
+    private void startIdentify() {
+        // start identifying a loco
+        final KnownLocoSelPane me = this;
+        IdentifyLoco id = new IdentifyLoco() {
+                private KnownLocoSelPane who = me;
+                protected void done(int dccAddress) {
 				// if Done, updated the selected decoder
-				who.selectLoco(dccAddress);
-			}
-			protected void message(String m) {
-				if (_statusLabel != null) _statusLabel.setText(m);
-			}
-			public void error() {}
-		};
-		id.start();
-	}
-
-	private void selectLoco(int dccAddress) {
-		// locate that loco
-		List l = Roster.instance().matchingList(null, null, Integer.toString(dccAddress),
-												null, null, null, null);
-		if (log.isDebugEnabled()) log.debug("selectLoco found "+l.size()+" matches");
-		if (l.size() > 0) {
-			RosterEntry r = (RosterEntry)l.get(0);
-			String id = r.getId();
-			if (log.isDebugEnabled()) log.debug("Loco id is "+id);
-			for (int i = 0; i<locoBox.getItemCount(); i++) {
-				if (id.equals((String)locoBox.getItemAt(i))) locoBox.setSelectedIndex(i);
-			}
-		} else {
-			log.warn("Read address "+dccAddress+", but no such loco in roster");
-		}
-	}
-
-	private JComboBox locoBox = null;
-
-	/** handle pushing the open programmer button by finding names, then calling a template method */
-	protected void openButton() {
-
-		RosterEntry re = Roster.instance().entryFromTitle((String)locoBox.getSelectedItem());
-		if (re == null) log.error("RosterEntry is null during open; that shouldnt be possible");
-
-		startProgrammer(null, re);
-	}
-
-	/** meant to be overridden to start the desired type of programmer */
-	protected void startProgrammer(DecoderFile decoderFile, RosterEntry r) {
-		log.error("startProgrammer method in NewLocoSelPane should have been overridden");
-	}
-
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(KnownLocoSelPane.class.getName());
-
+                    who.selectLoco(dccAddress);
+                }
+                protected void message(String m) {
+                    if (_statusLabel != null) _statusLabel.setText(m);
+                }
+                public void error() {}
+            };
+        id.start();
+    }
+    
+    private void selectLoco(int dccAddress) {
+        // locate that loco
+        List l = Roster.instance().matchingList(null, null, Integer.toString(dccAddress),
+                                                null, null, null, null);
+        if (log.isDebugEnabled()) log.debug("selectLoco found "+l.size()+" matches");
+        if (l.size() > 0) {
+            RosterEntry r = (RosterEntry)l.get(0);
+            String id = r.getId();
+            if (log.isDebugEnabled()) log.debug("Loco id is "+id);
+            for (int i = 0; i<locoBox.getItemCount(); i++) {
+                if (id.equals((String)locoBox.getItemAt(i))) locoBox.setSelectedIndex(i);
+            }
+        } else {
+            log.warn("Read address "+dccAddress+", but no such loco in roster");
+        }
+    }
+    
+    private JComboBox locoBox = null;
+    
+    /** handle pushing the open programmer button by finding names, then calling a template method */
+    protected void openButton() {
+        
+        RosterEntry re = Roster.instance().entryFromTitle((String)locoBox.getSelectedItem());
+        if (re == null) log.error("RosterEntry is null during open; that shouldnt be possible");
+        
+        startProgrammer(null, re);
+    }
+    
+    /** meant to be overridden to start the desired type of programmer */
+    protected void startProgrammer(DecoderFile decoderFile, RosterEntry r) {
+        log.error("startProgrammer method in NewLocoSelPane should have been overridden");
+    }
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(KnownLocoSelPane.class.getName());
+    
 }
