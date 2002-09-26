1f653b6475f1b8e31991d90285bf38783488c901
Bob Jacobsen <jacobsen@mac.com>
2002-09-26 14:26:02 +0000

    tests currently passing. Some improvements in coverage

--- test/jmri/jmrit/symbolicprog/tabbedframe/PaneProgFrameTest.java
+++ test/jmri/jmrit/symbolicprog/tabbedframe/PaneProgFrameTest.java
@@ -24,66 +24,68 @@ import jmri.jmrit.roster.*;
  * Test PaneProgFrame
  *
  * @author			Bob Jacobsen
- * @version			$Revision: 1.4 $
+ * @version			$Revision: 1.5 $
  */
 public class PaneProgFrameTest extends TestCase {
-    
+
     // test creating a pane in config file
     public void testPane() {
         setupDoc();
-        
-        // create test object with speciai implementation of the newPane(String) operation
+
+        // create test object
         result = null;
-        
-        PaneProgFrame p = new PaneProgFrame(null, null,
-                                            "test frame", "no programmer", new jmri.progdebugger.ProgDebugger()) {
+
+        PaneProgFrame p = new PaneProgFrame(null, new RosterEntry(),
+                                            "test frame", "programmers/Basic.xml",
+                                            new jmri.progdebugger.ProgDebugger()) {
                 // dummy implementations
                 JPanel getModePane() { return null; }
             };
-        
+
         // invoke
         result = null;
         p.readConfig(root, new RosterEntry());
-        Assert.assertEquals("paneList length ", 3, p.paneList.size());
+        Assert.assertEquals("paneList length ", 4, p.paneList.size());
+        // three panes in root, plus roster entry pane
     }
-    
-    
+
+
     // show me the specially-created frame
     public void testFrame() {
         setupDoc();
-        PaneProgFrame p = new PaneProgFrame(null, null,
-                                            "test frame", "no programmer", new jmri.progdebugger.ProgDebugger()) {
+        PaneProgFrame p = new PaneProgFrame(null, new RosterEntry(),
+                                            "test frame", "programmers/Basic.xml", new jmri.progdebugger.ProgDebugger()) {
                 // dummy implementations
                 JPanel getModePane() { return null; }
             };
-        
+
         // ugly, temporary way to load the decoder info
         jmri.jmrit.decoderdefn.DecoderFileTest t = new jmri.jmrit.decoderdefn.DecoderFileTest("");
         t.setupDecoder();
         DecoderFile df = new DecoderFile();  // used as a temporary
         df.loadVariableModel(t.decoder, p.variableModel);
-        
+
         p.readConfig(root, new RosterEntry());
         p.pack();
         p.show();
     }
-    
+
     // static variables for internal classes to report their interpretations
     static String result = null;
     static int colCount = -1;
     static int varCount = -1;
-    
+
     // static variables for the test XML structures
     Element root = null;
     Document doc = null;
-    
+
     // provide a test document in the above static variables
     void setupDoc() {
         // create a JDOM tree with just some elements
         root = new Element("programmer-config");
         doc = new Document(root);
         doc.setDocType(new DocType("programmer-config","programmer-config.dtd"));
-        
+
         // add some elements
         root.addContent(new Element("programmer")
             .addContent(new Element("pane")
@@ -135,28 +137,28 @@ public class PaneProgFrameTest extends TestCase {
                 )
             )
             ; // end of adding contents
-        
+
         return;
     }
-    
+
     // from here down is testing infrastructure
-    
+
     public PaneProgFrameTest(String s) {
         super(s);
     }
-    
+
     // Main entry point
     static public void main(String[] args) {
         String[] testCaseName = {PaneProgFrameTest.class.getName()};
         junit.swingui.TestRunner.main(testCaseName);
     }
-    
+
     // test suite from all defined tests
     public static Test suite() {
         TestSuite suite = new TestSuite(PaneProgFrameTest.class);
         return suite;
     }
-    
+
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(PaneProgFrameTest.class.getName());
-    
+
 }
