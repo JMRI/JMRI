ec090313b6922864c1df26dea52e4e460d9e25af
Bob Jacobsen <jacobsen@mac.com>
2002-09-26 14:46:29 +0000

    cleanup

--- jmri/jmrit/simpleprog/SimpleProgAction.java
+++ jmri/jmrit/simpleprog/SimpleProgAction.java
@@ -10,21 +10,21 @@ import java.awt.event.ActionEvent;
  *       			SimpleProgAction object
  *
  * @author			Bob Jacobsen    Copyright (C) 2001
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  */public class SimpleProgAction 			extends AbstractAction {
 
-	public SimpleProgAction(String s) {
-		super(s);
-	}
-
-    public void actionPerformed(ActionEvent e) {
-
-		// create a SimpleProgFrame
-		SimpleProgFrame f = new SimpleProgFrame();
-		f.show();
-
-	}
-
-}
+     public SimpleProgAction(String s) {
+         super(s);
+     }
+     
+     public void actionPerformed(ActionEvent e) {
+         
+         // create a SimpleProgFrame
+         SimpleProgFrame f = new SimpleProgFrame();
+         f.show();
+         
+     }
+     
+ }
 
 /* @(#)SimpleProgAction.java */
