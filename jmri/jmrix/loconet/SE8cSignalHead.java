5590a7059e5dbe72c65bae756a047a18810b8a9f
Bob Jacobsen <jacobsen@mac.com>
2002-10-02 16:33:54 +0000

    functionign

--- jmri/jmrix/loconet/SE8cSignalHead.java
+++ jmri/jmrix/loconet/SE8cSignalHead.java
@@ -7,63 +7,103 @@ import jmri.*;
 /**
  * Extend jmri.SignalHead for signals implemented by an SE8c
  * <P>
- * This implementation implements the "SENT" feedback,
- * where LocoNet messages originating on the layout can change both KnownState
- * and CommandedState.  We change both because we consider a LocoNet message to
- * reflect how the turnout should be, even if its a readback status message.
- * E.g. if you use a DS54 local input to change the state, resulting in a
- * status message, we still consider that to be a commanded state change.
+ * This implementation writes out to the physical signal when
+ * it's commanded to change appearance, and updates it's internal state
+ * when it hears commands from other places.
  *
  * @author			Bob Jacobsen Copyright (C) 2002
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  */public class SE8cSignalHead extends AbstractSignalHead implements LocoNetListener {
 
 
-     public SE8cSignalHead(int number) {
-         _number = number;
+     public SE8cSignalHead(int pNumber) {
+         mNumber = pNumber;
+         mAppearance = RED;  // start turned off
          // At construction, register for messages
          LnTrafficController.instance().addLocoNetListener(~0, this);
      }
-     
-     public int getNumber() { return _number; }
+
+     public int getAppearance() { return mAppearance; }
+    public void setAppearance(int newAppearance) {
+        int oldAppearance = mAppearance;
+        mAppearance = newAppearance;
+        if (oldAppearance != newAppearance) forwardCommandChangeToLayout(mAppearance);
+    }
+
+     public int getNumber() { return mNumber; }
      public String getSystemName() { return "LH"+getNumber(); }
-     
+
      // Handle a request to change state by sending a LocoNet command
-     protected void forwardCommandChangeToLayout(int s) throws jmri.JmriException {
+     protected void forwardCommandChangeToLayout(int s)  {
          // send SWREQ for close
          LocoNetMessage l = new LocoNetMessage(4);
          l.setOpCode(LnConstants.OPC_SW_REQ);
-         
+
+         int address = 0;
+         boolean closed = false;
+         // which of the four states?
+         switch (s) {
+            case RED:
+                address = mNumber;
+                closed = false;
+                break;
+            case YELLOW:
+                address = mNumber+1;
+                closed = false;
+                break;
+            case FLASHYELLOW:
+                address = mNumber+1;
+                closed = true;
+                break;
+            case GREEN:
+                address = mNumber;
+                closed = true;
+                break;
+            default:
+                log.error("Invalid state request: "+s);
+                return;
+         }
+
          // compute address fields
-         int hiadr = (_number-1)/128;
-         int loadr = (_number-1)-hiadr*128;
-         
+         int hiadr = (address-1)/128;
+         int loadr = (address-1)-hiadr*128;
+         if (closed) hiadr |= 0x20;
+
          // store and send
          l.setElement(1,loadr);
          l.setElement(2,hiadr);
          LnTrafficController.instance().sendLocoNetMessage(l);
      }
-     
+
      // implementing classes will typically have a function/listener to get
      // updates from the layout, which will then call
      //		public void firePropertyChange(String propertyName,
-     //										Object oldValue,
-     //										Object newValue)
+     //						Object oldValue,
+     //						Object newValue)
      // _once_ if anything has changed state (or set the commanded state directly)
      public void message(LocoNetMessage l) {
+        int oldAppearance = mAppearance;
          // parse message type
          switch (l.getOpCode()) {
          case LnConstants.OPC_SW_REQ: {               /* page 9 of Loconet PE */
              int sw1 = l.getElement(1);
              int sw2 = l.getElement(2);
              if (myAddress(sw1, sw2)) {
-                 if (log.isDebugEnabled()) log.debug("SW_REQ received with valid address");
                  if ((sw2 & LnConstants.OPC_SW_REQ_DIR)!=0) {
-                     newCommandedState(CLOSED);
-                     newKnownState(CLOSED);
+                     // was set CLOSED
+                     mAppearance = GREEN;
                  } else {
-                     newCommandedState(THROWN);
-                     newKnownState(THROWN);
+                     // was set THROWN
+                     mAppearance = RED;
+                 }
+             }
+             if (myAddressPlusOne(sw1, sw2)) {
+                 if ((sw2 & LnConstants.OPC_SW_REQ_DIR)!=0) {
+                     // was set CLOSED
+                     mAppearance = FLASHYELLOW;
+                 } else {
+                     // was set THROWN
+                     mAppearance = YELLOW;
                  }
              }
              break;
@@ -72,52 +112,61 @@ import jmri.*;
              int sw1 = l.getElement(1);
              int sw2 = l.getElement(2);
              if (myAddress(sw1, sw2)) {
-                 if (log.isDebugEnabled()) log.debug("SW_REP received with valid address");
                  // see if its a turnout state report
                  if ((sw2 & LnConstants.OPC_SW_REP_INPUTS)==0) {
-    	        		// sort out states
-                     switch (sw2 &
-                             (LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN)) {
-                         
-                     case LnConstants.OPC_SW_REP_CLOSED:
-                         newCommandedState(CLOSED);
-                         newKnownState(CLOSED);
-                         break;
-                     case LnConstants.OPC_SW_REP_THROWN:
-                         newCommandedState(THROWN);
-                         newKnownState(THROWN);
-                         break;
-                     case LnConstants.OPC_SW_REP_CLOSED|LnConstants.OPC_SW_REP_THROWN:
-                         newCommandedState(CLOSED+THROWN);
-                         newKnownState(CLOSED+THROWN);
-                         break;
-                     default:
-                         newCommandedState(0);
-                         newKnownState(0);
-                         break;
-                     }
+                    // sort out states
+                    if ((sw2 & LnConstants.OPC_SW_REP_CLOSED) != 0) {
+                        // was set CLOSED
+                        mAppearance = GREEN;
+                    }
+                    if ((sw2 & LnConstants.OPC_SW_REP_THROWN) != 0) {
+                        // was set THROWN
+                        mAppearance = RED;
+                    }
+                 }
+             }
+             if (myAddressPlusOne(sw1, sw2)) {
+                 // see if its a turnout state report
+                 if ((sw2 & LnConstants.OPC_SW_REP_INPUTS)==0) {
+                    // sort out states
+                    if ((sw2 & LnConstants.OPC_SW_REP_CLOSED) != 0) {
+                        // was set CLOSED
+                        mAppearance = FLASHYELLOW;
+                    }
+                    if ((sw2 & LnConstants.OPC_SW_REP_THROWN) != 0) {
+                        // was set THROWN
+                        mAppearance = YELLOW;
+                    }
                  }
              }
          }
          default:
              return;
          }
-         // reach here only in error
+         // reach here if the state has updated
+         if (oldAppearance != mAppearance) {
+            firePropertyChange("Appearance", new Integer(oldAppearance), new Integer(mAppearance));
+         }
      }
-     
+
      public void dispose() {
          LnTrafficController.instance().removeLocoNetListener(~0, this);
      }
-     
+
      // data members
-     int _number;   // loconet turnout number
-     
+     int mNumber;   // loconet turnout number with lower address (0 based)
+     int mAppearance;   // current value of appearance variable
+
      private boolean myAddress(int a1, int a2) {
          // the "+ 1" in the following converts to throttle-visible numbering
-         return (((a2 & 0x0f) * 128) + (a1 & 0x7f) + 1) == _number;
+         return (((a2 & 0x0f) * 128) + (a1 & 0x7f) + 1) == mNumber;
+     }
+     private boolean myAddressPlusOne(int a1, int a2) {
+         // the "+ 1" in the following converts to throttle-visible numbering
+         return (((a2 & 0x0f) * 128) + (a1 & 0x7f) + 1) == mNumber+1;
      }
      static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(SE8cSignalHead.class.getName());
-     
+
  }
 
 
