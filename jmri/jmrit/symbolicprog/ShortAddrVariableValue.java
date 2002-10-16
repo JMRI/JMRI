512b8cd561ca65f6efa42ce935072ac9dc4a450f
Bob Jacobsen <jacobsen@mac.com>
2002-10-16 13:35:51 +0000

    new class, not yet tested

---
+++ jmri/jmrit/symbolicprog/ShortAddrVariableValue.java
@@ -0,0 +1,119 @@
+// ShortAddrVariableValue.java
+
+package jmri.jmrit.symbolicprog;
+
+import jmri.Programmer;
+import jmri.InstanceManager;
+import jmri.ProgListener;
+
+import java.awt.Component;
+import java.awt.event.*;
+import java.beans.PropertyChangeListener;
+import java.util.Vector;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.text.*;
+import com.sun.java.util.collections.ArrayList;
+
+/**
+ * Representation of a short address (CV1).
+ * <P>
+ * This is a decimal value, extended to modify the other CVs when
+ * written.
+ *
+ * @author			Bob Jacobsen   Copyright (C) 2001
+ * @version             $Revision: 1.1 $
+ *
+ */
+public class ShortAddrVariableValue extends DecVariableValue {
+
+    public ShortAddrVariableValue(String name, String comment, boolean readOnly,
+                            int cvNum, String mask, int minVal, int maxVal,
+                            Vector v, JLabel status, String stdname) {
+        super(name, comment, readOnly, cvNum, mask, minVal, maxVal, v, status, stdname);
+    }
+
+
+    /**
+     * Invoked when a permanent change to the JTextField has been
+     * made.  Note that this does _not_ notify property listeners;
+     * that should be done by the invoker, who may or may not
+     * know what the old value was. Can be overwridden in subclasses
+     * that want to display the value differently.
+     */
+    void updatedTextField() {
+        if (log.isDebugEnabled()) log.debug("updatedTextField");
+        // called for new values - set the CV as needed
+        CvValue cv = (CvValue)_cvVector.elementAt(getCvNum());
+        // compute new cv value by combining old and request
+        int oldCv = cv.getValue();
+        int newVal;
+        try {
+            newVal = Integer.valueOf(_value.getText()).intValue();
+        }
+        catch (java.lang.NumberFormatException ex) { newVal = 0; }
+        int newCv = newValue(oldCv, newVal, getMask());
+		if (oldCv != newCv)
+                    cv.setValue(newCv);
+    }
+
+    public void setIntValue(int i) {
+        setValue(i);
+    }
+
+
+    /**
+     * Set a new value, including notification as needed.  This does the
+     * conversion from string to int, so if the place where formatting
+     * needs to be applied
+     */
+    public void setValue(int value) {
+        int oldVal;
+        try {
+            oldVal = Integer.valueOf(_value.getText()).intValue();
+        } catch (java.lang.NumberFormatException ex) { oldVal = -999; }
+        if (log.isDebugEnabled()) log.debug("setValue with new value "+value+" old value "+oldVal);
+        if (oldVal != value) {
+            _value.setText(""+value);
+            updatedTextField();
+            prop.firePropertyChange("Value", new Integer(oldVal), new Integer(value));
+        }
+    }
+
+    public void write() {
+        if (getReadOnly()) log.error("unexpected write operation when readOnly is set");
+        setBusy(true);  // will be reset when value changes
+        //super.setState(STORED);
+        ((CvValue)_cvVector.elementAt(getCvNum())).write(_status);
+    }
+
+    // handle incoming parameter notification
+    public void propertyChange(java.beans.PropertyChangeEvent e) {
+        // notification from CV; check for Value being changed
+        if (log.isDebugEnabled()) log.debug("Property changed: "+e.getPropertyName());
+        if (e.getPropertyName().equals("Busy")) {
+            if (((Boolean)e.getNewValue()).equals(Boolean.FALSE)) setBusy(false);
+        }
+        else if (e.getPropertyName().equals("State")) {
+            CvValue cv = (CvValue)_cvVector.elementAt(getCvNum());
+            setState(cv.getState());
+        }
+        else if (e.getPropertyName().equals("Value")) {
+            //setBusy(false);
+            // update value of Variable
+            CvValue cv = (CvValue)_cvVector.elementAt(getCvNum());
+            int newVal = (cv.getValue() & maskVal(getMask())) >>> offsetVal(getMask());
+            setValue(newVal);  // check for duplicate done inside setVal
+        }
+    }
+
+    // clean up connections when done
+    public void dispose() {
+        super.dispose();
+    }
+
+    // initialize logging
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(ShortAddrVariableValue.class.getName());
+
+}
