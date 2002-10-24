74c35f2c7ee3882dce27086dddc572c9b47c890c
Bob Jacobsen <jacobsen@mac.com>
2002-10-24 16:52:47 +0000

    cleanup

--- jmri/jmrit/symbolicprog/DccAddressPanel.java
+++ jmri/jmrit/symbolicprog/DccAddressPanel.java
@@ -13,51 +13,51 @@ import com.sun.java.util.collections.List;
  * Provide a graphical representation of the DCC address, either long or short
  *
  * @author			Bob Jacobsen   Copyright (C) 2001
- * @version			$Id: DccAddressPanel.java,v 1.2 2002-10-24 16:52:17 jacobsen Exp $
+ * @version			$Revision: 1.3 $
  */
 public class DccAddressPanel extends JPanel {
-    
+
     JTextField val = new JTextField(6);
-    
+
     VariableValue primaryAddr = null;
     VariableValue extendAddr = null;
     EnumVariableValue addMode = null;
-    
+
     VariableTableModel variableModel = null;
-    
+
     public DccAddressPanel(VariableTableModel mod) {
         variableModel = mod;
-        
+
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        
+
         add(new JLabel("DCC Address: "));
         val.setToolTipText("This field shows the DCC address currently in use. CV1 provides the short address; CV17 & 18 provide the long address");
         add(val);
-        
+
         // arrange for the field to be updated when any of the variables change
         java.beans.PropertyChangeListener dccNews = new java.beans.PropertyChangeListener() {
                 public void propertyChange(java.beans.PropertyChangeEvent e) { updateDccAddress(); }
             };
-        
+
         // connect to variables
         primaryAddr = variableModel.findVar("Short Address");
         if (primaryAddr==null) log.debug("DCC Address monitor did not find a Short Address variable");
         else primaryAddr.addPropertyChangeListener(dccNews);
-        
+
         extendAddr = variableModel.findVar("Long Address");
         if (extendAddr==null) log.debug("DCC Address monitor did not find an Long Address variable");
         else extendAddr.addPropertyChangeListener(dccNews);
-        
+
         addMode = (EnumVariableValue)variableModel.findVar("Address Format");
         if (addMode==null) log.debug("DCC Address monitor didnt find an Address Format variable");
         else addMode.addPropertyChangeListener(dccNews);
-        
+
         // show the selection
         if (addMode != null) {
             add(new JLabel("  Extended Addressing: "));
             add(addMode.getRep("checkbox"));
         }
-        
+
         // update initial contents & color
         if (addMode == null || extendAddr == null || addMode.getIntValue()==0) {
             if (primaryAddr!=null) {
@@ -70,7 +70,7 @@ public class DccAddressPanel extends JPanel {
             val.setBackground(extendAddr.getValue().getBackground());
             val.setDocument( ((JTextField)extendAddr.getValue()).getDocument());
         }
-        
+
         // start listening for changes to this value
         val.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -100,11 +100,11 @@ public class DccAddressPanel extends JPanel {
                     exitField();
                 }
             });
-        
+
     }
-    
+
     String oldContents = "";
-    
+
     void enterField() {
         oldContents = val.getText();
     }
@@ -126,7 +126,7 @@ public class DccAddressPanel extends JPanel {
             }
         }
     }
-    
+
     void updateDccAddress() {
         if (log.isDebugEnabled())
             log.debug("updateDccAddress: short "+(primaryAddr==null?"<null>":primaryAddr.getValueString())+
@@ -147,8 +147,8 @@ public class DccAddressPanel extends JPanel {
             if (log.isDebugEnabled()) log.debug("set color: "+extendAddr.getValue().getBackground());
         }
     }
-    
+
     // initialize logging
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(DccAddressPanel.class.getName());
-    
+
 }
