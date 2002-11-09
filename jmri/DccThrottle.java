53eaf6f28e95cb033c8b3caf57f0f6bbf41dc759
Glen Oberhauser <glenobie@users.sourceforge.net>
2002-11-09 19:00:00 +0000

    removed setDccAddress

--- jmri/DccThrottle.java
+++ jmri/DccThrottle.java
@@ -5,18 +5,17 @@ package jmri;
 /**
  * Description:		<describe the DccThrottle interface here>
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.4 $
+ * @version			$Revision: 1.5 $
  */
 public interface DccThrottle extends Throttle {
 
     // extends Throttle to allow asking about DCC items
 
     public int getDccAddress();
-    public void setDccAddress(int address);
 
     // to handle quantized speed. Note this can change! Valued returned is
     // always positive.
-    public float speedIncrement();
+    public float getSpeedIncrement();
 
     // information on consisting  (how do we set consisting?)
 
