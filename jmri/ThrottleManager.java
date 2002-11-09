20356fc838dae79e0c29a78204b06a52b2ae5fbe
Glen Oberhauser <glenobie@users.sourceforge.net>
2002-11-09 18:38:55 +0000

    added requestThrottle method

--- jmri/ThrottleManager.java
+++ jmri/ThrottleManager.java
@@ -3,9 +3,15 @@ package jmri;
 /**
  * Interface for controlling throttles
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  */
 public interface ThrottleManager
 {
+
+    /**
+     * Get a throttle, given a decoder address.
+     * @param address The decoder address desired.
+     */
+    public void requestThrottle(int address, ThrottleListener l);
 
 }
