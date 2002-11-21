b5d8379189c87bebccd5a81141115912b3e919d8
Glen Oberhauser <glenobie@users.sourceforge.net>
2002-11-21 14:50:51 +0000

    Minor code cleanup

--- jmri/jmrit/throttle/FunctionButton.java
+++ jmri/jmrit/throttle/FunctionButton.java
@@ -13,7 +13,7 @@ import java.awt.event.*;
  * have a right-click popup menu with several configuration options:
  * <ul>
  * <li> Set the text
- * <li> Set an icon
+ * <li> Set the locking state
  * <li>
  * </ul>
  */
@@ -65,20 +65,31 @@ public class FunctionButton extends JButton implements ActionListener
         this.isOn = isOn;
     }
 
+    /**
+     * Set the locking state of the button
+     * @param isLockable True if the a clicking and releasing the button
+     * changes the function state. False if the state is changed
+     * back when the button is released
+     */
     public void setIsLockable(boolean isLockable)
     {
         this.isLockable = isLockable;
         changeLockStateItem.setSelected(isLockable);
     }
 
-
+    /**
+     * Get the locking state of the function
+     * @return True if the a clicking and releasing the button
+     * changes the function state. False if the state is changed
+     * back when the button is released
+     */
     public boolean getIsLockable()
     {
         return isLockable;
     }
 
     /**
-     * Button is clicked
+     * Handle the selection from the popup menu.
      * @param e The ActionEvent causing the action.
      */
     public void actionPerformed(ActionEvent e)
@@ -97,6 +108,10 @@ public class FunctionButton extends JButton implements ActionListener
         }
     }
 
+    /**
+     * Change the state of the function.
+     * @parem newState The new state. True = Is on, False = Is off.
+     */
     private void changeState(boolean newState)
     {
         isOn = newState;
@@ -107,6 +122,10 @@ public class FunctionButton extends JButton implements ActionListener
 
     }
 
+    /**
+     * Present a dialog to allow the user to change the text
+     * of the button.
+     */
     private void editButtonText()
     {
         Object input = JOptionPane.showInputDialog(this, "Enter text for this button",
@@ -128,37 +147,62 @@ public class FunctionButton extends JButton implements ActionListener
         this.listener = l;
     }
 
-
+    /**
+     * A PopupListener to handle mouse clicks and releases. Handles
+     * the popup menu.
+     */
     class PopupListener extends MouseAdapter
     {
+        /**
+         * If the event is the popup trigger, which is dependent on
+         * the platform, present the popup menu. Otherwise change
+         * the state of the function depending on the locking state
+         * of the button.
+         * @param e The MouseEvent causing the action.
+         */
         public void mousePressed(MouseEvent e)
         {
+            JButton button = (JButton)e.getSource();
             if (e.isPopupTrigger())
             {
                 popup.show(e.getComponent(),
                            e.getX(), e.getY());
             }
-            else if (((e.getModifiers() & e.BUTTON1_MASK) > 0)
+            /* Must check button mask since some platforms wait
+            for mouse release to do popup. */
+            else if (button.isEnabled() &&
+                    ((e.getModifiers() & e.BUTTON1_MASK) > 0)
                      && !getIsLockable())
             {
                 changeState(true);
             }
         }
 
+        /**
+         * If the event is the popup trigger, which is dependent on
+         * the platform, present the popup menu. Otherwise change
+         * the state of the function depending on the locking state
+         * of the button.
+         * @param e The MouseEvent causing the action.
+         */
         public void mouseReleased(MouseEvent e)
         {
+            JButton button = (JButton)e.getSource();
             if (e.isPopupTrigger())
             {
                 popup.show(e.getComponent(),
                            e.getX(), e.getY());
             }
-            else if (!getIsLockable())
+            else if (button.isEnabled())
             {
-                changeState(false);
-            }
-            else
-            {
-                changeState(!isOn);
+                if (!getIsLockable())
+                {
+                    changeState(false);
+                }
+                else
+                {
+                    changeState(!isOn);
+                }
             }
         }
 
