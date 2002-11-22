233370cc62ce500419d2562e8d9272ac2cb7481c
Glen Oberhauser <glenobie@users.sourceforge.net>
2002-11-22 17:47:08 +0000

    minor code cleanup

--- jmri/jmrit/throttle/FunctionListener.java
+++ jmri/jmrit/throttle/FunctionListener.java
@@ -1,7 +1,14 @@
 package jmri.jmrit.throttle;
 
-
+/**
+ * A Listener for a function state change.
+ */
 public interface FunctionListener extends java.util.EventListener
 {
+    /**
+     * Receive notification that a function has changed state.
+     * @param functionNumber The id of the function
+     * @param isOn True if the function is activated, false otherwise.
+     */
     public void notifyFunctionStateChanged(int functionNumber, boolean isOn);
 }
