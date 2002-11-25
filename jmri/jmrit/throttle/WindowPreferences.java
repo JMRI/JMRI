8f07bb42639c3a61610a3953a92073771d426173
Glen Oberhauser <glenobie@users.sourceforge.net>
2002-11-25 18:57:26 +0000

    minor code clean-up

--- jmri/jmrit/throttle/WindowPreferences.java
+++ jmri/jmrit/throttle/WindowPreferences.java
@@ -3,8 +3,22 @@ package jmri.jmrit.throttle;
 import org.jdom.Element;
 import java.awt.Container;
 
+/**
+ * A helper class for getting and setting XML attributes of a Container.
+ */
 public class WindowPreferences
 {
+    /**
+     * Collect Container preferences.
+     * @param c The Container being XMLed.
+     * @return An Element containing the following prefs:
+     * <ul>
+     * <li> x location
+     * <li> y location
+     * <li> width
+     * <li> height
+     * </ul>
+     */
     public Element getPreferences(Container c)
     {
         Element window = new Element("window");
@@ -15,6 +29,17 @@ public class WindowPreferences
         return window;
     }
 
+    /**
+     * Set Container preferences from an XML Element.
+     * @param c The Container being set.
+     * @param e An Element containing the following prefs:
+     * <ul>
+     * <li> x location
+     * <li> y location
+     * <li> width
+     * <li> height
+     * </ul>
+     */
     public void setPreferences(Container c, Element e)
     {
         try
