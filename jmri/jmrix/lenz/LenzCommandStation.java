65f3ec9e580eb9e2abef70757fabf46fbab0928f
Bob Jacobsen <jacobsen@mac.com>
2003-01-12 22:55:24 +0000

    cleanup

--- jmri/jmrix/lenz/LenzCommandStation.java
+++ jmri/jmrix/lenz/LenzCommandStation.java
@@ -9,60 +9,60 @@ package jmri.jmrix.lenz;
  * Defines standard operations for Dcc command stations.
  *
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.5 $
+ * @version			$Revision: 1.6 $
  */
 public class LenzCommandStation implements jmri.jmrix.DccCommandStation {
-
-	/**
-	 * Lenz does use a service mode
-	 */
-	public boolean getHasServiceMode() {return true;}
-
-	/**
-	 * If this command station has a service mode, is the command
-	 * station currently in that mode?
-	 */
-	public boolean getInServiceMode() { return mInServiceMode; }
-
-	/**
-	 * Provides the version string returned during the initial check.
-	 * This function is not yet implemented...
-	 **/
-    public String getVersionString() { return "<unknown>"; }
-
+    
     /**
-	 * Remember whether or not in service mode
-	 **/
+     * Lenz does use a service mode
+     */
+    public boolean getHasServiceMode() {return true;}
+    
+    /**
+     * If this command station has a service mode, is the command
+     * station currently in that mode?
+     */
+    public boolean getInServiceMode() { return mInServiceMode; }
+    
+    /**
+     * Provides the version string returned during the initial check.
+     * This function is not yet implemented...
+     **/
+    public String getVersionString() { return "<unknown>"; }
+    
+    /**
+     * Remember whether or not in service mode
+     **/
     boolean mInServiceMode = false;
-
+    
     /**
      * Generate a message to change turnout state
      */
     public XNetMessage getTurnoutCommandMsg(int pNumber, boolean pClose,
-                                    boolean pThrow, boolean pOn) {
-		XNetMessage l = new XNetMessage(4);
-		l.setElement(0,0x52);
-
-		// compute address byte fields
-		int hiadr = (pNumber-1)/4;
-		int loadr = ((pNumber-1)-hiadr*4)*2;
-
-		// load On/Off with on
+                                            boolean pThrow, boolean pOn) {
+        XNetMessage l = new XNetMessage(4);
+        l.setElement(0,0x52);
+        
+        // compute address byte fields
+        int hiadr = (pNumber-1)/4;
+        int loadr = ((pNumber-1)-hiadr*4)*2;
+        
+        // load On/Off with on
         loadr |= 0x10;
-		if (!pOn) loadr |= 0x40;
-		if (pThrow) loadr |= 0x01;
-
+        if (!pOn) loadr |= 0x40;
+        if (pThrow) loadr |= 0x01;
+        
         // we don't know how to command both states right now!
         if (pClose & pThrow)
             log.error("XPressNet turnout logic can't handle both THROWN and CLOSED yet");
-
-		// store and send
-		l.setElement(1,hiadr);
-		l.setElement(2,loadr);
-
+        
+        // store and send
+        l.setElement(1,hiadr);
+        l.setElement(2,loadr);
+        
         return l;
     }
-
+    
     /**
      * If this is a turnout-type message, return address. Otherwise
      * return -1.
@@ -77,84 +77,84 @@ public class LenzCommandStation implements jmri.jmrix.DccCommandStation {
         }
         else return -1;
     }
-
+    
     /**
      * Is this a command to change turnout state?
      */
     public boolean isTurnoutCommand(XNetMessage pMsg) {
         return pMsg.getOpCode()==0x05;
     }
-
+    
     // start of programming messages
     public XNetMessage getServiceModeResultsMsg() {
-		XNetMessage m = new XNetMessage(3);
+        XNetMessage m = new XNetMessage(3);
         m.setElement(0, 0x21);
         m.setElement(1, 0x10);
-		return m;
-	}
-
-	public XNetMessage getExitProgModeMsg() {
-		XNetMessage m = new XNetMessage(3);
+        return m;
+    }
+    
+    public XNetMessage getExitProgModeMsg() {
+        XNetMessage m = new XNetMessage(3);
         m.setElement(0, 0x21);
         m.setElement(1, 0x81);
-		return m;
-	}
-
-	public XNetMessage getReadPagedCVMsg(int cv) {
-		XNetMessage m = new XNetMessage(4);
+        return m;
+    }
+    
+    public XNetMessage getReadPagedCVMsg(int cv) {
+        XNetMessage m = new XNetMessage(4);
         m.setElement(0, 0x22);
         m.setElement(1, 0x14);
         m.setElement(2, cv);
-		return m;
-	}
-
-	public XNetMessage getReadDirectCVMsg(int cv) {
-		XNetMessage m = new XNetMessage(4);
+        return m;
+    }
+    
+    public XNetMessage getReadDirectCVMsg(int cv) {
+        XNetMessage m = new XNetMessage(4);
         m.setElement(0, 0x22);
         m.setElement(1, 0x15);
         m.setElement(2, cv);
-		return m;
-	}
-
-	public XNetMessage getWritePagedCVMsg(int cv, int val) {
-		XNetMessage m = new XNetMessage(5);
+        return m;
+    }
+    
+    public XNetMessage getWritePagedCVMsg(int cv, int val) {
+        XNetMessage m = new XNetMessage(5);
         m.setElement(0, 0x23);
         m.setElement(1, 0x17);
         m.setElement(2, cv);
         m.setElement(3, val);
-		return m;
-	}
-
-	public XNetMessage getWriteDirectCVMsg(int cv, int val) {
-		XNetMessage m = new XNetMessage(5);
+        return m;
+    }
+    
+    public XNetMessage getWriteDirectCVMsg(int cv, int val) {
+        XNetMessage m = new XNetMessage(5);
         m.setElement(0, 0x23);
         m.setElement(1, 0x16);
         m.setElement(2, cv);
         m.setElement(3, val);
-		return m;
-	}
-
-	public XNetMessage getReadRegisterMsg(int reg) {
-		if (reg>8) log.error("register number too large: "+reg);
-		XNetMessage m = new XNetMessage(4);
+        return m;
+    }
+    
+    public XNetMessage getReadRegisterMsg(int reg) {
+        if (reg>8) log.error("register number too large: "+reg);
+        XNetMessage m = new XNetMessage(4);
         m.setElement(0, 0x22);
         m.setElement(1, 0x11);
         m.setElement(2, reg);
-		return m;
-	}
-
-	public XNetMessage getWriteRegisterMsg(int reg, int val) {
-		if (reg>8) log.error("register number too large: "+reg);
-		XNetMessage m = new XNetMessage(5);
+        return m;
+    }
+    
+    public XNetMessage getWriteRegisterMsg(int reg, int val) {
+        if (reg>8) log.error("register number too large: "+reg);
+        XNetMessage m = new XNetMessage(5);
         m.setElement(0, 0x23);
         m.setElement(1, 0x12);
         m.setElement(2, reg);
         m.setElement(3, val);
-		return m;
+        return m;
     }
-
+    
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LenzCommandStation.class.getName());
-
+    
 }
 
 
--- jmri/jmrix/lenz/XNetTurnout.java
+++ jmri/jmrix/lenz/XNetTurnout.java
@@ -3,7 +3,7 @@
  *
  * Description:		extend jmri.AbstractTurnout for XNet layouts
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.3 $
+ * @version			$Revision: 1.4 $
  */
 
 package jmri.jmrix.lenz;
@@ -11,53 +11,53 @@ package jmri.jmrix.lenz;
 import jmri.AbstractTurnout;
 
 public class XNetTurnout extends AbstractTurnout implements XNetListener {
-
-	public XNetTurnout(int pNumber) {  // a human-readable turnout number must be specified!
-		mNumber = pNumber;
-		// At construction, register for messages
-		XNetTrafficController.instance().addXNetListener(~0, this);
-	}
-
-	public int getNumber() { return mNumber; }
-	public String getSystemName() { return "XT"+getNumber(); }
-
-	// Handle a request to change state by sending a LocoNet command
-	protected void forwardCommandChangeToLayout(int s) throws jmri.JmriException {
+    
+    public XNetTurnout(int pNumber) {  // a human-readable turnout number must be specified!
+        mNumber = pNumber;
+        // At construction, register for messages
+        XNetTrafficController.instance().addXNetListener(~0, this);
+    }
+    
+    public int getNumber() { return mNumber; }
+    public String getSystemName() { return "XT"+getNumber(); }
+    
+    // Handle a request to change state by sending a LocoNet command
+    protected void forwardCommandChangeToLayout(int s) {
         // find the command station
         LenzCommandStation cs = XNetTrafficController.instance().getCommandStation();
         // get the right packet
         XNetMessage msg = cs.getTurnoutCommandMsg(mNumber,
-                                                    (s & CLOSED)!=0,
-                                                    (s & THROWN)!=0,
-                                                    true );
-
-		XNetTrafficController.instance().sendXNetMessage(msg, this);
-	}
-
-	// implementing classes will typically have a function/listener to get
-	// updates from the layout, which will then call
-	//		public void firePropertyChange(String propertyName,
-	//										Object oldValue,
-	//										Object newValue)
-	// _once_ if anything has changed state (or set the commanded state directly)
-	public void message(XNetMessage l) {
+                                                  (s & CLOSED)!=0,
+                                                  (s & THROWN)!=0,
+                                                  true );
+        
+        XNetTrafficController.instance().sendXNetMessage(msg, this);
+    }
+    
+    // implementing classes will typically have a function/listener to get
+    // updates from the layout, which will then call
+    //		public void firePropertyChange(String propertyName,
+    //										Object oldValue,
+    //										Object newValue)
+    // _once_ if anything has changed state (or set the commanded state directly)
+    public void message(XNetMessage l) {
         // check validity & addressing
         if (XNetTrafficController.instance()
-                    .getCommandStation()
-                    .getTurnoutMsgAddr(l) != mNumber) return;
-		// is for this object, parse message type
+            .getCommandStation()
+            .getTurnoutMsgAddr(l) != mNumber) return;
+        // is for this object, parse message type
         log.error("message function invoked, but not yet prepared");
-	}
-
-	public void dispose() {
-		XNetTrafficController.instance().removeXNetListener(~0, this);
-	}
-
-	// data members
-	int mNumber;   // loconet turnout number
-
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetTurnout.class.getName());
-
+    }
+    
+    public void dispose() {
+        XNetTrafficController.instance().removeXNetListener(~0, this);
+    }
+    
+    // data members
+    int mNumber;   // loconet turnout number
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetTurnout.class.getName());
+    
 }
 
 
