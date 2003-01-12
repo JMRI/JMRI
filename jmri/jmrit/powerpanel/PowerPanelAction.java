0958a993f05c82d4c80b4592868d2610bd01960e
Bob Jacobsen <jacobsen@mac.com>
2003-01-12 22:58:03 +0000

    cleanup

--- jmri/jmrit/powerpanel/PowerPanelAction.java
+++ jmri/jmrit/powerpanel/PowerPanelAction.java
@@ -1,30 +1,26 @@
-/**
- * PowerPanelAction.java
- *
- * Description:		Swing action to create and register a
- *       			PowerPanel object
- *
- * @author			Bob Jacobsen    Copyright (C) 2001
- * @version
- */
+// PowerPanelAction.java
 
 package jmri.jmrit.powerpanel;
 
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
-public class PowerPanelAction 			extends AbstractAction {
-
-	public PowerPanelAction(String s) { super(s);}
-
+/**
+ * Swing action to create and register a
+ * PowerPanelFrame object
+ *
+ * @author	    Bob Jacobsen    Copyright (C) 2001
+ * @version         $Revision: 1.2 $
+ */
+public class PowerPanelAction extends AbstractAction {
+    
+    public PowerPanelAction(String s) { super(s);}
+    
     public void actionPerformed(ActionEvent e) {
-
-		// create a PowerPanelFrame
-		PowerPanelFrame f = new PowerPanelFrame();
-		f.show();
-
-	}
+        // create a PowerPanelFrame
+        PowerPanelFrame f = new PowerPanelFrame();
+        f.show();
+    }
 }
-
 
 /* @(#)PowerPanelAction.java */
--- jmri/jmrit/powerpanel/PowerPanelFrame.java
+++ jmri/jmrit/powerpanel/PowerPanelFrame.java
@@ -1,56 +1,52 @@
-/** 
- * PowerPanelFrame.java
- *
- * Description:		Frame for user input of LocoNet messages
- * @author			Bob Jacobsen   Copyright (C) 2001
- * @version			
- */
+// PowerPanelFrame.java
 
-
-package jmri.jmrit.powerpanel;
+ package jmri.jmrit.powerpanel;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/**
+ * Frame for user input of LocoNet messages
+ * @author		Bob Jacobsen   Copyright (C) 2001
+ * @version             $Revision: 1.2 $
+ */
 public class PowerPanelFrame extends javax.swing.JFrame {
 
-	// GUI member declarations
-	PowerPane pane	= new PowerPane();
-	
-	public PowerPanelFrame() {
+    // GUI member declarations
+    PowerPane pane	= new PowerPane();
 
-		// general GUI config
+    public PowerPanelFrame() {
 
-		// install items in GUI
-		getContentPane().add(pane);
-		pack();
-	}
+        // general GUI config
 
-	// handle resizing when first shown
-  	private boolean mShown = false;
-	public void addNotify() {
-		super.addNotify();
-		if (mShown)
-			return;			
-		// resize frame to account for menubar
-		JMenuBar jMenuBar = getJMenuBar();
-		if (jMenuBar != null) {
-			int jMenuBarHeight = jMenuBar.getPreferredSize().height;
-			Dimension dimension = getSize();
-			dimension.height += jMenuBarHeight;
-			setSize(dimension);
-		}
-		mShown = true;
-	}
+        // install items in GUI
+        getContentPane().add(pane);
+        pack();
+    }
 
-	// Close the window when the close box is clicked
-	void thisWindowClosing(java.awt.event.WindowEvent e) {
-		setVisible(false);
-		pane.dispose();
-		dispose();
+    // handle resizing when first shown
+    private boolean mShown = false;
+    public void addNotify() {
+        super.addNotify();
+        if (mShown)
+            return;
+        // resize frame to account for menubar
+        JMenuBar jMenuBar = getJMenuBar();
+        if (jMenuBar != null) {
+            int jMenuBarHeight = jMenuBar.getPreferredSize().height;
+            Dimension dimension = getSize();
+            dimension.height += jMenuBarHeight;
+            setSize(dimension);
+        }
+        mShown = true;
+    }
+
+    // Close the window when the close box is clicked
+    void thisWindowClosing(java.awt.event.WindowEvent e) {
+        setVisible(false);
+        pane.dispose();
+        dispose();
 	// and disconnect from the SlotManager
-	
-	}
-
+    }
 }
