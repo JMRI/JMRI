817dca29f363a462fe0b2948fd3ea43cdf426eff
Bob Jacobsen <jacobsen@mac.com>
2003-02-15 19:57:38 +0000

    cleanup

--- jmri/jmrix/loconet/LnTrafficController.java
+++ jmri/jmrix/loconet/LnTrafficController.java
@@ -11,68 +11,68 @@ import java.util.Vector;
  * method for locating the local implementation.
  *
  * @author			Bob Jacobsen  Copyright (C) 2001
- * @version 		$Id: LnTrafficController.java,v 1.2 2002-03-18 04:52:44 jacobsen Exp $
+ * @version 		$Id: LnTrafficController.java,v 1.3 2003-02-15 19:57:38 jacobsen Exp $
  *
  */
 public abstract class LnTrafficController implements LocoNetInterface {
-
+    
     /**
-	 * static function returning the LnTrafficController instance to use.
-	 * @return The registered LnTrafficController instance for general use,
-	 *         if need be creating one.
-	 */
-	static public LnTrafficController instance() {
-		return self;
-	}
-
-	static protected LnTrafficController self = null;
-
+     * static function returning the LnTrafficController instance to use.
+     * @return The registered LnTrafficController instance for general use,
+     *         if need be creating one.
+     */
+    static public LnTrafficController instance() {
+        return self;
+    }
+    
+    static protected LnTrafficController self = null;
+    
     // Abstract methods for the LocoNetInterface
     abstract public boolean status();
-
-	/**
-	 * Forward a preformatted LocoNetMessage to the actual interface.
+    
+    /**
+     * Forward a preformatted LocoNetMessage to the actual interface.
      * @param m Message to send; will be updated with CRC
-	 */
-	abstract public void sendLocoNetMessage(LocoNetMessage m);
-
+     */
+    abstract public void sendLocoNetMessage(LocoNetMessage m);
+    
     // The methods to implement adding and removing listeners
-	protected Vector listeners = new Vector();
-
-	public synchronized void addLocoNetListener(int mask, LocoNetListener l) {
+    protected Vector listeners = new Vector();
+    
+    public synchronized void addLocoNetListener(int mask, LocoNetListener l) {
         // add only if not already registered
         if (l == null) throw new java.lang.NullPointerException();
         if (!listeners.contains(l)) {
-    		listeners.addElement(l);
-		}
+            listeners.addElement(l);
+        }
     }
-
-	public synchronized void removeLocoNetListener(int mask, LocoNetListener l) {
+    
+    public synchronized void removeLocoNetListener(int mask, LocoNetListener l) {
     	if (listeners.contains(l)) {
-    		listeners.removeElement(l);
+            listeners.removeElement(l);
     	}
     }
-
-	/**
-	 * Forward a LocoNetMessage to all registered listeners.
+    
+    /**
+     * Forward a LocoNetMessage to all registered listeners.
      * @param m Message to forward. Listeners should not modify it!
-	 */
-	protected void notify(LocoNetMessage m) {
-		// make a copy of the listener vector to synchronized not needed for transmit
-		Vector v;
-		synchronized(this) {
-			v = (Vector) listeners.clone();
-		}
-		if (log.isDebugEnabled()) log.debug("notify of incoming LocoNet packet: "+m.toString());
-		// forward to all listeners
-		int cnt = v.size();
-		for (int i=0; i < cnt; i++) {
-			LocoNetListener client = (LocoNetListener) listeners.elementAt(i);
-			client.message(m);
-		}
-	}
-
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnTrafficController.class.getName());
+     */
+    protected void notify(LocoNetMessage m) {
+        // make a copy of the listener vector to synchronized not needed for transmit
+        Vector v;
+        synchronized(this) {
+            v = (Vector) listeners.clone();
+        }
+        if (log.isDebugEnabled()) log.debug("notify of incoming LocoNet packet: "+m.toString());
+        // forward to all listeners
+        int cnt = v.size();
+        for (int i=0; i < cnt; i++) {
+            LocoNetListener client = (LocoNetListener) listeners.elementAt(i);
+            client.message(m);
+        }
+    }
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnTrafficController.class.getName());
 }
 
 
