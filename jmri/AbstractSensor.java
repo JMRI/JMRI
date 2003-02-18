fc237362d10fce0d9be56aea05dcfd362d793500
Bob Jacobsen <jacobsen@mac.com>
2003-02-18 00:16:19 +0000

    move setOwnState from LnSensor

--- jmri/AbstractSensor.java
+++ jmri/AbstractSensor.java
@@ -6,26 +6,26 @@ import jmri.Sensor;
 /**
  * Abstract class providing the basic logic of the Sensor interface
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version         $Revision: 1.6 $
+ * @version         $Revision: 1.7 $
  */
 public abstract class AbstractSensor implements Sensor, java.io.Serializable {
-    
+
     // ctor takes a system-name string for initialization
     public AbstractSensor(String s) { _id = s; }
-    
+
     // implementing classes will typically have a function/listener to get
     // updates from the layout, which will then call
     //		public void firePropertyChange(String propertyName,
     //					       Object oldValue,
     //					       Object newValue)
     // _once_ if anything has changed state
-    
+
     // interface function implementations
-    
+
     public String getID() {return _id;}
-    
+
     public int getKnownState() {return _knownState;}
-    
+
     // setKnownState() for implementations that can't
     // actually do it on the layout. Not intended for use by implementations
     // that can
@@ -36,11 +36,22 @@ public abstract class AbstractSensor implements Sensor, java.io.Serializable {
             firePropertyChange("KnownState", new Integer(oldState), new Integer(_knownState));
         }
     }
-    
+
+    /**
+     * Set out internal state information, and notify bean listeners.
+     */
+    public void setOwnState(int s) {
+        if (_knownState != s) {
+            int oldState = _knownState;
+            _knownState = s;
+            firePropertyChange("KnownState", new Integer(oldState), new Integer(_knownState));
+        }
+    }
+
     // internal data members
     private String _id;
     protected int _knownState     = UNKNOWN;
-    
+
     // since we can't do a "super(this)" in the ctor to inherit from PropertyChangeSupport, we'll
     // reflect to it
     java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
@@ -51,7 +62,7 @@ public abstract class AbstractSensor implements Sensor, java.io.Serializable {
     public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
         pcs.removePropertyChangeListener(l);
     }
-    
+
 }
 
 /* @(#)AbstractSensor.java */
