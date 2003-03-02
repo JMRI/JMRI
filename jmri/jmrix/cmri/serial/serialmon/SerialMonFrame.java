34acccfc2af4473f0d0969f8ddcb5bd85c7b1351
Bob Jacobsen <jacobsen@mac.com>
2003-03-02 22:22:47 +0000

    cleanup

--- jmri/jmrix/cmri/serial/serialmon/SerialMonFrame.java
+++ jmri/jmrix/cmri/serial/serialmon/SerialMonFrame.java
@@ -1,6 +1,5 @@
 // SerialMonFrame.java
 
-
 package jmri.jmrix.cmri.serial.serialmon;
 
 import java.awt.*;
@@ -20,27 +19,27 @@ import jmri.jmrix.cmri.serial.SerialReply;
 /**
  * Frame displaying (and logging) CMRI serial command messages
  * @author	    Bob Jacobsen   Copyright (C) 2001
- * @version         $Revision: 1.3 $
+ * @version         $Revision: 1.4 $
  */
 
 public class SerialMonFrame extends jmri.jmrix.AbstractMonFrame implements SerialListener {
-
-	public SerialMonFrame() {
-		super();
+    
+    public SerialMonFrame() {
+        super();
+    }
+    
+    protected String title() { return "CMRI Serial Command Monitor"; }
+    
+    protected void init() {
+        // connect to TrafficController
+        SerialTrafficController.instance().addSerialListener(this);
+    }
+    
+    public void dispose() {
+        SerialTrafficController.instance().removeSerialListener(this);
 	}
-
-	protected String title() { return "CMRI Serial Command Monitor"; }
-
-	protected void init() {
-		// connect to TrafficController
-		SerialTrafficController.instance().addSerialListener(this);
-	}
-
-	public void dispose() {
-		SerialTrafficController.instance().removeSerialListener(this);
-	}
-
-	public synchronized void message(SerialMessage l) {  // receive a message and log it
+    
+    public synchronized void message(SerialMessage l) {  // receive a message and log it
         if (l.isPoll()) {
             nextLine("Poll ua="+l.getUA()+"\n", l.toString()+"\n");
         } else if (l.isXmt()) {
@@ -50,17 +49,17 @@ public class SerialMonFrame extends jmri.jmrix.AbstractMonFrame implements Seria
             nextLine(s+"\n", l.toString()+"\n");
         } else if (l.isInit()) {
             String s = "Init ua="+l.getUA()
-                    +" type="+((char)l.getElement(2))
-                    +" DL="+(l.getElement(3)*256+l.getElement(4));
+                +" type="+((char)l.getElement(2))
+                +" DL="+(l.getElement(3)*256+l.getElement(4));
             s+=" NS="+l.getElement(5)+" CT: ";
             for (int i=6; i<l.getNumDataElements(); i++)
                 s+=Integer.toHexString(l.getElement(i))+" ";
             nextLine(s+"\n", l.toString()+"\n");
         } else
-    		nextLine("unrecognized cmd: \""+l.toString()+"\"\n", "");
-	}
-
-	public synchronized void reply(SerialReply l) {  // receive a reply message and log it
+            nextLine("unrecognized cmd: \""+l.toString()+"\"\n", "");
+    }
+    
+    public synchronized void reply(SerialReply l) {  // receive a reply message and log it
         if (l.isRcv()) {
             String s = "Receive ua="+l.getUA()+" IB=";
             for (int i=2; i<l.getNumDataElements(); i++)
@@ -68,8 +67,8 @@ public class SerialMonFrame extends jmri.jmrix.AbstractMonFrame implements Seria
             nextLine(s+"\n", l.toString()+"\n");
         } else
             nextLine("unrecognized rep: \""+l.toString()+"\"\n", "");
-	}
-
-   static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(SerialMonFrame.class.getName());
-
+    }
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(SerialMonFrame.class.getName());
+    
 }
