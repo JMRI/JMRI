469af640cbdcaa70ba74ad4a68cbf8a19cf5de33
Bob Jacobsen <jacobsen@mac.com>
2003-03-16 06:32:59 +0000

    minor comment cleanup

--- jmri/jmrix/loconet/locomon/LocoMonAction.java
+++ jmri/jmrix/loconet/locomon/LocoMonAction.java
@@ -1,11 +1,11 @@
-/** 
+/**
  * LocoMonAction.java
  *
- * Description:		Swing action to create and register a 
+ * Description:		Swing action to create and register a
  *       			LocoMonFrame object
  *
  * @author			Bob Jacobsen    Copyright (C) 2001
- * @version			
+ * @version
  */
 
 package jmri.jmrix.loconet.locomon;
@@ -19,9 +19,10 @@ import jmri.jmrix.loconet.LnTrafficController;
 public class LocoMonAction 			extends AbstractAction {
 
 	public LocoMonAction(String s) { super(s);}
-	
+
     public void actionPerformed(ActionEvent e) {
 		// create a LocoMonFrame
+                log.debug("starting LocoMon frame creation");
 		LocoMonFrame f = new LocoMonFrame();
 		try {
 			f.initComponents();
@@ -29,8 +30,8 @@ public class LocoMonAction 			extends AbstractAction {
 		catch (Exception ex) {
 			log.warn("LocoMonAction starting LocoMonFrame: Exception: "+ex.toString());
 			}
-		f.show();	
-		
+		f.show();
+
 	}
 
 	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LocoMonAction.class.getName());
