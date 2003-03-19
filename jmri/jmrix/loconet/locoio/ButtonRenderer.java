1b677551b68aaa174e68540fe893400dfeead1a7
Bob Jacobsen <jacobsen@mac.com>
2003-03-19 04:03:19 +0000

    cleanup

--- jmri/jmrix/loconet/locoio/ButtonRenderer.java
+++ jmri/jmrix/loconet/locoio/ButtonRenderer.java
@@ -11,72 +11,73 @@ import java.awt.*;
 
 // A holder for data and an associated icon
 public class ButtonRenderer extends JButton 
-							implements TableCellRenderer {
-	public ButtonRenderer() {
-		this.border = getBorder();
-		this.setOpaque(true);		
-	}
+    implements TableCellRenderer {
 
-	public void setForeground(Color foreground) {
-		this.foreground = foreground;
-		super.setForeground(foreground);
-	}
-
-	public void setBackground(Color background) {
-		this.background = background;
-		super.setBackground(background);
-	}
-
-	public void setFont(Font font) {
-		this.font = font;
-		super.setFont(font);
-	}
-
-	public Component getTableCellRendererComponent(JTable table,
-										Object value, boolean isSelected,
-										boolean hasFocus,
-										int row, int column) {
-		Color cellForeground = foreground != null ? foreground 
-											: table.getForeground();
-	    Color cellBackground = background != null ? background 
-											: table.getBackground();
-		
-		setFont(font != null ? font : table.getFont());
-
-		if (hasFocus) {
-			setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
-			if (table.isCellEditable(row, column)) {
-			cellForeground = UIManager.getColor("Table.focusCellForeground");
-			cellBackground = UIManager.getColor("Table.focusCellBackground");
-			}
-		} else {
-			setBorder(border);
-		}
-
-		super.setForeground(cellForeground);
-		super.setBackground(cellBackground);
-
-		// Customize the component's appearance
-		setValue(value);
-
-		return this;
-	}
-
-	protected void setValue(Object value) {
-		if (value == null) {
-			setText("");
-			setIcon(null);
-		} else if (value instanceof Icon) {
-			setText("");
-			setIcon((Icon)value);
-		} else {
-			setText(value.toString());
-			setIcon(null);
-		}		
-	}
-
-	protected Color foreground;
-	protected Color background;
-	protected Font font;
-	protected Border border;
+    public ButtonRenderer() {
+        this.border = getBorder();
+        this.setOpaque(true);		
+    }
+    
+    public void setForeground(Color foreground) {
+        this.foreground = foreground;
+        super.setForeground(foreground);
+    }
+    
+    public void setBackground(Color background) {
+        this.background = background;
+        super.setBackground(background);
+    }
+    
+    public void setFont(Font font) {
+        this.font = font;
+        super.setFont(font);
+    }
+    
+    public Component getTableCellRendererComponent(JTable table,
+                                                   Object value, boolean isSelected,
+                                                   boolean hasFocus,
+                                                   int row, int column) {
+        Color cellForeground = foreground != null ? foreground 
+            : table.getForeground();
+        Color cellBackground = background != null ? background 
+            : table.getBackground();
+        
+        setFont(font != null ? font : table.getFont());
+        
+        if (hasFocus) {
+            setBorder(UIManager.getBorder("Table.focusCellHighlightBorder"));
+            if (table.isCellEditable(row, column)) {
+                cellForeground = UIManager.getColor("Table.focusCellForeground");
+                cellBackground = UIManager.getColor("Table.focusCellBackground");
+            }
+        } else {
+            setBorder(border);
+        }
+        
+        super.setForeground(cellForeground);
+        super.setBackground(cellBackground);
+        
+        // Customize the component's appearance
+        setValue(value);
+        
+        return this;
+    }
+    
+    protected void setValue(Object value) {
+        if (value == null) {
+            setText("");
+            setIcon(null);
+        } else if (value instanceof Icon) {
+            setText("");
+            setIcon((Icon)value);
+        } else {
+            setText(value.toString());
+            setIcon(null);
+        }		
+    }
+    
+    protected Color foreground;
+    protected Color background;
+    protected Font font;
+    protected Border border;
 }
