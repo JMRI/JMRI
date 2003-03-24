3a20d854f5601d962f0a74609884acd04e980e3c
Bob Jacobsen <jacobsen@mac.com>
2003-03-24 01:50:41 +0000

    cleanup, fix JavaDoc

--- jmri/AbstractTurnout.java
+++ jmri/AbstractTurnout.java
@@ -18,7 +18,7 @@ import jmri.Turnout;
  *
  * Description:		Abstract class providing the basic logic of the Turnout interface
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.6 $
+ * @version			$Revision: 1.7 $
  */
 public abstract class AbstractTurnout implements Turnout, java.io.Serializable {
 
@@ -26,87 +26,86 @@ public abstract class AbstractTurnout implements Turnout, java.io.Serializable {
      * Handle a request to change state, typically by
      * sending a message to the layout in some child class.
      * @param s new state value
-     * @throws JmriException
      */
-	abstract protected void forwardCommandChangeToLayout(int s);
-
-	// implementing classes will typically have a function/listener to get
-	// updates from the layout, which will then call
-	//		public void firePropertyChange(String propertyName,
-	//										Object oldValue,
-	//										Object newValue)
-	// _once_ if anything has changed state
-
-	// Implementation methods
-
-	/**
+    abstract protected void forwardCommandChangeToLayout(int s);
+    
+    // implementing classes will typically have a function/listener to get
+    // updates from the layout, which will then call
+    //		public void firePropertyChange(String propertyName,
+    //					       	Object oldValue,
+    //						Object newValue)
+    // _once_ if anything has changed state
+    
+    // Implementation methods
+    
+    /**
      * Sets a new Commanded state, if need be notifying the
-	 * listeners, but does NOT send the command downstream.  This is used
-	 * when a new commanded state is noticed from another command.
+     * listeners, but does NOT send the command downstream.  This is used
+     * when a new commanded state is noticed from another command.
      */
-	public void newCommandedState(int s) {
-		if (_commandedState != s) {
-			int oldState = _commandedState;
-			_commandedState = s;
-			firePropertyChange("CommandedState", new Integer(oldState),
-								new Integer(_commandedState));
-			}
-	}
-
-	// interface function implementations
-
-	public String getUserName() {return _id;}
-	public void   setUserName(String s) {
-		String old = _id;
-		_id = s;
-		firePropertyChange("UserName", old, s);
-	}
-
-	public int getKnownState() {return _knownState;}
-
-	public void setCommandedState(int s) {
-		forwardCommandChangeToLayout(s);
-		newCommandedState(s);
+    public void newCommandedState(int s) {
+        if (_commandedState != s) {
+            int oldState = _commandedState;
+            _commandedState = s;
+            firePropertyChange("CommandedState", new Integer(oldState),
+                               new Integer(_commandedState));
+        }
+    }
+    
+    // interface function implementations
+    
+    public String getUserName() {return _id;}
+    public void   setUserName(String s) {
+        String old = _id;
+        _id = s;
+        firePropertyChange("UserName", old, s);
+    }
+    
+    public int getKnownState() {return _knownState;}
+    
+    public void setCommandedState(int s) {
+        forwardCommandChangeToLayout(s);
+        newCommandedState(s);
         newKnownState(s);       // for NONE feedback
-	}
-
-	public int getCommandedState() {return _commandedState;}
-
-	public int getFeedbackType() {return _feedbackType;}
-
+    }
+    
+    public int getCommandedState() {return _commandedState;}
+    
+    public int getFeedbackType() {return _feedbackType;}
+    
     /**
      * Add a protected newKnownState() for use by implementations. Not intended for
      * general use, e.g. for users to set the KnownState, but rather for
      * the code to have a way to set the state and fire notifications.
      */
-	protected void newKnownState(int s) {
-		if (_knownState != s) {
-			int oldState = _knownState;
-			_knownState = s;
-			firePropertyChange("KnownState", new Integer(oldState), new Integer(_knownState));
-			}
-	}
-
-	// internal data members
-	private String _id;
-	private int _feedbackType   = NONE;
-	private int _knownState     = UNKNOWN;
-	private int _commandedState = UNKNOWN;
-
-	// since we can't do a "super(this)" in the ctor to inherit from PropertyChangeSupport, we'll
-	// reflect to it.
-	// Note that dispose() doesn't act on these.  Its not clear whether it should...
-	java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
-
-	public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
-		pcs.addPropertyChangeListener(l);
-	}
-
-	protected void firePropertyChange(String p, Object old, Object n) { pcs.firePropertyChange(p,old,n);}
-
-	public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
-		pcs.removePropertyChangeListener(l);
-	}
+    protected void newKnownState(int s) {
+        if (_knownState != s) {
+            int oldState = _knownState;
+            _knownState = s;
+            firePropertyChange("KnownState", new Integer(oldState), new Integer(_knownState));
+        }
+    }
+    
+    // internal data members
+    private String _id;
+    private int _feedbackType   = NONE;
+    private int _knownState     = UNKNOWN;
+    private int _commandedState = UNKNOWN;
+    
+    // since we can't do a "super(this)" in the ctor to inherit from PropertyChangeSupport, we'll
+    // reflect to it.
+    // Note that dispose() doesn't act on these.  Its not clear whether it should...
+    java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
+    
+    public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
+        pcs.addPropertyChangeListener(l);
+    }
+    
+    protected void firePropertyChange(String p, Object old, Object n) { pcs.firePropertyChange(p,old,n);}
+    
+    public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
+        pcs.removePropertyChangeListener(l);
+    }
 }
 
 
