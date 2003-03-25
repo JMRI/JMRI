4108a7c2b4fd9b7d8ca2d8d7a19f46a1680cb47a
Glen Oberhauser <glenobie@users.sourceforge.net>
2003-03-25 17:48:13 +0000

    New class that takes over some of ThrottleManager's work. Does stuff specific to ThrottleFrames for saving/loading to XML

---
+++ jmri/jmrit/throttle/ThrottleFrameManager.java
@@ -0,0 +1,54 @@
+package jmri.jmrit.throttle;
+
+import java.util.Iterator;
+import java.util.ArrayList;
+
+/**
+ * Interface for allocating and deallocating throttles frames. Not to be confused
+ * with ThrottleManager
+ * @author			Glen Oberhauser
+ * @version			$Revision: 1.1 $
+ */
+public class ThrottleFrameManager
+{
+    private ArrayList throttleFrames;
+    private FunctionButtonPropertyEditor functionButtonEditor;
+
+    /**
+     * Tell this manager that a new ThrottleFrame was created.
+     * @param tf The new ThrottleFrame.
+     */
+    public void notifyNewThrottleFrame(ThrottleFrame tf)
+    {
+        if (throttleFrames == null)
+        {
+            throttleFrames = new ArrayList(2);
+        }
+        throttleFrames.add(tf);
+    }
+
+    /**
+     * Retrieve an Iterator over all the ThrottleFrames in existence.
+     * @return The Iterator on the list of ThrottleFrames.
+     */
+    public Iterator getThrottleFrames()
+    {
+        return throttleFrames.iterator();
+    }
+
+	/**
+     * Get a reference to the Function Editor
+	 * Allows us to have one editor without disposing and creating each time.
+     */
+    public jmri.jmrit.throttle.FunctionButtonPropertyEditor getFunctionButtonEditor()
+    {
+        if (functionButtonEditor == null)
+        {
+            functionButtonEditor = new FunctionButtonPropertyEditor();
+        }
+        return functionButtonEditor;
+    }
+
+}
+
+
