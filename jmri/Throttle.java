6b0fcd9bd8d41295ff93df2204dd7d99133fc883
Bob Jacobsen <jacobsen@mac.com>
2003-03-29 17:27:08 +0000

    add release and dispatch

--- jmri/Throttle.java
+++ jmri/Throttle.java
@@ -22,63 +22,63 @@ package jmri;
  * and deassign locos from this throttle, but that doesn't seem right.
  *
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Id: Throttle.java,v 1.6 2003-03-29 02:54:38 jacobsen Exp $
+ * @version			$Id: Throttle.java,v 1.7 2003-03-29 17:27:08 jacobsen Exp $
  */
 public interface Throttle {
-    
+
     /** Speed - expressed as a value 0.0 -> 1.0. Negative means emergency stop.
      * This is an bound parameter.
      */
     public float getSpeedSetting();
     public void setSpeedSetting(float speed);
-    
+
     /** direction
      * This is an bound parameter.
      */
     public boolean getIsForward();
     public void setIsForward(boolean forward);
-    
+
     // functions - note that we use the naming for DCC, though that's not the implication;
     // see also DccThrottle interface
     public boolean getF0();
     public void setF0(boolean f0);
-    
+
     public boolean getF1();
     public void setF1(boolean f1);
-    
+
     public boolean getF2();
     public void setF2(boolean f2);
-    
+
     public boolean getF3();
     public void setF3(boolean f3);
-    
+
     public boolean getF4();
     public void setF4(boolean f4);
-    
+
     public boolean getF5();
     public void setF5(boolean f5);
-    
+
     public boolean getF6();
     public void setF6(boolean f6);
-    
+
     public boolean getF7();
     public void setF7(boolean f7);
-    
+
     public boolean getF8();
     public void setF8(boolean f8);
-    
+
     public boolean getF9();
     public void setF9(boolean f9);
-    
+
     public boolean getF10();
     public void setF10(boolean f10);
-    
+
     public boolean getF11();
     public void setF11(boolean f11);
-    
+
     public boolean getF12();
     public void setF12(boolean f12);
-    
+
     /**
      * Locomotive identification.  The exact format is defined by the
      * specific implementation, but its intended that this is a user-specified
@@ -87,7 +87,7 @@ public interface Throttle {
      * This is an unbound parameter.
      */
     public String getLocoIdentification();
-    
+
     /**
      * Locomotive address.  The exact format is defined by the
      * specific implementation, but for DCC systems it is intended that this
@@ -97,20 +97,47 @@ public interface Throttle {
      * This is an unbound parameter.
      */
     public String getLocoAddress();
-    
+
     // register for notification if any of the properties change
     public void removePropertyChangeListener(java.beans.PropertyChangeListener p);
     public void addPropertyChangeListener(java.beans.PropertyChangeListener p);
-    
+
     /**
-     * Dispose when finished with this object.  After this, further usage of
+     * Dispose when finished with this object.  This does not
+     * free any hardware resources used; rather, it just cleans up the
+     * software implmentation.
+     * <P>
+     * After this, further usage of
      * this Throttle object will result in a JmriException.
-     *
-     * This is quite problematic, because a using object doesn't know when
-     * it's the last user.
      */
     public void dispose();
-    
+
+    /**
+     * Finished with this Throttle, tell the layout that the locomotive
+     * is available for reuse/reallocation by somebody else.
+     * <P>
+     * After this, further usage of
+     * this Throttle object will result in a JmriException.
+     * Do not call dispose after release.
+     * <P>
+     * Normally, release ends with a call to dispose.
+     */
+    public void release();
+
+    /**
+     * Finished with this Throttle, tell the layout that the locomotive
+     * is available for reuse/reallocation by somebody else. If possible,
+     * tell the layout that this locomotive has been dispatched to another user.
+     * Not all layouts will implement this, in which case it is synomous with
+     * release();
+     * <P>
+     * After this, further usage of
+     * this Throttle object will result in a JmriException.
+     * <P>
+     * Normally, dispatch ends with a call to dispose.
+     */
+    public void dispatch();
+
 }
 
 
