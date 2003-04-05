3543aadb8c8b616e2e317aa7352ca8dd39ad059c
Glen Oberhauser <glenobie@users.sourceforge.net>
2003-04-05 18:51:54 +0000

    new file

---
+++ jmri/jmrit/throttle/EditThrottlePreferencesAction.java
@@ -0,0 +1,35 @@
+package jmri.jmrit.throttle;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+
+/**
+ * Create a new throttle.
+ *
+ * @author			Glen Oberhauser
+ * @version
+ */
+public class EditThrottlePreferencesAction extends AbstractAction {
+
+    /**
+     * Constructor
+     * @param s Name for the action.
+     */
+    public EditThrottlePreferencesAction(String s) {
+        super(s);
+    }
+
+    /**
+     * The action is performed. Create a new ThrottleFrame.
+     * @param e The event causing the action.
+     */
+    public void actionPerformed(ActionEvent e) 
+	{
+		javax.swing.JOptionPane.showMessageDialog(null, "Coming Soon!");
+    }
+
+    // initialize logging
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(EditThrottlePreferencesAction.class.getName());
+
+}
