8aba5adc6fab7a3bf1d339425525e66bb227d0d7
Bob Jacobsen <jacobsen@mac.com>
2003-04-06 19:20:56 +0000

    new file. Compiles, perhaps OK, but testing not yet complete

---
+++ jmri/AbstractSignalHeadManager.java
@@ -0,0 +1,73 @@
+// AbstractSignalHeadManager.java
+
+package jmri;
+
+import java.util.Hashtable;
+import java.util.Enumeration;
+import com.sun.java.util.collections.List;
+import com.sun.java.util.collections.ArrayList;
+import com.sun.java.util.collections.Collections;
+
+
+/**
+ * Abstract partial implementation of a SignalHeadManager.
+ *
+ * @author      Bob Jacobsen Copyright (C) 2003
+ * @version	$Revision: 1.1 $
+ */
+public abstract class AbstractSignalHeadManager
+		implements SignalHeadManager, java.beans.PropertyChangeListener {
+
+        // abstract methods to be provided by subclasses
+
+	// abstract methods to be extended by subclasses
+	// to free resources when no longer used
+	public void dispose() throws JmriException {
+		_tsys.clear();
+		_tuser.clear();
+	}
+
+	// implemented methods
+	protected Hashtable _tsys = new Hashtable();   // stores known Turnout instances by system name
+	protected Hashtable _tuser = new Hashtable();   // stores known Turnout instances by user name
+
+	public SignalHead getBySystemName(String key) {
+		return (SignalHead)_tsys.get(key);
+	}
+	public Turnout getByAddress(TurnoutAddress key) {
+		Turnout t = (Turnout)_tuser.get(key.getUserName());
+		if (t != null) return t;
+		return (Turnout)_tsys.get(key.getSystemName());
+	}
+	public SignalHead getByUserName(String key) {
+		return (SignalHead)_tuser.get(key);
+	}
+
+	// keep track of SignalHead user name changes
+	public void propertyChange(java.beans.PropertyChangeEvent e) {
+		if (e.getPropertyName().equals("UserName")) {
+			String old = (String) e.getOldValue();
+			String now = (String) e.getNewValue();
+			SignalHead t = getByUserName(old);
+			_tuser.remove(old);
+			_tuser.put(now, t);
+		}
+	}
+
+    public List getSystemNameList() {
+        String[] arr = new String[_tsys.size()];
+        List out = new ArrayList();
+        Enumeration en = _tsys.keys();
+        int i=0;
+        while (en.hasMoreElements()) {
+            arr[i] = (String)en.nextElement();
+            i++;
+        }
+        java.util.Arrays.sort(arr);
+        for (i=0; i<arr.length; i++) out.add(arr[i]);
+        return out;
+    }
+}
+
+
+/* @(#)AbstractSignalHeadManager.java */
