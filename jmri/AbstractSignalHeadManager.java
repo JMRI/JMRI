66cc3ed57068f9abf93db1c4ff17d8f97a1d4835
Bob Jacobsen <jacobsen@mac.com>
2003-04-08 05:40:07 +0000

    no longer pure abstract. cLeanup. Add a register method.

--- jmri/AbstractSignalHeadManager.java
+++ jmri/AbstractSignalHeadManager.java
@@ -11,26 +11,38 @@ import com.sun.java.util.collections.Collections;
 
 /**
  * Abstract partial implementation of a SignalHeadManager.
+ * <P>
+ * Not truly an abstract class, this might have been better named
+ * DefaultSignalHeadManager.  But we've got it here for the eventual
+ * need to provide system-specific implementations.
+ * <P>
+ * Note that this does not enforce any particular system naming convention
+ * at the present time.  They're just names...
  *
  * @author      Bob Jacobsen Copyright (C) 2003
- * @version	$Revision: 1.2 $
+ * @version	$Revision: 1.3 $
  */
-public abstract class AbstractSignalHeadManager
+public class AbstractSignalHeadManager
     implements SignalHeadManager, java.beans.PropertyChangeListener {
-    
-    // abstract methods to be provided by subclasses
-    
+
+    public AbstractSignalHeadManager() {
+        // register the result for later configuration
+        InstanceManager.configureManagerInstance().register(this);
+        log.debug("register");
+    }
+
     // abstract methods to be extended by subclasses
     // to free resources when no longer used
     public void dispose() throws JmriException {
+        InstanceManager.configureManagerInstance().deregister(this);
         _tsys.clear();
         _tuser.clear();
     }
-    
+
     // implemented methods
     protected Hashtable _tsys = new Hashtable();   // stores known Turnout instances by system name
     protected Hashtable _tuser = new Hashtable();   // stores known Turnout instances by user name
-    
+
     public SignalHead getBySystemName(String key) {
         return (SignalHead)_tsys.get(key);
     }
@@ -42,8 +54,21 @@ public abstract class AbstractSignalHeadManager
     public SignalHead getByUserName(String key) {
         return (SignalHead)_tuser.get(key);
     }
-    
-    // keep track of SignalHead user name changes
+
+    /**
+     * Remember a SignalHead created outside the manager.
+     */
+    public void register(SignalHead s, String systemName, String userName) {
+        _tuser.put(userName, s);
+        _tsys.put(systemName, s);
+    }
+
+    /**
+     * The PropertyChangeListener interface in this class is
+     * intended to keep track of SignalHead user name changes.
+     * It is not completely implemented yet. In particular, listeners
+     * are not added to newly registered SignalHead objects.
+     */
     public void propertyChange(java.beans.PropertyChangeEvent e) {
         if (e.getPropertyName().equals("UserName")) {
             String old = (String) e.getOldValue();
@@ -53,7 +78,7 @@ public abstract class AbstractSignalHeadManager
             _tuser.put(now, t);
         }
     }
-    
+
     public List getSystemNameList() {
         String[] arr = new String[_tsys.size()];
         List out = new ArrayList();
@@ -67,6 +92,9 @@ public abstract class AbstractSignalHeadManager
         for (i=0; i<arr.length; i++) out.add(arr[i]);
         return out;
     }
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(AbstractSignalHeadManager.class.getName());
+
 }
 
 /* @(#)AbstractSignalHeadManager.java */
