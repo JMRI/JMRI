a8f053493de3d145c98ea7d38de29cfb238915b8
Bob Jacobsen <jacobsen@mac.com>
2003-04-26 01:56:42 +0000

    cleanup before updating to work with new threading

--- test/jmri/jmrix/lenz/XNetPacketizerTest.java
+++ test/jmri/jmrix/lenz/XNetPacketizerTest.java
@@ -9,14 +9,14 @@ import javax.swing.*;
  * <p>Description: </p>
  * <p>Copyright: Copyright (c) 2002</p>
  * @author Bob Jacobsen
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class XNetPacketizerTest extends TestCase {
 
-  public XNetPacketizerTest(String s) {
-    super(s);
-  }
-
+    public XNetPacketizerTest(String s) {
+        super(s);
+    }
+    
     public void testOutbound() throws Exception {
         LenzCommandStation lcs = new LenzCommandStation();
         XNetPacketizer c = new XNetPacketizer(lcs);
@@ -34,59 +34,59 @@ public class XNetPacketizerTest extends TestCase {
         Assert.assertEquals("parity", 0x45, p.tostream.readByte()&0xff);
         Assert.assertEquals("remaining ", 0, p.tostream.available());
     }
-
-	public void testInbound() throws Exception {
+    
+    public void testInbound() throws Exception {
         LenzCommandStation lcs = new LenzCommandStation();
-		XNetPacketizer c = new XNetPacketizer(lcs);
-
+        XNetPacketizer c = new XNetPacketizer(lcs);
+        
         // make sure Swing is up
         JFrame j = new JFrame();
         j.pack();
-
-		// connect to iostream via port controller
-		XNetPortControllerScaffold p = new XNetPortControllerScaffold();
-		c.connectPort(p);
+        
+        // connect to iostream via port controller
+        XNetPortControllerScaffold p = new XNetPortControllerScaffold();
+        c.connectPort(p);
         c.startThreads();
-
-		// object to receive reply
-		XNetListenerScaffold l = new XNetListenerScaffold();
-		c.addXNetListener(0xff, l);
-
-		// send a message
+        
+        // object to receive reply
+        XNetListenerScaffold l = new XNetListenerScaffold();
+        c.addXNetListener(0xff, l);
+        
+        // send a message
         XNetMessage m = lcs.getTurnoutCommandMsg(22, true, false, true);
-		// that's already tested, so don't do here.
-
-		// now send reply
-		p.tistream.write(0x52);
-		p.tistream.write(0x12);
-		p.tistream.write(0x12);
-		p.tistream.write(0x52);
-
-		// check that the message was picked up by the read thread.
-		Assert.assertTrue("reply received ", waitForReply(l));
-		Assert.assertEquals("first char of reply ", 0x52, l.rcvdMsg.getElement(0));
-	}
-
-
-	private boolean waitForReply(XNetListenerScaffold l) {
-		// wait for reply (normally, done by callback; will check that later)
-		int i = 0;
-		while ( l.rcvdMsg == null && i++ < 100  )  {
-			try {
-				Thread.sleep(10);
-			} catch (Exception e) {
-			}
-		}
-		if (log.isDebugEnabled()) log.debug("past loop, i="+i
-									+" reply="+l.rcvdMsg);
-		if (i==0) log.warn("waitForReply saw an immediate return; is threading right?");
-		return i<100;
-	}
-
+        // that's already tested, so don't do here.
+        
+        // now send reply
+        p.tistream.write(0x52);
+        p.tistream.write(0x12);
+        p.tistream.write(0x12);
+        p.tistream.write(0x52);
+        
+        // check that the message was picked up by the read thread.
+        Assert.assertTrue("reply received ", waitForReply(l));
+        Assert.assertEquals("first char of reply ", 0x52, l.rcvdMsg.getElement(0));
+    }
+    
+    
+    private boolean waitForReply(XNetListenerScaffold l) {
+        // wait for reply (normally, done by callback; will check that later)
+        int i = 0;
+        while ( l.rcvdMsg == null && i++ < 100  )  {
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+            }
+        }
+        if (log.isDebugEnabled()) log.debug("past loop, i="+i
+                                            +" reply="+l.rcvdMsg);
+        if (i==0) log.warn("waitForReply saw an immediate return; is threading right?");
+        return i<100;
+    }
+    
     // The minimal setup for log4J
     apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
     protected void setUp() { log4jfixtureInst.setUp(); }
     protected void tearDown() { log4jfixtureInst.tearDown(); }
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetPacketizerTest.class.getName());
-
+    
 }
--- test/jmri/jmrix/lenz/XNetTest.java
+++ test/jmri/jmrix/lenz/XNetTest.java
@@ -3,7 +3,7 @@
  *
  * Description:	    tests for the jmri.jmrix.lenz package
  * @author			Bob Jacobsen
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  */
 
 package jmri.jmrix.lenz;
@@ -14,31 +14,31 @@ import junit.framework.TestSuite;
 
 public class XNetTest extends TestCase {
 
-	// from here down is testing infrastructure
-
-	public XNetTest(String s) {
-		super(s);
-	}
-
-	// Main entry point
-	static public void main(String[] args) {
-		String[] testCaseName = {XNetTest.class.getName()};
-		junit.swingui.TestRunner.main(testCaseName);
-	}
-
-	// test suite from all defined tests
-	public static Test suite() {
-		TestSuite suite = new TestSuite("jmri.jmrix.lenz.XNetTest");  // no tests in this class itself
-		suite.addTest(new TestSuite(XNetMessageTest.class));
-		suite.addTest(new TestSuite(XNetTurnoutTest.class));
-		suite.addTest(new TestSuite(XNetPacketizerTest.class));
-		suite.addTest(new TestSuite(jmri.jmrix.lenz.packetgen.PacketGenFrameTest.class));
-		suite.addTest(new TestSuite(XNetTurnoutManagerTest.class));
-		suite.addTest(new TestSuite(XNetTrafficControllerTest.class));
-		suite.addTest(new TestSuite(XNetTrafficRouterTest.class));
-		return suite;
-	}
-
-	 static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetTest.class.getName());
-
+    // from here down is testing infrastructure
+    
+    public XNetTest(String s) {
+        super(s);
+    }
+    
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {XNetTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+    
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite("jmri.jmrix.lenz.XNetTest");  // no tests in this class itself
+        suite.addTest(new TestSuite(XNetMessageTest.class));
+        suite.addTest(new TestSuite(XNetTurnoutTest.class));
+        suite.addTest(new TestSuite(XNetPacketizerTest.class));
+        suite.addTest(new TestSuite(jmri.jmrix.lenz.packetgen.PacketGenFrameTest.class));
+        suite.addTest(new TestSuite(XNetTurnoutManagerTest.class));
+        suite.addTest(new TestSuite(XNetTrafficControllerTest.class));
+        suite.addTest(new TestSuite(XNetTrafficRouterTest.class));
+        return suite;
+    }
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetTest.class.getName());
+    
 }
