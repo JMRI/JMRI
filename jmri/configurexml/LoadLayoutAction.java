0329a9d8d6ae85c71fcbbc3cab293236ad947e06
Bob Jacobsen <jacobsen@mac.com>
2003-05-03 17:21:03 +0000

    class is obsolete. Note that in comment, and plan on eventual removal

--- jmri/configurexml/LoadLayoutAction.java
+++ jmri/configurexml/LoadLayoutAction.java
@@ -2,43 +2,39 @@
 
 package jmri.configurexml;
 
-import javax.swing.AbstractAction;
-import java.awt.event.ActionEvent;
-
+import java.awt.event.*;
 import java.io.*;
-import javax.swing.*;
-import org.jdom.*;
-import org.jdom.input.*;
-import com.sun.java.util.collections.List;
 
-import jmri.jmrit.XmlFile;
+import javax.swing.*;
 
 /**
- * Load the layout hardware config
- *
+ * Load the layout hardware config.
+ * <P>
+ * Note: This class is obsolete, and not being used.
+ *<P>
  * @author			Bob Jacobsen   Copyright (C) 2002
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  * @see             jmri.jmrit.XmlFile
  */
 public class LoadLayoutAction extends AbstractAction {
 
-	public LoadLayoutAction(String s) {
-		super(s);
-	}
-
+    public LoadLayoutAction(String s) {
+        super(s);
+    }
+    
     public void actionPerformed(ActionEvent e) {
-		File fp = new File("layout.temp.config.xml");
+        File fp = new File("layout.temp.config.xml");
         LayoutConfigXML layout = new LayoutConfigXML();
-
-		// write it out
-		//try {
-			layout.readFile(fp);
-		//} catch (java.io.IOException ex) {
-		//	log.error("Error writing layout config file: "+ex.getMessage());
-		//}
-	}
-
-	// initialize logging
+        
+        // write it out
+        //try {
+        layout.readFile(fp);
+        //} catch (java.io.IOException ex) {
+        //	log.error("Error writing layout config file: "+ex.getMessage());
+        //}
+    }
+    
+    // initialize logging
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LoadLayoutAction.class.getName());
-
+    
 }
