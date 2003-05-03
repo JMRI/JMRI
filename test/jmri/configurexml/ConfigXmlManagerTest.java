e12ce8a9bd3992bf574ab43480a9912cf790473b
Bob Jacobsen <jacobsen@mac.com>
2003-05-03 17:29:17 +0000

    add tests for find(String)

--- test/jmri/configurexml/ConfigXmlManagerTest.java
+++ test/jmri/configurexml/ConfigXmlManagerTest.java
@@ -1,44 +1,62 @@
+// ConfigXmlManagerTest.java
 
 package jmri.configurexml;
 
 import junit.framework.*;
+import java.io.File;
 
-
-
+/**
+ * Tests for ConfigXmlManager.
+ * <P>
+ * Uses the local preferences for test files.
+ * @author Bob Jacobsen Copyright 2003
+ * @version $Revision: 1.2 $
+ */
 public class ConfigXmlManagerTest extends TestCase {
 
     public ConfigXmlManagerTest(String s) {
         super(s);
     }
-
+    
     boolean innerFlag;
-
+    
     public void testRegister() {
         ConfigXmlManager configxmlmanager = new ConfigXmlManager(){
-            void locateFailed(java.lang.ClassNotFoundException ex, String adapterName, Object o) {
-                innerFlag=true;
-            }
-        };
+                void locateFailed(java.lang.ClassNotFoundException ex, String adapterName, Object o) {
+                    innerFlag=true;
+                }
+            };
         Object o1=  "";
         innerFlag=false;
         configxmlmanager.register(o1);
         Assert.assertTrue("register didn't find class", innerFlag);
     }
-
+    
     public void testAdapterName() {
         ConfigXmlManager c = new ConfigXmlManager();
         Assert.assertEquals("String class adapter", "java.lang.configurexml.StringXml",
                             c.adapterName(""));
     }
-
-  	// test suite from all defined tests
-	public static Test suite() {
-		TestSuite suite = new TestSuite(ConfigXmlManagerTest.class);
-		return suite;
-	}
-
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(ConfigXmlManagerTest.class.getName());
-
+    
+    public void testFindFile() {
+        ConfigXmlManager configxmlmanager = new ConfigXmlManager(){
+                void locateFailed(java.lang.ClassNotFoundException ex, String adapterName, Object o) {
+                    innerFlag=true;
+                }
+            };
+        File result;
+        result = configxmlmanager.find("foo.biff");
+        Assert.assertTrue("dont find foo.biff", result==null);
+    }
+    
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(ConfigXmlManagerTest.class);
+        return suite;
+    }
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(ConfigXmlManagerTest.class.getName());
+    
     // The minimal setup for log4J
     apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
     protected void setUp() { log4jfixtureInst.setUp(); }
