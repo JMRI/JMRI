0eae681db5b03e3996debe78fed2f7ec33ec41d2
Bob Jacobsen <jacobsen@mac.com>
2003-05-03 17:12:04 +0000

    cleanup

--- jmri/configurexml/LayoutConfigXML.java
+++ jmri/configurexml/LayoutConfigXML.java
@@ -1,10 +1,11 @@
 package jmri.configurexml;
 
-import java.io.File;
+import jmri.*;
+import java.io.*;
+
+import com.sun.java.util.collections.*;
 import org.jdom.*;
 import org.jdom.output.*;
-import com.sun.java.util.collections.List;
-import jmri.*;
 
 /**
  * Provides the mechanisms for storing an entire layout configuration
@@ -12,25 +13,25 @@ import jmri.*;
  * systems, etc.
  * @see <A HREF="package-summary.html">Package summary for details of the overall structure</A>
  * @author Bob Jacobsen Copyright: Copyright (c) 2002
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class LayoutConfigXML extends jmri.jmrit.XmlFile {
 
     public LayoutConfigXML() {
     }
 
-	public void writeFile(File file) {
-		try {
-			// This is taken in large part from "Java and XML" page 368
+    public void writeFile(File file) {
+        try {
+            // This is taken in large part from "Java and XML" page 368
 
-			// create root element
-			Element root = new Element("layout-config");
-			Document doc = new Document(root);
-			doc.setDocType(new DocType("layout-config","layout-config.dtd"));
+            // create root element
+            Element root = new Element("layout-config");
+            Document doc = new Document(root);
+            doc.setDocType(new DocType("layout-config","layout-config.dtd"));
 
-			// add top-level elements
-			Element turnouts;  // will fill this with turnout info
-			root.addContent(turnouts = new Element("turnouts"))
+            // add top-level elements
+            Element turnouts;  // will fill this with turnout info
+            root.addContent(turnouts = new Element("turnouts"))
                 .addContent(new Element("sensors"))
                 .addContent(new Element("blocks"))
                 .addContent(new Element("signals"));
@@ -39,48 +40,48 @@ public class LayoutConfigXML extends jmri.jmrit.XmlFile {
             TurnoutManager tm = InstanceManager.turnoutManagerInstance();
             if (tm!=null) {
                 com.sun.java.util.collections.Iterator iter =
-                                    tm.getSystemNameList().iterator();
+                    tm.getSystemNameList().iterator();
 
                 while (iter.hasNext()) {
                     String sname = (String)iter.next();
                     String uname = tm.getBySystemName(sname).getUserName();
                     Element elem = new Element("turnout")
-                            .addAttribute("systemName", sname);
+                        .addAttribute("systemName", sname);
                     if (uname!=null) elem.addAttribute("userName", uname);
                     log.debug("store turnout "+sname+":"+uname);
                     turnouts.addContent(elem);
 
                 }
             }
-			// write the result to selected file
-			java.io.FileOutputStream o = new java.io.FileOutputStream(file);
-			XMLOutputter fmt = new XMLOutputter();
-			fmt.setNewlines(true);   // pretty printing
-			fmt.setIndent(true);
-			fmt.output(doc, o);
+            // write the result to selected file
+            java.io.FileOutputStream o = new java.io.FileOutputStream(file);
+            XMLOutputter fmt = new XMLOutputter();
+            fmt.setNewlines(true);   // pretty printing
+            fmt.setIndent(true);
+            fmt.output(doc, o);
         }
-		catch (Exception e) {
-			log.error(e);
-		}
-	}
+        catch (Exception e) {
+            log.error(e);
+        }
+    }
 
     public void readFile(File fi) {
         try {
             Element root = super.rootFromFile(fi);
             // get the turnouts
             Element turnouts = root.getChild("turnouts");
-			List turnoutList = turnouts.getChildren("turnout");
-			if (log.isDebugEnabled()) log.debug("Found "+turnoutList.size()+" turnouts");
+            List turnoutList = turnouts.getChildren("turnout");
+            if (log.isDebugEnabled()) log.debug("Found "+turnoutList.size()+" turnouts");
             TurnoutManager tm = InstanceManager.turnoutManagerInstance();
 
-			for (int i=0; i<turnoutList.size(); i++) {
-				if ( ((Element)(turnoutList.get(i))).getAttribute("systemName") == null) {
-					  if (log.isDebugEnabled()) log.debug("unexpected null in systemName "+((Element)(turnoutList.get(i)))+" "+((Element)(turnoutList.get(i))).getAttributes());
-					  break;
-				}
+            for (int i=0; i<turnoutList.size(); i++) {
+                if ( ((Element)(turnoutList.get(i))).getAttribute("systemName") == null) {
+                    if (log.isDebugEnabled()) log.debug("unexpected null in systemName "+((Element)(turnoutList.get(i)))+" "+((Element)(turnoutList.get(i))).getAttributes());
+                    break;
+                }
                 String sysName = ((Element)(turnoutList.get(i))).getAttribute("systemName").getValue();
                 String userName = null;
-        		if ( ((Element)(turnoutList.get(i))).getAttribute("userName") != null)
+                if ( ((Element)(turnoutList.get(i))).getAttribute("userName") != null)
                     userName = ((Element)(turnoutList.get(i))).getAttribute("userName").getValue();
 
                 tm.newTurnout(sysName, userName);
@@ -91,8 +92,8 @@ public class LayoutConfigXML extends jmri.jmrit.XmlFile {
         catch (java.io.IOException e) { log.error("Exception reading: "+e); }
     }
 
-	static public String fileLocation = "layout"+File.separator;
+    static public String fileLocation = "layout"+File.separator;
 
-	// initialize logging
+    // initialize logging
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LayoutConfigXML.class.getName());
 }
--- jmri/jmrit/XmlFile.java
+++ jmri/jmrit/XmlFile.java
@@ -2,74 +2,66 @@
 
 package jmri.jmrit;
 
-import jmri.Programmer;
-import jmri.InstanceManager;
-import jmri.ProgListener;
-
-import java.awt.Color;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import java.util.Date;
-
 import java.io.*;
+import java.util.*;
 
+import com.sun.java.util.collections.List;
 import org.jdom.*;
 import org.jdom.input.*;
-import com.sun.java.util.collections.*;
 
 /**
  * XmlFile contains various member implementations for handling aspects of XML files.
  *
  * @author		Bob Jacobsen   Copyright (C) 2001, 2002
- * @version		$Revision: 1.4 $
+ * @version		$Revision: 1.5 $
  */
 public abstract class XmlFile {
 
 
-	/**
-	 * Read the contents of an XML file from its name.  The search order is implemented in
-	 * this routine via testing for the existance, not the parsebility, of the files
+    /**
+     * Read the contents of an XML file from its name.  The search order is implemented in
+     * this routine via testing for the existance, not the parsebility, of the files
      * @param name Filename, without xml or preferences part (which is searched for)
      * @throws org.jdom.JDOMException
      * @throws java.io.FileNotFoundException
      * @return null if not found, else root element of located file
-	 */
-	public Element rootFromName(String name) throws org.jdom.JDOMException, java.io.FileNotFoundException {
+     */
+    public Element rootFromName(String name) throws org.jdom.JDOMException, java.io.FileNotFoundException {
 
-		File fp = findFile(name);
-		if (fp != null) {
-			if (log.isDebugEnabled()) log.debug("readFile: "+name+" from "+fp.getAbsolutePath());
-			return rootFromFile(fp);
-		}
-		else {
-			log.warn("Did not find file "+name+" in "+prefsDir()+" or "+xmlDir());
-			return null;
-		}
-	}
+        File fp = findFile(name);
+        if (fp != null) {
+            if (log.isDebugEnabled()) log.debug("readFile: "+name+" from "+fp.getAbsolutePath());
+            return rootFromFile(fp);
+        }
+        else {
+            log.warn("Did not find file "+name+" in "+prefsDir()+" or "+xmlDir());
+            return null;
+        }
+    }
 
-	/**
-	 * Read a File as XML.
+    /**
+     * Read a File as XML.
      * @throws org.jdom.JDOMException
      * @throws java.io.FileNotFoundException
      * @param file File to be parsed.  A FileNotFoundException is thrown if it doesn't exist.
      * @return root element from the file. This should never be null, as an
      *          exception should be thrown if anything goes wrong.
-	 */
-	public Element rootFromFile(File file) throws org.jdom.JDOMException, java.io.FileNotFoundException {
-		return rootFromStream(new BufferedInputStream(new FileInputStream(file)));
-	}
+     */
+    public Element rootFromFile(File file) throws org.jdom.JDOMException, java.io.FileNotFoundException {
+        return rootFromStream(new BufferedInputStream(new FileInputStream(file)));
+    }
 
-	/**
-	 * Read the contents of a stream as XML, and return the root object
+    /**
+     * Read the contents of a stream as XML, and return the root object
      * @throws org.jdom.JDOMException
      * @throws java.io.FileNotFoundException
      * @param stream to be parsed.
      * @return root element within the stream. This should never be null, as an
      *          exception should be thrown if anything goes wrong.
-	 */
-	public Element rootFromStream(InputStream stream) throws org.jdom.JDOMException, java.io.FileNotFoundException {
-		String rawpath = new File("xml"+File.separator+"DTD"+File.separator).getAbsolutePath();
-		String apath = rawpath;
+     */
+    public Element rootFromStream(InputStream stream) throws org.jdom.JDOMException, java.io.FileNotFoundException {
+        String rawpath = new File("xml"+File.separator+"DTD"+File.separator).getAbsolutePath();
+        String apath = rawpath;
         if (File.separatorChar != '/') {
             apath = apath.replace(File.separatorChar, '/');
         }
@@ -79,125 +71,125 @@ public abstract class XmlFile {
         if (!apath.endsWith("/")) {
             apath = apath+"/";
         }
-		String path = "file:"+apath;
+        String path = "file:"+apath;
 
-		if (log.isDebugEnabled()) log.debug("readFile from stream, search path:"+path);
-		// This is taken in large part from "Java and XML" page 354
+        if (log.isDebugEnabled()) log.debug("readFile from stream, search path:"+path);
+        // This is taken in large part from "Java and XML" page 354
 
-		// Open and parse file
+        // Open and parse file
 
-		// DOMBuilder builder = new DOMBuilder(verify);  // argument controls validation, on for now
-		// Document doc = builder.build(new BufferedInputStream(stream));
+        // DOMBuilder builder = new DOMBuilder(verify);  // argument controls validation, on for now
+        // Document doc = builder.build(new BufferedInputStream(stream));
 
-		SAXBuilder builder = new SAXBuilder(verify);  // argument controls validation, on for now
-		Document doc = builder.build(new BufferedInputStream(stream),path);
+        SAXBuilder builder = new SAXBuilder(verify);  // argument controls validation, on for now
+        Document doc = builder.build(new BufferedInputStream(stream),path);
 
-		// find root
-		return doc.getRootElement();
-	}
-
-
-	/**
-	* Check if a file of the given name exists. This is here so it can
-	* be overridden during tests. Note that it also obeys the
-	* search rules.
-	* @parameter name subdirectory and file name, not including the leading path
-    *                   to either the xml or preferences directory
-    * @return true if the file exists in a searched place
-	*/
-	protected boolean checkFile(String name) {
-		File fp = new File(prefsDir()+name);
-		if (fp.exists()) {
-			return true;
-		}
-		else {
-			File fx = new File(xmlDir()+name);
-			if (fx.exists()) {
-				return true;
-			}
-			else {
-			return false;
-			}
-		}
-	}
+        // find root
+        return doc.getRootElement();
+    }
 
 
-	/**
-	* Return a File object for a name. This is here to implement the
-	* search rule: Look first in prefsDir, then xmlDir()
-    * @param name Filename without path information, but perhaps containing
-    *               subdirectory information (e.g. "decoders/Mine.xml")
-    * @return null if file found, otherwise the located File
-	*/
-	protected File findFile(String name) {
-		File fp = new File(prefsDir()+name);
-		if (fp.exists()) {
-			return fp;
-		}
-		else {
-			File fx = new File(xmlDir()+name);
-			if (fx.exists()) {
-				return fx;
-			}
-			else {
-			return null;
-			}
-		}
-	}
+    /**
+     * Check if a file of the given name exists. This is here so it can
+     * be overridden during tests. Note that it also obeys the
+     * search rules.
+     * @parameter name subdirectory and file name, not including the leading path
+     *                   to either the xml or preferences directory
+     * @return true if the file exists in a searched place
+     */
+    protected boolean checkFile(String name) {
+        File fp = new File(prefsDir()+name);
+        if (fp.exists()) {
+            return true;
+        }
+        else {
+            File fx = new File(xmlDir()+name);
+            if (fx.exists()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
 
 
-	/**
-	* Diagnostic printout of as much as we can find
-    * @param name Element to print, should not be null
-	*/
-	static public void dumpElement(Element name) {
-		List l = name.getChildren();
-		for (int i = 0; i<l.size(); i++) {
-			System.out.println(" Element: "+((Element)l.get(i)).getName()+" ns: "+((Element)l.get(i)).getNamespace());
-		}
-	}
+    /**
+     * Return a File object for a name. This is here to implement the
+     * search rule: Look first in prefsDir, then xmlDir()
+     * @param name Filename without path information, but perhaps containing
+     *               subdirectory information (e.g. "decoders/Mine.xml")
+     * @return null if file found, otherwise the located File
+     */
+    protected File findFile(String name) {
+        File fp = new File(prefsDir()+name);
+        if (fp.exists()) {
+            return fp;
+        }
+        else {
+            File fx = new File(xmlDir()+name);
+            if (fx.exists()) {
+                return fx;
+            }
+            else {
+                return null;
+            }
+        }
+    }
 
-	/**
-	* Move original file to a backup. Use this before writing out a new version of the file.
-	* @param name Last part of file pathname i.e. subdir/name, without the
-    *               pathname for either the xml or preferences directory.
-	*/
-	public void makeBackupFile(String name) {
-		File file = findFile(name);
-		if (file!=null) {
-			file.renameTo(new File(backupFileName(name)));
-		}
-		else log.info("No "+name+" file to backup");
-	}
 
-	/**
-	* Return the name of a new, unique backup file. This is here so it can
-	* be overridden during tests. File to be backed-up must be within the
-    * preferences directory tree.
-    * @param name Filename without preference path information, e.g. "decoders/Mine.xml".
-    * @return Complete filename, including path information into preferences directory
-	*/
-	public String backupFileName(String name) {
-		// File.createTempFile is not available in java 1, so use millisecond time as unique string
-		String f = prefsDir()+name+"-"
-							+((new Date()).getTime());
-		if (log.isDebugEnabled()) log.debug("backup file name is "+f);
-		return f;
-	}
+    /**
+     * Diagnostic printout of as much as we can find
+     * @param name Element to print, should not be null
+     */
+    static public void dumpElement(Element name) {
+        List l = name.getChildren();
+        for (int i = 0; i<l.size(); i++) {
+            System.out.println(" Element: "+((Element)l.get(i)).getName()+" ns: "+((Element)l.get(i)).getNamespace());
+        }
+    }
 
-	/**
-	 * Ensure that a subdirectory is present; if not, create it.
+    /**
+     * Move original file to a backup. Use this before writing out a new version of the file.
+     * @param name Last part of file pathname i.e. subdir/name, without the
+     *               pathname for either the xml or preferences directory.
+     */
+    public void makeBackupFile(String name) {
+        File file = findFile(name);
+        if (file!=null) {
+            file.renameTo(new File(backupFileName(name)));
+        }
+        else log.info("No "+name+" file to backup");
+    }
+
+    /**
+     * Return the name of a new, unique backup file. This is here so it can
+     * be overridden during tests. File to be backed-up must be within the
+     * preferences directory tree.
+     * @param name Filename without preference path information, e.g. "decoders/Mine.xml".
+     * @return Complete filename, including path information into preferences directory
+     */
+    public String backupFileName(String name) {
+        // File.createTempFile is not available in java 1, so use millisecond time as unique string
+        String f = prefsDir()+name+"-"
+            +((new Date()).getTime());
+        if (log.isDebugEnabled()) log.debug("backup file name is "+f);
+        return f;
+    }
+
+    /**
+     * Ensure that a subdirectory is present; if not, create it.
      * @param name Complete pathname of directory to be checked/created.
-	 */
-	static public void ensurePrefsPresent(String name) {
-		File f = new File(name);
-		if (! f.exists()) {
-			log.warn("Creating a missing preferences directory: "+name);
-			f.mkdirs();
-		}
-	}
+     */
+    static public void ensurePrefsPresent(String name) {
+        File f = new File(name);
+        if (! f.exists()) {
+            log.warn("Creating a missing preferences directory: "+name);
+            f.mkdirs();
+        }
+    }
 
-	static public String xmlDir() {return "xml"+File.separator;}
+    static public String xmlDir() {return "xml"+File.separator;}
 
     /**
      * Define the location of the preferences directory.  This is system-specific
@@ -213,7 +205,7 @@ public abstract class XmlFile {
      * </DL>
      * @return Pathname in local form, with a terminating separator
      */
-	static public String prefsDir() {
+    static public String prefsDir() {
         String osName       = System.getProperty("os.name","<unknown>");
         String mrjVersion   = System.getProperty("mrj.version","<unknown>");
         String userHome     = System.getProperty("user.home","");
@@ -228,42 +220,42 @@ public abstract class XmlFile {
             if (osName.equals("Mac OS X")) {
                 // Mac OS X
                 result = userHome+"Library"+File.separator+"Preferences"
-                            +File.separator+"JMRI"+File.separator;
+                    +File.separator+"JMRI"+File.separator;
             } else {
                 // Mac Classic, by elimination. Check consistency of mrjVersion
                 // with that assumption
                 if (!(mrjVersion.charAt(0)=='2'))
                     log.error("Decided Mac Classic, but mrj.version is \""
-                                    +mrjVersion+"\" os.name is \""
-                                    +osName+"\"");
+                              +mrjVersion+"\" os.name is \""
+                              +osName+"\"");
                 // userHome is the overall preferences directory
                 result = userHome+"JMRI"+File.separator;
             }
         } else if (osName.equals("Linux")) {
             // Linux, so use an invisible file
-                result = userHome+".jmri"+File.separator;
+            result = userHome+".jmri"+File.separator;
         } else {
             // Could be Windows, other
             result = userHome+"JMRI"+File.separator;
         }
 
         if (log.isDebugEnabled()) log.debug("prefsDir defined as \""+result+
-                    "\" based on os.name=\""
-                    +osName
-                    +"\" mrj.version=\""
-                    +mrjVersion
-                    +"\" user.home=\""
-                    +userHome
-                    +"\"");
+                                            "\" based on os.name=\""
+                                            +osName
+                                            +"\" mrj.version=\""
+                                            +mrjVersion
+                                            +"\" user.home=\""
+                                            +userHome
+                                            +"\"");
         return result;
     }
 
-	static boolean verify = true;
+    static boolean verify = true;
 
-	// initialize SAXbuilder
-	static private SAXBuilder builder = new SAXBuilder(verify);  // argument controls validation, on for now
+    // initialize SAXbuilder
+    static private SAXBuilder builder = new SAXBuilder(verify);  // argument controls validation, on for now
 
-	// initialize logging
+    // initialize logging
     static private org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XmlFile.class.getName());
 
 }
