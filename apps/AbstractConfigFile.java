3850c8efe35ed5b50606823f5c6ee749c50e4b5e
Bob Jacobsen <jacobsen@mac.com>
2003-05-06 05:12:50 +0000

    refactor file write

--- apps/AbstractConfigFile.java
+++ apps/AbstractConfigFile.java
@@ -15,42 +15,77 @@ import org.jdom.output.*;
  * application. Works with the AbstractConfigFrame
  *
  * @author			Bob Jacobsen   Copyright (C) 2001
- * @version		 	$Revision: 1.5 $
+ * @version		 	$Revision: 1.6 $
  * @see apps.AbstractConfigFrame
  */
 abstract public class AbstractConfigFile extends XmlFile {
-    
+
     public void readFile(String name) throws java.io.FileNotFoundException, org.jdom.JDOMException {
         Element root = rootFromName(name);
         _connection = root.getChild("connection");
         _gui = root.getChild("gui");
         _programmer = root.getChild("programmer");
     }
-    
+
     // access to the three elements
     public Element getConnectionElement() {
         return _connection;
     }
-    
+
     public Element getGuiElement() {
         return _gui;
     }
-    
+
     public Element getProgrammerElement() {
         return _programmer;
     }
-    
+
     Element _connection;
     Element _gui;
     Element _programmer;
-    
-    abstract public void writeFile(String name, AbstractConfigFrame f);
-    
+
+    public void writeFile(String name, AbstractConfigFrame f) {
+        try {
+            // This is taken in large part from "Java and XML" page 368
+
+            // create file Object
+            XmlFile.ensurePrefsPresent(XmlFile.prefsDir());
+            File file = new File(prefsDir()+name);
+
+            // create root element
+            Element root = new Element("preferences-config");
+            Document doc = new Document(root);
+            doc.setDocType(new DocType("preferences-config","preferences-config.dtd"));
+
+            // add connection element
+            Element values;
+            root.addContent(f.getConnection());
+
+            // add gui element
+            root.addContent(f.getGUI());
+
+            // add programmer element
+            root.addContent(f.getProgrammer());
+
+            // write the result to selected file
+            java.io.FileOutputStream o = new java.io.FileOutputStream(file);
+            XMLOutputter fmt = new XMLOutputter();
+            fmt.setNewlines(true);   // pretty printing
+            fmt.setIndent(true);
+            fmt.output(doc, o);
+            o.close();
+        }
+        catch (Exception e) {
+            log.error(e);
+            e.printStackTrace();
+        }
+    }
+
     abstract protected String configFileName();
-    
+
     public String defaultConfigFilename() { return configFileName();}
-    
+
     // initialize logging
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(AbstractConfigFile.class.getName());
-    
+
 }
