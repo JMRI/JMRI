2c19cb31846e355d169e3bc1dffa67f20901d97f
Bob Jacobsen <jacobsen@mac.com>
2003-05-07 04:53:13 +0000

    cleanup

--- jmri/jmrix/easydcc/EasyDccTurnoutManager.java
+++ jmri/jmrix/easydcc/EasyDccTurnoutManager.java
@@ -1,17 +1,18 @@
-/**
- * EasyDccTurnoutManager.java
- *
- * Description:		Implement turnout manager for EasyDcc systems
- * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Id: EasyDccTurnoutManager.java,v 1.7 2003-05-07 04:41:18 jacobsen Exp $
- */
-
-// System names are "ETnnn", where nnn is the turnout number without padding.
+// EasyDccTurnoutManager.java
 
 package jmri.jmrix.easydcc;
 
 import jmri.JmriException;
 import jmri.Turnout;
+
+/**
+ * Implement turnout manager for EasyDcc systems
+ * <P>
+ * System names are "ETnnn", where nnn is the turnout number without padding.
+ *
+ * @author	Bob Jacobsen Copyright (C) 2001
+ * @version	$Revision: 1.8 $
+ */
 
 public class EasyDccTurnoutManager extends jmri.AbstractTurnoutManager {
 
@@ -20,42 +21,42 @@ public class EasyDccTurnoutManager extends jmri.AbstractTurnoutManager {
         _instance = this;
     }
 
-	// to free resources when no longer used
-	public void dispose() {
-		super.dispose();
-	}
+    // to free resources when no longer used
+    public void dispose() {
+        super.dispose();
+    }
 
-	// EasyDcc-specific methods
+    // EasyDcc-specific methods
 
-	public void putBySystemName(EasyDccTurnout t) {
-		String system = prefix+t.getNumber();
-		_tsys.put(system, t);
-	}
+    public void putBySystemName(EasyDccTurnout t) {
+        String system = prefix+t.getNumber();
+        _tsys.put(system, t);
+    }
 
-	public Turnout newTurnout(String systemName, String userName) {
-		// if system name is null, supply one from the number in userName
-		if (systemName == null) systemName = prefix+userName;
+    public Turnout newTurnout(String systemName, String userName) {
+        // if system name is null, supply one from the number in userName
+        if (systemName == null) systemName = prefix+userName;
 
-		// return existing if there is one
-		Turnout t;
-		if ( (userName != null) && ((t = getByUserName(userName)) != null)) return t;
-		if ( (t = getBySystemName(systemName)) != null) return t;
+        // return existing if there is one
+        Turnout t;
+        if ( (userName != null) && ((t = getByUserName(userName)) != null)) return t;
+        if ( (t = getBySystemName(systemName)) != null) return t;
 
-		// get number from name
-		if (!systemName.startsWith(prefix)) {
-			log.error("Invalid system name for EasyDcc turnout: "+systemName);
-			return null;
-		}
-		int addr = Integer.valueOf(systemName.substring(2)).intValue();
-		t = new EasyDccTurnout(addr);
-		t.setUserName(userName);
+        // get number from name
+        if (!systemName.startsWith(prefix)) {
+            log.error("Invalid system name for EasyDcc turnout: "+systemName);
+            return null;
+        }
+        int addr = Integer.valueOf(systemName.substring(2)).intValue();
+        t = new EasyDccTurnout(addr);
+        t.setUserName(userName);
 
-		_tsys.put(systemName, t);
-		if (userName!=null) _tuser.put(userName, t);
-		t.addPropertyChangeListener(this);
+        _tsys.put(systemName, t);
+        if (userName!=null) _tuser.put(userName, t);
+        t.addPropertyChangeListener(this);
 
-		return t;
-	}
+        return t;
+    }
 
     static public EasyDccTurnoutManager instance() {
         if (_instance == null) _instance = new EasyDccTurnoutManager();
@@ -63,7 +64,7 @@ public class EasyDccTurnoutManager extends jmri.AbstractTurnoutManager {
     }
     static EasyDccTurnoutManager _instance = null;
 
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(EasyDccTurnoutManager.class.getName());
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(EasyDccTurnoutManager.class.getName());
 
 }
 
--- jmri/jmrix/lenz/XNetTurnoutManager.java
+++ jmri/jmrix/lenz/XNetTurnoutManager.java
@@ -1,12 +1,4 @@
-/**
- * XNetTurnoutManager.java
- *
- * Description:		Implement turnout manager
- * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.5 $
- */
-
-// System names are "XTnnn", where nnn is the turnout number without padding.
+// XNetTurnoutManager.java
 
 package jmri.jmrix.lenz;
 
@@ -19,78 +11,77 @@ import jmri.Turnout;
  * System names are "XTnnn", where nnn is the turnout number without padding.
  *
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.5 $
+ * @version			$Revision: 1.6 $
  */
 public class XNetTurnoutManager extends jmri.AbstractTurnoutManager implements XNetListener {
-
-	// ctor has to register for XNet events
-	public XNetTurnoutManager() {
-            prefix = "XT";
-            _instance = this;
-            XNetTrafficController.instance().addXNetListener(~0, this);
-	}
-
-	// to free resources when no longer used
-	public void dispose() {
-	}
-
-	// XNet-specific methods
-
-	public void putBySystemName(XNetTurnout t) {
-		String system = prefix+t.getNumber();
-		_tsys.put(system, t);
-	}
-
-	public Turnout newTurnout(String systemName, String userName) {
-		// if system name is null, supply one from the number in userName
-		if (systemName == null) systemName = prefix+userName;
-
-		// return existing if there is one
-		Turnout t;
-		if ( (userName!=null) && ((t = getByUserName(userName)) != null)) return t;
-		if ( (t = getBySystemName(systemName)) != null) return t;
-
-		// get number from name
-		if (!systemName.startsWith(prefix)) {
-			log.error("Invalid system name for XPressNet turnout: "+systemName);
-			return null;
-		}
-		int addr = Integer.valueOf(systemName.substring(2)).intValue();
-		t = new XNetTurnout(addr);
-		t.setUserName(userName);
-
-		_tsys.put(systemName, t);
-		if (userName!=null) _tuser.put(userName, t);
-		t.addPropertyChangeListener(this);
-
-		return t;
-	}
-
-	// listen for turnouts, creating them as needed
-	public void message(XNetMessage l) {
-		// parse message type
-		int addr = XNetTrafficController.instance()
-                    .getCommandStation().getTurnoutMsgAddr(l);
+    
+    // ctor has to register for XNet events
+    public XNetTurnoutManager() {
+        prefix = "XT";
+        _instance = this;
+        XNetTrafficController.instance().addXNetListener(~0, this);
+    }
+    
+    // to free resources when no longer used
+    public void dispose() {
+    }
+    
+    // XNet-specific methods
+    
+    public void putBySystemName(XNetTurnout t) {
+        String system = prefix+t.getNumber();
+        _tsys.put(system, t);
+    }
+    
+    public Turnout newTurnout(String systemName, String userName) {
+        // if system name is null, supply one from the number in userName
+        if (systemName == null) systemName = prefix+userName;
+        
+        // return existing if there is one
+        Turnout t;
+        if ( (userName!=null) && ((t = getByUserName(userName)) != null)) return t;
+        if ( (t = getBySystemName(systemName)) != null) return t;
+        
+        // get number from name
+        if (!systemName.startsWith(prefix)) {
+            log.error("Invalid system name for XPressNet turnout: "+systemName);
+            return null;
+        }
+        int addr = Integer.valueOf(systemName.substring(2)).intValue();
+        t = new XNetTurnout(addr);
+        t.setUserName(userName);
+        
+        _tsys.put(systemName, t);
+        if (userName!=null) _tuser.put(userName, t);
+        t.addPropertyChangeListener(this);
+        
+        return t;
+    }
+    
+    // listen for turnouts, creating them as needed
+    public void message(XNetMessage l) {
+        // parse message type
+        int addr = XNetTrafficController.instance()
+            .getCommandStation().getTurnoutMsgAddr(l);
         if (log.isDebugEnabled()) log.debug("message had address: "+addr);
         if (addr<=0)  return; // indicates no message
-		// reach here for switch command; make sure we know about this one
-		String s = prefix+addr;
-		if (null == getBySystemName(s)) {
-			// need to store a new one
-			XNetTurnout t = new XNetTurnout(addr);
-			putBySystemName(t);
-		}
-	}
-
+        // reach here for switch command; make sure we know about this one
+        String s = prefix+addr;
+        if (null == getBySystemName(s)) {
+            // need to store a new one
+            XNetTurnout t = new XNetTurnout(addr);
+            putBySystemName(t);
+        }
+    }
+    
     static public XNetTurnoutManager instance() {
         if (_instance == null) _instance = new XNetTurnoutManager();
         return _instance;
     }
     static XNetTurnoutManager _instance = null;
-
-	 static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetTurnoutManager.class.getName());
-
+    
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XNetTurnoutManager.class.getName());
+    
 }
-
 
 /* @(#)XNetTurnoutManager.java */
