3fc95bb39be9dab19c5ac5ab1d7b0cd153e2e5f2
Bob Jacobsen <jacobsen@mac.com>
2003-05-09 06:01:54 +0000

    add getSystemNameList member

--- jmri/AbstractSensorManager.java
+++ jmri/AbstractSensorManager.java
@@ -2,37 +2,55 @@
 
 package jmri;
 
+import java.util.*;
 import java.util.Hashtable;
+
+import com.sun.java.util.collections.ArrayList;
+import com.sun.java.util.collections.List;
 
 /**
  * Abstract base implementation of the SensorManager interface
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.4 $
+ * @version			$Revision: 1.5 $
  */
 public abstract class AbstractSensorManager implements SensorManager{
 
     // abstract methods to be provided by subclasses
     public abstract Sensor newSensor(String systemName, String userName);
-    
+
     // abstract methods to be extended by subclasses
     // to free resources when no longer used
     public void dispose() throws JmriException {
         _tsys.clear();
         _tuser.clear();
     }
-    
+
     // implemented methods
     protected Hashtable _tsys = new Hashtable();   // stores known Sensor instances by system name
     protected Hashtable _tuser = new Hashtable();   // stores known Sensor instances by user name
-    
+
     public Sensor getBySystemName(String key) {
         return (Sensor)_tsys.get(key);
     }
-    
+
     public Sensor getByUserName(String key) {
         return (Sensor)_tuser.get(key);
     }
-    
+
+    public List getSystemNameList() {
+        String[] arr = new String[_tsys.size()];
+        List out = new ArrayList();
+        Enumeration en = _tsys.elements();
+        int i=0;
+        while (en.hasMoreElements()) {
+            arr[i] = ((Sensor)en.nextElement()).getSystemName();
+            i++;
+        }
+        java.util.Arrays.sort(arr);
+        for (i=0; i<arr.length; i++) out.add(arr[i]);
+        return out;
+    }
+
 }
 
 /* @(#)AbstractTurnoutManager.java */
