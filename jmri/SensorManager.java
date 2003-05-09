f1e594a711a656fdc43d1f18f5e2005f692cb7c2
Bob Jacobsen <jacobsen@mac.com>
2003-05-09 06:01:30 +0000

    cleanup.  Add getSystemNameList member

--- jmri/SensorManager.java
+++ jmri/SensorManager.java
@@ -2,20 +2,24 @@
 
 package jmri;
 
+import com.sun.java.util.collections.List;
+
 /**
  * Interface for controlling sensors
- * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.4 $
+ * @author	Bob Jacobsen Copyright (C) 2001
+ * @version	$Revision: 1.5 $
  */
 public interface SensorManager {
 
-	// to free resources when no longer used
-	public void dispose() throws JmriException;
+    // to free resources when no longer used
+    public void dispose() throws JmriException;
 
-	public Sensor newSensor(String systemName, String userName);
+    public Sensor newSensor(String systemName, String userName);
 
-	public Sensor getByUserName(String s);
-	public Sensor getBySystemName(String s);
+    public Sensor getByUserName(String s);
+    public Sensor getBySystemName(String s);
+
+    public List getSystemNameList();
 
 }
 
