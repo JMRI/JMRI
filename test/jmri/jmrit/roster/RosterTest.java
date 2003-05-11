8f35605332272639b3e9b1ea6dfeecfbdcafb0f1
Bob Jacobsen <jacobsen@mac.com>
2003-05-11 03:34:26 +0000

    cleanup before adding directories to roster. Tests now pass

--- test/jmri/jmrit/roster/RosterTest.java
+++ test/jmri/jmrit/roster/RosterTest.java
@@ -10,24 +10,24 @@ import junit.framework.*;
 /**
  * Tests for the jmrit.roster package & jmrit.roster.Roster class
  * @author	Bob Jacobsen
- * @version     $Revision: 1.7 $
+ * @version     $Revision: 1.8 $
  */
 public class RosterTest extends TestCase {
-
+    
     public void testDirty() {
         Roster r = new Roster();
         Assert.assertEquals("new object ", false, r.isDirty());
         r.addEntry(null);
         Assert.assertEquals("after add ", true, r.isDirty());
     }
-
+    
     public void testAdd() {
         Roster r = new Roster();
         Assert.assertEquals("empty length ", 0, r.numEntries());
         r.addEntry(new RosterEntry("file name Bob"));
         Assert.assertEquals("one item ", 1, r.numEntries());
     }
-
+    
     public void testAddrSearch() {
         Roster r = new Roster();
         RosterEntry e = new RosterEntry("file name Bob");
@@ -36,7 +36,7 @@ public class RosterTest extends TestCase {
         Assert.assertEquals("search not OK ", false, r.checkEntry(0, null, "321", null, null, null, null, null));
         Assert.assertEquals("search OK ", true, r.checkEntry(0, null, "123", null, null, null, null, null));
     }
-
+    
     public void testSearchList() {
         Roster r = new Roster();
         RosterEntry e;
@@ -58,11 +58,11 @@ public class RosterTest extends TestCase {
         Assert.assertEquals("search for 1 ", 1, r.matchingList("UP", null,  null, null, null, null, null).size());
         Assert.assertEquals("search for 3 ", 3, r.matchingList(null, "123", null, null, null, null, null).size());
     }
-
+    
     public void testBackupFile() throws Exception {
         // this test uses explicit filenames intentionally, to ensure that
         // the resulting files go into the test tree area.
-
+        
         // create a file in "temp"
         XmlFile.ensurePrefsPresent(XmlFile.prefsDir());
         XmlFile.ensurePrefsPresent(XmlFile.prefsDir()+"temp");
@@ -74,14 +74,14 @@ public class RosterTest extends TestCase {
         String contents = "stuff"+"           ";
         PrintStream p = new PrintStream (new FileOutputStream(f));
         p.println(contents);
-
+        
         // now do the backup
         Roster r = new Roster() {
                 public String backupFileName(String name)
                 { return XmlFile.prefsDir()+File.separator+"temp"+File.separator+"rosterBackupTest"; }
             };
         r.makeBackupFile("temp"+File.separator+"roster.xml");
-
+        
         // and check
         InputStream in = new FileInputStream(new File(XmlFile.prefsDir()+"temp"+File.separator+"rosterBackupTest"));
         Assert.assertEquals("read 0 ", contents.charAt(0), in.read());
@@ -89,12 +89,12 @@ public class RosterTest extends TestCase {
         Assert.assertEquals("read 2 ", contents.charAt(2), in.read());
         Assert.assertEquals("read 3 ", contents.charAt(3), in.read());
     }
-
+    
     public void testReadWrite() throws Exception {
         // this test uses explicit filenames intentionally, to ensure that
         // the resulting files go into the test tree area.  This is not
         // a test of prefsDir, and shouldn't use that.
-
+        
         // store files in "temp"
         XmlFile.ensurePrefsPresent(XmlFile.prefsDir());
         XmlFile.ensurePrefsPresent(XmlFile.prefsDir()+"temp");
@@ -102,7 +102,7 @@ public class RosterTest extends TestCase {
         File f = new File(XmlFile.prefsDir()+"temp"+File.separator+"rosterTest.xml");
         // remove existing roster if its there
         f.delete();
-
+        
         // create a roster
         Roster r = new Roster();
         RosterEntry e;
@@ -120,33 +120,33 @@ public class RosterTest extends TestCase {
         e.setRoadNumber("123");
         e.setRoadName("UP");
         r.addEntry(e);
-
+        
         // write it
         r.writeFile("temp"+File.separator+"rosterTest.xml");
-
+        
         // create new roster & read
         Roster t = new Roster();
         t.readFile("temp"+File.separator+"rosterTest.xml");
-
+        
         // check contents
         Assert.assertEquals("search for 0 ", 0, t.matchingList(null, "321", null, null, null, null, null).size());
         Assert.assertEquals("search for 1 ", 1, t.matchingList("UP", null,  null, null, null, null, null).size());
         Assert.assertEquals("search for 3 ", 3, t.matchingList(null, "123", null, null, null, null, null).size());
     }
-
-
+    
+    
     // from here down is testing infrastructure
-
+    
     public RosterTest(String s) {
         super(s);
     }
-
+    
     // Main entry point
     static public void main(String[] args) {
         String[] testCaseName = {Roster.class.getName()};
         junit.swingui.TestRunner.main(testCaseName);
     }
-
+    
     // test suite from all defined tests
     public static Test suite() {
         TestSuite suite = new TestSuite(RosterTest.class);
@@ -155,7 +155,7 @@ public class RosterTest extends TestCase {
         suite.addTest(jmri.jmrit.roster.RosterEntryTest.suite());
         return suite;
     }
-
+    
     // The minimal setup for log4J
     apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
     protected void setUp() { log4jfixtureInst.setUp(); }
