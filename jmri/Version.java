646f011fbca8142751ed9760b6a0dc9c22b5be3f
Bob Jacobsen <jacobsen@mac.com>
2003-05-22 03:42:36 +0000

    1.1.13

--- jmri/Version.java
+++ jmri/Version.java
@@ -3,9 +3,9 @@ package jmri;
 /**
  * Defines a simple place to get the JMRI version string
  * @author  Bob Jacobsen   Copyright 2002
- * @version $Revision: 1.24 $
+ * @version $Revision: 1.25 $
  */
 
 public class Version {
-    static public String name() { return "1.1.12"; }
+    static public String name() { return "1.1.13"; }
 }
