3fcb1977a87cfc0a04891377109a43f2633d6f01
Alex Shepherd <kiwi64ajs@gmail.com>
2003-05-23 11:12:59 +0000

    initial checkin

---
+++ jmri/jmrit/FileChooserFilter.java
@@ -0,0 +1,56 @@
+package jmri.jmrit;
+
+import java.io.FileFilter;
+import java.io.File;
+import java.util.HashSet;
+
+/**
+ * <p> </p>
+ * <p> </p>
+ * <p> </p>
+ * <p> </p>
+ * @author unascribed
+ * @version $Revision: 1.1 $
+ */
+
+public class FileChooserFilter extends javax.swing.filechooser.FileFilter {
+
+  String mDescription ;
+  HashSet allowedExtensions ;
+
+  public FileChooserFilter( String pDescription ) {
+    mDescription = pDescription ;
+    allowedExtensions = new HashSet() ;
+  }
+
+  public void addExtension( String ext ){
+    allowedExtensions.add( ext ) ;
+  }
+
+  private String getFileExtension(File f) {
+    if(f != null) {
+      String filename = f.getName();
+      int i = filename.lastIndexOf( '.' );
+      if( i > 0 && i < filename.length() - 1) {
+        return filename.substring( i + 1 ).toLowerCase();
+      };
+    }
+    return null;
+  }
+
+  public String getDescription(){
+    return mDescription ;
+  }
+
+  public boolean accept( File f ){
+    if(f != null) {
+      if(f.isDirectory()) {
+        return true;
+      }
+      String extension = getFileExtension( f );
+      if(extension != null && allowedExtensions.contains( extension ) )
+        return true;
+    }
+    return false;
+  }
+}
