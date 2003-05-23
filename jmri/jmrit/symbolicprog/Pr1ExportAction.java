efe770d1775367d21b99b3e27097b88c796a60d1
Bob Jacobsen <jacobsen@mac.com>
2003-05-23 15:50:02 +0000

    First attempt at writing the PR1 output format

--- jmri/jmrit/symbolicprog/Pr1ExportAction.java
+++ jmri/jmrit/symbolicprog/Pr1ExportAction.java
@@ -4,37 +4,72 @@ package jmri.jmrit.symbolicprog;
 
 import java.awt.event.*;
 import java.util.*;
+import java.io.*;
 
 import javax.swing.*;
 
 /**
- * Action to import the CV values from a PR1WIN/PR1DOS data file.
+ * Action to export the CV values to a PR1WIN data file.
+ * <P>
+ * Note that this format is somewhat different from the PR1DOS format, and
+ * it's not clear they will interoperate.
  *
  * @author	Bob Jacobsen   Copyright (C) 2003
- * @version     $Revision: 1.1 $
+ * @version     $Revision: 1.2 $
  */
 public class Pr1ExportAction  extends AbstractAction {
 
-    public Pr1ExportAction(String actionName, CvTableModel pModel) {
+    /**
+     * Create the action
+     * @param actionName String name to be displayed in menus, etc
+     * @param pModel  CvTableModel that contains the data to (eventually) be exported
+     * @param pParent JFrame that will eventually invoke the action, used to anchor a file dialog
+     */
+    public Pr1ExportAction(String actionName, CvTableModel pModel, JFrame pParent) {
         super(actionName);
         mModel = pModel;
+        mParent = pParent ;
     }
+
+    JFileChooser fileChooser ;
+    JFrame mParent ;
 
     /**
      * CvTableModel to load
      */
     CvTableModel mModel;
 
+
     public void actionPerformed(ActionEvent e) {
 
-        log.debug("start to export PR1 file");
+        if ( fileChooser == null ){
+            fileChooser = new JFileChooser() ;
+        }
 
-        // Sample of how to access the existing CV values:
-        for (int i=0; i<mModel.getRowCount(); i++) {
-            CvValue cv = mModel.getCvByRow(i);
-            int num = cv.number();
-            int value = cv.getValue();
-            log.debug("CV"+num+" = "+value);
+        int retVal = fileChooser.showSaveDialog( mParent ) ;
+
+        if(retVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            if (log.isDebugEnabled())  log.debug("start to export to PR1 file "+file);
+
+            try {
+
+                PrintStream str = new PrintStream(new FileOutputStream(file));
+
+                str.println("[DecoderData]");
+                for (int i=0; i<mModel.getRowCount(); i++) {
+                    CvValue cv = mModel.getCvByRow(i);
+                    int num = cv.number();
+                    int value = cv.getValue();
+                    str.println("CV"+num+"="+value);
+                }
+                str.println("Version=0");
+                str.flush();
+                str.close();
+            }
+            catch (IOException ex) {
+                log.error("Error writing file: "+ex);
+            }
         }
     }
 
--- jmri/jmrit/symbolicprog/Pr1ImportAction.java
+++ jmri/jmrit/symbolicprog/Pr1ImportAction.java
@@ -12,49 +12,49 @@ import jmri.jmrit.FileChooserFilter;
  * Action to import the CV values from a PR1WIN/PR1DOS data file.
  *
  * @author	Bob Jacobsen   Copyright (C) 2003
- * @version     $Revision: 1.2 $
+ * @version     $Revision: 1.3 $
  */
 
 public class Pr1ImportAction  extends AbstractAction {
-
-  CvTableModel mModel;
-  JFrame mParent ;
-  FileChooserFilter fileFilter ;
-  JFileChooser fileChooser ;
-
+    
+    CvTableModel mModel;
+    JFrame mParent ;
+    FileChooserFilter fileFilter ;
+    JFileChooser fileChooser ;
+    
     public Pr1ImportAction(String actionName, CvTableModel pModel, JFrame pParent) {
         super(actionName);
         mModel = pModel;
         mParent = pParent ;
-
+        
     }
-
+    
     public void actionPerformed(ActionEvent e) {
-
-      log.debug("start to import PR1 file");
-
-      if( fileChooser == null ){
-        fileChooser = new JFileChooser() ;
-        fileFilter = new FileChooserFilter( "PR1 Filter" ) ;
-        fileFilter.addExtension( "dec" );
-
-        fileChooser.setFileFilter( fileFilter );
-      }
-
-      int retVal = fileChooser.showOpenDialog( mParent ) ;
-
-      if(retVal == JFileChooser.APPROVE_OPTION) {
-        File file = fileChooser.getSelectedFile();
-        if (log.isDebugEnabled()) log.debug("Import from PR1 file: " + file );
-
-        try {
-          Pr1Importer importer = new Pr1Importer( file ) ;
-          importer.setCvTable( mModel ) ;
+        
+        log.debug("start to import PR1 file");
+        
+        if( fileChooser == null ){
+            fileChooser = new JFileChooser() ;
+            fileFilter = new FileChooserFilter( "PR1 Filter" ) ;
+            fileFilter.addExtension( "dec" );
+            
+            fileChooser.setFileFilter( fileFilter );
         }
-        catch (IOException ex) {
+        
+        int retVal = fileChooser.showOpenDialog( mParent ) ;
+        
+        if(retVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            if (log.isDebugEnabled()) log.debug("Import from PR1 file: " + file );
+            
+            try {
+                Pr1Importer importer = new Pr1Importer( file ) ;
+                importer.setCvTable( mModel ) ;
+            }
+            catch (IOException ex) {
+            }
         }
-      }
     }
-
+    
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(Pr1ImportAction.class.getName());
 }
