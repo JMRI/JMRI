7234c2f1e781c6978d9ddb79bf1bc378770ee6dd
Bob Jacobsen <jacobsen@mac.com>
2003-05-30 19:12:43 +0000

    new sections, include yellow

---
+++ apps/cornwall/CrrSection7B.java
@@ -0,0 +1,43 @@
+// CrrSection7B.java
+
+package apps.cornwall;
+
+import jmri.*;
+
+/**
+ * Automate section 7B of the Cornwall RR.
+ * <P>
+ * Based on Crr0029.bas
+ *
+ * @author	Bob Jacobsen    Copyright (C) 2003
+ * @version     $Revision: 1.1 $
+ */
+public class CrrSection7B extends CrrSection {
+
+    void defineIO() {
+        sig  = InstanceManager.signalHeadManagerInstance().getByUserName("Signal 7B");
+        inputs = new NamedBean[]{ bo[3], si[6] };
+    }
+
+    /**
+     * Set outputs to match the sensor state
+     */
+    void setOutput() {
+        boolean bo3  = ( bo[ 3].getKnownState() == Sensor.ACTIVE);
+        boolean si6  = ( si[ 6].getCommandedState() == THROWN);
+
+        int value = RED;
+        if ( bo3 ) {
+            value = RED;
+        } else {
+            value = GREEN;
+        }
+
+        if (value == GREEN && si6)
+            value = YELLOW;
+
+        sig.setAppearance(value);
+    }
+}
+
+/* @(#)CrrSection7B.java */
---
+++ apps/cornwall/CrrSection9B.java
@@ -0,0 +1,51 @@
+// CrrSection9B.java
+
+package apps.cornwall;
+
+import jmri.*;
+
+/**
+ * Automate section 9B of the Cornwall RR.
+ * <P>
+ * Based on Crr0029.bas
+ *
+ * @author	Bob Jacobsen    Copyright (C) 2003
+ * @version     $Revision: 1.1 $
+ */
+public class CrrSection9B extends CrrSection {
+
+    void defineIO() {
+        sig  = InstanceManager.signalHeadManagerInstance().getByUserName("Signal 9B");
+        inputs = new NamedBean[]{ tu[14], tu[15], bo[1], bo[20], bo[21], si[60] };
+    }
+
+    /**
+     * Set outputs to match the sensor state
+     */
+    void setOutput() {
+        boolean tu14 = ( tu[14].getKnownState() == Sensor.ACTIVE);
+        boolean tu15 = ( tu[15].getKnownState() == Sensor.ACTIVE);
+
+        boolean bo1  = ( bo[ 1].getKnownState() == Sensor.ACTIVE);
+        boolean bo20 = ( bo[20].getKnownState() == Sensor.ACTIVE);
+        boolean bo21 = ( bo[21].getKnownState() == Sensor.ACTIVE);
+
+        boolean si60 = ( si[60].getCommandedState() == THROWN);
+
+        int value = RED;
+        if ( bo1 || !tu14 || !tu15 ) {
+            value = RED;
+        } else if ( bo20 && bo21 ) {
+            value = RED;
+        } else {
+            value = GREEN;
+        }
+
+        if (value == GREEN && si60)
+            value = YELLOW;
+
+        sig.setAppearance(value);
+    }
+}
+
+/* @(#)CrrSection9B.java */
