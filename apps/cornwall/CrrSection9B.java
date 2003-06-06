a7574d3b4e827bf87d27840d2a4e1d7908fff243
Bob Jacobsen <jacobsen@mac.com>
2003-06-06 18:06:17 +0000

    update form. No longer specifically liks crr029.bas, this has corrected logic

--- apps/cornwall/CrrSection9B.java
+++ apps/cornwall/CrrSection9B.java
@@ -10,13 +10,13 @@ import jmri.*;
  * Based on Crr0029.bas
  *
  * @author	Bob Jacobsen    Copyright (C) 2003
- * @version     $Revision: 1.2 $
+ * @version     $Revision: 1.3 $
  */
 public class CrrSection9B extends CrrSection {
 
     void defineIO() {
         sig  = InstanceManager.signalHeadManagerInstance().getByUserName("Signal 9B");
-        inputs = new NamedBean[]{ tu[14], tu[15], bo[1], bo[20], bo[21], si[60] };
+        inputs = new NamedBean[]{ tu[14], tu[15], bo[1], si[60] };
     }
 
     /**
@@ -27,15 +27,11 @@ public class CrrSection9B extends CrrSection {
         boolean tu15 = ( tu[15].getKnownState() == Sensor.ACTIVE);
 
         boolean bo1  = ( bo[ 1].getKnownState() == Sensor.ACTIVE);
-        boolean bo20 = ( bo[20].getKnownState() == Sensor.ACTIVE);
-        boolean bo21 = ( bo[21].getKnownState() == Sensor.ACTIVE);
 
         boolean si60 = ( si[60].getCommandedState() == THROWN);
 
         int value = GREEN;
         if ( bo1 || !tu14 || !tu15 )
-            value = RED;
-        else if ( bo20 && bo21 )
             value = RED;
 
         if (value == GREEN && si60)
