50a99c516ee0f80c464791c94e3de361569719a0
Bob Jacobsen <jacobsen@mac.com>
2003-06-08 02:00:54 +0000

    update for Mac Classic compatibility

--- jmri/jmrit/symbolicprog/Pr1ImportAction.java
+++ jmri/jmrit/symbolicprog/Pr1ImportAction.java
@@ -12,41 +12,44 @@ import jmri.jmrit.FileChooserFilter;
  * Action to import the CV values from a PR1WIN/PR1DOS data file.
  *
  * @author	Bob Jacobsen   Copyright (C) 2003
- * @version     $Revision: 1.3 $
+ * @version     $Revision: 1.4 $
  */
 
 public class Pr1ImportAction  extends AbstractAction {
-    
+
     CvTableModel mModel;
     JFrame mParent ;
     FileChooserFilter fileFilter ;
     JFileChooser fileChooser ;
-    
+
     public Pr1ImportAction(String actionName, CvTableModel pModel, JFrame pParent) {
         super(actionName);
         mModel = pModel;
         mParent = pParent ;
-        
+
     }
-    
+
     public void actionPerformed(ActionEvent e) {
-        
+
         log.debug("start to import PR1 file");
-        
+
         if( fileChooser == null ){
             fileChooser = new JFileChooser() ;
-            fileFilter = new FileChooserFilter( "PR1 Filter" ) ;
-            fileFilter.addExtension( "dec" );
-            
-            fileChooser.setFileFilter( fileFilter );
+            try {  // This may fail on old JVMs, in which case we provide no filter
+                fileFilter = new FileChooserFilter( "PR1 Filter" ) ;
+                fileFilter.addExtension( "dec" );
+
+                fileChooser.setFileFilter( fileFilter );
+            } catch (NoClassDefFoundError ex) {}
+
         }
-        
+
         int retVal = fileChooser.showOpenDialog( mParent ) ;
-        
+
         if(retVal == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
             if (log.isDebugEnabled()) log.debug("Import from PR1 file: " + file );
-            
+
             try {
                 Pr1Importer importer = new Pr1Importer( file ) ;
                 importer.setCvTable( mModel ) ;
@@ -55,6 +58,6 @@ public class Pr1ImportAction  extends AbstractAction {
             }
         }
     }
-    
+
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(Pr1ImportAction.class.getName());
 }
