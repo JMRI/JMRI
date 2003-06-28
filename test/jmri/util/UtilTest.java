838ab3e0173b4adf73b2a0928e5d1547636e409c
Bob Jacobsen <jacobsen@mac.com>
2003-06-28 18:12:01 +0000

    new root test routine for jmri.util tree

---
+++ test/jmri/util/UtilTest.java
@@ -0,0 +1,39 @@
+// UtilTest.java
+
+package jmri.util;
+
+import junit.framework.*;
+
+/**
+ * Invokes complete set of tests in the jmri.util tree
+ *
+ * @author	    Bob Jacobsen  Copyright 2003
+ * @version         $Revision: 1.1 $
+ */
+public class UtilTest extends TestCase {
+
+    // from here down is testing infrastructure
+    public UtilTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {UtilTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite("jmri.util.UtilTest");   // no tests in this class itself
+        suite.addTest(jmri.util.StringUtilTest.suite());
+
+        return suite;
+    }
+
+    // The minimal setup for log4J
+    apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
+    protected void setUp() { log4jfixtureInst.setUp(); }
+    protected void tearDown() { log4jfixtureInst.tearDown(); }
+
+}
