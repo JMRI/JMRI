d9e7aee66fd798a694783420b7a9baf0ec67ae5c
Bob Jacobsen <jacobsen@mac.com>
2003-06-28 18:14:41 +0000

    cleanup, include new util tree

--- test/jmri/JmriTest.java
+++ test/jmri/JmriTest.java
@@ -2,42 +2,38 @@
 
 package jmri;
 
-import java.io.*;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import org.jdom.*;
-import org.jdom.output.*;
+import junit.framework.*;
 
 /**
  * Invoke complete set of tests for the Jmri package
- * @author			Bob Jacobsen, Copyright (C) 2001, 2002
- * @version         $Revision: 1.5 $
+ * @author	Bob Jacobsen, Copyright (C) 2001, 2002
+ * @version         $Revision: 1.6 $
  */
 public class JmriTest extends TestCase {
 
-	// from here down is testing infrastructure
+    // from here down is testing infrastructure
 
-	public JmriTest(String s) {
-		super(s);
-	}
+    public JmriTest(String s) {
+        super(s);
+    }
 
-	// Main entry point
-	static public void main(String[] args) {
-		String[] testCaseName = {JmriTest.class.getName()};
-		junit.swingui.TestRunner.main(testCaseName);
-	}
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {JmriTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
 
-	// test suite from all defined tests
-	public static Test suite() {
-		apps.tests.AllTest.initLogging();
-		TestSuite suite = new TestSuite("jmri.JmriTest");  // no tests in this class itself
-		suite.addTest(jmri.progdebugger.ProgDebuggerTest.suite());
-		suite.addTest(jmri.NmraPacketTest.suite());
-		suite.addTest(jmri.configurexml.ConfigXmlTest.suite());
-		suite.addTest(jmri.jmrit.JmritTest.suite());
-		suite.addTest(jmri.jmrix.JmrixTest.suite());
-		return suite;
-	}
+    // test suite from all defined tests
+    public static Test suite() {
+        apps.tests.AllTest.initLogging();
+        TestSuite suite = new TestSuite("jmri.JmriTest");  // no tests in this class itself
+        suite.addTest(jmri.progdebugger.ProgDebuggerTest.suite());
+        suite.addTest(jmri.NmraPacketTest.suite());
+        suite.addTest(jmri.configurexml.ConfigXmlTest.suite());
+        suite.addTest(jmri.util.UtilTest.suite());
+        suite.addTest(jmri.jmrit.JmritTest.suite());
+        suite.addTest(jmri.jmrix.JmrixTest.suite());  // last due to threading issues?
+        return suite;
+    }
 
 }
