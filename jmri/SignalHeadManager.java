afccdb2bbcf0e35e4098d29ac527f0198a821615
Bob Jacobsen <jacobsen@mac.com>
2003-07-21 01:26:46 +0000

    dispose doesnt throww, inherit from Manager

--- jmri/SignalHeadManager.java
+++ jmri/SignalHeadManager.java
@@ -7,16 +7,16 @@ import com.sun.java.util.collections.List;
 /**
  * Interface for obtaining signal heads.
  * <P>
- * This doesn't have a "new" methods, as SignalHeads
+ * This doesn't have a "new" method, as SignalHeads
  * are separately implemented, instead of being system-specific.
  *
  * @author      Bob Jacobsen Copyright (C) 2001
- * @version	$Revision: 1.3 $
+ * @version	$Revision: 1.4 $
  */
-public interface SignalHeadManager {
+public interface SignalHeadManager extends Manager {
 
     // to free resources when no longer used
-    public void dispose() throws JmriException;
+    public void dispose();
 
     public SignalHead getByUserName(String s);
     public SignalHead getBySystemName(String s);
