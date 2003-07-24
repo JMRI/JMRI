e5860cc20dcab28f06bef1dde63635005c18fb91
Bob Jacobsen <jacobsen@mac.com>
2003-07-24 12:33:37 +0000

    I8N, refactor menus

---
+++ jmri/jmrit/DebugMenu.java
@@ -0,0 +1,44 @@
+/**
+ * DebugMenu.java
+ */
+
+package jmri.jmrit;
+
+import javax.swing.*;
+import java.util.*;
+
+/**
+ * Create a "Debug" menu containing the Jmri system-independent debugging tools
+ *
+ * @author	Bob Jacobsen   Copyright 2003
+ * @version     $Revision: 1.1 $
+ */
+public class DebugMenu extends JMenu {
+    public DebugMenu(String name, JPanel panel) {
+        this(panel);
+        setText(name);
+    }
+
+    public DebugMenu(JPanel panel) {
+
+        super();
+
+        ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrit.JmritDebugBundle");
+
+        setText(rb.getString("MenuDebug"));
+
+        add(new jmri.jmrit.MemoryFrameAction(rb.getString("MenuItemMemoryUsageMonitor")));
+        add(new JSeparator());
+        add(new jmri.jmrit.decoderdefn.DecoderIndexCreateAction(rb.getString("MenuItemRecreateDecoderIndex")));
+        add(new jmri.jmrit.roster.RecreateRosterAction(rb.getString("MenuItemRecreateRoster")));
+        add(new JSeparator());
+        add(new jmri.jmrit.XmlFileCheckAction(rb.getString("MenuItemCheckXMLFile"), panel));
+        add(new jmri.jmrit.decoderdefn.NameCheckAction(rb.getString("MenuItemCheckDecoderNames"), panel));
+        add(new jmri.jmrit.symbolicprog.tabbedframe.ProgCheckAction(rb.getString("MenuItemCheckProgrammerNames"), panel));
+
+    }
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(DebugMenu.class.getName());
+}
+
+
