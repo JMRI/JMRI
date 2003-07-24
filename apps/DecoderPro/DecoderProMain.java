5733a46b2feaa59e7b7fc54bdf0e8c2e219e98ec
Bob Jacobsen <jacobsen@mac.com>
2003-07-24 12:38:08 +0000

    I8N. Move to simpler menu system. Fix typo in program splash screen

--- apps/DecoderPro/DecoderProMain.java
+++ apps/DecoderPro/DecoderProMain.java
@@ -2,23 +2,28 @@
 
 package apps.DecoderPro;
 
-import java.awt.*;
-import java.awt.event.*;
+import jmri.util.oreilly.BasicWindowMonitor;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.swing.*;
-
-import jmri.util.oreilly.*;
 
 /**
  * DecoderPro application main class.
  *
  * @author                      Bob Jacobsen
- * @version                     $Revision: 1.4 $
+ * @version                     $Revision: 1.5 $
  */
 public class DecoderProMain extends JPanel {
     public DecoderProMain(JFrame frame) {
 
         super(true);
+
+        ResourceBundle rb = ResourceBundle.getBundle("apps.AppsBundle");
 
         // create basic GUI
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -28,21 +33,21 @@ public class DecoderProMain extends JPanel {
 
         // load preferences
         DecoderProConfigAction prefs
-            = new DecoderProConfigAction("Preferences...");
+            = new DecoderProConfigAction(rb.getString("MenuItemPreferences"));
 
         // populate GUI
 
         // create actions with side-effects if you need to reference them more than once
-        Action serviceprog = new jmri.jmrit.symbolicprog.tabbedframe.PaneProgAction("Use programming track ...");
-        Action opsprog = new jmri.jmrit.symbolicprog.tabbedframe.PaneOpsProgAction("Program on main track ...");
-        Action quit = new AbstractAction("Quit"){
+        Action serviceprog = new jmri.jmrit.symbolicprog.tabbedframe.PaneProgAction(rb.getString("DpButtonUseProgrammingTrack"));
+        Action opsprog = new jmri.jmrit.symbolicprog.tabbedframe.PaneOpsProgAction(rb.getString("DpButtonProgramOnMainTrack"));
+        Action quit = new AbstractAction(rb.getString("MenuItemQuit")){
                 public void actionPerformed(ActionEvent e) {
                     System.exit(0);
                 }
             };
 
         // Create menu categories and add to the menu bar, add actions to menus
-        JMenu fileMenu = new JMenu("File");
+        JMenu fileMenu = new JMenu(rb.getString("MenuFile"));
         menuBar.add(fileMenu);
         fileMenu.add(serviceprog);
         fileMenu.add(opsprog);
@@ -50,45 +55,24 @@ public class DecoderProMain extends JPanel {
         fileMenu.add(new jmri.jmrit.decoderdefn.PrintDecoderListAction(frame));
         fileMenu.add(quit);
 
-        JMenu editMenu = new JMenu("Edit");
+        JMenu editMenu = new JMenu(rb.getString("MenuEdit"));
         menuBar.add(editMenu);
         editMenu.add(prefs);
 
-        menuBar.add(new jmri.jmrit.roster.RosterMenu("Roster", jmri.jmrit.roster.RosterMenu.MAINMENU, this));
+        menuBar.add(new jmri.jmrit.roster.RosterMenu(rb.getString("MenuRoster"), jmri.jmrit.roster.RosterMenu.MAINMENU, this));
 
-        JMenu toolMenu = new JMenu("Tools");
-        menuBar.add(toolMenu);
-        toolMenu.add(new jmri.jmrit.powerpanel.PowerPanelAction("Power Control"));
-        toolMenu.add(new jmri.jmrit.speedometer.SpeedometerAction( "Speedometer" ));
-        toolMenu.add(new jmri.jmrit.simpleprog.SimpleProgAction("Single CV Programmer"));
-        toolMenu.add(new jmri.jmrit.dualdecoder.DualDecoderToolAction());
-        toolMenu.add(new JSeparator());
+        menuBar.add(new jmri.jmrit.ToolsMenu());
 
-        JMenu throttleMenu = new JMenu("Throttles");
-        throttleMenu.add(new jmri.jmrit.throttle.ThrottleCreationAction( "New Throttle..." ));
-        throttleMenu.add(new jmri.jmrit.throttle.StoreXmlThrottleAction( "Save Throttle Layout" ));
-        throttleMenu.add(new jmri.jmrit.throttle.LoadXmlThrottleAction( "Load Throttle Layout" ));
-        throttleMenu.add(new jmri.jmrit.throttle.EditThrottlePreferencesAction( "Edit Throttle Preferences" ));
-        toolMenu.add(throttleMenu);
-        toolMenu.add(new JSeparator());
-
-        toolMenu.add(new jmri.jmrit.XmlFileCheckAction("Check XML File", this));
-        toolMenu.add(new jmri.jmrit.decoderdefn.NameCheckAction("Check decoder names", this));
-        toolMenu.add(new jmri.jmrit.symbolicprog.tabbedframe.ProgCheckAction("Check programmer names", this));
-        toolMenu.add(new jmri.jmrit.decoderdefn.DecoderIndexCreateAction("Create decoder index"));
-        toolMenu.add(new JSeparator());
-        toolMenu.add(new jmri.jmrit.roster.RecreateRosterAction("Recreate roster"));
-
-        JMenu debugMenu = new JMenu("Debug");
+        JMenu debugMenu = new jmri.jmrit.DebugMenu(this);
         menuBar.add(debugMenu);
-        debugMenu.add(new jmri.jmrix.easydcc.easydccmon.EasyDccMonAction("EasyDCC Command Monitor"));
-        debugMenu.add(new jmri.jmrix.lenz.mon.XNetMonAction("Lenz/Atlas Command Monitor"));
-        debugMenu.add(new jmri.jmrix.loconet.locomon.LocoMonAction("LocoNet Monitor"));
-        debugMenu.add(new jmri.jmrix.nce.ncemon.NceMonAction("NCE Command Monitor"));
-        debugMenu.add(new jmri.jmrix.sprog.sprogmon.SprogMonAction("SPROG Command Monitor"));
-        debugMenu.add(new jmri.jmrit.MemoryFrameAction("Memory usage monitor"));
         debugMenu.add(new JSeparator());
-        debugMenu.add(new jmri.jmrix.serialsensor.SerialSensorAction("Serial port sensors"));
+        debugMenu.add(new jmri.jmrix.easydcc.easydccmon.EasyDccMonAction(rb.getString("MenuItemEasyDCCCommandMonitor")));
+        debugMenu.add(new jmri.jmrix.lenz.mon.XNetMonAction(rb.getString("MenuItemLenzAtlasCommandMonitor")));
+        debugMenu.add(new jmri.jmrix.loconet.locomon.LocoMonAction(rb.getString("MenuItemLocoNetMonitor")));
+        debugMenu.add(new jmri.jmrix.nce.ncemon.NceMonAction(rb.getString("MenuItemNCECommandMonitor")));
+        debugMenu.add(new jmri.jmrix.sprog.sprogmon.SprogMonAction(rb.getString("MenuItemSPROGCommandMonitor")));
+        debugMenu.add(new JSeparator());
+        debugMenu.add(new jmri.jmrix.serialsensor.SerialSensorAction(rb.getString("MenuItemSerialPortSensors")));
 
         // Label & text
         JPanel pane1 = new JPanel();
@@ -96,39 +80,45 @@ public class DecoderProMain extends JPanel {
         pane1.add(new JLabel(new ImageIcon(ClassLoader.getSystemResource("resources/decoderpro.gif"),"Decoder Pro label"), JLabel.LEFT));
         JPanel pane2 = new JPanel();
         pane2.setLayout(new BoxLayout(pane2, BoxLayout.Y_AXIS));
-        pane2.add(new JLabel(" Decoder Pro "+jmri.Version.name()+", part of the JMRI project "));
-        pane2.add(new JLabel("   http://jmri.sf.net/DecoderPro "));
+        pane2.add(new JLabel(MessageFormat.format(rb.getString("DpCredit1"),
+                                new String[]{jmri.Version.name()}
+                            )));
+        pane2.add(new JLabel("http://jmri.sf.net/DecoderPro "));
         pane2.add(new JLabel(" "));
-        pane2.add(new JLabel(" Connected via "+prefs.getCurrentProtocolName()));
-        pane2.add(new JLabel(" on port "+prefs.getCurrentPortName()));
+        pane2.add(new JLabel(MessageFormat.format(rb.getString("DpCredit2"),
+                                new String[]{prefs.getCurrentProtocolName()}
+                            )));
+        pane2.add(new JLabel(MessageFormat.format(rb.getString("DpCredit3"),
+                                new String[]{prefs.getCurrentPortName()}
+                            )));
         pane2.add(new JLabel(" "));
-        pane2.add(new JLabel(" Java version "+System.getProperty("java.version","<unknown>")));
+        pane2.add(new JLabel(MessageFormat.format(rb.getString("DpCredit4"),
+                                new String[]{System.getProperty("java.version","<unknown>"),
+                                            Locale.getDefault().toString()}
+                            )));
         pane1.add(pane2);
         add(pane1);
 
         // Buttons
-        JButton b1 = new JButton("Use programming track ...");
+        JButton b1 = new JButton(rb.getString("DpButtonUseProgrammingTrack"));
         b1.addActionListener(serviceprog);
         b1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         add(b1);
 
-        JButton m1 = new JButton("Program on main track ...");
+        JButton m1 = new JButton(rb.getString("DpButtonProgramOnMainTrack"));
         m1.addActionListener(opsprog);
         m1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         add(m1);
         if (jmri.InstanceManager.programmerManagerInstance()==null ||
             !jmri.InstanceManager.programmerManagerInstance().isOpsModePossible()) {
             m1.setEnabled(false);
-            m1.setToolTipText("This button is disabled because your command station can't do "
-                            +"ops mode programming, or we don't yet have code to do "
-                            +"it for that type of system");
+            m1.setToolTipText(rb.getString("MsgOpsButtonDisabled"));
         }
 
-        JButton q1 = new JButton("Quit");
+        JButton q1 = new JButton(rb.getString("ButtonQuit"));
         q1.addActionListener(quit);
         q1.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         add(q1);
-
     }
 
     // Main entry point
@@ -150,7 +140,7 @@ public class DecoderProMain extends JPanel {
         log.info("DecoderPro starts");
 
         // create the primary frame
-        JFrame frame = new JFrame("Decoder Pro");
+        JFrame frame = new JFrame("DecoderPro");
         DecoderProMain pane = new DecoderProMain(frame);
         frame.addWindowListener(new BasicWindowMonitor());
         // insert contents
