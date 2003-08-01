e0c053c8625ecb92118ce68c13872a02886734f0
Bob Jacobsen <jacobsen@mac.com>
2003-08-01 13:39:02 +0000

    add comment, classes and jython.jar in path

---
+++ scripts/LocoTools.bat
@@ -0,0 +1,5 @@
+# Start the PanelPro.bat Java program
+# ($Revision:)
+# The -Dsun.java2d.noddraw option cures a Windows-specific incompatibility
+# with DirectX
+java -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Djava.class.path=".;classes;jmri.jar;lib\collections.jar;lib\log4j.jar;lib\jdom-jdk11.jar;lib\crimson.jar;lib\comm.jar;lib/jython.jar;$VFS" -Dsun.java2d.noddraw apps.LocoTools.LocoTools
