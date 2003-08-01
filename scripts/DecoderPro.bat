0f9f93769fff683bcc82298aca88796035dc4c02
Bob Jacobsen <jacobsen@mac.com>
2003-08-01 20:08:11 +0000

    change to REM for comments

--- scripts/DecoderPro.bat
+++ scripts/DecoderPro.bat
@@ -1,6 +1,3 @@
-# Start the PanelPro.bat Java program
-# ($Revision:)
-# The -Dsun.java2d.noddraw option cures a Windows-specific incompatibility
-# with DirectX
+REM Start the DecoderPro Java program ($Revision: 1.6 $)
 java -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Djava.class.path=".;classes;jmri.jar;lib\collections.jar;lib\log4j.jar;lib\jdom-jdk11.jar;lib\crimson.jar;lib\comm.jar;lib\comm.jar;lib/jython.jar;$VFS" -Dsun.java2d.noddraw apps.DecoderPro.DecoderPro
 
