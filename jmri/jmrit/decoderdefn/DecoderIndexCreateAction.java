eba929e53780d4c96581314fbded679722c3fb63
Bob Jacobsen <jacobsen@mac.com>
2003-08-05 05:37:39 +0000

    minor cleanup

--- jmri/jmrit/decoderdefn/DecoderIndexCreateAction.java
+++ jmri/jmrit/decoderdefn/DecoderIndexCreateAction.java
@@ -2,35 +2,28 @@
 
 package jmri.jmrit.decoderdefn;
 
-import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
-import java.io.*;
-import javax.swing.*;
-import org.jdom.*;
-import org.jdom.input.*;
-import com.sun.java.util.collections.List;
-
-import jmri.jmrit.XmlFile;
+import javax.swing.AbstractAction;
 
 /**
  * Check the names in an XML decoder file against the names.xml definitions
  *
- * @author			Bob Jacobsen   Copyright (C) 2001
- * @version			$Revision: 1.4 $
- * @see             jmri.jmrit.XmlFile
+ * @author	Bob Jacobsen   Copyright (C) 2001
+ * @version	$Revision: 1.5 $
+ * @see         jmri.jmrit.XmlFile
  */
 public class DecoderIndexCreateAction extends AbstractAction {
 
-	public DecoderIndexCreateAction(String s) {
-		super(s);
-	}
+    public DecoderIndexCreateAction(String s) {
+        super(s);
+    }
 
     public void actionPerformed(ActionEvent e) {
         DecoderIndexFile.forceCreationOfNewIndex();
-	}
+    }
 
-	// initialize logging
+    // initialize logging
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(DecoderIndexCreateAction.class.getName());
 
 }
