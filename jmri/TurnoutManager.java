d3868ea18024c4ec3801bc1916d638276e767dc7
Bob Jacobsen <jacobsen@mac.com>
2003-08-05 06:02:48 +0000

    clarify ambiguous comment that casued me to make a coding mistake when using this class

--- jmri/TurnoutManager.java
+++ jmri/TurnoutManager.java
@@ -26,7 +26,7 @@ import com.sun.java.util.collections.List;
  * class, which provides a simple GUI for controlling a single turnout.
  *
  * @author			Bob Jacobsen Copyright (C) 2001
- * @version			$Revision: 1.11 $
+ * @version			$Revision: 1.12 $
  * @see             jmri.Turnout
  * @see             jmri.AbstractTurnoutManager
  * @see             jmri.InstanceManager
@@ -36,8 +36,10 @@ public interface TurnoutManager extends Manager {
 
     /**
      * Locate via user name, then system name if needed.
-     * If that fails, create a new turnout using this as a
-     * default name.
+     * If that fails, create a new turnout. If the name
+     * is a valid system name, it will be used for the new
+     * turnout.  Otherwise, the makeSystemName method
+     * will attempt to turn it into a valid system name.
      *
      * @param name
      * @return Never null under normal circumstances
@@ -46,8 +48,7 @@ public interface TurnoutManager extends Manager {
 
     /**
      * Locate via user name, then system name if needed.
-     * If that fails, create a new turnout using this as a
-     * default name.
+     * If that fails, return null
      *
      * @param name
      * @return null if no match found
@@ -81,7 +82,7 @@ public interface TurnoutManager extends Manager {
      *    with the Turnout object created; a valid system name must be provided
      *<LI>If both names are provided, the system name defines the
      *    hardware access of the desired turnout, and the user address
-     *    is associated with it.
+     *    is associated with it. The system name must be valid.
      *</UL>
      * Note that it is possible to make an inconsistent request if both
      * addresses are provided, but the given values are associated with
