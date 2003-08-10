3cf746f1df9c699d5eaf00154b833705e2c6bda7
Bob Jacobsen <jacobsen@mac.com>
2003-08-10 04:07:05 +0000

    skeleton GUI, no persistance yet

---
+++ jmri/jmrit/blockboss/BlockBossAction.java
@@ -0,0 +1,67 @@
+// BlockBossAction.java
+
+package jmri.jmrit.blockboss;
+
+import jmri.InstanceManager;
+import jmri.Manager;
+import jmri.NamedBean;
+import jmri.SignalHead;
+import java.awt.event.ActionEvent;
+import java.util.*;
+
+import javax.swing.AbstractAction;
+import javax.swing.*;
+
+/**
+ * Swing action to create and register a
+ * "Simple Signal Logic" GUI
+ *
+ * @author	Bob Jacobsen    Copyright (C) 2003
+ * @version     $Revision: 1.1 $
+ */
+
+public class BlockBossAction extends AbstractAction {
+
+    public BlockBossAction(String s) { super(s);}
+
+    public void actionPerformed(ActionEvent e) {
+
+        // create the frame
+        JFrame f = new JFrame("Simple Signal Logic");
+
+        f.getContentPane().setLayout(new BoxLayout(f.getContentPane(), BoxLayout.Y_AXIS));
+
+        // add lines of GUI
+        JPanel line = new JPanel();
+        line.add(new JLabel("Signal name: "));
+        line.add(new JTextField());
+        f.getContentPane().add(line);
+
+        f.getContentPane().add(new JSeparator(JSeparator.HORIZONTAL));
+
+        line = new JPanel();
+        line.add(new JLabel("Protects sensor: "));
+        line.add(new JTextField());
+        f.getContentPane().add(line);
+
+        line = new JPanel();
+        line.add(new JLabel("Protects turnout: "));
+        line.add(new JTextField());
+        line.add(new JLabel("when "));
+        line.add(new JRadioButton("Closed"));
+        line.add(new JRadioButton("Thrown"));
+        f.getContentPane().add(line);
+
+        line = new JPanel();
+        line.add(new JLabel("Protects signal: "));
+        line.add(new JTextField());
+        line.add(new JCheckBox("with flashing yellow"));
+        f.getContentPane().add(line);
+
+        f.show();
+    }
+
+}
+
+
+/* @(#)SignalHeadTableAction.java */
