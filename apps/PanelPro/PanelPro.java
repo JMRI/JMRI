df6d906f42199b4219b88619ad540cfb35c68474
Bob Jacobsen <jacobsen@mac.com>
2003-08-14 03:19:31 +0000

    JavaDoc requires a period

--- apps/PanelPro/PanelPro.java
+++ apps/PanelPro/PanelPro.java
@@ -11,14 +11,14 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 
 /**
- * The JMRI program for creating control panels
+ * The JMRI program for creating control panels.
  * <P>
  * If an argument is provided at startup, it will be used as the name of
  * the configuration file.  Note that this is just the name, not the path;
  * the file is searched for in the usual way, first in the preferences tree and then in
  * xml/
  * @author	Bob Jacobsen   Copyright 2003
- * @version     $Revision: 1.9 $
+ * @version     $Revision: 1.10 $
  */
 public class PanelPro extends Apps {
 
