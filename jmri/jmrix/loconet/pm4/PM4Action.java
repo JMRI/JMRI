9e9d51fd2acaa8d9481e38828b74f3dd059f8e80
Bob Jacobsen <jacobsen@mac.com>
2003-08-18 00:43:13 +0000

    minor cleanup

--- jmri/jmrix/loconet/pm4/PM4Action.java
+++ jmri/jmrix/loconet/pm4/PM4Action.java
@@ -2,35 +2,28 @@
 
 package jmri.jmrix.loconet.pm4;
 
-import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
-import jmri.jmrix.loconet.LnTrafficController;
+import javax.swing.AbstractAction;
 
 
 /**
- * PM4Action.java
- *
- * Description:		Swing action to create and register a
- *       			PM4Frame object
+ * Create and register a PM4Frame object.
  *
  * @author			Bob Jacobsen    Copyright (C) 2002
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  */
-public class PM4Action 			extends AbstractAction {
+public class PM4Action 	extends AbstractAction {
 
     public PM4Action(String s) { super(s);}
 
     public PM4Action() { this("PM4 programmer");}
 
     public void actionPerformed(ActionEvent e) {
-		// create a PM4Frame
-		PM4Frame f = new PM4Frame();
-		f.show();
-	}
-
-	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(PM4Action.class.getName());
-
+        // create a PM4Frame
+        PM4Frame f = new PM4Frame();
+        f.show();
+    }
 }
 
 
