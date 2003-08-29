570425e98e1b16ae4970be306ee1a1fae99242c1
Bob Jacobsen <jacobsen@mac.com>
2003-08-29 07:07:15 +0000

    import cleanup for 1.1.8

--- test/jmri/jmrit/XmlFileTest.java
+++ test/jmri/jmrit/XmlFileTest.java
@@ -2,9 +2,12 @@
 
 package jmri.jmrit;
 
-import java.io.*;
+import java.io.File;
 
-import junit.framework.*;
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
  * Tests for the XmlFile class.
@@ -12,7 +15,7 @@ import junit.framework.*;
  * Uses (creates, modifies, destroys) files in the local preferences directory
  *
  * @author	    Bob Jacobsen  Copyright 2001
- * @version         $Revision: 1.5 $
+ * @version         $Revision: 1.6 $
  */
 public class XmlFileTest extends TestCase {
 
@@ -20,7 +23,7 @@ public class XmlFileTest extends TestCase {
         // XmlFile is abstract, so can't check ctor directly; use local class
         XmlFile x = new XmlFile() {
             };
-        
+
         // this test uses explicit filenames intentionally, to ensure that
         // the resulting files go into the test tree area.  This is not
         // a test of prefsDir, and shouldn't use that.
@@ -29,26 +32,26 @@ public class XmlFileTest extends TestCase {
         Assert.assertTrue("existing file ", x.checkFile("decoders"));  // should be in xml
         Assert.assertTrue("non-existing file ", !x.checkFile("dummy file not expected to exist"));
     }
-    
-    
+
+
     // from here down is testing infrastructure
-    
+
     public XmlFileTest(String s) {
         super(s);
     }
-    
+
     // Main entry point
     static public void main(String[] args) {
         String[] testCaseName = {XmlFileTest.class.getName()};
         junit.swingui.TestRunner.main(testCaseName);
     }
-    
+
     // test suite from all defined tests
     public static Test suite() {
         TestSuite suite = new TestSuite(XmlFileTest.class);
         return suite;
     }
-    
+
     // static private org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(XmlFileTest.class.getName());
-    
+
 }
