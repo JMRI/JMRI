58623a5b3e0eb7046014ba693f8e45025aaebeb4
Bob Jacobsen <jacobsen@mac.com>
2003-10-20 04:03:38 +0000

    add PM42, more space for address

--- jmri/jmrix/loconet/pm4/PM4Frame.java
+++ jmri/jmrix/loconet/pm4/PM4Frame.java
@@ -14,7 +14,8 @@ import javax.swing.*;
 
 /**
  * Frame displaying and programming a PM4 configuration.
- * <P>The read and write require a sequence of operations, which
+ * <P>
+ * The read and write require a sequence of operations, which
  * we handle with a state variable.
  * <P>
  * Programming of the PM4 is done via configuration messages, so
@@ -28,7 +29,7 @@ import javax.swing.*;
  * contact Digitrax Inc for separate permission.
  *
  * @author			Bob Jacobsen   Copyright (C) 2002
- * @version			$Revision: 1.6 $
+ * @version			$Revision: 1.7 $
  */
 public class PM4Frame extends JFrame implements LocoNetListener {
 
@@ -53,7 +54,7 @@ public class PM4Frame extends JFrame implements LocoNetListener {
 
         JPanel pane1 = new JPanel();
         pane1.setLayout(new FlowLayout());
-            pane1.add(new JLabel("Section 1:  Slow "));
+            pane1.add(new JLabel("Section 1:  Speed "));
             pane1.add(slow1);
             pane1.add(new JLabel(" Autoreversing "));
             pane1.add(rev1);
@@ -61,7 +62,7 @@ public class PM4Frame extends JFrame implements LocoNetListener {
 
         JPanel pane2 = new JPanel();
         pane2.setLayout(new FlowLayout());
-            pane2.add(new JLabel("Section 2:  Slow "));
+            pane2.add(new JLabel("Section 2:  Speed "));
             pane2.add(slow2);
             pane2.add(new JLabel(" Autoreversing "));
             pane2.add(rev2);
@@ -69,7 +70,7 @@ public class PM4Frame extends JFrame implements LocoNetListener {
 
         JPanel pane3 = new JPanel();
         pane3.setLayout(new FlowLayout());
-            pane3.add(new JLabel("Section 3:  Slow "));
+            pane3.add(new JLabel("Section 3:  Speed "));
             pane3.add(slow3);
             pane3.add(new JLabel(" Autoreversing "));
             pane3.add(rev3);
@@ -77,13 +78,19 @@ public class PM4Frame extends JFrame implements LocoNetListener {
 
         JPanel pane4 = new JPanel();
         pane4.setLayout(new FlowLayout());
-            pane4.add(new JLabel("Section 4:  Slow "));
+            pane4.add(new JLabel("Section 4:  Speed "));
             pane4.add(slow4);
             pane4.add(new JLabel(" Autoreversing "));
             pane4.add(rev4);
         appendLine(pane4);
 
         appendLine(status);
+
+        slow1.setSelectedIndex(1);
+        slow2.setSelectedIndex(1);
+        slow3.setSelectedIndex(1);
+        slow4.setSelectedIndex(1);
+        addrField.setText("1");
 
         // install read all, write all button handlers
         readAllButton.addActionListener( new ActionListener() {
@@ -167,15 +174,37 @@ public class PM4Frame extends JFrame implements LocoNetListener {
         }
     }
 
+    void setSpeedFromDisplay(int offset, JComboBox box) {
+        switch (box.getSelectedIndex()) {
+        case 0:
+            opsw[offset] = false;
+            opsw[offset+2] = true;      // true is closed
+            return;
+        default:
+        case 1:
+            opsw[offset] = false;
+            opsw[offset+2] = false;
+            return;
+        case 2:
+            opsw[offset] = true;
+            opsw[offset+2] = true;
+            return;
+        case 3:
+            opsw[offset] = true;
+            opsw[offset+2] = false;
+            return;
+        }
+    }
+
     void writeAll() {
         // copy over the display
-        opsw[5] = slow1.isSelected();
+        setSpeedFromDisplay(3, slow1);
         opsw[6] = rev1.isSelected();
-        opsw[13] = slow2.isSelected();
+        setSpeedFromDisplay(11, slow2);
         opsw[14] = rev2.isSelected();
-        opsw[21] = slow3.isSelected();
+        setSpeedFromDisplay(19, slow3);
         opsw[22] = rev3.isSelected();
-        opsw[29] = slow4.isSelected();
+        setSpeedFromDisplay(27, slow4);
         opsw[30] = rev4.isSelected();
         // get current limit
         // bit 9 is the low bit, but "closed" is a 0 in the calculation
@@ -229,14 +258,21 @@ public class PM4Frame extends JFrame implements LocoNetListener {
         }
     }
 
+    void setDisplaySpeed(int offset, JComboBox box) {
+        int index = 0;
+        if (!opsw[index+2]) index++;
+        if (opsw[index]) index+=2;
+        box.setSelectedIndex(index);
+    }
+
     void updateDisplay() {
-        slow1.setSelected(opsw[5]);
+        setDisplaySpeed(3, slow1);
         rev1.setSelected(opsw[6]);
-        slow2.setSelected(opsw[13]);
+        setDisplaySpeed(11, slow2);
         rev2.setSelected(opsw[14]);
-        slow3.setSelected(opsw[21]);
+        setDisplaySpeed(19, slow3);
         rev3.setSelected(opsw[22]);
-        slow4.setSelected(opsw[29]);
+        setDisplaySpeed(27, slow4);
         rev4.setSelected(opsw[30]);
         // display current limit
         // bit 9 is the low bit, but "closed" is a 0 in the calculation
@@ -252,14 +288,18 @@ public class PM4Frame extends JFrame implements LocoNetListener {
     int nextState() {
         switch (state) {
             case 1: return 2;
-            case 2: return 5;
+            case 2: return 3;
+            case 3: return 5;
             case 5: return 6;
             case 6: return 9;
-            case 9: return 13;
+            case 9: return 11;
+            case 11: return 13;
             case 13: return 14;
-            case 14: return 21;
+            case 14: return 19;
+            case 19: return 21;
             case 21: return 22;
-            case 22: return 29;
+            case 22: return 27;
+            case 27: return 29;
             case 29: return 30;
             case 30: return 0;   // done!
             default:
@@ -268,18 +308,22 @@ public class PM4Frame extends JFrame implements LocoNetListener {
         }
     }
 
-    JTextField addrField = new JTextField("1");
+    JTextField addrField = new JTextField(5);
 
     JComboBox current = new JComboBox(new String[]{ "1.5 amps", "3 amps", "4.5 amps", "6 amps",
                 				"7.5 amps", "9 amps", "10.5 amps", "12 amps"});
 
-    JCheckBox slow1 = new JCheckBox();
+    JComboBox slow1 = new JComboBox(new String[]{ "Slow", "Standard",
+                                            "Faster (PM42 only)", "Fastest (PM42 only)"});
     JCheckBox rev1  = new JCheckBox();
-    JCheckBox slow2 = new JCheckBox();
+    JComboBox slow2 = new JComboBox(new String[]{ "Slow", "Standard",
+                                            "Faster (PM42 only)", "Fastest (PM42 only)"});
     JCheckBox rev2  = new JCheckBox();
-    JCheckBox slow3 = new JCheckBox();
+    JComboBox slow3 = new JComboBox(new String[]{ "Slow", "Standard",
+                                            "Faster (PM42 only)", "Fastest (PM42 only)"});
     JCheckBox rev3  = new JCheckBox();
-    JCheckBox slow4 = new JCheckBox();
+    JComboBox slow4 = new JComboBox(new String[]{ "Slow", "Standard",
+                                            "Faster (PM42 only)", "Fastest (PM42 only)"});
     JCheckBox rev4  = new JCheckBox();
 
     JLabel status = new JLabel("The PM4 should be in normal mode. (Don't push the buttons on the PM4)");
