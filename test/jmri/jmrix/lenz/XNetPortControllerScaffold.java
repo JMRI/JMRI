95132f9da2dc15843ad72b7776456cbdb7662b46
Bob Jacobsen <jacobsen@mac.com>
2003-11-11 18:06:52 +0000

    clean up JavaDoc.  Add dummy member that was added to superclass

--- test/jmri/jmrix/lenz/XNetPortControllerScaffold.java
+++ test/jmri/jmrix/lenz/XNetPortControllerScaffold.java
@@ -3,12 +3,10 @@
  *
  * Description:	    test implementation of XNetPortController
  * @author			Bob Jacobsen
- * @version         $Revision: 1.2 $
+ * @version         $Revision: 1.3 $
  */
 
 package jmri.jmrix.lenz;
-
-import jmri.*;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -17,35 +15,55 @@ import java.io.PipedOutputStream;
 
 class XNetPortControllerScaffold extends XNetPortController {
 
-            public java.util.Vector getPortNames() { return null; }
-	    public String openPort(String portName, String appName) { return null; }
-	    public void configure() {}
-	    public String[] validBaudRates() { return null; }
+    public java.util.Vector getPortNames() { return null; }
+    public String openPort(String portName, String appName) { return null; }
+    public void configure() {}
+    public String[] validBaudRates() { return null; }
 
-		protected XNetPortControllerScaffold() throws Exception {
-			PipedInputStream tempPipe;
-			tempPipe = new PipedInputStream();
-			tostream = new DataInputStream(tempPipe);
-			ostream = new DataOutputStream(new PipedOutputStream(tempPipe));
-			tempPipe = new PipedInputStream();
-			istream = new DataInputStream(tempPipe);
-			tistream = new DataOutputStream(new PipedOutputStream(tempPipe));
-		}
+    protected XNetPortControllerScaffold() throws Exception {
+        PipedInputStream tempPipe;
+        tempPipe = new PipedInputStream();
+        tostream = new DataInputStream(tempPipe);
+        ostream = new DataOutputStream(new PipedOutputStream(tempPipe));
+        tempPipe = new PipedInputStream();
+        istream = new DataInputStream(tempPipe);
+        tistream = new DataOutputStream(new PipedOutputStream(tempPipe));
+    }
 
-		// returns the InputStream from the port
-		public DataInputStream getInputStream() { return istream; }
+    /**
+     * Returns the InputStream from the port.
+     */
+    public DataInputStream getInputStream() { return istream; }
 
-		// returns the outputStream to the port
-		public DataOutputStream getOutputStream() { return ostream; }
+    /**
+     * Returns the outputStream to the port.
+     */
+    public DataOutputStream getOutputStream() { return ostream; }
 
-		// check that this object is ready to operate
-		public boolean status() { return true; }
-        public boolean okToSend() { return true; }
+    /**
+     * Check that this object is ready to operate.
+     */
+    public boolean status() { return true; }
+    public boolean okToSend() { return true; }
 
-	    DataOutputStream ostream;  // Traffic controller writes to this
-	    DataInputStream  tostream; // so we can read it from this
+    public void setOutputBufferEmpty(boolean s) {}
 
-	    DataOutputStream tistream; // tests write to this
-	    DataInputStream  istream;  // so the traffic controller can read from this
+    /**
+     * Traffic controller writes to this.
+     */
+    DataOutputStream ostream;
+    /**
+     * Can read test data from this.
+     */
+    DataInputStream  tostream;
+
+    /**
+     * Tests write to this.
+     */
+    DataOutputStream tistream;
+    /**
+     * The traffic controller can read test data from this.
+     */
+    DataInputStream  istream;
 
 }
