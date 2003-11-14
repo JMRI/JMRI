3b8a3a6deaf1c074ddc63f2dc9360f2148ba4ac3
Bob Jacobsen <jacobsen@mac.com>
2003-11-14 00:46:45 +0000

    new file, refactoring a Swing2 method

---
+++ jmri/util/JTabbedPaneUtil.java
@@ -0,0 +1,32 @@
+// JTabbedPaneUtil.java
+
+package jmri.util;
+
+import javax.swing.JTabbedPane;
+
+/**
+ * Common utility methods for working with JTabbedPanes.
+ * <P>
+ * We needed a place to refactor common JTabbedPane-processing idioms in JMRI
+ * code, so this class was created. It's more of a library of procedures
+ * than a real class, as (so far) all of the operations have needed no state
+ * information.
+ * <P>
+ * In particular, this is intended to provide Java 2 functionality on a
+ * Java 1.1.8 system, or at least try to fake it.
+ *
+ * @author Bob Jacobsen  Copyright 2003
+ * @version $Revision: 1.1 $
+ */
+
+public class JTabbedPaneUtil {
+
+    static public void setToolTipTextAt(JTabbedPane p, int i, String s) {
+        try {
+            p.setToolTipTextAt(i, s);
+        } catch (NoSuchMethodError e) {
+        }
+        return;
+    }
+
+}
