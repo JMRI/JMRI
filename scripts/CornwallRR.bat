63bde0da0b71320fe060ec2754779978799b1ea9
Bob Jacobsen <jacobsen@mac.com>
2003-11-14 14:54:08 +0000

    This commit was manufactured by cvs2svn to create tag 'Release-1-3-5'.

--- scripts/CornwallRR.bat
+++
@@ -1,2 +0,0 @@
-REM Start the CornwallRR Java program ($Revision: 1.2 $)
-java -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Djava.class.path=".;classes;jmri.jar;lib\collections.jar;lib\log4j.jar;lib\jdom-jdk11.jar;lib\crimson.jar;lib\comm.jar;lib/jython.jar;$VFS" -Dsun.java2d.noddraw apps.cornwall.CornwallRR
