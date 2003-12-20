edd5f058969a930d24a40d7fab201c284214e237
Bob Jacobsen <jacobsen@mac.com>
2003-12-20 17:21:04 +0000

    first working version

---
+++ apps/PacketPro/PacketPro.java
@@ -0,0 +1,69 @@
+/**
+ * PacketPro.java
+ */
+
+package apps.PacketPro;
+
+import apps.Apps;
+
+import java.text.MessageFormat;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+
+/**
+ * Main program for the PacketProprogram based on JMRI.
+ * <P>
+ * PacketPro is a tool for sending NMRA DCC packets, intended to make
+ * testing of DCC components easier.  It's similar in intent to the
+ * NMRA PacketScript program, though it uses a different scripting language.
+ * <P>
+ * If an argument is provided at startup, it will be used as the name of
+ * the configuration file.  Note that this is just the name, not the path;
+ * the file is searched for in the usual way, first in the preferences tree and then in
+ * xml/
+ * @author	Bob Jacobsen   Copyright 2002
+ * @version     $Revision: 1.1 $
+ */
+public class PacketPro extends Apps {
+
+    PacketPro(JFrame p) {
+        super(p);
+        }
+
+    protected void createMenus(JMenuBar menuBar, JFrame frame) {
+        fileMenu(menuBar, frame);
+        editMenu(menuBar, frame);
+        toolsMenu(menuBar, frame);
+        panelMenu(menuBar, frame);
+        systemsMenu(menuBar, frame);
+        debugMenu(menuBar, frame);
+        helpMenu(menuBar, frame);
+    }
+
+    protected String line1() {
+        return MessageFormat.format(rb.getString("PacketProVersionCredit"),
+                                new String[]{jmri.Version.name()});
+    }
+
+    // Main entry point
+    public static void main(String args[]) {
+
+        // show splash screen early
+        splash(true);
+
+        initLog4J();
+        log.info(apps.Apps.startupInfo("PacketPro"));
+
+        setConfigFilename("PacketProConfig2.xml", args);
+        JFrame f = new JFrame("PacketPro");
+        createFrame(new PacketPro(f), f);
+
+        log.info("main initialization done");
+        splash(false);
+    }
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(PacketPro.class.getName());
+}
+
+
