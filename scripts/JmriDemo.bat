d8df8e5e291b1e37ad518a1bfbfbaece4bf52af7
Bob Jacobsen <jacobsen@mac.com>
2003-12-24 05:23:08 +0000

    add comm.jar

--- scripts/JmriDemo.bat
+++ scripts/JmriDemo.bat
@@ -1,5 +1,3 @@
-REM Start the JmriDemo Java program ($Revision: 1.5 $)
+REM Start the JmriDemo Java program ($Revision: 1.6 $)
 
-java -noverify -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw \
-     -Djava.class.path=".;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/crimson.jar;lib/jdom-jdk11.jar;lib/jython.jar" \
-     apps.JmriDemo.JMRIdemo
+java -noverify -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/comm.jar;lib/crimson.jar;lib/jdom-jdk11.jar;lib/jython.jar" apps.JmriDemo.JMRIdemo
