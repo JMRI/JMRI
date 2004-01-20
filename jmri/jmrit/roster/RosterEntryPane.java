335a49a577490d5f7fe7dbf7cb520d389949fb33
Dennis Miller <millerdl@telusplanet.net>
2004-01-20 04:35:17 +0000

    Change GUI to allow multiline comment and decoder comment fields and change layout manager to display new GUI

--- jmri/jmrit/roster/RosterEntryPane.java
+++ jmri/jmrit/roster/RosterEntryPane.java
@@ -11,14 +11,21 @@ import java.util.ResourceBundle;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
 
 import com.sun.java.util.collections.List;
 
 /**
  * Display and edit a RosterEntry.
  *
- * @author	Bob Jacobsen   Copyright (C) 2001
- * @version	$Revision: 1.5 $
+ * @author	Bob Jacobsen   Copyright (C) 2001; Dennis Miller Copyright 2004
+ * @version	$Revision: 1.6 $
  */
 public class RosterEntryPane extends javax.swing.JPanel  {
 
@@ -29,11 +36,19 @@ public class RosterEntryPane extends javax.swing.JPanel  {
     JTextField model		= new JTextField(12);
     JTextField owner		= new JTextField(12);
     JLabel dccAddress		= new JLabel();
-    JTextField comment		= new JTextField(12);
+    JTextArea comment		= new JTextArea(3,30);
+    //JScrollPanes are defined with scroll bars on always to avoid undesireable resizing behavior
+    //Without this the field will shrink to minimum size any time the scroll bars become needed and
+    //the scroll bars are inside, not outside the field area, obscuring their contents.
+    //This way the shrinking does not happen and the scroll bars are outside the field area,
+    //leaving the contents visible
+    JScrollPane commentScroller = new JScrollPane(comment,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     JLabel filename 		= new JLabel();
     JLabel decoderModel 	= new JLabel();
     JLabel decoderFamily 	= new JLabel();
-    JTextField decoderComment 	= new JTextField(12);
+    JTextArea decoderComment	= new JTextArea(3,30);
+    JScrollPane decoderCommentScroller = new JScrollPane(decoderComment,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+
 
     Component pane = null;
     RosterEntry re = null;
@@ -74,44 +89,141 @@ public class RosterEntryPane extends javax.swing.JPanel  {
                 }
             });
 
-        // assemble the GUI
-        setLayout(new GridLayout(12,2));
+        // New GUI to allow multiline Comment and Decoder Comment fields
+        //Set up constraints objects for convenience in GridBagLayout alignment
+        GridBagLayout gbLayout = new GridBagLayout();
+        GridBagConstraints cL = new GridBagConstraints();
+        GridBagConstraints cR = new GridBagConstraints();
+        Dimension minFieldDim = new Dimension(150,20);
+        Dimension minScrollerDim = new Dimension(165,42);
+        setLayout(gbLayout);
 
-        add(new JLabel(rb.getString("FieldID")));
+        cL.gridx = 0;
+        cL.gridy = 0;
+        cL.ipadx = 3;
+        cL.anchor = GridBagConstraints.NORTHWEST;
+        cL.insets = new Insets (0,0,0,15);
+        JLabel row0Label = new JLabel(rb.getString("FieldID"));
+        gbLayout.setConstraints(row0Label,cL);
+        add(row0Label);
+
+        cR.gridx = 1;
+        cR.gridy = 0;
+        cR.anchor = GridBagConstraints.WEST;
+        id.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(id,cR);
         add(id);
 
-        add(new JLabel(rb.getString("FieldRoadName")));
+        cL.gridy = 1;
+        JLabel row1Label = new JLabel(rb.getString("FieldRoadName"));
+        gbLayout.setConstraints(row1Label,cL);
+        add(row1Label);
+
+        cR.gridy = 1;
+        roadName.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(roadName,cR);
         add(roadName);
 
-        add(new JLabel(rb.getString("FieldRoadNumber")));
+        cL.gridy = 2;
+        JLabel row2Label = new JLabel(rb.getString("FieldRoadNumber"));
+        gbLayout.setConstraints(row2Label,cL);
+        add(row2Label);
+
+        cR.gridy = 2;
+        roadNumber.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(roadNumber,cR);
         add(roadNumber);
 
-        add(new JLabel(rb.getString("FieldManufacturer")));
+        cL.gridy = 3;
+        JLabel row3Label = new JLabel(rb.getString("FieldManufacturer"));
+        gbLayout.setConstraints(row3Label,cL);
+        add(row3Label);
+
+        cR.gridy = 3;
+        mfg.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(mfg,cR);
         add(mfg);
 
-        add(new JLabel(rb.getString("FieldOwner")));
+        cL.gridy = 4;
+        JLabel row4Label = new JLabel(rb.getString("FieldOwner"));
+        gbLayout.setConstraints(row4Label,cL);
+        add(row4Label);
+
+        cR.gridy = 4;
+        owner.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(owner,cR);
         add(owner);
 
-        add(new JLabel(rb.getString("FieldModel")));
+        cL.gridy = 5;
+        JLabel row5Label = new JLabel(rb.getString("FieldModel"));
+        gbLayout.setConstraints(row5Label,cL);
+        add(row5Label);
+
+        cR.gridy = 5;
+        model.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(model,cR);
         add(model);
 
-        add(new JLabel(rb.getString("FieldDCCAddress")));
+        cL.gridy = 6;
+        JLabel row6Label = new JLabel(rb.getString("FieldDCCAddress"));
+        gbLayout.setConstraints(row6Label,cL);
+        add(row6Label);
+
+        cR.gridy = 6;
+        dccAddress.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(dccAddress,cR);
         add(dccAddress);
 
-        add(new JLabel(rb.getString("FieldComment")));
-        add(comment);
+        cL.gridy = 7;
+        JLabel row7Label = new JLabel(rb.getString("FieldComment"));
+        gbLayout.setConstraints(row7Label,cL);
+        add(row7Label);
 
-        add(new JLabel(rb.getString("FieldDecoderFamily")));
+        cR.gridy = 7;
+        commentScroller.setMinimumSize(minScrollerDim);
+        gbLayout.setConstraints(commentScroller,cR);
+        add(commentScroller);
+
+        cL.gridy = 8;
+        JLabel row8Label = new JLabel(rb.getString("FieldDecoderFamily"));
+        gbLayout.setConstraints(row8Label,cL);
+        add(row8Label);
+
+        cR.gridy = 8;
+        decoderFamily.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(decoderFamily,cR);
         add(decoderFamily);
 
-        add(new JLabel(rb.getString("FieldDecoderModel")));
+        cL.gridy = 9;
+        JLabel row9Label = new JLabel(rb.getString("FieldDecoderModel"));
+        gbLayout.setConstraints(row9Label,cL);
+        add(row9Label);
+
+        cR.gridy = 9;
+        decoderModel.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(decoderModel,cR);
         add(decoderModel);
 
-        add(new JLabel(rb.getString("FieldDecoderComment")));
-        add(decoderComment);
+        cL.gridy = 10;
+        JLabel row10Label = new JLabel(rb.getString("FieldDecoderComment"));
+        gbLayout.setConstraints(row10Label,cL);
+        add(row10Label);
 
-        add(new JLabel(rb.getString("FieldFilename")));
+        cR.gridy = 10;
+        decoderCommentScroller.setMinimumSize(minScrollerDim);
+        gbLayout.setConstraints(decoderCommentScroller,cR);
+        add(decoderCommentScroller);
+
+        cL.gridy = 11;
+        JLabel row11Label = new JLabel(rb.getString("FieldFilename"));
+        gbLayout.setConstraints(row11Label,cL);
+        add(row11Label);
+
+        cR.gridy = 11;
+        filename.setMinimumSize(minFieldDim);
+        gbLayout.setConstraints(filename,cR);
         add(filename);
+
 
     }
 
