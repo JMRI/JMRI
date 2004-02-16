fc142e980a0116f907f04f7632300b0e7ca67746
Bob Jacobsen <jacobsen@mac.com>
2004-02-16 04:34:48 +0000

    allow selection of file when invoked

--- jmri/jmrit/automat/JythonAutomatonAction.java
+++ jmri/jmrit/automat/JythonAutomatonAction.java
@@ -4,22 +4,36 @@ package jmri.jmrit.automat;
 
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
+import java.io.*;
+import javax.swing.*;
 
 /**
  * Swing action to create and register a
  * JythonAutomaton object
  *
  * @author	    Bob Jacobsen    Copyright (C) 2003
- * @version         $Revision: 1.1 $
+ * @version         $Revision: 1.2 $
  */
 public class JythonAutomatonAction extends AbstractAction {
 
-    public JythonAutomatonAction(String s) { super(s);}
+    public JythonAutomatonAction(String s, JPanel who) {
+        super(s);
+        _who = who;
+    }
+
+    JPanel _who;
 
     public void actionPerformed(ActionEvent e) {
         // create a SampleAutomaton
-        AbstractAutomaton f = new JythonAutomaton();
-        f.start();
+        JFileChooser fci = new JFileChooser(" ");
+        fci.setDialogTitle("Find desired script file");
+        int retVal = fci.showOpenDialog(_who);
+        // handle selection or cancel
+        if (retVal == JFileChooser.APPROVE_OPTION) {
+            File file = fci.getSelectedFile();
+            // create an object to handle script and run
+            (new JythonAutomaton(file.toString())).start();
+        }
     }
 }
 
