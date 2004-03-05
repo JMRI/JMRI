a1d065c96663197a1da907d654f34942636b45e4
Bob Jacobsen <jacobsen@mac.com>
2004-03-05 00:38:21 +0000

    much cleanup

--- scripts/jython/AutomatonExample.py
+++ scripts/jython/AutomatonExample.py
@@ -1,20 +1,49 @@
 # This is an example script for a JMRI "Automat" in Python
+#
+# It listens to two sensors, running a locomotive back and 
+# forth between them by changing its direction when a sensor
+# detects the engine
+#
+# $Revision: 1.3 $
 
-# init() is called exactly once at the beginning, and can 
-# be used to do various initialization
-def init():
-    print "init was run!"
-    return
+import jarray
+import jmri
 
-# handle() is repeatedly called until it returns false
-# Typically, it waits for a particular interesting condition to
-# be valid, does the needed action, and then returns true to
-# repeat. This sample just runs three times, printing a message,
-# then quits.
-def handle():
-	global nloop
-	print " handle count: ", nloop
-	nloop = nloop + 1
-	return (nloop != 3)  # run three times, then stop
+class AutomatExample(jmri.jmrit.automat.AbstractAutomaton) :
 
-nloop = 0
+	# init() is called exactly once at the beginning to do
+	# any necessary configuration.
+	def init(self):
+		
+		# get the sensor and throttle objects
+		self.fwdSensor = sensors.provideSensor("12")
+		self.revSensor = sensors.provideSensor("13")
+		self.throttle = self.getThrottle(1234, True)  # long address 1234
+		 		
+		return
+
+	# handle() is called repeatedly until it returns false.
+	#
+	# Modify this to do your calculation.
+	def handle(self):
+
+		# set loco to forward
+		self.throttle.setIsForward(True)
+		
+		# wait for sensor in forward direction to trigger
+		self.waitSensorActive(self.fwdSensor)
+		
+		# set loco to reverse
+		self.throttle.setIsForward(False)
+		
+		# wait for sensor in reverse direction to trigger
+		self.waitSensorActive(self.revSensor)
+		
+		# and continue around again
+		return 1	# to continue
+	
+# end of class definition
+
+# start one of these up
+AutomatExample().start()
+
