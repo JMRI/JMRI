d9bb6e0b43222691087e820f88862ca70d1c8136
Bob Jacobsen <jacobsen@mac.com>
2004-03-06 18:52:07 +0000

    fix annoying typo in comment

--- scripts/PacketPro.bat
+++ scripts/PacketPro.bat
@@ -1,12 +1,12 @@
 
 REM ##############################################################################
 
-REM Invoke the the PacketPro JMRI appliction, which provide a Jython
+REM Invoke the the PacketPro JMRI application, which provide a Jython
 REM command line.
 
 REM There must be a local jython interpreter and JMRI libraries available
 
-REM $Revision: 1.1 $ (CVS maintains this line, do not edit please)
+REM $Revision: 1.2 $ (CVS maintains this line, do not edit please)
 
 SET CLASSPATH=.;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/crimson.jar;lib/jdom-jdk11.jar
      
