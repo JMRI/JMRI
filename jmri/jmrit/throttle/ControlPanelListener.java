14e7717c59cc398737236e96d8754efece7fdf00
Bob Jacobsen <jacobsen@mac.com>
2004-03-15 04:35:48 +0000

    This commit was manufactured by cvs2svn to create tag 'prescroll20040520'.

--- jmri/jmrit/throttle/ControlPanelListener.java
+++
@@ -1,8 +0,0 @@
-package jmri.jmrit.throttle;
-
-public interface ControlPanelListener extends java.util.EventListener
-{
-        public void notifySpeedChanged(int speed);
-        public void notifyDirectionChanged(boolean isForward);
-
-}
