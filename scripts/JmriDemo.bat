c09dff258703f1dc4505d194d583b5db3e7fa675
Bob Jacobsen <jacobsen@mac.com>
2004-03-25 04:17:58 +0000

    This commit was manufactured by cvs2svn to create tag 'jake20040325a'.

--- scripts/JmriDemo.bat
+++
@@ -1,3 +0,0 @@
-REM Start the JmriDemo Java program ($Revision: 1.6 $)
-
-java -noverify -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/comm.jar;lib/crimson.jar;lib/jdom-jdk11.jar;lib/jython.jar" apps.JmriDemo.JMRIdemo
