a4676e0e6d690375062a674eaf84ee309c872907
Bob Jacobsen <jacobsen@mac.com>
2004-04-07 02:07:14 +0000

    update from Paul Fraker

---
+++ site/throttles/Revision.js
@@ -0,0 +1,14 @@
+// JMRI Throttle Documentation
+//    Creation Date: 04.05.2004
+//    Author: Paul Fraker
+//            paul@frakers.com
+//            www.frakers.com
+//
+// Revision.js
+//
+// Javascript that simply applies the current revision number and date.
+
+document.write("<P CLASS=\"Tiny\">Revision 2 — 04.05.2004 — based on JMRI 1.3.10</P>");
+
+// End of script
+
