24703f2a01f2fcbaad520e030c397359871e5109
Bob Jacobsen <jacobsen@mac.com>
2004-04-21 22:54:43 +0000

    extend SerialNode to contain comms info

--- jmri/jmrix/cmri/serial/SerialNode.java
+++ jmri/jmrix/cmri/serial/SerialNode.java
@@ -19,7 +19,7 @@ import jmri.Sensor;
  * that node is polled.
  *
  * @author	Bob Jacobsen Copyright (C) 2003
- * @version	$Revision: 1.5 $
+ * @version	$Revision: 1.6 $
  */
 public class SerialNode {
 
@@ -34,6 +34,26 @@ public class SerialNode {
      * Must be less than, and is general one less than,
      * {@link SerialSensorManager#SENSORSPERUA}
      */
+    // class constants 
+    public static final int SMINI = 1;          // SMINI node type
+    public static final int USIC_SUSIC = 2;     // USIC/SUSIC node type 
+    public static final byte INPUT_CARD = 1;    // USIC/SUSIC input card type for specifying location
+    public static final byte OUTPUT_CARD = 2;   // USIC/SUSIC output card type for specifying location
+    public static final byte NO_CARD = 0;       // USIC/SUSIC unused location
+    // node definition instance variables
+    public int nodeAddress = 0;                 // UA, Node address, 0-127 allowed
+    protected int nodeType = SMINI;             // See above
+    protected int numInputCards = 1;            // 1 on SMINI, 0-63 on USIC/SUSIC 
+    protected int numOutputCards = 2;           // 2 on SMINI, 0-63 on USIC/SUSIC
+                                                // Note:  Only 64 total cards allowed on USIC/SUSIC
+    protected int bitsPerCard = 24;             // 24 for SMINI and USIC, 24 or 32 for SUSIC
+    protected int num2LSearchLights = 0;        // SMINI only, 'NS' number of two lead bicolor signals
+    protected byte[] locSearchLightBits = new byte[48]; // SMINI only, 0 = not searchlight LED, 
+                                                //   1 = searchlight LED, 2*NS bits must be set to 1
+    protected byte[] cardTypeLocation = new byte[64]; // USIC/SUSIC only, the first numInputCards+numOutputCards
+                                                //   must be set to either INPUT_CARD or OUTPUT_CARD, and 
+                                                //   the remaining locations must be set to NO_CARD
+         
     static final int MAXSENSORS = 999;
     protected int LASTUSEDSENSOR = 1;  // grows as sensors defined
 
