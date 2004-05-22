766dc422956e9c7f19142fbea401798ecf9d6603
Bob Jacobsen <jacobsen@mac.com>
2004-05-22 04:56:01 +0000

    complete

---
+++ jmri/jmrix/cmri/serial/nodeconfig/NodeConfigAction.java
@@ -0,0 +1,37 @@
+// NodeConfigAction.java
+
+package jmri.jmrix.cmri.serial.nodeconfig;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+/**
+ * Swing action to create and register a
+ *       			NodeConfigFrame object
+ *
+ * @author	Bob Jacobsen    Copyright (C) 2001
+ * @version	$Revision: 1.1 $
+ */
+public class NodeConfigAction extends AbstractAction {
+
+	public NodeConfigAction(String s) { super(s);}
+
+    public NodeConfigAction() {
+        this("Configure C/MRI nodes");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+		NodeConfigFrame f = new NodeConfigFrame();
+		try {
+			f.initComponents();
+			}
+		catch (Exception ex) {
+			log.error("Exception: "+ex.toString());
+			}
+		f.show();
+	}
+   static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(NodeConfigAction.class.getName());
+}
+
+
+/* @(#)SerialPacketGenAction.java */
