cb9666fdbc7af9d89488df4cebf6004ff7bf90ac
Bob Jacobsen <jacobsen@mac.com>
2004-05-28 02:34:10 +0000

    add button to start configuring nodes

--- jmri/jmrix/cmri/serial/serialdriver/ConnectionConfig.java
+++ jmri/jmrix/cmri/serial/serialdriver/ConnectionConfig.java
@@ -2,14 +2,15 @@
 
 package jmri.jmrix.cmri.serial.serialdriver;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import jmri.jmrix.cmri.serial.nodeconfig.NodeConfigAction;
 
 /**
  * Definition of objects to handle configuring an LocoBuffer layout connection
  * via an C/MRI SerialDriverAdapter object.
  *
  * @author      Bob Jacobsen   Copyright (C) 2001, 2003
- * @version	$Revision: 1.2 $
+ * @version	$Revision: 1.3 $
  */
 public class ConnectionConfig  extends jmri.jmrix.AbstractConnectionConfig {
 
@@ -28,10 +29,25 @@ public class ConnectionConfig  extends jmri.jmrix.AbstractConnectionConfig {
     }
 
     public void loadDetails(JPanel details) {
-        super.loadDetails(details);
-        opt1Box.setEditable(true);
+    	// have to embed the usual one in a new JPanel
+    	
+    	JPanel p = new JPanel();
+        super.loadDetails(p);
+
+		details.setLayout(new BoxLayout(details,BoxLayout.Y_AXIS));
+		details.add(p);
+
+		// add another button
+		JButton b = new JButton("Edit node configurations");
+
+		details.add(b);
+						
+		b.addActionListener(new NodeConfigAction());		
+        
     }
 
+	JFrame frame;
+	
     public String name() { return "C/MRI"; }
 
     protected void setInstance() { adapter = SerialDriverAdapter.instance(); }
