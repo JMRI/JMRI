07ca345fd07a5104df63cc41ca6370904f3b8bbe
Dave Duchamp <djduchamp@users.sourceforge.net>
2004-05-29 01:46:45 +0000

    Minor proofing change

--- jmri/jmrix/cmri/serial/nodeconfig/NodeConfigAction.java
+++ jmri/jmrix/cmri/serial/nodeconfig/NodeConfigAction.java
@@ -10,26 +10,27 @@ import java.awt.event.ActionEvent;
  *       			NodeConfigFrame object
  *
  * @author	Bob Jacobsen    Copyright (C) 2001
- * @version	$Revision: 1.1 $
+ * @version	$Revision: 1.2 $
  */
 public class NodeConfigAction extends AbstractAction {
 
 	public NodeConfigAction(String s) { super(s);}
 
     public NodeConfigAction() {
-        this("Configure C/MRI nodes");
+        this("Configure C/MRI Nodes");
     }
 
     public void actionPerformed(ActionEvent e) {
-		NodeConfigFrame f = new NodeConfigFrame();
-		try {
-			f.initComponents();
-			}
-		catch (Exception ex) {
-			log.error("Exception: "+ex.toString());
-			}
-		f.show();
-	}
+        NodeConfigFrame f = new NodeConfigFrame();
+        try {
+            f.initComponents();
+            }
+        catch (Exception ex) {
+            log.error("Exception: "+ex.toString());
+            }
+        f.setLocation(100,30);
+        f.show();
+    }
    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(NodeConfigAction.class.getName());
 }
 
