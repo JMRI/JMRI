7b0cc0d0e86f37632cc97374c324b555987b5d6f
Dave Duchamp <djduchamp@users.sourceforge.net>
2004-06-11 17:59:54 +0000

    Add Routes

--- jmri/jmrit/ToolsMenu.java
+++ jmri/jmrit/ToolsMenu.java
@@ -11,7 +11,7 @@ import java.util.*;
  * Create a "Tools" menu containing the Jmri system-independent tools
  *
  * @author	Bob Jacobsen   Copyright 2003
- * @version     $Revision: 1.6 $
+ * @version     $Revision: 1.7 $
  */
 public class ToolsMenu extends JMenu {
     public ToolsMenu(String name) {
@@ -41,6 +41,7 @@ public class ToolsMenu extends JMenu {
         add(new jmri.jmrit.beantable.SensorTableAction(rb.getString("MenuItemSensorTable")));
         add(new jmri.jmrit.beantable.LightTableAction(rb.getString("MenuItemLightTable")));
         add(new jmri.jmrit.beantable.SignalHeadTableAction(rb.getString("MenuItemSignalTable")));
+        add(new jmri.jmrit.beantable.RouteTableAction(rb.getString("MenuItemRouteTable")));
         add(new jmri.jmrit.blockboss.BlockBossAction(rb.getString("MenuItemSimpleSignal")));
         add(new JSeparator());
 
