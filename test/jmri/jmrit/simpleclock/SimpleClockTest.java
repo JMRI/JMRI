8ca23baf33082476a7e80d0edb706f6a0f0b8b10
Bob Jacobsen <jacobsen@mac.com>
2004-06-14 01:56:29 +0000

    working tests for new package

---
+++ test/jmri/jmrit/simpleclock/SimpleClockTest.java
@@ -0,0 +1,36 @@
+// SimpleClockTest.java
+
+package jmri.jmrit.simpleclock;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.util.ResourceBundle;
+
+/**
+ * Tests for the jmrit.simpleclock package
+ * @author			Bob Jacobsen
+ * @version $Revision: 1.1 $
+ */
+public class SimpleClockTest extends TestCase {
+
+	// from here down is testing infrastructure
+
+	public SimpleClockTest(String s) {
+		super(s);
+	}
+
+	// Main entry point
+	static public void main(String[] args) {
+		String[] testCaseName = {SimpleClockTest.class.getName()};
+		junit.swingui.TestRunner.main(testCaseName);
+	}
+
+	// test suite from all defined tests
+	public static Test suite() {
+		TestSuite suite = new TestSuite("jmri.jmrit.simpleclock.SimpleClockTest"); // no tests in class itself
+		suite.addTest(jmri.jmrit.simpleclock.SimpleTimebaseTest.suite());
+		return suite;
+	}
+
+}
