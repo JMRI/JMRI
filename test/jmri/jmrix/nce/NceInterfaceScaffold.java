d701f71301896969621a3541e77de8248b8eeaa5
Bob Jacobsen <jacobsen@mac.com>
2004-07-03 22:19:22 +0000

    new service file for NCE tests

---
+++ test/jmri/jmrix/nce/NceInterfaceScaffold.java
@@ -0,0 +1,26 @@
+/** 
+ * NceInterfaceScaffold.java
+ *
+ * Description:	    Stands in for the NceTrafficController class
+ * @author			Bob Jacobsen
+ * @version			$Revision: 1.1 $
+ */
+
+package jmri.jmrix.nce;
+
+import jmri.*;
+
+class NceInterfaceScaffold implements jmri.jmrix.nce.NceListener {
+	public NceInterfaceScaffold() {
+		rcvdReply = null;
+		rcvdMsg = null;
+	}
+	public void message(NceMessage m) {rcvdMsg = m;}
+	public void reply(NceReply r) {rcvdReply = r;}
+
+	NceReply rcvdReply;
+	NceMessage rcvdMsg;
+	
+	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(NceInterfaceScaffold.class.getName());
+
+}
