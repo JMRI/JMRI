378d691409b8592e99a89e1f8d0281ee1ba433be
Bob Jacobsen <jacobsen@mac.com>
2004-07-04 00:06:19 +0000

    This commit was manufactured by cvs2svn to create tag 'Release-1-4-1'.

--- scripts/DecoderPro.bat
+++
@@ -1,4 +0,0 @@
-REM Start the DecoderPro Java program ($Revision: 1.8 $)
-
-java -noverify -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/comm.jar;lib/crimson.jar;lib/jdom-jdk11.jar;lib/jython.jar" apps.DecoderPro.DecoderPro
-
