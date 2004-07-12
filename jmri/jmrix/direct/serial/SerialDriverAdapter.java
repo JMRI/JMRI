ceb07e0ed3f6bf5db1bdc3917f9f1daece9ff5f3
Bob Jacobsen <jacobsen@mac.com>
2004-07-12 22:12:02 +0000

    get smarter about output baud rate

--- jmri/jmrix/direct/serial/SerialDriverAdapter.java
+++ jmri/jmrix/direct/serial/SerialDriverAdapter.java
@@ -8,12 +8,18 @@ import jmri.jmrix.direct.TrafficController;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Enumeration;
 import java.util.Vector;
 
 import javax.comm.CommPortIdentifier;
 import javax.comm.PortInUseException;
 import javax.comm.SerialPort;
+
+import Serialio.SerInputStream;
+import Serialio.SerOutputStream;
+import Serialio.SerialConfig;
+import Serialio.SerialPortLocal;
 
 /**
  * Implements SerialPortAdapter for direct serial drive
@@ -25,7 +31,7 @@ import javax.comm.SerialPort;
  *
  *
  * @author			Bob Jacobsen   Copyright (C) 2001, 2002, 2004
- * @version			$Revision: 1.3 $
+ * @version			$Revision: 1.4 $
  */
 public class SerialDriverAdapter extends PortController  implements jmri.jmrix.SerialPortAdapter {
 
@@ -33,59 +39,139 @@ public class SerialDriverAdapter extends PortController  implements jmri.jmrix.S
     SerialPort activeSerialPort = null;
 
     public Vector getPortNames() {
-        // first, check that the comm package can be opened and ports seen
-        portNameVector = new Vector();
-        Enumeration portIDs = CommPortIdentifier.getPortIdentifiers();
-        // find the names of suitable ports
-        while (portIDs.hasMoreElements()) {
-            CommPortIdentifier id = (CommPortIdentifier) portIDs.nextElement();
-            // accumulate the names in a vector
-            portNameVector.addElement(id.getName());
-		  }
+        portNameVector = null;
+        try {
+            // this has to work through one of two sets of class. If
+            // Serialio.SerialConfig exists on this machine, we use that
+            // else we revert to javax.comm
+            try {
+                Class.forName("Serialio.SerialConfig");
+                log.debug("openPort using SerialIO");
+                InnerSerial inner = new InnerSerial();
+                inner.getPortNames();
+            } catch (ClassNotFoundException e) {
+                log.debug("openPort using javax.comm");
+                InnerJavaComm inner = new InnerJavaComm();
+                inner.getPortNames();
+            } catch (java.lang.UnsatisfiedLinkError e) {
+                log.debug("openPort using javax.comm");
+                InnerJavaComm inner = new InnerJavaComm();
+                inner.getPortNames();
+            }
+        }
+        catch (Exception ex) {
+            log.error("error listing port names");
+            ex.printStackTrace();
+        }
+
         return portNameVector;
     }
 
-    public String openPort(String portName, String appName)  {
-        // open the port, check ability to set moderators
-        try {
+    class InnerSerial {
+        public Vector getPortNames() {
+            // first, check that the comm package can be opened and ports seen
+            portNameVector = new Vector();
+            try {
+                String[] names = SerialPortLocal.getPortList();
+                // accumulate the names in a vector
+                for (int i=0; i<names.length; i++) {
+                    portNameVector.addElement(names[i]);
+                }
+            }
+            catch (java.io.IOException e) { log.error("IO exception listing ports: "+e); }
+            return portNameVector;
+        }
+        public String openPort(String portName, String appName) throws java.io.IOException {
+            // get and open the primary port
+            SerialConfig config = new SerialConfig(portName);
+
+            // try to set it for 16457 baud, then fall back if needed
+            config.setBitRate(16457);
+            config.setDataBits(SerialConfig.LN_8BITS);
+            config.setStopBits(SerialConfig.ST_1BITS);
+            config.setParity(SerialConfig.PY_NONE);
+            config.setHandshake(SerialConfig.HS_NONE);
+            Serialio.SerialPort activeSerialPort = new SerialPortLocal(config);
+
+            // set RTS high, DTR low to power the MS100
+            activeSerialPort.setRTS(true);		// not connected in some serial ports and adapters
+            activeSerialPort.setDTR(false);		// pin 1 in DIN8; on main connector, this is DTR
+
+            // get and save stream
+            serialInStream = new SerInputStream(activeSerialPort);
+            serialOutStream = new SerOutputStream(activeSerialPort);
+
+            // report status
+            if (log.isInfoEnabled()) {
+                log.info(portName+" port opened, sees "
+                         +" DSR: "+activeSerialPort.sigDSR()
+                         +" CTS: "+activeSerialPort.sigCTS()
+                         +"  CD: "+activeSerialPort.sigCD()
+                         );
+            }
+            return null;
+        }
+    }
+
+    class InnerJavaComm  {
+        public Vector getPortNames() {
+            // first, check that the comm package can be opened and ports seen
+            portNameVector = new Vector();
+            Enumeration portIDs = CommPortIdentifier.getPortIdentifiers();
+            // find the names of suitable ports
+            while (portIDs.hasMoreElements()) {
+                CommPortIdentifier id = (CommPortIdentifier) portIDs.nextElement();
+                // accumulate the names in a vector
+                portNameVector.addElement(id.getName());
+            }
+            return portNameVector;
+        }
+
+        public String openPort(String portName, String appName) throws javax.comm.NoSuchPortException, javax.comm.UnsupportedCommOperationException,
+                                                                       java.io.IOException {
             // get and open the primary port
             CommPortIdentifier portID = CommPortIdentifier.getPortIdentifier(portName);
+            javax.comm.SerialPort activeSerialPort = null;
             try {
                 activeSerialPort = (SerialPort) portID.open(appName, 100);  // name of program, msec to wait
-            } catch (PortInUseException p) {
+            }
+            catch (PortInUseException p) {
                 return handlePortBusy(p, portName, log);
             }
 
-            // try to set it for comunication via SerialDriver
+            // try to set it for 17240, then 16457 baud, then 19200 if needed
             try {
-                activeSerialPort.setSerialPortParams(19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+                activeSerialPort.setSerialPortParams(17240, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
             } catch (javax.comm.UnsupportedCommOperationException e) {
-                log.error("Cannot set serial parameters on port "+portName+": "+e.getMessage());
-                return "Cannot set serial parameters on port "+portName+": "+e.getMessage();
+                // assume that's a baudrate problem, fall back.
+                log.warn("attempting to fall back to 16457 baud after 17240 failed");
+                try {
+                    activeSerialPort.setSerialPortParams(16457, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+                } catch (javax.comm.UnsupportedCommOperationException e2) {
+                    log.warn("trouble setting 16457 baud");
+                    activeSerialPort.setSerialPortParams(19200, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
+                    javax.swing.JOptionPane.showMessageDialog(null,
+                                                              "Failed to set the correct baud rate. Port is set to "
+                                                              +activeSerialPort.getBaudRate()+
+                                                              " baud. See the README file for more info.",
+                                                              "Connection failed", javax.swing.JOptionPane.ERROR_MESSAGE);
+                }
             }
 
-            // set RTS high, DTR high
-            activeSerialPort.setRTS(true);		// not connected in some serial ports and adapters
-            activeSerialPort.setDTR(true);		// pin 1 in DIN8; on main connector, this is DTR
+            // set RTS high, DTR low in case power is needed
+            activeSerialPort.setRTS(true);          // not connected in some serial ports and adapters
+            activeSerialPort.setDTR(false);         // pin 1 in DIN8; on main connector, this is DTR
 
             // disable flow control; hardware lines used for signaling, XON/XOFF might appear in data
             activeSerialPort.setFlowControlMode(0);
 
-            // set timeout
             // activeSerialPort.enableReceiveTimeout(1000);
             log.debug("Serial timeout was observed as: "+activeSerialPort.getReceiveTimeout()
                       +" "+activeSerialPort.isReceiveTimeoutEnabled());
 
             // get and save stream
-            serialStream = activeSerialPort.getInputStream();
-
-            // purge contents, if any
-            int count = serialStream.available();
-            log.debug("input stream shows "+count+" bytes available");
-            while ( count > 0) {
-                serialStream.skip(count);
-                count = serialStream.available();
-            }
+            serialInStream = activeSerialPort.getInputStream();
+            serialOutStream = activeSerialPort.getOutputStream();
 
             // report status?
             if (log.isInfoEnabled()) {
@@ -93,24 +179,55 @@ public class SerialDriverAdapter extends PortController  implements jmri.jmrix.S
                          +activeSerialPort.getBaudRate()+" baud, sees "
                          +" DTR: "+activeSerialPort.isDTR()
                          +" RTS: "+activeSerialPort.isRTS()
-						+" DSR: "+activeSerialPort.isDSR()
+                         +" DSR: "+activeSerialPort.isDSR()
                          +" CTS: "+activeSerialPort.isCTS()
                          +"  CD: "+activeSerialPort.isCD()
                          );
             }
+            return null;
+        }
+    }
+
+    public String openPort(String portName, String appName)  {
+        try {
+            // this has to work through one of two sets of class. If
+            // Serialio.SerialConfig exists on this machine, we use that
+            // else we revert to javax.comm
+            try {
+                Class.forName("Serialio.SerialConfig");
+                log.debug("openPort using SerialIO");
+                InnerSerial inner = new InnerSerial();
+                String result = inner.openPort(portName, appName);
+                if (result!=null) return result;
+            } catch (ClassNotFoundException e) {
+                log.debug("openPort using javax.comm");
+                InnerJavaComm inner = new InnerJavaComm();
+                String result = inner.openPort(portName, appName);
+                if (result!=null) return result;
+            } catch (java.lang.UnsatisfiedLinkError e) {
+                log.debug("openPort using javax.comm");
+                InnerJavaComm inner = new InnerJavaComm();
+                String result = inner.openPort(portName, appName);
+                if (result!=null) return result;
+            }
+
+            // port is open, regardless of method, start work on the stream
+            // purge contents, if any
+            int count = serialInStream.available();
+            log.debug("input stream shows "+count+" bytes available");
+            while ( count > 0) {
+                serialInStream.skip(count);
+                count = serialInStream.available();
+            }
 
             opened = true;
 
-        } catch (javax.comm.NoSuchPortException p) {
-            return handlePortNotFound(p, portName, log);
-        } catch (Exception ex) {
-            log.error("Unexpected exception while opening port "+portName+" trace follows: "+ex);
+        }
+        catch (Exception ex) {
             ex.printStackTrace();
-            return "Unexpected error while opening port "+portName+": "+ex;
         }
 
-        return null; // indicates OK return
-
+        return null; // normal termination
     }
 
     /**
@@ -137,18 +254,12 @@ public class SerialDriverAdapter extends PortController  implements jmri.jmrix.S
             log.error("getInputStream called before load(), stream not available");
             return null;
         }
-        return new DataInputStream(serialStream);
+        return new DataInputStream(serialInStream);
     }
 
     public DataOutputStream getOutputStream() {
         if (!opened) log.error("getOutputStream called before load(), stream not available");
-        try {
-            return new DataOutputStream(activeSerialPort.getOutputStream());
-        }
-        catch (java.io.IOException e) {
-            log.error("getOutputStream exception: "+e);
-     	}
-     	return null;
+        return new DataOutputStream(serialOutStream);
     }
 
     public boolean status() {return opened;}
@@ -162,7 +273,8 @@ public class SerialDriverAdapter extends PortController  implements jmri.jmrix.S
 
     // private control members
     private boolean opened = false;
-    InputStream serialStream = null;
+    InputStream serialInStream = null;
+    OutputStream serialOutStream = null;
 
     static public SerialDriverAdapter instance() {
         if (mInstance == null) mInstance = new SerialDriverAdapter();
