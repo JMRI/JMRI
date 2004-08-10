42834e815699db1b0e361bf46f4fbc359774f3e0
Bob Jacobsen <jacobsen@mac.com>
2004-08-10 05:05:46 +0000

    change to LocoBuffer-II, with hyphen.  Correct note about switch settings

--- jmri/jmrix/loconet/locobufferii/LocoBufferIIAdapter.java
+++ jmri/jmrix/loconet/locobufferii/LocoBufferIIAdapter.java
@@ -9,7 +9,7 @@ import jmri.jmrix.loconet.locobuffer.LocoBufferAdapter;
  * refers to the switch settings on the new LocoBuffer II
  
  * @author			Bob Jacobsen   Copyright (C) 2004
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  */
 public class LocoBufferIIAdapter extends LocoBufferAdapter {
 
@@ -26,7 +26,10 @@ public class LocoBufferIIAdapter extends LocoBufferAdapter {
     public String[] validBaudRates() {
         return validSpeeds;
     }
-    protected String [] validSpeeds = new String[]{"19,200 baud (J1 off)", "57,600 baud (J1 on)"};
+    protected String [] validSpeeds = new String[]{"19,200 baud (Sw1 off, Sw3 off)", 
+    											"57,600 baud (Sw1 on, Sw3 off)"};
+
+    public String option1Name() { return "LocoBuffer-II connection uses "; }
 
     static public LocoBufferAdapter instance() {
         if (m2Instance == null) m2Instance = new LocoBufferIIAdapter();
