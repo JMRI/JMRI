32a6bf6bbda214383e6b7cbb2e401932030e3ffe
Bob Jacobsen <jacobsen@mac.com>
2004-09-01 13:00:49 +0000

    example of playing a sound, working version

---
+++ scripts/jython/SampleSound.py
@@ -0,0 +1,15 @@
+# This is an example script for playing a sound in a JMRI script
+#
+# The next line is maintained by CVS, please don't change it
+# $Revision: 1.1 $
+
+import jmri
+
+# create the sound object by loading a file
+snd = jmri.jmrit.Sound("resources/sounds/RlyClick.wav")
+
+# play the sound once
+snd.play()
+
+# You can also do snd.loop() to start playing the sound
+# as a continuous loop, and snd.stop() to stop it
