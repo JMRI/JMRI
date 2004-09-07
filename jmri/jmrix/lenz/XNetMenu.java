eb63fd514830a8fc7c0ccf4476f1de4d4b8a69c2
Paul Bender <paul.bender@acm.org>
2004-09-07 03:33:13 +0000

    reorganize menu

--- jmri/jmrix/lenz/XNetMenu.java
+++ jmri/jmrix/lenz/XNetMenu.java
@@ -10,7 +10,7 @@ import javax.swing.JMenu;
  * Create a menu containing the XPressNet specific tools
  *
  * @author	Paul Bender   Copyright 2003
- * @version     $Revision: 2.1 $
+ * @version     $Revision: 2.2 $
  */
 public class XNetMenu extends JMenu {
     public XNetMenu(String name) {
@@ -28,11 +28,12 @@ public class XNetMenu extends JMenu {
 
         add(new jmri.jmrix.lenz.mon.XNetMonAction(rb.getString("MenuItemXNetCommandMonitor")));
         add(new jmri.jmrix.lenz.systeminfo.SystemInfoAction(rb.getString("MenuItemXNetSystemInformation")));
+        add(new jmri.jmrix.lenz.packetgen.PacketGenAction(rb.getString("MenuItemSendXNetCommand")));
+	add(new javax.swing.JSeparator());
         //add(new jmri.jmrix.lenz.datamon.DataMonAction(rb.getString("MenuItemCSDatabaseManager")));
         add(new jmri.jmrix.lenz.li101.LI101Action(rb.getString("MenuItemLI101ConfigurationManager")));
         add(new jmri.jmrix.lenz.lzv100.LZV100Action(rb.getString("MenuItemLZV100ConfigurationManager")));
         add(new jmri.jmrix.lenz.lv102.LV102Action(rb.getString("MenuItemLV102ConfigurationManager")));
-        add(new jmri.jmrix.lenz.packetgen.PacketGenAction(rb.getString("MenuItemSendXNetCommand")));
 
     }
 
