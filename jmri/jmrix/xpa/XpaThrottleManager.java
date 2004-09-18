dca4927e78064719eada3725a250accf74f03a44
Paul Bender <paul.bender@acm.org>
2004-09-18 07:02:55 +0000

    Update to use AbstractThrottleManager.

--- jmri/jmrix/xpa/XpaThrottleManager.java
+++ jmri/jmrix/xpa/XpaThrottleManager.java
@@ -4,15 +4,15 @@ import jmri.ThrottleListener;
 import jmri.ThrottleManager;
 import com.sun.java.util.collections.HashMap;
 
+import jmri.jmrix.AbstractThrottleManager;
+
 /**
  * XPA implementation of a ThrottleManager
  * @author     Paul Bender Copyright (C) 2004
- * @version    $Revision: 1.1 $
+ * @version    $Revision: 1.2 $
  */
 
-public class XpaThrottleManager implements ThrottleManager {
-    private HashMap throttleListeners;
-    private HashMap throttleMap;
+public class XpaThrottleManager extends AbstractThrottleManager implements ThrottleManager {
 
     /**
      * Constructor.
@@ -23,52 +23,16 @@ public class XpaThrottleManager implements ThrottleManager {
     }
 
     /**
-     * Request a throttle, given a decoder address. When the decoder address
-     * is located, the ThrottleListener gets a callback via the ThrottleListener.notifyThrottleFound
-     * method.
-     * @param address The decoder address desired.
-     * @param l The ThrottleListener awaiting notification of a found throttle.
-     * @return True if the request will not continue, false if the request
-     * will be made.  False may be returned if the throttle is already in use.
-     */
-    public boolean requestThrottle(int address, ThrottleListener l)
-    {
-	boolean throttleInUse = false;
-        if (throttleListeners == null)
-        {
-            throttleListeners = new HashMap(5);
-        }
-
-        Integer addressKey = new Integer(address);
-        if (throttleListeners.containsKey(addressKey))
-        {
- 	    throttleInUse=true;
-        }
-	else
-	{
-        	throttleListeners.put(addressKey, l);
-		XpaThrottle throttle=new XpaThrottle(address);
-		l.notifyThrottleFound(throttle);
-        }
-        return(throttleInUse);
-    }
-
-    /**
-     * Cancel a request for a throttle
-     * @param address The decoder address desired.
-     * @param l The ThrottleListener cancelling request for a throttle.
-     */
-    public void cancelThrottleRequest(int address, ThrottleListener l)
-    {
-        if (throttleListeners != null)
-        {
-            Integer addressKey = new Integer(address);
-            throttleListeners.remove(addressKey);
-        }
-    }
+     * Request a new throttle object be creaetd for the address, and let
+     * the throttle listeners know about it.
+     **/
+     public void requestThrottleSetup(int address) {
+	XpaThrottle throttle=new XpaThrottle(address);
+        notifyThrottleKnown(throttle,address);
+     }
 
     /*
-     * Lenz Systems DO NOT use the Dispatch Function
+     * The XPA DOES NOT use the Dispatch Function
      */
     public boolean hasDispatchFunction() { return false; }
 
