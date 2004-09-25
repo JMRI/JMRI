d2fb71f06972d028195c26c9d328229d93979ab4
Bob Jacobsen <jacobsen@mac.com>
2004-09-25 04:46:43 +0000

    add additional info to error message on link faliure

--- jmri/jmrix/AbstractConnectionConfig.java
+++ jmri/jmrix/AbstractConnectionConfig.java
@@ -15,7 +15,7 @@ import javax.swing.JPanel;
  * Abstract base class for common implementation of the ConnectionConfig
  *
  * @author      Bob Jacobsen   Copyright (C) 2001, 2003
- * @version	$Revision: 1.6 $
+ * @version	$Revision: 1.7 $
  */
 abstract public class AbstractConnectionConfig  implements jmri.jmrix.ConnectionConfig {
 
@@ -93,6 +93,7 @@ abstract public class AbstractConnectionConfig  implements jmri.jmrix.Connection
             }
         } catch (java.lang.UnsatisfiedLinkError e1) {
             log.error("UnsatisfiedLinkError - the javax.comm library has not been installed properly");
+            log.error("java.library.path="+System.getProperty("java.library.path","<unknown>"));
             javax.swing.JOptionPane.showMessageDialog(null, "Failed to load comm library.\nYou have to fix that before setting preferences.");
             return;
         }
