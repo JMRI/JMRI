c0976472fdbcb24c33e4d7dd7b7782eaf7cc7436
Bob Jacobsen <jacobsen@mac.com>
2004-11-14 17:21:15 +0000

    RFE 989410 - turn off java3D acceleration

--- scripts/LocoTools.bat
+++ scripts/LocoTools.bat
@@ -1,3 +1,3 @@
-REM Start the LocoTools Java program ($Revision: 1.4 $)
+REM Start the LocoTools Java program ($Revision: 1.5 $)
 
-java -noverify -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/comm.jar;lib/crimson.jar;lib/jdom-jdk11.jar;lib/jython.jar" apps.LocoTools.LocoTools
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;lib/jmriplugins.jar;jmri.jar;lib/log4j.jar;lib/collections.jar;lib/jh.jar;lib/comm.jar;lib/crimson.jar;lib/jdom-jdk11.jar;lib/jython.jar" apps.LocoTools.LocoTools
