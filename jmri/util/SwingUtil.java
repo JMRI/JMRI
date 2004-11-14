4adf08aedf93e49f2cb1ef35a3c705d24abf69be
Alex Shepherd <kiwi64ajs@gmail.com>
2004-11-14 22:06:29 +0000

    fix indentation

--- jmri/util/SwingUtil.java
+++ jmri/util/SwingUtil.java
@@ -9,20 +9,19 @@ import javax.swing.JComponent;
  * Java 1.1.8 system, or at least try to fake it.
  *
  * @author Alex Shepherd  Copyright 2004
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 
 public class SwingUtil {
 
-	static public boolean setFocusable( JComponent component, boolean focusable ) {
-	try {
-		component.setFocusable( focusable) ;
-		return true ;
-	} catch (Throwable e) { // NoSuchMethodError, NoClassDefFoundError and others on early JVMs
-		// just carry on with original fonts
-		return false;
+	static public boolean setFocusable( JComponent component, boolean focusable )
+	{
+		try {
+			component.setFocusable( focusable) ;
+			return true ;
+		} catch (Throwable e) { // NoSuchMethodError, NoClassDefFoundError and others on early JVMs
+			// just carry on with original fonts
+			return false;
+		}
 	}
-}
-
-
 }
