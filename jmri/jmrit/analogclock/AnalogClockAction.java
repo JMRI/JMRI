bfce5e4e357499059c634572d6d45ce7d15a28b8
Bob Jacobsen <jacobsen@mac.com>
2004-12-06 01:55:36 +0000

    disable on Mac classic, as Java 1.1.8 cant handle Graphics2D

--- jmri/jmrit/analogclock/AnalogClockAction.java
+++ jmri/jmrit/analogclock/AnalogClockAction.java
@@ -11,7 +11,7 @@ import java.awt.event.*;
  * Copied from code for NixieClockAction by Bob Jacobsen
  *
  * @author			Dennis Miller    Copyright (C) 2004
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  */
 
 
@@ -19,6 +19,10 @@ import java.awt.event.*;
 public class AnalogClockAction extends AbstractAction {
   public AnalogClockAction(String s) {
     super(s);
+    // see if on a system where shouldnt even try
+    if (System.getProperty("os.name").equals("Mac OS")) {
+        setEnabled(false);
+    }
   }
   public void actionPerformed(ActionEvent e) {
 
