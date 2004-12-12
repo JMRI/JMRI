f30d7100cda60d974232adec3aff6442c2e647d3
Bob Jacobsen <jacobsen@mac.com>
2004-12-12 00:14:20 +0000

    remove code that disabled this action on Mac Classic.  Dennis updated the AnalogClockFrame class to run under Java 1.1.8, so this is no longer needed

--- jmri/jmrit/analogclock/AnalogClockAction.java
+++ jmri/jmrit/analogclock/AnalogClockAction.java
@@ -11,7 +11,7 @@ import java.awt.event.*;
  * Copied from code for NixieClockAction by Bob Jacobsen
  *
  * @author			Dennis Miller    Copyright (C) 2004
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  */
 
 
@@ -19,10 +19,6 @@ import java.awt.event.*;
 public class AnalogClockAction extends AbstractAction {
   public AnalogClockAction(String s) {
     super(s);
-    // see if on a system where shouldnt even try
-    if (System.getProperty("os.name").equals("Mac OS")) {
-        setEnabled(false);
-    }
   }
   public void actionPerformed(ActionEvent e) {
 
