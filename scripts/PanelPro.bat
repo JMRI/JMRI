e9b4b474ec97dbd016fbeecd1669adced330173b
Bob Jacobsen <jacobsen@mac.com>
2005-04-07 17:38:18 +0000

    This commit was manufactured by cvs2svn to create tag 'tag'.

--- scripts/PanelPro.bat
+++
@@ -1,3 +0,0 @@
-REM Start the PanelPro Java program ($Revision: 1.8 $)
-
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;collections.jar;jh.jar;crimson.jar;jdom-jdk11.jar;jython.jar;MRJAdapter.jar" apps.PanelPro.PanelPro
