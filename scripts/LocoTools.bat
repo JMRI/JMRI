b01beb950ff122b13d494b49251007961cb089b4
Alex Shepherd <kiwi64ajs@gmail.com>
2005-06-13 23:52:13 +0000

    added % to end of command line to allow options to be passed from shortcuts removed lib/ path of security policy to allow lib directory to be removed

--- scripts/LocoTools.bat
+++ scripts/LocoTools.bat
@@ -1,3 +1,3 @@
-REM Start the LocoTools Java program ($Revision: 1.8 $)
+REM Start the LocoTools Java program ($Revision: 1.9 $)
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=lib/security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;collections.jar;jh.jar;crimson.jar;jdom-jdk11.jar;jython.jar;MRJAdapter.jar" apps.LocoTools.LocoTools
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;collections.jar;jh.jar;crimson.jar;jdom-jdk11.jar;jython.jar;MRJAdapter.jar" apps.LocoTools.LocoTools %*
