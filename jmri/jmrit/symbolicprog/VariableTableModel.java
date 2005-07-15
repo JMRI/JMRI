196236d402b153ee3fd7a3ae7717cce3a2ba85d6
Howard G. Penny <innkeeper@users.sourceforge.net>
2005-07-15 02:26:49 +0000

    Added support for paired indexed CVs.

--- jmri/jmrit/symbolicprog/VariableTableModel.java
+++ jmri/jmrit/symbolicprog/VariableTableModel.java
@@ -21,7 +21,7 @@ import org.jdom.Element;
  *
  * @author    Bob Jacobsen   Copyright (C) 2001
  * @author    Howard G. Penny   Copyright (C) 2005
- * @version   $Revision: 1.24 $
+ * @version   $Revision: 1.25 $
  */
 public class VariableTableModel extends AbstractTableModel implements ActionListener, PropertyChangeListener {
 
@@ -374,7 +374,6 @@ public class VariableTableModel extends AbstractTableModel implements ActionList
             v.setIntValue(Integer.valueOf(val).intValue());
             _cvModel.getCvByNumber(CV).setState(VariableValue.FROMFILE);  // correct for transition to "edited"
         }
-
     }
 
     private int _piCv = -1;
@@ -406,7 +405,7 @@ public class VariableTableModel extends AbstractTableModel implements ActionList
 
         // get the values for the VariableValue ctor
         String name = e.getAttribute("label").getValue(); 	// Note the name variable is actually the label attribute
-        if (log.isDebugEnabled()) log.debug("Starting to setRow \""+name+"\"");
+        if (log.isDebugEnabled()) log.debug("Starting to setIndexedRow \""+name+"\"");
         String cvName = e.getAttributeValue("CVname");
         String item = ( e.getAttribute("item")!=null ?
                         e.getAttribute("item").getValue() :
@@ -520,6 +519,46 @@ public class VariableTableModel extends AbstractTableModel implements ActionList
             }
             v1.lastItem();
 
+        } else if (( child = e.getChild("indexedPairVal")) != null) {
+            if ((a = child.getAttribute("min")) != null)
+                minVal = Integer.valueOf(a.getValue()).intValue();
+            if ((a = child.getAttribute("max")) != null)
+                maxVal = Integer.valueOf(a.getValue()).intValue();
+            int factor = 1;
+            if ((a = child.getAttribute("factor")) != null)
+                factor = Integer.valueOf(a.getValue()).intValue();
+            int offset = 0;
+            if ((a = child.getAttribute("offset")) != null)
+                offset = Integer.valueOf(a.getValue()).intValue();
+            String uppermask = "VVVVVVVV";
+            if ((a = child.getAttribute("upperMask")) != null)
+                uppermask = a.getValue();
+            String highCVname = "";
+            int highCVnumber = -1;
+            int highCVpiVal = -1;
+            int highCVsiVal = -1;
+            if ((a = child.getAttribute("highCVname")) != null) {
+                highCVname = a.getValue();
+                int x = highCVname.indexOf('.');
+                highCVnumber = Integer.valueOf(highCVname.substring(0, x)).intValue();
+                int y = highCVname.indexOf('.',x+1);
+                if (y > 0) {
+                    highCVpiVal = Integer.valueOf(highCVname.substring(x+1, y)).intValue();
+                    x = highCVname.lastIndexOf('.');
+                    highCVsiVal = Integer.valueOf(highCVname.substring(x+1)).intValue();
+                } else {
+                    x = highCVname.lastIndexOf('.');
+                    highCVpiVal = Integer.valueOf(highCVname.substring(x+1)).intValue();
+                }
+            }
+            // ensure highCVnumber exists
+            int highCVrow = _indxCvModel.addIndxCV(row, highCVname,
+                                                   _piCv, highCVpiVal, _siCv, highCVsiVal, highCVnumber,
+                                                   readOnly, infoOnly, writeOnly);
+            iv = new IndexedPairVariableValue(row, name, comment, cvName, readOnly, infoOnly, writeOnly, opsOnly,
+                                       cv, mask, minVal, maxVal, _indxCvModel.allIndxCvVector(),
+                                       _status, item, highCVrow, highCVname, factor, offset, uppermask);
+
         } else {
             reportBogus();
             return -1;
@@ -536,15 +575,17 @@ public class VariableTableModel extends AbstractTableModel implements ActionList
         iv.addPropertyChangeListener(this);
 
         // set to default value if specified (CV load may later override this)
-        if ( (a = e.getAttribute("default")) != null) {
+        if ((a = e.getAttribute("default")) != null) {
             String val = a.getValue();
             if (log.isDebugEnabled()) log.debug("Found default value: "+val+" for "+name);
             iv.setIntValue(Integer.valueOf(val).intValue());
-            if (_indxCvModel.getCvByNumber(row).getInfoOnly()) {
-                _indxCvModel.getCvByNumber(row).setState(VariableValue.READ);
+            if (_indxCvModel.getCvByRow(row).getInfoOnly()) {
+                _indxCvModel.getCvByRow(row).setState(VariableValue.READ);
             } else {
-                _indxCvModel.getCvByNumber(row).setState(VariableValue.FROMFILE); // correct for transition to "edited"
+                _indxCvModel.getCvByRow(row).setState(VariableValue.FROMFILE); // correct for transition to "edited"
             }
+        } else {
+            _indxCvModel.getCvByRow(row).setState(VariableValue.UNKNOWN);
         }
         return row;
     }
