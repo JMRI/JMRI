367a41a5d736820bea7354882000990b49a737b9
Paul Bender <paul.bender@acm.org>
2005-07-16 05:12:08 +0000

    Add Stack Monitor to menu.

--- jmri/jmrix/lenz/XNetMenu.java
+++ jmri/jmrix/lenz/XNetMenu.java
@@ -10,7 +10,7 @@ import javax.swing.JMenu;
  * Create a menu containing the XPressNet specific tools
  *
  * @author	Paul Bender   Copyright 2003
- * @version     $Revision: 2.3 $
+ * @version     $Revision: 2.4 $
  */
 public class XNetMenu extends JMenu {
     public XNetMenu(String name) {
@@ -30,7 +30,7 @@ public class XNetMenu extends JMenu {
         add(new jmri.jmrix.lenz.systeminfo.SystemInfoAction(rb.getString("MenuItemXNetSystemInformation")));
         add(new jmri.jmrix.lenz.packetgen.PacketGenAction(rb.getString("MenuItemSendXNetCommand")));
 	add(new javax.swing.JSeparator());
-        //add(new jmri.jmrix.lenz.datamon.DataMonAction(rb.getString("MenuItemCSDatabaseManager")));
+        add(new jmri.jmrix.lenz.stackmon.StackMonAction(rb.getString("MenuItemCSDatabaseManager")));
         add(new jmri.jmrix.lenz.li101.LI101Action(rb.getString("MenuItemLI101ConfigurationManager")));
         add(new jmri.jmrix.lenz.lz100.LZ100Action(rb.getString("MenuItemLZ100ConfigurationManager")));
         add(new jmri.jmrix.lenz.lzv100.LZV100Action(rb.getString("MenuItemLZV100ConfigurationManager")));
