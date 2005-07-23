ffb1ae13218e2b1c31051123a69ee9e481b68999
Bob Jacobsen <jacobsen@mac.com>
2005-07-23 05:18:45 +0000

    add javadoc header

--- jmri/jmrit/throttle/ControlPanelListener.java
+++ jmri/jmrit/throttle/ControlPanelListener.java
@@ -1,5 +1,10 @@
 package jmri.jmrit.throttle;
 
+/**
+ *
+ * @author     glen  Copyright (C) 2002
+ * @version    $Revision: 1.2 $
+ */
 public interface ControlPanelListener extends java.util.EventListener
 {
         public void notifySpeedChanged(int speed);
