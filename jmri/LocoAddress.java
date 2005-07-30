1d44572d7e83f6719318a57b20c511240ddb5706
Bob Jacobsen <jacobsen@mac.com>
2005-07-30 09:37:41 +0000

    new classes for encapsulating loco addressing

---
+++ jmri/LocoAddress.java
@@ -0,0 +1,20 @@
+// LocoAddress.java
+
+package jmri;
+
+
+/** 
+ * Inteface for generic Locomotive Address.
+ *
+ * Note that this is not DCC-specific.
+ *
+ * @author			Bob Jacobsen Copyright (C) 2005
+ * @version			$Revision: 1.1 $
+ */
+
+public interface LocoAddress {
+
+}
+
+
+/* @(#)LocoAddress.java */
