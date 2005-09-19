e243e4e4735db7ce043d6c10e2d4f9764ca1217e
Bob Jacobsen <jacobsen@mac.com>
2005-09-19 04:07:06 +0000

    new test classes

--- test/jmri/util/UtilTest.java
+++ test/jmri/util/UtilTest.java
@@ -8,7 +8,7 @@ import junit.framework.*;
  * Invokes complete set of tests in the jmri.util tree
  *
  * @author	    Bob Jacobsen  Copyright 2003
- * @version         $Revision: 1.1 $
+ * @version         $Revision: 1.2 $
  */
 public class UtilTest extends TestCase {
 
@@ -26,6 +26,8 @@ public class UtilTest extends TestCase {
     // test suite from all defined tests
     public static Test suite() {
         TestSuite suite = new TestSuite("jmri.util.UtilTest");   // no tests in this class itself
+        suite.addTest(jmri.util.JSpinnerUtilTest.suite());
+        suite.addTest(jmri.util.VectorUtilTest.suite());
         suite.addTest(jmri.util.StringUtilTest.suite());
 
         return suite;
