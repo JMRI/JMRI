ed4c79ba09e46513c57d4df502f0e068d7bbd52f
Bob Jacobsen <jacobsen@mac.com>
2005-12-18 06:33:16 +0000

    Add formating. Add timed requests for updates

--- jmri/jmrix/pricom/pockettester/StatusFrame.java
+++ jmri/jmrix/pricom/pockettester/StatusFrame.java
@@ -3,6 +3,7 @@
 package jmri.jmrix.pricom.pockettester;
 
 import javax.swing.*;
+import com.sun.java.util.collections.Hashtable;
 
 /**
  * Simple to display DCC status from Pocket Tester
@@ -10,34 +11,36 @@ import javax.swing.*;
  * For more info on the product, see http://www.pricom.com
  *
  * @author			Bob Jacobsen   Copyright (C) 2005
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  */
 public class StatusFrame extends javax.swing.JFrame implements DataListener {
 
     static java.util.ResourceBundle rb 
             = java.util.ResourceBundle.getBundle("jmri.jmrix.pricom.pockettester.TesterBundle");
 
-    public StatusFrame() { super(); }
+    javax.swing.Timer timer = new javax.swing.Timer(500, new java.awt.event.ActionListener() {
+        public void actionPerformed(java.awt.event.ActionEvent e) {
+            sendRequest();
+        }
+    });
+
+    JTabbedPane tabPane = new JTabbedPane();
+    Hashtable displayHash = new Hashtable();
+    Hashtable formatHash = new Hashtable();
+    DataSource source = null;
+    
+    public StatusFrame() { super(rb.getString("TitleStatus")); }
     
     /**
      * Add GUI elements
      */
     public void initComponents() {
-        JTabbedPane tabPane = new JTabbedPane();
         getContentPane().add(tabPane);
         
-        // sequence through, adding panes
-        // first, do timing manually
-        {
-            JPanel p = new JPanel();
-            p.add(new JLabel("place-saver"));
-            
-            tabPane.addTab(rb.getString("CSMainPaneName"), p);
-        }
-        
-        // next, loop over the auto definitions from the properties file.
+        // loop over the auto definitions from the properties file, adding panes
         // get pane count
         int numAutoPane = Integer.parseInt(rb.getString("CSNumAutoPanes"));
+        
         for (int i = 0; i<numAutoPane; i++) {
             // create and install tabbed pane
             JPanel p = new JPanel();
@@ -48,45 +51,112 @@ public class StatusFrame extends javax.swing.JFrame implements DataListener {
             int numVars = Integer.parseInt(rb.getString("CS"+i+"NumVars"));
             for (int j = 0; j<numVars; j++) {
                 p.add(new JLabel(rb.getString("CS"+i+"Var"+j+"Name")));
-                p.add(new JLabel("-----"));
+                JLabel val = new JLabel("-----");
+                // record the label and format for later
+                displayHash.put(rb.getString("CS"+i+"Var"+j+"ID"),val);
+                formatHash.put(rb.getString("CS"+i+"Var"+j+"ID"),rb.getString("CS"+i+"Var"+j+"Format"));
+                p.add(val);
             }
         }
         
+        // add a listener to hear about selections
+        tabPane.addChangeListener(new javax.swing.event.ChangeListener(){
+                public void stateChanged(javax.swing.event.ChangeEvent e) {
+                    sendRequest();  // to get a fast update
+                }
+            }
+        );
         
+        // want to dispose this window when closed
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         
-        // and get ready to display
+        // get ready to display
         pack();
+
+        // start the timer for updates
+        timer.setRepeats(true);     // in case we run by
+        timer.start();
+
     }
     
     protected String title() {
-        String title = filter;
-        if (filter == null) title = "";
-        return java.text.MessageFormat.format(rb.getString("TitleStatus"),
-                                                              new String[] {title});
+        return rb.getString("TitleStatus");
     }
 
+    public void asciiFormattedMessage(String m) { 
+        // check if interesting
+        if (!m.substring(0,1).equals(" ")) return;
+        if (!m.substring(3,4).equals("=")) return;
+        // basically OK. Break into tokens, store if match, quit when done.
+        while (m.length() > 4) {
+            String id = m.substring(1, 3);
+            String value = m.substring(4, 14);
+            if (log.isDebugEnabled()) log.debug("set var "+id+":"+value);
+            JLabel label = (JLabel)displayHash.get(id);
+            String format = (String)formatHash.get(id);
+            if (label != null) label.setText(convertValue(value, format));
+            m = m.substring(14);
+        }
+    }
+    
+    protected String convertValue(String val, String format) {
+        if (format.equals("address")) {
+            // long or short address format
+            int address = Integer.valueOf(val).intValue();
+            if (address >= 0x8000)
+                return ""+(address-0x8000)+" (long)";
+            else 
+                return ""+address+" (short)";
+                
+        } else if (format.equals("msec")) {
+            // value is in 10's of nsec, add decimal point for msec
+            return val.substring(0,5)+"."+val.substring(5)+" msec";
+            
+        } else if (format.equals("none")) {
+            // no formatting, used for counts, etc
+            return val; // don't change the content
+            
+        } else
+        // didn't find matching format name, warn
+        return val+" (bad format)";
+    }
+    
+    /**
+     * Time to send the next request
+     */
+    private void sendRequest() {
+        int i = tabPane.getSelectedIndex();
+        String prompt = rb.getString("CS"+i+"PromptChar");
+        if (log.isDebugEnabled()) log.debug("send "+prompt+" for pane "+i);
+        
+        if (source == null) {
+            log.error("DataSource should not be null in sendRequest");
+            timer.stop();
+            return;
+        }
+        
+        source.sendBytes(prompt.getBytes());
+        
+    }
+        
+    // Close the window when the close box is clicked
+    void thisWindowClosing(java.awt.event.WindowEvent e) {
+        setVisible(false);
+        // dispose();
+        dispose();
+    }
+
+    public void setSource(DataSource s) {
+        source = s;
+    }
+    
     public void dispose() {
+        // stop timer
+        timer.stop();
         // and clean up parent
         super.dispose();
     }
 
-    public void asciiFormattedMessage(String m) { 
-        //if ( (filter==null) || m.startsWith(filter) )
-        //    nextLine(m, "");
-    }
-    String filter = null;
-    
-    /**
-     * Start filtering input to include only lines that
-     * start with a specific string.
-     * A null input passes all.
-     */
-    public void setFilter(String s) {
-        filter = s;
-        setTitle(title());
-    }
-    
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(StatusFrame.class.getName());
-
 
 }
