13f7926df69931a7a108b8bda77882822386e98d
Bob Jacobsen <jacobsen@mac.com>
2006-01-17 05:33:14 +0000

    minor cleanup. Move command station programmer up with rst of programmers

--- jmri/jmrix/loconet/LocoNetMenu.java
+++ jmri/jmrix/loconet/LocoNetMenu.java
@@ -10,7 +10,7 @@ import javax.swing.JMenu;
  * Create a "Systems" menu containing the Jmri LocoNet-specific tools.
  *
  * @author	Bob Jacobsen   Copyright 2003
- * @version     $Revision: 1.6 $
+ * @version     $Revision: 1.7 $
  */
 public class LocoNetMenu extends JMenu {
     public LocoNetMenu(String name) {
@@ -28,6 +28,7 @@ public class LocoNetMenu extends JMenu {
 
         add(new jmri.jmrix.loconet.locomon.LocoMonAction(rb.getString("MenuItemLocoNetMonitor")));
         add(new jmri.jmrix.loconet.slotmon.SlotMonAction(rb.getString("MenuItemSlotMonitor")));
+        add(new jmri.jmrix.loconet.locobuffer.LocoBufferStatsAction( rb.getString("MenuItemLocoBufferStats")));
         add(new jmri.jmrix.loconet.locogen.LocoGenAction(rb.getString("MenuItemSendPacket")));
         add(new javax.swing.JSeparator());
         add(new jmri.jmrix.loconet.bdl16.BDL16Action(rb.getString("MenuItemBDL16Programmer")));
@@ -35,12 +36,11 @@ public class LocoNetMenu extends JMenu {
         add(new jmri.jmrix.loconet.pm4.PM4Action(rb.getString("MenuItemPM4Programmer")));
         add(new jmri.jmrix.loconet.se8.SE8Action(rb.getString("MenuItemSE8cProgrammer")));
         add(new jmri.jmrix.loconet.ds64.DS64Action(rb.getString("MenuItemDS64Programmer")));
+        add(new jmri.jmrix.loconet.cmdstnconfig.CmdStnConfigAction( rb.getString("MenuItemCmdStnConfig")));
         add(new javax.swing.JSeparator());
         add(new jmri.jmrit.messager.MessageFrameAction( rb.getString("MenuItemThrottleMessages")));
         add(new jmri.jmrix.loconet.locormi.LnMessageServerAction( rb.getString("MenuItemStartLocoNetServer")));
-        add(new javax.swing.JSeparator());
         add(new jmri.jmrix.loconet.clockmon.ClockMonAction( rb.getString("MenuItemClockMon")));
-        add(new jmri.jmrix.loconet.locobuffer.LocoBufferStatsAction( rb.getString("MenuItemLocoBufferStats")));
         add(new javax.swing.JSeparator());
         add(new jmri.jmrix.loconet.downloader.LoaderPanelAction( rb.getString("MenuItemDownload")));
 
