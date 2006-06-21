9ac3b82ace92cbd8d6ca94d924354715c9f622ba
Bob Jacobsen <jacobsen@mac.com>
2006-06-21 03:57:54 +0000

    fix ambig call due to JVM 1.5 varargs parsing

--- jmri/util/PythonInterp.java
+++ jmri/util/PythonInterp.java
@@ -21,7 +21,7 @@ import java.io.*;
  * on Windows machines where it might normally appear in a filename.
  *
  * @author	Bob Jacobsen    Copyright (C) 2004
- * @version     $Revision: 1.4 $
+ * @version     $Revision: 1.5 $
  */
 public class PythonInterp {
 
@@ -109,7 +109,7 @@ public class PythonInterp {
             // PySystemState.initialize();
             Class cs = Class.forName("org.python.core.PySystemState");
             java.lang.reflect.Method initialize =
-                        cs.getMethod("initialize",null);
+                        cs.getMethod("initialize",(Class[])null);
             initialize.invoke(null, null);
 
             // interp = new PythonInterpreter();
