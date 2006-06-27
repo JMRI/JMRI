68c357852ae28d21576746cdc3775ea42bd26d7e
Bob Jacobsen <jacobsen@mac.com>
2006-06-27 05:09:14 +0000

    new test, passes

---
+++ test/jmri/jmrit/display/PanelEditorTest.java
@@ -0,0 +1,79 @@
+package jmri.jmrit.display;
+
+import jmri.jmrit.XmlFile;
+
+import java.io.File;
+import java.io.FileWriter;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.beans.PropertyChangeEvent;
+
+/**
+ * PanelEditorTest.java
+ *
+ * Description:
+ * @author			Bob Jacobsen
+ * @version			$Revision: 1.1 $
+ */
+public class PanelEditorTest extends TestCase {
+
+    TurnoutIcon to = null;
+
+    // The minimal setup is for log4J
+    apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
+    protected void setUp() { log4jfixtureInst.setUp(); }
+    protected void tearDown() { log4jfixtureInst.tearDown(); }
+
+	public void testShow() throws java.io.IOException {
+	    // create a test file
+        XmlFile.ensurePrefsPresent("prefs");
+        XmlFile.ensurePrefsPresent("prefs"+File.separator+"temp");
+	    File f = new File(XmlFile.prefsDir()+File.separator+"temp"+File.separator+"PanelEditorTest1.xml");
+	    FileWriter fw = new FileWriter(f);
+	    fw.write(layerTestData);
+	    fw.close();
+	    
+	    // load and display
+	    (new jmri.configurexml.ConfigXmlManager()).load(f);
+	}
+
+
+	// from here down is testing infrastructure
+
+	public PanelEditorTest(String s) {
+		super(s);
+	}
+
+	// Main entry point
+	static public void main(String[] args) {
+		String[] testCaseName = {PanelEditorTest.class.getName()};
+		junit.swingui.TestRunner.main(testCaseName);
+	}
+
+	// test suite from all defined tests
+	public static Test suite() {
+		TestSuite suite = new TestSuite(PanelEditorTest.class);
+		return suite;
+	}
+
+String layerTestData = 
+"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+"<!DOCTYPE layout-config SYSTEM \"layout-config.dtd\">"+
+"<layout-config>"+
+"  <paneleditor class=\"jmri.jmrit.display.configurexml.PanelEditorXml\" name=\"Layer Name Test\" x=\"20\" y=\"22\" height=\"226\" width=\"204\" editable=\"no\" positionable=\"yes\" controlling=\"yes\" hide=\"yes\" panelmenu=\"no\">"+
+"    <positionablelabel forcecontroloff=\"false\" fixed=\"false\" showtooltip=\"true\" class=\"jmri.jmrit.display.configurexml.PositionableLabelXml\" x=\"40\" y=\"130\" level=\"5\" text=\"Checks should be\" size=\"13\" style=\"0\" />"+
+"    <positionablelabel forcecontroloff=\"false\" fixed=\"false\" showtooltip=\"true\" class=\"jmri.jmrit.display.configurexml.PositionableLabelXml\" x=\"51\" y=\"149\" level=\"5\" text=\"in front of Xs\" size=\"13\" style=\"0\" />"+
+"    <turnouticon turnout=\"IT1\" x=\"45\" y=\"55\" level=\"10\" closed=\"resource:resources/icons/misc/Checkmark-green.gif\" thrown=\"resource:resources/icons/misc/Checkmark-green.gif\" unknown=\"resource:resources/icons/misc/Checkmark-green.gif\" inconsistent=\"resource:resources/icons/misc/Checkmark-green.gif\" rotate=\"0\" forcecontroloff=\"false\" class=\"jmri.jmrit.display.configurexml.TurnoutIconXml\" />"+
+"    <sensoricon sensor=\"IS1\" x=\"46\" y=\"53\" level=\"7\" active=\"resource:resources/icons/misc/X-red.gif\" inactive=\"resource:resources/icons/misc/X-red.gif\" unknown=\"resource:resources/icons/misc/X-red.gif\" inconsistent=\"resource:resources/icons/misc/X-red.gif\" rotate=\"0\" forcecontroloff=\"false\" momentary=\"false\" class=\"jmri.jmrit.display.configurexml.SensorIconXml\" />"+
+"    <turnouticon turnout=\"IT1\" x=\"136\" y=\"53\" level=\"7\" closed=\"resource:resources/icons/misc/X-red.gif\" thrown=\"resource:resources/icons/misc/X-red.gif\" unknown=\"resource:resources/icons/misc/X-red.gif\" inconsistent=\"resource:resources/icons/misc/X-red.gif\" rotate=\"0\" forcecontroloff=\"false\" class=\"jmri.jmrit.display.configurexml.TurnoutIconXml\" />"+
+"    <sensoricon sensor=\"IS1\" x=\"138\" y=\"54\" level=\"10\" active=\"resource:resources/icons/misc/Checkmark-green.gif\" inactive=\"resource:resources/icons/misc/Checkmark-green.gif\" unknown=\"resource:resources/icons/misc/Checkmark-green.gif\" inconsistent=\"resource:resources/icons/misc/Checkmark-green.gif\" rotate=\"0\" forcecontroloff=\"false\" momentary=\"false\" class=\"jmri.jmrit.display.configurexml.SensorIconXml\" />"+
+"  </paneleditor>"+
+"</layout-config>";
+
+
+
+	// static private org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(TurnoutIconTest.class.getName());
+
+}
