4b93d5ef3fbe79f3b85c2192d1f7b1c723c06ff0
Bob Jacobsen <jacobsen@mac.com>
2006-08-26 16:38:11 +0000

    add support for .wav sounds and playing from memory

--- jmri/jmrit/Sound.java
+++ jmri/jmrit/Sound.java
@@ -4,6 +4,8 @@ import java.applet.AudioClip;
 import java.net.URL;
 import java.net.MalformedURLException;
 
+import javax.sound.sampled.*;
+
 /**
  * Provide simple way to load and play sounds in JMRI.
  * <P>
@@ -13,9 +15,11 @@ import java.net.MalformedURLException;
  * of a basic interface, which would put it in jmri.jmrix.  It seems
  * most like a "tool using JMRI", or perhaps a tool for use with JMRI,
  * so it was placed in jmri.jmrit.
+ * <P>
+ * S@see jmri.jmrit.sound
  *
- * @author	Bob Jacobsen  Copyright (C) 2004
- * @version	$Revision: 1.1 $
+ * @author	Bob Jacobsen  Copyright (C) 2004, 2006
+ * @version	$Revision: 1.2 $
  */
 public class Sound  {
 
@@ -65,11 +69,93 @@ public class Sound  {
         }
     }
 
+    /**
+     * Play a sound from a buffer
+     *
+     */
+    public static void playSoundBuffer(byte[] wavData) {
+
+        // get characteristics from buffer
+        float sampleRate = 11200.0f;
+        int sampleSizeInBits = 8;
+        int channels = 1;
+        boolean signed = (sampleSizeInBits > 8 ? true : false);
+        boolean bigEndian = true;
+        
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+        SourceDataLine line;
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format); // format is an AudioFormat object
+        if (!AudioSystem.isLineSupported(info)) {
+            // Handle the error.
+            log.error("line not supported: "+info);
+            return;
+        }
+        // Obtain and open the line.
+        try {
+            line = (SourceDataLine) AudioSystem.getLine(info);
+            line.open(format);
+        } catch (LineUnavailableException ex) {
+            // Handle the error.
+            log.error("error opening line: "+ex);
+            return;
+        }    
+        line.start();
+        // write(byte[] b, int off, int len) 
+        line.write(wavData,0,wavData.length);
+
+    }
+
 
     /**
      * The actual sound, stored as an AudioClip
      */
     public AudioClip audioClip = null;
+
+    public class WavBuffer {
+        public WavBuffer(byte[] content) {
+            buffer = content;
+            
+            // find fmt chunk and set offset
+            int index = 12; // skip RIFF header
+            while (index<buffer.length) {
+                // new chunk
+                if (buffer[index]==0x66 && 
+                    buffer[index+1]==0x6D &&
+                    buffer[index+2]==0x74 &&
+                    buffer[index+3]==0x20) {
+                        // found it
+                        fmtOffset = index;
+                        return;
+                } else {
+                    // skip
+                    index = index + 8
+                            +buffer[index+4]
+                            +buffer[index+5]*256
+                            +buffer[index+6]*256*256
+                            +buffer[index+7]*256*256*256;
+                    System.out.println("index now "+index);
+                }
+            }
+            log.error("Didn't find fmt chunk");
+            
+        }
+        
+        int fmtOffset;
+        
+        byte[] buffer;
+        
+        float getSampleRate() {
+            return 11200.0f;
+        }
+        int getSampleSizeInBits() {
+            return 8;
+        }
+        int getChannels() {
+            return 1;
+        }
+        boolean getBigEndian() {return false;}
+        boolean getSigned() { return (getSampleSizeInBits()>8); }
+    }
 
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(Sound.class.getName());
 }
