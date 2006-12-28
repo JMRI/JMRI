d629516e994fd5534dd84a2e3ba3e9c938a581bc
Bob Jacobsen <jacobsen@mac.com>
2006-12-28 02:23:09 +0000

    intial version

---
+++ jmri/jmrix/loconet/soundloader/EditorFrame.java
@@ -0,0 +1,142 @@
+// LoaderFrame.java
+
+package jmri.jmrix.loconet.soundloader;
+
+import java.awt.Dimension;
+import java.util.ResourceBundle;
+import java.io.IOException;
+
+import javax.swing.*;
+import java.awt.*;
+
+import jmri.util.JmriJFrame;
+
+/**
+ * Frame for editing Digitrax SPJ files.
+ *
+ * This is just an enclosure for the EditorPane, which does the real work.
+ * 
+ * This handles file read/write.
+ *
+ * @author		Bob Jacobsen   Copyright (C) 2006
+ * @version             $Revision: 1.1 $
+ */
+public class EditorFrame extends JmriJFrame {
+
+    // GUI member declarations
+    EditorPane pane;
+
+    ResourceBundle res;
+    JButton open;
+    JButton save;
+    
+    public EditorFrame() {
+        super(ResourceBundle.getBundle("jmri.jmrix.loconet.soundloader.Editor").getString("TitleEditor"));
+        
+        // Its unfortunate that we have to read that bundle twice, but it's due to Java init order
+        res = ResourceBundle.getBundle("jmri.jmrix.loconet.soundloader.Editor");
+
+        // general GUI config
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        
+        // add file button
+        open = new JButton(res.getString("ButtonOpen"));
+        open.addActionListener(new AbstractAction() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                selectInputFile();
+            }
+        });
+
+        save = new JButton(res.getString("ButtonSave"));
+        save.addActionListener(new AbstractAction() {
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                selectSaveFile();
+            }
+        });
+        getContentPane().add(save);
+        save.setEnabled(false);
+
+        JPanel p = new JPanel();
+        p.setLayout(new FlowLayout());
+        p.add(open);
+        p.add(save);
+        getContentPane().add(p);
+        
+        // for now, for debugging, load the file
+        // from a fixed name
+        // pane = new EditorPane("ac4400.spj");
+        // pane = new EditorPane("ac4400-silence.spj");
+        // pane = new EditorPane("java/test/jmri/jmrix/loconet/spjfile/sd38_2.spj");
+        //getContentPane().add(pane);
+
+        pack();
+ 
+    }
+
+    static JFileChooser chooser;  // shared across all of these
+    
+    void selectInputFile() {
+        if (chooser == null) chooser = new JFileChooser(System.getProperty("user.dir"));
+        int retVal = chooser.showOpenDialog(this);
+        if (retVal != JFileChooser.APPROVE_OPTION) return;  // give up if no file selected
+        
+        // success, open the file
+        addFile(chooser.getSelectedFile().getName());
+    }
+
+    void selectSaveFile() {
+        if (chooser == null) chooser = new JFileChooser(System.getProperty("user.dir"));
+        int retVal = chooser.showSaveDialog(this);
+        if (retVal != JFileChooser.APPROVE_OPTION) return;  // give up if no file selected
+        
+        // success, open the file
+        try {
+            saveFile(chooser.getSelectedFile().getName());
+        } catch (IOException e) {
+            // failed, warn user
+            JOptionPane.showMessageDialog(this, "Error during save: "+e, 
+                                    "Save failed!", JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    void addFile(String name) {
+        if (pane != null) {
+            // already defined
+            return;
+        }
+        pane = new EditorPane(name);
+        getContentPane().add(pane);
+        open.setEnabled(false);
+        save.setEnabled(true);
+        pack();
+    }
+    
+    void saveFile(String name) throws IOException {
+        pane.saveFile(name);
+    }
+    
+    // handle resizing when first shown
+    private boolean mShown = false;
+    public void addNotify() {
+        super.addNotify();
+        if (mShown)
+            return;
+        // resize frame to account for menubar
+        JMenuBar jMenuBar = getJMenuBar();
+        if (jMenuBar != null) {
+            int jMenuBarHeight = jMenuBar.getPreferredSize().height;
+            Dimension dimension = getSize();
+            dimension.height += jMenuBarHeight;
+            setSize(dimension);
+        }
+        mShown = true;
+    }
+
+    // Close the window when the close box is clicked
+    void thisWindowClosing(java.awt.event.WindowEvent e) {
+        setVisible(false);
+        pane.dispose();
+        dispose();
+	// and disconnect from the SlotManager
+    }
+}
---
+++ jmri/jmrix/loconet/soundloader/EditorPane.java
@@ -0,0 +1,93 @@
+// EditorPane.java
+
+package jmri.jmrix.loconet.soundloader;
+
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+import javax.swing.text.*;
+import java.util.ResourceBundle;
+import java.io.IOException;
+
+import jmri.jmrix.loconet.*;
+import jmri.jmrix.loconet.spjfile.SpjFile;
+
+import jmri.util.JTableUtil;
+import jmri.util.com.sun.TableSorter;
+
+
+/**
+ * Pane for editing Digitrax SPJ files
+ * @author	    Bob Jacobsen   Copyright (C) 2006
+ * @version	    $Revision: 1.1 $
+ */
+public class EditorPane extends javax.swing.JPanel {
+
+    // GUI member declarations
+    static ResourceBundle res = ResourceBundle.getBundle("jmri.jmrix.loconet.soundloader.Editor");
+    
+    SpjFile file;
+    
+    public EditorPane(String name) {
+        // open and save file
+        try {
+            file = new SpjFile(name);
+            file.read();
+        } catch (IOException e) {
+            log.error("Exception reading file: "+e);
+            e.printStackTrace();
+            return;
+        }
+
+        // start to configure GUI
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // create and include table
+        EditorTableDataModel dataModel = new EditorTableDataModel(file);
+        
+        JTable dataTable	= JTableUtil.sortableDataModel(dataModel);
+        JScrollPane dataScroll	= new JScrollPane(dataTable);
+
+        // give system name column a smarter sorter and use it initially
+        try {
+            TableSorter tmodel = ((TableSorter)dataTable.getModel());
+            tmodel.setColumnComparator(String.class, new jmri.util.SystemNameComparator());
+            tmodel.setSortingStatus(EditorTableDataModel.HEADERCOL, TableSorter.ASCENDING);
+        } catch (java.lang.ClassCastException e) {}  // happens if not sortable table
+        
+        // configure items for GUI
+        dataModel.configureTable(dataTable);
+
+        add(dataScroll);
+        
+        // some stuff at bottom for now       
+        add(new JSeparator());
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.Y_AXIS));
+        
+        JPanel p1 = new JPanel();
+        p1.add(new JLabel(res.getString("LabelSize")));
+        JTextField t1 = new JTextField(12);
+        t1.setEditable(false);
+        p1.add(t1);
+        
+        bottom.add(p1);
+        add(bottom);
+    }
+    
+    public void saveFile(String name) throws java.io.IOException {
+        file.save(name);
+    }
+    
+    /**
+     * Get rid of any held resources
+     */
+    void dispose() {
+        file.dispose();
+        file = null;  // not for GC, this flags need to reinit
+    }
+
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(EditorPane.class.getName());
+
+}
