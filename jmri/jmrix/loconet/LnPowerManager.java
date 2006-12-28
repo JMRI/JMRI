cf51759681806f3be876afe04857cb392e1534ce
Bob Jacobsen <jacobsen@mac.com>
2006-12-28 02:25:15 +0000

    general cleanup while working on this during PR2 development

--- jmri/jmrix/loconet/LnPowerManager.java
+++ jmri/jmrix/loconet/LnPowerManager.java
@@ -2,8 +2,8 @@
 
 package jmri.jmrix.loconet;
 
-import jmri.JmriException;
 import jmri.PowerManager;
+import jmri.JmriException;
 
 /**
  * PowerManager implementation for controlling layout power
@@ -15,58 +15,60 @@ import jmri.PowerManager;
  * contact Digitrax Inc for separate permission.
  * <P>
  * @author	Bob Jacobsen Copyright (C) 2001
- * @version         $Revision: 1.4 $
+ * @version         $Revision: 1.5 $
  */
 public class LnPowerManager implements PowerManager, LocoNetListener {
 
 	public LnPowerManager() {
-		// connect to the TrafficManager
+	    // standard LocoNet - connect
 		tc = LnTrafficController.instance();
 		tc.addLocoNetListener(~0, this);
 	}
 
-	int power = UNKNOWN;
+	protected int power = UNKNOWN;
 
 	public void setPower(int v) throws JmriException {
 		power = UNKNOWN;
-		checkTC();
-		if (v==ON) {
-			// send GPON
-			LocoNetMessage l = new LocoNetMessage(2);
-			l.setOpCode(LnConstants.OPC_GPON);
-			tc.sendLocoNetMessage(l);
-		} else if (v==OFF) {
-			// send GPOFF
-			LocoNetMessage l = new LocoNetMessage(2);
-			l.setOpCode(LnConstants.OPC_GPOFF);
-			tc.sendLocoNetMessage(l);
-		}
+		
+        checkTC();
+        if (v==ON) {
+            // send GPON
+            LocoNetMessage l = new LocoNetMessage(2);
+            l.setOpCode(LnConstants.OPC_GPON);
+            tc.sendLocoNetMessage(l);
+        } else if (v==OFF) {
+            // send GPOFF
+            LocoNetMessage l = new LocoNetMessage(2);
+            l.setOpCode(LnConstants.OPC_GPOFF);
+            tc.sendLocoNetMessage(l);
+        }
+
 		firePropertyChange("Power", null, null);
 	}
 
 	public int getPower() { return power;}
 
 	// to free resources when no longer used
-	public void dispose() throws JmriException {
-		tc.removeLocoNetListener(~0, this);
+	public void dispose() {
+		if (tc!=null) tc.removeLocoNetListener(~0, this);
 		tc = null;
 	}
 
+	LnTrafficController tc = null;
 	private void checkTC() throws JmriException {
-		if (tc == null) throw new JmriException("attempt to use LnPowerManager after dispose");
+		if (tc == null) throw new JmriException("Use power manager after dispose");
 	}
 
 	// to hear of changes
 	java.beans.PropertyChangeSupport pcs = new java.beans.PropertyChangeSupport(this);
 	public synchronized void addPropertyChangeListener(java.beans.PropertyChangeListener l) {
 		pcs.addPropertyChangeListener(l);
-		}
+	}
 	protected void firePropertyChange(String p, Object old, Object n) { pcs.firePropertyChange(p,old,n);}
 	public synchronized void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
 		pcs.removePropertyChangeListener(l);
-		}
+	}
 
-	LnTrafficController tc = null;
 
 	// to listen for status changes from LocoNet
 	public void message(LocoNetMessage m) {
