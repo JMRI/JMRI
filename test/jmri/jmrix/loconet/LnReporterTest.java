c96690a15cf318106904b831cd3e7080607019ba
Bob Jacobsen <jacobsen@mac.com>
2007-01-24 06:47:27 +0000

    new LnReporter tests in transponding and LISSY mode

---
+++ test/jmri/jmrix/loconet/LnReporterTest.java
@@ -0,0 +1,87 @@
+// LnReporterTest.java
+
+package jmri.jmrix.loconet;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Tests for the jmri.jmrix.loconet.LnReporter class.
+ * @author	Bob Jacobsen Copyright 2001, 2002
+ * @version     $Revision: 1.1 $
+ */
+public class LnReporterTest extends TestCase {
+
+    public void testLnReporterCreate() {
+        LnReporter a1 = new LnReporter(1);
+    }
+
+    public void testTranspond3Enter() {
+        LnReporter a = new LnReporter(146);
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xD0, 0x21, 0x11, 0x7D, 0x03, 0x00});
+        a.message(l);
+        assertEquals("Transponding 3 enter", "3 enter", a.getLastReport().toString());
+    }
+
+    public void testTranspond257Enter() {
+        LnReporter a = new LnReporter(146);
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xD0, 0x21, 0x11, 0x02, 0x01, 0x00});
+        a.message(l);
+        assertEquals("Transponding 257 enter", "257 enter", a.getLastReport().toString());
+    }
+
+    public void testTranspond257Exit() {
+        LnReporter a = new LnReporter(146);
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xD0, 0x01, 0x11, 0x02, 0x01, 0x00});
+        a.message(l);
+        assertEquals("Transponding 257 exits", "257 exits", a.getLastReport().toString());
+    }
+
+    public void testTranspond3Exits() {
+        LnReporter a = new LnReporter(146);
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xD0, 0x01, 0x11, 0x7D, 0x03, 0x00});
+        a.message(l);
+        assertEquals("Transponding 3 exits", "3 exits", a.getLastReport().toString());
+    }
+
+    public void testLnReporterLissy1() {
+        LnReporter a1 = new LnReporter(1);
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xE4,0x08,0x00,0x60,0x01,0x42,0x35,0x05});
+        a1.message(l);
+        assertEquals("Lissy message 1", "8501 seen southbound", a1.getLastReport().toString());
+    }
+
+    public void testLnReporterLissy2() {
+        LnReporter a3 = new LnReporter(3);
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xE4,0x08,0x00,0x40,0x03,0x42,0x35,0x05});
+        a3.message(l);
+        assertEquals("Lissy message 2", "8501 seen northbound", a3.getLastReport().toString());
+    }
+
+    // from here down is testing infrastructure
+
+    public LnReporterTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+    	String[] testCaseName = {LnReporterTest.class.getName()};
+    	junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(LnReporterTest.class);
+        return suite;
+    }
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(LnReporterTest.class.getName());
+    // The minimal setup for log4J
+    apps.tests.Log4JFixture log4jfixtureInst = new apps.tests.Log4JFixture(this);
+    protected void setUp() { log4jfixtureInst.setUp(); }
+    protected void tearDown() { log4jfixtureInst.tearDown(); }
+
+}
