6157fd07da854a008ed009e1ebc3770bc2f43ce0
Bob Jacobsen <jacobsen@mac.com>
2007-02-04 06:19:15 +0000

    fix trail comment

--- jmri/jmrit/sendpacket/SendPacketAction.java
+++ jmri/jmrit/sendpacket/SendPacketAction.java
@@ -10,7 +10,7 @@ import javax.swing.*;
  * Swing action to create and register a SendPacketFrame object
  *
  * @author	Bob Jacobsen    Copyright (C) 2003
- * @version     $Revision: 1.4 $
+ * @version     $Revision: 1.5 $
  */
 public class SendPacketAction extends AbstractAction {
 
@@ -39,4 +39,4 @@ public class SendPacketAction extends AbstractAction {
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(SendPacketAction.class.getName());
 }
 
-/* @(#)LocoGenAction.java */
+/* @(#)SendPacketAction.java */
