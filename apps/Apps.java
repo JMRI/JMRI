812caf3861f59d08690c7d20deca6c5494a521c4
Bob Jacobsen <jacobsen@mac.com>
2007-02-23 16:27:18 +0000

    add location display to help menu

--- apps/Apps.java
+++ apps/Apps.java
@@ -28,7 +28,7 @@ import net.roydesign.mac.MRJAdapter;
  * <P>
  * @author	Bob Jacobsen   Copyright 2003
  * @author  Dennis Miller  Copyright 2005
- * @version     $Revision: 1.33 $
+ * @version     $Revision: 1.34 $
  */
 public class Apps extends JPanel {
 
@@ -291,9 +291,14 @@ public class Apps extends JPanel {
         } catch (java.lang.NoSuchMethodError e2) {
             log.error("Is jh.jar available? Error starting help system: "+e2);
         }
-        JMenuItem license = new JMenuItem("License");
+
+        JMenuItem license = new JMenuItem("License ...");
         helpMenu.add(license);
         license.addActionListener(new LicenseAction());
+
+        JMenuItem directories = new JMenuItem("Locations ...");
+        helpMenu.add(directories);
+        directories.addActionListener(new jmri.jmrit.XmlFileLocationAction());
     }
 
     protected String line1() {
