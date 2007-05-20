4ce4c1724a998354c88cc8262890189d01e4e625
Bob Jacobsen <jacobsen@mac.com>
2007-05-20 04:09:33 +0000

    from D Boudreau

--- jmri/jmrix/nce/macro/NceMacroGenFrame.java
+++ jmri/jmrix/nce/macro/NceMacroGenFrame.java
@@ -11,14 +11,19 @@ import javax.swing.*;
  * Frame for user input of Nce macros
  * @author	Bob Jacobsen   Copyright (C) 2001
  * @author Dan Boudreau 	Copyright (C) 2007
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  **/
-public class NceMacroGenFrame extends jmri.util.JmriJFrame implements jmri.jmrix.nce.NceListener {
 
-    // member declarations
+public class NceMacroGenFrame extends jmri.util.JFrame implements jmri.jmrix.nce.NceListener {
+
+	private static final int REPLY_LEN = 1;
+	
+	// member declarations
     javax.swing.JLabel jLabel1 = new javax.swing.JLabel();
+    javax.swing.JLabel macroText = new javax.swing.JLabel();
+    javax.swing.JLabel macroReply = new javax.swing.JLabel();
     javax.swing.JButton sendButton = new javax.swing.JButton();
-    javax.swing.JTextField packetTextField = new javax.swing.JTextField(12);
+    javax.swing.JTextField packetTextField = new javax.swing.JTextField(4);
 
     public NceMacroGenFrame() {
     }
@@ -26,8 +31,14 @@ public class NceMacroGenFrame extends jmri.util.JmriJFrame implements jmri.jmrix
     public void initComponents() throws Exception {
         // the following code sets the frame's initial state
 
-        jLabel1.setText("Macro: ");
+        jLabel1.setText("  Macro: ");
         jLabel1.setVisible(true);
+        
+        macroText.setText("  Reply: "); 
+        macroText.setVisible(true);
+        
+        macroReply.setText("unknown"); 
+        macroReply.setVisible(true);
 
         sendButton.setText("Send");
         sendButton.setVisible(true);
@@ -40,10 +51,12 @@ public class NceMacroGenFrame extends jmri.util.JmriJFrame implements jmri.jmrix
 
         
         setTitle("Execute NCE macro");
-        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        getContentPane().setLayout(new GridLayout(4,2));
 
         getContentPane().add(jLabel1);
         getContentPane().add(packetTextField);
+        getContentPane().add(macroText);
+        getContentPane().add(macroReply);
         getContentPane().add(sendButton);
  
         sendButton.addActionListener(new java.awt.event.ActionListener() {
@@ -51,7 +64,7 @@ public class NceMacroGenFrame extends jmri.util.JmriJFrame implements jmri.jmrix
                     sendButtonActionPerformed(e);
                 }
             });
-
+  
         // pack for display
         pack();
     }
@@ -62,21 +75,34 @@ public class NceMacroGenFrame extends jmri.util.JmriJFrame implements jmri.jmrix
 
 		NceMessage m = createMacroCmd(packetTextField.getText());
 		if (m == null) {
+			macroReply.setText("error");
 			JOptionPane.showMessageDialog(NceMacroGenFrame.this,
 					"Enter 1 to 255", "NCE Macro", JOptionPane.ERROR_MESSAGE);
 			return;
 		}
+		macroReply.setText("waiting");
 		NceTrafficController.instance().sendNceMessage(m, this);
 
 	}
 
     public void  message(NceMessage m) {}  // ignore replies
-    public void  reply(NceReply r) {} // ignore replies
+    public void reply(NceReply r) {
+		if (r.getNumDataElements() == REPLY_LEN) {
+
+			int recChar = r.getElement(0);
+			if (recChar == '!')
+				macroReply.setText("okay");
+			if (recChar == '0')
+				macroReply.setText("empty");
+
+		} else {
+			macroReply.setText("error");
+		}
+        
+    } 
 
     NceMessage createMacroCmd(String s) {
 		// gather bytes in result
-		if (s.length() == 0 | s.length() > 3)
-			return null; // max three char, macro 1 - 255
 		int macroNum = 0;
 		try {
 			macroNum = Integer.parseInt(s);
@@ -91,8 +117,8 @@ public class NceMacroGenFrame extends jmri.util.JmriJFrame implements jmri.jmrix
 		m.setElement(0, 0x9C); 		// Macro cmd
 		m.setElement(1, macroNum);	// Macro #
 		m.setBinary(true);
-		m.setReplyLen(1);
+		m.setReplyLen(REPLY_LEN);
 		return m;
-	}
+    }
 }
 
