2cda41e87dc2b9ff00f5e31c2d74f29492cf2735
Bob Jacobsen <jacobsen@mac.com>
2007-06-24 04:46:06 +0000

    remove jmrix tests for now

--- test/jmri/HeadLessTest.java
+++ test/jmri/HeadLessTest.java
@@ -9,7 +9,7 @@ import junit.framework.TestSuite;
 /**
  * Invoke complete set of tests for the jmri package
  * @author	Bob Jacobsen, Copyright (C) 2001, 2002, 2007
- * @version         $Revision: 1.1 $
+ * @version         $Revision: 1.2 $
  */
 public class HeadLessTest extends TestCase {
 
@@ -24,8 +24,6 @@ public class HeadLessTest extends TestCase {
         String[] testCaseName = {HeadLessTest.class.getName()};
         junit.textui.TestRunner.main(testCaseName);
     }
-
-!!
 
     // test suite from all defined tests
     public static Test suite() {
@@ -42,7 +40,6 @@ public class HeadLessTest extends TestCase {
         suite.addTest(jmri.configurexml.ConfigXmlTest.suite());
         suite.addTest(jmri.util.UtilTest.suite());
         suite.addTest(jmri.jmrit.JmritTest.suite());
-        suite.addTest(jmri.jmrix.JmrixTest.suite());  // last due to threading issues?
         return suite;
     }
 
