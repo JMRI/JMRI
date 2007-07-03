2b4ecdeeaae62056ab3bbb20068bbb4bdbada560
Bob Jacobsen <jacobsen@mac.com>
2007-07-03 06:59:30 +0000

    work with JUnitAppender

--- test/apps/tests/Log4JFixture.java
+++ test/apps/tests/Log4JFixture.java
@@ -3,14 +3,19 @@ package apps.tests;
 
 public class Log4JFixture extends java.lang.Object {
 
-  public Log4JFixture(Object obj) {
+  public Log4JFixture() {
     initLogging();
   }
 
-  public void setUp() {
+  static public void setUp() {
+    // always init logging if needed
+    initLogging();
+    //
+    jmri.util.JUnitAppender.start();
   }
 
-  public void tearDown() {
+  static public void tearDown() {
+    jmri.util.JUnitAppender.end();
   }
 
 	static boolean log4jinit = true;
@@ -18,16 +23,16 @@ public class Log4JFixture extends java.lang.Object {
 		if (log4jinit) {
 			log4jinit = false;
     		// initialize log4j - from logging control file (lcf) if you can find it
-     		String logFile = "default.lcf";
+     		String logFile = "tests.lcf";
     		if (new java.io.File(logFile).canRead()) {
     			System.out.println(logFile+" configures logging");
-    			org.apache.log4j.PropertyConfigurator.configure("default.lcf");
-   		} else {
-    			System.out.println(logFile+" not found, using default logging");
-	    		org.apache.log4j.BasicConfigurator.configure();
-	   			// only log warnings and above
-	   			org.apache.log4j.Category.getRoot().setPriority(org.apache.log4j.Priority.INFO);
-     		}
+    			org.apache.log4j.PropertyConfigurator.configure(logFile);
+            } else {
+                System.out.println(logFile+" not found, using default logging");
+                org.apache.log4j.BasicConfigurator.configure();
+                // only log warnings and above
+                org.apache.log4j.Category.getRoot().setPriority(org.apache.log4j.Priority.WARN);
+            }
 		}
 	}
 
