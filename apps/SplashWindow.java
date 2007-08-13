3c52d6f31bbdf8d1ce9b26b02581eb2123c7947a
Dennis Miller <millerdl@telusplanet.net>
2007-08-13 00:18:31 +0000

    Fix transparent frame background on splash screen

--- apps/SplashWindow.java
+++ apps/SplashWindow.java
@@ -17,7 +17,8 @@ import jmri.util.JmriJFrame;
 /**
  * A splash screen for showing during JMRI startup
  * @author	Bob Jacobsen   Copyright 2003
- * @version     $Revision: 1.4 $
+ * @author  Dennis Miller Copyright 2007
+ * @version     $Revision: 1.5 $
  */
 public class SplashWindow extends JmriJFrame {
     Image splashIm;
@@ -39,22 +40,23 @@ public class SplashWindow extends JmriJFrame {
         getContentPane().add(l);
         pack();
 
-        /* Center the window */
+        /* Center the window and pad the frame size slightly to put some space 
+         * between logo and frame border*/
         Dimension screenDim =
              Toolkit.getDefaultToolkit().getScreenSize();
         Rectangle winDim = getBounds();
+        winDim.height = winDim.height + 10;
+        winDim.width = winDim.width + 10;
         setLocation((screenDim.width - winDim.width) / 2,
                 (screenDim.height - winDim.height) / 2);
+        setSize(winDim.width, winDim.height);
 
         // and show
         setVisible(true);
     }
 
-    public void paint(Graphics g) {
-       if (splashIm != null) {
-           g.drawImage(splashIm,0,0,this);
-       }
-    }
+/*  paint method required for Java 1.1.8 removed as it caused a
+ *  transparent frame under Java 1.6 */
 }
 
 
