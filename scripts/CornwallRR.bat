c5e6962aabb4179671dca1c93321d2d8ac64c835
Bob Jacobsen <jacobsen@mac.com>
2007-08-22 16:35:50 +0000

    new regexp jar added to classpath

--- scripts/CornwallRR.bat
+++ scripts/CornwallRR.bat
@@ -1,3 +1,3 @@
-REM Start the CornwallRR Java program ($Revision: 1.11 $)
+REM Start the CornwallRR Java program ($Revision: 1.12 $)
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;vecmath.jar" apps.cornwall.CornwallRR %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar" apps.cornwall.CornwallRR %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/PacketPro.bat
+++ scripts/PacketPro.bat
@@ -6,9 +6,9 @@ REM command line.
 
 REM There must be a local jython interpreter and JMRI libraries available
 
-REM $Revision: 1.8 $ (CVS maintains this line, do not edit please)
+REM $Revision: 1.9 $ (CVS maintains this line, do not edit please)
 
-SET CLASSPATH=.;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;vecmath.jar
+SET CLASSPATH=.;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar
      
 jython jython/PacketPro.py %1 %2 %3 %4 %5 %6 %7 %8 %9
 
