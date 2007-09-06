2b2471965fe37156bf6729372d5ff57a4a057187
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2007-09-06 22:49:26 +0000

    NCE edit macros uses CS memory reads which are not supported by the NCE USB, therefore this fuction is disabled if NCE USB is detected

--- jmri/jmrix/nce/macro/NceMacroEditAction.java
+++ jmri/jmrix/nce/macro/NceMacroEditAction.java
@@ -13,10 +13,18 @@ package jmri.jmrix.nce.macro;
 
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
+import jmri.jmrix.nce.NceEpromChecker;
 
 public class NceMacroEditAction  extends AbstractAction {
 
-	public NceMacroEditAction(String s) { super(s);}
+	public NceMacroEditAction(String s) {
+		super(s);
+
+		// disable if NCE USB detected
+		if (NceEpromChecker.nceUSBdetected) {
+			setEnabled(false);
+		}
+	}
 	
     public void actionPerformed(ActionEvent e) {
 		NceMacroEditFrame f = new NceMacroEditFrame();
