0337fb3cbd9f08edc29918090cca294c7bfbb192
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2007-11-30 20:10:28 +0000

    USB doesn't support any clock commands, so I've disabled it from the NCE tools menu

--- jmri/jmrix/nce/clockmon/ClockMonAction.java
+++ jmri/jmrix/nce/clockmon/ClockMonAction.java
@@ -5,18 +5,27 @@ package jmri.jmrix.nce.clockmon;
 import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
+import jmri.jmrix.nce.NceUSB;
+
 
 /**
  * Create and register a NceClockMonitorFrame object.
  *
  * @author			Ken Cameron    Copyright (C) 2007
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  *
  * based on LocoNet.ClockMonAction by Bob Jacobsen Copyright (C) 2003
  */
 public class ClockMonAction extends AbstractAction {
 
-    public ClockMonAction(String s) { super(s);}
+	public ClockMonAction(String s) {
+		super(s);
+		
+		// disable if NCE USB detected
+		if (NceUSB.getUsbSystem() != NceUSB.USB_SYSTEM_NONE) {
+			setEnabled(false);
+		}
+	}
     
     public ClockMonAction() {
         this("NCE clock monitor");
