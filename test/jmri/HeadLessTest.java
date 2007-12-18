2d398a377cb97e416bcdaafee2a45ba6e0c2d205
Bob Jacobsen <jacobsen@mac.com>
2007-12-18 04:49:34 +0000

    now runs full tests

--- test/jmri/HeadLessTest.java
+++ test/jmri/HeadLessTest.java
@@ -23,7 +23,7 @@ import junit.framework.TestSuite;
  * for more details.
  * <P>
  * @author	Bob Jacobsen, Copyright (C) 2001, 2002, 2007
- * @version         $Revision: 1.6 $
+ * @version         $Revision: 1.7 $
  */
 public class HeadLessTest extends TestCase {
 
@@ -43,33 +43,8 @@ public class HeadLessTest extends TestCase {
     public static Test suite() {
         apps.tests.AllTest.initLogging();
         TestSuite suite = new TestSuite("jmri.JmriTest");  // no tests in this class itself
-		suite.addTest(jmri.BlockTest.suite());
-		suite.addTest(jmri.RouteTest.suite());
-		suite.addTest(jmri.BlockManagerTest.suite());
-		suite.addTest(jmri.BeanSettingTest.suite());
-		suite.addTest(jmri.PathTest.suite());
-        suite.addTest(jmri.DccLocoAddressTest.suite());
-        suite.addTest(jmri.progdebugger.ProgDebuggerTest.suite());
-        suite.addTest(jmri.NmraPacketTest.suite());
-        suite.addTest(jmri.configurexml.ConfigXmlTest.suite());
-        suite.addTest(jmri.util.UtilTest.suite());
-        
-        // suite.addTest(jmri.jmrix.JmrixTest.suite());
-        // The single line above expands to all of these, 
-        // cut & paste in here to allow adding one at a time.
-		suite.addTest(jmri.jmrix.AbstractProgrammerTest.suite());
-		suite.addTest(jmri.jmrix.cmri.serial.SerialTest.suite());
-		suite.addTest(jmri.jmrix.direct.DirectTest.suite());
-		//suite.addTest(jmri.jmrix.easydcc.EasyDccTest.suite());
-		suite.addTest(jmri.jmrix.lenz.XNetTest.suite());
-		suite.addTest(jmri.jmrix.loconet.LocoNetTest.suite());
-		//suite.addTest(jmri.jmrix.nce.NceTest.suite());
-		//suite.addTest(jmri.jmrix.pricom.PricomTest.suite());
-		//suite.addTest(jmri.jmrix.tmcc.SerialTest.suite());
-		//suite.addTest(jmri.jmrix.oaktree.SerialTest.suite());
-		//suite.addTest(jmri.jmrix.qsi.QsiTest.suite());
-
-        suite.addTest(jmri.jmrit.JmritTest.suite());
+        // all tests from other classes
+        suite.addTest(jmri.JmriTest.suite());
 
         return suite;
     }
