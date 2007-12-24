3b0a8022beeb02dc25fce27362c77aa0941a2cdb
Bob Jacobsen <jacobsen@mac.com>
2007-12-24 05:44:26 +0000

    fix indent

--- jmri/jmrit/XmlFileLocationAction.java
+++ jmri/jmrit/XmlFileLocationAction.java
@@ -12,30 +12,30 @@ import javax.swing.*;
  * Swing action to display the JMRI directory locations
  *
  * @author	    Bob Jacobsen    Copyright (C) 2004, 2007
- * @version         $Revision: 1.2 $
+ * @version         $Revision: 1.3 $
  */
 public class XmlFileLocationAction extends AbstractAction {
-
+    
     public XmlFileLocationAction() { super();}
-
+    
     public void actionPerformed(ActionEvent ev) {
-
-		JFrame frame = new jmri.util.JmriJFrame();  // to ensure fits
-		
+        
+        JFrame frame = new jmri.util.JmriJFrame();  // to ensure fits
+        
         javax.swing.JTextArea pane = new javax.swing.JTextArea();
         pane.setEditable(false);
         
         JScrollPane  scroll = new JScrollPane(pane);
         frame.getContentPane().add(scroll);
-
+        
         String prefs = jmri.jmrit.XmlFile.prefsDir();
         pane.append("Preferences directory: "+prefs+"\n");
         
         String prog = System.getProperty("user.dir");
         pane.append("Program directory: "+prog+"\n");
-        		
-		frame.pack();
-		frame.setVisible(true);
+        
+        frame.pack();
+        frame.setVisible(true);
     }
 }
 
