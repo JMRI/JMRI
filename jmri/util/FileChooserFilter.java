ccee57656dc9c8d431ff60d3fec5b4309877d990
Bob Jacobsen <jacobsen@mac.com>
2007-12-26 01:12:32 +0000

    match is regardless of case

--- jmri/util/FileChooserFilter.java
+++ jmri/util/FileChooserFilter.java
@@ -13,7 +13,7 @@ import java.util.*;
  * Except in that case, files without extensions fail.
  *
  * @author Alex Shepherd
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 
 public class FileChooserFilter extends javax.swing.filechooser.FileFilter {
@@ -27,7 +27,7 @@ public class FileChooserFilter extends javax.swing.filechooser.FileFilter {
   }
 
   public void addExtension( String ext ){
-    allowedExtensions.add( ext ) ;
+    allowedExtensions.add( ext.toLowerCase() ) ;
   }
 
   private String getFileExtension(File f) {
