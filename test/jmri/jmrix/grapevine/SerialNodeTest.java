18baa4367fa6005afdc919ff7896565771e22117
Bob Jacobsen <jacobsen@mac.com>
2008-01-11 18:14:05 +0000

    new adressing

--- test/jmri/jmrix/grapevine/SerialNodeTest.java
+++ test/jmri/jmrix/grapevine/SerialNodeTest.java
@@ -12,7 +12,7 @@ import junit.framework.TestSuite;
  * JUnit tests for the SerialNode class
  * @author		Bob Jacobsen  Copyright 2003, 2007, 2008
  * @author		Dave Duchamp  multi-node extensions 2003
- * @version		$Revision: 1.3 $
+ * @version		$Revision: 1.4 $
  */
 public class SerialNodeTest extends TestCase {
 		
@@ -71,14 +71,15 @@ public class SerialNodeTest extends TestCase {
         // new serial format
         
         b = new SerialNode(98,SerialNode.NODE2002V6);
-        SerialSensor s1 = new SerialSensor("GS98101","s1");
-        SerialSensor s2 = new SerialSensor("GS98102","s2");
-        SerialSensor s3 = new SerialSensor("GS98103","s3");
-        SerialSensor s4 = new SerialSensor("GS98104","s4");
-        b.registerSensor(s1, 101);
-        b.registerSensor(s2, 102);
-        b.registerSensor(s3, 103);
-        b.registerSensor(s4, 104);
+        
+        SerialSensor s1 = new SerialSensor("GS98001","s1");
+        SerialSensor s2 = new SerialSensor("GS98002","s2");
+        SerialSensor s3 = new SerialSensor("GS98003","s3");
+        SerialSensor s4 = new SerialSensor("GS98004","s4");
+        b.registerSensor(s1, 1);
+        b.registerSensor(s2, 2);
+        b.registerSensor(s3, 3);
+        b.registerSensor(s4, 4);
 
         SerialReply r = new SerialReply();
         r.setElement(0, 128+98);
@@ -104,19 +105,19 @@ public class SerialNodeTest extends TestCase {
         
         b = new SerialNode(1,SerialNode.NODE2002V6);
 
-        SerialSensor s1 = new SerialSensor("GS1001","s1");
-        SerialSensor s2 = new SerialSensor("GS1002","s2");
-        SerialSensor s3 = new SerialSensor("GS1003","s3");
-        b.registerSensor(s1, 1);
-        b.registerSensor(s2, 2);
-        b.registerSensor(s3, 3);
+        SerialSensor s1 = new SerialSensor("GS1041","s1");
+        SerialSensor s2 = new SerialSensor("GS1042","s2");
+        SerialSensor s3 = new SerialSensor("GS1043","s3");
+        b.registerSensor(s1, 41);
+        b.registerSensor(s2, 42);
+        b.registerSensor(s3, 43);
         Assert.assertTrue("check sensors active", b.sensorsActive());
         Assert.assertEquals("check s1", Sensor.UNKNOWN, s1.getKnownState());
         Assert.assertEquals("check s2", Sensor.UNKNOWN, s2.getKnownState());
         Assert.assertEquals("check s3", Sensor.UNKNOWN, s3.getKnownState());
 
         SerialReply r = new SerialReply();
-        r.setElement(0, 0x81); // sensor 1 (from 0) active, GS1102
+        r.setElement(0, 0x81); // sensor 1 (from 0) active, GS1042
         r.setElement(1, 0x03);
         r.setElement(2, 0x81);
         r.setElement(3, 0x40);
@@ -125,7 +126,7 @@ public class SerialNodeTest extends TestCase {
         Assert.assertEquals("check s2", Sensor.ACTIVE, s2.getKnownState());
         Assert.assertEquals("check s3", Sensor.UNKNOWN, s3.getKnownState());
 
-        r.setElement(0, 0x81); // sensor 1 (from 0) inactive, GS1102
+        r.setElement(0, 0x81); // sensor 1 (from 0) inactive, GS1042
         r.setElement(1, 0x02);
         r.setElement(2, 0x81);
         r.setElement(3, 0x40);
@@ -134,7 +135,7 @@ public class SerialNodeTest extends TestCase {
         Assert.assertEquals("check s2", Sensor.INACTIVE, s2.getKnownState());
         Assert.assertEquals("check s3", Sensor.UNKNOWN, s3.getKnownState());
 
-        r.setElement(0, 0x81); // sensor 0 (from 0) active, GS1101
+        r.setElement(0, 0x81); // sensor 0 (from 0) active, GS1041
         r.setElement(1, 0x01);
         r.setElement(2, 0x81);
         r.setElement(3, 0x40);
@@ -147,6 +148,80 @@ public class SerialNodeTest extends TestCase {
     public void testMarkChangesOldSerial1() {
         // old serial format
         
+        b = new SerialNode(1,SerialNode.NODE2002V6);
+
+        SerialSensor s1 = new SerialSensor("GS1021","s1");
+        SerialSensor s2 = new SerialSensor("GS1022","s2");
+        SerialSensor s3 = new SerialSensor("GS1023","s3");
+        SerialSensor s4 = new SerialSensor("GS1024","s4");
+        SerialSensor s5 = new SerialSensor("GS1025","s5");
+        SerialSensor s6 = new SerialSensor("GS1026","s6");
+        SerialSensor s7 = new SerialSensor("GS1027","s7");
+        SerialSensor s8 = new SerialSensor("GS1028","s8");
+        b.registerSensor(s1, 21);
+        b.registerSensor(s2, 22);
+        b.registerSensor(s3, 23);
+        b.registerSensor(s4, 24);
+        b.registerSensor(s5, 25);
+        b.registerSensor(s6, 26);
+        b.registerSensor(s7, 27);
+        b.registerSensor(s8, 28);
+        Assert.assertTrue("check sensors active", b.sensorsActive());
+        Assert.assertEquals("1 check s1", Sensor.UNKNOWN, s1.getKnownState());
+        Assert.assertEquals("1 check s2", Sensor.UNKNOWN, s2.getKnownState());
+        Assert.assertEquals("1 check s3", Sensor.UNKNOWN, s3.getKnownState());
+        Assert.assertEquals("1 check s4", Sensor.UNKNOWN, s4.getKnownState());
+        Assert.assertEquals("1 check s5", Sensor.UNKNOWN, s5.getKnownState());
+        Assert.assertEquals("1 check s6", Sensor.UNKNOWN, s6.getKnownState());
+        Assert.assertEquals("1 check s7", Sensor.UNKNOWN, s7.getKnownState());
+        Assert.assertEquals("1 check s8", Sensor.UNKNOWN, s8.getKnownState());
+
+        SerialReply r = new SerialReply();
+        r.setElement(0, 0x81); // sensor 1-4 (from 0) inactive, GS1001-GS1004
+        r.setElement(1, 0x2F);
+        r.setElement(2, 0x81);
+        r.setElement(3, 0x50);
+        b.markChanges(r);
+        Assert.assertEquals("2 check s1", Sensor.INACTIVE, s1.getKnownState());
+        Assert.assertEquals("2 check s2", Sensor.INACTIVE, s2.getKnownState());
+        Assert.assertEquals("2 check s3", Sensor.INACTIVE, s3.getKnownState());
+        Assert.assertEquals("2 check s4", Sensor.INACTIVE, s4.getKnownState());
+        Assert.assertEquals("2 check s5", Sensor.UNKNOWN, s5.getKnownState());
+        Assert.assertEquals("2 check s6", Sensor.UNKNOWN, s6.getKnownState());
+        Assert.assertEquals("2 check s7", Sensor.UNKNOWN, s7.getKnownState());
+        Assert.assertEquals("2 check s8", Sensor.UNKNOWN, s8.getKnownState());
+
+        r.setElement(0, 0x81); // sensor 1-4 (from 0) inactive
+        r.setElement(1, 0x20);
+        r.setElement(2, 0x81);
+        r.setElement(3, 0x50);
+        b.markChanges(r);
+        Assert.assertEquals("3 check s1", Sensor.ACTIVE, s1.getKnownState());
+        Assert.assertEquals("3 check s2", Sensor.ACTIVE, s2.getKnownState());
+        Assert.assertEquals("3 check s3", Sensor.ACTIVE, s3.getKnownState());
+        Assert.assertEquals("3 check s4", Sensor.ACTIVE, s4.getKnownState());
+        Assert.assertEquals("3 check s5", Sensor.UNKNOWN, s5.getKnownState());
+        Assert.assertEquals("3 check s6", Sensor.UNKNOWN, s6.getKnownState());
+        Assert.assertEquals("3 check s7", Sensor.UNKNOWN, s7.getKnownState());
+        Assert.assertEquals("3 check s8", Sensor.UNKNOWN, s8.getKnownState());
+
+        r.setElement(0, 0x81); // sensor 5-8 (from 0) mixed
+        r.setElement(1, 0x35);
+        r.setElement(2, 0x81);
+        r.setElement(3, 0x50);
+        b.markChanges(r);
+        Assert.assertEquals("4 check s1", Sensor.ACTIVE, s1.getKnownState());
+        Assert.assertEquals("4 check s2", Sensor.ACTIVE, s2.getKnownState());
+        Assert.assertEquals("4 check s3", Sensor.ACTIVE, s3.getKnownState());
+        Assert.assertEquals("4 check s4", Sensor.ACTIVE, s4.getKnownState());
+        Assert.assertEquals("4 check s5", Sensor.INACTIVE, s5.getKnownState());
+        Assert.assertEquals("4 check s6", Sensor.ACTIVE, s6.getKnownState());
+        Assert.assertEquals("4 check s7", Sensor.INACTIVE, s7.getKnownState());
+        Assert.assertEquals("4 check s8", Sensor.ACTIVE, s8.getKnownState());
+    }
+
+    public void testMarkChangesParallel() {
+
         b = new SerialNode(1,SerialNode.NODE2002V6);
 
         SerialSensor s1 = new SerialSensor("GS1001","s1");
@@ -177,7 +252,7 @@ public class SerialNodeTest extends TestCase {
 
         SerialReply r = new SerialReply();
         r.setElement(0, 0x81); // sensor 1-4 (from 0) inactive, GS1001-GS1004
-        r.setElement(1, 0x6F);
+        r.setElement(1, 0x0F);
         r.setElement(2, 0x81);
         r.setElement(3, 0x50);
         b.markChanges(r);
@@ -191,7 +266,7 @@ public class SerialNodeTest extends TestCase {
         Assert.assertEquals("2 check s8", Sensor.UNKNOWN, s8.getKnownState());
 
         r.setElement(0, 0x81); // sensor 1-4 (from 0) inactive
-        r.setElement(1, 0x60);
+        r.setElement(1, 0x00);
         r.setElement(2, 0x81);
         r.setElement(3, 0x50);
         b.markChanges(r);
@@ -205,81 +280,7 @@ public class SerialNodeTest extends TestCase {
         Assert.assertEquals("3 check s8", Sensor.UNKNOWN, s8.getKnownState());
 
         r.setElement(0, 0x81); // sensor 5-8 (from 0) mixed
-        r.setElement(1, 0x75);
-        r.setElement(2, 0x81);
-        r.setElement(3, 0x50);
-        b.markChanges(r);
-        Assert.assertEquals("4 check s1", Sensor.ACTIVE, s1.getKnownState());
-        Assert.assertEquals("4 check s2", Sensor.ACTIVE, s2.getKnownState());
-        Assert.assertEquals("4 check s3", Sensor.ACTIVE, s3.getKnownState());
-        Assert.assertEquals("4 check s4", Sensor.ACTIVE, s4.getKnownState());
-        Assert.assertEquals("4 check s5", Sensor.INACTIVE, s5.getKnownState());
-        Assert.assertEquals("4 check s6", Sensor.ACTIVE, s6.getKnownState());
-        Assert.assertEquals("4 check s7", Sensor.INACTIVE, s7.getKnownState());
-        Assert.assertEquals("4 check s8", Sensor.ACTIVE, s8.getKnownState());
-    }
-
-    public void testMarkChangesParallel() {
-
-        b = new SerialNode(1,SerialNode.NODE2002V6);
-
-        SerialSensor s1 = new SerialSensor("GS1101","s1");
-        SerialSensor s2 = new SerialSensor("GS1102","s2");
-        SerialSensor s3 = new SerialSensor("GS1103","s3");
-        SerialSensor s4 = new SerialSensor("GS1104","s4");
-        SerialSensor s5 = new SerialSensor("GS1105","s5");
-        SerialSensor s6 = new SerialSensor("GS1106","s6");
-        SerialSensor s7 = new SerialSensor("GS1107","s7");
-        SerialSensor s8 = new SerialSensor("GS1108","s8");
-        b.registerSensor(s1, 101);
-        b.registerSensor(s2, 102);
-        b.registerSensor(s3, 103);
-        b.registerSensor(s4, 104);
-        b.registerSensor(s5, 105);
-        b.registerSensor(s6, 106);
-        b.registerSensor(s7, 107);
-        b.registerSensor(s8, 108);
-        Assert.assertTrue("check sensors active", b.sensorsActive());
-        Assert.assertEquals("1 check s1", Sensor.UNKNOWN, s1.getKnownState());
-        Assert.assertEquals("1 check s2", Sensor.UNKNOWN, s2.getKnownState());
-        Assert.assertEquals("1 check s3", Sensor.UNKNOWN, s3.getKnownState());
-        Assert.assertEquals("1 check s4", Sensor.UNKNOWN, s4.getKnownState());
-        Assert.assertEquals("1 check s5", Sensor.UNKNOWN, s5.getKnownState());
-        Assert.assertEquals("1 check s6", Sensor.UNKNOWN, s6.getKnownState());
-        Assert.assertEquals("1 check s7", Sensor.UNKNOWN, s7.getKnownState());
-        Assert.assertEquals("1 check s8", Sensor.UNKNOWN, s8.getKnownState());
-
-        SerialReply r = new SerialReply();
-        r.setElement(0, 0x81); // sensor 1-4 (from 0) inactive, GS1001-GS1004
-        r.setElement(1, 0x4F);
-        r.setElement(2, 0x81);
-        r.setElement(3, 0x50);
-        b.markChanges(r);
-        Assert.assertEquals("2 check s1", Sensor.INACTIVE, s1.getKnownState());
-        Assert.assertEquals("2 check s2", Sensor.INACTIVE, s2.getKnownState());
-        Assert.assertEquals("2 check s3", Sensor.INACTIVE, s3.getKnownState());
-        Assert.assertEquals("2 check s4", Sensor.INACTIVE, s4.getKnownState());
-        Assert.assertEquals("2 check s5", Sensor.UNKNOWN, s5.getKnownState());
-        Assert.assertEquals("2 check s6", Sensor.UNKNOWN, s6.getKnownState());
-        Assert.assertEquals("2 check s7", Sensor.UNKNOWN, s7.getKnownState());
-        Assert.assertEquals("2 check s8", Sensor.UNKNOWN, s8.getKnownState());
-
-        r.setElement(0, 0x81); // sensor 1-4 (from 0) inactive
-        r.setElement(1, 0x40);
-        r.setElement(2, 0x81);
-        r.setElement(3, 0x50);
-        b.markChanges(r);
-        Assert.assertEquals("3 check s1", Sensor.ACTIVE, s1.getKnownState());
-        Assert.assertEquals("3 check s2", Sensor.ACTIVE, s2.getKnownState());
-        Assert.assertEquals("3 check s3", Sensor.ACTIVE, s3.getKnownState());
-        Assert.assertEquals("3 check s4", Sensor.ACTIVE, s4.getKnownState());
-        Assert.assertEquals("3 check s5", Sensor.UNKNOWN, s5.getKnownState());
-        Assert.assertEquals("3 check s6", Sensor.UNKNOWN, s6.getKnownState());
-        Assert.assertEquals("3 check s7", Sensor.UNKNOWN, s7.getKnownState());
-        Assert.assertEquals("3 check s8", Sensor.UNKNOWN, s8.getKnownState());
-
-        r.setElement(0, 0x81); // sensor 5-8 (from 0) mixed
-        r.setElement(1, 0x55);
+        r.setElement(1, 0x15);
         r.setElement(2, 0x81);
         r.setElement(3, 0x50);
         b.markChanges(r);
@@ -311,7 +312,17 @@ public class SerialNodeTest extends TestCase {
     }
 
     // The minimal setup for log4J
-    protected void setUp() { apps.tests.Log4JFixture.setUp(); }
+    protected void setUp() { 
+        apps.tests.Log4JFixture.setUp();
+        // replace the SensorManager
+        jmri.InstanceManager i = new jmri.InstanceManager(){
+            protected void init() {
+                super.init();
+                root = null;
+            }
+        };
+        jmri.InstanceManager.setSensorManager(new jmri.jmrix.grapevine.SerialSensorManager());
+    }
     protected void tearDown() { apps.tests.Log4JFixture.tearDown(); }
     
 }
