119b0a88c1d6222758981499e57626f88cf20b7e
Paul Bender <paul.bender@acm.org>
2008-02-03 21:37:24 +0000

    Add default constructor so XNetMonAction can be called from the preferences (for either a button or action)

--- jmri/jmrix/lenz/mon/XNetMonAction.java
+++ jmri/jmrix/lenz/mon/XNetMonAction.java
@@ -5,7 +5,8 @@
  *       			XNetMonFrame object
  *
  * @author			Bob Jacobsen    Copyright (C) 2002
- * @version         $Revision: 2.2 $
+ * @author			Paul Bender     Copyright (C) 2008
+ * @version         $Revision: 2.3 $
  */
 
 package jmri.jmrix.lenz.mon;
@@ -14,9 +15,13 @@ import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 
 
-public class XNetMonAction 			extends AbstractAction {
+public class XNetMonAction extends AbstractAction {
 
-	public XNetMonAction(String s) { super(s);}
+    public XNetMonAction(String s) { super(s);}
+
+    public XNetMonAction(){
+	this("XPressNet Monitor");
+    }
 
     public void actionPerformed(ActionEvent e) {
 		// create a XNetMonFrame
