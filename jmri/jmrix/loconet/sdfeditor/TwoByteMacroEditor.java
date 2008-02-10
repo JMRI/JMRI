b429782d4a2583d85d14f30a98ee437ac4876dea
Bob Jacobsen <jacobsen@mac.com>
2008-02-10 07:25:03 +0000

    improve comments

--- jmri/jmrix/loconet/sdfeditor/TwoByteMacroEditor.java
+++ jmri/jmrix/loconet/sdfeditor/TwoByteMacroEditor.java
@@ -6,9 +6,14 @@ import jmri.jmrix.loconet.sdf.SdfMacro;
 
 /**
  * Editor panel for the generic two-byte macros from the Digitrax sound definition language
+ * <p>
+ * In theory, this should never be invoked, because all the macros
+ * have specific editors.  But editors like this never keep up with
+ * changes to content of underlying data, so we provide this as an
+ * escape claus for unrecognized content.
  *
- * @author		Bob Jacobsen  Copyright (C) 2007
- * @version             $Revision: 1.2 $
+ * @author		Bob Jacobsen  Copyright (C) 2007, 2008
+ * @version             $Revision: 1.3 $
  */
 
 class TwoByteMacroEditor extends SdfMacroEditor {
