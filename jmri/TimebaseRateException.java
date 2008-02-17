1432128eee474b748463f1c7bf12eaa1baabcc77
Bob Jacobsen <jacobsen@mac.com>
2008-02-17 16:40:56 +0000

    minor javadoc fix

--- jmri/TimebaseRateException.java
+++ jmri/TimebaseRateException.java
@@ -2,19 +2,17 @@
 
 package jmri;
 
-
 /**
- * Thrown to indicate that a Timebase cant handle a particular
- * rate setting thats been requested.
+ * Thrown to indicate that a Timebase can't handle a particular
+ * rate setting that's been requested.
  *
  * @author			Bob Jacobsen Copyright (C) 2004
- * @version			$Revision: 1.1 $
+ * @version			$Revision: 1.2 $
  */
 public class TimebaseRateException extends JmriException {
 	public TimebaseRateException(String s) { super(s); }
 	public TimebaseRateException() {}
 
 }
-
 
 /* @(#)TimebaseRateException.java */
