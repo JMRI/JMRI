b75eae56918a3a4a9821ee3c39958adc675b01e7
Paul Bender <paul.bender@acm.org>
2008-03-01 17:21:26 +0000

    Move "dummy" functions for dimmable to the Abstract class; override in concrete class if it supports dimming.

--- jmri/jmrix/secsi/SerialLight.java
+++ jmri/jmrix/secsi/SerialLight.java
@@ -15,7 +15,7 @@ import jmri.Turnout;
  *
  * @author      Dave Duchamp Copyright (C) 2004
  * @author      Bob Jacobsen Copyright (C) 2006, 2007, 2008
- * @version     $Revision: 1.2 $
+ * @version     $Revision: 1.3 $
  */
 public class SerialLight extends AbstractLight {
 
@@ -98,29 +98,6 @@ public class SerialLight extends AbstractLight {
             // notify listeners, if any
             firePropertyChange("KnownState", new Integer(oldState), new Integer(newState));
 		}
-    }
-
-    // added dummy methods for dimmable stuff
-    public double getDimRequest() {
-    	if (mState == OFF) {
-    		return(0);
-    	} else {
-    		return(1);
-        }
-    }
-    public double getDimCurrent() {
-    	if (mState == OFF) {
-    		return(0);
-    	} else {
-    		return(1);
-        }
-    }
-    public void setDimRequest(double v) {
-    	if (v > 0) {
-    		setState(ON);
-    	} else {
-    		setState(OFF);
-    	}
     }
     
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(SerialLight.class.getName());
