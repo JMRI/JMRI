da90d9a6f47ec534593a55c9592540d72c52d0ee
Bob Jacobsen <jacobsen@mac.com>
2008-03-23 05:25:47 +0000

    add java.library.path setting

--- scripts/PanelPro.bat
+++ scripts/PanelPro.bat
@@ -1,3 +1,3 @@
-REM Start the PanelPro Java program ($Revision: 1.12 $)
+REM Start the PanelPro Java program ($Revision: 1.13 $)
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar" apps.PanelPro.PanelPro %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.:lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar" apps.PanelPro.PanelPro %1 %2 %3 %4 %5 %6 %7 %8 %9
