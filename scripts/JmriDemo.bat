3b4be083bcc4767aac41c6b943e4f02027e4594d
Bob Jacobsen <jacobsen@mac.com>
2008-03-30 01:16:13 +0000

    add jars

--- scripts/JmriDemo.bat
+++ scripts/JmriDemo.bat
@@ -1,3 +1,3 @@
-REM Start the JmriDemo Java program ($Revision: 1.15 $)
+REM Start the JmriDemo Java program ($Revision: 1.16 $)
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.:lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar" apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/LocoTools.bat
+++ scripts/LocoTools.bat
@@ -1,3 +1,3 @@
-REM Start the LocoTools Java program ($Revision: 1.13 $)
+REM Start the LocoTools Java program ($Revision: 1.14 $)
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.:lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar" apps.LocoTools.LocoTools %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.LocoTools.LocoTools %1 %2 %3 %4 %5 %6 %7 %8 %9
