c1b6daab68286726bf35d4ba93acda67fdbee9df
Bob Jacobsen <jacobsen@mac.com>
2008-04-04 15:58:45 +0000

    refactor to add common interface and impl to AbstractMRMessage and AbstractMRReply

---
+++ jmri/jmrix/AbstractMessage.java
@@ -0,0 +1,40 @@
+// AbstractMessage.java
+
+package jmri.jmrix;
+
+import jmri.util.StringUtil;
+
+/**
+ * Add description of class here.
+ *
+ * @author jake  Copyright _YEAR_
+ * @version   $Revision: 1.1 $
+ */
+public abstract class AbstractMessage implements Message {
+    
+    /** Creates a new instance of AbstractMessage */
+    public AbstractMessage() {
+    }
+
+    public int getElement(int n) {        return _dataChars[n];}
+
+
+    // accessors to the bulk data
+    public int getNumDataElements() {        return _nDataChars;}
+
+    public void setElement(int n, int v) {         _dataChars[n] = v;  }
+
+
+    // display format
+
+    protected int[] _dataChars = null;
+
+
+    // display format
+
+    // contents (private)
+    protected int _nDataChars = 0;
+
+    private static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(AbstractMessage.class.getName());
+
+}
