e71eabf54888a25b6bf82a256447bcb0c813507c
Bob Jacobsen <jacobsen@mac.com>
2008-04-26 16:07:58 +0000

    minor refactoring of ctors, needs to be moved lower eventually

--- jmri/jmrix/AbstractMessage.java
+++ jmri/jmrix/AbstractMessage.java
@@ -7,13 +7,34 @@ import jmri.util.StringUtil;
 /**
  * Add description of class here.
  *
- * @author Bob Jacobsen  Copyright 2007
- * @version   $Revision: 1.2 $
+ * @author Bob Jacobsen  Copyright 2007, 2008
+ * @version   $Revision: 1.3 $
  */
 public abstract class AbstractMessage implements Message {
     
     /** Creates a new instance of AbstractMessage */
     public AbstractMessage() {
+    }
+
+    public AbstractMessage(int n) {
+        if (n<1)
+            log.error("invalid length in call to ctor");
+        _nDataChars = n;
+        _dataChars = new int[n];
+    }
+
+    public AbstractMessage(String s) {
+        this(s.length());
+        for (int i = 0; i<_nDataChars; i++)
+            _dataChars[i] = s.charAt(i);
+    }
+
+    public AbstractMessage(AbstractMessage m) {
+        if (m == null)
+            log.error("copy ctor of null message throws exception");
+        _nDataChars = m._nDataChars;
+        _dataChars = new int[_nDataChars];
+        for (int i = 0; i<_nDataChars; i++) _dataChars[i] = m._dataChars[i];
     }
 
     public int getElement(int n) {        return _dataChars[n];}
