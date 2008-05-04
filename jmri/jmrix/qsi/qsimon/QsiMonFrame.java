0c7854ece717166862d13573266f4c64491c0a23
Bob Jacobsen <jacobsen@mac.com>
2008-05-04 01:56:30 +0000

    improved parsing

--- jmri/jmrix/qsi/qsimon/QsiMonFrame.java
+++ jmri/jmrix/qsi/qsimon/QsiMonFrame.java
@@ -10,7 +10,7 @@ import jmri.jmrix.qsi.QsiTrafficController;
 /**
  * Frame displaying (and logging) QSI command messages
  * @author			Bob Jacobsen   Copyright (C) 2007, 2008
- * @version			$Revision: 1.2 $
+ * @version			$Revision: 1.3 $
  */
 public class QsiMonFrame extends jmri.jmrix.AbstractMonFrame implements QsiListener {
 
@@ -31,11 +31,50 @@ public class QsiMonFrame extends jmri.jmrix.AbstractMonFrame implements QsiListe
 
 	public synchronized void message(QsiMessage l) {  // receive a message and log it
 	    int opcode = l.getElement(0);
-		nextLine("cmd: \""+opcode+"\"\n", l.toString());
+	    String text = null;
+	    if (l.getNumDataElements() == 1) {
+	        // special case - assume this is a reply ack
+	        text = "Ack of message for function "+l.getElement(0);
+	    } else {
+            switch (opcode) {
+            case 9:
+                text = "OP_REQ_READ_CV with CV="+l.getElement(3);
+                break;
+            case 30:
+                text = "OP_REQ_WRITE_CV with CV="+l.getElement(3)+" data="+l.getElement(4);
+                break;
+            case 17:
+                text = "OP_REQ_CLEAR_ERROR_STATUS";
+                break;
+            default:
+                text = "Unrecognized message with code "+opcode+": "+l.toString();
+                break;
+            }
+		}
+		nextLine("M: "+text+"\n", l.toString());
 	}
 	public synchronized void reply(QsiReply l) {  // receive a reply message and log it
-	    int opcode = l.getElement(0);
-		nextLine("rep: \""+opcode+"\"\n", l.toString());
+	    String text;
+	    
+	    if (l.getElement(0)=='A') {
+	        text = "A: Ack of "+l.getElement(1)+" with status "+l.getElement(2);
+	    } else if (l.getElement(0)=='S') {
+	        switch (l.getElement(1)&0xFF) {
+	        case 10:
+	            text = "S: OP_RPL_READ_CV status "+l.getElement(4)+" value "+l.getElement(5);
+	            break;
+	        case 31:
+	            text = "S: OP_RPL_WRITE_CV status "+l.getElement(4);
+	            break;
+            default:
+                text = "S: Response type "+l.getElement(1)+" with length "+(l.getElement(2)+256*l.getElement(3));
+                break;
+	        }
+	        
+	    } else {
+	        text = "U: Unrecognized reply: "+l.toString();
+	    }
+		nextLine(text+"\n", l.toString());
 	}
 
    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(QsiMonFrame.class.getName());
