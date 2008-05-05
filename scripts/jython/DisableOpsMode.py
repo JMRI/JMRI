eae5bd72d3d51fac4d796def765afcf3e5e2636e
Bob Jacobsen <jacobsen@mac.com>
2008-05-05 22:27:43 +0000

    wait before touching button

--- scripts/jython/DisableOpsMode.py
+++ scripts/jython/DisableOpsMode.py
@@ -8,30 +8,41 @@
 # Part of the JMRI distribution
 #
 # The next line is maintained by CVS, please don't change it
-# $Revision: 1.3 $
+# $Revision: 1.4 $
 
 import apps
 
-# navigate through the window structure
-mainWindow = jmri.util.JmriJFrame.getFrameList().get(0)
-if (mainWindow.getTitle() != "DecoderPro") :
-    mainWindow = jmri.util.JmriJFrame.getFrameList().get(1)
-if (mainWindow.getTitle() != "DecoderPro") :
-    mainWindow = jmri.util.JmriJFrame.getFrameList().get(2)
-#print mainWindow.getTitle()
+# because the 1st window doesn't show up for a while, 
+# we have to run this later
 
-contentPane = mainWindow.getContentPane()
+class DisableOpsMode(jmri.jmrit.automat.AbstractAutomaton) :
+  def handle(self):
+    # wait for long enough for window
+    self.waitMsec(10*1000)
+    # navigate through the window structure
+    mainWindow = jmri.util.JmriJFrame.getFrameList().get(0)
+    if (mainWindow.getTitle() != "DecoderPro") :
+        mainWindow = jmri.util.JmriJFrame.getFrameList().get(1)
+    if (mainWindow.getTitle() != "DecoderPro") :
+        mainWindow = jmri.util.JmriJFrame.getFrameList().get(2)
+    #print mainWindow.getTitle()
+    
+    contentPane = mainWindow.getContentPane()
+    
+    decoderProPane = contentPane.getComponents()[0]
+    
+    statusPane = decoderProPane.getComponents()[0]
+    
+    opsButton = statusPane.getComponents()[2]
+    
+    # now disable it and say why
+    opsButton.setEnabled(False)
+    opsButton.setToolTipText("We have disabled this button for the club layout")
+    
+    # alternately, uncomment the following to have the button not appear at all
+    #
+    # opsButton.setVisible(False)
+    return 0
 
-decoderProPane = contentPane.getComponents()[0]
-
-statusPane = decoderProPane.getComponents()[0]
-
-opsButton = statusPane.getComponents()[2]
-
-# now disable it and say why
-opsButton.setEnabled(False)
-opsButton.setToolTipText("We have disabled this button for the club layout")
-
-# alternately, uncomment the following to have the button not appear at all
-#
-# opsButton.setVisible(False)
+# now make it go
+DisableOpsMode().start()
