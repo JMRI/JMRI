9565dad24d212f8765af75b48d715feb7a513fe3
Bob Jacobsen <jacobsen@mac.com>
2008-05-17 16:38:04 +0000

    initial implementation, just builds now

---
+++ jmri/jmrix/rps/swing/controlpanel/DisplayAction.java
@@ -0,0 +1,41 @@
+// DisplayAction.java
+
+package jmri.jmrix.rps.swing.controlpanel;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+/**
+ * Swing action to create and register a
+ *       			DisplayFrame object
+ *
+ * @author			Bob Jacobsen    Copyright (C) 2008
+ * @version         $Revision: 1.1 $
+ */
+public class DisplayAction 			extends AbstractAction {
+
+	public DisplayAction(String s) { super(s);}
+
+    public DisplayAction() {
+        this("RPS Calculations Display");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        log.debug("starting frame creation");
+		DisplayFrame f = new DisplayFrame();
+		try {
+			f.initComponents();
+			}
+		catch (Exception ex) {
+			log.warn("starting frame: Exception: "+ex.toString());
+			}
+		f.setVisible(true);
+
+	}
+
+	static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(DisplayAction.class.getName());
+
+}
+
+
+/* @(#)DisplayAction.java */
