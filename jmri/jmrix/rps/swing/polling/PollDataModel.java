bc075893e4a2d747c6ba53a6594907f01643c769
Bob Jacobsen <jacobsen@mac.com>
2008-05-27 04:55:13 +0000

    display last position

--- jmri/jmrix/rps/swing/polling/PollDataModel.java
+++ jmri/jmrix/rps/swing/polling/PollDataModel.java
@@ -3,6 +3,7 @@
 package jmri.jmrix.rps.swing.polling;
 
 import jmri.jmrix.rps.Engine;
+import jmri.jmrix.rps.Measurement;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -19,7 +20,7 @@ import jmri.util.table.ButtonRenderer;
  * Pane for user management of RPS alignment.
  
  * @author	Bob Jacobsen   Copyright (C) 2008
- * @version	$Revision: 1.2 $
+ * @version	$Revision: 1.3 $
  */
 public class PollDataModel extends AbstractTableModel {
 
@@ -29,10 +30,14 @@ public class PollDataModel extends AbstractTableModel {
     static final int ADDRCOL = 1;
     static final int LONGCOL = 2;
     static final int POLLCOL = 3;
-    static final int TYPECOL = 4;
-    
-    static final int LAST = 4;
+    static final int LASTXCOL = 4;
+    static final int LASTYCOL = 5;
+    static final int LASTZCOL = 6;
+
+    static final int LAST = 6;
     jmri.ModifiedFlag modifiedFlag;
+    
+    static final int TYPECOL = -1;
     
     public PollDataModel(jmri.ModifiedFlag flag) {
         super();
@@ -57,6 +62,12 @@ public class PollDataModel extends AbstractTableModel {
             return rb.getString("TitlePollCol");
         case TYPECOL:
             return rb.getString("TitleTypeCol");
+        case LASTXCOL:
+            return rb.getString("TitleXCol");
+        case LASTYCOL:
+            return rb.getString("TitleYCol");
+        case LASTZCOL:
+            return rb.getString("TitleZCol");
         default:
             return "";
         }
@@ -69,6 +80,8 @@ public class PollDataModel extends AbstractTableModel {
             return Integer.class;
         else if (c == TYPECOL)
             return JComboBox.class;
+        else if (c == LASTXCOL || c == LASTYCOL || c == LASTZCOL )
+            return Double.class;
         else 
             return String.class;
     }
@@ -82,6 +95,8 @@ public class PollDataModel extends AbstractTableModel {
 
     public Object getValueAt (int r,int c) {
         // r is row number, from 0
+        Measurement m;
+        double val;
         switch (c) {
         case IDCOL:
             return Engine.instance().getTransmitter(r).getID();
@@ -94,6 +109,21 @@ public class PollDataModel extends AbstractTableModel {
         case TYPECOL:
             JComboBox b = new JComboBox(new String[]{"F2", "F3", "BSCI"});
             return b;
+        case LASTXCOL:
+            m = Engine.instance().getTransmitter(r).getLastMeasurement();
+            if (m == null) return null;
+            val = m.getX();
+            return new Double(val);
+        case LASTYCOL:
+            m = Engine.instance().getTransmitter(r).getLastMeasurement();
+            if (m == null) return null;
+            val = m.getY();
+            return new Double(val);
+        case LASTZCOL:
+            m = Engine.instance().getTransmitter(r).getLastMeasurement();
+            if (m == null) return null;
+            val = m.getZ();
+            return new Double(val);
         default:
             return null;
         }
