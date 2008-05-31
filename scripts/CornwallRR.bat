6391cfa1ea410834562c39dc6d752c21a74a8937
Bob Jacobsen <jacobsen@mac.com>
2008-05-31 03:26:10 +0000

    version number

--- scripts/CornwallRR.bat
+++ scripts/CornwallRR.bat
@@ -1,3 +1,3 @@
-REM Start the CornwallRR Java program ($Revision: 1.14 $)
+REM Start the CornwallRR program from JMRI 2.1.6 ($Revision: 1.15 $)
 
 java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.cornwall.CornwallRR %1 %2 %3 %4 %5 %6 %7 %8 %9
