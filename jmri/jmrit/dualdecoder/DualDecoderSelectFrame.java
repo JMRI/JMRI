3e55544967eba3158aa9cd4651975b9c8648912b
Bob Jacobsen <jacobsen@mac.com>
2008-06-03 06:31:58 +0000

    Title update. Add help

--- jmri/jmrit/dualdecoder/DualDecoderSelectFrame.java
+++ jmri/jmrit/dualdecoder/DualDecoderSelectFrame.java
@@ -7,13 +7,21 @@ package jmri.jmrit.dualdecoder;
  * Frame for user dual-decoder select too.
  * This allows the user (person) to select an active decoder from multiple ones in a loco
  * @author   Bob Jacobsen   Copyright (C) 2003
- * @version  $Revision: 1.4 $
+ * @version  $Revision: 1.5 $
  */
 public class DualDecoderSelectFrame extends jmri.util.JmriJFrame {
 
     public DualDecoderSelectFrame() {
-        super();
+        this("Multi-Decoder Control");
+    }
+    
+    public DualDecoderSelectFrame(String title) {
+        super(title);
         getContentPane().add(new DualDecoderSelectPane());
+        
+        // add help menu to window
+    	addHelpMenu("package.jmri.jmrit.dualdecoder.DualDecoderSelectFrame", true);
+
         pack();
     }
 }
