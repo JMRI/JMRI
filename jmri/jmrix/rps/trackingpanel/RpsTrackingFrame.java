d222bad91e2ad3871c1ce770aff2ac5d0b4b6f8e
Bob Jacobsen <jacobsen@mac.com>
2008-06-08 02:42:09 +0000

    add clear.  Adjust layout

--- jmri/jmrix/rps/trackingpanel/RpsTrackingFrame.java
+++ jmri/jmrix/rps/trackingpanel/RpsTrackingFrame.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionListener;
  * Frame containing the entire display tool
  *
  * @author			Bob Jacobsen    Copyright (C) 2006, 2008
- * @version         $Revision: 1.1 $
+ * @version         $Revision: 1.2 $
  */
 public class RpsTrackingFrame extends jmri.util.JmriJFrame {
     
@@ -27,16 +27,29 @@ public class RpsTrackingFrame extends jmri.util.JmriJFrame {
         
         getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
         
-        // add controls; first, button for handling errors
+        // add controls; first, clear button
         JPanel controls = new JPanel();
         controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
+        JPanel line = new JPanel();
+        line.setLayout(new FlowLayout());
+        
+        clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent event) {
+                    clearButtonChanged();
+                }
+            });
+        line.add(clearButton);
+        
+        // button for handling errors
         showButton = new JCheckBox("Show error points");
         showButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent event) {
                     showButtonChanged();
                 }
             });
-        controls.add(showButton);
+        line.add(showButton);
+        controls.add(line);
         
         // then alignment control
         panel = new RpsTrackingPanel();
@@ -57,11 +70,15 @@ public class RpsTrackingFrame extends jmri.util.JmriJFrame {
         pack();        
     }
     
+    JButton clearButton;
     JCheckBox showButton;
     RpsTrackingPanel panel;
     
     void showButtonChanged() {
         panel.setShowErrors(showButton.isSelected());
+    }
+    void clearButtonChanged() {
+        panel.clear();
     }
 }
 
