47fc5d419e35f53a01091beafdbef5e59b9b0447
Bob Jacobsen <jacobsen@mac.com>
2008-06-22 00:17:12 +0000

    basically works

--- scripts/jython/CanExample.py
+++ scripts/jython/CanExample.py
@@ -5,15 +5,18 @@
 # Part of the JMRI distribution
 #
 # The next line is maintained by CVS, please don't change it
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 import java
 
 # First, example of receiving. Put a listener in place.
 class MyCanListener (jmri.jmrix.can.CanListener) :
     def message(self, msg) :
+        # this handles messages being sent by ignoring them
+        return
+    def reply(self, msg) :
         print "received Frame"
-        print "ID:", msg.getId()
+        print "ID: 0x"+java.lang.Integer.toHexString(msg.getId())
         print "content: ", msg.toString()
         return
     
@@ -21,9 +24,9 @@ jmri.jmrix.can.TrafficController.instance().addCanListener(MyCanListener())
 
 
 # Send a frame
-frame = jmri.jmrix.can.CanMessage()
+frame = jmri.jmrix.can.CanMessage(2)  # will load 2 bytes
 frame.setId(0x123)
 frame.setElement(0, 0x45)
 frame.setElement(1, 0x67)
-jmri.jmrix.can.TrafficController.instance().sendCanMessage(packet)
+jmri.jmrix.can.TrafficController.instance().sendCanMessage(frame, None)
 
