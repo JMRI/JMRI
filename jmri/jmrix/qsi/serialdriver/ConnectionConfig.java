574071143fbd540c1baba438c26cd3b315fd7cbb
Bob Jacobsen <jacobsen@mac.com>
2008-06-28 02:21:58 +0000

    name change

--- jmri/jmrix/qsi/serialdriver/ConnectionConfig.java
+++ jmri/jmrix/qsi/serialdriver/ConnectionConfig.java
@@ -8,7 +8,7 @@ package jmri.jmrix.qsi.serialdriver;
  * via an QSI SerialDriverAdapter object.
  *
  * @author      Bob Jacobsen   Copyright (C) 2007
- * @version	$Revision: 1.2 $
+ * @version	$Revision: 1.3 $
  */
 public class ConnectionConfig  extends jmri.jmrix.AbstractConnectionConfig {
 
@@ -26,7 +26,7 @@ public class ConnectionConfig  extends jmri.jmrix.AbstractConnectionConfig {
         super();
     }
 
-    public String name() { return "QSI Programmer"; }
+    public String name() { return "Quantum Programmer"; }
 
     protected void setInstance() { adapter = SerialDriverAdapter.instance(); }
 }
