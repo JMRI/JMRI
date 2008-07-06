49565c250a0c77189ffabbdc3cc9c690217faf34
Ken Cameron <kcameron@twcny.rr.com>
2008-07-06 01:27:21 +0000

    Attempt at making a special startup that has testing and pauses so folks (Windows) can see if they have Java and JMRI installed right. Basically one big copy from the starts for DecoderPro. Nothing special in the JMRI part yet.

---
+++ scripts/InstallTest.bat
@@ -0,0 +1,29 @@
+@REM Start the InstallTest program from JMRI 2.1.6 ($Revision: 1.1 $)
+@REM @author Ken Cameron
+@echo testing for Java working
+@echo . 
+java -version
+
+@IF NOT ERRORLEVEL 1 GOTO javaOk
+@echo .
+@echo Some problem finding/running JAVA.
+
+@echo You must install JAVA first or fix your JAVA install.
+@echo .
+@pause
+@goto skipJMRI
+:javaOk
+@echo .
+@echo Java is correctly working.
+@pause
+@echo Now testing JMRI
+
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.InstallTest.InstallTest %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+@IF NOT ERRORLEVEL 1 GOTO skipJMRI
+
+@echo .
+@echo Something is wrong with invoking JMRI. Check JMRI installation.
+@pause
+
+:skipJMRI
