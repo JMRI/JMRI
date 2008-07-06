f63b1ce427d16a94196fd97ff8e5056fbaac5350
Bob Jacobsen <jacobsen@mac.com>
2008-07-06 21:32:55 +0000

    start 2.1.7

--- scripts/InstallTest.bat
+++ scripts/InstallTest.bat
@@ -1,4 +1,4 @@
-@REM Start the InstallTest program from JMRI 2.1.6 ($Revision: 1.1 $)
+@REM Start the InstallTest program from JMRI 2.1.7 ($Revision: 1.1 $)
 @REM @author Ken Cameron
 @echo testing for Java working
 @echo . 
@@ -18,7 +18,7 @@ java -version
 @pause
 @echo Now testing JMRI
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.InstallTest.InstallTest %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.InstallTest.InstallTest %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 @IF NOT ERRORLEVEL 1 GOTO skipJMRI
 
--- scripts/PacketPro.bat
+++ scripts/PacketPro.bat
@@ -1,12 +1,12 @@
 
 REM ##############################################################################
 
-REM Invoke the the PacketPro JMRI 2.1.6 application, which provide a Jython
+REM Invoke the the PacketPro JMRI 2.1.7 application, which provide a Jython
 REM command line.
 
 REM There must be a local jython interpreter and JMRI libraries available
 
-REM $Revision: 1.10 $ (CVS maintains this line, do not edit please)
+REM $Revision: 1.11 $ (CVS maintains this line, do not edit please)
 
 SET CLASSPATH=.;classes;jmriplugins.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;MRJAdapter.jar;jakarta-regexp-1.5.jar;vecmath.jar
      
