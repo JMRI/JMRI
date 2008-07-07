7d134114361b57e67079d7214f3c6c59b7a50057
Andrew Crosland <andrewcrosland@talktalk.net>
2008-07-07 18:17:54 +0000

    Missing file added

---
+++ jmri/jmrix/can/cbus/CbusConstants.java
@@ -0,0 +1,25 @@
+// CbusConstants.java
+
+package jmri.jmrix.can.cbus;
+
+/**
+ * CbusConstants.java
+ *
+ * Description:		Constants to represent CBUS protocol
+ *
+ * @author		Andrew Crosland   Copyright (C) 2008
+ * @version $Revision: 1.1 $
+ */
+public final class CbusConstants {
+    
+    /**
+     * CBUS Opcodes
+     */
+    public static final int CBUS_OP_EV_ON = 0x90;
+    public static final int CBUS_OP_EV_OFF = 0x91;
+    public static final int CBUS_OP_EV_ON_DATA = 0xB0;
+    public static final int CBUS_OP_EV_OFF_DATA = 0xB1;
+
+}
+
+/* @(#)CbusConstants.java */
