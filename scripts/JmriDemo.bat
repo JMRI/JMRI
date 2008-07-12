8c40d4f8f62d998d9aea95c990c1c6a7b60ef767
Bob Jacobsen <jacobsen@mac.com>
2008-07-12 13:28:14 +0000

    update version number for 2.1.8

--- scripts/JmriDemo.bat
+++ scripts/JmriDemo.bat
@@ -1,3 +1,3 @@
-REM Start the JmriDemo program from JMRI 2.1.7 ($Revision: 1.18 $)
+REM Start the JmriDemo program from JMRI 2.1.8 ($Revision: 1.19 $)
 
 java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
