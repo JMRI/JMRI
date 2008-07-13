a5b35a966a42e8e4b0ae0a1e39fedfcb9ee44ae9
Bob Jacobsen <jacobsen@mac.com>
2008-07-13 22:10:20 +0000

    2.2

--- scripts/InstallTest.bat
+++ scripts/InstallTest.bat
@@ -1,4 +1,4 @@
-@REM Start the InstallTest program from JMRI 2.1.8 ($Revision: 1.1 $)
+@REM Start the InstallTest program from JMRI 2.2 ($Revision: 1.1 $)
 @REM @author Ken Cameron
 @echo testing for Java working
 @echo . 
--- scripts/PanelPro.bat
+++ scripts/PanelPro.bat
@@ -1,3 +1,3 @@
-REM Start the PanelPro program from JMRI 2.1.8 ($Revision: 1.17 $)
+REM Start the PanelPro program from JMRI 2.2 ($Revision: 1.18 $)
 
 java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.PanelPro.PanelPro %1 %2 %3 %4 %5 %6 %7 %8 %9
