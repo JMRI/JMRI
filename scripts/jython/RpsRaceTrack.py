377168942d70e887a83201aabb1a10690465637c
Bob Jacobsen <jacobsen@mac.com>
2008-07-20 16:59:27 +0000

    used at show

--- scripts/jython/RpsRaceTrack.py
+++ scripts/jython/RpsRaceTrack.py
@@ -1,10 +1,22 @@
 # RaceTrack using RPS position measurement
 #
+# loco1 is faster, loco2 is slower.
+# When loco1 comes within dmin=24 inches
+# of loco2, it will:
+#   slow to 1/4 of it's speed
+#   wait 6 seconds
+#   return to it's original speed
+# and check again
+#
+# This is time-based (check every 200 msec), rather
+# than measurement based (listeners) for ease of debugging.
+# Changing it over later on will be good.
+#
 # Author:  Bob Jacobsen (C) Copyright 2008
 # Part of the JMRI distribution
 #
 # The next line is maintained by CVS, please don't change it
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 import jarray
 import jmri
@@ -12,8 +24,8 @@ import jmri
 class RpsRaceTrack(jmri.jmrit.automat.AbstractAutomaton) :
 
     def init(self):
-        loco1 = 15
-        loco2 = 21
+        loco1 = 42
+        loco2 = 15
         # set up transmitter and throttle numbers
         self.rps = jmri.jmrix.rps.Engine.instance()
         self.t1 = self.rps.getTransmitterByAddress(loco1)
@@ -32,32 +44,31 @@ class RpsRaceTrack(jmri.jmrit.automat.AbstractAutomaton) :
     def handle(self):
     
         # tuning
-        dmin = 12
-        dmax = 24
+        dmin = 24
         
         # each turn of the loop, get positions
         
         self.p1 = self.t1.getLastMeasurement().getPoint()
-        print "t1 position "+self.p1
+        #print "t1 position ",self.p1
 
         self.p2 = self.t2.getLastMeasurement().getPoint()
-        print "t2 position "+self.p2
+        #print "t2 position ",self.p2
         
-        self.d = p1.distance(p2)
-        print "distance "+self.d
+        self.d = self.p1.distance(self.p2)
+        #print "distance",self.d
         
-        if (d<dmin) :
+        if (self.d<dmin) :
             # slow down
             spd = self.throttle.getSpeedSetting()
-            nspd = spd*0.5
-            print "set speed from "+nspd+" to "+spd
+            nspd = spd*0.25
+            print "set speed from",nspd," to ",spd
             self.throttle.setSpeedSetting(nspd)
-            self.waitMsec(500)
+            self.waitMsec(6000)
             self.throttle.setSpeedSetting(spd)
             return 1
         # OK, so let run
-        # wait 1 second for layout to catch up
-        self.waitMsec(1000)                 
+        # wait for layout to catch up
+        self.waitMsec(200)                 
 
         # and continue around again
         print "End of Loop"
