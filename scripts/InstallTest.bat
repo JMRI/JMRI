ae9171387cac04fa727316d26c0535c36ab1c2d7
Bob Jacobsen <jacobsen@mac.com>
2008-08-03 06:59:13 +0000

    add servlet

--- scripts/InstallTest.bat
+++ scripts/InstallTest.bat
@@ -18,7 +18,7 @@ java -version
 @pause
 @echo Now testing JMRI
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;vecmath.jar" apps.InstallTest.InstallTest %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;servlet.jar;vecmath.jar" apps.InstallTest.InstallTest %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 @IF NOT ERRORLEVEL 1 GOTO skipJMRI
 
