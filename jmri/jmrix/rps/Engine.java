e45d053e4a04937e2104f742e45140209e7e39bf
Bob Jacobsen <jacobsen@mac.com>
2008-08-24 02:17:56 +0000

    refactor transmitter to separate class

--- jmri/jmrix/rps/Engine.java
+++ jmri/jmrix/rps/Engine.java
@@ -29,7 +29,7 @@ import java.io.*;
  *</ul>
  *
  * @author	   Bob Jacobsen   Copyright (C) 2006, 2008
- * @version   $Revision: 1.18 $
+ * @version   $Revision: 1.19 $
  */
 
 
@@ -136,16 +136,23 @@ public class Engine implements ReadingListener {
         
         int count = 0;
         for (int i = 0; i<receivers.length; i++) {
-            if (receivers[i]!=null) count++;
+            if (receivers[i]!=null) 
+                count++;
+            else
+                log.error("receivers out of synch at index="+i);
         }
                 
         int index = 0;
         Point3d list[] = new Point3d[count];
+        Point3d zero = new Point3d(0.,0.,0.);
         for (int i = 0; i<receivers.length; i++) {
             if (receivers[i]==null) continue;  // skip receivers not present
             Point3d p = getReceiverPosition(i);
             if ( p != null ) {
-                list[index] = p;
+                if (receivers[i].isActive())
+                    list[index] = p;
+                else
+                    list[index] = zero;
                 index++;
             } else {
                 log.error("Unexpected null position for receiver "+i);
@@ -471,53 +478,6 @@ public class Engine implements ReadingListener {
                 if (jmri.InstanceManager.commandStationInstance() != null)
                     jmri.InstanceManager.commandStationInstance().sendPacket(packet, 1);
             }
-        }
-    }
-    
-    public class Transmitter implements jmri.ThrottleListener {
-        Transmitter(String id, boolean polled, int address, boolean longAddress) {
-            setID(id);
-            setPolled(polled);
-            setAddress(address);
-            setLongAddress(longAddress);
-        }
-        
-        public String getID() { return id; }
-        String id;
-        public void setID(String id) { this.id = id; }
-        
-        public boolean isLongAddress() { return longAddress; }
-        boolean longAddress;
-        public void setLongAddress(boolean longAddress) { this.longAddress = longAddress; }
-        
-        public int getAddress() { return address; }
-        int address;
-        public void setAddress(int address) { this.address = address; }
-        
-        public boolean isPolled() { return polled; }
-        boolean polled;
-        public void setPolled(boolean polled) { this.polled = polled; }
-        
-        Measurement lastMeasurement = null;
-        public void setLastMeasurement(Measurement last) { lastMeasurement = last; }
-        public Measurement getLastMeasurement() { return lastMeasurement; }
-    
-        // stuff to do F2 poll
-        DccThrottle throttle;
-        boolean needReqThrottle = true;
-        
-        DccThrottle getThrottle() { return throttle; }
-        boolean checkInit() {
-            if (throttle != null) return true;
-            if (!needReqThrottle) return false;
-            // request throttle
-            InstanceManager.throttleManagerInstance().requestThrottle(address,longAddress, this);
-            return false;
-        }
-
-        public void notifyThrottleFound(DccThrottle t) {
-            needReqThrottle = false;
-            throttle = t;
         }
     }
 
