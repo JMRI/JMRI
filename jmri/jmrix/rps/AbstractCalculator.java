347b939372b0e05633b6ffa33a1cf2fe8448bc65
Bob Jacobsen <jacobsen@mac.com>
2008-08-26 19:46:43 +0000

    new base implementaton

---
+++ jmri/jmrix/rps/AbstractCalculator.java
@@ -0,0 +1,60 @@
+// AbstractCalculator.java
+
+package jmri.jmrix.rps;
+
+import javax.vecmath.Point3d;
+
+/**
+ * Some helpful implementations and values for Calculators
+ *
+ * @author	Bob Jacobsen  Copyright (C) 2006
+ * @version	$Revision: 1.1 $
+ */
+public abstract class AbstractCalculator implements Calculator {
+
+    // Sensor position objects
+    Point3d sensors[];
+
+    // values for usual calculators
+    double [] Tr;
+    double [] Xr;
+    double [] Yr;
+    double [] Zr;
+    int nr;
+    
+    public void prep(Reading r) {
+        
+        if (log.isDebugEnabled()) {
+            log.debug("Reading: "+r.toString());
+            log.debug("Sensors: "+sensors.length);
+            if (sensors.length>=1) log.debug("Sensor[0]: "+sensors[0].x+","+sensors[0].y+","+sensors[0].z);
+        }
+
+        nr = r.getNSample();
+        if (nr != sensors.length) log.error("Mismatch: "+nr+" readings, "+sensors.length+" receivers");
+        nr = Math.min(nr, sensors.length); // accept the shortest
+        
+        Tr = new double[nr];
+        Xr = new double[nr];
+        Yr = new double[nr];
+        Zr = new double[nr];
+        
+        // generate vector
+        int j = 0;
+        for (int i = 0; i<nr; i++) {
+            if (sensors[i]!=null) {
+                Tr[j] = r.getValue(i);
+                Xr[j] = sensors[i].x;
+                Yr[j] = sensors[i].y;
+                Zr[j] = sensors[i].z;
+                j++;
+            }
+        }
+        nr = j;
+    }       
+
+    static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(Ash2_2Algorithm.class.getName());
+
+}
+
+/* @(#)AbstractCalculator.java */
