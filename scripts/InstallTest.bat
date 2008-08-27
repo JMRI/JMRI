9eec81bf49760852e00030480b72f238afed4007
Bob Jacobsen <jacobsen@mac.com>
2008-08-27 07:21:43 +0000

    2.3.2 prep

--- scripts/InstallTest.bat
+++ scripts/InstallTest.bat
@@ -1,4 +1,4 @@
-@REM Start the InstallTest program from JMRI 2.3.1 ($Revision: 1.1 $)
+@REM Start the InstallTest program from JMRI 2.3.2 ($Revision: 1.1 $)
 @REM @author Ken Cameron
 @echo testing for Java working
 @echo . 
