97e93f2653490fcd9af35a633b0121793792038b
Bob Jacobsen <jacobsen@mac.com>
2008-09-09 04:02:02 +0000

    better debug

--- jmri/jmrix/rps/AbstractCalculator.java
+++ jmri/jmrix/rps/AbstractCalculator.java
@@ -8,7 +8,7 @@ import javax.vecmath.Point3d;
  * Some helpful implementations and values for Calculators
  *
  * @author	Bob Jacobsen  Copyright (C) 2006
- * @version	$Revision: 1.1 $
+ * @version	$Revision: 1.2 $
  */
 public abstract class AbstractCalculator implements Calculator {
 
@@ -27,7 +27,8 @@ public abstract class AbstractCalculator implements Calculator {
         if (log.isDebugEnabled()) {
             log.debug("Reading: "+r.toString());
             log.debug("Sensors: "+sensors.length);
-            if (sensors.length>=1) log.debug("Sensor[0]: "+sensors[0].x+","+sensors[0].y+","+sensors[0].z);
+            if (sensors.length>=1 && sensors[0]!=null) log.debug("Sensor[0]: "+sensors[0].x+","+sensors[0].y+","+sensors[0].z);
+            if (sensors.length>=2 && sensors[1]!=null) log.debug("Sensor[1]: "+sensors[1].x+","+sensors[1].y+","+sensors[1].z);
         }
 
         nr = r.getNSample();
@@ -51,6 +52,7 @@ public abstract class AbstractCalculator implements Calculator {
             }
         }
         nr = j;
+        log.debug("nr is "+nr);
     }       
 
     static org.apache.log4j.Category log = org.apache.log4j.Category.getInstance(Ash2_2Algorithm.class.getName());
