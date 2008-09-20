446cbea126c0ec99b9ffa4763678123ad0ac6eeb
Bob Jacobsen <jacobsen@mac.com>
2008-09-20 22:37:47 +0000

    Xmx200 Xms30

--- scripts/JmriDemo.bat
+++ scripts/JmriDemo.bat
@@ -1,3 +1,3 @@
-REM Start the JmriDemo program from JMRI 2.3.2 ($Revision: 1.23 $)
+REM Start the JmriDemo program from JMRI 2.3.2 ($Revision: 1.24 $)
 
-java -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;servlet.jar;vecmath.jar" apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -Xms30m -Xmx200m -noverify -Dsun.java2d.d3d=false -Djava.security.policy=security.policy -Djava.library.path=.;lib -Djava.rmi.server.codebase=file:java/classes/ -Dsun.java2d.noddraw -Djava.class.path=".;classes;jmriplugins.jar;jmriweb.jar;jmriserver.jar;jmri.jar;comm.jar;Serialio.jar;log4j.jar;jhall.jar;crimson.jar;jdom.jar;jython.jar;javacsv.jar;jinput.jar;ch.ntb.usb.jar;MRJAdapter.jar;servlet.jar;vecmath.jar" apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
