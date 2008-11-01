222b42e8da3579d6912b655b31c10253bb662fb3
Bob Jacobsen <jacobsen@mac.com>
2008-11-01 19:40:29 +0000

    2.3.5

--- scripts/DecoderPro.bat
+++ scripts/DecoderPro.bat
@@ -1,3 +1,3 @@
-REM Start the DecoderPro program from JMRI 2.3.4 ($Revision: 1.28 $)
+REM Start the DecoderPro program from JMRI 2.3.5 ($Revision: 1.29 $)
 
 LaunchJMRI.exe apps.DecoderPro.DecoderPro %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/LocoTools.bat
+++ scripts/LocoTools.bat
@@ -1,3 +1,3 @@
-REM Start the LocoTools program from JMRI 2.3.4 ($Revision: 1.24 $)
+REM Start the LocoTools program from JMRI 2.3.5 ($Revision: 1.25 $)
 
 LaunchJMRI.exe apps.LocoTools.LocoTools %1 %2 %3 %4 %5 %6 %7 %8 %9
