899e87d62ee0b84ec77b3a6d452b95623fdf9932
Bob Jacobsen <jacobsen@mac.com>
2008-11-30 09:24:08 +0000

    tests for can frames

--- test/jmri/jmrix/can/CanReplyTest.java
+++ test/jmri/jmrix/can/CanReplyTest.java
@@ -1,4 +1,4 @@
-// CanTest.java
+// CanReplyTest.java
 
 package jmri.jmrix.can;
 
@@ -8,34 +8,43 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Tests for the jmri.jmrix.can package.
+ * Tests for the jmri.jmrix.can.CanReply class
+ *
  * @author      Bob Jacobsen  Copyright 2008
- * @version   $Revision: 1.2 $
+ * @version   $Revision: 1.1 $
  */
-public class CanTest extends TestCase {
+public class CanReplyTest extends TestCase {
 
     public void testDefinitions() {
+    }
+
+    public void testStdExt() {
+        CanReply m = new CanReply();
+        Assert.assertTrue("std at start", !m.isExtended());
+        m.setExtended(true);
+        Assert.assertTrue("extended", m.isExtended());
+        m.setExtended(false);
+        Assert.assertTrue("std at end", !m.isExtended());
+        
     }
     
     // from here down is testing infrastructure
 
-    public CanTest(String s) {
+    public CanReplyTest(String s) {
         super(s);
     }
 
     // Main entry point
     static public void main(String[] args) {
         apps.tests.AllTest.initLogging();
-        String[] testCaseName = {"-noloading", CanTest.class.getName()};
+        String[] testCaseName = {"-noloading", CanReplyTest.class.getName()};
         junit.swingui.TestRunner.main(testCaseName);
     }
 
     // test suite from all defined tests
     public static Test suite() {
-        TestSuite suite = new TestSuite("jmri.jmrix.can.CanTest");
-        suite.addTest(jmri.jmrix.can.CanMessageTest.suite());
-        suite.addTest(jmri.jmrix.can.CanReplyTest.suite());
-        suite.addTest(jmri.jmrix.can.cbus.CbusTest.suite());
+        apps.tests.AllTest.initLogging();
+        TestSuite suite = new TestSuite(CanReplyTest.class);
         return suite;
     }
 
