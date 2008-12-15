c97124b41ce136829a4f2f3ce00ede1518dd3009
Bob Jacobsen <jacobsen@mac.com>
2008-12-15 01:53:02 +0000

    fix false fail when RPS not init on test machine

--- test/jmri/jmrix/rps/swing/debugger/DebuggerTest.java
+++ test/jmri/jmrix/rps/swing/debugger/DebuggerTest.java
@@ -14,18 +14,16 @@ import javax.vecmath.Point3d;
 /**
  * Tests for the jmri.jmrix.rps.swing.debugger package
  * @author      Bob Jacobsen  Copyright 2008
- * @version   $Revision: 1.3 $
+ * @version   $Revision: 1.4 $
  */
 public class DebuggerTest extends TestCase {
 
     public void testCtor() throws Exception {
 
         // create a context
-        Engine.instance().setMaxReceiverNumber(4);
+        Engine.instance().setMaxReceiverNumber(2);
         Engine.instance().setReceiver(1,new Receiver(new Point3d(1,2,3)));
         Engine.instance().setReceiver(2,new Receiver(new Point3d(1,2,3)));
-        Engine.instance().setReceiver(3,new Receiver(new Point3d(1,2,3)));
-        Engine.instance().setReceiver(4,new Receiver(new Point3d(1,2,3)));
         
         Reading r = new Reading("21", new double[]{11,12,13,14});
         Measurement m = new Measurement(r, -0.5, 0.5, 0.0, 0.133, 3, "source");
