9c7a542decdb7e247e27d042c0ae3d9c0c2bd070
Bob Jacobsen <jacobsen@mac.com>
2008-12-28 07:36:27 +0000

    new, not hardware tested

---
+++ jmri/jmrix/can/cbus/CbusProgrammerManager.java
@@ -0,0 +1,39 @@
+/* CbusProgrammerManager.java */
+
+package jmri.jmrix.can.cbus;
+
+import jmri.DefaultProgrammerManager;
+import jmri.Programmer;
+
+/**
+ * Extend DefaultProgrammerManager to provide node variable programmer for CBUS
+ *
+ * @see     jmri.ProgrammerManager
+ * @author	Bob Jacobsen Copyright (C) 2008
+ * @version	$Revision: 1.1 $
+ */
+public class CbusProgrammerManager  extends DefaultProgrammerManager {
+
+    public CbusProgrammerManager() {
+        super(null);  // no service mode programmer available
+    }
+
+    /**
+     * Global not relevant for CBUS
+     * @return false
+     */
+    public boolean isGlobalModePossible() {return false;}
+
+    public boolean isAddressedModePossible() {return true;}
+
+    public Programmer getAddressedProgrammer(boolean pLongAddress, int pAddress) {
+        return new CbusProgrammer(pAddress);
+    }
+
+    public Programmer reserveAddressedProgrammer(boolean pLongAddress, int pAddress) {
+        return null;
+    };
+}
+
+
+/* @(#)CbusProgrammerManager.java */
