7fa4416d3c0843f2285766ab80ae2e6e8c53913d
Ken Cameron <kcameron@twcny.rr.com>
2009-01-20 01:28:33 +0000

    Opps. Missing one more case of cm11 that was still in the file.

--- jmri/jmrix/powerline/cp290/configurexml/SpecificLightManagerXml.java
+++ jmri/jmrix/powerline/cp290/configurexml/SpecificLightManagerXml.java
@@ -9,13 +9,13 @@ package jmri.jmrix.powerline.cp290.configurexml;
  * <p>
  * This extends {@link jmri.jmrix.powerline.configurexml.SerialLightManagerXml}
  * without changing any of its behavior.  This lets the configuration system
- * locate a mirror class for {@link jmri.jmrix.powerline.cm11.SpecificLightManager},
+ * locate a mirror class for {@link jmri.jmrix.powerline.cp290.SpecificLightManager},
  * but the actual loading and storing will be done by {@link jmri.jmrix.powerline.configurexml.SerialLightManagerXml}
  * in terms of which-ever manager object is currently installed.
  *
  * @author Bob Jacobsen 2008
  * @author Ken Cameron 2009
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class SpecificLightManagerXml extends jmri.jmrix.powerline.configurexml.SerialLightManagerXml {
 
