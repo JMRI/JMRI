9b6451f490fe15e4d2c390228337e8cf21c542a9
Bob Jacobsen <jacobsen@mac.com>
2009-03-26 21:59:39 +0000

    2.5.2

--- scripts/LocoTools.bat
+++ scripts/LocoTools.bat
@@ -1,3 +1,3 @@
-REM Start the LocoTools program from JMRI 2.5.1 ($Revision: 1.30 $)
+REM Start the LocoTools program from JMRI 2.5.2 ($Revision: 1.31 $)
 
 LaunchJMRI.exe apps.LocoTools.LocoTools %1 %2 %3 %4 %5 %6 %7 %8 %9
