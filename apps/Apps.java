137c1b76c1486b87fa7edf34d8cc46d470a1f0a0
Bob Jacobsen <jacobsen@mac.com>
2009-03-26 17:01:21 +0000

    protect against errors preventing quit

--- apps/Apps.java
+++ apps/Apps.java
@@ -38,7 +38,7 @@ import net.roydesign.mac.MRJAdapter;
  * @author	Bob Jacobsen   Copyright 2003, 2007, 2008
  * @author  Dennis Miller  Copyright 2005
  * @author Giorgio Terdina Copyright 2008
- * @version     $Revision: 1.76 $
+ * @version     $Revision: 1.77 $
  */
 public class Apps extends JPanel implements PropertyChangeListener, java.awt.event.WindowListener {
 
@@ -633,7 +633,11 @@ public class Apps extends JPanel implements PropertyChangeListener, java.awt.eve
      */
     static public void handleQuit() {
         log.debug("Start handleQuit");
-        InstanceManager.shutDownManagerInstance().shutdown();
+        try {
+            InstanceManager.shutDownManagerInstance().shutdown();
+        } catch (Exception e) {
+            log.error("Continuing after error in handleQuit: "+e);
+        }
     }
     
     static protected boolean log4JSetUp = false;
