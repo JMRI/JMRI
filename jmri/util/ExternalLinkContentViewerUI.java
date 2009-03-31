6291eb411d72486f8a593808254e76084365ea06
Bob Jacobsen <jacobsen@mac.com>
2009-03-31 21:57:19 +0000

    file requires Java 1.6, so content is commented out, but has been compiled into a separate plugin jar until we migrate to 1.6

---
+++ jmri/util/ExternalLinkContentViewerUI.java
@@ -0,0 +1,48 @@
+// ExternalLinkContentViewerUI.java
+
+package jmri.util;
+
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.plaf.*;
+import javax.help.*;
+import javax.help.plaf.*;
+import javax.help.plaf.basic.*;
+
+/**
+ * A UI subclass that will open external links (website 
+ * or mail links) in an external browser
+ * <P>
+ * To use:
+ * SwingHelpUtilities.setContentViewerUI("jmri.util.ExternalLinkContentViewerUI");
+ * 
+ @see http://forums.sun.com/thread.jspa?threadID=728061
+ @since JMRI 2.5.3 (or perhaps later, please check CVS)
+ */
+ 
+/* public class ExternalLinkContentViewerUI extends BasicContentViewerUI{ */
+/*     public ExternalLinkContentViewerUI(JHelpContentViewer x){ */
+/* 		super(x); */
+/* 	} */
+/*      */
+/*     public static javax.swing.plaf.ComponentUI createUI(JComponent x){ */
+/*         return new ExternalLinkContentViewerUI((JHelpContentViewer)x); */
+/*     } */
+/*  */
+/*     public void hyperlinkUpdate(HyperlinkEvent he){ */
+/*         if(he.getEventType()==HyperlinkEvent.EventType.ACTIVATED){ */
+/*             try{ */
+/*                 URL u = he.getURL(); */
+/*                 if(u.getProtocol().equalsIgnoreCase("mailto")||u.getProtocol().equalsIgnoreCase("http")||u.getProtocol().equalsIgnoreCase("ftp")){ */
+/*                     java.awt.Desktop.getDesktop().browse(new URI(u.toString())); */
+/*                     return; */
+/*                 } */
+/*             } */
+/*             catch(Throwable t){} */
+/*         } */
+/*         super.hyperlinkUpdate(he); */
+/*     } */
+/* } */
+
