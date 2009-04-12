a475f0506dc2a60f950b6f22e371f9bffeed809c
Pete Cressman <pete_cressman@sbcglobal.net>
2009-04-12 21:37:51 +0000

    Test cases

---
+++ test/jmri/jmrit/beantable/LRouteTableActionTest.java
@@ -0,0 +1,99 @@
+
+// LRouteTableActionTest.java
+
+package jmri.jmrit.beantable;
+
+import junit.framework.*;
+
+import java.util.ResourceBundle;
+
+import jmri.InstanceManager;
+import jmri.managers.InternalSensorManager;
+import jmri.managers.InternalTurnoutManager;
+import jmri.Conditional;
+import jmri.Light;
+import jmri.Route;
+import jmri.Sensor;
+import jmri.SignalHead;
+import jmri.Turnout;
+
+
+/**
+ * Tests for the jmri.jmrit.beantable.LRouteTableAction class
+ * @author	Pete Cressman  Copyright 2009
+ */
+public class LRouteTableActionTest extends TestCase {
+
+	static final ResourceBundle rbx = ResourceBundle
+			.getBundle("jmri.jmrit.beantable.LRouteTableBundle");
+
+    private LRouteTableAction _lRouteTable;
+
+    public void testCreate() {
+        assertNotNull("LRouteTableAction is null!", _lRouteTable);        // test has begun
+        _lRouteTable.addPressed(null);
+        Thread.currentThread().yield();     // allow event thread to draw window 
+        _lRouteTable._userName.setText("TestLRoute");    
+        _lRouteTable._systemName.setText("T");
+        for (int i=0; i<20; i++)
+        {
+            _lRouteTable._inputList.get(3*i).setIncluded(true);
+            _lRouteTable._outputList.get(3*i).setIncluded(true);
+        }
+        _lRouteTable._alignList.get(5).setIncluded(true);
+        _lRouteTable.createPressed(null);
+        Thread.currentThread().yield();     // allow event thread to draw window 
+
+        _lRouteTable.m.setValueAt((Object)rbx.getString("ButtonEdit"), 0, 
+                                  LRouteTableAction.LBeanTableDataModel.EDITCOL);
+    }
+
+    // from here down is testing infrastructure
+
+    public LRouteTableActionTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {LRouteTableActionTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(LRouteTableActionTest.class);
+        return suite;
+    }
+
+    // The minimal setup for log4J
+    protected void setUp() { 
+        apps.tests.Log4JFixture.setUp(); 
+        _lRouteTable = new LRouteTableAction("LRoute");
+        InstanceManager.setSensorManager(new InternalSensorManager());
+        InstanceManager.setTurnoutManager(new InternalTurnoutManager());
+        InstanceManager.setLightManager(new jmri.jmrix.cmri.serial.SerialLightManager());
+        for (int i=1; i<20; i++)
+        {
+            Sensor s = InstanceManager.sensorManagerInstance().newSensor("IS"+i, "Sensor"+i);
+            assertNotNull("Sensor is null!", s);
+            Turnout t = InstanceManager.turnoutManagerInstance().newTurnout("IT"+i, "Turnout"+i);
+            assertNotNull("Turnout is null!", t);
+            Light l = InstanceManager.lightManagerInstance().newLight("CL"+(i), "Light"+i);
+            assertNotNull(i+"th Light is null!", l);
+            Conditional c = InstanceManager.conditionalManagerInstance().createNewConditional(
+                                                        "Conditional"+i, "Conditional"+i);
+            assertNotNull(i+"th Conditional is null!", c);
+            SignalHead sh = new jmri.VirtualSignalHead("Signal"+i);
+            assertNotNull(i+"th SignalHead is null!", sh);
+            InstanceManager.signalHeadManagerInstance().register(sh);
+            Route r = new jmri.DefaultRoute("Route"+i);
+            assertNotNull(i+"th Route is null!", r);
+            InstanceManager.routeManagerInstance().register(r);
+        }
+    }
+    protected void tearDown() { apps.tests.Log4JFixture.tearDown(); }
+
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(LRouteTableActionTest.class.getName());
+}
+
--- test/jmri/jmrit/beantable/LogixTableActionTest.java
+++ test/jmri/jmrit/beantable/LogixTableActionTest.java
@@ -4,16 +4,15 @@ package jmri.jmrit.beantable;
 
 import junit.framework.*;
 
-import javax.swing.JComboBox;
 import java.util.ResourceBundle;
 
 import jmri.InstanceManager;
 import jmri.managers.InternalSensorManager;
 import jmri.managers.InternalTurnoutManager;
 import jmri.Conditional;
-import jmri.ConditionalVariable;
 import jmri.Light;
 import jmri.Memory;
+import jmri.Route;
 import jmri.Sensor;
 import jmri.SignalHead;
 import jmri.Turnout;
@@ -21,11 +20,16 @@ import jmri.Turnout;
 
 /**
  * Tests for the jmri.jmrit.beantable.LogixTableAction class
+ *
+ * Note that the thread executing this test must yield to allow the event thread access
+ * to create and dispose of window frames.  i.e. those calls that simulate an
+ * actionPerformed event.
+ * 
  * @author	Pete Cressman  Copyright 2009
  */
 public class LogixTableActionTest extends TestCase {
 
-    String[] _varNames = new String[] {"is1", "Sensor2", "it1", "Turnout1", "Conditional1", "Conditional2",
+    String[] _varNames = new String[] {"is1", "Sensor2", "it1", "Turnout1", "C1", "C2",
                                        "cl1", "Light2", "IMemory1", "FastClock", "Signal1", "Signal2",
                                        "Signal3", "Signal4", "Signal5", "Signal6", "Signal7", "Signal8", 
                                        "Signal9"};
@@ -35,19 +39,17 @@ public class LogixTableActionTest extends TestCase {
                                         "SoundFile", "ScriptFile", "Turnout4", "Turnout5", "Sensor5", "Sensor6",
                                         "Turnout11", "Turnout12", "FastClock", "FastClock", "FastClock",
                                         "IMemory11", "Light15", "Light16"};
-    String[] _strData = new String[] {"", "",  "",  "", "", "", 
-                                       "", "", "10", "9:30", "IMemory5", "", "", 
-                                       "SoundFile.wav", "ScriptFile.jln", "13", "", "15", "",
-                                        "22", "", "3:30", "", "",
-                                        "IMemory19", "50", "100"};
+
+    String[] _strData = new String[] {"", "",  "",  "", "", "", "", "", "10", "9:30", "IMemory5", "", "", 
+                                       "SoundFile.wav", "jython\\Sensor-sound.py", "13", "", "15", "",
+                                        "22", "", "3:30", "", "", "IMemory19", "50", "100"};
+
 	static final ResourceBundle rbx = ResourceBundle
 			.getBundle("jmri.jmrit.beantable.LogixTableBundle");
 
-    private LogixTableAction _logixTable;
+    private static LogixTableAction _logixTable;
 
-    public void testCreate() {
-        assertNotNull("LogixTableAction is null!", _logixTable);        // test has begun
-        _logixTable._suppressReminder = true;
+    public void testCreateLogix() {
         _logixTable.actionPerformed(null);
         _logixTable.addPressed(null);
         _logixTable._addUserName.setText("TestLogix");    
@@ -61,13 +63,14 @@ public class LogixTableActionTest extends TestCase {
         _logixTable.conditionalUserName.setText("TestConditional");
         _logixTable.updateConditionalPressed(null);
         _logixTable.conditionalTableModel.setValueAt(null, 0, LogixTableAction.ConditionalTableModel.BUTTON_COLUMN);
+        Thread.currentThread().yield();     // allow event thread to draw window 
         _logixTable.addVariablePressed(null);
         _logixTable.cancelEditVariablePressed();
         for (int i=0; i<Conditional.NUM_STATE_VARIABLE_TYPES; i++)
         {
             _logixTable.addVariablePressed(null);
             _logixTable._variableTypeBox.setSelectedIndex(i);
-            Thread.currentThread().yield(); 
+            Thread.currentThread().yield();     // allow time for selection to repaint window 
             _logixTable._variableNameField.setText(_varNames[i]);
             _logixTable._variableData1Field.setText(_strData[i]);
             _logixTable._variableData2Field.setText("11");
@@ -90,6 +93,21 @@ public class LogixTableActionTest extends TestCase {
             _logixTable._actionStringField.setText(_strData[i]);
             _logixTable.updateActionPressed();
         }
+        _logixTable.updateConditionalPressed(null);
+        Assert.assertEquals(_logixTable._curLogix.getNumConditionals(),1);
+
+        _logixTable.donePressed(null);
+        // note: _logixTable.m.EDITCOL = BeanTableDataModel.DELETECOL
+        _logixTable.m.setValueAt((Object)rbx.getString("ButtonEdit"), 0, BeanTableDataModel.DELETECOL);
+        Thread.currentThread().yield(); 
+        _logixTable.conditionalTableModel.setValueAt(null, 0, LogixTableAction.ConditionalTableModel.BUTTON_COLUMN);
+        Thread.currentThread().yield();
+        _logixTable.conditionalUserName.setText("FirstConditional");
+        assertEquals( 
+                _logixTable._curConditional.getCopyOfStateVariables().size(), Conditional.NUM_STATE_VARIABLE_TYPES);
+        assertEquals(_logixTable._curConditional.getCopyOfActions().size(), Conditional.NUM_ACTION_TYPES-3);
+        _logixTable.updateConditionalPressed(null);
+        _logixTable.calculatePressed(null);
     }
 
     // from here down is testing infrastructure
@@ -112,23 +130,32 @@ public class LogixTableActionTest extends TestCase {
 
     // The minimal setup for log4J
     protected void setUp() { 
+        System.out.println("setUp called");
         apps.tests.Log4JFixture.setUp(); 
         _logixTable = new LogixTableAction();
+        assertNotNull("LogixTableAction is null!", _logixTable);        // test has begun
+        _logixTable._suppressReminder = true;
         InstanceManager.setSensorManager(new InternalSensorManager());
         InstanceManager.setTurnoutManager(new InternalTurnoutManager());
         InstanceManager.setLightManager(new jmri.jmrix.cmri.serial.SerialLightManager());
         for (int i=1; i<20; i++)
         {
             Sensor s = InstanceManager.sensorManagerInstance().newSensor("IS"+i, "Sensor"+i);
-            assertNotNull("Sensor is null!", s);
+            assertNotNull(i+"th Sensor is null!", s);
             Turnout t = InstanceManager.turnoutManagerInstance().newTurnout("IT"+i, "Turnout"+i);
-            assertNotNull("Turnout is null!", t);
+            assertNotNull(i+"th Turnout is null!", t);
             Light l = InstanceManager.lightManagerInstance().newLight("CL"+(i), "Light"+i);
             assertNotNull(i+"th Light is null!", l);
-            InstanceManager.conditionalManagerInstance().createNewConditional("Conditional"+i, "Conditional"+i);
+            Conditional c = InstanceManager.conditionalManagerInstance().createNewConditional("C"+i, "Conditional"+i);
+            assertNotNull(i+"th Conditional is null!", c);
             Memory m = InstanceManager.memoryManagerInstance().provideMemory("IMemory"+i);
-            InstanceManager.signalHeadManagerInstance().register(new jmri.VirtualSignalHead("Signal"+i));
-            InstanceManager.routeManagerInstance().register(new jmri.DefaultRoute("Route"+i));
+            assertNotNull(i+"th Memory is null!", m);
+            SignalHead sh = new jmri.VirtualSignalHead("Signal"+i);
+            assertNotNull(i+"th SignalHead is null!", sh);
+            InstanceManager.signalHeadManagerInstance().register(sh);
+            Route r = new jmri.DefaultRoute("Route"+i);
+            assertNotNull(i+"th Route is null!", r);
+            InstanceManager.routeManagerInstance().register(r);
         }
     }
     protected void tearDown() { apps.tests.Log4JFixture.tearDown(); }
