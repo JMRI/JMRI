c2090d30aa2794eb39f2b1a728d3d39dedc681f3
Pete Cressman <pete_cressman@sbcglobal.net>
2009-04-12 21:38:47 +0000

    Add Logix and LRouteTest cases

--- test/jmri/JmriTest.java
+++ test/jmri/JmriTest.java
@@ -9,7 +9,7 @@ import junit.framework.TestSuite;
 /**
  * Invoke complete set of tests for the Jmri package
  * @author	Bob Jacobsen, Copyright (C) 2001, 2002, 2007
- * @version         $Revision: 1.17 $
+ * @version         $Revision: 1.18 $
  */
 public class JmriTest extends TestCase {
 
@@ -32,6 +32,8 @@ public class JmriTest extends TestCase {
 		suite.addTest(jmri.LightTest.suite());
 		suite.addTest(jmri.BlockTest.suite());
 		suite.addTest(jmri.RouteTest.suite());
+		suite.addTest(jmri.jmrit.beantable.LogixTableActionTest.suite());
+		suite.addTest(jmri.jmrit.beantable.LRouteTableActionTest.suite());
 		suite.addTest(jmri.BlockManagerTest.suite());
 		suite.addTest(jmri.BeanSettingTest.suite());
 		suite.addTest(jmri.PathTest.suite());
