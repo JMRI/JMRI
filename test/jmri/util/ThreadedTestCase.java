581f10cba377c21b6f6db1bcb708dfaa256a9f6f
Bob Jacobsen <jacobsen@mac.com>
2009-04-13 14:40:55 +0000

    new utility method

--- test/jmri/util/ThreadedTestCase.java
+++ test/jmri/util/ThreadedTestCase.java
@@ -9,29 +9,35 @@ import junit.framework.TestCase;
  * temporarily
  *
  * @author	Bob Jacobsen - Copyright 2009
- * @version	$Revision: 1.1 $
+ * @version	$Revision: 1.2 $
  */
  
 public class ThreadedTestCase extends TestCase {
 
     public ThreadedTestCase(String s) { super(s); }
     
+    static int DEFAULTDELAY = 200;
+    
     /** 
      * Release the current thread, allowing other 
      * threads to process
      */
-	public synchronized void releaseThread() {
+	public synchronized void releaseThread(int delay) {
 		try {
 		    int priority = Thread.currentThread().getPriority(); 
 		    Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
 		    Thread.yield();
-		    Thread.sleep(200);
+		    Thread.sleep(delay);
 		    Thread.currentThread().setPriority(priority);
-			super.wait(100);
+			super.wait(delay);
 		}
 		catch (InterruptedException e) {
 		    Assert.fail("failed due to InterruptedException");
 		}
 	}
 
+	public synchronized void releaseThread() {
+	    releaseThread(DEFAULTDELAY);
+    }
+    
 }
