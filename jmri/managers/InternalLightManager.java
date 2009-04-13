bff00e59be9ba1ec6e9fad00efedd229aca15353
Bob Jacobsen <jacobsen@mac.com>
2009-04-13 16:14:04 +0000

    part of implementing internal lights

---
+++ jmri/managers/InternalLightManager.java
@@ -0,0 +1,36 @@
+// InternalLightManager.java
+
+package jmri.managers;
+
+import jmri.Light;
+import jmri.AbstractLight;
+import jmri.AbstractLightManager;
+
+/**
+ * Implement a light manager for "Internal" (virtual) lights.
+ *
+ * @author			Bob Jacobsen Copyright (C) 2009
+ * @version			$Revision: 1.1 $
+ */
+public class InternalLightManager extends AbstractLightManager {
+
+    /**
+     * Create and return an internal (no layout connection) Light
+     */
+    protected Light createNewLight(String systemName, String userName) {
+        return new AbstractLight(systemName, userName){
+            protected void forwardCommandChangeToLayout(int s) {}
+        };
+    }
+    
+    public char systemLetter() { return 'I'; }
+    public boolean validSystemNameConfig(String systemName) {
+        return true;
+    }
+    public boolean validSystemNameFormat(String systemName) {
+        return true;
+    }
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(InternalLightManager.class.getName());
+}
+
+/* @(#)InternalLightManager.java */
