9a065f29fed5812193089f8a42edcb2367c913ec
Bob Jacobsen <jacobsen@mac.com>
2009-06-06 19:27:06 +0000

    add internal light manager persistance

---
+++ jmri/managers/configurexml/InternalLightManagerXml.java
@@ -0,0 +1,35 @@
+package jmri.managers.configurexml;
+
+import org.jdom.Element;
+
+/**
+ * Provides load and store functionality for
+ * configuring InternalLightManagers.
+ * <P>
+ * Uses the store method from the abstract base class, but
+ * provides a load method here.
+ *
+ * @author Bob Jacobsen Copyright: Copyright (c) 2009
+ * @version $Revision: 1.1 $
+ */
+public class InternalLightManagerXml extends jmri.managers.configurexml.AbstractLightManagerConfigXML {
+
+    public InternalLightManagerXml() {
+        super();
+    }
+
+    public void setStoreElementClass(Element lights) {
+        lights.setAttribute("class",this.getClass().getName());
+    }
+
+    public void load(Element element, Object o) {
+        log.error("Invalid method called");
+    }
+
+    public void load(Element lights) {
+        // load individual lights
+        loadLights(lights);
+    }
+
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(InternalLightManagerXml.class.getName());
+}
