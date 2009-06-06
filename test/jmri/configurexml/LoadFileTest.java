b0e731a04c4c402756da3a679bbbc926b8722820
Bob Jacobsen <jacobsen@mac.com>
2009-06-06 19:25:22 +0000

    add I/O tests for typical file

---
+++ test/jmri/configurexml/LoadFileTest.java
@@ -0,0 +1,102 @@
+// LoadFileText.java
+
+package jmri.configurexml;
+
+import jmri.jmrit.XmlFile;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import jmri.util.JUnitUtil;
+import jmri.InstanceManager;
+
+/**
+ * Test upper level loading of a file
+ * 
+ * @author Bob Jacobsen Copyright 2009
+ * @since 2.5.5
+ * @version $Revision: 1.1 $
+ */
+public class LoadFileTest extends TestCase {
+
+    public void testLoadOne() {
+        // load file
+        InstanceManager.configureManagerInstance()
+            .load(new java.io.File("java/test/jmri/configurexml/LoadFileTest.xml"));
+    
+        // check existance of a few objects
+        Assert.assertNotNull(InstanceManager.sensorManagerInstance().getSensor("IS1"));
+        Assert.assertNull(InstanceManager.sensorManagerInstance().getSensor("no sensor"));
+
+        Assert.assertNotNull(InstanceManager.turnoutManagerInstance().getTurnout("IT1"));
+        Assert.assertNull(InstanceManager.turnoutManagerInstance().getTurnout("no sensor"));
+        
+        Assert.assertNotNull(InstanceManager.memoryManagerInstance().getMemory("IM1"));
+        Assert.assertNull(InstanceManager.memoryManagerInstance().getMemory("no memory"));
+        
+    }
+    
+    public void testLoadStoreOne() {
+        // load manager
+        java.io.File inFile = new java.io.File("java/test/jmri/configurexml/LoadFileTest.xml");
+        
+        // load file
+        InstanceManager.configureManagerInstance()
+            .load(inFile);
+    
+        // store file
+        XmlFile.ensurePrefsPresent(XmlFile.prefsDir()+"temp");
+        java.io.File outFile = new java.io.File("temp/LoadFileTest.xml");
+        InstanceManager.configureManagerInstance()
+            .storeConfig(outFile);
+        
+        // compare lengths
+        Assert.assertEquals("length", inFile.length(), outFile.length());
+    }
+    
+    // from here down is testing infrastructure
+
+    public LoadFileTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+		String[] testCaseName = {"-noloading", LoadFileTest.class.getName()};
+		junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(LoadFileTest.class);
+        return suite;
+    }
+
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(LoadFileTest.class.getName());
+
+    // The minimal setup for log4J
+    protected void setUp() throws Exception { 
+        super.setUp();
+        apps.tests.Log4JFixture.setUp(); 
+        JUnitUtil.resetInstanceManager();
+        InstanceManager.setConfigureManager(new ConfigXmlManager());
+        JUnitUtil.initInternalTurnoutManager();
+        JUnitUtil.initInternalLightManager();
+        JUnitUtil.initInternalSensorManager();
+        InstanceManager.configureManagerInstance().registerConfig(
+                InstanceManager.memoryManagerInstance());
+    }
+    
+    protected void tearDown() throws Exception { 
+        JUnitUtil.resetInstanceManager();
+        super.tearDown();
+        apps.tests.Log4JFixture.tearDown(); 
+    }
+}
