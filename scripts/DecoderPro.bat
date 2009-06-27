038d38a55a435ef298cacbe8a2273b1fd2311417
Bob Jacobsen <jacobsen@mac.com>
2009-06-27 05:03:23 +0000

    2.5.7

--- scripts/DecoderPro.bat
+++ scripts/DecoderPro.bat
@@ -1,3 +1,3 @@
-REM Start the DecoderPro program from JMRI 2.5.6 ($Revision: 1.39 $)
+REM Start the DecoderPro program from JMRI 2.5.7 ($Revision: 1.40 $)
 
 LaunchJMRI.exe apps.DecoderPro.DecoderPro %1 %2 %3 %4 %5 %6 %7 %8 %9
