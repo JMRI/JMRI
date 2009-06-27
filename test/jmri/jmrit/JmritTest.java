e86c3d2679986b3e59b492318ec4be106647b861
Bob Jacobsen <jacobsen@mac.com>
2009-06-27 16:25:09 +0000

    include jython

--- test/jmri/jmrit/JmritTest.java
+++ test/jmri/jmrit/JmritTest.java
@@ -8,7 +8,7 @@ import junit.framework.*;
  * Invokes complete set of tests in the jmri.jmrit tree
  *
  * @author	    Bob Jacobsen  Copyright 2001, 2003
- * @version         $Revision: 1.19 $
+ * @version         $Revision: 1.20 $
  */
 public class JmritTest extends TestCase {
 
@@ -35,6 +35,7 @@ public class JmritTest extends TestCase {
         suite.addTest(jmri.jmrit.beantable.BeanTableTest.suite());
         suite.addTest(jmri.jmrit.decoderdefn.DecoderDefnTest.suite());
         suite.addTest(jmri.jmrit.display.DisplayTest.suite());
+        suite.addTest(jmri.jmrit.jython.JythonTest.suite());
         suite.addTest(jmri.jmrit.operations.OperationsTest.suite());
         suite.addTest(jmri.jmrit.powerpanel.PowerPanelTest.suite());
         suite.addTest(jmri.jmrit.roster.RosterTest.suite());
