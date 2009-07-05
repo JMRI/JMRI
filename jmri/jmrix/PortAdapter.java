0d8473eaf4e461ad0693bfb42e86016bb0855abf
Paul Bender <paul.bender@acm.org>
2009-07-05 22:27:52 +0000

    Add a new port adapter which configures a common port.  NOTE:  This currently is a decendent of SerialPortAdapter, that may change in the future.

--- jmri/jmrix/PortAdapter.java
+++ jmri/jmrix/PortAdapter.java
@@ -2,6 +2,9 @@
 
 package jmri.jmrix;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
 /**
  * Enables basic setup of a interface
  * for a jmrix implementation.
@@ -9,7 +12,7 @@ package jmri.jmrix;
  * This has no e.g. serial-specific information.
  *
  * @author	Bob Jacobsen   Copyright (C) 2001, 2003, 2008
- * @version	$Revision: 1.1 $
+ * @version	$Revision: 1.2 $
  * @see         jmri.jmrix.SerialConfigException
  * @since 2.3.1
  */
@@ -22,5 +25,50 @@ public interface PortAdapter  {
 	/** Query the status of this connection.  If all OK, at least
 	 * as far as is known, return true */
 	public boolean status();
+
+	// returns the InputStream from the port
+	public DataInputStream getInputStream();
+        // returns the outputStream to the port
+        public DataOutputStream getOutputStream();
+
+        /**         * Get an array of valid values for "option 1"; used to display valid options.
+ *          * May not be null, but may have zero entries
+ *                   */
+        public String[] validOption1();
+
+        /**
+ *          * Get a String that says what Option 1 represents
+ *                   * May be an empty string, but will not be null
+ *                            */
+        public String option1Name();
+
+        /**
+ *          * Set the first port option.  Only to be used after construction, but
+ *                   * before the openPort call
+ *                            */
+        public void configureOption1(String value);
+   public String getCurrentOption1Setting();
+
+        /**         * Get an array of valid values for "option 2"; used to display valid options.         * May not be null, but may have zero entries
+ *          */
+        public String[] validOption2();
+
+        /**
+ *          * Get a String that says what Option 2 represents
+ *                   * May be an empty string, but will not be null
+ *                            */
+        public String option2Name();
+
+        /**
+ *          * Set the second port option.  Only to be used after construction, but
+ *                   * before the openPort call
+ *                            */
+        public void configureOption2(String value);
+
+   /**
+ *      * Get current option 2 value
+ *           */
+    public String getCurrentOption2Setting();
+
 
 }
