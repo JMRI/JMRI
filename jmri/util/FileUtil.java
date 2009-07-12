5dc4b12b474c2f7e25bb1b177b7004b26d2b2f5b
Bob Jacobsen <jacobsen@mac.com>
2009-07-12 23:09:13 +0000

    new util

--- jmri/util/FileUtil.java
+++ jmri/util/FileUtil.java
@@ -16,11 +16,122 @@ import java.io.File;
  * Java 1.1.8 system, or at least try to fake it.
  *
  * @author Bob Jacobsen  Copyright 2003, 2005, 2006
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
 
 public class FileUtil {
 
+    /**
+     * Find the resource file corresponding to a name.
+     * There are five cases:
+     * <UL>
+     * <LI> Starts with "program:", treat the rest as a relative pathname below the program directory
+     * <LI> Starts with "preference:", treat the rest as a relative path below the preferences directory
+     * <LI> Starts with "resource:", treat the rest as a pathname relative to the program directory
+     *                  (deprecated; see "program:" above)
+     * <LI> Starts with "file:", treat the rest as a relative path below the resource directory in the preferences directory
+     *                  (deprecated; see "preference:" above)
+     * <LI> Otherwise, treat the name as a relative path below the program directory
+     * </UL>
+     * In any case, absolute pathnames will work.
+     *
+     * @param pName The name string, possibly starting with file: or resource:
+     * @return Absolute file name to use, or null.
+     * @since 2.7.2
+     */
+    static public String getExternalFilename(String pName) {
+        if (pName == null || pName.length() == 0) {
+            return null;
+        }
+        if (pName.startsWith("resource:")) {
+            // convert to relative filename 
+            String temp = pName.substring("resource:".length());
+            if ((new File(temp)).isAbsolute())
+                return temp;
+            else
+                return "resources"+File.separator+temp;
+         } else if (pName.startsWith("program:")) {
+            // both relative and absolute are just returned
+            return pName.substring("program:".length());
+        } else if (pName.startsWith("preference:")) {
+            String filename = pName.substring("preference:".length());
+            
+            // Check for absolute path name
+            if ((new File(filename)).isAbsolute()) {
+                if (log.isDebugEnabled()) log.debug("Load from absolute path: "+filename);
+                return filename;
+            }
+            // assume this is a relative path from the
+            // preferences directory
+            filename = jmri.jmrit.XmlFile.userFileLocationDefault()+filename;
+            if (log.isDebugEnabled()) log.debug("load from user preferences file: "+filename);
+            return filename;
+        } else if (pName.startsWith("file:")) {
+            String filename = pName.substring("file:".length());
+            
+            // historically, absolute path names could be stored 
+            // in the 'file' format.  Check for those, and
+            // accept them if present
+            if ((new File(filename)).isAbsolute()) {
+                if (log.isDebugEnabled()) log.debug("Load from absolute path: "+filename);
+                return filename;
+            }
+            // assume this is a relative path from the
+            // preferences directory
+            filename = jmri.jmrit.XmlFile.userFileLocationDefault()+"resources"+File.separator+filename;
+            if (log.isDebugEnabled()) log.debug("load from user preferences file: "+filename);
+            return filename;
+        }
+        // must just be a (hopefully) valid name
+        else return pName;
+    }
+
+    /**
+     * Convert a File object to our preferred storage form.
+     *
+     * This is the inverse of {@link getExternalFilename(String pName)}.
+     * Deprecated forms are not created.
+     *
+     * @param file File to be represented
+     * @since 2.7.2
+     */
+    static public String getPortableFilename(File file) {
+        // compare full path name to see if same as preferences
+        String filename = file.getAbsolutePath();
+
+        String preferencePrefix = jmri.jmrit.XmlFile.userFileLocationDefault();
+        if (filename.startsWith(preferencePrefix)) 
+            return "preference:"+filename.substring(preferencePrefix.length(), filename.length());
+        
+        // now check for relative to program dir
+        String progname = new File("").getAbsolutePath()+File.separator;
+        if (filename.startsWith(progname)) 
+            return "program:"+filename.substring(progname.length(), filename.length());
+        return filename;   // absolute, and doesn't match
+    }
+    
+    /**
+     * Convert a filename string to our preferred storage form.
+     *
+     * This is the inverse of {@link getExternalFilename(String pName)}.
+     * Deprecated forms are not created.
+     *
+     * @param filename Filename to be represented
+     * @since 2.7.2
+     */
+    static public String getPortableFilename(String filename) {
+        // if this already contains prefix, convert
+        if (filename.startsWith("file:") ||
+            filename.startsWith("resource:") ||
+            filename.startsWith("program:") ||
+            filename.startsWith("preference:") ) {
+            return getPortableFilename(getExternalFilename(filename));
+        } else {
+            // treat as pure filename
+            return getPortableFilename(new File(filename));
+        }
+    }
+    
     /**
      * Provide the URL for a particular file/directory via the
      * URI form.
--- test/jmri/util/FileUtilTest.java
+++ test/jmri/util/FileUtilTest.java
@@ -5,18 +5,121 @@ package jmri.util;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import junit.framework.Assert;
+
+import java.io.*;
+
+import jmri.jmrit.XmlFile;
 
 /**
  * Tests for the jmri.util.FileUtil class.
- * @author	Bob Jacobsen  Copyright 2003
- * @version	$Revision: 1.3 $
+ * @author	Bob Jacobsen  Copyright 2003, 2009
+ * @version	$Revision: 1.4 $
  */
 public class FileUtilTest extends TestCase {
 
 
-    public void testVoid() {
+    // tests of internal to external mapping
+    
+    public void testGEFRel() {
+        String name = FileUtil.getExternalFilename("resources/icons");
+        Assert.assertEquals("resources/icons", name);
     }
 
+    public void testGEFAbs() {
+        File f = new File("resources/icons");
+        String name = FileUtil.getExternalFilename(f.getAbsolutePath());
+        Assert.assertEquals(f.getAbsolutePath(), name);
+    }
+
+    public void testGEFResourceRel() {
+        String name = FileUtil.getExternalFilename("resource:icons");
+        Assert.assertEquals("resources/icons", name);
+    }
+
+    public void testGEFResourceAbs() {
+        File f = new File("resources/icons");
+        String name = FileUtil.getExternalFilename("resource:"+f.getAbsolutePath());
+        Assert.assertEquals(f.getAbsolutePath(), name);
+    }
+
+    public void testGEFProgramRel() {
+        String name = FileUtil.getExternalFilename("program:jython");
+        Assert.assertEquals("jython", name);
+    }
+
+    public void testGEFProgramAbs() {
+        File f = new File("resources/icons");
+        String name = FileUtil.getExternalFilename("program:"+f.getAbsolutePath());
+        Assert.assertEquals(f.getAbsolutePath(), name);
+    }
+
+    public void testGEFPrefRel() {
+        String name = FileUtil.getExternalFilename("preference:foo");
+        Assert.assertEquals(XmlFile.userFileLocationDefault()+"foo", name);
+    }
+
+    public void testGEFPrefAbs() {
+        File f = new File("resources/icons");
+        String name = FileUtil.getExternalFilename("preference:"+f.getAbsolutePath());
+        Assert.assertEquals(f.getAbsolutePath(), name);
+    }
+
+    public void testGEFFileRel() {
+        String name = FileUtil.getExternalFilename("file:foo");
+        Assert.assertEquals(XmlFile.userFileLocationDefault()+"resources"+File.separator+"foo", name);
+    }
+
+    public void testGEFFileAbs() {
+        File f = new File("resources/icons");
+        String name = FileUtil.getExternalFilename("file:"+f.getAbsolutePath());
+        Assert.assertEquals(f.getAbsolutePath(), name);
+    }
+
+    // tests of external to internal mapping
+
+    public void testGetpfPreferenceF() throws IOException {
+        File f = new File(XmlFile.prefsDir()+File.separator+"foo");
+        String name = FileUtil.getPortableFilename(f);
+        Assert.assertEquals("preference:foo", name);
+    }
+
+    public void testGetpfPreferenceS() {
+        String name = FileUtil.getPortableFilename("preference:foo");
+        Assert.assertEquals("preference:foo", name);
+    }
+
+    public void testGetpfResourceF() throws IOException {
+        File f = new File(XmlFile.prefsDir()+File.separator+"resources"+File.separator+"foo");
+        String name = FileUtil.getPortableFilename(f);
+        Assert.assertEquals("preference:resources/foo", name);
+    }
+
+    public void testGetpfResourceS() {
+        String name = FileUtil.getPortableFilename("preference:resources/foo");
+        Assert.assertEquals("preference:resources/foo", name);
+    }
+
+    public void testGetpfProgramF() throws IOException {
+        File f = new File("resources"+File.separator+"icons");
+        String name = FileUtil.getPortableFilename(f);
+        Assert.assertEquals("program:resources/icons", name);
+    }
+
+    public void testGetpfProgramS() {
+        String name = FileUtil.getPortableFilename("program:resources/icons");
+        Assert.assertEquals("program:resources/icons", name);
+    }
+
+    public void testGetpfFileS() {
+        String name = FileUtil.getPortableFilename("file:icons");
+        Assert.assertEquals("preference:resources/icons", name);
+    }
+
+    public void testGetpfFileS2() {
+        String name = FileUtil.getPortableFilename("resource:icons");
+        Assert.assertEquals("program:resources/icons", name);
+    }
 
 	// from here down is testing infrastructure
 
