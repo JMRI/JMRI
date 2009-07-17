0924bfa0f3a69e015451668500f90e41a9e18349
Bob Jacobsen <jacobsen@mac.com>
2009-07-17 00:06:44 +0000

    for 2.7.2

--- scripts/JmriDemo.bat
+++ scripts/JmriDemo.bat
@@ -1,3 +1,3 @@
-REM Start the JmriDemo program from JMRI 2.6.1 ($Revision: 1.41 $)
+REM Start the JmriDemo program from JMRI 2.7.2 ($Revision: 1.42 $)
 
 LaunchJMRI.exe apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/PanelPro.bat
+++ scripts/PanelPro.bat
@@ -1,3 +1,3 @@
-REM Start the PanelPro program from JMRI 2.6.1 ($Revision: 1.40 $)
+REM Start the PanelPro program from JMRI 2.7.2 ($Revision: 1.41 $)
 
 LaunchJMRI.exe apps.PanelPro.PanelPro %1 %2 %3 %4 %5 %6 %7 %8 %9
