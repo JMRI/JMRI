cecbedc03af36528ecb544fa454ac1a34759afe9
Kevin Dickerson <kev.public@ntlworld.com>
2009-07-31 22:08:33 +0000

    Extended the scope of details stored from the ecos.

--- jmri/jmrix/ecos/EcosLocoAddress.java
+++ jmri/jmrix/ecos/EcosLocoAddress.java
@@ -1,40 +1,107 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package jmri.jmrix.ecos;
 
 /**
+ * Stores all the loco information from the Ecos into JMRI
  *
  * @author Kevin Dickerson
+ * @version     $Revision: 1.2 $
  */
 public class EcosLocoAddress {
-    int _ecosObject = 0;
+    String _ecosObject = null;
     int _dccAddress = 0;
+    String _ecosDescription = null;
+    String _rosterId = null;
+    String _protocol = null;
+    String _cv8 = null;
+    String _cv7 = null;
+    
 
     public EcosLocoAddress(int dCCAddress){
-
         _dccAddress=dCCAddress;
     }
+    
+    public EcosLocoAddress(String ecosObject){
+        _ecosObject=ecosObject;
+    }
+    
+    public void setCV8(String cv8){
+        _cv8=cv8;
+    }
+    
+    public String getCV8(){
+        return _cv8;
+    }
+    
+    public void setCV7(String cv7){
+        _cv7=cv7;
+    }
+    
+    public String getCV7(){
+        return _cv7;
+    }
+    
+    public void setEcosLocoAddress(int dCCAddress){
+        _dccAddress=dCCAddress;
+    }
+    
+    public int getEcosLocoAddress(){
+        return _dccAddress;
+    }
 
-    public int getEcosObject(){
+    public String getEcosObject(){
 
         return _ecosObject;
     }
 
-    public int getDCCAddress(){
-        return _dccAddress;
-    }
-
-    public void setDCCAddress(int dCCAddress){
-        _dccAddress = dCCAddress;
-    }
-
-   public void setEcosObject(int ecosObject){
+    //Should this option be made public? should setting the object only be available when the Loco is created.
+    //It needs a bit of a re-think!
+    //It is set this way because of adhoc locos being created for throttles.
+    public void setEcosObject(String ecosObject){
         _ecosObject = ecosObject;
     }
 
+    public String getEcosDescription(){
+        return _ecosDescription;
+    }
+
+    public void setEcosDescription(String description){
+        _ecosDescription = description;
+    }    
+    
+    public String getRosterId(){
+        return _rosterId;
+    }
+
+    public void setRosterId(String roster){
+        _rosterId = roster;
+    }    
+    //Protocol is here as it is a potential value from the ecos
+    // We may not actually use it.
+    public String getProtocol(){
+        return _protocol;
+    }
+
+    public void setProtocol(String protocol){
+        _protocol = protocol;
+    }   
+    
+    /*
+    The Temporary Entry Field is used to determine if JMRI has had to create the entry on an ad-hoc basis
+    for the throttle.  If this is set to True, the throttle can evaluate this field to determine if the 
+    loco should be removed from the Ecos Database when it has been released.
+    
+    */
+    
+    boolean _tempEntry = false;
+    
+    public void setEcosTempEntry(boolean boo){
+        _tempEntry = boo;
+    }
+    
+    public boolean getEcosTempEntry(){
+        return _tempEntry;
+    }
+    
     // implementing classes will typically have a function/listener to get
     // updates from the layout, which will then call
     //		public void firePropertyChange(String propertyName,
