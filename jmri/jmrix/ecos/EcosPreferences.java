0803e9c7757c1309d64ad906ce65adb7d1154e18
Kevin Dickerson <kev.public@ntlworld.com>
2009-08-01 10:40:12 +0000

    resolve javadoc issue

--- jmri/jmrix/ecos/EcosPreferences.java
+++ jmri/jmrix/ecos/EcosPreferences.java
@@ -7,7 +7,7 @@ package jmri.jmrix.ecos;
  * with JMRI.
  *
  * @author	Kevin Dickerson  Copyright (C) 2009
- * @version     $Revision: 1.2 $
+ * @version     $Revision: 1.21 $
  */
 
 public class EcosPreferences {
@@ -15,15 +15,11 @@ public class EcosPreferences {
     public EcosPreferences(){
 	_instance = this;
     }
-    //protected static boolean _addlocotoecos = false;
-    
-
     
      /**
-     * Stores the users preferance for when a loco is created in the Roster it is also
-     * created in the ECOS, if it does not exist.
+     * Stores the users preferance for when a loco is created in the Roster
+     * should it also be created in the ECOS, if it does not exist.
      * Currently not implemented.
-     * @return true if the user has set it to be added
      */
      
     protected static boolean _addlocotoecos = false;
@@ -40,7 +36,6 @@ public class EcosPreferences {
      * Stores the users preferance if a loco has been created on the Ecos,
      * should an entry in the JMRI Roster be created.
      * Currently not implemented.
-     * @return true if the user has set it to be added
      */
      
     protected static boolean _addlocotojmri = false;
@@ -56,7 +51,7 @@ public class EcosPreferences {
      /**
      * Stores the users preferance on how the ecos loco description,
      * should be formated.
-     * @return A user configured string.
+     * Currently not implemented
      */    
     
     protected static String _ecoslocodescription = null;
@@ -73,7 +68,6 @@ public class EcosPreferences {
      * If there is a conflict in loco information between the Ecos and JMRI,
      * this determines which system wins.
      * Currently not implemented.
-     * @return 0x00 NOSYNC, 0x01 WARN, 0x02 JMRI Wins, 0x03 ECOS Wins.
      */
      
     private static final int NOSYNC     = 0x00;
@@ -115,9 +109,6 @@ public class EcosPreferences {
     * Stores the users preferance if a loco has been created ad-hoc,
     * on the Throttle, should the entry created for it in the ECOS be deleted.
     * Currently not implemented.
-    * @return 0x00 if the user has not set a remember this value
-    * 0x01 if the user sets the preference to always leave the loco from the Ecos when finished.
-    * 0x02 if the user sets the preference to always remove the loco on the Ecos when finished.
     */
      
     protected static int _adhoclocofromecos = 0x00;
@@ -131,27 +122,9 @@ public class EcosPreferences {
     }
     
     /**
-    * Stores the users preferance as to whether the setting _removeadhoclocofromecos,
-    * should be used all the time
-    * Currently not implemented.
-    * @return true if the user has set it to be added
-    */
-     
-    /*protected static boolean _rememberadhoclocofromecos = false;
-     
-    public boolean getRememberAdhocLocoFromEcos(){
-        return _rememberadhoclocofromecos;
-    }
-    
-    public void setRememberAdhocLocoFromEcos(boolean boo){
-        _rememberadhoclocofromecos = boo;
-    }
-    */
-    /**
     * Stores the users preferance for deleting a loco from the roster should it,
     * also be deleted from the ECOS.
     * Currently not implemented.
-    * @return true if the user has set it to be added
     */
       
     protected static boolean _removelocofromecos = false;
@@ -168,7 +141,6 @@ public class EcosPreferences {
     * Stores the users preferance for deleting a loco from the ECOS should it,
     * also be deleted from the JMRI Roster.
     * Currently not implemented.
-    * @return true if the user has set it to be added
     */
     
     protected static boolean _removelocofromjmri = false;
@@ -185,7 +157,6 @@ public class EcosPreferences {
     * Stores the users preferance when creating a turnout in JMRI should it
     * also be created on the ECOS.
     * Currently not implemented.
-    * @return true if the user has set it to be added
     */    
     protected static boolean _addturnoutstoecos = false;
     
@@ -201,7 +172,6 @@ public class EcosPreferences {
     * Stores the users preferance when a new turnout is created on the ECOS should it
     * also be created in JMRI.
     * Currently not implemented.
-    * @return true if the user has set it to be added
     */
     
     protected static boolean _addturnoutstojmri = false;
@@ -218,7 +188,6 @@ public class EcosPreferences {
     * Stores the users preferance when a new turnout is removed from the ECOS should it
     * also be removed from JMRI..
     * Currently not implemented.
-    * @return true if the user has set it to be added
     */
     
     protected static boolean _removeturnoutsfromjmri = false;
@@ -235,7 +204,6 @@ public class EcosPreferences {
     * Stores the users preferance when a new turnout is removed from JMRI should it
     * also be removed from the ECOS.
     * Currently not implemented.
-    * @return true if the user has set it to be added
     */    
     protected static boolean _removeturnoutsfromecos = false;
     
@@ -255,11 +223,6 @@ public class EcosPreferences {
     static public EcosPreferences instance() {
        if (_instance == null) _instance = new EcosPreferences();
         return _instance; 
-       /* if (_instance == null){
-            _instance = new EcosPreferences();
-            jmri.InstanceManager.configureManagerInstance().registerPref(_instance);
-        }
-        return _instance;*/
     }
     static EcosPreferences _instance = null;
 
