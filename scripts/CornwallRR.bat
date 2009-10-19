8a4a6470c64c7e09080d3fc0fcaec04c32386f4f
Bob Jacobsen <jacobsen@mac.com>
2009-10-19 05:15:25 +0000

    for 2.7.7

--- scripts/CornwallRR.bat
+++ scripts/CornwallRR.bat
@@ -1,3 +1,3 @@
-REM Start the CornwallRR program from JMRI 2.7.6 ($Revision: 1.44 $)
+REM Start the CornwallRR program from JMRI 2.7.7 ($Revision: 1.45 $)
 
 LaunchJMRI.exe apps.cornwall.CornwallRR %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/SoundPro.bat
+++ scripts/SoundPro.bat
@@ -1,3 +1,3 @@
-REM Start the SoundPro program from JMRI 2.7.6 ($Revision: 1.2 $)
+REM Start the SoundPro program from JMRI 2.7.7 ($Revision: 1.3 $)
 
 LaunchJMRI.exe apps.SoundPro.SoundPro %1 %2 %3 %4 %5 %6 %7 %8 %9
