fd47abdf3f51a77b5c83b8911909f4f6f9fc47b1
Kevin Dickerson <kev.public@ntlworld.com>
2009-10-21 20:05:20 +0000

    Table to view locos loaded from the ecos, match the loco to a roster entry, and also import the loco from the ecos to the roster.

---
+++ jmri/jmrix/ecos/swing/locodatabase/EcosLocoTableAction.java
@@ -0,0 +1,61 @@
+package jmri.jmrix.ecos.swing.locodatabase;
+
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import jmri.jmrix.ecos.EcosLocoAddress;
+import jmri.jmrix.ecos.EcosLocoAddressManager;
+
+
+public class EcosLocoTableAction extends AbstractTableAction {
+
+    /**
+     * Create an action with a specific title.
+     * <P>
+     * Note that the argument is the Action title, not the title of the
+     * resulting frame.  Perhaps this should be changed?
+     * @param s
+     */
+   public EcosLocoTableAction(String s) {
+        super(s);
+        
+    }
+    public EcosLocoTableAction() { this("Ecos Loco Table");}
+    
+    void createModel() {
+    
+        m = new EcosLocoTableDataModel() {
+    		public int getColumnCount(){ return NUMCOLUMN;}
+
+            public void clickOn(jmri.jmrix.ecos.EcosLocoAddressManager m) {}
+            
+            public EcosLocoAddressManager getManager() {return (EcosLocoAddressManager)jmri.InstanceManager.getDefault(EcosLocoAddressManager.class);}
+
+            public EcosLocoAddress getByEcosObject(String object) {return ((EcosLocoAddressManager)jmri.InstanceManager.getDefault(EcosLocoAddressManager.class)).getByEcosObject(object);}
+            public EcosLocoAddress getByDccAddress(int address) {return ((EcosLocoAddressManager)jmri.InstanceManager.getDefault(EcosLocoAddressManager.class)).getByDccAddress(address);}
+            
+            public String getValue(String s) {
+                return "Set";
+            }
+            public JButton configureButton() {
+                return new JButton(" Set ");
+            }
+        };
+    }
+
+    
+    void setTitle() {
+        f.setTitle("Ecos Loco Table");
+    }
+    
+    String helpTarget() {
+        return "package.jmri.jmrix.ecos.ecosLocoTable";
+    }
+    
+    void savePressed(ActionEvent e){
+        //System.out.println("anything");
+        //jmri.jmrit.roster.Roster.writeRosterFile();
+    //This function isn't available.
+    }
+}
