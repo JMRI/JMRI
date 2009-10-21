f0784dd8b8d300e63d45b2b4f79fcb08b4ab585b
Kevin Dickerson <kev.public@ntlworld.com>
2009-10-21 20:07:15 +0000

    Front end for the user to set preferences when dealing with the Eos

---
+++ jmri/jmrix/ecos/swing/preferences/PreferencesFrame.java
@@ -0,0 +1,59 @@
+// PreferencesFrame.java
+
+package jmri.jmrix.ecos.swing.preferences;
+
+import jmri.jmrix.ecos.*;
+import javax.swing.*;
+
+
+/**
+ * Frame for ECoS preferences
+ * @author	Kevin Dickerson   Copyright (C) 2009
+ * @version $Revision: 1.1 $
+ */
+public class PreferencesFrame extends jmri.util.JmriJFrame {
+
+    public PreferencesFrame() {
+        super();
+    }
+
+    JButton sendButton;
+    PreferencesPane preferencesPane;
+    
+    public void initComponents() throws Exception {
+        // the following code sets the frame's initial state
+        
+    /*    sendButton = new JButton("Update");
+        sendButton.setVisible(true);
+        sendButton.setToolTipText("User Preferences ");*/
+
+        preferencesPane = new PreferencesPane();
+        
+        setTitle("ECoS User Preferences");
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        getContentPane().add(preferencesPane);
+        /*getContentPane().add(sendButton);
+
+        sendButton.addActionListener(new java.awt.event.ActionListener() {
+                public void actionPerformed(java.awt.event.ActionEvent e) {
+                    sendButtonActionPerformed(e);
+                }
+            });*/
+
+		addHelpMenu("package.jmri.jmrix.ecos.swing.preferencesframe.PreferencesFrame", true);
+
+        // pack for display
+        pack();
+    }
+
+    /*public void sendButtonActionPerformed(java.awt.event.ActionEvent e) {
+        preferencesPane.reset();
+        
+        EcosMessage m = new EcosMessage("get(1, info)");
+        EcosTrafficController.instance().sendEcosMessage(m, null);
+
+	}*/
+
+}
+
---
+++ jmri/jmrix/ecos/swing/preferences/PreferencesFrameAction.java
@@ -0,0 +1,35 @@
+// PreferencesFrameAction.java
+
+package jmri.jmrix.ecos.swing.preferences;
+
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+
+/**
+ * Swing action to create and register a 
+ *       			PreferencesFrame object
+ *
+ * @author	    Kevin Dickerson    Copyright (C) 2009
+ * @version		$Revision: 1.1 $	
+ */
+
+public class PreferencesFrameAction extends AbstractAction {
+
+	public PreferencesFrameAction(String s) { super(s);}
+	
+    public void actionPerformed(ActionEvent e) {
+		PreferencesFrame f = new PreferencesFrame();
+		try {
+			f.initComponents();
+			}
+		catch (Exception ex) {
+			log.error("Exception: "+ex.toString());
+			ex.printStackTrace();
+			}
+		f.setVisible(true);	
+	}
+   static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(PreferencesFrameAction.class.getName());
+}
+
+
+/* @(#)PreferencesFrameAction.java */
