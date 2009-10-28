c3149824e72214eaea3552566586e78cbd7aa0de
Bob Jacobsen <jacobsen@mac.com>
2009-10-28 03:20:45 +0000

    add several state tests, use permanent file.

--- test/jmri/jmrit/display/PanelEditorTest.java
+++ test/jmri/jmrit/display/PanelEditorTest.java
@@ -15,24 +15,13 @@ import junit.framework.TestSuite;
  *
  * Description:
  * @author			Bob Jacobsen
- * @version			$Revision: 1.9 $
+ * @version			$Revision: 1.10 $
  */
 public class PanelEditorTest extends TestCase {
 
     TurnoutIcon to = null;
 
 	public void testShow() throws java.io.IOException {
-        // ensure demo directory exists
-        XmlFile.ensurePrefsPresent("temp");
-        XmlFile.ensurePrefsPresent("temp"+File.separator+"prefs");
-	    // don't care about logged messages
-	    jmri.util.JUnitAppender.clearBacklog();
-	    // create a test file
-	    File f = new File("temp"+File.separator+"prefs"+File.separator+"PanelEditorTest1.xml");
-	    FileWriter fw = new FileWriter(f);
-	    fw.write(layerTestData);
-	    fw.close();
-	    
 	    // reset instance manager, configuration manager
         jmri.InstanceManager i = new jmri.InstanceManager(){
             protected void init() {
@@ -45,6 +34,7 @@ public class PanelEditorTest extends TestCase {
 	    };
 	    
 	    // load and display
+	    File f = new File("java"+File.separator+"test"+File.separator+"jmri"+File.separator+"jmrit"+File.separator+"display"+File.separator+"PanelEditorTest1.xml");
         cm.load(f);
         
         // check some errors
@@ -77,22 +67,6 @@ public class PanelEditorTest extends TestCase {
     // The minimal setup for log4J
     protected void setUp() { apps.tests.Log4JFixture.setUp(); }
     protected void tearDown() { apps.tests.Log4JFixture.tearDown(); }
-
-String layerTestData = 
-"<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
-"<!DOCTYPE layout-config SYSTEM \"layout-config.dtd\">"+
-"<layout-config>"+
-"  <paneleditor class=\"jmri.jmrit.display.configurexml.PanelEditorXml\" name=\"Layer Name Test\" x=\"20\" y=\"22\" height=\"226\" width=\"204\" editable=\"no\" positionable=\"yes\" controlling=\"yes\" hide=\"yes\" panelmenu=\"no\">"+
-"    <positionablelabel forcecontroloff=\"false\" fixed=\"false\" showtooltip=\"true\" class=\"jmri.jmrit.display.configurexml.PositionableLabelXml\" x=\"40\" y=\"130\" level=\"5\" text=\"Checks should be\" size=\"13\" style=\"0\" />"+
-"    <positionablelabel forcecontroloff=\"false\" fixed=\"false\" showtooltip=\"true\" class=\"jmri.jmrit.display.configurexml.PositionableLabelXml\" x=\"51\" y=\"149\" level=\"5\" text=\"in front of Xs\" size=\"13\" style=\"0\" />"+
-"    <turnouticon turnout=\"IT1\" x=\"45\" y=\"55\" level=\"10\" closed=\"program:resources/icons/misc/Checkmark-green.gif\" thrown=\"program:resources/icons/misc/Checkmark-green.gif\" unknown=\"program:resources/icons/misc/Checkmark-green.gif\" inconsistent=\"program:resources/icons/misc/Checkmark-green.gif\" rotate=\"0\" forcecontroloff=\"false\" class=\"jmri.jmrit.display.configurexml.TurnoutIconXml\" />"+
-"    <sensoricon sensor=\"IS1\" x=\"46\" y=\"53\" level=\"7\" active=\"program:resources/icons/misc/X-red.gif\" inactive=\"program:resources/icons/misc/X-red.gif\" unknown=\"program:resources/icons/misc/X-red.gif\" inconsistent=\"program:resources/icons/misc/X-red.gif\" rotate=\"0\" forcecontroloff=\"false\" momentary=\"false\" class=\"jmri.jmrit.display.configurexml.SensorIconXml\" />"+
-"    <turnouticon turnout=\"IT1\" x=\"136\" y=\"53\" level=\"7\" closed=\"program:resources/icons/misc/X-red.gif\" thrown=\"program:resources/icons/misc/X-red.gif\" unknown=\"program:resources/icons/misc/X-red.gif\" inconsistent=\"program:resources/icons/misc/X-red.gif\" rotate=\"0\" forcecontroloff=\"false\" class=\"jmri.jmrit.display.configurexml.TurnoutIconXml\" />"+
-"    <sensoricon sensor=\"IS1\" x=\"138\" y=\"54\" level=\"10\" active=\"program:resources/icons/misc/Checkmark-green.gif\" inactive=\"program:resources/icons/misc/Checkmark-green.gif\" unknown=\"program:resources/icons/misc/Checkmark-green.gif\" inconsistent=\"program:resources/icons/misc/Checkmark-green.gif\" rotate=\"0\" forcecontroloff=\"false\" momentary=\"false\" class=\"jmri.jmrit.display.configurexml.SensorIconXml\" />"+
-"  </paneleditor>"+
-"</layout-config>";
-
-
 
 	// static private org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TurnoutIconTest.class.getName());
 
