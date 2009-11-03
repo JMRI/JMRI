9e9c46809f83ba5b16f21f58104c0282a69c0e2e
Kevin Dickerson <kev.public@ntlworld.com>
2009-11-03 18:59:58 +0000

    Updated so that it transfers over the icon data to a text type sensor and the text data over to a icon type sensor.  This allows for an undo method, while editing the panel.

--- jmri/jmrit/display/SensorChangeType.java
+++ jmri/jmrit/display/SensorChangeType.java
@@ -3,6 +3,7 @@ package jmri.jmrit.display;
 
 import jmri.Sensor;
 import jmri.jmrit.catalog.NamedIcon;
+import java.awt.Color;
 //import jmri.jmrit.display.LayoutEditor;
 
 public class SensorChangeType {
@@ -11,35 +12,17 @@ public class SensorChangeType {
     
     public SensorChangeType(SensorIcon l, LayoutEditor p){
     
-        //LayoutEditor p = LayoutEditor();
-        SensorIcon nl;
-        int x = l.getX();
-        int y = l.getY();
-        String s = l.getSensor().getSystemName().toString();
-        int level = l.getDisplayLevel();
-        boolean momentary = l.getMomentary();
-        boolean forceControl = l.getForceControlOff();
-        boolean icon = l.isIcon();
+        getOldValues(l);
         
 
         if(!icon) {
-            //Changeto Icon
             nl = new SensorIcon(new NamedIcon("resources/icons/smallschematics/tracksegments/circuit-error.gif", "resources/icons/smallschematics/tracksegments/circuit-error.gif"));
-            nl.setForceControlOff(forceControl);
-            nl.setMomentary(momentary);
-            nl.setSensor(s);
-            nl.setLocation(x,y);
-            nl.setDisplayLevel(level);
+            putValuesIntoNew();
             p.putSensor(nl);
             
         } else {
-            nl = new SensorIcon(new String("  "));
-            // We must be changing to text
-            nl.setForceControlOff(forceControl);
-            nl.setMomentary(momentary);
-            nl.setSensor(s);
-            nl.setLocation(x,y);
-            nl.setDisplayLevel(level);
+            nl = new SensorIcon(new String("   "));
+            putValuesIntoNew();
             p.putSensor(nl);
         }
         
@@ -50,36 +33,18 @@ public class SensorChangeType {
     }
     
     public SensorChangeType(SensorIcon l, PanelEditor p){
-    
+        getOldValues(l);
         //LayoutEditor p = LayoutEditor();
-        SensorIcon nl;
-        int x = l.getX();
-        int y = l.getY();
-        String s = l.getSensor().getSystemName().toString();
-        int level = l.getDisplayLevel();
-        boolean momentary = l.getMomentary();
-        boolean forceControl = l.getForceControlOff();
-        boolean icon = l.isIcon();
-        
 
+        
         if(!icon) {
-            //Changeto Icon
             nl = new SensorIcon(new NamedIcon("resources/icons/smallschematics/tracksegments/circuit-error.gif", "resources/icons/smallschematics/tracksegments/circuit-error.gif"));
-            nl.setForceControlOff(forceControl);
-            nl.setMomentary(momentary);
-            nl.setSensor(s);
-            nl.setLocation(x,y);
-            nl.setDisplayLevel(level);
+            putValuesIntoNew();
             p.putLabel(nl);
             
         } else {
-            nl = new SensorIcon(new String("  "));
-            // We must be changing to text
-            nl.setForceControlOff(forceControl);
-            nl.setMomentary(momentary);
-            nl.setSensor(s);
-            nl.setLocation(x,y);
-            nl.setDisplayLevel(level);
+            nl = new SensorIcon(new String("   "));
+            putValuesIntoNew();
             p.putLabel(nl);
         }
         
@@ -88,7 +53,107 @@ public class SensorChangeType {
         l.dispose();
     
     }
+    
+        SensorIcon nl;
+        int x;
+        int y;
+        String s;
+        int level;
+        boolean momentary;
+        boolean forceControl;
+        boolean icon;
+        NamedIcon active;
+        NamedIcon inconsistent;
+        NamedIcon inactive;
+        NamedIcon unknown;
+        String strActive;
+        String strInactive;
+        String strInconsistent;
+        String strUnknown;
+        String strText;
+        Color clrActiveText;
+        Color clrActiveBack;
+        Color clrInActiveText;
+        Color clrInActiveBack;
+        Color clrUnknownText;
+        Color clrUnknownBack;
+        Color clrInconsistentText;
+        Color clrInconsistentBack;
+        boolean opaque = false;
 
+        
+    void getOldValues(SensorIcon l){
+        x = l.getX();
+        y = l.getY();
+        s = l.getSensor().getSystemName().toString();
+        level = l.getDisplayLevel();
+        opaque = l.isOpaque();
+        momentary = l.getMomentary();
+        forceControl = l.getForceControlOff();
+        icon = l.isIcon();
+        if (l.getOriginalText()==null)
+            strText = l.getText();
+        else
+            strText = l.getOriginalText();
+        active = l.getActiveIcon();
+        inconsistent = l.getInconsistentIcon();
+        inactive = l.getInactiveIcon();
+        unknown = l.getUnknownIcon();
+        strActive = l.getActiveText();
+        strInactive = l.getInactiveText();
+        strInconsistent = l.getInconsistentText();
+        strUnknown = l.getUnknownText();
+        clrActiveText = l.getTextActive();
+        clrActiveBack = l.getBackgroundActive();
+        clrInActiveText = l.getTextInActive();
+        clrInActiveBack  = l.getBackgroundInActive();
+        clrUnknownText = l.getTextUnknown();
+        clrUnknownBack  = l.getBackgroundActive();
+        clrInconsistentText = l.getTextInconsistent();
+        clrInconsistentBack  = l.getBackgroundUnknown();
+    }
+    
+    void putValuesIntoNew(){
+        nl.setForceControlOff(forceControl);
+        nl.setMomentary(momentary);
+        nl.setSensor(s);
+        nl.setLocation(x,y);
+        nl.setDisplayLevel(level);
+        nl.setActiveIcon(active);
+        nl.setInconsistentIcon(inconsistent);
+        nl.setInactiveIcon(inactive);
+        nl.setUnknownIcon(unknown);
+        nl.setOpaque(opaque);
+        //nl.setText(strText);
+        if (strText!=null){
+            nl.setText(strText);
+            nl.setOriginalText(strText);
+        }
+        if (strActive!=null)
+            nl.setActiveText(strActive);
+        if (strInactive!=null)
+            nl.setInactiveText(strInactive);
+        if (strInconsistent!=null)
+            nl.setInconsistentText(strInconsistent);
+        if (strUnknown!=null)
+            nl.setUnknownText(strUnknown);
+        if (clrActiveText!=null)
+            nl.setTextActive(clrActiveText);
+        if (clrActiveBack!=null)
+            nl.setBackgroundActive(clrActiveBack);
+        if (clrInActiveText!=null)
+            nl.setTextInActive(clrInActiveText);
+        if (clrInActiveBack!=null)
+            nl.setBackgroundInActive(clrInActiveBack);
+        if (clrUnknownText!=null)
+            nl.setTextUnknown(clrUnknownText);
+        if (clrUnknownBack!=null)
+            nl.setBackgroundActive(clrUnknownBack);
+        if (clrInconsistentText!=null)
+            nl.setTextInconsistent(clrInconsistentText);
+        if (clrInconsistentBack!=null)
+            nl.setBackgroundUnknown(clrInconsistentBack);
+    }
 
 
 }
