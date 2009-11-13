04b69515983ebb7c9bf9c332db4fe94d0ebbafef
Pete Cressman <pete_cressman@sbcglobal.net>
2009-11-13 02:16:11 +0000

    added needed import

--- test/jmri/jmrit/beantable/LogixTableActionTest.java
+++ test/jmri/jmrit/beantable/LogixTableActionTest.java
@@ -3,7 +3,7 @@
 package jmri.jmrit.beantable;
 
 import junit.framework.*;
-//import junit.extensions.jfcunit.*;
+import junit.extensions.jfcunit.*;
 //import junit.extensions.jfcunit.finder.*;
 
 import java.util.ResourceBundle;
