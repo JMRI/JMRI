97c4e56abda87d18e8e3300fa3a0fd93f980e2b3
Bob Jacobsen <jacobsen@mac.com>
2009-11-14 05:19:41 +0000

    for 2.7.8

--- test/jmri/configurexml/LoadFileTest.java
+++ test/jmri/configurexml/LoadFileTest.java
@@ -18,7 +18,7 @@ import jmri.InstanceManager;
  * 
  * @author Bob Jacobsen Copyright 2009
  * @since 2.5.5
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
 public class LoadFileTest extends TestCase {
 
@@ -56,41 +56,9 @@ public class LoadFileTest extends TestCase {
         
     }
     
-    public void testLoadStoreOne() throws Exception {
+    public void testLoadStoreCurrent() throws Exception {
         // load manager
         java.io.File inFile = new java.io.File("java/test/jmri/configurexml/LoadFileTest.xml");
-        
-        // load file
-        InstanceManager.configureManagerInstance()
-            .load(inFile);
-    
-        // store file
-        XmlFile.ensurePrefsPresent(XmlFile.prefsDir()+"temp");
-        java.io.File outFile = new java.io.File(XmlFile.prefsDir()+"temp/LoadFileTest.xml");
-        InstanceManager.configureManagerInstance()
-            .storeConfig(outFile);
-        
-        // compare files, except for certain special lines
-        BufferedReader inFileStream = new BufferedReader(
-                new InputStreamReader(
-                    new FileInputStream(
-                        new java.io.File("java/test/jmri/configurexml/LoadFileTestRef.xml"))));
-        BufferedReader outFileStream = new BufferedReader(
-                new InputStreamReader(
-                    new FileInputStream(outFile)));
-        String inLine;
-        String outLine;
-        while ( (inLine = inFileStream.readLine())!=null && (outLine = outFileStream.readLine())!=null) {
-            if (!inLine.startsWith("  <!--Written by JMRI version")
-                && !inLine.startsWith("  <timebase")   // time changes from timezone to timezone
-                && !inLine.startsWith("<?xml-stylesheet"))   // Linux seems to put attributes in different order
-                    Assert.assertEquals(inLine, outLine);
-        }
-    }
-        
-    public void testLoadStore277() throws Exception {
-        // load manager
-        java.io.File inFile = new java.io.File("java/test/jmri/configurexml/LoadFileTest277.xml");
         
         // load file
         InstanceManager.configureManagerInstance()
@@ -123,15 +91,15 @@ public class LoadFileTest extends TestCase {
     public void testValidateOne() {
         validate(new java.io.File("java/test/jmri/configurexml/LoadFileTest.xml"));
     }
-    
+
     public void testValidate277() {
         validate(new java.io.File("java/test/jmri/configurexml/LoadFileTest277.xml"));
     }
-    
+
     public void testValidateRef() {
         validate(new java.io.File("java/test/jmri/configurexml/LoadFileTestRef.xml"));
     }
-    
+
     // testing services
     public void validate(File file) {
         boolean original = XmlFile.getVerify();
