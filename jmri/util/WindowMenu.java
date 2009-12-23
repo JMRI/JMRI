cf0984e3060028d369a227a31b0f0ad812c8c508
Bob Jacobsen <jacobsen@mac.com>
2009-12-23 06:39:58 +0000

    don't force initialization to (attempt to) fix problem with openJDK

--- jmri/util/WindowMenu.java
+++ jmri/util/WindowMenu.java
@@ -15,7 +15,7 @@ import javax.swing.event.MenuEvent;
  * and allows to bring one in front
  * <P>
  * @author	Giorgio Terdina   Copyright 2008
- * @version     $Revision: 1.6 $
+ * @version     $Revision: 1.7 $
  * 18-Nov-2008 GT Replaced blank menu lines, due to untitled windows, with "Untitled" string
  */
 
@@ -30,8 +30,6 @@ public class WindowMenu extends JMenu implements javax.swing.event.MenuListener 
         super(java.util.ResourceBundle.getBundle("apps.AppsBundle").getString("MenuWindow"));
 		parentFrame = frame;
 		addMenuListener(this);
-		// make sure selected at least once
-		setSelected(true); setSelected(false);
     }
     
 
