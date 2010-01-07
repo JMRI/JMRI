95f7d521d41a08dc748b5b8a5da9882df3ec4bc1
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2010-01-07 16:47:25 +0000

    Right ")" missing in error messages.  Error status messages in ProgrammingError.properties are not terminated with "; ".  Fix to only require ";" as the termination character.

--- jmri/jmrix/AbstractProgrammer.java
+++ jmri/jmrix/AbstractProgrammer.java
@@ -14,7 +14,7 @@ import java.util.ResourceBundle;
  * Common implementations for the Programmer interface.
  *
  * @author	Bob Jacobsen  Copyright (C) 2001
- * @version     $Revision: 1.18 $
+ * @version     $Revision: 1.19 $
  */
 public abstract class AbstractProgrammer implements Programmer {
 
@@ -23,18 +23,18 @@ public abstract class AbstractProgrammer implements Programmer {
     public String decodeErrorCode(int code) {
         if (code == ProgListener.OK) return rb.getString("OK");
         StringBuffer sbuf = new StringBuffer("");
-        // add each code; terminate each string with "; " please.
-        if ((code & ProgListener.NoLocoDetected) != 0) sbuf.append(rb.getString("NoLocoDetected"));
-        if ((code & ProgListener.ProgrammerBusy) != 0) sbuf.append(rb.getString("ProgrammerBusy"));
-        if ((code & ProgListener.NotImplemented) != 0) sbuf.append(rb.getString("NotImplemented"));
-        if ((code & ProgListener.UserAborted) != 0) sbuf.append(rb.getString("UserAborted"));
-        if ((code & ProgListener.ConfirmFailed) != 0) sbuf.append(rb.getString("ConfirmFailed"));
-        if ((code & ProgListener.FailedTimeout) != 0) sbuf.append(rb.getString("FailedTimeout"));
-        if ((code & ProgListener.UnknownError) != 0) sbuf.append(rb.getString("UnknownError"));
-        if ((code & ProgListener.NoAck) != 0) sbuf.append(rb.getString("NoAck"));
-	    if ((code & ProgListener.ProgrammingShort) != 0) sbuf.append(rb.getString("ProgrammingShort"));
-	    if ((code & ProgListener.SequenceError) != 0) sbuf.append(rb.getString("SequenceError"));
-	    if ((code & ProgListener.CommError) != 0) sbuf.append(rb.getString("CommError"));
+        // add each code; terminate each string with ";" please.
+        if ((code & ProgListener.NoLocoDetected) != 0) sbuf.append(rb.getString("NoLocoDetected")+" ");
+        if ((code & ProgListener.ProgrammerBusy) != 0) sbuf.append(rb.getString("ProgrammerBusy")+" ");
+        if ((code & ProgListener.NotImplemented) != 0) sbuf.append(rb.getString("NotImplemented")+" ");
+        if ((code & ProgListener.UserAborted) != 0) sbuf.append(rb.getString("UserAborted")+" ");
+        if ((code & ProgListener.ConfirmFailed) != 0) sbuf.append(rb.getString("ConfirmFailed")+" ");
+        if ((code & ProgListener.FailedTimeout) != 0) sbuf.append(rb.getString("FailedTimeout")+" ");
+        if ((code & ProgListener.UnknownError) != 0) sbuf.append(rb.getString("UnknownError")+" ");
+        if ((code & ProgListener.NoAck) != 0) sbuf.append(rb.getString("NoAck")+" ");
+	    if ((code & ProgListener.ProgrammingShort) != 0) sbuf.append(rb.getString("ProgrammingShort")+" ");
+	    if ((code & ProgListener.SequenceError) != 0) sbuf.append(rb.getString("SequenceError")+" ");
+	    if ((code & ProgListener.CommError) != 0) sbuf.append(rb.getString("CommError")+" ");
 
         // remove trailing separators
         if (sbuf.length() > 2) sbuf.setLength(sbuf.length()-2);
