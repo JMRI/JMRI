678c06c9149bdc1bdd46c8d8b0f941a6c9242b61
Bob Jacobsen <jacobsen@mac.com>
2010-01-09 03:36:16 +0000

    remove comm port access will simulating

--- jmri/jmrix/cmri/serial/sim/SerialDriverAdapter.java
+++ jmri/jmrix/cmri/serial/sim/SerialDriverAdapter.java
@@ -20,143 +20,19 @@ import gnu.io.SerialPortEventListener;
  * act as simulated connection.
  *
  * @author			Bob Jacobsen   Copyright (C) 2002, 2008
- * @version			$Revision: 1.8 $
+ * @version			$Revision: 1.9 $
  */
 public class SerialDriverAdapter extends jmri.jmrix.cmri.serial.serialdriver.SerialDriverAdapter {
 
     SerialPort activeSerialPort = null;
 
     public String openPort(String portName, String appName)  {
-        try {
-            // get and open the primary port
-            CommPortIdentifier portID = CommPortIdentifier.getPortIdentifier(portName);
-            try {
-                activeSerialPort = (SerialPort) portID.open(appName, 2000);  // name of program, msec to wait
-            }
-            catch (PortInUseException p) {
-                return handlePortBusy(p, portName, log);
-            }
-            // try to set it for CMRI serial
-            try {
-                setSerialPort();
-            } catch (gnu.io.UnsupportedCommOperationException e) {
-                log.error("Cannot set serial parameters on port "+portName+": "+e.getMessage());
-                return "Cannot set serial parameters on port "+portName+": "+e.getMessage();
-            }
+            // don't even try to get port
 
-
-            // set framing (end) character
-            try {
-                activeSerialPort.enableReceiveFraming(0x03);
-                log.debug("Serial framing was observed as: "+activeSerialPort.isReceiveFramingEnabled()
-                      +" "+activeSerialPort.getReceiveFramingByte());
-            } catch (Exception ef) {
-                log.info("failed to set serial framing: "+ef);
-            }
-
-            // set timeout; framing should work before this anyway
-            try {
-                activeSerialPort.enableReceiveTimeout(10);
-                log.debug("Serial timeout was observed as: "+activeSerialPort.getReceiveTimeout()
-                      +" "+activeSerialPort.isReceiveTimeoutEnabled());
-            } catch (Exception et) {
-                log.info("failed to set serial timeout: "+et);
-            }
-            
             // get and save stream
-            serialStream = activeSerialPort.getInputStream();
-
-            // purge contents, if any
-            int count = serialStream.available();
-            log.debug("input stream shows "+count+" bytes available");
-            while ( count > 0) {
-                serialStream.skip(count);
-                count = serialStream.available();
-            }
-
-            // report status?
-            if (log.isInfoEnabled()) {
-                // report now
-                log.info(portName+" port opened at "
-                         +activeSerialPort.getBaudRate()+" baud with"
-                         +" DTR: "+activeSerialPort.isDTR()
-                         +" RTS: "+activeSerialPort.isRTS()
-                         +" DSR: "+activeSerialPort.isDSR()
-                         +" CTS: "+activeSerialPort.isCTS()
-                         +"  CD: "+activeSerialPort.isCD()
-                         );
-            }
-            if (log.isDebugEnabled()) {
-                // report additional status
-                log.debug(" port flow control shows "+
-                          (activeSerialPort.getFlowControlMode()==SerialPort.FLOWCONTROL_RTSCTS_OUT?"hardware flow control":"no flow control"));
-            }
-            if (log.isDebugEnabled()) {
-                // arrange to notify later
-                activeSerialPort.addEventListener(new SerialPortEventListener(){
-                        public void serialEvent(SerialPortEvent e) {
-                            int type = e.getEventType();
-                            switch (type) {
-                            case SerialPortEvent.DATA_AVAILABLE:
-                                log.info("SerialEvent: DATA_AVAILABLE is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.OUTPUT_BUFFER_EMPTY:
-                                log.info("SerialEvent: OUTPUT_BUFFER_EMPTY is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.CTS:
-                                log.info("SerialEvent: CTS is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.DSR:
-                                log.info("SerialEvent: DSR is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.RI:
-                                log.info("SerialEvent: RI is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.CD:
-                                log.info("SerialEvent: CD is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.OE:
-                                log.info("SerialEvent: OE (overrun error) is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.PE:
-                                log.info("SerialEvent: PE (parity error) is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.FE:
-                                log.info("SerialEvent: FE (framing error) is "+e.getNewValue());
-                                return;
-                            case SerialPortEvent.BI:
-                                log.info("SerialEvent: BI (break interrupt) is "+e.getNewValue());
-                                return;
-                            default:
-                                log.info("SerialEvent of unknown type: "+type+" value: "+e.getNewValue());
-                                return;
-                            }
-                        }
-                    }
-                                                  );
-                try { activeSerialPort.notifyOnFramingError(true); }
-                catch (Exception e) { log.debug("Could not notifyOnFramingError: "+e); }
-
-                try { activeSerialPort.notifyOnBreakInterrupt(true); }
-                catch (Exception e) { log.debug("Could not notifyOnBreakInterrupt: "+e); }
-
-                try { activeSerialPort.notifyOnParityError(true); }
-                catch (Exception e) { log.debug("Could not notifyOnParityError: "+e); }
-
-                try { activeSerialPort.notifyOnOverrunError(true); }
-                catch (Exception e) { log.debug("Could not notifyOnOverrunError: "+e); }
-
-            }
+            serialStream = null; //activeSerialPort.getInputStream();
 
             opened = true;
-
-        } catch (gnu.io.NoSuchPortException p) {
-            return handlePortNotFound(p, portName, log);
-        } catch (Exception ex) {
-            log.error("Unexpected exception while opening port "+portName+" trace follows: "+ex);
-            ex.printStackTrace();
-            return "Unexpected error while opening port "+portName+": "+ex;
-        }
 
         return null; // normal operation
     }
@@ -195,22 +71,17 @@ public class SerialDriverAdapter extends jmri.jmrix.cmri.serial.serialdriver.Ser
 
     // base class methods for the SerialPortController interface
     public DataInputStream getInputStream() {
-        if (!opened) {
-            log.error("getInputStream called before load(), stream not available");
-            return null;
-        }
-        return new DataInputStream(serialStream);
+        try {
+            return new DataInputStream(new java.io.PipedInputStream(new java.io.PipedOutputStream()));
+        } catch (Exception e ) { return null; }
+        //return new DataInputStream(serialStream);
     }
 
     public DataOutputStream getOutputStream() {
-        if (!opened) log.error("getOutputStream called before load(), stream not available");
-        try {
-            return new DataOutputStream(activeSerialPort.getOutputStream());
-        }
-     	catch (java.io.IOException e) {
-            log.error("getOutputStream exception: "+e.getMessage());
-     	}
-     	return null;
+        return new DataOutputStream(new java.io.OutputStream() {
+            public void write(int b) throws java.io.IOException {
+            }
+        });
     }
 
     public boolean status() {return opened;}
@@ -219,21 +90,6 @@ public class SerialDriverAdapter extends jmri.jmrix.cmri.serial.serialdriver.Ser
      * Local method to do specific port configuration
      */
     protected void setSerialPort() throws gnu.io.UnsupportedCommOperationException {
-        // find the baud rate value, configure comm options
-        int baud = 19200;  // default, but also defaulted in the initial value of selectedSpeed
-        for (int i = 0; i<validSpeeds.length; i++ )
-            if (validSpeeds[i].equals(selectedSpeed))
-                baud = validSpeedValues[i];
-        activeSerialPort.setSerialPortParams(baud, SerialPort.DATABITS_8,
-                                SerialPort.STOPBITS_2, SerialPort.PARITY_NONE);
-
-        // set RTS high, DTR high - done early, so flow control can be configured after
-        activeSerialPort.setRTS(true);		// not connected in some serial ports and adapters
-        activeSerialPort.setDTR(true);		// pin 1 in DIN8; on main connector, this is DTR
-
-        // find and configure flow control
-        int flow = SerialPort.FLOWCONTROL_NONE; // default
-        activeSerialPort.setFlowControlMode(flow);
     }
 
     /**
