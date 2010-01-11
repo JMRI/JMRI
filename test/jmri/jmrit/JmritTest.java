bc7c5464ee3a00713e3288331f493edfe3e38c92
Bob Jacobsen <jacobsen@mac.com>
2010-01-11 00:59:58 +0000

    test revhistory

--- test/jmri/jmrit/JmritTest.java
+++ test/jmri/jmrit/JmritTest.java
@@ -8,7 +8,7 @@ import junit.framework.*;
  * Invokes complete set of tests in the jmri.jmrit tree
  *
  * @author	    Bob Jacobsen  Copyright 2001, 2003
- * @version         $Revision: 1.21 $
+ * @version         $Revision: 1.22 $
  */
 public class JmritTest extends TestCase {
 
@@ -39,6 +39,7 @@ public class JmritTest extends TestCase {
         suite.addTest(jmri.jmrit.jython.JythonTest.suite());
         suite.addTest(jmri.jmrit.operations.OperationsTest.suite());
         suite.addTest(jmri.jmrit.powerpanel.PowerPanelTest.suite());
+        suite.addTest(jmri.jmrit.revhistory.RevHistoryTest.suite());
         suite.addTest(jmri.jmrit.roster.RosterTest.suite());
         suite.addTest(jmri.jmrit.sendpacket.SendPacketTest.suite());
         suite.addTest(jmri.jmrit.sensorgroup.SensorGroupTest.suite());
