c602d0b52f03f70b84257251efb1fa25f5b0a7c7
Bob Jacobsen <jacobsen@mac.com>
2010-02-02 20:43:41 +0000

    put back two forwarding classes for config file use

---
+++ jmri/jmrit/display/LayoutEditorAction.java
@@ -0,0 +1,16 @@
+package jmri.jmrit.display;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+/**
+ * Dummy class so exiting config files continue to load
+ *
+ * @author	Bob Jacobsen   Copyright (C) 2008, 2010
+ * @version	$Revision: 1.5 $
+ * @see         jmri.jmrit.display.layoutEditor.LayoutEditorAction
+ */
+public class LayoutEditorAction extends jmri.jmrit.display.layoutEditor.LayoutEditorAction {
+
+}
---
+++ jmri/jmrit/display/PanelEditorAction.java
@@ -0,0 +1,19 @@
+package jmri.jmrit.display;
+
+import jmri.util.JmriJFrame;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+/**
+ * Dummy class so exiting config files continue to load
+ *
+ * @author	Bob Jacobsen   Copyright (C) 2002, 2010
+ * @version	$Revision: 1.19 $
+ * @see  jmri.jmrit.display.panelEditor.PanelEditorAction
+ * @deprecated
+ */
+@Deprecated
+public class PanelEditorAction extends jmri.jmrit.display.panelEditor.PanelEditorAction {
+
+}
