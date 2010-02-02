619e5da1d40ff4d9406d9205467a2d207ae3f3f5
Pete Cressman <pete_cressman@sbcglobal.net>
2010-02-02 21:34:25 +0000

    Adapter needs this class

---
+++ jmri/jmrit/display/layoutEditor/configurexml/MemoryIconXml.java
@@ -0,0 +1,11 @@
+package jmri.jmrit.display.layoutEditor.configurexml;
+
+/**
+ * Handle configuration for display.layoutEditor.MemoryIcon objects.
+ * Adapter needs to find a class here
+ *
+ * @author Pete Cressman Copyright: Copyright (c) 2010
+ */
+public class MemoryIconXml extends jmri.jmrit.display.configurexml.MemoryIconXml {
+    public MemoryIconXml() {}
+}
