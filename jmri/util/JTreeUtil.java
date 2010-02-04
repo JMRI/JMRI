5a5fc12a33d5123ae848d6f2a1b6fa3ed13fb058
Bob Jacobsen <jacobsen@mac.com>
2010-02-04 03:28:26 +0000

    new util for building JTrees from config file

--- jmri/util/JTreeUtil.java
+++ jmri/util/JTreeUtil.java
@@ -3,31 +3,45 @@
 package jmri.util;
 
 import javax.swing.JTree;
+import javax.swing.tree.*;
+import java.io.File;
+import org.jdom.*;
 
 /**
  * Common utility methods for working with JTrees.
  * <P>
- * We needed a place to refactor common JTree-processing idioms in JMRI
- * code, so this class was created. It's more of a library of procedures
- * than a real class, as (so far) all of the operations have needed no state
- * information.
- * <P>
- * In particular, this is intended to provide Java 2 functionality on a
- * Java 1.1.8 system, or at least try to fake it.
+ * Chief among these is the loadTree method, for
+ * creating a tree from an XML definition
  *
- * @author Bob Jacobsen  Copyright 2003
- * @version $Revision: 1.3 $
+ * @author Bob Jacobsen  Copyright 2003, 2010
+ * @version $Revision: 1.4 $
  */
 
 public class JTreeUtil {
 
-    static public void setExpandsSelectedPaths(JTree dTree, boolean value) {
-        try {   // following might not be present on Mac Classic, but
-                //doesn't have a big effect
-            dTree.setExpandsSelectedPaths(value);
-        } catch (Throwable e) { // NoSuchMethodError, NoClassDefFoundError and others on early JVMs
-            // nothing to do here
+    static public TreeNode loadTree(String filename) {
+        Element root;
+        
+        try {
+            root = new jmri.jmrit.XmlFile(){}.rootFromName(filename);
+        } catch (Exception e) {
+            log.error("Could not parse JTree file \""+filename+"\" due to: "+e);
+            return null;
         }
+        return treeFromElement(root);
     }
-
+    
+    static DefaultMutableTreeNode treeFromElement(Element main) {
+        String name = "<none>";
+        Element e = main.getChild("name");
+        if (e != null) name = e.getText();
+        DefaultMutableTreeNode node = new DefaultMutableTreeNode(name);
+        
+        for (Object child : main.getChildren("node")) {
+            node.add(treeFromElement((Element)child));
+        }
+        return node;
+    }
+    
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(JTreeUtil.class.getName());
 }
