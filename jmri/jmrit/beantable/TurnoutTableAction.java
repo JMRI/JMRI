c4ee43d7a8089239c96d115c6bfbee10882fb6b7
Paul Bender <paul.bender@acm.org>
2010-03-09 01:56:48 +0000

    When creating an editor for a TurnoutOperation, log an error if the dialog cannot be created.

--- jmri/jmrit/beantable/TurnoutTableAction.java
+++ jmri/jmrit/beantable/TurnoutTableAction.java
@@ -42,7 +42,7 @@ import jmri.util.JmriJFrame;
  * TurnoutTable GUI.
  *
  * @author	Bob Jacobsen    Copyright (C) 2003, 2004, 2007
- * @version     $Revision: 1.76 $
+ * @version     $Revision: 1.77 $
  */
 
 public class TurnoutTableAction extends AbstractTableAction {
@@ -559,7 +559,9 @@ public class TurnoutTableAction extends AbstractTableAction {
     	}
     	if (op != null) {
             if (!op.isNonce()) op = op.makeNonce(t);
-    	    new TurnoutOperationEditor(this, f, op, t, box);
+            TurnoutOperationEditor dialog = new TurnoutOperationEditor(this, f, op, t, box);
+            if(dialog == null)
+               log.error("Error Creating Turnout Operation Editor Dialog.");
     	} else {
             JOptionPane.showMessageDialog(f, new String("There is no operation type suitable for this turnout"),
                                           "No operation type", JOptionPane.ERROR_MESSAGE);
