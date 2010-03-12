c701b39ca8de278a47b2e4b650f6ff7d8430d6e6
Bob Jacobsen <jacobsen@mac.com>
2010-03-12 04:24:44 +0000

    update to new Editor class structure

--- scripts/jython/ControlPanel.py
+++ scripts/jython/ControlPanel.py
@@ -11,7 +11,7 @@
 # Part of the JMRI distribution
 #
 # The next line is maintained by CVS, please don't change it
-# $Revision: 1.2 $
+# $Revision: 1.3 $
 
 import java
 import javax.swing
@@ -24,34 +24,12 @@ f.getContentPane().setLayout(java.awt.FlowLayout())
 def whenMyButtonClicked(event) :
         name  = event.getActionCommand()
         # find any PanelEditor panel(s) to show
-        i = 1
-        panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-            java.lang.Class.forName("jmri.jmrit.display.PanelEditor"),
-            i)
-        while (panel != None) :
-            if (name == panel.getFrame().getTitle()) : 
-                panel.getFrame().setVisible(not panel.getFrame().isVisible())
-                return            
-            # loop again
-            i = i + 1
-            panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-                java.lang.Class.forName("jmri.jmrit.display.PanelEditor"),
-                i)
-        # find any LayoutEditor panel(s) to show
-        i = 1
-        panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-            java.lang.Class.forName("jmri.jmrit.display.LayoutEditor"),
-            i)
-        while (panel != None) :
+        for panel in jmri.jmrit.display.PanelMenu.instance().getEditorPanelList() :
             if (name == panel.getTitle()) : 
                 panel.setVisible(not panel.isVisible())
                 return            
-            # loop again
-            i = i + 1
-            panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-                java.lang.Class.forName("jmri.jmrit.display.LayoutEditor"),
-                i)
-
+        return
+        
 # Now loop to create buttons.
 #
 # The "action" in each button is the name of the panel.
@@ -60,43 +38,12 @@ def whenMyButtonClicked(event) :
 # window produced from the file, so we can show/hide the 
 # proper panel window.
 
-# first, panel editor panels
-i = 1
-panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-    java.lang.Class.forName("jmri.jmrit.display.PanelEditor"),
-    i)
-
 #loop, creating a button for each panel
-while (panel != None) :
-    # create a button for this panel
-    b = javax.swing.JButton(panel.getFrame().getTitle())
-    b.actionPerformed = whenMyButtonClicked
-    f.contentPane.add(b)
-    
-    # loop again
-    i = i + 1
-    panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-        java.lang.Class.forName("jmri.jmrit.display.PanelEditor"),
-        i)
-    
-# second, layout editor panels
-i = 1
-panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-    java.lang.Class.forName("jmri.jmrit.display.LayoutEditor"),
-    i)
-
-#loop, creating a button for each panel
-while (panel != None) :
+for panel in jmri.jmrit.display.PanelMenu.instance().getEditorPanelList() :
     # create a button for this panel
     b = javax.swing.JButton(panel.getTitle())
     b.actionPerformed = whenMyButtonClicked
     f.contentPane.add(b)
-    
-    # loop again
-    i = i + 1
-    panel = jmri.InstanceManager.configureManagerInstance().findInstance(
-        java.lang.Class.forName("jmri.jmrit.display.LayoutEditor"),
-        i)
     
 
 # show the control panel frame
