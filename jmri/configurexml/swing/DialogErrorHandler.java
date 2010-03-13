41f885965a752b209a6c7ee7038b5d90e6d5d899
Bob Jacobsen <jacobsen@mac.com>
2010-03-13 20:43:00 +0000

    cant use HTML in dialog boxes generated here

--- jmri/configurexml/swing/DialogErrorHandler.java
+++ jmri/configurexml/swing/DialogErrorHandler.java
@@ -5,7 +5,7 @@ package jmri.configurexml.swing;
  * Shows each one, could save until end if needed.
  *
  * @author Bob Jacobsen  Copyright (c) 2010
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
     
 public class DialogErrorHandler extends jmri.configurexml.ErrorHandler {
@@ -18,13 +18,13 @@ public class DialogErrorHandler extends jmri.configurexml.ErrorHandler {
         super.handle(e);
 
         // then do dialog
-        String m = "<html>"+e.description;
-        if (e.systemName!=null) m += "<br> System name \""+e.systemName+"\"";
+        String m = e.description;
+        if (e.systemName!=null) m += " System name \""+e.systemName+"\"";
         if (e.userName!=null && !e.userName.equals("")) m += "<br> User name \""+e.userName+"\"";
-        if (e.operation!=null) m += "<br> while "+e.operation;
-        if (e.adapter!=null) m += "<br> in adaptor of type "+e.adapter.getClass().getName();
-        if (e.exception!=null) m += "<br> Exception: "+e.exception.toString();
-        m +="</html>";
+        if (e.operation!=null) m += " while "+e.operation;
+        if (e.adapter!=null) m += " in adaptor of type "+e.adapter.getClass().getName();
+        if (e.exception!=null) m += " Exception: "+e.exception.toString();
+
         if (e.level == org.apache.log4j.Level.ERROR) {
             jmri.InstanceManager.getDefault(jmri.UserPreferencesManager.class).
                             showInfoMessage("Error during loading",m,e.description,true, false, e.level);
