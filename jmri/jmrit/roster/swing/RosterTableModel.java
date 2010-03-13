bed83f0b9ae97c5a0fb4c14ee614b3043b00d1bf
Bob Jacobsen <jacobsen@mac.com>
2010-03-13 04:28:18 +0000

    add image column

--- jmri/jmrit/roster/swing/RosterTableModel.java
+++ jmri/jmrit/roster/swing/RosterTableModel.java
@@ -3,6 +3,7 @@
 package jmri.jmrit.roster.swing;
 
 import jmri.jmrit.roster.*;
+import javax.swing.ImageIcon;
 
 /**
  * Table data model for display of Roster variable values.
@@ -14,21 +15,22 @@ import jmri.jmrit.roster.*;
  * and only shows some of the fields.  But it's a start....
  *
  * @author              Bob Jacobsen   Copyright (C) 2009, 2010
- * @version             $Revision: 1.4 $
+ * @version             $Revision: 1.5 $
  * @since 2.7.5
  */
 public class RosterTableModel extends javax.swing.table.AbstractTableModel {
 
     static final int IDCOL = 0;
     static final int ADDRESSCOL = 1;
-    static final int DECODERCOL = 2;
-    static final int ROADNAMECOL = 3;
-    static final int ROADNUMBERCOL = 4;
-    static final int MFGCOL = 5;
-    static final int OWNERCOL = 6;
-    static final int DATEUPDATECOL = 7;
+    static final int ICONCOL = 2;
+    static final int DECODERCOL = 3;
+    static final int ROADNAMECOL = 4;
+    static final int ROADNUMBERCOL = 5;
+    static final int MFGCOL = 6;
+    static final int OWNERCOL = 7;
+    static final int DATEUPDATECOL = 8;
 
-    static final int NUMCOL = 7+1;
+    static final int NUMCOL = 8+1;
     
     public int getRowCount() {
         return Roster.instance().numEntries();
@@ -44,6 +46,7 @@ public class RosterTableModel extends javax.swing.table.AbstractTableModel {
         case ROADNAMECOL:   return "Road Name";
         case ROADNUMBERCOL: return "Road Number";
         case MFGCOL:        return "Manufacturer";
+        case ICONCOL:       return "Icon";
         case OWNERCOL:      return "Owner";
         case DATEUPDATECOL: return "Last Updated";
         default:            return "<UNKNOWN>";
@@ -52,6 +55,7 @@ public class RosterTableModel extends javax.swing.table.AbstractTableModel {
     
     public Class<?> getColumnClass(int col) {
         if (col == ADDRESSCOL) return Integer.class;
+        if (col == ICONCOL) return ImageIcon.class;
         return String.class;
     }
     
@@ -79,6 +83,7 @@ public class RosterTableModel extends javax.swing.table.AbstractTableModel {
         case ROADNAMECOL:   return re.getRoadName();
         case ROADNUMBERCOL: return re.getRoadNumber();
         case MFGCOL:        return re.getMfg();
+        case ICONCOL:       return new ImageIcon(re.getIconPath());
         case OWNERCOL:      return re.getOwner();
         case DATEUPDATECOL: return re.getDateUpdated();
         default:            return "<UNKNOWN>";
@@ -91,9 +96,14 @@ public class RosterTableModel extends javax.swing.table.AbstractTableModel {
     public int getPreferredWidth(int column) {
         int retval = 20; // always take some width
         retval = Math.max(retval, new javax.swing.JLabel(getColumnName(column)).getPreferredSize().width+15);  // leave room for sorter arrow
-        for (int row = 0 ; row < getRowCount(); row++)
-            retval = Math.max(retval, new javax.swing.JLabel(getValueAt(row, column).toString()).getPreferredSize().width);
-            
+        for (int row = 0 ; row < getRowCount(); row++) {
+            if (getColumnClass(column).equals(String.class))
+                retval = Math.max(retval, new javax.swing.JLabel(getValueAt(row, column).toString()).getPreferredSize().width);
+            else if (getColumnClass(column).equals(Integer.class))
+                retval = Math.max(retval, new javax.swing.JLabel(getValueAt(row, column).toString()).getPreferredSize().width);
+            else if (getColumnClass(column).equals(ImageIcon.class))
+                retval = Math.max(retval, new javax.swing.JLabel((ImageIcon)getValueAt(row, column)).getPreferredSize().width);
+        }    
         return retval+5;
     }
     
