a0523758906af0444a1a8a9095c57450ab30880d
Bob Jacobsen <jacobsen@mac.com>
2010-03-13 04:30:12 +0000

    more dummy demo

--- apps/gui3/paned/DecoderProAction.java
+++ apps/gui3/paned/DecoderProAction.java
@@ -3,10 +3,18 @@
  package apps.gui3.paned;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
 
 import jmri.util.swing.*;
+
+// for ugly code
+import jmri.progdebugger.*;
+import jmri.jmrit.symbolicprog.*;
+import jmri.jmrit.symbolicprog.tabbedframe.*;
+import jmri.jmrit.roster.*;
+import org.jdom.*;
 
 /**
  * Action to produce a new, standalone DecoderPro window.
@@ -14,7 +22,7 @@ import jmri.util.swing.*;
  * Ignores WindowInterface.
  *
  * @author		Bob Jacobsen Copyright (C) 2010
- * @version		$Revision: 1.5 $
+ * @version		$Revision: 1.6 $
  */
  
 public class DecoderProAction extends jmri.util.swing.JmriAbstractAction {
@@ -36,7 +44,7 @@ public class DecoderProAction extends jmri.util.swing.JmriAbstractAction {
         jmri.util.swing.multipane.ThreePaneTLRWindow mainFrame 
             = new jmri.util.swing.multipane.ThreePaneTLRWindow("DecoderPro", 
     	        new File("xml/config/apps/decoderpro/Gui3Menus.xml"), 
-    	        new File("xml/config/apps/decoderpro/Gui3MainToolBar.xml"));
+    	        null);  // no toolbar
     	        
     	mainFrame.getTop().add(createTop());
     	mainFrame.getLeft().add(createLeft());
@@ -79,25 +87,18 @@ public class DecoderProAction extends jmri.util.swing.JmriAbstractAction {
         JPanel retval = new JPanel();
         retval.setLayout(new BoxLayout(retval, BoxLayout.Y_AXIS));
 
-        JLabel l = new JLabel("Display of a particular pane will go here");
-        l.setPreferredSize(new java.awt.Dimension(100, 200));
+        JComponent l = createPane(); // new JLabel("Display of a particular pane will go here");
+        //l.setPreferredSize(new java.awt.Dimension(100, 200));
         retval.add(l);
 
         retval.add(Box.createVerticalGlue());
         retval.add(new JSeparator());
 
-        JPanel p = new JPanel();
-        p.setLayout(new BoxLayout(p, BoxLayout.X_AXIS));
-        p.add(new JButton("Read These"));
-        p.add(new JButton("Read These Changes"));
-        p.add(new JButton("Write These"));
-        p.add(new JButton("Write These Changes"));
-        retval.add(p);
-        
-        // toolbar too
+        // toolbar alternate for programming
         JToolBar bar = JToolBarUtil.loadToolBar(
             new java.io.File("xml/config/apps/decoderpro/ProgramingButtons.xml"),
             wi, null);
+        bar.setOrientation(JToolBar.HORIZONTAL);
         retval.add(bar);
         
         return retval;
@@ -106,17 +107,22 @@ public class DecoderProAction extends jmri.util.swing.JmriAbstractAction {
     JComponent createLeft() {
         JPanel retval = new JPanel();
         retval.setLayout(new BoxLayout(retval, BoxLayout.Y_AXIS));
+        float defaultXAlignment = 0.f;
         
-        retval.add(new JComboBox(new String[]{
-            "Read All",
-            "Read All Changes",
-            "Write All",
-            "Write All Changes"
-        }));
+        JToolBar bar = JToolBarUtil.loadToolBar(
+            new java.io.File("xml/config/apps/decoderpro/GlobalProgButtons.xml"),
+            wi, null);
+        bar.setOrientation(JToolBar.VERTICAL);
+        bar.setAlignmentX(defaultXAlignment);
+        retval.add(bar);
         
+        retval.add(Box.createRigidArea(new Dimension(10,10)));
         retval.add(new JSeparator());
         
-        retval.add(new JList(new String[]{
+        bar = new JToolBar("Panes");
+        bar.setOrientation(JToolBar.VERTICAL);
+        bar.setAlignmentX(defaultXAlignment);
+        bar.add(new JList(new String[]{
                 "Roster", 
                 "Function Keys", 
                 "Images", 
@@ -128,16 +134,132 @@ public class DecoderProAction extends jmri.util.swing.JmriAbstractAction {
                 "Lighting", 
                 "CVs" 
             }));
-
+        retval.add(bar);
+        
         retval.add(new JSeparator());
         
-        retval.add(new JList(new String[]{
-                "Reset All", 
-                "Reset Except Speed Table"
-            }));
+        bar = new JToolBar("Resets");
+        bar.setOrientation(JToolBar.VERTICAL);
+        bar.setAlignmentX(defaultXAlignment);
+        bar.add(new JButton("Reset All")); 
+        bar.add(new JButton("Reset Except Speed Table"));
+        retval.add(bar);
+        
+        //retval.add(new JList(new String[]{
+        //        "Reset All", 
+        //        "Reset Except Speed Table"
+        //    }));
         return retval;
     }
     
+    // amazingly ugly temp pane code
+    static String result = null;
+    static int colCount = -1;
+    static int varCount = -1;
+    ProgDebugger p = new ProgDebugger();
+    JComponent createPane() {
+        // create a JDOM tree with just some elements
+        Element root = null;
+        Element pane1 = null;
+        Element pane2 = null;
+        Element pane3 = null;
+        Document doc = null;
+
+        root = new Element("programmer-config");
+        doc = new Document(root);
+        doc.setDocType(new DocType("programmer-config","programmer-config.dtd"));
+
+        // add some elements
+        root.addContent(new Element("programmer")
+            .addContent(pane1 = new Element("pane")
+                .setAttribute("name","Basic")
+                .addContent(new Element("column")
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Primary Address")
+                        )
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Start voltage")
+                        )
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Normal direction of motion")
+                        )
+                    )
+                .addContent(new Element("column")
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Address")
+                        )
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Normal direction of motion")
+                        )
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Normal direction of motion")
+                        .setAttribute("format","checkbox")
+                        )
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Normal direction of motion")
+                        .setAttribute("format","radiobuttons")
+                        )
+                    )
+                )
+            .addContent(pane2 = new Element("pane")
+                .setAttribute("name", "CV")
+                .addContent(new Element("column")
+                    .addContent(new Element("cvtable"))
+                    )
+                )
+            .addContent(pane3 = new Element("pane")
+                .setAttribute("name", "Other")
+                .addContent(new Element("column")
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Address")
+                        )
+                    .addContent(new Element("display")
+                        .setAttribute("item", "Normal direction of motion")
+                        )
+                    )
+                )
+            )
+            ; // end of adding contents
+
+        PaneProgFrame pFrame = new PaneProgFrame(null, new RosterEntry(),
+                                                 "test frame", "programmers/Basic.xml",
+                                                 p, false) {
+            // dummy implementations
+            protected JPanel getModePane() { return null; }
+        };
+        CvTableModel cvModel = new CvTableModel(new JLabel(), p);
+        IndexedCvTableModel icvModel = new IndexedCvTableModel(new JLabel(), p);
+
+        String[] args = {"CV", "Name"};
+        VariableTableModel varModel = new VariableTableModel(null, args, cvModel, icvModel);
+        varModel.setRow(0,new Element("variable")
+                .setAttribute("label", "Primary Address")
+                .setAttribute("CV", "1")
+                .setAttribute("item", "Short Address")
+                .addContent(new Element("shortAddressVal"))            
+        );
+        varModel.setRow(1,new Element("variable")
+                .setAttribute("label", "Start Voltage")
+                .setAttribute("CV", "2")
+                .addContent(new Element("decVal"))            
+        );
+        varModel.setRow(0,new Element("variable")
+                .setAttribute("label", "Normal direction of motion")
+                .setAttribute("CV", "29")
+                .addContent(new Element("enumVal")
+                    .addContent(new Element("enumChoice").setAttribute("choice", "fwd"))
+                    .addContent(new Element("enumChoice").setAttribute("choice", "rev"))
+                )            
+        );
+        
+        // create test object with special implementation of the newColumn(String) operation
+        colCount = 0;
+        PaneProgPane retval = new PaneProgPane(pFrame, "name", pane1, cvModel, icvModel, varModel, null) {
+            };
+            
+        return retval;
+    }
+
     // never invoked, because we overrode actionPerformed above
     public void dispose() {
         throw new IllegalArgumentException("Should not be invoked");
