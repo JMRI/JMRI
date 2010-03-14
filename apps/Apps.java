bb32442dc44e751f2e170104654b0fada54398de
Bob Jacobsen <jacobsen@mac.com>
2010-03-14 21:39:13 +0000

    better reporting of errors during shutdown tasks

--- apps/Apps.java
+++ apps/Apps.java
@@ -37,7 +37,7 @@ import net.roydesign.mac.MRJAdapter;
  * @author	Bob Jacobsen   Copyright 2003, 2007, 2008, 2010
  * @author  Dennis Miller  Copyright 2005
  * @author Giorgio Terdina Copyright 2008
- * @version     $Revision: 1.112 $
+ * @version     $Revision: 1.113 $
  */
 public class Apps extends JPanel implements PropertyChangeListener, java.awt.event.WindowListener {
 
@@ -591,7 +591,7 @@ public class Apps extends JPanel implements PropertyChangeListener, java.awt.eve
         try {
             InstanceManager.shutDownManagerInstance().shutdown();
         } catch (Exception e) {
-            log.error("Continuing after error in handleQuit: "+e);
+            log.error("Continuing after error in handleQuit",e);
         }
     }
     
