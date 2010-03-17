8628ee17fe06d12516e36a2c1e6547f73069b083
Kevin Dickerson <kev.public@ntlworld.com>
2010-03-17 19:09:38 +0000

    Changes made for the new tab connection procedures and dealing with system connection memo New options for adding a remove connection tabs, along with procedures for dealing with system connection memo

--- apps/gui3/TabbedPreferences.java
+++ apps/gui3/TabbedPreferences.java
@@ -4,10 +4,13 @@ package apps.gui3;
 
 import apps.AppConfigBase;
 import jmri.jmrix.JmrixConfigPane;
+//import jmri.util.swing.ButtonTabComponent;
+import javax.swing.plaf.basic.BasicButtonUI;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ChangeEvent;
+import java.util.ArrayList;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -17,11 +20,11 @@ import javax.swing.*;
  * tabbed pane
  * <P>
  * @author	Bob Jacobsen   Copyright 2010
- * @version $Revision: 1.16 $
+ * @version $Revision: 1.17 $
  */
 public class TabbedPreferences extends AppConfigBase {
     
-    public String getHelpTarget() { return "package.apps.TabbedPreferences"; }
+    public String getHelpTarget() { return "package.apps.AppConfigPanel"; }
     public String getTitle() { return rb.getString("TitlePreferences"); }
 
     String choices[] = {"Connections", "Start Up", "Display", "Messages", "Roster"/*, "Throttle"*/};
@@ -31,7 +34,10 @@ public class TabbedPreferences extends AppConfigBase {
     // class! How about switching AppConfigPanel to tabbed?
 
     JPanel detailpanel = new JPanel();
-    JTabbedPane connectionPanel = new JTabbedPane();
+    final JTabbedPane connectionPanel = new JTabbedPane();
+    
+    ArrayList<Integer> connectionTabInstance = new ArrayList<Integer>();
+    
 
     public TabbedPreferences() {
         super();
@@ -64,35 +70,54 @@ public class TabbedPreferences extends AppConfigBase {
         buttonpanel.add(save);
         
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-
+        ArrayList<Object> connList=null;
         int count = 0;
-        while (null != jmri.InstanceManager.configureManagerInstance()
-                                 .findInstance(jmri.jmrix.ConnectionConfig.class, count)){
-            addItem(connectionPanel, rb.getString("TabbedLayoutConnection")+(count+1), null, JmrixConfigPane.instance(count), true, JmrixConfigPane.instance(count).getCurrentProtocolName());
-            count++;
-        }
-        if (count == 0){
-            while (null != jmri.InstanceManager.configureManagerInstance()
-                                 .findInstance(jmri.jmrix.JmrixConfigPane.class, count)){
-            addItem(connectionPanel, rb.getString("TabbedLayoutConnection")+(count+1), null, JmrixConfigPane.instance(count), true, JmrixConfigPane.instance(count).getCurrentProtocolName());
-            count++;
+        
+        connList = jmri.InstanceManager.configureManagerInstance().getInstanceList(jmri.jmrix.JmrixConfigPane.class);
+        if(connList!=null){
+            for (int x = 0; x<connList.size(); x++){
+                int newinstance = jmri.jmrix.JmrixConfigPane.getInstanceNumber((jmri.jmrix.JmrixConfigPane)connList.get(x));
+                /*This extra check is here a some of the original code automatically created four connection instances
+                therefore we need to filter out those that are set to none.*/
+                if(!((jmri.jmrix.JmrixConfigPane)connList.get(x)).getCurrentProtocolName().equals("(none)")){
+                    addConnection(count, newinstance);
+                    count++;
+                }
+            }
+        
+        } else {
+            connList = jmri.InstanceManager.configureManagerInstance().getInstanceList(jmri.jmrix.ConnectionConfig.class);
+            if (connList!=null){
+                for (int x = 0; x<connList.size(); x++){
+                    /*This extra check is here a some of the original code automatically created four connection instances
+                    therefore we need to filter out those that are set to none.*/
+                    if(!(jmri.jmrix.JmrixConfigPane.instance(x).getCurrentProtocolName().equals("(none)"))){
+                        addConnection(count, x);
+                        count++;
+                    }
+                }
             }
         }
-        if (count == 0) {
-            addItem(connectionPanel, rb.getString("TabbedLayoutConnection"), null, JmrixConfigPane.instance(0), true, null);
-        }
-        addItem(connectionPanel, "+", null, newConnectionTab(), false, "Add New Connection");
+        
+        if (count==0)
+            addConnection(0,0);
+        newConnectionTab();
         connectionPanel.addChangeListener(new ChangeListener() { 
             // This method is called whenever the selected tab changes 
             public void stateChanged(ChangeEvent evt) { 
-            JTabbedPane pane = (JTabbedPane)evt.getSource(); 
-            // Get current tab 
-            int sel = pane.getSelectedIndex();
-            String paneTitle = pane.getTitleAt(sel);
-
-            if (paneTitle.equals("+")){
-                addConnectionTab(sel);
-            }
+                JTabbedPane pane = (JTabbedPane)evt.getSource(); 
+                // Get current tab 
+                int sel = pane.getSelectedIndex();
+                if (sel == -1){
+                    addConnectionTab();
+                }
+                else {
+                    String paneTitle = pane.getTitleAt(sel);
+                    
+                    if (paneTitle.equals("+")){
+                        addConnectionTab();
+                    }
+                }
             } 
         }); 
         detailpanel.setLayout(new CardLayout());
@@ -156,7 +181,8 @@ public class TabbedPreferences extends AppConfigBase {
      *        else is handling the storage responsibility.
      */
     void addItem(JTabbedPane panel, String title, String labelKey, JComponent item, boolean store, String tooltip) {
-        JComponent p= new JPanel();
+        JComponent p = new JPanel();
+        //JComponent p = new ButtonTabComponent(panel);
         p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
 
         if (labelKey != null) {
@@ -171,26 +197,194 @@ public class TabbedPreferences extends AppConfigBase {
         }
         p.add(item);
         p.add(Box.createVerticalGlue());
-
+        
         panel.addTab(title, null, p, tooltip);
         if (store) items.add(item);  
     }
     
-    void addConnectionTab(int x){
-        connectionPanel.remove(x);
-        addItem(connectionPanel, rb.getString("TabbedLayoutConnection")+(x+1), null, JmrixConfigPane.instance(x), true, null);
-        addItem(connectionPanel, "+", null, newConnectionTab(), false, "Add new Connection");
-        connectionPanel.setSelectedIndex(x);
+    void addConnection(int tabPosition, int instance){
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.PAGE_AXIS));
+        
+        p.add(JmrixConfigPane.instance(instance));
+        p.add(Box.createVerticalGlue());
+        p.setToolTipText(JmrixConfigPane.instance(instance).getCurrentProtocolName());
+        String title = rb.getString("TabbedLayoutConnection")+(instance+1);
+        if(JmrixConfigPane.instance(instance).getCurrentProtocolName()!=null){
+            title = JmrixConfigPane.instance(instance).getCurrentProtocolName();
+        }
+        connectionTabInstance.add(instance);
+        connectionPanel.add(title, p);
+        connectionPanel.setTitleAt(tabPosition, title);
+        connectionPanel.setToolTipTextAt(tabPosition, JmrixConfigPane.instance(instance).getCurrentProtocolName());
+        connectionPanel.setTabComponentAt(tabPosition, new ButtonTabComponent(connectionPanel));
+        items.add(JmrixConfigPane.instance(instance));
     }
     
-    JComponent newConnectionTab(){
+    
+    void addConnectionTab(){
+        if(connectionPanel.indexOfTab("+")>0)
+            connectionPanel.removeTabAt(connectionPanel.indexOfTab("+"));
+        int newinstance = connectionTabInstance.get(connectionTabInstance.size()-1)+1;
+        addConnection(connectionTabInstance.size(), newinstance);
+        newConnectionTab();
+    }
+    
+    void newConnectionTab(){
         JComponent p = new JPanel();
         p.add(Box.createVerticalGlue());
-        return p;
+        p.setToolTipText("Add New Connection");
+        connectionPanel.add("+", p);
+        connectionPanel.setTabComponentAt(connectionPanel.getTabCount()-1, null);
+        connectionPanel.setSelectedIndex(connectionPanel.getTabCount()-2);
     }
+    
     jmri.jmrix.JmrixConfigPane comm1;
     jmri.GuiLafConfigPane guiPrefs;
         
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TabbedPreferences.class.getName());
     
+    private void removeTab(ActionEvent e, JComponent c, int x){
+
+        int i = connectionPanel.indexOfTabComponent(c);
+        if (i != -1) {
+            int n = JOptionPane.showConfirmDialog(
+                null,
+                "Do you really want to delete connection " + connectionPanel.getTitleAt(i) + "?",
+                "Delete Connection",
+                JOptionPane.YES_NO_OPTION);
+            if(n!=0)
+                return;
+            if(connectionPanel.getChangeListeners().length >0)
+                connectionPanel.removeChangeListener(connectionPanel.getChangeListeners()[0]);
+            int jmrixinstance = connectionTabInstance.get(i);
+            
+            connectionPanel.remove(i);  //was x
+            items.remove(JmrixConfigPane.instance(jmrixinstance));
+            JmrixConfigPane.dispose(jmrixinstance);
+            connectionTabInstance.remove(i);
+            if(connectionPanel.getTabCount()==1){
+                addConnectionTab();
+            }
+            connectionPanel.setSelectedIndex(connectionPanel.getTabCount()-2);
+            connectionPanel.addChangeListener(new ChangeListener() { 
+            // This method is called whenever the selected tab changes 
+                public void stateChanged(ChangeEvent evt) { 
+                    JTabbedPane pane = (JTabbedPane)evt.getSource(); 
+                    // Get current tab 
+                    int sel = pane.getSelectedIndex();
+                    if (sel == -1){
+                        addConnectionTab();
+                    }
+                    else {
+                        String paneTitle = pane.getTitleAt(sel);
+                        if (paneTitle.equals("+")){
+                            addConnectionTab();
+                        }
+                    }
+                } 
+            });
+        }        
+    }
+    
+    private class ButtonTabComponent extends JPanel {
+        private final JTabbedPane pane;
+
+        public ButtonTabComponent(final JTabbedPane pane) {
+            //unset default FlowLayout' gaps
+            super(new FlowLayout(FlowLayout.LEFT, 0, 0));
+            if (pane == null) {
+                throw new NullPointerException("TabbedPane is null");
+            }
+            this.pane = pane;
+            setOpaque(false);
+            
+            //make JLabel read titles from JTabbedPane
+            JLabel label = new JLabel() {
+                public String getText() {
+                    int i = pane.indexOfTabComponent(ButtonTabComponent.this);
+                    if (i != -1) {
+                        return pane.getTitleAt(i);
+                    }
+                    return null;
+                }
+            };
+            
+            add(label);
+            //add more space between the label and the button
+            label.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 5));
+            //tab button
+            JButton button = new TabButton();
+            add(button);
+            //add more space to the top of the component
+            setBorder(BorderFactory.createEmptyBorder(2, 0, 0, 0));
+        }
+
+        private class TabButton extends JButton implements ActionListener {
+            public TabButton() {
+                int size = 17;
+                setPreferredSize(new Dimension(size, size));
+                setToolTipText("Delete This Connection");
+                //Make the button looks the same for all Laf's
+                setUI(new BasicButtonUI());
+                //No need to be focusable
+                setFocusable(false);
+                setBorderPainted(false);
+                //Making nice rollover effect
+                //we use the same listener for all buttons
+                addMouseListener(buttonMouseListener);
+                setRolloverEnabled(true);
+                //Close the proper tab by clicking the button
+                addActionListener(this);
+                setBackground(Color.RED);
+            }
+
+            public void actionPerformed(ActionEvent e) {
+                removeTab(e, ButtonTabComponent.this, pane.getSelectedIndex());
+            }
+
+            //we don't want to update UI for this button
+            public void updateUI() {
+            }
+
+            //paint the cross
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2 = (Graphics2D) g.create();
+                //shift the image for pressed buttons
+                if (getModel().isPressed()) {
+                    g2.translate(1, 1);
+                }
+                g2.setStroke(new BasicStroke(2));
+                g2.setColor(Color.WHITE);
+                if (getModel().isRollover()) {
+                    g2.setColor(Color.RED);
+                }
+                int delta = 6;
+                
+                g2.drawLine(delta, delta, getWidth() - delta - 1, getHeight() - delta - 1);
+                g2.drawLine(getWidth() - delta - 1, delta, delta, getHeight() - delta - 1);
+                g2.dispose();
+            }
+        }
+        
+
+        private final MouseListener buttonMouseListener = new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                Component component = e.getComponent();
+                if (component instanceof AbstractButton) {
+                    AbstractButton button = (AbstractButton) component;
+                    button.setBackground(Color.WHITE);
+                }
+            }
+
+            public void mouseExited(MouseEvent e) {
+                Component component = e.getComponent();
+                if (component instanceof AbstractButton) {
+                    AbstractButton button = (AbstractButton) component;
+                    button.setBackground(Color.RED);
+                }
+            }
+        };
+    }
 }
