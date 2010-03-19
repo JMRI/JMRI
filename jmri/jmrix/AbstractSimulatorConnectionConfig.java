ba6c58294d974e04d2a7aac7cd6496c12f4ef153
Kevin Dickerson <kev.public@ntlworld.com>
2010-03-19 17:37:20 +0000

    Initial Simulator Connection Config, still requires futher work, but allows the display and changing of the connection name and system prefix

---
+++ jmri/jmrix/AbstractSimulatorConnectionConfig.java
@@ -0,0 +1,274 @@
+// AbstractSimulatorConnectionConfig.java
+
+package jmri.jmrix;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import java.util.Vector;
+
+import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Abstract base class for common implementation of the Simulator ConnectionConfig
+ * Currently uses the serial adapter, but this will change to the simulator adapter
+ * in due course.
+ *
+ * @author      Kevin Dickerson   Copyright (C) 2001, 2003
+ * @version	$Revision: 1.1 $
+ */
+
+//
+abstract public class AbstractSimulatorConnectionConfig extends AbstractConnectionConfig implements jmri.jmrix.ConnectionConfig {
+
+    /**
+     * Ctor for an object being created during load process
+     * Currently uses the serialportadapter, but this will 
+     * change to a simulator port adapter in due course.
+     */
+    public AbstractSimulatorConnectionConfig(jmri.jmrix.SerialPortAdapter p){
+        adapter = p;
+    }
+
+    public jmri.jmrix.SerialPortAdapter getAdapter() { return adapter; }
+    
+    /**
+     * Ctor for a functional object with no prexisting adapter.
+     * Expect that the subclass setInstance() will fill the adapter member.
+     */
+    public AbstractSimulatorConnectionConfig() {
+        adapter = null;
+    }
+
+    boolean init = false;
+
+    protected void checkInitDone() {
+    	if (log.isDebugEnabled()) log.debug("init called for "+name());
+        if (init) return;
+
+        opt1Box.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                adapter.configureOption1((String)opt1Box.getSelectedItem());
+            }
+        });
+        opt2Box.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                adapter.configureOption2((String)opt2Box.getSelectedItem());
+            }
+        });
+        if(adapter.getSystemConnectionMemo()!=null){
+            systemPrefixField.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(!adapter.getSystemConnectionMemo().setSystemPrefix(systemPrefixField.getText())){
+                        JOptionPane.showMessageDialog(null, "System Prefix " + systemPrefixField.getText() + " is already assigned");
+                        systemPrefixField.setText(adapter.getSystemConnectionMemo().getSystemPrefix());
+                    }
+                }
+            });
+            systemPrefixField.addFocusListener( new FocusListener() {
+                public void focusLost(FocusEvent e){
+                    if(!adapter.getSystemConnectionMemo().setSystemPrefix(systemPrefixField.getText())){
+                        JOptionPane.showMessageDialog(null, "System Prefix " + systemPrefixField.getText() + " is already assigned");
+                        systemPrefixField.setText(adapter.getSystemConnectionMemo().getSystemPrefix());
+                    }
+                }
+                public void focusGained(FocusEvent e){ }
+            });
+            connectionNameField.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(!adapter.getSystemConnectionMemo().setUserName(connectionNameField.getText())){
+                        JOptionPane.showMessageDialog(null, "Connection Name " + connectionNameField.getText() + " is already assigned");
+                        connectionNameField.setText(adapter.getSystemConnectionMemo().getUserName());
+                    }
+                }
+            });
+            connectionNameField.addFocusListener( new FocusListener() {
+                public void focusLost(FocusEvent e){
+                    if(!adapter.getSystemConnectionMemo().setUserName(connectionNameField.getText())){
+                        JOptionPane.showMessageDialog(null, "Connection Name " + connectionNameField.getText() + " is already assigned");
+                        connectionNameField.setText(adapter.getSystemConnectionMemo().getUserName());
+                    }
+                }
+                public void focusGained(FocusEvent e){ }
+            });
+        }
+        init = true;
+    }
+
+    protected JComboBox opt1Box = new JComboBox();
+    protected JLabel opt1BoxLabel = new JLabel();
+    
+    protected JComboBox opt2Box = new JComboBox();
+
+    protected JLabel opt2BoxLabel = new JLabel();
+    protected JCheckBox showAdvanced = new JCheckBox("Additional Connection Settings");
+    protected String[] opt1List;
+    protected String[] opt2List;
+    protected String[] baudList;
+    protected jmri.jmrix.SerialPortAdapter adapter = null;
+    protected JPanel _details;
+
+    protected JLabel systemPrefixLabel = new JLabel("Connection Prefix");
+    protected JLabel connectionNameLabel = new JLabel("Connection Name");
+    protected JTextField systemPrefixField = new JTextField();
+    protected JTextField connectionNameField = new JTextField();
+    protected String systemPrefix;
+    protected String connectionName;
+
+    /**
+     * Load the adapter with an appropriate object
+     * <i>unless</I> its already been set.
+     */
+    abstract protected void setInstance();
+
+    public String getInfo() {
+        return "Simulator";
+    }
+
+    static java.util.ResourceBundle rb = 
+        java.util.ResourceBundle.getBundle("jmri.jmrix.JmrixBundle");
+    protected int NUMOPTIONS = 0;
+    
+	public void loadDetails(final JPanel details) {
+        _details = details;
+        setInstance();
+
+        if(adapter.getSystemConnectionMemo()!=null){
+            systemPrefixField.setText(adapter.getSystemConnectionMemo().getSystemPrefix());
+            connectionNameField.setText(adapter.getSystemConnectionMemo().getUserName());
+            NUMOPTIONS=NUMOPTIONS+2;
+        }
+    	
+        opt1List = adapter.validOption1();
+        opt1Box.removeAllItems();
+        // need to remove ActionListener before addItem() or action event will occur
+        if(opt1Box.getActionListeners().length >0)
+        	opt1Box.removeActionListener(opt1Box.getActionListeners()[0]);
+        for (int i=0; i<opt1List.length; i++) opt1Box.addItem(opt1List[i]);
+        opt2List = adapter.validOption2();
+        opt2Box.removeAllItems();
+        // need to remove ActionListener before addItem() or action event will occur
+        if(opt2Box.getActionListeners().length >0)
+        	opt2Box.removeActionListener(opt2Box.getActionListeners()[0]);
+        for (int i=0; i<opt2List.length; i++) opt2Box.addItem(opt2List[i]);
+
+        if (opt1List.length>1) {
+            NUMOPTIONS++;
+            opt1Box.setToolTipText("The first option is strongly recommended. See README for more info.");
+            opt1Box.setEnabled(true);
+            opt1Box.setSelectedItem(adapter.getCurrentOption1Setting());
+        } else {
+            opt1Box.setToolTipText("There are no options for this protocol");
+            opt1Box.setEnabled(false);
+        }
+        if (opt2List.length>1) {
+            NUMOPTIONS++;
+            opt2Box.setToolTipText("");
+            opt2Box.setEnabled(true);
+            opt2Box.setSelectedItem(adapter.getCurrentOption1Setting());
+        } else {
+            opt2Box.setToolTipText("There are no options for this protocol");
+            opt2Box.setEnabled(false);
+        }
+    
+        showAdvanced.setFont(showAdvanced.getFont().deriveFont(9f));
+        showAdvanced.setForeground(Color.blue);
+        showAdvanced.addItemListener(
+            new ItemListener() {
+                public void itemStateChanged(ItemEvent e){
+                    showAdvancedItems();
+                }
+            });
+        showAdvancedItems();
+        init = false;		// need to reload action listeners
+        checkInitDone();
+    }
+    
+    protected void showAdvancedItems(){
+        _details.removeAll();
+        int stdrows = 0;
+        boolean incAdvancedOptions=true;
+        if ((!isOptList1Advanced())&&(opt1List.length>1)) stdrows++;
+        if ((!isOptList2Advanced())&&(opt2List.length>1)) stdrows++;
+        if(adapter.getSystemConnectionMemo()!=null) stdrows=stdrows+2;
+        if (stdrows == NUMOPTIONS){
+            incAdvancedOptions=false;
+        } else{
+            stdrows++;
+        }
+        if (showAdvanced.isSelected()) {
+            int advrows = stdrows;
+            if ((isOptList1Advanced())&&(opt1List.length>1)) advrows++;
+            if ((isOptList2Advanced())&&(opt2List.length>1)) advrows++;
+            _details.setLayout(new GridLayout(advrows,2));
+            addStandardDetails(incAdvancedOptions);
+            if ((isOptList1Advanced())&&(opt1List.length>1)) {
+                _details.add(opt1BoxLabel = new JLabel(adapter.option1Name()));
+                _details.add(opt1Box);
+            }
+            if ((isOptList2Advanced())&&(opt2List.length>1)) {
+                _details.add(opt2BoxLabel = new JLabel(adapter.option2Name()));
+                _details.add(opt2Box);
+            }
+        } else {
+            _details.setLayout(new GridLayout(stdrows,2));
+            addStandardDetails(incAdvancedOptions);
+        }
+        _details.validate();
+        if (_details.getTopLevelAncestor()!=null)
+            ((jmri.util.JmriJFrame)_details.getTopLevelAncestor()).repaint();
+        _details.repaint();
+    }
+    
+    protected void addStandardDetails(boolean incAdvanced){
+        if ((!isOptList1Advanced())&&(opt1List.length>1)){
+            _details.add(opt1BoxLabel);
+            _details.add(opt1Box);
+        }
+        
+        if ((!isOptList2Advanced())&&(opt2List.length>1)) {
+            _details.add(opt2BoxLabel);
+            _details.add(opt2Box);
+        }
+        if(adapter.getSystemConnectionMemo()!=null){
+            _details.add(systemPrefixLabel);
+            _details.add(systemPrefixField);
+            _details.add(connectionNameLabel);
+            _details.add(connectionNameField);
+        }
+        if (incAdvanced){
+            _details.add(new JLabel(" "));
+            _details.add(showAdvanced);
+        }
+    }
+
+    public String getManufacturer() { return adapter.getManufacturer(); }
+    public void setManufacturer(String manufacturer) { adapter.setManufacturer(manufacturer); }
+    
+    public String getConnectionName() {
+        if(adapter.getSystemConnectionMemo()!=null)
+            return adapter.getSystemConnectionMemo().getUserName();
+        else return null;
+    }
+    
+    public void dispose(){
+        if (adapter!=null){
+            adapter.dispose();
+            adapter=null;
+        }
+    }
+
+    static protected org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(AbstractSimulatorConnectionConfig.class.getName());
+}
