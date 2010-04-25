3cc0f353d8da189b399912546d0aa29ec75d4d3e
Bob Jacobsen <jacobsen@mac.com>
2010-04-25 06:10:43 +0000

    add toString

--- jmri/jmrit/symbolicprog/tabbedframe/PaneProgPane.java
+++ jmri/jmrit/symbolicprog/tabbedframe/PaneProgPane.java
@@ -64,7 +64,7 @@ import java.util.List;
  * @author    Bob Jacobsen   Copyright (C) 2001, 2003, 2004, 2005, 2006
  * @author    D Miller Copyright 2003
  * @author    Howard G. Penny   Copyright (C) 2005
- * @version   $Revision: 1.72 $
+ * @version   $Revision: 1.73 $
  * @see       jmri.jmrit.symbolicprog.VariableValue#isChanged
  *
  */
@@ -302,6 +302,7 @@ public class PaneProgPane extends javax.swing.JPanel
     }
 
     public String getName() { return mName; }
+    public String toString() { return getName(); }
     
     /**
      * Enable the read all and read changes button if possible.
