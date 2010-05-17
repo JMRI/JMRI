56b9fbd28c8d369d499d02cebdd2e6c36f91b6ec
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2010-05-17 19:04:25 +0000

    New print car roster feature.  The ability to control which car attributes are printed.

--- jmri/jmrit/operations/rollingstock/cars/PrintCarRosterAction.java
+++ jmri/jmrit/operations/rollingstock/cars/PrintCarRosterAction.java
@@ -11,6 +11,8 @@ import javax.swing.*;
 
 import java.util.List;
 import java.util.ResourceBundle;
+import jmri.jmrit.operations.OperationsFrame;
+import jmri.jmrit.operations.setup.Control;
 
 
 /**
@@ -21,8 +23,8 @@ import java.util.ResourceBundle;
  *
  * @author	Bob Jacobsen   Copyright (C) 2003
  * @author  Dennis Miller  Copyright (C) 2005
- * @author Daniel Boudreau Copyright (C) 2008
- * @version     $Revision: 1.10 $
+ * @author Daniel Boudreau Copyright (C) 2008, 2010
+ * @version     $Revision: 1.11 $
  */
 public class PrintCarRosterAction  extends AbstractAction {
 	
@@ -46,13 +48,16 @@ public class PrintCarRosterAction  extends AbstractAction {
      */
     boolean isPreview;
     CarsTableFrame panel;
-    
+    CarPrintOptionFrame cpof = null;
 
     public void actionPerformed(ActionEvent e) {
-
-        // Print all cars?
-        boolean printAll = true;
-        boolean askQuestion = true;
+    	if (cpof == null)
+    		cpof = new CarPrintOptionFrame(this);
+    	else
+    		cpof.setVisible(true);
+    }
+    
+    private void printCars(){
  
         // obtain a HardcopyWriter to do this
         HardcopyWriter writer = null;
@@ -69,102 +74,181 @@ public class PrintCarRosterAction  extends AbstractAction {
         String number;
         String road;
         String type;
-        String length;
-        String weight;
-        String color;
-        String owner;
-        String built;
+        String length = "";
+        String weight = "";
+        String color = "";
+        String owner = "";
+        String built = "";
+        
+        boolean printOnly = printCarsWithLocation.isSelected();
+        boolean printLength = printCarLength.isSelected();
+        boolean printWeight = printCarWeight.isSelected();
+        boolean printColor = printCarColor.isSelected();
+        boolean printOwner = printCarOwner.isSelected();
+        boolean printBuilt = printCarBuilt.isSelected();
+        
+		int locMaxLen = 29;
+		int ownerMaxLen = 4;
+		// adjust the max length for the location and track field
+		if (!printLength)
+			locMaxLen = locMaxLen +5;
+		if (!printWeight)
+			locMaxLen = locMaxLen +5;
+		if (!printColor)
+			locMaxLen = locMaxLen +12;
+   		if (!printOwner)
+			locMaxLen = locMaxLen +5;
+  		if (!printBuilt)
+			locMaxLen = locMaxLen +5;
+  		// now adjust the owner name field if there is space available
+  		if (locMaxLen > Control.MAX_LEN_STRING_LOCATION_NAME + Control.MAX_LEN_STRING_TRACK_NAME + ownerMaxLen){
+  			ownerMaxLen = locMaxLen - (Control.MAX_LEN_STRING_LOCATION_NAME + Control.MAX_LEN_STRING_TRACK_NAME);
+  			if (ownerMaxLen > Control.MAX_LEN_STRING_ATTRIBUTE)
+  				ownerMaxLen = Control.MAX_LEN_STRING_ATTRIBUTE;
+  		}
         
         List<String> cars = panel.getSortByList();
         try {
         	String s = rb.getString("Number") + "\t" + rb.getString("Road")
-					+ "\t" + rb.getString("Type") + "\t "
-					+ rb.getString("Length") + " " + rb.getString("Weight") +" "
-					+ rb.getString("Color") + "    " + rb.getString("Owner") +" "
-					+ rb.getString("Built") + "\t" + rb.getString("Location")
-					+ newLine;
+        	+ "\t" + rb.getString("Type") + "\t "
+        	+ (printLength?rb.getString("Length")+ " ":" ") 
+        	+ (printWeight?rb.getString("Weight")+ " ":" ")
+        	+ (printColor?rb.getString("Color")+ "     ":"")
+        	+ (printOwner?rb.getString("Owner")+" ":"")
+        	+ (printBuilt?rb.getString("Built"):"")
+        	+ "\t" + rb.getString("Location") + " - "+ rb.getString("Track")
+        	+ newLine;
         	writer.write(s, 0, s.length());
         	for (int i=0; i<cars.size(); i++){
         		Car car = manager.getById(cars.get(i));
-        		// skip cars without a location?
-        		if (car.getLocationName().equals("")){
-        			if (!printAll)
-        				continue;
-        			else if (askQuestion){
-        				int selectedValue = JOptionPane.showOptionDialog(mFrame, 
-        						rb.getString("PrintAllCars"), null,
-        						JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,
-        						null, new Object[] { rb.getString("ButtonYes"), rb.getString("ButtonNoOnly")},
-        						rb.getString("ButtonYes"));
-        				askQuestion = false;
-        				if (selectedValue == 1)
-        					printAll = false;
-        			}
 
-        		}
-        			
         		location = "";
+        		
         		if (!car.getLocationName().equals("")){
         			location = car.getLocationName().trim() + " - " + car.getTrackName().trim();
-        			if (location.length() > 29)
-        				location = location.substring(0, 29);
-        		}
+        			if (location.length() > locMaxLen)
+        				location = location.substring(0, locMaxLen);
+        		}else if (printOnly)
+        			continue;	// car doesn't have a location skip
         		// pad out the fields
-         		number = car.getNumber().trim();
-         		for (int j=number.length(); j<7; j++)
-         			number += " ";
-         		
-         		road = car.getRoad().trim();
+        		number = car.getNumber().trim();
+        		for (int j=number.length(); j<7; j++)
+        			number += " ";
+
+        		road = car.getRoad().trim();
         		if (road.length() > 7)
         			road = road.substring(0, 7);
-         		for (int j=road.length(); j<7; j++)
-         			road += " ";
-         		
-         		type = car.getType().trim();
-           		if (type.length() > 11)
+        		for (int j=road.length(); j<7; j++)
+        			road += " ";
+
+        		type = car.getType().trim();
+        		if (type.length() > 11)
         			type = type.substring(0, 11);
-         		for (int j=type.length(); j<11; j++)
-         			type += " ";
-         		
-        		length = car.getLength().trim();
-         		for (int j=length.length(); j<3; j++)
-         			length += " ";
- 
-        		weight = car.getWeight().trim();
-          		if (weight.length() > 4)
-          			weight = weight.substring(0, 4);
-        		for (int j=weight.length(); j<4; j++)
-        			weight += " ";
-        		
-           		color = car.getColor().trim();
-          		if (color.length() > 11)
-        			color = color.substring(0, 11);
-         		for (int j=color.length(); j<11; j++)
-         			color += " ";
-         		
-          		owner = car.getOwner().trim();
-          		if (owner.length() > 4)
-          			owner = owner.substring(0, 4);
-         		for (int j=owner.length(); j<4; j++)
-         			owner += " ";
-         		
-         		built = car.getBuilt().trim();
-          		if (built.length() > 4)
-          			built = built.substring(0, 4);
-         		for (int j=built.length(); j<4; j++)
-         			built += " ";
-         		         		
- 				s = number + " " + road + " " + type + " " + length + " "
-						+ weight + " " + color + " " + owner + " " + built
-						+ " " + location + newLine;
+        		for (int j=type.length(); j<11; j++)
+        			type += " ";
+
+        		if (printLength){
+        			length = car.getLength().trim();
+        			for (int j=length.length(); j<5; j++)
+        				length += " ";
+        		}
+
+        		if (printWeight){
+        			weight = car.getWeight().trim();
+        			if (weight.length() > 4)
+        				weight = weight.substring(0, 4);
+        			for (int j=weight.length(); j<5; j++)
+        				weight += " ";
+        		}
+
+        		if (printColor){
+        			color = car.getColor().trim();
+        			if (color.length() > 11)
+        				color = color.substring(0, 11);
+        			for (int j=color.length(); j<12; j++)
+        				color += " ";
+        		}
+
+        		if (printOwner){
+        			owner = car.getOwner().trim();
+        			if (owner.length() > ownerMaxLen)
+        				owner = owner.substring(0, ownerMaxLen);
+        			for (int j=owner.length(); j<ownerMaxLen+1; j++)
+        				owner += " ";
+        		}
+
+        		if (printBuilt){
+        			built = car.getBuilt().trim();
+        			if (built.length() > 4)
+        				built = built.substring(0, 4);
+        			for (int j=built.length(); j<5; j++)
+        				built += " ";
+        		}
+
+        		s = number + " " + road + " " + type + " " 
+        		+ length + weight + color + owner + built
+        		+ location + newLine;
         		writer.write(s, 0, s.length());
         	}
 
         	// and force completion of the printing
         	writer.close();
         } catch (IOException we) {
-        	log.error("Error printing ConsistRosterEntry: " + e);
+        	log.error("Error printing car roster");
         }
+    }
+    
+    JCheckBox printCarsWithLocation = new JCheckBox(rb.getString("PrintCarsWithLocation"));
+    JCheckBox printCarLength = new JCheckBox(rb.getString("PrintCarLength"));
+    JCheckBox printCarWeight = new JCheckBox(rb.getString("PrintCarWeight"));
+    JCheckBox printCarColor = new JCheckBox(rb.getString("PrintCarColor"));
+    JCheckBox printCarOwner = new JCheckBox(rb.getString("PrintCarOwner"));
+    JCheckBox printCarBuilt = new JCheckBox(rb.getString("PrintCarBuilt"));
+    
+    JButton okayButton = new JButton(rb.getString("ButtonOkay"));
+    JButton cancelButton = new JButton(rb.getString("ButtonCancel"));
+    
+    public class CarPrintOptionFrame extends OperationsFrame{
+    	 PrintCarRosterAction pcr;
+    	
+    	public CarPrintOptionFrame(PrintCarRosterAction pcr){
+    		super();
+    		this.pcr = pcr;
+    		// create panel
+    		JPanel pPanel = new JPanel();
+    		pPanel.setLayout(new BoxLayout(pPanel,BoxLayout.Y_AXIS));
+    		pPanel.setBorder(BorderFactory.createTitledBorder(rb.getString("PrintOptions")));
+    		pPanel.add(printCarsWithLocation);
+    		pPanel.add(printCarLength);
+    		pPanel.add(printCarWeight);
+    		pPanel.add(printCarColor);
+    		pPanel.add(printCarOwner);
+    		pPanel.add(printCarBuilt);
+    		
+    		// set defaults
+    		printCarsWithLocation.setSelected(false);
+    		printCarLength.setSelected(true);
+    		printCarWeight.setSelected(false);
+    		printCarColor.setSelected(true);
+    		printCarOwner.setSelected(false);
+    		printCarBuilt.setSelected(false);
+    		
+    		JPanel pButtons = new JPanel();  
+    		pButtons.setLayout(new GridBagLayout());
+    		pButtons.add(okayButton);
+    		addButtonAction(okayButton);
+    	   		
+    		getContentPane().setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+    		getContentPane().add(pPanel);
+    		getContentPane().add(pButtons);
+    		pack();
+    		setVisible(true);
+    	}
+    	
+    	public void buttonActionPerformed(java.awt.event.ActionEvent ae) { 		
+    		setVisible(false);
+    		pcr.printCars();  		
+    	}
     }
 
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(PrintCarRosterAction.class.getName());
