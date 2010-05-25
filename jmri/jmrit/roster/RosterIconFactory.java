8f7f67efad9a8e6428b01d354a9eb3358b7d574f
Lionel Jeanson <hebbos@users.sourceforge.net>
2010-05-25 20:45:38 +0000

    Added a pane in "Roster Media" pane to view and eventually change custom attributes for a roster. Added French translations. Added missing headers.

--- jmri/jmrit/roster/RosterIconFactory.java
+++ jmri/jmrit/roster/RosterIconFactory.java
@@ -4,6 +4,24 @@ import java.util.HashMap;
 
 import javax.swing.ImageIcon;
 
+/*
+* <hr>
+* This file is part of JMRI.
+* <P>
+* JMRI is free software; you can redistribute it and/or modify it under 
+* the terms of version 2 of the GNU General Public License as published 
+* by the Free Software Foundation. See the "COPYING" file for a copy
+* of this license.
+* <P>
+* JMRI is distributed in the hope that it will be useful, but WITHOUT 
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+* for more details.
+* <P>
+* @author	Lionel Jeanson   Copyright (C) 2009
+* @version	$$
+*/
+
 public class RosterIconFactory {
 	private int iconHeight;
 	HashMap<String,ImageIcon> icons = new HashMap<String,ImageIcon>();
