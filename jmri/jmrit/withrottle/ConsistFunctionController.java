df40143d062a85ab622b0660c98305d5ad8e3be8
Brett Hoffman <bhoffman351@mac.com>
2010-06-08 03:29:55 +0000

    Revise how consist functions are handled

--- jmri/jmrit/withrottle/ConsistFunctionController.java
+++ jmri/jmrit/withrottle/ConsistFunctionController.java
@@ -1,6 +1,5 @@
 package jmri.jmrit.withrottle;
 
-import java.lang.reflect.Method;
 import jmri.DccLocoAddress;
 import jmri.DccThrottle;
 import jmri.ThrottleListener;
@@ -8,109 +7,34 @@ import jmri.ThrottleListener;
 /**
  *
  *	@author Brett Hoffman   Copyright (C) 2010
- *	@version $Revision: 1.2 $
+ *	@version $Revision: 1.3 $
  */
-public class ConsistFunctionController extends AbstractController implements ThrottleListener{
+public class ConsistFunctionController implements ThrottleListener{
 
-    private DccLocoAddress address;
     private DccThrottle throttle;
+    private ThrottleController throttleController;
 
-    public ConsistFunctionController(DccLocoAddress address){
-        this.address = address;
-
+    public ConsistFunctionController(ThrottleController tc){
+        throttleController = tc;
     }
     
     public void notifyThrottleFound(DccThrottle t) {
-        log.debug("Lead Loco throttle found");
+        if (log.isDebugEnabled()) log.debug("Lead Loco throttle found: " + t);
         throttle = t;
+        throttleController.setFunctionThrottle(t);
     }
     
     public void dispose(){
         throttle.release();
     }
 
-
-    @Override
-    boolean verifyCreation() {
-        log.debug("verify creation");
-        return jmri.InstanceManager.throttleManagerInstance().requestThrottle(address.getNumber(), address.isLongAddress(), this);
+    public DccThrottle getThrottle(){
+        return throttle;
     }
 
-    @Override
-    void handleMessage(String inPackage){
-        log.debug("handleMessage in CFC");
-        //	get the function # sent from device
-        String receivedFunction = inPackage.substring(2);
-        Boolean state = false;
-
-        if (inPackage.charAt(1) == '1'){	//	Function Button down
-            if(log.isDebugEnabled()) log.debug("Trying to set function " + receivedFunction);
-            //	Toggle button state:
-            try{
-                Method getF = throttle.getClass().getMethod("getF"+receivedFunction,(Class[])null);
-
-                Class partypes[] = {Boolean.TYPE};
-                Method setF = throttle.getClass().getMethod("setF"+receivedFunction, partypes);
-
-                state = (Boolean)getF.invoke(throttle, (Object[])null);
-                Object data[] = {new Boolean(!state)};
-
-                setF.invoke(throttle, data);
-
-
-            }catch (NoSuchMethodException ea){
-                log.warn(ea);
-            }catch (IllegalAccessException eb){
-                log.warn(eb);
-            }catch (java.lang.reflect.InvocationTargetException ec){
-                log.warn(ec);
-            }
-
-        }else {	//	Function Button up
-
-            //  F2 is momentary for horn
-            //  Need to figure out what to do, Should this be in prefs?
-
-            if (receivedFunction.equals("2")){
-                throttle.setF2(false);
-                return;
-            }
-
-            //	Do nothing if lockable, turn off if momentary
-            try{
-                Method getFMom = throttle.getClass().getMethod("getF"+receivedFunction+"Momentary",(Class[])null);
-
-                Class partypes[] = {Boolean.TYPE};
-                Method setF = throttle.getClass().getMethod("setF"+receivedFunction, partypes);
-
-                if ((Boolean)getFMom.invoke(throttle, (Object[])null)){
-                    Object data[] = {new Boolean(false)};
-
-                    setF.invoke(throttle, data);
-                }
-
-            }catch (NoSuchMethodException ea){
-                log.warn(ea);
-            }catch (IllegalAccessException eb){
-                log.warn(eb);
-            }catch (java.lang.reflect.InvocationTargetException ec){
-                log.warn(ec);
-            }
-
-        }
-
+    boolean requestThrottle(DccLocoAddress loco) {
+        return jmri.InstanceManager.throttleManagerInstance().requestThrottle(loco.getNumber(), loco.isLongAddress(), this);
     }
-
-    //  no list is needed
-    public void checkCanBuildList(){}
-
-    @Override
-    void register() {}
-
-    @Override
-    void deregister() {}
-
-
 
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ConsistFunctionController.class.getName());
 
