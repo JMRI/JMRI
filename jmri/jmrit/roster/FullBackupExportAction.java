6a141132acf1b3ac58cb3a73764d87adef1f6b7d
david d zuhn <zoo@statebeltrailway.org>
2010-09-06 00:45:08 +0000

    clean up some warnings on output stream closure

--- jmri/jmrit/roster/FullBackupExportAction.java
+++ jmri/jmrit/roster/FullBackupExportAction.java
@@ -13,7 +13,6 @@ import java.util.zip.ZipOutputStream;
 import javax.swing.AbstractAction;
 import javax.swing.JFileChooser;
 
-
 /**
  * Offer an easy mechanism to save the entire roster contents from one
  * instance of DecoderPro.  The result is a zip format file, containing 
@@ -22,116 +21,127 @@ import javax.swing.JFileChooser;
  * @author david d zuhn
  *
  */
+public class FullBackupExportAction
+        extends AbstractAction {
 
-public class FullBackupExportAction extends AbstractAction {
+    private static final long serialVersionUID = 1L;
+    // parent component for GUI
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    private Component _parent;
 
-	private Component _who;
+    /**
+     * @param s
+     *            Name of this action, e.g. in menus
+     * @param who
+     *            Component that action is associated with, used to ensure
+     *            proper position in of dialog boxes
+     */
+    public FullBackupExportAction(String s, Component parent) {
+        super(s);
+        _parent = parent;
+    }
 
-	/**
-	 * @param s
-	 *            Name of this action, e.g. in menus
-	 * @param who
-	 *            Component that action is associated with, used to ensure
-	 *            proper position in of dialog boxes
-	 */
-	public FullBackupExportAction(String s, Component who) {
-		super(s);
-		_who = who;
-	}
+    public void actionPerformed(ActionEvent e) {
+        Roster roster = Roster.instance();
 
-	public void actionPerformed(ActionEvent e) {
-		Roster roster = Roster.instance();
+        ZipOutputStream zipper = null;
 
-		try {
-			String roster_filename_extension = ".roster";
+        try {
+            String roster_filename_extension = ".roster";
 
-			JFileChooser chooser = new JFileChooser();
-			/* restore this when Java 1.6 is acceptable
-			FileNameExtensionFilter filter = new FileNameExtensionFilter(
-					"JMRI Roster files", roster_filename_extension);
-			chooser.setFileFilter(filter);
-			*/
-			
-			int returnVal = chooser.showSaveDialog(_who);
-			if (returnVal != JFileChooser.APPROVE_OPTION) {
-				return;
-			}
-			
-			String filename = chooser.getSelectedFile().getAbsolutePath();
-			
-			if (! filename.endsWith(roster_filename_extension)) {
-				filename = filename.concat(roster_filename_extension);
-			}
+            JFileChooser chooser = new JFileChooser();
+            /* restore this when Java 1.6 is acceptable
+            FileNameExtensionFilter filter = new FileNameExtensionFilter(
+            "JMRI Roster files", roster_filename_extension);
+            chooser.setFileFilter(filter);
+             */
 
-			ZipOutputStream zipper = new ZipOutputStream(new FileOutputStream(filename));
-			
-			// create a zip file roster entry for each entry in the main roster
-			for (int index = 0; index < roster.numEntries(); index++) {
-				RosterEntry entry = roster.getEntry(index);
-				copyFileToStream(entry.getPathName(), "roster", zipper, entry.getId());
-			}
-			
-			// Now the full roster entry
-			copyFileToStream (Roster.defaultRosterFilename(), null, zipper, null);
+            int returnVal = chooser.showSaveDialog(_parent);
+            if (returnVal != JFileChooser.APPROVE_OPTION) {
+                return;
+            }
 
-			zipper.setComment("Roster file saved from DecoderPro " + jmri.Version.name());
-			
-			zipper.close();
+            String filename = chooser.getSelectedFile().getAbsolutePath();
 
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+            if (!filename.endsWith(roster_filename_extension)) {
+                filename = filename.concat(roster_filename_extension);
+            }
 
-	}
-	
-	/**
-	 * Copy a file to an entry in a zip file.
-	 * 
-	 * The basename of the source file will be used in the zip file, placed in
-	 * the directory of the zip file specified by dirname.  If dirname is null,
-	 * the file will be placed in the root level of the zip file.
-	 * 
-	 * @param filename the file to copy
-	 * @param dirname the zip file "directory" to place this file in
-	 * @param zipper the ZipOutputStream
-	 * @throws IOException
-	 */
+            zipper = new ZipOutputStream(new FileOutputStream(filename));
 
-	private void copyFileToStream(String filename, String dirname, ZipOutputStream zipper, String comment)
-			throws IOException {
-		File file = new File (filename);
-		String entryName;
-		
-		if (dirname != null) {
-			entryName = dirname + "/" + file.getName();
-		}
-		else {
-			entryName = file.getName();
-		}
-		
-		ZipEntry zipEntry = new ZipEntry(entryName);
+            // create a zip file roster entry for each entry in the main roster
+            for (int index = 0; index < roster.numEntries(); index++) {
+                RosterEntry entry = roster.getEntry(index);
+                copyFileToStream(entry.getPathName(), "roster", zipper, entry.getId());
+            }
 
-		zipEntry.setTime(file.lastModified());
-		zipEntry.setSize(file.length());
-		if (comment != null) {
-			zipEntry.setComment(comment);
-		}
+            // Now the full roster entry
+            copyFileToStream(Roster.defaultRosterFilename(), null, zipper, null);
 
-		zipper.putNextEntry(zipEntry);
+            zipper.setComment("Roster file saved from DecoderPro " + jmri.Version.name());
 
-		FileInputStream fis = new FileInputStream(file);
-		int c;
-		while ((c = fis.read()) != -1) {
-			zipper.write(c);
-		}
-		
-		zipper.closeEntry();
-	}
+            zipper.close();
+
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (zipper != null) {
+
+                try {
+                    zipper.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Copy a file to an entry in a zip file.
+     *
+     * The basename of the source file will be used in the zip file, placed in
+     * the directory of the zip file specified by dirname.  If dirname is null,
+     * the file will be placed in the root level of the zip file.
+     *
+     * @param filename the file to copy
+     * @param dirname the zip file "directory" to place this file in
+     * @param zipper the ZipOutputStream
+     * @throws IOException
+     */
+    private void copyFileToStream(String filename, String dirname, ZipOutputStream zipper, String comment)
+            throws IOException {
+        File file = new File(filename);
+        String entryName;
+
+        if (dirname != null) {
+            entryName = dirname + "/" + file.getName();
+        } else {
+            entryName = file.getName();
+        }
+
+        ZipEntry zipEntry = new ZipEntry(entryName);
+
+        zipEntry.setTime(file.lastModified());
+        zipEntry.setSize(file.length());
+        if (comment != null) {
+            zipEntry.setComment(comment);
+        }
+
+        zipper.putNextEntry(zipEntry);
+
+        FileInputStream fis = new FileInputStream(file);
+        try {
+            int c;
+            while ((c = fis.read()) != -1) {
+                zipper.write(c);
+            }
+        } finally {
+            fis.close();
+        }
+
+        zipper.closeEntry();
+    }
 }
