13ad8bb4f107b769093c6bf223ca781e5170a117
Bob Jacobsen <jacobsen@mac.com>
2010-09-07 05:16:53 +0000

    restore configurexml stand-in classes that weren't as obsolete as planned

--- jmri/configurexml/DefaultRouteManagerXml.java
+++ jmri/configurexml/DefaultRouteManagerXml.java
@@ -2,14 +2,16 @@ package jmri.configurexml;
 
 /**
  * Dummy class, just present so files that refer to this 
- * class (e.g. pre JMRI 2.5.4 files) can still be read by
+ * class (e.g. pre JMRI 2.8 files) can still be read by
  * deferring to the present class.
  *
  * @author Bob Jacobsen Copyright: Copyright (c) 2009
- * @version $Revision: 1.25 $
- * @deprecated 2.5.4
+ * @version $Revision: 1.26 $
+ * @deprecated 2.7.8
  */
 @Deprecated
+@edu.umd.cs.findbugs.annotations.SuppressWarnings(value="NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
+// This is a configurexml migration class, same name is OK
 public class DefaultRouteManagerXml 
     extends jmri.managers.configurexml.DefaultRouteManagerXml {
 }
