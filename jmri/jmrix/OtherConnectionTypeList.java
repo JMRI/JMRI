b86fe734d40f2a54a976c39e4a597868dfc1fc07
Paul Bender <paul.bender@acm.org>
2010-09-19 03:57:17 +0000

    remove reference to lenz, which wasn't appropraite for this file.

--- jmri/jmrix/OtherConnectionTypeList.java
+++ jmri/jmrix/OtherConnectionTypeList.java
@@ -4,11 +4,11 @@ package jmri.jmrix;
 
 
 /**
- * Returns a list of valid lenz XpressNet Connection Types
+ * Returns a list of valid Connection Types
  * <P>
  * @author      Bob Jacobsen   Copyright (C) 2010
  * @author      Kevin Dickerson    Copyright (C) 2010
- * @version	$Revision: 1.1 $
+ * @version	$Revision: 1.2 $
  *
  */
 public class OtherConnectionTypeList  implements jmri.jmrix.ConnectionTypeList {
