91b1b9c640d7ffdd4eef76955ed78add162052e8
Bob Jacobsen <jacobsen@mac.com>
2010-09-21 05:32:09 +0000

    annote away wanring due to migration and temporary deprecation

--- jmri/GuiLafConfigPane.java
+++ jmri/GuiLafConfigPane.java
@@ -6,10 +6,11 @@ package jmri;
  * Migration stand-in for {@link apps.GuiLafConfigPane}
  *
  * @author      Bob Jacobsen   Copyright (C) 2010
- * @version	$Revision: 1.11 $
+ * @version	$Revision: 1.12 $
  * @deprecated 2.9.5
  */
 @Deprecated
+@edu.umd.cs.findbugs.annotations.SuppressWarnings(value="NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")  // duplicate name OK while deprecated, for XML config migration
 public class GuiLafConfigPane extends apps.GuiLafConfigPane {
 }
 
