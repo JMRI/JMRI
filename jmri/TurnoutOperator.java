43934b6da968e34dfcc8fddb819175eec9f10deb
Bob Jacobsen <jacobsen@mac.com>
2010-09-21 05:43:29 +0000

    efficiency improvement:  make inner class static so it doesn't carry parent ref not needed

--- jmri/TurnoutOperator.java
+++ jmri/TurnoutOperator.java
@@ -21,17 +21,18 @@ public abstract class TurnoutOperator extends Thread {
 		setName("Operating turnout "+t.getSystemName());
 	}
 	
-	/**
-	 * Exception thrown when the turnout's operator has changed while the
-	 * operator is running. This implies that another operation has been
-	 * started and that this one should just quietly stop doing its thing.
-	 */
-	public class TurnoutOperatorException extends java.lang.Exception { }
-	
 	protected void operatorCheck() throws TurnoutOperatorException {
 		if (myTurnout.getCurrentOperator()!= this) {
 			throw new TurnoutOperatorException();
 		}
 	}
+
+	/**
+	 * Exception thrown when the turnout's operator has changed while the
+	 * operator is running. This implies that another operation has been
+	 * started and that this one should just quietly stop doing its thing.
+	 */
+	static public class TurnoutOperatorException extends java.lang.Exception { }
+	
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TurnoutOperator.class.getName());
 }
