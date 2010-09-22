99793f0f8e441881cd679b688e85662254e83a7b
Paul Bender <paul.bender@acm.org>
2010-09-22 04:40:11 +0000

    initial tests for srcp server.  The only active tests are a couple on the parser.

---
+++ test/jmri/jmris/srcp/parser/SRCPParserTest.java
@@ -0,0 +1,48 @@
+// SRCPParserTest.java
+
+package jmri.jmris.srcp.parser;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.TestCase;
+
+import java.io.StringReader;
+
+/**
+ * Tests for the {@link jmri.jmris.srcp.parser.SRCPParser} class.
+ * @author          Paul Bender
+ * @version         $Revision: 1.1 $
+ */
+public class SRCPParserTest extends TestCase {
+
+        public void testParseFailure() {
+           boolean exceptionOccured = false;
+           String code = "POWER SET\n\r";
+           SRCPParser p = new SRCPParser(new StringReader(code));
+           try {
+             p.command();
+           } catch(ParseException pe) {
+             exceptionOccured = true;
+           }
+           assertTrue(exceptionOccured);
+        }
+
+
+        // Main entry point
+        static public void main(String[] args) {
+                String[] testCaseName = {SRCPParserTest.class.getName()};
+                junit.swingui.TestRunner.main(testCaseName);
+        }
+
+        // test suite from all defined tests
+        public static Test suite() {
+        TestSuite suite = new TestSuite(SRCPParserTest.class);
+                return suite;
+
+        }
+    // The minimal setup for log4J
+    protected void setUp() { apps.tests.Log4JFixture.setUp(); }
+    protected void tearDown() { apps.tests.Log4JFixture.tearDown(); }
+
+}
