fcd55ca06727fedfb5bad22f4967f42f1db05306
Bob Jacobsen <jacobsen@mac.com>
2010-10-31 22:13:28 +0000

    add package-level annotations

---
+++ jmri/jmris/srcp/parser/package-info.java
@@ -0,0 +1,16 @@
+
+/**
+ * The contents of this package are automatically
+ * generated (except for the SRCPVistor class).
+ *
+ * We turn off selected FindBugs checking in the resulting code.
+ */
+@edu.umd.cs.findbugs.annotations.SuppressWarnings(
+    value="EI_EXPOSE_REP2 
+           MS_OOI_PKGPROTECT 
+           MS_PKGPROTECT 
+           NM_METHOD_NAMING_CONVENTION 
+           SBSC_USE_STRINGBUFFER_CONCATENATION", 
+    justification="Automatically generated code that we don't control"
+    )
+package jmri.jmris.srcp.parser;
