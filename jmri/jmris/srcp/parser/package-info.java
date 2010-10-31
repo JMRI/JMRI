a717654ec5d389c74fab83f9e9cf4dbd9ada4493
Bob Jacobsen <jacobsen@mac.com>
2010-10-31 22:18:05 +0000

    fix split string

--- jmri/jmris/srcp/parser/package-info.java
+++ jmri/jmris/srcp/parser/package-info.java
@@ -6,11 +6,7 @@
  * We turn off selected FindBugs checking in the resulting code.
  */
 @edu.umd.cs.findbugs.annotations.SuppressWarnings(
-    value="EI_EXPOSE_REP2 
-           MS_OOI_PKGPROTECT 
-           MS_PKGPROTECT 
-           NM_METHOD_NAMING_CONVENTION 
-           SBSC_USE_STRINGBUFFER_CONCATENATION", 
+    value="EI_EXPOSE_REP2 MS_OOI_PKGPROTECT MS_PKGPROTECT NM_METHOD_NAMING_CONVENTION SBSC_USE_STRINGBUFFER_CONCATENATION", 
     justification="Automatically generated code that we don't control"
     )
 package jmri.jmris.srcp.parser;
