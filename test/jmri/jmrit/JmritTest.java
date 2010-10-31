7d4b53c1471daa8568b5c503756c89e6473a996e
Bob Jacobsen <jacobsen@mac.com>
2010-10-31 02:45:14 +0000

    add OPath tests

--- test/jmri/jmrit/JmritTest.java
+++ test/jmri/jmrit/JmritTest.java
@@ -8,7 +8,7 @@ import junit.framework.*;
  * Invokes complete set of tests in the jmri.jmrit tree
  *
  * @author	    Bob Jacobsen  Copyright 2001, 2003
- * @version         $Revision: 1.30 $
+ * @version         $Revision: 1.31 $
  */
 public class JmritTest extends TestCase {
 
@@ -37,6 +37,7 @@ public class JmritTest extends TestCase {
         suite.addTest(jmri.jmrit.decoderdefn.DecoderDefnTest.suite());
         suite.addTest(jmri.jmrit.display.DisplayTest.suite());
         suite.addTest(jmri.jmrit.log.PackageTest.suite());
+        suite.addTest(jmri.jmrit.logix.PackageTest.suite());
         suite.addTest(jmri.jmrit.roster.RosterTest.suite());
         suite.addTest(jmri.jmrit.simpleclock.SimpleClockTest.suite());
         suite.addTest(jmri.jmrit.tracker.TrackerTest.suite());
