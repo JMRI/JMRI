658fbbcc92b4fb9648c39ee4acfa436ac65ff8ca
Bob Jacobsen <jacobsen@mac.com>
2010-10-31 02:45:27 +0000

    fix test form

--- test/jmri/jmrit/log/PackageTest.java
+++ test/jmri/jmrit/log/PackageTest.java
@@ -7,10 +7,24 @@ import junit.framework.*;
 /**
  * Invokes complete set of tests in the jmri.jmrit.log tree
  *
- * @author	    Bob Jacobsen  Copyright 2003
- * @version         $Revision: 1.2 $
+ * @author	    Bob Jacobsen  Copyright 2003, 2010
+ * @version         $Revision: 1.3 $
  */
 public class PackageTest extends TestCase {
+    
+    public void testShow() {
+        org.apache.log4j.Logger.getLogger("jmri.jmrix");
+        org.apache.log4j.Logger.getLogger("apps.foo");
+        org.apache.log4j.Logger.getLogger("jmri.util");
+
+        if (!System.getProperty("jmri.headlesstest","false").equals("true")) {
+    
+            try { new jmri.util.swing.JmriNamedPaneAction("Log4J Tree", 
+                new jmri.util.swing.sdi.JmriJFrameInterface(),
+                "jmri.jmrit.log.Log4JTreePane").actionPerformed(null);
+            } catch (Exception e) {}
+        }
+    }
     
     // from here down is testing infrastructure
     public PackageTest(String s) {
@@ -25,20 +39,7 @@ public class PackageTest extends TestCase {
 
     // test suite from all defined tests
     public static Test suite() {
-        TestSuite suite = new TestSuite("jmri.util.swing.PackageTest");   // no tests in this class itself
-
-        org.apache.log4j.Logger.getLogger("jmri.jmrix");
-        org.apache.log4j.Logger.getLogger("apps.foo");
-        org.apache.log4j.Logger.getLogger("jmri.util");
-
-        try { new jmri.util.swing.JmriNamedPaneAction("Log4J Tree", 
-            new jmri.util.swing.sdi.JmriJFrameInterface(),
-            "jmri.jmrit.log.Log4JTreePane").actionPerformed(null);
-        } catch (Exception e) {}
-        
-            //suite.addTest(jmri.util.swing.mdi.PackageTest.suite());
-
-        return suite;
+        return new TestSuite(PackageTest.class);
     }
 
     // The minimal setup for log4J
