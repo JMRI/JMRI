427102f2cd982c9ba84ed197d61cdb357d8f033a
Bob Jacobsen <jacobsen@mac.com>
2010-11-01 06:52:17 +0000

    proper syntax for multiple bugs

--- jmri/jmris/srcp/parser/package-info.java
+++ jmri/jmris/srcp/parser/package-info.java
@@ -3,14 +3,7 @@
  * The contents of this package are automatically
  * generated (except for the SRCPVistor class).
  *
- * We turn off selected FindBugs checking in the resulting code.
+ * We turn off selected FindBugs checking in the resulting generated code at the package level.
  */
-//@edu.umd.cs.findbugs.annotations.SuppressWarnings(
-//    value="EI_EXPOSE_REP2 MS_OOI_PKGPROTECT MS_PKGPROTECT NM_METHOD_NAMING_CONVENTION SBSC_USE_STRINGBUFFER_CONCATENATION", 
-//    justification="Automatically generated code that we don't control"
-//    )
-@edu.umd.cs.findbugs.annotations.SuppressWarnings(
-    value="EI_EXPOSE_REP2", 
-    justification="Automatically generated code that we don't control"
-    )
+@edu.umd.cs.findbugs.annotations.SuppressWarnings({"EI_EXPOSE_REP2", "MS_OOI_PKGPROTECT", "MS_PKGPROTECT", "NM_METHOD_NAMING_CONVENTION", "SBSC_USE_STRINGBUFFER_CONCATENATION"} )
 package jmri.jmris.srcp.parser;
