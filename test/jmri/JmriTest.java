c574f1f2f6e6440a8342ef6eb5823289c43d9493
Bob Jacobsen <jacobsen@mac.com>
2010-11-03 06:00:33 +0000

    include TurnoutTest.  Run in different order

--- test/jmri/JmriTest.java
+++ test/jmri/JmriTest.java
@@ -9,7 +9,7 @@ import junit.framework.TestSuite;
 /**
  * Invoke complete set of tests for the Jmri package
  * @author	Bob Jacobsen, Copyright (C) 2001, 2002, 2007
- * @version         $Revision: 1.25 $
+ * @version         $Revision: 1.26 $
  */
 public class JmriTest extends TestCase {
 
@@ -28,21 +28,23 @@ public class JmriTest extends TestCase {
     // test suite from all defined tests
     public static Test suite() {
         TestSuite suite = new TestSuite("jmri.JmriTest");  // no tests in this class itself
+
+		suite.addTest(jmri.BeanSettingTest.suite());
+		suite.addTest(jmri.BlockTest.suite());
+		suite.addTest(jmri.BlockManagerTest.suite());
+        suite.addTest(jmri.DccLocoAddressTest.suite());
 		suite.addTest(jmri.InstanceManagerTest.suite());
 		suite.addTest(jmri.LightTest.suite());
-		suite.addTest(jmri.BlockTest.suite());
-		suite.addTest(jmri.implementation.ImplementationTest.suite());
-		suite.addTest(jmri.BlockManagerTest.suite());
-		suite.addTest(jmri.BeanSettingTest.suite());
+        suite.addTest(jmri.NmraPacketTest.suite());
 		suite.addTest(jmri.PathTest.suite());
 		suite.addTest(jmri.PushbuttonPacketTest.suite());
-        suite.addTest(jmri.DccLocoAddressTest.suite());
+		suite.addTest(jmri.TurnoutTest.suite());
         
         if (!System.getProperty("jmri.headlesstest","false").equals("true"))
             suite.addTest(jmri.progdebugger.ProgDebuggerTest.suite());
         
-        suite.addTest(jmri.NmraPacketTest.suite());
         suite.addTest(jmri.configurexml.ConfigXmlTest.suite());
+		suite.addTest(jmri.implementation.ImplementationTest.suite());
         suite.addTest(jmri.managers.ManagersTest.suite());
         suite.addTest(jmri.jmrix.JmrixTest.suite());  // last due to threading issues?
         suite.addTest(jmri.jmrit.JmritTest.suite());  // last due to classloader issues?
