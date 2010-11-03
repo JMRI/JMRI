b522036e6260f172b5db15ea119f21d43795cb4a
Bob Jacobsen <jacobsen@mac.com>
2010-11-03 06:00:06 +0000

    check state values

--- test/jmri/LightTest.java
+++ test/jmri/LightTest.java
@@ -9,12 +9,20 @@ import junit.framework.TestSuite;
 
 /**
  * Tests for the Light class
- * @author	Bob Jacobsen  Copyright (C) 2008
- * @version $Revision: 1.1 $
+ * @author	Bob Jacobsen  Copyright (C) 2008, 2010
+ * @version $Revision: 1.2 $
  */
 public class LightTest extends TestCase {
 
-	public void testConstants() {
+    public void testStateConstants() {
+	    Assert.assertTrue("On and Off differ", (Light.ON&Light.OFF) == 0);
+	    Assert.assertTrue("On and Unknown differ", (Light.ON&Light.UNKNOWN) == 0);
+	    Assert.assertTrue("Off and Unknown differ", (Light.OFF&Light.UNKNOWN) == 0);
+	    Assert.assertTrue("On and Inconsistent differ", (Light.ON&Light.INCONSISTENT) == 0);
+	    Assert.assertTrue("Off and Inconsistent differ", (Light.OFF&Light.INCONSISTENT) == 0);
+    }
+    
+	public void testTransitionConstants() {
 	    Assert.assertTrue("On and INTERMEDIATE are bits", (Light.ON&Light.INTERMEDIATE) == 0);
         
 	    Assert.assertTrue("TRANSITIONINGTOFULLON overlap", (Light.TRANSITIONINGTOFULLON&Light.TRANSITIONING) != 0);
