058b88c3a71d4c21c2b416ce3157d1cce3f2ff5c
Kevin Dickerson <kev.public@ntlworld.com>
2010-11-16 19:02:29 +0000

    Refactored to use the jmri.jmrit.beantable

--- jmri/jmrix/ecos/swing/locodatabase/EcosLocoTableAction.java
+++ jmri/jmrix/ecos/swing/locodatabase/EcosLocoTableAction.java
@@ -1,14 +1,20 @@
 package jmri.jmrix.ecos.swing.locodatabase;
 
 
-
+import jmri.InstanceManager;
+import jmri.Manager;
 import java.awt.event.ActionEvent;
-
+import jmri.NamedBean;
 import jmri.jmrix.ecos.EcosLocoAddress;
 import jmri.jmrix.ecos.EcosLocoAddressManager;
 
+import jmri.jmrit.roster.Roster;
+import jmri.jmrit.roster.RosterEntry;
+import jmri.jmrix.ecos.utilities.EcosLocoToRoster;
+import java.util.List;
 
-public class EcosLocoTableAction extends AbstractTableAction {
+
+public class EcosLocoTableAction extends jmri.jmrit.beantable.AbstractTableAction {
 
     /**
      * Create an action with a specific title.
@@ -23,7 +29,22 @@ public class EcosLocoTableAction extends AbstractTableAction {
     }
     public EcosLocoTableAction() { this("Ecos Loco Table");}
     
-    void createModel() {
+    
+    public void actionPerformed(ActionEvent e) {
+        // create the JTable model, with changes for specific NamedBean
+        createModel();
+
+        // create the frame
+        f = new jmri.jmrit.beantable.BeanTableFrame(m, helpTarget()){
+        };
+        setMenuBar(f);
+        setTitle();
+        addToFrame(f);
+        f.pack();
+        f.setVisible(true);
+    }
+    
+    protected void createModel() {
     
         m = new EcosLocoTableDataModel() {
 
@@ -31,30 +52,63 @@ public class EcosLocoTableAction extends AbstractTableAction {
 
             public void clickOn(jmri.jmrix.ecos.EcosLocoAddressManager m) {}
             
-/*<<<<<<< EcosLocoTableAction.java
-            //public EcosLocoAddressManager getManager() {return (EcosLocoAddressManager)jmri.InstanceManager.getDefault(EcosLocoAddressManager.class);}
-            public EcosLocoAddressManager getManager() {return jmri.jmrix.ecos.EcosLocoAddressManager.instance();}
-            public EcosLocoAddress getByEcosObject(String object) {return getManager().getByEcosObject(object);}
-            //public EcosLocoAddress getByDccAddress(int address) {return ((EcosLocoAddressManager)jmri.InstanceManager.getDefault(EcosLocoAddressManager.class)).getByDccAddress(address);}
-            public EcosLocoAddress getByDccAddress(int address) {return getManager().getByDccAddress(address);}
-=======*/
             public EcosLocoAddressManager getManager() {return jmri.InstanceManager.getDefault(EcosLocoAddressManager.class);}
 
             public EcosLocoAddress getByEcosObject(String object) {return (jmri.InstanceManager.getDefault(EcosLocoAddressManager.class)).getByEcosObject(object);}
             public EcosLocoAddress getByDccAddress(int address) {return (jmri.InstanceManager.getDefault(EcosLocoAddressManager.class)).getByDccAddress(address);}
             
-//>>>>>>> 1.4
             public String getValue(String s) {
                 return "Set";
             }
-            /*public JButton configureButton() {
-                return new JButton(" Set ");
+            
+            public int getDisplayDeleteMsg() { return -1; }
+            public void setDisplayDeleteMsg(int boo) {  }
+            public void clickOn(jmri.NamedBean t) {
+            	// don't do anything on click; not used in this class, because 
+            	// we override setValueAt
+            }
+            
+            /*public void setValueAt(Object value, int row, int col) {
+                if (col==ROSTERIDCOL) {
+                    List<RosterEntry> l;
+                    if (value==null)
+                        return;
+                    if (value.equals(" ")){
+                        l = Roster.instance().getEntriesWithAttributeKeyValue("EcosObject", ecosObjectIdList.get(row));
+                        if(l.size()!=0){
+                            l.get(0).deleteAttribute("EcosObject");
+                            getByEcosObject(ecosObjectIdList.get(row)).setRosterId((String) value);
+                            l.get(0).updateFile();
+                        }
+                    } else{
+                        l = Roster.instance().matchingList(null, null, null, null, null, null, (String) value);
+                        for (int i = 0; i < l.size(); i++) {
+                            if ((l.get(i).getAttribute("EcosObject")==null)||(l.get(i).getAttribute("EcosObject").equals(""))){
+                                l.get(i).putAttribute("EcosObject", ecosObjectIdList.get(row));
+                                getByEcosObject(ecosObjectIdList.get(row)).setRosterId((String) value);
+                            } else{
+                                value=null;
+                                //return;
+                            }
+                            l.get(i).updateFile();
+                        }
+                    }
+                    fireTableRowsUpdated(row, row);
+                    Roster.writeRosterFile();
+
+                }  else if (col==ADDTOROSTERCOL) {
+                    // button fired, delete Bean
+                    addToRoster(row, col);
+                }
             }*/
+            
+            public NamedBean getBySystemName(String name) { return null;}
+            public NamedBean getByUserName(String name) { return null;}
         };
     }
 
     
-    void setTitle() {
+    protected void setTitle() {
         f.setTitle("Ecos Loco Table");
     }
     
@@ -62,9 +116,5 @@ public class EcosLocoTableAction extends AbstractTableAction {
         return "package.jmri.jmrix.ecos.ecosLocoTable";
     }
     
-    void savePressed(ActionEvent e){
-        //System.out.println("anything");
-        //jmri.jmrit.roster.Roster.writeRosterFile();
-    //This function isn't available.
-    }
+    protected void addPressed(ActionEvent e){ }
 }
--- jmri/jmrix/ecos/swing/locodatabase/EcosLocoTableDataModel.java
+++ jmri/jmrix/ecos/swing/locodatabase/EcosLocoTableDataModel.java
@@ -2,8 +2,6 @@
 
 package jmri.jmrix.ecos.swing.locodatabase;
 
-//import jmri.jmrix.ecos.EcosLocoAddressManager;
-
 import java.beans.PropertyChangeListener;
 import java.util.List;
 
@@ -19,25 +17,16 @@ import javax.swing.table.TableColumn;
 import jmri.jmrit.roster.Roster;
 import jmri.jmrit.roster.RosterEntry;
 import jmri.jmrix.ecos.utilities.EcosLocoToRoster;
-import jmri.util.table.ButtonEditor;
-import jmri.util.table.ButtonRenderer;
 
 /**
  * Table data model for display of jmri.jmrix.ecos.EcosLocoAddressManager manager contents
+ * This extends the BeanTableDataModel, but the majority of it is customised.
  * @author		Kevin Dickerson   Copyright (C) 2009
- * @version		$Revision: 1.8 $
+ * @version		$Revision: 1.9 $
  */
-abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractTableModel
-            implements PropertyChangeListener  {
+abstract public class EcosLocoTableDataModel extends jmri.jmrit.beantable.BeanTableDataModel {
 
-    static public final int ECOSOBJECTCOL  = 0;
-    static public final int ECOSDESCRIPTCOL = 1;
-    static public final int DCCADDRESSCOL = 2;
-    static public final int PROTOCOL = 4;
-    static public final int ROSTERIDCOL = 3;
-    static public final int PROTOCOLCOL = 4;
     static public final int ADDTOROSTERCOL = 5;
-
 
     static public final int NUMCOLUMN = 6;  //Set to 5 as the code behind ADDTOROSTERCOL isn't ready
 
@@ -82,7 +71,7 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
             // a value changed.  Find it, to avoid complete redraw
             String object = ((jmri.jmrix.ecos.EcosLocoAddress)e.getSource()).getEcosObject();
             //if (log.isDebugEnabled()) log.debug("Update cell "+ecosObjectIdList.indexOf(name)+","
-             //                                   +DCCADDRESSCOL+" for "+name);
+             //                                   +VALUECOL+" for "+name);
             // since we can add columns, the entire row is marked as updated
             int row = ecosObjectIdList.indexOf(object);
             fireTableRowsUpdated(row, row);
@@ -94,10 +83,7 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
 	 * <P>
 	 * Note that events will come both from the jmri.jmrix.ecos.EcosLocoAddressManagers and also from the manager
 	 */
-    boolean matchPropertyName(java.beans.PropertyChangeEvent e) {
-//		return (e.getPropertyName().indexOf("Start Signal")>=0 || e.getPropertyName().indexOf("Appearance")>=0
-//		        || e.getPropertyName().indexOf("Finish Signal")>=0);
-    	//return (e.getPropertyName().indexOf("Start Signal")>=0 || e.getPropertyName().indexOf("Finish Signal")>=0);
+    protected boolean matchPropertyName(java.beans.PropertyChangeEvent e) {
         refreshSelections();
         return true;
     }
@@ -106,15 +92,13 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
         return ecosObjectIdList.size();
     }
 
-    public int getColumnCount( ){ return NUMCOLUMN;}
-
     public String getColumnName(int col) {
          switch (col) {
-        case ECOSOBJECTCOL: return "ECoS Object Id";
-        case ECOSDESCRIPTCOL: return "ECoS Descritpion";
-        case DCCADDRESSCOL: return "ECoS Address";
-        case ROSTERIDCOL: return "JMRI Roster Id";
-        case PROTOCOLCOL: return "ECOS Protocol";
+        case SYSNAMECOL: return "ECoS Object Id";
+        case USERNAMECOL: return "ECoS Descritpion";
+        case VALUECOL: return "ECoS Address";
+        case COMMENTCOL: return "JMRI Roster Id";
+        case DELETECOL: return "ECOS Protocol";
         case ADDTOROSTERCOL: return "Add to Roster";
         default: return "unknown";
         }
@@ -122,18 +106,15 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
 
     public Class<?> getColumnClass(int col) {
         switch (col) {
-        case ECOSOBJECTCOL:
-            return String.class;
-        case ECOSDESCRIPTCOL:
-            return String.class;
-        case ROSTERIDCOL:
-          return JComboBox.class;
-        case DCCADDRESSCOL:
-            return String.class;
-        case PROTOCOLCOL:
+        case SYSNAMECOL:
+        case USERNAMECOL:
+        case VALUECOL:
+        case DELETECOL:
             return String.class;
         case ADDTOROSTERCOL:
             return JButton.class;
+        case COMMENTCOL:
+          return JComboBox.class;
         default:
             return null;
         }
@@ -141,14 +122,10 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
 
     public boolean isCellEditable(int row, int col) {
         switch (col) {
-        case ECOSDESCRIPTCOL:
-            return false;
-        case DCCADDRESSCOL:
-            return false;
-        case ROSTERIDCOL:
+        case COMMENTCOL:
             return true;
         case ADDTOROSTERCOL:
-            if (getValueAt(row, ROSTERIDCOL)==null)
+            if (getValueAt(row, COMMENTCOL)==null)
                 return true;
             else
                 return false;
@@ -165,19 +142,16 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
     	}
         jmri.jmrix.ecos.EcosLocoAddress b;
         switch (col) {
-        case ECOSOBJECTCOL:
+        case SYSNAMECOL:
             return ecosObjectIdList.get(row);
-        case ECOSDESCRIPTCOL:  // return user name
+        case USERNAMECOL:  // return user name
             // sometimes, the TableSorter invokes this on rows that no longer exist, so we check
             b = getByEcosObject(ecosObjectIdList.get(row));
             return (b!=null) ? b.getEcosDescription() : null;
-        case DCCADDRESSCOL:  //
+        case VALUECOL:  //
             b = getByEcosObject(ecosObjectIdList.get(row));
             return (b!=null) ? b.getEcosLocoAddress() : null;
-        /*case PROTOCOL:
-            b = getByEcosObject((String)ecosObjectIdList.get(row));
-            return (b!=null) ? b.getProtocol() : null;*/
-        case ROSTERIDCOL:
+        case COMMENTCOL:
             b = getByEcosObject(ecosObjectIdList.get(row));
             if (b!=null){
                 if (b.getRosterId()!=null){
@@ -185,11 +159,11 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
                 }
             }
             return (b!=null) ? b.getRosterId() : null;
-        case PROTOCOLCOL:
+        case DELETECOL:
             b = getByEcosObject(ecosObjectIdList.get(row));
             return (b!=null) ? b.getProtocol() : null;
         case ADDTOROSTERCOL:  //
-            if (getValueAt(row, ROSTERIDCOL)==null)
+            if (getValueAt(row, COMMENTCOL)==null)
                 return "Add To Roster";
             else
                 return " ";
@@ -202,7 +176,7 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
     
     JComboBox selections;
     
-    public void setUpRosterIdCol(TableColumn Rosterid){
+    public void setUpCOMMENTCOL(TableColumn Rosterid){
         selections = Roster.instance().fullRosterComboBoxGlobal();
         selections.insertItemAt(" ",0);
         selections.setSelectedIndex(-1);
@@ -222,15 +196,15 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
     }
     public int getPreferredWidth(int col) {
         switch (col) {
-        case ECOSOBJECTCOL:
+        case SYSNAMECOL:
             return new JTextField(5).getPreferredSize().width;
-        case ROSTERIDCOL:
+        case COMMENTCOL:
             return 75;
-        case ECOSDESCRIPTCOL:
+        case USERNAMECOL:
             return new JTextField(20).getPreferredSize().width;
         case ADDTOROSTERCOL: // not actually used due to the configureTable, setColumnToHoldButton, configureButton
             return new JTextField(22).getPreferredSize().width;
-        case DCCADDRESSCOL: 
+        case VALUECOL: 
             return new JTextField(5).getPreferredSize().width;
         default:
         	//log.warn("Unexpected column in getPreferredWidth: "+col);
@@ -240,7 +214,7 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
 
     abstract public String getValue(String systemName);
 
-    abstract jmri.jmrix.ecos.EcosLocoAddressManager getManager();
+    abstract protected jmri.jmrix.ecos.EcosLocoAddressManager getManager();
 
     abstract jmri.jmrix.ecos.EcosLocoAddress getByEcosObject(String object);
     abstract jmri.jmrix.ecos.EcosLocoAddress getByDccAddress(int address);
@@ -248,7 +222,7 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
 
     public void setValueAt(Object value, int row, int col) {
     
-        if (col==ROSTERIDCOL) {
+        if (col==COMMENTCOL) {
             List<RosterEntry> l;
             if (value==null)
                 return;
@@ -276,7 +250,6 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
             Roster.writeRosterFile();
 
         }  else if (col==ADDTOROSTERCOL) {
-            // button fired, delete Bean
             addToRoster(row, col);
         }
     }
@@ -289,9 +262,6 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
             fireTableRowsUpdated(row, row);
         }
     }
-
-	boolean noWarnDelete = false;
-
     
     /**
      * Configure a table to have our standard rows and columns.
@@ -300,48 +270,25 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
      * @param table
      */
     public void configureTable(JTable table) {
-        // allow reordering of the columns
-        table.getTableHeader().setReorderingAllowed(true);
+        super.configureTable(table);
 
-        // have to shut off autoResizeMode to get horizontal scroll to work (JavaSwing p 541)
-        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-
-        // resize columns as requested
-        for (int i=0; i<table.getColumnCount(); i++) {
-            int width = getPreferredWidth(i);
-            table.getColumnModel().getColumn(i).setPreferredWidth(width);
-        }
-        table.sizeColumnsToFit(-1);
-        setUpRosterIdCol(table.getColumnModel().getColumn(3));
-        configAddToRosterColumn(table);// Remarked out until the code for the add to roster has been completed
-        
+        setUpCOMMENTCOL(table.getColumnModel().getColumn(COMMENTCOL));
+        configAddToRosterColumn(table);
     }
-
-   void configAddToRosterColumn(JTable table) {
+    
+    protected void configValueColumn(JTable table) {
+        // We don't have a delete Button!
+    }
+   
+    void configAddToRosterColumn(JTable table) {
         setColumnToHoldButton(table, ADDTOROSTERCOL, 
                 new JButton("Add to Roster"));
     }
     
-    /**
-     * Service method to setup a column so that it will hold a
-     * button for it's values
-     * @param table
-     * @param column
-     * @param sample Typical button, used for size
-     */
-    void setColumnToHoldButton(JTable table, int column, JButton sample) {
-        //TableColumnModel tcm = table.getColumnModel();
-        // install a button renderer & editor
-        ButtonRenderer buttonRenderer = new ButtonRenderer();
-		table.setDefaultRenderer(JButton.class,buttonRenderer);
-        TableCellEditor buttonEditor = new ButtonEditor(new JButton());
-		table.setDefaultEditor(JButton.class,buttonEditor);
-        // ensure the table rows, columns have enough room for buttons
-        table.setRowHeight(sample.getPreferredSize().height);
-        table.getColumnModel().getColumn(column)
-			.setPreferredWidth((sample.getPreferredSize().width)+4);
+    protected void configDeleteColumn(JTable table) {
+        // We don't have a delete Button!
     }
-
+    
     synchronized public void dispose() {
     //This needs to be sorted later.
         getManager().removePropertyChangeListener(this);
@@ -354,112 +301,6 @@ abstract public class EcosLocoTableDataModel extends javax.swing.table.AbstractT
         selections=null;
     }
     
-    /**
-     * Method to self print or print preview the table.
-     * Printed in equally sized columns across the page with headings and
-     * vertical lines between each column. Data is word wrapped within a column.
-     * Can handle data as strings, comboboxes or booleans
-     */
-/*    public void printTable(HardcopyWriter w) {
-        // determine the column size - evenly sized, with space between for lines
-        int columnSize = (w.getCharactersPerLine()- this.getColumnCount() - 1)/this.getColumnCount();
-        
-        // Draw horizontal dividing line
-        w.write(w.getCurrentLineNumber(), 0, w.getCurrentLineNumber(),
-              (columnSize+1)*this.getColumnCount());
-        
-        // print the column header labels
-        String[] columnStrings = new String[this.getColumnCount()];
-        // Put each column header in the array
-        for (int i = 0; i < this.getColumnCount(); i++){
-            columnStrings[i] = this.getColumnName(i);
-        }
-        w.setFontStyle(Font.BOLD);
-        printColumns(w, columnStrings, columnSize);
-        w.setFontStyle(0);
-        w.write(w.getCurrentLineNumber(), 0, w.getCurrentLineNumber(),
-                (columnSize+1)*this.getColumnCount());
-  
-        // now print each row of data
-        // create a base string the width of the column
-        String spaces = "";
-        for (int i = 0; i < columnSize; i++) {
-            spaces = spaces + " ";
-        }
-        for (int i = 0; i < this.getRowCount(); i++) {
-            for (int j = 0; j < this.getColumnCount(); j++) {
-                //check for special, non string contents
-                if (this.getValueAt(i, j) == null) {
-                    columnStrings[j] = spaces;
-                } else if (this.getValueAt(i, j)instanceof JComboBox){
-                        columnStrings[j] = (String)((JComboBox) this.getValueAt(i, j)).getSelectedItem();
-                    } else if (this.getValueAt(i, j)instanceof Boolean){
-                            columnStrings[j] = ( this.getValueAt(i, j)).toString();
-                        }else columnStrings[j] = (String) this.getValueAt(i, j);
-            }
-        printColumns(w, columnStrings, columnSize);
-        w.write(w.getCurrentLineNumber(), 0, w.getCurrentLineNumber(),
-                (columnSize+1)*this.getColumnCount());
-        }            
-        w.close();
-    }
-    
-    protected void printColumns(HardcopyWriter w, String columnStrings[], int columnSize) {
-        String columnString = "";
-        String lineString = "";
-        // create a base string the width of the column
-        String spaces = "";
-        for (int i = 0; i < columnSize; i++) {
-            spaces = spaces + " ";
-        }
-        // loop through each column
-        boolean complete = false;
-        while (!complete){
-            complete = true;
-            for (int i = 0; i < columnStrings.length; i++) {
-                // if the column string is too wide cut it at word boundary (valid delimiters are space, - and _)
-                // use the intial part of the text,pad it with spaces and place the remainder back in the array
-                // for further processing on next line
-                // if column string isn't too wide, pad it to column width with spaces if needed
-                if (columnStrings[i].length() > columnSize) {
-                    boolean noWord = true;
-                    for (int k = columnSize; k >= 1 ; k--) {
-                        if (columnStrings[i].substring(k-1,k).equals(" ") 
-                            || columnStrings[i].substring(k-1,k).equals("-")
-                            || columnStrings[i].substring(k-1,k).equals("_")) {
-                            columnString = columnStrings[i].substring(0,k) 
-                                + spaces.substring(columnStrings[i].substring(0,k).length());
-                            columnStrings[i] = columnStrings[i].substring(k);
-                            noWord = false;
-                            complete = false;
-                            break;
-                        }
-                    }
-                    if (noWord) {
-                        columnString = columnStrings[i].substring(0,columnSize);
-                        columnStrings[i] = columnStrings[i].substring(columnSize);
-                        complete = false;
-                    }
-                    
-                } else {
-                    columnString = columnStrings[i] + spaces.substring(columnStrings[i].length());
-                    columnStrings[i] = "";
-                }
-                lineString = lineString + columnString + " ";
-            }
-            try {
-                w.write(lineString);
-                //write vertical dividing lines
-                for (int i = 0; i < w.getCharactersPerLine(); i = i+columnSize+1) {
-                    w.write(w.getCurrentLineNumber(), i, w.getCurrentLineNumber() + 1, i);
-                }
-                lineString = "\n";
-                w.write(lineString);
-                lineString = "";
-            } catch (IOException e) { log.warn("error during printing: "+e);}
-        }
-    }*/
-
     static final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(EcosLocoTableDataModel.class.getName());
 
 }
