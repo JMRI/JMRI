691b7c2165a74cba3f12ab87b2de2dd69a9c941d
Bob Jacobsen <jacobsen@mac.com>
2010-12-13 13:08:11 +0000

    This commit was manufactured by cvs2svn to create tag 'nceupdate2010-start'.

--- scripts/LocoTools.bat
+++
@@ -1,3 +0,0 @@
-REM Start the LocoTools program from JMRI ($Revision: 1.51 $)
-
-LaunchJMRI.exe apps.LocoTools.LocoTools %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/PanelPro.bat
+++
@@ -1,3 +0,0 @@
-REM Start the PanelPro program from JMRI ($Revision: 1.52 $)
-
-LaunchJMRI.exe apps.PanelPro.PanelPro %1 %2 %3 %4 %5 %6 %7 %8 %9
