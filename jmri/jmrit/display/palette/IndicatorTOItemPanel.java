772f5d3ee5c1242d386affbdc8d333fdd4462ef8
Bob Jacobsen <jacobsen@mac.com>
2010-12-28 04:59:37 +0000

    make constant package level instead of public

--- jmri/jmrit/display/palette/IndicatorTOItemPanel.java
+++ jmri/jmrit/display/palette/IndicatorTOItemPanel.java
@@ -31,7 +31,7 @@ import jmri.jmrit.picker.PickPanel;
 */
 public class IndicatorTOItemPanel extends TableItemPanel {
 
-    public final static String[] STATUS_KEYS = {"ClearTrack", "OccupiedTrack", "PositionTrack", 
+    final static String[] STATUS_KEYS = {"ClearTrack", "OccupiedTrack", "PositionTrack", 
                             "AllocatedTrack", "DontUseTrack", "ErrorTrack"};
 
     private DetectionPanel  _detectPanel;
