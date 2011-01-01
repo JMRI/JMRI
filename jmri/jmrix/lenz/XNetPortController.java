e7c93d4a6d9866e29bce176117f91f6c452c4f18
Paul Bender <paul.bender@acm.org>
2011-01-01 01:54:01 +0000

    Change XNetPortController from implementation to interface.

--- jmri/jmrix/lenz/XNetPortController.java
+++ jmri/jmrix/lenz/XNetPortController.java
@@ -6,59 +6,32 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 /**
- * Abstract base for classes representing a XNet communications port
+ * interface for XNetPortController objects.
  * <p>
  *
- * @author			Bob Jacobsen    Copyright (C) 2001, 2008
- * @author			Paul Bender    Copyright (C) 2004,2010
- * @version			$Revision: 2.11 $
+ * @author			Paul Bender    Copyright (C) 2010
+ * @version			$Revision: 3.0 $
  */
-public abstract class XNetPortController extends jmri.jmrix.AbstractSerialPortController {
+public interface XNetPortController extends jmri.jmrix.PortAdapter {
 
-    public XNetPortController(){
-       super();
-       adaptermemo = new XNetSystemConnectionMemo();
-    }
-
-    // base class. Implementations will provide InputStream and OutputStream
-    // objects to XNetTrafficController classes, who in turn will deal in messages.    
-    // returns the InputStream from the port
-    public abstract DataInputStream getInputStream();
-    
-    // returns the outputStream to the port
-    public abstract DataOutputStream getOutputStream();
-    
     /**
      * Check that this object is ready to operate. This is a question
      * of configuration, not transient hardware status.
      */
-    public abstract boolean status();
+    public boolean status();
     
     /**
      * Can the port accept additional characters?  This might
      * go false for short intervals, but it might also stick
      * off if something goes wrong.
      */
-    public abstract boolean okToSend();
+    public boolean okToSend();
     
     /**
      * We need a way to say if the output buffer is empty or not
      */
-    public abstract void setOutputBufferEmpty(boolean s);
+    public void setOutputBufferEmpty(boolean s);
    
-
-    /* Option 2 is not currently used with RxTx 2.0.  In the past, it
-       was used for the "check buffer status when sending" If this is still set        in a configuration file, we need to handle it, but we are not writing it        to new configuration files. */
-    public String getCurrentOption2Setting() {
-        if(mOpt2==null) return("no");
-        else return mOpt2;
-    }
-
-    protected String [] validOption2 = new String[]{"yes", "no"};
-    protected boolean checkBuffer = false;
-
-    protected XNetSystemConnectionMemo adaptermemo = null;
-
 
 }
 
