b61919237d7651e83dad16aa505607fb5a16c3a4
Bob Jacobsen <jacobsen@mac.com>
2011-01-02 22:23:10 +0000

    James LeFevre added functions

--- jmri/jmrix/tmcc/SerialThrottle.java
+++ jmri/jmrix/tmcc/SerialThrottle.java
@@ -11,7 +11,7 @@ import jmri.jmrix.AbstractThrottle;
  * over 100 are considered long addresses. 
  *
  * @author	Bob Jacobsen  Copyright (C) 2001, 2006
- * @version     $Revision: 1.6 $
+ * @version     $Revision: 1.7 $
  */
 public class SerialThrottle extends AbstractThrottle
 {
@@ -35,12 +35,19 @@ public class SerialThrottle extends AbstractThrottle
         this.f7           = false;
         this.f8           = false;
         this.f9           = false;
-        this.f10           = false;
-        this.f11           = false;        
-		this.f12           = false;
-		this.f13           = false;
-		this.f14           = false;
-        this.address      = address;
+        this.f10          = false;
+        this.f11          = false;        
+		this.f12          = false;
+		this.f13          = false;
+		this.f14          = false;
+		this.f15          = false;        
+		this.f16          = false;
+		this.f17          = false;
+		this.f18          = false;
+		this.f19          = false;        
+		this.f20          = false;
+		this.f21          = false;
+		this.address      = address;
         this.isForward    = true;
 
     }
@@ -51,7 +58,7 @@ public class SerialThrottle extends AbstractThrottle
 
     public void setF0(boolean f0) {
         this.f0 = f0;
-        // AUX2 does headlight
+        // aux 2
         sendToLayout(0x000D + address.getNumber()*128);
     }
 
@@ -81,63 +88,106 @@ public class SerialThrottle extends AbstractThrottle
 
     public void setF5(boolean f5) {
         this.f5 = f5;
-		// aux 1 + 0
+		// 0
 		sendToLayout(0x0010 + address.getNumber()*128);
     }
 
     public void setF6(boolean f6) {
         this.f6 = f6;
-		// aux 1 + 1
+		// 1
 		sendToLayout(0x0011 + address.getNumber()*128);
     }
 
     public void setF7(boolean f7) {
         this.f7 = f7;
-		// aux 1 + 2
+		// 2
 		sendToLayout(0x0012 + address.getNumber()*128);
     }
 
     public void setF8(boolean f8) {
         this.f8 = f8;
-		// aux 1 + 3
+		// 3
 		sendToLayout(0x0013 + address.getNumber()*128);
     }
 
     public void setF9(boolean f9) {
         this.f9 = f9;
-		// aux 1 + 4
+		// 4
 		sendToLayout(0x0014 + address.getNumber()*128);
     }
 
     public void setF10(boolean f10) {
         this.f10 = f10;
-		// aux 1 + 5
+		// 5
 		sendToLayout(0x0015 + address.getNumber()*128);
     }
 
     public void setF11(boolean f11) {
         this.f11 = f11;
-		// aux 1 + 6
+		// 6
 		sendToLayout(0x0016 + address.getNumber()*128);
     }
 
     public void setF12(boolean f12) {
         this.f12 = f12;
-		// aux 1 + 7
+		// 7
 		sendToLayout(0x0017 + address.getNumber()*128);
     }
 	
     public void setF13(boolean f13) {
         this.f13 = f13;
-		// aux 1 + 8
+		// 8
 		sendToLayout(0x0018 + address.getNumber()*128);
 	}
 	
 	public void setF14(boolean f14) {
 		this.f14 = f14;
-		// aux 1 + 9
+		// 9
 		sendToLayout(0x0019 + address.getNumber()*128);
 	}
+	public void setF15(boolean f15) {
+		this.f15 = f15;
+		// aux 1
+		sendToLayout(0x0009 + address.getNumber()*128);
+	}
+	
+	public void setF16(boolean f16) {
+		this.f16 = f16;
+		// letoff sound
+		sendToLayout(0x001E + address.getNumber()*128);
+	}
+	
+	public void setF17(boolean f17) {
+		this.f17 = f17;
+		// forward direction
+		sendToLayout(0x0000 + address.getNumber()*128);
+	}
+	
+	public void setF18(boolean f18) {
+		this.f18 = f18;
+		// reverse direction
+		sendToLayout(0x0003 + address.getNumber()*128);
+	}
+	public void setF19(boolean f19) {
+		this.f19 = f19;
+		// toggle direction
+		sendToLayout(0x0003 + address.getNumber()*128);
+	}
+	public void setF20(boolean f20) {
+		this.f20 = f20;
+		// boost
+		sendToLayout(0x0004 + address.getNumber()*128);
+	}
+	
+	public void setF21(boolean f21) {
+		this.f21 = f21;
+		// brake
+		sendToLayout(0x0007 + address.getNumber()*128);
+	}
+	
+	
+	
+	
 
     /**
      * Set the speed
