cbc1d46f7adb0a4727f6369c36f8f7ba4986cdd0
Paul Bender <paul.bender@acm.org>
2011-01-05 04:32:43 +0000

    remove refrences to Frames in the adapters.

--- test/jmri/jmrix/lenz/XNetTurnoutTest.java
+++ test/jmri/jmrix/lenz/XNetTurnoutTest.java
@@ -9,7 +9,7 @@ import junit.framework.TestSuite;
 /**
  * Tests for the {@link jmri.jmrix.lenz.XNetTurnout} class.
  * @author	    Bob Jacobsen
- * @version         $Revision: 2.6 $
+ * @version         $Revision: 2.7 $
  */
 public class XNetTurnoutTest extends jmri.implementation.AbstractTurnoutTest {
 
@@ -32,6 +32,7 @@ public class XNetTurnoutTest extends jmri.implementation.AbstractTurnoutTest {
 	}
 
 	public void checkIncoming() {
+                t.setFeedbackMode(jmri.Turnout.MONITORING);
 		// notify the object that somebody else changed it...
 		XNetReply m = new XNetReply();
 		m.setElement(0, 0x42);
@@ -39,7 +40,7 @@ public class XNetTurnoutTest extends jmri.implementation.AbstractTurnoutTest {
 		m.setElement(2, 0x04);     // set CLOSED
 		m.setElement(3, 0x43);
 		lnis.sendTestMessage(m);
-		Assert.assertTrue(t.getCommandedState() == jmri.Turnout.CLOSED);
+		Assert.assertTrue(t.getKnownState() == jmri.Turnout.CLOSED);
 
 		m = new XNetReply();
 		m.setElement(0, 0x42);
@@ -47,11 +48,12 @@ public class XNetTurnoutTest extends jmri.implementation.AbstractTurnoutTest {
 		m.setElement(2, 0x08);     // set THROWN
 		m.setElement(3, 0x4F);
 		lnis.sendTestMessage(m);
-		Assert.assertTrue(t.getCommandedState() == jmri.Turnout.THROWN);
+		Assert.assertTrue(t.getKnownState() == jmri.Turnout.THROWN);
 	}
 
-	// XNetTurnout test for incoming status message
-	public void testXNetTurnoutStatusMsg() {
+	// Test the XNetTurnout message sequence.
+	public void testXNetTurnoutMsgSequence() {
+                t.setFeedbackMode(jmri.Turnout.EXACT);
 		// prepare an interface
 		// set closed
 		try {
@@ -60,16 +62,62 @@ public class XNetTurnoutTest extends jmri.implementation.AbstractTurnoutTest {
 		}
 		Assert.assertTrue(t.getCommandedState() == jmri.Turnout.CLOSED);
 
-		// notify that somebody else changed it...
+		Assert.assertEquals("on message sent","52 05 88 DF",
+                lnis.outbound.elementAt(lnis.outbound.size()-1).toString());
+ 
+		// notify that the command station received the reply
 		XNetReply m = new XNetReply();
 		m.setElement(0, 0x42);
 		m.setElement(1, 0x05);
 		m.setElement(2, 0x04);     // set CLOSED
 		m.setElement(3, 0x43);
-		lnis.sendTestMessage(m);
-		Assert.assertTrue(t.getCommandedState() == jmri.Turnout.CLOSED);
+                ((jmri.jmrix.lenz.XNetTurnout) t).message(m);
 
+		
+                Assert.assertEquals("off message sent","52 05 80 D7",
+                lnis.outbound.elementAt(lnis.outbound.size()-1).toString());
+
+                // the turnout will not set its state until it sees an OK message.
+		m = new XNetReply();
+                m.setElement(0,0x01);
+                m.setElement(1,0x04);
+                m.setElement(2,0x05);
+                ((jmri.jmrix.lenz.XNetTurnout) t).message(m);
+
+		m = new XNetReply();
+                m.setElement(0,0x01);
+                m.setElement(1,0x04);
+                m.setElement(2,0x05);
+                ((jmri.jmrix.lenz.XNetTurnout) t).message(m);
+
+                Assert.assertEquals("off message sent","52 05 80 D7",
+                lnis.outbound.elementAt(lnis.outbound.size()-1).toString());
+
+		Assert.assertTrue(t.getKnownState() == jmri.Turnout.CLOSED);
 	}
+
+        // Test that property change events are properly sent from the parent
+        // to the propertyChange listener (this handles events for one sensor 
+        // and twosensor feedback).
+	public void testXNetTurnoutPropertyChange() {
+		// prepare an interface
+		// set thrown
+		try {
+			t.setCommandedState(jmri.Turnout.THROWN);
+		} catch (Exception e) { log.error("TO exception: "+e);
+		}
+		Assert.assertTrue(t.getCommandedState() == jmri.Turnout.THROWN);
+
+                t.setFeedbackMode(jmri.Turnout.ONESENSOR);
+                jmri.Sensor s = jmri.InstanceManager.sensorManagerInstance().provideSensor("IS1");
+                t.provideFirstFeedbackSensor(s);
+                try {
+                    s.setState(jmri.Sensor.ACTIVE);
+                } catch (Exception x) {log.error("TO exception: " +x);
+                } 
+                // check to see if the turnout state changes.
+		Assert.assertTrue(t.getKnownState() == jmri.Turnout.THROWN);
+        }
 
 
 	// from here down is testing infrastructure
