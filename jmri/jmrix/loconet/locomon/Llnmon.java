a230de2bb7225d0d1b349e914d561738f1ae4908
Ken Cameron <kcameron@twcny.rr.com>
2011-01-07 04:16:16 +0000

    Added some more codes so DCS51 replies look like we expect from other command stations.

--- jmri/jmrix/loconet/locomon/Llnmon.java
+++ jmri/jmrix/loconet/locomon/Llnmon.java
@@ -35,7 +35,7 @@ import jmri.util.StringUtil;
  * used with permission.
  *
  * @author			Bob Jacobsen  Copyright 2001, 2002, 2003
- * @version			$Revision: 1.47 $
+ * @version			$Revision: 1.48 $
  */
 public class Llnmon {
 
@@ -362,7 +362,7 @@ public class Llnmon {
                     return "LONG_ACK: The Slot Write command was rejected\n";
                 } else if (ack1 == 0x01) {
                     return "LONG_ACK: The Slot Write command was accepted\n";
-                } else if (ack1 == 0x23) {
+                } else if (ack1 == 0x23 || ack1 == 0x2b || ack1 == 0x6B) {
                     return "LONG_ACK: DCS51 programming reply, thought to mean OK\n";
                 } else if (ack1 == 0x40) {
                     return "LONG_ACK: The Slot Write command was accepted blind (no response will be sent)\n";
