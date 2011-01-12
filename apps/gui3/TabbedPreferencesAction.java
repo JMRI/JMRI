1b1449f58971be95db81de04015474a73fc073cc
Matthew Harris <matthew.john.harris@gmail.com>
2011-01-12 19:41:18 +0000

    Fix GUI anomaly on Tabbed Preferences window

--- apps/gui3/TabbedPreferencesAction.java
+++ apps/gui3/TabbedPreferencesAction.java
@@ -2,13 +2,14 @@ package apps.gui3;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.SwingUtilities;
 
     /**
      * Tabbed Preferences Action for dealing with all the preferences in a single view
      * with a list option to the left hand side.
      * <P>
      * @author	Kevin Dickerson   Copyright (C) 2009
-     * @version	$Revision: 1.2 $
+     * @version	$Revision: 1.3 $
      */
 
 public class TabbedPreferencesAction extends AbstractAction {
@@ -52,6 +53,10 @@ public class TabbedPreferencesAction extends AbstractAction {
             jmri.InstanceManager.tabbedPreferencesInstance().init();
             f = new TabbedPreferencesFrame(){
             };
+            // Update the GUI Look and Feel
+            // This is needed as certain controls are instantiated
+            // prior to the setup of the Look and Feel
+            SwingUtilities.updateComponentTreeUI(f);
         }
         
         f.gotoPreferenceItem(preferencesItem, preferenceSubCat);
