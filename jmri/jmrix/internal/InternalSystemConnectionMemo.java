084bfb46002987cec56a1c6373ea90175fb3450b
Kevin Dickerson <kev.public@ntlworld.com>
2011-02-21 23:11:10 +0000

    no message

--- jmri/jmrix/internal/InternalSystemConnectionMemo.java
+++ jmri/jmrix/internal/InternalSystemConnectionMemo.java
@@ -13,7 +13,7 @@ import jmri.InstanceManager;
  * particular system.
  *
  * @author		Bob Jacobsen  Copyright (C) 2010
- * @version             $Revision: 1.5 $
+ * @version             $Revision: 1.6 $
  */
 public class InternalSystemConnectionMemo extends jmri.jmrix.SystemConnectionMemo {
 
@@ -77,8 +77,9 @@ public class InternalSystemConnectionMemo extends jmri.jmrix.SystemConnectionMem
             return true;
         return false; // nothing, by default
     }
-    
-        public <T> T get(Class<?> T) {
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(Class<?> T) {
         if (getDisabled())
             return null;
         if (T.equals(jmri.ProgrammerManager.class))
