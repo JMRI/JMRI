bf30b164078fc16fb6631adb9b89c66bdc954d0a
Matthew Harris <matthew.john.harris@gmail.com>
2011-04-10 14:40:35 +0000

    Update due to improved Speedometer API

--- scripts/jython/SpeedometerInitExample.py
+++ scripts/jython/SpeedometerInitExample.py
@@ -5,10 +5,9 @@
 # Part of the JMRI distribution
 #
 # The next line is maintained by CVS, please don't change it
-# $Revision: 1.1 $
+# $Revision: 1.2 $
 
 from jmri.jmrit.speedometer import SpeedometerFrame
-from javax.swing import JPanel, JTextField, JRadioButton, JButton
 
 # Define the sensors
 startSensor = "IS1"
@@ -35,45 +34,14 @@ sf = SpeedometerFrame()
 # Set the sensors and distances
 sf.setInputs(startSensor, stopSensor1, stopSensor2, distance1, distance2)
 
-# Set other parameters
-#
-# This is a rather 'hacky' way of doing it as the SpeedometerFrame object
-# does not publicly expose these settings so we need to directly manipulate
-# the components in the frame - yuck!!
-#
-# Loop through components...
-for comp in sf.contentPane.components:
-    temp = JPanel()
-    sensor = ""
-    # Search for a JPanel
-    if comp.getClass() == temp.getClass():
-        # Loop through components in this panel
-        for sComp in comp.components:
-            # Search for a JTextField
-            temp = JTextField()
-            if sComp.getClass() == temp.getClass():
-                # Found JTextField - now grab the contents
-                sensor = sComp.getText()
-            # Search for a JRadioButton
-            temp = JRadioButton()
-            if (sComp.getClass() == temp.getClass()):
-                # Found JRadioButton - is it the 'exit' one?
-                if (sComp.getText() == "exit"):
-                    # Yes, so set it if required
-                    if sensor == startSensor:
-                        sComp.setSelected(startSensorExit)
-                    elif sensor == stopSensor1:
-                        sComp.setSelected(stopSensor1Exit)
-                    elif sensor == stopSensor2:
-                        sComp.setSelected(stopSensor2Exit)
-            # Search for a JButton
-            temp = JButton()
-            if sComp.getClass() == temp.getClass():
-                # Found JButton - is it the 'To metric units' one?
-                if sComp.getText() == "To metric units":
-                    # Yes, so activate the JButton if necessary
-                    if isMetric==True:
-                        sComp.doClick()
+# Set the sensor change parameters
+# Done this way round as setInputBehavior requires the inverse of our parameter
+# settings.
+# Useful as an example of how to achieve boolean inversion.
+sf.setInputBehavior((startSensorExit==False), (stopSensor1Exit==False), (stopSensor2Exit==False))
+
+# Set unit parameters
+sf.setUnitsMetric(isMetric)
 
 # Finally, show on screen
 sf.setVisible(True)
