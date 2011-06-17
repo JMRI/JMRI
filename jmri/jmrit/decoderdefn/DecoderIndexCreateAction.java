7182b1bf46c44d032ce7a94a147f72bcc46139f3
Bob Jacobsen <jacobsen@mac.com>
2011-06-17 05:45:59 +0000

    add main() for standalone

--- jmri/jmrit/decoderdefn/DecoderIndexCreateAction.java
+++ jmri/jmrit/decoderdefn/DecoderIndexCreateAction.java
@@ -9,8 +9,8 @@ import javax.swing.AbstractAction;
 /**
  * Check the names in an XML decoder file against the names.xml definitions
  *
- * @author	Bob Jacobsen   Copyright (C) 2001
- * @version	$Revision: 1.6 $
+ * @author	Bob Jacobsen   Copyright (C) 2001, 2011
+ * @version	$Revision: 1.7 $
  * @see         jmri.jmrit.XmlFile
  */
 public class DecoderIndexCreateAction extends AbstractAction {
@@ -25,5 +25,20 @@ public class DecoderIndexCreateAction extends AbstractAction {
 
     // initialize logging
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(DecoderIndexCreateAction.class.getName());
+
+    // main entry point to run standalone
+    static public void main(String[] args) {
+        String logFile = "default.lcf";
+        try {
+            if (new java.io.File(logFile).canRead()) {
+                org.apache.log4j.PropertyConfigurator.configure("default.lcf");
+            } else {
+                org.apache.log4j.BasicConfigurator.configure();
+            }
+        }
+        catch (java.lang.NoSuchMethodError e) { System.out.println("Exception starting logging: "+e); }
+
+        (new DecoderIndexCreateAction(null)).actionPerformed(null);
+    }
 
 }
