3302e8f9b4be02f74376a487fc69bc2300f32feb
Andrew Crosland <andrewcrosland@talktalk.net>
2011-06-21 18:44:19 +0000

    Change CBUS event capture menu entry to match tool frame title

--- jmri/jmrix/can/cbus/CbusMenu.java
+++ jmri/jmrix/can/cbus/CbusMenu.java
@@ -11,7 +11,7 @@ import javax.swing.JMenu;
  *
  * @author	    Bob Jacobsen   Copyright 2003, 2008, 2009
  * @author      Andrew Crosland 2008
- * @version     $Revision: 1.2 $
+ * @version     $Revision: 1.3 $
  */
 public class CbusMenu extends JMenu {
     public CbusMenu(String name) {
@@ -29,7 +29,9 @@ public class CbusMenu extends JMenu {
 
         add(new jmri.jmrix.can.cbus.swing.console.CbusConsoleAction(rb.getString("MenuItemConsole")));
         add(new jmri.jmrix.can.swing.send.CanSendAction(rb.getString("MenuItemSendFrame")));
-        add(new jmri.jmrix.can.cbus.swing.configtool.ConfigToolAction(rb.getString("MenuItemConfigTool")));
+        add(new jmri.jmrix.can.cbus.swing.configtool.ConfigToolAction(ResourceBundle
+                .getBundle("jmri.jmrix.can.cbus.swing.configtool.ConfigToolBundle")
+                        .getString("MenuItemConfigTool")));
         add(new jmri.jmrix.can.cbus.swing.eventtable.CbusEventTableAction(rb.getString("MenuItemEventTable")));
 
         add(new jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolAction("Node Config Tool"));
