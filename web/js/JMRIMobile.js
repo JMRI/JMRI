8646f4797be144da3d3bb87a771dcabe3195731d
Steve Todd <mstevetodd@mstevetodd.com>
2011-06-23 19:01:16 +0000

    JMRIMobile speed improvements

--- web/js/JMRIMobile.js
+++ web/js/JMRIMobile.js
@@ -1,55 +1,91 @@
+/**********************************************************************************************
+*  JMRI Mobile - Browser interface to JMRI operational functions
+*  
+*  Written using jQuery Mobile and Templates, ajax calls for retrieving the data from JMRI XMLIO servlet
+*  
+*  HTML page contains templates for "pages" and list items, with actual values replaced by template processing
+*  
+*  Uses two connections to XMLIO server:
+*    1) a "monitoring" connection that sends a full list of items to monitor, and waits for the server
+*          to reply with a new list when anything changes.  This new list is then sent back as the basis for 
+*          starting a new monitoring connection
+*     2) a "change" connection which only sends "set" commands based on user input  
+*
+**********************************************************************************************/
+
+//TODO: fix memory variable monitoring (sending constantly now)
+//TODO: checking a new function creates a new, duplicate connection (not needed)
+//TODO: turn off "waiting" message even when no changes made
+//TODO: track current values and only update the html for values which really changed
 //TODO: send periodic request for refresh, to verify server connection (maybe do on server side as well?)
-//TODO: handle ajax errors
-//TODO: preserve filter on update
-//TODO: use array for globalXhr to support limited number of open requests
-//TODO: (long-term) read panel xml and "draw" panels on page
+//TODO: preserve/reapply filters on update
 //TODO: "wide-screen" version that shows multiple "pages" at once, for use on wider browsers
 //TODO: add edit of memory variable values
 //TODO: support addition of memory variables, maybe turnouts?
 //TODO: remove "page" and button on uncheck (maybe just force refresh?)
+//TODO: handle ajax errors, only alert on disconnect?
+//TODO: (long-term) read panel xml and "draw" panels on page
+//TODO: set static parms as defaults in ajaxSetup()
 
-var $globalXhr; //global variable to allow closing earlier connections 
+var $gXhrList; //persistent  variable to allow closing previous list connection 
+var $gPrevType = ""; //persistent variable to help refresh views only on change 
+var $gLastLogMsgTime =new Date().getTime();  //save last time (for logging elapsed time in debug messages)
 
-//handle button press, send request for immediate change, plus request for lists  TODO: allow user to turn off some of the lists
+//handle button press, send request for immediate change 
 var $sendChange = function($type, $name, $nextValue){
-	$.mobile.pageLoading();  //show pageloading message
+	$.mobile.showPageLoadingMsg();  //show pageloading message
 	var $commandstr = '<xmlio><item><type>' + $type + '</type><name>' + $name + '</name><set>' + $nextValue +'</set></item>' 
-	+  $getXMLListCommands(false) + '</xmlio>';
-	$sendXMLIO($commandstr);
+	+ '</xmlio>';
+	$sendXMLIOChg($commandstr);
 };
 
-
-var $sendXMLIO = function($commandstr){
-
-	//kill the pending requests  TODO: find way to reuse connections instead of killing them 
-	if ($globalXhr) {
-		$globalXhr.abort();
-	}
-	var $success = "";
-	$globalXhr = $.ajax({  //remember the request, to kill it later if needed
+//send a command to the server, and setup callback to process the response (used for lists)
+var $sendXMLIOList = function($commandstr){
+	$gXHRList = $.ajax({ 
 		type: 'POST',
 		url:  '/xmlio',
 		data: $commandstr,
 		success: function($r, $s, $x){
-			$processResponse($r, $s, $x); //set up callback
+			$processResponse($r, $s, $x); //handle returned data
 		},
 		async: true,
 		dataType: 'xml' //<--dataType
 	});
 };
 
-var $processResponse = function($returnedData, $success, $xhr) {
+//send a command to the server, and ignore the response (used for change commands)
+var $sendXMLIOChg = function($commandstr){
+	$.ajax({  
+		type: 'POST',
+		url:  '/xmlio',
+		data: $commandstr,
+		success: function($r, $s, $x){
+			//ignore this response
+		},
+		async: true,
+		dataType: 'xml' //<--dataType
+	});
+};
 
-	$.mobile.pageLoading();  //show pageloading message
+//process the response returned for the "list" command
+var $processResponse = function($returnedData, $success, $xhr) {
+	$logMsg('starting processresponse');	
+
+	$.mobile.showPageLoadingMsg();  //show pageloading message
 
 	var $xml = $($returnedData);  //jQuery-ize returned data for easier access
+
+//	$logMsg('calling xmlclean');
 	$xml.xmlClean();  //remove whitespace 
 
+	$logMsg('------before find each item loop');
 	$xml.find('item').each( //find and process all "item" entries (list)
 			function() {
 				//put data from current xml items into $currentItem object
 				var $currentItem = {};
 
+				$logMsg('before copy to currentitem loop');
+				//copy current item into #currentItem variable for easier reference
 				for (var $i=0; $i < this.childNodes.length; $i++) {
 					if (this.childNodes[$i].nodeName != "#text") { //skip empty elements (whitespace, etc.)
 						if (this.childNodes[$i].textContent) {
@@ -59,59 +95,93 @@ var $processResponse = function($returnedData, $success, $xhr) {
 						}
 					}
 				}
+				$logMsg('after copy to currentitem loop');
 				var $type = $currentItem.type;  //shortcut since this is used so many times
+
+				//reapply jqm formatting to previous page, if current page is different
+				if ($type != $gPrevType) {
+					if ($gPrevType != "") {
+						$logMsg('before refreshing listview for ' + $gPrevType);
+						$("#type-" + $gPrevType + " ul.listview").listview("refresh");
+						$logMsg('after refreshing listview');
+					}
+					$gPrevType = $type;
+				}
+
 				//add nextValue from current value
+				$logMsg('before setting next and valuetext');
 				if ($currentItem.value) {
 					$currentItem.nextValue = $getNextValue($currentItem.type, $currentItem.value); 
 					$currentItem.valueText = $getValueText($currentItem.type, $currentItem.value); 
 				}
+				$logMsg('before setting safe name');
 				//include a "safe" version of name for use as ID   TODO: other cleanup needed?
 				$currentItem.safeName = $currentItem.name.replace(/:/g, "_").replace(/ /g, "_");
+				$logMsg('after setting safe name');
 
 				//remove non-monitorable from xml
 				if ($type == 'roster' || $type == 'panel') {
 					$(this).remove();
 				}
 
+				$logMsg('before looking for page');
 				//if a "page" of this type doesn't exist yet, create it, and add menu buttons to all
-				if (!$("div#type-" + $type).length) {
+				if (!$("#type-" + $type).length) {
+					$logMsg('before adding a new page');
 					//add the new page, following the settings page  TODO: support specific page templates
 					var $templateID = $getTemplate('Page', $type);
-					$("div#settings").after($($templateID).tmpl({type: $type}));
+					$("#settings").after($($templateID).tmpl({type: $type}));
 					//add the menu item _inside_ footer on each page
-					$("div# div#footer").append("<a data-role='button' href='#type-" + $type + "' data-theme='b'>" + $type +"</a>");
+					$("#footer").append("<a data-role='button' href='#type-" + $type + "' data-theme='b'>" + $type +"</a>");
 					//make sure the buttons have correct mobile formatting
-					$("div#settings div#footer").find('[data-role="button"]').not('.ui-btn').buttonMarkup();
+					$("#settings #footer").find('[data-role="button"]').not('.ui-btn').buttonMarkup();
 
 					//render the changes to settings page, and then the new page
-					$("div#settings").page();
-					$("div#type-" + $type).page();
+					$("#settings").page();
+					$("#type-" + $type).page();
 
 					//copy footer from settings to all pages
 					$("div#footer").html($("div#settings div#footer").html());
-				}
+				}  //end of adding new page
+				$logMsg('after looking for page');
 
 				//if a list item for this name, for this card, doesn't exist yet, add it or update existing item
-				var $index = 'div#type-' + $type + ' ul.listview li#name-' + $currentItem.safeName;
+				var $index = '#type-' + $type + ' ul.listview li#name-' + $currentItem.safeName;
 				var $templateID = $getTemplate('Item', $type);
+				$logMsg('before looking for item');
 				if (!$($index).length) {
-					$($templateID).tmpl($currentItem).appendTo("div#type-" + $type + " ul.listview");
-
+					$logMsg('after finding item');
+					$logMsg('before adding item');
+					$($templateID).tmpl($currentItem).appendTo("#type-" + $type + " ul.listview");
+					$logMsg('before refreshing listview');
 				} else {  //update this list item if already exists 
+					$logMsg('after finding item');
+					$logMsg('before updating item');
 					$($index).replaceWith($($templateID).tmpl($currentItem));
+					$logMsg('after updating item');
 				}
-				//apply mobile formatting to changed items
-				$("div#type-" + $type + " ul.listview").listview("refresh");
 			}
 	);
 
-	//update the string with any changes and cleanup		
+	//apply mobile formatting to last page
+	if ($gPrevType != "") {
+		$logMsg('before refreshing listview for last page ' + $gPrevType);
+		$("#type-" + $gPrevType + " ul.listview").listview("refresh");
+		$logMsg('after refreshing listview');
+	}
+
+	$gPrevType = "";
+
+	//update the string with changes (removed items) and cleanup		
+	$logMsg('calling xml2str for update');
 	var $xmlstr = xml2Str($xml[0]);
 
-	//echo last command received back to server, which will cause server to monitor for changes
-	$sendXMLIO($xmlstr);
+	//echo value list back to server, which will cause server to monitor for changes
+	$sendXMLIOList($xmlstr);
 
-	$.mobile.pageLoading(true); //hide the pageloading message
+	$.mobile.hidePageLoadingMsg(); //hide the pageloading message
+
+	$logMsg('end of processresponse');
 };    	
 
 //handle the toggling of the next value for buttons
@@ -123,7 +193,7 @@ var $getNextValue = function($type, $value){
 	return $nextValue;
 };
 
-//get name for template (which exists on html page)
+//get name for template (must already exist on html page)
 var $getTemplate = function( $cat, $type){
     var $tempateID = '';
 	//use specific item template if found, generic if not 
@@ -151,7 +221,7 @@ var $getValueText = function($type, $value){
 		} else {
 			return '<img src="/web/inControl/PowerGrey24.png">';
 		}
-	} else if ($type == 'memory') {
+	} else if ($type == 'memory' || ($type == 'metadata')) {
 		return $value;
 	}
 	return '???';
@@ -190,7 +260,7 @@ function xml2Str(xmlNode) {
 //get xml for list elements of selected types
 function $getXMLListCommands($includeMonitorables) {
 	var $cmdStr = '';
-	$.each($('div#includes input:checkbox:checked'),function(i,e){
+	$.each($('#includes input:checkbox:checked'),function(i,e){
 		var $t = $(e).attr("id").substring(8);
 		if ($includeMonitorables || ($t != 'roster' && $t != 'panel' ))  {
 			// add each checked value to list
@@ -203,7 +273,7 @@ function $getXMLListCommands($includeMonitorables) {
 //get array of checked types for persistent local storage
 function $getSettingsArray() {
 	var $arrInputs = [];
-	$.each($('div#includes input:checkbox:checked'), function(i,e){
+	$.each($('#includes input:checkbox:checked'), function(i,e){
 		var $t = $(e).attr("id").substring(8);
 		$arrInputs.push($t); //append to list
 	});
@@ -214,19 +284,32 @@ $(document).ajaxError(function(e,xhr,opt){
     if (window.console && window.console.log) {
         window.console.log("AJAX Error requesting " + opt.url + ", status= " + xhr.status + " " + xhr.statusText);
     }
+    if (xhr.statusText == "error") {
+    	$logMsg('status=' + xhr.status + " " + xhr.statusText);
+    }    
 });
+
+//output log messages to console log and to screen
+function $logMsg(msg) {
+/*	var elapsedTime = new Date().getTime() - $gLastLogMsgTime;
+	$('div.errorMessage').html(msg);
+	console.log(elapsedTime + " " + msg);
+	$gLastLogMsgTime = new Date().getTime();  //save last time
+*/
+}
 
 //javascript processing starts here (main)
 $(document).ready(function() {
 
 	//if trying to load a page with a hash (#), remove it and reload
 	//  needed because hash is used by jQueryMobile
+	//TODO: improve or remove this as jqm advances
 	var $hashLoc = location.href.indexOf("#"); 
 	if ( $hashLoc > -1) {
 	    location.assign(location.href.substr(0, $hashLoc));
 	}
 	
-	$.mobile.pageLoading();  //show pageloading message
+	$.mobile.showPageLoadingMsg();  //show pageloading message
 
 	//retrieve checked values from localstorage and set checkboxes to match
 	var $savedInputs = ["turnout","panel"];  //default selections
@@ -234,19 +317,20 @@ $(document).ready(function() {
 		$savedInputs=JSON.parse(localStorage['savedInputs']);
 	}
 	$.each($savedInputs, function(key, value) {  //check the boxes 
-		$('div#includes input:checkbox#include-' + value).attr('checked', true).checkboxradio("refresh"); 
+		$('#includes input:checkbox#include-' + value).attr('checked', true); 
 	});
 
-	//listen for changes to checkboxes, and resend updated list command on each
-	$('div#includes input:checkbox').change(function() {
-		$sendXMLIO('<xmlio>' + $getXMLListCommands(true) + '</xmlio>');
+	//listen for changes to checkboxes, and refresh page after a change
+	$('#includes input:checkbox').change(function() {
 		//save current settings
 		localStorage['savedInputs']=JSON.stringify($getSettingsArray());
+	    location.assign(location.href);  //jump to this page
 	});
 
-	//setup pages based on selected items
-	$sendXMLIO('<xmlio>' + $getXMLListCommands(true) + '</xmlio>');
+	$.mobile.hidePageLoadingMsg();  //hide pageloading message
 
-	$.mobile.pageLoading(true);  //hide pageloading message
+	//setup pages based on selected items
+	$sendXMLIOList('<xmlio>' + $getXMLListCommands(true) + '</xmlio>');
+
 
 });
