dc633a4426326873e2bd55075e5ea6f6ab6d75d2
Bob Jacobsen <jacobsen@mac.com>
2011-08-05 16:00:56 +0000

    status comment

--- jython/JmriDemo.py
+++ jython/JmriDemo.py
@@ -1,5 +1,10 @@
 # Python code to start a JmriDemo app, complete with menu bar
 #
+#
+# Obsolete: the usual method now is to start DecoderPro or PanelPro
+# and then run Python scripts from within it; left as
+# an example of the approach
+#
 # Author: Bob Jacobsen, copyright 2004
 # Part of the JMRI distribution
 #
