261a23cb9d65947d1767491e20f95d62a648889b
Randall Wood <randall.h.wood@alexandriasoftware.com>
2011-09-15 03:55:59 +0000

    Add ability to create roster groups with a selection of roster entries.

--- java/src/jmri/jmrit/roster/CreateRosterGroupAction.java
+++ java/src/jmri/jmrit/roster/CreateRosterGroupAction.java
@@ -50,12 +50,13 @@ public class CreateRosterGroupAction extends JmriAbstractAction {
     }
 
     Component _who;
+    String[] IDs;
 
     @Override
     public void actionPerformed(ActionEvent event) {
 
         String entry = (String)JOptionPane.showInputDialog(_who,
-                                     "<html><b>Create new roster group.</b><br>Roster group names cannot be changed once created.</html>",
+                                     "<html><b>Create new roster group</b></html>",
                                      "New Roster Group",
                                      JOptionPane.INFORMATION_MESSAGE,
                                      null, // icon
@@ -64,7 +65,20 @@ public class CreateRosterGroupAction extends JmriAbstractAction {
         if(entry == null || entry.equals(Roster.ALLENTRIES)){
             return;
         }
-
+        if (IDs != null) {
+            for (String ID : IDs) {
+                if (!"".equals(ID)) {
+                    RosterEntry re = Roster.instance().entryFromTitle(ID);
+                    if (re == null) {
+                        log.warn("Attempted to create RosterEntry from invalid title: " + ID);
+                    } else {
+                        log.debug("Adding RosterEntry " + ID + " to new group " + entry);
+                        re.putAttribute(Roster.instance().getRosterGroupPrefix() + entry, "yes");
+                        re.updateFile();
+                    }
+                }
+            }
+        }
         Roster.instance().addRosterGroupList(entry);
         Roster.writeRosterFile();
     }
@@ -73,7 +87,25 @@ public class CreateRosterGroupAction extends JmriAbstractAction {
     public jmri.util.swing.JmriPanel makePanel() {
         throw new IllegalArgumentException("Should not be invoked");
     }
-    
+
+    /**
+     * Set a String parameter
+     * <p>
+     * This method accepts the following key, with the following value:
+     * <dl>
+     * <dt>RosterEntryIDs</dt>
+     * <dd>A string of RosterEntry IDs separated by # characters.
+     * </dl>
+     * @param key
+     * @param value
+     */
+    @Override
+    public void setParameter(String key, String value) {
+        if (key.equals("RosterEntryIDs")) {
+            IDs = value.split("#");
+        }
+    }
+
     // initialize logging
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(CreateRosterGroupAction.class.getName());
 }
