d8f4213a73410b89425862765b52de56710c7ece
Paul Bender <paul.bender@acm.org>
2011-10-13 05:21:03 +0000

    add constructor test for SimpleServer

---
+++ java/test/jmri/jmris/simpeleserver/SimpleServerTest.java
@@ -0,0 +1,55 @@
+//SimpleServerTest.java
+
+package jmri.jmris.simpleserver;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Tests for the jmri.jmris.simpleserver package
+ * @author                      Paul Bender
+ * @version                     $Revision: 17977 $
+ */
+public class SimpleServerTest extends TestCase {
+
+    public void testCtor() {
+        SimpleServer a = new SimpleServer();
+        Assert.assertNotNull(a);
+    }
+
+    public void testCtorwithParameter() {
+        SimpleServer a = new SimpleServer(2048);
+        Assert.assertNotNull(a);
+    }
+
+
+    // from here down is testing infrastructure
+
+    public SimpleServerTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {SimpleServerTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(jmri.jmris.simpleserver.SimpleServerTest.class);
+//        suite.addTest(jmri.jmris.simpleserver.parser.SimpleServerParserTests.suite());
+
+        if (!System.getProperty("jmri.headlesstest","false").equals("true")) {
+           // put any tests that require a UI here.
+        }
+
+        return suite;
+    }
+
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SimpleServerTest.class.getName());
+
+}
+
