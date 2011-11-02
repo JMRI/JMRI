618ce64c190451c10e7856ba258d9a7e4d1c8988
Lionel Jeanson <hebbos@users.sourceforge.net>
2011-11-02 20:22:00 +0000

    Cleaning imports.

--- jython/Jynstruments/ThrottleWindowToolBar/WiimoteThrottle.jyn/WiimoteThrottle.py
+++ jython/Jynstruments/ThrottleWindowToolBar/WiimoteThrottle.jyn/WiimoteThrottle.py
@@ -38,8 +38,6 @@ valueSpeedIncrement = 0.05
 delay4double = 500 # delay for double tap on button (ms)
 
 import jmri.jmrit.jython.Jynstrument as Jynstrument
-import java.awt.CardLayout as CardLayout
-import jmri.util.swing.ResizableImagePanel as ResizableImagePanel
 import java.beans.PropertyChangeListener as PropertyChangeListener
 import jmri.jmrit.throttle.AddressListener as AddressListener
 import javax.swing.Timer as Timer
