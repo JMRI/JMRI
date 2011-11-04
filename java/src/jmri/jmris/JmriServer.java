6ff367afd93afe4c7a9fd1bd2587683cb389c53f
Randall Wood <randall.h.wood@alexandriasoftware.com>
2011-11-04 09:48:25 +0000

    Reformat to use consistent indentations and character spacing.

--- java/src/jmri/jmris/JmriServer.java
+++ java/src/jmri/jmris/JmriServer.java
@@ -1,5 +1,4 @@
 // JmriServer.java
-
 package jmri.jmris;
 
 import java.net.*;
@@ -18,175 +17,180 @@ import jmri.util.zeroconf.ZeroConfService;
  */
 public class JmriServer {
 
-     protected int portNo = 3000; // Port to listen to for new clients.
-     protected ServerSocket connectSocket;
-     protected ZeroConfService service;
-     private Thread listenThread = null;
-     private Vector<clientListener> connectedClientThreads = new Vector<clientListener>();
+    protected int portNo = 3000; // Port to listen to for new clients.
+    protected ServerSocket connectSocket;
+    protected ZeroConfService service;
+    private Thread listenThread = null;
+    private Vector<clientListener> connectedClientThreads = new Vector<clientListener>();
+    private static JmriServer _instance = null;
 
-     private static JmriServer _instance = null;
+    public synchronized static JmriServer instance() {
+        if (_instance == null) {
+            _instance = new JmriServer();
+        }
+        return _instance;
+    }
 
-     public synchronized static JmriServer instance(){
-         if(_instance==null) _instance=new JmriServer();
-         return _instance;
-     }
+    // Create a new server using the default port
+    public JmriServer() {
+        this(3000);
+    }
 
-     // Create a new server using the default port
-     public JmriServer() {
-	this(3000);
-     }
-
-     // Create a new server using a given port
-     public JmriServer(int port) {
-	super();
-	// Try registering the server on the given port
-	try {
-		connectSocket = new ServerSocket(port);
-	} catch (IOException e) {
-	    log.error("Failed to connect to port " + port);
-	}
-
-	this.portNo = port;
- 
+    // Create a new server using a given port
+    public JmriServer(int port) {
+        super();
+        // Try registering the server on the given port
+        try {
+            connectSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            log.error("Failed to connect to port " + port);
         }
 
-     // Maintain a vector of connected clients
+        this.portNo = port;
 
-     // Add a new client
-     private synchronized void addClient(clientListener client){
-	if(!connectedClientThreads.contains(client)) {
-		connectedClientThreads.addElement(client);
-                client.start();
-        }
-     }
+    }
 
-     //Remove a client
-     private synchronized void removeClient(clientListener client) {
-        if(connectedClientThreads.contains(client)) {
-                connectedClientThreads.removeElement(client);
+    // Maintain a vector of connected clients
+    // Add a new client
+    private synchronized void addClient(clientListener client) {
+        if (!connectedClientThreads.contains(client)) {
+            connectedClientThreads.addElement(client);
+            client.start();
         }
-     }
+    }
 
-     public void start(){
-	/* Start the server thread */
-        if(listenThread==null) {
-           listenThread = new Thread(new newClientListener(connectSocket));
-           listenThread.start();
-           advertise();
+    //Remove a client
+    private synchronized void removeClient(clientListener client) {
+        if (connectedClientThreads.contains(client)) {
+            connectedClientThreads.removeElement(client);
         }
-     }
-    
-     // Advertise the service with ZeroConf
-     protected void advertise(){
+    }
+
+    public void start() {
+        /* Start the server thread */
+        if (listenThread == null) {
+            listenThread = new Thread(new newClientListener(connectSocket));
+            listenThread.start();
+            advertise();
+        }
+    }
+
+    // Advertise the service with ZeroConf
+    protected void advertise() {
         service = ZeroConfService.create("_jmri._tcp.local.", portNo);
         service.publish();
-           }
+    }
 
-     @SuppressWarnings("deprecation")
-     public void stop(){
-           listenThread.stop();
-           service.stop();
-     }
+    @SuppressWarnings("deprecation")
+    public void stop() {
+        listenThread.stop();
+        service.stop();
+    }
 
-     // Internal thread to listen for new connections
-     class newClientListener implements Runnable {
+    // Internal thread to listen for new connections
+    class newClientListener implements Runnable {
 
         ServerSocket listenSocket = null;
         boolean running = true;
 
         public newClientListener(ServerSocket socket) {
 
-	   listenSocket = socket;
+            listenSocket = socket;
         }
 
         public void run() {
-	   // Listen for connection requests
-	   try{
-	      while(running) {
-	         Socket clientSocket = listenSocket.accept();
-	         if(log.isDebugEnabled())
-		    log.debug(" Client Connected from IP " + clientSocket.getInetAddress() + " port " +clientSocket.getPort() );
-	         addClient(new clientListener(clientSocket));
-	      }
-	      } catch (IOException e) {
-		log.error("IOException while Listening for clients");
-	   }
-	}
-
-        public void stop(){
-                //super.stop();
-                running=false;
-                try{
-                    listenSocket.close();
-                    if(log.isDebugEnabled()) 
-                       log.debug("Listen Socket closed");
-                } catch (IOException e){
-                    log.error("socket in ThreadedServer won't close");
-                 return;
-           }
+            // Listen for connection requests
+            try {
+                while (running) {
+                    Socket clientSocket = listenSocket.accept();
+                    if (log.isDebugEnabled()) {
+                        log.debug(" Client Connected from IP " + clientSocket.getInetAddress() + " port " + clientSocket.getPort());
+                    }
+                    addClient(new clientListener(clientSocket));
+                }
+            } catch (IOException e) {
+                log.error("IOException while Listening for clients");
+            }
         }
 
-     } // end of newClientListener class
+        public void stop() {
+            //super.stop();
+            running = false;
+            try {
+                listenSocket.close();
+                if (log.isDebugEnabled()) {
+                    log.debug("Listen Socket closed");
+                }
+            } catch (IOException e) {
+                log.error("socket in ThreadedServer won't close");
+                return;
+            }
+        }
+    } // end of newClientListener class
 
     // Internal class to handle a client
     class clientListener implements Runnable {
 
-     Socket clientSocket = null;
-     DataInputStream inStream = null;
-     DataOutputStream outStream = null;
-     Thread clientThread = null;     
+        Socket clientSocket = null;
+        DataInputStream inStream = null;
+        DataOutputStream outStream = null;
+        Thread clientThread = null;
 
-     public clientListener(Socket socket) {
-        if(log.isDebugEnabled()) log.debug("Starting new Client");
-	clientSocket = socket;
-	try {
-	  inStream = new DataInputStream(clientSocket.getInputStream());
-	  outStream = new DataOutputStream(clientSocket.getOutputStream());
-	} catch (IOException e) {
-	  log.error("Error obtaining I/O Stream from socket.");
-	}
-     }
+        public clientListener(Socket socket) {
+            if (log.isDebugEnabled()) {
+                log.debug("Starting new Client");
+            }
+            clientSocket = socket;
+            try {
+                inStream = new DataInputStream(clientSocket.getInputStream());
+                outStream = new DataOutputStream(clientSocket.getOutputStream());
+            } catch (IOException e) {
+                log.error("Error obtaining I/O Stream from socket.");
+            }
+        }
 
-     public void start() {
-                clientThread = new Thread(this);
-                clientThread.start();
-     }
+        public void start() {
+            clientThread = new Thread(this);
+            clientThread.start();
+        }
 
-     public void run() {
-        // handle a client.
-        try{
-          handleClient(inStream,outStream);
-	} catch (IOException e1) {
-	   // When we get an IO exception here, we're done
-           if(log.isDebugEnabled()) log.debug("Server Exiting");
-	   // Unregister with the server
-	   removeClient(this);
-	   return;
-        } catch (java.lang.NullPointerException e2) {
-	   // When we get an IO exception here, we're done with this client
-           if(log.isDebugEnabled()) log.debug("Client Disconnect");
-	   // Unregister with the server
-	   removeClient(this);
-	   return;
-        }        
-     }
-     } // end of clientListener class.
-	
+        public void run() {
+            // handle a client.
+            try {
+                handleClient(inStream, outStream);
+            } catch (IOException e1) {
+                // When we get an IO exception here, we're done
+                if (log.isDebugEnabled()) {
+                    log.debug("Server Exiting");
+                }
+                // Unregister with the server
+                removeClient(this);
+                return;
+            } catch (java.lang.NullPointerException e2) {
+                // When we get an IO exception here, we're done with this client
+                if (log.isDebugEnabled()) {
+                    log.debug("Client Disconnect");
+                }
+                // Unregister with the server
+                removeClient(this);
+                return;
+            }
+        }
+    } // end of clientListener class.
 
-     // Handle communication to a client through inStream and outStream
-     public void handleClient(DataInputStream inStream, DataOutputStream outStream) throws IOException {
+    // Handle communication to a client through inStream and outStream
+    public void handleClient(DataInputStream inStream, DataOutputStream outStream) throws IOException {
         // Listen for commands from the client until the connection closes
-	byte cmd[] = new byte[100]; 
-	int count;
-	while(true) {
-	   // Read the command from the client
-           count = inStream.read(cmd);
-	   // Echo the input back to the client
-	   if(count!=0) {
-	      outStream.write(cmd);
-	   }
-	 }	
-       }
-
-     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(JmriServer.class.getName());
+        byte cmd[] = new byte[100];
+        int count;
+        while (true) {
+            // Read the command from the client
+            count = inStream.read(cmd);
+            // Echo the input back to the client
+            if (count != 0) {
+                outStream.write(cmd);
+            }
+        }
+    }
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(JmriServer.class.getName());
 }
--- java/src/jmri/jmris/simpleserver/SimpleServer.java
+++ java/src/jmri/jmris/simpleserver/SimpleServer.java
@@ -1,5 +1,4 @@
 // SimpleServer.java
-
 package jmri.jmris.simpleserver;
 
 import jmri.jmris.*;
@@ -10,111 +9,110 @@ import java.util.ResourceBundle;
 
 import jmri.InstanceManager;
 
-
 /**
- * This is an implementaiton of a simple server for JMRI.
+ * This is an implementation of a simple server for JMRI.
  * There is currently no handshaking in this server.  You may just start 
  * sending commands.
  * @author Paul Bender Copyright (C) 2010
  * @version $Revision$
  *
  */
-public class SimpleServer extends JmriServer{
+public class SimpleServer extends JmriServer {
 
-     private static JmriServer _instance = null;
+    private static JmriServer _instance = null;
+    static ResourceBundle rb = ResourceBundle.getBundle("jmri.jmris.simpleserver.SimpleServerBundle");
 
-     static ResourceBundle rb = ResourceBundle.getBundle("jmri.jmris.simpleserver.SimpleServerBundle");
+    public static JmriServer instance() {
+        if (_instance == null) {
+            int port = java.lang.Integer.parseInt(rb.getString("SimpleServerPort"));
+            _instance = new SimpleServer(port);
+        }
+        return _instance;
+    }
 
-     public static JmriServer instance(){
-         if(_instance==null) {
-           int port=java.lang.Integer.parseInt(rb.getString("SimpleServerPort"));
-           _instance=new SimpleServer(port);
-         }
-         return _instance;
-     }
+    // Create a new server using the default port
+    public SimpleServer() {
+        super(java.lang.Integer.parseInt(rb.getString("SimpleServerPort")));
+    }
 
-     // Create a new server using the default port
-     public SimpleServer() {
-	super(java.lang.Integer.parseInt(rb.getString("SimpleServerPort")));
-     }
+    public SimpleServer(int port) {
+        super(port);
+    }
 
-     public SimpleServer(int port) {
-	super(port);
-     }
-
-     // Handle communication to a client through inStream and outStream
-	 @SuppressWarnings("deprecation")
-     public void handleClient(DataInputStream inStream, DataOutputStream outStream) throws IOException {
-        java.util.Scanner inputScanner=new java.util.Scanner(new InputStreamReader(inStream));
+    // Handle communication to a client through inStream and outStream
+    @SuppressWarnings("deprecation")
+    public void handleClient(DataInputStream inStream, DataOutputStream outStream) throws IOException {
+        java.util.Scanner inputScanner = new java.util.Scanner(new InputStreamReader(inStream));
 
         // Listen for commands from the client until the connection closes
-	String cmd; 
+        String cmd;
 
         // interface components
-        SimplePowerServer powerServer = new SimplePowerServer(inStream,outStream);
-        SimpleTurnoutServer turnoutServer = new SimpleTurnoutServer(inStream,outStream);
-        SimpleLightServer lightServer = new SimpleLightServer(inStream,outStream);
-        SimpleSensorServer sensorServer = new SimpleSensorServer(inStream,outStream);
-        SimpleReporterServer reporterServer = new SimpleReporterServer(inStream,outStream);
-        SimpleOperationsServer operationsServer = new SimpleOperationsServer(inStream,outStream);
+        SimplePowerServer powerServer = new SimplePowerServer(inStream, outStream);
+        SimpleTurnoutServer turnoutServer = new SimpleTurnoutServer(inStream, outStream);
+        SimpleLightServer lightServer = new SimpleLightServer(inStream, outStream);
+        SimpleSensorServer sensorServer = new SimpleSensorServer(inStream, outStream);
+        SimpleReporterServer reporterServer = new SimpleReporterServer(inStream, outStream);
+        SimpleOperationsServer operationsServer = new SimpleOperationsServer(inStream, outStream);
 
         // Start by sending a welcome message
         outStream.writeBytes("JMRI " + jmri.Version.name() + " \n");
 
-	    while(true) {
-           inputScanner.skip("[\r\n]*");// skip any stray end of line characters.
-	   // Read the command from the client
-           try {
-              cmd = inputScanner.nextLine();
-           } catch(java.util.NoSuchElementException nse) {
-             // we get an nse when we are finished with this client
-             // so break out of the loop.
-             break;
-           }
-           
-           if(log.isDebugEnabled()) log.debug("Received from client: " + cmd);
-              if(cmd.startsWith("POWER")){
-	             try {
-                        powerServer.parseStatus(cmd);
-		        powerServer.sendStatus(InstanceManager.powerManagerInstance().getPower());
-                     } catch(jmri.JmriException je) {
-                       outStream.writeBytes("not supported\n");
-                     }
-                 } else if(cmd.startsWith("TURNOUT")){
-	             try {
-                       turnoutServer.parseStatus(cmd);
-                     } catch(jmri.JmriException je) {
-                       outStream.writeBytes("not supported\n");
-                     }
-                 } else if(cmd.startsWith("LIGHT")){
-	             try {
-                       lightServer.parseStatus(cmd);
-                     } catch(jmri.JmriException je) {
-                       outStream.writeBytes("not supported\n");
-                     }
-                 } else if(cmd.startsWith("SENSOR")){
-	             try {
-                       sensorServer.parseStatus(cmd);
-                     } catch(jmri.JmriException je) {
-                       outStream.writeBytes("not supported\n");
-                     }
-                 } else if(cmd.startsWith("REPORTER")){
-	             try {
-                       reporterServer.parseStatus(cmd);
-                     } catch(jmri.JmriException je) {
-                       outStream.writeBytes("not supported\n");
-                     }
-                 } else if(cmd.startsWith("OPERATIONS")){
-	             try {
-                       operationsServer.parseStatus(cmd);
-                     } catch(jmri.JmriException je) {
-                       outStream.writeBytes("not supported\n");
-                     }
-                 } else {
-	      outStream.writeBytes("Unknown Command " + cmd +"\n");
-           } 
-	 }	
-       }
+        while (true) {
+            inputScanner.skip("[\r\n]*");// skip any stray end of line characters.
+            // Read the command from the client
+            try {
+                cmd = inputScanner.nextLine();
+            } catch (java.util.NoSuchElementException nse) {
+                // we get an nse when we are finished with this client
+                // so break out of the loop.
+                break;
+            }
 
-     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SimpleServer.class.getName());
+            if (log.isDebugEnabled()) {
+                log.debug("Received from client: " + cmd);
+            }
+            if (cmd.startsWith("POWER")) {
+                try {
+                    powerServer.parseStatus(cmd);
+                    powerServer.sendStatus(InstanceManager.powerManagerInstance().getPower());
+                } catch (jmri.JmriException je) {
+                    outStream.writeBytes("not supported\n");
+                }
+            } else if (cmd.startsWith("TURNOUT")) {
+                try {
+                    turnoutServer.parseStatus(cmd);
+                } catch (jmri.JmriException je) {
+                    outStream.writeBytes("not supported\n");
+                }
+            } else if (cmd.startsWith("LIGHT")) {
+                try {
+                    lightServer.parseStatus(cmd);
+                } catch (jmri.JmriException je) {
+                    outStream.writeBytes("not supported\n");
+                }
+            } else if (cmd.startsWith("SENSOR")) {
+                try {
+                    sensorServer.parseStatus(cmd);
+                } catch (jmri.JmriException je) {
+                    outStream.writeBytes("not supported\n");
+                }
+            } else if (cmd.startsWith("REPORTER")) {
+                try {
+                    reporterServer.parseStatus(cmd);
+                } catch (jmri.JmriException je) {
+                    outStream.writeBytes("not supported\n");
+                }
+            } else if (cmd.startsWith("OPERATIONS")) {
+                try {
+                    operationsServer.parseStatus(cmd);
+                } catch (jmri.JmriException je) {
+                    outStream.writeBytes("not supported\n");
+                }
+            } else {
+                outStream.writeBytes("Unknown Command " + cmd + "\n");
+            }
+        }
+    }
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SimpleServer.class.getName());
 }
