517cd7418bb06ab747d64e0659fea060801bcc42
Kevin Dickerson <kev.public@ntlworld.com>
2011-11-09 14:36:46 +0000

    Add a new Signal Mast driver for hardware that can drive signal mast appearance via a turnout addresses, rather than requiring a separate signal head driver to be created.

---
+++ java/src/jmri/implementation/TurnoutSignalMast.java
@@ -0,0 +1,193 @@
+// TurnoutSignalMast.javaa
+package jmri.implementation;
+import java.util.*;
+
+import jmri.*;
+import jmri.NamedBeanHandle;
+
+ /**
+ * SignalMast implemented via Turnout objects.
+ * <p>
+ * A Signalmast that is built up using turnouts to control
+ * a specific appearance.
+ *
+ * @author	Bob Jacobsen Copyright (C) 2009
+ * @version     $Revision: 19027 $
+ */
+public class TurnoutSignalMast extends AbstractSignalMast {
+
+    public TurnoutSignalMast(String systemName, String userName) {
+        super(systemName, userName);
+        configureFromName(systemName);
+    }
+
+    public TurnoutSignalMast(String systemName) {
+        super(systemName);
+        configureFromName(systemName);
+    }
+        
+    void configureFromName(String systemName) {
+        // split out the basic information
+        String[] parts = systemName.split(":");
+        if (parts.length < 3) { 
+            log.error("SignalMast system name needs at least three parts: "+systemName);
+            throw new IllegalArgumentException("System name needs at least three parts: "+systemName);
+        }
+        if (!parts[0].equals("IF$tsm")) {
+            log.warn("SignalMast system name should start with IF: "+systemName);
+        }
+        String prefix = parts[0];
+        String system = parts[1];
+        String mast = new String(parts[2]);
+        String reg = new String(parts[2]);
+
+        mast = mast.substring(0, mast.indexOf("("));
+        String tmp = reg.substring(reg.indexOf("($")+2, reg.indexOf(")"));
+        try {
+            int autoNumber = Integer.parseInt(tmp);
+            if (autoNumber > lastRef) {
+                lastRef = autoNumber;
+            } 
+        } catch (NumberFormatException e){
+            log.warn("Auto generated SystemName "+ systemName + " is not in the correct format");
+        }
+        
+        String interim = systemName.substring(prefix.length()+1+system.length()+1);
+        /*String parenstring = interim.substring(interim.indexOf("("), interim.length());
+        java.util.List<String> parens = jmri.util.StringUtil.splitParens(parenstring);*/
+        configureSignalSystemDefinition(system);
+        configureAspectTable(system, mast);
+    }
+    
+    void configureSignalSystemDefinition(String name) {
+        systemDefn = InstanceManager.signalSystemManagerInstance().getSystem(name);
+        if (systemDefn == null) {
+            log.error("Did not find signal definition: "+name);
+            throw new IllegalArgumentException("Signal definition not found: "+name);
+        }
+    }
+    
+    void configureAspectTable(String signalSystemName, String aspectMapName) {
+        map = DefaultSignalAppearanceMap.getMap(signalSystemName, aspectMapName);
+    }
+
+    @Override
+    public void setAspect(String aspect) { 
+        // check it's a choice
+        if ( !map.checkAspect(aspect)) {
+            // not a valid aspect
+            log.warn("attempting to set invalid aspect: "+aspect+" on mast: "+getDisplayName());
+            throw new IllegalArgumentException("attempting to set invalid aspect: "+aspect+" on mast: "+getDisplayName());
+        }
+        
+        int stateToSet = turnouts.get(aspect).getTurnoutState();
+        Turnout turnToSet = turnouts.get(aspect).getTurnout();
+        //Clear all the current states, this will result in the signalmast going blank for a very short time.
+        for(String appearances: turnouts.keySet()){
+            int setState = Turnout.CLOSED;
+            if(turnouts.get(appearances).getTurnoutState()==Turnout.CLOSED)
+                setState = Turnout.THROWN;
+            if(turnouts.get(appearances).getTurnout().getKnownState()!=setState){
+                turnouts.get(appearances).getTurnout().setCommandedState(setState);
+            }
+        }
+        //Set the new signal mast state
+        turnToSet.setCommandedState(stateToSet);
+        super.setAspect(aspect);
+    }
+    
+    public Vector<String> getValidAspects() {
+        java.util.Enumeration<String> e = map.getAspects();
+        Vector<String> v = new Vector<String>();
+        while (e.hasMoreElements()) {
+            v.add(e.nextElement());
+        }
+        return v;
+    }
+
+    public SignalSystem getSignalSystem() {
+        return systemDefn;
+    }
+    
+    public SignalAppearanceMap getAppearanceMap() {
+        return map;
+    }
+    
+    @Override
+    public void setLit(boolean state) {
+        // set all Heads to state
+        for(String appearances: turnouts.keySet()){
+            int setState = Turnout.CLOSED;
+            if(turnouts.get(appearances).getTurnoutState()==Turnout.CLOSED)
+                setState = Turnout.THROWN;
+            if(turnouts.get(appearances).getTurnout().getKnownState()!=setState){
+                turnouts.get(appearances).getTurnout().setCommandedState(setState);
+            }
+        }
+        super.setLit(state);
+    }
+    
+    public String getTurnoutName(String appearance){
+        TurnoutAspect aspect = turnouts.get(appearance);
+        if(aspect!=null)
+            return aspect.getTurnoutName();
+        return "";
+    }
+    
+    public int getTurnoutState(String appearance){
+        TurnoutAspect aspect = turnouts.get(appearance);
+        if(aspect!=null)
+            return aspect.getTurnoutState();
+        return -1;
+    }
+    
+    public void setTurnout(String appearance, String turn, int state){
+        if(turnouts.containsKey(appearance)){
+            log.debug("Appearance " + appearance + " is already defined so will override");
+            turnouts.remove(appearance);
+        }
+        turnouts.put(appearance, new TurnoutAspect(turn, state));
+    }
+    
+    DefaultSignalAppearanceMap map;
+    SignalSystem systemDefn;
+    
+    HashMap<String, TurnoutAspect> turnouts = new HashMap<String, TurnoutAspect>();
+    
+    static class TurnoutAspect{
+        
+        NamedBeanHandle<Turnout> namedTurnout;
+        int state;
+    
+        TurnoutAspect(String turnoutName, int turnoutState){
+            Turnout turn = jmri.InstanceManager.turnoutManagerInstance().getTurnout(turnoutName);
+            namedTurnout = jmri.InstanceManager.getDefault(jmri.NamedBeanHandleManager.class).getNamedBeanHandle(turnoutName, turn);
+            state = turnoutState;
+        }
+        
+        Turnout getTurnout(){
+            return namedTurnout.getBean();
+        }
+        
+        String getTurnoutName() {
+            return namedTurnout.getName();
+        }
+        
+        int getTurnoutState(){
+            return state;
+        }
+    
+    }
+    
+    public List<NamedBeanHandle<Turnout>> getHeadsUsed(){
+        return new ArrayList<NamedBeanHandle<Turnout>>();
+    }
+    
+    public static int getLastRef(){ return lastRef; }
+    
+    static int lastRef = 0;
+    
+    static final protected org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TurnoutSignalMast.class.getName());
+}
+
+/* @(#)TurnoutSignalMast.java */
---
+++ java/src/jmri/implementation/configurexml/TurnoutSignalMastXml.java
@@ -0,0 +1,96 @@
+package jmri.implementation.configurexml;
+
+import jmri.InstanceManager;
+import jmri.SignalMast;
+import jmri.Turnout;
+import jmri.implementation.TurnoutSignalMast;
+import jmri.SignalAppearanceMap;
+import jmri.managers.DefaultSignalMastManager;
+
+import java.util.List;
+import java.util.Enumeration;
+
+import org.jdom.Element;
+
+/**
+ * Handle XML configuration for a DefaultSignalMastManager objects.
+ *
+ * @author Bob Jacobsen Copyright: Copyright (c) 2009
+ * @version $Revision: 18102 $
+ */
+public class TurnoutSignalMastXml 
+            extends jmri.managers.configurexml.AbstractNamedBeanManagerConfigXML {
+
+    public TurnoutSignalMastXml() {}
+
+    /**
+     * Default implementation for storing the contents of a
+     * DefaultSignalMastManager
+     * @param o Object to store, of type TripleTurnoutSignalHead
+     * @return Element containing the complete info
+     */
+    public Element store(Object o) {
+        TurnoutSignalMast p = (TurnoutSignalMast)o;
+        Element e = new Element("signalmast");
+        e.setAttribute("class", this.getClass().getName());
+        e.addContent(new Element("systemName").addContent(p.getSystemName()));
+
+        storeCommon(p, e);
+        SignalAppearanceMap appMap = p.getAppearanceMap();
+        if(appMap!=null){
+            java.util.Enumeration<String> aspects = appMap.getAspects();
+            while(aspects.hasMoreElements()){
+                String key = (String) aspects.nextElement();
+                Element el = new Element("aspect");
+                el.setAttribute("defines", key);
+                el.addContent(new Element("turnout").addContent(p.getTurnoutName(key)));
+                if(p.getTurnoutState(key)==Turnout.CLOSED)
+                    el.addContent(new Element("turnoutstate").addContent("closed"));
+                else
+                    el.addContent(new Element("turnoutstate").addContent("thrown"));
+                e.addContent(el);
+            }
+        }
+        return e;
+    }
+
+    /**
+     * Create a DefaultSignalMastManager
+     * @param element Top level Element to unpack.
+     * @return true if successful
+     */
+    public boolean load(Element element) {
+        TurnoutSignalMast m;
+        String sys = getSystemName(element);
+        m = new jmri.implementation.TurnoutSignalMast(sys);
+        
+        if (getUserName(element) != null)
+            m.setUserName(getUserName(element));
+        
+        loadCommon(m, element);
+        
+        List<Element> list = element.getChildren("aspect");
+        for (int i = 0; i < list.size(); i++) {
+            Element e = list.get(i);
+            String aspect = e.getAttribute("defines").getValue();
+            String turnout = e.getChild("turnout").getText();
+            String turnoutState = e.getChild("turnoutstate").getText();
+            int turnState = Turnout.THROWN;
+            if(turnoutState.equals("closed"))
+                turnState = Turnout.CLOSED;
+            m.setTurnout(aspect, turnout, turnState);
+        }
+        
+    
+        InstanceManager.signalMastManagerInstance()
+            .register(m);
+        
+        return true;
+    }
+
+    public void load(Element element, Object o) {
+        log.error("Invalid method called");
+    }
+    
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(TurnoutSignalMastXml.class.getName());
+}
