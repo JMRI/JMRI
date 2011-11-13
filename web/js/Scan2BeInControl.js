09556024d42fe649212ddb7187630904c3753090
Steve Todd <mstevetodd@mstevetodd.com>
2011-11-13 23:19:52 +0000

    New 'inControl' version - Oscar Moutinho

--- web/js/Scan2BeInControl.js
+++ web/js/Scan2BeInControl.js
@@ -26,14 +26,13 @@ var $isLocalhost;
 var $xmlRosterPath = "/prefs/roster.xml";
 var $inControlURL;
 var $help =
-	"Version 1.1 - by Oscar Moutinho" +
+	"Version 1.2 - by Oscar Moutinho" +
 	"\n" +
 	"\nThis application lists the roster (locos) defined in JMRI." +
 	"\n" +
 	"\nTo open a web throttle using 'inControl', users may:" +
 	"\n- scan the image using a smartphone (with qrCode reader application)" +
-	"\n- click or touch the link" +
-	"\n- copy and paste the link" +
+	"\n- click or touch the Loco image or link" +
 	"\n" +
 	"\nThe parameters from JMRI are merged with the ones already stored in your device (computer, tablet or smartphone)." +
 	"\nIf you want to use only the parameters from JMRI, you must delete the loco from your device before scan the qrCode graph or use the link." +
@@ -66,6 +65,7 @@ var $loadRoster = function(){
 					roadName: $.trim($(this).attr("roadName")),
 					roadNumber: $.trim($(this).attr("roadNumber")),
 					address: parseInt($(this).attr("dccAddress"), 10),
+					icon: $.trim($(this).attr("iconFilePath")),
 					image: $.trim($(this).attr("imageFilePath")),
 					shunt: $.trim($(this).attr("IsShuntingOn")),
 					functions: []
@@ -90,8 +90,10 @@ var $loadRoster = function(){
 			var $tittleLoco;
 			var $lnkLoco;
 			var $infoLoco;
+			var $auxLocoImg;
+			var $auxImg;
 			for(var i = 0; i < $locos.length; i++){
-				s+= "<p><div id='loco" + i + "' class='divLoco'>";
+				s+= "<div id='loco" + i + "' class='divLoco'>";
 				s+= "<table><tr><td>";
 				s+= "<div id='qrCode" + i + "' class='imgLoco'></div>";
 				s+= "</td><td>";
@@ -104,30 +106,34 @@ var $loadRoster = function(){
 				s+= "</td></tr></table>";
 				s+= "</td></tr></table>";
 				s+= "<hr />";
-				s+= "</div></p>";
+				s+= "</div>";
 			}
 			$("#roster").html(s);
 			for(var i = 0; i < $locos.length; i++){
+				if(i > 0) $("#loco" + i).css("page-break-before", "always");
 				$queryString = "loconame=" + encodeURIComponent($locos[i].name);
 				$queryString+= "&locoaddress=" + encodeURIComponent($locos[i].address);
-				if($locos[i].image.length > 0) $queryString+= "&locoimage=" + encodeURIComponent($locos[i].image);
-				$queryString+= "&" + encodeURIComponent($locos[i].shunt) + "shunt=x";
+				$auxLocoImg = $auxImage($locos[i].image, $locos[i].icon);
+				if($auxLocoImg.length > 0) $queryString+= "&locoimage=" + encodeURIComponent($auxLocoImg);
+				if($locos[i].shunt.length > 0) $queryString+= "&" + encodeURIComponent($locos[i].shunt) + "shunt=x";
 				for(var j = 0; j < $locos[i].functions.length; j++){
 					$queryString+= "&f" + $locos[i].functions[j].number + "active=x";
 					if($locos[i].functions[j].toggle) $queryString+= "&f" + $locos[i].functions[j].number + "toggle=x";
 					if($locos[i].functions[j].label.length > 0) $queryString+= "&f" + $locos[i].functions[j].number + "label=" + encodeURIComponent($locos[i].functions[j].label);
-					if($locos[i].functions[j].image.length > 0) $queryString+= "&f" + $locos[i].functions[j].number + "image=" + encodeURIComponent($locos[i].functions[j].image);
-					if($locos[i].functions[j].imagePressed.length > 0) $queryString+= "&f" + $locos[i].functions[j].number + "imagepressed=" + encodeURIComponent($locos[i].functions[j].imagePressed);
+					$auxImg = $auxImage($locos[i].functions[j].image);
+					if($auxImg.length > 0) $queryString+= "&f" + $locos[i].functions[j].number + "image=" + encodeURIComponent($auxImg);
+					$auxImg = $auxImage($locos[i].functions[j].imagePressed);
+					if($auxImg.length > 0) $queryString+= "&f" + $locos[i].functions[j].number + "imagepressed=" + encodeURIComponent($auxImg);
 				}
 				$qrCode = $("#qrCode" + i);
 				$tittleLoco = $("#tittleLoco" + i);
 				$lnkLoco = $("#lnkLoco" + i);
 				$infoLoco = $("#infoLoco" + i);
 				if(!$isLocalhost) $qrCode.qrcode($inControlURL + "?" + $queryString);
-				$tittleLoco.html(($isLocalhost ? "" : "Point your smartphone qrCode scanner to the square graph to open the throttle in your device.<br />") + "To open the throttle in this computer, click at the link.");
+				$tittleLoco.html(($isLocalhost ? "" : "Point your smartphone qrCode scanner to the square graph to open the throttle in your device.<br />") + "To open the throttle in this computer, click at the " +  ($auxLocoImg.length > 0 ? "Loco image." : "link."));
 				$lnkLoco.attr("href", $inControlURL + "?" + $queryString);
 				$lnkLoco.attr("target", $locos[i].name);
-				$lnkLoco.html($("<div />").text($inControlURL).html() + "?<wbr>" + $("<div />").text($queryString).html().replace(/&amp;/g, "&amp;<wbr>"));				// HTML encode
+				if($auxLocoImg.length > 0) $lnkLoco.html("<img src='/prefs/resources/" + $auxLocoImg + "?MaxHeight=120' title='Open a throttle to control this Loco' />"); else $lnkLoco.html($("<div />").text($inControlURL).html() + "?<wbr>" + $("<div />").text($queryString).html().replace(/&amp;/g, "&amp;<wbr>"));				// HTML encode
 				$infoLoco.html(
 					"<b>Loco name (DCC address):</b> " + $("<div />").text($locos[i].name).html() + " (" + $("<div />").text($locos[i].address).html() + ")" +				// HTML encode
 					"<br /><b>Road name / number:</b> " + $("<div />").text($locos[i].roadName).html() + " / " + $("<div />").text($locos[i].roadNumber).html()	// HTML encode
@@ -135,6 +141,14 @@ var $loadRoster = function(){
 			}
 		}
 	});
+}
+
+//----------------------------------------- Aux image URL
+var $auxImage = function(Url1, Url2){
+	var Url = "";
+	if(Url1) if(Url1.length > 0 && Url1.image != "__noImage.jpg") Url = Url1;
+	if(Url.length == 0) if(Url2) if(Url2.length > 0 && Url2.image != "__noImage.jpg") Url = Url2;
+	return Url;
 }
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End of file
