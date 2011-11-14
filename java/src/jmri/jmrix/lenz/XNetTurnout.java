b028b7110a6b7d19b90ea04789b51a412d49d252
Paul Bender <paul.bender@acm.org>
2011-11-14 07:14:11 +0000

    make sure we send a request for the right nibble when requesting turnout status. NOTE: Command station may return lower nibble if upper nibble is requested, but no turnout in the group has been operated.

--- java/src/jmri/jmrix/lenz/XNetTurnout.java
+++ java/src/jmri/jmrix/lenz/XNetTurnout.java
@@ -237,7 +237,7 @@ public class XNetTurnout extends AbstractTurnout implements XNetListener {
        // is a turnout address.  As a result, we substitute our base
        // address in for the address. after the message is returned.
        XNetMessage msg = XNetMessage.getFeedbackRequestMsg(mNumber,
-                                                 (mNumber%4)<2);
+                                                 ((mNumber-1)%4)<2);
        internalState=STATUSREQUESTSENT; 
        tc.sendXNetMessage(msg, null); //status is returned via the manager.
 
