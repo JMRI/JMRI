465cdc0115a84c61c16a9bb6b31748343ee01ad7
david d zuhn <zoo@statebeltrailway.org>
2011-12-17 20:31:00 +0000

    * set Java version to 1.6 for compiler settings * link to Java 6 in Javadoc output * remove some obsolete installation help (MacClassicProperties,RXTX-install) * unify MacOSX & Linux startup scripts   -- create the app specific scripts during build time   -- also create the MacOSX .app directories from the template at build time   -- add version numbers to these scripts during creation time (instead of checked in to SVN) * create java/manifest as needed, with the correct version number * update HOWTO-distribution to reflect these changes * remove obsolete .bat files, .csh scripts, and app startup scripts

--- scripts/CornwallRR.bat
+++
@@ -1,3 +0,0 @@
-REM Start the CornwallRR program from JMRI ($Revision$)
-
-LaunchJMRI.exe apps.cornwall.CornwallRR %1 %2 %3 %4 %5 %6 %7 %8 %9
--- scripts/JmriDemo.bat
+++
@@ -1,3 +0,0 @@
-REM Start the JmriDemo program from JMRI ($Revision$)
-
-LaunchJMRI.exe apps.JmriDemo.JMRIdemo %1 %2 %3 %4 %5 %6 %7 %8 %9
