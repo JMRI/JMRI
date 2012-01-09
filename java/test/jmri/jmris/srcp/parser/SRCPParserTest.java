11c481a0ed16cc21efa755d62e21380f471a12db
Paul Bender <paul.bender@acm.org>
2012-01-09 06:12:09 +0000

    new tests for SRCPParser

--- java/test/jmri/jmris/srcp/parser/SRCPParserTest.java
+++ java/test/jmri/jmris/srcp/parser/SRCPParserTest.java
@@ -27,6 +27,41 @@ public class SRCPParserTest extends TestCase {
            assertTrue(exceptionOccured);
         }
 
+       public void testSetPowerOn(){
+           boolean exceptionOccured = false;
+           String code = "SET 1 POWER ON\n\r";
+           SRCPParser p = new SRCPParser(new StringReader(code));
+           try {
+             p.command();
+           } catch(ParseException pe) {
+             exceptionOccured = true;
+           }
+           assertFalse(exceptionOccured);
+       }
+
+       public void testSetPowerOff(){
+           boolean exceptionOccured = false;
+           String code = "SET 1 POWER OFF\n\r";
+           SRCPParser p = new SRCPParser(new StringReader(code));
+           try {
+             p.command();
+           } catch(ParseException pe) {
+             exceptionOccured = true;
+           }
+           assertFalse(exceptionOccured);
+       }
+
+       public void testGetPower(){
+           boolean exceptionOccured = false;
+           String code = "GET 1 POWER\n\r";
+           SRCPParser p = new SRCPParser(new StringReader(code));
+           try {
+             p.command();
+           } catch(ParseException pe) {
+             exceptionOccured = true;
+           }
+           assertFalse(exceptionOccured);
+       }
 
         // Main entry point
         static public void main(String[] args) {
