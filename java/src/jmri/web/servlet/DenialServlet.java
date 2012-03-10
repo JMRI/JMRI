2472c17f94ad3dafaca43a29a5e72d5fe5406b9d
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-03-10 17:11:57 +0000

    Simple servlet to provide HTTP 403 FORBIDDEN messages.

---
+++ java/src/jmri/web/servlet/DenialServlet.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmri.web.servlet;
+
+import java.io.IOException;
+import java.util.ResourceBundle;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet that simply sends an HTTP 403 FORBIDDEN error.
+ * 
+ * Passing requests for certain resources protects those resources from
+ * network access.
+ *
+ * @author rhwood
+ */
+public class DenialServlet extends HttpServlet {
+
+    static ResourceBundle htmlStrings = ResourceBundle.getBundle("jmri.web.server.Html");
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+}
