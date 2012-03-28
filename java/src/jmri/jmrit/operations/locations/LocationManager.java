b5d38306d8738e32d196b723eb2728b31591d63f
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2012-03-28 20:04:21 +0000

    Minor cleanup

--- java/src/jmri/jmrit/operations/locations/LocationManager.java
+++ java/src/jmri/jmrit/operations/locations/LocationManager.java
@@ -2,8 +2,6 @@
 
 package jmri.jmrit.operations.locations;
 
-import java.awt.Dimension;
-import java.awt.Point;
 import java.util.Enumeration;
 
 import java.util.ArrayList;
@@ -29,11 +27,6 @@ import jmri.jmrit.operations.rollingstock.engines.EngineTypes;
 public class LocationManager implements java.beans.PropertyChangeListener {
 	public static final String LISTLENGTH_CHANGED_PROPERTY = "locationsListLength";
 	
-	// Edit Location frame attributes
-	protected LocationEditFrame _locationEditFrame = null;
-	protected Dimension _editFrameDimension = null;
-	protected Point _editFramePosition = null;
-    
 	public LocationManager() {
 		CarTypes.instance().addPropertyChangeListener(this);
 		CarRoads.instance().addPropertyChangeListener(this);
@@ -57,20 +50,6 @@ public class LocationManager implements java.beans.PropertyChangeListener {
 		if (Control.showInstance && log.isDebugEnabled()) log.debug("LocationManager returns instance "+_instance);
 		return _instance;
 	}
-
-	/* all JMRI window position and size are now saved
-	public void setLocationEditFrame(LocationEditFrame frame){
-		_locationEditFrame = frame;
-	}
-
-	public Dimension getLocationEditFrameSize(){
-		return _editFrameDimension;
-	}
-
-	public Point getLocationEditFramePosition(){
-		return _editFramePosition;
-	}
-	*/
 
     public void dispose() {
         _locationHashTable.clear();
@@ -236,7 +215,7 @@ public class LocationManager implements java.beans.PropertyChangeListener {
     
     /**
      * Returns all tracks of type
-     * @param type Siding, Yard, Interchange, or Staging
+     * @param type Siding, Yard, Interchange, Staging, or null (returns all track types)
      * @return List of tracks ordered by use
      */
     public List<Track> getTracks(String type){
