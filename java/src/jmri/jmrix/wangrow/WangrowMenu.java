50919c32ac935bcad2a295ddd5a73db010624ffe
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2012-05-08 22:53:28 +0000

    Fix  Wangrow connection

--- java/src/jmri/jmrix/wangrow/WangrowMenu.java
+++ java/src/jmri/jmrix/wangrow/WangrowMenu.java
@@ -2,12 +2,7 @@
 
 package jmri.jmrix.wangrow;
 
-import java.util.ResourceBundle;
-
 import jmri.jmrix.nce.NceSystemConnectionMemo;
-import jmri.jmrix.nce.swing.NceNamedPaneAction;
-
-import javax.swing.JMenu;
 
 /**
  * Create a "Systems" menu containing the Jmri NCE-specific tools.
@@ -18,51 +13,11 @@ import javax.swing.JMenu;
  * @author	Bob Jacobsen   Copyright 2003
  * @version     $Revision$
  */
-public class WangrowMenu extends JMenu {
-	
-	NceSystemConnectionMemo memo = null;
-	
-    public WangrowMenu(NceSystemConnectionMemo m) {
-
-        super();
-        this.memo = m;
-
-        ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrix.JmrixSystemsBundle");
-        
-        if (memo != null)
-            setText(memo.getUserName());
-        else
-            setText(rb.getString("MenuWangrow"));
-
-        jmri.util.swing.WindowInterface wi = new jmri.util.swing.sdi.JmriJFrameInterface();
-        
-        for (Item item : panelItems) {
-            if (item == null) {
-                add(new javax.swing.JSeparator());
-            } else {
-                add(new NceNamedPaneAction( rb.getString(item.name), wi, item.load, memo));
-            }
-        }
-        add(new javax.swing.JSeparator());
-
-        //setText(rb.getString("MenuItemWangrow"));
-
-        //add(new jmri.jmrix.nce.ncemon.NceMonAction(rb.getString("MenuItemCommandMonitor")));
-        //add(new jmri.jmrix.nce.packetgen.NcePacketGenAction(rb.getString("MenuItemSendCommand")));
-    }
-    
-    private Item[] panelItems = new Item[] {
-        new Item("MenuItemCommandMonitor", "jmri.jmrix.nce.ncemon.NceMonPanel"),
-        new Item("MenuItemSendCommand", "jmri.jmrix.nce.packetgen.NcePacketGenPanel")
-    };
-        
-    static class Item {
-        Item(String name, String load) {
-            this.name = name;
-            this.load = load;
-        }
-        String name;
-        String load;
+@Deprecated
+@edu.umd.cs.findbugs.annotations.SuppressWarnings(value="NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
+public class WangrowMenu extends jmri.jmrix.nce.swing.NceMenu {
+    public WangrowMenu(NceSystemConnectionMemo memo) {
+        super(memo);
     }
 }
 
--- java/src/jmri/jmrix/wangrow/serialdriver/ConnectionConfig.java
+++ java/src/jmri/jmrix/wangrow/serialdriver/ConnectionConfig.java
@@ -2,6 +2,8 @@
 
 package jmri.jmrix.wangrow.serialdriver;
 
+import jmri.jmrix.nce.serialdriver.SerialDriverAdapter;
+
 /**
  * Definition of objects to handle configuring a layout connection
  * via an Wangrow SerialDriverAdapter object.
@@ -29,6 +31,9 @@ public class ConnectionConfig  extends jmri.jmrix.AbstractSerialConnectionConfig
 
     public String name() { return NAME; }
 
-    protected void setInstance() { adapter = SerialDriverAdapter.instance(); }
+    protected void setInstance() {
+        if (adapter == null)
+            adapter = new SerialDriverAdapter();
+    }
 }
 
