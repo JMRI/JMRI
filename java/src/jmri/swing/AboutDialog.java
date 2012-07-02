da7dc6e73ddc133cc5653b34e1f0e4b5a525a35e
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-07-02 22:16:47 +0000

    Better-looking about box. List all connections, not just the first four.

--- java/src/jmri/swing/AboutDialog.java
+++ java/src/jmri/swing/AboutDialog.java
@@ -1,10 +1,9 @@
 // AboutDialog.java
 package jmri.swing;
 
-import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
+import java.awt.Font;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Locale;
@@ -13,8 +12,6 @@ import javax.swing.*;
 import jmri.Application;
 import jmri.Version;
 import jmri.jmrix.ConnectionConfig;
-import jmri.jmrix.ConnectionStatus;
-import jmri.jmrix.JmrixConfigPane;
 
 /**
  * About dialog.
@@ -22,7 +19,7 @@ import jmri.jmrix.JmrixConfigPane;
  * @author Randall Wood Copyright (C) 2012
  * @version $Revision$
  */
-public class AboutDialog extends JDialog implements PropertyChangeListener {
+public class AboutDialog extends JDialog {
 
     ConnectionConfig[] connection = {null, null, null, null};
     protected static ResourceBundle rb = ResourceBundle.getBundle("apps.AppsBundle");
@@ -45,72 +42,6 @@ public class AboutDialog extends JDialog implements PropertyChangeListener {
         this.setLocationRelativeTo(null); // center on screen
         log.debug("End constructor");
     }
-    // line 4
-    JLabel cs4 = new JLabel();
-
-    protected void buildLine4(JPanel pane) {
-        if (connection[0] != null) {
-            buildLine(connection[0], cs4, pane);
-        }
-    }
-    // line 5 optional
-    JLabel cs5 = new JLabel();
-
-    protected void buildLine5(JPanel pane) {
-        if (connection[1] != null) {
-            buildLine(connection[1], cs5, pane);
-        }
-    }
-    // line 6 optional
-    JLabel cs6 = new JLabel();
-
-    protected void buildLine6(JPanel pane) {
-        if (connection[2] != null) {
-            buildLine(connection[2], cs6, pane);
-        }
-    }
-    // line 7 optional
-    JLabel cs7 = new JLabel();
-
-    protected void buildLine7(JPanel pane) {
-        if (connection[3] != null) {
-            buildLine(connection[3], cs7, pane);
-        }
-    }
-
-    protected void buildLine(ConnectionConfig conn, JLabel cs, JPanel pane) {
-        if (conn.name().equals(JmrixConfigPane.NONE)) {
-            cs.setText(" ");
-            return;
-        }
-        ConnectionStatus.instance().addConnection(conn.name(), conn.getInfo());
-        cs.setFont(pane.getFont());
-        updateLine(conn, cs);
-        pane.add(cs);
-    }
-    
-    protected void updateLine(ConnectionConfig conn, JLabel cs) {
-        if (conn.getDisabled()) {
-            return;
-        }
-        String name = conn.getConnectionName();
-        if (name == null) {
-            name = conn.getManufacturer();
-        }
-        if (ConnectionStatus.instance().isConnectionOk(conn.getInfo())) {
-            cs.setForeground(Color.black);
-            String cf = MessageFormat.format(rb.getString("ConnectionSucceeded"),
-                    new Object[]{name, conn.name(), conn.getInfo()});
-            cs.setText(cf);
-        } else {
-            cs.setForeground(Color.red);
-            String cf = MessageFormat.format(rb.getString("ConnectionFailed"),
-                    new Object[]{name, conn.name(), conn.getInfo()});
-            cf = cf.toUpperCase();
-            cs.setText(cf);
-        }
-
-    }
 
     protected JPanel namePane() {
         String logo = Application.getLogo();
@@ -119,77 +50,46 @@ public class AboutDialog extends JDialog implements PropertyChangeListener {
         if (log.isDebugEnabled()) {
             log.debug("Fetch main logo: " + logo + " " + getToolkit().getImage(logo));
         }
-        pane.add(new JLabel(new ImageIcon(getToolkit().getImage(logo), "JMRI logo"), JLabel.CENTER));
+        addCenteredComponent(new JLabel(new ImageIcon(getToolkit().getImage(logo), "JMRI logo"), JLabel.CENTER), pane);
         pane.add(Box.createRigidArea(new Dimension(0, 15)));
-        pane.add(new JLabel(Application.getApplicationName()));
-        pane.add(new JLabel(Application.getURL()));
+        JLabel appName = new JLabel(Application.getApplicationName(), JLabel.CENTER);
+        appName.setFont(pane.getFont().deriveFont(Font.BOLD, pane.getFont().getSize() * 1.2f));
+        addCenteredComponent(appName, pane);
+        addCenteredComponent(new JLabel(Application.getURL(), JLabel.CENTER), pane);
         pane.add(Box.createRigidArea(new Dimension(0, 15)));
+        pane.setAlignmentX(Component.CENTER_ALIGNMENT);
         return pane;
     }
 
     protected JPanel infoPane() {
         JPanel pane1 = new JPanel();
-        String spacer = " ";
         pane1.setLayout(new BoxLayout(pane1, BoxLayout.Y_AXIS));
 
         log.debug("start labels");
 
         // add listerner for Com port updates
-        ConnectionStatus.instance().addPropertyChangeListener(this);
-        ArrayList<Object> connList = jmri.InstanceManager.configureManagerInstance().getInstanceList(jmri.jmrix.ConnectionConfig.class);
-        int i = 0;
-        if (connList != null) {
-            for (int x = 0; x < connList.size(); x++) {
-                jmri.jmrix.ConnectionConfig conn = (jmri.jmrix.ConnectionConfig) connList.get(x);
-                if (!conn.getDisabled()) {
-                    connection[i] = conn;
-                    i++;
-                }
-                if (i > 3) {
-                    break;
+        ArrayList connList = jmri.InstanceManager.configureManagerInstance().getInstanceList(jmri.jmrix.ConnectionConfig.class);
+        if (!connList.isEmpty()) {
+            for (Object conn : connList) {
+                if (!((ConnectionConfig) conn).getDisabled()) {
+                    pane1.add(new ConnectionLabel((ConnectionConfig) conn));
                 }
             }
+            pane1.add(Box.createRigidArea(new Dimension(0, 15)));
         }
-        buildLine4(pane1);
-        buildLine5(pane1);
-        buildLine6(pane1);
-        buildLine7(pane1);
 
-        pane1.add(new JLabel(spacer));
         pane1.add(new JLabel(MessageFormat.format(rb.getString("DefaultVersionCredit"),
                 new Object[]{Version.name()})));
         pane1.add(new JLabel(MessageFormat.format(rb.getString("JavaVersionCredit"),
                 new Object[]{System.getProperty("java.version", "<unknown>"),
                     Locale.getDefault().toString()})));
+        pane1.setAlignmentX(Component.CENTER_ALIGNMENT);
         return pane1;
     }
 
     protected void addCenteredComponent(JComponent c, JPanel p) {
-        // c.setAlignmentX(Component.CENTER_ALIGNMENT); // doesn't work
+        c.setAlignmentX(Component.CENTER_ALIGNMENT); // doesn't work
         p.add(c);
-    }
-
-    @Override
-    public void propertyChange(PropertyChangeEvent ev) {
-        if (log.isDebugEnabled()) {
-            log.debug("property change: comm port status update");
-        }
-        if (connection[0] != null) {
-            updateLine(connection[0], cs4);
-        }
-
-        if (connection[1] != null) {
-            updateLine(connection[1], cs5);
-        }
-
-        if (connection[2] != null) {
-            updateLine(connection[2], cs6);
-        }
-
-        if (connection[3] != null) {
-            updateLine(connection[3], cs7);
-        }
-
     }
     static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(AboutDialog.class.getName());
 }
---
+++ java/src/jmri/swing/ConnectionLabel.java
@@ -0,0 +1,61 @@
+// ConnectionLabel.java
+package jmri.swing;
+
+import java.awt.Color;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+import javax.swing.JLabel;
+import jmri.jmrix.ConnectionConfig;
+import jmri.jmrix.ConnectionStatus;
+import jmri.jmrix.JmrixConfigPane;
+
+/**
+ * A JLabel that listens to a system connection and reports its status
+ *
+ * @author rhwood
+ */
+public final class ConnectionLabel extends JLabel implements PropertyChangeListener {
+
+    ConnectionConfig connection;
+    protected static ResourceBundle appsBundle = ResourceBundle.getBundle("apps.AppsBundle");
+
+    public ConnectionLabel(ConnectionConfig connection) {
+        super();
+        this.connection = connection;
+        if (connection.name().equals(JmrixConfigPane.NONE)) {
+            this.setText("");
+        } else {
+            ConnectionStatus.instance().addConnection(connection.name(), connection.getInfo());
+            this.update();
+        }
+        ConnectionStatus.instance().addPropertyChangeListener(this);
+    }
+
+    protected void update() {
+        if (this.connection.getDisabled()) {
+            return;
+        }
+        String name = this.connection.getConnectionName();
+        if (name == null) {
+            name = this.connection.getManufacturer();
+        }
+        if (ConnectionStatus.instance().isConnectionOk(this.connection.getInfo())) {
+            this.setForeground(Color.BLACK);
+            this.setText(MessageFormat.format(appsBundle.getString("ConnectionSucceeded"),
+                    new Object[]{name, this.connection.name(), this.connection.getInfo()}));
+        } else {
+            this.setForeground(Color.RED);
+            this.setText(MessageFormat.format(appsBundle.getString("ConnectionFailed"),
+                    new Object[]{name, this.connection.name(), this.connection.getInfo()}));
+        }
+        this.revalidate();
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        this.update();
+    }
+
+}
