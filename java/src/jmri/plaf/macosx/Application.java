f6af35cf0404b68a69d6289b073a3efa5c890ebb
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-07-30 00:42:03 +0000

    Remove Java 1.5 supporting elements deprecated in OS X 10.5+.

--- java/src/jmri/plaf/macosx/Application.java
+++ java/src/jmri/plaf/macosx/Application.java
@@ -4,7 +4,6 @@ package jmri.plaf.macosx;
 import com.apple.eawt.AppEvent.AboutEvent;
 import com.apple.eawt.AppEvent.PreferencesEvent;
 import com.apple.eawt.AppEvent.QuitEvent;
-import com.apple.eawt.ApplicationEvent;
 import com.apple.eawt.FullScreenListener;
 import com.apple.eawt.FullScreenUtilities;
 import com.apple.eawt.QuitResponse;
@@ -13,22 +12,19 @@ import jmri.util.SystemType;
 
 /**
  * Wrapper for Apple provided extensions to Java that allow Java apps to feel
- * more "Mac-like" on Mac OS X.
- * <p>
- * <b>NOTE</b> All use of this class must be wrapped in a conditional test that
- * ensures that JMRI is not running on Mac OS X or in Try-Catch blocks. The
- * easiest test is:
+ * more "Mac-like" on Mac OS X. <p> <b>NOTE</b> All use of this class must be
+ * wrapped in a conditional test that ensures that JMRI is not running on Mac OS
+ * X or in Try-Catch blocks. The easiest test is:
  * <code><pre>
  * if (SystemType.isMacOSX()) {
  *     ...
  * }
- * </pre></code>
- * A Try-Catch block will need to catch {@link java.lang.NoClassDefFoundError}
- * Failure to use one of these methods will result in crashes.
- * <p>
- * This wrapper currently provides incomplete support for the Apple
- * {@link com.apple.eawt.Application} class, as it only provides support for
- * those integration aspects that were implemented in JMRI 3.1.
+ * </pre></code> A Try-Catch block will need to catch
+ * {@link java.lang.NoClassDefFoundError} Failure to use one of these methods
+ * will result in crashes. <p> This wrapper currently provides incomplete
+ * support for the Apple {@link com.apple.eawt.Application} class, as it only
+ * provides support for those integration aspects that were implemented in JMRI
+ * 3.1.
  *
  * @author rhwood
  * @see com.apple.eawt.Application
@@ -37,7 +33,6 @@ public class Application {
 
     private static Application sharedApplication = null;
     private com.apple.eawt.Application application = null;
-    private ApplicationListener legacyListener = null;
 
     public static Application getApplication() {
         if (!SystemType.isMacOSX()) {
@@ -68,8 +63,8 @@ public class Application {
                 application.setAboutHandler(null);
             }
         } catch (java.lang.NoClassDefFoundError ex) {
-            legacyListener();
-            legacyListener.setAboutHandler(handler);
+            // this simply means that the OS X version is too old to implement this
+            // so we ignore it
         }
     }
 
@@ -86,8 +81,8 @@ public class Application {
                 application.setPreferencesHandler(null);
             }
         } catch (java.lang.NoClassDefFoundError ex) {
-            legacyListener();
-            legacyListener.setPreferencesHandler(handler);
+            // this simply means that the OS X version is too old to implement this
+            // so we ignore it
         }
     }
 
@@ -116,7 +111,7 @@ public class Application {
     public static void removeFullScreenListenerFrom(Window window, FullScreenListener listener) {
         if (SystemType.isMacOSX()) {
             try {
-               FullScreenUtilities.removeFullScreenListenerFrom(window, listener);
+                FullScreenUtilities.removeFullScreenListenerFrom(window, listener);
             } catch (java.lang.NoClassDefFoundError ex) {
                 // this simply means that the OS X version is too old to implement this
                 // so we ignore it
@@ -141,89 +136,8 @@ public class Application {
                 application.setQuitHandler(null);
             }
         } catch (java.lang.NoClassDefFoundError ex) {
-            legacyListener();
-            legacyListener.setQuitHandler(handler);
-        }
-    }
-
-    // Support Java 1.5
-    private void legacyListener() {
-        if (legacyListener == null) {
-            legacyListener = new ApplicationListener();
-            application.addApplicationListener(legacyListener);
-        }
-    }
-
-    // Support Java 1.5
-    private class ApplicationListener implements com.apple.eawt.ApplicationListener {
-
-        private QuitHandler quitHandler = null;
-        private AboutHandler aboutHandler = null;
-        private PreferencesHandler preferencesHandler = null;
-
-        protected void setAboutHandler(AboutHandler handler) {
-            aboutHandler = handler;
-            if (handler != null) {
-                application.addAboutMenuItem();
-                application.setEnabledAboutMenu(true);
-            } else {
-                application.setEnabledAboutMenu(false);
-                application.removeAboutMenuItem();
-            }
-        }
-
-        protected void setPreferencesHandler(PreferencesHandler handler) {
-            preferencesHandler = handler;
-            if (handler != null) {
-                application.addPreferencesMenuItem();
-                application.setEnabledPreferencesMenu(true);
-            } else {
-                application.setEnabledPreferencesMenu(false);
-                application.removePreferencesMenuItem();
-            }
-        }
-
-        protected void setQuitHandler(QuitHandler handler) {
-            quitHandler = handler;
-        }
-
-        @Override
-        public void handleAbout(ApplicationEvent ae) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public void handleOpenApplication(ApplicationEvent ae) {
-            // Do not take any special activity if a user opens the application
-        }
-
-        @Override
-        public void handleOpenFile(ApplicationEvent ae) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public void handlePreferences(ApplicationEvent ae) {
-            if (preferencesHandler != null) {
-                preferencesHandler.handlePreferences(ae);
-            }
-        }
-
-        @Override
-        public void handlePrintFile(ApplicationEvent ae) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
-
-        @Override
-        public void handleQuit(ApplicationEvent ae) {
-            if (quitHandler != null) {
-                ae.setHandled(quitHandler.handleQuitRequest(ae));
-            }
-        }
-
-        @Override
-        public void handleReOpenApplication(ApplicationEvent ae) {
-            // Do not take any special activity if a user opens the application
+            // this simply means that the OS X version is too old to implement this
+            // so we ignore it
         }
     }
 }
