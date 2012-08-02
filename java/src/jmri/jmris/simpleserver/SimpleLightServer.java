9c478b388c1555f4b65d917d6e949075c0465eb8
Randall Wood <randall.h.wood@alexandriasoftware.com>
2012-08-02 11:00:12 +0000

    Change error message from TURNOUT to LIGHT. Reformat text, add @Override annotations. Explicitly import classes instead of including packages in class names.

--- java/src/jmri/jmris/simpleserver/SimpleLightServer.java
+++ java/src/jmri/jmris/simpleserver/SimpleLightServer.java
@@ -1,65 +1,64 @@
 //SimpleLightServer.java
-
 package jmri.jmris.simpleserver;
 
 import java.io.*;
-
+import jmri.JmriException;
 import jmri.Light;
-
 import jmri.jmris.AbstractLightServer;
+import org.apache.log4j.Logger;
 
 /**
- * Simple Server interface between the JMRI light manager and a
- * network connection
- * @author          Paul Bender Copyright (C) 2010
- * @version         $Revision$
+ * Simple Server interface between the JMRI light manager and a network
+ * connection
+ *
+ * @author Paul Bender Copyright (C) 2010
+ * @version $Revision$
  */
-
 public class SimpleLightServer extends AbstractLightServer {
 
-   private DataOutputStream output;
+    private DataOutputStream output;
 
-   public SimpleLightServer(DataInputStream inStream,DataOutputStream outStream){
+    public SimpleLightServer(DataInputStream inStream, DataOutputStream outStream) {
 
-        output=outStream;
+        output = outStream;
     }
 
 
     /*
      * Protocol Specific Abstract Functions
      */
-
-     public void sendStatus(String lightName, int Status) throws IOException
-     {
-	if(Status==Light.ON){
-		output.writeBytes("LIGHT " + lightName + " ON\n");
-        } else if (Status==Light.OFF){
-		output.writeBytes("LIGHT " + lightName + " OFF\n");
+    @Override
+    public void sendStatus(String lightName, int Status) throws IOException {
+        if (Status == Light.ON) {
+            output.writeBytes("LIGHT " + lightName + " ON\n");
+        } else if (Status == Light.OFF) {
+            output.writeBytes("LIGHT " + lightName + " OFF\n");
         } else {
-               //  unknown state
-		output.writeBytes("LIGHT " + lightName + " UNKNOWN\n");
+            //  unknown state
+            output.writeBytes("LIGHT " + lightName + " UNKNOWN\n");
         }
-     }
+    }
 
-     public void sendErrorStatus(String lightName) throws IOException {
- 	output.writeBytes("TURNOUT ERROR\n");
-     }
+    @Override
+    public void sendErrorStatus(String lightName) throws IOException {
+        output.writeBytes("LIGHT ERROR\n");
+    }
 
-     public void parseStatus(String statusString) throws jmri.JmriException,java.io.IOException {
-            int index;
-            index=statusString.indexOf(" ")+1;
-	    if(statusString.contains("ON")){
-                   if(log.isDebugEnabled())
-                      log.debug("Setting Light ON");
-                   lightOn(statusString.substring(index,statusString.indexOf(" ",index+1)));
-            } else if(statusString.contains("OFF")){
-                   if(log.isDebugEnabled())
-                      log.debug("Setting Light OFF");
-                   lightOff(statusString.substring(index,statusString.indexOf(" ",index+1)));
+    @Override
+    public void parseStatus(String statusString) throws JmriException, IOException {
+        int index;
+        index = statusString.indexOf(" ") + 1;
+        if (statusString.contains("ON")) {
+            if (log.isDebugEnabled()) {
+                log.debug("Setting Light ON");
             }
-     }
-
-
-    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SimpleLightServer.class.getName());
-
+            lightOn(statusString.substring(index, statusString.indexOf(" ", index + 1)));
+        } else if (statusString.contains("OFF")) {
+            if (log.isDebugEnabled()) {
+                log.debug("Setting Light OFF");
+            }
+            lightOff(statusString.substring(index, statusString.indexOf(" ", index + 1)));
+        }
+    }
+    static Logger log = Logger.getLogger(SimpleLightServer.class.getName());
 }
