7ca22d409e4895577915622c73df74be2c1db860
Bob Jacobsen <jacobsen@mac.com>
2012-09-05 22:12:51 +0000

    more directories

---
+++ resources/icons/largeschematics/aspects/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JMRI Icons</title>
+</head>
+<body>
+<h1>JMRI Resources</h1>
+
+
+<?php
+
+require("../../../showIcons.php");
+
+showSubdirs();
+showFiles();
+
+?>
+</table>
+
+
+</body>
+</html>
