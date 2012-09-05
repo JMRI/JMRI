db824cd0c78e040417403cc13b5d7218bebe575f
Bob Jacobsen <jacobsen@mac.com>
2012-09-05 22:05:17 +0000

    more on appearance. Refactor to single codefile

--- resources/icons/index.php
+++ resources/icons/index.php
@@ -9,35 +9,10 @@
 
 <?php
 
-function endswith($string, $test) {
-    $strlen = strlen($string);
-    $testlen = strlen($test);
-    if ($testlen > $strlen) return false;
-    return substr_compare(strtolower($string), strtolower($test), -$testlen) === 0;
-}
+require("../showIcons.php");
 
-
-echo "<h2>Subdirectories</h2>\n";
-
-$d = dir(".");
-while (false !== ($entry = $d->read())) {
-   if (is_dir($entry)) {
-    echo '<a href="'.$entry.'">'.$entry.'</a></td><p>'."\n";
-   }
-}
-$d->close();
-
-echo "<h2>Icons</h2>\n";
-
-echo '<table border="1">';
-
-$d = dir(".");
-while (false !== ($entry = $d->read())) {
-   if (endswith($entry, ".gif") || endswith($entry, ".jpg") || endswith($entry, ".png")) {
-    echo '<tr><td>'.$entry.' </td><td><a href="'.$entry.'"><img src="'.$entry.'"></a></td>'."\n";
-   }
-}
-$d->close();
+showSubdirs();
+showFiles();
 
 ?>
 </table>
