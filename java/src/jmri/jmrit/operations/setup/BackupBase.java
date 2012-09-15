28532c1a8998f77e3a67d9090ff9333eb15b606c
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2012-09-15 13:23:46 +0000

    Fix FindBug warning

--- java/src/jmri/jmrit/operations/setup/BackupBase.java
+++ java/src/jmri/jmrit/operations/setup/BackupBase.java
@@ -430,7 +430,7 @@ public abstract class BackupBase {
 		 * @param overwrite
 		 * @throws IOException
 		 */
-		//@edu.umd.cs.findbugs.annotations.SuppressWarnings(value="OBL_UNSATISFIED_OBLIGATION")
+		@edu.umd.cs.findbugs.annotations.SuppressWarnings(value="OBL_UNSATISFIED_OBLIGATION")
 		public static void copy(String sourceFileName, String destFileName,
 				Boolean overwrite) throws IOException {
 
