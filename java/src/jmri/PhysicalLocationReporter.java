80d212c2d94b3bb8a3cff45b4652d22f1268b13e
Mark Underwood <msunderwd@gmail.com>
2012-10-20 03:29:03 +0000

    Interface for getting a locomotive number from a Reporter that supports it.  Not all reporters do or ever will.

---
+++ java/src/jmri/PhysicalLocationReporter.java
@@ -0,0 +1,35 @@
+package jmri;
+
+import jmri.util.PhysicalLocation;
+
+/* <hr>
+ * PhysicalLocationReporter Interface
+ * </hr>
+ * Implements a common way that a Reporter that supports having a Physical Location
+ * can report that location to querying objects.
+/*
+
+/*
+ * <hr>
+ * This file is part of JMRI.
+ * <P>
+ * JMRI is free software; you can redistribute it and/or modify it under 
+ * the terms of version 2 of the GNU General Public License as published 
+ * by the Free Software Foundation. See the "COPYING" file for a copy
+ * of this license.
+ * <P>
+ * JMRI is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
+ * for more details.
+ * <P>
+ *
+ * @author			Mark Underwood Copyright (C) 2012
+ * @version			$Revision: 18722 $
+ */
+
+public interface PhysicalLocationReporter {
+
+    public LocoAddress getLocoAddress(String s);
+}
+
