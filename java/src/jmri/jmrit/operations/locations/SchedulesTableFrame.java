02ad612163802a60475661fb2c8749358a8ad7bb
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2012-12-29 16:58:20 +0000

    Minor code clean up

--- java/src/jmri/jmrit/operations/locations/SchedulesTableFrame.java
+++ java/src/jmri/jmrit/operations/locations/SchedulesTableFrame.java
@@ -1,13 +1,12 @@
 // SchedulesTableFrame.java
 
 package jmri.jmrit.operations.locations;
- 
+
 import jmri.jmrit.operations.setup.Control;
 import jmri.jmrit.operations.OperationsFrame;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.text.MessageFormat;
 import java.util.ResourceBundle;
 
 import javax.swing.BoxLayout;
@@ -19,112 +18,116 @@ import javax.swing.ScrollPaneConstants;
 
 /**
  * Frame for adding and editing the Schedule roster for operations.
- *
- * @author		Bob Jacobsen   Copyright (C) 2001
- * @author Daniel Boudreau Copyright (C) 2009
- * @version             $Revision$
+ * 
+ * @author Bob Jacobsen Copyright (C) 2001
+ * @author Daniel Boudreau Copyright (C) 2009, 2012
+ * @version $Revision$
  */
 public class SchedulesTableFrame extends OperationsFrame {
-	
-	static ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrit.operations.locations.JmritOperationsLocationsBundle");
+
+	static ResourceBundle rb = ResourceBundle
+			.getBundle("jmri.jmrit.operations.locations.JmritOperationsLocationsBundle");
 
 	SchedulesTableModel schedulesModel = new SchedulesTableModel();
 	javax.swing.JTable schedulesTable = new javax.swing.JTable(schedulesModel);
 	JScrollPane schedulesPane;
-	
+
 	// labels
 	javax.swing.JLabel textSort = new javax.swing.JLabel();
 	javax.swing.JLabel textSep = new javax.swing.JLabel();
-	
+
 	// radio buttons
-    javax.swing.JRadioButton sortByName = new javax.swing.JRadioButton(rb.getString("Name"));
-    javax.swing.JRadioButton sortById = new javax.swing.JRadioButton(rb.getString("Id"));
+	javax.swing.JRadioButton sortByName = new javax.swing.JRadioButton(rb.getString("Name"));
+	javax.swing.JRadioButton sortById = new javax.swing.JRadioButton(rb.getString("Id"));
 
 	// major buttons
-	javax.swing.JButton addButton = new javax.swing.JButton();
+	// javax.swing.JButton addButton = new javax.swing.JButton();
 
-    public SchedulesTableFrame() {
-        super(ResourceBundle.getBundle("jmri.jmrit.operations.locations.JmritOperationsLocationsBundle").getString("TitleSchedulesTable"));
-        // general GUI config
+	public SchedulesTableFrame() {
+		super(ResourceBundle.getBundle(
+				"jmri.jmrit.operations.locations.JmritOperationsLocationsBundle").getString(
+				"TitleSchedulesTable"));
+		// general GUI config
 
-        getContentPane().setLayout(new BoxLayout(getContentPane(),BoxLayout.Y_AXIS));
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-    	// Set up the jtable in a Scroll Pane..
-    	schedulesPane = new JScrollPane(schedulesTable);
-    	schedulesPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-    	schedulesPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-       	schedulesModel.initTable(this, schedulesTable);
-     	getContentPane().add(schedulesPane);
-     	
-     	// Set up the control panel
-    	JPanel controlPanel = new JPanel();
-    	controlPanel.setLayout(new FlowLayout());
-    	
-    	textSort.setText("Sort by");
-    	controlPanel.add(textSort);
-    	controlPanel.add(sortByName);
-    	sortByName.setSelected(true);
-    	controlPanel.add(sortById);
-    	textSep.setText("          ");
-    	controlPanel.add(textSep);
+		// Set up the jtable in a Scroll Pane..
+		schedulesPane = new JScrollPane(schedulesTable);
+		schedulesPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		schedulesPane
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		schedulesModel.initTable(this, schedulesTable);
+		getContentPane().add(schedulesPane);
 
-		addButton.setText(rb.getString("Add"));
-		addButton.setVisible(true);
+		// Set up the control panel
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new FlowLayout());
+
+		textSort.setText(rb.getString("SortBy"));
+		controlPanel.add(textSort);
+		controlPanel.add(sortByName);
+		sortByName.setSelected(true);
+		controlPanel.add(sortById);
+		textSep.setText("          ");
+		controlPanel.add(textSep);
+
 		// TODO allow user to add schedule to a spur
-		//controlPanel.add (addButton);
+		// addButton.setText(rb.getString("Add"));
+		// addButton.setVisible(true);
+		// controlPanel.add (addButton);
 		controlPanel.setMaximumSize(new Dimension(Control.panelWidth, 50));
-	   	getContentPane().add(controlPanel);
-	   	
+		getContentPane().add(controlPanel);
+
 		// setup buttons
-		addButtonAction(addButton);
-		
-		addRadioButtonAction (sortByName);
-		addRadioButtonAction (sortById);
-    	
-		//	build menu
+		// addButtonAction(addButton);
+
+		addRadioButtonAction(sortByName);
+		addRadioButtonAction(sortById);
+
+		// build menu
 		JMenuBar menuBar = new JMenuBar();
 		JMenu toolMenu = new JMenu("Tools");
 		toolMenu.add(new SchedulesByLoadAction(rb.getString("MenuItemShowSchedulesByLoad")));
 		menuBar.add(toolMenu);
 		setJMenuBar(menuBar);
-    	addHelpMenu("package.jmri.jmrit.operations.Operations_Schedules", true);
-    	
-    	pack();
+		addHelpMenu("package.jmri.jmrit.operations.Operations_Schedules", true);
+
+		pack();
 		if (getWidth() < Control.panelWidth)
 			setSize(Control.panelWidth, getHeight());
-    	
-    }
-    
+
+	}
+
 	public void radioButtonActionPerformed(java.awt.event.ActionEvent ae) {
 		log.debug("radio button activated");
-		if (ae.getSource() == sortByName){
+		if (ae.getSource() == sortByName) {
 			sortByName.setSelected(true);
 			sortById.setSelected(false);
 			schedulesModel.setSort(schedulesModel.SORTBYNAME);
 		}
-		if (ae.getSource() == sortById){
+		if (ae.getSource() == sortById) {
 			sortByName.setSelected(false);
 			sortById.setSelected(true);
 			schedulesModel.setSort(schedulesModel.SORTBYID);
 		}
 	}
-    
+
 	// add button
-	public void buttonActionPerformed(java.awt.event.ActionEvent ae) {
-//		log.debug("add schedule button activated");
-		if (ae.getSource() == addButton){
-			ScheduleEditFrame f = new ScheduleEditFrame();
-			f.setTitle(MessageFormat.format(rb.getString("TitleScheduleAdd"), new Object[]{"Track Name"}));
-			f.initComponents(null, null, null);
-		}
+	// public void buttonActionPerformed(java.awt.event.ActionEvent ae) {
+	// log.debug("add schedule button activated");
+	// if (ae.getSource() == addButton){
+	// ScheduleEditFrame f = new ScheduleEditFrame();
+	// f.setTitle(MessageFormat.format(rb.getString("TitleScheduleAdd"), new Object[]{"Track Name"}));
+	// f.initComponents(null, null, null);
+	// }
+	// }
+
+	public void dispose() {
+		saveTableDetails(schedulesTable);
+		schedulesModel.dispose();
+		super.dispose();
 	}
 
-    public void dispose() {
-    	saveTableDetails(schedulesTable);
-    	schedulesModel.dispose();
-        super.dispose();
-    }
-    
 	static org.apache.log4j.Logger log = org.apache.log4j.Logger
-	.getLogger(SchedulesTableFrame.class.getName());
+			.getLogger(SchedulesTableFrame.class.getName());
 }
