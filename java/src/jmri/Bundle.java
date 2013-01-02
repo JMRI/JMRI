c705a5a55b21d55dcdc5d49af24c27855f92e1c2
Bob Jacobsen <jacobsen@mac.com>
2013-01-02 01:05:44 +0000

    improve JavaDoc

--- java/src/jmri/Bundle.java
+++ java/src/jmri/Bundle.java
@@ -43,13 +43,36 @@ public class Bundle {
      *<p>
      * Note that this is intentionally package-local
      * access.
+     * @param key Bundle key to be translated
+     * @return Internationalized text
      */
     static String getString(String key) {
         return b.handleGetString(key);
     }
+    /**
+     * Provides access to a string for a given 
+     * key from the package resource bundle or 
+     * parent.
+     *<p>
+     * Note that this is intentionally package-local
+     * access.
+     * @param key Bundle key to be translated
+     * @return Internationalized text
+     */
     static String getMessage(String key) {
         return b.handleGetMessage(key);
     }
+    /**
+     * Merges user data with a string for a given 
+     * key from the package resource bundle or 
+     * parent.
+     *<p>
+     * Note that this is intentionally package-local
+     * access.
+     * @param key Bundle key to be translated
+     * @param subs One or more objects to be inserted into the message
+     * @return Internationalized text
+     */
     static String getMessage(String key, Object ... subs) {
         return b.handleGetMessage(key, subs);
     }
--- java/src/jmri/jmrit/Bundle.java
+++ java/src/jmri/jmrit/Bundle.java
@@ -5,22 +5,8 @@ package jmri.jmrit;
 /**
  * Provides standard access for resource bundles in a package.
  * 
- * Convention pattern is to provide a subclass of this name
+ * Convention is to provide a subclass of this name
  * in each package, working off the local resource bundle name.
- *
- * <hr>
- * This file is part of JMRI.
- * <P>
- * JMRI is free software; you can redistribute it and/or modify it under
- * the terms of version 2 of the GNU General Public License as published
- * by the Free Software Foundation. See the "COPYING" file for a copy
- * of this license.
- * <P>
- * JMRI is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- * <P>
  *
  * @author      Bob Jacobsen  Copyright (C) 2012
  * @version     $Revision: 17977 $
@@ -30,6 +16,10 @@ public class Bundle extends jmri.Bundle {
 
     protected static Bundle b = new Bundle("jmri.jmrit.Bundle"); // NOI18N
 
+    //
+    // below here is boilerplate to be copied exactly
+    //
+    
     /**
      * Provides access to a string for a given 
      * key from the package resource bundle or 
@@ -37,13 +27,36 @@ public class Bundle extends jmri.Bundle {
      *<p>
      * Note that this is intentionally package-local
      * access.
+     * @param key Bundle key to be translated
+     * @return Internationalized text
      */
     static String getString(String key) {
         return b.handleGetString(key);
     }
+    /**
+     * Provides access to a string for a given 
+     * key from the package resource bundle or 
+     * parent.
+     *<p>
+     * Note that this is intentionally package-local
+     * access.
+     * @param key Bundle key to be translated
+     * @return Internationalized text
+     */
     static String getMessage(String key) {
         return b.handleGetMessage(key);
     }
+    /**
+     * Merges user data with a string for a given 
+     * key from the package resource bundle or 
+     * parent.
+     *<p>
+     * Note that this is intentionally package-local
+     * access.
+     * @param key Bundle key to be translated
+     * @param subs One or more objects to be inserted into the message
+     * @return Internationalized text
+     */
     static String getMessage(String key, Object ... subs) {
         return b.handleGetMessage(key, subs);
     }
