1a87d842ba73d0ba590724b417eee59246c9fe01
Randall Wood <randall.h.wood@alexandriasoftware.com>
2013-01-14 11:54:18 +0000

    Search through jmri.jar for programmer XML definitions if none could be found in the directory returned by XmlFile.xmlDir(). Also use XmlFilenameFilter to filter File.list() instead of iterating over the results.

--- java/src/jmri/jmrit/symbolicprog/ProgDefault.java
+++ java/src/jmri/jmrit/symbolicprog/ProgDefault.java
@@ -1,23 +1,26 @@
 // ProgDefault.java
-
 package jmri.jmrit.symbolicprog;
 
-import jmri.jmrit.*;
-import java.io.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.jar.JarEntry;
+import jmri.jmrit.XmlFile;
+import jmri.util.FileUtil;
+import jmri.util.XmlFilenameFilter;
+import org.apache.log4j.Logger;
 
 /**
- * Functions for use with programmer files, including the default file name.
- * <P>
- * This was refactored from LocoSelPane in JMRI 1.5.3, which was the
- * the right thing to do anyway.  But the real reason was that
- * on MacOS Classic the static member holding the default programmer name
- * was being overwritten when the class was (erroneously) initialized 
- * for a second time.  This refactoring did not fix the problem.
- * What did fix it was an ugly hack in the 
- * {@link CombinedLocoSelPane} class; see comments there for 
- * more information.
+ * Functions for use with programmer files, including the default file name. <P>
+ * This was refactored from LocoSelPane in JMRI 1.5.3, which was the the right
+ * thing to do anyway. But the real reason was that on MacOS Classic the static
+ * member holding the default programmer name was being overwritten when the
+ * class was (erroneously) initialized for a second time. This refactoring did
+ * not fix the problem. What did fix it was an ugly hack in the
+ * {@link CombinedLocoSelPane} class; see comments there for more information.
  *
- * @author	Bob Jacobsen   Copyright (C) 2001, 2002
+ * @author	Bob Jacobsen Copyright (C) 2001, 2002
  * @version	$Revision$
  */
 public class ProgDefault {
@@ -26,54 +29,76 @@ public class ProgDefault {
         // create an array of file names from prefs/programmers, count entries
         int i;
         int np = 0;
-        String[] sp = null;
-        XmlFile.ensurePrefsPresent(XmlFile.prefsDir()+"programmers");
-        File fp = new File(XmlFile.prefsDir()+"programmers");
+        String[] sp = {};
+        XmlFile.ensurePrefsPresent(XmlFile.prefsDir() + "programmers");
+        File fp = new File(XmlFile.prefsDir() + "programmers");
+        XmlFilenameFilter filter = new XmlFilenameFilter();
         if (fp.exists()) {
-            sp = fp.list();
-            for (i=0; i<sp.length; i++) {
-                if (sp[i].endsWith(".xml")) np++;
-            }
+            sp = fp.list(filter);
+            np = sp.length;
         } else {
-            log.warn(XmlFile.prefsDir()+"programmers was missing, though tried to create it");
+            log.warn(XmlFile.prefsDir() + "programmers was missing, though tried to create it");
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("Got " + np + " programmers from " + fp.getPath());
         }
         // create an array of file names from xml/programmers, count entries
-        String[] sx = (new File(XmlFile.xmlDir()+"programmers")).list();
-        int nx = 0;
-        for (i=0; i<sx.length; i++) {
-            if (sx[i].endsWith(".xml")) nx++;
+        fp = new File(XmlFile.xmlDir() + "programmers");
+        int nx;
+        String[] sx;
+        if (fp.exists()) {
+            sx = fp.list(filter);
+            nx = sx.length;
+            if (log.isDebugEnabled()) {
+                log.debug("Got " + nx + " programmers from " + fp.getPath());
+            }
+        } else {
+            // create an array of file names from jmri.jar!xml/programmers, count entries
+            List<String> sr = new ArrayList<String>();
+            Enumeration je = FileUtil.jmriJarFile().entries();
+            while (je.hasMoreElements()) {
+                String name = ((JarEntry) je.nextElement()).getName();
+                if (name.startsWith("xml" + File.separator + "programmers") && name.endsWith(".xml")) {
+                    sr.add(name.substring(name.lastIndexOf(File.separator)));
+                }
+            }
+            sx = sr.toArray(new String[sr.size()]);
+            nx = sx.length;
+            if (log.isDebugEnabled()) {
+                log.debug("Got " + nx + " programmers from jmri.jar");
+            }
         }
         // copy the programmer entries to the final array
         // note: this results in duplicate entries if the same name is also local.
         // But for now I can live with that.
-        String sbox[] = new String[np+nx];
-        int n=0;
-        if (sp != null && np> 0)
-            for (i=0; i<sp.length; i++) {
-                if (sp[i].endsWith(".xml")) sbox[n++] = sp[i].substring(0, sp[i].length()-1-3);
-            }
-        for (i=0; i<sx.length; i++) {
-            if (sx[i].endsWith(".xml")) sbox[n++] = sx[i].substring(0, sx[i].length()-1-3);
+        String sbox[] = new String[np + nx];
+        int n = 0;
+        for (String s : sp) {
+            sbox[n++] = s.substring(0, s.length() - 4);
+        }
+        for (String s : sx) {
+            sbox[n++] = s.substring(0, s.length() - 4);
         }
         return sbox;
     }
-	
- 	volatile static String defaultProgFile;
+    volatile static String defaultProgFile;
 
     synchronized static public String getDefaultProgFile() {
-    	if (log.isDebugEnabled()) log.debug("get programmer: "+defaultProgFile);
-    	return defaultProgFile;
-    }
-    
-     synchronized static public void setDefaultProgFile(String s) { 
-    	if (log.isDebugEnabled()) log.debug("set programmer: "+s);
-    	defaultProgFile = s;
+        if (log.isDebugEnabled()) {
+            log.debug("get programmer: " + defaultProgFile);
+        }
+        return defaultProgFile;
     }
 
-	static {
-		defaultProgFile = null;
-	}
-	
-    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(ProgDefault.class.getName());
+    synchronized static public void setDefaultProgFile(String s) {
+        if (log.isDebugEnabled()) {
+            log.debug("set programmer: " + s);
+        }
+        defaultProgFile = s;
+    }
 
+    static {
+        defaultProgFile = null;
+    }
+    static Logger log = Logger.getLogger(ProgDefault.class);
 }
