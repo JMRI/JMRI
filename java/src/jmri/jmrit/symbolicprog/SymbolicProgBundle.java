1893b1f63adc5537890e1680057af4f0a8249846
Randall Wood <randall.h.wood@alexandriasoftware.com>
2013-01-31 11:28:27 +0000

    Support use in new I18N model.

--- java/src/jmri/jmrit/symbolicprog/SymbolicProgBundle.java
+++ java/src/jmri/jmrit/symbolicprog/SymbolicProgBundle.java
@@ -1,5 +1,4 @@
 // SymbolicProgBundle.java
-
 package jmri.jmrit.symbolicprog;
 
 import java.util.ResourceBundle;
@@ -7,19 +6,36 @@ import java.util.ResourceBundle;
 /**
  * Common access to the SymbolicProgBundle of properties.
  *
- * Putting this in a class allows it to be loaded only
- * once.
+ * Putting this in a class allows it to be loaded only once.
  *
- * @author Bob Jacobsen  Copyright 2010
+ * @author Bob Jacobsen Copyright 2010
  * @since 2.9.4
  * @version $Revision$
  */
-
 public class SymbolicProgBundle {
 
-    static public final ResourceBundle b
-            = java.util.ResourceBundle.getBundle("jmri.jmrit.symbolicprog.SymbolicProgBundle");
+    @Deprecated
+    static public final ResourceBundle b = java.util.ResourceBundle.getBundle("jmri.jmrit.symbolicprog.SymbolicProgBundle");
 
-    static public ResourceBundle bundle() { return b; }
-    
+    /**
+     * Get a reference to the Symbolic Programmer resource bundle.
+     *
+     * It is preferable to use the {@link #getMessage(java.lang.String) } or {@link #getMessage(java.lang.String, java.lang.Object[])
+     * } methods instead.
+     *
+     * @return
+     * @deprecated
+     */
+    @Deprecated
+    static public ResourceBundle bundle() {
+        return b;
+    }
+
+    static public String getMessage(String key) {
+        return Bundle.getMessage(key);
+    }
+
+    static public String getMessage(String key, Object... subs) {
+        return Bundle.getMessage(key, subs);
+    }
 }
