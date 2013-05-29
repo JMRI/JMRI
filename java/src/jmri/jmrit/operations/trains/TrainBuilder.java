078e91bcfa0ff864011757d50222caeb50b894d9
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2013-05-29 02:56:29 +0000

    Don't count moves requested in a route if there isn't any work

--- java/src/jmri/jmrit/operations/trains/TrainBuilder.java
+++ java/src/jmri/jmrit/operations/trains/TrainBuilder.java
@@ -206,24 +206,26 @@ public class TrainBuilder extends TrainCommon {
 				rl.setCarMoves(rl.getMaxCarMoves()); // don't allow car moves for this location
 			} else {
 				// we're going to use this location, so initialize the location
-				rl.setCarMoves(0); // clear the number of moves
-				requested = requested + rl.getMaxCarMoves(); // add up the total number of car moves requested
+				rl.setCarMoves(0); // clear the number of moves				
 				// show the type of moves allowed at this location
-				if (!rl.canDrop() && !rl.canPickup())
+				if (!rl.canDrop() && !rl.canPickup()) {
 					addLine(buildReport, THREE, MessageFormat.format(Bundle
 							.getMessage("buildLocNoDropsOrPickups"), new Object[] { rl.getName() }));
-				else if (rl.canDrop() && rl.canPickup())
-					addLine(buildReport, THREE, MessageFormat.format(Bundle
-							.getMessage("buildLocRequestMoves"), new Object[] { rl.getName(),
-							rl.getMaxCarMoves() }));
-				else if (!rl.canDrop())
-					addLine(buildReport, THREE, MessageFormat.format(Bundle
-							.getMessage("buildLocRequestPickups"), new Object[] { rl.getName(),
-							rl.getMaxCarMoves() }));
-				else
-					addLine(buildReport, THREE, MessageFormat.format(Bundle
-							.getMessage("buildLocRequestDrops"), new Object[] { rl.getName(),
-							rl.getMaxCarMoves() }));
+				} else {
+					requested = requested + rl.getMaxCarMoves(); // add up the total number of car moves requested
+					if (rl.canDrop() && rl.canPickup())
+						addLine(buildReport, THREE, MessageFormat.format(Bundle
+								.getMessage("buildLocRequestMoves"), new Object[] { rl.getName(),
+								rl.getMaxCarMoves() }));
+					else if (!rl.canDrop())
+						addLine(buildReport, THREE, MessageFormat.format(Bundle
+								.getMessage("buildLocRequestPickups"), new Object[] { rl.getName(),
+								rl.getMaxCarMoves() }));
+					else
+						addLine(buildReport, THREE, MessageFormat.format(Bundle
+								.getMessage("buildLocRequestDrops"), new Object[] { rl.getName(),
+								rl.getMaxCarMoves() }));
+				}
 			}
 			rl.setTrainWeight(0); // clear the total train weight
 			rl.setTrainLength(0); // and length
