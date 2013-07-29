d976b8342ab29f386d62ebcb50a8f6cbd66b2b3c
Bob Jacobsen <jacobsen@mac.com>
2013-07-29 02:22:34 +0000

    add tests for initial X instead of x on full hex OpenLCB address

--- java/test/jmri/jmrix/openlcb/OlcbTurnoutTest.java
+++ java/test/jmri/jmrix/openlcb/OlcbTurnoutTest.java
@@ -66,6 +66,68 @@ public class OlcbTurnoutTest extends TestCase {
         Assert.assertTrue(s.getCommandedState()==Turnout.CLOSED);
         Assert.assertTrue(new OlcbAddress("1.2.3.4.5.6.7.9").match(t.rcvMessage));
     }
+
+    public void testNameFormatXlower() {
+        // load dummy TrafficController
+        TestTrafficController t = new TestTrafficController();
+        Assert.assertNotNull("tc exists", t );
+        OlcbTurnout s = new OlcbTurnout("MT", "x0501010114FF2000;x0501010114FF2001", t);
+        Assert.assertNotNull("to exists", s );
+        
+        // message for Active and Inactive
+        CanMessage mActive = new CanMessage(
+                    new int[]{0x05,0x01,0x01,0x01,0x14,0xFF,0x20,0x00},
+                    0x195B4000
+        );
+        mActive.setExtended(true);
+        
+        CanMessage mInactive = new CanMessage(
+                    new int[]{0x05,0x01,0x01,0x01,0x14,0xFF,0x20,0x01},
+                    0x195B4000
+        );
+        mInactive.setExtended(true);
+
+        // check states
+        Assert.assertTrue(s.getCommandedState()==Turnout.UNKNOWN);
+        
+        s.message(mActive);
+        Assert.assertTrue(s.getCommandedState()==Turnout.THROWN);
+        
+        s.message(mInactive);
+        Assert.assertTrue(s.getCommandedState()==Turnout.CLOSED);
+        
+    }
+    
+    public void testNameFormatXupper() {
+        // load dummy TrafficController
+        TestTrafficController t = new TestTrafficController();
+        Assert.assertNotNull("tc exists", t );
+        OlcbTurnout s = new OlcbTurnout("MT", "X0501010114FF2000;X0501010114FF2001", t);
+        Assert.assertNotNull("to exists", s );
+        
+        // message for Active and Inactive
+        CanMessage mActive = new CanMessage(
+                    new int[]{0x05,0x01,0x01,0x01,0x14,0xFF,0x20,0x00},
+                    0x195B4000
+        );
+        mActive.setExtended(true);
+        
+        CanMessage mInactive = new CanMessage(
+                    new int[]{0x05,0x01,0x01,0x01,0x14,0xFF,0x20,0x01},
+                    0x195B4000
+        );
+        mInactive.setExtended(true);
+
+        // check states
+        Assert.assertTrue(s.getCommandedState()==Turnout.UNKNOWN);
+        
+        s.message(mActive);
+        Assert.assertTrue(s.getCommandedState()==Turnout.THROWN);
+        
+        s.message(mInactive);
+        Assert.assertTrue(s.getCommandedState()==Turnout.CLOSED);
+        
+    }
     
     // from here down is testing infrastructure
 
