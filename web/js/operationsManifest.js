c50ade6667a7d399f6956906484458b9d1264533
Steve Todd <mstevetodd@mstevetodd.com>
2013-08-13 20:33:59 +0000

    operationsManifest.js now displays in-transit trains properly

--- web/js/operationsManifest.js
+++ web/js/operationsManifest.js
@@ -10,9 +10,9 @@ var $getTrainData = function($trainId){
 };
 
 //process the json data defining a train into an online manifest
-//  output some header info, then loop through locations.  for each, 
-//  output loco pickups, then car actions, then loco setouts
-//  all elements have css classes to facilitate later formatting and presentation options
+//output some header info, then loop through locations.  for each, 
+//output loco pickups, then car actions, then loco setouts
+//all elements have css classes to facilitate later formatting and presentation options
 var $buildManifest = function($r, $s, $x){
 	var $h = "";
 	$train = $r.data;  //everything of interest is in the data element
@@ -26,9 +26,12 @@ var $buildManifest = function($r, $s, $x){
 		$pickups = 0;
 		$setouts = 0;
 		$moves = 0;
-		$hl = "";  //use for car list, so parent can indicate count
+		$aboard = 0;
+		$ht = "";  //build list of cars on train, display later
+		$hl = "";  //build list of work to do at current location, display later
+		$locationClass = "";  //set additional class for location based on location of train
 		$.each($train.engines, function(j, $e) {  //loop thru enginelist to find any engine pickups
-			if ($rl.id == $e.location) {
+			if ($rl.id == $e.location  && $e.locationTrack !== "") {
 				$hl += "  <li class='car engine pickup'><span class='action'>Pick up</span> " +
 				"<span class='roadNumber'>" + $e.road + " " + $e.number + "</span> " +
 				"<span class='model hideable'>" + $e.model + "</span> from " +
@@ -36,28 +39,37 @@ var $buildManifest = function($r, $s, $x){
 				$pickups++;
 			}
 		});
-		$.each($train.cars, function(j, $c) {  //loop thru carlist to find work for this location
-			if ($rl.id == $c.location && $rl.id == $c.destination) { //local move
-				$hl += "  <li class='car move'><span class='action'>Move</span> "+
+		$.each($train.cars, function(j, $c) {  //loop thru carlist to determine where to show each car (on train, to be picked up, etc.)
+			if ($c.locationTrack == "") {   //this car is on the train
+				$ht += "  <li class='car aboard'>"+
 				"<span class='roadNumber'>"+$c.road+" "+$c.number+"</span> "+
 				"<span class='description hideable'>"+$c.type+" "+$c.length+"' "+$c.color+" "+"</span> "+
-				"<span class='load hideable'>"+escapeHtml($c.load)+"</span> from "+
-				"<span class='trackName'>"+$c.locationTrack + "</span> to <span class='trackName'>" + $c.destinationTrack + "</span></li>";
-				$moves++;
-			} else if ($rl.id == $c.location) { //pickup
-				$hl += "  <li class='car pickup'><span class='action'>Pick up</span> "+
-				"<span class='roadNumber'>"+$c.road+" "+$c.number+"</span> "+
-				"<span class='description hideable'>"+$c.type+" "+$c.length+"' "+$c.color+" "+"</span> "+
-				"<span class='load hideable'>"+escapeHtml($c.load)+"</span> from "+
-				"<span class='trackName'>"+$c.locationTrack+ "</span></li>";
-				$pickups++;
-			} else if ($rl.id == $c.destination) { //setout
-				$hl += "  <li class='car setout'><span class='action'>Set out</span> "+
-				"<span class='roadNumber'>"+$c.road+" "+$c.number+"</span> "+
-				"<span class='description hideable'>"+$c.type+" "+$c.length+"' "+$c.color+" "+"</span> "+
-				"<span class='load hideable'>"+escapeHtml($c.load)+"</span> to "+
-				"<span class='trackName'>"+$c.destinationTrack+ "</span></li>";
-				$setouts++;
+				"<span class='load hideable'>"+escapeHtml($c.load)+"</span></li>";
+				$aboard++;
+				if ($rl.id == $c.destination) { //setout
+					$hl += "  <li class='car setout'><span class='action'>Set out</span> "+
+					"<span class='roadNumber'>"+$c.road+" "+$c.number+"</span> "+
+					"<span class='description hideable'>"+$c.type+" "+$c.length+"' "+$c.color+" "+"</span> "+
+					"<span class='load hideable'>"+escapeHtml($c.load)+"</span> to "+
+					"<span class='trackName'>"+$c.destinationTrack+ "</span></li>";
+					$setouts++;
+				}
+			} else {  //this car is not on the train
+				if ($rl.id == $c.location && $rl.id == $c.destination) { //local move
+					$hl += "  <li class='car move'><span class='action'>Move</span> "+
+					"<span class='roadNumber'>"+$c.road+" "+$c.number+"</span> "+
+					"<span class='description hideable'>"+$c.type+" "+$c.length+"' "+$c.color+" "+"</span> "+
+					"<span class='load hideable'>"+escapeHtml($c.load)+"</span> from "+
+					"<span class='trackName'>"+$c.locationTrack + "</span> to <span class='trackName'>" + $c.destinationTrack + "</span></li>";
+					$moves++;
+				} else if ($rl.id == $c.location && $c.locationTrack !== "") { //pickup
+					$hl += "  <li class='car pickup'><span class='action'>Pick up</span> "+
+					"<span class='roadNumber'>"+$c.road+" "+$c.number+"</span> "+
+					"<span class='description hideable'>"+$c.type+" "+$c.length+"' "+$c.color+" "+"</span> "+
+					"<span class='load hideable'>"+escapeHtml($c.load)+"</span> from "+
+					"<span class='trackName'>"+$c.locationTrack+ "</span></li>";
+					$pickups++;
+				}
 			}
 		});
 		$.each($train.engines, function(j, $e) {  //loop thru enginelist to find any engine setouts
@@ -69,43 +81,61 @@ var $buildManifest = function($r, $s, $x){
 				$setouts++;
 			}
 		});
-		$h += "  <li class='location'>" + $rl.name + " (" + $rl.expectedArrivalTime + ") "; 
+		$msg = "";
+		if ($rl.expectedArrivalTime == -1 && $train.location !== $rl.name) { //location has been passed
+			$msg += " - complete ";
+			$locationClass = "complete";
+		} else  { 
+			if ($train.location == $rl.name) {
+				$msg += " (current) "; //show that train is at this location
+				$locationClass = "current";
+			} 
+			if ($pickups==0 && $moves==0 && $setouts==0) {
+				$msg += "No work ";
+				$locationClass = "nowork";
+			} else {
+				if ($pickups>0) $msg += $pickups + " pickups ";
+				if ($setouts>0)	$msg += $setouts + " setouts ";
+				if ($moves>0) 	$msg += $moves + " local moves ";
+				$msg += "    <ul class='cars'>" + $hl + "</ul>";
+			}
 
-		if ($pickups==0 && $moves==0 && $setouts==0) {
-			$h += "No work ";
-		} else {
-			if ($pickups>0) $h += $pickups + " pickups ";
-			if ($setouts>0)	$h += $setouts + " setouts ";
-			if ($moves>0) 	$h += $moves + " local moves ";
-			$h += "    <ul class='cars'>" + $hl + "</ul>";
 		}
+		$h += "  <li class='location " + $locationClass + " '>" + $rl.name;
+		if ($rl.expectedArrivalTime != -1) {
+			$h += " (" + $rl.expectedArrivalTime + ") ";
+		}
+		$h += $msg;
 		$h += "  </li>";
 	});
 
+	if ($ht !== "") {
+		$h += "<li class='aboard'> Train contents<ul>" + $ht + "</ul></li>";
+	}
 	$h += "</ul>";
 	$('div#displayArea').html($h); //put output on page
 
-	//insert checkbox image in all li's, hidden until row is clicked
+	//insert checkbox image in all actionable li's, hidden until row is clicked
 	$('li.car>span.action').before("<img class='actionComplete' src='/images/VerySmallCheck.png'>");
 	//set up click handler on car items to toggle actionCompleted class
-	$('li.car').click(function () {
+	$('ul.cars>li.car').click(function () {
 		$(this).toggleClass("actionCompleted");
 	});
-	
+
 	//add simple collapsible for locations, 
 	//   copied from https://codeblitz.wordpress.com/2009/04/15/jquery-animated-collapsible-list/
 	$(function(){
 		$('li:has(ul)')
-			.click(function(event){
-				if (this == event.target) {
-					$(this).css('list-style-image',
+		.click(function(event){
+			if (this == event.target) {
+				$(this).css('list-style-image',
 						(!$(this).children().is(':hidden')) ? 'url(/web/images/plusbox.gif)' : 'url(/web/images/minusbox.gif)');
-					$(this).children().toggle('slow');
-				}
-				return false;
-			})
-			.css({cursor:'pointer', 'list-style-image':'url(/web/images/plusbox.gif)'})
-			.children().hide();
+				$(this).children().toggle('slow');
+			}
+			return false;
+		})
+		.css({cursor:'pointer', 'list-style-image':'url(/web/images/plusbox.gif)'})
+		.children().hide();
 		$('li:not(:has(ul))').css({'list-style-image':'none'});
 	});
 
@@ -116,8 +146,8 @@ var $getRailRoad = function(e){
 	$.ajax({
 		url:  '/json/railroad', //request proper url for railroad name
 		success: function($r, $s, $x){
-            railroad = $r.data.name;  //name is returned in data object
-            e.html(railroad);
+			railroad = $r.data.name;  //name is returned in data object
+			e.html(railroad);
 		},
 		dataType: 'json' //<--dataType
 	});
