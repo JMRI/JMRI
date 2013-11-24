27d0bc6fb68c387f78123386a57b0c6367410416
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2013-11-24 16:29:25 +0000

    Don't move car if routing failed with new train option

--- java/src/jmri/jmrit/operations/trains/TrainBuilder.java
+++ java/src/jmri/jmrit/operations/trains/TrainBuilder.java
@@ -1825,7 +1825,7 @@ public class TrainBuilder extends TrainCommon {
 					// don't move car if routing issue was track space but not departing staging
 //					if ((!Router.instance().getStatus().startsWith(Track.LENGTH) && !Router.instance()
 //							.getStatus().startsWith(Track.CAPACITY))
-					if ((!Router.instance().getStatus().startsWith(Track.LENGTH))
+					if ((!Router.instance().getStatus().startsWith(Track.LENGTH) && !train.isServiceAllCarsWithFinalDestinationsEnabled())
 							|| (car.getLocationName().equals(departLocation.getName()) && departStageTrack != null))
 						// move this car, routing failed!
 						findDestinationAndTrack(car, rl, routeIndex, routeList.size());
