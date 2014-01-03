db5a8586d48eefcd7f4b236f4f772b5f38650c61
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-01-03 11:57:17 +0000

    Attempt to make node identity generatation more reliable on systems that return the loopback interface as the localhost (SO suggests this an issue primarilly on Ubuntu). Also remove some unused code.

--- java/src/jmri/util/node/NodeIdentity.java
+++ java/src/jmri/util/node/NodeIdentity.java
@@ -1,7 +1,6 @@
 package jmri.util.node;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.InetAddress;
@@ -12,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import jmri.util.FileUtil;
+import jmri.web.server.WebServerManager;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -22,19 +22,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Provide a unique network identity for JMRI. If a stored
- * identity does not exist, the identity is created by taking the MAC address of
- * the first {@link java.net.InetAddress} and prepending it with "jmri-". and
- * removing all :s from the address.
+ * Provide a unique network identity for JMRI. If a stored identity does not
+ * exist, the identity is created by taking the MAC address of the first
+ * {@link java.net.InetAddress} and prepending it with "jmri-". and removing all
+ * colons from the address.
  *
  * If a stored identity is found, it is discarded if none of the InetAddresses
  * for the host match the MAC address in the identity and regenerated.
  *
  * If the MAC address of the localhost cannot be read, fall back on using the
- * hostname or IP address. If no local IP address is available provide an empty
- * string.
+ * hostname or IP address. If no local IP address is available, fall back on
+ * using the railroad name.
  *
- * @author Randall Wood (C) 2013
+ * @author Randall Wood (C) 2013, 2014
  */
 public class NodeIdentity {
 
@@ -105,31 +105,39 @@ public class NodeIdentity {
     }
 
     synchronized private void getIdentity(boolean save) {
-        FileOutputStream os = null;
         try {
-            this.identity = this.createIdentity(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress());
+            try {
+                this.identity = this.createIdentity(NetworkInterface.getByInetAddress(InetAddress.getLocalHost()).getHardwareAddress());
+                if (this.identity == null) {
+                    Enumeration nics = NetworkInterface.getNetworkInterfaces();
+                    while (nics.hasMoreElements()) {
+                        NetworkInterface nic = (NetworkInterface) nics.nextElement();
+                        if (!nic.isLoopback() && !nic.isVirtual()) {
+                            this.identity = this.createIdentity(nic.getHardwareAddress());
+                            if (this.identity != null) { // NOI18N
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (this.identity == null) {
+                    try {
+                        this.identity = InetAddress.getLocalHost().getHostName();
+                    } catch (UnknownHostException ex1) {
+                        this.identity = InetAddress.getLocalHost().getHostAddress();
+                    }
+                }
+            } catch (SocketException ex) {
+                try {
+                    this.identity = InetAddress.getLocalHost().getHostName();
+                } catch (UnknownHostException ex1) {
+                    this.identity = InetAddress.getLocalHost().getHostAddress();
+                }
+            }
         } catch (UnknownHostException ex) {
-            try {
-                this.identity = InetAddress.getLocalHost().getHostName();
-            } catch (UnknownHostException ex1) {
-                try {
-                    this.identity = InetAddress.getLocalHost().getHostAddress();
-                } catch (UnknownHostException ex2) {
-                    this.identity = "";
-                    log.error("Cannot get host address or name {}", ex2.getLocalizedMessage());
-                }
-            }
-        } catch (SocketException ex) {
-            try {
-                this.identity = InetAddress.getLocalHost().getHostName();
-            } catch (UnknownHostException ex1) {
-                try {
-                    this.identity = InetAddress.getLocalHost().getHostAddress();
-                } catch (UnknownHostException ex2) {
-                    this.identity = "";
-                    log.error("Cannot get host address or name {}", ex2.getLocalizedMessage());
-                }
-            }
+            this.identity = WebServerManager.getWebServerPreferences().getRailRoadName().replaceAll("[^A-Za-z0-9 ]", "-"); // NOI18N
+            log.error("Cannot get host address or name {}", ex.getLocalizedMessage());
+            log.error("Using {} as a fallback.", this.identity);
         }
         if (save) {
             this.saveIdentity();
@@ -178,13 +186,9 @@ public class NodeIdentity {
                 sb.append(String.format("%02X", mac[i])); // NOI18N
             }
         } catch (NullPointerException ex) {
-            // not all interfaces have MAC addresses, but we don't care
+            return null;
         }
         return sb.toString();
     }
 
-    private void retainFormerIdentitytt(String identity) {
-        this.formerIdentities.add(identity);
-        this.saveIdentity();
-    }
 }
