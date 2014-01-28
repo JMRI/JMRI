5690f05af225d099b23ebebb393dbc99c3c02ae4
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-01-28 03:40:46 +0000

    Add the open panels to the Panels menu item in the Operations servlet.

--- web/js/main.js
+++ web/js/main.js
@@ -2,10 +2,28 @@
  * Common JavaScript functionality for BootStrap-based servlets.
  */
 
+var $getPanels = function() {
+    $.ajax({
+        url: "/json/panels",
+        data: {},
+        success: function(data) {
+            $(".navbar-panel-item").remove();
+            if (data.length !== 0) {
+                $("#empty-panel-list").addClass("hidden").removeClass("show");
+                $.each(data.reverse(), function(index, value) {
+                    $("#navbar-panels").prepend("<li class=\"navbar-panel-item\"><a href=\"/panel?name=" + value.name + "\">" + value.userName + "</a></li>");
+                });
+            } else {
+                $("#empty-panel-list").addClass("show").removeClass("hidden");
+            }
+        }
+    });
+};
+
 //-----------------------------------------javascript processing starts here (main) ---------------------------------------------
 $(document).ready(function() {
-    // TODO: list panels
-    
+    $getPanels();
+
     // temporary - should be handled by Servlet
     $(".context-panel-only").addClass("hidden");
 });
