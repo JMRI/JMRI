66158cce8fec499cb9d4e0509210c9eb0076ec8d
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-03-16 14:29:30 +0000

    Allow the class to be loaded without forcing headless mode. Also some refactoring to possibly better show how its customizable.

--- java/src/apps/JmriFaceless.java
+++ java/src/apps/JmriFaceless.java
@@ -1,5 +1,4 @@
 // JmriFaceless.java
-
 package apps;
 
 import org.slf4j.Logger;
@@ -7,50 +6,50 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Application for running JMRI server functions without a graphical interface.
- *   Goal is to run on very light hardware, such as a Raspberry Pi, without requiring the 
- *   overhead associated with display functions.  Needs an existing JMRI configuration file
- *   passed as parm to program, or it will use the default of JmriFacelessConfig3.xml 
- * Copied from apps.gui3.demo3.Demo3, then removed all ui-related elements
- * NOTE: JMRI "server" functions based on ui components (such as JmriJFrames)
- *   may need to be modified to check isHeadless() and adjust their 
- *   behavior as needed. 
+ * Goal is to run on very light hardware, such as a Raspberry Pi, without
+ * requiring the overhead associated with display functions. Needs an existing
+ * JMRI configuration file passed as parm to program, or it will use the default
+ * of JmriFacelessConfig3.xml Copied from apps.gui3.demo3.Demo3, then removed
+ * all ui-related elements NOTE: JMRI "server" functions based on ui components
+ * (such as JmriJFrames) may need to be modified to check isHeadless() and
+ * adjust their behavior as needed.
  * <P>
  *
  * <hr>
  * This file is part of JMRI.
  * <P>
- * JMRI is free software; you can redistribute it and/or modify it under 
- * the terms of version 2 of the GNU General Public License as published 
- * by the Free Software Foundation. See the "COPYING" file for a copy
- * of this license.
+ * JMRI is free software; you can redistribute it and/or modify it under the
+ * terms of version 2 of the GNU General Public License as published by the Free
+ * Software Foundation. See the "COPYING" file for a copy of this license.
  * <P>
- * JMRI is distributed in the hope that it will be useful, but WITHOUT 
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License 
- * for more details.
+ * JMRI is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * @author	Steve Todd   Copyright 2012
- * @version     $Revision: 17977 $
+ * @author	Steve Todd Copyright 2012
+ * @version $Revision: 17977 $
  */
 public class JmriFaceless extends apps.AppsBase {
 
-	// insure that the headless property is true for downstream use
-	static {           
-		System.setProperty("java.awt.headless", "true");
-	}
+    private static final Logger log = LoggerFactory.getLogger(JmriFaceless.class);
 
     public JmriFaceless(String[] args) {
         super("JmriFaceless", "JmriFacelessConfig3.xml", args);
-        this.start();
-      
+    }
+
+    @Override
+    public void start() {
+        // Once the configServlet is usable, require the web server
+        // WebServerManager.getWebServer().start();
         //start LocoNetOverTcp server if configured to autostart
-        jmri.jmrix.loconet.loconetovertcp.Server.getInstance();  
+        jmri.jmrix.loconet.loconetovertcp.Server.getInstance();
     }
 
-	// Main entry point
+    // Main entry point
     public static void main(String args[]) {
-        new JmriFaceless(args);
+        System.setProperty("java.awt.headless", "true");
+        JmriFaceless app = new JmriFaceless(args);
+        app.start();
     }
 
-    static Logger log = LoggerFactory.getLogger(JmriFaceless.class.getName());
 }
