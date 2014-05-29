9420b681c83251cae6e9ea1a62b14bdb84ddbda1
Kevin Dickerson <kev.public@ntlworld.com>
2014-05-29 11:07:59 +0000

    Update so that setting the function set directly sends the command off to the Ecos, rather than sending out the function groups which the system doesn't support.

--- java/src/jmri/jmrix/ecos/EcosDccThrottle.java
+++ java/src/jmri/jmrix/ecos/EcosDccThrottle.java
@@ -135,158 +135,352 @@ public class EcosDccThrottle extends AbstractThrottle implements EcosListener
         return ( (lSpeed)/126.f);
     }
     
-    /**
-     * Send the message to set the state of functions F0, F1, F2, F3, F4.
-     */
     @Override
-    protected void sendFunctionGroup1() {
-        if(!_haveControl) return;
-        
-        String message = "set("+this.objectNumber+", func[0, "+(getF0()? 1 : 0 )+"])";
-        EcosMessage m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[1, "+(getF1()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[2, "+(getF2()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[3, "+(getF3()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[4, "+(getF4()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-    }
-
-    /**
-     * Send the message to set the state of
-     * functions F5, F6, F7, F8.
-     */
-    @Override
-    protected void sendFunctionGroup2() {
-        if(!_haveControl) return;
-
-        String message = "set("+this.objectNumber+", func[5, "+(getF5()? 1 : 0 )+"])";
-        EcosMessage m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[6, "+(getF6()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[7, "+(getF7()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[8, "+(getF8()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-    }
-
-    /**
-     * Send the message to set the state of
-     * functions F9, F10, F11, F12.
-     */
-    @Override
-    protected void sendFunctionGroup3() {
-        if(!_haveControl) return;
-        
-        String message = "set("+this.objectNumber+", func[9, "+(getF9()? 1 : 0 )+"])";
-        EcosMessage m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[10, "+(getF10()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[11, "+(getF11()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[12, "+(getF12()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);    
+    public void setF0(boolean f0) {
+    	boolean old = this.f0;
+        this.f0 = f0;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[0, "+(getF0()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f0)
+        	notifyPropertyChangeListener(Throttle.F0, old, this.f0 );
     }
     
     @Override
-    protected void sendFunctionGroup4() {
-        if(!_haveControl) return;
-
-        String message = "set("+this.objectNumber+", func[13, "+(getF13()? 1 : 0 )+"])";
-        EcosMessage m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[14, "+(getF14()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[15, "+(getF15()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[16, "+(getF16()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[17, "+(getF17()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[18, "+(getF18()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-        message = "set("+this.objectNumber+", func[19, "+(getF19()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[20, "+(getF20()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
+    public void setF1(boolean f1) {
+    	boolean old = this.f1;
+        this.f1 = f1;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[1, "+(getF1()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f1)
+        	notifyPropertyChangeListener(Throttle.F1, old, this.f1 );
     }
     
     @Override
-    protected void sendFunctionGroup5() {
-        if(!_haveControl) return;
-
-        String message = "set("+this.objectNumber+", func[21, "+(getF21()? 1 : 0 )+"])";
-        EcosMessage m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[22, "+(getF22()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[23, "+(getF23()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[24, "+(getF24()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-        
-
-        message = "set("+this.objectNumber+", func[25, "+(getF25()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[26, "+(getF26()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[27, "+(getF27()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
-
-        message = "set("+this.objectNumber+", func[28, "+(getF28()? 1 : 0 )+"])";
-        m = new EcosMessage(message);
-        tc.sendEcosMessage(m, this);
+    public void setF2(boolean f2) {
+    	boolean old = this.f2;
+        this.f2 = f2;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[2, "+(getF2()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f2)
+        	notifyPropertyChangeListener(Throttle.F2, old, this.f2 );
+    }
     
+    @Override
+    public void setF3(boolean f3) {
+    	boolean old = this.f3;
+        this.f3 = f3;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[3, "+(getF3()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f3)
+        	notifyPropertyChangeListener(Throttle.F3, old, this.f3 );
+    }
+    
+    @Override
+    public void setF4(boolean f4) {
+    	boolean old = this.f4;
+        this.f4 = f4;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[4, "+(getF4()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f4)
+        	notifyPropertyChangeListener(Throttle.F4, old, this.f4 );
+    }
+    
+    @Override
+    public void setF5(boolean f5) {
+    	boolean old = this.f5;
+        this.f5 = f5;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[5, "+(getF5()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f5)
+        	notifyPropertyChangeListener(Throttle.F5, old, this.f5 );
+    }
+    
+    @Override
+    public void setF6(boolean f6) {
+    	boolean old = this.f6;
+        this.f6 = f6;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[6, "+(getF6()? 6 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f6)
+        	notifyPropertyChangeListener(Throttle.F6, old, this.f6 );
+    }
+    
+    @Override
+    public void setF7(boolean f7) {
+    	boolean old = this.f7;
+        this.f7 = f7;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[7, "+(getF7()? 6 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f7)
+        	notifyPropertyChangeListener(Throttle.F7, old, this.f7 );
+    }
+    
+    @Override
+    public void setF8(boolean f8) {
+    	boolean old = this.f8;
+        this.f8 = f8;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[8, "+(getF8()? 6 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f8)
+        	notifyPropertyChangeListener(Throttle.F8, old, this.f8 );
+    }
+    
+    @Override
+    public void setF9(boolean f9) {
+    	boolean old = this.f9;
+        this.f9 = f9;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[9, "+(getF9()? 6 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f9)
+        	notifyPropertyChangeListener(Throttle.F9, old, this.f9 );
+    }
+    
+    @Override
+    public void setF10(boolean f10) {
+    	boolean old = this.f10;
+        this.f10 = f10;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[10, "+(getF10()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f10)
+        	notifyPropertyChangeListener(Throttle.F10, old, this.f10 );
+    }
+    
+    @Override
+    public void setF11(boolean f11) {
+    	boolean old = this.f11;
+        this.f11 = f11;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[11, "+(getF11()? 11 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f11)
+        	notifyPropertyChangeListener(Throttle.F11, old, this.f11 );
+    }
+    
+    @Override
+    public void setF12(boolean f12) {
+    	boolean old = this.f12;
+        this.f12 = f12;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[12, "+(getF12()? 11 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f12)
+        	notifyPropertyChangeListener(Throttle.F12, old, this.f12 );
+    }
+    
+    @Override
+    public void setF13(boolean f13) {
+    	boolean old = this.f13;
+        this.f13 = f13;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[13, "+(getF13()? 11 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f13)
+        	notifyPropertyChangeListener(Throttle.F13, old, this.f13 );
+    }
+    
+    @Override
+    public void setF14(boolean f14) {
+    	boolean old = this.f14;
+        this.f14 = f14;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[14, "+(getF14()? 11 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f14)
+        	notifyPropertyChangeListener(Throttle.F14, old, this.f14 );
+    }
+    
+    @Override
+    public void setF15(boolean f15) {
+    	boolean old = this.f15;
+        this.f15 = f15;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[15, "+(getF15()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f15)
+        	notifyPropertyChangeListener(Throttle.F15, old, this.f15 );
+    }
+    
+    @Override
+    public void setF16(boolean f16) {
+    	boolean old = this.f16;
+        this.f16 = f16;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[16, "+(getF16()? 16 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f16)
+        	notifyPropertyChangeListener(Throttle.F16, old, this.f16 );
+    }
+    
+    @Override
+    public void setF17(boolean f17) {
+    	boolean old = this.f17;
+        this.f17 = f17;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[17, "+(getF17()? 16 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f17)
+        	notifyPropertyChangeListener(Throttle.F17, old, this.f17 );
+    }
+    
+    @Override
+    public void setF18(boolean f18) {
+    	boolean old = this.f18;
+        this.f18 = f18;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[18, "+(getF18()? 16 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f18)
+        	notifyPropertyChangeListener(Throttle.F18, old, this.f18 );
+    }
+
+    @Override
+    public void setF19(boolean f19) {
+    	boolean old = this.f19;
+        this.f19 = f19;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[19, "+(getF19()? 16 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f19)
+        	notifyPropertyChangeListener(Throttle.F19, old, this.f19 );
+    }
+    
+    @Override
+    public void setF20(boolean f20) {
+    	boolean old = this.f20;
+        this.f20 = f20;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[20, "+(getF20()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f20)
+        	notifyPropertyChangeListener(Throttle.F20, old, this.f20 );
+    }
+
+    @Override
+    public void setF21(boolean f21) {
+    	boolean old = this.f21;
+        this.f21 = f21;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[21, "+(getF21()? 21 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f21)
+        	notifyPropertyChangeListener(Throttle.F21, old, this.f21 );
+    }
+
+    @Override
+    public void setF22(boolean f22) {
+    	boolean old = this.f22;
+        this.f22 = f22;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[22, "+(getF22()? 21 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f22)
+        	notifyPropertyChangeListener(Throttle.F22, old, this.f22 );
+    }
+
+    @Override
+    public void setF23(boolean f23) {
+    	boolean old = this.f23;
+        this.f23 = f23;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[23, "+(getF23()? 21 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f23)
+        	notifyPropertyChangeListener(Throttle.F23, old, this.f23 );
+    }
+
+    @Override
+    public void setF24(boolean f24) {
+    	boolean old = this.f24;
+        this.f24 = f24;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[24, "+(getF24()? 21 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f24)
+        	notifyPropertyChangeListener(Throttle.F24, old, this.f24 );
+    }
+    
+        @Override
+    public void setF25(boolean f25) {
+    	boolean old = this.f25;
+        this.f25 = f25;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[25, "+(getF25()? 1 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f25)
+        	notifyPropertyChangeListener(Throttle.F25, old, this.f25 );
+    }
+
+    @Override
+    public void setF26(boolean f26) {
+    	boolean old = this.f26;
+        this.f26 = f26;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[26, "+(getF26()? 26 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f26)
+        	notifyPropertyChangeListener(Throttle.F26, old, this.f26 );
+    }
+
+    @Override
+    public void setF27(boolean f27) {
+    	boolean old = this.f27;
+        this.f27 = f27;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[27, "+(getF27()? 26 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f27)
+        	notifyPropertyChangeListener(Throttle.F27, old, this.f27 );
+    }
+
+    @Override
+    public void setF28(boolean f28) {
+    	boolean old = this.f28;
+        this.f28 = f28;
+        if(_haveControl){
+            EcosMessage m = new EcosMessage("set("+this.objectNumber+", func[28, "+(getF28()? 26 : 0 )+"])");
+            tc.sendEcosMessage(m, this);
+        }
+        if (old != this.f28)
+        	notifyPropertyChangeListener(Throttle.F28, old, this.f28 );
     }
 
     /**
