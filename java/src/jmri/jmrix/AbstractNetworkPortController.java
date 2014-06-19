36220a40b6f5a366c479ec41491c6b3a53b4faab
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-06-19 09:38:45 +0000

    Formatting and @Override annotations.

--- java/src/jmri/jmrix/AbstractNetworkPortController.java
+++ java/src/jmri/jmrix/AbstractNetworkPortController.java
@@ -1,138 +1,162 @@
 // AbstractNetworkPortController.java
-
 package jmri.jmrix;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-
-import java.net.*;
+import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Enables basic setup of a network client interface
- * for a jmrix implementation.
+ * Enables basic setup of a network client interface for a jmrix implementation.
  *
  *
- * @author      Kevin Dickerson  Copyright (C) 2010
- * @author      Based upon work originally done by Paul Bender  Copyright (C) 2009
+ * @author Kevin Dickerson Copyright (C) 2010
+ * @author Based upon work originally done by Paul Bender Copyright (C) 2009
  * @version	$Revision$
- * @see         jmri.jmrix.NetworkConfigException
+ * @see jmri.jmrix.NetworkConfigException
  */
-abstract public class AbstractNetworkPortController extends AbstractPortController implements NetworkPortAdapter{
+abstract public class AbstractNetworkPortController extends AbstractPortController implements NetworkPortAdapter {
 
     // the host name and port number identify what we are 
     // talking to.
-    protected String m_HostName=null;
-    private String m_HostAddress=null;  // Internal IP address for  ZeroConf 
-                                        // configured clients.
-    protected int m_port=0;
+    protected String m_HostName = null;
+    private String m_HostAddress = null;  // Internal IP address for  ZeroConf
+    // configured clients.
+    protected int m_port = 0;
     // keep the socket provides our connection.
     protected Socket socketConn = null;
-    
+
+    @Override
     public void connect(String host, int port) throws Exception {
         setHostName(host);
         setPort(port);
         try {
             connect();
-        } catch (Exception e){
+        } catch (Exception e) {
             throw e;
         }
     }
-    
+
+    @Override
     public void connect() throws Exception {
-        opened=false;
-        if (getHostAddress()==null || m_port==0){
+        opened = false;
+        if (getHostAddress() == null || m_port == 0) {
             log.error("No host name or port set :" + m_HostName + ":" + m_port);
             return;
         }
         try {
-            socketConn = new Socket(getHostAddress(),  m_port);
+            socketConn = new Socket(getHostAddress(), m_port);
             socketConn.setKeepAlive(true);
             opened = true;
         } catch (Exception e) {
-            log.error("error opening network connection: "+e);
-            if(m_port!=0){
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName+":"+m_port, ConnectionStatus.CONNECTION_DOWN);
+            log.error("error opening network connection: " + e);
+            if (m_port != 0) {
+                ConnectionStatus.instance().setConnectionState(
+                        m_HostName + ":" + m_port, ConnectionStatus.CONNECTION_DOWN);
             } else {
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName, ConnectionStatus.CONNECTION_DOWN);
+                ConnectionStatus.instance().setConnectionState(
+                        m_HostName, ConnectionStatus.CONNECTION_DOWN);
             }
-            throw(e);
+            throw (e);
         }
-            if(opened && m_port!=0){
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName+":"+m_port, ConnectionStatus.CONNECTION_UP);
-            } else if(opened){
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName, ConnectionStatus.CONNECTION_UP);
-            }
+        if (opened && m_port != 0) {
+            ConnectionStatus.instance().setConnectionState(
+                    m_HostName + ":" + m_port, ConnectionStatus.CONNECTION_UP);
+        } else if (opened) {
+            ConnectionStatus.instance().setConnectionState(
+                    m_HostName, ConnectionStatus.CONNECTION_UP);
+        }
     }
-    
-	/** Query the status of this connection.  If all OK, at least
-	 * as far as is known, return true */
-    public boolean status() {return opened;}
+
+    /**
+     * Query the status of this connection. If all OK, at least as far as is
+     * known, return true
+     */
+    @Override
+    public boolean status() {
+        return opened;
+    }
 
     /**
      * Remember the associated host name
+     *
      * @param s
      */
-    public void setHostName(String s){
-        m_HostName=s;
-        if (s.equals("") && !getMdnsConfigure()) m_HostName = JmrixConfigPane.NONE;
+    @Override
+    public void setHostName(String s) {
+        m_HostName = s;
+        if (s.equals("") && !getMdnsConfigure()) {
+            m_HostName = JmrixConfigPane.NONE;
+        }
     }
 
-    public String getHostName(){
+    @Override
+    public String getHostName() {
         return m_HostName;
     }
 
     /**
-     * Remember the associated IP Address
-     * This is used internally for mDNS configuration.  Public
-     * access to the IP address is through the hostname field.
+     * Remember the associated IP Address This is used internally for mDNS
+     * configuration. Public access to the IP address is through the hostname
+     * field.
+     *
      * @param s
      */
-    protected void setHostAddress(String s){
-        m_HostAddress=s;
-        if (s.equals("")) m_HostAddress = m_HostName;
+    protected void setHostAddress(String s) {
+        m_HostAddress = s;
+        if (s.equals("")) {
+            m_HostAddress = m_HostName;
+        }
     }
 
-    protected String getHostAddress(){
-        if(m_HostAddress==null) return m_HostName;
+    protected String getHostAddress() {
+        if (m_HostAddress == null) {
+            return m_HostName;
+        }
         return m_HostAddress;
     }
 
-     /**
-      * Remeber the associated port number
-      * @param p
-      **/
-	public void setPort(int p){
-        m_port=p;
+    /**
+     * Remeber the associated port number
+     *
+     * @param p
+     *
+     */
+    @Override
+    public void setPort(int p) {
+        m_port = p;
     }
-    
-    public void setPort(String p){
-        m_port=Integer.parseInt(p);
+
+    @Override
+    public void setPort(String p) {
+        m_port = Integer.parseInt(p);
     }
-    
-	public int getPort(){
+
+    @Override
+    public int getPort() {
         return m_port;
     }
-    
+
     /**
-    * Returns the connection name for the network connection in the format of ip_address:port
-    * @return ip_address:port
-    **/
+     * Returns the connection name for the network connection in the format of
+     * ip_address:port
+     *
+     * @return ip_address:port
+     *
+     */
+    @Override
     public String getCurrentPortName() {
         String t = getHostName();
         int p = getPort();
         if (t != null && !t.equals("")) {
-            if (p!=0){
-                return t+":"+p;
+            if (p != 0) {
+                return t + ":" + p;
             }
             return t;
+        } else {
+            return JmrixConfigPane.NONE;
         }
-        else return JmrixConfigPane.NONE;
     }
 
     /*
@@ -141,7 +165,8 @@ abstract public class AbstractNetworkPortController extends AbstractPortControll
      * Note: Default implementation ignores the parameter.
      * @param autoconfig boolean value.
      */
-    public void setMdnsConfigure(boolean autoconfig){
+    @Override
+    public void setMdnsConfigure(boolean autoconfig) {
     }
 
     /*
@@ -150,116 +175,147 @@ abstract public class AbstractNetworkPortController extends AbstractPortControll
      * Default implemntation always returns false.
      * @return true if configured using MDNS.
      */
-    public boolean getMdnsConfigure() { return false; }
+    @Override
+    public boolean getMdnsConfigure() {
+        return false;
+    }
 
     /*
      * set the server's host name and port
      * using mdns autoconfiguration.
      * Default implementation does nothing.
      */
-    public void autoConfigure() { 
+    @Override
+    public void autoConfigure() {
     }
 
-   /*
-    * Get and set the ZeroConf/mDNS advertisement name.
-    * Default implementation does nothing.
-    */
-   public void setAdvertisementName(String AdName) {}
-   public String getAdvertisementName() { return null; }
+    /*
+     * Get and set the ZeroConf/mDNS advertisement name.
+     * Default implementation does nothing.
+     */
+    @Override
+    public void setAdvertisementName(String AdName) {
+    }
 
-   /* 
-    * Get and set the ZeroConf/mDNS service type.
-    * Default implementation does nothing.
-    */
-   public void setServiceType(String ServiceType) {}
-   public String getServiceType(){ return null; }
+    @Override
+    public String getAdvertisementName() {
+        return null;
+    }
 
-    
+    /*
+     * Get and set the ZeroConf/mDNS service type.
+     * Default implementation does nothing.
+     */
+    @Override
+    public void setServiceType(String ServiceType) {
+    }
+
+    @Override
+    public String getServiceType() {
+        return null;
+    }
+
+    @Override
     public DataInputStream getInputStream() {
         if (!opened) {
             log.error("getInputStream called before load(), stream not available");
-            if(m_port!=0){
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName+":"+m_port, ConnectionStatus.CONNECTION_DOWN);
+            if (m_port != 0) {
+                ConnectionStatus.instance().setConnectionState(
+                        m_HostName + ":" + m_port, ConnectionStatus.CONNECTION_DOWN);
             } else {
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName, ConnectionStatus.CONNECTION_DOWN);
+                ConnectionStatus.instance().setConnectionState(
+                        m_HostName, ConnectionStatus.CONNECTION_DOWN);
             }
         }
         try {
             return new DataInputStream(socketConn.getInputStream());
         } catch (java.io.IOException ex1) {
-            log.error("Exception getting input stream: "+ex1);
+            log.error("Exception getting input stream: " + ex1);
             return null;
         }
     }
-    
+
+    @Override
     public DataOutputStream getOutputStream() {
-        if (!opened) log.error("getOutputStream called before load(), stream not available");
+        if (!opened) {
+            log.error("getOutputStream called before load(), stream not available");
+        }
         try {
             return new DataOutputStream(socketConn.getOutputStream());
-        }
-     	catch (java.io.IOException e) {
-            log.error("getOutputStream exception: "+e);
-            if(m_port!=0){
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName+":"+m_port, ConnectionStatus.CONNECTION_DOWN);
+        } catch (java.io.IOException e) {
+            log.error("getOutputStream exception: " + e);
+            if (m_port != 0) {
+                ConnectionStatus.instance().setConnectionState(
+                        m_HostName + ":" + m_port, ConnectionStatus.CONNECTION_DOWN);
             } else {
-               ConnectionStatus.instance().setConnectionState(
-            		m_HostName, ConnectionStatus.CONNECTION_DOWN);
+                ConnectionStatus.instance().setConnectionState(
+                        m_HostName, ConnectionStatus.CONNECTION_DOWN);
             }
-     	}
-     	return null;
+        }
+        return null;
     }
-    
+
     /*This in place here until all systems are converted over to the systemconnection memo
-    this will then become abstract, once all the code has been refactored*/
-    public SystemConnectionMemo getSystemConnectionMemo() { return null; }
-    
+     this will then become abstract, once all the code has been refactored*/
+    @Override
+    public SystemConnectionMemo getSystemConnectionMemo() {
+        return null;
+    }
+
     /*Set disable should be handled by the local port controller in each connection
-    this is abstract in the Portcontroller and can be removed once all the other codes has
-    been refactored */
-    public void setDisabled(boolean disabled) { 
+     this is abstract in the Portcontroller and can be removed once all the other codes has
+     been refactored */
+    @Override
+    public void setDisabled(boolean disabled) {
         mDisabled = disabled;
     }
     /*Dispose should be handled by the port adapters and this should be abstract
-    However this is in place until all the other code has been refactored */
-    public void dispose(){ return; }
+     However this is in place until all the other code has been refactored */
+
+    @Override
+    public void dispose() {
+        return;
+    }
 
     //private boolean allowConnectionRecovery = false;
-
     /**
-     * This is called when a connection is initially lost.  It closes the client side
-     * socket connection, resets the open flag and attempts a reconnection.
+     * This is called when a connection is initially lost. It closes the client
+     * side socket connection, resets the open flag and attempts a reconnection.
      */
-        @edu.umd.cs.findbugs.annotations.SuppressWarnings(value="DE_MIGHT_IGNORE",
-                                                justification="we are trying to close a failed connection, it doesn't matter if it generates an error")
-    public void recover(){
-        if (!allowConnectionRecovery) return;
+    @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "DE_MIGHT_IGNORE",
+            justification = "we are trying to close a failed connection, it doesn't matter if it generates an error")
+    @Override
+    public void recover() {
+        if (!allowConnectionRecovery) {
+            return;
+        }
         opened = false;
         try {
             socketConn.close();
-        } catch (Exception e) { }
+        } catch (Exception e) {
+        }
         reconnect();
     }
 
     /**
      * Attempts to reconnect to a failed Server
      */
-    public void reconnect(){
-		
+    public void reconnect() {
+
         // If the connection is already open, then we shouldn't try a re-connect.
-        if (opened && !allowConnectionRecovery) return;
+        if (opened && !allowConnectionRecovery) {
+            return;
+        }
         reconnectwait thread = new reconnectwait();
         thread.setName("Connection Recovery " + getHostName());
         thread.start();
-        try{
+        try {
             thread.join();
         } catch (InterruptedException e) {
             log.error("Unable to join to the reconnection thread");
         }
 
-        if (!opened){
+        if (!opened) {
             log.error("Failed to re-establish connectivity");
         } else {
             resetupConnection();
@@ -268,55 +324,62 @@ abstract public class AbstractNetworkPortController extends AbstractPortControll
     }
 
     /**
-     * Customizable method to deal with resetting a system connection after
-     * a sucessful recovery of a connection.
+     * Customizable method to deal with resetting a system connection after a
+     * sucessful recovery of a connection.
      */
-    protected void resetupConnection() {}
+    protected void resetupConnection() {
+    }
 
-    class reconnectwait extends Thread{
-        public final static int THREADPASS     = 0;
-        public final static int THREADFAIL     = 1;
-        int         _status;
-        
+    class reconnectwait extends Thread {
+
+        public final static int THREADPASS = 0;
+        public final static int THREADFAIL = 1;
+        int _status;
+
         public int status() {
             return _status;
         }
+
         public reconnectwait() {
             _status = THREADFAIL;
         }
-        @edu.umd.cs.findbugs.annotations.SuppressWarnings(value="DE_MIGHT_IGNORE",
-                                                justification="we are testing for a the ability to re-connect and this is likely to generate an error which can be ignored")
+
+        @edu.umd.cs.findbugs.annotations.SuppressWarnings(value = "DE_MIGHT_IGNORE",
+                justification = "we are testing for a the ability to re-connect and this is likely to generate an error which can be ignored")
+        @Override
         public void run() {
             boolean reply = true;
             int count = 0;
             int secondCount = 0;
-            while(reply){
+            while (reply) {
                 safeSleep(reconnectinterval, "Waiting");
                 count++;
                 try {
                     // if the device allows autoConfiguration,
                     // we need to run the autoConfigure() call
                     // before we try to reconnect.
-                    if(getMdnsConfigure()) autoConfigure();
+                    if (getMdnsConfigure()) {
+                        autoConfigure();
+                    }
                     connect();
                 } catch (Exception e) {
                 }
-                reply=!opened;
-                if (count >=retryAttempts){
+                reply = !opened;
+                if (count >= retryAttempts) {
                     log.error("Unable to reconnect after " + count + " Attempts, increasing duration of retries");
                     //retrying but with twice the retry interval.
-                    reconnectinterval = reconnectinterval*2;
+                    reconnectinterval = reconnectinterval * 2;
                     count = 0;
                     secondCount++;
                 }
-                if (secondCount >=10){
+                if (secondCount >= 10) {
                     log.error("Giving up on reconnecting after 100 attempts");
-                    reply=false;
+                    reply = false;
                 }
             }
         }
     }
-   
+
     final static protected Logger log = LoggerFactory.getLogger(AbstractNetworkPortController.class.getName());
-    
+
 }
