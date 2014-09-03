c14d5777b53cd8ddc2e296555e4f68ee600775bb
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-09-03 10:08:51 +0000

    Formatting, whitespace, JavaDocs, and @Override annotations. This is in preparation for ensuring setImagePath(null) does not create a potentially invalid imagePath.

--- java/src/jmri/util/swing/ResizableImagePanel.java
+++ java/src/jmri/util/swing/ResizableImagePanel.java
@@ -11,6 +11,7 @@ import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,12 +19,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A class extending JPanels to have a image display in a panel, supports:
- *     + drag'n drop of image file
- *     + can resize container
- *     + can scale content to size
- *     + respect aspect ratio by default (when resizing content)
- *
+ * A class extending JPanels to have a image display in a panel, supports<ul>
+ * <li>drag'n drop of image file</li>
+ * <li>can resize container</li>
+ * <li>can scale content to size</li>
+ * <li>respect aspect ratio by default (when resizing content)</li>
+ * </ul>
  * (overrides paintComponent for performances)
  *
  * @author Lionel Jeanson - Copyright 2009
@@ -31,20 +32,20 @@ import org.slf4j.LoggerFactory;
 public class ResizableImagePanel extends JPanel implements ComponentListener {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 4576214324220842001L;
-	private String _imagePath;
+     *
+     */
+    private static final long serialVersionUID = 4576214324220842001L;
+    private String _imagePath;
     protected JLabel bgImg = null;
     private BufferedImage image = null;
     private BufferedImage scaledImage = null;
     private boolean _resizeContainer = false;
     private boolean _respectAspectRatio = true;
-    static private Color BackGroundColor = Color.BLACK ;
+    static private Color BackGroundColor = Color.BLACK;
     boolean toResize = false;
-    final static Dimension smallDim = new Dimension(10,10);
-    
-    /** 
+    final static Dimension smallDim = new Dimension(10, 10);
+
+    /**
      * Default constructor.
      *
      */
@@ -53,13 +54,13 @@ public class ResizableImagePanel extends JPanel implements ComponentListener {
         super.setBackground(BackGroundColor);
         setVisible(false);
     }
-    
-    /** 
-     * Constructor with initial image file path as parameter.
-     * 		Component will be (preferred) sized to image sized
-     *		
+
+    /**
+     * Constructor with initial image file path as parameter. Component will be
+     * (preferred) sized to image sized
      *
-     * @param imagePath Path to image to display 
+     *
+     * @param imagePath Path to image to display
      */
     public ResizableImagePanel(String imagePath) {
         super();
@@ -67,7 +68,7 @@ public class ResizableImagePanel extends JPanel implements ComponentListener {
         setImagePath(imagePath);
     }
 
-    /** 
+    /**
      * Constructor for ResizableImagePanel with forced initial size
      *
      * @param imagePath Path to image to display
@@ -81,10 +82,11 @@ public class ResizableImagePanel extends JPanel implements ComponentListener {
         super.setBackground(BackGroundColor);
         setImagePath(imagePath);
     }
-    
+
+    @Override
     public void setBackground(Color bckCol) {
-    	super.setBackground(bckCol);
-    	setScaledImage();
+        super.setBackground(bckCol);
+        setScaledImage();
     }
 
     /**
@@ -99,179 +101,196 @@ public class ResizableImagePanel extends JPanel implements ComponentListener {
     /**
      * Can this DnDImagePanel resize its container?
      *
+     * @return true if container can be resized
      */
     public boolean isResizingContainer() {
         return _resizeContainer;
     }
-    
+
     /**
      * Is this DnDImagePanel respecting aspect ratio when resizing content?
      *
+     * @return true is aspect ratio is maintained
      */
     public boolean isRespectingAspectRatio() {
-    	return _respectAspectRatio;
+        return _respectAspectRatio;
     }
-    
+
     /**
-     * Allow this ResizableImagePanel to respect aspect ratio when resizing content
+     * Allow this ResizableImagePanel to respect aspect ratio when resizing
+     * content
      *
      * @param b
      */
     public void setRespectAspectRatio(boolean b) {
-    	_respectAspectRatio = b;
+        _respectAspectRatio = b;
     }
-    
+
     /**
-     * Return curent image file path
+     * Return current image file path
      *
+     * @return The image path or "/" if no image is specified
      */
     public String getImagePath() {
         return _imagePath;
     }
 
     /**
-     * Set image file path, display will be updated
-     * If passed value is null, blank image
+     * Set image file path, display will be updated If passed value is null,
+     * blank image
      *
+     * @param s
      */
     public void setImagePath(String s) {
-    	if (s==null) {
-    		if ((_imagePath!=null) && (! (new File(_imagePath)).isDirectory()))
-    			_imagePath = (new File(_imagePath)).getParent() ; 
-    		else 
-    			if (_imagePath==null)
-    				_imagePath = "/"  ;
-    	}
-    	else
-    		_imagePath = s;
-    	log.debug("Image path is now : {}", _imagePath);
+        if (s == null) {
+            if ((_imagePath != null) && (!(new File(_imagePath)).isDirectory())) {
+                _imagePath = (new File(_imagePath)).getParent();
+            } else if (_imagePath == null) {
+                // What is the advantage of passing an invalid path for an image
+                // if no image is specified?
+                _imagePath = "/";
+            }
+        } else {
+            _imagePath = s;
+        }
+        log.debug("Image path is now : {}", _imagePath);
         try {
             image = ImageIO.read(new File(_imagePath));
-        } catch (Exception ex) {
-        	log.debug("{} is not a valid image file, exception: ", _imagePath, ex);
-        	image = null;
+        } catch (IOException ex) {
+            log.debug("{} is not a valid image file, exception: ", _imagePath, ex);
+            image = null;
             scaledImage = null;
         }
-        if (isResizingContainer())
-        	resizeContainer();       
-        setScaledImage();       
+        if (isResizingContainer()) {
+            resizeContainer();
+        }
+        setScaledImage();
         setVisible(true);
         repaint();
-        if (getParent() != null)
-        	getParent().repaint();
+        if (getParent() != null) {
+            getParent().repaint();
+        }
     }
-	
+
     //
     // componentListener methods, for auto resizing and scaling
     //
+    @Override
     public void componentResized(ComponentEvent e) {
-    	if ( ! (isResizingContainer()))
-    		if ( e.getComponent().isVisible() ) {
-    			setSize(e.getComponent().getSize());
-    			setPreferredSize(e.getComponent().getSize());
-    			setScaledImage();
-    			toResize = false;
-    		}
-    		else
-    			toResize = true;
+        if (!(isResizingContainer())) {
+            if (e.getComponent().isVisible()) {
+                setSize(e.getComponent().getSize());
+                setPreferredSize(e.getComponent().getSize());
+                setScaledImage();
+                toResize = false;
+            } else {
+                toResize = true;
+            }
+        }
         repaint();
-        if (getParent() != null)
-        	getParent().repaint();
-    }
-
-    public void componentMoved(ComponentEvent e) {
-    }
-
-    public void componentShown(ComponentEvent e) {
-    	if (isResizingContainer())
-    		resizeContainer();
-    	else {
-    		if ((toResize) || (scaledImage==null)) {
-    			setSize(e.getComponent().getSize());
-    			setPreferredSize(e.getComponent().getSize());
-    			setScaledImage();
-    			toResize = false;
-    		}
+        if (getParent() != null) {
+            getParent().repaint();
         }
     }
 
-    public void componentHidden(ComponentEvent e) {
-    	log.debug("Component hidden");
-    	if (isResizingContainer()) {
-    		resizeContainer(smallDim);
-    	}
+    @Override
+    public void componentMoved(ComponentEvent e) {
     }
-	
-	private void resizeContainer(Dimension d) {
-		log.debug("Resizing container");
-    	Container p1 = getParent();
-    	if ((p1 != null) && (image!=null))
-    	{
-    		setPreferredSize(d);
-    		setSize(d);
-    		p1.setPreferredSize(d);
-    		p1.setSize(d);
-    		if ((getTopLevelAncestor() != null) && (getTopLevelAncestor() instanceof Window))
-    			((Window) getTopLevelAncestor()).pack(); // yes, lucky hack, possibly dirty
-    	}
-	}
-    
-	private void resizeContainer() {
-		if (scaledImage != null)
-			resizeContainer( new Dimension(scaledImage.getWidth(null), scaledImage.getHeight(null)) );
-		else
-			if (image != null)
-				resizeContainer( new Dimension(image.getWidth(null), image.getHeight(null)) );
-	}
 
-	//override paintComponent
-    public void paintComponent(Graphics g) {
-    	super.paintComponent(g);
-    	if (scaledImage != null)
-    		g.drawImage(scaledImage, 0, 0, this);
-    	else
-			g.clearRect(0,0, (int)getSize().getWidth(), (int)getSize().getHeight());
+    @Override
+    public void componentShown(ComponentEvent e) {
+        if (isResizingContainer()) {
+            resizeContainer();
+        } else {
+            if ((toResize) || (scaledImage == null)) {
+                setSize(e.getComponent().getSize());
+                setPreferredSize(e.getComponent().getSize());
+                setScaledImage();
+                toResize = false;
+            }
+        }
     }
-    
+
+    @Override
+    public void componentHidden(ComponentEvent e) {
+        log.debug("Component hidden");
+        if (isResizingContainer()) {
+            resizeContainer(smallDim);
+        }
+    }
+
+    private void resizeContainer(Dimension d) {
+        log.debug("Resizing container");
+        Container p1 = getParent();
+        if ((p1 != null) && (image != null)) {
+            setPreferredSize(d);
+            setSize(d);
+            p1.setPreferredSize(d);
+            p1.setSize(d);
+            if ((getTopLevelAncestor() != null) && (getTopLevelAncestor() instanceof Window)) {
+                ((Window) getTopLevelAncestor()).pack(); // yes, lucky hack, possibly dirty
+            }
+        }
+    }
+
+    private void resizeContainer() {
+        if (scaledImage != null) {
+            resizeContainer(new Dimension(scaledImage.getWidth(null), scaledImage.getHeight(null)));
+        } else if (image != null) {
+            resizeContainer(new Dimension(image.getWidth(null), image.getHeight(null)));
+        }
+    }
+
+    //override paintComponent
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (scaledImage != null) {
+            g.drawImage(scaledImage, 0, 0, this);
+        } else {
+            g.clearRect(0, 0, (int) getSize().getWidth(), (int) getSize().getHeight());
+        }
+    }
+
     /**
-     * Get curent scaled Image 
+     * Get current scaled Image
+     *
+     * @return the image resized as specified
      */
     public BufferedImage getScaledImage() {
-    	return scaledImage;
+        return scaledImage;
     }
 
     private void setScaledImage() {
-    	if (image != null) {
-    		if ((getSize().getWidth() != 0) && (getSize().getHeight() != 0) && 
-    				((getSize().getWidth() != image.getWidth(null)) || (getSize().getHeight() != image.getHeight(null)))) {
-    			int newW = (int)getSize().getWidth();
-    			int newH = (int)getSize().getHeight();
-    			int new0x = 0;
-    			int new0y = 0;
-    			log.debug("Actually resizing image {} to {}x{}", this.getImagePath(), newW, newH);
-    			scaledImage = new BufferedImage(newW, newH,  image.getType() == 0? BufferedImage.TYPE_INT_ARGB : image.getType());  
-    			Graphics2D g = scaledImage.createGraphics();
-    			g.setBackground(getBackground());
-    			g.clearRect(0, 0, newW, newH);
-    			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY ); 
-    			if ( _respectAspectRatio )
-    				if (  (getSize().getWidth() / getSize().getHeight()) > ((double)image.getWidth(null) / (double)image.getHeight(null)) )
-    				{ // Fill on height
-    					newW = image.getWidth(null)*newH/image.getHeight(null);
-    					new0x = (int)(getSize().getWidth()-newW)/2;
-    				}
-    				else
-    				{ // Fill on width
-    					newH = image.getHeight(null)*newW/image.getWidth(null);
-    					new0y = (int)(getSize().getHeight()-newH)/2;
-    				}
-    			g.drawImage(image, new0x, new0y, new0x+newW, new0y+newH, 0, 0, image.getWidth(), image.getHeight(), this);              
-    			g.dispose();  
-    		} else {
-    			scaledImage = image;
-    		}
-    	}
+        if (image != null) {
+            if ((getSize().getWidth() != 0) && (getSize().getHeight() != 0)
+                    && ((getSize().getWidth() != image.getWidth(null)) || (getSize().getHeight() != image.getHeight(null)))) {
+                int newW = (int) getSize().getWidth();
+                int newH = (int) getSize().getHeight();
+                int new0x = 0;
+                int new0y = 0;
+                log.debug("Actually resizing image {} to {}x{}", this.getImagePath(), newW, newH);
+                scaledImage = new BufferedImage(newW, newH, image.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : image.getType());
+                Graphics2D g = scaledImage.createGraphics();
+                g.setBackground(getBackground());
+                g.clearRect(0, 0, newW, newH);
+                g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                if (_respectAspectRatio) {
+                    if ((getSize().getWidth() / getSize().getHeight()) > ((double) image.getWidth(null) / (double) image.getHeight(null))) { // Fill on height
+                        newW = image.getWidth(null) * newH / image.getHeight(null);
+                        new0x = (int) (getSize().getWidth() - newW) / 2;
+                    } else { // Fill on width
+                        newH = image.getHeight(null) * newW / image.getWidth(null);
+                        new0y = (int) (getSize().getHeight() - newH) / 2;
+                    }
+                }
+                g.drawImage(image, new0x, new0y, new0x + newW, new0y + newH, 0, 0, image.getWidth(), image.getHeight(), this);
+                g.dispose();
+            } else {
+                scaledImage = image;
+            }
+        }
     }
-    
+
     static private Logger log = LoggerFactory.getLogger(ResizableImagePanel.class.getName());
 }
