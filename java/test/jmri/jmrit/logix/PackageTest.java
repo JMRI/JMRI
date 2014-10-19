dc29b0d0b988f5807dee8f0b4f4a1b77621c6b03
Bob Jacobsen <jacobsen@mac.com>
2014-10-19 05:45:16 +0000

    add test of collisions with Block constants

--- java/test/jmri/jmrit/logix/PackageTest.java
+++ java/test/jmri/jmrit/logix/PackageTest.java
@@ -29,6 +29,7 @@ public class PackageTest extends TestCase {
         
 //		Something wrong in the xsd files?  maybe using -2-9-6 version?
         suite.addTest(SchemaTest.suite());
+        suite.addTest(OBlockTest.suite());
         suite.addTest(OPathTest.suite());
         suite.addTest(LogixActionTest.suite());
 
