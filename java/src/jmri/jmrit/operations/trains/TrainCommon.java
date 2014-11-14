8e91e7b4a85eec3168cc8084c45bfcf535f3cc12
Dan Boudreau <dan_boudreau@users.sourceforge.net>
2014-11-14 23:57:55 +0000

    Check for divide by zero when determining the number of characters per line.

--- java/src/jmri/jmrit/operations/trains/TrainCommon.java
+++ java/src/jmri/jmrit/operations/trains/TrainCommon.java
@@ -1696,7 +1696,10 @@ public class TrainCommon {
 		JLabel label = new JLabel();
 		FontMetrics metrics = label.getFontMetrics(font);
 		int charwidth = metrics.charWidth('m');
-
+		if (charwidth == 0) {
+			log.error("Line length charater width equal to zero. font size: {}, fontName: {}", fontSize, fontName);
+			return 80; // use 80 characters per line.
+		}
 		// compute lines and columns within margins
 		return getPageSize(orientation).width / charwidth;
 	}
