22625fe8e645a75428e2ad15b1939566c11ed7c4
Bob Jacobsen <jacobsen@mac.com>
2014-11-30 03:37:09 +0000

    initial infrastructure for Modbus support; no code yet

--- java/test/jmri/jmrix/PackageTest.java
+++ java/test/jmri/jmrix/PackageTest.java
@@ -27,7 +27,7 @@ public class PackageTest extends TestCase {
 
 	// test suite from all defined tests
 	public static Test suite() {
-		TestSuite suite = new TestSuite("jmri.jmrix.JmrixTest");
+		TestSuite suite = new TestSuite("jmri.jmrix.PackageTest");
 
         suite.addTest(jmri.jmrix.ActiveSystemFlagTest.suite());
         suite.addTest(jmri.jmrix.AbstractProgrammerTest.suite());
@@ -44,10 +44,11 @@ public class PackageTest extends TestCase {
         suite.addTest(jmri.jmrix.jmriclient.PackageTest.suite());
         suite.addTest(jmri.jmrix.lenz.PackageTest.suite());
         suite.addTest(jmri.jmrix.loconet.PackageTest.suite());
+        suite.addTest(jmri.jmrix.maple.PackageTest.suite());
+        suite.addTest(jmri.jmrix.modbus.PackageTest.suite());
         suite.addTest(jmri.jmrix.nce.PackageTest.suite());
         suite.addTest(jmri.jmrix.oaktree.PackageTest.suite());
 	    suite.addTest(jmri.jmrix.openlcb.PackageTest.suite());
-        suite.addTest(jmri.jmrix.maple.PackageTest.suite());
         suite.addTest(jmri.jmrix.powerline.PackageTest.suite());
         suite.addTest(jmri.jmrix.pricom.PackageTest.suite());
         suite.addTest(jmri.jmrix.qsi.PackageTest.suite());
