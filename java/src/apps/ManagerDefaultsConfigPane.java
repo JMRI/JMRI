d76d2f5d910b6cc75516921ed2bcb40dc8bb7a13
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-12-27 12:57:30 +0000

    Whitespace, formatting, Java 1.8-isms.

--- java/src/apps/ManagerDefaultsConfigPane.java
+++ java/src/apps/ManagerDefaultsConfigPane.java
@@ -1,75 +1,77 @@
 // ManagerDefaultsConfigPane.java
-
 package apps;
 
-import javax.swing.*;
-
-import java.util.List;
+import java.awt.event.ActionEvent;
 import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
-import jmri.managers.ManagerDefaultSelector;
+import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import jmri.InstanceManager;
 import jmri.jmrix.SystemConnectionMemo;
+import jmri.managers.ManagerDefaultSelector;
+import jmri.util.javaworld.GridLayout2;
 
 /**
  * Provide GUI to configure InstanceManager defaults.
  * <P>
  *
- * @author      Bob Jacobsen   Copyright (C)  2010
+ * @author Bob Jacobsen Copyright (C) 2010
  * @version	$Revision$
  * @since 2.9.5
  */
 public class ManagerDefaultsConfigPane extends jmri.util.swing.JmriPanel {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 4382220076212974325L;
+     *
+     */
+    private static final long serialVersionUID = 4382220076212974325L;
 
-	public ManagerDefaultsConfigPane() {
-    
+    public ManagerDefaultsConfigPane() {
+
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        
+
         matrix = new JPanel();
         add(matrix);
-        ManagerDefaultSelector.instance.addPropertyChangeListener(new PropertyChangeListener(){
-            public void propertyChange(PropertyChangeEvent e) {
-                if(e.getPropertyName().equals("Updated")){
-                    update();
-                }
+        ManagerDefaultSelector.instance.addPropertyChangeListener((PropertyChangeEvent e) -> {
+            if (e.getPropertyName().equals("Updated")) {
+                update();
             }
         });
         update();
     }
-    
+
     JPanel matrix;
-    
+
     /**
-     * Invoke when first displayed to load
-     * and present options
+     * Invoke when first displayed to load and present options
      */
     public void update() {
         matrix.removeAll();
-        
+
         // this doesn't find non-migrated systems, how do we handle that eventually?
-        List<SystemConnectionMemo> connList = jmri.InstanceManager.getList(SystemConnectionMemo.class);
-        if (connList!=null){
+        List<SystemConnectionMemo> connList = InstanceManager.getList(SystemConnectionMemo.class);
+        if (connList != null) {
             reloadConnections(connList);
         } else {
             matrix.add(new JLabel("No new-form system connections configured"));
         }
     }
-    
+
     void reloadConnections(List<SystemConnectionMemo> connList) {
-        matrix.setLayout(new jmri.util.javaworld.GridLayout2(connList.size()+1, ManagerDefaultSelector.instance.knownManagers.length+1));
+        matrix.setLayout(new GridLayout2(connList.size() + 1, ManagerDefaultSelector.instance.knownManagers.length + 1));
         matrix.add(new JLabel(""));
-        
+
         for (ManagerDefaultSelector.Item item : ManagerDefaultSelector.instance.knownManagers) {
             matrix.add(new JLabel(item.typeName));
         }
         groups = new ButtonGroup[ManagerDefaultSelector.instance.knownManagers.length];
-        for (int i = 0; i<ManagerDefaultSelector.instance.knownManagers.length; i++) groups[i] = new ButtonGroup();
-        for (int x = 0; x<connList.size(); x++){
+        for (int i = 0; i < ManagerDefaultSelector.instance.knownManagers.length; i++) {
+            groups[i] = new ButtonGroup();
+        }
+        for (int x = 0; x < connList.size(); x++) {
             jmri.jmrix.SystemConnectionMemo memo = connList.get(x);
             String name = memo.getUserName();
             matrix.add(new JLabel(name));
@@ -79,8 +81,8 @@ public class ManagerDefaultsConfigPane extends jmri.util.swing.JmriPanel {
                     JRadioButton r = new SelectionButton(name, item.managerClass);
                     matrix.add(r);
                     groups[i].add(r);
-                    if (x == connList.size()-1 && ManagerDefaultSelector.instance.getDefault(item.managerClass)==null) {
-                    	r.setSelected(true);
+                    if (x == connList.size() - 1 && ManagerDefaultSelector.instance.getDefault(item.managerClass) == null) {
+                        r.setSelected(true);
                     }
                 } else {
                     // leave a blank
@@ -92,43 +94,46 @@ public class ManagerDefaultsConfigPane extends jmri.util.swing.JmriPanel {
             }
         }
         revalidate();
-        
+
     }
-    
+
     ButtonGroup[] groups;
-    
+
     /**
      * Captive class to track changes
      */
     static class SelectionButton extends JRadioButton {
+
         /**
-		 * 
-		 */
-		private static final long serialVersionUID = -2572336492673634333L;
-		SelectionButton(String name, Class<?> managerClass) {
+         *
+         */
+        private static final long serialVersionUID = -2572336492673634333L;
+
+        SelectionButton(String name, Class<?> managerClass) {
             super();
             this.managerClass = managerClass;
             this.name = name;
-            
-            if (name.equals(ManagerDefaultSelector.instance.getDefault(managerClass))) 
-                this.setSelected(true);
 
-            addActionListener(new java.awt.event.ActionListener() {
-                public void actionPerformed(java.awt.event.ActionEvent e) {
-                    if (isSelected())
-                        ManagerDefaultSelector.instance.setDefault(SelectionButton.this.managerClass, SelectionButton.this.name);
+            if (name.equals(ManagerDefaultSelector.instance.getDefault(managerClass))) {
+                this.setSelected(true);
+            }
+
+            addActionListener((ActionEvent e) -> {
+                if (isSelected()) {
+                    ManagerDefaultSelector.instance.setDefault(SelectionButton.this.managerClass, SelectionButton.this.name);
                 }
             });
 
         }
         String name;
         Class<?> managerClass;
+
         @Override
         public void setSelected(boolean t) {
             super.setSelected(t);
-            if (t)
+            if (t) {
                 ManagerDefaultSelector.instance.setDefault(this.managerClass, this.name);
+            }
         }
     }
 }
-
