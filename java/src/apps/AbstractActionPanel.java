5ac9e5017d3a59298cbface8c923ef8394ade053
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-12-27 11:40:36 +0000

    Whitespace, formatting, Java 1.8-isms.

--- java/src/apps/AbstractActionPanel.java
+++ java/src/apps/AbstractActionPanel.java
@@ -1,49 +1,43 @@
 // AbstractActionPanel.java
-
 package apps;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ResourceBundle;
 import java.util.Arrays;
-
+import java.util.List;
+import java.util.ResourceBundle;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.beans.PropertyChangeListener;
-
-import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Provide an abstract GUI for configuring use of Actions.
  * <P>
- * A {@link PerformActionPanel}/{@link PerformActionModel} object pair
- * invokes a Swing Action when
- * the program is started.
+ * A {@link PerformActionPanel}/{@link PerformActionModel} object pair invokes a
+ * Swing Action when the program is started.
  * <P>
- * A {@link CreateButtonPanel}/{@link CreateButtonModel} object pair
- * creates a new GUI button when
- * the program is started.
+ * A {@link CreateButtonPanel}/{@link CreateButtonModel} object pair creates a
+ * new GUI button when the program is started.
  * <P>
  *
  * <P>
- * @author	Bob Jacobsen   Copyright 2003
- * @version     $Revision$
+ * @author	Bob Jacobsen Copyright 2003
+ * @version $Revision$
  */
 abstract public class AbstractActionPanel extends JPanel {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = -5092031602319689380L;
-	JPanel self;  // used for synchronization
+     *
+     */
+    private static final long serialVersionUID = -5092031602319689380L;
+    JPanel self;  // used for synchronization
     protected ResourceBundle rb;
 
     String removeButtonKey;
@@ -58,33 +52,27 @@ abstract public class AbstractActionPanel extends JPanel {
         rb = ResourceBundle.getBundle("apps.AppsConfigBundle");
 
         // add existing items
-
         JButton addButton = new JButton(rb.getString(addButtonKey));
         JPanel panel = new JPanel();  // button is a horizontal item too; expands to fill BoxLayout
         panel.setLayout(new FlowLayout());
         panel.add(addButton);
         add(panel);
-        addButton.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    addItem();
-                }
-            }
-        );
+        addButton.addActionListener((ActionEvent e) -> {
+            addItem();
+        });
 
         // are there any existing objects from reading existing config?
         int n = rememberedObjects().size();
-        for (int i = 0; i< n; i++) {
+        for (int i = 0; i < n; i++) {
             AbstractActionModel m = (AbstractActionModel) rememberedObjects().get(i);
             add(new Item(m));
         }
-        jmri.InstanceManager.getDefault(apps.CreateButtonModel.class).addPropertyChangeListener(new PropertyChangeListener() {
-            public void propertyChange(java.beans.PropertyChangeEvent e) {
-                if (e.getPropertyName().equals("length")){
-                    Component[] l = getComponents();
-                    for(Component m: l){
-                        if ( (m!= null) && (m instanceof AbstractActionPanel.Item)) {
-                            ((Item) m).updateCombo();
-                        }
+        jmri.InstanceManager.getDefault(apps.CreateButtonModel.class).addPropertyChangeListener((java.beans.PropertyChangeEvent e) -> {
+            if (e.getPropertyName().equals("length")) {
+                Component[] l = getComponents();
+                for (Component m : l) {
+                    if ((m != null) && (m instanceof AbstractActionPanel.Item)) {
+                        ((Item) m).updateCombo();
                     }
                 }
             }
@@ -94,30 +82,34 @@ abstract public class AbstractActionPanel extends JPanel {
     abstract List<?> rememberedObjects();
 
     protected void addItem() {
-        synchronized(self) {
+        synchronized (self) {
             add(new Item());
             validate();
-            if (getTopLevelAncestor()!=null) ((JFrame)getTopLevelAncestor()).pack();
+            if (getTopLevelAncestor() != null) {
+                ((JFrame) getTopLevelAncestor()).pack();
+            }
         }
     }
 
     abstract AbstractActionModel getNewModel();
 
     public class Item extends JPanel implements ActionListener {
+
         /**
-		 * 
-		 */
-		private static final long serialVersionUID = -2499516926618516181L;
-		JButton removeButton = new JButton(rb.getString(removeButtonKey));
-            
+         *
+         */
+        private static final long serialVersionUID = -2499516926618516181L;
+        JButton removeButton = new JButton(rb.getString(removeButtonKey));
+
         Item() {
             setLayout(new FlowLayout());
             add(removeButton);
             removeButton.addActionListener(this);
             // create the list of possibilities
-            selections = new JComboBox<String>(AbstractActionModel.nameList());
+            selections = new JComboBox<>(AbstractActionModel.nameList());
             add(selections);
         }
+
         Item(AbstractActionModel m) {
             this();
             model = m;
@@ -126,28 +118,31 @@ abstract public class AbstractActionPanel extends JPanel {
 
         AbstractActionModel model = null;
         JComboBox<String> selections;
-        
-        void updateCombo(){
-            String current = (String)selections.getSelectedItem();
+
+        void updateCombo() {
+            String current = (String) selections.getSelectedItem();
             selections.removeAllItems();
             String[] items = AbstractActionModel.nameList();
-            for(int i = 0; i<items.length; i++){
-                selections.addItem(items[i]);
+            for (String item : items) {
+                selections.addItem(item);
             }
-            if(Arrays.asList(items).contains(current)){
+            if (Arrays.asList(items).contains(current)) {
                 selections.setSelectedItem(current);
             } else {
                 log.info("Item " + current + " has been removed as it is no longer a valid option");
                 actionPerformed(null);
             }
         }
-        
+
         public AbstractActionModel updatedModel() {
-            if (model==null) model = getNewModel();
-            model.setName((String)selections.getSelectedItem());
+            if (model == null) {
+                model = getNewModel();
+            }
+            model.setName((String) selections.getSelectedItem());
             return model;
         }
-        
+
+        @Override
         public void actionPerformed(ActionEvent e) {
             synchronized (self) {
                 // remove this item from display
@@ -155,7 +150,9 @@ abstract public class AbstractActionPanel extends JPanel {
                 Component topParent = this.getTopLevelAncestor();
                 parent.remove(this);
                 parent.validate();
-                if (topParent!=null) ((JFrame)topParent).pack();
+                if (topParent != null) {
+                    ((JFrame) topParent).pack();
+                }
                 parent.repaint();
                 // unlink to encourage garbage collection
                 removeButton.removeActionListener(this);
@@ -163,8 +160,6 @@ abstract public class AbstractActionPanel extends JPanel {
             }
         }
     }
-    
+
     static Logger log = LoggerFactory.getLogger(AbstractActionPanel.class.getName());
 }
-
-
