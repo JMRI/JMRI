df341e7076c03b4cbc623c27d84d6bf73256a816
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-12-27 10:15:23 +0000

    Whitespace, formatting, Java 1.8-isms.

--- java/src/apps/SystemConsoleConfigPanel.java
+++ java/src/apps/SystemConsoleConfigPanel.java
@@ -1,13 +1,10 @@
 // SystemConsoleConfigPanel.java
-
 package apps;
 
 import apps.SystemConsole.Scheme;
-import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 import javax.swing.BoxLayout;
 import javax.swing.JComboBox;
@@ -15,7 +12,6 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
-import javax.swing.ListCellRenderer;
 import javax.swing.UIManager;
 import jmri.util.swing.FontComboUtil;
 
@@ -24,28 +20,26 @@ import jmri.util.swing.FontComboUtil;
  * <hr>
  * This file is part of JMRI.
  * <P>
- * JMRI is free software; you can redistribute it and/or modify it under
- * the terms of version 2 of the GNU General Public License as published
- * by the Free Software Foundation. See the "COPYING" file for a copy
- * of this license.
+ * JMRI is free software; you can redistribute it and/or modify it under the
+ * terms of version 2 of the GNU General Public License as published by the Free
+ * Software Foundation. See the "COPYING" file for a copy of this license.
  * <P>
- * JMRI is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
+ * JMRI is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * <P>
  *
- * @author Matthew Harris  copyright (c) 2010, 2011
+ * @author Matthew Harris copyright (c) 2010, 2011
  * @version $Revision$
  */
 public class SystemConsoleConfigPanel extends JPanel {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 2050602237831555417L;
+     *
+     */
+    private static final long serialVersionUID = 2050602237831555417L;
 
-	private static final ResourceBundle rbc = ResourceBundle.getBundle("apps.AppsConfigBundle");
+    private static final ResourceBundle rbc = ResourceBundle.getBundle("apps.AppsConfigBundle");
 
     private static final Integer fontSizes[] = {
         8,
@@ -58,24 +52,24 @@ public class SystemConsoleConfigPanel extends JPanel {
         16,
         18,
         20,
-        24 };
+        24};
 
     private static final Integer wrapStyles[] = {
         SystemConsole.WRAP_STYLE_NONE,
         SystemConsole.WRAP_STYLE_LINE,
-        SystemConsole.WRAP_STYLE_WORD };
+        SystemConsole.WRAP_STYLE_WORD};
 
     private static final String wrapStyleNames[] = {
         rbc.getString("ConsoleWrapStyleNone"),
         rbc.getString("ConsoleWrapStyleLine"),
-        rbc.getString("ConsoleWrapStyleWord") };
+        rbc.getString("ConsoleWrapStyleWord")};
 
     private static final JToggleButton fontStyleBold = new JToggleButton("B");
 
     private static final JToggleButton fontStyleItalic = new JToggleButton("I");
 
     private static final JComboBox scheme = new JComboBox(SystemConsole.getInstance().getSchemes());
-    
+
     private static final JComboBox fontFamily = FontComboUtil.getFontCombo(FontComboUtil.MONOSPACED, 14);
 
     private static final JComboBox fontSize = new JComboBox(fontSizes);
@@ -88,57 +82,42 @@ public class SystemConsoleConfigPanel extends JPanel {
         p.add(new JLabel(rbc.getString("ConsoleScheme")));
 
         scheme.setSelectedIndex(SystemConsole.getInstance().getScheme());
-        scheme.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                SystemConsole.getInstance().setScheme(((JComboBox)e.getSource()).getSelectedIndex());
-            }
+        scheme.addActionListener((ActionEvent e) -> {
+            SystemConsole.getInstance().setScheme(((JComboBox) e.getSource()).getSelectedIndex());
         });
 
-        scheme.setRenderer(new ListCellRenderer() {
-            public Component getListCellRendererComponent(JList list,
-                    Object value,
-                    int index,
-                    boolean isSelected,
-                    boolean hasFocus) {
-
-                Scheme scheme = (Scheme)value;
-
-                JPanel p = new JPanel();
-                p.setOpaque(index > -1);
-
-                if (isSelected && index > -1) {
-                    p.setBackground(list.getSelectionBackground());
-                    p.setForeground(list.getSelectionForeground());
-                } else {
-                    p.setBackground(list.getBackground());
-                    p.setForeground(list.getForeground());
-                }
-
-                JLabel l = new JLabel(" " + scheme.description + " ");
-                l.setOpaque(true);
-                l.setFont(new Font("Monospaced", SystemConsole.getInstance().getFontStyle(), 12));
-                l.setForeground(scheme.foreground);
-                l.setBackground(scheme.background);
-                p.add(l);
-
-                // 'Oribble hack as CDE/Motif JComboBox doesn't seem to like
-                // displaying JPanel objects in the JComboBox header
-                if(UIManager.getLookAndFeel().getName().equals("CDE/Motif") && index==-1 ) {
-                    return l;
-                }
-                return p;
+        scheme.setRenderer((JList list, Object value, int index, boolean isSelected, boolean hasFocus) -> {
+            Scheme scheme1 = (Scheme) value;
+            JPanel p1 = new JPanel();
+            p1.setOpaque(index > -1);
+            if (isSelected && index > -1) {
+                p1.setBackground(list.getSelectionBackground());
+                p1.setForeground(list.getSelectionForeground());
+            } else {
+                p1.setBackground(list.getBackground());
+                p1.setForeground(list.getForeground());
             }
+            JLabel l = new JLabel(" " + scheme1.description + " ");
+            l.setOpaque(true);
+            l.setFont(new Font("Monospaced", SystemConsole.getInstance().getFontStyle(), 12));
+            l.setForeground(scheme1.foreground);
+            l.setBackground(scheme1.background);
+            p1.add(l);
+            // 'Oribble hack as CDE/Motif JComboBox doesn't seem to like
+            // displaying JPanel objects in the JComboBox header
+            if (UIManager.getLookAndFeel().getName().equals("CDE/Motif") && index == -1) {
+                return l;
+            }
+            return p1;
         });
 
         p.add(scheme);
         add(p);
 
         p = new JPanel(new FlowLayout());
-        fontFamily.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                SystemConsole.getInstance().setFontFamily((String) ((JComboBox)e.getSource()).getSelectedItem());
-                scheme.repaint();
-            }
+        fontFamily.addActionListener((ActionEvent e) -> {
+            SystemConsole.getInstance().setFontFamily((String) ((JComboBox) e.getSource()).getSelectedItem());
+            scheme.repaint();
         });
         fontFamily.setSelectedItem(SystemConsole.getInstance().getFontFamily());
 
@@ -148,10 +127,8 @@ public class SystemConsoleConfigPanel extends JPanel {
         p.add(fontFamilyLabel);
         p.add(fontFamily);
 
-        fontSize.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                SystemConsole.getInstance().setFontSize((Integer) ((JComboBox)e.getSource()).getSelectedItem());
-            }
+        fontSize.addActionListener((ActionEvent e) -> {
+            SystemConsole.getInstance().setFontSize((Integer) ((JComboBox) e.getSource()).getSelectedItem());
         });
         fontSize.setToolTipText(rbc.getString("ConsoleFontSize"));
         fontSize.setSelectedItem(SystemConsole.getInstance().getFontSize());
@@ -161,22 +138,18 @@ public class SystemConsoleConfigPanel extends JPanel {
         p.add(fontSizeUoM);
 
         fontStyleBold.setFont(fontStyleBold.getFont().deriveFont(Font.BOLD));
-        fontStyleBold.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                doFontStyle();
-            }
+        fontStyleBold.addActionListener((ActionEvent e) -> {
+            doFontStyle();
         });
         fontStyleBold.setToolTipText(rbc.getString("ConsoleFontStyleBold"));
-        fontStyleBold.setSelected((SystemConsole.getInstance().getFontStyle()&Font.BOLD)==Font.BOLD);
+        fontStyleBold.setSelected((SystemConsole.getInstance().getFontStyle() & Font.BOLD) == Font.BOLD);
         p.add(fontStyleBold);
 
         fontStyleItalic.setFont(fontStyleItalic.getFont().deriveFont(Font.ITALIC));
-        fontStyleItalic.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                doFontStyle();
-            }
+        fontStyleItalic.addActionListener((ActionEvent e) -> {
+            doFontStyle();
         });
-        fontStyleItalic.setSelected((SystemConsole.getInstance().getFontStyle()&Font.ITALIC)==Font.ITALIC);
+        fontStyleItalic.setSelected((SystemConsole.getInstance().getFontStyle() & Font.ITALIC) == Font.ITALIC);
         fontStyleItalic.setToolTipText(rbc.getString("ConsoleFontStyleItalic"));
         p.add(fontStyleItalic);
 
@@ -184,10 +157,8 @@ public class SystemConsoleConfigPanel extends JPanel {
 
         p = new JPanel(new FlowLayout());
         final JComboBox wrapStyle = new JComboBox(wrapStyleNames);
-        wrapStyle.addActionListener(new ActionListener() {
-           public void actionPerformed(ActionEvent e)  {
-               SystemConsole.getInstance().setWrapStyle(wrapStyles[((JComboBox)e.getSource()).getSelectedIndex()]);
-           }
+        wrapStyle.addActionListener((ActionEvent e) -> {
+            SystemConsole.getInstance().setWrapStyle(wrapStyles[((JComboBox) e.getSource()).getSelectedIndex()]);
         });
         wrapStyle.setSelectedIndex(SystemConsole.getInstance().getWrapStyle());
 
@@ -203,8 +174,8 @@ public class SystemConsoleConfigPanel extends JPanel {
 
     private static void doFontStyle() {
         SystemConsole.getInstance().setFontStyle(
-                (fontStyleBold.isSelected()?Font.BOLD:Font.PLAIN)|
-                (fontStyleItalic.isSelected()?Font.ITALIC:Font.PLAIN));
+                (fontStyleBold.isSelected() ? Font.BOLD : Font.PLAIN)
+                | (fontStyleItalic.isSelected() ? Font.ITALIC : Font.PLAIN));
         scheme.repaint();
     }
 }
