15b74558f147a39ae298aaa336cde60b2205f3c5
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-12-31 14:02:38 +0000

    Protect against concurrent writes to config files. This may turn out to be a precaution against a scenario that can't be triggered, but I'm not certain a modularized JMRI may not attempt concurrent writes to the config files.

--- java/src/jmri/configurexml/ConfigXmlManager.java
+++ java/src/jmri/configurexml/ConfigXmlManager.java
@@ -1,24 +1,19 @@
 package jmri.configurexml;
 
-import jmri.InstanceManager;
-import jmri.jmrit.revhistory.FileHistory;
-
 import java.io.File;
 import java.net.URL;
-
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Collections;
-import java.util.Map;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import jmri.InstanceManager;
+import jmri.jmrit.revhistory.FileHistory;
 import jmri.util.FileUtil;
-
-//import org.jdom2.DocType;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.ProcessingInstruction;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -366,15 +361,19 @@ public class ConfigXmlManager extends jmri.jmrit.XmlFile
     }
     
     public void storePrefs(File file) {
-        Element root = initStore();
-        addPrefsStore(root);
-        finalStore(root, file);
+        synchronized (this) {
+            Element root = initStore();
+            addPrefsStore(root);
+            finalStore(root, file);
+        }
     }
     
     public void storeUserPrefs(File file) {
-        Element root = initStore();
-        addUserPrefsStore(root);
-        finalStore(root, file);
+        synchronized (this) {
+            Element root = initStore();
+            addUserPrefsStore(root);
+            finalStore(root, file);
+        }
     }
 
     /**
