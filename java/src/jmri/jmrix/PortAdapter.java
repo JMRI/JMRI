e7b8ea195d05aa392380e595b69fe4cf60adca37
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-01-01 11:20:12 +0000

    Whitespace, formatting, and JavaDocs.

--- java/src/jmri/jmrix/PortAdapter.java
+++ java/src/jmri/jmrix/PortAdapter.java
@@ -1,110 +1,129 @@
 // PortAdapter.java
-
 package jmri.jmrix;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
 /**
- * Enables basic setup of a interface
- * for a jmrix implementation.
- *<P>
+ * Enables basic setup of a interface for a jmrix implementation.
+ * <P>
  * This has no e.g. serial-specific information.
  *
- * @author	Bob Jacobsen   Copyright (C) 2001, 2003, 2008, 2010
+ * @author	Bob Jacobsen Copyright (C) 2001, 2003, 2008, 2010
  * @version	$Revision$
- * @see         jmri.jmrix.SerialConfigException
+ * @see jmri.jmrix.SerialConfigException
  * @since 2.3.1
  */
-public interface PortAdapter  {
+public interface PortAdapter {
 
-	/** 
-	 * Configure all of the other jmrix widgets needed to work with this adapter
-	 */
-	public void configure();
-
-	/** 
-	 * Query the status of this connection.  If all OK, at least
-	 * as far as is known, return true 
-	 */
-	public boolean status();
-    
     /**
-     *  Open the connection
+     * Configure all of the other jmrix widgets needed to work with this adapter
+     */
+    public void configure();
+
+    /**
+     * Query the status of this connection. If all OK, at least as far as is
+     * known, return true
+     *
+     * @return true if OK
+     */
+    public boolean status();
+
+    /**
+     * Open the connection
+     *
+     * @throws java.lang.Exception
      */
     public void connect() throws Exception;
-    
+
     public String getCurrentPortName();
 
-	// returns the InputStream from the port
-	public DataInputStream getInputStream();
+    // returns the InputStream from the port
+    public DataInputStream getInputStream();
+
     // returns the outputStream to the port
     public DataOutputStream getOutputStream();
-    
+
     public String getOption1Name();
-    
+
     public String getOption2Name();
-    
+
     public String getOption3Name();
 
     public String getOption4Name();
 
     /**
-     * Set the first port option.  Only to be used after construction, but
-     * before the openPort call
+     * Set the first port option. Only to be used after construction, but before
+     * the openPort call
+     *
+     * @param value
      */
     public void configureOption1(String value);
 
     /**
-    * Set the second port option.  Only to be used after construction, but
-    * before the openPort call
-    */
+     * Set the second port option. Only to be used after construction, but
+     * before the openPort call
+     *
+     * @param value
+     */
     public void configureOption2(String value);
 
     /**
-    * Set the third port option.  Only to be used after construction, but
-    * before the openPort call
-    */
+     * Set the third port option. Only to be used after construction, but before
+     * the openPort call
+     *
+     * @param value
+     */
     public void configureOption3(String value);
-    
+
     /**
-    * Set the fourth port option.  Only to be used after construction, but
-    * before the openPort call
-    */
+     * Set the fourth port option. Only to be used after construction, but
+     * before the openPort call
+     *
+     * @param value
+     */
     public void configureOption4(String value);
-    
+
     public String[] getOptions();
-    
+
     public boolean isOptionAdvanced(String option);
-    
+
     public String getOptionDisplayName(String option);
-    
+
     public void setOptionState(String option, String value);
-    
+
     public String getOptionState(String option);
-    
+
     public String[] getOptionChoices(String option);
 
-     /**
+    /**
      * Return the System Manufacturers Name
+     *
+     * @return Manufacturer's Name
      */
     public String getManufacturer();
-    
+
     /**
-    * Set the System Manufacturers Name
-    */
+     * Set the System Manufacturers Name
+     *
+     * @param Manufacturer
+     */
     public void setManufacturer(String Manufacturer);
-    
+
     /**
      * Return the disabled state of the adapter
+     *
+     * @return true if disabled
      */
     public boolean getDisabled();
-    
+
     /**
-    * Sets whether the connection is disabled
-    */
+     * Sets whether the connection is disabled
+     *
+     * @param disabled
+     */
     public void setDisabled(boolean disabled);
-    
+
     public SystemConnectionMemo getSystemConnectionMemo();
 
     public void dispose();
