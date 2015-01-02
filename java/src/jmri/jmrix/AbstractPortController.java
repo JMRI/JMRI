995f668c6ae77d2952770476cc9b5a099a4fd06b
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-01-02 10:35:25 +0000

    Whitespace, formatting, annotations, and JavaDocs.

--- java/src/jmri/jmrix/AbstractPortController.java
+++ java/src/jmri/jmrix/AbstractPortController.java
@@ -1,5 +1,4 @@
 // AbstractPortController.java
-
 package jmri.jmrix;
 
 import org.slf4j.Logger;
@@ -12,196 +11,240 @@ import java.util.HashMap;
 /**
  * Provide an abstract base for *PortController classes.
  * <P>
- * This is complicated by the lack of multiple inheritance.
- * SerialPortAdapter is an Interface, and its implementing
- * classes also inherit from various PortController types.  But we
- * want some common behaviors for those, so we put them here.
+ * This is complicated by the lack of multiple inheritance. SerialPortAdapter is
+ * an Interface, and its implementing classes also inherit from various
+ * PortController types. But we want some common behaviors for those, so we put
+ * them here.
  *
  * @see jmri.jmrix.SerialPortAdapter
  *
- * @author			Bob Jacobsen   Copyright (C) 2001, 2002
- * @version			$Revision$
+ * @author	Bob Jacobsen Copyright (C) 2001, 2002
+ * @version	$Revision$
  */
 abstract public class AbstractPortController implements PortAdapter {
 
     // returns the InputStream from the port
+    @Override
     public abstract DataInputStream getInputStream();
 
     // returns the outputStream to the port
+    @Override
     public abstract DataOutputStream getOutputStream();
 
     // check that this object is ready to operate
-    public boolean status() {return opened;}
-    
+    @Override
+    public boolean status() {
+        return opened;
+    }
+
     protected boolean opened = false;
-    
-    protected void setOpened() {opened = true; }
-    protected void setClosed() {opened = false; }
-    
+
+    protected void setOpened() {
+        opened = true;
+    }
+
+    protected void setClosed() {
+        opened = false;
+    }
+
     //These are to support the old legacy files.
     protected String option1Name = "1";
     protected String option2Name = "2";
     protected String option3Name = "3";
     protected String option4Name = "4";
 
+    @Override
     abstract public String getCurrentPortName();
     /*
-        The next set of configureOptions are to support the old configuration files.
-    */
-    public void configureOption1(String value) { 
-        if(options.containsKey(option1Name)){
+     The next set of configureOptions are to support the old configuration files.
+     */
+
+    @Override
+    public void configureOption1(String value) {
+        if (options.containsKey(option1Name)) {
             options.get(option1Name).configure(value);
         }
     }
 
-    public void configureOption2(String value) { 
-        if(options.containsKey(option2Name)){
+    @Override
+    public void configureOption2(String value) {
+        if (options.containsKey(option2Name)) {
             options.get(option2Name).configure(value);
         }
     }
-    
-    public void configureOption3(String value) { 
-        if(options.containsKey(option3Name)){
+
+    @Override
+    public void configureOption3(String value) {
+        if (options.containsKey(option3Name)) {
             options.get(option3Name).configure(value);
         }
     }
 
-    public void configureOption4(String value) { 
-        if(options.containsKey(option4Name)){
+    @Override
+    public void configureOption4(String value) {
+        if (options.containsKey(option4Name)) {
             options.get(option4Name).configure(value);
         }
     }
 
-        /*
-        The next set of getOption Names are to support legacy configuration files
-    */
-    public String getOption1Name(){
+    /*
+     The next set of getOption Names are to support legacy configuration files
+     */
+    @Override
+    public String getOption1Name() {
         return option1Name;
     }
-    
-    public String getOption2Name(){
+
+    @Override
+    public String getOption2Name() {
         return option2Name;
     }
-    
-    public String getOption3Name(){
+
+    @Override
+    public String getOption3Name() {
         return option3Name;
     }
 
-    public String getOption4Name(){
+    @Override
+    public String getOption4Name() {
         return option4Name;
     }
 
     /**
-    * Get a list of all the options configured against this adapter.
-    */
-    public String[] getOptions(){
+     * Get a list of all the options configured against this adapter.
+     *
+     * @return Array of option identifiers
+     */
+    @Override
+    public String[] getOptions() {
         String[] arr = options.keySet().toArray(new String[0]);
         java.util.Arrays.sort(arr);
         return arr;
-    
+
     }
 
     /**
-    * Set the value of an option
-    */
-    public void setOptionState(String option, String value){
-        if(options.containsKey(option)){
+     * Set the value of an option
+     *
+     * @param option
+     * @param value
+     */
+    @Override
+    public void setOptionState(String option, String value) {
+        if (options.containsKey(option)) {
             options.get(option).configure(value);
         }
     }
-    
+
     /**
-    *  Get the value of a specific option
-    */
-    public String getOptionState(String option){
-        if(options.containsKey(option)){
+     * Get the value of a specific option.
+     *
+     * @param option
+     * @return the option value
+     */
+    @Override
+    public String getOptionState(String option) {
+        if (options.containsKey(option)) {
             return options.get(option).getCurrent();
         }
         return null;
     }
-    
+
     /**
-    *   return a list of the various choices allowed with an option.
-    */
-    public String[] getOptionChoices(String option){
-        if(options.containsKey(option)){
+     * Return a list of the various choices allowed with an option.
+     *
+     * @param option
+     * @return list of valid values for the option
+     */
+    @Override
+    public String[] getOptionChoices(String option) {
+        if (options.containsKey(option)) {
             return options.get(option).getOptions();
         }
         return null;
     }
-    
-    public String getOptionDisplayName(String option){
-        if(options.containsKey(option)){
+
+    @Override
+    public String getOptionDisplayName(String option) {
+        if (options.containsKey(option)) {
             return options.get(option).getDisplayText();
         }
         return null;
     }
-    
-    public boolean isOptionAdvanced(String option){
-        if(options.containsKey(option)){
+
+    @Override
+    public boolean isOptionAdvanced(String option) {
+        if (options.containsKey(option)) {
             return options.get(option).isAdvanced();
         }
         return false;
     }
-    
-    protected HashMap<String, Option> options = new HashMap<String, Option>();
-    
+
+    protected HashMap<String, Option> options = new HashMap<>();
+
     static protected class Option {
-        
+
         String currentValue = null;
         String displayText;
         String[] options;
         Boolean advancedOption = true;
-        
-        public Option(String displayText, String[] options, boolean advanced){
+
+        public Option(String displayText, String[] options, boolean advanced) {
             this(displayText, options);
             this.advancedOption = advanced;
         }
-        
-        public Option(String displayText, String[] options){
+
+        public Option(String displayText, String[] options) {
             this.displayText = displayText;
             this.options = new String[options.length];
-            System.arraycopy( options, 0, this.options, 0, options.length );
+            System.arraycopy(options, 0, this.options, 0, options.length);
         }
-        
-        void configure(String value){
+
+        void configure(String value) {
             currentValue = value;
         }
-        
-        String getCurrent(){
-            if(currentValue==null) return options[0];
+
+        String getCurrent() {
+            if (currentValue == null) {
+                return options[0];
+            }
             return currentValue;
         }
-        
-        String[] getOptions(){
+
+        String[] getOptions() {
             return options;
         }
-        
-        String getDisplayText(){
+
+        String getDisplayText() {
             return displayText;
         }
-        
+
         boolean isAdvanced() {
             return advancedOption;
         }
-        
+
         boolean isDirty() {
             return (currentValue != null && !currentValue.equals(options[0]));
         }
     }
-    
+
     /**
-    * Get and set of the Manufacturer for network (TCP/IP) based
-    * connections is handled by the ConnectionConfig code in each
-    * connector.  this is here as we implement the serialdriveradpter.
-    */
-    public String getManufacturer() { return mManufacturer; }
-    public void setManufacturer(String manufacturer) { 
-        log.debug("update manufacturer from "+mManufacturer+" to "+manufacturer);
-        mManufacturer = manufacturer; 
+     * Get and set of the Manufacturer for network (TCP/IP) based connections is
+     * handled by the ConnectionConfig code in each connector. this is here as
+     * we implement the serialdriveradpter.
+     */
+    @Override
+    public String getManufacturer() {
+        return mManufacturer;
+    }
+
+    @Override
+    public void setManufacturer(String manufacturer) {
+        log.debug("update manufacturer from " + mManufacturer + " to " + manufacturer);
+        mManufacturer = manufacturer;
     }
     protected String mManufacturer = null;
-    
+
     @Override
     public boolean getDisabled() {
         if (this.getSystemConnectionMemo() != null) {
@@ -209,10 +252,11 @@ abstract public class AbstractPortController implements PortAdapter {
         }
         return this.mDisabled;
     }
-   
+
     /**
-     * Set the connection disabled or enabled. By default connections are enabled.
-     * 
+     * Set the connection disabled or enabled. By default connections are
+     * enabled.
+     *
      * If the implementing class does not use a
      * {@link jmri.jmrix.SystemConnectionMemo}, this method must be overridden.
      * Overriding methods must call <code>super.setDisabled(boolean)</code> to
@@ -231,25 +275,25 @@ abstract public class AbstractPortController implements PortAdapter {
         }
         this.mDisabled = disabled;
     }
-    
+
     protected boolean mDisabled = false;
-    private boolean loadedDisabled = false; 
+    private boolean loadedDisabled = false;
     private boolean setDisabledCalled = false;
-    
+
     protected boolean allowConnectionRecovery = false;
 
+    @Override
     abstract public void recover();
 
     protected int reconnectinterval = 1000;
     protected int retryAttempts = 10;
 
     protected static void safeSleep(long milliseconds, String s) {
-          try {
-             Thread.sleep(milliseconds);
-          }
-          catch (InterruptedException e) {
-             log.error("Sleep Exception raised during reconnection attempt" +s);
-          }
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            log.error("Sleep Exception raised during reconnection attempt" + s);
+        }
     }
 
     @Override
