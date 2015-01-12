52c4d4ef3200a4fe7d4cce2767d8701762b9acb5
Bob Jacobsen <jacobsen@mac.com>
2015-01-12 04:56:35 +0000

    un-needed variable

--- java/src/jmri/jmrit/roster/IdentifyLoco.java
+++ java/src/jmri/jmrit/roster/IdentifyLoco.java
@@ -32,7 +32,8 @@ abstract public class IdentifyLoco extends jmri.jmrit.AbstractIdentify {
 
     // steps of the identification state machine
     public boolean test1() {
-        Programmer p = InstanceManager.programmerManagerInstance().getGlobalProgrammer();
+        // insure available?
+        InstanceManager.programmerManagerInstance().getGlobalProgrammer();
         // request contents of CV 29
         statusUpdate(java.util.ResourceBundle.getBundle("jmri/jmrit/roster/JmritRosterBundle").getString("READ CV 29"));
         readCV(29);
