0f708c1dd3d41ff3f4edec4ccd94a9440cffb957
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-01-18 12:45:12 +0000

    Whitespace, formatting, and JavaDocs.

--- java/src/jmri/jmrix/SystemConnectionMemo.java
+++ java/src/jmri/jmrix/SystemConnectionMemo.java
@@ -1,93 +1,93 @@
 // SystemConnectionMemo.java
-
 package jmri.jmrix;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.Vector;
-
-import java.util.ResourceBundle;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.ResourceBundle;
+import java.util.Vector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Lightweight abstract class to denote that a system is active,
- * and provide general information.
+ * Lightweight abstract class to denote that a system is active, and provide
+ * general information.
  * <p>
- * Objects of specific subtypes are registered
- * in the instance manager to activate their
- * particular system.
+ * Objects of specific subtypes are registered in the instance manager to
+ * activate their particular system.
  *
- * @author		Bob Jacobsen  Copyright (C) 2010
- * @version             $Revision$
+ * @author	Bob Jacobsen Copyright (C) 2010
+ * @version $Revision$
  */
 abstract public class SystemConnectionMemo {
 
     protected SystemConnectionMemo(String prefix, String userName) {
         initialise();
-        if(!setSystemPrefix(prefix)){
-            for (int x = 2; x<50; x++){
-                if(setSystemPrefix(prefix+x)){
+        if (!setSystemPrefix(prefix)) {
+            for (int x = 2; x < 50; x++) {
+                if (setSystemPrefix(prefix + x)) {
                     break;
                 }
             }
         }
 
-        if(!setUserName(userName)){
-            for (int x = 2; x<50; x++){
-                if(setUserName(userName+x)){
+        if (!setUserName(userName)) {
+            for (int x = 2; x < 50; x++) {
+                if (setUserName(userName + x)) {
                     break;
                 }
             }
         }
         addToActionList();
     }
-    
+
     private static boolean initialised = false;
+
     /**
      * Provides a method to reserve System Names and prefixes at creation
      */
-    private static void initialise(){
-        if (!initialised){
+    private static void initialise() {
+        if (!initialised) {
             addUserName("Internal");
             addSystemPrefix("I");
             initialised = true;
         }
     }
-    
-    final protected static ArrayList<String> userNames = new ArrayList<String>();
-    final protected static ArrayList<String> sysPrefixes = new ArrayList<String>();
-    
-    private synchronized static boolean addUserName(String userName){      
-        if (userNames.contains(userName))
+
+    final protected static ArrayList<String> userNames = new ArrayList<>();
+    final protected static ArrayList<String> sysPrefixes = new ArrayList<>();
+
+    private synchronized static boolean addUserName(String userName) {
+        if (userNames.contains(userName)) {
             return false;
+        }
 
         userNames.add(userName);
         return true;
     }
-    
+
     //This should probably throwing an exception
-    private synchronized static boolean addSystemPrefix(String systemPrefix){
-        if (sysPrefixes.contains(systemPrefix))
+    private synchronized static boolean addSystemPrefix(String systemPrefix) {
+        if (sysPrefixes.contains(systemPrefix)) {
             return false;
+        }
         sysPrefixes.add(systemPrefix);
         return true;
     }
-    
-    private synchronized static void removeUserName(String userName){
-        if(userNames!=null){
-            if (userNames.contains(userName)){
+
+    private synchronized static void removeUserName(String userName) {
+        if (userNames != null) {
+            if (userNames.contains(userName)) {
                 int index = userNames.indexOf(userName);
                 userNames.remove(index);
             }
         }
     }
-    
-    private synchronized static void removeSystemPrefix(String systemPrefix){
-        if(sysPrefixes!=null){
-            if (sysPrefixes.contains(systemPrefix)){
+
+    private synchronized static void removeSystemPrefix(String systemPrefix) {
+        if (sysPrefixes != null) {
+            if (sysPrefixes.contains(systemPrefix)) {
                 int index = sysPrefixes.indexOf(systemPrefix);
                 sysPrefixes.remove(index);
             }
@@ -95,28 +95,32 @@ abstract public class SystemConnectionMemo {
     }
 
     /**
-     * Store in InstanceManager with 
-     * proper ID for later retrieval as a 
-     * generic system
+     * Store in InstanceManager with proper ID for later retrieval as a generic
+     * system
      */
     public void register() {
         jmri.InstanceManager.store(this, SystemConnectionMemo.class);
         notifyPropertyChangeListener("ConnectionAdded", null, null);
     }
-    
+
     /**
-     * Provides access to the system prefix string.
-     * This was previously called the "System letter"
+     * Provides access to the system prefix string. This was previously called
+     * the "System letter"
+     *
+     * @return System prefix
      */
-    public String getSystemPrefix() { return prefix; }
+    public String getSystemPrefix() {
+        return prefix;
+    }
     private String prefix;
+
     //This should probably throwing an exception
     public boolean setSystemPrefix(String systemPrefix) {
         if (systemPrefix.equals(prefix)) {
             return true;
         }
         String oldPrefix = prefix;
-        if(addSystemPrefix(systemPrefix)){
+        if (addSystemPrefix(systemPrefix)) {
             prefix = systemPrefix;
             removeSystemPrefix(oldPrefix);
             notifyPropertyChangeListener("ConnectionPrefixChanged", oldPrefix, systemPrefix);
@@ -124,19 +128,25 @@ abstract public class SystemConnectionMemo {
         }
         return false;
     }
-    
+
     /**
-     * Provides access to the system user name string.
-     * This was previously fixed at configuration time.
+     * Provides access to the system user name string. This was previously fixed
+     * at configuration time.
+     *
+     * @return User name
      */
-    public String getUserName() { return userName; }
+    public String getUserName() {
+        return userName;
+    }
     private String userName;
+
     //This should probably throwing an exception
     public boolean setUserName(String name) {
-        if (name.equals(userName))
+        if (name.equals(userName)) {
             return true;
+        }
         String oldUserName = this.userName;
-        if(addUserName(name)){
+        if (addUserName(name)) {
             this.userName = name;
             removeUserName(oldUserName);
             notifyPropertyChangeListener("ConnectionNameChanged", oldUserName, name);
@@ -144,39 +154,51 @@ abstract public class SystemConnectionMemo {
         }
         return false;
     }
-    
-    /** 
+
+    /**
      * Does this connection provide a manager of this type?
+     *
+     * @param c The class type for the manager to be provided
+     * @return true if the specified manager is provided
      */
     public boolean provides(Class<?> c) {
         return false; // nothing, by default
     }
-    
-    /** 
+
+    /**
      * Does this connection provide a manager of this type?
+     *
+     * @param <T> Type of manager to get
+     * @param T Type of manager to get
+     * @return The manager or null
      */
     public <T> T get(Class<?> T) {
         return null; // nothing, by default
     }
-    
-    public void dispose(){
+
+    public void dispose() {
         removeFromActionList();
         removeUserName(userName);
         removeSystemPrefix(prefix);
         jmri.InstanceManager.deregister(this, SystemConnectionMemo.class);
         notifyPropertyChangeListener("ConnectionRemoved", userName, null);
     }
-    
+
     private boolean mDisabled = false;
-    public boolean getDisabled() { return mDisabled; }
-    public void setDisabled(boolean disabled) { 
-        if(disabled==mDisabled)
+
+    public boolean getDisabled() {
+        return mDisabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        if (disabled == mDisabled) {
             return;
+        }
         boolean oldDisabled = mDisabled;
         mDisabled = disabled;
         notifyPropertyChangeListener("ConnectionDisabled", oldDisabled, disabled);
     }
-    
+
     public static void removePropertyChangeListener(PropertyChangeListener l) {
         if (listeners.contains(l)) {
             listeners.removeElement(l);
@@ -192,59 +214,64 @@ abstract public class SystemConnectionMemo {
 
     /**
      * Trigger the notification of all PropertyChangeListeners
+     *
+     * @param property The property name
+     * @param oldValue The property's old value
+     * @param newValue The property's new value
      */
-	protected void notifyPropertyChangeListener(String property, Object oldValue, Object newValue) {
+    protected void notifyPropertyChangeListener(String property, Object oldValue, Object newValue) {
         // make a copy of the listener vector to synchronized not needed for transmit
         Vector<PropertyChangeListener> v;
-        synchronized(this)
-            {
-                v = new Vector<PropertyChangeListener>(listeners);
-            }
+        synchronized (this) {
+            v = new Vector<PropertyChangeListener>(listeners);
+        }
         // forward to all listeners
         int cnt = v.size();
-        for (int i=0; i < cnt; i++) {
+        for (int i = 0; i < cnt; i++) {
             PropertyChangeListener client = v.elementAt(i);
             client.propertyChange(new PropertyChangeEvent(this, property, oldValue, newValue));
         }
     }
-    
+
     abstract protected ResourceBundle getActionModelResourceBundle();
-    
-    protected void addToActionList(){
+
+    protected void addToActionList() {
         apps.CreateButtonModel bm = jmri.InstanceManager.getDefault(apps.CreateButtonModel.class);
         ResourceBundle rb = getActionModelResourceBundle();
-        if (rb==null || bm==null)
+        if (rb == null || bm == null) {
             return;
+        }
         Enumeration<String> e = rb.getKeys();
         while (e.hasMoreElements()) {
             String key = e.nextElement();
             try {
                 bm.addAction(key, rb.getString(key));
             } catch (ClassNotFoundException ex) {
-                log.error("Did not find class "+key);
+                log.error("Did not find class " + key);
             }
         }
     }
-    
-    protected void removeFromActionList(){
+
+    protected void removeFromActionList() {
         apps.CreateButtonModel bm = jmri.InstanceManager.getDefault(apps.CreateButtonModel.class);
-         ResourceBundle rb = getActionModelResourceBundle();
-        if (rb==null || bm==null)
+        ResourceBundle rb = getActionModelResourceBundle();
+        if (rb == null || bm == null) {
             return;
+        }
         Enumeration<String> e = rb.getKeys();
         while (e.hasMoreElements()) {
             String key = e.nextElement();
             try {
                 bm.removeAction(key);
             } catch (ClassNotFoundException ex) {
-                log.error("Did not find class "+key);
+                log.error("Did not find class " + key);
             }
         }
     }
 
     // data members to hold contact with the property listeners
     final private static Vector<PropertyChangeListener> listeners = new Vector<PropertyChangeListener>();
-    
+
     static Logger log = LoggerFactory.getLogger(SystemConnectionMemo.class.getName());
 }
 
