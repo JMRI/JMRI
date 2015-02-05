a685be8a3b877a19da2a6b67112459f550c8c4d8
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-02-05 10:36:36 +0000

    Prevent potential NPE.

--- java/src/jmri/jmrit/symbolicprog/ProgrammerConfigPane.java
+++ java/src/jmri/jmrit/symbolicprog/ProgrammerConfigPane.java
@@ -2,6 +2,8 @@
 package jmri.jmrit.symbolicprog;
 
 import java.util.ResourceBundle;
+import javax.annotation.CheckForNull;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -28,31 +30,33 @@ public class ProgrammerConfigPane extends JPanel implements PreferencesPanel {
         JPanel p = new JPanel();
         p.setLayout(new java.awt.FlowLayout());
         p.add(new JLabel("Format:"));
-        p.add(programmerBox = new JComboBox<String>(jmri.jmrit.symbolicprog.ProgDefault.findListOfProgFiles()));
-        programmerBox.setSelectedItem(jmri.jmrit.symbolicprog.ProgDefault.getDefaultProgFile());
+        p.add(programmerBox = new JComboBox<>(ProgDefault.findListOfProgFiles()));
+        programmerBox.setSelectedItem(ProgDefault.getDefaultProgFile());
         add(p);
 
         // also create the advanced panel
         advancedPanel = new JPanel();
         advancedPanel.setLayout(new BoxLayout(advancedPanel, BoxLayout.Y_AXIS));
         advancedPanel.add(showEmptyTabs = new JCheckBox("Show empty tabs"));
-        showEmptyTabs.setSelected(jmri.jmrit.symbolicprog.tabbedframe.PaneProgFrame.getShowEmptyPanes());
+        showEmptyTabs.setSelected(PaneProgFrame.getShowEmptyPanes());
         advancedPanel.add(ShowCvNums = new JCheckBox("Show CV numbers in tool tips"));
-        ShowCvNums.setSelected(jmri.jmrit.symbolicprog.tabbedframe.PaneProgFrame.getShowCvNumbers());
+        ShowCvNums.setSelected(PaneProgFrame.getShowCvNumbers());
         this.add(advancedPanel);
-        this.add(javax.swing.Box.createVerticalGlue());
+        this.add(Box.createVerticalGlue());
     }
     JComboBox<String> programmerBox;
 
     /**
      * This constructor does nothing different than the default constructor.
      *
+     * @param include ignored
      * @deprecated since 3.9.5
      */
     public ProgrammerConfigPane(boolean include) {
         this();
     }
 
+    @CheckForNull
     public String getSelectedItem() {
         return (String) programmerBox.getSelectedItem();
     }
@@ -115,9 +119,12 @@ public class ProgrammerConfigPane extends JPanel implements PreferencesPanel {
 
     @Override
     public boolean isDirty() {
+        String programmer = this.getSelectedItem();
         return (this.getShowEmptyTabs() != PaneProgFrame.getShowEmptyPanes()
                 || this.getShowCvNums() != PaneProgFrame.getShowCvNumbers()
-                || !this.getSelectedItem().equals(ProgDefault.getDefaultProgFile()));
+                || ((programmer != null) 
+                ? !programmer.equals(ProgDefault.getDefaultProgFile())
+                : ProgDefault.getDefaultProgFile() != null));
     }
 
     @Override
