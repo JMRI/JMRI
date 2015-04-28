ed4d19b60e26abc968550b64cd4302cb9fb8b4c7
Ken Cameron <kcameron@twcny.rr.com>
2015-04-28 13:01:31 +0000

    Added diff of argument from the .csh version.

--- help/en/JHelpDev.bat
+++ help/en/JHelpDev.bat
@@ -1,3 +1,3 @@
 REM Windows batch file to launch JHelpDev with the current 
 
-java -DHOMEDIR=${PWD} -classpath ".;jhelpdev.jar;lib/jhall.jar;lib/xmlenc.jar" net.sourceforge.jhelpdev.JHelpDevFrame
+java -DHOMEDIR=${PWD} -Xmx2000m -classpath ".;jhelpdev.jar;lib/jhall.jar;lib/xmlenc.jar" net.sourceforge.jhelpdev.JHelpDevFrame
