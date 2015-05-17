a1d5817cf508021bd65e0c33b39a8266c932f7b0
Pete Cressman <pete_cressman@sbcglobal.net>
2015-05-17 00:19:12 +0000

    add help comments

--- java/src/jmri/jmrit/logix/OBlock.java
+++ java/src/jmri/jmrit/logix/OBlock.java
@@ -554,8 +554,10 @@ public class OBlock extends jmri.Block implements java.beans.PropertyChangeListe
             if (!_warrant.equals(warrant)) {
                 // check if _warrant is registered
                 if (jmri.InstanceManager.getDefault(WarrantManager.class).getBySystemName(_warrant.getSystemName()) != null) {
-                    return "cannot deAllocate. warrant \"" + _warrant.getDisplayName()
+                    String msg = "cannot deAllocate. warrant \"" + _warrant.getDisplayName()
                             + "\" owns block \"" + getDisplayName() + "\"!";
+                    log.error(msg);
+                    return msg;
                 }
                 // warrant not found, fall through and clear
             }
