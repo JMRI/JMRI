02eff63bff134f7ebd11446d03748370d91eaf5a
Matthew Harris <matthew.john.harris@gmail.com>
2015-05-19 10:15:27 +0000

    Correction to copyright symbol

--- java/src/jmri/Version.java
+++ java/src/jmri/Version.java
@@ -184,7 +184,7 @@ public class Version {
      */
     static public String getCopyright() {
         // TODO Internatonalize with Bundle.getMessage()
-        return "Copyright ï¿½ " + versionBundle.getString("jmri.copyright.year") + " JMRI Community";
+        return "Copyright © " + versionBundle.getString("jmri.copyright.year") + " JMRI Community";
     }
 
     /**
