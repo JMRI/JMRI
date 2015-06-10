a061f787d226ad4b807976afc83ff6adacbf35ee
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-06-10 12:06:28 +0000

    Make class abstract.

--- java/src/jmri/beans/ConstrainedBean.java
+++ java/src/jmri/beans/ConstrainedBean.java
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
  *
  * @author rhwood
  */
-public class ConstrainedBean extends Bean {
+public abstract class ConstrainedBean extends Bean {
 
     protected VetoableChangeSupport vetoableChangeSupport = null;
 
