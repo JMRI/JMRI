984bf52d0f10ff6f3bdd782cc88be70fadee8e10
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-07-05 11:07:15 +0000

    Add class that provides a java.util.Properties implementation that allows keys to written in an alphabetically sorted manner.

---
+++ java/src/jmri/util/JmriProperties.java
@@ -0,0 +1,30 @@
+package jmri.util;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * {@Link java.util.Properties} implementation that enumerates properties sorted
+ * by key.
+ * 
+ * See http://stackoverflow.com/a/3253071/176160
+ * 
+ * @author Randall Wood
+ */
+public class JmriProperties extends Properties {
+
+    private static final long serialVersionUID = 4901931181290330210L;
+
+    @Override
+    public Set<Object> keySet() {
+        return Collections.unmodifiableSet(new TreeSet<Object>(super.keySet()));
+    }
+
+    @Override
+    public synchronized Enumeration<Object> keys() {
+        return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+    }
+}
