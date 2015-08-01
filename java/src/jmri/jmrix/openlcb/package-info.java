d5fca9f7f518cd0204e2579de95a82c70230f41b
Bob Jacobsen <jacobsen@mac.com>
2015-08-01 23:24:18 +0000

    cleanup

---
+++ java/src/jmri/jmrix/openlcb/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Implementation of the JMRI OpenLCB hardware interface.
+ *
+ * <h2>Related Documentation</h2>
+ *
+ * For overviews, tutorials, examples, guides, and tool documentation, please
+ * see:
+ * <ul>
+ * <li><a href="http://jmri.org/">JMRI project overview page</a>
+ * <li><a href="http://jmri.org/help/en/html/doc/Technical/index.shtml">JMRI
+ * project technical info</a>
+ * </ul>
+ *
+ * <!-- Put @see and @since tags down here. -->
+ *
+ * @see jmri.managers
+ * @see jmri.implementation
+ */
+package jmri.jmrix.openlcb;
---
+++ java/src/jmri/jmrix/openlcb/swing/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Head package for Swing-based packages for JMRI's OpenLCB implementation.
+ *
+ * <h2>Related Documentation</h2>
+ *
+ * For overviews, tutorials, examples, guides, and tool documentation, please
+ * see:
+ * <ul>
+ * <li><a href="http://jmri.org/">JMRI project overview page</a>
+ * <li><a href="http://jmri.org/help/en/html/doc/Technical/index.shtml">JMRI
+ * project technical info</a>
+ * </ul>
+ *
+ * <!-- Put @see and @since tags down here. -->
+ *
+ * @see jmri.managers
+ * @see jmri.implementation
+ */
+package jmri.jmri.openlcb.swing;
