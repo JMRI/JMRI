3e1b06c264956d7a220d187ed2c3ce46ff0bb411
Bob Jacobsen <jacobsen@mac.com>
2015-08-02 02:51:38 +0000

    convert to new action form so can be used in DP3

--- java/src/jmri/jmrit/dualdecoder/DualDecoderToolAction.java
+++ java/src/jmri/jmrit/dualdecoder/DualDecoderToolAction.java
@@ -3,6 +3,12 @@ package jmri.jmrit.dualdecoder;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import javax.swing.Icon;
+import jmri.AddressedProgrammerManager;
+import jmri.GlobalProgrammerManager;
+import jmri.InstanceManager;
+import jmri.util.swing.JmriAbstractAction;
+import jmri.util.swing.WindowInterface;
 
 /**
  * Swing action to create and register a DualDecoderTool
@@ -10,21 +16,32 @@ import javax.swing.AbstractAction;
  * @author Bob Jacobsen Copyright (C) 2001
  * @version $Revision$
  */
-public class DualDecoderToolAction extends AbstractAction {
+public class DualDecoderToolAction extends JmriAbstractAction {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = -6247002513212825213L;
+    public DualDecoderToolAction(String s, WindowInterface wi) {
+        super(s, wi);
+    }
+
+    public DualDecoderToolAction(String s, Icon i, WindowInterface wi) {
+        super(s, i, wi);
+    }
 
     public DualDecoderToolAction(String s) {
         super(s);
 
         // disable ourself if programming is not possible
-        if (jmri.InstanceManager.programmerManagerInstance() == null) {
-            setEnabled(false);
+        boolean enabled = false;
+        if ((InstanceManager.getList(GlobalProgrammerManager.class) != null)
+                && (InstanceManager.getList(GlobalProgrammerManager.class).size() > 0)) {
+            enabled = true;
+        }
+        if ((InstanceManager.getList(AddressedProgrammerManager.class) != null)
+                && (InstanceManager.getList(AddressedProgrammerManager.class).size() > 0)) {
+            enabled = true;
         }
 
+        setEnabled(enabled);
+        System.out.println("enabled: "+enabled);
     }
 
     public DualDecoderToolAction() {
