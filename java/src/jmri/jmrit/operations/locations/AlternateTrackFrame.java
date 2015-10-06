c89f458f76619aff89aaa1e7e3b93a54438d084d
DanielBoudreau <daboudreau@hotmail.com>
2015-10-05 22:16:42 -0400

    Provide the option to control if cars with custom loads are always sent to spurs with schedules

--- java/src/jmri/jmrit/operations/locations/AlternateTrackFrame.java
+++ java/src/jmri/jmrit/operations/locations/AlternateTrackFrame.java
@@ -1,11 +1,13 @@
-//AlternateTrackAction.java
+//AlternateTrackFrame.java
 package jmri.jmrit.operations.locations;
 
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
+import java.text.MessageFormat;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 import jmri.jmrit.operations.OperationsFrame;
@@ -16,9 +18,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Action to launch selection of alternate track.
+ * Frame that allows user to select alternate track and options.
  *
- * @author Daniel Boudreau Copyright (C) 2011
+ * @author Daniel Boudreau Copyright (C) 2011, 2015
  * @version $Revision: 17977 $
  */
 class AlternateTrackFrame extends OperationsFrame {
@@ -30,6 +32,9 @@ class AlternateTrackFrame extends OperationsFrame {
 
     // combo boxes
     JComboBox<Track> trackBox = new JComboBox<>();
+    
+    //
+    JCheckBox forwardCars = new JCheckBox();
 
     // radio buttons
     // major buttons
@@ -38,7 +43,7 @@ class AlternateTrackFrame extends OperationsFrame {
     Track _track;
 
     public AlternateTrackFrame(TrackEditFrame tef) {
-        super();
+        super(Bundle.getMessage("AlternateTrack"));
 
         // the following code sets the frame's initial state
         getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
@@ -55,6 +60,13 @@ class AlternateTrackFrame extends OperationsFrame {
         _track.getLocation().updateComboBox(trackBox);
         trackBox.removeItem(_track);	// remove this track from consideration
         trackBox.setSelectedItem(_track.getAlternateTrack());
+        
+        JPanel pOptions = new JPanel();
+        pOptions.setLayout(new GridBagLayout());
+        pOptions.setBorder(BorderFactory.createTitledBorder(""));
+        addItem(pOptions, forwardCars, 0, 0);
+        forwardCars.setSelected(_track.isForwardCarsWithCustomLoadsEnabled());
+        forwardCars.setText( MessageFormat.format(Bundle.getMessage("ForwardCarsWithCustomLoads"), _track.getName()));
 
         JPanel pControls = new JPanel();
         pControls.add(saveButton);
@@ -63,17 +75,17 @@ class AlternateTrackFrame extends OperationsFrame {
         addButtonAction(saveButton);
 
         getContentPane().add(pAlternate);
+        getContentPane().add(pOptions);
         getContentPane().add(pControls);
-
-        setTitle(Bundle.getMessage("AlternateTrack"));
-        pack();
-        setMinimumSize(new Dimension(Control.panelWidth300, Control.panelHeight100));
-        setVisible(true);
+        
+        initMinimumSize(new Dimension(Control.panelWidth600, Control.panelHeight200));
+        
     }
 
     public void buttonActionPerformed(java.awt.event.ActionEvent ae) {
         if (ae.getSource() == saveButton) {
             _track.setAlternateTrack((Track) trackBox.getSelectedItem());
+            _track.setForwardCarsWithCustomLoadsEnabled(forwardCars.isSelected());
             OperationsXml.save();
             if (Setup.isCloseWindowOnSaveEnabled()) {
                 dispose();
@@ -81,5 +93,5 @@ class AlternateTrackFrame extends OperationsFrame {
         }
     }
 
-    static Logger log = LoggerFactory.getLogger(TrackEditFrame.class.getName());
+    static Logger log = LoggerFactory.getLogger(AlternateTrackFrame.class.getName());
 }
