ad71c171826d67fbaaee03dc3c0afc1cacd95533
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-11-13 06:06:06 -0500

    Excessive caution
    There is no code path that allows the defaultOwner to be null when
    getDefaultOwner is called, but we check anyway to be doubly sure.

--- java/src/jmri/jmrit/roster/RosterConfigManager.java
+++ java/src/jmri/jmrit/roster/RosterConfigManager.java
@@ -66,6 +66,10 @@ public class RosterConfigManager extends AbstractPreferencesProvider {
      * @return the defaultOwner
      */
     public @Nonnull String getDefaultOwner() {
+        // defaultOwner should never be null, but check anyway to ensure its not
+        if (defaultOwner == null) {
+            defaultOwner = "";
+        }
         return defaultOwner;
     }
 
