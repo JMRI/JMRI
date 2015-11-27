f76d67ba46f2d736adad201b2f9d252bf39b6dbd
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-11-27 10:09:41 -0500

    Add file missing from 433b243

--- java/src/jmri/jmrix/AbstractSimulatorConnectionConfig.java
+++ java/src/jmri/jmrix/AbstractSimulatorConnectionConfig.java
@@ -245,6 +245,7 @@ abstract public class AbstractSimulatorConnectionConfig extends AbstractConnecti
     }
 
     public String getConnectionName() {
+        if (adapter.getSystemConnectionMemo() != null) {
             return adapter.getSystemConnectionMemo().getUserName();
         } else {
             return null;
@@ -263,6 +264,7 @@ abstract public class AbstractSimulatorConnectionConfig extends AbstractConnecti
     }
 
     public void dispose() {
+        super.dispose();
         if (adapter != null) {
             adapter.dispose();
             adapter = null;
