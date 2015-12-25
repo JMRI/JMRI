cc824ea70c8e99c6072fcfd26e3e3bd9198d9f9e
Bob Jacobsen <jacobsen@mac.com>
2015-12-24 18:32:20 -0800

    New graphics test utility

--- java/test/jmri/util/SwingTestCase.java
+++ java/test/jmri/util/SwingTestCase.java
@@ -1,5 +1,13 @@
 package jmri.util;
 
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.TestHelper;
@@ -20,6 +28,31 @@ public class SwingTestCase extends JFCTestCase {
         super(s);
     }
 
+    /**
+     * Get the displayed content of a JComponent.
+     * 
+     * static so that it can in invoked outside SwingTestCases subclasses
+     *
+     * Note: this does no adjustment, e.g. pack, etc.  That should have been already been done as required.
+     * 
+     * @param component Typically a JComponent, could be a JFrame, the item to be returned
+     * @param upLeft the upper-left corner of the returned area in component's coordinates
+     * @param size dimension of returned array
+     * @return int[] array of ARGB values
+     */
+    public static int[] getDisplayedContent(java.awt.Container component, Dimension size, Point upLeft) {
+        // check pixel color (from http://stackoverflow.com/questions/13307962/how-to-get-the-color-of-a-point-in-a-jpanel )
+        BufferedImage image = new BufferedImage(size.width, size.height, BufferedImage.TYPE_4BYTE_ABGR);
+        Graphics2D g2 = image.createGraphics();
+        component.paint(g2);
+        
+        int[] retval = image.getRGB(upLeft.x, upLeft.y, size.width, size.height, null, 0, size.width);
+
+        g2.dispose();
+        return retval;
+    }
+    
+    
     protected void setUp() throws Exception {
         super.setUp();
         // Choose the test Helper
--- java/test/jmri/util/SwingTestCaseTest.java
+++ java/test/jmri/util/SwingTestCaseTest.java
@@ -1,8 +1,16 @@
 // SwingTestCaseTest.java
 package jmri.util;
 
+import java.awt.Dimension;
+import java.awt.Point;
+
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
+
 import junit.extensions.jfcunit.eventdata.MouseEventData;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
 import junit.framework.Assert;
@@ -20,9 +28,6 @@ public class SwingTestCaseTest extends SwingTestCase {
     /**
      * Simple test of creating a Swing frame with a checkbox, checking the box,
      * and seeing that the check changed its state.
-     * <p>
-     * In this case, the frame is left open after the test completes because
-     * this is the JMRI default.
      */
     public void testCheckBox() {
         // create a little GUI with a single check box
@@ -44,6 +49,99 @@ public class SwingTestCaseTest extends SwingTestCase {
 
         // test for selected
         Assert.assertTrue(testBox.isSelected());
+
+        f.dispose();
+    }
+
+    public void testGetDisplayedContentGreen() {
+    
+        JFrame f = new JFrame();
+        f.setUndecorated(true); // skip frame decoration, which can force a min size.
+
+        JLabel wIcon = new JLabel(new ImageIcon("resources/icons/greenSquare.gif")); // 13x13
+        f.add(wIcon);
+        f.pack();
+        flushAWT();
+        
+        Assert.assertEquals("icon size", new Dimension(13,13).toString(),wIcon.getSize().toString());
+ 
+        int[] val = getDisplayedContent(wIcon, wIcon.getSize(), new Point(0,0));
+        
+        Assert.assertEquals("icon arraylength", val.length, 13*13);
+        Assert.assertEquals("icon first", "0xff00ff00", 
+                String.format("0x%8s", Integer.toHexString(val[0])).replace(' ', '0'));
+        Assert.assertEquals("icon middle", "0xff00ff00", 
+                String.format("0x%8s", Integer.toHexString(val[(int)Math.floor(wIcon.getSize().height/2)*wIcon.getSize().width+(int)Math.floor(wIcon.getSize().width/2)-1]).replace(' ', '0')));
+        Assert.assertEquals("icon last", "0xff00ff00", 
+                String.format("0x%8s", Integer.toHexString(val[wIcon.getSize().height*wIcon.getSize().width-1]).replace(' ', '0')));
+
+        f.dispose();
+        
+    }
+    
+    public void testGetDisplayedContentRedTransparentBkg() {
+    
+        JFrame f = new JFrame();
+        f.setUndecorated(true); // skip frame decoration, which can force a min size.
+        
+        JLabel wIcon = new JLabel(new ImageIcon("resources/icons/redTransparentBox.gif")); // 13x13
+        wIcon.setOpaque(true);
+        wIcon.setBackground(java.awt.Color.blue);
+        
+        f.add(wIcon);
+        f.pack();
+        flushAWT();
+        Assert.assertEquals("icon size", new Dimension(13,13).toString(),wIcon.getSize().toString());
+         
+        int[] val = getDisplayedContent(wIcon, wIcon.getSize(), new Point(0,0));
+        
+        Assert.assertEquals("icon arraylength", val.length, 13*13);
+        Assert.assertEquals("icon first", "0xffff0000", 
+                String.format("0x%8s", Integer.toHexString(val[0])).replace(' ', '0'));
+        Assert.assertEquals("icon middle", "0xff0000ff", 
+                String.format("0x%8s", Integer.toHexString(val[(int)Math.floor(wIcon.getSize().height/2)*wIcon.getSize().width+(int)Math.floor(wIcon.getSize().width/2)-1])).replace(' ', '0'));
+        Assert.assertEquals("icon last", "0xffff0000", 
+                String.format("0x%8s", Integer.toHexString(val[13*13-1])).replace(' ', '0'));
+        
+        f.dispose();
+    }
+    
+    public void testGetDisplayedContentRedTransparentTransp() {
+    
+        JFrame f = new JFrame();
+        f.getContentPane().setBackground(java.awt.Color.blue);
+        f.setUndecorated(true); // skip frame decoration, which can force a min size.
+        
+        JLabel wIcon = new JLabel(new ImageIcon("resources/icons/redTransparentBox.gif")); // 13x13
+        wIcon.setOpaque(false);
+        
+        f.add(wIcon);
+        f.pack();
+        flushAWT();
+        Assert.assertEquals("icon size", new Dimension(13,13).toString(),wIcon.getSize().toString());
+         
+        int[] val = getDisplayedContent(wIcon, wIcon.getSize(), new Point(0,0));
+        
+        Assert.assertEquals("icon arraylength", val.length, 13*13);
+        Assert.assertEquals("icon first", "0xffff0000", 
+                String.format("0x%8s", Integer.toHexString(val[0])).replace(' ', '0'));
+        Assert.assertEquals("icon middle", "0x00000000", 
+                String.format("0x%8s", Integer.toHexString(val[(int)Math.floor(wIcon.getSize().height/2)*wIcon.getSize().width+(int)Math.floor(wIcon.getSize().width/2)-1])).replace(' ', '0'));
+        Assert.assertEquals("icon last", "0xffff0000", 
+                String.format("0x%8s", Integer.toHexString(val[13*13-1])).replace(' ', '0'));
+        
+        // now check that background shows through
+        // Need to find the icon location first
+        Point p = SwingUtilities.convertPoint(wIcon,0,0,f.getContentPane());
+        
+        val = getDisplayedContent(f.getContentPane(), wIcon.getSize(), p);
+        Assert.assertEquals("frame arraylength", val.length, 13*13);
+        Assert.assertEquals("frame first", "0xffff0000", 
+                String.format("0x%8s", Integer.toHexString(val[0])).replace(' ', '0'));
+        Assert.assertEquals("frame middle", "0xff0000ff", 
+                String.format("0x%8s", Integer.toHexString(val[(int)Math.floor(wIcon.getSize().height/2)*wIcon.getSize().width+(int)Math.floor(wIcon.getSize().width/2)-1])).replace(' ', '0'));
+        Assert.assertEquals("frame last", "0xffff0000", 
+                String.format("0x%8s", Integer.toHexString(val[13*13-1])).replace(' ', '0'));
 
         f.dispose();
     }
