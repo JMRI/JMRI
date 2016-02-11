b3594feb4795e8c02224c897ba51b0b793f1a7a8
Bob Jacobsen <jacobsen@mac.com>
2016-02-11 07:49:18 -0800

    test using jmri/jmrit/display/verify/AA1UPtest.xml

---
+++ java/test/jmri/jmrit/display/SignalSystemTest.java
@@ -0,0 +1,122 @@
+// SignalSystemTest.java
+package jmri.jmrit.display;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import junit.framework.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jmri.*;
+import jmri.util.JUnitUtil;
+/**
+ * Test signal system via a specific layout file
+ *
+ * This is in jmri.jmrit.display instead of jmri.implementation because the files
+ * it loads display the signals for debugging purposes; if their panels were editted
+ * out, jmri.implementation would be a better location
+ *
+ * @author Bob Jacobsen Copyright 2016
+ * @since 4.3.2
+ */
+public class SignalSystemTest extends jmri.configurexml.SchemaTestBase {
+
+    public void testLoadAA1UPtest() throws jmri.JmriException {
+        if (System.getProperty("jmri.headlesstest", "false").equals("true")) { return; }
+        
+        // load file
+        InstanceManager.configureManagerInstance()
+                .load(new java.io.File("java/test/jmri/jmrit/display/verify/AA1UPtest.xml"));
+        InstanceManager.logixManagerInstance().activateAllLogixs();
+        InstanceManager.getDefault(jmri.jmrit.display.layoutEditor.LayoutBlockManager.class).initializeLayoutBlockPaths();
+                
+        // check aspects
+        checkAspect("IF$vsm:BNSF-1996:SE-1A($0152)", "Stop");
+
+        checkAspect("IF$vsm:UP-2008:SL-3A($0170)", "Advance Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0171)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0172)", "Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0173)", "Restricting");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0174)", "Advance Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0176)", "Clear");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0178)", "Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0179)", "Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0181)", "Clear");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0183)", "Clear");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0185)", "Advance Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0186)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0230)", "Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0231)", "Advance Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0232)", "Clear");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0233)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0234)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0235)", "Restricting");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0236)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0237)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0238)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0239)", "Clear");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0240)", "Clear");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0241)", "Restricting");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0242)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0243)", "Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0244)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0245)", "Approach");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0246)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0247)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3A($0248)", "Stop");
+        checkAspect("IF$vsm:UP-2008:SL-3L2($0249)", "Approach Restricting");
+        checkAspect("IF$vsm:UP-2008:SL-3L2($0250)", "Approach");
+       
+        // clean up messages from file
+        jmri.util.JUnitAppender.assertErrorMessage("No facing block found for source mast IF$vsm:BNSF-1996:SL-2A($0100)");
+        jmri.util.JUnitAppender.clearBacklog();
+        jmri.util.JUnitAppender.verifyNoBacklog();
+        log.warn("suppressing multiple messages from AA1UPtest.xml file");
+    }
+    
+    void checkAspect(String mastName, String aspect) {
+        SignalMast mast = InstanceManager.signalMastManagerInstance().getSignalMast(mastName);
+        if (mast == null) Assert.fail("Mast "+mastName+" not found");
+        // wait present or error
+        jmri.util.JUnitUtil.waitFor(()->{return mast.getAspect().equals(aspect);},
+            "mast " + mastName + " aspect \""+aspect+"\" expected, currently showing \""+mast.getAspect()+"\"");
+    }
+        
+    // from here down is testing infrastructure
+    public SignalSystemTest(String s) {
+        super(s);
+    }
+
+    // Main entry point
+    static public void main(String[] args) {
+        String[] testCaseName = {"-noloading", SignalSystemTest.class.getName()};
+        junit.swingui.TestRunner.main(testCaseName);
+    }
+
+    // test suite from all defined tests
+    public static Test suite() {
+        TestSuite suite = new TestSuite(SignalSystemTest.class);
+        return suite;
+    }
+
+    // The minimal setup for log4J
+    protected void setUp() throws Exception {
+        super.setUp();
+        apps.tests.Log4JFixture.setUp();
+        JUnitUtil.resetInstanceManager();
+        JUnitUtil.initConfigureManager();
+        InstanceManager.store(new NamedBeanHandleManager(), NamedBeanHandleManager.class);
+        JUnitUtil.initInternalTurnoutManager();
+        JUnitUtil.initInternalLightManager();
+        JUnitUtil.initInternalSensorManager();
+        JUnitUtil.initMemoryManager();
+    }
+
+    protected void tearDown() throws Exception {
+        JUnitUtil.resetInstanceManager();
+        super.tearDown();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    static Logger log = LoggerFactory.getLogger(SignalSystemTest.class.getName());
+}
