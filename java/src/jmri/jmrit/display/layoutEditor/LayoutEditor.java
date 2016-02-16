4949d2da289390dc8ebe1c6d020393ab59d9cd74
Steve Rawlinson <steve@tagadab.com>
2016-02-16 14:03:51 +0000

    Initial experiment with keyboard shortcuts in layout editor

--- java/src/jmri/jmrit/display/layoutEditor/LayoutEditor.java
+++ java/src/jmri/jmrit/display/layoutEditor/LayoutEditor.java
@@ -52,6 +52,7 @@ import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
+import javax.swing.KeyStroke;
 import jmri.InstanceManager;
 import jmri.Memory;
 import jmri.NamedBean;
@@ -233,7 +234,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
     //private int numTurnouts = 0;
     private TrackSegment newTrack = null;
     private boolean panelChanged = false;
-    
+
     // grid size in pixels
     private int gridSize = 10;
 
@@ -797,6 +798,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
 
     void setupToolsMenu(JMenuBar menuBar) {
         JMenu toolsMenu = new JMenu(rb.getString("MenuTools"));
+        toolsMenu.setMnemonic('T');
         menuBar.add(toolsMenu);
         // scale track diagram
         JMenuItem scaleItem = new JMenuItem(rb.getString("ScaleTrackDiagram") + "...");
@@ -943,10 +945,13 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
 
     protected JMenu setupOptionMenu(JMenuBar menuBar) {
         JMenu optionMenu = new JMenu(rbx.getString("Options"));
+        optionMenu.setMnemonic('O');
         menuBar.add(optionMenu);
         // edit mode item
         editModeItem = new JCheckBoxMenuItem(rb.getString("EditMode"));
         optionMenu.add(editModeItem);
+        editModeItem.setMnemonic('E');
+        editModeItem.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_E, java.awt.Event.CTRL_MASK));
         editModeItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 setAllEditable(editModeItem.isSelected());
@@ -1032,7 +1037,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
         });
         snapToGridOnMoveItem.setSelected(snapToGridOnMove);
 
-        // specify grid square size 
+        // specify grid square size
         JMenuItem gridSizeItem = new JMenuItem(rb.getString("EditGridSize") + "...");
         optionMenu.add(gridSizeItem);
         gridSizeItem.addActionListener(new ActionListener() {
@@ -1058,7 +1063,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
                 repaint();
             }
         });
-        
+
         // Show/Hide Scroll Bars
         scrollMenu = new JMenu(rb.getString("ScrollBarsSubMenu"));
         optionMenu.add(scrollMenu);
@@ -1344,7 +1349,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
         JMenu turnoutOptionsMenu = new JMenu(rb.getString("TurnoutOptions"));
         optionMenu.add(turnoutOptionsMenu);
 
-        // circle on Turnouts 
+        // circle on Turnouts
         turnoutCirclesOnItem = new JCheckBoxMenuItem(rb.getString("TurnoutCirclesOn"));
         turnoutOptionsMenu.add(turnoutCirclesOnItem);
         turnoutCirclesOnItem.addActionListener(new ActionListener() {
@@ -1355,7 +1360,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
         });
         turnoutCirclesOnItem.setSelected(turnoutCirclesWithoutEditMode);
 
-        // select turnout circle color 
+        // select turnout circle color
         JMenu turnoutCircleColorMenu = new JMenu(rb.getString("TurnoutCircleColor"));
         turnoutCircleColorButtonGroup = new ButtonGroup();
         addTurnoutCircleColorMenuEntry(turnoutCircleColorMenu, rb.getString("UseDefaultTrackColor"), null);
@@ -1374,7 +1379,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
         addTurnoutCircleColorMenuEntry(turnoutCircleColorMenu, rb.getString("Cyan"), Color.cyan);
         turnoutOptionsMenu.add(turnoutCircleColorMenu);
 
-        // select turnout circle size 
+        // select turnout circle size
         JMenu turnoutCircleSizeMenu = new JMenu(rb.getString("TurnoutCircleSize"));
         turnoutCircleSizeButtonGroup = new ButtonGroup();
         addTurnoutCircleSizeMenuEntry(turnoutCircleSizeMenu, "1", 1);
@@ -1442,6 +1447,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
 
     private void setupZoomMenu(JMenuBar menuBar) {
         JMenu zoomMenu = new JMenu(rb.getString("MenuZoom"));
+        zoomMenu.setMnemonic('Z');
         menuBar.add(zoomMenu);
         ButtonGroup zoomButtonGroup = new ButtonGroup();
         // add zoom choices to menu
@@ -1533,6 +1539,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
 
     private void setupMarkerMenu(JMenuBar menuBar) {
         JMenu markerMenu = new JMenu(rbx.getString("MenuMarker"));
+        markerMenu.setMnemonic('M');
         menuBar.add(markerMenu);
         markerMenu.add(new AbstractAction(rbx.getString("AddLoco") + "...") {
             /**
@@ -1568,6 +1575,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
 
     private void setupDispatcherMenu(JMenuBar menuBar) {
         JMenu dispMenu = new JMenu(Bundle.getMessage("MenuDispatcher"));
+        dispMenu.setMnemonic('D');
         dispMenu.add(new JMenuItem(new jmri.jmrit.dispatcher.DispatcherAction(Bundle.getMessage("MenuItemOpen"))));
         menuBar.add(dispMenu);
         JMenuItem newTrainItem = new JMenuItem(Bundle.getMessage("MenuItemNewTrain"));
@@ -7507,12 +7515,12 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
         gridSize = newSize;
         return gridSize;
     }
-    
+
     public int getGridSize() {
         int gs = gridSize;
         return gs;
     }
-            
+
     public int getMainlineTrackWidth() {
         int wid = (int) mainlineTrackWidth;
         return wid;
@@ -9263,7 +9271,7 @@ public class LayoutEditor extends jmri.jmrit.display.panelEditor.PanelEditor imp
                     double delX = end1.getX() - end2.getX();
                     double delY = end1.getY() - end2.getY();
                     double cLength = Math.sqrt((delX * delX) + (delY * delY));
-                    // note: The preferred dimension of a dash (solid + blank space) is 
+                    // note: The preferred dimension of a dash (solid + blank space) is
                     //         5 * the track width - about 60% solid and 40% blank.
                     int nDashes = (int) (cLength / ((trackWidth) * 5.0));
                     if (nDashes < 3) {
