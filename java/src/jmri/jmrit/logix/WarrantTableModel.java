e4eb0fa2cffa720d9515c154a07bd2213930e324
Bob Jacobsen <jacobsen@mac.com>
2016-03-05 22:40:16 -0800

    Fix invalid Integer comparison.
    FindBugs RC_REF_COMPARISON: comparing Integers with == is an Object comparison, instead of intended int value comparison

--- java/src/jmri/jmrit/logix/WarrantTableModel.java
+++ java/src/jmri/jmrit/logix/WarrantTableModel.java
@@ -602,10 +602,8 @@ class WarrantTableModel extends jmri.jmrit.beantable.BeanTableDataModel // Abstr
                 if (bean.equals(_warList.get(i))) {
 
                     if (_warNX.contains(bean)
-                            && ((property.equals("runMode") && e.getNewValue() == Integer
-                                    .valueOf(Warrant.MODE_NONE)) || (property
-                                    .equals("controlChange") && e.getNewValue() == Integer
-                                    .valueOf(Warrant.ABORT)))) {
+                            && ((property.equals("runMode") && ((Integer)e.getNewValue()).intValue() == Warrant.MODE_NONE) 
+                                    || (property.equals("controlChange") && ((Integer)e.getNewValue()).intValue() == Warrant.ABORT))) {
                         fireTableRowsDeleted(i, i);
                         removeNXWarrant(bean);
                     } else {
