94816b58777bcd89b79cfa7a40daad5ecf112110
Bob Jacobsen <jacobsen@mac.com>
2016-03-16 21:36:09 -0700

    minor cleanup

--- java/src/jmri/JmriException.java
+++ java/src/jmri/JmriException.java
@@ -1,4 +1,3 @@
-// JmriException.java
 package jmri;
 
 /**
@@ -6,14 +5,8 @@ package jmri;
  * type-safety.
  *
  * @author	Bob Jacobsen Copyright (C) 2001, 2008, 2010
- * @version	$Revision$
  */
 public class JmriException extends Exception {
-
-    /**
-     *
-     */
-    private static final long serialVersionUID = 2804039803790645735L;
 
     public JmriException(String s, Throwable t) {
         super(s, t);
@@ -27,5 +20,3 @@ public class JmriException extends Exception {
     }
 
 }
-
-/* @(#)JmriException.java */
