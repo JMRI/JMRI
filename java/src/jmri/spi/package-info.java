091b34d00691475c43b2d093a99cf13fbe6e3927
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-03-19 10:07:34 -0400

    Initial documentation of the jmri.spi package.
    The SPI provides mechanisms for extending the behavior of JMRI
    applications in ways that the script engine and JmriPlugin class can
    not.

---
+++ java/src/jmri/spi/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Interfaces that can be implemented outside the JMRI.jar file to implement new
+ * behavior in JMRI.
+ *
+ * Implementing classes need to be included in the containing JAR's
+ * META-INF.services directory per the Java Service Provider standards.
+ *
+ * @see java.util.ServiceLoader
+ */
+package jmri.spi;
