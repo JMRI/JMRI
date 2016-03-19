94299244ef876c7efe29f4c5b73f14671fedaf01
Bob Jacobsen <jacobsen@mac.com>
2016-03-18 20:21:41 -0700

    fix bad logic found by FindBugs

--- java/src/jmri/jmrix/powerline/X10Sequence.java
+++ java/src/jmri/jmrix/powerline/X10Sequence.java
@@ -275,7 +275,7 @@ public class X10Sequence {
      * Translate House Value (1 to 16) to text
      */
     public static String houseValueToText(int hV) {
-        if (hV >= 1 || hV <= 16) {
+        if (hV >= 1 && hV <= 16) {
             return houseValueDecoder[hV];
         } else {
             return "??";
