bdcb4f6c5228e5735f23b346023ddcca846557c5
Bob Jacobsen <jacobsen@mac.com>
2016-03-20 11:04:29 -0700

    minor celanup

--- java/src/jmri/managers/InternalReporterManager.java
+++ java/src/jmri/managers/InternalReporterManager.java
@@ -1,4 +1,3 @@
-// InternalReporterManager.java
 package jmri.managers;
 
 import jmri.Reporter;
@@ -8,7 +7,6 @@ import jmri.implementation.AbstractReporter;
  * Implementation of the InternalReporterManager interface.
  *
  * @author	Bob Jacobsen Copyright (C) 2010
- * @version	$Revision$
  * @since 2.9.4
  */
 public class InternalReporterManager extends AbstractReporterManager {
@@ -20,10 +18,6 @@ public class InternalReporterManager extends AbstractReporterManager {
      */
     protected Reporter createNewReporter(String systemName, String userName) {
         return new AbstractReporter(systemName, userName) {
-            /**
-             *
-             */
-            private static final long serialVersionUID = -2544443870907571291L;
 
             public int getState() {
                 return state;
@@ -46,4 +40,3 @@ public class InternalReporterManager extends AbstractReporterManager {
     }
 }
 
-/* @(#)InternalReporterManager.java */
