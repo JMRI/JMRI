1139471ac290b03326d580d5e2785091dc343a2c
Bob Jacobsen <jacobsen@mac.com>
2016-03-20 11:05:02 -0700

    copied from jmri.managers

---
+++ java/src/jmri/jmrix/internal/InternalReporterManager.java
@@ -0,0 +1,40 @@
+package jmri.jmrix.internal;
+
+import jmri.Reporter;
+import jmri.implementation.AbstractReporter;
+
+/**
+ * Implementation of the InternalReporterManager interface.
+ *
+ * @author	Bob Jacobsen Copyright (C) 2010
+ * @since 2.9.4
+ */
+public class InternalReporterManager extends AbstractReporterManager {
+
+    /**
+     * Create an internal (dummy) reporter object
+     *
+     * @return new null
+     */
+    protected Reporter createNewReporter(String systemName, String userName) {
+        return new AbstractReporter(systemName, userName) {
+            public int getState() {
+                return state;
+            }
+
+            public void setState(int s) {
+                state = s;
+            }
+            int state = 0;
+        };
+    }
+
+    @Override
+    public boolean allowMultipleAdditions(String systemName) {
+        return true;
+    }
+
+    public String getSystemPrefix() {
+        return "I";
+    }
+}
