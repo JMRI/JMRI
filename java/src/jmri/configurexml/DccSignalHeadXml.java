66262d3572fc8187a8716805072695656bb9ba14
Matthew Harris <matthew.john.harris@gmail.com>
2016-03-24 13:24:14 +0100

    Update FindBugs annotations to include justification Clean-up removing no longer required serialVersionUID and SVN properties Add some @Override notations and missing JavaDoc

--- java/src/jmri/configurexml/DccSignalHeadXml.java
+++ java/src/jmri/configurexml/DccSignalHeadXml.java
@@ -8,7 +8,7 @@ package jmri.configurexml;
  * @deprecated 2.5.4
  */
 @Deprecated
-@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", justification = "duplicate name OK while deprecated, for XML config migration")
 public class DccSignalHeadXml
         extends jmri.implementation.configurexml.DccSignalHeadXml {
 }
