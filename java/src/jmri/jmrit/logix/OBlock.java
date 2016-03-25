e0195b70855be83403ce0183d87caed2cc12e546
Bob Jacobsen <jacobsen@mac.com>
2016-03-25 16:39:26 -0700

    DE_MIGHT_IGNORE not clear

--- java/src/jmri/jmrit/logix/OBlock.java
+++ java/src/jmri/jmrit/logix/OBlock.java
@@ -569,6 +569,7 @@ public class OBlock extends jmri.Block implements java.beans.PropertyChangeListe
                 removePropertyChangeListener(_warrant);
             } catch (Exception ex) {
                 // disposed warrant may throw null pointer - continue deallocation
+                log.warn("Perhaps normal? Code not clear.", e);
             }
         }
         if (_pathName != null) {
