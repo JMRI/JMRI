c1ff2d0882f06d7a548a6274936d503e27717fdd
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-04-14 21:06:29 -0400

    Add startup action to assign a script to a button.
    This new Startup action assigns a script to a button, as has been
    repeatedly requested in the user’s group.
    Note that due to limitations in the 2.9.6 schema for startup actions,
    this uses a new startup action schema.

---
+++ java/src/apps/startup/ScriptButtonModelFactory.java
@@ -0,0 +1,72 @@
+package apps.startup;
+
+import apps.StartupModel;
+import java.awt.Component;
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+import jmri.script.ScriptFileChooser;
+
+/**
+ *
+ * @author Randall Wood 2016
+ */
+public class ScriptButtonModelFactory implements StartupModelFactory {
+
+    private ScriptFileChooser chooser = null;
+    
+    public ScriptButtonModelFactory() {
+    }
+
+    @Override
+    public Class<? extends StartupModel> getModelClass() {
+        return ScriptButtonModel.class;
+    }
+
+    @Override
+    public ScriptButtonModel newModel() {
+        return new ScriptButtonModel();
+    }
+
+    @Override
+    public String getDescription() {
+        return Bundle.getMessage(this.getModelClass().getCanonicalName());
+    }
+
+    @Override
+    public String getActionText() {
+        return Bundle.getMessage("EditableStartupAction", this.getDescription()); // NOI18N
+    }
+
+    @Override
+    public void editModel(StartupModel model, Component parent) {
+        if (this.chooser == null) {
+            this.chooser = new ScriptFileChooser();
+            this.chooser.setDialogTitle(this.getDescription());
+            this.chooser.setDialogType(JFileChooser.CUSTOM_DIALOG);
+        }
+        if (this.getModelClass().isInstance(model)) {
+            ScriptButtonPanel buttonPanel = new ScriptButtonPanel(this.chooser, parent);
+            if (((ScriptButtonModel) model).getScript() != null) {
+                buttonPanel.setScript(((ScriptButtonModel) model).getScript().getPath());
+            } else {
+                buttonPanel.setScript(""); // NOI18N
+            }
+            buttonPanel.setButtonName(model.getName());
+            JOptionPane.showMessageDialog(parent,
+                    buttonPanel,
+                    this.getDescription(),
+                    JOptionPane.PLAIN_MESSAGE);
+            if (!buttonPanel.getButtonName().isEmpty()) {
+                model.setName(buttonPanel.getButtonName());
+                ((ScriptButtonModel) model).setScript(new File(buttonPanel.getScript()));
+            }
+        }
+    }
+
+    @Override
+    public void initialize() {
+        // nothing to do
+    }
+
+}
