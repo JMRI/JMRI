bb8317b274c43d09101a2f6454d645b562a54c8b
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-04-17 14:10:36 -0400

    Use profile:profile/profile.properties for preferences
    - Migrate the SRCP and Simple server preferences from
    preferences:networkServices/*.xml to profile:profile/profile.properties
    - Move the SRCP and Simple server preferences from configurexml
    packages to the server packages since the configurexml pattern was and
    is not used by these servers, and the preferences are now saved by
    package.

--- java/src/jmri/jmris/srcp/JmriSRCPServerPreferences.java
+++ java/src/jmri/jmris/srcp/JmriSRCPServerPreferences.java
@@ -1,11 +1,11 @@
-package jmri.jmris.srcp.configurexml;
-
+package jmri.jmris.srcp;
 
 public class JmriSRCPServerPreferences extends jmri.jmris.AbstractServerPreferences {
 
     public static final int DEFAULT_PORT = 2056;
     static final String XML_PREFS_ELEMENT = "SRCPServerPreferences"; // NOI18N
     static final String PORT = "port"; // NOI18N
+
     public JmriSRCPServerPreferences(String fileName) {
         super(fileName);
     }
