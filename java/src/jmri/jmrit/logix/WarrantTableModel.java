afa4e13fea5084f9dfce7041560e6e6964671eee
Paul Bender <paul.bender@acm.org>
2016-05-06 23:49:05 -0400

    SF_SWITCH_NO_DEFAULT

--- java/src/jmri/jmrit/logix/WarrantTableModel.java
+++ java/src/jmri/jmrit/logix/WarrantTableModel.java
@@ -552,6 +552,9 @@ class WarrantTableModel extends jmri.jmrit.beantable.BeanTableDataModel // Abstr
 
             }
             break;
+        default:
+           log.error("Invalid Column " + col + " requested.");
+           throw new java.lang.IllegalArgumentException("Invalid Column " + col + " requested.");
         }
         if (msg != null) {
             JOptionPane.showMessageDialog(null, msg,
