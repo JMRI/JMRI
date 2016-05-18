747b698719706dcc4bb0cabcedfcd015e89041ac
mstevetodd <mstevetodd@mstevetodd.com>
2016-05-18 14:09:01 -0400

    improve getFullyFormattedDisplayName() and some debugging

--- java/src/jmri/implementation/AbstractTurnout.java
+++ java/src/jmri/implementation/AbstractTurnout.java
@@ -49,6 +49,9 @@ public abstract class AbstractTurnout extends AbstractNamedBean implements
     public String getBeanType() {
         return Bundle.getMessage("BeanNameTurnout");
     }
+    
+    private String closedText = InstanceManager.turnoutManagerInstance().getClosedText();
+    private String thrownText = InstanceManager.turnoutManagerInstance().getThrownText();
 
     /**
      * Handle a request to change state, typically by sending a message to the
@@ -94,8 +97,8 @@ public abstract class AbstractTurnout extends AbstractNamedBean implements
      * layout and hope for the best.
      */
     public void setCommandedState(int s) {
-        log.debug("set commanded state for turnout " + getSystemName() + " to "
-                + s);
+        log.debug("set commanded state for turnout {} to {}", getFullyFormattedDisplayName(), 
+                (s==Turnout.CLOSED ? closedText : thrownText));
         newCommandedState(s);
         myOperator = getTurnoutOperator(); // MUST set myOperator before starting the thread
         if (myOperator == null) {
