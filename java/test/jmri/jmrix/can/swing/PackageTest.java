c5d4941aeba2f56ba53ed5b4cfb2ab5aac9c7a52
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-05-26 18:51:49 -0400

    Use text-based TestRunner

--- java/test/jmri/jmrix/can/swing/PackageTest.java
+++ java/test/jmri/jmrix/can/swing/PackageTest.java
@@ -23,7 +23,7 @@ public class PackageTest extends TestCase {
     static public void main(String[] args) {
         apps.tests.AllTest.initLogging();
         String[] testCaseName = {"-noloading", PackageTest.class.getName()};
-        junit.swingui.TestRunner.main(testCaseName);
+        junit.textui.TestRunner.main(testCaseName);
     }
 
     // test suite from all defined tests
