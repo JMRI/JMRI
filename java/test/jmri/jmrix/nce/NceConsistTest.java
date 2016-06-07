c38d69fc7653abd202792faf86aff73ef2da78b2
Paul Bender <paul.bender@acm.org>
2016-06-07 12:53:56 -0400

    make the NceInterfaceScaffold's sendTestMessage and sendTestReply methods public so they can be called from tests. Fix error messages caused by consist reading.

--- java/test/jmri/jmrix/nce/NceConsistTest.java
+++ java/test/jmri/jmrix/nce/NceConsistTest.java
@@ -25,53 +25,58 @@ public class NceConsistTest {
 
     @Test public void testCtor() {
         NceConsist m = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         Assert.assertNotNull(m);
     }
 
     @Test public void testCtor2() {
         // DccLocoAddress constructor test.
         NceConsist c = new NceConsist(new DccLocoAddress(3, false),memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         Assert.assertNotNull(c);
     }
 
-    @Ignore("Not quite ready yet")
-    @Test(expected=java.lang.NullPointerException.class) 
-    public void checkDisposeMethod(){
+    @Ignore("not quite ready yet")
+    @Test public void checkDisposeMethod(){
         NceConsist c =  new NceConsist(3,memo);
-        jmri.DccLocoAddress A = new jmri.DccLocoAddress(200,true);
-        jmri.DccLocoAddress B = new jmri.DccLocoAddress(250,true);
-        c.restore(A,true); // use restore here, as it does not send
-                           // any data to the command station
-        c.restore(B,false); // use restore here, as it does not send
-                           // any data to the command station
-        // before dispose, this should succeed.
-        Assert.assertTrue("Advanced Consist Contains",c.contains(A));
-        Assert.assertTrue("Advanced Consist Contains",c.contains(B));
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
+        // verify that c has been added to the traffic controller's
+        // list of listeners.
+        int listeners = nnis.numListeners();
         c.dispose();
-        // after dispose, this should fail
-        Assert.assertTrue("Advanced Consist Contains",c.contains(A));
-        Assert.assertTrue("Advanced Consist Contains",c.contains(B));
+        Assert.assertEquals("dispose check",listeners -1, nnis.numListeners());
     }
 
     @Test public void testGetConsistType(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         Assert.assertEquals("default consist type",jmri.Consist.ADVANCED_CONSIST,c.getConsistType());
     }
 
     @Test public void testSetConsistTypeAdvanced(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.ADVANCED_CONSIST);
         Assert.assertEquals("default consist type",jmri.Consist.ADVANCED_CONSIST,c.getConsistType());
     }
 
     @Test public void testSetConsistTypeCS(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.CS_CONSIST);
         jmri.util.JUnitAppender.assertErrorMessage("Consist Type Not Supported");
     }
 
     @Test public void testSetConsistTypeOther(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(255);
         // make sure an error message is generated.
         jmri.util.JUnitAppender.assertErrorMessage("Consist Type Not Supported");
@@ -79,24 +84,32 @@ public class NceConsistTest {
 
     @Test public void checkAddressAllowedGoodAdvanced(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.ADVANCED_CONSIST);
         Assert.assertTrue("AddressAllowed", c.isAddressAllowed(new jmri.DccLocoAddress(200,true)));
     }
 
     @Test public void checkAddressAllowedBadAdvanced(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.ADVANCED_CONSIST);
         Assert.assertFalse("AddressAllowed", c.isAddressAllowed(new jmri.DccLocoAddress(0,false)));
     }
 
     @Test public void checkSizeLimitAdvanced(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.ADVANCED_CONSIST);
         Assert.assertEquals("Advanced Consist Limit",6,c.sizeLimit());   
     } 
 
     @Test public void checkContainsAdvanced(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.ADVANCED_CONSIST);
         jmri.DccLocoAddress A = new jmri.DccLocoAddress(200,true);
         jmri.DccLocoAddress B = new jmri.DccLocoAddress(250,true);
@@ -116,6 +129,8 @@ public class NceConsistTest {
 
     @Test public void checkGetLocoDirectionAdvanced(){
         NceConsist c = new NceConsist(3,memo);
+        // send a reply the memory read instruction trigged by the message above.
+        nnis.sendTestReply(new NceReply(nnis,"00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00"),null);
         c.setConsistType(jmri.Consist.ADVANCED_CONSIST);
         jmri.DccLocoAddress A = new jmri.DccLocoAddress(200,true);
         jmri.DccLocoAddress B = new jmri.DccLocoAddress(250,true);
