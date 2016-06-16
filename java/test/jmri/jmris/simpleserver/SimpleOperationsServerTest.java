f25bdd5a3b24ab29b60d68a9afeb613756cc37bf
Paul Bender <paul.bender@acm.org>
2016-06-16 11:13:32 -0400

    more tests and conversions for simpleserver.

--- java/test/jmri/jmris/simpleserver/SimpleOperationsServerTest.java
+++ java/test/jmri/jmris/simpleserver/SimpleOperationsServerTest.java
@@ -1,18 +1,20 @@
 //SimpleOperationsServerTest.java
 package jmri.jmris.simpleserver;
 
-import junit.framework.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Tests for the jmri.jmris.simpleserver.SimpleOperationsServer class
  *
  * @author Paul Bender
  */
-public class SimpleOperationsServerTest extends TestCase {
+public class SimpleOperationsServerTest {
 
+    @Test
     public void testCtor() {
         java.io.DataOutputStream output = new java.io.DataOutputStream(
                 new java.io.OutputStream() {
@@ -27,6 +29,7 @@ public class SimpleOperationsServerTest extends TestCase {
         Assert.assertNotNull(a);
     }
 
+    @Test
     public void testConnectionCtor() {
         java.io.DataOutputStream output = new java.io.DataOutputStream(
                 new java.io.OutputStream() {
@@ -41,28 +44,78 @@ public class SimpleOperationsServerTest extends TestCase {
         Assert.assertNotNull(a);
     }
 
-    // from here down is testing infrastructure
-    public SimpleOperationsServerTest(String s) {
-        super(s);
+
+    // test sending a message.
+    @Test public void testSendMessage() {
+        StringBuilder sb = new StringBuilder();
+        java.io.DataOutputStream output = new java.io.DataOutputStream(
+                new java.io.OutputStream() {
+                    @Override
+                    public void write(int b) throws java.io.IOException {
+                        sb.append((char)b);
+                    }
+                });
+        java.io.DataInputStream input = new java.io.DataInputStream(System.in);
+        SimpleLightServer a = new SimpleLightServer(input, output);
+        // NOTE: this test uses reflection to test a private method.
+        java.lang.reflect.Method sendMessageMethod=null;
+        try {
+          sendMessageMethod = a.getClass().getDeclaredMethod("sendMessage", String.class);
+        } catch(java.lang.NoSuchMethodException nsm) {
+          Assert.fail("Could not find method sendMessage in SimpleLightServer class. " );
+        }
+
+        Assert.assertNotNull(sendMessageMethod);
+        sendMessageMethod.setAccessible(true);
+        try {
+           sendMessageMethod.invoke(a,"Hello World");
+           Assert.assertEquals("SendMessage Check","Hello World",sb.toString());
+        } catch (java.lang.IllegalAccessException iae) {
+           Assert.fail("Could not access method sendMessage in SimpleLightServer class");
+        } catch (java.lang.reflect.InvocationTargetException ite){
+          Throwable cause = ite.getCause();
+          Assert.fail("sendMessage executon failed reason: " + cause.getMessage());
+       }
     }
 
-    // Main entry point
-    static public void main(String[] args) {
-        String[] testCaseName = {SimpleOperationsServerTest.class.getName()};
-        junit.textui.TestRunner.main(testCaseName);
-    }
+    // test sending a message.
+    @Test public void testSendMessageWithConnection() {
+        StringBuilder sb = new StringBuilder();
+        java.io.DataOutputStream output = new java.io.DataOutputStream(
+                new java.io.OutputStream() {
+                    @Override
+                    public void write(int b) throws java.io.IOException {
+                        sb.append((char)b);
+                    }
+                });
+        jmri.jmris.JmriConnectionScaffold jcs = new jmri.jmris.JmriConnectionScaffold(output);
+        SimpleLightServer a = new SimpleLightServer(jcs);
+        // NOTE: this test uses reflection to test a private method.
+        java.lang.reflect.Method sendMessageMethod=null;
+        try {
+          sendMessageMethod = a.getClass().getDeclaredMethod("sendMessage", String.class);
+        } catch(java.lang.NoSuchMethodException nsm) {
+          Assert.fail("Could not find method sendMessage in SimpleLightServer class. " );
+        }
 
-    // test suite from all defined tests
-    public static Test suite() {
-        TestSuite suite = new TestSuite(jmri.jmris.simpleserver.SimpleOperationsServerTest.class);
-
-        return suite;
+        // override the default permissions.
+        Assert.assertNotNull(sendMessageMethod);
+        sendMessageMethod.setAccessible(true);
+        try {
+           sendMessageMethod.invoke(a,"Hello World");
+           Assert.assertEquals("SendMessage Check","Hello World",jcs.getOutput());
+        } catch (java.lang.IllegalAccessException iae) {
+           Assert.fail("Could not access method sendMessage in SimpleLightServer class");
+        } catch (java.lang.reflect.InvocationTargetException ite){
+          Throwable cause = ite.getCause();
+          Assert.fail("sendMessage executon failed reason: " + cause.getMessage());
+       }
     }
 
     // The minimal setup for log4J
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         apps.tests.Log4JFixture.setUp();
-        super.setUp();
         jmri.util.JUnitUtil.resetInstanceManager();
         jmri.util.JUnitUtil.initInternalTurnoutManager();
         jmri.util.JUnitUtil.initInternalLightManager();
@@ -70,9 +123,9 @@ public class SimpleOperationsServerTest extends TestCase {
         jmri.util.JUnitUtil.initDebugThrottleManager();
     }
 
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         jmri.util.JUnitUtil.resetInstanceManager();
-        super.tearDown();
         apps.tests.Log4JFixture.tearDown();
     }
 
