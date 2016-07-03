d49b6dc19abfbb95c167fd20216bc53e131a5a97
Bob Jacobsen <jacobsen@mac.com>
2016-07-02 20:54:30 -0700

    Restore migration (late deprecation) files removed (erroneously) during merge-to-master

---
+++ java/src/jmri/jmrit/display/configurexml/LayoutBlockManagerXml.java
@@ -0,0 +1,12 @@
+package jmri.jmrit.display.configurexml;
+
+/**
+ * Stand-in class for backward compatibility
+ *
+ * @deprecated 4.5.1 (was originally 2.9.4, but there was a problem writing files)
+ */
+@Deprecated
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS")
+public class LayoutBlockManagerXml extends
+        jmri.jmrit.display.layoutEditor.configurexml.LayoutBlockManagerXml {
+}
