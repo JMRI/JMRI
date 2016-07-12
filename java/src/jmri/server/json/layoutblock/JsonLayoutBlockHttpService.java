2b59c51647a0b7e9bf4c417a5df232a54913f0a4
SteveT <mstevetodd@mstevetodd.com>
2016-07-12 15:13:12 -0400

    minor Dispatcher-related improvements from Jay Janzen (#1652)
    * minor Dispatcher-related improvements from Jay Janzen
    * a couple more json layoutBlock elements

--- java/src/jmri/server/json/layoutblock/JsonLayoutBlockHttpService.java
+++ java/src/jmri/server/json/layoutblock/JsonLayoutBlockHttpService.java
@@ -1,11 +1,13 @@
 package jmri.server.json.layoutblock;
 
 import static jmri.server.json.JSON.BLOCK_COLOR;
-import static jmri.server.json.JSON.BLOCK_EXTRA_COLOR;
-import static jmri.server.json.JSON.BLOCK_OCCUPIED_COLOR;
 import static jmri.server.json.JSON.COMMENT;
 import static jmri.server.json.JSON.DATA;
+import static jmri.server.json.JSON.EXTRA_COLOR;
 import static jmri.server.json.JSON.NAME;
+import static jmri.server.json.JSON.OCCUPANCY_SENSOR;
+import static jmri.server.json.JSON.OCCUPIED_COLOR;
+import static jmri.server.json.JSON.OCCUPIED_SENSE;
 import static jmri.server.json.JSON.STATE;
 import static jmri.server.json.JSON.TRACK_COLOR;
 import static jmri.server.json.JSON.TYPE;
@@ -39,52 +41,43 @@ public class JsonLayoutBlockHttpService extends JsonHttpService {
         ObjectNode root = mapper.createObjectNode();
         root.put(TYPE, LAYOUTBLOCK);
         ObjectNode data = root.putObject(DATA);
-        LayoutBlock layoutblock = InstanceManager.getDefault(LayoutBlockManager.class).getLayoutBlock(name);
-        if (layoutblock == null) {
+        LayoutBlock layoutBlock = InstanceManager.getDefault(LayoutBlockManager.class).getLayoutBlock(name);
+        if (layoutBlock == null) {
             throw new JsonException(404, Bundle.getMessage(locale, "ErrorObject", LAYOUTBLOCK, name));
         }
-        data.put(NAME, layoutblock.getSystemName());
-        data.put(USERNAME, layoutblock.getUserName());
-        data.put(COMMENT, layoutblock.getComment());
-        data.put(STATE, layoutblock.getState());
-        data.put(USE_EXTRA_COLOR, layoutblock.getUseExtraColor());
-        data.put(BLOCK_COLOR, jmri.util.ColorUtil.colorToColorName(layoutblock.getBlockColor()));
-        data.put(TRACK_COLOR, jmri.util.ColorUtil.colorToColorName(layoutblock.getBlockTrackColor()));
-        data.put(BLOCK_OCCUPIED_COLOR, jmri.util.ColorUtil.colorToColorName(layoutblock.getBlockOccupiedColor()));
-        data.put(BLOCK_EXTRA_COLOR, jmri.util.ColorUtil.colorToColorName(layoutblock.getBlockExtraColor()));
+        data.put(NAME, layoutBlock.getSystemName());
+        data.put(USERNAME, layoutBlock.getUserName());
+        data.put(COMMENT, layoutBlock.getComment());
+        data.put(STATE, layoutBlock.getState());
+        data.put(USE_EXTRA_COLOR, layoutBlock.getUseExtraColor());
+        data.put(BLOCK_COLOR, jmri.util.ColorUtil.colorToColorName(layoutBlock.getBlockColor()));
+        data.put(TRACK_COLOR, jmri.util.ColorUtil.colorToColorName(layoutBlock.getBlockTrackColor()));
+        data.put(OCCUPIED_COLOR, jmri.util.ColorUtil.colorToColorName(layoutBlock.getBlockOccupiedColor()));
+        data.put(EXTRA_COLOR, jmri.util.ColorUtil.colorToColorName(layoutBlock.getBlockExtraColor()));
+        data.put(OCCUPANCY_SENSOR, layoutBlock.getOccupancySensorName());
+        data.put(OCCUPIED_SENSE, layoutBlock.getOccupiedSense());
         
         return root;
     }
 
     @Override
     public JsonNode doPost(String type, String name, JsonNode data, Locale locale) throws JsonException {
-        LayoutBlock layoutblock = InstanceManager.getDefault(LayoutBlockManager.class).getLayoutBlock(name);
-        if (layoutblock == null) {
+        LayoutBlock layoutBlock = InstanceManager.getDefault(LayoutBlockManager.class).getLayoutBlock(name);
+        if (layoutBlock == null) {
             throw new JsonException(404, Bundle.getMessage(locale, "ErrorObject", LAYOUTBLOCK, name));
         }
         if (data.path(USERNAME).isTextual()) {
-            layoutblock.setUserName(data.path(USERNAME).asText());
+            layoutBlock.setUserName(data.path(USERNAME).asText());
         }
         if (data.path(COMMENT).isTextual()) {
-            layoutblock.setComment(data.path(COMMENT).asText());
+            layoutBlock.setComment(data.path(COMMENT).asText());
         }
         //layoutBlock.state is a bogus construct, so don't expect valid results from this
         if (!data.path(STATE).isMissingNode()) {
-            layoutblock.setState(data.path(STATE).asInt());
+            layoutBlock.setState(data.path(STATE).asInt());
         }
         return this.doGet(type, name, locale);
     }
-
-//    @Override
-//    public JsonNode doPut(String type, String name, JsonNode data, Locale locale) throws JsonException {
-//        try {
-//            InstanceManager.getDefault(LayoutBlockManager.class).provideLayoutBlock(name);
-//            InstanceManager.blockManagerInstance().provideBlock(name);
-//        } catch (Exception ex) {
-//            throw new JsonException(500, Bundle.getMessage(locale, "ErrorCreatingObject", LAYOUTBLOCK, name));
-//        }
-//        return this.doPost(type, name, data, locale);
-//    }
 
     @Override
     public JsonNode doGetList(String type, Locale locale) throws JsonException {
