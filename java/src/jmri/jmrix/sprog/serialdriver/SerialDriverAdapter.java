147460d3ffb9a250c07381533f9a5944d56dd9f5
Paul Bender <paul.bender@acm.org>
2016-07-21 11:25:00 -0400

    cleanup

--- java/src/jmri/jmrix/sprog/serialdriver/SerialDriverAdapter.java
+++ java/src/jmri/jmrix/sprog/serialdriver/SerialDriverAdapter.java
@@ -216,8 +216,6 @@ public class SerialDriverAdapter extends SprogPortController implements jmri.jmr
         }
     }
 
-    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD",
-            justification = "temporary until mult-system; only set when disposed")
     @Override
     public void dispose() {
         super.dispose();
