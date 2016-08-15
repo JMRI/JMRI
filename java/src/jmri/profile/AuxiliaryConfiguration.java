b689385a12ed5f424e64429994d552d19d2f757b
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-08-15 12:03:56 -0500

    Remove meaningless annotation.

--- java/src/jmri/profile/AuxiliaryConfiguration.java
+++ java/src/jmri/profile/AuxiliaryConfiguration.java
@@ -58,7 +58,7 @@ public interface AuxiliaryConfiguration {
      *                 profile, false if the fragment is for just the current
      *                 computer.
      */
-    void putConfigurationFragment(@Nonnull Element fragment, @Nonnull boolean shared);
+    void putConfigurationFragment(@Nonnull Element fragment, boolean shared);
 
     /**
      * Remove a configuration fragment from the configuration.
