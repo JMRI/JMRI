640bf7bc676bb800d3061b329abc81d6e90a53d5
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-08-20 12:16:04 -0500

    Clarify and expand API

--- java/src/jmri/swing/JTablePersistenceManager.java
+++ java/src/jmri/swing/JTablePersistenceManager.java
@@ -25,6 +25,9 @@ import javax.swing.JTable;
  * to changes in the {@link javax.swing.table.TableColumnModel} of the table.
  * Column visibility is persisted only if the TableColumnModel is assignable
  * from {@link jmri.util.swing.XTableColumnModel}.
+ * <p>
+ * <strong>Note:</strong> A JTable with UI state being persisted must have a
+ * unique non-null name.
  *
  * @author Randall Wood Copyright (C) 2016
  */
@@ -38,25 +41,38 @@ public interface JTablePersistenceManager {
      *
      * @param table the table to persist
      * @throws IllegalArgumentException if another table instance is already
-     *                                  persisted by the same name
+     *                                  persisted by the same name or if the
+     *                                  table name is null
      */
     public void persist(@Nonnull JTable table) throws IllegalArgumentException;
 
     /**
      * Stop persisting the table. This does not clear the persistence state, but
      * merely causes the JTablePersistenceManager to stop listening to the
-     * table.
+     * table. No error is thrown if the table state was not being persisted.
      *
      * @param table the table to stop persisting
+     * @throws NullPointerException if the table name is null
      */
-    public void stopPersisting(@Nonnull JTable table);
+    public void stopPersisting(@Nonnull JTable table) throws NullPointerException;
 
     /**
-     * Forget persistent state for a table.
+     * Remove the persistent state for a table from the cache. This does not
+     * cause the JTablePersistanceManager to stop persisting the table.
      *
-     * @param table the table to forget
+     * @param table the table to clear
+     * @throws NullPointerException if the table name is null
      */
-    public void clearState(@Nonnull JTable table);
+    public void clearState(@Nonnull JTable table) throws NullPointerException;
+
+    /**
+     * Add the current state for a table to the cache. This does not cause the
+     * JTablePersistanceManager to start persisting the table.
+     *
+     * @param table the table to cache
+     * @throws NullPointerException if the table name is null
+     */
+    public void cacheState(@Nonnull JTable table) throws NullPointerException;
 
     /**
      * Pause saving persistence data to storage. If setting paused to false,
