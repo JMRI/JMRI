9d6947fa849dad033bd5c04bd6dd97c6dfbae426
Paul Bender <paul.bender@acm.org>
2016-08-24 22:46:54 -0400

    remove jmri.jmrix.loconet.LocoNetBundle and replace all references to it with references to appropriate Bundle classes.  Also, create tests for all new Bundles added.

--- java/test/jmri/jmrix/loconet/locogen/PackageTest.java
+++ java/test/jmri/jmrix/loconet/locogen/PackageTest.java
@@ -7,31 +7,11 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Tests for the jmri.jmrix.loconet.locogen.LocoGenPanel class
+ * Tests for the jmri.jmrix.loconet.locogen package
  *
  * @author Bob Jacobsen Copyright 2001, 2003
  */
 public class PackageTest extends TestCase {
-
-    public void testPacketNull() {
-        LocoGenPanel t = new LocoGenPanel();
-        LocoNetMessage m = t.createPacket("");
-        Assert.assertEquals("null pointer", null, m);
-    }
-
-    public void testPacketCreate() {
-        LocoGenPanel t = new LocoGenPanel();
-        LocoNetMessage m = t.createPacket("12 34 AB 3 19 6 B B1");
-        Assert.assertEquals("length", 8, m.getNumDataElements());
-        Assert.assertEquals("0th byte", 0x12, m.getElement(0) & 0xFF);
-        Assert.assertEquals("1st byte", 0x34, m.getElement(1) & 0xFF);
-        Assert.assertEquals("2nd byte", 0xAB, m.getElement(2) & 0xFF);
-        Assert.assertEquals("3rd byte", 0x03, m.getElement(3) & 0xFF);
-        Assert.assertEquals("4th byte", 0x19, m.getElement(4) & 0xFF);
-        Assert.assertEquals("5th byte", 0x06, m.getElement(5) & 0xFF);
-        Assert.assertEquals("6th byte", 0x0B, m.getElement(6) & 0xFF);
-        Assert.assertEquals("7th byte", 0xB1, m.getElement(7) & 0xFF);
-    }
 
     // from here down is testing infrastructure
     public PackageTest(String s) {
@@ -47,6 +27,8 @@ public class PackageTest extends TestCase {
     // test suite from all defined tests
     public static Test suite() {
         TestSuite suite = new TestSuite(PackageTest.class);
+        suite.addTest(new TestSuite(LocoGenTest.class));
+        suite.addTest(new TestSuite(BundleTest.class));
         return suite;
     }
 
