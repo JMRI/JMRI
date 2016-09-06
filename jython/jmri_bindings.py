c28683da2e68be6ded4e7c1ac263b6d273f50154
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-09-06 15:50:39 -0400

    Do not notify that jmri_bindings is not needed anymore.

--- jython/jmri_bindings.py
+++ jython/jmri_bindings.py
@@ -4,61 +4,55 @@
 # This file may be needed only if jython.exec=true in python.properties
 #
 # These should be kept consistent with those in jmri.script.JmriScriptEngineManager
-#
+# 
 
 # Default imports
 import java
 import jmri
 
 # JMRI default managers
-try:
-    turnouts
-    print "Use of jmri_bindings.py is not needed if your script already includes:"
-    print "import java"
-    print "import jmri"
-except NameError:
-    turnouts     = jmri.InstanceManager.turnoutManagerInstance()
-    sensors      = jmri.InstanceManager.sensorManagerInstance()
-    signals      = jmri.InstanceManager.signalHeadManagerInstance()
-    masts        = jmri.InstanceManager.signalMastManagerInstance()
-    lights       = jmri.InstanceManager.lightManagerInstance()
-    dcc          = jmri.InstanceManager.commandStationInstance()
-    reporters    = jmri.InstanceManager.reporterManagerInstance()
-    memories     = jmri.InstanceManager.memoryManagerInstance()
-    routes       = jmri.InstanceManager.routeManagerInstance()
-    blocks       = jmri.InstanceManager.blockManagerInstance()
-    powermanager = jmri.InstanceManager.powerManagerInstance()
-    programmers  = jmri.managers.WarningProgrammerManager(jmri.InstanceManager.programmerManagerInstance())
-    addressedProgrammers = jmri.InstanceManager.getDefault(jmri.AddressedProgrammerManager)
-    globalProgrammers = jmri.InstanceManager.getDefault(jmri.globalProgrammerManager)
-    shutdown     = jmri.InstanceManager.shutDownManagerInstance()
-    audio        = jmri.InstanceManager.audioManagerInstance()
-    layoutblocks = jmri.InstanceManager.getDefault(jmri.jmrit.display.layoutEditor.LayoutBlockManager)
-    warrants     = jmri.InstanceManager.getDefault(jmri.jmrit.logix.WarrantManager)
+turnouts     = jmri.InstanceManager.turnoutManagerInstance()
+sensors      = jmri.InstanceManager.sensorManagerInstance()
+signals      = jmri.InstanceManager.signalHeadManagerInstance()
+masts        = jmri.InstanceManager.signalMastManagerInstance()
+lights       = jmri.InstanceManager.lightManagerInstance()
+dcc          = jmri.InstanceManager.commandStationInstance()
+reporters    = jmri.InstanceManager.reporterManagerInstance()
+memories     = jmri.InstanceManager.memoryManagerInstance()
+routes       = jmri.InstanceManager.routeManagerInstance()
+blocks       = jmri.InstanceManager.blockManagerInstance()
+powermanager = jmri.InstanceManager.powerManagerInstance()
+programmers  = jmri.managers.WarningProgrammerManager(jmri.InstanceManager.programmerManagerInstance())
+addressedProgrammers = jmri.InstanceManager.getDefault(jmri.AddressedProgrammerManager)
+globalProgrammers = jmri.InstanceManager.getDefault(jmri.globalProgrammerManager)
+shutdown     = jmri.InstanceManager.shutDownManagerInstance()
+audio        = jmri.InstanceManager.audioManagerInstance()
+layoutblocks = jmri.InstanceManager.getDefault(jmri.jmrit.display.layoutEditor.LayoutBlockManager)
+warrants     = jmri.InstanceManager.getDefault(jmri.jmrit.logix.WarrantManager)
 
-    # common JMRI constants
-    import jmri.Turnout.CLOSED         as CLOSED
-    import jmri.Turnout.THROWN         as THROWN
-    import jmri.Turnout.CABLOCKOUT         as CABLOCKOUT
-    import jmri.Turnout.PUSHBUTTONLOCKOUT  as PUSHBUTTONLOCKOUT
-    import jmri.Turnout.UNLOCKED       as UNLOCKED
-    import jmri.Turnout.LOCKED         as LOCKED
+# common JMRI constants
+import jmri.Turnout.CLOSED         as CLOSED
+import jmri.Turnout.THROWN         as THROWN
+import jmri.Turnout.CABLOCKOUT         as CABLOCKOUT
+import jmri.Turnout.PUSHBUTTONLOCKOUT  as PUSHBUTTONLOCKOUT
+import jmri.Turnout.UNLOCKED       as UNLOCKED
+import jmri.Turnout.LOCKED         as LOCKED
 
-    import jmri.Sensor.ACTIVE          as ACTIVE
-    import jmri.Sensor.INACTIVE        as INACTIVE
+import jmri.Sensor.ACTIVE          as ACTIVE
+import jmri.Sensor.INACTIVE        as INACTIVE
 
-    import jmri.Light.ON               as ON
-    import jmri.Light.OFF              as OFF
+import jmri.Light.ON               as ON
+import jmri.Light.OFF              as OFF
 
-    import jmri.NamedBean.UNKNOWN      as UNKNOWN
-    import jmri.NamedBean.INCONSISTENT as INCONSISTENT
+import jmri.NamedBean.UNKNOWN      as UNKNOWN
+import jmri.NamedBean.INCONSISTENT as INCONSISTENT
 
-    import jmri.SignalHead.DARK        as DARK
-    import jmri.SignalHead.RED         as RED
-    import jmri.SignalHead.YELLOW      as YELLOW
-    import jmri.SignalHead.GREEN       as GREEN
-    import jmri.SignalHead.LUNAR       as LUNAR
-    import jmri.SignalHead.FLASHRED    as FLASHRED
-    import jmri.SignalHead.FLASHYELLOW as FLASHYELLOW
-    import jmri.SignalHead.FLASHGREEN  as FLASHGREEN
-    import jmri.SignalHead.FLASHLUNAR  as FLASHLUNAR
+import jmri.SignalHead.DARK        as DARK
+import jmri.SignalHead.RED         as RED
+import jmri.SignalHead.YELLOW      as YELLOW
+import jmri.SignalHead.GREEN       as GREEN
+import jmri.SignalHead.LUNAR       as LUNAR
+import jmri.SignalHead.FLASHRED    as FLASHRED
+import jmri.SignalHead.FLASHYELLOW as FLASHYELLOW
+import jmri.SignalHead.FLASHGREEN  as FLASHGREEN
+import jmri.SignalHead.FLASHLUNAR  as FLASHLUNAR
