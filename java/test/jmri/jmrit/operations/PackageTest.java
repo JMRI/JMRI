69c8208a1d88d051cd20ae44dcb10f7f6c866ee5
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-09-14 13:10:13 -0400

    Rename package test suites to PackageTest
    This makes it possible to exclude a smaller number of tests when
    running all tests sequentially and skipping over *Test classes that
    only run tests in other classes.

--- java/test/jmri/jmrit/operations/PackageTest.java
+++ java/test/jmri/jmrit/operations/PackageTest.java
@@ -9,22 +9,22 @@ import junit.framework.TestSuite;
  *
  * @author	Bob Coleman
  */
-public class OperationsTest extends TestCase {
+public class PackageTest extends TestCase {
 
     // from here down is testing infrastructure
-    public OperationsTest(String s) {
+    public PackageTest(String s) {
         super(s);
     }
 
     // Main entry point
     static public void main(String[] args) {
-        String[] testCaseName = {"-noloading", OperationsTest.class.getName()};
+        String[] testCaseName = {"-noloading", PackageTest.class.getName()};
         junit.textui.TestRunner.main(testCaseName);
     }
 
     // test suite from all defined tests
     public static Test suite() {
-        TestSuite suite = new TestSuite("jmri.jmrit.operations.OperationsTest"); // no tests in class itself
+        TestSuite suite = new TestSuite("jmri.jmrit.operations.PackageTest"); // no tests in class itself
         suite.addTest(jmri.jmrit.operations.setup.PackageTest.suite());
         suite.addTest(jmri.jmrit.operations.locations.PackageTest.suite()); // fixed references to Swing, 10/10/2012
         suite.addTest(jmri.jmrit.operations.rollingstock.PackageTest.suite());
