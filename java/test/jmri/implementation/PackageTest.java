4d7e2995567637190cae32eb5bb2f773b4af2c21
Egbert Broerse <dcc2@ebroerse.nl>
2016-09-15 00:04:35 +0200

    Fix name of test class

--- java/test/jmri/implementation/PackageTest.java
+++ java/test/jmri/implementation/PackageTest.java
@@ -57,7 +57,7 @@ public class PackageTest extends TestCase {
         suite.addTest(new junit.framework.JUnit4TestAdapter(NmraConsistTest.class));
         suite.addTest(new junit.framework.JUnit4TestAdapter(SE8cSignalHeadTest.class));
         suite.addTest(new junit.framework.JUnit4TestAdapter(SignalHeadSignalMastTest.class));
-        suite.addTest(new junit.framework.JUnit4TestAdapter(MatrixSignalMastTest.java));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(MatrixSignalMastTest.class));
 
         // sub-packages
         suite.addTest(jmri.implementation.swing.PackageTest.suite());
