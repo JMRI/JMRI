68f6d7aaca176f4ca0f1a6b697c2d932915d0505
Paul Bender <paul.bender@acm.org>
2016-09-17 00:20:36 -0400

    work on tests for RaspberryPiSensorManager

--- java/test/jmri/jmrix/pi/RaspberryPiSensorManagerTest.java
+++ java/test/jmri/jmrix/pi/RaspberryPiSensorManagerTest.java
@@ -11,28 +11,35 @@ import org.junit.Test;
  * </P>
  * @author Paul Bender Copyright (C) 2016
  */
-public class RaspberryPiSensorManagerTest {
+public class RaspberryPiSensorManagerTest extends jmri.managers.AbstractSensorMgrTest {
+
+    @Override
+    public String getSystemName(int i) {
+        return "PiS" + i;
+    }
+
 
    @Test
    public void ConstructorTest(){
-       RaspberryPiSensorManager m = new RaspberryPiSensorManager("Pi");
-       Assert.assertNotNull(m);
+       Assert.assertNotNull(l);
    }
 
    @Test
    public void checkPrefix(){
-       RaspberryPiSensorManager m = new RaspberryPiSensorManager("Pi");
-       Assert.assertEquals("Prefix","Pi",m.getSystemPrefix());
+       Assert.assertEquals("Prefix","Pi",l.getSystemPrefix());
    }
 
     // The minimal setup for log4J
     @Before
     public void setUp() {
-        apps.tests.Log4JFixture.setUp();
+       apps.tests.Log4JFixture.setUp();
+       jmri.util.JUnitUtil.resetInstanceManager();
+       l = new RaspberryPiSensorManager("Pi");
     }
 
     @After
     public void tearDown() {
+       jmri.util.JUnitUtil.resetInstanceManager();
         apps.tests.Log4JFixture.tearDown();
     }
 
