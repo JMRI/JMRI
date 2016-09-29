c908d244787d99f80035de8cc9a44c77b8364862
Paul Bender <paul.bender@acm.org>
2016-09-29 16:41:40 -0400

    update Z21 version of throttle tests to JUnit4

--- java/test/jmri/jmrix/roco/z21/Z21Test.java
+++ java/test/jmri/jmrix/roco/z21/Z21Test.java
@@ -32,7 +32,7 @@ public class Z21Test extends TestCase {
         suite.addTest(new TestSuite(Z21SystemConnectionMemoTest.class));
         suite.addTest(new TestSuite(Z21XPressNetTunnelTest.class));
         suite.addTest(new TestSuite(Z21XNetProgrammerTest.class));
-        suite.addTest(new TestSuite(Z21XNetThrottleManagerTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(Z21XNetThrottleManagerTest.class));
         suite.addTest(new junit.framework.JUnit4TestAdapter(Z21XNetThrottleTest.class));
         suite.addTest(new junit.framework.JUnit4TestAdapter(Z21XNetTurnoutManagerTest.class));
         suite.addTest(new TestSuite(Z21XNetTurnoutTest.class));
