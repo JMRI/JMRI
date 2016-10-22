10daaebbdc0e63f965a4bf74068d14bcc64fb0e8
Paul Bender <paul.bender@acm.org>
2016-10-22 11:03:12 -0400

    new tests.

---
+++ java/test/apps/gui3/dp3/DecoderPro3Test.java
@@ -0,0 +1,39 @@
+package apps.gui3.dp3;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import java.awt.GraphicsEnvironment;
+
+/**
+ *
+ * Description: Tests for the DecoderPro3 application.
+ *
+ * @author  Paul Bender Copyright (C) 2016
+ */
+public class DecoderPro3Test {
+
+    @Test
+    public void testCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());        
+        String[] args = {"DecoderProConfig3.xml"};
+        apps.AppsBase a = new DecoderPro3(args);
+        Assert.assertNotNull(a);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+
+}
---
+++ java/test/apps/gui3/mdi/MDITest.java
@@ -0,0 +1,40 @@
+package apps.gui3.mdi;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import java.awt.GraphicsEnvironment;
+
+
+/**
+ *
+ * Description: Tests for the MDI application.
+ *
+ * @author  Paul Bender Copyright (C) 2016
+ */
+public class MDITest {
+
+    @Test
+    public void testCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        String[] args = {"DecoderProConfig3.xml"};
+        apps.AppsBase a = new MDI(args);
+        Assert.assertNotNull(a);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+
+}
