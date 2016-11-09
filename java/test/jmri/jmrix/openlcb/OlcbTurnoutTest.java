23853bea7c69798a307df25badc2ce8f4bcbbe83
Balazs Racz <balazs.racz@gmail.com>
2016-11-09 23:49:08 +0100

    fix formatting

--- java/test/jmri/jmrix/openlcb/OlcbTurnoutTest.java
+++ java/test/jmri/jmrix/openlcb/OlcbTurnoutTest.java
@@ -22,7 +22,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
-
 /**
  * Tests for the jmri.jmrix.openlcb.OlcbTurnout class.
  *
