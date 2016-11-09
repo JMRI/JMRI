55a898a0306241ba998cc401a18788c6e40a2670
Balazs Racz <balazs.racz@gmail.com>
2016-11-09 18:53:19 +0100

    Rewrites OlcbTurnout to use the BitProducerConsumer library. This makes JMRI conformant to the OpenLCB standards by fixing numerous issues (e.g. messages created with alias = 000; never sending producer and consumer identified messages; not responding to identify events, identify producers, identify consumers messages).  An additional feature is that unless the user has set DIRECT feedback mode, then the turnout's position will be recovered by querying the layout upon startup (or panel load).  The code also allows multiple JMRI computers on the same layout to recover state from each other.

--- java/src/jmri/jmrix/openlcb/OlcbTurnout.java
+++ java/src/jmri/jmrix/openlcb/OlcbTurnout.java
@@ -1,20 +1,21 @@
 package jmri.jmrix.openlcb;
 
-import jmri.Turnout;
-import jmri.jmrix.can.CanListener;
-import jmri.jmrix.can.CanMessage;
-import jmri.jmrix.can.CanReply;
-import jmri.jmrix.can.TrafficController;
+import org.openlcb.EventID;
+import org.openlcb.OlcbInterface;
+import org.openlcb.implementations.BitProducerConsumer;
+import org.openlcb.implementations.VersionedValueListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import jmri.Turnout;
+import jmri.jmrix.can.CanMessage;
 
 /**
  * Turnout for OpenLCB connections.
  *
  * @author Bob Jacobsen Copyright (C) 2001, 2008, 2010, 2011
  */
-public class OlcbTurnout extends jmri.implementation.AbstractTurnout
-        implements CanListener {
+public class OlcbTurnout extends jmri.implementation.AbstractTurnout {
 
     /**
      *
@@ -22,23 +23,26 @@ public class OlcbTurnout extends jmri.implementation.AbstractTurnout
     private static final long serialVersionUID = -2709042631708878196L;
     OlcbAddress addrThrown;   // go to thrown state
     OlcbAddress addrClosed;   // go to closed state
+    final OlcbInterface iface;
+
+    VersionedValueListener<Boolean> turnoutListener;
+    BitProducerConsumer pc;
+
     private static final String[] validFeedbackNames = {"MONITORING", "ONESENSOR", "TWOSENSOR",
             "DIRECT"};
     private static final int[] validFeedbackModes = {MONITORING, ONESENSOR, TWOSENSOR, DIRECT};
     private static final int validFeedbackTypes = MONITORING | ONESENSOR | TWOSENSOR | DIRECT;
     private static final int defaultFeedbackType = MONITORING;
 
-    protected OlcbTurnout(String prefix, String address, TrafficController tc) {
+    protected OlcbTurnout(String prefix, String address, OlcbInterface iface) {
         super(prefix + "T" + address);
-        this.tc = tc;
+        this.iface = iface;
         this._validFeedbackNames = validFeedbackNames;
         this._validFeedbackModes = validFeedbackModes;
         this._validFeedbackTypes = validFeedbackTypes;
         this._activeFeedbackType = defaultFeedbackType;
         init(address);
     }
-
-    TrafficController tc;
 
     /**
      * Common initialization for both constructors.
@@ -54,19 +58,6 @@ public class OlcbTurnout extends jmri.implementation.AbstractTurnout
             return;
         }
         switch (v.length) {
-            case 1:
-                addrThrown = v[0];
-                // need to complement here for addr 1
-                // so address _must_ start with address + or -
-                if (address.startsWith("+")) {
-                    addrClosed = new OlcbAddress("-" + address);
-                } else if (address.startsWith("-")) {
-                    addrClosed = new OlcbAddress("+" + address);
-                } else {
-                    log.error("can't make 2nd event from systemname " + address);
-                    return;
-                }
-                break;
             case 2:
                 addrThrown = v[0];
                 addrClosed = v[1];
@@ -75,8 +66,55 @@ public class OlcbTurnout extends jmri.implementation.AbstractTurnout
                 log.error("Can't parse OpenLCB Turnout system name: " + address);
                 return;
         }
-        // connect
-        tc.addCanListener(this);
+    }
+
+    /**
+     * Helper function that will be invoked after construction once the feedback type has been
+     * set. Used specifically for preventing double initialization when loading turnouts from XML.
+     */
+    public void finishLoad() {
+        // Clear some objects first.
+        if (turnoutListener != null) turnoutListener.release();
+        if (pc != null) pc.release();
+        turnoutListener = null;
+        pc = null;
+
+        int flags = 0;
+        switch (_activeFeedbackType) {
+            case MONITORING:
+            default:
+                flags = BitProducerConsumer.IS_PRODUCER | BitProducerConsumer.IS_CONSUMER |
+                        BitProducerConsumer.LISTEN_EVENT_IDENTIFIED | BitProducerConsumer
+                        .QUERY_AT_STARTUP;
+                break;
+            case DIRECT:
+                flags = BitProducerConsumer.IS_PRODUCER;
+                break;
+
+        }
+        pc = new BitProducerConsumer(iface, new EventID(addrThrown.toString()), new EventID
+                (addrClosed.toString()), flags);
+        turnoutListener = new VersionedValueListener<Boolean>(pc.getValue()) {
+            @Override
+            public void update(Boolean value) {
+                int s = value ? THROWN : CLOSED;
+                if (_activeFeedbackType != DIRECT) {
+                    newCommandedState(s);
+                    if (_activeFeedbackType == MONITORING) {
+                        newKnownState(s);
+                    }
+                }
+            }
+        };
+    }
+
+    @Override
+    public void setFeedbackMode(int mode) throws IllegalArgumentException {
+        boolean recreate = (mode != _activeFeedbackType) && (pc != null);
+        super.setFeedbackMode(mode);
+        if (recreate) {
+            finishLoad();
+        }
     }
 
     /**
@@ -87,54 +125,29 @@ public class OlcbTurnout extends jmri.implementation.AbstractTurnout
     protected void forwardCommandChangeToLayout(int s) {
         CanMessage m;
         if (s == Turnout.THROWN) {
-            m = addrThrown.makeMessage();
-            tc.sendCanMessage(m, this);
-            // TC will not give us back the message we sent to the port, so short-circuit the
-            // monitoring feedback here
+            turnoutListener.setFromOwner(true);
             if (_activeFeedbackType == MONITORING) {
                 newKnownState(THROWN);
             }
         } else if (s == Turnout.CLOSED) {
-            m = addrClosed.makeMessage();
-            tc.sendCanMessage(m, this);
-            // TC will not give us back the message we sent to the port, so short-circuit the
-            // monitoring feedback here
+            turnoutListener.setFromOwner(false);
             if (_activeFeedbackType == MONITORING) {
-                newKnownState(CLOSED);
-            }
-        }
-    }
-
-    public void message(CanMessage f) {
-        if (addrThrown.match(f)) {
-            if (_activeFeedbackType == MONITORING) {
-                newCommandedState(THROWN);
-                newKnownState(THROWN);
-            }
-        } else if (addrClosed.match(f)) {
-            if (_activeFeedbackType == MONITORING) {
-                newCommandedState(CLOSED);
-                newKnownState(CLOSED);
-            }
-        }
-    }
-
-    public void reply(CanReply f) {
-        if (addrThrown.match(f)) {
-            if (_activeFeedbackType == MONITORING) {
-                newCommandedState(THROWN);
-                newKnownState(THROWN);
-            }
-        } else if (addrClosed.match(f)) {
-            if (_activeFeedbackType == MONITORING) {
-                newCommandedState(CLOSED);
                 newKnownState(CLOSED);
             }
         }
     }
 
     protected void turnoutPushbuttonLockout(boolean locked) {
+        // TODO: maybe we could get another pair of events in the address and use that event pair
+        // to perform a lockout change on the turnout decoder itself.
+    }
+
+    public void dispose() {
+        if (turnoutListener != null) turnoutListener.release();
+        if (pc != null) pc.release();
+        super.dispose();
     }
 
     private final static Logger log = LoggerFactory.getLogger(OlcbTurnout.class.getName());
+
 }
