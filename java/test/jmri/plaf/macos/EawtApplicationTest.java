d68c32f011771f69f37ba2e0e2b59719e061c4d7
Paul Bender <paul.bender@acm.org>
2016-11-21 23:13:45 -0500

    new tests

---
+++ java/test/jmri/plaf/macos/EawtApplicationTest.java
@@ -0,0 +1,20 @@
+package jmri.plaf.macosx;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Tests for the EawtApplication class
+ *
+ * @author Paul Bender Copyright (C) 2016
+ */
+public class EawtApplicationTest  {
+
+   @Test
+   public void testCtor(){
+      Assert.assertNotNull(new EawtApplication());
+   }
+
+}
