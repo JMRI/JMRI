9b338d6546a199d4bff3bc4ef52f3bf9b51e02ab
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-11-21 22:28:24 -0500

    Move headless protection from suite to test

---
+++ java/test/jmri/jmrix/rps/swing/PackageTest.java
@@ -0,0 +1,20 @@
+package jmri.jmrix.rps.swing;
+
+import jmri.jmrix.rps.swing.debugger.DebuggerTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Tests for the jmri.jmrix.rps.swing package.
+ *
+ * @author Bob Jacobsen Copyright 2008
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    AffineEntryPanelTest.class,
+    jmri.jmrix.rps.swing.polling.PackageTest.class,
+    DebuggerTest.class,
+    BundleTest.class
+})
+public class PackageTest {
+}
