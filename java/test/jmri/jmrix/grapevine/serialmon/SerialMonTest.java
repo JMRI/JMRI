d2150d71e6bc9dd750f8fa2e72d6669f2fac91a0
Randall Wood <randall.h.wood@alexandriasoftware.com>
2016-11-22 20:28:10 -0500

    Move headless checks from suite to test

--- java/test/jmri/jmrix/grapevine/serialmon/SerialMonTest.java
+++ java/test/jmri/jmrix/grapevine/serialmon/SerialMonTest.java
@@ -1,24 +1,23 @@
 package jmri.jmrix.grapevine.serialmon;
 
+import java.awt.GraphicsEnvironment;
 import jmri.jmrix.grapevine.SerialMessage;
 import jmri.jmrix.grapevine.SerialReply;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * Tests for the jmri.jmrix.grapevine.serialmon package.
  *
  * @author Bob Jacobsen Copyright 2003, 2007, 2008
  */
-public class SerialMonTest extends TestCase {
+public class SerialMonTest {
 
-    // from here down is testing infrastructure
-    public SerialMonTest(String s) {
-        super(s);
-    }
-
+    @Test
     public void testDisplay() throws Exception {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
         // create a SerialMonFrame
         SerialMonFrame f = new SerialMonFrame() {
             {
@@ -50,31 +49,16 @@ public class SerialMonTest extends TestCase {
         f.dispose();
     }
 
-    // Main entry point
-    static public void main(String[] args) {
-        String[] testCaseName = {SerialMonTest.class.getName()};
-        junit.textui.TestRunner.main(testCaseName);
-    }
-
-    // test suite from all defined tests
-    public static Test suite() {
-        apps.tests.AllTest.initLogging();
-        TestSuite suite = new TestSuite(SerialMonTest.class);
-        return suite;
-    }
-
-    // The minimal setup for log4J
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         apps.tests.Log4JFixture.setUp();
-
-        super.setUp();
         jmri.util.JUnitUtil.resetInstanceManager();
         jmri.util.JUnitUtil.initDefaultUserMessagePreferences();
     }
 
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         jmri.util.JUnitUtil.resetInstanceManager();
-        super.tearDown();
         apps.tests.Log4JFixture.tearDown();
     }
 }
