7dbc1979d9835b8ce00e96e97faa4ff273847f20
Paul Bender <paul.bender@acm.org>
2016-12-04 21:40:23 -0500

    JUnit4 in parent package; another test for SpeedometerAction

--- java/test/jmri/jmrit/speedometer/SpeedometerActionTest.java
+++ java/test/jmri/jmrit/speedometer/SpeedometerActionTest.java
@@ -9,6 +9,9 @@ import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 import java.awt.GraphicsEnvironment;
+import javax.swing.JFrame;
+import org.netbeans.jemmy.operators.JFrameOperator;
+
 
 /**
  * Test simple functioning of SpeedometerAction
@@ -37,6 +40,22 @@ public class SpeedometerActionTest {
         SpeedometerAction action = new SpeedometerAction("Test SpeedometerAction");
         action.makePanel(); // this should throw an IllegalArgumentException. 
     }
+
+    @Test
+    public void testActionCreateAndFire() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        SpeedometerAction action = new SpeedometerAction("Sensor Group");
+        action.actionPerformed(null);
+        // wait for frame with "Speedometer" in title, case insensitive
+        // first boolean is false for exact to allow substring to match
+        // second boolean is false to all case insensitive match
+        JFrame frame = JFrameOperator.waitJFrame("Speedometer", false, false);
+        Assert.assertNotNull(frame);
+        // verify the action provided the expected frame class
+        Assert.assertEquals(SpeedometerFrame.class.getName(), frame.getClass().getName());
+    }
+
+
 
     @Before
     public void setUp() {
