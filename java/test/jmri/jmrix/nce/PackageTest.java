ef7dddebe0bc4b4002fb84e3bc381e2edd7be0a3
Paul Bender <paul.bender@acm.org>
2016-12-11 01:02:17 -0500

    more new tests

--- java/test/jmri/jmrix/nce/PackageTest.java
+++ java/test/jmri/jmrix/nce/PackageTest.java
@@ -57,6 +57,7 @@ public class PackageTest extends TestCase {
         suite.addTest(new junit.framework.JUnit4TestAdapter(jmri.jmrix.nce.ncemon.PackageTest.class));
         suite.addTest(jmri.jmrix.nce.packetgen.NcePacketGenPanelTest.suite());
         suite.addTest(new junit.framework.JUnit4TestAdapter(NceNetworkPortControllerTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(NcePortControllerTest.class));
         return suite;
     }
 
