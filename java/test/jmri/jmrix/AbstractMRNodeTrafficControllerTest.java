68d4778657ef9e041f320730f77fc72cdac4f555
Paul Bender <paul.bender@acm.org>
2016-12-12 16:48:00 -0500

    new tests for AbstractMRTrafficController and AbstractMRNodeTrafficController.

---
+++ java/test/jmri/jmrix/AbstractMRNodeTrafficControllerTest.java
@@ -0,0 +1,63 @@
+package jmri.jmrix;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.ResourceBundle;
+import jmri.InstanceManager;
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for AbstractMRNodeTrafficController.
+ * @author Paul Bender Copyright (C) 2016
+ */
+public class AbstractMRNodeTrafficControllerTest {
+    
+    // derived classes should set the value of tc appropriately.
+    protected AbstractMRNodeTrafficController tc;
+
+    @Test
+    public void testCtor() {
+        Assert.assertNotNull(tc);
+    }
+
+
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp(); 
+        JUnitUtil.resetInstanceManager();
+        tc = new AbstractMRNodeTrafficController(){
+           @Override
+           protected void setInstance() {};
+           @Override
+           protected void forwardMessage(AbstractMRListener client, AbstractMRMessage m){
+           }
+           @Override
+           protected AbstractMRMessage pollMessage(){ return null; }
+           @Override
+           protected AbstractMRListener pollReplyHandler() { return null; }
+           @Override
+           protected AbstractMRMessage enterProgMode() { return null; }
+           @Override
+           protected AbstractMRMessage enterNormalMode() { return null; }
+           @Override
+           protected void forwardReply(AbstractMRListener client, AbstractMRReply m){}
+           @Override
+           protected AbstractMRReply newReply() { return null; }
+           @Override
+           protected boolean endOfMessage(AbstractMRReply r) {return true; }
+        };
+    }
+
+    @After
+    public void tearDown(){
+       tc = null;
+        JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown(); 
+    }
+
+}
