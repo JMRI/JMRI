420ccedf8439e09f6e3444eadc64adcabc51a582
Paul Bender <paul.bender@acm.org>
2016-12-21 10:13:10 -0500

    more tests

--- java/test/jmri/jmrit/display/layoutEditor/TransitCreationToolTest.java
+++ java/test/jmri/jmrit/display/layoutEditor/TransitCreationToolTest.java
@@ -6,6 +6,10 @@ import org.junit.Assert;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import jmri.JmriException;
+import jmri.NamedBean;
 
 /**
  * Test simple functioning of TransitCreationTool
@@ -18,6 +22,73 @@ public class TransitCreationToolTest {
     public void testCtor() {
         TransitCreationTool  t = new TransitCreationTool();
         Assert.assertNotNull("exists", t );
+    }
+
+    @Test
+    public void testGetBeans(){
+        TransitCreationTool  t = new TransitCreationTool();
+        // getBeans should return an empty list.
+        List<NamedBean> list = t.getBeans();
+        Assert.assertNotNull("list exists",list);
+        Assert.assertTrue("list empty",list.isEmpty());
+    }
+
+    @Test
+    public void testToolInUseAtStart(){
+        TransitCreationTool  t = new TransitCreationTool();
+        // tool should not be in use.
+        Assert.assertFalse("tool in use at start",t.isToolInUse());
+    }
+
+    @Test
+    public void testInUseAfterAdd(){
+        TransitCreationTool  t = new TransitCreationTool();
+        // add a new named bean to the list.
+        try {
+           t.addNamedBean(new jmri.implementation.AbstractNamedBean("sys","usr"){
+               public int getState() {
+                   return 0;
+               }
+               public void setState(int i) {
+               }
+               public String getBeanType() {
+                   return "";
+               }
+           });
+        } catch (JmriException je) {
+             Assert.fail("Unable to add new named bean");
+        }
+
+        // tool should be in use.
+        Assert.assertTrue("tool in use after add",t.isToolInUse());
+    }
+ 
+    @Test
+    public void testAddandCancel(){
+        TransitCreationTool  t = new TransitCreationTool();
+        // add a new named bean to the list.
+        try {
+           t.addNamedBean(new jmri.implementation.AbstractNamedBean("sys","usr"){
+               public int getState() {
+                   return 0;
+               }
+               public void setState(int i) {
+               }
+               public String getBeanType() {
+                   return "";
+               }
+           });
+        } catch (JmriException je) {
+             Assert.fail("Unable to add new named bean");
+        }
+
+        // tool should be in use.
+        Assert.assertTrue("tool in use after add",t.isToolInUse());
+        // clear the list
+        t.cancelTransitCreate();
+        // tool should no longer be in use.
+        Assert.assertFalse("tool in use after cancel",t.isToolInUse());
+ 
     }
 
     // from here down is testing infrastructure
