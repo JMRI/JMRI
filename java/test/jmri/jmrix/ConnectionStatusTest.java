bb58f40c20c2bf505e3e71cf9bbe305a08528a6b
Paul Bender <paul.bender@acm.org>
2016-12-27 23:31:19 -0500

    add connection name to search criteria for Connection Status.

--- java/test/jmri/jmrix/ConnectionStatusTest.java
+++ java/test/jmri/jmrix/ConnectionStatusTest.java
@@ -28,21 +28,66 @@ public class ConnectionStatusTest {
         Assert.assertNotNull("exists", cs);
     }
 
+
+    @Test
+    public void test2ParamterGetState(){
+        ConnectionStatus cs = new ConnectionStatus();
+        Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UNKNOWN,cs.getConnectionState("Foo","Bar"));
+    }
+
+    @Test
+    public void testAddAnd2ParameterGetState(){
+        ConnectionStatus cs = new ConnectionStatus();
+        cs.addConnection("Foo","Bar");
+        // set the status of the new connection so we know we are not 
+        // retreiving a new value.
+        cs.setConnectionState("Foo","Bar",ConnectionStatus.CONNECTION_UP);
+        Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UP,cs.getConnectionState("Foo","Bar"));
+    }
+
+    @Test
+    public void test2ParameterSetAndGetState(){
+        ConnectionStatus cs = new ConnectionStatus();
+        cs.setConnectionState("Foo","Bar",ConnectionStatus.CONNECTION_UP);
+        Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UP,cs.getConnectionState("Foo","Bar"));
+    }
+
+    @Test
+    public void test2ParamterIsConnectionOk(){
+        ConnectionStatus cs = new ConnectionStatus();
+        cs.setConnectionState("Foo","Bar",ConnectionStatus.CONNECTION_UP);
+        Assert.assertTrue("connection OK",cs.isConnectionOk("Foo","Bar"));
+        cs.setConnectionState("Foo","Bar",ConnectionStatus.CONNECTION_DOWN);
+        Assert.assertFalse("connection OK",cs.isConnectionOk("Foo","Bar"));
+    }
+
     @Test
     public void testGetState(){
+        // the port only version of getConnectionState is deprecated begining 
+        // with JMRI 4.7.1  This test will need to be removed when the method
+        // is removed.
         ConnectionStatus cs = new ConnectionStatus();
         Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UNKNOWN,cs.getConnectionState("Bar"));
     }
 
     @Test
     public void testAddAndGetState(){
+        // the port only version of getConnectionState is deprecated begining 
+        // with JMRI 4.7.1  This test will need to be removed when the method
+        // is removed.
         ConnectionStatus cs = new ConnectionStatus();
         cs.addConnection("Foo","Bar");
-        Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UNKNOWN,cs.getConnectionState("Bar"));
+        // set the status of the new connection so we know we are not 
+        // retreiving a new value.
+        cs.setConnectionState("Foo","Bar",ConnectionStatus.CONNECTION_UP);
+        Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UP,cs.getConnectionState("Bar"));
     }
 
     @Test
     public void testSetAndGetState(){
+        // the port only version of setConnectionState and getConnectionState 
+        // are deprecated begining with JMRI 4.7.1  This test will need to be
+        // removed when the methods are removed.
         ConnectionStatus cs = new ConnectionStatus();
         cs.setConnectionState("Bar",ConnectionStatus.CONNECTION_UP);
         Assert.assertEquals("connection status",ConnectionStatus.CONNECTION_UP,cs.getConnectionState("Bar"));
