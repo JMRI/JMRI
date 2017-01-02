4c30bf338a3e0dc4f593f7e4ecf2c00fe983be3a
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-01-02 18:31:25 -0500

    Some JavaDocs and code cleanup for NmraPacket
    Includes some new tests for NmraPacket that were generated by watching
    the outputs of NmraPacket, but have not been independently verified.

--- java/test/jmri/NmraPacketTest.java
+++ java/test/jmri/NmraPacketTest.java
@@ -7,15 +7,17 @@
  */
 package jmri;
 
+import jmri.util.JUnitAppender;
+import org.junit.After;
 import org.junit.Assert;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Before;
+import org.junit.Test;
 
-public class NmraPacketTest extends TestCase {
+public class NmraPacketTest {
 
     // output values for some of these tests were provided by Bob Scheffler
     // create an accessory decoder packet
+    @Test
     public void testAccDecoderPacket1() {
         // test fixed bits
         byte[] ba = NmraPacket.accDecoderPkt(1, 0, 0);
@@ -24,6 +26,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x71, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket2() {
         // test C bit
         byte[] ba = NmraPacket.accDecoderPkt(1, 1, 0);
@@ -32,6 +35,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x79, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket3() {
         // test D bits
         byte[] ba = NmraPacket.accDecoderPkt(1, 0, 7);
@@ -40,6 +44,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x76, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket4() {
         // test short part of address
         byte[] ba = NmraPacket.accDecoderPkt(3, 0, 0);
@@ -48,6 +53,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x73, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket5() {
         // test top part of address
         byte[] ba = NmraPacket.accDecoderPkt(128, 0, 0);
@@ -56,6 +62,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x50, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket6() {
         // "typical packet" test
         byte[] ba = NmraPacket.accDecoderPkt(33, 1, 5);
@@ -64,6 +71,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x5C, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket7() {
         // address 256
         byte[] ba = NmraPacket.accDecoderPkt(256, true);
@@ -72,6 +80,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x6F, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket8() {
         // address 257
         byte[] ba = NmraPacket.accDecoderPkt(257, true);
@@ -80,6 +89,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x68, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket9() {
         // address 512
         byte[] ba = NmraPacket.accDecoderPkt(512, true);
@@ -88,6 +98,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x5F, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket10() {
         // address 513
         byte[] ba = NmraPacket.accDecoderPkt(513, true);
@@ -96,6 +107,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x58, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket11() {
         // address 1024
         byte[] ba = NmraPacket.accDecoderPkt(1024, true);
@@ -104,6 +116,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x3F, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket12() {
         // address 1025
         byte[] ba = NmraPacket.accDecoderPkt(1025, true);
@@ -112,6 +125,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("third byte ", 0x38, ba[2] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPacket13() {
         // invalid address (0)
         int addr = 0;
@@ -128,6 +142,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertTrue("Expected exception", threw);
     }
 
+    @Test
     public void testOpsModeLong() {
         // "typical packet" test
         byte[] ba = NmraPacket.opsCvWriteByte(2065, true, 21, 75);
@@ -139,6 +154,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("sixth byte ", 0x6A, ba[5] & 0xFF);
     }
 
+    @Test
     public void testOpsModeShort() {
         // "typical packet" test
         byte[] ba = NmraPacket.opsCvWriteByte(65, false, 21, 75);
@@ -149,6 +165,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0xF2, ba[4] & 0xFF);
     }
 
+    @Test
     public void testAnalog1() {
         // "typical packet" test
         byte[] ba = NmraPacket.analogControl(60, false, 1, 00);
@@ -159,6 +176,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0x3C ^ 0x3D ^ 0x01, ba[4] & 0xFF);
     }
 
+    @Test
     public void testAnalog2() {
         // "typical packet" test
         byte[] ba = NmraPacket.analogControl(60, false, 1, 12);
@@ -169,6 +187,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0x3C ^ 0x3D ^ 0x01 ^ 0x0C, ba[4] & 0xFF);
     }
 
+    @Test
     public void testF13F20A() {
         // "typical packet" test, short address
         byte[] ba = NmraPacket.function13Through20Packet(60, false, true, false, true, false, true, false, true, false);
@@ -178,6 +197,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fourth byte ", 0x3C ^ 0xDE ^ 0x55, ba[3] & 0xFF);
     }
 
+    @Test
     public void testF13F20B() {
         // "typical packet" test, long address
         byte[] ba = NmraPacket.function13Through20Packet(2065, true, true, false, true, false, true, false, true, false);
@@ -188,6 +208,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0xC8 ^ 0x11 ^ 0xDE ^ 0x55, ba[4] & 0xFF);
     }
 
+    @Test
     public void testF21F28A() {
         // "typical packet" test, short address
         byte[] ba = NmraPacket.function21Through28Packet(60, false, true, false, true, false, true, false, true, false);
@@ -197,6 +218,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fourth byte ", 0x3C ^ 0xDF ^ 0x55, ba[3] & 0xFF);
     }
 
+    @Test
     public void testF21F28B() {
         // "typical packet" test, long address
         byte[] ba = NmraPacket.function21Through28Packet(2065, true, true, false, true, false, true, false, true, false);
@@ -207,6 +229,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0xC8 ^ 0x11 ^ 0xDF ^ 0x55, ba[4] & 0xFF);
     }
 
+    @Test
     public void testConsist1() {
         // "typical packet" test
         byte[] ba = NmraPacket.consistControl(60, false, 1, true);
@@ -216,6 +239,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fourth byte ", 0x3C ^ 0x12 ^ 0x01, ba[3] & 0xFF);
     }
 
+    @Test
     public void testConsist2() {
         // "typical packet" test
         byte[] ba = NmraPacket.consistControl(2065, true, 12, false);
@@ -226,6 +250,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0xC8 ^ 0x11 ^ 0x13 ^ 0x0C, ba[4] & 0xFF);
     }
 
+    @Test
     public void testConsist3() {
         // "typical packet" test
         byte[] ba = NmraPacket.consistControl(2065, true, 0, false);
@@ -236,108 +261,127 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("fifth byte ", 0xC8 ^ 0x11 ^ 0x13 ^ 0x00, ba[4] & 0xFF);
     }
 
+    @Test
     public void testIsAccSignalDecoderPktOK() {
         byte[] ba = NmraPacket.accSignalDecoderPkt(123, 12);
         Assert.assertTrue(NmraPacket.isAccSignalDecoderPkt(ba));
     }
 
+    @Test
     public void testIsAccSignalDecoderPktFalseConsist() {
         byte[] ba = NmraPacket.consistControl(2065, true, 0, false);
         Assert.assertFalse(NmraPacket.isAccSignalDecoderPkt(ba));
     }
 
+    @Test
     public void testIsAccSignalDecoderPktFalseFunction() {
         byte[] ba = NmraPacket.function21Through28Packet(2065, true, true, false, true, false, true, false, true, false);
         Assert.assertFalse(NmraPacket.isAccSignalDecoderPkt(ba));
     }
 
+    @Test
     public void testIsAccSignalDecoderPktFalseAnalog() {
         byte[] ba = NmraPacket.analogControl(60, false, 1, 12);
         Assert.assertFalse(NmraPacket.isAccSignalDecoderPkt(ba));
     }
 
+    @Test
     public void testIsAccSignalDecoderPktFalseOpsWrite() {
         byte[] ba = NmraPacket.opsCvWriteByte(65, false, 21, 75);
         Assert.assertFalse(NmraPacket.isAccSignalDecoderPkt(ba));
     }
 
+    @Test
     public void testIsAccSignalDecoderPktFalseAccDecoder() {
         byte[] ba = NmraPacket.accDecoderPkt(257, true);
         Assert.assertFalse(NmraPacket.isAccSignalDecoderPkt(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr1() {
         int addr = 1;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr2() {
         int addr = 2;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr4() {
         int addr = 4;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr8() {
         int addr = 8;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr16() {
         int addr = 16;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr32() {
         int addr = 32;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr64() {
         int addr = 64;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr128() {
         int addr = 128;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr256() {
         int addr = 256;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr512() {
         int addr = 512;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr1024() {
         int addr = 1024;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testGetAccSignalDecoderPktAddr2044() { // max valid value
         int addr = 2044;
         byte[] ba = NmraPacket.accSignalDecoderPkt(addr, 12);
         Assert.assertEquals(addr, NmraPacket.getAccSignalDecoderPktAddress(ba));
     }
 
+    @Test
     public void testAccDecoderPktOpsModeLegacy1() {
         int address = 12;
         int cv = 556;
@@ -353,6 +397,7 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("byte 4", data, ba[4] & 0xFF);
     }
 
+    @Test
     public void testAccDecoderPktOpsModeLegacy2() {
         int address = 13;
         int cv = 557;
@@ -368,63 +413,48 @@ public class NmraPacketTest extends TestCase {
         Assert.assertEquals("byte 4", data, ba[4] & 0xFF);
     }
 
+    @Test
     public void testExtractAddressTypeAcc() {
         byte[] ba = NmraPacket.accSignalDecoderPkt(123, 12);
         Assert.assertEquals("Accessory", NmraPacket.DccAddressType.ACCESSORY_ADDRESS, NmraPacket.extractAddressType(ba));
     }
 
+    @Test
     public void testExtractAddressTypeShort() {
         byte[] bs = NmraPacket.function13Through20Packet(60, false, true, false, true, false, true, false, true, false);
         Assert.assertEquals("Short Loco", NmraPacket.DccAddressType.LOCO_SHORT_ADDRESS, NmraPacket.extractAddressType(bs));
     }
 
+    @Test
     public void testExtractAddressTypeLong() {
         byte[] bl = NmraPacket.function13Through20Packet(2060, true, true, false, true, false, true, false, true, false);
         Assert.assertEquals("Long Loco", NmraPacket.DccAddressType.LOCO_LONG_ADDRESS, NmraPacket.extractAddressType(bl));
     }
 
+    @Test
     public void testExtractAddressNumberAcc() {
         byte[] ba = NmraPacket.accSignalDecoderPkt(123, 12);
         NmraPacket.extractAddressNumber(ba);
         jmri.util.JUnitAppender.assertWarnMessage("extractAddressNumber can't handle ACCESSORY_ADDRESS in 9F 75 0C E6 ");
     }
 
+    @Test
     public void testExtractAddressNumberShort() {
         byte[] bs = NmraPacket.function13Through20Packet(60, false, true, false, true, false, true, false, true, false);
         Assert.assertEquals("Short Loco", 60, NmraPacket.extractAddressNumber(bs));
     }
 
+    @Test
     public void testExtractAddressNumberLong() {
         byte[] bl = NmraPacket.function13Through20Packet(2060, true, true, false, true, false, true, false, true, false);
         Assert.assertEquals("Long Loco", 2060, NmraPacket.extractAddressNumber(bl));
     }
 
-    // from here down is testing infrastructure
-    public NmraPacketTest(String s) {
-        super(s);
     }
 
-    // Main entry point
-    static public void main(String[] args) {
-        String[] testCaseName = {NmraPacketTest.class.getName()};
-        junit.textui.TestRunner.main(testCaseName);
-    }
-
-    // test suite from all defined tests
-    public static Test suite() {
-        apps.tests.AllTest.initLogging();
-        TestSuite suite = new TestSuite(NmraPacketTest.class);
-        return suite;
-    }
-
-    // The minimal setup for log4J
-    protected void setUp() throws Exception {
         apps.tests.Log4JFixture.setUp();
-        super.setUp();
     }
 
-    protected void tearDown() throws Exception {
-        super.tearDown();
         apps.tests.Log4JFixture.tearDown();
     }
 
