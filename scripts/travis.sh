a576a6b0d8a8df04a232a5a393df3fea5d99cb38
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-01-03 19:22:29 -0500

    Keep travis-specific commands out of the build script

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -9,10 +9,10 @@ export MAVEN_OPTS=-Xmx1536m
 if [[ "$HEADLESS" == "true" ]] ; then
     # run FindBugs only on headless, failing build if bugs are found
     # FindBugs configuration is in pom.xml
-    travis_wait 20 mvn test -U --batch-mode -DskipTests=true -Djava.awt.headless=${HEADLESS} findbugs:check
+    mvn test -U --batch-mode -DskipTests=true -Djava.awt.headless=${HEADLESS} findbugs:check
     # run headless tests
-    travis_wait 30 mvn test -U -P travis --batch-mode -Dant.jvm.args="-Djava.awt.headless=${HEADLESS}" -Djava.awt.headless=${HEADLESS}
+    mvn test -U -P travis --batch-mode -Dant.jvm.args="-Djava.awt.headless=${HEADLESS}" -Djava.awt.headless=${HEADLESS}
 else
     # run full GUI test suite and fail on coverage issues
-    travis_wait 30 mvn test -U -P travis-coverage --batch-mode -Dant.jvm.args="-Djava.awt.headless=${HEADLESS}" -Djava.awt.headless=${HEADLESS}
+    mvn test -U -P travis-coverage --batch-mode -Dant.jvm.args="-Djava.awt.headless=${HEADLESS}" -Djava.awt.headless=${HEADLESS}
 fi
