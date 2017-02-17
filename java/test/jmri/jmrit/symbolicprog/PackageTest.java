b901c1abf8b98dfc3404b188e6156e614c1420ad
Paul Bender <paul.bender@acm.org>
2017-02-17 15:27:11 -0500

    more new constructor tests.  fix missing resource bundle entries

--- java/test/jmri/jmrit/symbolicprog/PackageTest.java
+++ java/test/jmri/jmrit/symbolicprog/PackageTest.java
@@ -61,6 +61,19 @@ public class PackageTest extends TestCase {
         suite.addTest(new junit.framework.JUnit4TestAdapter(jmri.jmrit.symbolicprog.autospeed.PackageTest.class));
         suite.addTest(new junit.framework.JUnit4TestAdapter(jmri.jmrit.symbolicprog.symbolicframe.PackageTest.class));
         suite.addTest(new junit.framework.JUnit4TestAdapter(QualifierCombinerTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(ConstantValueTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(IndexedEnumVariableValueTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(IndexedVariableValueTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(LocoSelPaneTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(NameFileTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(NewLocoSelPaneTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(ProgDefaultTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(ProgrammerConfigManagerTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(ProgrammerConfigPaneTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(SpeedTableVarValueTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(ValueEditorTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(ValueRendererTest.class));
+
         return suite;
     }
 
