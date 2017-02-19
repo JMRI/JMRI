199c6a96d5a69e8262ef12f69127c2634380f9ac
Paul Bender <paul.bender@acm.org>
2017-02-19 16:18:06 -0500

    missing headless checks

--- java/test/jmri/jmrit/lcdclock/LcdClockFrameTest.java
+++ java/test/jmri/jmrit/lcdclock/LcdClockFrameTest.java
@@ -2,11 +2,13 @@ package jmri.jmrit.lcdclock;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.awt.GraphicsEnvironment;
 
 /**
  *
@@ -16,6 +18,7 @@ public class LcdClockFrameTest {
 
     @Test
     public void testCTor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
         LcdClockFrame t = new LcdClockFrame();
         Assert.assertNotNull("exists",t);
     }
