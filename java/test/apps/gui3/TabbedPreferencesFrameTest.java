d186fea458faebd6eafc13442e0d1b4525816902
Paul Bender <paul.bender@acm.org>
2017-02-23 14:47:13 -0500

    include graphics check

--- java/test/apps/gui3/TabbedPreferencesFrameTest.java
+++ java/test/apps/gui3/TabbedPreferencesFrameTest.java
@@ -2,11 +2,13 @@ package apps.gui3;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.awt.GraphicsEnvironment;
 
 /**
  *
@@ -16,6 +18,7 @@ public class TabbedPreferencesFrameTest {
 
     @Test
     public void testCTor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
         TabbedPreferencesFrame t = new TabbedPreferencesFrame();
         Assert.assertNotNull("exists",t);
     }
