4d78d3d12936beab7edfb1e4c0f184a18629202a
Bob Jacobsen <jacobsen@mac.com>
2017-02-23 21:53:00 -0800

    add check for headless

--- jython/test/JButtonActionExampleTest.py
+++ jython/test/JButtonActionExampleTest.py
@@ -1,7 +1,10 @@
 # test JButtonActionExample
 #  Just starts one up to make sure of syntax, etc.
 
-execfile("jython/JButtonActionExample.py")
-
-# close the window
-f.dispose()
+import java
+if (not java.awt.GraphicsEnvironment.isHeadless()) : 
+    
+    execfile("jython/JButtonActionExample.py")
+    
+    # close the window
+    f.dispose()
