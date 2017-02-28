e305ed5313ed3d80aefe5eeec3d6e6a10ce9055d
Paul Bender <paul.bender@acm.org>
2017-02-28 10:19:59 -0500

    more srcp server tests

--- java/test/jmri/jmris/srcp/SRCPTest.java
+++ java/test/jmri/jmris/srcp/SRCPTest.java
@@ -19,7 +19,9 @@ import org.junit.runners.Suite;
         JmriSRCPThrottleServerTest.class,
 	JmriSRCPServerMenuTest.class,
 	JmriSRCPServerPreferencesPanelTest.class,
-	JmriSRCPServerPreferencesTest.class
+	JmriSRCPServerPreferencesTest.class,
+	srcp/JmriSRCPServiceHandlerTest.class
+	srcp/TimeStampedOutputTest.class
 })
 
 /**
