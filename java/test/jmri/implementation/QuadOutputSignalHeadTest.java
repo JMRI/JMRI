0316c984d6ed65213a8a67e0f91f9a604b5aa802
Paul Bender <paul.bender@acm.org>
2017-03-01 13:29:01 -0500

    new and improved tests

--- java/test/jmri/implementation/QuadOutputSignalHeadTest.java
+++ java/test/jmri/implementation/QuadOutputSignalHeadTest.java
@@ -16,6 +16,16 @@ public class QuadOutputSignalHeadTest {
 
     @Test
     public void testCTor() {
+        Turnout it = (InstanceManager.getDefault(TurnoutManager.class)).proivdeTurnout("IT1");
+        NamedBeanHandle green = new NamedBeanHandle("green handle",it);
+        Turnout it2 = (InstanceManager.getDefault(TurnoutManager.class)).proivdeTurnout("IT3");
+        NamedBeanHandle red = new NamedBeanHandle("red handle",it2);
+        Turnout it3 = (InstanceManager.getDefault(TurnoutManager.class)).proivdeTurnout("IT2");
+        NamedBeanHandle yellow = new NamedBeanHandle("yellpw handle",it3);
+        Turnout it4 = (InstanceManager.getDefault(TurnoutManager.class)).proivdeTurnout("IT4");
+        NamedBeanHandle lunar = new NamedBeanHandle("lunar handle",it4);
+
+
         QuadOutputSignalHead t = new QuadOutputSignalHead();
         Assert.assertNotNull("exists",t);
     }
@@ -25,6 +35,7 @@ public class QuadOutputSignalHeadTest {
     public void setUp() {
         apps.tests.Log4JFixture.setUp();
         jmri.util.JUnitUtil.resetInstanceManager();
+        jmri.util.JUnitUtil.initInternalTurnoutManager();
     }
 
     @After
