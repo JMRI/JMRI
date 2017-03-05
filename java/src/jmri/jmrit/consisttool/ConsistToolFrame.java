2b077a21db9236c2b0c304e5f99df626c1926422
Paul Bender <paul.bender@acm.org>
2017-03-04 23:41:05 -0500

    make sure that when the user types a new locotive address, the roster entry is cleared.

--- java/src/jmri/jmrit/consisttool/ConsistToolFrame.java
+++ java/src/jmri/jmrit/consisttool/ConsistToolFrame.java
@@ -3,6 +3,8 @@ package jmri.jmrit.consisttool;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
@@ -181,6 +183,22 @@ public class ConsistToolFrame extends jmri.util.JmriJFrame implements jmri.Consi
 
         locoSelector.setToolTipText(Bundle.getMessage("LocoSelectorToolTip"));
         locoSelector.setVisible(true);
+
+        locoSelector.addKeyListener(new KeyListener(){
+            @Override
+            public void keyPressed(KeyEvent e){
+               // if we start typing, set the selected index of the locoRosterbox to nothing.
+               locoRosterBox.setSelectedIndex(0);
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
 
         locoRosterBox = new GlobalRosterEntryComboBox();
         locoRosterBox.setNonSelectedItem("");
