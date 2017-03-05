75bc4486fd4e431499567061628064032d27e4c7
Paul Bender <paul.bender@acm.org>
2017-03-05 10:37:48 -0500

    a few new XPressNet related tests

---
+++ java/test/jmri/jmrix/lenz/XNetFeedbackMessageCacheTest.java
@@ -0,0 +1,38 @@
+package jmri.jmrix.lenz;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class XNetFeedbackMessageCacheTest {
+
+    @Test
+    public void testCTor() {
+        XNetFeedbackMessageCache t = new XNetFeedbackMessageCache();
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(XNetFeedbackMessageCacheTest.class.getName());
+
+}
