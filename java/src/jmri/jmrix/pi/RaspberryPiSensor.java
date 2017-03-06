64930ad9cdd8bc6bdc6c68b04aa00832355b9fed
Paul Bender <paul.bender@acm.org>
2017-03-05 22:20:37 -0500

    allow specification of pin pull direction.

--- java/src/jmri/jmrix/pi/RaspberryPiSensor.java
+++ java/src/jmri/jmrix/pi/RaspberryPiSensor.java
@@ -16,42 +16,63 @@ import org.slf4j.LoggerFactory;
 /**
  * Extend jmri.AbstractSensor for RaspberryPi GPIO pins.
  * <P>
- * @author			Paul Bender Copyright (C) 2003-2010
+ * @author			Paul Bender Copyright (C) 2003-2017
  */
 public class RaspberryPiSensor extends AbstractSensor implements GpioPinListenerDigital {
 
     private static GpioController gpio = null;
     private int address;
     private GpioPinDigitalInput pin = null;
+    private PinPullResistance pull = PinPullResistance.PULL_DOWN;
 
     public RaspberryPiSensor(String systemName, String userName) {
-        this(systemName,userName,GpioFactory.getInstance());
+        this(systemName,userName,GpioFactory.getInstance(),PinPullResistance.PULL_DOWN);
+    }
+
+    public RaspberryPiSensor(String systemName, String userName,PinPullResistance p) {
+        this(systemName,userName,GpioFactory.getInstance(),p);
     }
 
     public RaspberryPiSensor(String systemName) {
-        this(systemName,GpioFactory.getInstance());
+        this(systemName,GpioFactory.getInstance(),PinPullResistance.PULL_DOWN);
+    }
+
+    public RaspberryPiSensor(String systemName, PinPullResistance p) {
+        this(systemName,GpioFactory.getInstance(),p);
     }
 
     public RaspberryPiSensor(String systemName, String userName,GpioController _gpio) {
         super(systemName, userName);
-        init(systemName,_gpio);
+        // default pull is Pull Down
+        init(systemName,_gpio,PinPullResistance.PULL_DOWN);
+    }
+
+    public RaspberryPiSensor(String systemName, String userName,GpioController _gpio, PinPullResistance p) {
+        super(systemName, userName);
+        init(systemName,_gpio,p);
     }
 
     public RaspberryPiSensor(String systemName,GpioController _gpio) {
         super(systemName);
-        init(systemName,_gpio);
+        init(systemName,_gpio,PinPullResistance.PULL_DOWN);
+    }
+
+    public RaspberryPiSensor(String systemName,GpioController _gpio, PinPullResistance p) {
+        super(systemName);
+        init(systemName,_gpio,p);
     }
 
     /**
      * Common initialization for all constructors
      */
-    private void init(String id,GpioController _gpio){
+    private void init(String id,GpioController _gpio,PinPullResistance p){
         log.debug("Provisioning sensor {}", id);
         if(gpio==null)
            gpio=_gpio;
         address=Integer.parseInt(id.substring(id.lastIndexOf("S")+1));
+        pull = p;
         try {
-           pin = gpio.provisionDigitalInputPin(RaspiPin.getPinByName("GPIO "+address),getSystemName(),PinPullResistance.PULL_DOWN);
+           pin = gpio.provisionDigitalInputPin(RaspiPin.getPinByName("GPIO "+address),getSystemName(),pull);
         } catch(java.lang.RuntimeException re) {
             log.error("Provisioning sensor {} failed with: {}", id, re.getMessage());
             throw new IllegalArgumentException(re.getMessage());
