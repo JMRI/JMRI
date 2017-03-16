cc3930c239abbf7316b3548af027a46a35a7ece6
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-03-16 06:08:05 -0400

    Add modifier as explicitly settable item in version string
    Also:
    - build metadata in version string is delimited with plus signs
    - build timestamp is ISO 8601 parsable
    - order of build metadata in version string matches documented order
    - Copyright string is copyrightable

--- java/test/jmri/PackageTest.java
+++ java/test/jmri/PackageTest.java
@@ -54,7 +54,7 @@ public class PackageTest extends TestCase {
         suite.addTest(jmri.IdTagTest.suite());
         suite.addTest(new JUnit4TestAdapter(jmri.SchemaTest.class));
         suite.addTest(jmri.ProgrammingModeTest.suite());
-        suite.addTest(jmri.VersionTest.suite());
+        suite.addTest(new JUnit4TestAdapter(VersionTest.class));
         suite.addTest(jmri.beans.PackageTest.suite());
         suite.addTest(jmri.progdebugger.PackageTest.suite());
         suite.addTest(jmri.configurexml.PackageTest.suite());
