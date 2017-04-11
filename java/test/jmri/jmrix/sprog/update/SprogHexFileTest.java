7e279690e4a92f0f41ad6fe1d3c49c6a0b515b9f
Paul Bender <paul.bender@acm.org>
2017-04-10 21:20:27 -0400

    missed graphics check

--- java/test/jmri/jmrix/sprog/update/SprogHexFileTest.java
+++ java/test/jmri/jmrix/sprog/update/SprogHexFileTest.java
@@ -2,11 +2,13 @@ package jmri.jmrix.sprog.update;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.awt.GraphicsEnvironment;
 
 /**
  *
@@ -16,6 +18,7 @@ public class SprogHexFileTest {
 
     @Test
     public void testCTor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
         SprogHexFile t = new SprogHexFile("test"); // the parameter is a file name.
         Assert.assertNotNull("exists",t);
     }
