c1054ddbbee9bf9e86846f220e5112d49e966d85
Paul Bender <paul.bender@acm.org>
2017-04-12 11:19:30 -0400

    new tests

---
+++ java/test/jmri/jmrix/acela/swing/AcelaComponentFactoryTest.java
@@ -0,0 +1,43 @@
+package jmri.jmrix.acela.swing;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jmri.jmrix.acela.AcelaSystemConnectionMemo;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class AcelaComponentFactoryTest {
+
+    private AcelaSystemConnectionMemo memo = null;
+
+    @Test
+    public void testCTor() {
+        AcelaComponentFactory t = new AcelaComponentFactory(memo);
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+        AcelaTrafficController tc = new AcelaTrafficControlScaffold();
+        memo = new AcelaSystemConnectionMemo(tc);
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(AcelaComponentFactoryTest.class.getName());
+
+}
