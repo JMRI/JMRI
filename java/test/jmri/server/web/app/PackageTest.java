e3ace22cad52dd60332d1f9d708440a394fd68bf
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-04-17 05:57:11 -0400

    Add some tests

---
+++ java/test/jmri/server/web/app/PackageTest.java
@@ -0,0 +1,17 @@
+package jmri.server.web.app;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ *
+ * @author Randall Wood (C) 2016
+ */
+@RunWith(Suite.class)
+@SuiteClasses({
+    BundleTest.class,
+    WebAppConfigurationTest.class
+})
+public class PackageTest {
+}
