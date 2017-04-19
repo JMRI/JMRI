a3f373c0bca0c8443560e977692fcc5c3b534534
Paul Bender <paul.bender@acm.org>
2017-04-19 13:36:10 -0400

    new tests

---
+++ java/test/jmri/jmrix/can/CanConfigurationManagerTest.java
@@ -0,0 +1,38 @@
+package jmri.jmrix.can;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class CanConfigurationManagerTest {
+
+    @Test
+    public void testCTor() {
+        CanConfigurationManager t = new CanConfigurationManager(new CanSystemConnectionMemo());
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(CanConfigurationManagerTest.class.getName());
+
+}
