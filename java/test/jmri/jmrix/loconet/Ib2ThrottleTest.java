adef57d74795de63c0b1cb48669b98ce2b630f83
Paul Bender <paul.bender@acm.org>
2017-04-21 12:24:39 -0400

    new tests

---
+++ java/test/jmri/jmrix/loconet/Ib2ThrottleTest.java
@@ -0,0 +1,38 @@
+package jmri.jmrix.loconet;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2027	
+ */
+public class Ib2ThrottleTest {
+
+    @Test
+    public void testCTor() {
+        Ib2Throttle t = new Ib2Throttle(new LocoNetSystemConnectionMemo(),new LocoNetSlot(5));
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(Ib2ThrottleTest.class.getName());
+
+}
