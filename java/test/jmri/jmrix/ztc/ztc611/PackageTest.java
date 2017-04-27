26c8bc53038e9eff779aa0a20f3514a9a548d50d
Paul Bender <paul.bender@acm.org>
2017-04-26 23:28:02 -0400

    initial commit of support for ZTC611 connections (currently a copy of the ZTC640 support).

---
+++ java/test/jmri/jmrix/ztc/ztc611/PackageTest.java
@@ -0,0 +1,22 @@
+package jmri.jmrix.ztc.ztc611;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   jmri.jmrix.ztc.ztc611.configurexml.PackageTest.class,
+   ConnectionConfigTest.class,
+   ZTC611AdapterTest.class,
+   ZTC611XNetPacketizerTest.class
+})
+
+/**
+ * Tests for the jmri.jmrix.ztc package
+ *
+ * @author	Bob Jacobsen
+ * @author      Paul Bender Copyright (C) 2017
+ */
+public class PackageTest {
+}
