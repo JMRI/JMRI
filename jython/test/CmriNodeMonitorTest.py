9ab4c037766ec994e5ba6d393953c83791cf23ad
Bob Jacobsen <jacobsen@mac.com>
2017-05-22 14:40:44 -0700

    for headless testing

--- jython/test/CmriNodeMonitorTest.py
+++ jython/test/CmriNodeMonitorTest.py
@@ -1,11 +1,13 @@
 # Test the jython/CmriNodeMonitor.py script to make sure it basically parses
 
 import jmri
+import java
 
 # create and self-register
 c = jmri.jmrix.cmri.CMRISystemConnectionMemo()
 
-execfile("jython/CmriNodeMonitor.py")
-
-# need to kill the thread
-jmri.jmrit.automat.AutomatSummary.instance().get("CmriNodeMonitor").stop()
+if (! java.awt.GraphicsEnvironment.isHeadless()) : 
+    execfile("jython/CmriNodeMonitor.py")
+    
+    # need to kill the thread
+    jmri.jmrit.automat.AutomatSummary.instance().get("CmriNodeMonitor").stop()
--- jython/test/CmriNodeToolTest.py
+++ jython/test/CmriNodeToolTest.py
@@ -1,11 +1,13 @@
 # Test the jython/CmriNodeTool.py script to make sure it basically parses
 
 import jmri
+import java
 
 # create and self-register
 c = jmri.jmrix.cmri.CMRISystemConnectionMemo()
 
-execfile("jython/CmriNodeTool.py")
-
-# need to kill the display
-a.scriptFrame.dispose()
+if (! java.awt.GraphicsEnvironment.isHeadless()) : 
+    execfile("jython/CmriNodeTool.py")
+    
+    # need to kill the display
+    a.scriptFrame.dispose()
