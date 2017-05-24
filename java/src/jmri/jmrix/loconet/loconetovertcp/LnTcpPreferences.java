251d138e0a41bcf1f5a3eb67810dba7770b79379
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-05-24 06:32:19 -0400

    Restore ability to read old preferences
    Allow old preferences to be migrated to new preferences structure.

--- java/src/jmri/jmrix/loconet/loconetovertcp/LnTcpPreferences.java
+++ java/src/jmri/jmrix/loconet/loconetovertcp/LnTcpPreferences.java
@@ -1,10 +1,17 @@
 package jmri.jmrix.loconet.loconetovertcp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
+import jmri.InstanceManager;
 import jmri.beans.PreferencesBean;
 import jmri.profile.ProfileManager;
 import jmri.profile.ProfileUtils;
+import jmri.util.FileUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +23,14 @@ import org.slf4j.LoggerFactory;
 public class LnTcpPreferences extends PreferencesBean {
 
     public final static String PORT = jmri.web.server.WebServerPreferences.PORT;
+    private static final String PORT_NUMBER_KEY = "PortNumber";
+    private static final String SETTINGS_FILE_NAME = "LocoNetOverTcpSettings.ini";
+
+    public static LnTcpPreferences getDefault() {
+        return InstanceManager.getOptionalDefault(LnTcpPreferences.class).orElseGet(() -> {
+            return InstanceManager.setDefault(LnTcpPreferences.class, new LnTcpPreferences());
+        });
+    }
 
     private int port = 1234;
     private final static Logger log = LoggerFactory.getLogger(LnTcpPreferences.class);
@@ -27,8 +42,33 @@ public class LnTcpPreferences extends PreferencesBean {
     }
 
     private void readPreferences(Preferences sharedPreferences) {
-        this.port = sharedPreferences.getInt(PORT, this.getPort());
-        this.setIsDirty(false);
+        boolean migrate = false;
+        try {
+            if (sharedPreferences.keys().length == 0) {
+                log.debug("No LocoNetOverTCP preferences exist.");
+                migrate = true;
+            }
+        } catch (BackingStoreException ex) {
+            log.debug("No preferences file exists.");
+            migrate = true;
+        }
+        if (!migrate) {
+            this.port = sharedPreferences.getInt(PORT, this.getPort());
+            this.setIsDirty(false);
+        } else {
+            Properties settings = new Properties();
+            File file = new File(FileUtil.getUserFilesPath(), SETTINGS_FILE_NAME);
+            log.debug("Opening settings file {}", file);
+            try (FileInputStream stream = new FileInputStream(file)) {
+                settings.load(stream);
+                this.port = Integer.parseInt(settings.getProperty(PORT_NUMBER_KEY, Integer.toString(this.getPort())));
+                this.setIsDirty(true);
+            } catch (FileNotFoundException ex) {
+                log.debug("old preferences file not found");
+            } catch (IOException ex) {
+                log.debug("exception reading old preferences file", ex);
+            }
+        }
     }
 
     public void savePreferences() {
