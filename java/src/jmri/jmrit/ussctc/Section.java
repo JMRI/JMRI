65d8876a4f3498816c676dc8950afccb40e5f4b2
Bob Jacobsen <jacobsen@mac.com>
2017-06-30 19:26:24 -0700

    refactor to ensure independence and consistency of transport

--- java/src/jmri/jmrit/ussctc/Section.java
+++ java/src/jmri/jmrit/ussctc/Section.java
@@ -8,31 +8,6 @@ import jmri.*;
  * The type argument defines the communications from central to field and from field to central
  * @author Bob Jacobsen Copyright (C) 2007, 2017
  */
-public interface Section<To extends Enum<To>, From extends Enum<To>> {
-
-    /**
-     * Inform CTC machine part of section that a code-send operation (from machine to field)
-     * has started.
-     * @return The value to be conveyed to the field.
-     */
-    public To codeSendStart();
-    
-    /**
-     * Provide the value to the field unit at the end of the code-send operation.
-     */
-    public void codeValueDelivered(To value);    
-    
-    /**
-     * Inform field part of section that an indication operation (from field to machine)
-     * has started.
-     * @return The value to be conveyed to the CTC machine.
-     */
-    public From indicationStart();
-
-    /**
-     * Provide value to the CTC machine at the end of the indication-send operation
-     */
-    public void indicationComplete(From value);    
-    
+public interface Section extends CentralSection, FieldSection {
     
 }
