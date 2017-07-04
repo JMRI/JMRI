0933dbb12d079c134f82775c31ec1a66b29a35cb
Egbert Broerse <dcc2@ebroerse.nl>
2017-07-05 00:41:30 +0200

    Cbus NodeConfig convert to Bundle

--- java/src/jmri/jmrix/can/cbus/swing/nodeconfig/NodeConfigToolPane.java
+++ java/src/jmri/jmrix/can/cbus/swing/nodeconfig/NodeConfigToolPane.java
@@ -12,14 +12,14 @@ import jmri.jmrix.can.CanSystemConnectionMemo;
 import jmri.jmrix.can.TrafficController;
 
 /**
- * Pane to for setting node configuration
+ * Pane to for setting node configuration.
  *
  * @author Bob Jacobsen Copyright (C) 2008
   * @since 2.3.1
  */
 public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements CanListener {
 
-    static ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolBundle");
+    //static ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolBundle");
 
     JTextField number;
     JButton setNN;
@@ -40,9 +40,9 @@ public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements
     @Override
     public String getTitle() {
         if (memo != null) {
-            return (memo.getUserName() + " " + ResourceBundle.getBundle("jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolBundle").getString("Title"));
+            return (memo.getUserName() + " " + Bundle.getMessage("Title"));
         }
-        return ResourceBundle.getBundle("jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolBundle").getString("Title");
+        return Bundle.getMessage("Title");
     }
 
     public NodeConfigToolPane() {
@@ -54,40 +54,40 @@ public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements
         // get event number
         p1 = new JPanel();
         p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
-        p1.add(new JLabel(rb.getString("LabelNodeNumber")));
+        p1.add(new JLabel(Bundle.getMessage("LabelNodeNumber")));
         number = new JTextField(5);
         p1.add(number);
 
-        p1.setToolTipText(rb.getString("ToolTipNodeNumber"));
-        setNN = new JButton(rb.getString("ButtonSetNodeNumber"));
-        setNN.setToolTipText(rb.getString("ToolTipSetNodeNumber"));
+        p1.setToolTipText(Bundle.getMessage("ToolTipNodeNumber"));
+        setNN = new JButton(Bundle.getMessage("ButtonSetNodeNumber"));
+        setNN.setToolTipText(Bundle.getMessage("ToolTipSetNodeNumber"));
         p1.add(setNN);
 
-        p1.setBorder(BorderFactory.createTitledBorder(rb.getString("BorderNodeNumber")));
+        p1.setBorder(BorderFactory.createTitledBorder(Bundle.getMessage("BorderNodeNumber")));
         add(p1);
 
         // set node variables
         p1 = new JPanel();
         p1.setLayout(new java.awt.GridLayout(3, 2));
 
-        p1.add(new JLabel(rb.getString("LabelVariableNumber")));
+        p1.add(new JLabel(Bundle.getMessage("LabelVariableNumber")));
         varnumber = new JTextField(5);
-        varnumber.setToolTipText(rb.getString("ToolTipVariableNumber"));
+        varnumber.setToolTipText(Bundle.getMessage("ToolTipVariableNumber"));
         p1.add(varnumber);
 
-        p1.add(new JLabel(rb.getString("LabelVariableValue")));
+        p1.add(new JLabel(Bundle.getMessage("LabelVariableValue")));
         varvalue = new JTextField(5);
-        varvalue.setToolTipText(rb.getString("ToolTipVariableValue"));
+        varvalue.setToolTipText(Bundle.getMessage("ToolTipVariableValue"));
         p1.add(varvalue);
 
-        read = new JButton(rb.getString("ButtonRead"));
-        read.setToolTipText(rb.getString("ToolTipRead"));
+        read = new JButton(Bundle.getMessage("ButtonRead"));
+        read.setToolTipText(Bundle.getMessage("ToolTipRead"));
         p1.add(read);
-        write = new JButton(rb.getString("ButtonWrite"));
-        write.setToolTipText(rb.getString("ToolTipWrite"));
+        write = new JButton(Bundle.getMessage("ButtonWrite"));
+        write.setToolTipText(Bundle.getMessage("ToolTipWrite"));
         p1.add(write);
 
-        p1.setBorder(BorderFactory.createTitledBorder(rb.getString("BorderNodeVariables")));
+        p1.setBorder(BorderFactory.createTitledBorder(Bundle.getMessage("BorderNodeVariables")));
         add(p1);
 
     }
@@ -109,15 +109,16 @@ public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements
     }
 
     /**
-     * Nested class to create one of these using old-style defaults
+     * Nested class to create one of these using old-style defaults.
      */
     static public class Default extends jmri.jmrix.can.swing.CanNamedPaneAction {
 
         public Default() {
-            super(ResourceBundle.getBundle("jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolBundle").getString("Title"),
+            super(Bundle.getMessage("Title"),
                     new jmri.util.swing.sdi.JmriJFrameInterface(),
                     NodeConfigToolPane.class.getName(),
                     jmri.InstanceManager.getDefault(CanSystemConnectionMemo.class));
         }
     }
+
 }
