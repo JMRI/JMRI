63766e7a18a19a50ffb5fd01114b02f61036e7aa
Egbert Broerse <dcc2@ebroerse.nl>
2017-07-06 16:39:49 +0200

    Can Cbus Node Config using JSpinner (but tool is not yet functional, added dialog to inform user) Use shared Bundle keys (jmrix/NBB) Clean up props headers Delete danish props (fully identical to default)

--- java/src/jmri/jmrix/can/cbus/swing/nodeconfig/NodeConfigToolPane.java
+++ java/src/jmri/jmrix/can/cbus/swing/nodeconfig/NodeConfigToolPane.java
@@ -1,10 +1,15 @@
 package jmri.jmrix.can.cbus.swing.nodeconfig;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ResourceBundle;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import jmri.jmrix.can.CanListener;
@@ -12,19 +17,19 @@ import jmri.jmrix.can.CanSystemConnectionMemo;
 import jmri.jmrix.can.TrafficController;
 
 /**
- * Pane to for setting node configuration.
+ * Pane for setting node configuration.
+ * <p>
+ * No actions active in buttons, as of JMRI 4.8
  *
  * @author Bob Jacobsen Copyright (C) 2008
  * @since 2.3.1
  */
 public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements CanListener {
 
-    //static ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrix.can.cbus.swing.nodeconfig.NodeConfigToolBundle");
-
-    JTextField number;
+    JSpinner numberSpinner;
     JButton setNN;
-    JTextField varnumber;
-    JTextField varvalue;
+    JSpinner varNumberSpinner;
+    JSpinner varValueSpinner;
     JButton read;
     JButton write;
 
@@ -55,11 +60,13 @@ public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements
         p1 = new JPanel();
         p1.setLayout(new BoxLayout(p1, BoxLayout.X_AXIS));
         p1.add(new JLabel(Bundle.getMessage("LabelNodeNumber")));
-        number = new JTextField(5);
-        p1.add(number);
 
-        p1.setToolTipText(Bundle.getMessage("ToolTipNodeNumber"));
-        setNN = new JButton(Bundle.getMessage("ButtonSetNodeNumber"));
+        numberSpinner = new JSpinner(new SpinnerNumberModel(256, 256, 1000000, 1));
+        //number = new JTextField(5);
+        p1.add(numberSpinner);
+        numberSpinner.setToolTipText(Bundle.getMessage("ToolTipNodeNumber"));
+
+        setNN = new JButton(Bundle.getMessage("ButtonSet"));
         setNN.setToolTipText(Bundle.getMessage("ToolTipSetNodeNumber"));
         p1.add(setNN);
 
@@ -71,20 +78,34 @@ public class NodeConfigToolPane extends jmri.jmrix.can.swing.CanPanel implements
         p1.setLayout(new java.awt.GridLayout(3, 2));
 
         p1.add(new JLabel(Bundle.getMessage("LabelVariableNumber")));
-        varnumber = new JTextField(5);
-        varnumber.setToolTipText(Bundle.getMessage("ToolTipVariableNumber"));
-        p1.add(varnumber);
+        varNumberSpinner = new JSpinner(new SpinnerNumberModel(1, 1, 256, 1));
+        //varnumber = new JTextField(5);
+        varNumberSpinner.setToolTipText(Bundle.getMessage("ToolTipVariableNumber"));
+        p1.add(varNumberSpinner);
 
         p1.add(new JLabel(Bundle.getMessage("LabelVariableValue")));
-        varvalue = new JTextField(5);
-        varvalue.setToolTipText(Bundle.getMessage("ToolTipVariableValue"));
-        p1.add(varvalue);
+        varValueSpinner = new JSpinner(new SpinnerNumberModel(0, 0, 255, 1));
+        // varvalue = new JTextField(5);
+        varValueSpinner.setToolTipText(Bundle.getMessage("ToolTipVariableValue"));
+        p1.add(varValueSpinner);
 
         read = new JButton(Bundle.getMessage("ButtonRead"));
         read.setToolTipText(Bundle.getMessage("ToolTipRead"));
+        read.addActionListener((ActionEvent e1) -> {
+            //not yet functional
+            JOptionPane.showMessageDialog(null, Bundle.getMessage("NotYetDialogString", Bundle.getMessage("Title")),
+                    Bundle.getMessage("WarningTitle"), JOptionPane.ERROR_MESSAGE);
+            return;
+        });
         p1.add(read);
         write = new JButton(Bundle.getMessage("ButtonWrite"));
         write.setToolTipText(Bundle.getMessage("ToolTipWrite"));
+        write.addActionListener((ActionEvent e2) -> {
+            //not yet functional
+            JOptionPane.showMessageDialog(null, Bundle.getMessage("NotYetDialogString", Bundle.getMessage("Title")),
+                    Bundle.getMessage("WarningTitle"), JOptionPane.ERROR_MESSAGE);
+            return;
+        });
         p1.add(write);
 
         p1.setBorder(BorderFactory.createTitledBorder(Bundle.getMessage("BorderNodeVariables")));
