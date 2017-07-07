3b2e8aa41a4cf3e81283a67cab1ae5c70de840fe
Paul Bender <paul.bender@acm.org>
2017-07-07 10:00:05 -0400

    refactor time server tests.

--- java/test/jmri/jmris/srcp/JmriSRCPTimeServerTest.java
+++ java/test/jmri/jmris/srcp/JmriSRCPTimeServerTest.java
@@ -10,28 +10,10 @@ import org.junit.After;
  *
  * @author Paul Bender Copyright (C) 2012,2016
  */
-public class JmriSRCPTimeServerTest {
-
-    private JmriSRCPTimeServer a = null;
-
-    @Test
-    public void testCtor() {
-        Assert.assertNotNull(a);
-    }
-
-    @Test
-    public void addAndRemoveListener(){
-       jmri.Timebase t = jmri.InstanceManager.getDefault(jmri.Timebase.class);
-       int n = t.getMinuteChangeListeners().length;
-       a.listenToTimebase(true);
-       Assert.assertEquals("added listener",n+1,t.getMinuteChangeListeners().length);
-       a.listenToTimebase(false);
-       // per the jmri.jmrit.simpleclock.SimpleTimebase class, remove is not 
-       // implemented, so the following check doesn't work.
-       //Assert.assertEquals("removed listener",n,t.getMinuteChangeListeners().length);
-    }
+public class JmriSRCPTimeServerTest extends jmri.jmris.AbstractTimeServerTestBase {
 
     @Before
+    @Override
     public void setUp(){
         jmri.util.JUnitUtil.resetInstanceManager();
         java.io.DataOutputStream output = new java.io.DataOutputStream(
@@ -46,6 +28,7 @@ public class JmriSRCPTimeServerTest {
     }
 
     @After
+    @Override
     public void tearDown(){
        a = null;
        jmri.util.JUnitUtil.resetInstanceManager();
