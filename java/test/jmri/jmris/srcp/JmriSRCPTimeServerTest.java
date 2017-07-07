c5964ffaa1ad9b45a606caac874fbe32f0c75fd9
Paul Bender <paul.bender@acm.org>
2017-07-06 22:38:47 -0400

    new test

--- java/test/jmri/jmris/srcp/JmriSRCPTimeServerTest.java
+++ java/test/jmri/jmris/srcp/JmriSRCPTimeServerTest.java
@@ -2,6 +2,8 @@ package jmri.jmris.srcp;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
 /**
  * Tests for the jmri.jmris.srcp.JmriSRCPTimeServer class
@@ -10,8 +12,28 @@ import org.junit.Test;
  */
 public class JmriSRCPTimeServerTest {
 
+    private JmriSRCPTimeServer a = null;
+
     @Test
     public void testCtor() {
+        Assert.assertNotNull(a);
+    }
+
+    @Test
+    public void addAndRemoveListener(){
+       jmri.Timebase t = jmri.InstanceManager.getDefault(jmri.Timebase.class);
+       int n = t.getMinuteChangeListeners().length;
+       a.listenToTimebase(true);
+       Assert.assertEquals("added listener",n+1,t.getMinuteChangeListeners().length);
+       a.listenToTimebase(false);
+       // per the jmri.jmrit.simpleclock.SimpleTimebase class, remove is not 
+       // implemented, so the following check doesn't work.
+       //Assert.assertEquals("removed listener",n,t.getMinuteChangeListeners().length);
+    }
+
+    @Before
+    public void setUp(){
+        jmri.util.JUnitUtil.resetInstanceManager();
         java.io.DataOutputStream output = new java.io.DataOutputStream(
                 new java.io.OutputStream() {
                     // null output string drops characters
@@ -20,8 +42,13 @@ public class JmriSRCPTimeServerTest {
                     public void write(int b) throws java.io.IOException {
                     }
                 });
-        JmriSRCPTimeServer a = new JmriSRCPTimeServer(output);
-        Assert.assertNotNull(a);
+        a = new JmriSRCPTimeServer(output);
+    }
+
+    @After
+    public void tearDown(){
+       a = null;
+       jmri.util.JUnitUtil.resetInstanceManager();
     }
 
 }
