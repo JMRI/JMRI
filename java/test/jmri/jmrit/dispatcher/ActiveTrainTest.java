0e6f566dceb391e188ac53fa9f64d949f2fc4172
Paul Bender <paul.bender@acm.org>
2017-07-12 12:42:10 -0400

    new tests

---
+++ java/test/jmri/jmrit/dispatcher/ActiveTrainTest.java
@@ -0,0 +1,39 @@
+package jmri.jmrit.dispatcher;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class ActiveTrainTest {
+
+    @Test
+    public void testCTor() {
+        jmri.Transit transit = new jmri.Transit("TT1");
+        ActiveTrain t = new ActiveTrain(transit,"Train",ActiveTrain.USER);
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(ActiveTrainTest.class.getName());
+
+}
