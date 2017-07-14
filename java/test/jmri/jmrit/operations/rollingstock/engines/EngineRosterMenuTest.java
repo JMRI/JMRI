12795b27b8af0a4bafaa21eda24cfbd2deab5589
Paul Bender <paul.bender@acm.org>
2017-07-14 15:25:17 -0400

    new test

--- java/test/jmri/jmrit/operations/rollingstock/engines/EngineRosterMenuTest.java
+++ java/test/jmri/jmrit/operations/rollingstock/engines/EngineRosterMenuTest.java
@@ -2,11 +2,13 @@ package jmri.jmrit.operations.rollingstock.engines;
 
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.awt.GraphicsEnvironment;
 
 /**
  *
@@ -16,7 +18,9 @@ public class EngineRosterMenuTest {
 
     @Test
     public void testCTor() {
-        EngineRosterMenu t = new EngineRosterMenu();
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        EnginesTableFrame etf = new EnginesTableFrame();
+        EngineRosterMenu t = new EngineRosterMenu("test menu",1,etf);
         Assert.assertNotNull("exists",t);
     }
 
