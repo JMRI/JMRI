24107eb5f2fd821265ba0e365c68935fc26de137
Paul Bender <paul.bender@acm.org>
2017-07-18 12:05:02 -0400

    add new test

---
+++ java/test/jmri/util/usb/PackageTest.java
@@ -0,0 +1,19 @@
+package jmri.util.usb;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   BundleTest.class
+})
+/**
+ * Tests for the jmri.util.usb package
+ *
+ * @author      Paul Bender Copyright (C) 2017
+ */
+public class PackageTest {
+
+
+}
