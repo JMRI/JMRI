565b69970a98c69ea680bb99a3b10785c231932b
Paul Bender <paul.bender@acm.org>
2017-07-20 15:43:52 -0400

    new tests

---
+++ java/test/jmri/jmrit/turnoutoperations/TurnoutOperationFrameTest.java
@@ -0,0 +1,43 @@
+package jmri.jmrit.turnoutoperations;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.awt.GraphicsEnvironment;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class TurnoutOperationFrameTest {
+
+    @Test
+    public void testCTor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        jmri.util.JmriJFrame jf = new jmri.util.JmriJFrame("Turnout Operation Frame Test");
+        TurnoutOperationFrame t = new TurnoutOperationFrame(jf);
+        Assert.assertNotNull("exists",t);
+        jf.dispose();
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(TurnoutOperationFrameTest.class.getName());
+
+}
