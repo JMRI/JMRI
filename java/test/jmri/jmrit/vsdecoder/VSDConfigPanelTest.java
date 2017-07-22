b3807d5b81e3b41f34b6f7dbe83e74a64ca20cbb
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-07-21 22:01:09 -0400

    Assert need for correct audio factory in tests.

--- java/test/jmri/jmrit/vsdecoder/VSDConfigPanelTest.java
+++ java/test/jmri/jmrit/vsdecoder/VSDConfigPanelTest.java
@@ -1,23 +1,27 @@
 package jmri.jmrit.vsdecoder;
 
+import jmri.AudioManager;
+import jmri.InstanceManager;
+import jmri.jmrit.audio.JoalAudioFactory;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  *
- * @author Paul Bender Copyright (C) 2017	
+ * @author Paul Bender Copyright (C) 2017
  */
 public class VSDConfigPanelTest {
 
     @Test
     public void testCTor() {
+        Assume.assumeTrue("Requires JOAL Audio", InstanceManager.getDefault(AudioManager.class).getActiveAudioFactory() instanceof JoalAudioFactory);
         VSDConfigPanel t = new VSDConfigPanel();
-        Assert.assertNotNull("exists",t);
+        Assert.assertNotNull("exists", t);
     }
 
     // The minimal setup for log4J
@@ -25,10 +29,12 @@ public class VSDConfigPanelTest {
     public void setUp() {
         apps.tests.Log4JFixture.setUp();
         jmri.util.JUnitUtil.resetInstanceManager();
+        InstanceManager.getDefault(AudioManager.class).init();
     }
 
     @After
     public void tearDown() {
+        InstanceManager.getDefault(AudioManager.class).cleanUp();
         jmri.util.JUnitUtil.resetInstanceManager();
         apps.tests.Log4JFixture.tearDown();
     }
