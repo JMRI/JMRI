8eef6e8a3ac9f691d519277175d2f8833bc64a48
Paul Bender <paul.bender@acm.org>
2017-07-26 23:21:40 -0400

    setup for test

--- java/test/jmri/jmrix/loconet/loconetovertcp/LnTcpPreferencesPanelTest.java
+++ java/test/jmri/jmrix/loconet/loconetovertcp/LnTcpPreferencesPanelTest.java
@@ -25,6 +25,7 @@ public class LnTcpPreferencesPanelTest {
     public void setUp() {
         apps.tests.Log4JFixture.setUp();
         jmri.util.JUnitUtil.resetInstanceManager();
+        jmri.util.JUnitUtil.installStartupActionsManager();
     }
 
     @After
