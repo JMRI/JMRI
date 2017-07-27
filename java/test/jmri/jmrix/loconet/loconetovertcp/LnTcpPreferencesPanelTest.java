d9389d02f64d2d7e980099c5b55dbc918b413b9d
Paul Bender <paul.bender@acm.org>
2017-07-27 00:01:45 -0400

    correct spelling on method name

--- java/test/jmri/jmrix/loconet/loconetovertcp/LnTcpPreferencesPanelTest.java
+++ java/test/jmri/jmrix/loconet/loconetovertcp/LnTcpPreferencesPanelTest.java
@@ -25,7 +25,7 @@ public class LnTcpPreferencesPanelTest {
     public void setUp() {
         apps.tests.Log4JFixture.setUp();
         jmri.util.JUnitUtil.resetInstanceManager();
-        jmri.util.JUnitUtil.installStartupActionsManager();
+        jmri.util.JUnitUtil.initStartupActionsManager();
     }
 
     @After
