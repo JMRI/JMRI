763ad427d553f70b2258516d66198c68f6b861cd
Paul Bender <paul.bender@acm.org>
2017-08-03 10:16:16 -0400

    new constructor tests

---
+++ java/test/jmri/jmrix/loconet/locostats/LocoStatsFuncTest.java
@@ -0,0 +1,40 @@
+package jmri.jmrix.loconet.locostats;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jmri.jmrix.loconet.LocoNetSystemConnectionMemo;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class LocoStatsFuncTest {
+
+    @Test
+    public void testCTor() {
+        LocoNetSystemConnectionMemo memo = new LocoNetSystemConnectionMemo();
+        LocoStatsFunc t = new LocoStatsFunc(memo);
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(LocoStatsFuncTest.class.getName());
+
+}
