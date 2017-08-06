67c96794dcd91c577f9fcabb936d60008923a0bd
Bob Jacobsen <jacobsen@mac.com>
2017-08-06 10:48:28 -0700

    clear and clearAll on GUI thread

--- java/src/jmri/InstanceManager.java
+++ java/src/jmri/InstanceManager.java
@@ -751,12 +751,13 @@ public final class InstanceManager {
      * Clear all managed instances from this InstanceManager.
      * <p>
      * Realistically, JMRI can't ensure that all objects
+     * and combination of objects
      * held by the InstanceManager are threadsafe.  This call
-     * therefore defers to the GUI thread to reduce risk.
+     * therefore defers to the GUI thread to become atomic and reduce risk.
      */
     public void clearAll() {
-        log.debug("Clearing InstanceManager");
         jmri.util.ThreadingUtil.runOnGUI( ()->{ 
+            log.debug("Clearing InstanceManager");
             managerLists.keySet().forEach((type) -> {
                 clear(type);
              } );
@@ -766,16 +767,23 @@ public final class InstanceManager {
     /**
      * Clear all managed instances of a particular type from this
      * InstanceManager.
+     * <p>
+     * Realistically, JMRI can't ensure that all objects 
+     * and combination of objects
+     * held by the InstanceManager are threadsafe.  This call
+     * therefore defers to the GUI thread to become atomic and reduce risk.
      *
      * @param type the type to clear
      */
     public void clear(@Nonnull Class<?> type) {
-        log.trace("Clearing managers of {}", type.getName());
-        getInstances(type).stream().filter((o) -> (o instanceof Disposable)).forEachOrdered((o) -> {
-            dispose((Disposable) o);
+        jmri.util.ThreadingUtil.runOnGUI( ()->{ 
+            log.trace("Clearing managers of {}", type.getName());
+            getInstances(type).stream().filter((o) -> (o instanceof Disposable)).forEachOrdered((o) -> {
+                dispose((Disposable) o);
+            });
+            // Should this be sending notifications of removed instances to listeners?
+            managerLists.put(type, new ArrayList<>());
         });
-        // Should this be sending notifications of removed instances to listeners?
-        managerLists.put(type, new ArrayList<>());
     }
 
     /**
