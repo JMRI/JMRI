f5ba0ec67394c3ad478d66054a666f4cd0089d2e
Paul Bender <paul.bender@acm.org>
2017-08-07 22:18:14 -0400

    new test

--- java/test/jmri/jmrit/picker/PackageTest.java
+++ java/test/jmri/jmrit/picker/PackageTest.java
@@ -27,6 +27,7 @@ public class PackageTest extends TestCase {
         TestSuite suite = new TestSuite("jmri.jmrit.picker.PackageTest");   // no tests in this class itself
 
         suite.addTest(new junit.framework.JUnit4TestAdapter(BundleTest.class));
+        suite.addTest(new junit.framework.JUnit4TestAdapter(PickFrameTest.class));
 
         return suite;
     }
