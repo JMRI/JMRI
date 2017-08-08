9cdfac1db7143b864413e2c0cd7d07134c38e651
Paul Bender <paul.bender@acm.org>
2017-08-08 09:22:04 -0400

    new constructor tests

---
+++ java/test/jmri/jmrit/display/palette/ReporterItemPanelTest.java
@@ -0,0 +1,49 @@
+package jmri.jmrit.display.palette;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.awt.GraphicsEnvironment;
+import jmri.Reporter;
+import jmri.util.JmriJFrame;
+import jmri.jmrit.picker.PickListModel;
+import jmri.jmrit.display.Editor;
+import jmri.jmrit.display.EditorScaffold;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class ReporterItemPanelTest {
+
+    @Test
+    public void testCTor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        PickListModel tableModel = PickListModel.reporterPickModelInstance(); // N11N
+        JmriJFrame jf = new JmriJFrame("Reporter Item Panel Test");
+        Editor editor = new EditorScaffold();
+        ReporterItemPanel t = new ReporterItemPanel(jf,"IR01","",tableModel,editor);
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(ReporterItemPanelTest.class.getName());
+
+}
