345dcc6607db24874504aa7cbe4f3011cb0258ef
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-08-11 07:40:34 -0400

    Only reset the operations managers prior to a test that uses them. The Operations Managers get marked for garbage collection when resetting the InstanceManager, so there is no need to set them to a blank state when tearing down a test that also resets the InstanceManager. Also take advantage of FileUtil to ensure the operations directory is empty for tests.

--- java/test/jmri/jmrit/operations/trains/TrainCsvManifestTest.java
+++ java/test/jmri/jmrit/operations/trains/TrainCsvManifestTest.java
@@ -30,7 +30,6 @@ public class TrainCsvManifestTest {
 
     @After
     public void tearDown() {
-        jmri.util.JUnitOperationsUtil.resetOperationsManager();
         jmri.util.JUnitUtil.resetInstanceManager();
         apps.tests.Log4JFixture.tearDown();
     }
--- java/test/jmri/jmrit/operations/trains/TrainIconAnimationTest.java
+++ java/test/jmri/jmrit/operations/trains/TrainIconAnimationTest.java
@@ -385,7 +385,6 @@ public class TrainIconAnimationTest {
 
     @After
     public void tearDown() {
-        jmri.util.JUnitOperationsUtil.resetOperationsManager();
         jmri.util.JUnitUtil.resetInstanceManager();
         apps.tests.Log4JFixture.tearDown();
     }
