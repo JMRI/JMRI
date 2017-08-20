f07a01a10692f6f87f7f557d2547698cd61928ef
dsand47 <gh@dfm.sandsys.org>
2017-08-20 11:18:55 -0500

    Create Logix Add and Delete tests

--- java/test/jmri/jmrit/beantable/LogixTableActionTest.java
+++ java/test/jmri/jmrit/beantable/LogixTableActionTest.java
@@ -8,6 +8,7 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.JTextField;
 
 import jmri.InstanceManager;
 import jmri.Logix;
@@ -26,6 +27,7 @@ import org.netbeans.jemmy.operators.JFrameOperator;
 import org.netbeans.jemmy.operators.JMenuBarOperator;
 import org.netbeans.jemmy.operators.JMenuItemOperator;
 import org.netbeans.jemmy.operators.JMenuOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,7 +47,7 @@ public class LogixTableActionTest extends AbstractTableActionBase {
     @Test
     public void testStringCtor() {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
-        Assert.assertNotNull("ConditionalTreeEdit Constructor Return", new LogixTableAction("test"));  // NOI18N
+        Assert.assertNotNull("LogixTableActionTest Constructor Return", new LogixTableAction("test"));  // NOI18N
     }
 
     @Override
@@ -77,7 +79,7 @@ public class LogixTableActionTest extends AbstractTableActionBase {
 
         lgxTable.browserPressed("IX101");  // NOI18N
 
-        JFrame frame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("BrowserTitle"), false, false);  // NOI18N
+        JFrame frame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("BrowserTitle"), true, true);  // NOI18N
         Assert.assertNotNull(frame);
         frame.dispose();
     }
@@ -89,7 +91,7 @@ public class LogixTableActionTest extends AbstractTableActionBase {
 
         lgxTable.makeWhereUsedWindow();
 
-        JFrame frame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("DisplayWhereUsed"), false, false);  // NOI18N
+        JFrame frame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("DisplayWhereUsed"), true, true);  // NOI18N
         Assert.assertNotNull(frame);
         frame.dispose();
     }
@@ -101,11 +103,71 @@ public class LogixTableActionTest extends AbstractTableActionBase {
 
         lgxTable.editPressed("IX101");  // NOI18N
 
-        JFrame frame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("TitleEditLogix"), false, false);  // NOI18N
+        JFrame frame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("TitleEditLogix"), true, true);  // NOI18N
         Assert.assertNotNull(frame);
         frame.dispose();
     }
-    
+
+    @Test
+    public void testAddLogix() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        LogixTableAction lgxTable = (LogixTableAction) a;
+
+        lgxTable.actionPerformed(null); // show table
+        JFrame lgxFrame = JFrameOperator.waitJFrame(Bundle.getMessage("TitleLogixTable"), true, true);  // NOI18N
+        Assert.assertNotNull("Found Logix Frame", lgxFrame);  // NOI18N
+
+        lgxTable.addPressed(null);
+        JFrameOperator addFrame = new JFrameOperator(LogixTableAction.rbx.getString("TitleAddLogix"));  // NOI18N
+        Assert.assertNotNull("Found Add Logix Frame", addFrame);  // NOI18N
+
+        new JTextFieldOperator(addFrame, 0).setText("104");  // NOI18N
+        new JTextFieldOperator(addFrame, 1).setText("Logix 104");  // NOI18N
+        new JButtonOperator(addFrame, Bundle.getMessage("ButtonCreate")).push();  // NOI18N
+
+        Logix chk104 = jmri.InstanceManager.getDefault(jmri.LogixManager.class).getLogix("Logix 104");  // NOI18N
+        Assert.assertNotNull("Verify IX104 Added", chk104);  // NOI18N
+
+        // Add creates an edit frame; find and dispose
+        JFrame editFrame = JFrameOperator.waitJFrame(LogixTableAction.rbx.getString("TitleEditLogix"), true, true);  // NOI18N
+        editFrame.dispose();
+
+        lgxFrame.dispose();
+    }
+
+    @Test
+    public void testDeleteLogix() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        LogixTableAction lgxTable = (LogixTableAction) a;
+
+        lgxTable.actionPerformed(null); // show table
+        JFrame lgxFrame = JFrameOperator.waitJFrame(Bundle.getMessage("TitleLogixTable"), true, true);  // NOI18N
+        Assert.assertNotNull("Found Logix Frame", lgxFrame);  // NOI18N
+
+        // Delete IX102, respond No
+        createModalDialogOperatorThread(LogixTableAction.rbx.getString("ConfirmTitle"), Bundle.getMessage("ButtonNo"));  // NOI18N
+        lgxTable.deletePressed("IX102");  // NOI18N
+        Logix chk102 = jmri.InstanceManager.getDefault(jmri.LogixManager.class).getBySystemName("IX102");  // NOI18N
+        Assert.assertNotNull("Verify IX102 Not Deleted", chk102);  // NOI18N
+
+        // Delete IX103, respond Yes
+        createModalDialogOperatorThread(LogixTableAction.rbx.getString("ConfirmTitle"), Bundle.getMessage("ButtonYes"));  // NOI18N
+        lgxTable.deletePressed("IX103");  // NOI18N
+        Logix chk103 = jmri.InstanceManager.getDefault(jmri.LogixManager.class).getBySystemName("IX103");  // NOI18N
+        Assert.assertNull("Verify IX103 Is Deleted", chk103);  // NOI18N
+
+        lgxFrame.dispose();
+    }
+
+    void createModalDialogOperatorThread(String dialogTitle, String buttonText) {
+        new Thread(() -> {
+            // constructor for jdo will wait until the dialog is visible
+            JDialogOperator jdo = new JDialogOperator(dialogTitle);
+            JButtonOperator jbo = new JButtonOperator(jdo, buttonText);
+            jbo.pushNoBlock();
+        }).start();
+    }
+
     @Before
     @Override
     public void setUp() {
@@ -116,6 +178,7 @@ public class LogixTableActionTest extends AbstractTableActionBase {
 
         Logix x1 = InstanceManager.getDefault(jmri.LogixManager.class).createNewLogix("IX101", "Logix 101");  // NOI18N
         Logix x2 = InstanceManager.getDefault(jmri.LogixManager.class).createNewLogix("IX102", "Logix 102");  // NOI18N
+        Logix x3 = InstanceManager.getDefault(jmri.LogixManager.class).createNewLogix("IX103", "Logix 103");  // NOI18N
 
         a = new LogixTableAction();
     }
