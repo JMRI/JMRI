ed3e0c341e26a879c3679e87f3cfda5b70655dfd
Bob Jacobsen <jacobsen@mac.com>
2017-08-23 06:31:53 -0700

    add example of suppressing Info

--- java/test/apps/PanelPro/PanelProTest.java
+++ java/test/apps/PanelPro/PanelProTest.java
@@ -60,6 +60,8 @@ public class PanelProTest {
 
             JUnitUtil.waitFor(()->{return JmriJFrame.getFrame("PanelPro") != null;},"window up");
         
+            JUnitUtil.waitFor(()->{return JUnitAppender.checkForMessageStartingWith("PanelPro version") != null;}, "first Info line seen");
+
             // maybe have it run a script to indicate that it's really up?
             
             // now clean up frames, depending on what's actually left
