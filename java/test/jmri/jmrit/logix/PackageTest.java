eec39656b28b9fdca484cd93eea54d18879f0dd0
Paul Bender <paul.bender@acm.org>
2017-08-31 11:57:44 -0400

    new tests

--- java/test/jmri/jmrit/logix/PackageTest.java
+++ java/test/jmri/jmrit/logix/PackageTest.java
@@ -61,6 +61,7 @@ public class PackageTest extends TestCase {
         suite.addTest(new JUnit4TestAdapter(SpeedUtilTest.class));
         suite.addTest(new JUnit4TestAdapter(FunctionPanelTest.class));
         suite.addTest(new JUnit4TestAdapter(WarrantShutdownTaskTest.class));
+        suite.addTest(new JUnit4TestAdapter(SpeedProfilePanelTest.class));
         return suite;
     }
 
