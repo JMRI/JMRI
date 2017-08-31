b3b786fec0a31271c282247cfe0138739376733a
Paul Bender <paul.bender@acm.org>
2017-08-30 21:25:56 -0400

    start adding acceptance tests for the web server

--- java/acceptancetest/step_definitions/jmri/web/HomeServletAcceptanceSteps.java
+++ java/acceptancetest/step_definitions/jmri/web/HomeServletAcceptanceSteps.java
@@ -4,6 +4,8 @@ import cucumber.api.java.en.*;
 import cucumber.api.java8.En;
 import cucumber.api.PendingException;
 import org.junit.Assert;
+import org.openqa.selenium.support.events.EVentFiringWebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
  * Cucumber step defintions for Home Servlet Acceptance tests.
@@ -11,12 +13,15 @@ import org.junit.Assert;
  * @author  Paul Bender Copyright (C) 2017
  */
 public class HomeServletAcceptanceSteps implements En {
+     
+   private EventFiringWebDriver webDriver;
       
    public HomeServletAcceptanceSteps() {
 
+      webDriver = new EventFiringWebDriver(new FireFoxDriver());
+
       When("^I ask for the /index\\.html$", () -> {
-         // Write code here that turns the phrase above into concrete actions
-         throw new PendingException();
+         webDriver.get("http://localhost:12080");
       });
 
       Then("^the home page is returned$", () -> {
