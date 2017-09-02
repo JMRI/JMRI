JMRI/JMRI

commit a12cc426d3bcba204d2594687f355c22783b1a04

Rename classes with names causing high priority FindBugs warnings.

java/src/jmri/jmrix/ieee802154/xbee/swing/XBeeMenu.java
@@ -28,7 +28,7 @@ public class XBeeMenu extends JMenu {
         }
 
         add(new jmri.jmrix.ieee802154.xbee.swing.packetgen.PacketGenAction(rb.getString("jmri.jmrix.ieee802154.xbee.swing.packetgen.PacketGenAction"), memo));
-        add(new jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction(rb.getString("jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction"), memo));
+        add(new jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction(rb.getString("jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction"), memo));
 
     }
 

java/src/jmri/jmrix/ieee802154/IEEE802154ActionListBundle.properties
@@ -8,5 +8,5 @@ jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction = Open IEEE 802.15.4 Monitor
 jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction = Send IEEE 802.15.4 Message
 jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction = Open IEEE 802.15.4 Node Configuration
 #jmri.jmrix.ieee802154.xbee.swing.mon.IEEE802154MonAction = Open XBee Monitor
-jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction = Open XBee Node Configuration
+jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction=Open XBee Node Configuration
 jmri.jmrix.ieee802154.xbee.swing.packetgen.PacketGenAction = Send XBee Message

java/src/jmri/jmrix/ieee802154/IEEE802154ActionListBundle_da.properties
@@ -7,4 +7,4 @@
 jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction = Open IEEE 802.15.4 Monitor
 jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction = Send IEEE 802.15.4 Message
 jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction = Open IEEE 802.15.4 Node Configuration
-jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction = Open XBee Node Configuration
+jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction=Open XBee Node Configuration

java/src/jmri/jmrix/ieee802154/IEEE802154ActionListBundle_de.properties
@@ -7,4 +7,4 @@
 jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction          = \u00d6ffne IEEE 802.15.4 Monitor
 jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction        = Sende IEEE 802.15.4 Bericht
 jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction      = \u00d6ffne IEEE 802.15.4 Node Konfigurator
-jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction = \u00d6ffne XBee Node Konfigurator
+jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction=\u00d6ffne XBee Node Konfigurator

java/src/jmri/jmrix/ieee802154/IEEE802154ActionListBundle_fr.properties
@@ -4,7 +4,7 @@
 # in various forms in advanced preferences
 # do not add other keys
 
-jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction = Ouvrir Moniteur IEEE 802.15.4 
-jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction = Envoyer Message IEEE 802.15.4 
-jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction = Ouvrir Configuration des N\u0153uds IEEE 802.15.4 
-jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction = Ouvrir XBee Configuration des N\u0153uds
+jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction = Ouvrir Moniteur IEEE 802.15.4
+jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction = Envoyer Message IEEE 802.15.4
+jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction = Ouvrir Configuration des N\u0153uds IEEE 802.15.4
+jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction=Ouvrir XBee Configuration des N\u0153uds

java/src/jmri/jmrix/ieee802154/IEEE802154ActionListBundle_it.properties
@@ -7,4 +7,4 @@
 jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction = Apri Monitor IEEE 802.15.4
 jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction = Invia Messaggio IEEE 802.15.4
 jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction = Configura Nodo IEEE 802.15.4
-jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction = Configura Nodo
+jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction=Configura Nodo

java/src/jmri/jmrix/ieee802154/IEEE802154ActionListBundle_nl.properties
@@ -7,4 +7,4 @@
 jmri.jmrix.ieee802154.swing.mon.IEEE802154MonAction          = Open IEEE 802.15.4 Monitor
 jmri.jmrix.ieee802154.swing.packetgen.PacketGenAction        = Verstuur IEEE 802.15.4 Bericht
 jmri.jmrix.ieee802154.swing.nodeconfig.NodeConfigAction      = Open IEEE 802.15.4 Node-configuratie
-jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction = Open XBee Node-configuratie
+jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction=Open XBee Node-configuratie

java/src/jmri/jmrix/ieee802154/swing/nodeconfig/AddNodeFrame.java
@@ -26,8 +26,6 @@ public class AddNodeFrame extends jmri.util.JmriJFrame {
     protected javax.swing.JButton addButton = new javax.swing.JButton(Bundle.getMessage("ButtonAdd"));
     protected javax.swing.JButton cancelButton = new javax.swing.JButton(Bundle.getMessage("ButtonCancel"));
 
-    protected javax.swing.JPanel panel = new JPanel();
-
     protected IEEE802154Node curNode = null;    // IEEE802154 Node being editted
 
     private IEEE802154TrafficController itc = null;
@@ -82,11 +80,8 @@ public class AddNodeFrame extends jmri.util.JmriJFrame {
         addButton.setText(Bundle.getMessage("ButtonAdd"));
         addButton.setVisible(true);
         addButton.setToolTipText(Bundle.getMessage("TipAddButton"));
-        addButton.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent e) {
-                addButtonActionPerformed();
-            }
+        addButton.addActionListener((java.awt.event.ActionEvent e) -> {
+            addButtonActionPerformed();
         });
         panel4.add(addButton);
         panel4.add(cancelButton);
@@ -94,11 +89,8 @@ public class AddNodeFrame extends jmri.util.JmriJFrame {
         cancelButton.setVisible(true);
         cancelButton.setToolTipText(Bundle.getMessage("TipCancelButton"));
         panel4.add(cancelButton);
-        cancelButton.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent e) {
-                cancelButtonActionPerformed();
-            }
+        cancelButton.addActionListener((java.awt.event.ActionEvent e) -> {
+            cancelButtonActionPerformed();
         });
         contentPane.add(panel4);
 
@@ -138,7 +130,7 @@ public class AddNodeFrame extends jmri.util.JmriJFrame {
      * Method to handle cancel button
      */
     public void cancelButtonActionPerformed() {
-        // Reset 
+        // Reset
         curNode = null;
         // Switch buttons
         addButton.setVisible(true);
@@ -147,16 +139,15 @@ public class AddNodeFrame extends jmri.util.JmriJFrame {
     }
 
     /**
-     * Read node address from the nodeAddressField or nodeAddr64Field 
-     * as appropriate and return as a string.  
+     * Read node address from the nodeAddressField or nodeAddr64Field
+     * as appropriate and return as a string.
      *
      * @return String containing the short (two byte) address of the node.
      *         if the two byte node address is either "FF FF" or "FF FE",
      *         returns the long (64 bit) address.
      */
     private String readNodeAddress() {
-        String addr = "";
-        addr = nodeAddrField.getText();
+        String addr = nodeAddrField.getText();
         if (addr.equals("FF FF ") || addr.equals("FF FE ") || addr.equals("")) {
             addr = nodeAddr64Field.getText();
         }

commit 7fb20aeb1f5583705c8dad1f11f74b4f450f9f14

Correct a test failure caused by a12cc42

java/src/jmri/jmrix/ieee802154/xbee/swing/XBeeMenu.java
@@ -28,7 +28,7 @@ public class XBeeMenu extends JMenu {
         }
 
         add(new jmri.jmrix.ieee802154.xbee.swing.packetgen.PacketGenAction(rb.getString("jmri.jmrix.ieee802154.xbee.swing.packetgen.PacketGenAction"), memo));
-        add(new jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction(rb.getString("jmri.jmrix.ieee802154.xbee.swing.nodeconfig.NodeConfigAction"), memo));
+        add(new jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction(rb.getString("jmri.jmrix.ieee802154.xbee.swing.nodeconfig.XBeeNodeConfigAction"), memo));
 
     }
 

java/test/jmri/jmrix/ieee802154/xbee/swing/XBeeMenuTest.java
@@ -11,19 +11,18 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Test simple functioning of XBeeMenu
+ * Test simple functioning of XBeeMenu.
  *
  * @author	Paul Bender Copyright (C) 2016
  */
 public class XBeeMenuTest {
 
-
     private XBeeTrafficController tc = null;
     private XBeeConnectionMemo m = null;
- 
+
     @Test
     public void testCtor() {
-        Assume.assumeFalse(GraphicsEnvironment.isHeadless()); 
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
         XBeeMenu action = new XBeeMenu(m);
         Assert.assertNotNull("exists", action);
     }
@@ -35,10 +34,12 @@ public class XBeeMenuTest {
             @Override
             public void setInstance() {
             }
+
             @Override
             protected jmri.jmrix.AbstractMRReply newReply() {
                 return null;
             }
+
             @Override
             public jmri.jmrix.ieee802154.IEEE802154Node newNode() {
                 return null;
@@ -51,6 +52,8 @@ public class XBeeMenuTest {
     }
 
     @After
-    public void tearDown() {        JUnitUtil.tearDown();        tc = null;
+    public void tearDown() {
+        JUnitUtil.tearDown();
+        tc = null;
     }
 }