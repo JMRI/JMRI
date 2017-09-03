f16cb306f13f7ccc9f7d93f6f234b9d5d436b33d
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-09-03 08:53:52 -0400

    Suppress a FindBugs warning about class name.

--- java/src/jmri/util/ResizableImagePanel.java
+++ java/src/jmri/util/ResizableImagePanel.java
@@ -1,5 +1,7 @@
 package jmri.util;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * A class extending JPanels to have a image display in a panel, supports: +
  * drag'n drop of image file + can resize container + can scale content to size
@@ -10,5 +12,6 @@ package jmri.util;
  * {@link jmri.util.swing.ResizableImagePanel} instead
  */
 @Deprecated
+@SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", justification = "Deprecated package for same class in different package.")
 public class ResizableImagePanel extends jmri.util.swing.ResizableImagePanel {
 }
