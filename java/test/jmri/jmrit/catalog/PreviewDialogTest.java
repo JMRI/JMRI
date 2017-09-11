14f8963f817b71e614f2f2b27594af9428766bb8
Paul Bender <paul.bender@acm.org>
2017-09-11 10:33:22 -0400

    new test

---
+++ java/test/jmri/jmrit/catalog/PreviewDialogTest.java
@@ -0,0 +1,48 @@
+package jmri.jmrit.catalog;
+
+import java.awt.GraphicsEnvironment;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import jmri.util.JmriJFrame;
+
+/**
+ *
+ * @author Paul Bender Copyright (C) 2017	
+ */
+public class PreviewDialogTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void testCTor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        JmriJFrame jf = new JmriJFrame("PreviewDialog test frame");
+        PreviewDialog t = new PreviewDialog(jf,"preview test",folder.getRoot(),new String[0]);
+        Assert.assertNotNull("exists",t);
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        apps.tests.Log4JFixture.setUp();
+        jmri.util.JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.resetInstanceManager();
+        apps.tests.Log4JFixture.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(PreviewDialogTest.class.getName());
+
+}
