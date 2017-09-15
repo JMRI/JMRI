88c49ec053359af385a9d8e8504bd4e928f9bf3a
Steve Rawlinson <steve@tagadab.com>
2017-09-15 16:06:28 +0100

    missing semi colon

--- java/src/jmri/jmrit/automat/AbstractAutomaton.java
+++ java/src/jmri/jmrit/automat/AbstractAutomaton.java
@@ -850,7 +850,7 @@ public class AbstractAutomaton implements Runnable {
             if (prompt != null) {
                 log.trace("waitChange continues from {}", prompt.getSource());
             } else {
-                log.trace("waitChange continues")
+                log.trace("waitChange continues");
             }
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt(); // retain if needed later
