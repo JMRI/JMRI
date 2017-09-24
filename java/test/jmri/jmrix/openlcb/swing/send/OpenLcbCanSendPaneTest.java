2760d7b8042d5a2f775aa499ce950feb402710f6
Paul Bender <paul.bender@acm.org>
2017-09-23 23:01:50 -0400

    new test

--- java/test/jmri/jmrix/openlcb/swing/send/OpenLcbCanSendPaneTest.java
+++ java/test/jmri/jmrix/openlcb/swing/send/OpenLcbCanSendPaneTest.java
@@ -4,6 +4,7 @@ import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 /**
  * @author Bob Jacobsen Copyright 2013
@@ -20,16 +21,42 @@ public class OpenLcbCanSendPaneTest {
         Assert.assertNotNull("Pane object non-null", p);
     }
 
+    @Test
+    @Ignore("Needs more setup")
+    public void testInitComponents() throws Exception{
+        OpenLcbCanSendPane p = new OpenLcbCanSendPane();
+        // for now, just makes ure there isn't an exception.
+        p.initComponents(memo);
+    }
+
+    @Test
+    @Ignore("Needs more setup")
+    public void testInitContext() throws Exception {
+        OpenLcbCanSendPane p = new OpenLcbCanSendPane();
+        // for now, just makes ure there isn't an exception.
+        p.initContext(memo);
+    }
+
+    @Test
+    public void testGetHelpTarget(){
+        OpenLcbCanSendPane p = new OpenLcbCanSendPane();
+        Assert.assertEquals("help target","package.jmri.jmrix.openlcb.swing.send.OpenLcbCanSendPane",p.getHelpTarget());
+    }
+
+    @Test
+    public void testGetTitle(){
+        OpenLcbCanSendPane p = new OpenLcbCanSendPane();
+        Assert.assertEquals("title","Send CAN Frames and OpenLCB Messages",p.getTitle());
+    }
+
     // The minimal setup for log4J
     @Before
     public void setUp() {
         JUnitUtil.setUp();
-
         memo = new jmri.jmrix.can.CanSystemConnectionMemo();
-        tc = new jmri.jmrix.can.adapters.loopback.LoopbackTrafficController();
+        tc = new jmri.jmrix.can.TestTrafficController();
         memo.setTrafficController(tc);
         memo.setProtocol(jmri.jmrix.can.ConfigurationManager.OPENLCB);
-
     }
 
     @After
