6f1e63e287bb3ae9a60ce7a36e5215df04a69893
Paul Bender <paul.bender@acm.org>
2017-09-24 23:45:27 -0400

    new tests

--- java/test/jmri/jmrix/loconet/slotmon/SlotMonPaneTest.java
+++ java/test/jmri/jmrix/loconet/slotmon/SlotMonPaneTest.java
@@ -1,5 +1,9 @@
 package jmri.jmrix.loconet.slotmon;
 
+import jmri.jmrix.loconet.LnTrafficController;
+import jmri.jmrix.loconet.LocoNetInterfaceScaffold;
+import jmri.jmrix.loconet.LocoNetSystemConnectionMemo;
+import jmri.jmrix.loconet.SlotManager;
 import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -16,6 +20,28 @@ public class SlotMonPaneTest {
     public void testCTor() {
         SlotMonPane t = new SlotMonPane();
         Assert.assertNotNull("exists",t);
+    }
+
+    @Test
+    public void testGetHelpTarget() {
+        SlotMonPane t = new SlotMonPane();
+        Assert.assertEquals("help target","package.jmri.jmrix.loconet.slotmon.SlotMonFrame",t.getHelpTarget());
+    }
+
+    @Test
+    public void testGetTitle() {
+        SlotMonPane t = new SlotMonPane();
+        Assert.assertEquals("title","Monitor Slots",t.getTitle());
+    }
+
+    @Test
+    public void testInitComponents() {
+        SlotMonPane t = new SlotMonPane();
+        LnTrafficController lnis = new LocoNetInterfaceScaffold();
+        SlotManager slotmanager = new SlotManager(lnis);
+        LocoNetSystemConnectionMemo memo = new LocoNetSystemConnectionMemo(lnis,slotmanager);
+        // we are just making sure that initComponents doesn't cause an exception.
+        t.initComponents(memo);
     }
 
     // The minimal setup for log4J
