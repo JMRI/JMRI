ebca2e68c6dd9354b2822a5b7d2dbdb55aebd64d
Bob Jacobsen <jacobsen@mac.com>
2017-10-06 06:18:39 -0700

    allow Jenkins tidy-scan of HTML to focus on one language

--- scripts/tidy-scan.sh
+++ scripts/tidy-scan.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
-# scan the English help files for HMTL errors, used in Jenkins
+# scan the help files for HMTL errors, used in Jenkins
+# if an argument is provided, i.e. "help/en/*/" scan there, otherwise scan all the help
 
-find help/*/*/ -name \*html -exec echo Filename: {} \; -exec tidy -e -access 0 {} \; 2>&1 | grep -v '<table> lacks "summary" attribute' | grep -v '<img> lacks "alt" attribute' | tr '<' '&lt;' | tr '>' '&gt;' | awk -f scripts/tidy.awk
+if [[ "$@" == "" ]] ; then
+    WHERE=help/*/*/
+else
+    WHERE=$@
+fi
+
+find ${WHERE} -name \*html -exec echo Filename: {} \; -exec tidy -e -access 0 {} \; 2>&1 | grep -v '<table> lacks "summary" attribute' | grep -v '<img> lacks "alt" attribute' | tr '<' '&lt;' | tr '>' '&gt;' | awk -f scripts/tidy.awk
