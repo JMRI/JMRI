e0ac189c3b814f35ce2436e5fb76bdbdfa8fac95
Steve G <RailRodder@sympatico.ca>
2017-10-08 15:50:47 -0400

    Use warn message not error.

--- java/src/jmri/jmrix/loconet/LnThrottleManager.java
+++ java/src/jmri/jmrix/loconet/LnThrottleManager.java
@@ -185,7 +185,7 @@ public class LnThrottleManager extends AbstractThrottleManager implements Thrott
             log.debug("Attempting to update slot with this JMRI instance's throttle id ({})", throttleID);
             tc.sendLocoNetMessage(s.writeThrottleID(throttleID));
         } else {
-            log.error("Slot [{}} has not gone IN-USE in required time, slot is in unreliable state.",s.getSlot());
+            log.warn("Slot [{}} has not gone IN-USE in required time, slot is in unreliable state.",s.getSlot());
         }
 
         s.notifySlotListeners();    // make sure other listeners for this slot know about what's going on!
