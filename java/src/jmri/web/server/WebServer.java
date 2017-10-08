91326a4941aa1563c00f0b44d6e98f404ba337f8
Paul Bender <paul.bender@acm.org>
2017-10-07 21:44:36 -0400

    stop publishing the zeroconf service when the server stops.

--- java/src/jmri/web/server/WebServer.java
+++ java/src/jmri/web/server/WebServer.java
@@ -144,6 +144,7 @@ public final class WebServer implements LifeCycle, LifeCycle.Listener {
     @Override
     public void stop() throws Exception {
         server.stop();
+        zeroConfService.stop();
     }
 
     /**
