316c060d7467b948613b63c2d0187ec3466c2f7b
Paul Bender <paul.bender@acm.org>
2017-10-11 21:31:23 -0400

    more throttle related tests

--- java/test/jmri/jmrix/loconet/LnThrottleManagerTest.java
+++ java/test/jmri/jmrix/loconet/LnThrottleManagerTest.java
@@ -10,21 +10,21 @@ import org.junit.Test;
  *
  * @author Paul Bender Copyright (C) 2017	
  */
-public class LnThrottleManagerTest {
+public class LnThrottleManagerTest extends jmri.managers.AbstractThrottleManagerTestBase {
 
     @Test
     public void testCTor() {
-        LnTrafficController lnis = new LocoNetInterfaceScaffold();
-        SlotManager slotmanager = new SlotManager(lnis);
-        LocoNetSystemConnectionMemo memo = new LocoNetSystemConnectionMemo(lnis,slotmanager);
-        LnThrottleManager t = new LnThrottleManager(memo);
-        Assert.assertNotNull("exists",t);
+        Assert.assertNotNull("exists",tm);
     }
 
     // The minimal setup for log4J
     @Before
     public void setUp() {
         JUnitUtil.setUp();
+        LnTrafficController lnis = new LocoNetInterfaceScaffold();
+        SlotManager slotmanager = new SlotManager(lnis);
+        LocoNetSystemConnectionMemo memo = new LocoNetSystemConnectionMemo(lnis,slotmanager);
+        tm = new LnThrottleManager(memo);
     }
 
     @After
