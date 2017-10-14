406115e79a16400e5548b833760b1c2a44e1fd02
Balazs Racz <balazs.racz@gmail.com>
2017-10-14 17:57:31 +0200

    Adds a helper class for deduping property change notifications.

---
+++ java/test/jmri/util/swing/DedupingPropertyChangeListenerTest.java
@@ -0,0 +1,30 @@
+package jmri.util.swing;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import jmri.util.JUnitUtil;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bracz on 10/14/17.
+ */
+public class DedupingPropertyChangeListenerTest {
+    @Before
+    public void setUp() throws Exception {
+        JUnitUtil.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        JUnitUtil.tearDown();
+    }
+
+    @Test
+    public void propertyChange() throws Exception {
+
+    }
+
+}
