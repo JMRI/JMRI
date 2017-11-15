14df807863a05199aab5fee319ce4f59b4c722b9
Bob Jacobsen <jacobsen@mac.com>
2017-11-15 12:25:10 -0800

    javadoc fix

--- java/src/jmri/jmrit/display/layoutEditor/LayoutEditor.java
+++ java/src/jmri/jmrit/display/layoutEditor/LayoutEditor.java
@@ -2104,7 +2104,7 @@ public class LayoutEditor extends PanelEditor implements VetoableChangeListener,
     /**
      * set the maximum number of rows based on screen size
      *
-     * @param inComboBox
+     * @param inComboBox JComboxBox to be set up
      */
     public static void setupComboBoxMaxRows(@Nonnull JmriBeanComboBox inComboBox) {
         // find the max height of all popup items
