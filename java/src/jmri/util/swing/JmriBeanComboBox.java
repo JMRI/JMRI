cf1b1943268c45e893f649676a6db516ca3c67ea
Bob Jacobsen <jacobsen@mac.com>
2017-11-18 20:17:13 -0800

    refactor location of JComboBox static service method

--- java/src/jmri/util/swing/JmriBeanComboBox.java
+++ java/src/jmri/util/swing/JmriBeanComboBox.java
@@ -848,32 +848,6 @@ public class JmriBeanComboBox extends JComboBox<String> implements java.beans.Pr
         }
     }
 
-    public static <T extends javax.swing.JComboBox> void setupComboBoxMaxRows(T inComboBox) {
-        // find the max height of all popup items
-        BasicComboPopup popup = (BasicComboPopup) inComboBox.getAccessibleContext().getAccessibleChild(0);
-        JList list = popup.getList();
-        ListModel lm = list.getModel();
-        ListCellRenderer renderer = list.getCellRenderer();
-        int maxItemHeight = 12; // pick some absolute minimum here
-        for (int i = 0; i < lm.getSize(); ++i) {
-            Object value = lm.getElementAt(i);
-            Component c = renderer.getListCellRendererComponent(list, value, i, false, false);
-            maxItemHeight = Math.max(maxItemHeight, c.getPreferredSize().height);
-        }
-
-        int itemsPerScreen = inComboBox.getItemCount();
-        // calculate the number of items that will fit on the screen
-        if (!GraphicsEnvironment.isHeadless()) {
-            // note: this line returns the maximum available size, accounting all
-            // taskbars etc. no matter where they are aligned:
-            Rectangle maxWindowBounds = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
-            itemsPerScreen = (int) maxWindowBounds.getHeight() / maxItemHeight;
-        }
-
-        int c = Math.min(itemsPerScreen, inComboBox.getItemCount());
-        inComboBox.setMaximumRowCount(c);
-    }
-
     private final static Logger log = LoggerFactory.getLogger(JmriBeanComboBox.class);
 
 }
