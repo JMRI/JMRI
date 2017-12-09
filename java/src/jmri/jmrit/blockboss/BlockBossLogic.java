3499666f493debe047d4dff03252863dbd701d90
Bob Jacobsen <jacobsen@mac.com>
2017-12-09 14:10:43 -0800

    fix PR#4516 on this branch

--- java/src/jmri/jmrit/blockboss/BlockBossLogic.java
+++ java/src/jmri/jmrit/blockboss/BlockBossLogic.java
@@ -167,11 +167,11 @@ public class BlockBossLogic extends Siglet implements java.beans.VetoableChangeL
      * @return system name of the driven signal head
      */
     public String getDrivenSignal() {
+        if (driveSignal == null) return null;
         return driveSignal.getName();
     }
 
     public NamedBeanHandle<SignalHead> getDrivenSignalNamedBean() {
-        if (driveSignal == null) return null;
         return driveSignal;
     }
 
