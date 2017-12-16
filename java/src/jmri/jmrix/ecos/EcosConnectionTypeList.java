47c660521a33c0371b3354131490d258ffe0908b
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-12-16 08:51:04 -0500

    Demonstrate a plugin capability.
    Demonstrate that multiple ConnectionTypeLists for a single manufacturer can be maintained. This capability allows the development of new connection types for a single manufacturer from outside JMRI.

--- java/src/jmri/jmrix/ecos/EcosConnectionTypeList.java
+++ java/src/jmri/jmrix/ecos/EcosConnectionTypeList.java
@@ -4,7 +4,11 @@ import jmri.jmrix.ConnectionTypeList;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
- * Returns a list of valid ESU Ecos Connection Types
+ * Maintains a list of valid ESU ECoS Connection Types. Note that
+ * {@link jmri.jmrix.ecos.simulator.EcosSimulatorConnectionTypeList} also
+ * provides a list of of valid ESU ECoS Connection Types. Using two different
+ * lists of valid {@link #ESU} Connection Types demonstrates the capability to
+ * add new Connection Types for an existing manufacturer using third-party JARs.
  *
  * @author Bob Jacobsen Copyright (C) 2010
  * @author Kevin Dickerson Copyright (C) 2010
@@ -18,8 +22,7 @@ public class EcosConnectionTypeList implements jmri.jmrix.ConnectionTypeList {
     @Override
     public String[] getAvailableProtocolClasses() {
         return new String[]{
-            "jmri.jmrix.ecos.networkdriver.ConnectionConfig",
-            "jmri.jmrix.ecos.simulator.EcosSimulatorConnectionConfig"};
+            "jmri.jmrix.ecos.networkdriver.ConnectionConfig"};
     }
 
     @Override
