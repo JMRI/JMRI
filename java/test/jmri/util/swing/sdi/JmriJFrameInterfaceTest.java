c3e02fb45fb9fb8631c160cc6813ee6df602230c
Paul Bender <paul.bender@acm.org>
2017-12-19 00:44:17 -0500

    remove duplicative test

--- java/test/jmri/util/swing/sdi/JmriJFrameInterfaceTest.java
+++ java/test/jmri/util/swing/sdi/JmriJFrameInterfaceTest.java
@@ -1,5 +1,6 @@
 package jmri.util.swing.sdi;
 
+import java.awt.GraphicsEnvironment;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import jmri.util.JUnitUtil;
@@ -21,19 +22,6 @@ public class JmriJFrameInterfaceTest {
     public void testCTor() {
         JmriJFrameInterface t = new JmriJFrameInterface();
         Assert.assertNotNull("exists",t);
-    }
-
-    @Test
-    public void testAction() {
-        JmriJFrame f = new JmriJFrame("SDI test");
-        JButton b = new JButton(new ButtonTestAction(
-                "new frame", new jmri.util.swing.sdi.JmriJFrameInterface()));
-        f.add(b);
-        f.pack();
-        f.setVisible(true);
-        JFrame f2 = jmri.util.JmriJFrame.getFrame("SDI test");
-        Assert.assertTrue("found frame", f2 != null);
-        JUnitUtil.dispose(f2);
     }
 
     // The minimal setup for log4J
