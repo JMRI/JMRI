c8d5bf58969b95a4244a053ac70bc45935c04ace
Bob Jacobsen <jacobsen@mac.com>
2017-12-20 22:11:03 -0800

    test of system name, missing terminal match

--- java/test/jmri/util/AlphanumComparatorTest.java
+++ java/test/jmri/util/AlphanumComparatorTest.java
@@ -1,5 +1,6 @@
 package jmri.util;
 
+import java.util.Comparator;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import org.junit.After;
@@ -14,6 +15,31 @@ import org.junit.Before;
 public class AlphanumComparatorTest extends TestCase {
 
     private static AlphanumComparator ac;
+
+    public void testAlphanumCompareEquals() {
+        Assert.assertEquals(" 1 == 1", 0, ac.compare("1", "1"));
+        Assert.assertEquals(" 10 == 10", 0, ac.compare("10", "10"));
+        Assert.assertEquals(" 100 == 100", 0, ac.compare("100", "100"));
+    }
+
+    public void testAlphanumCompareNumbersGreater() {
+        Assert.assertEquals(" 1 > 0", 1, ac.compare("1", "0"));
+        Assert.assertEquals(" 10 > 2", 1, ac.compare("10", "2"));
+        Assert.assertEquals(" 2 > 1", 1, ac.compare("2", "1"));
+    }
+
+    public void testAlphanumCompareNumbersLesser() {
+        Assert.assertEquals(" 1 < 2", -1, ac.compare("1", "2"));
+        Assert.assertEquals(" 1 < 10", -1, ac.compare("1", "10"));
+        Assert.assertEquals(" 2 < 10 ", -1, ac.compare("2", "10"));
+    }
+
+    public void testAlphanumCompareNestedNumeric() {
+        Assert.assertEquals(" 1.1.0 < 2.1.0", -1, ac.compare("1.1.0", "2.1.0"));
+        Assert.assertEquals(" 1.1.1 == 1.1.1", 0, ac.compare("1.1.1", "1.1.1"));
+        Assert.assertEquals(" 2.1.0 > 1.1.0", 1, ac.compare("2.1.0", "1.1.0"));
+    }
+
 
     public void testAlphanumCompareALTB() {
         Assert.assertEquals("A < B", ac.compare("A", "B") < 0, true);
@@ -63,6 +89,13 @@ public class AlphanumComparatorTest extends TestCase {
         Assert.assertEquals("A10Z10 > A10Z2", ac.compare("A10Z10", "A10Z2") > 0, true);
     }
 
+    public void testMixedComparison() {     
+        System.out.println("start");   
+        Assert.assertEquals("IS100 < IS100A", -1, ac.compare("IS100", "IS100A"));
+        Assert.assertEquals("IS100A > IS100", +1, ac.compare("IS100A", "IS100"));
+    }
+
+    
     // from here down is testing infrastructure
     @Before
     protected void setUp() throws Exception {
