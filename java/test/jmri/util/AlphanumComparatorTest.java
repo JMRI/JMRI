992254116f06143f820cafb835fc979d2a524dfa
Bob Jacobsen <jacobsen@mac.com>
2017-12-21 07:08:05 -0800

    more tests of hex, CV syntax

--- java/test/jmri/util/AlphanumComparatorTest.java
+++ java/test/jmri/util/AlphanumComparatorTest.java
@@ -16,6 +16,10 @@ public class AlphanumComparatorTest extends TestCase {
 
     private static AlphanumComparator ac;
 
+    public void testAlphanumCompare1LTA() {
+        Assert.assertEquals("1 < A", ac.compare("1", "A") < 0, true);
+    }
+
     public void testAlphanumCompareEquals() {
         Assert.assertEquals(" 1 == 1", 0, ac.compare("1", "1"));
         Assert.assertEquals(" 10 == 10", 0, ac.compare("10", "10"));
@@ -38,8 +42,24 @@ public class AlphanumComparatorTest extends TestCase {
         Assert.assertEquals(" 1.1.0 < 2.1.0", -1, ac.compare("1.1.0", "2.1.0"));
         Assert.assertEquals(" 1.1.1 == 1.1.1", 0, ac.compare("1.1.1", "1.1.1"));
         Assert.assertEquals(" 2.1.0 > 1.1.0", 1, ac.compare("2.1.0", "1.1.0"));
+
+        Assert.assertEquals(" 1.10.0 > 1.2.0", 1, ac.compare("1.10.0", "1.2.0"));
+        Assert.assertEquals(" 1.2.0 < 1.10.0", -1, ac.compare("1.2.0", "1.10.0"));
+        
+        // non-intuitive, but what it does
+        Assert.assertEquals(" 1.1.10 < 1.1.2", 1, ac.compare("1.1.10", "1.1.2"));
+        
     }
 
+    public void testHexadecimal() {
+        Assert.assertEquals(" A0 < B0", -1, ac.compare("A0", "B0"));
+
+        Assert.assertEquals(" 21.1F > 21.10", 1, ac.compare("21.1F", "21.10"));
+        Assert.assertEquals(" 21.A0 < 21.B0", -1, ac.compare("21.A0", "21.B0"));
+
+        // non-intuitive, but what it does
+        Assert.assertEquals(" 1F < 10", -1, ac.compare("1F", "10"));
+    }
 
     public void testAlphanumCompareALTB() {
         Assert.assertEquals("A < B", ac.compare("A", "B") < 0, true);
