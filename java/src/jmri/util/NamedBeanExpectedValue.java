203a579f58f46993e8a008e41b2b596af4ff787d
Randall Wood <randall.h.wood@alexandriasoftware.com>
2017-12-31 17:50:35 -0500

    Refactor NamedBeanExpected{State,Value}
    NamedBeanExpectedState is now a special case of NamedBeanExpectedValue which only accepts non-null state.
    NamedBeanExpectedValue holds the NamedBean in a NamedBeanHandle.

--- java/src/jmri/util/NamedBeanExpectedValue.java
+++ java/src/jmri/util/NamedBeanExpectedValue.java
@@ -1,11 +1,18 @@
 package jmri.util;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import jmri.ExpectedState;
+import jmri.InstanceManager;
 import jmri.NamedBean;
+import jmri.NamedBeanHandle;
+import jmri.NamedBeanHandleManager;
 import jmri.beans.Bean;
 
 /**
  * Retain a NamedBean and its expected value (called a state in this class).
+ * <p>
+ * Note the NamedBean is held in a {@link NamedBeanHandle}.
  *
  * @author Randall Wood Copyright 2017
  * @param <T> the supported type of NamedBean
@@ -13,12 +20,16 @@ import jmri.beans.Bean;
  */
 public class NamedBeanExpectedValue<T extends NamedBean, S extends Object> extends Bean implements ExpectedState<T, S> {
 
-    private final T bean;
+    private final NamedBeanHandle<T> handle;
     private S state;
 
-    public NamedBeanExpectedValue(T bean, S state) {
-        this.bean = bean;
-        NamedBeanExpectedValue.this.setExpectedState(state);
+    public NamedBeanExpectedValue(@Nonnull T bean, @Nonnull String name, @Nullable S state) {
+        this.handle = InstanceManager.getDefault(NamedBeanHandleManager.class).getNamedBeanHandle(name, bean);
+        this.state = state;
+    }
+
+    public NamedBeanExpectedValue(@Nonnull T bean, S state) {
+        this(bean, bean.getDisplayName(), state);
     }
 
     @Override
@@ -35,7 +46,10 @@ public class NamedBeanExpectedValue<T extends NamedBean, S extends Object> exten
 
     @Override
     public T getObject() {
-        return this.bean;
+        return this.handle.getBean();
     }
 
+    public String getName() {
+        return this.handle.getName();
+    }
 }
