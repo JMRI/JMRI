32ff496677f287129ac03519149e117cd22a680a
Randall Wood <randall.h.wood@alexandriasoftware.com>
2018-01-01 13:52:00 -0500

    jmri.util.NamedBeanHandle is a jmri.NamedBeanHandle
    Since I do not know if the intention was that jmri.NamedBeanHandle be replaced by or replaces jmri.util.NamedBeanHandle, this is not deprecated.

--- java/src/jmri/util/NamedBeanHandle.java
+++ java/src/jmri/util/NamedBeanHandle.java
@@ -1,25 +1,16 @@
 package jmri.util;
 
+import jmri.NamedBean;
+
 /**
  * Utility class for managing access to a NamedBean
  *
  * @author Bob Jacobsen Copyright 2009
+ * @param <T> the type of handled NamedBean
  */
-public class NamedBeanHandle<T> {
+public class NamedBeanHandle<T extends NamedBean> extends jmri.NamedBeanHandle<T> {
 
     public NamedBeanHandle(String name, T bean) {
-        this.name = name;
-        this.bean = bean;
+        super(name, bean);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public T getBean() {
-        return bean;
-    }
-
-    String name;
-    T bean;
 }
