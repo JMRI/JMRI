a19846836ab3de547f6393173d9b6f2e79b13105
Bob Jacobsen <jacobsen@mac.com>
2018-01-23 10:08:58 -0800

    more tests

--- java/test/jmri/jmrit/beantable/signalmast/SignalMastAddPaneTest.java
+++ java/test/jmri/jmrit/beantable/signalmast/SignalMastAddPaneTest.java
@@ -15,17 +15,27 @@ public class SignalMastAddPaneTest {
 
     @Test
     public void testLoad() {
-    
+        // ground these in a single test, as the services can only be loaded once.
         Assert.assertNotNull(SignalMastAddPane.getInstancesCollection());
         Assert.assertNotNull(SignalMastAddPane.getInstancesMap());
         Assert.assertTrue(SignalMastAddPane.getInstancesMap().size() > 0); // found something
+        Assert.assertEquals(SignalMastAddPane.getInstancesMap().size(), SignalMastAddPane.getInstancesCollection().size());
         
-        // check sorted order
+        // check map sorted order, lookup
         Map<String, SignalMastAddPane> map = SignalMastAddPane.getInstancesMap();
+        Collection<SignalMastAddPane> collection = SignalMastAddPane.getInstancesCollection();
         String last = "";
         for (String name : map.keySet()) {
             Assert.assertTrue(name.compareTo(last) > 0);  // no identical ones
+            Assert.assertTrue(collection.contains(map.get(name)));
             last = name;
+        }
+
+        // check collection sorted order
+        last = "";
+        for (SignalMastAddPane pane : collection) {
+            Assert.assertTrue(pane.getPaneName().compareTo(last) > 0);  // no identical ones
+            last = pane.getPaneName();
         }
     }
 
