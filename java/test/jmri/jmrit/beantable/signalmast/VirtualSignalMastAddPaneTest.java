daaacf45f56af2aa327ffabcc802374aff90903b
Bob Jacobsen <jacobsen@mac.com>
2018-01-28 20:59:07 -0800

    test create now OK

--- java/test/jmri/jmrit/beantable/signalmast/VirtualSignalMastAddPaneTest.java
+++ java/test/jmri/jmrit/beantable/signalmast/VirtualSignalMastAddPaneTest.java
@@ -19,13 +19,25 @@ public class VirtualSignalMastAddPaneTest {
     @Test
     public void testSetMast() {
         VirtualSignalMast s1 = new VirtualSignalMast("IF$vsm:basic:one-searchlight($1)", "user name");
+        MatrixSignalMast m1 = new MatrixSignalMast("IF$xsm:basic:one-low($0001)-3t", "user");
 
         VirtualSignalMastAddPane vp = new VirtualSignalMastAddPane();
-        MatrixSignalMast m1 = new MatrixSignalMast("IF$xsm:basic:one-low($0001)-3t", "user");
         
         Assert.assertFalse(vp.setMast(null));
         Assert.assertTrue(vp.setMast(s1));
         Assert.assertFalse(vp.setMast(m1));
+        
+    }
+
+    @Test
+    public void testCreateMast() {
+        VirtualSignalMastAddPane vp = new VirtualSignalMastAddPane();
+
+        vp.createMast("AAR-1946", "appearance-PL-2-high.xml", "user name");
+                
+        Assert.assertNotNull(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name"));
+        System.err.println("ref "+InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name").getSystemName());
+        Assert.assertNotNull(InstanceManager.getDefault(jmri.SignalMastManager.class).getBySystemName("IF$vsm:AAR-1946:PL-2-high($0001)"));
         
     }
 
