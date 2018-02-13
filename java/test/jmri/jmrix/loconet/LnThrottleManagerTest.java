378122a7dfe88258db0387430f593eff6d0ffd06
devel-bobm <devel-bobm@sbcglobal.net>
2018-02-13 17:10:03 -0500

    add multiple throttles with steal test

--- java/test/jmri/jmrix/loconet/LnThrottleManagerTest.java
+++ java/test/jmri/jmrix/loconet/LnThrottleManagerTest.java
@@ -531,7 +531,7 @@ public class LnThrottleManagerTest extends jmri.managers.AbstractThrottleManager
 
 
 
-        @Test
+    @Test
     public void testShareSingleLnThrottleScenario1() {
         // test case:
         // . acquire loco X to "throttle"
@@ -634,7 +634,7 @@ public class LnThrottleManagerTest extends jmri.managers.AbstractThrottleManager
         throtListen2 = null;
     }
 
-        @Test
+    @Test
     public void testShareSingleLnThrottleScenario2() {
         // test case:
         // . acquire loco X to "throttle"
@@ -759,7 +759,7 @@ public class LnThrottleManagerTest extends jmri.managers.AbstractThrottleManager
         throtListen2 = null;
     }
 
-        @Test
+    @Test
     public void testShareSingleLnThrottleScenario3() {
         // test case:
         // . acquire loco X to "throttle"
@@ -911,6 +911,124 @@ public class LnThrottleManagerTest extends jmri.managers.AbstractThrottleManager
         throtListen = null;
         throtListen2 = null;
         throtListen3 = null;
+    }
+
+    @Test
+    public void testShareSingleLnThrottleScenario4() {
+        // test case:
+        // . attempt acquire loco X to "throttle"
+        // . notified that steal is required
+        // . steal is agreed to by throttle user
+        // . acquisition is completed under steal protocol
+        // . acquire loco X to "throttle2"
+        // . (Steal not required!)
+        // . set speed for loco X
+        // . release "throttle2"
+        // . set speed for loco X
+        // . release "throttle"
+
+        throttle = null;
+        tm = memo.throttleManager;
+
+        ThrottleListener throtListen = new ThrottleListener() {
+            @Override
+            public void notifyThrottleFound(DccThrottle t) {
+                throttle = t;
+                log.error("created a throttle");
+            }
+
+            @Override
+            public void notifyFailedThrottleRequest(jmri.LocoAddress address, String reason) {
+                log.error("Throttle request failed for " + address + " because " + reason);
+                failedThrottleRequest = true;
+            }
+
+            @Override
+            public void notifyStealThrottleRequired(jmri.LocoAddress address){
+                // this is a never-stealing impelementation.
+                flagGotStealRequest = address.getNumber();
+                log.debug("going to steal loco {}", address);
+                tm.stealThrottleRequest(address, this, true);
+            }
+        };
+
+        ThrottleListener throtListen2 = new ThrottleListener() {
+            @Override
+            public void notifyThrottleFound(DccThrottle t) {
+                throttle2 = t;
+                log.error("created a throttle2");
+            }
+
+            @Override
+            public void notifyFailedThrottleRequest(jmri.LocoAddress address, String reason) {
+                log.error("Throttle2 request failed for " + address + " because " + reason);
+                failedThrottleRequest2 = true;
+            }
+
+            @Override
+            public void notifyStealThrottleRequired(jmri.LocoAddress address){
+                // this is a never-stealing impelementation.
+                flagGotStealRequest2 = address.getNumber();
+                log.debug("Throttle2 going to steal loco {}", address);
+                tm.stealThrottleRequest(address, this, false);
+            }
+        };
+
+        tm.requestThrottle(262, throtListen);
+
+        Assert.assertEquals("address request message",
+                "BF 02 06 00",
+                lnis.outbound.elementAt(lnis.outbound.size() - 1).toString());
+        memo.getSlotManager().message(lnis.outbound.elementAt(lnis.outbound.size()-1));
+
+        Assert.assertEquals("count is correct", 1, lnis.outbound.size());
+        LocoNetMessage cmdStationReply = new LocoNetMessage(new int[] {
+                0xe7, 0x0e, 0x0B, 0x30, 0x06, 0x0, 0x0, 0x7, 0x0, 0x02, 0x00, 0x13, 0x01, 0x53});  // slot is in-use
+        lnis.sendTestMessage(cmdStationReply);
+
+        Assert.assertEquals("Got a steal request", 262, flagGotStealRequest);
+        jmri.util.JUnitAppender.assertWarnMessage("slot 11 address 262 is already in-use.");
+        Assert.assertNotNull("Throttle should be created and non-null", throttle);
+        jmri.util.JUnitAppender.assertWarnMessage("user agreed to steal address 262, but no code is in-place to handle the 'steal' (yet)");
+        jmri.util.JUnitAppender.assertErrorMessage("created a throttle");
+
+        int netTxMsgCount = lnis.outbound.size()-1;
+
+        tm.requestThrottle(262, throtListen2);  // An additional user of the same throttle
+        jmri.util.JUnitAppender.assertErrorMessage("created a throttle2");
+
+        Assert.assertNotNull("Throttle should be created and non-null", throttle2);
+        Assert.assertEquals("both throttle users point to the same throttle object",
+                throttle, throttle2);
+        Assert.assertEquals("no new LocoNet traffic generated", netTxMsgCount, lnis.outbound.size()-1);
+
+        throttle.setSpeedSetting(0.1f);
+        Assert.assertEquals("sent speed message",
+                "A0 0B 17 00", lnis.outbound.elementAt(lnis.outbound.size() -1). toString());
+        Assert.assertEquals("only one new loconet message", netTxMsgCount +1, lnis.outbound.size()-1);
+        tm.releaseThrottle(throttle2, throtListen2);
+        Assert.assertEquals("No loconet traffic at throttle2 release", netTxMsgCount +1, lnis.outbound.size()-1);
+
+        Assert.assertTrue("Address still required",
+                InstanceManager.throttleManagerInstance().addressStillRequired(new DccLocoAddress(262, true)));
+
+        Assert.assertEquals("got correct number of LocoNet messages", netTxMsgCount+1, lnis.outbound.size()-1);
+        throttle.setSpeedSetting(0.2f);
+        Assert.assertEquals("got correct number of LocoNet messages", netTxMsgCount+2, lnis.outbound.size()-1);
+        Assert.assertEquals("sent speed message",
+                "A0 0B 22 00", lnis.outbound.elementAt(lnis.outbound.size() -1). toString());
+
+        tm.releaseThrottle(throttle, throtListen);
+
+        Assert.assertFalse("Address no longer required",
+                InstanceManager.throttleManagerInstance().addressStillRequired(new DccLocoAddress(262, true)));
+
+        Assert.assertEquals("One loconet message sent at throttle release", netTxMsgCount +3, lnis.outbound.size()-1);
+        Assert.assertEquals("sent set slot status to COMMON message",
+                "B5 0B 10 00", lnis.outbound.elementAt(lnis.outbound.size() -1). toString());
+
+        throtListen = null;
+        throtListen2 = null;
     }
 
     LocoNetInterfaceScaffold lnis;
