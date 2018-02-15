76eea2b7149e2fdb2720c0b7c8478937a3335700
Randall Wood <randall.h.wood@alexandriasoftware.com>
2018-02-15 07:30:40 -0500

    Remove last message type not handled by a service.
    There are some special cases in onMessage(), but those are related to closing the connection and ensuring services get complete messages.

--- java/test/jmri/server/json/JsonClientHandlerTest.java
+++ java/test/jmri/server/json/JsonClientHandlerTest.java
@@ -7,17 +7,10 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
-import jmri.InstanceManager;
-import jmri.Version;
-import jmri.jmris.json.JsonServerPreferences;
 import jmri.profile.NullProfile;
-import jmri.profile.Profile;
-import jmri.profile.ProfileManager;
 import jmri.util.FileUtil;
 import jmri.util.JUnitAppender;
 import jmri.util.JUnitUtil;
-import jmri.util.node.NodeIdentity;
-import jmri.web.server.WebServerPreferences;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -179,29 +172,6 @@ public class JsonClientHandlerTest {
         Assert.assertTrue("Error response is an object", root.isObject());
         Assert.assertEquals("Error response is an ERROR", JsonException.ERROR, root.path(JSON.TYPE).asText());
         Assert.assertEquals("Error response is type 400", 400, data.path(JsonException.CODE).asInt());
-    }
-
-    /**
-     * Test of sendHello method, of class JsonClientHandler.
-     */
-    @Test
-    public void testSendHello() throws Exception {
-        JsonMockConnection connection = new JsonMockConnection((DataOutputStream) null);
-        JsonClientHandler instance = new TestJsonClientHandler(connection);
-        instance.sendHello(1000);
-        JsonNode root = connection.getMessage();
-        JsonNode data = root.path(JSON.DATA);
-        Assert.assertEquals("Hello type", JSON.HELLO, root.path(JSON.TYPE).asText());
-        Assert.assertEquals("JMRI Version", Version.name(), data.path(JSON.JMRI).asText());
-        Assert.assertEquals("JSON Verson", JSON.JSON_PROTOCOL_VERSION, data.path(JSON.JSON).asText());
-        Assert.assertEquals("Heartbeat", Math.round(InstanceManager.getDefault(JsonServerPreferences.class).getHeartbeatInterval() * 0.9f), data.path(JSON.HEARTBEAT).asInt());
-        Assert.assertEquals("RR Name", InstanceManager.getDefault(WebServerPreferences.class).getRailroadName(), data.path(JSON.RAILROAD).asText());
-        Assert.assertEquals("Node Identity", NodeIdentity.identity(), data.path(JSON.NODE).asText());
-        Profile profile = ProfileManager.getDefault().getActiveProfile();
-        Assert.assertNotNull(profile);
-        Assert.assertEquals("Profile", profile.getName(), data.path(JSON.ACTIVE_PROFILE).asText());
-        Assert.assertEquals("Message has 2 elements", 2, root.size());
-        Assert.assertEquals("Message data has 6 elements", 6, data.size());
     }
 
     private static class TestJsonClientHandler extends JsonClientHandler {
