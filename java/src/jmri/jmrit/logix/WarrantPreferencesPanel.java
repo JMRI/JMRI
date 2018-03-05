842850797d71f83a48bc99f6530b7aadcc7cb6eb
Bob Jacobsen <jacobsen@mac.com>
2018-03-05 10:38:05 -0800

    replace deprecated calls

--- java/src/jmri/jmrit/logix/WarrantPreferencesPanel.java
+++ java/src/jmri/jmrit/logix/WarrantPreferencesPanel.java
@@ -551,7 +551,7 @@ public class WarrantPreferencesPanel extends JPanel implements PreferencesPanel,
         if (tce != null) {
             tce.stopCellEditing();
         }
-        if (preferences.getSpeedNamesSize() != _speedNameMap.size()) {
+        if (preferences.getSpeedNames().size() != _speedNameMap.size()) {
             different = true;
         } else {
             for (int i = 0; i < _speedNameMap.size(); i++) {
@@ -631,7 +631,6 @@ public class WarrantPreferencesPanel extends JPanel implements PreferencesPanel,
     public void savePreferences() {
         setValues();
         if (_isDirty) {
-            WarrantPreferences.getDefault().apply();
             WarrantPreferences.getDefault().save();
             _isDirty = false;
         }
