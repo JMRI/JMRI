150c025f8b7845f6554289612b89d025cecc75b6
Paul Bender <paul.bender@acm.org>
2018-03-16 11:23:27 -0400

    more thread work

--- java/src/jmri/jmrit/logix/Engineer.java
+++ java/src/jmri/jmrit/logix/Engineer.java
@@ -1172,5 +1172,12 @@ public class Engineer extends Thread implements Runnable, java.beans.PropertyCha
         }
     }
 
+    /*
+     * clean up local threads and storage
+     */
+    public void dispose(){
+       _ramp.cancel(true);
+    }
+  
     private final static Logger log = LoggerFactory.getLogger(Engineer.class);
 }
