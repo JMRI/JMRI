a77b40af1ab051850625984468902b6d120011a6
Egbert Broerse <dcc2@ebroerse.nl>
2018-03-17 22:06:30 +0100

    update tests for oaktree multichar prefix

--- java/test/jmri/jmrix/oaktree/SerialPortControllerTest.java
+++ java/test/jmri/jmrix/oaktree/SerialPortControllerTest.java
@@ -16,12 +16,12 @@ public class SerialPortControllerTest extends jmri.jmrix.AbstractSerialPortContr
     @Before
     public void setUp(){
        JUnitUtil.setUp();
-       new SerialTrafficController(){
+       OakTreeSystemConnectionMemo memo = new OakTreeSystemConnectionMemo();
+       new SerialTrafficController(memo){
           @Override
           public void sendSerialMessage(SerialMessage m,SerialListener reply) {
           }
        };
-       OakTreeSystemConnectionMemo memo = new OakTreeSystemConnectionMemo();
        apc = new SerialPortController(memo){
             @Override
             public boolean status(){
