e509f3127b54ab815caddde6aa03d8844d69b05c
Randall Wood <297232+rhwood@users.noreply.github.com>
2018-03-24 07:15:37 -0400

    Fix JSON power server schema
    Also always include power manager name in JSON power messages.

--- java/test/jmri/server/json/power/JsonPowerSocketServiceTest.java
+++ java/test/jmri/server/json/power/JsonPowerSocketServiceTest.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import jmri.InstanceManager;
 import jmri.JmriException;
 import jmri.PowerManager;
+import jmri.jmris.json.JsonServerPreferences;
 import jmri.server.json.JSON;
 import jmri.server.json.JsonException;
 import jmri.server.json.JsonMockConnection;
@@ -91,6 +92,7 @@ public class JsonPowerSocketServiceTest {
         JUnitUtil.initInternalSensorManager();
         JUnitUtil.initDebugThrottleManager();
         JUnitUtil.initDebugPowerManager();
+        InstanceManager.getDefault(JsonServerPreferences.class).setValidateServerMessages(true);
     }
 
     @After
