fd0de6bc076df2021a89cfe5eec2fe0d3474020a
Randall Wood <297232+rhwood@users.noreply.github.com>
2018-03-25 13:34:14 -0400

    Allow testing that IOExceptions sending JSON messages are handled correctly.

--- java/test/jmri/server/json/JsonMockConnection.java
+++ java/test/jmri/server/json/JsonMockConnection.java
@@ -18,12 +18,25 @@ public class JsonMockConnection extends JsonConnection {
 
     private JsonNode message = null;
     private boolean open = true;
+    private boolean throwIOException = false;
 
+    /**
+     * Create a connection; by default, this connection validates all messages
+     * sent to it.
+     *
+     * @param connection the connection to use; can be null
+     */
     public JsonMockConnection(Session connection) {
         super(connection);
         InstanceManager.getDefault(JsonServerPreferences.class).setValidateServerMessages(true);
     }
 
+    /**
+     * Create a connection; by default, this connection validates all messages
+     * sent to it.
+     *
+     * @param output the output stream to use; can be null
+     */
     public JsonMockConnection(DataOutputStream output) {
         super(output);
         InstanceManager.getDefault(JsonServerPreferences.class).setValidateServerMessages(true);
@@ -32,10 +45,17 @@ public class JsonMockConnection extends JsonConnection {
     /**
      * {@inheritDoc}
      * <p>
-     * This implementation accepts a null message.
+     * This implementation accepts a null message, and if
+     * {@link #isThrowIOException()} is true throws an {@link IOException}. Note
+     * that after throwing the IOException, {@link #isThrowIOException()} will
+     * return false.
      */
     @Override
-    public void sendMessage(@Nullable JsonNode message) {
+    public void sendMessage(@Nullable JsonNode message) throws IOException {
+        if (this.throwIOException) {
+            this.throwIOException = false;
+            throw new IOException();
+        }
         if (message != null && this.preferences.getValidateServerMessages()) {
             try {
                 this.schemas.validateMessage(message, true, this.getLocale());
@@ -59,5 +79,25 @@ public class JsonMockConnection extends JsonConnection {
 
     public boolean isOpen() {
         return this.open;
+    }
+
+    /**
+     * Set if next message sent to this connection should throw an
+     * {@link IOException}.
+     *
+     * @param throwIOException true to throw on next message; false otherwise
+     */
+    public void setThrowIOException(boolean throwIOException) {
+        this.throwIOException = throwIOException;
+    }
+
+    /**
+     * Check if next message sent to this connection should throw an
+     * {@link IOException}.
+     *
+     * @return true to throw on next message; false otherwise
+     */
+    public boolean isThrowIOException() {
+        return this.throwIOException;
     }
 }
