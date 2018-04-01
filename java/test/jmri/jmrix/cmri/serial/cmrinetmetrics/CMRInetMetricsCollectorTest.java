9cb826582b30a152122844061011d2c7be9e09f7
Chuck Catania <cpcrr@charter.net>
2018-04-01 14:15:39 -0700

    Added multiple connection CMRInet metric and error data collector. Added connection memo name to all window titles " - Connection <_memo>". Fixed multiple connection Diagnostic bug. Added CTC switch heater plates, model board tower and station icons. Added CMRInet metrics tests.

---
+++ java/test/jmri/jmrix/cmri/serial/cmrinetmetrics/CMRInetMetricsCollectorTest.java
@@ -0,0 +1,44 @@
+package jmri.jmrix.cmri.serial.cmrinetmetrics;
+
+import apps.tests.Log4JFixture;
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import java.awt.GraphicsEnvironment;
+
+/**
+ * Test simple functioning of CMRInetMetricsCollector
+ *
+ * @author	Chuck Catania Copyright (C) 2017
+ */
+public class CMRInetMetricsCollectorTest {
+
+    @Test
+    public void testStringCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        CMRInetMetricsCollectorTest action = new CMRInetMetricsCollectorTest(); 
+        Assert.assertNotNull("exists", action);
+    }
+
+    @Test
+    public void testCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        CMRInetMetricsCollectorTest action = new CMRInetMetricsCollectorTest(); 
+        Assert.assertNotNull("exists", action);
+    }
+
+    @Before
+    public void setUp() {
+        Log4JFixture.setUp();
+        JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.resetInstanceManager();
+        Log4JFixture.tearDown();
+    }
+}
---
+++ java/test/jmri/jmrix/cmri/serial/cmrinetmetrics/CMRInetMetricsDataTest.java
@@ -0,0 +1,37 @@
+package jmri.jmrix.cmri.serial.cmrinetmetrics;
+
+import apps.tests.Log4JFixture;
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import java.awt.GraphicsEnvironment;
+
+/**
+ * Test simple functioning of CMRInetMetricsData
+ *
+ * @author	Chuck Catania Copyright (C) 2017
+ */
+public class CMRInetMetricsDataTest {
+
+    @Test
+    public void testMemoCtor() {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        CMRInetMetricsDataTest action = new CMRInetMetricsDataTest(); 
+        Assert.assertNotNull("exists", action);
+    }
+
+    @Before
+    public void setUp() {
+        Log4JFixture.setUp();
+        JUnitUtil.resetInstanceManager();
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.resetInstanceManager();
+        Log4JFixture.tearDown();
+    }
+}
