3a117c9946e66cb4344afde217d26d1e15db52a0
Egbert Broerse <dcc2@ebroerse.nl>
2018-04-04 08:40:42 +0200

    sort import, pull changes

--- java/src/jmri/jmrix/oaktree/packetgen/SerialPacketGenAction.java
+++ java/src/jmri/jmrix/oaktree/packetgen/SerialPacketGenAction.java
@@ -2,9 +2,9 @@ package jmri.jmrix.oaktree.packetgen;
 
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
+import jmri.jmrix.oaktree.OakTreeSystemConnectionMemo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import jmri.jmrix.oaktree.OakTreeSystemConnectionMemo;
 
 /**
  * Swing action to create and register a SerialPacketGenFrame
