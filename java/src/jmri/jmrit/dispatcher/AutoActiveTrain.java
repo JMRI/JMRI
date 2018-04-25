10d1b8dd211029257e16ae679b82f7a52b4eed47
Steve G <RailRodder@sympatico.ca>
2018-04-25 08:33:00 -0400

    reset AutoActiveTRain to origin

--- java/src/jmri/jmrit/dispatcher/AutoActiveTrain.java
+++ java/src/jmri/jmrit/dispatcher/AutoActiveTrain.java
@@ -1645,8 +1645,7 @@ public class AutoActiveTrain implements ThrottleListener {
                             }
                         }
                         // test if need to change speed
-                        _currentSpeed = _throttle.getSpeedSetting(); //use latest value
-			if (java.lang.Math.abs(_currentSpeed - _targetSpeed) > 0.001) {
+                        if (java.lang.Math.abs(_currentSpeed - _targetSpeed) > 0.001) {
                             if (_currentRampRate == RAMP_NONE) {
                                 // set speed immediately
                                 _currentSpeed = _targetSpeed;
