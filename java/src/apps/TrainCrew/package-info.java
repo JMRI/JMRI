2c18168e7c6de59044d29b49a9384d39c118d66c
Bob Jacobsen <jacobsen@mac.com>
2018-05-26 16:15:26 -0700

    Add Install TrainCrew app action and support files

---
+++ java/src/apps/TrainCrew/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * JMRI support for the TrainCrew application.
+ * <p>
+ * For more info on TrainCrew, see
+ * <a href="http://traincrew.conrail1285.com">http://traincrew.conrail1285.com</a>
+ * and
+ * <a href="https://github.com/ekapus/TrainCrew">https://github.com/ekapus/TrainCrew</a>.
+ *
+ * </ul>
+ *
+ */
+package apps.TrainCrew;
---
+++ java/test/jmri/util/UnzipFileClassTest.java
@@ -0,0 +1,16 @@
+package jmri.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for the UnzipFileClassTest class
+ *
+ * @author Bob Jacobsen Copyright (C) 2018
+ */
+public class UnzipFileClassTest  {
+
+    @Test public void testCtor() {
+        new UnzipFileClassTest();
+    }
+}
