bc6b2dbda3077b94db88467ba64660ff14bb8967
DanielBoudreau <daboudreau@hotmail.com>
2018-06-06 00:46:19 -0400

    Fix test

--- java/test/jmri/jmrit/operations/trains/TrainsTableFrameTest.java
+++ java/test/jmri/jmrit/operations/trains/TrainsTableFrameTest.java
@@ -3,6 +3,12 @@ package jmri.jmrit.operations.trains;
 import java.awt.GraphicsEnvironment;
 import jmri.InstanceManager;
 import jmri.jmrit.operations.OperationsSwingTestCase;
+import jmri.jmrit.operations.locations.LocationManager;
+import jmri.jmrit.operations.rollingstock.cars.Car;
+import jmri.jmrit.operations.rollingstock.cars.CarManager;
+import jmri.jmrit.operations.rollingstock.engines.Engine;
+import jmri.jmrit.operations.rollingstock.engines.EngineManager;
+import jmri.jmrit.operations.routes.RouteManager;
 import jmri.util.JUnitUtil;
 import jmri.util.JmriJFrame;
 import org.junit.After;
@@ -82,15 +88,61 @@ public class TrainsTableFrameTest extends OperationsSwingTestCase {
     // The minimal setup for log4J
     @Override
     @Before
-    public void setUp() {
-        JUnitUtil.setUp();
+    public void setUp() throws Exception {
+        super.setUp();
+        loadTrains();
     }
 
-    @Override
-    @After
-    public void tearDown() {
-        JUnitUtil.tearDown();
-    }
+     private void loadTrains() {
+         // Add some cars for the various tests in this suite
+         CarManager cm = InstanceManager.getDefault(CarManager.class);
+         String roadNames[] = Bundle.getMessage("carRoadNames").split(",");
+         // add caboose to the roster
+         Car c = cm.newCar(roadNames[2], "687");
+         c.setCaboose(true);
+         c = cm.newCar("CP", "435");
+         c.setCaboose(true);
+
+         // load engines
+         EngineManager emanager = InstanceManager.getDefault(EngineManager.class);
+         Engine e1 = emanager.newEngine("E", "1");
+         e1.setModel("GP40");
+         Engine e2 = emanager.newEngine("E", "2");
+         e2.setModel("GP40");
+         Engine e3 = emanager.newEngine("UP", "3");
+         e3.setModel("GP40");
+         Engine e4 = emanager.newEngine("UP", "4");
+         e4.setModel("FT");
+
+         TrainManager tmanager = InstanceManager.getDefault(TrainManager.class);
+         // turn off build fail messages
+         tmanager.setBuildMessagesEnabled(true);
+         // turn off print preview
+         tmanager.setPrintPreviewEnabled(false);
+
+         // load 5 trains
+         for (int i = 0; i < 5; i++) {
+             tmanager.newTrain("Test_Train " + i);
+         }
+
+         // load 6 locations
+         for (int i = 0; i < 6; i++) {
+             InstanceManager.getDefault(LocationManager.class).newLocation("Test_Location " + i);
+         }
+
+         // load 5 routes
+         InstanceManager.getDefault(RouteManager.class).newRoute("Test Route A");
+         InstanceManager.getDefault(RouteManager.class).newRoute("Test Route B");
+         InstanceManager.getDefault(RouteManager.class).newRoute("Test Route C");
+         InstanceManager.getDefault(RouteManager.class).newRoute("Test Route D");
+         InstanceManager.getDefault(RouteManager.class).newRoute("Test Route E");
+     }
+
+     @Override
+     @After
+     public void tearDown() throws Exception {
+         super.tearDown();
+     }
 
     // private final static Logger log = LoggerFactory.getLogger(TrainsTableFrameTest.class);
 
