a1ff6074f8ee4c353223eec9ffa510b0e116094d
Paul Bender <paul.bender@acm.org>
2018-06-08 13:57:06 -0400

    new test

--- java/test/jmri/jmris/simpleserver/SimpleServerTest.java
+++ java/test/jmri/jmris/simpleserver/SimpleServerTest.java
@@ -29,6 +29,34 @@ public class SimpleServerTest {
         jmri.util.JUnitAppender.suppressErrorMessage("Failed to connect to port 2048");
     }
 
+    @Test
+    // test sending a message.
+    public void testSendMessage() {
+        SimpleServer a = new SimpleServer();
+        Assert.assertNotNull(a);
+        jmri.util.JUnitAppender.suppressErrorMessage("Failed to connect to port 2048");
+        StringBuilder sb = new StringBuilder();
+        java.io.DataOutputStream output = new java.io.DataOutputStream(
+                new java.io.OutputStream() {
+                    @Override
+                    public void write(int b) throws java.io.IOException {
+                        sb.append((char)b);
+                    }
+                });
+        String code = "LIGHT IL1 OFF\n\r";
+        java.io.InputStream input = new java.io.ByteArrayInputStream(code.getBytes());
+        Thread t = new Thread(() -> { 
+            try{
+               a.handleClient(new java.io.DataInputStream(input),output); }
+            catch(java.io.IOException ioe){
+               // exception expected at end of input.
+               return;
+            }
+            });
+        t.setName("simpleserver client test thread");
+        t.start();
+    }
+
     // The minimal setup for log4J
     @Before
     public void setUp() {
