fca6b680cd398ab8884379dca9b157b3693c2df0
Bob Jacobsen <jacobsen@mac.com>
2018-06-20 10:11:21 -0700

    GUI tests

--- java/test/jmri/jmrit/beantable/signalmast/VirtualSignalMastAddPaneTest.java
+++ java/test/jmri/jmrit/beantable/signalmast/VirtualSignalMastAddPaneTest.java
@@ -4,12 +4,12 @@ import jmri.*;
 import jmri.implementation.*;
 import jmri.util.*;
 
-import java.util.*;
+import org.netbeans.jemmy.operators.*;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import java.util.*;
+import javax.swing.*;
+
+import org.junit.*;
 
 /**
  * @author	Bob Jacobsen Copyright 2018
@@ -46,6 +46,95 @@ public class VirtualSignalMastAddPaneTest {
         Assert.assertNotNull(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name"));
         Assert.assertNotNull(InstanceManager.getDefault(jmri.SignalMastManager.class).getBySystemName("IF$vsm:AAR-1946:PL-2-high($0005)"));
         
+    }
+
+    @Test
+    public void testCreateAndDisableViaGui() {
+        Assume.assumeFalse(java.awt.GraphicsEnvironment.isHeadless());
+        Assert.assertEquals(0, InstanceManager.getDefault(jmri.SignalMastManager.class).getObjectCount());
+        
+        VirtualSignalMastAddPane vp = new VirtualSignalMastAddPane();
+
+        vp.setAspectNames(
+            java.util.Collections.enumeration(
+                java.util.Arrays.asList(
+                    new String[]{"Clear","Approach Medium","Advance Approach"})));
+        
+        JFrame frame = new JFrame("Add/Edit Signal Mast");
+        frame.add(vp);
+        frame.pack();
+        frame.setVisible(true);
+        
+        JFrameOperator frameOp = new JFrameOperator("Add/Edit Signal Mast");
+        JCheckBoxOperator bBox = new JCheckBoxOperator(frameOp, "Approach Medium");
+        
+        // disable B
+        jmri.util.ThreadingUtil.runOnGUI(() -> {
+            bBox.push();
+            vp.createMast("AAR-1946", "appearance-PL-2-high.xml", "user name 1");
+        });
+
+        // check list of SignalMasts
+        Assert.assertEquals(1, InstanceManager.getDefault(jmri.SignalMastManager.class).getObjectCount());
+        Assert.assertNotNull(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 1"));
+        // system name not checked, depends on history of how many VirtualSignalMast objects have been created
+
+        // check aspect disabled
+        Assert.assertTrue(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 1").isAspectDisabled("Approach Medium"));
+        Assert.assertTrue(! InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 1").isAspectDisabled("Clear"));
+
+        jmri.util.ThreadingUtil.runOnGUI(() -> {
+            frame.dispose();
+        });
+    }
+
+    @Test
+    public void testEditAndDisableViaGui() {
+        Assume.assumeFalse(java.awt.GraphicsEnvironment.isHeadless());
+        Assert.assertEquals(0, InstanceManager.getDefault(jmri.SignalMastManager.class).getObjectCount());
+        VirtualSignalMast mast = new VirtualSignalMast("IF$vsm:basic:one-searchlight($1)", "user name 2"){
+            { lastRef = 7; } // reset references - this leads to $0007 below, just in case anybody else has created one
+        };
+        InstanceManager.getDefault(jmri.SignalMastManager.class).register(mast);
+        Assert.assertEquals(1, InstanceManager.getDefault(jmri.SignalMastManager.class).getObjectCount());
+        mast.setAspectDisabled("Stop");
+        mast.setAspectDisabled("Unlit"); // we will renable this below
+        
+        VirtualSignalMastAddPane vp = new VirtualSignalMastAddPane();
+        
+        vp.setAspectNames(mast.getAllKnownAspects().elements());
+        vp.setMast(mast);
+              
+        JFrame frame = new JFrame("Add/Edit Signal Mast");
+        frame.add(vp);
+        frame.pack();
+        frame.setVisible(true);
+        
+        JFrameOperator frameOp = new JFrameOperator("Add/Edit Signal Mast");
+        JCheckBoxOperator aBox = new JCheckBoxOperator(frameOp, "Approach");
+        JCheckBoxOperator uBox = new JCheckBoxOperator(frameOp, "Unlit");
+        
+        // disable Approach
+        jmri.util.ThreadingUtil.runOnGUI(() -> {
+            aBox.push(); // this should set disabled
+            uBox.push(); // this should set enabled
+            vp.createMast("basic", "appearance-one-searchlight.xml", "user name 2");
+        });
+
+        // check list of SignalMasts
+        Assert.assertEquals(1, InstanceManager.getDefault(jmri.SignalMastManager.class).getObjectCount());
+        Assert.assertNotNull(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 2"));
+        // system name not checked, depends on history of how many VirtualSignalMast objects have been created
+
+        // check correct aspect disabled
+        Assert.assertTrue(! InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 2").isAspectDisabled("Clear"));
+        Assert.assertTrue(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 2").isAspectDisabled("Approach"));
+        Assert.assertTrue(InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 2").isAspectDisabled("Stop"));
+        Assert.assertTrue(! InstanceManager.getDefault(jmri.SignalMastManager.class).getByUserName("user name 2").isAspectDisabled("Unlit"));
+
+        jmri.util.ThreadingUtil.runOnGUI(() -> {
+            frame.dispose();
+        });
     }
 
     @Before
