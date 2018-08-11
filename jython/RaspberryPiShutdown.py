282e8858ebcd16de1955dababecaa70536b7729e
mstevetodd <mstevetodd@mstevetodd.com>
2018-08-10 22:23:49 -0400

    update jmri shutdown command

--- jython/RaspberryPiShutdown.py
+++ jython/RaspberryPiShutdown.py
@@ -1,16 +1,14 @@
-# Shut down an Raspberry Pi or other Linux machine cleanly
+# Shutdown a Raspberry Pi or other Linux machine cleanly
 #
-# Run this from e.g. a Logix to end JMRI and make it save to turn off the machine
+# Run this from a Logix or button to safely restart the machine
 #
 # From a sequence by Dave Sand
 
-
 import java
 import jmri
-import jmri.InstanceManager
 
-# start the machine going down in one minute (requires that you can "sudo" without a password)
+# start the machine going down in 1 minute (requires that you can "sudo" without a password)
 java.lang.Runtime.getRuntime().exec("sudo shutdown -h +1")
 
 # do an immediate graceful shutdown of JMRI
-jmri.InstanceManager.shutDownManagerInstance().shutdown()
+shutdown.shutdown()
