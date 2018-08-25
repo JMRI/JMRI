920c04d4e71c06e7b6aea7091d321da4b55f1331
jcomuzzi <jcomuzzi@kateharper.com>
2018-08-24 20:54:31 -0400

    Revert "Add two tests to NamedIconTest"
    This reverts commit 6beba2059c4978c9442ab95dacd4f065c9e9d0a4.

--- java/test/jmri/jmrit/catalog/NamedIconTest.java
+++ java/test/jmri/jmrit/catalog/NamedIconTest.java
@@ -1,6 +1,5 @@
 package jmri.jmrit.catalog;
 
-import java.awt.geom.AffineTransform;
 import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -19,31 +18,6 @@ public class NamedIconTest {
         Assert.assertNotNull("exists",t);
     }
 
-    // Test that identity Affine Transform doesn't affect size.
-    @Test
-    public void testTransformImageIdentity() {
-        AffineTransform t = new AffineTransform();    //Identity transform
-        NamedIcon ni = new NamedIcon("program:resources/logo.gif","logo");
-        int h = ni.getIconHeight();
-        int w = ni.getIconWidth();
-        ni.transformImage(w,  h,  t, null);
-        Assert.assertEquals(h, ni.getIconHeight());
-        Assert.assertEquals(w,  ni.getIconWidth());
-    }
-    
-    // Test that resize Affine Transform correctly affects size.
-    @Test
-    public void testTransformImageScale() {
-        AffineTransform t = AffineTransform.getScaleInstance(0.5, 0.5);
-        NamedIcon ni = new NamedIcon("program:resources/logo.gif","logo");
-        int h = ni.getIconHeight();
-        int w = ni.getIconWidth();
-        ni.transformImage(w,  h,  t,  null);
-        Assert.assertEquals(h, ni.getIconHeight());
-        Assert.assertEquals(w, ni.getIconWidth());
-        
-    }
-    
     // The minimal setup for log4J
     @Before
     public void setUp() {
