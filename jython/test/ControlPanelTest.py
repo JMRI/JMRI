676f554173781aab870271f38e3617f545743e3a
Bob Jacobsen <jacobsen@mac.com>
2018-08-28 13:49:35 -0700

    only test in graphical mode

--- jython/test/ControlPanelTest.py
+++ jython/test/ControlPanelTest.py
@@ -1,5 +1,7 @@
 # Test the ControlPanel.py script - make sure it runs, then close its window
-
-execfile("jython/ControlPanel.py")
-f.dispose()
+import java
+if (java.awt.GraphicsEnvironment.isHeadless()) :
+    # just confirm that this runs OK in headless mode (when graphical, it prompts)
+    execfile("jython/ControlPanel.py")
+    f.dispose()
 
