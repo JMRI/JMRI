6e9592398e0c773e13a3f444b339f7305e1c6d48
Bob Jacobsen <jacobsen@mac.com>
2018-08-28 15:04:09 -0700

    Fix headless logic

--- jython/test/ControlPanelTest.py
+++ jython/test/ControlPanelTest.py
@@ -1,6 +1,6 @@
 # Test the ControlPanel.py script - make sure it runs, then close its window
 import java
-if (java.awt.GraphicsEnvironment.isHeadless()) :
+if ( ! java.awt.GraphicsEnvironment.isHeadless()) :
     # just confirm that this runs OK in headless mode (when graphical, it prompts)
     execfile("jython/ControlPanel.py")
     f.dispose()
