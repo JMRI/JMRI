930df16b91f28e435c7df2af441603e0ffa2ec03
Bob Jacobsen <jacobsen@mac.com>
2018-08-31 07:46:50 -0700

    add setup/teardown

--- java/test/jmri/SchemaTest.java
+++ java/test/jmri/SchemaTest.java
@@ -2,6 +2,8 @@ package jmri;
 
 import java.io.File;
 import jmri.configurexml.SchemaTestBase;
+
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
@@ -25,5 +27,15 @@ public class SchemaTest extends SchemaTestBase {
 
     public SchemaTest(File file, boolean pass) {
         super(file, pass);
+    }
+
+    @Before
+    public void setUp() {
+        jmri.util.JUnitUtil.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        jmri.util.JUnitUtil.tearDown();
     }
 }
