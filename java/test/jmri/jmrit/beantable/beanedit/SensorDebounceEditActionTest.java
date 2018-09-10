1933c35e4a47a35a8980a116efd1f95afca763f2
Bob Jacobsen <jacobsen@mac.com>
2018-09-10 15:20:50 -0700

    test to show failure

--- java/test/jmri/jmrit/beantable/beanedit/SensorDebounceEditActionTest.java
+++ java/test/jmri/jmrit/beantable/beanedit/SensorDebounceEditActionTest.java
@@ -1,6 +1,8 @@
 package jmri.jmrit.beantable.beanedit;
 
+import jmri.*;
 import jmri.util.JUnitUtil;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -18,10 +20,25 @@ public class SensorDebounceEditActionTest {
         Assert.assertNotNull("exists",t);
     }
 
+    @Test
+    public void testSet() {
+        SensorDebounceEditAction t = new SensorDebounceEditAction();
+
+        Sensor is1 = InstanceManager.sensorManagerInstance().provideSensor("IS1");
+
+        t.setBean(InstanceManager.sensorManagerInstance().getBySystemName("IS1"));
+        
+        BeanItemPanel p = t.sensorDebounce(null);
+        
+        t.saveDebounceItems(null);
+    }
+
     // The minimal setup for log4J
     @Before
     public void setUp() {
         JUnitUtil.setUp();
+        JUnitUtil.resetProfileManager();
+        JUnitUtil.initInternalSensorManager();
     }
 
     @After
