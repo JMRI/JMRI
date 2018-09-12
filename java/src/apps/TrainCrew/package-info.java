d08e3c208f2374b4702f89d9b6559d7c69dae5c2
Egbert Broerse <dcc2@ebroerse.nl>
2018-09-13 01:34:13 +0200

    NL menu items

--- java/src/apps/TrainCrew/package-info.java
+++ java/src/apps/TrainCrew/package-info.java
@@ -5,6 +5,5 @@
  * <a href="http://traincrew.conrail1285.com">http://traincrew.conrail1285.com</a>
  * and
  * <a href="https://github.com/ekapus/TrainCrew">https://github.com/ekapus/TrainCrew</a>.
- *
  */
 package apps.TrainCrew;
