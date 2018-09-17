e8f121786d201e323f05346caeeb6f8f6b9c96c2
Bob Jacobsen <jacobsen@mac.com>
2018-09-16 22:27:31 -0700

    better tests

--- java/test/jmri/implementation/TurnoutSignalMastTest.java
+++ java/test/jmri/implementation/TurnoutSignalMastTest.java
@@ -1,6 +1,8 @@
 package jmri.implementation;
 
+import jmri.*;
 import jmri.util.JUnitUtil;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,6 +18,28 @@ public class TurnoutSignalMastTest {
     public void testCTor() {
         TurnoutSignalMast t = new TurnoutSignalMast("IF$tsm:basic:one-searchlight($1)");
         Assert.assertNotNull("exists",t);
+    }
+
+    @Test
+    public void testSetAspect() {
+        TurnoutSignalMast t = new TurnoutSignalMast("IF$tsm:basic:one-searchlight($1)");
+        t.setTurnout("Stop", "IT1", Turnout.THROWN);
+        t.setTurnout("Clear", "IT2", Turnout.THROWN);
+        
+        t.setAspect("Clear");
+        Assert.assertEquals("Clear", t.getAspect());
+        
+        t.setAspect("Stop");
+        Assert.assertEquals("Stop", t.getAspect());
+        
+        try {
+            t.setAspect("Marblesnarb");
+        } catch (IllegalArgumentException ex) {
+            jmri.util.JUnitAppender.assertWarnMessage("attempting to set invalid aspect: Marblesnarb on mast: IF$tsm:basic:one-searchlight($1)");
+            return;
+        }
+        
+        Assert.fail("should have thrown");
     }
 
     // The minimal setup for log4J
