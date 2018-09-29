c1ffc930cdf68ca9db19e423411c89f7ecbad5e0
Daniel <db123@bergqvist.se>
2018-09-29 20:10:57 +0200

    Remove unused field

--- java/test/jmri/util/JUnitUtilTest.java
+++ java/test/jmri/util/JUnitUtilTest.java
@@ -392,6 +392,6 @@ public class JUnitUtilTest {
         jmri.util.JUnitUtil.tearDown();
     }
     
-    private final static Logger log = LoggerFactory.getLogger(JUnitUtilTest.class);
+//    private final static Logger log = LoggerFactory.getLogger(JUnitUtilTest.class);
     
 }
