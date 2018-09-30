9735afeda5d9fd1e507ae35675a27394f46feebd
Balazs Racz <balazs.racz@gmail.com>
2018-09-30 20:57:15 +0200

    Implements listening to clock updates from the layout.

--- java/src/jmri/jmrix/openlcb/OlcbClockControl.java
+++ java/src/jmri/jmrix/openlcb/OlcbClockControl.java
@@ -4,9 +4,15 @@ import org.openlcb.NodeID;
 import org.openlcb.OlcbInterface;
 import org.openlcb.protocols.TimeBroadcastConsumer;
 import org.openlcb.protocols.TimeProtocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.Date;
 
+import jmri.Timebase;
+import jmri.TimebaseRateException;
 import jmri.implementation.DefaultClockControl;
 
 /**
@@ -19,6 +25,39 @@ public class OlcbClockControl extends DefaultClockControl {
     public OlcbClockControl(OlcbInterface iface, boolean isMaster) {
         consumer = new TimeBroadcastConsumer(iface, TimeProtocol.DEFAULT_CLOCK);
         hardwareClock = consumer;
+        jmriClock = jmri.InstanceManager.getDefault(jmri.Timebase.class);
+        listener = new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+                clockUpdate(propertyChangeEvent.getPropertyName(), propertyChangeEvent.getNewValue());
+            }
+        };
+        hardwareClock.addPropertyChangeListener(listener);
+    }
+
+    public void dispose() {
+        hardwareClock.removePropertyChangeListener(listener);
+        if (consumer != null) {
+            consumer.dispose();
+            consumer = null;
+            hardwareClock = null;
+        }
+    }
+
+    /// Called when the layout sends an update to state, for example when someone else operates a
+    /// clock controlling node.
+    private void clockUpdate(String property, Object newValue) {
+        if (property.equals(TimeProtocol.PROP_RUN_UPDATE)) {
+            jmriClock.setRun(hardwareClock.isRunning());
+        } else if (property.equals(TimeProtocol.PROP_RATE_UPDATE)) {
+            try {
+                jmriClock.setRate(hardwareClock.getRate());
+            } catch (TimebaseRateException e) {
+                log.warn("Failed to set OpenLCB rate to internal clock.");
+            }
+        } else if (property.equals(TimeProtocol.PROP_TIME_UPDATE)) {
+            jmriClock.setTime(new Date(hardwareClock.getTimeInMsec()));
+        }
     }
 
     @Override
@@ -92,23 +131,25 @@ public class OlcbClockControl extends DefaultClockControl {
     public void initializeHardwareClock(double rate, Date now, boolean getTime) {
         if (!getTime) {
             hardwareClock.requestSetTime(now.getTime());
-        }
-        if (rate == 0) {
-            hardwareClock.requestStop();
+            if (rate == 0) {
+                hardwareClock.requestStop();
+            } else {
+                hardwareClock.requestSetRate(rate);
+                hardwareClock.requestStart();
+            }
         } else {
-            hardwareClock.requestSetRate(rate);
-            hardwareClock.requestStart();
+            hardwareClock.requestQuery();
         }
     }
 
+    /// Stores instance to the JMRI clock master.
+    private Timebase jmriClock;
     /// This is the interface to the clock generator or consumer.
     private TimeProtocol hardwareClock;
     /// If we instantiated a clock consumer, this is the object.
     private TimeBroadcastConsumer consumer;
+    /// The listener registered for the hardwareClock.
+    private PropertyChangeListener listener;
 
-    public void dispose() {
-        if (consumer != null) {
-            consumer.dispose();
-        }
-    }
+    private final static Logger log = LoggerFactory.getLogger(OlcbClockControl.class);
 }
