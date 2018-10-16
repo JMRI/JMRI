d69bd2c8de8f3949bc544663b7c12b6533fa7ea0
Paul Bender <paul.bender@acm.org>
2018-10-16 15:08:03 -0400

    suppress error message that sometimes occurs for serial connections

--- java/test/jmri/jmrix/configurexml/AbstractSerialConnectionConfigXmlTestBase.java
+++ java/test/jmri/jmrix/configurexml/AbstractSerialConnectionConfigXmlTestBase.java
@@ -1,6 +1,9 @@
 package jmri.jmrix.configurexml;
 
 import org.junit.*;
+import org.jdom2.Element;
+import jmri.jmrix.ConnectionConfig;
+import javax.swing.JPanel;
 
 /**
  * Base tests for SerialConnectionConfigXml objects.
@@ -9,4 +12,14 @@ import org.junit.*;
  */
 abstract public class AbstractSerialConnectionConfigXmlTestBase extends AbstractConnectionConfigXmlTestBase {
 
+    @Test
+    @Override
+    public void storeTest(){
+        Assume.assumeNotNull(cc);
+        cc.loadDetails(new JPanel());
+        // load details MAY produce an error message if no ports are found.
+        jmri.util.JUnitAppender.suppressErrorMessage("No usable ports returned");
+        Element e = xmlAdapter.store(cc);
+        Assert.assertNotNull("XML Element Produced",e); 
+    }
 }
