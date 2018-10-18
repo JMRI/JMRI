5d70b8a11a1107b2ff1c228a7e2ee15db5a336f9
Paul Bender <paul.bender@acm.org>
2018-10-18 13:12:10 -0400

    ignore test for USB based connections

--- java/test/jmri/jmrix/AbstractUsbConnectionConfigTestBase.java
+++ java/test/jmri/jmrix/AbstractUsbConnectionConfigTestBase.java
@@ -9,4 +9,11 @@ import javax.swing.JPanel;
  * @author Paul Bender Copyright (C) 2018	
  */
 abstract public class AbstractUsbConnectionConfigTestBase extends jmri.jmrix.AbstractConnectionConfigTestBase {
+
+    @Test
+    @Ignore
+    public void testLoadDetails(){
+       // don't try to load details on connections that use libusb, since it
+       // is not always available.
+    }
 }
