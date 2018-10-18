30b3f2cc84f3cb31bc7b28e35cc9f7ff38f4d302
danielboudreau <danielboudreau@HP-DanielBoudreau>
2018-10-17 22:09:01 -0400

    More testing

--- java/test/jmri/jmrit/operations/automation/actions/GotoSuccessActionTest.java
+++ java/test/jmri/jmrit/operations/automation/actions/GotoSuccessActionTest.java
@@ -1,5 +1,10 @@
 package jmri.jmrit.operations.automation.actions;
 
+import jmri.InstanceManager;
+import jmri.jmrit.operations.OperationsTestCase;
+import jmri.jmrit.operations.automation.Automation;
+import jmri.jmrit.operations.automation.AutomationItem;
+import jmri.jmrit.operations.automation.AutomationManager;
 import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -10,22 +15,109 @@ import org.junit.Test;
  *
  * @author Paul Bender Copyright (C) 2017	
  */
-public class GotoSuccessActionTest {
+public class GotoSuccessActionTest extends OperationsTestCase {
 
     @Test
     public void testCTor() {
         GotoSuccessAction t = new GotoSuccessAction();
         Assert.assertNotNull("exists",t);
     }
+    
+    @Test
+    public void testActionNoAutomationItem() {
+        GotoSuccessAction action = new GotoSuccessAction();
+        Assert.assertNotNull("exists", action);
+        // does nothing, no automationItem
+        action.doAction();
+    }
+
+    @Test
+    public void testGetActionName() {
+        GotoSuccessAction action = new GotoSuccessAction();
+        Assert.assertEquals("name", Bundle.getMessage("GotoIfSuccess"), action.getName());
+    }
+
+    /**
+     * Test conditional goto. A series of halts is assigned after the goto.
+     */
+    @Test
+    public void testAction() {
+        // this test needs an automation
+        Automation automation =
+                InstanceManager.getDefault(AutomationManager.class).newAutomation("Test_Goto_Automation");
+        GotoSuccessAction action = new GotoSuccessAction();
+        Assert.assertNotNull("exists", action);
+
+        AutomationItem automationItem1 = automation.addItem();
+        automationItem1.setAction(action);
+        Assert.assertEquals("confirm registered", automationItem1, action.getAutomationItem());
+
+        // fails, no goto assignment
+        action.doAction();
+        Assert.assertFalse(automationItem1.isActionSuccessful());
+        Assert.assertEquals("current automation item", automationItem1, automation.getCurrentAutomationItem());
+
+        AutomationItem automationItem2 = automation.addItem();
+        automationItem2.setAction(new HaltAction());
+
+        AutomationItem automationItem3 = automation.addItem();
+        automationItem3.setAction(new HaltAction());
+        
+        AutomationItem automationItem4 = automation.addItem();
+        automationItem4.setAction(new HaltAction());
+
+        // goto this item
+        automationItem1.setGotoAutomationItem(automationItem3);
+
+        automation.run();
+
+        Thread run = JUnitUtil.getThreadByName("Run action item: " + automationItem1.getId());
+
+        if (run != null) {
+            jmri.util.JUnitUtil.waitFor(() -> {
+                return run.getState().equals(Thread.State.TERMINATED);
+            }, "wait for terminated");
+        }
+
+        Assert.assertTrue(automationItem1.isActionSuccessful());
+        Assert.assertTrue(automationItem1.isActionRan());
+        Assert.assertFalse(automationItem1.isActionRunning());
+
+        // this test does not branch
+        Thread run2 = JUnitUtil.getThreadByName("Run action item: " + automationItem2.getId());
+
+        if (run2 != null) {
+            jmri.util.JUnitUtil.waitFor(() -> {
+                return run2.getState().equals(Thread.State.TERMINATED);
+            }, "wait for terminated");
+        }
+        
+        // the first halt is executed
+        Assert.assertTrue(automationItem2.isActionSuccessful());
+        Assert.assertTrue(automationItem2.isActionRan());
+        Assert.assertFalse(automationItem2.isActionRunning());
+       
+        // the 2nd halt is the next to execute
+        Assert.assertFalse(automationItem3.isActionSuccessful());
+        Assert.assertFalse(automationItem3.isActionRan());
+        Assert.assertFalse(automationItem3.isActionRunning());
+
+        // the next item to run is the 2nd halt
+        Assert.assertNotEquals("current automation item", automationItem1, automation.getCurrentAutomationItem());
+        Assert.assertEquals("current automation item", automationItem3, automation.getCurrentAutomationItem());
+        Assert.assertEquals("last automation item", automationItem2, automation.getLastAutomationItem());
+    }
 
     // The minimal setup for log4J
+    @Override
     @Before
     public void setUp() {
-        JUnitUtil.setUp();    }
+        super.setUp();    }
 
+    @Override
     @After
     public void tearDown() {
-        JUnitUtil.tearDown();
+        super.tearDown();
     }
 
     // private final static Logger log = LoggerFactory.getLogger(GotoSuccessActionTest.class);
