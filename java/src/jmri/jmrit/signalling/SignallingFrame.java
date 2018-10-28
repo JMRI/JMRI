d3bc8f7b39171f4d6c7fdf7be6cc85e92fe1c7a0
Egbert Broerse <dcc2@ebroerse.nl>
2018-10-28 19:59:33 +0100

    fix SML edit pane resizing, scroll center, label alignment

--- java/src/jmri/jmrit/signalling/SignallingFrame.java
+++ java/src/jmri/jmrit/signalling/SignallingFrame.java
@@ -2,13 +2,16 @@ package jmri.jmrit.signalling;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import jmri.SignalMast;
+import jmri.util.JmriJFrame;
 
 /**
- * Provide a JFrame to display a table of Signal Mast Logic.
+ * Provide a JFrame to display a pane to edit the Signal Mast Logic for one Signal Mast.
  *
  * @author Kevin Dickerson Copyright (C) 2011
+ * @author Egbert Broerse 2018
  */
-public class SignallingFrame extends jmri.util.JmriJFrame {
+public class SignallingFrame extends JmriJFrame {
 
     public SignallingFrame() {
         super(false, true);
@@ -21,16 +24,17 @@ public class SignallingFrame extends jmri.util.JmriJFrame {
      * Set the Signal Mast Logic frame's initial state.
      *
      * @param source The Signal Mast this SML is directly linked to
-     * @param dest The Signal Mast this SML is looking at
+     * @param dest   The Signal Mast this SML is looking at
+     * @see SignallingPanel(SignalMast, SignalMast, JmriJFrame)
      */
-    public void initComponents(jmri.SignalMast source, jmri.SignalMast dest) {
+    public void initComponents(SignalMast source, SignalMast dest) {
         // the following code sets the frame's initial state
         sigPanel = new SignallingPanel(source, dest, this);
 
         setTitle(Bundle.getMessage("SignallingPairs"));  // NOI18N
         getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 
-        getContentPane().add(sigPanel);
+        getContentPane().add(sigPanel); // panels are created in SignallingPanel()
 
         addHelpMenu("package.jmri.jmrit.signalling.AddEditSignallingLogic", true);  // NOI18N
 
