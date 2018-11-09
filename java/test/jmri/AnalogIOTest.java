83382792a6f12866e29ac0ee9b2afa9639dfce3c
Daniel <db123@bergqvist.se>
2018-11-09 14:15:59 +0100

    Fix AnalogIOTest

--- java/test/jmri/AnalogIOTest.java
+++ java/test/jmri/AnalogIOTest.java
@@ -16,24 +16,19 @@ public class AnalogIOTest {
 
     @Test
     public void testAnalogIO() throws JmriException {
+        float min = (float) -1.0;
+        float max = (float) 1.0;
         AnalogIO analogIO = new MyAnalogIO("Analog");
-        analogIO.setCommandedAnalogValue(AnalogIO.MIN_VALUE);
-        Assert.assertTrue("AnalogIO has MIN_VALUE", analogIO.getCommandedAnalogValue() == AnalogIO.MIN_VALUE);
-        analogIO.setCommandedAnalogValue(AnalogIO.MAX_VALUE);
-        Assert.assertTrue("AnalogIO has MAX_VALUE", analogIO.getCommandedAnalogValue() == AnalogIO.MAX_VALUE);
-        analogIO.setCommandedAnalogValue(AnalogIO.MIN_VALUE);
-        Assert.assertTrue("AnalogIO has MIN_VALUE", analogIO.getKnownAnalogValue() == AnalogIO.MIN_VALUE);
-        analogIO.setCommandedAnalogValue(AnalogIO.MAX_VALUE);
-        Assert.assertTrue("AnalogIO has MAX_VALUE", analogIO.getKnownAnalogValue() == AnalogIO.MAX_VALUE);
+        analogIO.setCommandedAnalogValue(min);
+        Assert.assertTrue("AnalogIO has value -1.0", analogIO.getCommandedAnalogValue() == min);
+        analogIO.setCommandedAnalogValue(max);
+        Assert.assertTrue("AnalogIO has value 1.0", analogIO.getCommandedAnalogValue() == max);
+        analogIO.setCommandedAnalogValue(min);
+        Assert.assertTrue("AnalogIO has value -1.0", analogIO.getKnownAnalogValue() == min);
+        analogIO.setCommandedAnalogValue(max);
+        Assert.assertTrue("AnalogIO has value 1.0", analogIO.getKnownAnalogValue() == max);
     }
     
-    @Test
-    public void testStateConstants() {
-        Assert.assertTrue("MIN_VALUE less than MAX_VALUE", AnalogIO.MIN_VALUE < AnalogIO.MAX_VALUE);
-        Assert.assertTrue("MIN_VALUE is the smallest number", AnalogIO.MIN_VALUE == Float.MIN_VALUE);
-        Assert.assertTrue("MAX_VALUE is the biggest number", AnalogIO.MAX_VALUE ==  Float.MAX_VALUE);
-    }
-
     @Before
     public void setUp() {
           jmri.util.JUnitUtil.setUp();
@@ -47,7 +42,7 @@ public class AnalogIOTest {
     
     private class MyAnalogIO extends AbstractNamedBean implements AnalogIO {
 
-        float _value = AnalogIO.MIDDLE_VALUE;
+        float _value = (float) 0.0;
         
         public MyAnalogIO(String sys) {
             super(sys);
