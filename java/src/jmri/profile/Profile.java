74cbcb1c8d99277146df9504b3b9f55dd868a62b
Bob Jacobsen <jacobsen@mac.com>
2018-11-16 22:29:24 -0800

    diagnostic

--- java/src/jmri/profile/Profile.java
+++ java/src/jmri/profile/Profile.java
@@ -96,8 +96,8 @@ public class Profile implements Comparable<Profile> {
             throw new IllegalArgumentException(path + " contains a profile in a subdirectory."); // NOI18N
         }
         if (Profile.inProfile(path) || Profile.inProfile(pathWithExt)) {
-            if (Profile.inProfile(path)) log.warn("Exception: Path {} is within an existing profile.", path); // NOI18N
-            if (Profile.inProfile(pathWithExt)) log.warn("Exception: pathWithExt {} is within an existing profile.", pathWithExt); // NOI18N
+            if (Profile.inProfile(path)) log.warn("Exception: Path {} is within an existing profile.", path, new Exception("traceback")); // NOI18N
+            if (Profile.inProfile(pathWithExt)) log.warn("Exception: pathWithExt {} is within an existing profile.", pathWithExt, new Exception("traceback")); // NOI18N
             throw new IllegalArgumentException(path + " is within an existing profile."); // NOI18N
         }
         this.name = name;
