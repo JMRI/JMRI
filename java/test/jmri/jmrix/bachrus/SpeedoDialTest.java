80cda4f58547056582e5975ffc6b8d4ffe48f94a
Paul Bender <paul.bender@acm.org>
2018-11-21 00:57:04 -0500

    test for Speed Dial

--- java/test/jmri/jmrix/bachrus/SpeedoDialTest.java
+++ java/test/jmri/jmrix/bachrus/SpeedoDialTest.java
@@ -1,31 +1,48 @@
 package jmri.jmrix.bachrus;
 
+import java.awt.GraphicsEnvironment;
 import jmri.util.JUnitUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 /**
  *
  * @author Paul Bender Copyright (C) 2017	
  */
 public class SpeedoDialTest {
+    
+    private SpeedoDial dial = null;
 
     @Test
     public void testCTor() {
-        SpeedoDial t = new SpeedoDial();
-        Assert.assertNotNull("exists",t);
+        Assert.assertNotNull("exists",dial);
+    }
+
+    @Test
+    public void testShow() {
+        // this test is really just making sure there are no exceptions
+        // when we draw the dial.  Most of the code in this class is in 
+        // the overriden paint method.
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        jmri.util.JmriJFrame frame = new jmri.util.JmriJFrame("SpeedoDial test frame");
+        frame.getContentPane().setPreferredSize(new java.awt.Dimension(600, 300));;
+        frame.getContentPane().add(dial);
+        frame.pack();
+        frame.setVisible(true);
+        dial.reset();
+        frame.setVisible(false);
+        frame.dispose();
     }
 
     // The minimal setup for log4J
     @Before
     public void setUp() {
         JUnitUtil.setUp();
+        dial = new SpeedoDial();
     }
 
     @After
     public void tearDown() {
+        dial = null;
         JUnitUtil.tearDown();
     }
 
