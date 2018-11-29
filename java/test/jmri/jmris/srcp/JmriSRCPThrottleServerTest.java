677cb7a18f87b7fbdfdc1d34d14d1183b7e49c8a
Paul Bender <paul.bender@acm.org>
2018-11-29 01:08:05 -0500

    remove extra imports

--- java/test/jmri/jmris/srcp/JmriSRCPThrottleServerTest.java
+++ java/test/jmri/jmris/srcp/JmriSRCPThrottleServerTest.java
@@ -1,10 +1,6 @@
 package jmri.jmris.srcp;
 
-import org.junit.Assert;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 
 import jmri.util.JUnitUtil;
 import jmri.DccLocoAddress;
