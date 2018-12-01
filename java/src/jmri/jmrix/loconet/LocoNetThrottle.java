f3e9e5c585fdb9cf12fdf6f8effec058759ebfca
Steve G <RailRodder@sympatico.ca>
2018-11-30 21:30:08 -0500

    Check for last throttle before dispatching

--- java/src/jmri/jmrix/loconet/LocoNetThrottle.java
+++ java/src/jmri/jmrix/loconet/LocoNetThrottle.java
@@ -1318,23 +1318,14 @@ public class LocoNetThrottle extends AbstractThrottle implements SlotListener {
     public void dispatchThrottle(DccThrottle t, ThrottleListener l) {
         log.debug("dispatchThrottle - throttle {}", t.getLocoAddress());
         // set status to common
-        if (t instanceof LocoNetThrottle){
+        if (t instanceof LocoNetThrottle) {
             LocoNetThrottle lnt = (LocoNetThrottle) t;
             LocoNetSlot tSlot = lnt.getLocoNetSlot();
             if (tSlot.slotStatus() != LnConstants.LOCO_COMMON) {
-                log.debug("dispatchThrottle is writing slot {} status to {}",
-                        tSlot,
-                        LnConstants.LOCO_COMMON);
-                network.sendLocoNetMessage(
-                        tSlot.releaseSlot());
-            }
-
-            jmri.util.ThreadingUtil.runOnLayoutDelayed( ()-> {
                 // and dispatch to slot 0
-                    log.debug("dispatchThrottle is dispatching slot {}", tSlot);
-                    network.sendLocoNetMessage(tSlot.dispatchSlot());
-                },
-                32);
+                log.debug("dispatchThrottle is dispatching slot {}", tSlot);
+                network.sendLocoNetMessage(tSlot.dispatchSlot());
+            }
         }
     }
 
