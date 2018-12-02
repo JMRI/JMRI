78ef970d64e28ac0bebc609e3fd1c5421f69fd80
Steve Young <icklesteve@yahoo.com>
2018-12-02 18:17:31 +0000

    import assume

--- java/test/jmri/jmrix/can/cbus/swing/CbusFilterFrameTest.java
+++ java/test/jmri/jmrix/can/cbus/swing/CbusFilterFrameTest.java
@@ -4,6 +4,7 @@ import java.awt.GraphicsEnvironment;
 import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
 
