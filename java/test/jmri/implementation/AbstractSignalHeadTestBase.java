42508c9d89b77f7acd78f53db945017b0003dd19
Steve Young <icklesteve@yahoo.com>
2019-01-03 14:15:51 +0000

    remove from AbstractSignalHeadTestBase

--- java/test/jmri/implementation/AbstractSignalHeadTestBase.java
+++ java/test/jmri/implementation/AbstractSignalHeadTestBase.java
@@ -27,7 +27,6 @@ public abstract class AbstractSignalHeadTestBase {
         Assert.assertTrue("set again", s.getLit());
     }
 
-    @SuppressWarnings("unlikely-arg-type") // Unlikely argument type int for contains(Object) on a Collection<int[]>
     private boolean validAppearance(int appearance, SignalHead s) {
         return Arrays.asList(s.getValidStates()).contains(SignalHead.RED);
     }
