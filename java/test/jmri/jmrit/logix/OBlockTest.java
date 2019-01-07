b1395e427df71d77801eab24aa9830fae0a3f915
pete_ <pete_@DESKTOP-O0N9TLF.attlocal.net>
2019-01-07 15:38:40 -0800

    Disallow running more than one warrant with the same address. Allow a warrant to have multiple sections. Change throttle increment increase for ramps from 8% to 10%. Some message improvements

--- java/test/jmri/jmrit/logix/OBlockTest.java
+++ java/test/jmri/jmrit/logix/OBlockTest.java
@@ -131,7 +131,9 @@ public class OBlockTest {
         OBlock b = blkMgr.createNewOBlock("OB102", "c");
         Assert.assertNull("Allocate w1", b.allocate(w1));
         Assert.assertEquals("state allocated & dark", OBlock.ALLOCATED|OBlock.UNDETECTED, b.getState());
-        Assert.assertEquals("Allocate w2", Bundle.getMessage("AllocatedToWarrant", w1.getDisplayName(), b.getDisplayName()), b.allocate(w2));
+        Assert.assertEquals("Allocate w2",
+                Bundle.getMessage("AllocatedToWarrant", w1.getDisplayName(), b.getDisplayName(), w1.getTrainName()),
+                b.allocate(w2));
         
         Assert.assertEquals("path not found", Bundle.getMessage("PathNotFound", "PathName", b.getDisplayName()), b.setPath("PathName", w1));
         OPath path1 = new OPath(b, "path1");
