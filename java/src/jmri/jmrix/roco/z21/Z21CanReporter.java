6ad31bf49487807784e4aedb876e609fb9432305
Paul Bender <paul.bender@acm.org>
2019-01-09 14:59:41 -0500

    refactor

--- java/src/jmri/jmrix/roco/z21/Z21CanReporter.java
+++ java/src/jmri/jmrix/roco/z21/Z21CanReporter.java
@@ -91,57 +91,49 @@ public class Z21CanReporter extends jmri.implementation.AbstractRailComReporter 
                int value1 = (msg.getElement(10)&0xFF) + ((msg.getElement(11)&0xFF) << 8);
                int value2 = (msg.getElement(12)&0xFF) + ((msg.getElement(13)&0xFF) << 8);
                log.debug("value 1: {}; value 2: {}",value1,value2);
-               if (msg.getCanDetectorLocoAddress(value1) != null ) { // 0 represents end of list or no railcom address.
-                  // get the first locomotive address from the message.
-                  DccLocoAddress l = msg.getCanDetectorLocoAddress(value1);
-                  if(l!=null) {
-                     log.debug("reporting tag for address 1 {}",l);
-                     // see if there is a tag for this address.
-                     RailCom tag = InstanceManager.getDefault(RailComManager.class).provideIdTag("" + l.getNumber());
-                     tag.setAddressType(l.isLongAddress()?RailCom.LONG_ADDRESS:RailCom.SHORT_ADDRESS);
-                     int direction = (0xC000&value1);
-                     switch (direction) {
-                        case 0x8000:
-                           tag.setOrientation(RailCom.ORIENTA);
-                           break;
-                        case 0xC000:
-                           tag.setOrientation(RailCom.ORIENTB);
-                           break;
-                        default:
-                           tag.setOrientation(0);
-                     }
+               RailCom tag = getRailComTagFromValue(msg,value1);
+               if(tag != null ) {
+                  notify(tag);
+                  tag = getRailComTagFromValue(msg,value2);
+                  if(tag != null ) {
                      notify(tag);
-                     if( msg.getCanDetectorLocoAddress(value2) != null ) { // again, 0 is end of list or no railcom address available.
-                        // get the second locomotive address from the message.
-                        DccLocoAddress l2 = msg.getCanDetectorLocoAddress(value2);
-                        if(l2!=null) {
-                           log.debug("reporting tag for address 2 {}",l2);
-                           // see if there is a tag for this address.
-                           RailCom tag2 = InstanceManager.getDefault(RailComManager.class).provideIdTag("" + l2.getNumber());
-                           tag2.setAddressType(l2.isLongAddress()?RailCom.LONG_ADDRESS:RailCom.SHORT_ADDRESS);
-                           direction = (0xC000&value2);
-                           switch (direction) {
-                             case 0x8000:
-                                 tag2.setOrientation(RailCom.ORIENTA);
-                                 break;
-                             case 0xC000:
-                                 tag2.setOrientation(RailCom.ORIENTB);
-                                 break;
-                              default:
-                                 tag2.setOrientation(0);
-                           }
-                           notify(tag2);
-                        }
-                     }
-                  } else if(type==0x11) { // address is 0 and first in list.
-                     notify(null);
                   }
+               } else if(type==0x11) { // address is 0 and first in list.
+                  notify(null);
                }
             } else if( type == 0x01 ) {
                 // status message, not a railcom value, so no report.
                 return;
              }
          }
+    }
+
+    /*
+     * private method to get and update a railcom tag based on the value 
+     * bytes from the message.
+     */
+    private RailCom getRailComTagFromValue(Z21Reply msg,int value){
+       DccLocoAddress l = msg.getCanDetectorLocoAddress(value);
+       if (l != null ) { // 0 represents end of list or no railcom address.
+          // get the first locomotive address from the message.
+          log.debug("reporting tag for address 1 {}",l);
+          // see if there is a tag for this address.
+          RailCom tag = InstanceManager.getDefault(RailComManager.class).provideIdTag("" + l.getNumber());
+          tag.setAddressType(l.isLongAddress()?RailCom.LONG_ADDRESS:RailCom.SHORT_ADDRESS);
+          int direction = (0xC000&value);
+          switch (direction) {
+             case 0x8000:
+                tag.setOrientation(RailCom.ORIENTA);
+                break;
+             case 0xC000:
+                tag.setOrientation(RailCom.ORIENTB);
+                break;
+             default:
+                tag.setOrientation(0);
+          }
+          return tag;
+       } 
+       return null; // address in the message indicates end of list.
     }
 
     /**
