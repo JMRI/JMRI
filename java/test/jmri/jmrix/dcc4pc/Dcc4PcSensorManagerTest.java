a29f5e17b60b9c47b520aeeca9b12ed673b49c16
Paul Bender <paul.bender@acm.org>
2019-01-22 12:20:26 -0500

    use abstract base class test; work on test of reply handling code.

--- java/test/jmri/jmrix/dcc4pc/Dcc4PcSensorManagerTest.java
+++ java/test/jmri/jmrix/dcc4pc/Dcc4PcSensorManagerTest.java
@@ -1,19 +1,44 @@
 package jmri.jmrix.dcc4pc;
 
 import jmri.util.JUnitUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 /**
  *
  * @author Paul Bender Copyright (C) 2017	
  */
-public class Dcc4PcSensorManagerTest {
+public class Dcc4PcSensorManagerTest extends jmri.managers.AbstractSensorMgrTestBase {
+
+    @Override
+    public String getSystemName(int i){
+        return "DS" + i;
+    }
 
     @Test
-    public void testCTor() {
+    @Ignore("need to trace receive for a better post reply check")
+    public void checkReceiveData(){
+       // message copied from DCC4PC omnibus specification
+       byte packetData[]={(byte)0x42,(byte)0x01,(byte)0x3F,(byte)0xA8,
+	       (byte)0x90,(byte)0x90,(byte)0xC0,(byte)0x00,(byte)0x00,
+               (byte)0x42,(byte)0x02,(byte)0xA3,(byte)0xAC,(byte)0x8B,
+	       (byte)0x69,(byte)0x4E,(byte)0xA5,(byte)0x99,(byte)0x9A,
+               (byte)0x01,(byte)0x01,(byte)0x91,(byte)0x04,(byte)0x22,
+               (byte)0x99,(byte)0xA5,(byte)0xA3,(byte)0xAC,(byte)0x81,
+               (byte)0x42,(byte)0x03,(byte)0x3F,(byte)0xBC,(byte)0x80,
+               (byte)0x90,(byte)0xC0,(byte)0x00,(byte)0x20,(byte)0x24,
+               (byte)0x8E,(byte)0xD4,(byte)0x4E,(byte)0x59,(byte)0xA3,
+	       (byte)0xAC};
+      Dcc4PcReply rep = new Dcc4PcReply(packetData);
+      rep.setUnsolicited();
+      ((Dcc4PcSensorManager)l).reply(rep);
+      Assert.assertNotNull("sensor exists after packet",l.getSensor("DS2"));   
+      Assert.assertEquals("sensor state after packet",jmri.Sensor.ACTIVE,l.getSensor("DS2").getState());
+    }
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
         Dcc4PcTrafficController tc = new Dcc4PcTrafficController(){
           @Override
           public void sendDcc4PcMessage(Dcc4PcMessage m,Dcc4PcListener reply) {
@@ -21,19 +46,13 @@ public class Dcc4PcSensorManagerTest {
        };
        Dcc4PcSystemConnectionMemo memo = new Dcc4PcSystemConnectionMemo(tc);
 
-       Dcc4PcSensorManager t = new Dcc4PcSensorManager(tc,memo);
-       Assert.assertNotNull("exists",t);
-       t.dispose();
-    }
-
-    // The minimal setup for log4J
-    @Before
-    public void setUp() {
-        JUnitUtil.setUp();
+       l = new Dcc4PcSensorManager(tc,memo);
     }
 
     @After
     public void tearDown() {
+	l.dispose();
+	l = null;
         JUnitUtil.tearDown();
     }
 
