7d8ee795a18a5ec3fb2a3415ce24d5f677fe80b1
Paul Bender <paul.bender@acm.org>
2019-01-22 00:52:21 -0500

    more cleanup

--- java/src/jmri/implementation/AbstractRailComReporter.java
+++ java/src/jmri/implementation/AbstractRailComReporter.java
@@ -1,18 +1,7 @@
 package jmri.implementation;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import jmri.DccLocoAddress;
-import jmri.IdTag;
-import jmri.IdTagListener;
-import jmri.InstanceManager;
 import jmri.LocoAddress;
-import jmri.PhysicalLocationReporter;
 import jmri.RailCom;
-import jmri.ReporterManager;
-import jmri.util.PhysicalLocation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Extend AbstractReporter for RailCom reporters
@@ -21,7 +10,7 @@ import org.slf4j.LoggerFactory;
  * <P>
  *
  * @author Matthew Harris Copyright (c) 2011
- * @author Paul Bender Copyright (c) 2016
+ * @author Paul Bender Copyright (c) 2016,2019
  * @since 4.5.4
  */
 public class AbstractRailComReporter extends AbstractIdTagReporter {
@@ -51,7 +40,5 @@ public class AbstractRailComReporter extends AbstractIdTagReporter {
         RailCom cr = (RailCom) this.getCurrentReport();
         return cr.getDccLocoAddress();
     }
-
-    private static final Logger log = LoggerFactory.getLogger(AbstractRailComReporter.class);
 
 }
