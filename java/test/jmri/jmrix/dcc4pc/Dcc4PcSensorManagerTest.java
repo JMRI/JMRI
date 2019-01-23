96ecdea152bc41b2ad8ecbc45ee451a4166ebc16
Paul Bender <paul.bender@acm.org>
2019-01-23 00:20:22 -0500

    fix sensor naming; work on packet decoding tests

--- java/test/jmri/jmrix/dcc4pc/Dcc4PcSensorManagerTest.java
+++ java/test/jmri/jmrix/dcc4pc/Dcc4PcSensorManagerTest.java
@@ -9,16 +9,41 @@ import org.junit.*;
  */
 public class Dcc4PcSensorManagerTest extends jmri.managers.AbstractSensorMgrTestBase {
 
+    Dcc4PcReporterManager rm = null;
+
     @Override
     public String getSystemName(int i){
-        return "DS" + i;
+        return "DS0:" + i;
     }
 
+    @Override
     @Test
-    @Ignore("need to trace receive for a better post reply check")
-    public void checkReceiveData(){
-       // message copied from DCC4PC omnibus specification
-       byte packetData[]={(byte)0x42,(byte)0x01,(byte)0x3F,(byte)0xA8,
+    public void testDefaultSystemName() {
+        // create -- requires module:contact form.
+        jmri.Sensor t = l.provideSensor("DS0:" + getNumToTest1());
+        // check
+        Assert.assertTrue("real object returned ", t != null);
+        Assert.assertEquals("system name correct ", t,l.getBySystemName(getSystemName(getNumToTest1())));
+    }
+    
+    @Override
+    @Test
+    public void testProvideName() {
+        // create -- requires module:contact form.
+        jmri.Sensor t = l.provideSensor("DS0:" + getNumToTest1());
+        // check
+        Assert.assertTrue("real object returned ", t != null);
+        Assert.assertTrue("system name correct ", t == l.getBySystemName(getSystemName(getNumToTest1())));
+    }
+
+
+    @Test
+    @Ignore("packet is decoded, but active blocks are not as expected")
+    public void checkReceiveRawRailComData(){
+       // Raw RailCom message copied from section 9.3 of the 
+       // DCC4PC omnibus specification.
+       // packet contains both occupancy information and RailCom data.
+       byte packetData[]={(byte)0x0b,(byte)0x00,(byte)0x42,(byte)0x01,(byte)0x3F,(byte)0xA8,
 	       (byte)0x90,(byte)0x90,(byte)0xC0,(byte)0x00,(byte)0x00,
                (byte)0x42,(byte)0x02,(byte)0xA3,(byte)0xAC,(byte)0x8B,
 	       (byte)0x69,(byte)0x4E,(byte)0xA5,(byte)0x99,(byte)0x9A,
@@ -29,12 +54,44 @@ public class Dcc4PcSensorManagerTest extends jmri.managers.AbstractSensorMgrTest
                (byte)0x8E,(byte)0xD4,(byte)0x4E,(byte)0x59,(byte)0xA3,
 	       (byte)0xAC};
       Dcc4PcReply rep = new Dcc4PcReply(packetData);
-      rep.setUnsolicited();
+      rep.setOriginalRequest(Dcc4PcMessage.pollBoard(0));
+      ((Dcc4PcSensorManager)l).addActiveBoard(0,"1",16,0);
+      l.provideSensor("DS0:1");
+      l.provideSensor("DS0:2");
+      l.provideSensor("DS0:3");
+      l.provideSensor("DS0:4");
+      // creae a reporter for DR0:4, because it has railcom data.
+      rm.provideReporter("DR0:4");
       ((Dcc4PcSensorManager)l).reply(rep);
-      Assert.assertNotNull("sensor exists after packet",l.getSensor("DS2"));   
-      Assert.assertEquals("sensor state after packet",jmri.Sensor.ACTIVE,l.getSensor("DS2").getState());
+       JUnitUtil.waitFor( () -> { return l.getSensor("DS0:4").getState() == jmri.Sensor.ACTIVE; });
+      Assert.assertEquals("sensor DS0:1 state after packet",jmri.Sensor.INACTIVE,l.getSensor("DS0:1").getState());
+      Assert.assertEquals("sensor DS0:2 state after packet",jmri.Sensor.INACTIVE,l.getSensor("DS0:2").getState());
+      // packet above has input 2 (sensor 3) occupied
+      Assert.assertEquals("sensor DS0:3 state after packet",jmri.Sensor.ACTIVE,l.getSensor("DS0:3").getState());
+      // packet above has input 3 (sensor 4) occupied with railcom data.
+      Assert.assertEquals("DR0:4 report","",rm.provideReporter("DR0:4").getCurrentReport());
+      Assert.assertEquals("sensor DS0:4 state after packet",jmri.Sensor.ACTIVE,l.getSensor("DS0:4").getState());
     }
 
+    @Test
+    @Ignore("it appears this packet is not yet decoded")
+    public void checkReceiveCookedRailComData(){
+       // Cooked RailCom message copied from section 9.4 of the 
+       // DCC4PC omnibus specification.
+       // This packet contains occupancy information only.
+       byte packetData[]={(byte)0x0b,(byte)0x00,(byte)0x02,(byte)0x01,(byte)0x00};
+       Dcc4PcReply rep = new Dcc4PcReply(packetData);
+       rep.setOriginalRequest(Dcc4PcMessage.pollBoard(0));
+       ((Dcc4PcSensorManager)l).addActiveBoard(0,"1",16,0);
+       l.provideSensor("DS0:1");
+       l.provideSensor("DS0:2");
+       l.provideSensor("DS0:3");
+       l.provideSensor("DS0:4");
+       ((Dcc4PcSensorManager)l).reply(rep);
+       JUnitUtil.waitFor( () -> { return l.getSensor("DS0:3").getState() == jmri.Sensor.ACTIVE; });
+       Assert.assertEquals("sensor state after packet",jmri.Sensor.ACTIVE,l.getSensor("DS0:3").getState());
+       
+    }
     // The minimal setup for log4J
     @Before
     public void setUp() {
@@ -45,8 +102,9 @@ public class Dcc4PcSensorManagerTest extends jmri.managers.AbstractSensorMgrTest
           }
        };
        Dcc4PcSystemConnectionMemo memo = new Dcc4PcSystemConnectionMemo(tc);
-
-       l = new Dcc4PcSensorManager(tc,memo);
+       memo.configureManagers();
+       l = memo.getSensorManager();
+       rm = memo.getReporterManager();
     }
 
     @After
