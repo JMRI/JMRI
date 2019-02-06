674ddf9de077ea1d1aef17243bdcffe6faeef200
dsand47 <gh@dfm.sandsys.org>
2019-02-06 12:02:11 -0600

    Change Help to About, duplicate help menu

--- java/src/jmri/jmrit/ctc/editor/gui/FrmMainForm.java
+++ java/src/jmri/jmrit/ctc/editor/gui/FrmMainForm.java
@@ -482,7 +482,7 @@ public class FrmMainForm extends jmri.util.JmriJFrame {
 
         jMenuBar1.add(jMenu1);
 
-        _mHelp.setText("Help");
+        _mHelp.setText("About");
 
         _mHelpAbout.setText("About");
         _mHelpAbout.addActionListener(new java.awt.event.ActionListener() {
@@ -1099,43 +1099,6 @@ public class FrmMainForm extends jmri.util.JmriJFrame {
         }
     }//GEN-LAST:event__mCheckEverythingWithJMRIActionPerformed
     
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FrmMainForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FrmMainForm().setVisible(true);
-            }
-        });
-    }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel _mCB_EditAlwaysEnabled;
     private javax.swing.JCheckBox _mCO_Enabled;
