04a87978943794f1b5e2158d3330eb5636b754b4
devel-bobm <devel-bobm@sbcglobal.net>
2019-02-20 21:08:16 -0500

    partial functionality

---
+++ java/src/jmri/jmrix/loconet/usb_dcs240/ConnectionConfig.java
@@ -0,0 +1,55 @@
+package jmri.jmrix.loconet.usb_dcs240;
+
+import jmri.util.SystemType;
+
+/**
+ * Definition of objects to handle configuring a DCS240 USB layout connection via a
+ * PR2Adapter object.
+ * <p>
+ * Copied from loconet.pr3.ConnectionConfig.java
+ * <p>
+ * @author Bob Jacobsen Copyright (C) 2001, 2003, 2008, 2010
+ * @author B. Milhaupt Copyright (C) 2019
+ */
+
+public class ConnectionConfig extends jmri.jmrix.AbstractSerialConnectionConfig {
+
+    /**
+     * Ctor for an object being created during load process; Swing init is
+     * deferred.
+     */
+    public ConnectionConfig(jmri.jmrix.SerialPortAdapter p) {
+        super(p);
+    }
+
+    /**
+     * Ctor for a functional Swing object with no preexisting adapter
+     */
+    public ConnectionConfig() {
+        super();
+    }
+
+    @Override
+    public String name() {
+        return "LocoNet DCS240 USB Interface"; // NOI18N
+    }
+
+    public boolean isOptList2Advanced() {
+        return false;
+    }
+
+    @Override
+    protected String[] getPortFriendlyNames() {
+        if (SystemType.isWindows()) {
+            return new String[]{"Communications Port"}; // NOI18N
+        }
+        return new String[]{};
+    }
+
+    @Override
+    protected void setInstance() {
+        if (adapter == null) {
+            adapter = new UsbDcs240Adapter();
+        }
+    }
+}
---
+++ java/src/jmri/jmrix/loconet/usb_dcs52/swing/Dcs52UsbModeSelectPane.java
@@ -0,0 +1,134 @@
+package jmri.jmrix.loconet.usb_dcs52.swing;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import jmri.jmrix.loconet.LnConstants;
+import jmri.jmrix.loconet.LocoNetListener;
+import jmri.jmrix.loconet.LocoNetMessage;
+import jmri.jmrix.loconet.LocoNetSystemConnectionMemo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Pane for downloading software updates to PRICOM products
+ *
+ * @author Bob Jacobsen Copyright (C) 2005
+  */
+public class Dcs52UsbModeSelectPane extends jmri.jmrix.loconet.swing.LnPanel implements LocoNetListener {
+
+    @Override
+    public String getHelpTarget() {
+        return "package.jmri.jmrix.loconet.usb_dcs52.swing.Dcs52UsbModeSelect"; // NOI18N
+    }
+
+    @Override
+    public String getTitle() {
+        return getTitle(Bundle.getMessage("MenuItemUsbDcs52ModeSelect"));
+    }
+
+    public Dcs52UsbModeSelectPane() {
+
+        // first build GUI
+        setLayout(new FlowLayout());
+
+        JButton b = new JButton(Bundle.getMessage("ButtonPr2Mode"));
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                selectPR2mode();
+            }
+        });
+        add(b);
+
+        b = new JButton(Bundle.getMessage("ButtonMs100Mode"));
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                selectMS100mode();
+            }
+        });
+        add(b);
+        add(status);
+
+    }
+
+    @Override
+    public void initComponents(LocoNetSystemConnectionMemo memo) {
+        super.initComponents(memo);
+
+        // listen for LocoNet messages
+        if (memo.getLnTrafficController() != null) {
+            memo.getLnTrafficController().addLocoNetListener(~0, this);
+        } else {
+            log.error("No LocoNet connection available, can't function");
+        }
+
+        // request status
+        LocoNetMessage msg = new LocoNetMessage(2);
+        msg.setOpCode(LnConstants.OPC_GPBUSY);
+        memo.getLnTrafficController().sendLocoNetMessage(msg);
+    }
+
+    JLabel status = new JLabel(Bundle.getMessage("StatusUnknown"));
+
+    void selectPR2mode() {
+        // set to PR2 mode
+        status.setText(Bundle.getMessage("StatusPr2"));
+        LocoNetMessage msg = new LocoNetMessage(6);
+        msg.setOpCode(0xD3);
+        msg.setElement(1, 0x10);
+        msg.setElement(2, 1);  // set PR2
+        msg.setElement(3, 0);
+        msg.setElement(4, 0);
+        memo.getLnTrafficController().sendLocoNetMessage(msg);
+    }
+
+    void selectMS100mode() {
+        // set to MS100 mode
+        status.setText(Bundle.getMessage("StatusMs100"));
+        LocoNetMessage msg = new LocoNetMessage(6);
+        msg.setOpCode(0xD3);
+        msg.setElement(1, 0x10);
+        msg.setElement(2, 0);  // set MS100
+        msg.setElement(3, 0);
+        msg.setElement(4, 0);
+        memo.getLnTrafficController().sendLocoNetMessage(msg);
+    }
+
+    @Override
+    public void message(LocoNetMessage msg) {
+        if ((msg.getOpCode() == LnConstants.OPC_PEER_XFER)
+                && (msg.getElement(1) == 0x10)
+                && (msg.getElement(2) == 0x22)
+                && (msg.getElement(3) == 0x22)
+                && (msg.getElement(4) == 0x01)) {  // Digitrax form, check "DCS52 USB as standalone programmer" or "DCS52 USB as LocoNet interface" mode
+            int mode = msg.getElement(8) & 0x0C;
+            if (mode == 0x00) {
+                // PR2 format (i.e. "DCS52 USB interface as standalone programmer" mode)
+                status.setText(Bundle.getMessage("StatusPr2"));
+            } else {
+                // MS100 format (i.e. "DCS52 USB interface as LocoNet interface" mode)
+                status.setText(Bundle.getMessage("StatusMs100"));
+            }
+        }
+    }
+
+    /**
+     * Nested class to create one of these using old-style defaults
+     */
+    static public class Default extends jmri.jmrix.loconet.swing.LnNamedPaneAction {
+
+        public Default() {
+            super(Bundle.getMessage("MenuItemUsbDcs52ModeSelect"),
+                    new jmri.util.swing.sdi.JmriJFrameInterface(),
+                    Dcs52UsbModeSelectPane.class.getName(),
+                    jmri.InstanceManager.getDefault(LocoNetSystemConnectionMemo.class));
+        }
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(Dcs52UsbModeSelectPane.class);
+
+}
