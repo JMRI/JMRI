a53b29cc146c095209988702994ada2b18d839d3
Paul Bender <paul.bender@acm.org>
2019-02-23 00:46:06 -0500

    update test case.

--- java/test/jmri/implementation/DefaultCabSignalTest.java
+++ java/test/jmri/implementation/DefaultCabSignalTest.java
@@ -5,9 +5,8 @@ import jmri.DccLocoAddress;
 import org.junit.*;
 
 /**
- * Tests for DefaultCabSignal
  *
- * @author Paul Bender Copyright (C) 2019
+ * @author Paul Bender Copyright (C) 2019	
  */
 public class DefaultCabSignalTest {
 
@@ -15,6 +14,13 @@ public class DefaultCabSignalTest {
     public void testCTor() {
         DefaultCabSignal cs = new DefaultCabSignal(new DccLocoAddress(1234,true));
         Assert.assertNotNull("exists",cs);
+        //check the defaults.
+        Assert.assertEquals("Address",new DccLocoAddress(1234,true),cs.getCabSignalAddress());
+        Assert.assertNull("current block",cs.getBlock());
+        Assert.assertNull("next block",cs.getNextBlock());
+        Assert.assertNull("next mast",cs.getNextMast());
+        Assert.assertTrue("cab signal active",cs.isCabSignalActive());
+        cs.dispose(); // verify no exceptions
     }
 
     // The minimal setup for log4J
@@ -27,5 +33,7 @@ public class DefaultCabSignalTest {
     public void tearDown() {
         JUnitUtil.tearDown();
     }
+
+    //private final static Logger log = LoggerFactory.getLogger(DefaultCabSignalTest.class);
 
 }
