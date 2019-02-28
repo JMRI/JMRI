756cf65f0bcb0b62f28f758d10a3fac790dcf551
Bob Milhaupt <devel-bobm@users.noreply.github.com>
2019-02-27 20:27:31 -0500

    Add connection configurations for Digitrax DCS240, DCS52, PR4 (#6599)
    Adds specific connection configurations for newer Digitrax USB hardware.  
    
    Adds "decoder" definition for DCS52's OpSws.  Adds and updates a variety of help files.  
    
    Fixes a couple of potential NPEs when a DCS52 or DCS240 is used in "Standalone programmer" mode and a "track power" control message is seen.
    
    Changes how menu items are presented in the LocoNet menu - does not display items which aren't useful when configured for an interface in "standalone programer" mode.
    
    Changes the order in which items are presented for the "Command station type":
    
    - For an interface with "standalone programmer" mode capability, prioritize that mode to the top of the list;
    
    - For an interface with built-in "command station" capability, prioritize the associated "command station" mode to be just below the associated "standalone programmer" mode.
    
    Updates various help pages to reflect functionality of the PR4, DCS52, and DCS240.

---
+++ java/test/jmri/jmrix/loconet/pr4/configurexml/ConnectionConfigXmlTest.java
@@ -0,0 +1,30 @@
+package jmri.jmrix.loconet.pr4.configurexml;
+
+import jmri.util.JUnitUtil;
+import org.junit.*;
+import jmri.jmrix.loconet.pr4.ConnectionConfig;
+
+/**
+ * ConnectionConfigXmlTest.java
+ *
+ * Description: tests for the ConnectionConfigXml class
+ *
+ * @author   Paul Bender  Copyright (C) 2016
+ */
+public class ConnectionConfigXmlTest extends jmri.jmrix.configurexml.AbstractSerialConnectionConfigXmlTestBase {
+
+    // The minimal setup for log4J
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+        xmlAdapter = new ConnectionConfigXml();
+        cc = new ConnectionConfig();
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.tearDown();
+        xmlAdapter = null;
+        cc = null;
+    }
+}
