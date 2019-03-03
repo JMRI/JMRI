286a2da6d3516624533a648de3ec288966db7bbf
Paul Bender <paul.bender@acm.org>
2019-03-03 00:19:19 -0500

    test with panel file, moving a train around a simple layout.

--- java/test/jmri/implementation/DefaultCabSignalTest.java
+++ java/test/jmri/implementation/DefaultCabSignalTest.java
@@ -3,6 +3,7 @@ package jmri.implementation;
 import java.awt.GraphicsEnvironment;
 import jmri.Block;
 import jmri.BlockManager;
+import jmri.JmriException;
 import jmri.Sensor;
 import jmri.SensorManager;
 import jmri.SignalMast;
@@ -15,6 +16,8 @@ import jmri.jmrit.display.EditorFrameOperator;
 import jmri.jmrit.display.layoutEditor.LayoutEditor;
 import jmri.jmrit.display.layoutEditor.ConnectivityUtil;
 import jmri.util.JUnitUtil;
+import jmri.util.JUnitAppender;
+import jmri.util.ThreadingUtil;
 import org.junit.*;
 
 
@@ -62,11 +65,11 @@ public class DefaultCabSignalTest {
     @Test
     public void testSignalSequence() throws jmri.JmriException {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
-        jmri.configurexml.ConfigXmlManager cm = new jmri.configurexml.ConfigXmlManager() {
-        };
         // load and display test panel file
+        InstanceManager.getDefault(jmri.jmrit.display.layoutEditor.LayoutBlockManager.class).setStabilisedSensor("IS_ROUTING_DONE");
+
         java.io.File f = new java.io.File("java/test/jmri/jmrit/cabsignals/SimpleCabSignalTestPanel.xml");
-        cm.load(f);
+        InstanceManager.getDefault(jmri.ConfigureManager.class).load(f);
 
         // Find new window by name (should be more distinctive, comes from sample file)
         EditorFrameOperator to = new EditorFrameOperator("Cab Signal Test");
@@ -85,23 +88,32 @@ public class DefaultCabSignalTest {
         InstanceManager.getDefault(jmri.LogixManager.class).activateAllLogixs();
         // make sure the block paths are initialized.
         InstanceManager.getDefault(jmri.jmrit.display.layoutEditor.LayoutBlockManager.class).initializeLayoutBlockPaths();
+        JUnitUtil.waitFor(() -> {
+            return InstanceManager.sensorManagerInstance().provideSensor("IS_ROUTING_DONE").getKnownState() == jmri.Sensor.ACTIVE;
+        },
+                "LayoutEditor stabilized sensor went ACTIVE");
         
         BlockManager bm = InstanceManager.getDefault(jmri.BlockManager.class);
         SensorManager sm = InstanceManager.getDefault(jmri.SensorManager.class);
         TurnoutManager tm = InstanceManager.getDefault(jmri.TurnoutManager.class);
         SignalMastManager smm = InstanceManager.getDefault(jmri.SignalMastManager.class);
 
-        tm.provideTurnout("EastTurnout").setState(Turnout.CLOSED);
-        tm.provideTurnout("WestTurnout").setState(Turnout.CLOSED);
+        ThreadingUtil.runOnLayout( ()-> { 
+             try{
+                tm.provideTurnout("EastTurnout").setState(Turnout.CLOSED);
+                tm.provideTurnout("WestTurnout").setState(Turnout.CLOSED);
 
-        sm.provideSensor("Mainline").setState(Sensor.ACTIVE);
-        sm.provideSensor("Siding").setState(Sensor.INACTIVE);
-        sm.provideSensor("EastTurnoutOS").setState(Sensor.INACTIVE);
-        sm.provideSensor("East1").setState(Sensor.INACTIVE);
-        sm.provideSensor("East2").setState(Sensor.INACTIVE);
-        sm.provideSensor("WestTurnoutOS").setState(Sensor.INACTIVE);
-        sm.provideSensor("West1").setState(Sensor.INACTIVE);
-        sm.provideSensor("West2").setState(Sensor.INACTIVE);
+                sm.provideSensor("Mainline").setState(Sensor.ACTIVE);
+                sm.provideSensor("Siding").setState(Sensor.INACTIVE);
+                sm.provideSensor("EastTurnoutOS").setState(Sensor.INACTIVE);
+                sm.provideSensor("East1").setState(Sensor.INACTIVE);
+                sm.provideSensor("East2").setState(Sensor.INACTIVE);
+                sm.provideSensor("WestTurnoutOS").setState(Sensor.INACTIVE);
+                sm.provideSensor("West1").setState(Sensor.INACTIVE);
+                sm.provideSensor("West2").setState(Sensor.INACTIVE);
+             } catch (JmriException je) {
+             }
+        });
 
         Block b1 = bm.provideBlock("MainlineBlock");
         // set the block contents to our locomotive address.
@@ -114,24 +126,80 @@ public class DefaultCabSignalTest {
         cs.setBlock();
 
         // and verify getBlock returns the block we set.
-        Assert.assertEquals("Block set",b1,cs.getBlock());
-        Assert.assertEquals("next Block set",bm.getBlock("EastTurnoutOSBlock"),cs.getNextBlock());
-        Assert.assertEquals("Mast set",smm.getSignalMast("IF$vsm:AAR-1946:PL-1-high-abs($0005)"),cs.getNextMast());
-        Assert.assertEquals("Mast Aspect clear","Clear",cs.getNextMast().getAspect());
+        checkBlock(cs,"MainlineBlock","EastTurnoutOSBlock","IF$vsm:AAR-1946:PL-1-high-abs($0005)");
+        moveBlock("Mainline","EastTurnoutOS");
+        checkBlock(cs,"EastTurnoutOSBlock","East1Block","IF$vsm:AAR-1946:PL-1-high-pbs($0002)");
+        moveBlock("EastTurnoutOS","East1");
+        checkBlock(cs,"East1Block","East2Block","IF$vsm:AAR-1946:PL-1-high-pbs($0002)");
+        moveBlock("East1","East2");
+        checkBlock(cs,"East2Block","","");
 
-        // use sensors to move to the next block.
-        sm.provideSensor("EastTurnoutOS").setState(Sensor.ACTIVE);
-        sm.provideSensor("Mainline").setState(Sensor.INACTIVE);
-        Assert.assertEquals("Block set",bm.getBlock("EastTurnoutOSBlock"),cs.getBlock());
-        Assert.assertEquals("next Block set",bm.getBlock("East1Block"),cs.getNextBlock());
-        Assert.assertEquals("Mast set",smm.getSignalMast("IF$vsm:AAR-1946:PL-1-high-pbs($0002)"),cs.getNextMast());
-        Assert.assertEquals("Mast Aspect clear","Clear",cs.getNextMast().getAspect());
+        moveBlock("East2","West2");
+        checkBlock(cs,"West2Block","","");
+        moveBlock("West2","West1");
+        checkBlock(cs,"West1Block","WestTurnoutOSBlock","IF$vsm:AAR-1946:SL-2-high-abs($0008)");
+        moveBlock("West1","WestTurnoutOS");
+        checkBlock(cs,"WestTurnoutOSBlock","MainlineBlock","IF$vsm:AAR-1946:PL-1-high-abs($0005)");
+        moveBlock("WestTurnoutOS","Mainline");
+        checkBlock(cs,"MainlineBlock","EastTurnoutOSBlock","IF$vsm:AAR-1946:PL-1-high-abs($0005)");
 
+        //throw the turnout behind the train.
+        ThreadingUtil.runOnLayout( ()-> { 
+             try{
+              tm.provideTurnout("WestTurnout").setState(Turnout.THROWN);
+             } catch (JmriException je) {
+             }
+        });
+        // and verify the state does not change.
+        checkBlock(cs,"MainlineBlock","EastTurnoutOSBlock","IF$vsm:AAR-1946:PL-1-high-abs($0005)");
+
+        // throw the turnout in front of the train
+        ThreadingUtil.runOnLayout( ()-> { 
+             try{
+              tm.provideTurnout("EastTurnout").setState(Turnout.THROWN);
+             } catch (JmriException je) {
+             }
+        });
+        // and verify the state changes.
+        checkBlock(cs,"MainlineBlock","","");
+
+        // and close the editor window
+        to.closeFrameWithConfirmations();
 
         cs.dispose(); // verify no exceptions
 
-        // and close the window
-        to.closeFrameWithConfirmations();
+        // We get some warnings and errors from the signal mast logic at this
+        // point.  It may be worth revisiting the signal mast choice used in
+        // the panel to eliminate these.
+        JUnitAppender.assertWarnMessage("attempting to set invalid aspect: Slow Clear on mast: East1ToEastTurnout");
+        JUnitAppender.assertErrorMessage("Exception while setting Signal Logic attempting to set invalid aspect: Slow Clear on mast: East1ToEastTurnout");
+        JUnitAppender.assertWarnMessage("attempting to set invalid aspect: Slow Clear on mast: West1ToWestTurnout");
+        JUnitAppender.assertErrorMessage("Exception while setting Signal Logic attempting to set invalid aspect: Slow Clear on mast: West1ToWestTurnout");
+    }
+
+    private void moveBlock(String startingBlock,String endingBlock) {
+        // use sensors to move to the next block.
+        ThreadingUtil.runOnLayout( ()-> { 
+             try{
+                 SensorManager sm = InstanceManager.getDefault(jmri.SensorManager.class);
+                 sm.provideSensor(endingBlock).setState(Sensor.ACTIVE);
+                 sm.provideSensor(startingBlock).setState(Sensor.INACTIVE); 
+             } catch (JmriException je) {
+             }
+        });
+    }
+
+    private void checkBlock(DefaultCabSignal cs,String currentBlock,String nextBlock,String mastName){
+        BlockManager bm = InstanceManager.getDefault(jmri.BlockManager.class);
+        SignalMastManager smm = InstanceManager.getDefault(jmri.SignalMastManager.class);
+        Assert.assertEquals("Block set",bm.getBlock(currentBlock),cs.getBlock());
+        Assert.assertEquals("next Block set",bm.getBlock(nextBlock),cs.getNextBlock());
+        Assert.assertEquals("Mast set",smm.getSignalMast(mastName),cs.getNextMast());
+        if(mastName!="") {
+           // no mast expected, so don't check the aspect.
+           JUnitUtil.waitFor( () -> { return "Clear".equals(cs.getNextMast().getAspect().toString());});
+           Assert.assertEquals("Mast " + mastName + " Aspect clear","Clear",cs.getNextMast().getAspect());
+        }
     }
 
     // The minimal setup for log4J
@@ -139,10 +207,14 @@ public class DefaultCabSignalTest {
     public void setUp() {
         JUnitUtil.setUp();
         JUnitUtil.resetProfileManager();
+        JUnitUtil.initConfigureManager();
         JUnitUtil.initInternalTurnoutManager();
         JUnitUtil.initInternalSensorManager();
-        JUnitUtil.initSignalMastLogicManager();
+        JUnitUtil.initMemoryManager();
+        InstanceManager.setDefault(jmri.BlockManager.class,new jmri.BlockManager());
         JUnitUtil.initLayoutBlockManager();
+        JUnitUtil.initDefaultSignalMastManager();
+        JUnitUtil.initSignalMastLogicManager();
         InstanceManager.setDefault(jmri.jmrit.display.PanelMenu.class,new jmri.jmrit.display.PanelMenu());
         JUnitUtil.initShutDownManager();
     }
