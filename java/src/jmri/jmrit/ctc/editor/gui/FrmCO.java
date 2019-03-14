f811ebed4f94fef6f3551d09ef89507f2c8bf42c
dsand47 <gh@dfm.sandsys.org>
2019-03-14 10:24:50 -0500

    Re-add the @Override entries removed by NetBeans

--- java/src/jmri/jmrit/ctc/editor/gui/FrmCO.java
+++ java/src/jmri/jmrit/ctc/editor/gui/FrmCO.java
@@ -183,6 +183,7 @@ public class FrmCO extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setTitle(Bundle.getMessage("TitleDlgCO"));
         addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 formWindowClosing(evt);
             }
