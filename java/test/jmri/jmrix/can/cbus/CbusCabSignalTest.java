2725848eabb0e32dbeeff0b2443ff97615b0caf0
Paul Bender <paul.bender@acm.org>
2019-03-15 22:19:39 -0400

    enable some additional tests for CbusCabSignals

--- java/test/jmri/jmrix/can/cbus/CbusCabSignalTest.java
+++ java/test/jmri/jmrix/can/cbus/CbusCabSignalTest.java
@@ -1,6 +1,7 @@
 package jmri.jmrix.can.cbus;
 
 import jmri.DccLocoAddress;
+import jmri.InstanceManager;
 import jmri.jmrix.can.CanSystemConnectionMemo;
 import jmri.jmrix.can.TrafficControllerScaffold;
 import jmri.util.JUnitUtil;
@@ -14,26 +15,35 @@ import org.junit.Test;
  * @author Paul Bender Copyright (C) 2017
  * @author Steve Young Copyright (C) 2019
  */
-public class CbusCabSignalTest {
+public class CbusCabSignalTest extends jmri.implementation.DefaultCabSignalTest {
 
-    @Test
-    public void testCtor() {
-        
+    // The minimal setup for log4J
+    @Before
+    @Override
+    public void setUp() {
+        JUnitUtil.setUp();
+        JUnitUtil.resetProfileManager();
+        JUnitUtil.initConfigureManager();
+        JUnitUtil.initInternalTurnoutManager();
+        JUnitUtil.initInternalSensorManager();
+        JUnitUtil.initMemoryManager();
+        InstanceManager.setDefault(jmri.BlockManager.class,new jmri.BlockManager());
+        JUnitUtil.initLayoutBlockManager();
+        JUnitUtil.initDefaultSignalMastManager();
+        JUnitUtil.initSignalMastLogicManager();
+        InstanceManager.setDefault(jmri.jmrit.display.PanelMenu.class,new jmri.jmrit.display.PanelMenu());
+        JUnitUtil.initShutDownManager();
+
+        // prepare the cab signal
         CanSystemConnectionMemo memo = new CanSystemConnectionMemo();
         TrafficControllerScaffold tc = new TrafficControllerScaffold();
         memo.setTrafficController(tc);
         
-        CbusCabSignal t = new CbusCabSignal(memo,new DccLocoAddress(1234,true));
-        Assert.assertNotNull("exists",t);
-    }
-
-    // The minimal setup for log4J
-    @Before
-    public void setUp() {
-        JUnitUtil.setUp();
+        cs = new CbusCabSignal(memo,new DccLocoAddress(1234,true));
     }
 
     @After
+    @Override
     public void tearDown() {
         JUnitUtil.tearDown();
     }
