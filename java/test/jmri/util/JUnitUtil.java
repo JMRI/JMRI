c96f0a6bdde6e49dc41ce9d9e890d415bbd29e4b
Paul Bender <paul.bender@acm.org>
2019-03-28 11:57:31 -0400

    suppress null pointer exception on window dispose.

--- java/test/jmri/util/JUnitUtil.java
+++ java/test/jmri/util/JUnitUtil.java
@@ -1126,7 +1126,12 @@ public class JUnitUtil {
         java.util.Objects.requireNonNull(window, "Window cannot be null");
         
         ThreadingUtil.runOnGUI(() -> {
-            window.dispose();
+            try{
+               window.dispose();
+	    } catch(java.lang.NullPointerException npe){
+	       // The window was null when we actually tried to dispose of it?
+	       // Were two dispose calls queued?
+	    }
         });
     }
     
