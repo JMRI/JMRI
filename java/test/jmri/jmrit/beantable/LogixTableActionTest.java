e91e60a87fca92d05bcc537a4baa234ec725d2f2
Paul Bender <paul.bender@acm.org>
2019-03-28 14:47:26 -0400

    more add button related tests for tables

--- java/test/jmri/jmrit/beantable/LogixTableActionTest.java
+++ java/test/jmri/jmrit/beantable/LogixTableActionTest.java
@@ -7,6 +7,7 @@ import jmri.Logix;
 
 import jmri.util.*;
 import jmri.util.junit.rules.*;
+import jmri.util.junit.annotations.*;
 
 import org.junit.*;
 import org.junit.rules.*;
@@ -211,6 +212,12 @@ public class LogixTableActionTest extends AbstractTableActionBase {
     @Override
     public String getAddFrameName(){
         return Bundle.getMessage("TitleAddLogix");
+    }
+
+    @Test
+    @Ignore("Logix create frame does not have a hardware address")
+    @ToDo("Re-write parent class test to use the right name")
+    public void testAddThroughDialog() {
     }
 
     @Before
