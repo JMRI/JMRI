49bf93168e64d41be8d0f4520d772dd212227ddb
Daniel <db123@bergqvist.se>
2019-03-28 09:11:50 +0100

    Create new bean with reflection

--- java/test/jmri/managers/AbstractManagerTestBase.java
+++ java/test/jmri/managers/AbstractManagerTestBase.java
@@ -6,7 +6,9 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyVetoException;
 import java.beans.VetoableChangeListener;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import jmri.util.JUnitAppender;
 import org.apache.log4j.Level;
 
@@ -121,18 +123,7 @@ public abstract class AbstractManagerTestBase<T extends Manager<E>, E extends Na
         Assert.assertFalse(s.isEmpty());
     }
 
-    private Field getField(Class c, String fieldName) {
-        try {
-            return c.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException ex) {
-            if (c.getSuperclass() != null)
-                return getField(c.getSuperclass(), fieldName);
-        }
-        
-        // Field not found
-        return null;
-    }
-    
+    @SuppressWarnings("unchecked")  // The call Constructor.newInstance() generates an unchecked cast warning
     @Test
     public void testRegisterDuplicateSystemName() throws PropertyVetoException, NoSuchFieldException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
         if (l instanceof ProvidingManager)  {
@@ -149,13 +140,24 @@ public abstract class AbstractManagerTestBase<T extends Manager<E>, E extends Na
 
             try {
                 e1 = m.provide(s1);
-                e2 = m.provide(s2);
             } catch (IllegalArgumentException | NullPointerException | ArrayIndexOutOfBoundsException ex) {
                 // jmri.jmrix.openlcb.OlcbLightManagerTest gives a NullPointerException here.
                 // jmri.jmrix.openlcb.OlcbSensorManagerTest gives a ArrayIndexOutOfBoundsException here.
                 // Some other tests give an IllegalArgumentException here.
 
                 // If the test is unable to provide a named bean, abort this test.
+                JUnitAppender.clearBacklog(Level.WARN);
+                Assume.assumeTrue("We got no exception", ex == null);
+                return;
+            }
+
+            try {
+                Constructor ctor = e1.getClass().getDeclaredConstructor(String.class, String.class);
+                e2 = (E) ctor.newInstance(s1, "My user name");
+            } catch (NoSuchMethodException | InstantiationException | InvocationTargetException ex) {
+                // jmri.jmrix.internal.InternalTurnoutManagerTest causes this exception
+                
+                // If the test is unable to create a named bean, abort this test.
                 JUnitAppender.clearBacklog(Level.WARN);
                 Assume.assumeTrue("We got no exception", ex == null);
                 return;
@@ -178,12 +180,6 @@ public abstract class AbstractManagerTestBase<T extends Manager<E>, E extends Na
             // Register bean twice. This should fail with a warning.
             l.register(e1);
             JUnitAppender.assertWarnMessage(expectedMessage);
-
-            // Use reflection to change the systemName of e2
-            // Try to find the field
-            Field f1 = getField(e2.getClass(), "mSystemName");
-            f1.setAccessible(true);
-            f1.set(e2, e1.getSystemName());
 
             expectedMessage = "systemName is already registered: " + e1.getSystemName();
             boolean hasException = false;
