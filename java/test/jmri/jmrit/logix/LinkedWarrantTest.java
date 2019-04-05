c07462a991be478be8804d4ea9cfdde5c4a66b75
pete_ <pete_@DESKTOP-O0N9TLF.attlocal.net>
2019-04-04 20:00:52 -0700

    restart.  Might as well add a test. What will Appveyor do this time?

--- java/test/jmri/jmrit/logix/LinkedWarrantTest.java
+++ java/test/jmri/jmrit/logix/LinkedWarrantTest.java
@@ -287,6 +287,112 @@ public class LinkedWarrantTest {
             panel.dispose();    // disposing this way allows test to be rerun (i.e. reload panel file) multiple times
     }
 
+    // Tests warrant launching 3 different warrants mid script - tinker to Evers to Chance
+    @Test
+    public void testLinkedMidScript() throws Exception {
+        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        // load and display
+        File f = new File("java/test/jmri/jmrit/logix/valid/NXWarrantTest.xml");
+        InstanceManager.getDefault(ConfigureManager.class).load(f);
+        _OBlockMgr = InstanceManager.getDefault(OBlockManager.class);
+        _sensorMgr = InstanceManager.getDefault(SensorManager.class);
+        _warrantMgr = InstanceManager.getDefault(WarrantManager.class);
+
+        final Sensor sensor0 = _sensorMgr.getBySystemName("IS0");
+        Assert.assertNotNull("Senor IS12 not found", sensor0);
+
+        jmri.util.ThreadingUtil.runOnLayout(() -> {
+            try {
+                sensor0.setState(Sensor.ACTIVE);
+            } catch (jmri.JmriException e) {
+                Assert.fail("Set "+sensor0.getDisplayName()+" ACTIVE Exception: " + e);
+            }
+        });
+        new org.netbeans.jemmy.QueueTool().waitEmpty(100);  //pause light sensor
+
+        WarrantTableFrame tableFrame = WarrantTableFrame.getDefault();
+        Assert.assertNotNull("tableFrame", tableFrame);
+
+        Warrant warrant = _warrantMgr.getWarrant("Tinker");
+        Assert.assertNotNull("warrant", warrant);
+       
+        tableFrame.runTrain(warrant, Warrant.MODE_RUN);
+
+        Warrant w = warrant;
+        jmri.util.JUnitUtil.waitFor(() -> {
+            String m =  w.getRunningMessage();
+            return m.endsWith("Cmd #8.");
+        }, "Tinker starts to move at 8th command");
+
+       // OBlock of route
+        String[] route1 = {"OB0", "OB1", "OB2", "OB3", "OB4", "OB5", "OB10"};
+        OBlock block = _OBlockMgr.getOBlock("OB10");
+
+        // Run the train, then checks end location
+        Assert.assertEquals("Tinker after first leg", block.getSensor().getDisplayName(), NXFrameTest.runtimes(route1, _OBlockMgr).getDisplayName());
+
+        Sensor sensor7 = _sensorMgr.getBySystemName("IS7");
+        Assert.assertNotNull("Senor IS7 not found", sensor7);
+        jmri.util.ThreadingUtil.runOnLayout(() -> {
+            try {
+                sensor7.setState(Sensor.ACTIVE);
+            } catch (jmri.JmriException e) {
+                Assert.fail("Set "+sensor7.getDisplayName()+" ACTIVE Exception: " + e);
+            }
+        });
+        warrant = _warrantMgr.getWarrant("Evers");
+
+        jmri.util.JUnitUtil.waitFor(() -> {
+            String m = tableFrame.getStatus();
+            return (m.startsWith("Launching warrant"));
+        }, "Tinker finished first leg");
+
+        Warrant ww = warrant;
+        jmri.util.JUnitUtil.waitFor(() -> {
+            String m =  ww.getRunningMessage();
+            return m.endsWith("Cmd #8.");
+        }, "Evers starts to move at 8th command");
+
+        String[] route2 = {"OB7", "OB3", "OB2", "OB1"};
+        block = _OBlockMgr.getOBlock("OB1");
+
+        Assert.assertEquals("Evers after second leg", block.getSensor().getDisplayName(), NXFrameTest.runtimes(route2, _OBlockMgr).getDisplayName());
+
+        Sensor sensor6 = _sensorMgr.getBySystemName("IS6");
+        Assert.assertNotNull("Senor IS6 not found", sensor6);
+        jmri.util.ThreadingUtil.runOnLayout(() -> {
+            try {
+                sensor6.setState(Sensor.ACTIVE);
+            } catch (jmri.JmriException e) {
+                Assert.fail("Set "+sensor6.getDisplayName()+" ACTIVE Exception: " + e);
+            }
+        });
+        Warrant www = _warrantMgr.getWarrant("Chance");
+
+        jmri.util.JUnitUtil.waitFor(() -> {
+            String m = tableFrame.getStatus();
+            return (m.startsWith("Launching warrant"));
+        }, "Evers finished second leg");
+
+        jmri.util.JUnitUtil.waitFor(() -> {
+            String m =  www.getRunningMessage();
+            return m.endsWith("Cmd #8.");
+        }, "Chance starts to move at 8th command");
+
+        String[] route3 = {"OB6", "OB3", "OB4", "OB5"};
+        block = _OBlockMgr.getOBlock("OB5");
+
+        Assert.assertEquals("Chance after third leg", block.getSensor().getDisplayName(), NXFrameTest.runtimes(route3, _OBlockMgr).getDisplayName());
+
+        new org.netbeans.jemmy.QueueTool().waitEmpty(100);  // pause to let things settle
+        // passed test - cleanup.  Do it here so failure leaves traces.
+        JFrameOperator jfo = new JFrameOperator(tableFrame);
+        jfo.requestClose();
+        // we may want to use jemmy to close the panel as well.
+        ControlPanelEditor panel = (ControlPanelEditor) jmri.util.JmriJFrame.getFrame("NXWarrantTest");
+        panel.dispose();    // disposing this way allows test to be rerun (i.e. reload panel file) multiple times
+    }
+
     @Before
     public void setUp() throws Exception {
         JUnitUtil.setUp();
