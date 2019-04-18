b9e59e090090055c8d919d69514d18f5f77a5871
Paul Bender <paul.bender@acm.org>
2019-04-18 16:46:21 -0400

    Use dummy node identifier

--- java/test/jmri/jmrix/ieee802154/xbee/swing/nodeconfig/XBeeEditNodeFrameTest.java
+++ java/test/jmri/jmrix/ieee802154/xbee/swing/nodeconfig/XBeeEditNodeFrameTest.java
@@ -32,7 +32,12 @@ public class XBeeEditNodeFrameTest extends jmri.util.JmriJFrameTestBase {
            byte pan[] = {(byte) 0x00, (byte) 0x42};
            byte uad[] = {(byte) 0x6D, (byte) 0x97};
            byte gad[] = {(byte) 0x00, (byte) 0x13, (byte) 0xA2, (byte) 0x00, (byte) 0x40, (byte) 0xA0, (byte) 0x4D, (byte) 0x2D};
-           XBeeNode node = new XBeeNode(pan,uad,gad);
+           XBeeNode node = new XBeeNode(pan,uad,gad){
+               @Override
+               public String getIdentifier(){ 
+                   return "";
+               }
+           };
            frame = new XBeeEditNodeFrame(tc,node,parent);
         }
     }
