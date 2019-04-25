0b2eed3dd56ea8691628820ae197cdc1de3bd861
Paul Bender <paul.bender@acm.org>
2019-04-25 12:23:39 -0400

    more JmriJFrame related tests

--- java/test/jmri/jmrit/automat/monitor/AutomatTableFrameTest.java
+++ java/test/jmri/jmrit/automat/monitor/AutomatTableFrameTest.java
@@ -2,36 +2,29 @@ package jmri.jmrit.automat.monitor;
 
 import java.awt.GraphicsEnvironment;
 import jmri.util.JUnitUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Assume;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 /**
  *
  * @author Paul Bender Copyright (C) 2017
  */
-public class AutomatTableFrameTest {
-
-    @Test
-    public void testCTor() {
-        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
-        AutomatTableFrame t = new AutomatTableFrame(new AutomatTableDataModel());
-        Assert.assertNotNull("exists", t);
-        JUnitUtil.dispose(t);
-    }
+public class AutomatTableFrameTest extends jmri.util.JmriJFrameTestBase {
 
     // The minimal setup for log4J
     @Before
+    @Override
     public void setUp() {
         JUnitUtil.setUp();
         JUnitUtil.resetProfileManager();
+        if(!GraphicsEnvironment.isHeadless()){
+           frame = new AutomatTableFrame(new AutomatTableDataModel());
+	}
     }
 
     @After
+    @Override
     public void tearDown() {
-        JUnitUtil.tearDown();
+        super.tearDown();
     }
 
     // private final static Logger log = LoggerFactory.getLogger(AutomatTableFrameTest.class);
