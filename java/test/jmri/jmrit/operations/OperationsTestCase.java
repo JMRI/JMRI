393fae306b6e0f9f915f1685bac4f1693907efda
Bob Jacobsen <jacobsen@mac.com>
2019-04-25 18:04:38 -0600

    clear remaining threads

--- java/test/jmri/jmrit/operations/OperationsTestCase.java
+++ java/test/jmri/jmrit/operations/OperationsTestCase.java
@@ -60,6 +60,8 @@ public class OperationsTestCase {
                 // ignore.
             }
         }
+        
+        JUnitUtil.clearShutDownManager();
         JUnitUtil.tearDown();
     }
 }
