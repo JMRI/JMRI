80ff08f933a73116438105a4052c4de26d74d57f
George Warner <geowar1@mac.com>
2019-05-01 06:20:25 -0700

    Revert LayoutEditorTest.xml

--- java/src/jmri/jmrit/display/layoutEditor/configurexml/LayoutEditorXml.java
+++ java/src/jmri/jmrit/display/layoutEditor/configurexml/LayoutEditorXml.java
@@ -93,7 +93,9 @@ public class LayoutEditorXml extends AbstractXmlAdapter {
         if (!turnoutCircleColor.equals(turnoutCircleThrownColor)) {
             panel.setAttribute("turnoutcirclethrowncolor", turnoutCircleThrownColor);
         }
-        panel.setAttribute("turnoutfillcontrolcircles", (p.isTurnoutFillControlCircles() ? "yes" : "no"));
+        if (p.isTurnoutFillControlCircles()) {
+            panel.setAttribute("turnoutfillcontrolcircles", "yes");
+        }
 
         panel.setAttribute("turnoutcirclesize", "" + p.getTurnoutCircleSize());
         panel.setAttribute("turnoutdrawunselectedleg", (p.isTurnoutDrawUnselectedLeg() ? "yes" : "no"));
