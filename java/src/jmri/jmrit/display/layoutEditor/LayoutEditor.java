e8e7c0d919b7a1db595a12e005434f59880e2cbc
Petr Sidlo <sidlo64@hotmail.com>
2019-05-03 21:25:00 +0200

    LayoutEditor fix javadoc

--- java/src/jmri/jmrit/display/layoutEditor/LayoutEditor.java
+++ java/src/jmri/jmrit/display/layoutEditor/LayoutEditor.java
@@ -2793,7 +2793,7 @@ public class LayoutEditor extends PanelEditor implements MouseWheelListener {
 
     /**
      * since 4.15.6 split variable defaultTrackColor and mainlineTrackColor/sidelineTrackColor
-     * @return 
+     * @return LayoutTrackDrawingOptions object
      */
     @Nonnull
     public LayoutTrackDrawingOptions getLayoutTrackDrawingOptions() {
@@ -2812,7 +2812,7 @@ public class LayoutEditor extends PanelEditor implements MouseWheelListener {
 
     /** 
      * since 4.15.6 split variable defaultTrackColor and mainlineTrackColor/sidelineTrackColor
-     * @param ltdo 
+     * @param ltdo LayoutTrackDrawingOptions object
      */
     public void setLayoutTrackDrawingOptions(LayoutTrackDrawingOptions ltdo) {
         layoutTrackDrawingOptions = ltdo;
