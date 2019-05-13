891dd510e50f3fada69f9311d52696f98ccf4a22
Petr Sidlo <sidlo64@hotmail.com>
2019-05-13 17:13:57 +0200

    SensorgroupBundle.properties renamed to Bundle.properties

--- java/src/jmri/jmrit/sensorgroup/Bundle.java
+++ java/src/jmri/jmrit/sensorgroup/Bundle.java
@@ -24,7 +24,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class Bundle extends jmri.jmrit.Bundle {
 
     @Nullable
-    private static final String name = "jmri.jmrit.sensorgroup.SensorgroupBundle"; // NOI18N
+    private static final String name = "jmri.jmrit.sensorgroup.Bundle"; // NOI18N
 
     //
     // below here is boilerplate to be copied exactly
