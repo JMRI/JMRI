f24fd9f075b73defc8cb8a3bec9984b1cc0d6c99
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-05-22 05:59:06 -0700

    test: test powerline turnout creatation with valid powerline names

--- java/test/jmri/jmrix/powerline/SerialTurnoutManagerTest.java
+++ java/test/jmri/jmrix/powerline/SerialTurnoutManagerTest.java
@@ -3,6 +3,9 @@ package jmri.jmrix.powerline;
 import jmri.Turnout;
 import jmri.jmrix.powerline.simulator.SpecificSystemConnectionMemo;
 import jmri.util.JUnitUtil;
+
+import java.beans.PropertyVetoException;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -13,9 +16,9 @@ import org.slf4j.LoggerFactory;
 /**
  * SerialTurnoutManagerTest.java
  *
- * Description:	tests for the SerialTurnoutManager class
+ * Description: tests for the SerialTurnoutManager class
  *
- * @author	Bob Jacobsen Copyright 2004, 2008 Converted to multiple connection
+ * @author Bob Jacobsen Copyright 2004, 2008 Converted to multiple connection
  * @author kcameron Copyright (C) 2011
  */
 public class SerialTurnoutManagerTest extends jmri.managers.AbstractTurnoutMgrTestBase {
@@ -93,6 +96,14 @@ public class SerialTurnoutManagerTest extends jmri.managers.AbstractTurnoutMgrTe
         Assert.assertNull(l.getTurnout(t.getSystemName().toLowerCase()));
     }
 
+    @Override
+    @Test
+    public void testRegisterDuplicateSystemName() throws PropertyVetoException, NoSuchFieldException,
+            NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        String s1 = l.makeSystemName("B1");
+        String s2 = l.makeSystemName("B2");
+        testRegisterDuplicateSystemName(l, s1, s2);
+    }
 
     // The minimal setup for log4J
     @After
