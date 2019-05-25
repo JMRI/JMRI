3131a9dda6e58a37dd611116f6ab3d0877f13561
Bob Jacobsen <jacobsen@mac.com>
2019-05-25 09:43:41 -0700

    use pre-existing TC instead of re-creating

--- java/src/jmri/jmrix/easydcc/networkdriver/NetworkDriverAdapter.java
+++ java/src/jmri/jmrix/easydcc/networkdriver/NetworkDriverAdapter.java
@@ -28,13 +28,13 @@ public class NetworkDriverAdapter extends EasyDccNetworkPortController {
      */
     @Override
     public void configure() {
-        // connect to the traffic controller
+        // connect to the traffic controller, which is provided via the memo
         log.debug("set tc for memo {}", getSystemConnectionMemo().getUserName());
-        EasyDccTrafficController control = new EasyDccTrafficController(getSystemConnectionMemo());
-        control.connectPort(this);
-        this.getSystemConnectionMemo().setEasyDccTrafficController(control);
+
+        getSystemConnectionMemo().getTrafficController().connectPort(this);
+
         // do the common manager config
-        this.getSystemConnectionMemo().configureManagers();
+        getSystemConnectionMemo().configureManagers();
     }
 
     @Override
