ea4fbe1ddf47e8f73b6f49a23455f99fc5092d02
Randall Wood <randall.h.wood@alexandriasoftware.com>
2019-07-04 14:10:56 -0400

    feat: Add JInputVerifier for NamedBean system names

---
+++ java/src/jmri/swing/SystemNameValidator.java
@@ -0,0 +1,93 @@
+package jmri.swing;
+
+import com.alexandriasoftware.swing.JInputValidator;
+import com.alexandriasoftware.swing.JInputValidatorPreferences;
+import com.alexandriasoftware.swing.Validation;
+import javax.swing.JComponent;
+import javax.swing.text.JTextComponent;
+import jmri.Manager;
+import jmri.Manager.NameValidity;
+
+/**
+ * A {@link com.alexandriasoftware.swing.JInputValidator} that validates a
+ * {@link jmri.NamedBean} system name.
+ * <p>
+ * Until the component gets focus, no validation icon is shown. Once the
+ * component has focus the following icons are shown:
+ * <ul>
+ * <li>If the component is blank and required was false when the validator was
+ * created, no validation is shown.</li>
+ * <li>If the component is blank and required was true when the validator was
+ * created, a warning icon is shown.</li>
+ * <li>If the component has an invalid system name, an error icon is shown.</li>
+ * <li>If the component has a potentially valid system name, a waring icon is
+ * shown.</li>
+ * <li>If the component has a valid system name, a success icon is shown.</li>
+ * </ul>
+ *
+ * @author Randall Wood Copyright 2019
+ */
+public class SystemNameValidator extends JInputValidator {
+
+    private final Manager<?> manager;
+    private boolean required = false;
+
+    /**
+     * Create a SystemNameValidator. Same as calling
+     * {@link #SystemNameValidator(javax.swing.JComponent, jmri.Manager, boolean)}
+     * with {@code required == false}.
+     *
+     * @param component the component to validate has a valid system name
+     * @param manager   the manager that will be used for validation
+     */
+    public SystemNameValidator(JComponent component, Manager<?> manager) {
+        this(component, manager, false);
+    }
+
+    /**
+     * Create a SystemNameValidator.
+     *
+     * @param component the component to validate has a valid system name
+     * @param manager   the manager that will be used for validation
+     * @param required  true if input must be valid and
+     *                  {@link javax.swing.InputVerifier#verify(javax.swing.JComponent)}
+     *                  must return true to allow focus change; false otherwise
+     */
+    public SystemNameValidator(JComponent component, Manager<?> manager, boolean required) {
+        super(component, true, required);
+        this.manager = manager;
+        this.required = required;
+    }
+
+    @Override
+    protected Validation getValidation(JComponent component, JInputValidatorPreferences preferences) {
+        if (component instanceof JTextComponent) {
+            JTextComponent jtc = (JTextComponent) component;
+            String text = jtc.getText();
+            if (text != null && !text.isEmpty()) {
+                try {
+                    manager.validateSystemNameFormat(text);
+                } catch (IllegalArgumentException ex) {
+                    if (manager.validSystemNameFormat(text) == NameValidity.VALID_AS_PREFIX_ONLY) {
+                        return new Validation(Validation.Type.WARNING, Bundle.getMessage("SystemNameValidatorValidPrefix", text, manager.getBeanTypeHandled(), 
+                                        trimHtmlTags(getToolTipText())), preferences);
+                    }
+                    return new Validation(Validation.Type.DANGER, Bundle.getMessage("SystemNameValidatorInvalid", ex.getMessage(), trimHtmlTags(getToolTipText())), preferences);
+                }
+                return new Validation(Validation.Type.SUCCESS, getToolTipText(), preferences);
+            }
+        }
+        if (required) {
+            return new Validation(Validation.Type.WARNING, Bundle.getMessage("SystemNameValidatorRequired", trimHtmlTags(getToolTipText())), preferences);
+        }
+        return getNoneValidation();
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+}
