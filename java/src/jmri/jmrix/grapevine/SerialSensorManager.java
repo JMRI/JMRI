6b64dc11d4077db1a1c8065a9857f72ef8e91b05
Randall Wood <randall.h.wood@alexandriasoftware.com>
2019-07-15 08:12:17 -0400

    fix: Add validation to Grapevine managers

--- java/src/jmri/jmrix/grapevine/SerialSensorManager.java
+++ java/src/jmri/jmrix/grapevine/SerialSensorManager.java
@@ -1,5 +1,6 @@
 package jmri.jmrix.grapevine;
 
+import java.util.Locale;
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import jmri.JmriException;
@@ -120,14 +121,19 @@ public class SerialSensorManager extends jmri.managers.AbstractSensorManager
     }
 
     /**
-     * Validate system name format.
-     *
-     * @return 'true' if system name has a valid format,
-     * else returns 'false'
+     * {@inheritDoc}
+     */
+    @Override
+    public String validateSystemNameFormat(String name, Locale locale) {
+        return SerialAddress.validateSystemNameFormat(name, this, locale);
+    }
+
+    /**
+     * {@inheritDoc}
      */
     @Override
     public NameValidity validSystemNameFormat(String systemName) {
-        return (SerialAddress.validSystemNameFormat(systemName, 'S', getSystemPrefix()));
+        return SerialAddress.validSystemNameFormat(systemName, typeLetter(), getSystemPrefix());
     }
 
     /**
