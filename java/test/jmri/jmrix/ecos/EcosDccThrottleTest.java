9a034006ff59fff15343d6fb1bc7083daaf9e24c
Randall Wood <randall.h.wood@alexandriasoftware.com>
2019-07-28 19:31:52 -0400

    fix: recover from merge gone wrong

--- java/test/jmri/jmrix/ecos/EcosDccThrottleTest.java
+++ java/test/jmri/jmrix/ecos/EcosDccThrottleTest.java
@@ -4,6 +4,7 @@ import java.awt.GraphicsEnvironment;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
