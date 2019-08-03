3aea592f5523da54744a2f465e1b629905f86de8
Daniel <db123@bergqvist.se>
2019-08-03 12:45:33 +0200

    Demonstrate that LL1 and LL01 are different

--- java/test/jmri/jmrix/loconet/LnLightManagerTest.java
+++ java/test/jmri/jmrix/loconet/LnLightManagerTest.java
@@ -1,5 +1,7 @@
 package jmri.jmrix.loconet;
 
+import jmri.Light;
+import jmri.LightManager;
 import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -21,6 +23,29 @@ public class LnLightManagerTest {
         Assert.assertNotNull("exists",t);
     }
 
+    @Test
+    public void testManager() {
+        LocoNetSystemConnectionMemo memo = new LocoNetSystemConnectionMemo();
+        LnTrafficController lnis = new LocoNetInterfaceScaffold(memo);
+        memo.setLnTrafficController(lnis);
+        LnLightManager lm = new LnLightManager(memo);
+        Assert.assertNotNull("exists",lm);
+        LnLight light1 = new LnLight("LL1", lnis, lm);
+        LnLight light2 = new LnLight("LL01", lnis, lm);
+        lm.register(light1);
+        lm.register(light2);
+        for (Light l : lm.getNamedBeanSet()) {
+            System.out.format("Light: %s%n", l.getSystemName());
+        }
+        
+        System.out.format("Light light1: %s%n", lm.getBySystemName("LL1"));
+        System.out.format("Light light2: %s%n", lm.getBySystemName("LL01"));
+        
+        Assert.assertEquals("lights are equal",
+                lm.getBySystemName("LL1"),
+                lm.getBySystemName("LL01"));
+    }
+    
     @Before
     public void setUp() {
         JUnitUtil.setUp();
