ab18215c9304d41fc4ef5f5ae2afc0f22b2de6d0
Randall Wood <randall.h.wood@alexandriasoftware.com>
2019-08-09 07:38:56 -0400

    chore: add missing override

--- java/src/jmri/jmrit/ctc/editor/gui/FrmMainForm.java
+++ java/src/jmri/jmrit/ctc/editor/gui/FrmMainForm.java
@@ -188,6 +188,7 @@ public class FrmMainForm extends JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
         setLocation(new java.awt.Point(0, 0));
         addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 formWindowClosing(evt);
             }
