865a9b522a97fd5aad15b40cbf040d2c48f7c6c5
devel-bobm <devel-bobm@sbcglobal.net>
2019-08-27 17:44:05 -0400

    coverage improvements

--- java/test/jmri/jmrix/loconet/LnReporterTest.java
+++ java/test/jmri/jmrix/loconet/LnReporterTest.java
@@ -1,10 +1,14 @@
 package jmri.jmrix.loconet;
 
+import jmri.IdTag;
 import jmri.InstanceManager;
 import jmri.Reportable;
 import jmri.LocoAddress;
+import jmri.NamedBean;
+import jmri.PhysicalLocationReporter;
 import jmri.jmrix.internal.InternalSystemConnectionMemo;
 import jmri.util.JUnitUtil;
+import jmri.util.PhysicalLocation;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -83,11 +87,64 @@ public class LnReporterTest extends jmri.implementation.AbstractReporterTestBase
         a3.messageFromManager(l);
         Assert.assertEquals("Lissy message 2", "8501 seen northbound", ((Reportable)a3.getLastReport()).toReportString());
     }
+    
+    @Test
+    public void testMessageFromManagerFindReport() {
+        Assert.assertEquals("MessageFromManagerFindReport- check initial state", 
+                -1, r.getState());
+        Assert.assertEquals("check reporter number", 3, ((LnReporter)r).getNumber());
+
+        LocoNetMessage m = new LocoNetMessage(new int[] {0xe5, 0x09, 0x00, 0x00, 0x07, 0x00, 0x00, 0x00, 0x00});
+        ((LnReporter)r).messageFromManager(m);
+
+        Assert.assertEquals("MessageFromManagerFindReport- check state after message 1", 
+                7, r.getState());
+        
+        Assert.assertEquals("MessageFromManagerFindReport- check report string after message 1", 
+                "7 enter", r.getCurrentReport().toString() );
+
+        Assert.assertEquals("MessageFromManagerFindReport- check last loco after message 1",
+                7, ((LnReporter)r).lastLoco);
+        
+        Assert.assertEquals("MessageFromManagerFindReport- check location last seen after message 1", 
+                "LR3", ((IdTag)((LnReporter)r).getCurrentReport()).getWhereLastSeen().toString());
+        Assert.assertEquals("MessageFromManagerFindReport- check ID last seen after message 1", 
+                "LD7", ((IdTag)((LnReporter)r).getCurrentReport()).getDisplayName());
+
+        LnReporter r2 = new LnReporter(17, tc, "L");
+        Assert.assertEquals("MessageFromManagerFindReport - check state of new reporter",
+                -1, r2.getState());
+
+        LocoNetMessage m2 = new LocoNetMessage(new int[] {0xe5, 0x09, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00});
+        ((LnReporter)r2).messageFromManager(m2);
+        Assert.assertEquals("MessageFromManagerFindReport- check state after message 1", 
+                3, r2.getState());
+        
+    }
+    
+    @Test
+    public void testGetBeanPhysicalLocation() {
+        // NOTE: it is unclear how JMRI makes use of the "physical location" 
+        // feature with respect to Reporters and IdTags, so testing here is 
+        // sketchy at best.
+        LocoNetMessage m = new LocoNetMessage(new int[]{0xD0, 0x21, 0x2E, 0x08, 0x20, 0x04});
+        ((LnReporter)r).messageFromManager(m);
+        Assert.assertEquals("check physical location message 1", PhysicalLocation.Origin, ((LnReporter)r).getPhysicalLocation());
+        Assert.assertEquals("check physical location string message 1", PhysicalLocation.Origin, ((LnReporter)r).getPhysicalLocation("balderdash"));
+    }
 
     @Test
     public void testLnReporterGetLocoAddress() {
         LocoAddress t = ((LnReporter)r).getLocoAddress("7413 enter");
-        Assert.assertEquals("getLocoAddress", t.getNumber(), 7413);
+        Assert.assertEquals("getLocoAddress 7431 enter", t.getNumber(), 7413);
+        
+        LocoAddress t2 = ((LnReporter)r).getLocoAddress(null);
+        Assert.assertNull("getLocoAddress <null>", t2);
+        
+        LocoAddress t3 = ((LnReporter)r).getLocoAddress("abdc enter");
+        Assert.assertNull("getLocoAddress abcd enter", t3);
+        
+        
     }
 
     @Test
@@ -146,6 +203,9 @@ public class LnReporterTest extends jmri.implementation.AbstractReporterTestBase
 
         m = new LocoNetMessage(new int[] {0xe5, 0x09, 0x7f, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
         Assert.assertFalse("isTranspondingFindReport-0xE5 0x09 0x7f", ((LnReporter)r).isTranspondingFindReport(m));
+
+        m = new LocoNetMessage(new int[] {0xe5, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00});
+        Assert.assertFalse("isTranspondingFindReport-0xE5 0x08 0x00", ((LnReporter)r).isTranspondingFindReport(m));
 
         m = new LocoNetMessage(new int[] {0xD0, 0x49, 0x00, 0x00, 0x00, 0x00});
         Assert.assertFalse("isTranspondingFindReport-0xD0 0x49", ((LnReporter)r).isTranspondingFindReport(m));
@@ -237,6 +297,85 @@ public class LnReporterTest extends jmri.implementation.AbstractReporterTestBase
 
         m = new LocoNetMessage(new int[] {0, 0, 0, 0x7D, 0x7f, 0});
         Assert.assertEquals("getLocoAddrFromTranspondingMsg-0 0 0 0x7D 0x7F 0", 127, ((LnReporter)r).getLocoAddrFromTranspondingMsg(m));
+    }
+    
+    @Test
+    public void testGetNumber() {
+        Assert.assertEquals("getNumber - A", 3, ((LnReporter)r).getNumber());
+        
+        r = new LnReporter(1, tc, "L");
+        Assert.assertEquals("getNumber - B", 1, ((LnReporter)r).getNumber());
+        
+        r = new LnReporter(42, tc, "L");
+        Assert.assertEquals("getNumber - C", 42, ((LnReporter)r).getNumber());
+    }
+    
+    @Test
+    public void testGetPhysicalLocationAndAddress() {
+//        Assert.assertEquals("initial physical location", 
+//            PhysicalLocationReporter.Direction.UNKNOWN, 
+//            ((LnReporter)r).getDirection(((Reportable)r.getLastReport()).toReportString()));
+        
+        
+        LocoNetMessage m = new LocoNetMessage(new int[] {0xe5, 0x09, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00});
+        ((LnReporter)r).messageFromManager(m);
+        Assert.assertEquals("physical location after message 1", 
+            PhysicalLocationReporter.Direction.ENTER, 
+            ((LnReporter)r).getDirection(((Reportable)r.getLastReport()).toReportString()));
+        Assert.assertEquals("loco addr after message 1", "128(D)", 
+                ((LnReporter)r).getLocoAddress(
+                        ((Reportable)r.getLastReport()).toReportString()).toString());
+
+        m = new LocoNetMessage(new int[]{0xD0, 0x01, 0x11, 0x02, 0x01, 0x00});
+        ((LnReporter)r).messageFromManager(m);
+        Assert.assertEquals("physical location after message 2", 
+            PhysicalLocationReporter.Direction.EXIT,
+            ((LnReporter)r).getDirection(((Reportable)r.getLastReport()).toReportString()));
+        Assert.assertEquals("loco addr after message 2", "257(D)", 
+                ((LnReporter)r).getLocoAddress(
+                        ((Reportable)r.getLastReport()).toReportString()).toString());
+        
+        m = new LocoNetMessage(new int[]{0xD0, 0x21, 0x11, 0x02, 0x01, 0x00});
+        ((LnReporter)r).messageFromManager(m);
+        Assert.assertEquals("physical location after message 2", 
+            PhysicalLocationReporter.Direction.ENTER,
+            ((LnReporter)r).getDirection(((Reportable)r.getLastReport()).toReportString()));
+
+        Assert.assertEquals("getDirection- check nonsense string", 
+                PhysicalLocationReporter.Direction.UNKNOWN,
+                ((LnReporter)r).getDirection("harrumph 84"));
+
+        Assert.assertEquals("getDirection- check short string", 
+                PhysicalLocationReporter.Direction.UNKNOWN,
+                ((LnReporter)r).getDirection("harrumph"));
+
+        Assert.assertEquals("getDirection- check seen lissy 1", 
+                PhysicalLocationReporter.Direction.ENTER,
+                ((LnReporter)r).getDirection("54 seen northbound"));
+
+        Assert.assertEquals("getDirection- check seen lissy 2", 
+                PhysicalLocationReporter.Direction.ENTER,
+                ((LnReporter)r).getDirection("54 seen southbound"));
+
+        Assert.assertEquals("getDirection- check exit", 
+                PhysicalLocationReporter.Direction.EXIT,
+                ((LnReporter)r).getDirection("155 exits"));
+
+    }
+
+    @Test
+    public void testLnReporterLissyBad() {
+        LnReporter a1 = new LnReporter(1, tc, "L");
+        
+        LocoNetMessage l = new LocoNetMessage(new int[]{0xE4, 0x07, 0x00, 0x60, 0x01, 0x42, 0x35, 0x05});
+        a1.messageFromManager(l);
+        Assert.assertEquals("bad Lissy message check report is null", null, ((Reportable)a1.getLastReport()));
+    }
+
+    
+    @Test
+    public void testGetDriectionString() {
+        
     }
     @Test
     public void testCollectionAfterMessage() {
