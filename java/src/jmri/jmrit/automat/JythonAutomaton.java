586af417fb248352ce72874cd34db51457eb894c
Randall Wood <randall.h.wood@alexandriasoftware.com>
2019-09-20 07:34:07 -0400

    fix: Use Jython directly instead of by reflection

--- java/src/jmri/jmrit/automat/JythonAutomaton.java
+++ java/src/jmri/jmrit/automat/JythonAutomaton.java
@@ -1,22 +1,20 @@
 package jmri.jmrit.automat;
 
-import java.lang.reflect.InvocationTargetException;
 import jmri.InstanceManager;
+
+import org.python.core.PySystemState;
+import org.python.util.PythonInterpreter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * This sample Automaton invokes a Jython interpreter to handle a script.
- * <p>
- * Access is via Java reflection so that both users and developers can work
- * without the jython-standalone-2.7.0.jar file in the classpath. To make it easier to read the
- * code, the "non-reflection" statements are in the comments
  *
  * @author Bob Jacobsen Copyright (C) 2003
  */
 public class JythonAutomaton extends AbstractAutomaton {
 
-    Object interp;
+    PythonInterpreter interp;
 
     public JythonAutomaton(String file) {
         filename = file;
@@ -38,34 +36,18 @@ public class JythonAutomaton extends AbstractAutomaton {
     @Override
     protected void init() {
 
-        try {
-            // PySystemState.initialize();
-            Class<?> cs = Class.forName("org.python.core.PySystemState");
-            java.lang.reflect.Method initialize
-                    = cs.getMethod("initialize", (Class[]) null);
-            initialize.invoke(null, (Object[]) null);
+        PySystemState.initialize();
 
-            // interp = new PythonInterpreter();
-            interp = Class.forName("org.python.util.PythonInterpreter").getDeclaredConstructor().newInstance();
+        interp = new PythonInterpreter();
 
-            // load some general objects
-            java.lang.reflect.Method set
-                    = interp.getClass().getMethod("set", new Class[]{String.class, Object.class});
-            set.invoke(interp, new Object[]{"dcc", InstanceManager.getNullableDefault(jmri.CommandStation.class)});
-            set.invoke(interp, new Object[]{"self", this});
+        // load some general objects
+        interp.set("dcc", InstanceManager.getNullableDefault(jmri.CommandStation.class));
 
-            // set up the method to exec python functions
-            exec = interp.getClass().getMethod("exec", new Class[]{String.class});
+        // have jython read the file
+        interp.execfile(filename);
 
-            // have jython read the file
-            exec.invoke(interp, new Object[]{"execfile(\"" + filename + "\")"});
-
-            // execute the init routine in the jython class
-            exec.invoke(interp, new Object[]{"init()"});
-
-        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
-            log.error("Exception creating jython system objects", e);
-        }
+        // execute the init routine in the jython class
+        interp.exec("init()");
     }
 
     /**
@@ -79,26 +61,14 @@ public class JythonAutomaton extends AbstractAutomaton {
             log.error("No interpreter, so cannot handle automat");
             return false; // to terminate operation
         }
-        try {
-            // execute the handle routine in the jython and check return value
-            exec.invoke(interp, new Object[]{"retval = handle()"});
-            java.lang.reflect.Method get
-                    = interp.getClass().getMethod("get", new Class[]{String.class});
-            Object retval = get.invoke(interp, new Object[]{"retval"});
-            System.out.println("retval = " + retval);
-            if (retval.toString().equals("1")) {
-                return true;
-            }
-            return false;
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            log.error("Exception during handle routine", e);
-            return false;
-        }
+        // execute the handle routine in the jython and check return value
+        interp.exec("retval = handle()");
+        Object retval = interp.get("retval");
+        log.info("retval = {}", retval);
+        return retval.toString().equals("1");
     }
 
-    java.lang.reflect.Method exec;
-
     // initialize logging
-    private final static Logger log = LoggerFactory.getLogger(JythonAutomaton.class);
+    private static final Logger log = LoggerFactory.getLogger(JythonAutomaton.class);
 
 }
--- java/src/jmri/jmrit/automat/JythonSiglet.java
+++ java/src/jmri/jmrit/automat/JythonSiglet.java
@@ -1,6 +1,9 @@
 package jmri.jmrit.automat;
 
 import jmri.InstanceManager;
+
+import org.python.core.PySystemState;
+import org.python.util.PythonInterpreter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,16 +16,12 @@ import org.slf4j.LoggerFactory;
  * <li>defineIO()
  * <li>setOutput()
  * </ul>
- * <p>
- * Access is via Java reflection so that both users and developers can work
- * without the jython-standalone-2.7.0.jar file in the classpath. To make it easier to read the
- * code, the "non-reflection" statements are in the comments.
  *
  * @author Bob Jacobsen Copyright (C) 2003
  */
 public class JythonSiglet extends Siglet {
 
-    Object interp;
+    PythonInterpreter interp;
 
     public JythonSiglet(String file) {
         filename = file;
@@ -44,59 +43,33 @@ public class JythonSiglet extends Siglet {
     @Override
     public void defineIO() {
 
-        try {
-            // PySystemState.initialize();
-            Class<?> cs = Class.forName("org.python.core.PySystemState");
-            java.lang.reflect.Method initialize
-                    = cs.getMethod("initialize", (Class[]) null);
-            initialize.invoke(null, (Object[]) null);
+        PySystemState.initialize();
 
-            // interp = new PythonInterpreter();
-            interp = Class.forName("org.python.util.PythonInterpreter").getDeclaredConstructor().newInstance();
+        interp = new PythonInterpreter();
 
-            // load some general objects
-            java.lang.reflect.Method set
-                    = interp.getClass().getMethod("set", new Class[]{String.class, Object.class});
-            set.invoke(interp, new Object[]{"self", this});
+        // load some general objects
+        interp.set("inputs", inputs);
+        interp.set("outputs", outputs);
+        interp.set("turnouts", InstanceManager.turnoutManagerInstance());
+        interp.set("sensors", InstanceManager.sensorManagerInstance());
+        interp.set("signals", InstanceManager.getDefault(jmri.SignalHeadManager.class));
+        interp.set("dcc", InstanceManager.getNullableDefault(jmri.CommandStation.class));
 
-            set.invoke(interp, new Object[]{"inputs", inputs});
-            set.invoke(interp, new Object[]{"outputs", outputs});
+        interp.set("CLOSED", Integer.valueOf(jmri.Turnout.CLOSED));
+        interp.set("THROWN", Integer.valueOf(jmri.Turnout.THROWN));
+        interp.set("ACTIVE", Integer.valueOf(jmri.Sensor.ACTIVE));
+        interp.set("INACTIVE", Integer.valueOf(jmri.Sensor.INACTIVE));
+        interp.set("GREEN", Integer.valueOf(jmri.SignalHead.GREEN));
+        interp.set("YELLOW", Integer.valueOf(jmri.SignalHead.YELLOW));
+        interp.set("RED", Integer.valueOf(jmri.SignalHead.RED));
 
-            set.invoke(interp, new Object[]{"turnouts", InstanceManager.turnoutManagerInstance()});
-            set.invoke(interp, new Object[]{"sensors", InstanceManager.sensorManagerInstance()});
-            set.invoke(interp, new Object[]{"signals", InstanceManager.getDefault(jmri.SignalHeadManager.class)});
-            set.invoke(interp, new Object[]{"dcc", InstanceManager.getNullableDefault(jmri.CommandStation.class)});
+        // have jython read the file
+        interp.execfile(filename);
 
-            set.invoke(interp, new Object[]{"CLOSED", Integer.valueOf(jmri.Turnout.CLOSED)});
-            set.invoke(interp, new Object[]{"THROWN", Integer.valueOf(jmri.Turnout.THROWN)});
-            set.invoke(interp, new Object[]{"ACTIVE", Integer.valueOf(jmri.Sensor.ACTIVE)});
-            set.invoke(interp, new Object[]{"INACTIVE", Integer.valueOf(jmri.Sensor.INACTIVE)});
-            set.invoke(interp, new Object[]{"GREEN", Integer.valueOf(jmri.SignalHead.GREEN)});
-            set.invoke(interp, new Object[]{"YELLOW", Integer.valueOf(jmri.SignalHead.YELLOW)});
-            set.invoke(interp, new Object[]{"RED", Integer.valueOf(jmri.SignalHead.RED)});
+        // execute the init routine in the jython class
+        interp.exec("defineIO()");
 
-            // set up the method to exec python functions
-            exec = interp.getClass().getMethod("exec", new Class[]{String.class});
-
-            // have jython read the file
-            exec.invoke(interp, new Object[]{"execfile(\"" + filename + "\")"});
-
-            // execute the init routine in the jython class
-            exec.invoke(interp, new Object[]{"defineIO()"});
-
-            System.out.println("inputs[0]: " + inputs[0]);
-
-        } catch (IllegalAccessException e) {
-            log.error("IllegalAccessException creating jython system objects", e);
-        } catch (NoSuchMethodException e) {
-            log.error("NoSuchMethodException creating jython system objects", e);
-        } catch (ClassNotFoundException e) {
-            log.error("ClassNotFoundException creating jython system objects", e);
-        } catch (InstantiationException e) {
-            log.error("InstantiationException creating jython system objects", e);
-        } catch (java.lang.reflect.InvocationTargetException e) {
-            log.error("InvocationTargetException creating jython system objects", e);
-        }
+        log.info("inputs[0]: {}", inputs[0]);
     }
 
     /**
@@ -108,17 +81,10 @@ public class JythonSiglet extends Siglet {
             log.error("No interpreter, so cannot handle automat");
             return;
         }
-        try {
-            // execute the handle routine in the jython
-            exec.invoke(interp, new Object[]{"setOutput()"});
-        } catch (Exception e) {
-            log.error("Exception invoking jython command:", e);
-        }
+        interp.exec("setOutput()");
     }
 
-    java.lang.reflect.Method exec;
-
     // initialize logging
-    private final static Logger log = LoggerFactory.getLogger(JythonSiglet.class);
+    private static final Logger log = LoggerFactory.getLogger(JythonSiglet.class);
 
 }
