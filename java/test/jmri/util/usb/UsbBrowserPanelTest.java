6ce5dac24f3e0aeb6f67fb3e0275535348edb4e0
Paul Bender <paul.bender@acm.org>
2019-09-23 18:34:59 -0400

    we need a hub too.

--- java/test/jmri/util/usb/UsbBrowserPanelTest.java
+++ java/test/jmri/util/usb/UsbBrowserPanelTest.java
@@ -6,6 +6,12 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.usb.*;
+import javax.usb.event.UsbDeviceListener;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Paul Bender Copyright (C) 2017	
@@ -18,12 +24,7 @@ public class UsbBrowserPanelTest {
         UsbBrowserPanel t = new UsbBrowserPanel(){
            @Override
            protected UsbTreeNode getRootNode() {
-              return new UsbTreeNode(new UsbDeviceScaffold("foo","bar"){
-                      @Override
-                      public boolean isUsbHub() {
-                          return true;
-                      }
-              });
+              return new UsbTreeNode(new UsbHubScaffold());
            }
         };
         Assert.assertNotNull("exists",t);
@@ -41,5 +42,44 @@ public class UsbBrowserPanelTest {
     }
 
     // private final static Logger log = LoggerFactory.getLogger(UsbBrowserPanelTest.class);
+
+    class UsbHubScaffold extends UsbDeviceScaffold implements UsbHub {
+
+        public UsbHubScaffold() {
+            super("jmri","testhub");
+        }
+
+        @Override
+        public boolean isUsbHub() {
+            return true;
+        }
+
+        @Override
+        public byte getNumberOfPorts() {
+            return 0;
+        }
+
+        @Override
+        public List getUsbPorts() {
+            return null;
+        }
+
+        @Override
+        public UsbPort getUsbPort(byte b) {
+            return null;
+        }
+
+        @Override
+        public List getAttachedUsbDevices() {
+            return new ArrayList<UsbDevice>();
+        }
+
+        @Override
+        public boolean isRootUsbHub() {
+            return false;
+        }
+    }
+
+}
 
 }
