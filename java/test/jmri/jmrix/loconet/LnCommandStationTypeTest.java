c220314ee372f23768fdae541c9103706e9ede71
devel-bobm <devel-bobm@sbcglobal.net>
2019-09-26 13:50:13 -0400

    Test dcs52 Loco_Reset support

--- java/test/jmri/jmrix/loconet/LnCommandStationTypeTest.java
+++ java/test/jmri/jmrix/loconet/LnCommandStationTypeTest.java
@@ -53,23 +53,24 @@ public class LnCommandStationTypeTest {
 
     @Test
     public void testSupportsLocoReset() {
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_DCS050.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_DCS051.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_DCS100.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_DB150.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_DCS200.getSupportsLocoReset());
-        Assert.assertTrue(LnCommandStationType.COMMAND_STATION_DCS210.getSupportsLocoReset());
-        Assert.assertTrue(LnCommandStationType.COMMAND_STATION_DCS240.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_PR2_ALONE.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_PR3_ALONE.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_STANDALONE.getSupportsLocoReset());
+        Assert.assertFalse("DCS050 loco reset expect false", LnCommandStationType.COMMAND_STATION_DCS050.getSupportsLocoReset());
+        Assert.assertFalse("DCS051 loco reset expect false", LnCommandStationType.COMMAND_STATION_DCS051.getSupportsLocoReset());
+        Assert.assertTrue("DCS052 loco reset expect true", LnCommandStationType.COMMAND_STATION_DCS052.getSupportsLocoReset());
+        Assert.assertFalse("DCS100 loco reset expect false", LnCommandStationType.COMMAND_STATION_DCS100.getSupportsLocoReset());
+        Assert.assertFalse("DB150 loco reset expect false", LnCommandStationType.COMMAND_STATION_DB150.getSupportsLocoReset());
+        Assert.assertFalse("DCS200 loco reset expect false", LnCommandStationType.COMMAND_STATION_DCS200.getSupportsLocoReset());
+        Assert.assertTrue("DCS210 loco reset expect true", LnCommandStationType.COMMAND_STATION_DCS210.getSupportsLocoReset());
+        Assert.assertTrue("DCS242 loco reset expect true", LnCommandStationType.COMMAND_STATION_DCS240.getSupportsLocoReset());
+        Assert.assertFalse("PR2 standalone loco reset expect false", LnCommandStationType.COMMAND_STATION_PR2_ALONE.getSupportsLocoReset());
+        Assert.assertFalse("PR3 standalone loco reset expect false", LnCommandStationType.COMMAND_STATION_PR3_ALONE.getSupportsLocoReset());
+        Assert.assertFalse("Standalone loco reset expect false", LnCommandStationType.COMMAND_STATION_STANDALONE.getSupportsLocoReset());
 
         // the following command station types are _assumed_ by the coder to not support "idle".
         // these assertions need to be verified with respect to real hardware.
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_IBX_TYPE_1.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_IBX_TYPE_2.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_LBPS.getSupportsLocoReset());
-        Assert.assertFalse(LnCommandStationType.COMMAND_STATION_MM.getSupportsLocoReset());
+        Assert.assertFalse("IBX Type 1 loco reset expect false", LnCommandStationType.COMMAND_STATION_IBX_TYPE_1.getSupportsLocoReset());
+        Assert.assertFalse("IBX Type 2 loco reset expect false", LnCommandStationType.COMMAND_STATION_IBX_TYPE_2.getSupportsLocoReset());
+        Assert.assertFalse("LBPS loco reset expect false", LnCommandStationType.COMMAND_STATION_LBPS.getSupportsLocoReset());
+        Assert.assertFalse("MM loco reset expect false", LnCommandStationType.COMMAND_STATION_MM.getSupportsLocoReset());
         }
 
     // The minimal setup for log4J
