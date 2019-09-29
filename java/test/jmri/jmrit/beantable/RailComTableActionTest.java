e3a3fb947ed6713428ea838635b049e2c99849f5
Randall Wood <randall.h.wood@alexandriasoftware.com>
2019-09-29 17:52:10 -0400

    test: Fix imports
    Got burned by an incremental build that failed to fail when a clean build failed completly.

--- java/test/jmri/jmrit/beantable/RailComTableActionTest.java
+++ java/test/jmri/jmrit/beantable/RailComTableActionTest.java
@@ -1,6 +1,6 @@
 package jmri.jmrit.beantable;
 
-import jmri.RailCom;
+import jmri.IdTag;
 import jmri.util.JUnitUtil;
 import org.junit.After;
 import org.junit.Assert;
@@ -11,7 +11,7 @@ import org.junit.Test;
  *
  * @author Paul Bender Copyright (C) 2017
  */
-public class RailComTableActionTest extends AbstractTableActionBase<RailCom> {
+public class RailComTableActionTest extends AbstractTableActionBase<IdTag> {
 
     @Test
     public void testCTor() {
