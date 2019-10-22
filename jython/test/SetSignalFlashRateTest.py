a6a035f88ebce514fab965aebceb73fc792026e2
Bob Jacobsen <jacobsen@mac.com>
2019-10-22 10:34:44 -0700

    better test

--- jython/test/SetSignalFlashRateTest.py
+++ jython/test/SetSignalFlashRateTest.py
@@ -3,6 +3,15 @@ import jmri
 
 execfile("jython/SetSignalFlashRate.py")
 
-if ( jmri.implementation.DefaultSignalHead.masterDelay != 1000) :
+initial = jmri.implementation.DefaultSignalHead.masterDelay
+
+jmri.implementation.DefaultSignalHead.masterDelay = 2*initial+1
+
+if ( jmri.implementation.DefaultSignalHead.masterDelay != 2*initial+1) :
     raise AssertionError("Time not changed")
+    
+jmri.implementation.DefaultSignalHead.masterDelay = initial
+
+if ( jmri.implementation.DefaultSignalHead.masterDelay != initial) :
+    raise AssertionError("Time not reset")
 
