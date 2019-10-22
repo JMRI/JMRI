d78640aba689fda8f35cccfebd52924258068e99
Bob Jacobsen <jacobsen@mac.com>
2019-10-22 10:28:11 -0700

    includes jar and file signing

--- scripts/diskimage-sign.sh
+++ scripts/diskimage-sign.sh
@@ -13,6 +13,10 @@
 #    Apple ID for notarization
 #    Application-specific password on that Apple ID (see appleid.apple.com)
 #
+# Although it might be consuming additional space on the final disk image,
+# we do the signing and jar-updating there to ensure this doesn't
+# cause issues for other uses of the raw as-built files
+#
 # Copyright 2007, 2011, 2016, 2019 Bob Jacobsen, david d zuhn
 #
 
@@ -29,7 +33,7 @@ AC_PASSWORD=$6
 
 # -----------------------------------------
 # Retry a command up to a specific number of times until it exits successfully.
-# Waits 20 seconds times exponential back off.
+# Waits 1 minute between retries
 #
 #  $ retry 5 echo Hello
 #  Hello
@@ -42,7 +46,7 @@ function retry {
   local count=0
   until "$@"; do
     exit=$?
-    wait=$(( (2 ** $count) * 20))
+    wait=60
     count=$(($count + 1))
     if [ $count -lt $retries ]; then
       echo "Retry $count/$retries exited $exit, retrying in $wait seconds..."
@@ -52,6 +56,28 @@ function retry {
       return $exit
     fi
   done
+  return 0
+}
+# -----------------------------------------
+# Sign a file (2nd arg) with a jar file (1st arg) in place
+#  
+function signJarMember {
+  local jar=$1
+  local file=$2
+
+  jar xf $jar $file
+  signFile $file
+  jar uvf $jar $file
+  rm $file
+
+  return 0
+}
+# -----------------------------------------
+# Sign a single file (1st arg)
+#  
+function signFile {
+  local file=$1
+  codesign -v -s "$CERTIFICATE" --deep $file
   return 0
 }
 # -----------------------------------------
@@ -106,7 +132,7 @@ hdiutil attach "$INPUTIMAGEFILE" -mountpoint "$tmpindir" -nobrowse
 
 # create disk image and mount
 jmrisize=`du -ms "$tmpindir" | awk '{print $1}'`
-imagesize=`expr $jmrisize + 40`
+imagesize=`expr $jmrisize + 80`
 
 if [ "$SYSTEM" = "MACOSX" ]
 then 
@@ -135,9 +161,27 @@ fi
 tar -C "$tmpindir" -cf - JMRI | $SUDO tar -C "$tmpoutdir" -xf -
 
 # sign the app files in output
-codesign -v -s "$CERTIFICATE" --deep $tmpoutdir/JMRI/PanelPro.app
-codesign -v -s "$CERTIFICATE" --deep $tmpoutdir/JMRI/DecoderPro.app
-codesign -v -s "$CERTIFICATE" --deep $tmpoutdir/JMRI/SoundPro.app
+signFile $tmpoutdir/JMRI/PanelPro.app
+signFile $tmpoutdir/JMRI/DecoderPro.app
+signFile $tmpoutdir/JMRI/SoundPro.app
+
+# sign the individual library files
+signFile $tmpoutdir/JMRI/lib/macosx/libgluegen-rt.jnilib
+signFile $tmpoutdir/JMRI/lib/macosx/libjinput-osx.jnilib
+signFile $tmpoutdir/JMRI/lib/macosx/libjoal.jnilib
+signFile $tmpoutdir/JMRI/lib/macosx/libopenal.1.15.1.dylib
+signFile $tmpoutdir/JMRI/lib/macosx/libopenal.1.dylib
+signFile $tmpoutdir/JMRI/lib/macosx/libopenal.dylib
+
+# sign libraries inside jar files
+signJarMember $tmpoutdir/JMRI/lib/libusb4java-1.2.0-osx-x86_64.jar org/usb4java/osx-x86_64/libusb4java.dylib
+signJarMember $tmpoutdir/JMRI/lib/bluecove-2.1.1-SNAPSHOT.jar libbluecove.jnilib
+signJarMember $tmpoutdir/JMRI/lib/jna-4.4.0.jar com/sun/jna/darwin/libjnidispatch.jnilib
+signJarMember $tmpoutdir/JMRI/lib/hid4java-0.5.0.jar darwin/libhidapi.dylib
+signJarMember $tmpoutdir/JMRI/lib/libusb4java-1.2.0-osx-x86.jar org/usb4java/osx-x86/libusb4java.dylib
+# signJarMember $tmpoutdir/JMRI/lib/selenium-server-standalone-3.6.0.jar com/sun/jna/darwin/libjnidispatch.jnilib # OMITTED DUE TO TOC ISSUE
+signJarMember $tmpoutdir/JMRI/lib/jython-standalone-2.7.1.jar META-INF/native/osx/libjansi.jnilib
+signJarMember $tmpoutdir/JMRI/lib/jython-standalone-2.7.1.jar jni/Darwin/libjffi-1.2.jnilib
 
 # add an Applications icon
 $SUDO ln -s /Applications "$tmpoutdir"
@@ -164,12 +208,13 @@ else
 fi
 
 # sign image file
-codesign -v -s "$CERTIFICATE" --deep "$OUTPUT"
+signFile "$OUTPUT"
 
 # notarize distribution: start by uploading
 xcrun altool --notarize-app --verbose --primary-bundle-id "org.jmri" --username "$AC_USER" --password "$AC_PASSWORD" --file "$OUTPUT"
 # stapling result will temporarily fail while the notatization is still happening at Apple
-retry 8 xcrun stapler staple "$OUTPUT"
+sleep 30
+retry 20 xcrun stapler staple "$OUTPUT"
 
 # clean up
 hdiutil detach $tmpindir 
