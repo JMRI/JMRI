180507b4ede321a0c824bf2106d138bcea10d757
Bob Jacobsen <jacobsen@mac.com>
2019-10-31 10:41:17 -0700

    initial add

---
+++ java/src/jmri/jmrit/logix/package-info.java
@@ -0,0 +1,24 @@
+ /**
+ * Support for implementing Warrants in JMRI.
+ * <p>
+ * Note that despite the name, this package does not implement Logix.
+ * Those are handled elsewhere.
+ * 
+ * <h2>Related Documentation</h2>
+ * 
+ * For overviews, tutorials, examples, guides, and tool documentation, please see:
+ * <ul>
+ *   <li>
+ * </ul>
+ * 
+ * Class diagram of how all this goes together:
+ * <a href="doc-files/PortalObjectDiagram.png"><img src="doc-files/PortalObjectDiagram.png" style="text-align: right;" alt="Code press" height="25%" width="25%"></a>
+ *
+ * <!-- Put @see and @since tags down here. -->
+ * @see jmri.managers
+ * @see jmri.implementation 
+*/
+
+//@annotations for the entire package go here
+
+package jmri.jmrit.ussctc;
