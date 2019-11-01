f59d66500146f8144e381c13520ece0161f6e2f0
Bob Jacobsen <jacobsen@mac.com>
2019-10-31 19:06:13 -0700

    somewhat better diagram

--- java/src/jmri/jmrit/logix/package-info.java
+++ java/src/jmri/jmrit/logix/package-info.java
@@ -8,17 +8,23 @@
  * 
  * For overviews, tutorials, examples, guides, and tool documentation, please see:
  * <ul>
- *   <li>
+ *   <li><a href="https://jmri.org/help/en/package/jmri/jmrit/logix/Warrant.shtml">Warrant help pages</a>
  * </ul>
  * 
  * Class diagram of how all this goes together:
- * <a href="doc-files/PortalObjectDiagram.png"><img src="doc-files/PortalObjectDiagram.png" style="text-align: right;" alt="Code press" height="25%" width="25%"></a>
+ * <a href="doc-files/PortalObjectDiagram.png">
+ *    <img src="doc-files/PortalObjectDiagram.png" style="text-align: right;" alt="Package Class Diagram" height="25%" width="25%"></a>
  *
  * <!-- Put @see and @since tags down here. -->
  * @see jmri.managers
  * @see jmri.implementation 
 */
 
+// The UML diagram referenced above is coded in ./doc/doc-files/PortalObjectDiagram.txt and 
+// can be regeneerated to a PNG file by executing on the command line:
+//    java -jar lib/plantuml.jar java/src/jmri/jmrit/logix/doc-files/PortalObjectDiagram.txt 
+// See https://jmri.org/help/en/html/doc//Technical/Javadoc.shtml
+
 //@annotations for the entire package go here
 
-package jmri.jmrit.ussctc;
+package jmri.jmrit.logix;
