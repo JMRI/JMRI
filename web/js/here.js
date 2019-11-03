a6785f820ae00d8e44766d4c020adedeeb9c19dc
Egbert Broerse <dcc2@ebroerse.nl>
2019-11-03 21:33:26 +0100

    refine script

--- web/js/here.js
+++ web/js/here.js
@@ -1,14 +1,15 @@
 /*
 Highlight the li element with href link to the current page in the left sidebar
-Since 11 2019
+Since Nov 2019
 */
 
 document.documentElement.className="hasJS";
 
 $(function(){
+var $page = window.location.href;
   $('a').each(function() {
-    if ($(this).prop('href') == window.location.href) {
+    if ($(this).prop('href') == $page) {
       $(this.parentNode).addClass('here');
     }
   });
-})
+});
