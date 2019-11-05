e6cd515160fe40954c57c0cdb44c290d1aa40df3
Matthew Harris <738651+mattharris@users.noreply.github.com>
2019-11-05 15:43:19 +0100

    Fix erroneous method deprecation

--- jython/AudioPlayer.py
+++ jython/AudioPlayer.py
@@ -168,9 +168,9 @@ class AudioPlayerFrame (JmriJFrame):
         # Now populate
         self.sourceCombo.addItem(self.SELECT)
         # Retrieve system name list of AudioSources
-        for source in audio.getNamedBeanSet(Audio.SOURCE):
+        for source in audio.getSystemNameList(Audio.SOURCE):
             # Add available sources to the list
-            self.sourceCombo.addItem(source.getSystemName())
+            self.sourceCombo.addItem(source)
 
     def whenRangeChanged(self, event):
         # store value & update sliders
