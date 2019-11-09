b3fe289ef17b062c34a1f9c8ae14faa497d263c0
Bob Jacobsen <jacobsen@mac.com>
2019-11-09 00:33:08 -0800

    hanlde JOAL test on non-JOAL system

--- java/test/jmri/jmrit/audio/JoalAudioSourceTest.java
+++ java/test/jmri/jmrit/audio/JoalAudioSourceTest.java
@@ -18,23 +18,41 @@ public class JoalAudioSourceTest {
 
     @Test
     public void testCtor() {
-        Assume.assumeNotNull(JoalAudioFactory.getAL());
-        JoalAudioSource l = new JoalAudioSource("test");
-        Assert.assertNotNull("exists", l);
-        Assert.assertEquals("test", l.getSystemName());
+        JoalAudioSource l;
+        // This structure is because Jacoco insists on us running some JoalAudioSource
+        // code even if there's no valid JOAL implementation present.
+        try {
+            l = new JoalAudioSource("test");
+            Assert.assertNotNull("exists", l);
+            Assert.assertEquals("test", l.getSystemName());
         
-        Assert.assertEquals(jmri.Audio.STATE_STOPPED, l.getState());
-        Assert.assertEquals(0, l.numProcessedBuffers());
-        Assert.assertEquals(0, l.numQueuedBuffers());
+            Assert.assertEquals(jmri.Audio.STATE_STOPPED, l.getState());
+            Assert.assertEquals(0, l.numProcessedBuffers());
+            Assert.assertEquals(0, l.numQueuedBuffers());
+        } catch (NullPointerException e) {
+            if (JoalAudioFactory.getAL() == null) {
+                // this is expected - no JOAL to check
+                return;
+            } else Assert.fail("AL OK, but NPE in test");
+        }
     }
 
     @Test
     public void testC2Stringtor() {
-        Assume.assumeNotNull(JoalAudioFactory.getAL());
-        JoalAudioSource l = new JoalAudioSource("testsysname","testusername");
-        Assert.assertNotNull("exists", l);
-        Assert.assertEquals("testsysname", l.getSystemName());
-        Assert.assertEquals("testusername", l.getUserName());
+        JoalAudioSource l;
+        // This structure is because Jacoco insists on us running some JoalAudioSource
+        // code even if there's no valid JOAL implementation present.
+        try {
+            l = new JoalAudioSource("testsysname","testusername");
+            Assert.assertNotNull("exists", l);
+            Assert.assertEquals("testsysname", l.getSystemName());
+            Assert.assertEquals("testusername", l.getUserName());
+        } catch (NullPointerException e) {
+            if (JoalAudioFactory.getAL() == null) {
+                // this is expected - no JOAL to check
+                return;
+            } else Assert.fail("AL OK, but NPE in test");
+        }
     }
 
     @Before
