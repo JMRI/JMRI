399928b82d21f3590cc9ddba2cb5de5a15f085c0
Matthew Harris <738651+mattharris@users.noreply.github.com>
2019-11-18 10:19:38 +0100

    Extend AudioManager tests

--- java/test/jmri/jmrit/audio/DefaultAudioManagerTest.java
+++ java/test/jmri/jmrit/audio/DefaultAudioManagerTest.java
@@ -12,12 +12,35 @@ import org.junit.Test;
  * Test simple functioning of DefaultAudioManager
  *
  * @author	Paul Bender Copyright (C) 2017
+ * @author	Matthew Harris Copyright (C) 2019
  */
 public class DefaultAudioManagerTest extends jmri.managers.AbstractManagerTestBase<jmri.AudioManager,jmri.Audio> {
 
     @Test
     public void testCtor() {
         Assert.assertNotNull("exists", l);
+    }
+
+    /**
+     * Test of getActiveAudioFactory method, of class DefaultAudioManager.
+     */
+    @Test
+    public void testGetActiveAudioFactory() {
+        AudioFactory result = l.getActiveAudioFactory();
+        l.init();
+        JUnitUtil.waitFor(()->{return l.isInitialised();});
+        Assert.assertNotNull("Verify that ActiveAudioFactory is not null", result);
+        l.cleanup();
+    }
+
+    /**
+     * Test of getXMLOrder method, of class DefaultAudioManager.
+     */
+    @Test
+    public void testGetXMLOrder() {
+        int expResult = jmri.Manager.AUDIO;
+        int result = l.getXMLOrder();
+        Assert.assertEquals("Verify DefaultAudioManager XMLOrder", expResult, result);
     }
 
     @Before
