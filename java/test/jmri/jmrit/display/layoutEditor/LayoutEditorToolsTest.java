173e732a486cec8db47069c31108f7e73f25b493
George Warner <geowar1@mac.com>
2019-12-22 20:44:12 -0800

    Update retry rules

--- java/test/jmri/jmrit/display/layoutEditor/LayoutEditorToolsTest.java
+++ java/test/jmri/jmrit/display/layoutEditor/LayoutEditorToolsTest.java
@@ -30,7 +30,7 @@ public class LayoutEditorToolsTest {
     public Timeout globalTimeout = Timeout.seconds(10);
 
     @Rule   //allow 2 retries of intermittent tests
-    public RetryRule retryRule = new RetryRule(0);
+    public RetryRule retryRule = new RetryRule(2);
 
     private static LayoutEditor layoutEditor = null;
     private static LayoutEditorTools let = null;
