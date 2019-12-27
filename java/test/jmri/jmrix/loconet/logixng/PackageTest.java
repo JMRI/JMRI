74598b1cd923cce3b860f88736fadc4a3b705aee
Daniel <db123@bergqvist.se>
2019-12-27 07:38:14 +0100

    Test Bundle

--- java/test/jmri/jmrix/loconet/logixng/PackageTest.java
+++ java/test/jmri/jmrix/loconet/logixng/PackageTest.java
@@ -5,9 +5,10 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    jmri.jmrit.logixng.analog.actions.configurexml.PackageTest.class,
+//    jmri.jmrix.loconet.logixng.configurexml.PackageTest.class,
     AnalogActionLocoNet_OPC_PEERTest.class,
     AnalogExpressionLocoNet_OPC_PEERTest.class,
+    BundleTest.class,
     StringActionLocoNet_OPC_PEERTest.class,
     StringExpressionLocoNet_OPC_PEERTest.class,
 })
