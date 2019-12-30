a8b77d9d046cfde333415f5e6d5c150b01b92548
Randall Wood <297232+rhwood@users.noreply.github.com>
2019-12-30 16:53:03 -0500

    test: Add property to silence dialogs in specific tests
    This is a temporary fix to allow tests to continue past a modal dialog. A better (but too complex for this PR) fix would be to have a mechanism to gather *all* messages together to display in a dialog similar to the list of messages shown when loading a profile if anything does not work.

--- java/src/jmri/managers/configurexml/DefaultRouteManagerXml.java
+++ java/src/jmri/managers/configurexml/DefaultRouteManagerXml.java
@@ -494,7 +494,10 @@ public class DefaultRouteManagerXml extends jmri.managers.configurexml.AbstractN
             r.activateRoute();
         }
         if (namesChanged > 0) {
-            if (!GraphicsEnvironment.isHeadless()) {
+            // TODO: replace the System property check with an in-application mechanism
+            // for notifying users of multiple changes that can be silenced as part of
+            // normal operations
+            if (!GraphicsEnvironment.isHeadless() && !Boolean.getBoolean("jmri.test.no-dialogs")) {
                 JOptionPane.showMessageDialog(null,
                         Bundle.getMessage(namesChanged > 1 ? "RouteManager.SystemNamesChanged.Message" : "RouteManager.SystemNameChanged.Message", namesChanged),
                         Bundle.getMessage("Manager.SystemNamesChanged.Title", namesChanged, tm.getBeanTypeHandled(namesChanged > 1)),
