68ef8b3d0c5d152ca5dfccc3ad443b73220a586c
Daniel <db123@bergqvist.se>
2019-12-31 08:52:11 +0100

    Create the class HelloWorld
    Create a new class to demonstrate a commit in GitHub Desktop

---
+++ java/src/apps/HelloWorld.java
@@ -0,0 +1,17 @@
+package apps;
+
+/**
+ * Hello World
+ * @author Daniel Bergqvist
+ */
+public class HelloWorld {
+    
+    /**
+     * Print "Hello World!"
+     */
+    public void hello() {
+        System.out.print("Hello");
+        System.out.println("World");
+    }
+    
+}
