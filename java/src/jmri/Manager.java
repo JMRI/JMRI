be91cb865e9ed65ea2cf007c59c8c6824d1e9136
Bob Jacobsen <jacobsen@mac.com>
2020-01-01 12:13:31 -0800

    clarify javadoc

--- java/src/jmri/Manager.java
+++ java/src/jmri/Manager.java
@@ -166,11 +166,9 @@ public interface Manager<E extends NamedBean> extends PropertyChangeProvider, Ve
     /**
      * Validate the format of a system name, returning it unchanged if valid.
      * <p>
-     * Although further restrictions may be added by system-specific
-     * implementations, at a minimum, the implementation must consider a name
-     * that does not start with the System Name prefix for this manager to be
-     * invalid, and must consider a name that is the same as the System Name
-     * prefix to be invalid.
+     * Provides a convenience form of 
+     * {@link #validateSystemNameFormat(java.lang.String, java.util.Locale)}
+     * that uses the current program Locale.
      * <p>
      * This method should not be overridden;
      * {@link #validateSystemNameFormat(java.lang.String, java.util.Locale)}
@@ -198,10 +196,9 @@ public interface Manager<E extends NamedBean> extends PropertyChangeProvider, Ve
      * <p>
      * Overriding implementations may rely on
      * {@link #validSystemNameFormat(java.lang.String)}, however they must
-     * provide an actionable message in the thrown exception if that method does
-     * not return {@link NameValidity#VALID}. Implementations of
-     * this method <em>must not</em> throw an exception, log an error, or
-     * otherwise disrupt the user.
+     * provide an actionable message in the thrown exception if 
+     * the {@link #validSystemNameFormat} method does
+     * not return {@link NameValidity#VALID}. 
      *
      * @param name      the system name to validate
      * @param locale    the locale for a localized exception; this is needed for
