e1a13cfa3e5b0247f8eb656fd6edc4f2512f9aee
DanielBoudreau <daboudreau@hotmail.com>
2020-01-12 19:55:12 -0500

    New option for switch list to print track comments

--- java/src/jmri/jmrit/operations/locations/tools/TrackCopyFrame.java
+++ java/src/jmri/jmrit/operations/locations/tools/TrackCopyFrame.java
@@ -1,17 +1,15 @@
 package jmri.jmrit.operations.locations.tools;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.text.MessageFormat;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+
+import javax.swing.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import jmri.InstanceManager;
 import jmri.jmrit.operations.OperationsFrame;
 import jmri.jmrit.operations.OperationsXml;
@@ -26,8 +24,6 @@ import jmri.jmrit.operations.rollingstock.cars.CarManager;
 import jmri.jmrit.operations.rollingstock.engines.EngineManager;
 import jmri.jmrit.operations.setup.Control;
 import jmri.jmrit.operations.trains.TrainCommon;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Frame for copying a track for operations.
@@ -265,7 +261,7 @@ public class TrackCopyFrame extends OperationsFrame implements java.beans.Proper
      * @return true if name entered OK and isn't too long
      */
     protected boolean checkName() {
-        if (trackNameTextField.getText().trim().equals("")) {
+        if (trackNameTextField.getText().trim().isEmpty()) {
             JOptionPane.showMessageDialog(this, Bundle.getMessage("MustEnterName"), MessageFormat.format(Bundle
                     .getMessage("CanNotTrack"), new Object[]{Bundle.getMessage("ButtonCopy")}), JOptionPane.ERROR_MESSAGE);
             return false;
