d5f9579ea71aa491f902d5338b39de11591ac1d1
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-01-17 18:15:14 -0500

    fix: suppress warning about class names

--- java/src/apps/gui/GuiLafPreferencesManager.java
+++ java/src/apps/gui/GuiLafPreferencesManager.java
@@ -1,11 +1,14 @@
 package apps.gui;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * 
  * @author Randall Wood Copyright 2019, 2020
  * @deprecated since 4.19.3; use {@link jmri.util.gui.GuiLafPreferencesManager} instead
  */
 @Deprecated
+@SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", justification = "Deprecated in favor of same class in different package")
 public class GuiLafPreferencesManager extends jmri.util.gui.GuiLafPreferencesManager {
 
     public GuiLafPreferencesManager() {
