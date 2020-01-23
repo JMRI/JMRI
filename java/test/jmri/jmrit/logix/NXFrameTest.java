8c43d001be71ff5568f634406e927eed2ec56522
Paul Bender <paul.bender@acm.org>
2020-01-22 23:01:19 -0500

    restore intermittent flags to be safe (some timeouts have occured)

--- java/test/jmri/jmrit/logix/NXFrameTest.java
+++ java/test/jmri/jmrit/logix/NXFrameTest.java
@@ -47,6 +47,7 @@ public class NXFrameTest {
     @Test
     public void testRoutePanel() throws Exception {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        Assume.assumeFalse("Ignoring intermittent test", Boolean.getBoolean("jmri.skipTestsRequiringSeparateRunning"));
 
         NXFrame nxFrame = new NXFrame();
         Assert.assertNotNull("NXFrame", nxFrame);
@@ -72,6 +73,7 @@ public class NXFrameTest {
     @Test
     public void testNXWarrantSetup() throws Exception {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        Assume.assumeFalse("Ignoring intermittent test", Boolean.getBoolean("jmri.skipTestsRequiringSeparateRunning"));
 
         // load and display
         File f = new File("java/test/jmri/jmrit/logix/valid/NXWarrantTest.xml");
@@ -139,6 +141,7 @@ public class NXFrameTest {
         // then goes on to test a Warrant through the WarrantTableFrame.
         // it is the WarrantTableframe portion of this test that hangs.
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        Assume.assumeFalse("Ignoring intermittent test", Boolean.getBoolean("jmri.skipTestsRequiringSeparateRunning"));
 
         // load and display
         File f = new File("java/test/jmri/jmrit/logix/valid/NXWarrantTest.xml");
@@ -257,6 +260,7 @@ public class NXFrameTest {
     @Test
     public void testWarrantLoopRun() throws Exception {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        Assume.assumeFalse("Ignoring intermittent test", Boolean.getBoolean("jmri.skipTestsRequiringSeparateRunning"));
 
         // load and display
         File f = new File("java/test/jmri/jmrit/logix/valid/NXWarrantTest.xml");
@@ -300,6 +304,7 @@ public class NXFrameTest {
     @Test
     public void testWarrantRampHalt() throws Exception {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        Assume.assumeFalse("Ignoring intermittent test", Boolean.getBoolean("jmri.skipTestsRequiringSeparateRunning"));
 
         // load and display
         File f = new File("java/test/jmri/jmrit/logix/valid/NXWarrantTest.xml");
--- java/test/jmri/jmrit/logix/TrackerTest.java
+++ java/test/jmri/jmrit/logix/TrackerTest.java
@@ -52,6 +52,7 @@ public class TrackerTest {
     @Test
     public void testMultipleStartBlocks() throws Exception {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+        Assume.assumeFalse("Ignoring intermittent test", Boolean.getBoolean("jmri.skipTestsRequiringSeparateRunning"));
         WarrantPreferences.getDefault().setShutdown(WarrantPreferences.Shutdown.NO_MERGE);
 
         // load and display
