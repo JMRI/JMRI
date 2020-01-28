5bc733ae9a4abcac4a53cc55ff3feefb4fbdc1ab
Paul Bender <paul.bender@acm.org>
2020-01-27 23:36:32 -0500

    remove duplicate imports

--- java/test/jmri/web/servlet/config/PackageTest.java
+++ java/test/jmri/web/servlet/config/PackageTest.java
@@ -2,8 +2,6 @@ package jmri.web.servlet.config;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 
--- java/test/jmri/web/servlet/directory/PackageTest.java
+++ java/test/jmri/web/servlet/directory/PackageTest.java
@@ -2,8 +2,6 @@ package jmri.web.servlet.directory;
 
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
 
