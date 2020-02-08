dc58ded4654f69184b18587a254a1c3c3428a260
crosland <andrewcrosland@talktalk.net>
2020-02-08 18:52:44 +0000

    Add missing test class
    Preventing AppVeyor passing for PR #8084

--- java/test/jmri/jmrix/openlcb/swing/protocoloptions/PackageTest.java
+++ java/test/jmri/jmrix/openlcb/swing/protocoloptions/PackageTest.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ProtocolOptionsFrameTest.class
+        ProtocolOptionsFrameTest.class,
+        ProtocolOptionsActionTest.class
 })
 
 /**
