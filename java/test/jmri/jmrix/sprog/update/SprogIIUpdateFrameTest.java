2bec53dc1778b1deea81d36a08f1648cb55d651d
Bob Jacobsen <jacobsen@mac.com>
2020-02-09 12:21:35 -0800

    wait for slot thread to terminate

--- java/test/jmri/jmrix/sprog/update/SprogIIUpdateFrameTest.java
+++ java/test/jmri/jmrix/sprog/update/SprogIIUpdateFrameTest.java
@@ -33,6 +33,7 @@ public class SprogIIUpdateFrameTest extends jmri.util.JmriJFrameTestBase {
     @Override
     public void tearDown() {
         m.getSlotThread().interrupt();
+        JUnitUtil.waitFor(() -> {return m.getSlotThread().getState() == Thread.State.TERMINATED;}, "Slot thread failed to stop");
         m.dispose();
         stcs.dispose();
         m = null;
