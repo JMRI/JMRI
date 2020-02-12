8d3d6703efcc446991397ce9760129518a7b4bfa
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-02-12 06:45:04 -0500

    fix: prevent SpotBugs from erroring on deprecated classes

--- java/src/jmri/jmris/json/JsonServerPreferences.java
+++ java/src/jmri/jmris/json/JsonServerPreferences.java
@@ -1,10 +1,13 @@
 package jmri.jmris.json;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 /**
  * @author Randall Wood
- * @deprecated since 4.19.3; use {@link jmri.server.json.JsonServerPreferences}
+ * @deprecated since 4.19.4; use {@link jmri.server.json.JsonServerPreferences}
  *             instead
  */
 @Deprecated
+@SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", justification = "Deprecated for replacement.")
 public class JsonServerPreferences extends jmri.server.json.JsonServerPreferences {
 }
