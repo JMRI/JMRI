8a4fa32049cb4a4dfff5f4387ad658970f0c7144
Paul Bender <paul.bender@acm.org>
2020-02-27 00:36:03 -0500

    remove unused imports

--- java/test/jmri/jmrix/can/cbus/node/CbusNodeTrickleFetchTest.java
+++ java/test/jmri/jmrix/can/cbus/node/CbusNodeTrickleFetchTest.java
@@ -3,8 +3,6 @@ package jmri.jmrix.can.cbus.node;
 import jmri.jmrix.can.CanSystemConnectionMemo;
 import jmri.jmrix.can.TrafficController;
 import jmri.util.JUnitUtil;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
