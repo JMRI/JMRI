a64d7f8f2de2c1363c7e19e47c710e12aff57eb5
Paul Bender <paul.bender@acm.org>
2020-02-27 00:28:26 -0500

    use mocks and junit5

--- java/test/jmri/jmrix/can/cbus/node/CbusNodeTrickleFetchTest.java
+++ java/test/jmri/jmrix/can/cbus/node/CbusNodeTrickleFetchTest.java
@@ -1,12 +1,14 @@
 package jmri.jmrix.can.cbus.node;
 
 import jmri.jmrix.can.CanSystemConnectionMemo;
-import jmri.jmrix.can.TrafficControllerScaffold;
+import jmri.jmrix.can.TrafficController;
 import jmri.util.JUnitUtil;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  *
@@ -18,30 +20,16 @@ public class CbusNodeTrickleFetchTest {
     @Test
     public void testCTor() {
         
-        CanSystemConnectionMemo memo = new CanSystemConnectionMemo();
-        TrafficControllerScaffold tcis = new TrafficControllerScaffold();
-        memo.setTrafficController(tcis);
+        CanSystemConnectionMemo memo = Mockito.mock(CanSystemConnectionMemo.class);
+        TrafficController tc = Mockito.mock(TrafficController.class);
+        CbusNodeTableDataModel model = Mockito.mock(CbusNodeTableDataModel.class);
+        Mockito.when(memo.getTrafficController()).thenReturn(tc);
         
-        CbusNodeTrickleFetch t = new CbusNodeTrickleFetch(memo,null,5L);
-        Assert.assertNotNull("exists",t);
+        CbusNodeTrickleFetch t = new CbusNodeTrickleFetch(memo,model,5L);
+        assertThat(t).isNotNull();
         
         t.dispose();
         
     }
-
-    // The minimal setup for log4J
-    @Before
-    public void setUp() {
-        JUnitUtil.setUp();
-    }
-
-    @After
-    public void tearDown() {
-        JUnitUtil.clearShutDownManager(); // put in place because AbstractMRTrafficController implementing subclass was not terminated properly
-        JUnitUtil.tearDown();
-
-    }
-
-    // private final static Logger log = LoggerFactory.getLogger(CbusNodeTrickleFetchTest.class);
 
 }
