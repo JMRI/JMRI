7ff4d739d5b9379b2c73522bb893406c3346fdd2
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-02-29 17:29:36 -0500

    test: put tidy version in Travis CI logs
    tidy on Travis CI is flagging [valid HTML 5][1] as invalid and causing a test failure, so checking on the version of tidy in use.
    
    [1]: https://www.w3.org/TR/2017/REC-html52-20171214/single-page.html#element-attrdef-global-id

--- scripts/tidy-check.sh
+++ scripts/tidy-check.sh
@@ -8,6 +8,9 @@ else
     WHERE=$@
 fi
 
+# spit out the tidy version
+tidy -version
+
 # first, scan for whether there's an issue (omitting known fragment files)
 find ${WHERE} -name \*html -exec grep -q DOCTYPE {} \; -exec echo Filename: {} \;  ! -exec tidy -eq -access 0 {} \; 2>&1 | grep -v '<table> lacks "summary" attribute' | grep -v '<img> lacks "alt" attribute' | awk -f scripts/tidy.awk | grep Warning 1>&2
 
