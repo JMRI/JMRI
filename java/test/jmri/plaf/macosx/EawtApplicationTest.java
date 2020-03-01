43e445201fb506957294dbf197cb754e9b96d62d
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-03-01 07:57:07 -0500

    test: use single test on macOS

--- java/test/jmri/plaf/macosx/EawtApplicationTest.java
+++ java/test/jmri/plaf/macosx/EawtApplicationTest.java
@@ -2,7 +2,6 @@ package jmri.plaf.macosx;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.awt.Desktop;
 import jmri.util.SystemType;
 import org.junit.Assume;
 import org.junit.Test;
@@ -15,16 +14,8 @@ import org.junit.Test;
 public class EawtApplicationTest {
 
     @Test
-    public void testCtorMacOSXandJDK8() {
+    public void testCtorMacOSX() {
         Assume.assumeTrue(SystemType.isMacOSX());
-        Assume.assumeFalse(Desktop.getDesktop().isSupported(Desktop.Action.valueOf("APP_ABOUT")));
-        assertThat(new EawtApplication()).isNotNull();
-    }
-
-    @Test(expected = NoClassDefFoundError.class)
-    public void testCtorMacOSXandJDK9plus() {
-        Assume.assumeTrue(SystemType.isMacOSX());
-        Assume.assumeTrue(Desktop.getDesktop().isSupported(Desktop.Action.valueOf("APP_ABOUT")));
         assertThat(new EawtApplication()).isNotNull();
     }
 
