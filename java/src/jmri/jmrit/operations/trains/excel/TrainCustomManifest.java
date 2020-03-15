f04b2edaf3128db46f7c4e56d9ffd69220809891
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-03-14 20:33:41 -0400

    chore: Reduce code duplication

--- java/src/jmri/jmrit/operations/trains/excel/TrainCustomManifest.java
+++ java/src/jmri/jmrit/operations/trains/excel/TrainCustomManifest.java
@@ -1,33 +1,10 @@
 package jmri.jmrit.operations.trains.excel;
 
-import jmri.InstanceManager;
 import jmri.InstanceManagerAutoDefault;
-import org.jdom2.Element;
 
 public class TrainCustomManifest extends TrainCustomCommon implements InstanceManagerAutoDefault {
 
-    private String directoryName = "csvManifests"; // NOI18N
-
-    @Override
-    public String getDirectoryName() {
-        return directoryName;
-    }
-
-    @Override
-    public void setDirectoryName(String name) {
-        directoryName = name;
-    }
-
-    @Override
-    public void load(Element options) {
-        Element mc = options.getChild(Xml.MANIFEST_CREATOR);
-        super.load(mc);
-    }
-
-    @Override
-    public void store(Element options) {
-        Element mc = new Element(Xml.MANIFEST_CREATOR);
-        super.store(mc);
-        options.addContent(mc);
+    public TrainCustomManifest() {
+        super("csvManifests", Xml.MANIFEST_CREATOR); // NOI18N
     }
 }
--- java/src/jmri/jmrit/operations/trains/excel/TrainCustomSwitchList.java
+++ java/src/jmri/jmrit/operations/trains/excel/TrainCustomSwitchList.java
@@ -1,33 +1,10 @@
 package jmri.jmrit.operations.trains.excel;
 
-import jmri.InstanceManager;
 import jmri.InstanceManagerAutoDefault;
-import org.jdom2.Element;
 
 public class TrainCustomSwitchList extends TrainCustomCommon implements InstanceManagerAutoDefault {
 
-    private String directoryName = "csvSwitchLists"; // NOI18N
-
-    @Override
-    public String getDirectoryName() {
-        return directoryName;
-    }
-
-    @Override
-    public void setDirectoryName(String name) {
-        directoryName = name;
-    }
-
-    @Override
-    public void load(Element options) {
-        Element mc = options.getChild(Xml.SWITCHLIST_CREATOR);
-        super.load(mc);
-    }
-
-    @Override
-    public void store(Element options) {
-        Element mc = new Element(Xml.SWITCHLIST_CREATOR);
-        super.store(mc);
-        options.addContent(mc);
+    public TrainCustomSwitchList() {
+        super("csvSwitchLists", Xml.SWITCHLIST_CREATOR); // NOI18N
     }
 }
