6eaee3232e1eadaf6c5391560f25d4527bcc0b72
Svata Dedic <svatopluk.dedic@gmail.com>
2020-04-22 21:26:24 +0200

    Added tests to check OFF messaes are re-sent after retransmittable error.

--- java/test/jmri/jmrix/lenz/XNetTrafficControllerIT.java
+++ java/test/jmri/jmrix/lenz/XNetTrafficControllerIT.java
@@ -2,27 +2,32 @@ package jmri.jmrix.lenz;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.IdentityHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 import jmri.InstanceManager;
 import jmri.Turnout;
 import jmri.jmrix.AbstractMRListener;
 import jmri.jmrix.AbstractMRMessage;
-import jmri.jmrix.AbstractMRTrafficController;
 import jmri.jmrix.lenz.liusb.LIUSBXNetPacketizer;
+import jmri.util.JUnitAppender;
 import jmri.util.JUnitUtil;
 import jmri.util.ThreadingUtil;
+import org.apache.log4j.Level;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -65,13 +70,30 @@ public class XNetTrafficControllerIT {
         public void sendMessage(AbstractMRMessage m, AbstractMRListener reply);
     }
     
+    volatile XNetMessage originalMessage;
+    Map<XNetMessage, AtomicInteger> retryCounters = new IdentityHashMap<>();
+    
     class TestUSBPacketizer extends LIUSBXNetPacketizer implements MessageOutput {
-
         public TestUSBPacketizer(LenzCommandStation pCommandStation) {
             super(pCommandStation);
             output = this;
         }
         
+        /**
+         * Counts the number of retries after errors, does NOT count retries
+         * after timeout.
+s         */
+        @Override
+        protected synchronized void forwardToPort(AbstractMRMessage m, AbstractMRListener reply) {
+            super.forwardToPort(m, reply);
+            XNetMessage msg = (XNetMessage)m;
+            // for each *instance* of message, track the number of attempts to send.
+            // XNetMessage with the same payload are equals(), but we want to track instances,
+            // to see what msg was actually re-send, and which was just replicated
+            retryCounters.computeIfAbsent(msg, (k) -> new AtomicInteger()).incrementAndGet();
+            originalMessage = msg;
+        }
+
         @Override
         protected AbstractMRMessage takeMessageToTransmit(AbstractMRListener[] ll) {
             AbstractMRMessage mrm = super.takeMessageToTransmit(ll);
@@ -161,6 +183,11 @@ public class XNetTrafficControllerIT {
         // all startup messages are already procesed.
         testAdapter.drainPackets(true);
     }
+    
+    /**
+     * If true, will clear errors at the end.
+     */
+    private boolean clearErrors;
 
     @After
     public void tearDown() throws Exception {
@@ -169,6 +196,11 @@ public class XNetTrafficControllerIT {
         ctrl.disconnectPort(testAdapter);
         testAdapter.dispose();
         JUnitUtil.clearShutDownManager(); // put in place because AbstractMRTrafficController implementing subclass was not terminated properly
+        if (clearErrors) {
+            JUnitAppender.end();
+            JUnitAppender.resetUnexpectedMessageFlags(Level.ERROR);
+        }
+        
         JUnitUtil.tearDown();
     }
 
@@ -430,4 +462,172 @@ public class XNetTrafficControllerIT {
         // or the OK-to-second OFF.
     }
 
+    interface AccRequestCallback {
+        public XNetReply getReply(int address, int output, boolean state);
+    }
+    
+    class BusyLenzSimulator extends XNetTestSimulator.LZV100_USB {
+        volatile AccRequestCallback cb;
+        Map<XNetMessage, XNetMessage> backToJMRIMessage = new IdentityHashMap<>();
+
+        @Override
+        protected XNetMessage readMessage() {
+            XNetMessage received = super.readMessage();
+            // the simulator deserializes messages from the stream; they are different objects
+            // than the original, so let's map them back with the help of hack in TrafficCOntroller:
+            backToJMRIMessage.put(received, originalMessage);
+            return received;
+        }
+        
+        @Override
+        protected XNetReply generateAccRequestReply(int address, int output, boolean state) {
+            if (cb != null) {
+                XNetReply r = cb.getReply(address, output, state);
+                if (r != null) {
+                    return r;
+                }
+            }
+            return super.generateAccRequestReply(address, output, state);
+        }
+    }
+    
+    @Test
+    public void testSendBusyToOutputOff() throws Exception {
+        BusyLenzSimulator simul = new BusyLenzSimulator();
+        initializeLayout(simul, new TestUSBPacketizer(new LenzCommandStation()));
+        
+        XNetTurnout t = (XNetTurnout)xnetManager.provideTurnout("XT21");
+        // excess OFF messages are sent
+        Thread.sleep(1000);
+        
+        // send just 3 BUSY to break "error handling"
+        AtomicInteger counter = new AtomicInteger(0);
+        
+        simul.cb = (a, o, s) -> {
+            // ignore ON messages, normal processing.
+            if (s) {
+                return null;
+            }
+            // send at most 3 BUSY messages.
+            if (counter.incrementAndGet() > 3) {
+                assertNotNull(lnis);
+                assertNotNull(simul);
+                return null;
+            }
+            XNetReply reply = new XNetReply("61 81 E0");
+            return reply;
+        };
+        simul.setCaptureMessages(true);
+        t.setCommandedState(XNetTurnout.THROWN);
+        
+        // wait for the timeout time to get potentially all repetitions.
+        Thread.sleep(6000);
+        
+        List<XNetMessage> outgoing = simul.getOutgoingMessages();
+        List<XNetMessage> justOFFs = outgoing.stream().filter((m) -> 
+                m.getElement(0) == 0x52 &&
+                (m.getElement(2) & 0x08) == 0).collect(Collectors.toList());
+        
+        // show individual messages were NOT retransmitted at all, the "retransmition"
+        // is just a FAKE because of OFFs replicated in advance. Counters for individual
+        // object instances are increased in 
+        int index = 1;
+        XNetMessage last = null;
+        for (XNetMessage m : justOFFs) {
+            // translate to the JMRI controller instance:
+            XNetMessage orig = simul.backToJMRIMessage.get(m);
+            if (orig == last) {
+                System.err.println("Message #" + index + ": retry previous");
+            } else {
+                AtomicInteger retries = retryCounters.get(orig);
+                System.err.println("Message #" + index + ": " + m + ", retries avail: " + retries.get());
+            }
+            last = orig;
+            index++;
+        }
+        
+        // 5 retransmissions are permitted by default, so after 3 rejected OFFs, some should be yet sent:
+        assertTrue("3 OFFs were rejected by station, they must be repeated !", justOFFs.size() > 3);
+        
+        // there were 3 Busy to OFF message. So 1st message should be sent 3 times unsuccessfully, and once successfully.
+        // the other (and last) OFF should be sent without any repetition
+        XNetMessage orig = simul.backToJMRIMessage.get(justOFFs.get(0));
+        assertEquals(4, retryCounters.get(orig).get());
+        orig = simul.backToJMRIMessage.get(justOFFs.get(justOFFs.size() - 1));
+        assertEquals(1, retryCounters.get(orig).get());
+
+        // permit cleaning errors logged by TrafficController, all other checks OK
+        clearErrors = true;
+    }
+
+    @Test
+    public void testSendBusyErrorIgnoredNewCommandSent() throws Exception {
+        BusyLenzSimulator simul = new BusyLenzSimulator();
+        initializeLayout(simul, new TestUSBPacketizer(new LenzCommandStation()));
+        
+        XNetTurnout t = (XNetTurnout)xnetManager.provideTurnout("XT21");
+        // excess OFF messages are sent
+        Thread.sleep(1000);
+        
+        // send just 1 BUSY in response to OFF message.
+        // station status query message that will happen BEFORE the erroneously
+        // multiplied OFFs:
+        AtomicInteger counter = new AtomicInteger(0);
+        
+        CountDownLatch msgLatch = new CountDownLatch(1);
+        
+        List<XNetMessage> messagesUntilBusy = new ArrayList<>();
+        
+        simul.cb = (a, o, s) -> {
+            // ignore ON messages, normal processing.
+            if (s) {
+                return null;
+            }
+            if (counter.incrementAndGet()> 1) {
+                return null;
+            }
+            XNetReply reply = new XNetReply("61 81 E0");
+            messagesUntilBusy.addAll(simul.getOutgoingMessages());
+            simul.clearMesages();
+            
+            // at this point, queue "incidentally" another command for transmission
+            // it has to be before pre-poll interval elapses.
+            
+            // It would be better to do right here, but Paul would probably not believe it is real,
+            // so rely on the main thread will wake up & execute the sendXNetMessage in less than 100ms.
+            msgLatch.countDown();
+            return reply;
+        };
+        simul.setCaptureMessages(true);
+        t.setCommandedState(XNetTurnout.THROWN);
+
+        List<XNetMessage>   outgoingAfterError = new ArrayList<>();
+        
+        // wait until the error is generated.
+        assertTrue(msgLatch.await(5, TimeUnit.SECONDS));
+        
+        CountDownLatch versionLatch = new CountDownLatch(1);
+        
+        // then just send a normal message at the same time OFF is rejected. 
+        // The rejected OFF should be repeated, before THIS message is processed:
+        lnis.sendXNetMessage(
+            XNetMessage.getCSVersionRequestMessage(),
+            new XNetListenerScaffold(){
+                @Override
+                public void message(XNetReply msg) {
+                    outgoingAfterError.addAll(simul.getOutgoingMessages());
+                    versionLatch.countDown();
+                }
+            }
+        );
+        // wait until the Version request is *replied*
+        assertTrue(versionLatch.await(5, TimeUnit.SECONDS));
+        
+        // the repeated OFF should have been in the captured outgoing messages before
+        // the CS version request:
+        assertEquals(0x52, outgoingAfterError.get(0).getElement(0));
+        
+        // permit cleaning errors logged by TrafficController, all other checks OK
+        clearErrors = true;
+    }
 }
