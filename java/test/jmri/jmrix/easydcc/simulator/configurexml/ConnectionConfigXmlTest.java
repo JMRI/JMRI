2b202cb20b77fb2fc2161e553e5a529d8d99c57b
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-04-25 13:27:38 -0400

    test: why does everything rely on the BlockManager?

--- java/test/jmri/jmrix/easydcc/simulator/configurexml/ConnectionConfigXmlTest.java
+++ java/test/jmri/jmrix/easydcc/simulator/configurexml/ConnectionConfigXmlTest.java
@@ -23,8 +23,9 @@ public class ConnectionConfigXmlTest extends jmri.jmrix.configurexml.AbstractSim
     @After
     @Override
     public void tearDown() {
-        JUnitUtil.tearDown();
         xmlAdapter = null;
         cc = null;
+        JUnitUtil.deregisterBlockManagerShutdownTask();
+        JUnitUtil.tearDown();
     }
 }
