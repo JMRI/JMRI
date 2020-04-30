f50823f6bc8c7da9520a1e008336d40804c4ae86
Bob Jacobsen <jacobsen@mac.com>
2020-04-29 23:31:12 -0700

    common arrow load; hook to edit and view

--- java/src/jmri/jmrit/display/layoutEditor/PositionablePoint.java
+++ java/src/jmri/jmrit/display/layoutEditor/PositionablePoint.java
@@ -972,80 +972,22 @@ public class PositionablePoint extends LayoutTrack {
 
                 jcbmi.setSelected((connect1.getArrowStyle() == 0) || !etherEnd);
 
-                ImageIcon imageIcon = new ImageIcon(FileUtil.findURL("program:resources/icons/decorations/ArrowStyle1.png"));
-                jcbmi = new JCheckBoxMenuItem(imageIcon);
-                arrowsCountMenu.add(jcbmi);
-                jcbmi.setToolTipText(Bundle.getMessage("DecorationStyleMenuToolTip"));
-                jcbmi.addActionListener((java.awt.event.ActionEvent e3) -> {
-                    if (connect1.getConnect1() == this) {
-                        connect1.setArrowEndStart(true);
-                    }
-                    if (connect1.getConnect2() == this) {
-                        connect1.setArrowEndStop(true);
-                    }
-                    connect1.setArrowStyle(1);
-                });
-                jcbmi.setSelected((connect1.getArrowStyle() == 1) && etherEnd);
+                // configure the arrows
+                for (int i = 1; i<=5; i++) {
+                    jcbmi = loadArrowImageToJCBItem(i, arrowsCountMenu);
+                    final int n = i;
+                    jcbmi.addActionListener((java.awt.event.ActionEvent e3) -> {
+                        if (connect1.getConnect1() == this) {
+                            connect1.setArrowEndStart(true);
+                        }
+                        if (connect1.getConnect2() == this) {
+                            connect1.setArrowEndStop(true);
+                        }
+                        connect1.setArrowStyle(n);
+                    });
+                    jcbmi.setSelected((connect1.getArrowStyle() == i) && etherEnd);             
+                }
 
-                imageIcon = new ImageIcon(FileUtil.findURL("program:resources/icons/decorations/ArrowStyle2.png"));
-                jcbmi = new JCheckBoxMenuItem(imageIcon);
-                arrowsCountMenu.add(jcbmi);
-                jcbmi.setToolTipText(Bundle.getMessage("DecorationStyleMenuToolTip"));
-                jcbmi.addActionListener((java.awt.event.ActionEvent e3) -> {
-                    if (connect1.getConnect1() == this) {
-                        connect1.setArrowEndStart(true);
-                    }
-                    if (connect1.getConnect2() == this) {
-                        connect1.setArrowEndStop(true);
-                    }
-                    connect1.setArrowStyle(2);
-                });
-                jcbmi.setSelected((connect1.getArrowStyle() == 2) && etherEnd);
-
-                imageIcon = new ImageIcon(FileUtil.findURL("program:resources/icons/decorations/ArrowStyle3.png"));
-                jcbmi = new JCheckBoxMenuItem(imageIcon);
-                arrowsCountMenu.add(jcbmi);
-                jcbmi.setToolTipText(Bundle.getMessage("DecorationStyleMenuToolTip"));
-                jcbmi.addActionListener((java.awt.event.ActionEvent e3) -> {
-                    if (connect1.getConnect1() == this) {
-                        connect1.setArrowEndStart(true);
-                    }
-                    if (connect1.getConnect2() == this) {
-                        connect1.setArrowEndStop(true);
-                    }
-                    connect1.setArrowStyle(3);
-                });
-                jcbmi.setSelected((connect1.getArrowStyle() == 3) && etherEnd);
-
-                imageIcon = new ImageIcon(FileUtil.findURL("program:resources/icons/decorations/ArrowStyle4.png"));
-                jcbmi = new JCheckBoxMenuItem(imageIcon);
-                arrowsCountMenu.add(jcbmi);
-                jcbmi.setToolTipText(Bundle.getMessage("DecorationStyleMenuToolTip"));
-                jcbmi.addActionListener((java.awt.event.ActionEvent e3) -> {
-                    if (connect1.getConnect1() == this) {
-                        connect1.setArrowEndStart(true);
-                    }
-                    if (connect1.getConnect2() == this) {
-                        connect1.setArrowEndStop(true);
-                    }
-                    connect1.setArrowStyle(4);
-                });
-                jcbmi.setSelected((connect1.getArrowStyle() == 4) && etherEnd);
-
-                imageIcon = new ImageIcon(FileUtil.findURL("program:resources/icons/decorations/ArrowStyle5.png"));
-                jcbmi = new JCheckBoxMenuItem(imageIcon);
-                arrowsCountMenu.add(jcbmi);
-                jcbmi.setToolTipText(Bundle.getMessage("DecorationStyleMenuToolTip"));
-                jcbmi.addActionListener((java.awt.event.ActionEvent e3) -> {
-                    if (connect1.getConnect1() == this) {
-                        connect1.setArrowEndStart(true);
-                    }
-                    if (connect1.getConnect2() == this) {
-                        connect1.setArrowEndStop(true);
-                    }
-                    connect1.setArrowStyle(5);
-                });
-                jcbmi.setSelected((connect1.getArrowStyle() == 5) && etherEnd);
 
                 JMenu arrowsDirMenu = new JMenu(Bundle.getMessage("ArrowsDirectionMenuTitle"));
                 arrowsDirMenu.setToolTipText(Bundle.getMessage("ArrowsDirectionMenuToolTip"));
