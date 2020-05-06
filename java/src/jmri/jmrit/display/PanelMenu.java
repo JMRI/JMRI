e5c7cafaf94f0e4889ba0c7f8609b2ed6ba3af02
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-05-06 08:33:01 -0400

    fix: avoid mismatches between EditorManage and PanelMenu

--- java/src/jmri/jmrit/display/PanelMenu.java
+++ java/src/jmri/jmrit/display/PanelMenu.java
@@ -1,20 +1,18 @@
 package jmri.jmrit.display;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.IndexedPropertyChangeEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.Set;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
 import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JSeparator;
 import jmri.InstanceManager;
 import jmri.jmrit.display.layoutEditor.LayoutEditor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Create the "Panels" menu for use in a menubar.
@@ -62,28 +60,38 @@ public class PanelMenu extends JMenu {
         super.add(new jmri.jmrit.jython.JythonWindow(Bundle.getMessage("MenuItemScriptLog")));
         super.add(new jmri.jmrit.jython.InputWindowAction(Bundle.getMessage("MenuItemScriptInput")));
         InstanceManager.getDefault(EditorManager.class).addPropertyChangeListener(listener);
+        updateMenu(null);
     }
 
-    private void updateMenu(PropertyChangeEvent evt) {
-        // an EditorManager notifies if an editor is added, removed, or renamed
-        // when editors are added or removed from the EditorManager, the event
-        // source is the EditorManager and the event is indexed
-        // when an editor is renamed, the event source is the Editor, and event
-        // is not indexed
-        if (evt.getSource().equals(InstanceManager.getDefault(EditorManager.class))
-                && evt instanceof IndexedPropertyChangeEvent) {
-            if (evt.getNewValue() != null) {
-                addEditorPanel((Editor) evt.getNewValue());
-            } else if (evt.getOldValue() != null) {
-                deletePanel((Editor) evt.getOldValue());
-            }
-        } else if (evt.getSource() instanceof Editor) {
-            Editor e = (Editor) evt.getSource();
-            if ("title".equals(evt.getPropertyName())) {
-                renameEditorPanel(e);
-            } else {
-                updateEditorPanel(e);
-            }
+    private void updateMenu(@CheckForNull PropertyChangeEvent evt) {
+        Set<Editor> editors = InstanceManager.getDefault(EditorManager.class).getAll();
+        panelsSubMenu.removeAll();
+        if (editors.isEmpty()) {
+            panelsSubMenu.add(noPanelsItem);
+        } else {
+            editors.forEach(editor -> {
+                JCheckBoxMenuItem menuItem = new JCheckBoxMenuItem(editor.getTitle());
+                ActionListener action = event -> {
+                    if (editor instanceof LayoutEditor) {
+                        editor.setVisible(true);
+                        editor.repaint();
+                    } else {
+                        editor.getTargetFrame().setVisible(true);
+                    }
+                    updateMenuItem(editor, menuItem);
+                };
+                menuItem.addActionListener(action);
+                updateMenuItem(editor, menuItem);
+                panelsSubMenu.add(menuItem);
+            });
+        }
+    }
+
+    private void updateMenuItem(@Nonnull Editor editor, @Nonnull JCheckBoxMenuItem menuItem) {
+        if (editor instanceof LayoutEditor) {
+            menuItem.setSelected(editor.isVisible());
+        } else {
+            menuItem.setSelected(editor.getTargetFrame().isVisible());
         }
     }
 
@@ -103,116 +111,44 @@ public class PanelMenu extends JMenu {
      * Delete a panel from Show Panel sub menu.
      *
      * @param panel the panel to remove from the menu
+     * @deprecated since 4.19.6 without public replacement
      */
+    @Deprecated
     public void deletePanel(Editor panel) {
-        log.debug("deletePanel");
-        ArrayList<Editor> panelsList = new ArrayList<>(InstanceManager.getDefault(EditorManager.class).getAll());
-        if (panelsList.isEmpty()) {
-            return;
-        }
-        for (int i = 0; i < panelsList.size(); i++) {
-            Object o = panelsList.get(i);
-            if (o == panel) {
-                // Editors that are their own TargetFrame dispose themselves
-                if (!panel.equals(panel.getTargetFrame())) {
-                    panel.getTargetFrame().dispose();
-                }
-                panelsList.remove(panel);
-                panelsSubMenu.remove(i);
-                // If there are no panels on the list,
-                // replace the 'No Panels' menu item
-                if (panelsList.isEmpty()) {
-                    panelsSubMenu.add(noPanelsItem);
-                }
-                return;
-            }
-        }
+        updateMenu(null);
     }
 
     /**
      * Add an Editor panel to Show Panels sub menu.
      *
      * @param panel the panel to add to the menu
+     * @deprecated since 4.19.6 without public replacement
      */
+    @Deprecated
     public void addEditorPanel(final Editor panel) {
-        // If this is the first panel, remove the 'No Panels' menu item
-        ArrayList<Editor> panelsList = new ArrayList<>(InstanceManager.getDefault(EditorManager.class).getAll());
-        if (panelsList.isEmpty()) {
-            panelsSubMenu.remove(noPanelsItem);
-        }
-        panelsList.add(panel);
-        ActionListener a = (ActionEvent e) -> {
-            if (panel instanceof LayoutEditor) {
-                panel.setVisible(true);
-                panel.repaint();
-            } else {
-                panel.getTargetFrame().setVisible(true);
-            }
-            updateEditorPanel(panel);
-        };
-        JCheckBoxMenuItem r = new JCheckBoxMenuItem(panel.getTitle());
-        r.addActionListener(a);
-        panelsSubMenu.add(r);
-        updateEditorPanel(panel);
+        updateMenu(null);
     }
 
     /**
      * Update an Editor type panel in Show Panels sub menu.
      *
      * @param panel the panel to update
+     * @deprecated since 4.19.6 without public replacement
      */
+    @Deprecated
     public void updateEditorPanel(Editor panel) {
-        ArrayList<Editor> panelsList = new ArrayList<>(InstanceManager.getDefault(EditorManager.class).getAll());
-        if (panelsList.isEmpty()) {
-            return;
-        }
-        for (int i = 0; i < panelsList.size(); i++) {
-            Object o = panelsList.get(i);
-            if (o == panel) {
-                JMenuItem subMenu = panelsSubMenu.getItem(i);
-                if (subMenu instanceof JCheckBoxMenuItem) {
-                    JCheckBoxMenuItem r = (JCheckBoxMenuItem) subMenu;
-                    if (panel instanceof LayoutEditor) {
-                        if (panel.isVisible()) {
-                            r.setSelected(true);
-                        } else {
-                            r.setSelected(false);
-                        }
-                    } else {
-                        JFrame target = panel.getTargetFrame();
-                        if (target != null && target.isVisible()) {
-                            r.setSelected(true);
-                        } else {
-                            r.setSelected(false);
-                        }
-                    }
-                }
-                return;
-            }
-        }
+        updateMenu(null);
     }
 
     /**
      * Rename an Editor type panel in Show Panels submenu.
      *
      * @param panel the panel to rename
+     * @deprecated since 4.19.6 without public replacement
      */
+    @Deprecated
     public void renameEditorPanel(Editor panel) {
-        ArrayList<Editor> panelsList = new ArrayList<>(InstanceManager.getDefault(EditorManager.class).getAll());
-        if (panelsList.isEmpty()) {
-            return;
-        }
-        for (int i = 0; i < panelsList.size(); i++) {
-            Object o = panelsList.get(i);
-            if (o == panel) {
-                JMenuItem subMenu = panelsSubMenu.getItem(i);
-                if (subMenu instanceof JCheckBoxMenuItem) {
-                    JCheckBoxMenuItem r = (JCheckBoxMenuItem) subMenu;
-                    r.setText(panel.getTitle());
-                }
-                return;
-            }
-        }
+        updateMenu(null);
     }
 
     /**
@@ -231,7 +167,8 @@ public class PanelMenu extends JMenu {
     /**
      * @param name the name of the editor
      * @return the editor or null if there is no matching editor
-     * @deprecated since 4.19.6; use {@link EditorManager#get(java.lang.String)} instead
+     * @deprecated since 4.19.6; use {@link EditorManager#get(java.lang.String)}
+     * instead
      */
     @Deprecated
     public Editor getEditorByName(String name) {
@@ -239,7 +176,7 @@ public class PanelMenu extends JMenu {
     }
 
     /**
-     * 
+     *
      * @return the list of Editors
      * @deprecated since 4.19.6; use {@link EditorManager#getAll()} instead
      */
@@ -249,15 +186,13 @@ public class PanelMenu extends JMenu {
     }
 
     /**
-     * 
+     *
      * @return the list of LayoutEditors
-     * @deprecated since 4.19.6; use {@link EditorManager#getAll(java.lang.Class)} instead
+     * @deprecated since 4.19.6; use
+     * {@link EditorManager#getAll(java.lang.Class)} instead
      */
     @Deprecated
     public ArrayList<LayoutEditor> getLayoutEditorPanelList() {
         return new ArrayList<>(InstanceManager.getDefault(EditorManager.class).getAll(LayoutEditor.class));
     }
-
-    private final static Logger log = LoggerFactory.getLogger(PanelMenu.class);
-
 }
