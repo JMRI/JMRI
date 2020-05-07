7f33d4f0dbe4bc75913d73385fa551755d9385c3
Andrew Crosland <andrew@crosland.me.uk>
2020-05-07 21:00:44 +0100

    Use correct classes and rename tests

--- java/test/jmri/jmrix/can/cbus/swing/modeswitcher/CbusModeSwitcherActionTest.java
+++ java/test/jmri/jmrix/can/cbus/swing/modeswitcher/CbusModeSwitcherActionTest.java
@@ -8,13 +8,13 @@ import org.junit.*;
  *
  * @author Andrew Crosland (C) 2020
  */
-public class ModeSwitcherActionTest {
+public class CbusModeSwitcherActionTest {
 
     @Test
     public void testAction() {
         Assume.assumeFalse(GraphicsEnvironment.isHeadless());
 
-        ModeSwitcherAction action = new ModeSwitcherAction("ModeSwitcherAction test");
+        CbusModeSwitcherAction action = new CbusModeSwitcherAction("CbusModeSwitcherAction test");
         Assert.assertNotNull("exists", action);
     }
 
