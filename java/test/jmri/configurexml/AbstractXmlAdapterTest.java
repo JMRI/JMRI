df5124ca1263a51070b9d655b12bdff6f1b7f678
Bob Jacobsen <jacobsen@mac.com>
2020-05-10 10:23:27 -0700

    add double

--- java/test/jmri/configurexml/AbstractXmlAdapterTest.java
+++ java/test/jmri/configurexml/AbstractXmlAdapterTest.java
@@ -59,6 +59,22 @@ public class AbstractXmlAdapterTest{
         Assert.assertEquals(21, adapter.getAttributeIntegerValue(testEl, "t21", 12));
     }
         
+    @Test
+    public void testGetAttributeDoubleValue() {
+        AbstractXmlAdapter adapter  = new AbstractXmlAdapter(){
+            public Element store(Object o) {return null;}
+            public void load(Element e, Object o) {}
+        };
+        
+        Element testEl = new Element("foo");
+        
+        Assert.assertEquals(12., adapter.getAttributeDoubleValue(testEl, "att", 12.), 0.001);
+                
+        testEl.setAttribute("t21", "21.");
+        
+        Assert.assertEquals(21., adapter.getAttributeDoubleValue(testEl, "t21", 12.), 0.001);
+    }
+        
     enum testEnum {Foo, Bar, Biff}
     
     @Test
