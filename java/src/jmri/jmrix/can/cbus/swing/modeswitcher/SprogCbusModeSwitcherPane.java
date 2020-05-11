8a9ee37da170686b0f318772763bbbb2d00ad3cb
Andrew Crosland <andrew@crosland.me.uk>
2020-05-11 13:00:45 +0100

    Catch possible null pointer

--- java/src/jmri/jmrix/can/cbus/swing/modeswitcher/SprogCbusModeSwitcherPane.java
+++ java/src/jmri/jmrix/can/cbus/swing/modeswitcher/SprogCbusModeSwitcherPane.java
@@ -53,79 +53,85 @@ public class SprogCbusModeSwitcherPane extends JmriJFrame {
     @Override
     public void initComponents() {
         preferences = jmri.InstanceManager.getDefault(jmri.jmrix.can.cbus.CbusPreferences.class);
-        
         pm = (CbusDccProgrammerManager)InstanceManager.getNullableDefault(GlobalProgrammerManager.class);
         
         JLabel label = new JLabel();
-        // Wrap  in html to get wrapping when added to border layout
-        label.setText("<html>"+Bundle.getMessage("HardwareModeLabel")+"</html>");
+        JPanel panel = new JPanel(new BorderLayout());
         
-        // Mode selector
-        JPanel modePane = new JPanel();
-        modePane.setLayout(new BoxLayout(modePane, BoxLayout.Y_AXIS));
-        modePane.setBorder(BorderFactory.createTitledBorder(
-            BorderFactory.createEtchedBorder(), Bundle.getMessage("HardwareMode")));
+        if (pm == null) {
+            // Wrap  in html to get wrapping when added to border layout
+            label.setText("<html>"+Bundle.getMessage("NoCbusProgrammer")+"</html>");
+            panel.add(label, BorderLayout.NORTH);
+        } else {
+            // Wrap  in html to get wrapping when added to border layout
+            label.setText("<html>"+Bundle.getMessage("HardwareModeLabel")+"</html>");
 
-        // Create selection buttons and set initial state
-        progModeButton = new JRadioButton(Bundle.getMessage("ProgMode"));
-        cmdModeButton = new JRadioButton(Bundle.getMessage("CmdMode"));
-        if (pm.isGlobalProgrammerAvailable()) {
-            progModeButton.setSelected(true);
-        } else {
-            progModeButton.setSelected(false);
-        }
-        if (pm.isAddressedModePossible()) {
-            cmdModeButton.setSelected(true);
-        } else {
-            cmdModeButton.setSelected(false);
-        }
-        
-        // Handle Programmer mode button activity
-        ActionListener setProgMode = ae -> {
-            if (progModeButton.isSelected()) {
-                // Enable service mode programmer
-                log.info("Setting Global Programmer Available");
-                pm.setGlobalProgrammerAvailable(true);
-            } else if (cmdModeButton.isSelected()) {
-                // Only disable service mode if ops mode active
-                log.info("Setting Global Programmer Unavailable");
-                pm.setGlobalProgrammerAvailable(false);
-            } else {
-                // Service mode is the default if all are deselected - reselect it
-                log.info("Cannot de-select programmer mode as only mode");
+            // Mode selector
+            JPanel modePane = new JPanel();
+            modePane.setLayout(new BoxLayout(modePane, BoxLayout.Y_AXIS));
+            modePane.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), Bundle.getMessage("HardwareMode")));
+
+            // Create selection buttons and set initial state
+            progModeButton = new JRadioButton(Bundle.getMessage("ProgMode"));
+            cmdModeButton = new JRadioButton(Bundle.getMessage("CmdMode"));
+            if (pm.isGlobalProgrammerAvailable()) {
                 progModeButton.setSelected(true);
-            }
-            preferences.setProgrammersAvailable(progModeButton.isSelected(), cmdModeButton.isSelected());
-        };
-        
-        // Handle command station mode button activity
-        ActionListener setCmdMode = ae -> {
-            if (cmdModeButton.isSelected()) {
-                // Enable ops mode programmer
-                log.info("Setting Addressed Programmer Available");
-                pm.setAddressedModePossible(true);
             } else {
-                // Disable ops mode programmer
-                log.info("Setting Addressed Programmer Unavailable");
-                pm.setAddressedModePossible(false);
-                if (!progModeButton.isSelected()) {
-                    // Re-enable service mode if all are deselected
-                    log.info("No current programmer, setting Global Programmer Available");
+                progModeButton.setSelected(false);
+            }
+            if (pm.isAddressedModePossible()) {
+                cmdModeButton.setSelected(true);
+            } else {
+                cmdModeButton.setSelected(false);
+            }
+
+            // Handle Programmer mode button activity
+            ActionListener setProgMode = ae -> {
+                if (progModeButton.isSelected()) {
+                    // Enable service mode programmer
+                    log.info("Setting Global Programmer Available");
                     pm.setGlobalProgrammerAvailable(true);
+                } else if (cmdModeButton.isSelected()) {
+                    // Only disable service mode if ops mode active
+                    log.info("Setting Global Programmer Unavailable");
+                    pm.setGlobalProgrammerAvailable(false);
+                } else {
+                    // Service mode is the default if all are deselected - reselect it
+                    log.info("Cannot de-select programmer mode as only mode");
                     progModeButton.setSelected(true);
                 }
-            }
-            preferences.setProgrammersAvailable(progModeButton.isSelected(), cmdModeButton.isSelected());
-        };
-        
-        progModeButton.addActionListener(setProgMode);
-        cmdModeButton.addActionListener(setCmdMode);
-        modePane.add(progModeButton);
-        modePane.add(cmdModeButton);
-        
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(label, BorderLayout.NORTH);
-        panel.add(modePane, BorderLayout.CENTER);
+                preferences.setProgrammersAvailable(progModeButton.isSelected(), cmdModeButton.isSelected());
+            };
+
+            // Handle command station mode button activity
+            ActionListener setCmdMode = ae -> {
+                if (cmdModeButton.isSelected()) {
+                    // Enable ops mode programmer
+                    log.info("Setting Addressed Programmer Available");
+                    pm.setAddressedModePossible(true);
+                } else {
+                    // Disable ops mode programmer
+                    log.info("Setting Addressed Programmer Unavailable");
+                    pm.setAddressedModePossible(false);
+                    if (!progModeButton.isSelected()) {
+                        // Re-enable service mode if all are deselected
+                        log.info("No current programmer, setting Global Programmer Available");
+                        pm.setGlobalProgrammerAvailable(true);
+                        progModeButton.setSelected(true);
+                    }
+                }
+                preferences.setProgrammersAvailable(progModeButton.isSelected(), cmdModeButton.isSelected());
+            };
+
+            progModeButton.addActionListener(setProgMode);
+            cmdModeButton.addActionListener(setCmdMode);
+            modePane.add(progModeButton);
+            modePane.add(cmdModeButton);
+
+            panel.add(label, BorderLayout.NORTH);
+            panel.add(modePane, BorderLayout.CENTER);
+        }
         
         this.add(panel);
         pack();
