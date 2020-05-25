f7e585986de979d1ef98a8a170ab8cff4eb6c5f7
Daniel <db123@bergqvist.se>
2020-05-25 18:30:58 +0200

    Test the StartupActionFactoryScaffold class

---
+++ java/test/apps/startup/StartupActionFactoryScaffoldTest.java
@@ -0,0 +1,43 @@
+package apps.startup;
+
+import jmri.util.JUnitUtil;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Test the StartupActionFactoryScaffold class
+ * 
+ * @author Daniel Bergqvist Copyright (C) 2020
+ */
+public class StartupActionFactoryScaffoldTest {
+
+    @Test
+    public void testAction() {
+        boolean found = false;
+        StartupActionModelUtil t = StartupActionModelUtil.getDefault();
+        Assert.assertNotNull("exists",t);
+        for (Class c : t.getClasses()) {
+            if ("java.lang.String".equals(c.getName())) found = true;
+        }
+        
+        Assert.assertTrue("class is loaded", found);
+    }
+
+    @Before
+    public void setUp() {
+        JUnitUtil.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        JUnitUtil.deregisterBlockManagerShutdownTask();
+        JUnitUtil.tearDown();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(StartupActionFactoryScaffoldTest.class);
+
+}
