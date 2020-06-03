7ca51f3a108f0b861c32615378f2041cd16a91b3
Paul Bender <paul.bender@acm.org>
2020-06-03 00:44:41 -0400

    linting

--- java/src/jmri/jmrix/lenz/XNetSensor.java
+++ java/src/jmri/jmrix/lenz/XNetSensor.java
@@ -113,14 +113,10 @@ public class XNetSensor extends AbstractSensor implements XNetListener {
         if (log.isDebugEnabled()) {
             log.debug("received message: {}", l);
         }
-        Boolean opt = l.selectModuleFeedback(address);
+        boolean opt = l.selectModuleFeedback(address);
         if (opt != null) {
             if (log.isDebugEnabled()) {
                         log.debug("Message for sensor {} (Address {} position {})", systemName, baseaddress, address - (baseaddress * 8));
-            }
-            if (statusRequested && l.isUnsolicited()) {
-                l.resetUnsolicited();
-                statusRequested = false;
             }
             if (opt ^ _inverted) {
                 setOwnState(Sensor.ACTIVE);
@@ -136,6 +132,7 @@ public class XNetSensor extends AbstractSensor implements XNetListener {
      */
     @Override
     public void message(XNetMessage l) {
+        // not currently listening for outgoing messages.
     }
 
     /**
@@ -145,13 +142,8 @@ public class XNetSensor extends AbstractSensor implements XNetListener {
     @Override
     public void notifyTimeout(XNetMessage msg) {
         if (log.isDebugEnabled()) {
-            log.debug("Notified of timeout on message: {}", msg.toString());
+            log.debug("Notified of timeout on message: {}", msg);
         }
-    }
-
-    @Override
-    public void dispose() {
-        super.dispose();
     }
 
     /**
