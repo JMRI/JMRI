a3a8807820df522134d80fabf424276e0c23967c
Daniel <db123@bergqvist.se>
2020-06-16 02:00:46 +0200

    Add tests

--- java/test/jmri/jmrit/logixng/digital/actions/AbstractScriptDigitalActionTest.java
+++ java/test/jmri/jmrit/logixng/digital/actions/AbstractScriptDigitalActionTest.java
@@ -1,5 +1,9 @@
 package jmri.jmrit.logixng.digital.actions;
 
+import java.beans.PropertyVetoException;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import jmri.*;
 import jmri.jmrit.logixng.*;
 import jmri.util.*;
@@ -55,182 +59,83 @@ public class AbstractScriptDigitalActionTest {
                 actionAbstractScriptDigitalAction.getChildCount());
     }
     
-/*    
-    @Override
-    public ConditionalNG getConditionalNG() {
-        return conditionalNG;
+    @Test
+    public void testGetShortDescription() {
+        boolean hasThrown = false;
+        try {
+            actionAbstractScriptDigitalAction.getShortDescription();
+        } catch (UnsupportedOperationException e) {
+            hasThrown = true;
+            Assert.assertEquals("Error message is correct",
+                    "Not supported", e.getMessage());
+        }
+        Assert.assertTrue("Exception is thrown", hasThrown);
     }
     
-    @Override
-    public LogixNG getLogixNG() {
-        return logixNG;
+    @Test
+    public void testGetLongDescription() {
+        boolean hasThrown = false;
+        try {
+            actionAbstractScriptDigitalAction.getLongDescription();
+        } catch (UnsupportedOperationException e) {
+            hasThrown = true;
+            Assert.assertEquals("Error message is correct",
+                    "Not supported", e.getMessage());
+        }
+        Assert.assertTrue("Exception is thrown", hasThrown);
     }
     
-    @Override
-    public MaleSocket getConnectableChild() {
-        return null;
-    }
-    
-    @Override
-    public String getExpectedPrintedTree() {
-        return String.format("Shutdown computer after 0 seconds%n");
-    }
-    
-    @Override
-    public String getExpectedPrintedTreeFromRoot() {
-        return String.format(
-                "LogixNG: A new logix for test%n" +
-                "   ConditionalNG: A conditionalNG%n" +
-                "      ! %n" +
-                "         Shutdown computer after 0 seconds%n");
-    }
-    
-    @Override
-    public NamedBean createNewBean(String systemName) {
-        return new AbstractScriptDigitalAction(null) {
+    @Test
+    public void testMethods() throws PropertyVetoException {
+        // This shouldn't do anything but call it for coverage
+        actionAbstractScriptDigitalAction.vetoableChange(null);
+        
+        // This shouldn't do anything but call it for coverage
+        actionAbstractScriptDigitalAction.setup();
+        
+        AtomicBoolean registerCalled = new AtomicBoolean(false);
+        AtomicBoolean unregisterCalled = new AtomicBoolean(false);
+        
+        AbstractScriptDigitalAction action = new AbstractScriptDigitalAction(null) {
             @Override
             public void execute() throws JmriException {
                 throw new UnsupportedOperationException("Not supported");
+            }
+            
+            @Override
+            public void registerScriptListeners() {
+                // Call super method for coverage
+                super.registerScriptListeners();
+                registerCalled.set(true);
+            }
+            
+            @Override
+            public void unregisterScriptListeners() {
+                // Call super method for coverage
+                super.unregisterScriptListeners();
+                unregisterCalled.set(true);
             }
         };
-    }
-    
-    @Override
-    public boolean addNewSocket() {
-        return false;
-    }
-/*    
-    @Test
-    public void testCtor() {
-        Assert.assertNotNull("exists", new AbstractScriptDigitalAction(null){
-            @Override
-            public void execute() throws JmriException {
-                throw new UnsupportedOperationException("Not supported");
-            }
-        });
-    }
-    
-    @Test
-    public void testGetChild() {
-        Assert.assertTrue("getChildCount() returns 0", 0 == actionAbstractScriptDigitalAction.getChildCount());
         
-        boolean hasThrown = false;
-        try {
-            actionAbstractScriptDigitalAction.getChild(0);
-        } catch (UnsupportedOperationException ex) {
-            hasThrown = true;
-            Assert.assertEquals("Error message is correct", "Not supported.", ex.getMessage());
-        }
-        Assert.assertTrue("Exception is thrown", hasThrown);
-    }
-    
-    @Test
-    public void testCategory() {
-        Assert.assertTrue("Category matches", Category.EXRAVAGANZA == _base.getCategory());
-    }
-    
-    @Test
-    public void testIsExternal() {
-        Assert.assertTrue("is external", _base.isExternal());
-    }
-/*    
-    @Test
-    @Override
-    // The only purpose of override this method is to catch the error message
-    // The reason for this is that we have a security manager installed to block
-    // the ShusaatdownComputer action to shut down the computer during this test.
-    public void testMaleSocketIsActive() {
-        super.testMaleSocketIsActive();
-        JUnitAppender.suppressMessage(Level.ERROR, "exec is not allowed during test of ShutdownCompuddter");
-    }
-    
-    @Test
-    @Override
-    // The only purpose of override this method is to catch the error message.
-    // The reason for this is that we have a security manager installed to block
-    // the SddhutdownComputer action to shut down the computer during this test.
-    public void testIsActive() {
-        super.testIsActive();
-        JUnitAppender.suppressMessage(Level.ERROR, "exec is not allowed during test of ShutdownComputerrr");
-    }
-    
-    @Test
-    public void testSetSeconds() {
-        ShutdownComputdder action = new SrrhutdownComputer("IQDA321", null, 52);
-        Assert.assertEquals("Correct number of seconds", 52, action.getSeconds());
-        action.setSeconds(7);
-        Assert.assertEquals("Correct number of seconds", 7, action.getSeconds());
-        boolean hasThrown = false;
-        try {
-            action.setSeconds(-12);
-        } catch (IllegalArgumentException e) {
-            hasThrown = true;
-            Assert.assertEquals("error message is correct", "seconds must not be negative", e.getMessage());
-        }
-        Assert.assertTrue("Exception is thrown", hasThrown);
-        Assert.assertEquals("Correct number of seconds", 7, action.getSeconds());
-        action.setSeconds(0);
-        Assert.assertEquals("Correct number of seconds", 0, action.getSeconds());
+        // Test that listeners get registered
+        Assert.assertFalse("registerCalled is false", registerCalled.get());
+        action.registerListeners();
+        Assert.assertTrue("registerCalled is true", registerCalled.get());
         
-        // This shouldn't do anything but we call setup() for coverage
-        action.setup();
-    }
-    
-    @Test
-    public void testExecute() throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-        SffhutdownComputer action = new SrrhutdownComputer("IQDA321", null, 52);
+        // Test that listeners don't get registered again when they are registered
+        registerCalled.set(false);
+        action.registerListeners();
+        Assert.assertFalse("registerCalled is false", registerCalled.get());
         
-        // During this test, we change SystemType.type, but we reset it in tearDown()
+        // Test that listeners get unregistered
+        Assert.assertFalse("registerCalled is false", unregisterCalled.get());
+        action.unregisterListeners();
+        Assert.assertTrue("registerCalled is true", unregisterCalled.get());
         
-        Field privateField = SystemType.class.
-                getDeclaredField("type");
-        
-        privateField.setAccessible(true);
-        
-        privateField.set(SystemType.class, SystemType.WINDOWS);
-        action.execute();
-        Assert.assertEquals("Shutdown command is correct", "shutdown.exe", lastExecutedCommand);
-        JUnitAppender.assertErrorMessage("Shutdown failed");
-        
-        privateField.set(SystemType.class, SystemType.MACOSX);
-        action.execute();
-        Assert.assertEquals("Shutdown command is correct", "shutdown", lastExecutedCommand);
-        JUnitAppender.assertErrorMessage("Shutdown failed");
-        
-        privateField.set(SystemType.class, SystemType.LINUX);
-        action.execute();
-        Assert.assertEquals("Shutdown command is correct", "shutdown", lastExecutedCommand);
-        JUnitAppender.assertErrorMessage("Shutdown failed");
-        
-        privateField.set(SystemType.class, SystemType.UNIX);
-        action.execute();
-        Assert.assertEquals("Shutdown command is correct", "shutdown", lastExecutedCommand);
-        JUnitAppender.assertErrorMessage("Shutdown failed");
-        
-        boolean hasThrown = false;
-        try {
-            privateField.set(SystemType.class, SystemType.MACCLASSIC);
-            action.execute();
-        } catch (UnsupportedOperationException e) {
-            hasThrown = true;
-            Assert.assertTrue("Error message is correct", e.getMessage().startsWith("Unknown OS: "));
-        }
-        Assert.assertTrue("Exception is thrown", hasThrown);
-        
-        hasThrown = false;
-        try {
-            privateField.set(SystemType.class, SystemType.OS2);
-            action.execute();
-        } catch (UnsupportedOperationException e) {
-            hasThrown = true;
-            Assert.assertTrue("Error message is correct", e.getMessage().startsWith("Unknown OS: "));
-        }
-        Assert.assertTrue("Exception is thrown", hasThrown);
-    }
-*/    
-    @Test
-    public void testTest() {
-        
+        // Test that listeners don't get unregistered again when they are unregistered
+        unregisterCalled.set(false);
+        action.unregisterListeners();
+        Assert.assertFalse("registerCalled is false", unregisterCalled.get());
     }
     
     // The minimal setup for log4J
