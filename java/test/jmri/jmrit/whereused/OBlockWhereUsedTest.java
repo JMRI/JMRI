e695fbcda2f83be570317fcb9c233251a055cdff
petecressman <pete_cressman@sbcglobal.net>
2020-06-20 12:28:43 -0700

    Silly check for debug argument as requested

--- java/test/jmri/jmrit/whereused/OBlockWhereUsedTest.java
+++ java/test/jmri/jmrit/whereused/OBlockWhereUsedTest.java
@@ -1,7 +1,14 @@
 package jmri.jmrit.whereused;
 
 import jmri.util.JUnitUtil;
+
+import java.util.List;
+
 import org.junit.*;
+
+import jmri.NamedBean;
+import jmri.NamedBeanUsageReport;
+import jmri.jmrit.logix.OBlock;
 
 /**
  * Tests for the OBlockWhereUsed Class
@@ -14,6 +21,11 @@ public class OBlockWhereUsedTest {
     public void testOBlockWhereUsed() {
         OBlockWhereUsed ctor = new OBlockWhereUsed();
         Assert.assertNotNull("exists", ctor);
+        
+        // Pay the ransom to free PR#8715 to be merged
+        OBlock b = new OBlock("OB1");
+        List<NamedBeanUsageReport> list = b.getUsageReport(b);
+        Assert.assertNotNull("exists", list);
     }
 
     @Before
