dd351792d857b4038e66917002a1e372614b0fbf
Gregory-J-Bedlek <gbjunkmail4@comcast.net>
2020-07-30 15:57:36 -0500

    GB Get rid of "programmer reminder" about where release notes are (as comment in CTCMain.java) per Daniels suggestion.

--- java/src/jmri/jmrit/ctc/CTCMain.java
+++ java/src/jmri/jmrit/ctc/CTCMain.java
@@ -1,7 +1,6 @@
 /**
  *  @author Gregory J. Bedlek Copyright (C) 2018, 2019
  * 
- * Release notes(files): /JMRI/help/en/releasenotes/current-draft-note.shtml Section: CTC Tool section.
  * Help document: /JMRI/help/en/package/jmri/jmrit/ctc/CTC.shtml
  */
 
