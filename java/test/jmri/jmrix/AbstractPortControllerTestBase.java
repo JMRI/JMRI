8decc39d3735a0d9a77bd13b5392141b00efbecd
Bob Jacobsen <jacobsen@mac.com>
2020-08-06 21:21:31 -0700

    test default method for coverage

--- java/test/jmri/jmrix/AbstractPortControllerTestBase.java
+++ java/test/jmri/jmrix/AbstractPortControllerTestBase.java
@@ -3,6 +3,7 @@ package jmri.jmrix;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
 import jmri.SystemConnectionMemo;
@@ -17,6 +18,12 @@ public abstract class AbstractPortControllerTestBase {
     @Test
     public void testisDirtyNotNPE() {
         apc.isDirty();
+    }
+
+    @Test
+    public void testDefaultMethod() {
+        Assert.assertFalse("default false", apc.isOptionTypeText("foo"));
+        jmri.util.JUnitAppender.assertErrorMessage("did not find option foo for type");
     }
 
     // from here down is testing infrastructure
