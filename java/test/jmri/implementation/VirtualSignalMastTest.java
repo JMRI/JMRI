3e95f5f780ee2020e59a8681350f111d11eb0976
Bob Jacobsen <jacobsen@mac.com>
2020-08-23 23:00:47 -0700

    test HeldPermissive state transitions

--- java/test/jmri/implementation/VirtualSignalMastTest.java
+++ java/test/jmri/implementation/VirtualSignalMastTest.java
@@ -16,6 +16,96 @@ public class VirtualSignalMastTest {
         VirtualSignalMast t = new VirtualSignalMast("IF$vsm:basic:one-searchlight($1)");
         Assert.assertNotNull("exists",t);
     }
+    
+    @Test
+    public void testHeldPermissiveStateStructure() {
+    
+        VirtualSignalMast t;
+        
+        // original form of Held
+        t = new VirtualSignalMast("IF$vsm:basic:one-searchlight($1)");
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+        
+        t.setHeld(true);
+        // waitFor event with Held true
+        Assert.assertTrue(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+       
+        t.setHeld(false);
+        // waitFor event with Held false
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+       
+       
+        // normal manipulation of HeldPermissive within Held
+        t = new VirtualSignalMast("IF$vsm:basic:one-searchlight($2)");
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+        
+        t.setHeld(true);
+        // waitFor event with Held true
+        Assert.assertTrue(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+ 
+        t.setHeldPermissive(true);
+        // waitFor event with HeldPermissive true
+        Assert.assertTrue(t.getHeld());
+        Assert.assertTrue(t.getHeldPermissive());
+      
+         t.setHeldPermissive(false);
+        // waitFor event with HeldPermissive false
+        Assert.assertTrue(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+      
+        t.setHeld(false);
+        // waitFor event with Held false
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+        
+        
+        // direct setting of HeldPermissive
+        // transition to HeldPermissive is by way of Held
+        t = new VirtualSignalMast("IF$vsm:basic:one-searchlight($3)");
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+        
+        t.setHeldPermissive(true);      // Held not set before this
+        // waitFor event with Held true
+        // waitFor event with HeldPermissive true
+        Assert.assertTrue(t.getHeld());
+        Assert.assertTrue(t.getHeldPermissive());
+      
+        t.setHeldPermissive(false);
+        // waitFor event with HeldPermissive false
+        Assert.assertTrue(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+      
+        t.setHeld(false);
+        // waitFor event with Held false
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+    
+    
+        // direct resetting of Held while HeldPermissive
+        // leaving Held requires not in HeldPermissive
+        t = new VirtualSignalMast("IF$vsm:basic:one-searchlight($4)");
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+        
+        t.setHeldPermissive(true);      // Held not set before this
+        // waitFor event with Held true
+        // waitFor event with HeldPermissive true
+        Assert.assertTrue(t.getHeld());
+        Assert.assertTrue(t.getHeldPermissive());
+      
+         t.setHeld(false);
+        // waitFor event with HeldPermissive false
+        // waitFor event with Held false
+        Assert.assertFalse(t.getHeld());
+        Assert.assertFalse(t.getHeldPermissive());
+    
+    }
 
     @BeforeEach
     public void setUp() {
