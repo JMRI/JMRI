26ba2e503c47eb476e19261b9c6eb59598d28d86
Egbert Broerse <dcc2@ebroerse.nl>
2020-08-29 10:59:26 +0200

    param undouble

--- java/src/jmri/managers/AbstractTurnoutManager.java
+++ java/src/jmri/managers/AbstractTurnoutManager.java
@@ -214,10 +214,8 @@ public abstract class AbstractTurnoutManager extends AbstractManager<Turnout>
      * Internal method to invoke the factory, after all the logic for returning
      * an existing Turnout has been invoked.
      *
-     * @param systemName system name.
-     * @param userName username.
-     * @param systemName the system name to use for the new Sensor
-     * @param userName   the user name to use for the new Sensor
+     * @param systemName the system name to use for the new Turnout
+     * @param userName   the user name to use for the new Turnout
      * @return the new Turnout or null if unsuccessful
      */
     abstract protected Turnout createNewTurnout(@Nonnull String systemName, String userName);
