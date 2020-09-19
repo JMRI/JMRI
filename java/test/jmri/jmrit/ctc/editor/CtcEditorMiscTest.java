da6c69869587c9603d049d51633d3d473842218e
DS <gk@dfm.sandsys.org>
2020-09-19 00:20:16 -0500

    Static analysis fix

--- java/test/jmri/jmrit/ctc/editor/CtcEditorMiscTest.java
+++ java/test/jmri/jmrit/ctc/editor/CtcEditorMiscTest.java
@@ -79,6 +79,5 @@ public class CtcEditorMiscTest {
         JUnitUtil.tearDown();
     }
 
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CtcEditorSignalMastsTest.class);
-
+//     private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CtcEditorSignalMastsTest.class);
 }
