63954ea33580d8217a153d878d2934179ff1c157
Daniel <db123@bergqvist.se>
2020-11-06 18:48:33 +0100

    Update Bundle.java

--- java/src/jmri/jmrit/logixng/actions/Bundle.java
+++ java/src/jmri/jmrit/logixng/actions/Bundle.java
@@ -24,7 +24,7 @@ import javax.annotation.ParametersAreNonnullByDefault;
 public class Bundle extends jmri.jmrit.logixng.Bundle {
 
     @CheckForNull
-    private static final String name = "jmri.jmrit.logixng.digital.actions.DigitalActionBundle";
+    private static final String name = "jmri.jmrit.logixng.actions.DigitalActionBundle";
 
     //
     // below here is boilerplate to be copied exactly
