30c1793943c971a6a73a635c2f86f8f90797d047
Daniel <db123@bergqvist.se>
2020-11-16 10:42:03 +0100

    Remove unused variable

--- java/test/jmri/jmrit/logixng/util/ProtectedTimerTaskTest.java
+++ java/test/jmri/jmrit/logixng/util/ProtectedTimerTaskTest.java
@@ -41,5 +41,5 @@ public class ProtectedTimerTaskTest {
         JUnitUtil.tearDown();
     }
     
-    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ProtectedTimerTaskTest.class);
+//    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ProtectedTimerTaskTest.class);
 }
