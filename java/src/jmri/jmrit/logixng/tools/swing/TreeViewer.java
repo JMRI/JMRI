26e005cc5808aed457401fd79889de673ec1de47
Daniel <db123@bergqvist.se>
2020-11-19 01:55:33 +0100

    Remove System.out

--- java/src/jmri/jmrit/logixng/tools/swing/TreeViewer.java
+++ java/src/jmri/jmrit/logixng/tools/swing/TreeViewer.java
@@ -147,17 +147,12 @@ public class TreeViewer extends JmriJFrame implements PropertyChangeListener {
             
             // Update the tree
             Base b = (Base)evt.getSource();
-            System.out.format("propertyChange: %s - %s%n", b.toString(), evt.getPropertyName());
             FemaleSocket.FemaleSocketAndRow socketAndRow = new FemaleSocket.FemaleSocketAndRow();
             _femaleRootSocket.getFemaleSocketAndRow(b, socketAndRow);
             
             FemaleSocket femaleSocket = socketAndRow._socket;
             int row = socketAndRow._row;
-            
-            System.out.format("%s: %s - %d - %s%n", getClass().getName(), evt.getPropertyName(), socketAndRow._row, socketAndRow._socket.getClass().getName());
-            
             TreePath path = tree.getPathForRow(row);
-            
             updateTree(femaleSocket, path);
         }
         
@@ -167,16 +162,8 @@ public class TreeViewer extends JmriJFrame implements PropertyChangeListener {
             
             FemaleSocket femaleSocket = ((FemaleSocket)evt.getSource());
             TreePath path = tree.getPathForRow(femaleSocketTreeModel.getRow(femaleSocket));
-            
-            System.out.format("%s: %s - %d - %s%n", getClass().getName(), evt.getPropertyName(), femaleSocketTreeModel.getRow(femaleSocket), femaleSocket.getClass().getName());
-            
             updateTree(femaleSocket, path);
         }
-        
-//        _femaleRootSocket.forEntireTree((Base b) -> {
-//            b.addPropertyChangeListener(TreeViewer.this);
-//        });
-        
     }
     
     protected void updateTree(FemaleSocket currentFemaleSocket, TreePath currentPath) {
