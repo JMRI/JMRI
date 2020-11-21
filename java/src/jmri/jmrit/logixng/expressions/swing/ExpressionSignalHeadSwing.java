a07e179a0227e8643a4751b0f95ade4aed5a8316
Daniel <db123@bergqvist.se>
2020-11-22 00:06:37 +0100

    Update ExpressionSignalHeadSwing.java

--- java/src/jmri/jmrit/logixng/expressions/swing/ExpressionSignalHeadSwing.java
+++ java/src/jmri/jmrit/logixng/expressions/swing/ExpressionSignalHeadSwing.java
@@ -4,22 +4,64 @@ import java.util.List;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
-import javax.swing.JPanel;
+import javax.swing.*;
 
-import jmri.InstanceManager;
-import jmri.jmrit.logixng.Base;
-import jmri.jmrit.logixng.DigitalExpressionManager;
-import jmri.jmrit.logixng.MaleSocket;
+import jmri.*;
+import jmri.jmrit.logixng.*;
 import jmri.jmrit.logixng.expressions.ExpressionSignalHead;
+import jmri.util.swing.BeanSelectCreatePanel;
 
 /**
  * Configures an ExpressionSignalHead object with a Swing JPanel.
  */
 public class ExpressionSignalHeadSwing extends AbstractDigitalExpressionSwing {
 
+    private NamedBeanHandle<SignalHead> _signalHeadHandle;
+    private BeanSelectCreatePanel<SignalHead> signalHeadBeanPanel;
+    private JComboBox<ExpressionSignalHead.QueryType> queryTypeComboBox;
+    private JComboBox<SignalHeadState> signalHeadStateComboBox;
+    
+    
     @Override
     protected void createPanel(@CheckForNull Base object, @Nonnull JPanel buttonPanel) {
+        if ((object != null) && !(object instanceof ExpressionSignalHead)) {
+            throw new IllegalArgumentException("object must be an ExpressionSignalHead but is a: "+object.getClass().getName());
+        }
+        ExpressionSignalHead expression = (ExpressionSignalHead)object;
+        
         panel = new JPanel();
+        signalHeadBeanPanel = new BeanSelectCreatePanel<>(InstanceManager.getDefault(SignalHeadManager.class), null);
+        
+        signalHeadStateComboBox = new JComboBox<>();
+        
+        queryTypeComboBox = new JComboBox<>();
+        for (ExpressionSignalHead.QueryType e : ExpressionSignalHead.QueryType.values()) {
+            queryTypeComboBox.addItem(e);
+        }
+        
+        if (expression != null) {
+            if (expression.getSignalHead() != null) {
+                signalHeadBeanPanel.setDefaultNamedBean(expression.getSignalHead().getBean());
+            }
+            queryTypeComboBox.setSelectedItem(expression.getQueryType());
+        }
+        
+        if ((expression != null) && (expression.getSignalHead() != null)) {
+            SignalHead sh = expression.getSignalHead().getBean();
+            
+            int[] states = sh.getValidStates();
+            for (int s : states) {
+                SignalHeadState shs = new SignalHeadState();
+                shs._state = s;
+                shs._name = sh.getAppearanceName(s);
+                signalHeadStateComboBox.addItem(shs);
+                if (expression.getAppearance() == s) signalHeadStateComboBox.setSelectedItem(shs);
+            }
+        }
+        panel.add(new JLabel(Bundle.getMessage("BeanNameSignalHead")));
+        panel.add(signalHeadBeanPanel);
+        panel.add(queryTypeComboBox);
+        panel.add(signalHeadStateComboBox);
     }
     
     /** {@inheritDoc} */
@@ -38,7 +80,26 @@ public class ExpressionSignalHeadSwing extends AbstractDigitalExpressionSwing {
     /** {@inheritDoc} */
     @Override
     public void updateObject(@Nonnull Base object) {
-        // Nothing to update
+        if (! (object instanceof ExpressionSignalHead)) {
+            throw new IllegalArgumentException("object must be an ActionMemory but is a: "+object.getClass().getName());
+        }
+        ExpressionSignalHead expression = (ExpressionSignalHead)object;
+        try {
+            SignalHead signalHead = signalHeadBeanPanel.getNamedBean();
+            if (signalHead != null) {
+                NamedBeanHandle<SignalHead> handle
+                        = InstanceManager.getDefault(NamedBeanHandleManager.class)
+                                .getNamedBeanHandle(signalHead.getDisplayName(), signalHead);
+                expression.setSignalHead(handle);
+            }
+        } catch (JmriException ex) {
+            log.error("Cannot get NamedBeanHandle for memory", ex);
+        }
+        
+        expression.setQueryType(queryTypeComboBox.getItemAt(queryTypeComboBox.getSelectedIndex()));
+        if (signalHeadStateComboBox.getItemCount() > 0) {
+            expression.setAppearance(signalHeadStateComboBox.getItemAt(signalHeadStateComboBox.getSelectedIndex())._state);
+        }
     }
     
     /** {@inheritDoc} */
@@ -52,6 +113,18 @@ public class ExpressionSignalHeadSwing extends AbstractDigitalExpressionSwing {
     }
     
     
-//    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TrueSwing.class);
+    private static class SignalHeadState {
+        
+        private int _state;
+        private String _name;
+        
+        @Override
+        public String toString() {
+            return _name;
+        }
+        
+    }
+    
+    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ExpressionSignalHeadSwing.class);
     
 }
