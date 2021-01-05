9999b47aeb528e41320bd041d554a7446f2f9faa
Daniel <db123@bergqvist.se>
2021-01-05 13:31:32 +0100

    Update ModuleEditor.java

--- java/src/jmri/jmrit/logixng/tools/swing/ModuleEditor.java
+++ java/src/jmri/jmrit/logixng/tools/swing/ModuleEditor.java
@@ -6,10 +6,12 @@ import java.util.*;
 import javax.annotation.Nonnull;
 
 import jmri.InstanceManager;
+import jmri.JmriException;
 import jmri.jmrit.beantable.BeanTableDataModel;
 import jmri.jmrit.beantable.BeanTableFrame;
 import jmri.jmrit.logixng.*;
 import jmri.jmrit.logixng.Module;
+import jmri.jmrit.logixng.implementation.AbstractBase;
 import jmri.jmrit.logixng.implementation.AbstractFemaleSocket;
 
 /**
@@ -47,7 +49,7 @@ public class ModuleEditor extends TreeEditor implements AbstractLogixNGEditor<Mo
     public ModuleEditor(BeanTableFrame<Module> f,
             BeanTableDataModel<Module> m, String sName) {
         
-        super(setupRootSocket(sName), true, true);
+        super(setupRootSocket(new Root(), sName), true, true);
 /*        
         super(new RootSocket(null, new FemaleSocketListener() {
             @Override
@@ -72,11 +74,13 @@ public class ModuleEditor extends TreeEditor implements AbstractLogixNGEditor<Mo
             throw new RuntimeException("Connected socket is not a Module");
         }
         _module = (Module) _femaleRootSocket.getConnectedSocket().getObject();
+        
+        ((Root)_femaleRootSocket.getParent())._femaleSocket = _femaleRootSocket;
 //        makeEditLogixNGWindow();
     }
     
-    private static FemaleSocket setupRootSocket(String sName) {
-        FemaleSocket socket = new RootSocket(null, new FemaleSocketListener() {
+    private static FemaleSocket setupRootSocket(Base parent, String sName) {
+        FemaleSocket socket = new RootSocket(parent, new FemaleSocketListener() {
             @Override
             public void connected(FemaleSocket socket) {
                 // Do nothing
@@ -210,6 +214,109 @@ public class ModuleEditor extends TreeEditor implements AbstractLogixNGEditor<Mo
         @Override
         public String getLongDescription(Locale locale) {
             return Bundle.getMessage(locale, "ModuleEditor_RootSocket_Long", getName());
+        }
+        
+    }
+    
+    
+    
+    private static class Root extends AbstractBase {
+
+        private FemaleSocket _femaleSocket;
+        
+        public Root() {
+            super("");
+        }
+        
+        @Override
+        protected void registerListenersForThisClass() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        protected void unregisterListenersForThisClass() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        protected void disposeMe() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public void setState(int s) throws JmriException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public int getState() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public String getBeanType() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public Base getDeepCopy(Map<String, String> systemNames, Map<String, String> userNames) throws JmriException {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public String getShortDescription(Locale locale) {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public String getLongDescription(Locale locale) {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public Base getParent() {
+            return null;
+        }
+
+        @Override
+        public void setParent(Base parent) {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public FemaleSocket getChild(int index) throws IllegalArgumentException, UnsupportedOperationException {
+            if (index != 0) throw new IllegalArgumentException("invalid index: "+Integer.toString(index));
+            return _femaleSocket;
+        }
+
+        @Override
+        public int getChildCount() {
+            return 1;
+        }
+
+        @Override
+        public Category getCategory() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public boolean isExternal() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public Lock getLock() {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public void setLock(Lock lock) {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public void setup() {
+            throw new UnsupportedOperationException("Not supported");
         }
         
     }
