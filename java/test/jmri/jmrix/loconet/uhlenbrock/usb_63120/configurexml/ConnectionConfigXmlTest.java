14cb47ffcdf323933d866c4236a3b104542ac4c9
Egbert Broerse <dcc2@ebroerse.nl>
2021-01-13 20:52:28 +0100

    test ctor w/Assertions

--- java/test/jmri/jmrix/loconet/uhlenbrock/usb_63120/configurexml/ConnectionConfigXmlTest.java
+++ java/test/jmri/jmrix/loconet/uhlenbrock/usb_63120/configurexml/ConnectionConfigXmlTest.java
@@ -4,7 +4,9 @@ import jmri.jmrix.loconet.locobufferusb.ConnectionConfig;
 import jmri.jmrix.loconet.locobufferusb.configurexml.ConnectionConfigXml;
 import jmri.util.JUnitUtil;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test for the usb_63120 ConnectionConfigXml class.
@@ -12,6 +14,12 @@ import org.junit.jupiter.api.BeforeEach;
  * @author   Paul Bender  Copyright (C) 2016, 2021
  */
 public class ConnectionConfigXmlTest extends jmri.jmrix.configurexml.AbstractSerialConnectionConfigXmlTestBase {
+
+    @Test
+    public void testCtor() {
+        jmri.jmrix.loconet.uhlenbrock.configurexml.ConnectionConfigXml ccx = new jmri.jmrix.loconet.uhlenbrock.configurexml.ConnectionConfigXml();
+        Assertions.assertNotNull(ccx, "Instance is created");
+    }
 
     @BeforeEach
     @Override
