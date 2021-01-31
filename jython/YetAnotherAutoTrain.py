47fc240ab5255b527876239e2d1f0ea1d27fa5a1
DS <gk@dfm.sandsys.org>
2021-01-31 09:19:40 -0600

    Fix URL

--- jython/YetAnotherAutoTrain.py
+++ jython/YetAnotherAutoTrain.py
@@ -17,7 +17,7 @@
 #         Add the ability to create custom extensions.
 # Author:  Dave Sand copyright (c) 2018 - 2020
 
-# The following help content is also available at https://jmri.org//help/en/html/scripthelp/yaat/YAAT.shtml
+# The following help content is also available at https://jmri.org/help/en/html/scripthelp/yaat/YAAT.shtml
 
 # Action Phrase Descriptions:
 #       <?? | ??> represent choices
