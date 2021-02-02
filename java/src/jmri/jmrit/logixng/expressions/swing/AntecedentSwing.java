e1a8aea6643a2185986ac1a37e344ee918d7d8b4
Daniel <db123@bergqvist.se>
2021-02-02 03:40:48 +0100

    Update AntecedentSwing

--- java/src/jmri/jmrit/logixng/expressions/swing/AntecedentSwing.java
+++ java/src/jmri/jmrit/logixng/expressions/swing/AntecedentSwing.java
@@ -1,25 +1,36 @@
 package jmri.jmrit.logixng.expressions.swing;
 
-import java.util.List;
+import java.util.*;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import jmri.InstanceManager;
+import jmri.JmriException;
 import jmri.jmrit.logixng.Base;
 import jmri.jmrit.logixng.DigitalExpressionManager;
 import jmri.jmrit.logixng.MaleSocket;
 import jmri.jmrit.logixng.expressions.Antecedent;
+import jmri.jmrit.logixng.util.parser.*;
 
 /**
  * Configures an ExpressionTurnout object with a Swing JPanel.
  */
 public class AntecedentSwing extends AbstractDigitalExpressionSwing {
 
+    private JTextField _antecedent;
+    
     @Override
     protected void createPanel(@CheckForNull Base object, @Nonnull JPanel buttonPanel) {
+        Antecedent expression = (Antecedent)object;
         panel = new JPanel();
+        JLabel label = new JLabel(Bundle.getMessage("Antecedent_Antecedent"));
+        _antecedent = new JTextField();
+        _antecedent.setColumns(40);
+        if (expression != null) _antecedent.setText(expression.getAntecedent());
+        panel.add(label);
+        panel.add(_antecedent);
     }
     
     /** {@inheritDoc} */
@@ -32,13 +43,24 @@ public class AntecedentSwing extends AbstractDigitalExpressionSwing {
     @Override
     public MaleSocket createNewObject(@Nonnull String systemName, @CheckForNull String userName) {
         Antecedent expression = new Antecedent(systemName, userName);
+        updateObject(expression);
         return InstanceManager.getDefault(DigitalExpressionManager.class).registerExpression(expression);
     }
     
     /** {@inheritDoc} */
     @Override
     public void updateObject(@Nonnull Base object) {
-        // Nothing to update
+        if (!(object instanceof Antecedent)) {
+            throw new IllegalArgumentException("object must be an Antecedent but is a: "+object.getClass().getName());
+        }
+        
+        Antecedent expression = (Antecedent)object;
+        
+        try {
+            expression.setAntecedent(_antecedent.getText());
+        } catch (JmriException ex) {
+            log.error("Error when parsing formula", ex);
+        }
     }
     
     /** {@inheritDoc} */
@@ -52,6 +74,6 @@ public class AntecedentSwing extends AbstractDigitalExpressionSwing {
     }
     
     
-//    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AntecedentSwing.class);
+    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AntecedentSwing.class);
     
 }
