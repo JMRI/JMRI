f661c085b8a18b9622477589c7f13dd9b70b9210
Egbert Broerse <dcc2@ebroerse.nl>
2021-02-08 18:33:23 +0100

    check getLookAndFeel()

--- java/test/jmri/jmrit/display/switchboardEditor/BeanSwitchTest.java
+++ java/test/jmri/jmrit/display/switchboardEditor/BeanSwitchTest.java
@@ -2,11 +2,11 @@ package jmri.jmrit.display.switchboardEditor;
 
 import jmri.*;
 import jmri.jmrit.beantable.AddNewDevicePanel;
-import jmri.jmrit.display.EditorFrameOperator;
+//import jmri.jmrit.display.EditorFrameOperator;
 import org.junit.jupiter.api.*;
-import java.awt.GraphicsEnvironment;
 import java.awt.event.MouseEvent;
 import java.util.Objects;
+import javax.swing.UIManager;
 
 import jmri.util.JUnitUtil;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
@@ -68,23 +68,24 @@ public class BeanSwitchTest {
         t.displayState(4);
         Assertions.assertTrue(t.getIconLabel().endsWith("T"), "Thrown label T");
 
-        Thread dialog_thread1 = new Thread(() -> {
-            JDialogOperator jdo = new JDialogOperator(Bundle.getMessage("EditNameTitle", ""));
-            // step down for items inside JOptionPane
-            ((JTextField)((JPanel)((JPanel)((JOptionPane)jdo.getContentPane().getComponent(0)).getComponent(0)).getComponent(0)).getComponent(1)).setText("intTurnThree");
-            new JButtonOperator(jdo, Bundle.getMessage("ButtonOK")).doClick();
-        });
-        dialog_thread1.setName("Edit bean user name dialog");
-        dialog_thread1.start();
+        if (UIManager.getLookAndFeel().getName().equals("Mac OS X")) {
+            Thread dialog_thread1 = new Thread(() -> {
+                JDialogOperator jdo = new JDialogOperator(Bundle.getMessage("EditNameTitle", ""));
+                // step down for items inside JOptionPane
+                ((JTextField) ((JPanel) ((JPanel) ((JOptionPane) jdo.getContentPane().getComponent(0)).getComponent(0)).getComponent(0)).getComponent(1)).setText("intTurnThree");
+                new JButtonOperator(jdo, Bundle.getMessage("ButtonOK")).doClick();
+            });
+            dialog_thread1.setName("Edit bean user name dialog");
+            dialog_thread1.start();
 
-        t.renameBeanDialog(); // dialog
+            t.renameBeanDialog(); // dialog
 
-        JUnitUtil.waitFor(()-> !(dialog_thread1.isAlive()), "Edit bean user name dialog");
-        Assertions.assertEquals("intTurnThree", nb.getUserName(), "New name 3 reapplied to nb");
-        // actual bean rename method
-        t.renameBean("intTurnTwo", "intTurnThree");
-        Assertions.assertEquals("intTurnTwo", t.getUserNameString(), "New name 2 applied to nb");
-
+            JUnitUtil.waitFor(() -> !(dialog_thread1.isAlive()), "Edit bean user name dialog");
+            Assertions.assertEquals("intTurnThree", nb.getUserName(), "New name 3 reapplied to nb");
+            // actual bean rename method
+            t.renameBean("intTurnTwo", "intTurnThree");
+            Assertions.assertEquals("intTurnTwo", t.getUserNameString(), "New name 2 applied to nb");
+        }
         t.cleanup(); // make sure no exception is thrown
     }
 
@@ -105,31 +106,33 @@ public class BeanSwitchTest {
         t.cleanup(); // make sure no exception is thrown
     }
 
-    @Disabled("no output received in last 10 min on Travis CI GUI test run")
+    //@Disabled("no output received in last 10 min on Travis CI GUI test run")
     @Test
     public void testSensorSymbolUnconnected() {
         swe.setSwitchType("S");
         swe.setSwitchManu("I"); // set explicitly
         BeanSwitch t = new BeanSwitch(1, null, "IS4", SwitchboardEditor.SYMBOL, swe);
         Assertions.assertNotNull(t, "exists");
-        Thread dialog_thread1 = new Thread(() -> {
-            JFrameOperator jfo = new JFrameOperator(Bundle.getMessage("ConnectNewMenu", ""));
-            // step down for items inside JFrame
-            ((JTextField)((JPanel)((AddNewDevicePanel)jfo.getContentPane().getComponent(0)).getComponent(0)).getComponent(3)).setText("intSensFour");
-            new JButtonOperator(jfo, Bundle.getMessage("ButtonOK")).doClick();
-        });
-        dialog_thread1.setName("Connect new Sensor dialog");
-        dialog_thread1.start();
+        if (UIManager.getLookAndFeel().getName().equals("Mac OS X")) {
+            Thread dialog_thread1 = new Thread(() -> {
+                JFrameOperator jfo = new JFrameOperator(Bundle.getMessage("ConnectNewMenu", ""));
+                // step down for items inside JFrame
+                ((JTextField) ((JPanel) ((AddNewDevicePanel) jfo.getContentPane().getComponent(0)).getComponent(0)).getComponent(3)).setText("intSensFour");
+                new JButtonOperator(jfo, Bundle.getMessage("ButtonOK")).doClick();
+            });
+            dialog_thread1.setName("Connect new Sensor dialog");
+            dialog_thread1.start();
 
-        t.connectNew(); // pops a dialog
+            t.connectNew(); // pops a dialog
 
-        JUnitUtil.waitFor(()-> !(dialog_thread1.isAlive()), "Connect new Sensor dialog");
-        // can't recreate this BeanSwitch to include a connection, so we just check sensor was created and available in the manager
-        Sensor newSensor = jmri.InstanceManager.getDefault(SensorManager.class).getSensor("IS4");
-        Assertions.assertNotNull(newSensor, "Sensor IS4 was created");
-        String newName = Objects.requireNonNull(InstanceManager.getDefault(SensorManager.class).getSensor("IS4")).getUserName();
-        Assertions.assertNotNull(newName, "Sensor IS4 has user name");
-        Assertions.assertEquals("intSensFour", Objects.requireNonNull(InstanceManager.getDefault(SensorManager.class).getSensor("IS4")).getUserName(), "User name applied to nb");
+            JUnitUtil.waitFor(() -> !(dialog_thread1.isAlive()), "Connect new Sensor dialog");
+            // can't recreate this BeanSwitch to include a connection, so we just check sensor was created and available in the manager
+            Sensor newSensor = jmri.InstanceManager.getDefault(SensorManager.class).getSensor("IS4");
+            Assertions.assertNotNull(newSensor, "Sensor IS4 was created");
+            String newName = Objects.requireNonNull(InstanceManager.getDefault(SensorManager.class).getSensor("IS4")).getUserName();
+            Assertions.assertNotNull(newName, "Sensor IS4 has user name");
+            Assertions.assertEquals("intSensFour", Objects.requireNonNull(InstanceManager.getDefault(SensorManager.class).getSensor("IS4")).getUserName(), "User name applied to nb");
+        }
         t.displayState(4);
         Assertions.assertEquals("IS4", t.getIconLabel(), "Active label (no sign until recreated as connected)");
 
