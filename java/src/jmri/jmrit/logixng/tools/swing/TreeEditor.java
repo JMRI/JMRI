eb1bcd5c0a1033c350aca874e00df83721813103
Daniel <db123@bergqvist.se>
2021-02-21 01:44:20 +0100

    Update TreeEditor.java

--- java/src/jmri/jmrit/logixng/tools/swing/TreeEditor.java
+++ java/src/jmri/jmrit/logixng/tools/swing/TreeEditor.java
@@ -10,7 +10,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 
 import javax.annotation.Nonnull;
 import javax.swing.*;
