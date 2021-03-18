2a3f689e17924a51dff61b2ffb8fa9394900623b
Andrew Crosland <andrew@crosland.me.uk>
2021-03-18 13:57:50 +0000

    Fix replies from Sprog simulator

--- java/src/jmri/jmrix/sprog/simulator/SimulatorAdapter.java
+++ java/src/jmri/jmrix/sprog/simulator/SimulatorAdapter.java
@@ -307,13 +307,13 @@ public class SimulatorAdapter extends SprogPortController implements Runnable {
             case 'C':
             case 'V':
                 log.debug("Read/Write CV detected");
-                reply = new SprogReply("= " + msg.toString().substring(2) + "\n"); // echo CV value (hex)
+                reply = new SprogReply("= h" + msg.toString().substring(2) + "\n"); // echo CV value (hex)
                 break;
 
             case 'D':
             case 'U':
                 log.debug("Read/Write CV with hint detected");
-                reply = new SprogReply("= " + msg.toString().substring(2) + "\n"); // echo CV hint value (hex)
+                reply = new SprogReply("= h" + msg.toString().substring(2) + "\n"); // echo CV hint value (hex)
                 break;
 
             case 'O':
