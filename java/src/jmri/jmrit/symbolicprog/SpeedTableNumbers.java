f2c93b374511845f506de55132c0598645423a7c
Daniel <db123@bergqvist.se>
2021-05-14 04:53:00 +0200

    Show numbers 1, 7, 14, 21, 28

--- java/src/jmri/jmrit/symbolicprog/SpeedTableNumbers.java
+++ java/src/jmri/jmrit/symbolicprog/SpeedTableNumbers.java
@@ -10,7 +10,10 @@ public enum SpeedTableNumbers {
     None(Bundle.getMessage("SpeedTable_None"), (index) -> {return false;}),
     All(Bundle.getMessage("SpeedTable_All"), (index) -> {return true;}),
     Only1of3(Bundle.getMessage("SpeedTable_1of3"), (index) -> {return (index % 3) == 0;}),
-    Only1of7(Bundle.getMessage("SpeedTable_1of7"), (index) -> {return (index % 7) == 0;});
+    Only1of7(Bundle.getMessage("SpeedTable_1of7"), (index) -> {
+        int index1 = index+1;   // index is 0 <= x < count;  index1 is 1 <= x <= count.
+        return (index1 == 1) || (index1 == 7) || (index1 == 14) || (index1 == 21) || (index1 == 28);
+    });
     
     private final String label;
     private final Filter filter;
