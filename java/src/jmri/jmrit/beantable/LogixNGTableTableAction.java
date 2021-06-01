db81ae3db939ae7eee1ee2adf622c39ed89511b0
Daniel <db123@bergqvist.se>
2021-06-01 02:35:36 +0200

    Implement Browse for LogixNG Table table

--- java/src/jmri/jmrit/beantable/LogixNGTableTableAction.java
+++ java/src/jmri/jmrit/beantable/LogixNGTableTableAction.java
@@ -109,8 +109,50 @@ public class LogixNGTableTableAction extends AbstractLogixNGTableAction<NamedTab
     }
 
     @Override
+    protected boolean browseMonoSpace() {
+        return true;
+    }
+
+    @Override
     protected String getBeanText(NamedTable bean) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int maxColumnWidth = 0;
+        int columnWidth[] = new int[bean.numColumns()];
+        String[][] cells = new String[bean.numRows()][];
+        for (int row=0; row < bean.numRows(); row++) {
+            cells[row] = new String[bean.numColumns()];
+            for (int col=0; col < bean.numColumns(); col++) {
+                Object value = bean.getCell(row, col);
+                cells[row][col] = value != null ? value.toString() : "<null>";
+                columnWidth[col] = Math.max(columnWidth[col], cells[row][col].length());
+                maxColumnWidth = Math.max(maxColumnWidth, columnWidth[col]);
+            }
+        }
+        String columnLine = "";
+        while (columnLine.length()+2 < maxColumnWidth) columnLine += "----------------------";
+        String columnPadding = String.format("%"+Integer.toString(maxColumnWidth)+"s", "");
+        StringBuilder sb = new StringBuilder();
+        sb.append("+");
+        for (int col=0; col < bean.numColumns(); col++) {
+            sb.append(columnLine.substring(0,columnWidth[col]+2));
+            sb.append("+");
+            if (col+1 == bean.numColumns()) sb.append(String.format("%n"));
+        }
+        for (int row=0; row < bean.numRows(); row++) {
+            sb.append("|");
+            for (int col=0; col < bean.numColumns(); col++) {
+                sb.append(" ");
+                sb.append((cells[row][col]+columnPadding).substring(0,columnWidth[col]));
+                sb.append(" |");
+                if (col+1 == bean.numColumns()) sb.append(String.format("%n"));
+            }
+            sb.append("+");
+            for (int col=0; col < bean.numColumns(); col++) {
+                sb.append(columnLine.substring(0,columnWidth[col]+2));
+                sb.append("+");
+                if (col+1 == bean.numColumns()) sb.append(String.format("%n"));
+            }
+        }
+        return sb.toString();
     }
 
     @Override
