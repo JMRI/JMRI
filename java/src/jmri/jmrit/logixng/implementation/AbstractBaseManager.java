e09d642810505860cae4aa0ab13af9d66b87c2b3
Daniel <db123@bergqvist.se>
2021-06-17 02:24:29 +0200

    Fix spotbugs error

--- java/src/jmri/jmrit/logixng/implementation/AbstractBaseManager.java
+++ java/src/jmri/jmrit/logixng/implementation/AbstractBaseManager.java
@@ -99,6 +99,6 @@ public abstract class AbstractBaseManager<E extends NamedBean> extends AbstractM
         _maleSocketFactories.add(factory);
     }
     
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AbstractBaseManager.class);
+//    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(AbstractBaseManager.class);
     
 }
