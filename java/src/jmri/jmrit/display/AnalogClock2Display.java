adccab956453a429d305f57a82dfc945694c966e
Daniel <db123@bergqvist.se>
2021-06-21 20:32:25 +0200

    Remove redundant imports

--- java/src/jmri/jmrit/display/AnalogClock2Display.java
+++ java/src/jmri/jmrit/display/AnalogClock2Display.java
@@ -12,8 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Date;
 
 import javax.swing.ButtonGroup;
@@ -24,7 +22,6 @@ import javax.swing.JRadioButtonMenuItem;
 
 import jmri.*;
 import jmri.jmrit.catalog.NamedIcon;
-import jmri.jmrit.display.Bundle;
 import jmri.util.swing.JmriColorChooser;
 
 import org.slf4j.Logger;
