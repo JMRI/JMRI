e739df980c0068fd2bfb2a871a28cc6585649a92
Daniel <db123@bergqvist.se>
2021-06-26 06:44:09 +0200

    Delete onload.js

--- help/en/package/jmri/jmrit/beantable/onload.js
+++
@@ -1,7 +0,0 @@
-    function onLoad() {
-	  if (window.location.href.startsWith("file:///") && !window.location.href.endsWith("?inside"))
-	  {
-        var urlParts = window.location.href.split("/help/en/");
-        window.location.href = urlParts[0] + "/help/en/local/index.html?url=" + window.location.href + "?inside";
-	  }
-    }
