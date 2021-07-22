b165a4281eb970e06e0b18020c0b112bcea7e6a4
TCSDCC-Jordan <jordan@tcsdcc.com>
2021-07-22 18:12:27 -0400

    Added unit test for new mode of decoder sensing
    Unit test confirms that integer transformation of the 4 hash CV's is accurate. This allows V5 TCS sound decoders to be identified down to any OEM project or full release.

--- java/src/jmri/jmrit/decoderdefn/IdentifyDecoder.java
+++ java/src/jmri/jmrit/decoderdefn/IdentifyDecoder.java
@@ -235,7 +235,7 @@ public abstract class IdentifyDecoder extends jmri.jmrit.AbstractIdentify {
             readCV("49");
             return false;
         } else if (mfgID == 153) {  // DIY
-            productIDhighest = value;
+            productIDlowest = value;
             statusUpdate("Read decoder product hash #2 CV 254");
             readCV("254");
             return false;
@@ -262,7 +262,7 @@ public abstract class IdentifyDecoder extends jmri.jmrit.AbstractIdentify {
             readCV("50");
             return false;
         } else if (mfgID == 153) {  // DIY
-            productIDhigh = value;
+            productIDlow = value;
             statusUpdate("Read decoder product hash #3 CV 255");
             readCV("255");
             return false;
@@ -287,7 +287,7 @@ public abstract class IdentifyDecoder extends jmri.jmrit.AbstractIdentify {
             productID = (((((productIDhighest << 8) | productIDhigh) << 8) | productIDlow) << 8) | productIDlowest;
             return true;
         } else if (mfgID == 153) {  // DIY
-            productIDlow = value;
+            productIDhigh = value;
             statusUpdate("Read decoder product hash #2 CV 256");
             readCV("256");
             return false;
@@ -308,8 +308,8 @@ public abstract class IdentifyDecoder extends jmri.jmrit.AbstractIdentify {
             readCV("264");
             return false;
         } else if (mfgID == 153) {  // DIY
-            productIDlowest = value;
-            productID = (productIDhighest*256*256*256) + (productIDhigh*256*256) + (productIDlow*256) + productIDlowest
+            productIDhighest = value;
+            productID = (productIDhighest*256*256*256) + (productIDhigh*256*256) + (productIDlow*256) + productIDlowest;
             return true;
         }
         log.error("unexpected step 8 reached with value: {}", value);
--- java/test/jmri/jmrit/decoderdefn/IdentifyDecoderTest.java
+++ java/test/jmri/jmrit/decoderdefn/IdentifyDecoderTest.java
@@ -429,6 +429,69 @@ public class IdentifyDecoderTest {
         jmri.util.JUnitAppender.assertWarnMessage("Stopping due to error: "
                             + p.decodeErrorCode(2));
     }
+    
+    /**
+     * Test TCS decoder with 4-digit productID.
+     * Should pass
+     */
+    @Test
+    public void testIdentifyTCS() {
+        // create our test object
+        IdentifyDecoder i = new IdentifyDecoder(p) {
+            @Override
+            public void done(int mfgID, int modelID, int productID) {
+            }
+
+            @Override
+            public void message(String m) {
+            }
+
+            @Override
+            public void error() {
+            }
+        };
+
+        i.start();
+        Assert.assertEquals("step 1 reads CV ", 8, cvRead);
+        Assert.assertEquals("running after 1 ", true, i.isRunning());
+
+        // simulate CV read complete on CV8, start 7
+        i.programmingOpReply(153, 0);
+        Assert.assertEquals("step 2 reads CV ", 7, cvRead);
+        Assert.assertEquals("running after 2 ", true, i.isRunning());
+
+        // simulate CV read complete on CV7, start 249
+        i.programmingOpReply(5, 0);
+        Assert.assertEquals("step 3 reads CV ", 249, cvRead);
+        Assert.assertEquals("running after 3 ", true, i.isRunning());
+
+        // simulate CV read complete on CV249, start 253
+        i.programmingOpReply(176, 0);
+        Assert.assertEquals("step 5 reads CV ", 253, cvRead);
+        Assert.assertEquals("running after 5 ", true, i.isRunning());
+        
+        // simulate CV read complete on CV253, start 254
+        i.programmingOpReply(1, 0);
+        Assert.assertEquals("step 6 reads CV ", 254, cvRead);
+        Assert.assertEquals("running after 6 ", true, i.isRunning());
+        
+        // simulate CV read complete on CV254, start 255
+        i.programmingOpReply(1, 0);
+        Assert.assertEquals("step 7 reads CV ", 255, cvRead);
+        Assert.assertEquals("running after 7 ", true, i.isRunning());
+        
+        // simulate CV read complete on CV255, start 256
+        i.programmingOpReply(1, 0);
+        Assert.assertEquals("step 8 reads CV ", 256, cvRead);
+        Assert.assertEquals("running after 8 ", true, i.isRunning());
+        
+        // simulate CV read complete on CV256, end
+        i.programmingOpReply(1, 0);
+
+        Assert.assertEquals("found mfg ID ", 153, i.mfgID);
+        Assert.assertEquals("found model ID ", 5, i.modelID);
+        Assert.assertEquals("found product ID ", 16843009, i.productID);
+    }
 
     /**
      * Initialize the system.
