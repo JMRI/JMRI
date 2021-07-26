9dd5b40706b0c9bddd21164a4cc4d2240599e676
TCSDCC-Jordan <jordan@tcsdcc.com>
2021-07-26 11:58:38 -0400

    converted greater than to html friendly encoding

--- java/src/jmri/jmrit/decoderdefn/IdentifyDecoder.java
+++ java/src/jmri/jmrit/decoderdefn/IdentifyDecoder.java
@@ -25,7 +25,7 @@ import org.slf4j.LoggerFactory;
  * {@link #isOptionalCv() isOptionalCv()} as documented below.)</li>
  * <li>TCS: (mfgID == 153) CV249 is physical hardware id, V5 and above use
  * CV253, CV254, CV255, and CV256 to identify specific sound sets and 
- * features. New productID process triggers if (CV249 > 175).</li>
+ * features. New productID process triggers if (CV249 &gt; 175).</li>
  * <li>Zimo: (mfgID == 145) CV250 is ID</li>
  * <li>SoundTraxx: (mfgID == 141, modelID == 70 or 71) CV253 is high byte, CV256
  * is low byte of ID</li>
