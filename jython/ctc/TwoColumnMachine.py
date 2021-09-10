1b9b0d7c428b764581bc4fc3659adc46c9116fdf
Bob Jacobsen <jacobsen@mac.com>
2021-09-09 21:02:29 -0400

    improve CTC sample script

--- jython/ctc/TwoColumnMachine.py
+++ jython/ctc/TwoColumnMachine.py
@@ -12,8 +12,9 @@ from jmri.jmrit.ussctc import *
 # to the layout
 
 # Define objects on layout
-turnouts.provideTurnout("IT1").setUserName("Code Sequencer Start")
-turnouts.provideTurnout("IT2").setUserName("Bell")
+turnouts.provideTurnout("IT1").setUserName("Code Indication Start")
+turnouts.provideTurnout("IT2").setUserName("Code Send Start")
+turnouts.provideTurnout("IT3").setUserName("Bell")
 
 sensors.provideSensor("IS101").setUserName("TC Left Approach")
 sensors.provideSensor("IS102").setUserName("TC Sta 1 OS")
@@ -31,21 +32,21 @@ turnouts.provideTurnout("IT12").setUserName("Sta 1 Code")
 sensors.provideSensor  ("IS12").setUserName("Sta 1 Code")
 turnouts.provideTurnout("IT13").setUserName("Sta 1 TO 1 N")
 sensors.provideSensor  ("IS13").setUserName("Sta 1 TO 1 N")
-turnouts.provideTurnout("IT13").setUserName("Sta 1 TO 1 R")
-sensors.provideSensor  ("IS13").setUserName("Sta 1 TO 1 R")
-turnouts.provideTurnout("IT14").setUserName("Sta 1 Left Approach TC")
-turnouts.provideTurnout("IT15").setUserName("Sta 1 OS TC")
+turnouts.provideTurnout("IT14").setUserName("Sta 1 TO 1 R")
+sensors.provideSensor  ("IS14").setUserName("Sta 1 TO 1 R")
+turnouts.provideTurnout("IT15").setUserName("Sta 1 Left Approach TC")
+turnouts.provideTurnout("IT16").setUserName("Sta 1 OS TC")
 
 turnouts.provideTurnout("IT22").setUserName("Sta 2 Code")
 sensors.provideSensor  ("IS22").setUserName("Sta 2 Code")
 turnouts.provideTurnout("IT23").setUserName("Sta 2 TO 3 N")
 sensors.provideSensor  ("IS23").setUserName("Sta 2 TO 3 N")
-turnouts.provideTurnout("IT23").setUserName("Sta 2 TO 3 R")
-sensors.provideSensor  ("IS23").setUserName("Sta 2 TO 3 R")
-turnouts.provideTurnout("IT14").setUserName("Sta 2 Main TC")
-turnouts.provideTurnout("IT15").setUserName("Sta 2 Siding TC")
-turnouts.provideTurnout("IT16").setUserName("Sta 2 OS TC")
-turnouts.provideTurnout("IT17").setUserName("Sta 2 Right Approach TC")
+turnouts.provideTurnout("IT24").setUserName("Sta 2 TO 3 R")
+sensors.provideSensor  ("IS24").setUserName("Sta 2 TO 3 R")
+turnouts.provideTurnout("IT25").setUserName("Sta 2 Main TC")
+turnouts.provideTurnout("IT26").setUserName("Sta 2 Siding TC")
+turnouts.provideTurnout("IT27").setUserName("Sta 2 OS TC")
+turnouts.provideTurnout("IT28").setUserName("Sta 2 Right Approach TC")
 
 # The core of the sample script starts here, defining & connecting
 # the USS CTC objects to run the panel
@@ -55,7 +56,7 @@ turnouts.provideTurnout("IT17").setUserName("Sta 2 Right Approach TC")
 bell = PhysicalBell("Bell")
 codeline = CodeLine("Code Indication Start", "Code Send Start", "IT101", "IT102", "IT103", "IT104")
 
-# Set up Station 1 - stations are numbered 1, 2, 3 etc. 
+# Set up Station 1 - stations are numbered 1, 2, 3 etc.
 # Station 1 is levers 1 and 2
 
 station = Station("1", codeline, CodeButton("Sta 1 Code", "Sta 1 Code"))
@@ -70,7 +71,7 @@ station.add(TrackCircuitSection("TC Sta 1 OS", "Sta 1 OS TC", station))
 
 station = Station("2", codeline, CodeButton("Sta 2 Code", "Sta 2 Code"))
 
-station.add(TurnoutSection("Sta 2 Layout TO", "Sta 2 TO 3 N", "Sta 2 TO 3 R", "Sta 2 TO 3 N", "Sta 2 TO 3 R", station))   
+station.add(TurnoutSection("Sta 2 Layout TO", "Sta 2 TO 3 N", "Sta 2 TO 3 R", "Sta 2 TO 3 N", "Sta 2 TO 3 R", station))
 station.add(TrackCircuitSection("TC Main", "Sta 2 Main TC", station))
 station.add(TrackCircuitSection("TC Siding", "Sta 2 Siding TC", station))
 station.add(TrackCircuitSection("TC Sta 2 OS", "Sta 2 OS TC", station))
