dc88b973ab76635524154ad9d318c96b518c002c
mstevetodd <mstevetodd@mstevetodd.com>
2021-09-25 19:45:52 -0400

    added descriptions for the example DCCpp messages

--- jython/SendDCCppMessages.py
+++ jython/SendDCCppMessages.py
@@ -23,12 +23,10 @@ dl = tc.addDCCppListener(0xFF,PeerListener())
 
 #-----------------------------------------------------------
 # list of messages to send, examples are from DCC-EX 3.1.7+
-#send("T")
-send("T 23 DCC 5 0")
-send("T 24 SERVO 100 410 205 2")
-send("T 25 VPIN 50")
-send("T 26 VPIN 164")
-send("T 27 DCC 27 1")
-send("T 28 DCC 28 0")
-send("T 29 DCC 29 1")
-send("T")
+send("T 24 SERVO 100 410 205 2") # Add Turnout Servo: ID:24, Pin:100, ThrownPos:410, ClosedPos:205, Profile:2
+send("T 25 VPIN 50")    # Add Turnout Vpin: ID:25, Pin:50
+send("T 26 VPIN 164")   # Add Turnout Vpin: ID:26, Pin:164   
+send("T 27 DCC 27 1")   # Add Turnout DCC: ID:27, Address:27, Subaddr:1
+send("T 28 DCC 28 0")   # Add Turnout DCC: ID:28, Address:28, Subaddr:0
+send("T 29 DCC 29 1")   # Add Turnout DCC: ID:29, Address:29, Subaddr:1
+send("T")               # request list of Turnout definitions
