fc88dc409cf24aca50a4fa13fd71733a52181fd5
mstevetodd <mstevetodd@mstevetodd.com>
2021-09-29 20:45:09 -0400

    add tests for new checks

--- java/src/jmri/jmrix/dccpp/swing/ConfigBaseStationFrame.java
+++ java/src/jmri/jmrix/dccpp/swing/ConfigBaseStationFrame.java
@@ -295,6 +295,10 @@ public class ConfigBaseStationFrame extends JmriJFrame implements DCCppListener 
         tabbedPane.setMnemonicAt(4, Mnemonics.get("OutputTab")); // NOI18N
         cTab = CurrentTab.SENSOR;
         tabbedPane.setSelectedIndex(0);
+        if (!tc.getCommandStation().isServoTurnoutCreationSupported()) {
+            tabbedPane.setEnabledAt(2, false);
+            tabbedPane.setEnabledAt(3, false);
+        }
         tabbedPane.addChangeListener((ChangeEvent e) -> {
             switch (tabbedPane.getSelectedIndex()) { // set button text and tooltips for selected tabs
                 case 4:
