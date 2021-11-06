bf75ea05c6a9d0a950901baaa89d6fd9f7820e0d
pete_ <pete_@DESKTOP-O0N9TLF.attlocal.net>
2021-11-06 11:49:34 -0700

    set speed assistance to 0

--- java/test/jmri/jmrit/logix/SCWarrantTest.java
+++ java/test/jmri/jmrit/logix/SCWarrantTest.java
@@ -127,6 +127,7 @@ public class SCWarrantTest extends WarrantTest {
         JUnitUtil.initRosterConfigManager();
 
         // setup the sc warrant preliminaries.
+        WarrantPreferences.getDefault().setSpeedAssistance(0);
         _OBlockMgr = InstanceManager.getDefault(OBlockManager.class);
         bWest = _OBlockMgr.createNewOBlock("OB1", "West");
         bEast = _OBlockMgr.createNewOBlock("OB2", "East");
