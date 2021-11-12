62de964b7cf650cd8eee1978e9f22ef6eca6c70e
Bob Jacobsen <jacobsen@mac.com>
2021-11-12 07:25:18 -0500

    full class name

--- java/test/jmri/jmrit/logixng/actions/swing/ActionLightSwingTest.java
+++ java/test/jmri/jmrit/logixng/actions/swing/ActionLightSwingTest.java
@@ -88,7 +88,7 @@ public class ActionLightSwingTest extends SwingConfiguratorInterfaceTestBase {
     @After
     public void tearDown() {
         // Java 11 integration temporary - clear messages to get JUnit 5 traceback
-        jmri.util.JUnitAppender.clearBacklog(Level.ERROR);  // REMOVE THIS!!!
+        jmri.util.JUnitAppender.clearBacklog(org.apache.log4j.Level.ERROR);  // REMOVE THIS!!!
 
         jmri.jmrit.logixng.util.LogixNG_Thread.stopAllLogixNGThreads();
         JUnitUtil.tearDown();
