1cc2707767a2d15f1e63f80f22cb1a308f6fc4c5
Daniel <db123@bergqvist.se>
2021-11-14 07:22:50 +0100

    Let Jython scripts get access to local variables by 'symbolTable'

--- java/src/jmri/jmrit/logixng/expressions/ExpressionScript.java
+++ java/src/jmri/jmrit/logixng/expressions/ExpressionScript.java
@@ -298,6 +298,9 @@ public class ExpressionScript extends AbstractDigitalExpression
         bindings.put("stringActions", InstanceManager.getNullableDefault(StringActionManager.class));
         bindings.put("stringExpressions", InstanceManager.getNullableDefault(StringExpressionManager.class));
         
+        SymbolTable symbolTable = getConditionalNG().getSymbolTable();
+        bindings.put("symbolTable", symbolTable);    // Give the script access to the local variable 'symbolTable'
+        
         bindings.put("result", result);     // Give the script access to the local variable 'result'
         
         ThreadingUtil.runOnLayoutWithJmriException(() -> {
