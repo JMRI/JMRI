e61c6231a79b0fc4b9dfd03bab56c802b259f8c7
Bob Jacobsen <jacobsen@mac.com>
2021-11-15 07:58:01 -0500

    initial TypeScript Ant support

---
+++ web/ts/HelloWorld.ts
@@ -0,0 +1,6 @@
+// This TypeSCript (.ts) file is part of JMRI.  If you see this comment in a JavaScript (.js)
+// file, the file has been automatically generated from a TypeScript file in the web/ts tree;
+// in that case, if you modify the .js file directly your work will be lost.
+
+let message: string = 'Hello, World!';
+console.log(message);
