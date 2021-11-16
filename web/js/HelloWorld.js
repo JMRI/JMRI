7fba541a68e65ec28b1f3704c39425c26a1b2e24
Bob Jacobsen <jacobsen@mac.com>
2021-11-16 09:06:23 -0500

    fix comment typo

--- web/js/HelloWorld.js
+++ web/js/HelloWorld.js
@@ -1,5 +1,5 @@
 "use strict";
-// This TypeSCript (.ts) file is part of JMRI.  If you see this comment in a JavaScript (.js)
+// This TypeScript (.ts) file is part of JMRI.  If you see this comment in a JavaScript (.js)
 // file, the file has been automatically generated from a TypeScript file in the web/ts tree;
 // in that case, if you modify the .js file directly your work will be lost.
 var message = 'Hello, World!';
--- web/ts/HelloWorld.ts
+++ web/ts/HelloWorld.ts
@@ -1,4 +1,4 @@
-// This TypeSCript (.ts) file is part of JMRI.  If you see this comment in a JavaScript (.js)
+// This TypeScript (.ts) file is part of JMRI.  If you see this comment in a JavaScript (.js)
 // file, the file has been automatically generated from a TypeScript file in the web/ts tree;
 // in that case, if you modify the .js file directly your work will be lost.
 
