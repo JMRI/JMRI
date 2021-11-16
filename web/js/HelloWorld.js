a852087e276257ba79e464d02fbd6714cef85497
Bob Jacobsen <jacobsen@mac.com>
2021-11-16 09:08:42 -0500

    use es6 target

--- web/js/HelloWorld.js
+++ web/js/HelloWorld.js
@@ -2,5 +2,5 @@
 // This TypeScript (.ts) file is part of JMRI.  If you see this comment in a JavaScript (.js)
 // file, the file has been automatically generated from a TypeScript file in the web/ts tree;
 // in that case, if you modify the .js file directly your work will be lost.
-var message = 'Hello, World!';
+let message = 'Hello, World!';
 console.log(message);
--- web/ts/README.md
+++ web/ts/README.md
@@ -1,1 +1,4 @@
 This directory contains JavaScript .js and TypeScript .ts files.  Modifications here will result in updates in the runtime ../js directory when rebuilt.
+
+Some files uses classes introduced in JavaScript [ES6](https://www.w3schools.com/js/js_es6.asp), so we use "es6" as the compilation target.
+
