c75bdf6863c37a14cc32d6967134747802a93633
Bob Jacobsen <jacobsen@mac.com>
2021-11-17 18:32:30 -0500

    stop clock during test

--- java/test/jmri/VariableLightTest.java
+++ java/test/jmri/VariableLightTest.java
@@ -14,43 +14,49 @@ public class VariableLightTest {
     @Test
     public void testIsConsistentState() {
         MyVariableLight light = new MyVariableLight("IL1");
-        
+
         light.setState(VariableLight.ON);
         Assert.assertTrue("State is consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.OFF);
         Assert.assertTrue("State is consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.INTERMEDIATE);
         Assert.assertTrue("State is consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.TRANSITIONINGTOFULLON);
         Assert.assertFalse("State is not consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.TRANSITIONINGHIGHER);
         Assert.assertFalse("State is not consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.TRANSITIONINGLOWER);
         Assert.assertFalse("State is not consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.TRANSITIONINGTOFULLOFF);
         Assert.assertFalse("State is not consistent", light.isConsistentState());
-        
+
         light.setState(VariableLight.TRANSITIONING);
         Assert.assertFalse("State is not consistent", light.isConsistentState());
     }
-    
+
+
+    private jmri.Timebase clock;
+
     @Before
     public void setUp() {
-          jmri.util.JUnitUtil.setUp();
-    }
+        jmri.util.JUnitUtil.setUp();
+        clock = jmri.InstanceManager.getDefault(jmri.Timebase.class);
+        clock.setRun(false);
+        clock.setTime(java.time.Instant.EPOCH);  // just a specific time
+     }
 
     @After
     public void tearDown() {
           jmri.util.JUnitUtil.tearDown();
     }
 
-    
+
     private static class MyVariableLight extends AbstractVariableLight {
 
         public MyVariableLight(String systemName) {
@@ -61,7 +67,7 @@ public class VariableLightTest {
         public void setState(int newState) {
             mState = newState;
         }
-        
+
         @Override
         protected void sendIntensity(double intensity) {
             throw new UnsupportedOperationException("Not supported");
@@ -76,7 +82,7 @@ public class VariableLightTest {
         protected int getNumberOfSteps() {
             throw new UnsupportedOperationException("Not supported");
         }
-        
+
     }
-    
+
 }
