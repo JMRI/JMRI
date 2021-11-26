fc2b985a3fbbf149f1f4807cec6a46cd2109c64c
steve young <39652002+icklesteve@users.noreply.github.com>
2021-11-26 09:28:02 +0000

    Default IDTag - Improve date format parse warning text

--- java/src/jmri/implementation/DefaultIdTag.java
+++ java/src/jmri/implementation/DefaultIdTag.java
@@ -50,9 +50,7 @@ public class DefaultIdTag extends AbstractIdTag {
         Date oldWhen = this.whenLastSeen;
         this.whereLastSeen = r;
         if (r != null) {
-            this.whenLastSeen = InstanceManager.getDefault(IdTagManager.class).isFastClockUsed()
-                    ? InstanceManager.getDefault(ClockControl.class).getTime()
-                    : Calendar.getInstance().getTime();
+            this.whenLastSeen = getDateNow();
         } else {
             this.whenLastSeen = null;
         }
@@ -61,11 +59,21 @@ public class DefaultIdTag extends AbstractIdTag {
         firePropertyChange("whenLastSeen", oldWhen, this.whenLastSeen);    // NOI18N
     }
 
+    private Date getDateNow() {
+        return InstanceManager.getDefault(IdTagManager.class).isFastClockUsed()
+            ? InstanceManager.getDefault(ClockControl.class).getTime()
+            : Calendar.getInstance().getTime();
+    }
+
+    private String getDateElementText(Date date) {
+        return DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM).format(date);
+    }
+
     private void setCurrentState(int state) {
         try {
             setState(state);
         } catch (JmriException ex) {
-            log.warn("Problem setting state of IdTag {}", getSystemName());
+            log.warn("Problem setting state of IdTag {} {}", getSystemName(),ex.getMessage());
         }
     }
 
@@ -98,7 +106,7 @@ public class DefaultIdTag extends AbstractIdTag {
             e.addContent(new Element("whereLastSeen").addContent(whereLast.getSystemName())); // NOI18N
         }
         if (this.getWhenLastSeen() != null && storeState) {
-            e.addContent(new Element("whenLastSeen").addContent(DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM).format(this.getWhenLastSeen()))); // NOI18N
+            e.addContent(new Element("whenLastSeen").addContent(getDateElementText(this.getWhenLastSeen()))); // NOI18N
         }
         return e;
     }
@@ -128,7 +136,8 @@ public class DefaultIdTag extends AbstractIdTag {
                 try {
                     this.whenLastSeen = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.MEDIUM).parse(e.getChild("whenLastSeen").getText()); // NOI18N
                 } catch (ParseException ex) {
-                    log.warn("Error parsing when last seen: {}", ex);
+                    log.warn("Error parsing when last seen: {}", ex.getMessage());
+                    log.warn("Expected format is \"{}\" ",getDateElementText(getDateNow()));
                 }
             }
         } else {
