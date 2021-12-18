82fb7354a6b3939c67bfe4f0e23efb08b9f5c933
Daniel <db123@bergqvist.se>
2021-12-19 00:12:38 +0100

    Add property change listeners to clipboard window when cut/copy

--- java/src/jmri/jmrit/logixng/tools/swing/TreeEditor.java
+++ java/src/jmri/jmrit/logixng/tools/swing/TreeEditor.java
@@ -41,9 +41,10 @@ public class TreeEditor extends TreeViewer {
     public enum EnableExecuteEvaluate { EnableExecuteEvaluate, DisableExecuteEvaluate }
 
 
-    private final LogixNGPreferences _prefs = InstanceManager.getDefault(LogixNGPreferences.class);
+    // There should only be one clipboard editor open at any time so this is static
+    private static ClipboardEditor _clipboardEditor = null;
 
-    ClipboardEditor _clipboardEditor = null;
+    private final LogixNGPreferences _prefs = InstanceManager.getDefault(LogixNGPreferences.class);
 
     private JDialog _renameSocketDialog = null;
     private JDialog _selectItemTypeDialog = null;
@@ -1627,6 +1628,10 @@ public class TreeEditor extends TreeViewer {
                                         JOptionPane.ERROR_MESSAGE);
                             }
                             ThreadingUtil.runOnGUIEventually(() -> {
+                                _treePane._femaleRootSocket.forEntireTree((Base b) -> {
+                                    b.removePropertyChangeListener(_treePane);
+                                    b.addPropertyChangeListener(_clipboardEditor._treePane);
+                                });
                                 _treePane._femaleRootSocket.registerListeners();
                                 _treePane.updateTree(_currentFemaleSocket, _currentPath.getPath());
                             });
@@ -1668,6 +1673,12 @@ public class TreeEditor extends TreeViewer {
                                             JOptionPane.ERROR_MESSAGE);
                                 });
                             }
+                            ThreadingUtil.runOnGUIEventually(() -> {
+                                _treePane._femaleRootSocket.forEntireTree((Base b) -> {
+                                    b.removePropertyChangeListener(_treePane);
+                                    b.addPropertyChangeListener(_clipboardEditor._treePane);
+                                });
+                            });
                         });
 
                         _treePane._femaleRootSocket.registerListeners();
