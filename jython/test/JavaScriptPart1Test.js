eaeef22906943821202c9b41e046862b5897f51f
Bob Jacobsen <jacobsen@mac.com>
2021-12-27 21:39:34 -0500

    js 2-part test; persitance not working

--- jython/test/JavaScriptPart1Test.js
+++ jython/test/JavaScriptPart1Test.js
@@ -2,4 +2,7 @@
 var Turnout = Java.type("jmri.Turnout")
 var logger = Java.type("org.apache.log4j.Logger").getLogger("JavaScriptTest.js")
 
-logger.warn("JavaScriptTest: Turnout.THROWN is "+Turnout.THROWN+" (WARN OK here)")
+logger.warn("JavaScriptPart1Test: Turnout.THROWN is "+Turnout.THROWN+" (WARN OK here)")
+
+// set for persistence check
+var jsPersistanceCheck = 31
