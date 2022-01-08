e86c49b15e148a7be7a020e46ff1841e40b372dd
Bob Jacobsen <jacobsen@mac.com>
2022-01-07 19:15:41 -0500

    pass-thru class

---
+++ java/src/jmri/jmrit/jython/InputWindowAction.java
@@ -0,0 +1,12 @@
+package jmri.jmrit.jython;
+
+/**
+ * This class is left in place so that existing preferences will still work until they
+ * are migrated. Do not use.
+ *
+ * @author Bob Jacobsen Copyright (C) 2004, 2022
+ * @deprecated For removal after JMRI 5.2
+ */
+@Deprecated(forRemoval=true)
+public class InputWindowAction extends jmri.script.swing.InputWindowAction {
+}
