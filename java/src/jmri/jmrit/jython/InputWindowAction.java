5a4e60d02c5af3303e8c785a1ab4895b46ad984d
Bob Jacobsen <jacobsen@mac.com>
2022-01-07 22:39:56 -0500

    spotbugs

--- java/src/jmri/jmrit/jython/InputWindowAction.java
+++ java/src/jmri/jmrit/jython/InputWindowAction.java
@@ -8,5 +8,8 @@ package jmri.jmrit.jython;
  * @deprecated For removal after JMRI 5.2
  */
 @Deprecated(forRemoval=true)
+@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value="NM_SAME_SIMPLE_NAME_AS_SUPERCLASS",
+            justification="Deliberate shadowing to migrate preferences files")
 public class InputWindowAction extends jmri.script.swing.InputWindowAction {
+    // everything is deferred to jmri.script.swing.InputWindowAction
 }
