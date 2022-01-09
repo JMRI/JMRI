581e77cad0b8f64c06114f0c211d7f1f36626185
Bob Jacobsen <jacobsen@mac.com>
2022-01-09 11:48:20 -0500

    new ScriptEngineSelector used in InputWindow

---
+++ java/src/jmri/script/swing/ScriptEngineSelector.java
@@ -0,0 +1,93 @@
+package jmri.script.swing;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.MissingResourceException;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptException;
+import javax.swing.JComboBox;
+
+import jmri.script.JmriScriptEngineManager;
+
+/**
+ * A JComboBox for selecting a valid scripting engine.
+ *
+ * Persistence of settings, if desired, should be handled in the using class.
+ *
+ * @author Bob Jacobsen Copyright (C) 2004, 2021, 2022
+ */
+public class ScriptEngineSelector extends JComboBox<String> {
+
+    /**
+     * Obtain one of these; direct calls to ctor are not permitted
+     */
+    @Nonnull
+    public static ScriptEngineSelector getScriptEngineSelector() {
+
+        List<String> languageNames = new ArrayList<>();
+        List<String> languageIDs = new ArrayList<>();
+
+        // get list of language names (to display) and IDs (for call)
+        JmriScriptEngineManager.getDefault().getManager().getEngineFactories().stream().forEach((ScriptEngineFactory factory) -> {
+            String version = factory.getEngineVersion();
+            if (version != null) {
+                String name = fileForLanguage(factory.getLanguageName());
+                if (!languageNames.contains(name)) {
+                    languageNames.add(name);
+                    languageIDs.add(factory.getLanguageName());
+                }
+            }
+        });
+
+        String[] nameArray = languageNames.toArray(new String[languageNames.size()]);
+
+        ScriptEngineSelector retval = new ScriptEngineSelector(nameArray);
+        retval.languageNames = languageNames;
+        retval.languageIDs = languageIDs;
+
+        return retval;
+    }
+
+    /**
+     * Get the currently selected engine
+     */
+    @CheckForNull
+    public ScriptEngine getEngine() {
+        String language = languageIDs.get(getSelectedIndex());
+        try {
+            return JmriScriptEngineManager.getDefault().getEngineByName(language);
+        } catch (ScriptException ex) {
+            log.warn("could not allocate script engine", ex);
+            return null;
+        }
+    }
+
+    private ScriptEngineSelector() {
+        super();
+    }
+
+    private ScriptEngineSelector(String[] arr) {
+        super(arr);
+    }
+
+    List<String> languageNames = new ArrayList<>();
+    List<String> languageIDs = new ArrayList<>();
+
+    private static String fileForLanguage(String language) {
+        try {
+            return Bundle.getMessage(language);
+        } catch (MissingResourceException ex) {
+            log.warn("Translation not found for language \"{}\"", language);
+            if (!language.endsWith(Bundle.getMessage("files"))) { // NOI18N
+                return language + " " + Bundle.getMessage("files");
+            }
+            return language;
+        }
+    }
+
+    // initialize logging
+    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ScriptEngineSelector.class);
+}
