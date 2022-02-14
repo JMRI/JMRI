1cce4fcec6702199cd484b4a224de837f2f3811c
Andrew Crosland <andrew@crosland.me.uk>
2022-02-14 10:28:43 +0000

    Fix for issue #10669

--- java/test/jmri/jmrix/can/cbus/swing/modules/CmdStaFlagsTest.java
+++ java/test/jmri/jmrix/can/cbus/swing/modules/CmdStaFlagsTest.java
@@ -7,17 +7,21 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 /**
- * Test simple functioning of CbusModulesCommon
+ * Test simple functioning of Cbus Modules Common Code
  *
- * @author Andrew Crosland Copyright (C) 2021
+ * @author Andrew Crosland Copyright (C) 2022
  */
-public class CbusModulesCommonTest {
+public class CmdStaFlagsTest {
+    
+    protected UpdateNV _update;
+    String [] flags = {"flag one", "flag two", "flag three", "flag four", "flag five", "flag six", "flag seven", "flag eight"};
+    String [] tooltips = {"tt one", "tt two", "tt three", "tt four", "tt five", "tt six", "tt seven", "tt eight"};
     
     @DisabledIfSystemProperty(named ="java.awt.headless", matches ="true")
     @Test
     public void testCtor() {
 
-        CbusModulesCommon t = new CbusModulesCommon();
+        CmdStaFlags t = new CmdStaFlags(3, "Title", flags, tooltips, _update);
         Assert.assertNotNull("exists",t);
     }
     
