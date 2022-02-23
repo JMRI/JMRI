3593ab45bd8683586e66a8b01772dad0b96551bf
Bob Jacobsen <jacobsen@mac.com>
2022-02-23 16:46:54 -0500

    thread cleanup

--- jython/test/ThreadingExampleTest.py
+++ jython/test/ThreadingExampleTest.py
@@ -4,4 +4,11 @@ print ("The ThreadingExample script will print a line about running on the GUI t
 execfile("jython/ThreadingExample.py")
 
 # just confirm that this runs OK in headless mode (when graphical, it prompts)
-ThreadingExample().start()
+te = ThreadingExample()
+te.start()
+
+# time to run
+from time import sleep
+sleep(0.020)
+
+# stops itself
