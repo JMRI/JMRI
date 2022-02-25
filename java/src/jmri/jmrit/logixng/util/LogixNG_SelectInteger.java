56f90ecd38bbab83763ec85ea62a89d9e63894b7
Daniel <db123@bergqvist.se>
2022-02-25 02:05:33 +0100

    Refactor LogixNG action Timeout

--- java/src/jmri/jmrit/logixng/util/LogixNG_SelectInteger.java
+++ java/src/jmri/jmrit/logixng/util/LogixNG_SelectInteger.java
@@ -14,39 +14,34 @@ import jmri.jmrit.logixng.util.parser.RecursiveDescentParser;
 import jmri.util.TypeConversionUtil;
 
 /**
- * Select an enum value for LogixNG actions and expressions.
- *
- * @param <E> the type of enum
+ * Select an integer for LogixNG actions and expressions.
  *
  * @author Daniel Bergqvist (C) 2022
  */
-public class LogixNG_SelectEnum<E extends Enum<?>> {
+public class LogixNG_SelectInteger {
 
     private final AbstractBase _base;
     private final InUse _inUse;
-    private final E[] _enumArray;
     private final LogixNG_SelectTable _selectTable;
 
     private NamedBeanAddressing _addressing = NamedBeanAddressing.Direct;
-    private E _enum;
+    private int _value;
     private String _reference = "";
     private String _localVariable = "";
     private String _formula = "";
     private ExpressionNode _expressionNode;
 
 
-    public LogixNG_SelectEnum(AbstractBase base, E[] enumArray, E initialEnum) {
+    public LogixNG_SelectInteger(AbstractBase base) {
         _base = base;
         _inUse = () -> true;
-        _enumArray = enumArray;
-        _enum = initialEnum;
         _selectTable = new LogixNG_SelectTable(_base, _inUse);
     }
 
 
-    public void copy(LogixNG_SelectEnum<E> copy) throws ParserException {
+    public void copy(LogixNG_SelectInteger copy) throws ParserException {
         copy.setAddressing(_addressing);
-        copy.setEnum(_enum);
+        copy.setValue(_value);
         copy.setLocalVariable(_localVariable);
         copy.setReference(_reference);
         copy.setFormula(_formula);
@@ -62,20 +57,13 @@ public class LogixNG_SelectEnum<E extends Enum<?>> {
         return _addressing;
     }
 
-    public void setEnum(@Nonnull E e) {
+    public void setValue(@Nonnull int value) {
         _base.assertListenersAreNotRegistered(log, "setEnum");
-        _enum = e;
+        _value = value;
     }
 
-    public E getEnum() {
-        return _enum;
-    }
-
-    public E getEnum(String name) {
-        for (E e : _enumArray) {
-            if (e.name().equals(name)) return e;
-        }
-        return null;
+    public int getValue() {
+        return _value;
     }
 
     public void setReference(@Nonnull String reference) {
@@ -121,43 +109,39 @@ public class LogixNG_SelectEnum<E extends Enum<?>> {
         return _selectTable;
     }
 
-    public E evaluateEnum(ConditionalNG conditionalNG) throws JmriException {
+    public int evaluateValue(ConditionalNG conditionalNG) throws JmriException {
 
         if (_addressing == NamedBeanAddressing.Direct) {
-            return _enum;
+            return _value;
         } else {
-            String name;
+            Object val;
 
             switch (_addressing) {
                 case Reference:
-                    name = ReferenceUtil.getReference(
+                    val = ReferenceUtil.getReference(
                             conditionalNG.getSymbolTable(), _reference);
                     break;
 
                 case LocalVariable:
                     SymbolTable symbolNamedBean = conditionalNG.getSymbolTable();
-                    name = TypeConversionUtil
-                            .convertToString(symbolNamedBean.getValue(_localVariable), false);
+                    val = symbolNamedBean.getValue(_localVariable);
                     break;
 
                 case Formula:
-                    name = _expressionNode  != null
-                            ? TypeConversionUtil.convertToString(
-                                    _expressionNode.calculate(
-                                            conditionalNG.getSymbolTable()), false)
+                    val = _expressionNode != null
+                            ? _expressionNode.calculate(conditionalNG.getSymbolTable())
                             : null;
                     break;
 
                 case Table:
-                    name = TypeConversionUtil.convertToString(
-                            _selectTable.evaluateTableData(conditionalNG), false);
+                    val = _selectTable.evaluateTableData(conditionalNG);
                     break;
 
                 default:
                     throw new IllegalArgumentException("invalid _addressing state: " + _addressing.name());
             }
 
-            return getEnum(name);
+            return (int) TypeConversionUtil.convertToLong(val);
         }
     }
 
@@ -165,7 +149,7 @@ public class LogixNG_SelectEnum<E extends Enum<?>> {
         String enumName;
         switch (_addressing) {
             case Direct:
-                enumName = Bundle.getMessage(locale, "AddressByDirect", _enum.toString());
+                enumName = Bundle.getMessage(locale, "AddressByDirect", _value);
                 break;
 
             case Reference:
@@ -195,5 +179,5 @@ public class LogixNG_SelectEnum<E extends Enum<?>> {
         return enumName;
     }
 
-    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogixNG_SelectEnum.class);
+    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogixNG_SelectInteger.class);
 }
