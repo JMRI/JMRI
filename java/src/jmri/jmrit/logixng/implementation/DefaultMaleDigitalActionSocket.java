9a4738319a330911d979de7c9c4ae038b10093dd
Daniel <db123@bergqvist.se>
2022-02-26 22:55:08 +0100

    Add temporary debug statements

--- java/src/jmri/jmrit/logixng/implementation/DefaultMaleDigitalActionSocket.java
+++ java/src/jmri/jmrit/logixng/implementation/DefaultMaleDigitalActionSocket.java
@@ -38,6 +38,11 @@ public class DefaultMaleDigitalActionSocket
 
         ConditionalNG conditionalNG = getConditionalNG();
 
+        log.warn("");
+        log.warn("this: {}", this);
+        log.warn("this.getConditionalNG(): {}", this.getConditionalNG());
+        log.warn("conditionalNG: {}", conditionalNG);
+        log.warn("conditionalNG.getStack(): {}", conditionalNG.getStack());
         int currentStackPos = conditionalNG.getStack().getCount();
 
         try {
