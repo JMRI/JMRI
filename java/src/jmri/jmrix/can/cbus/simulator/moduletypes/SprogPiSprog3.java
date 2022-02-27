3b08030b9f8ee1d637d02f564364919f04c11200
steve young <39652002+icklesteve@users.noreply.github.com>
2022-02-27 15:42:40 +0000

    Update SprogPiSprog3.java
    add NVs

--- java/src/jmri/jmrix/can/cbus/simulator/moduletypes/SprogPiSprog3.java
+++ java/src/jmri/jmrix/can/cbus/simulator/moduletypes/SprogPiSprog3.java
@@ -28,33 +28,58 @@ public class SprogPiSprog3 extends CbusSimulatedModuleProvider {
     }
 
     @Override
-    public void setDummyNodeParameters(@Nonnull CbusDummyNode node) {
+    public void configureDummyNode(@Nonnull CbusDummyNode node) {
         int[] _params = new int[]{ 
-        20, /* 0 num parameters   */
-        SPROG_DCC, /* 1 manufacturer ID   */
-        'f', /* 2 Minor code version   */
-        1, /* 3 Manufacturer module identifier   */
-        0, /* 4 Number of supported events   */
-        0, /* 5 Number of Event Variables per event   */
-        1, /* 6 Number of Node Variables   */
-        3, /* 7 Major version   */
-        0, /* 8 Node flags   */ 
-        25, /* 9 Processor type   */
-        0, /* 10 Bus type   */
-        0, /* 11 load address, 1/4 bytes   */
-        8, /* 12 load address, 2/4 bytes   */
-        4, /* 13 load address, 3/4 bytes   */
-        0, /* 14 load address, 4/4 bytes   */
-        0, /* 15 CPU manufacturer's id 1/4  */
-        0, /* 16 CPU manufacturer's id 2/4  */
-        0, /* 17 CPU manufacturer's id 3/4  */
-        0, /* 18 CPU manufacturer's id 4/4  */
-        1, /* 19 CPU manufacturer code   */
-        1, /* 20 Beta revision   */
+            20, /* 0 num parameters   */
+            SPROG_DCC, /* 1 manufacturer ID   */
+            'f', /* 2 Minor code version   */
+            1, /* 3 Manufacturer module identifier   */
+            0, /* 4 Number of supported events   */
+            0, /* 5 Number of Event Variables per event   */
+            21, /* 6 Number of Node Variables   */
+            3, /* 7 Major version   */
+            0, /* 8 Node flags   */ 
+            25, /* 9 Processor type   */
+            0, /* 10 Bus type   */
+            0, /* 11 load address, 1/4 bytes   */
+            8, /* 12 load address, 2/4 bytes   */
+            4, /* 13 load address, 3/4 bytes   */
+            0, /* 14 load address, 4/4 bytes   */
+            0, /* 15 CPU manufacturer's id 1/4  */
+            0, /* 16 CPU manufacturer's id 2/4  */
+            0, /* 17 CPU manufacturer's id 3/4  */
+            0, /* 18 CPU manufacturer's id 4/4  */
+            1, /* 19 CPU manufacturer code   */
+            1, /* 20 Beta revision   */
         };
-
         node.getNodeParamManager().setParameters(_params);
-        node.getNodeNvManager().setNVs( new int[]{ 1 , 0 } ); // 1 NV, NV1 set at 0
+
+        int[] _nvArray = new int[]{
+            21, // 21 NV's, defined in position 0
+            0, // 1 command station number
+            0x76, // 2 User flags
+            0, // 3 Operations flags
+            0, // 4 Debug flags
+            0, // 5 Prog track power mode
+            250, // 6 Prog track trip limit
+            0, // 7 Read only vinsense
+            0, // 8 Read only isense_main
+            0x02, // 9 DCC accessory packet repeat count
+            0x00, // 10 Multimeter enable
+            0, // 11 NN to map to DCC hi byte
+            0, // 12 NN to map to DCC lo byte
+            250, // 13 Main track trip limit
+            0, // 14 Read only isense_prog
+            0, // 15 Read only main current sense high water mark
+            0, // 16 Read only prog current sense high water mark
+            0, // 17 Setup mode
+            114, // 18 Default CAN ID
+            0xFF, // 19 Node number hi byte
+            0xFE, // 20 Node number lo byte
+            16, // 21 Number of pre-amble bits
+        }; 
+        node.getNodeNvManager().setNVs( _nvArray );
+
         node.setNodeNameFromName("PiSPRG3");
     }
 
