57b649258c804a5ad1c2190b5a410ee3ba6bffaf
dsand47 <gh@dfm.sandsys.org>
2022-03-03 23:04:54 -0600

    Implement the ability to copy a route when creating a new route

--- java/src/jmri/jmrit/beantable/routetable/AbstractRouteAddEditFrame.java
+++ java/src/jmri/jmrit/beantable/routetable/AbstractRouteAddEditFrame.java
@@ -756,6 +756,94 @@ public abstract class AbstractRouteAddEditFrame extends JmriJFrame {
         }
     }
 
+    /**
+     * Populate the page fields.  The route names are not included since they are handled
+     * by the Edit or Add actions.
+     * <p>
+     * The route is either the route being edited or a source route for doing a copy during
+     * the add action.
+     *
+     * @param route The route that contains the content.
+     */
+    protected void setPageContent(Route route) {
+        // set up Turnout list for this route
+        int setRow = 0;
+        for (int i = _turnoutList.size() - 1; i >= 0; i--) {
+            RouteTurnout turnout = _turnoutList.get(i);
+            String tSysName = turnout.getSysName();
+            if (route.isOutputTurnoutIncluded(tSysName)) {
+                turnout.setIncluded(true);
+                turnout.setState(route.getOutputTurnoutSetState(tSysName));
+                setRow = i;
+            } else {
+                turnout.setIncluded(false);
+                turnout.setState(Turnout.CLOSED);
+            }
+        }
+        setRow -= 1;
+        if (setRow < 0) {
+            setRow = 0;
+        }
+        _routeTurnoutScrollPane.getVerticalScrollBar().setValue(setRow * ROW_HEIGHT);
+        _routeTurnoutModel.fireTableDataChanged();
+
+        // set up Sensor list for this route
+        for (int i = _sensorList.size() - 1; i >= 0; i--) {
+            RouteSensor sensor = _sensorList.get(i);
+            String tSysName = sensor.getSysName();
+            if (route.isOutputSensorIncluded(tSysName)) {
+                sensor.setIncluded(true);
+                sensor.setState(route.getOutputSensorSetState(tSysName));
+                setRow = i;
+            } else {
+                sensor.setIncluded(false);
+                sensor.setState(Sensor.INACTIVE);
+            }
+        }
+        setRow -= 1;
+        if (setRow < 0) {
+            setRow = 0;
+        }
+        _routeSensorScrollPane.getVerticalScrollBar().setValue(setRow * ROW_HEIGHT);
+        _routeSensorModel.fireTableDataChanged();
+
+        // get Sound and  Script file names
+        scriptFile.setText(route.getOutputScriptName());
+        soundFile.setText(route.getOutputSoundName());
+
+        // get Turnout Aligned sensor
+        turnoutsAlignedSensor.setSelectedItem(route.getTurnoutsAlgdSensor());
+
+        // set up Control Sensors if there are any
+        Sensor[] temNames = new Sensor[Route.MAX_CONTROL_SENSORS];
+        int[] temModes = new int[Route.MAX_CONTROL_SENSORS];
+        for (int k = 0; k < Route.MAX_CONTROL_SENSORS; k++) {
+            temNames[k] = route.getRouteSensor(k);
+            temModes[k] = route.getRouteSensorMode(k);
+        }
+        sensor1.setSelectedItem(temNames[0]);
+        setSensorModeBox(temModes[0], sensor1mode);
+
+        sensor2.setSelectedItem(temNames[1]);
+        setSensorModeBox(temModes[1], sensor2mode);
+
+        sensor3.setSelectedItem(temNames[2]);
+        setSensorModeBox(temModes[2], sensor3mode);
+
+        // set up Control Turnout if there is one
+        cTurnout.setSelectedItem(route.getCtlTurnout());
+
+        setTurnoutModeBox(route.getControlTurnoutState(), cTurnoutStateBox);
+
+        // set up Lock Control Turnout if there is one
+        cLockTurnout.setSelectedItem(route.getLockCtlTurnout());
+
+        setTurnoutModeBox(route.getLockControlTurnoutState(), cLockTurnoutStateBox);
+
+        // set up additional route specific Delay
+        timeDelay.setValue(route.getRouteCommandDelay());
+    }
+
     private void clearPage() {
         _systemName.setText("");
         _userName.setText("");
