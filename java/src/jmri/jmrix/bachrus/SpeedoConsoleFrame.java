c1cb8767b112f0e2e77bf22bf36eb7fbd52ef7d6
Bob Jacobsen <jacobsen@mac.com>
2022-03-08 18:05:41 -0500

    add radio button group; handle connection name

--- java/src/jmri/jmrix/bachrus/SpeedoConsoleFrame.java
+++ java/src/jmri/jmrix/bachrus/SpeedoConsoleFrame.java
@@ -143,6 +143,7 @@ public class SpeedoConsoleFrame extends JmriJFrame implements SpeedoListener,
     protected JRadioButton numButton = new JRadioButton(Bundle.getMessage("Numeric"));
     protected JRadioButton dialButton = new JRadioButton(Bundle.getMessage("Dial"));
     protected SpeedoDial speedoDialDisplay = new SpeedoDial();
+    protected ButtonGroup dirGroup = new ButtonGroup();
     protected JRadioButton dirFwdButton = new JRadioButton(Bundle.getMessage("Forward"));
     protected JRadioButton dirRevButton = new JRadioButton(Bundle.getMessage("Reverse"));
     protected JRadioButton toggleGridButton = new JRadioButton(Bundle.getMessage("ToggleGrid"));
@@ -369,7 +370,7 @@ public class SpeedoConsoleFrame extends JmriJFrame implements SpeedoListener,
         formatter = new SimpleDateFormat("EEE d MMM yyyy", Locale.getDefault());
         today = new Date();
         result = formatter.format(today);
-        String annotate = "Bachrus MTS-DCC " + Bundle.getMessage("ProfileFor") + " "
+        String annotate = Bundle.getMessage("ProfileFor") + " "
                 + locomotiveAddress.getNumber() + " " + Bundle.getMessage("CreatedOn")
                 + " " + result;
         printTitleText.setText(annotate);
@@ -738,6 +739,8 @@ public class SpeedoConsoleFrame extends JmriJFrame implements SpeedoListener,
         dirRevButton.setToolTipText(Bundle.getMessage("TTMeasRev"));
         dirFwdButton.setForeground(Color.RED);
         dirRevButton.setForeground(Color.BLUE);
+        dirGroup.add(dirFwdButton);
+        dirGroup.add(dirRevButton);
         profileControlPane.add(dirFwdButton);
         profileControlPane.add(dirRevButton);
         toggleGridButton.setSelected(true);
