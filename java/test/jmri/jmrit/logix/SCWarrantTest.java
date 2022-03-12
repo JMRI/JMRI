510fb3ec34ffc8368fbf715877eea9bf4565d285
pete_ <pete_@DESKTOP-O0N9TLF.attlocal.net>
2022-03-11 22:48:07 -0800

    Try another version of SCWarrantTest.

--- java/test/jmri/jmrit/logix/SCWarrantTest.java
+++ java/test/jmri/jmrit/logix/SCWarrantTest.java
@@ -101,13 +101,23 @@ public class SCWarrantTest extends WarrantTest {
         });
         jmri.util.JUnitUtil.releaseThread(this, 100); // What should we specifically waitFor?
 
-        try {
-            sWest.setState(Sensor.INACTIVE);
-            JUnitUtil.waitFor(100);
-            sSouth.setState(Sensor.ACTIVE);
-        } catch (jmri.JmriException e) {
-            Assert.fail("Unexpected Exception: " + e);
-        }
+        jmri.util.ThreadingUtil.runOnLayout(() -> {
+            try {
+                sWest.setState(Sensor.INACTIVE);
+            } catch (jmri.JmriException e) {
+                Assert.fail("Unexpected Exception: " + e);
+            }
+        });
+        jmri.util.JUnitUtil.releaseThread(this, 100); // What should we specifically waitFor?
+
+        jmri.util.ThreadingUtil.runOnLayout(() -> {
+            try {
+                sSouth.setState(Sensor.ACTIVE);
+            } catch (jmri.JmriException e) {
+                Assert.fail("Unexpected Exception: " + e);
+            }
+        });
+        jmri.util.JUnitUtil.releaseThread(this, 100); // What should we specifically waitFor?
 
         // wait for done
         jmri.util.JUnitUtil.waitFor(() -> {
