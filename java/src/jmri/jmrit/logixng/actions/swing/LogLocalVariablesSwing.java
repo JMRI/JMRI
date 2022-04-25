57b42f066974fcbed2ee38648fc635e07d1f41aa
Daniel <db123@bergqvist.se>
2022-04-26 01:35:36 +0200

    LogixNG action Log local variables can log global variables

--- java/src/jmri/jmrit/logixng/actions/swing/LogLocalVariablesSwing.java
+++ java/src/jmri/jmrit/logixng/actions/swing/LogLocalVariablesSwing.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
+import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 
 import jmri.InstanceManager;
@@ -14,43 +15,62 @@ import jmri.jmrit.logixng.actions.LogLocalVariables;
 
 /**
  * Configures an LogLocalVariables object with a Swing JPanel.
- * 
+ *
  * @author Daniel Bergqvist Copyright 2021
  */
 public class LogLocalVariablesSwing extends AbstractDigitalActionSwing {
 
+    private JCheckBox _includeGlobalVariables;
+
     @Override
     protected void createPanel(@CheckForNull Base object, @Nonnull JPanel buttonPanel) {
+        LogLocalVariables action = (LogLocalVariables)object;
+
         panel = new JPanel();
+
+        _includeGlobalVariables = new JCheckBox(Bundle.getMessage("LogLocalVariablesSwing_IncludeGlobalVariables"));
+        panel.add(_includeGlobalVariables);
+
+        if (action != null) {
+            _includeGlobalVariables.setSelected(action.isIncludeGlobalVariables());
+        } else {
+            _includeGlobalVariables.setSelected(true);
+        }
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public boolean validate(@Nonnull List<String> errorMessages) {
         return true;
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public MaleSocket createNewObject(@Nonnull String systemName, @CheckForNull String userName) {
         LogLocalVariables action = new LogLocalVariables(systemName, userName);
         return InstanceManager.getDefault(DigitalActionManager.class).registerAction(action);
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public void updateObject(@Nonnull Base object) {
-        // Do nothing
+        if (!(object instanceof LogLocalVariables)) {
+            throw new IllegalArgumentException("object must be an LogLocalVariables but is a: "+object.getClass().getName());
+        }
+
+        LogLocalVariables action = (LogLocalVariables)object;
+
+        action.setIncludeGlobalVariables(_includeGlobalVariables.isSelected());
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public String toString() {
         return Bundle.getMessage("LogLocalVariables_Short");
     }
-    
+
     @Override
     public void dispose() {
     }
-    
+
 }
