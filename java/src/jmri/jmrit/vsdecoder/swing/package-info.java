888e24742cf1418b01f0ed8e4b11957139394db3
Klaus Killinger <klaus.killinger@t-online.de>
2022-04-26 09:31:19 +0200

    update documentation and help

---
+++ java/src/jmri/jmrit/vsdecoder/swing/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Sub-package for jmri.jmrit.vsdecoder.
+ *
+ * <h2>Related Documentation</h2>
+ *
+ *   <a href="http://jmri.org/help/en/html/tools/vsd/index.shtml">Virtual Sound Decoder</a>
+*/
+
+//@annotations for the entire package go here
+// include empty DefaultAnnotation to avoid excessive recompilation
+@edu.umd.cs.findbugs.annotations.DefaultAnnotation(value={})
+package jmri.jmrit.vsdecoder.swing;
