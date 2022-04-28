7c1d80c6c2ce982b97bf3ab5a80650d56e4a86ad
Daniel <db123@bergqvist.se>
2022-04-28 21:27:00 +0200

    Update script examples for LogixNG

--- jython/LogixNG/InitVariableToTableContents.py
+++ jython/LogixNG/InitVariableToTableContents.py
@@ -3,24 +3,25 @@
 #
 # The variable is initialized to a map that has the values
 # from a LogixNG Table.
+#
+# In this example, we use ConcurrentHashMap since it's thread safe
 
-import jmri
-import java
 
 # Configure this script
+#
 # tableName is the name of the table
 # columnName is the name of the column you want to use for the values
+
 tableName = "MyTable"
 columnName = "Turnouts"
 
 
 
+import jmri
+import java
+
 myTable = logixngTables.getNamedTable(tableName)
 
-print "Table: "
-print myTable
-
-# ConcurrentHashMap is thread safe
 myMap = java.util.concurrent.ConcurrentHashMap()
 
 print "Table: " + myTable.getSystemName()
@@ -29,9 +30,7 @@ column = myTable.getColumnNumber(columnName)
 
 for row in range(1, myTable.numRows()):
     key = myTable.getCell(row, 0)
-    print "key: " + key
     value = myTable.getCell(row, column)
-    print "value: " + value
     myMap.put(key, value)
 
 
