1d7ddbaf83a80ed203ba6e681776100387c978c3
Bob Jacobsen <jacobsen@mac.com>
2022-05-27 07:22:21 -0400

    new L10N tool

---
+++ java/test/jmri/util/i18n/PropertiesMergeTool.java
@@ -0,0 +1,91 @@
+/**
+ * A command line tool to maintain properties files for translation.
+ * Takes tne names of two properties files.  The first is the master, default
+ * English version, and the second is a partially translated file.
+ *
+ * When run, the program will re-write the 2nd file with
+ *   All the key/value pairs in the order they appear in the first file
+ *   With k/v pairs missing in the 2nd file filled out with the k/v from the first file.
+ *
+ * Effectively, this puts English into the 2nd file for later translation, while
+ * maintaining the files in the same order for readability.
+ *
+ * Note that whitespace and comments are not preserved.  If desired, those have
+ * to be added back in by hand.
+ *
+ * Usage:
+ * java java/test/jmri/util/i18n/PropertiesMergeTool.java java/src/jmri/NamedBeanBundle.properties java/src/jmri/NamedBeanBundle_es.properties
+ */
+package jmri.util.i18n;
+
+import java.util.*;
+import java.io.*;
+
+class PropertiesMergeTool {
+    static public void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Needs two file names");
+            return;
+        }
+
+        try (var stream2 = new FileInputStream(args[1])) {
+
+            ResourceBundle bundle2 = new PropertyResourceBundle(stream2);
+
+            // now that bundle is read, open same file for output
+            outFile = new File(args[1]);
+            outWriter = new FileWriter(outFile);
+
+            // Apparently there's no way to address these in order, so we separately
+            // read the file to do that.
+            File inFile  = new File(args[0]);
+            try (var fileReader = new BufferedReader(new FileReader(inFile))) {
+                // process every line
+                for(String line; (line = fileReader.readLine()) != null; ) {
+                    // decode line types
+                    int index;
+                    if (line.startsWith("#")) {
+                        writeOutLine(line);
+                    } else if ((index = line.indexOf("=")) >= 0) {
+                        String part1 = line.substring(0, index);
+                        int keyIndex = line.indexOf(" ");
+                        if (keyIndex == -1) keyIndex = 99999; // no space in line
+                        String key = line.substring(0, Math.min(index, keyIndex));
+                        // check if translation in 2nd file
+                        if (bundle2.containsKey(key)) {
+                            // write translated value
+                            String part2 = line.substring(index+1);
+                            String pad = "";
+                            // pad with same number of leading spaces
+                            while (part2.startsWith(" ")) {
+                                pad = pad+" ";
+                                part2 = part2.substring(1);
+                            }
+                            writeOutLine(part1+"="+pad+bundle2.getString(key));
+                        } else {
+                            // write untranslated line
+                            writeOutLine(line);
+                        }
+                    } else {
+                        // don't understand the line, just write it out
+                        // usually lines of spaces
+                        writeOutLine(line);
+                    }
+                }
+            }
+
+            // done, flush and close output
+            outWriter.close();
+
+        } catch (Exception e) {
+            System.out.println("Exception: "+e);
+            e.printStackTrace();
+        }
+    }
+
+    static File outFile = null;
+    static Writer outWriter = null;
+    static public void writeOutLine(String line) throws IOException {
+        outWriter.write(line+"\n");
+    }
+}
