1efefb0b940d830f7277ace340b89c609b10d6fa
pete_ <pete_@DESKTOP-O0N9TLF.attlocal.net>
2022-06-18 17:55:33 -0700

    Remove another tab

--- java/src/jmri/jmrit/logixng/actions/ActionWarrant.java
+++ java/src/jmri/jmrit/logixng/actions/ActionWarrant.java
@@ -274,11 +274,11 @@ public class ActionWarrant extends AbstractDigitalAction
                     }
                     break;
                 case SetTrainId:
-                	if (warrant.getRunMode() == Warrant.MODE_NONE) {
+                    if (warrant.getRunMode() == Warrant.MODE_NONE) {
                         if(!warrant.getSpeedUtil().setAddress(getNewData(theOper))) {
                             throw new JmriException("invalid train ID in action - " + warrant.getDisplayName());  // NOI18N
                         }
-                	}
+                    }
                     break;
                 case SetTrainName:
                     warrant.setTrainName(getNewData(theOper));
