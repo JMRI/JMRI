0659677c37f4e3d782427a63d32e8704ddf7d400
Daniel <db123@bergqvist.se>
2022-07-14 17:38:51 +0200

    Update SocketOperationTest.java

--- java/test/jmri/jmrit/logixng/SocketOperationTest.java
+++ java/test/jmri/jmrit/logixng/SocketOperationTest.java
@@ -183,7 +183,6 @@ public class SocketOperationTest {
         CreateLogixNGTreeScaffold.tearDown();
     }
 
-
 //    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DeepCopyTest.class);
 
 }
