135be3011626c34b2e068f38fd36f97de76f0f07
Andrew Crosland <andrew@crosland.me.uk>
2022-07-27 19:24:19 +0100

    Add prog track only mode to SPROG 3 Plus mode switcher

--- java/src/jmri/jmrix/can/cbus/swing/modeswitcher/SprogCbusSprog3PlusModeSwitcherFrame.java
+++ java/src/jmri/jmrix/can/cbus/swing/modeswitcher/SprogCbusSprog3PlusModeSwitcherFrame.java
@@ -20,10 +20,12 @@ public class SprogCbusSprog3PlusModeSwitcherFrame extends SprogCbusModeSwitcherF
     public static final int PROG_OFF_MODE = 0; // Prog track off when not programming
     public static final int PROG_ON_MODE = 1;  // Prog track follows main when not programming
     public static final int PROG_AR_MODE = 2;  // Prog track is auto-reverse power district, no programming on progtrack
+    public static final int PROG_ONLY_MODE = 3;// Prog track only, no main. Power control will manipulate prog track
 
     private JRadioButton progOffButton;
     private JRadioButton progOnButton;
     private JRadioButton progArButton;
+    private JRadioButton progOnlyButton;
     
     public SprogCbusSprog3PlusModeSwitcherFrame(CanSystemConnectionMemo memo) {
         super(memo, Bundle.getMessage("SprogCbusPlusModeSwitcher"));
@@ -43,10 +45,12 @@ public class SprogCbusSprog3PlusModeSwitcherFrame extends SprogCbusModeSwitcherF
             progOffButton = new JRadioButton(Bundle.getMessage("ProgOffMode"));
             progOnButton = new JRadioButton(Bundle.getMessage("ProgOnMode"));
             progArButton = new JRadioButton(Bundle.getMessage("ProgArMode"));
+            progOnlyButton = new JRadioButton(Bundle.getMessage("ProgOnlyMode"));
             ButtonGroup buttons = new ButtonGroup();
             buttons.add(progOffButton);
             buttons.add(progOnButton);
             buttons.add(progArButton);
+            buttons.add(progOnlyButton);
             
             // Get current preferences
             // It is expected that the saved preferences will usually match the hardware.
@@ -62,6 +66,10 @@ public class SprogCbusSprog3PlusModeSwitcherFrame extends SprogCbusModeSwitcherF
                 progArButton.setSelected(true);
                 mode = PROG_AR_MODE;
                 pm.setGlobalProgrammerAvailable(false);
+            } else if (preferences.getProgTrackMode() == PROG_ONLY_MODE) {
+                progOnlyButton.setSelected(true);
+                mode = PROG_ONLY_MODE;
+                pm.setGlobalProgrammerAvailable(true);
             } else {
                 // Default if inconsistent preference
                 progOffButton.setSelected(true);
@@ -83,6 +91,11 @@ public class SprogCbusSprog3PlusModeSwitcherFrame extends SprogCbusModeSwitcherF
                     mode = PROG_AR_MODE;
                     setHardwareMode(mode);
                     pm.setGlobalProgrammerAvailable(false);
+                } else if (progOnlyButton.isSelected()) {
+                    log.info("Setting prog track only mode");
+                    mode = PROG_ONLY_MODE;
+                    setHardwareMode(mode);
+                    pm.setGlobalProgrammerAvailable(true);
                 } else {
                     log.info("Setting prog track off when not programming");
                     mode = PROG_OFF_MODE;
@@ -95,9 +108,11 @@ public class SprogCbusSprog3PlusModeSwitcherFrame extends SprogCbusModeSwitcherF
             progOffButton.addActionListener(listener);
             progOnButton.addActionListener(listener);
             progArButton.addActionListener(listener);
+            progOnlyButton.addActionListener(listener);
             modePane.add(progOffButton);
             modePane.add(progOnButton);
             modePane.add(progArButton);
+            modePane.add(progOnlyButton);
 
             panel.add(label, BorderLayout.NORTH);
             panel.add(modePane, BorderLayout.CENTER);
