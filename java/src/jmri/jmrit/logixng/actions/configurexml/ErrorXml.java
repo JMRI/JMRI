0bb7c85897a85babd57e082970a1d9d12040033e
Daniel <db123@bergqvist.se>
2022-08-11 02:51:05 +0200

    Comment unused variable

--- java/src/jmri/jmrit/logixng/actions/configurexml/ErrorXml.java
+++ java/src/jmri/jmrit/logixng/actions/configurexml/ErrorXml.java
@@ -53,5 +53,5 @@ public class ErrorXml extends jmri.managers.configurexml.AbstractNamedBeanManage
         return true;
     }
 
-    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ErrorXml.class);
+//    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ErrorXml.class);
 }
