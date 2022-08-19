77650c097048a1f3e92ce53432133f0d4daae5c5
steve young <39652002+icklesteve@users.noreply.github.com>
2022-08-19 11:13:25 +0100

    Update LayoutRHXOverTest.java
    add headless check

--- java/test/jmri/jmrit/display/layoutEditor/LayoutRHXOverTest.java
+++ java/test/jmri/jmrit/display/layoutEditor/LayoutRHXOverTest.java
@@ -1,10 +1,13 @@
 package jmri.jmrit.display.layoutEditor;
 
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+
 /**
  * Test common functioning of LayoutRHXOver.
  *
  * @author Bob Jacobsen Copyright (C) 2020
  */
+@DisabledIfSystemProperty(named ="java.awt.headless", matches ="true")
 public class LayoutRHXOverTest extends LayoutXOverTest {
 
 }
