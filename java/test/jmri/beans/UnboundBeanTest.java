61cb77d41e1b0939db69f1ba1cecd31b90812f0f
steve young <39652002+icklesteve@users.noreply.github.com>
2022-09-10 15:17:58 +0100

    Update UnboundBeanTest.java
    add setUp / tearDown methods
    update exception assertion
    Implementation class can be static

--- java/test/jmri/beans/UnboundBeanTest.java
+++ java/test/jmri/beans/UnboundBeanTest.java
@@ -1,15 +1,14 @@
 package jmri.beans;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.junit.jupiter.api.Test;
+import jmri.util.JUnitUtil;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
@@ -36,15 +35,12 @@ public class UnboundBeanTest {
         UnboundBean instance = new UnboundBeanImpl();
         assertNull(instance.getIndexedProperty(NOT_A_PROPERTY, 0));
         assertEquals(OLD_VALUE, instance.getIndexedProperty(INDEXED_PROPERTY, 0));
-        // Really wish we were using JUnit 4 with its ability to assert that an
-        // expected Exception was thrown
-        boolean outOfBounds = false;
-        try {
+
+        Exception ex = assertThrows(IndexOutOfBoundsException.class,() -> {
             instance.getIndexedProperty(INDEXED_PROPERTY, 1);
-        } catch (IndexOutOfBoundsException ex) {
-            outOfBounds = true;
-        }
-        assertTrue(outOfBounds);
+        });
+        assertNotNull(ex);
+
         assertNull(instance.getIndexedProperty(MAPPED_INDEXED, 0));
         assertNull(instance.getIndexedProperty(MAPPED_INDEXED, 1));
     }
@@ -136,12 +132,22 @@ public class UnboundBeanTest {
         assertNull(instance.getProperty(NEW_PROPERTY));
     }
 
-    public class UnboundBeanImpl extends UnboundBean {
+    @BeforeEach
+    public void setUp() {
+        JUnitUtil.setUp();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        JUnitUtil.tearDown();
+    }
+
+    private static class UnboundBeanImpl extends UnboundBean {
 
         private String stringProperty = OLD_VALUE;
         private final ArrayList<String> indexedProperty = new ArrayList<>();
 
-        public UnboundBeanImpl() {
+        private UnboundBeanImpl() {
             this.indexedProperty.add(0, OLD_VALUE);
             this.setProperty(MAPPED_STRING, OLD_VALUE);
             this.setIndexedProperty(MAPPED_INDEXED, 0, OLD_VALUE);
