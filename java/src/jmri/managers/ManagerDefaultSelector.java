18744f29e801061c3ec1a7a942a756acce8955cf
Daniel <db123@bergqvist.se>
2022-10-22 02:17:17 +0200

    Suppress raw warning

--- java/src/jmri/managers/ManagerDefaultSelector.java
+++ java/src/jmri/managers/ManagerDefaultSelector.java
@@ -214,7 +214,7 @@ public class ManagerDefaultSelector extends AbstractPreferencesManager {
      *         occur
      */
     @CheckForNull
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings({"unchecked", "rawtypes"})
     public InitializationException configure(Profile profile) {
         InitializationException error = null;
         List<SystemConnectionMemo> connList = InstanceManager.getList(SystemConnectionMemo.class);
