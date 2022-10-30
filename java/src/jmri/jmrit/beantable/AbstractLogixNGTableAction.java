ceb2c6e2e298d47aa3abb9d54c026658b8e350f1
Daniel <db123@bergqvist.se>
2022-10-30 20:28:26 +0100

    Refactor delete LogixNG beans

--- java/src/jmri/jmrit/beantable/AbstractLogixNGTableAction.java
+++ java/src/jmri/jmrit/beantable/AbstractLogixNGTableAction.java
@@ -21,6 +21,7 @@ import jmri.NamedBean.BadUserNameException;
 import jmri.UserPreferencesManager;
 import jmri.jmrit.logixng.Base;
 import jmri.jmrit.logixng.tools.swing.AbstractLogixNGEditor;
+import jmri.jmrit.logixng.tools.swing.DeleteBean;
 import jmri.util.FileUtil;
 import jmri.util.JmriJFrame;
 
@@ -56,6 +57,7 @@ public abstract class AbstractLogixNGTableAction<E extends NamedBean> extends Ab
     static final String PRINT_SYSTEM_NAMES_OPTION = "jmri.jmrit.logixng.SystemNames";
 
     JTextArea _textContent;
+    DeleteBean<E> deleteBean = new DeleteBean<>(getManager());
 
     /**
      * Create a AbstractLogixNGTableAction instance.
@@ -659,121 +661,12 @@ public abstract class AbstractLogixNGTableAction<E extends NamedBean> extends Ab
         }
 
         final E x = getManager().getBySystemName(sName);
-        final jmri.UserPreferencesManager p;
-        p = jmri.InstanceManager.getNullableDefault(jmri.UserPreferencesManager.class);
 
         if (x == null) return;  // This should never happen
 
-        StringBuilder message = new StringBuilder();
-        try {
-            getManager().deleteBean(x, "CanDelete");  // NOI18N
-        } catch (PropertyVetoException e) {
-            if (e.getPropertyChangeEvent().getPropertyName().equals("DoNotDelete")) { // NOI18N
-                log.warn("{} : Do Not Delete", e.getMessage());
-                message.append(Bundle.getMessage("VetoDeleteBean", x.getBeanType(), x.getDisplayName(NamedBean.DisplayOptions.USERNAME_SYSTEMNAME), e.getMessage()));
-                JOptionPane.showMessageDialog(null, message.toString(),
-                        Bundle.getMessage("QuestionTitle"),
-                        JOptionPane.ERROR_MESSAGE);
-                return;
-            }
-            message.append(e.getMessage());
-        }
-        List<String> listenerRefs = new ArrayList<>();
-        getListenerRefsIncludingChildren(x, listenerRefs);
-        int listenerRefsCount = listenerRefs.size();
-        log.debug("Delete with {}", listenerRefsCount);
-        if (p != null && p.getMultipleChoiceOption(getClassName(), "delete") == 0x02 && message.toString().isEmpty()) {
-            deleteBean(x);
-        } else {
-            final JDialog dialog = new JDialog();
-            dialog.setTitle(Bundle.getMessage("QuestionTitle"));
-            dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-            JPanel container = new JPanel();
-            container.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-            container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
-
-            if (listenerRefsCount > 0) { // warn of listeners attached before delete
-                String prompt = hasChildren(x)
-                        ? "DeleteWithChildrenPrompt" : "DeletePrompt";
-                JLabel question = new JLabel(Bundle.getMessage(prompt, x.getDisplayName(NamedBean.DisplayOptions.USERNAME_SYSTEMNAME)));
-                question.setAlignmentX(Component.CENTER_ALIGNMENT);
-                container.add(question);
-
-                ArrayList<String> listeners = new ArrayList<>();
-                for (String listenerRef : listenerRefs) {
-                    if (!listeners.contains(listenerRef)) {
-                        listeners.add(listenerRef);
-                    }
-                }
-
-                message.append("<br>");
-                message.append(Bundle.getMessage("ReminderInUse", listenerRefsCount));
-                message.append("<ul>");
-                for (String listener : listeners) {
-                    message.append("<li>");
-                    message.append(listener);
-                    message.append("</li>");
-                }
-                message.append("</ul>");
-
-                JEditorPane pane = new JEditorPane();
-                pane.setContentType("text/html");
-                pane.setText("<html>" + message.toString() + "</html>");
-                pane.setEditable(false);
-                JScrollPane jScrollPane = new JScrollPane(pane);
-                container.add(jScrollPane);
-            } else {
-                String prompt = hasChildren(x)
-                        ? "DeleteWithChildrenPrompt" : "DeletePrompt";
-                String msg = MessageFormat.format(
-                        Bundle.getMessage(prompt), x.getSystemName());
-                JLabel question = new JLabel(msg);
-                question.setAlignmentX(Component.CENTER_ALIGNMENT);
-                container.add(question);
-            }
-
-            final JCheckBox remember = new JCheckBox(Bundle.getMessage("MessageRememberSetting"));
-            remember.setFont(remember.getFont().deriveFont(10f));
-            remember.setAlignmentX(Component.CENTER_ALIGNMENT);
-
-            JButton yesButton = new JButton(Bundle.getMessage("ButtonYes"));
-            JButton noButton = new JButton(Bundle.getMessage("ButtonNo"));
-            JPanel button = new JPanel();
-            button.setAlignmentX(Component.CENTER_ALIGNMENT);
-            button.add(yesButton);
-            button.add(noButton);
-            container.add(button);
-
-            noButton.addActionListener((ActionEvent e) -> {
-                //there is no point in remembering this the user will never be
-                //able to delete a bean!
-                dialog.dispose();
-            });
-
-            yesButton.addActionListener((ActionEvent e) -> {
-                if (remember.isSelected() && p != null) {
-                    p.setMultipleChoiceOption(getClassName(), "delete", 0x02);  // NOI18N
-                }
-                deleteBean(x);
-                dialog.dispose();
-            });
-            container.add(remember);
-            container.setAlignmentX(Component.CENTER_ALIGNMENT);
-            container.setAlignmentY(Component.CENTER_ALIGNMENT);
-            dialog.getContentPane().add(container);
-            dialog.pack();
-
-            dialog.getRootPane().setDefaultButton(noButton);
-            noButton.requestFocusInWindow(); // set default keyboard focus, after pack() before setVisible(true)
-            dialog.getRootPane().registerKeyboardAction(e -> { // escape to exit
-                    dialog.setVisible(false);
-                    dialog.dispose(); },
-                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_IN_FOCUSED_WINDOW);
-
-            dialog.setLocation((Toolkit.getDefaultToolkit().getScreenSize().width) / 2 - dialog.getWidth() / 2, (Toolkit.getDefaultToolkit().getScreenSize().height) / 2 - dialog.getHeight() / 2);
-            dialog.setModal(true);
-            dialog.setVisible(true);
-        }
+        deleteBean.delete(x, hasChildren(x), (t)->{deleteBean(t);},
+                (t,list)->{getListenerRefsIncludingChildren(t,list);},
+                getClassName());
     }
 
     @Override
