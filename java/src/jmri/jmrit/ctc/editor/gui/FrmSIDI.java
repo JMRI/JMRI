ac7243dbf65bba0783c8f91a3cf4f42485e5ddc7
dsand47 <gh@dfm.sandsys.org>
2022-11-16 23:31:21 -0600

    Revert NetBeans change

--- java/src/jmri/jmrit/ctc/editor/gui/FrmSIDI.java
+++ java/src/jmri/jmrit/ctc/editor/gui/FrmSIDI.java
@@ -533,12 +533,12 @@ public class FrmSIDI extends javax.swing.JFrame {
                 ""
             }
         ) {
-            Class[] types = new Class [] {
+            Class<?>[] types = new Class<?> [] {
                 java.lang.String.class
             };
 
             @Override
-            public Class getColumnClass(int columnIndex) {
+            public Class<?> getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
         });
@@ -675,12 +675,12 @@ public class FrmSIDI extends javax.swing.JFrame {
                 ""
             }
         ) {
-            Class[] types = new Class [] {
+            Class<?>[] types = new Class<?> [] {
                 java.lang.String.class
             };
 
             @Override
-            public Class getColumnClass(int columnIndex) {
+            public Class<?> getColumnClass(int columnIndex) {
                 return types [columnIndex];
             }
         });
