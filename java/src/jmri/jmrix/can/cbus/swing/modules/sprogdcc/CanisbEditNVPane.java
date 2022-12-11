22217a86cfa9918291cd780cf1dc6b26c63f3553
Andrew Crosland <andrew@crosland.me.uk>
2022-12-11 20:26:55 +0000

    Add diagnostics pane

--- java/src/jmri/jmrix/can/cbus/swing/modules/sprogdcc/CanisbEditNVPane.java
+++ java/src/jmri/jmrix/can/cbus/swing/modules/sprogdcc/CanisbEditNVPane.java
@@ -1,18 +1,194 @@
 package jmri.jmrix.can.cbus.swing.modules.sprogdcc;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+
 import jmri.jmrix.can.cbus.node.CbusNode;
 import jmri.jmrix.can.cbus.node.CbusNodeNVTableDataModel;
-import jmri.jmrix.can.cbus.swing.modules.base.Servo8BaseEditNVPane;
+import jmri.jmrix.can.cbus.swing.modules.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Node Variable edit frame for a SPROG DCC CANSERVOIO module
  *
  * @author Andrew Crosland Copyright (C) 2021
  */
-public class CanisbEditNVPane extends Servo8BaseEditNVPane {
+public class CanisbEditNVPane extends AbstractEditNVPane {
+    
+    private final UpdateNV canIdUpdateFn = new UpdateCanId();
+    private final UpdateNV nodeNumberUpdateFn = new UpdateNodeNumber();
+
+    private JRadioButton setup;
+    private TitledSpinner canIdSpinner;
+    private TitledSpinner nodeNumberSpinner;
+    private JRadioButton disable;
+    private JTextArea canRxErrCount;
+    private JButton clearRxErrCount;
+    private JTextArea canTxErrCount;
+    private JButton clearTxErrCount;
     
     protected CanisbEditNVPane(CbusNodeNVTableDataModel dataModel, CbusNode node) {
         super(dataModel, node);
     }
     
+    /** {@inheritDoc} */
+    @Override
+    public AbstractEditNVPane getContent() {
+        JTabbedPane tabbedPane = new JTabbedPane();
+        
+        JPanel cbusPane = new CbusPane();
+        JPanel diagnosticsPane = new DiagnosticsPane();
+        tabbedPane.addTab("CBUS", cbusPane);
+        tabbedPane.addTab("CBUS Diagnostics", diagnosticsPane);
+        
+        JScrollPane scroll = new JScrollPane(tabbedPane);
+        add(scroll);
+        
+        return this;
+    }
+    
+    /** {@inheritDoc} */
+    @Override
+    public void tableChanged(TableModelEvent e) {
+//        log.debug("canisb table changed");
+        if (e.getType() == TableModelEvent.UPDATE) {
+            int row = e.getFirstRow();
+            int nv = row + 1;
+        }
+    }
+    
+    /**
+     * Update the CAN ID
+     * 
+     * For debug only, CAN ID is not normally set this way
+     */
+    protected class UpdateCanId implements UpdateNV {
+        
+        /** {@inheritDoc} */
+        @Override
+        public void setNewVal(int index) {
+            int canId = canIdSpinner.getIntegerValue();
+            // Note that changing the data model will result in tableChanged() being called, which can manipulate the buttons, etc
+            _dataModel.setValueAt(canId, index - 1, CbusNodeNVTableDataModel.NV_SELECT_COLUMN);
+        }
+    }
+        
+    /**
+     * Update the node number
+     * 
+     * For debug only, CAN ID is not normally set this way
+     */
+    protected class UpdateNodeNumber implements UpdateNV {
+        
+        /** {@inheritDoc} */
+        @Override
+        public void setNewVal(int index) {
+            int nn = nodeNumberSpinner.getIntegerValue();
+            int nnHi = nn/256;
+            int nnLo = nn%256;
+            // Note that changing the data model will result in tableChanged() being called, which can manipulate the buttons, etc
+            _dataModel.setValueAt(nnHi, index - 1, CbusNodeNVTableDataModel.NV_SELECT_COLUMN);
+            _dataModel.setValueAt(nnLo, index, CbusNodeNVTableDataModel.NV_SELECT_COLUMN);
+        }
+    }
+    
+    /**
+     * Panel to display CBUS operation related NVs
+     */
+    public class CbusPane extends JPanel {
+        public CbusPane() {
+            super();
+
+            JPanel gridPane = new JPanel(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+
+            c.weightx = 1;
+            c.weighty = 1;
+            c.gridx = 0;
+            c.gridy = 0;
+
+            canIdSpinner = new TitledSpinner(Bundle.getMessage("CanId"), CanisbPaneProvider.CANID, canIdUpdateFn);
+            canIdSpinner.setToolTip(Bundle.getMessage("CanIdTt"));
+            canIdSpinner.init(getSelectValue(CanisbPaneProvider.CANID, 100), 100, 127, 1);
+            gridPane.add(canIdSpinner, c);
+            c.gridy++;
+
+            nodeNumberSpinner = new TitledSpinner(Bundle.getMessage("NodeNumber"), CanisbPaneProvider.NN_HI, nodeNumberUpdateFn);
+            nodeNumberSpinner.setToolTip(Bundle.getMessage("NodeNumberTt"));
+            int nn = getSelectValue(CanisbPaneProvider.NN_HI, CanisbPaneProvider.NN_LO, 65520);
+            nodeNumberSpinner.init(nn, 65520, 65534, 1);
+            gridPane.add(nodeNumberSpinner, c);
+            c.gridy++;
+            
+            setup = new JRadioButton("SetupMode");
+            setup.setSelected(false);
+            setup.setToolTipText(Bundle.getMessage("SetupModeTt"));
+            gridPane.add(setup, c);
+            c.gridy++;
+            
+            disable = new JRadioButton("DisableCan");
+            disable.setSelected(false);
+            disable.setToolTipText(Bundle.getMessage("DisableCanTt"));
+            gridPane.add(disable, c);
+            c.gridy++;
+            
+            add(gridPane);
+        }
+    }
+    
+    /**
+     * Panel to display CBUS operation related NVs
+     */
+    public class DiagnosticsPane extends JPanel {
+        public DiagnosticsPane() {
+            super();
+
+            JPanel gridPane = new JPanel(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+            c.fill = GridBagConstraints.HORIZONTAL;
+
+            c.weightx = 1;
+            c.weighty = 1;
+            c.gridx = 0;
+            c.gridy = 0;
+
+            String [] errCountTitle = {Bundle.getMessage("CanRxErrorCount"),
+                Bundle.getMessage("CanTxErrorCount"),
+                Bundle.getMessage("CanTxFailureCount"),
+                Bundle.getMessage("CanRxOverflowCount")
+            };
+                    
+            JTextArea [] errText = new JTextArea[4];
+            JTextArea [] errCount = new JTextArea[4];
+            JButton [] clearErr = new JButton[4];
+            
+            for(int i=0; i<4; i++) {
+                errText[i] = new JTextArea();
+                errText[i].setText(errCountTitle[i]);
+                gridPane.add(errText[i], c);
+                c.gridx++;
+                
+                errCount[i] = new JTextArea();
+                errCount[i].setText("0");
+                gridPane.add(errCount[i], c);
+                c.gridx++;
+                
+                clearErr[i] = new JButton(Bundle.getMessage("Clear"));
+                gridPane.add(clearErr[i], c);
+                c.gridx = 0;
+                c.gridy++;
+            }
+            
+            add(gridPane);
+        }
+    }
+    
+    private final static Logger log = LoggerFactory.getLogger(CanisbEditNVPane.class);
+
 }
--- java/src/jmri/jmrix/can/cbus/swing/modules/sprogdcc/CanisbPaneProvider.java
+++ java/src/jmri/jmrix/can/cbus/swing/modules/sprogdcc/CanisbPaneProvider.java
@@ -6,8 +6,7 @@ import javax.annotation.Nonnull;
 
 import jmri.jmrix.can.cbus.node.CbusNode;
 import jmri.jmrix.can.cbus.node.CbusNodeNVTableDataModel;
-import jmri.jmrix.can.cbus.swing.modules.AbstractEditNVPane;
-import jmri.jmrix.can.cbus.swing.modules.CbusConfigPaneProvider;
+import jmri.jmrix.can.cbus.swing.modules.*;
 
 import org.openide.util.lookup.ServiceProvider;
 
@@ -24,12 +23,19 @@ public class CanisbPaneProvider extends CbusConfigPaneProvider {
     public static final int RX_ERR_CNT = 1;
     public static final int TX_ERR_CNT = 2;
     public static final int TX_FAIL_CNT = 3;
-    public static final int TIMEOUT = 4;
+    public static final int RX_OVFLW_COUNT = 4;
     public static final int SETUP = 5;
     public static final int CAN_DISABLE = 6;
     public static final int CANID = 7;
     public static final int NN_HI = 8;
     public static final int NN_LO = 9;
+    public static final int CAN_ERR_STATUS = 10;
+    public static final int CAN_NOM_BIT_RATE_RX_COUNT = 11;
+    public static final int CAN_NOM_BIT_RATE_TX_COUNT = 12;
+    public static final int CAN_ERR_FREE_COUNT_LO = 13;
+    public static final int CAN_ERR_FREE_COUNT_HI = 14;
+    public static final int CAN_DIAGNOSTICS_LO = 15;
+    public static final int CAN_DIAGNOSTICS_HI = 16;
 
     public CanisbPaneProvider() {
         super();
@@ -57,12 +63,19 @@ public class CanisbPaneProvider extends CbusConfigPaneProvider {
         result.put(RX_ERR_CNT, Bundle.getMessage("CanRxErrorCount"));
         result.put(TX_ERR_CNT, Bundle.getMessage("CanTxErrorCount"));
         result.put(TX_FAIL_CNT, Bundle.getMessage("CanTxFailureCount"));
-        result.put(TIMEOUT, Bundle.getMessage("CanTimeoutCount"));
+        result.put(RX_OVFLW_COUNT, Bundle.getMessage("CanRxOverflowCount"));
         result.put(SETUP, Bundle.getMessage("SetupMode"));
         result.put(CAN_DISABLE, Bundle.getMessage("CanDisable"));
         result.put(CANID, Bundle.getMessage("CanId"));
         result.put(NN_HI, Bundle.getMessage("NodeNumberHi"));
         result.put(NN_LO, Bundle.getMessage("NodeNumberLo"));
+        result.put(CAN_ERR_STATUS, Bundle.getMessage("CanErrStatus"));
+        result.put(CAN_NOM_BIT_RATE_RX_COUNT, Bundle.getMessage("CanNomBitRateRxCount"));
+        result.put(CAN_NOM_BIT_RATE_TX_COUNT, Bundle.getMessage("CanNomBitRateTxCount"));
+        result.put(CAN_ERR_FREE_COUNT_LO, Bundle.getMessage("CanErrFreeCountHi"));
+        result.put(CAN_ERR_FREE_COUNT_HI, Bundle.getMessage("CanErrFreeCountLo"));
+        result.put(CAN_DIAGNOSTICS_LO, Bundle.getMessage("CanDiagnosticsLo"));
+        result.put(CAN_DIAGNOSTICS_HI, Bundle.getMessage("CanDiagnosticsHi"));
 
         return Collections.unmodifiableMap(result);
     }    
@@ -88,7 +101,10 @@ public class CanisbPaneProvider extends CbusConfigPaneProvider {
     /** {@inheritDoc} */
     @Override
     public AbstractEditNVPane getEditNVFrame(CbusNodeNVTableDataModel dataModel, CbusNode node) {
-        _nVarEditFrame = new CanisbEditNVPane(dataModel, node);
+        if (_nVarEditFrame == null ){
+        	_nVarEditFrame = new CanisbEditNVPane(dataModel, node);
+        }
         return _nVarEditFrame.getContent();
     }
+    
 }
