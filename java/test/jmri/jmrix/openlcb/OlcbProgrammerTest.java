c09964ebd9cd6286ed1ca1896d4cadddfdd62ca9
Balazs Racz <balazs.racz@gmail.com>
2023-01-01 21:32:27 +0100

    Switches over to the new OlcbTestHelper class for scaffold. Adds mock for program track listener. Reimplements find program track test. Adds success tests for reads and writes. Adds test helper functions to make these tests easier to write.

--- java/test/jmri/jmrix/openlcb/OlcbProgrammerTest.java
+++ java/test/jmri/jmrix/openlcb/OlcbProgrammerTest.java
@@ -1,33 +1,42 @@
 package jmri.jmrix.openlcb;
 
+import cucumber.api.java8.No;
+import jmri.ProgListener;
+import jmri.ProgrammerException;
 import jmri.util.JUnitUtil;
 import jmri.ProgrammingMode;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.jupiter.api.*;
+import org.junit.rules.TestName;
+import org.mockito.Mockito;
+import org.openlcb.DatagramAcknowledgedMessage;
+import org.openlcb.DatagramMessage;
+import org.openlcb.EventState;
 import org.openlcb.NodeID;
+import org.openlcb.ProducerIdentifiedMessage;
 
 /**
  * OlcbProgrammerTest.java
- *
+ * <p>
  * Test for the jmri.jmrix.openlcb.OlcbProgrammer class
  *
  * @author Bob Jacobsen
  */
 public class OlcbProgrammerTest extends jmri.jmrix.AbstractProgrammerTest {
-
     @Test
     @Override
     public void testDefault() {
-        Assert.assertEquals("Check Default", ProgrammingMode.DIRECTBYTEMODE,
-                programmer.getMode());        
+        Assert.assertEquals("Check Default", ProgrammingMode.DIRECTBYTEMODE, programmer.getMode());
     }
-    
+
     @Override
     @Test
     public void testDefaultViaBestMode() {
         Assert.assertEquals("Check Default", ProgrammingMode.DIRECTBYTEMODE,
-                ((OlcbProgrammer)programmer).getBestMode());        
+                ((OlcbProgrammer) programmer).getBestMode());
     }
 
     @Test
@@ -37,69 +46,141 @@ public class OlcbProgrammerTest extends jmri.jmrix.AbstractProgrammerTest {
     }
 
     @Test
-    public void testFindProgramTrack() {
-        ti.flush();
-        ti.assertSentMessage(":X19914C4CN090099FEFFFF0002;");
-        ti.clearSentMessages();
-        Assert.assertNull(prog.nid);
-        // Seeds alias map with a node ID.
-        ti.sendMessage(":X19100555N050101011807;");
-        ti.flush();
-        // Producer identified.
-        ti.sendMessage(":X19547555N090099FEFFFF0002;");
-        ti.flush();
-        Assert.assertEquals(new NodeID("05.01.01.01.18.07"), prog.nid);
-        ti.assertNoSentMessages();
+    @Override
+    public void testWriteCVNullListener() throws ProgrammerException {
+        registerProgramTrack();
+        super.testWriteCVNullListener();
+        // Write CV1 = 42.
+        h.expectMessage(new DatagramMessage(localNid, progNid, new int[]{0x20, 0x00, 0, 0, 0, 0, 0xF8, 42}));
     }
 
     @Test
-    public void testFindProgramTrack2() {
+    public void testFindProgramTrack() {
         programmer = prog = new OlcbProgrammer(h.iface, null);
         h.expectFrame(":X19914333N090099FEFFFF0002;");
         Assert.assertNull(prog.nid);
         // Seeds alias map with a node ID.
-        h.setRemoteAlias(0x555, new NodeID(0x050101011807L));
+        h.setRemoteAlias(0x555, progNid);
         // Producer identified.
         h.sendFrame(":X19547555N090099FEFFFF0002;");
-        Assert.assertEquals(new NodeID("05.01.01.01.18.07"), prog.nid);
+        Assert.assertEquals(progNid, prog.nid);
         h.expectNoFrames();
     }
 
     @Test
     public void testAddressedReadLookup() {
-        ti.flush();
-        ti.clearSentMessages();
-        programmer = prog = new OlcbProgrammer(ti.iface, true, 15);
+        programmer = prog = new OlcbProgrammer(h.iface, true, 15);
         Assert.assertEquals(new NodeID("06.01.00.00.C0.0F"), prog.nid);
         // There is a verify node ID message sent.
-        ti.assertSentMessage(":X194904C4N06010000C00F;");
-        //ti.assertNoSentMessages();
+        h.expectFrame(":X19490333N06010000C00F;");
     }
+
+    @Test
+    public void testAddressedReadShortAddress() {
+        programmer = prog = new OlcbProgrammer(h.iface, false, 15);
+        Assert.assertEquals(new NodeID("06.01.00.00.00.0F"), prog.nid);
+        // There is a verify node ID message sent.
+        h.expectFrame(":X19490333N06010000000F;");
+    }
+
+    @Test
+    public void testAddressedReadSuccess() throws ProgrammerException {
+        programmer = prog = new OlcbProgrammer(h.iface, true, 15);
+        Assert.assertEquals(new NodeID("06.01.00.00.C0.0F"), prog.nid);
+        // There is a verify node ID message sent.
+        h.expectFrame(":X19490333N06010000C00F;");
+        h.setRemoteAlias(0x999, pomNid);
+        programmer.readCV("1", p);
+        // Read datagram
+        h.expectMessageAndNoMore(new DatagramMessage(localNid, pomNid, new int[]{0x20, 0x40, 0, 0, 0, 0, 0xF8, 1}));
+        // Acknowledged
+        h.sendMessage(new DatagramAcknowledgedMessage(pomNid, localNid));
+        Mockito.verifyNoInteractions(p);
+        // Response datagram
+        h.sendMessageAndExpectResult(new DatagramMessage(pomNid, localNid, new int[]{0x20, 0x50, 0, 0, 0, 0, 0xF8, 37}),
+                new DatagramAcknowledgedMessage(localNid, pomNid));
+        // Passed on to listener
+        Mockito.verify(p).programmingOpReply(37, ProgListener.OK);
+        h.expectNoMessages();
+    }
+
+    @Test
+    public void testWriteOK() throws ProgrammerException {
+        registerProgramTrack();
+        prog.writeCV("13", 42, p);
+        expectInteraction(new int[]{0x20,0x00,0,0,0,12,0xF8, 42}, new int[]{0x20,0x10,0,0,0,12,0xF8});
+        Mockito.verify(p).programmingOpReply(42, 0); //ProgListener.OK
+    }
+
+    @Test
+    public void testReadOK() throws ProgrammerException {
+        registerProgramTrack();
+        prog.readCV("13", p);
+        expectInteraction(new int[]{0x20, 0x40, 0, 0, 0, 12, 0xF8, 1}, new int[]{0x20, 0x50, 0, 0, 0, 12, 0xF8, 37});
+        Mockito.verify(p).programmingOpReply(37, ProgListener.OK);
+    }
+
 
     @Override
     @BeforeEach
     public void setUp() {
         JUnitUtil.setUp();
-        ti = new OlcbTestInterface();
-        ti.waitForStartup();
-        ti.flush();
         log.warn("hello");
-        programmer = prog = new OlcbProgrammer(ti.iface, null);
+        //h.enableSingleThreaded();
+        programmer = prog = new OlcbProgrammer(h.iface, null);
+        h.expectFrame(":X19914333N090099FEFFFF0002;");
+        h.expectNoFrames();
     }
 
     @Override
     @AfterEach
     public void tearDown() {
         programmer = null;
+        h.expectNoFrames();
+        Mockito.verifyNoMoreInteractions(p);
         h.dispose();
-        ti.dispose();
         JUnitUtil.clearShutDownManager(); // put in place because AbstractMRTrafficController implementing subclass was not terminated properly
         JUnitUtil.tearDown();
     }
 
-    OlcbTestInterface ti;
+    /**
+     * Helper function that simulates the startup of the remote program track. This interaction is necessary before the
+     * default programmer object can execute any programming operations.
+     */
+    private void registerProgramTrack() {
+        h.setRemoteAlias(0x555, progNid);
+        h.sendMessage(
+                new ProducerIdentifiedMessage(progNid, OlcbProgrammer.IS_PROGRAMMINGTRACK_EVENT, EventState.Unknown));
+    }
+
+    /**
+     * Helper function to verify a programming track interaction comprised of a request datagram, and a matching
+     * response datagram. Covers the necessary datagram acknowledgements as well. Assumes that the program track is
+     * assigned (not a POM).
+     *
+     * @param dgRequest int array of the request datagram contents, example: `new int[]{0x20, 0x40, 0,0,0,0, 0xF8, 1}` for read CV 1.
+     * @param dgResponse int array of the response datagram contents, example: `new int[]{0x20, 0x50, 0,0,0,0, 0xF8, 23}` for read response=23.
+     */
+    private void expectInteraction(int[] dgRequest, int[] dgResponse) {
+        h.expectMessageAndNoMore(new DatagramMessage(localNid, progNid, dgRequest));
+        // Acknowledged
+        h.sendMessage(new DatagramAcknowledgedMessage(progNid, localNid, 0x80));
+        Mockito.verifyNoInteractions(p);
+        // Response datagram
+        h.sendMessageAndExpectResult(new DatagramMessage(progNid, localNid, dgResponse),
+                new DatagramAcknowledgedMessage(localNid, progNid));
+        h.expectNoMessages();
+    }
+
     OlcbTestHelper h = new OlcbTestHelper();
     OlcbProgrammer prog;
 
+    NodeID localNid = h.iface.getNodeId();
+    NodeID progNid = new NodeID(0x050101011807L);
+    NodeID pomNid = new NodeID(0x06010000C00FL);
+
+    ProgListener p = Mockito.mock(ProgListener.class);
+
     private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(OlcbProgrammerTest.class);
+
 }
