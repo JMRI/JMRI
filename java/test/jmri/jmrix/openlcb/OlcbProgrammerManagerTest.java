2bd8151305b289d9907ac1f20c34abb6a5678536
Balazs Racz <balazs.racz@gmail.com>
2023-01-02 14:01:08 +0100

    Fixes programmer manager test, ensuring that there is an interface present.

--- java/test/jmri/jmrix/openlcb/OlcbProgrammerManagerTest.java
+++ java/test/jmri/jmrix/openlcb/OlcbProgrammerManagerTest.java
@@ -5,6 +5,7 @@ import jmri.util.JUnitUtil;
 
 import org.junit.jupiter.api.*;
 import org.junit.Assert;
+import org.openlcb.IdentifyProducersMessage;
 
 /**
  * OlcbProgrammerManagerTest.java
@@ -14,20 +15,26 @@ import org.junit.Assert;
  * @author Bob Jacobsen
  */
 public class OlcbProgrammerManagerTest {
-    protected SystemConnectionMemo adapterMemo = new OlcbSystemConnectionMemoScaffold();
+    protected OlcbSystemConnectionMemoScaffold adapterMemo = new OlcbSystemConnectionMemoScaffold();
+    protected OlcbTestHelper h = new OlcbTestHelper();
+
     @Test
     public void testCtor() {
         OlcbProgrammerManager s = new OlcbProgrammerManager(adapterMemo);
         Assert.assertNotNull(s);
+        h.expectMessage(new IdentifyProducersMessage(h.iface.getNodeId(), OlcbProgrammer.IS_PROGRAMMINGTRACK_EVENT));
     }
 
     @BeforeEach
     public void setUp() {
         JUnitUtil.setUp();
+        adapterMemo.setInterface(h.iface);
     }
 
     @AfterEach
     public void tearDown() {
+        h.dispose();
+        adapterMemo.dispose();
         JUnitUtil.tearDown();
     }
 }
