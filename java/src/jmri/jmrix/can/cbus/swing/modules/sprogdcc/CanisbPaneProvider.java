83948c9e369648569326a4e56aa785d6a31062fd
Andrew Crosland <andrew@crosland.me.uk>
2023-01-12 13:02:19 +0000

    Correct default.lcf and typo

--- java/src/jmri/jmrix/can/cbus/swing/modules/sprogdcc/CanisbPaneProvider.java
+++ java/src/jmri/jmrix/can/cbus/swing/modules/sprogdcc/CanisbPaneProvider.java
@@ -37,7 +37,7 @@ public class CanisbPaneProvider extends CbusConfigPaneProvider {
     public static final int CAN_DIAGNOSTICS_LO = 15;
     public static final int CAN_DIAGNOSTICS_HI = 16;
     
-    // These may be overridden in scripts for unbusual use cases
+    // These may be overridden in scripts for unusual use cases
     public static int MIN_CANID = 100;
     public static int MAX_CANID = 127;
     public static int MIN_NN = 65520;
