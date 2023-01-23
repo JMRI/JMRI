bcb49352cbabb5cecaf0014864db4dced583805a
Bob Jacobsen <jacobsen@mac.com>
2023-01-23 14:39:53 -0500

    proper delay to work on polled systems

--- jython/SetAllTurnoutsClosed.py
+++ jython/SetAllTurnoutsClosed.py
@@ -1,11 +1,11 @@
-# Sample script to set all un-closed turnouts to Closed
+# Sample script to set all un-CLOSED turnouts to CLOSED
 #
 # After this script is turn, all the Turnouts defined in JMRI
 # should be in the CLOSED state.
 #
 # Note that some "Turnouts" may actually drive other things on the layout,
 # such as signal heads. This script should be run _before_ anything that
-# sets those.  
+# sets those.
 #
 # Part of the JMRI distribution
 
@@ -13,18 +13,23 @@ import jmri
 
 from time import sleep
 
-toCnt = 0
-chgCnt = 0
+class TurnoutSetClosed(jmri.jmrit.automat.AbstractAutomaton) :
+        # handle() is called just one when it returns false.
+        def handle(self):
+            # loop thru all defined turnouts
+            toCnt = 0
+            chgCnt = 0
+            for to in turnouts.getNamedBeanSet():
+              toCnt += 1
+              cs = to.getState()
+              if (cs != CLOSED) :
+                chgCnt += 1
+                to.setState(CLOSED)
+                self.waitMsec(100) # pause for 1/10 second between commands
+            print str(toCnt) + " turnouts found, " + str(chgCnt) + " changed to CLOSED"
+            return FALSE
 
-# loop thru all defined turnouts
-for to in turnouts.getNamedBeanSet():
-  toCnt += 1
-  cs = to.getState()
-  if (cs != CLOSED) :
-    chgCnt += 1
-    to.setState(CLOSED)
-    sleep(0.1) #pause for 1/10 second between commands
-print str(toCnt) + " turnouts found, " + str(chgCnt) + " changed to CLOSED"
+TurnoutSetClosed().start()
 
 
 
