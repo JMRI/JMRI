137494834cc1288c306274cadf7697b29063c141
Balazs Racz <balazs.racz@gmail.com>
2023-03-14 23:30:27 +0100

    Adds OlcbReporter and OlcbReporterManager classes.

---
+++ java/src/jmri/jmrix/openlcb/OlcbReporter.java
@@ -0,0 +1,295 @@
+package jmri.jmrix.openlcb;
+
+import jmri.NamedBean;
+import jmri.Sensor;
+import jmri.implementation.AbstractSensor;
+import org.openlcb.OlcbInterface;
+import org.openlcb.implementations.BitProducerConsumer;
+import org.openlcb.implementations.EventTable;
+import org.openlcb.implementations.VersionedValueListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nonnull;
+import javax.annotation.OverridingMethodsMustInvokeSuper;
+import java.util.TimerTask;
+
+/**
+ * Extend jmri.AbstractSensor for OpenLCB controls.
+ *
+ * @author Bob Jacobsen Copyright (C) 2008, 2010, 2011
+ */
+public class OlcbReporter extends AbstractSensor {
+
+    static final int ON_TIME = 500; // time that sensor is active after being tripped
+
+    OlcbAddress addrActive;    // go to active state
+    OlcbAddress addrInactive;  // go to inactive state
+    final OlcbInterface iface;
+
+    VersionedValueListener<Boolean> sensorListener;
+    BitProducerConsumer pc;
+    EventTable.EventTableEntryHolder activeEventTableEntryHolder = null;
+    EventTable.EventTableEntryHolder inactiveEventTableEntryHolder = null;
+    private static final boolean DEFAULT_IS_AUTHORITATIVE = true;
+    private static final boolean DEFAULT_LISTEN = true;
+    private static final int PC_DEFAULT_FLAGS = BitProducerConsumer.DEFAULT_FLAGS &
+            (~BitProducerConsumer.LISTEN_INVALID_STATE);
+
+    private TimerTask timerTask;
+
+    public OlcbReporter(String prefix, String address, OlcbInterface iface) {
+        super(prefix + "S" + address);
+        this.iface = iface;
+        init(address);
+    }
+
+    /**
+     * Common initialization for both constructors.
+     * <p>
+     *
+     */
+    private void init(String address) {
+        // build local addresses
+        OlcbAddress a = new OlcbAddress(address);
+        OlcbAddress[] v = a.split();
+        if (v == null) {
+            log.error("Did not find usable system name: {}", address);
+            return;
+        }
+        switch (v.length) {
+            case 1:
+                // momentary sensor
+                addrActive = v[0];
+                addrInactive = null;
+                break;
+            case 2:
+                addrActive = v[0];
+                addrInactive = v[1];
+                break;
+            default:
+                log.error("Can't parse OpenLCB Sensor system name: {}", address);
+        }
+
+    }
+
+    /**
+     * Helper function that will be invoked after construction once the properties have been
+     * loaded. Used specifically for preventing double initialization when loading sensors from
+     * XML.
+     */
+    void finishLoad() {
+        int flags = PC_DEFAULT_FLAGS;
+        flags = OlcbUtils.overridePCFlagsFromProperties(this, flags);
+        log.debug("Sensor Flags: default {} overridden {} listen bit {}", PC_DEFAULT_FLAGS, flags,
+                    BitProducerConsumer.LISTEN_EVENT_IDENTIFIED);
+        disposePc();
+        if (addrInactive == null) {
+            pc = new BitProducerConsumer(iface, addrActive.toEventID(), BitProducerConsumer.nullEvent, flags);
+
+            sensorListener = new VersionedValueListener<Boolean>(pc.getValue()) {
+                @Override
+                public void update(Boolean value) {
+                    setOwnState(value ? Sensor.ACTIVE : Sensor.INACTIVE);
+                    if (value) {
+                        setTimeout();
+                    }
+                }
+            };
+        } else {
+            pc = new BitProducerConsumer(iface, addrActive.toEventID(),
+                    addrInactive.toEventID(), flags);
+            sensorListener = new VersionedValueListener<Boolean>(pc.getValue()) {
+                @Override
+                public void update(Boolean value) {
+                    setOwnState(value ? Sensor.ACTIVE : Sensor.INACTIVE);
+                }
+            };
+        }
+        activeEventTableEntryHolder = iface.getEventTable().addEvent(addrActive.toEventID(), getEventName(true));
+        if (addrInactive != null) {
+            inactiveEventTableEntryHolder = iface.getEventTable().addEvent(addrInactive.toEventID(), getEventName(false));
+        }
+    }
+
+    /**
+     * Computes the display name of a given event to be entered into the Event Table.
+     * @param isActive true for sensor active, false for inactive.
+     * @return user-visible string to represent this event.
+     */
+    private String getEventName(boolean isActive) {
+        String name = getUserName();
+        if (name == null) name = mSystemName;
+        String msgName = isActive ? "SensorActiveEventName": "SensorInactiveEventName";
+        return Bundle.getMessage(msgName, name);
+    }
+
+    /**
+     * Updates event table entries when the user name changes.
+     * @param s new user name
+     * @throws BadUserNameException see {@link NamedBean}
+     */
+    @Override
+    @OverridingMethodsMustInvokeSuper
+    public void setUserName(String s) throws BadUserNameException {
+        super.setUserName(s);
+        if (activeEventTableEntryHolder != null) {
+            activeEventTableEntryHolder.getEntry().updateDescription(getEventName(true));
+        }
+        if (inactiveEventTableEntryHolder != null) {
+            inactiveEventTableEntryHolder.getEntry().updateDescription(getEventName(false));
+        }
+    }
+
+    /**
+     * Request an update on status by sending an OpenLCB message.
+     */
+    @Override
+    public void requestUpdateFromLayout() {
+        if (pc != null) {
+            pc.resetToDefault();
+            pc.sendQuery();
+        }
+    }
+
+    /**
+     * User request to set the state, which means that we broadcast that to all
+     * listeners by putting it out on CBUS. In turn, the code in this class
+     * should use setOwnState to handle internal sets and bean notifies.
+     *
+     */
+    @Override
+    public void setKnownState(int s) {
+        if (s == Sensor.ACTIVE) {
+            sensorListener.setFromOwnerWithForceNotify(true);
+            if (addrInactive == null) {
+                setTimeout();
+            }
+        } else if (s == Sensor.INACTIVE) {
+            sensorListener.setFromOwnerWithForceNotify(false);
+        } else if (s == Sensor.UNKNOWN) {
+            if (pc != null) {
+                pc.resetToDefault();
+            }
+        }
+        setOwnState(s);
+    }
+
+    /**
+     * Have sensor return to inactive after delay, used if no inactive event was
+     * specified
+     */
+    void setTimeout() {
+        timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                timerTask = null;
+                jmri.util.ThreadingUtil.runOnGUI(() -> setKnownState(Sensor.INACTIVE));
+            }
+        };
+        jmri.util.TimerUtil.schedule(timerTask, ON_TIME);
+    }
+
+    /**
+     * Changes how the turnout reacts to inquire state events. With authoritative == false the
+     * state will always be reported as UNKNOWN to the layout when queried.
+     *
+     * @param authoritative whether we should respond true state or unknown to the layout event
+     *                      state inquiries.
+     */
+    public void setAuthoritative(boolean authoritative) {
+        boolean recreate = (authoritative != isAuthoritative()) && (pc != null);
+        setProperty(OlcbUtils.PROPERTY_IS_AUTHORITATIVE, authoritative);
+        if (recreate) {
+            finishLoad();
+        }
+    }
+
+    /**
+     * @return whether this producer/consumer is enabled to return state to the layout upon queries.
+     */
+    public boolean isAuthoritative() {
+        Boolean value = (Boolean) getProperty(OlcbUtils.PROPERTY_IS_AUTHORITATIVE);
+        if (value != null) {
+            return value;
+        }
+        return DEFAULT_IS_AUTHORITATIVE;
+    }
+
+    @Override
+    public void setProperty(@Nonnull String key, Object value) {
+        Object old = getProperty(key);
+        super.setProperty(key, value);
+        if (value.equals(old)) return;
+        if (pc == null) return;
+        finishLoad();
+    }
+
+    /**
+     * @return whether this producer/consumer is always listening to state declaration messages.
+     */
+    public boolean isListeningToStateMessages() {
+        Boolean value = (Boolean) getProperty(OlcbUtils.PROPERTY_LISTEN);
+        if (value != null) {
+            return value;
+        }
+        return DEFAULT_LISTEN;
+    }
+
+    /**
+     * Changes how the turnout reacts to state declaration messages. With listen == true state
+     * declarations will update local state at all times. With listen == false state declarations
+     * will update local state only if local state is unknown.
+     *
+     * @param listen whether we should always listen to state declaration messages.
+     */
+    public void setListeningToStateMessages(boolean listen) {
+        boolean recreate = (listen != isListeningToStateMessages()) && (pc != null);
+        setProperty(OlcbUtils.PROPERTY_LISTEN, listen);
+        if (recreate) {
+            finishLoad();
+        }
+    }
+
+    /*
+     * since the events that drive a sensor can be whichever state a user
+     * wants, the order of the event pair determines what is the 'active' state
+     */
+    @Override
+    public boolean canInvert() {
+        return false;
+    }
+
+    @Override
+    public void dispose() {
+        disposePc();
+        if (timerTask!=null) timerTask.cancel();
+        super.dispose();
+    }
+
+    private void disposePc() {
+        if (sensorListener != null) {
+            sensorListener.release();
+            sensorListener = null;
+        }
+        if (pc != null) {
+            pc.release();
+            pc = null;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * Sorts by decoded EventID(s)
+     */
+    @CheckReturnValue
+    @Override
+    public int compareSystemNameSuffix(@Nonnull String suffix1, @Nonnull String suffix2, @Nonnull NamedBean n) {
+        return OlcbAddress.compareSystemNameSuffix(suffix1, suffix2);
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(OlcbReporter.class);
+
+}
---
+++ java/src/jmri/jmrix/openlcb/OlcbReporterManager.java
@@ -0,0 +1,218 @@
+package jmri.jmrix.openlcb;
+
+import java.util.*;
+
+import javax.annotation.Nonnull;
+import jmri.BooleanPropertyDescriptor;
+import jmri.JmriException;
+import jmri.NamedBean;
+import jmri.NamedBeanPropertyDescriptor;
+import jmri.Sensor;
+import jmri.jmrix.can.CanListener;
+import jmri.jmrix.can.CanMessage;
+import jmri.jmrix.can.CanReply;
+import jmri.jmrix.can.CanSystemConnectionMemo;
+import org.openlcb.OlcbInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Manage the OpenLCB-specific Sensor implementation.
+ *
+ * System names are "MSnnn", where M is the user configurable system prefix,
+ * nnn is the sensor number without padding.
+ *
+ * @author Bob Jacobsen Copyright (C) 2008, 2010
+ */
+public class OlcbSensorManager extends jmri.managers.AbstractSensorManager implements CanListener {
+
+    // Whether we accumulate partially loaded objects in pendingSensors.
+    private boolean isLoading = false;
+    // Turnouts that are being loaded from XML.
+    private final ArrayList<OlcbSensor> pendingSensors = new ArrayList<>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @Nonnull
+    public CanSystemConnectionMemo getMemo() {
+        return (CanSystemConnectionMemo) memo;
+    }
+
+    @Override
+    @Nonnull
+    public List<NamedBeanPropertyDescriptor<?>> getKnownBeanProperties() {
+        List<NamedBeanPropertyDescriptor<?>> l = new ArrayList<>();
+        l.add(new BooleanPropertyDescriptor(OlcbUtils.PROPERTY_IS_AUTHORITATIVE, OlcbTurnout
+                .DEFAULT_IS_AUTHORITATIVE) {
+            @Override
+            public String getColumnHeaderText() {
+                return Bundle.getMessage("OlcbStateAuthHeader");
+            }
+
+            @Override
+            public boolean isEditable(NamedBean bean) {
+                return OlcbUtils.isOlcbBean(bean);
+            }
+        });
+        l.add(new BooleanPropertyDescriptor(OlcbUtils.PROPERTY_LISTEN, OlcbTurnout
+                .DEFAULT_LISTEN) {
+            @Override
+            public String getColumnHeaderText() {
+                return Bundle.getMessage("OlcbStateListenHeader");
+            }
+
+            @Override
+            public boolean isEditable(NamedBean bean) {
+                return OlcbUtils.isOlcbBean(bean);
+            }
+        });
+        return l;
+    }
+
+    // to free resources when no longer used
+    @Override
+    public void dispose() {
+        getMemo().getTrafficController().removeCanListener(this);
+        super.dispose();
+    }
+
+    // Implemented ready for new system connection memo
+    public OlcbSensorManager(CanSystemConnectionMemo memo) {
+        super(memo);
+        memo.getTrafficController().addCanListener(this);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IllegalArgumentException when SystemName can't be converted
+     */
+    @Override
+    @Nonnull
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings( value = "SLF4J_FORMAT_SHOULD_BE_CONST",
+        justification = "passing exception text")
+    protected Sensor createNewSensor(@Nonnull String systemName, String userName) throws IllegalArgumentException {
+        String addr = systemName.substring(getSystemNamePrefix().length());
+        // first, check validity
+        try {
+            validateSystemNameFormat(systemName,Locale.getDefault());
+        } catch (jmri.NamedBean.BadSystemNameException e) {
+            log.error(e.getMessage());
+            throw e;
+        }
+        // OK, make
+        OlcbSensor s = new OlcbSensor(getSystemPrefix(), addr, memo.get(OlcbInterface.class));
+        s.setUserName(userName);
+
+        synchronized (pendingSensors) {
+            if (isLoading) {
+                pendingSensors.add(s);
+            } else {
+                s.finishLoad();
+            }
+        }
+        return s;
+    }
+
+    /**
+     * This function is invoked before an XML load is started. We defer initialization of the
+     * newly created Sensors until finishLoad because the feedback type might be changing as we
+     * are parsing the XML.
+     */
+    public void startLoad() {
+        log.debug("Sensor manager : start load");
+        synchronized (pendingSensors) {
+            isLoading = true;
+        }
+    }
+
+    /**
+     * This function is invoked after the XML load is complete and all Sensors are instantiated
+     * and their feedback type is read in. We use this hook to finalize the construction of the
+     * OpenLCB objects whose instantiation was deferred until the feedback type was known.
+     */
+    public void finishLoad() {
+        log.debug("Sensor manager : finish load");
+        synchronized (pendingSensors) {
+            pendingSensors.forEach(OlcbSensor::finishLoad);
+            pendingSensors.clear();
+            isLoading = false;
+        }
+    }
+
+    @Override
+    public boolean allowMultipleAdditions(@Nonnull String systemName) {
+        return false;
+    }
+
+    @Override
+    @Nonnull
+    public String createSystemName(@Nonnull String curAddress, @Nonnull String prefix) throws JmriException {
+        String tmpPrefix = prefix + typeLetter();
+        String tmpSName  = tmpPrefix + curAddress;
+        try {
+            OlcbAddress.validateSystemNameFormat(tmpSName,Locale.getDefault(),tmpPrefix);
+        }
+        catch ( jmri.NamedBean.BadSystemNameException ex ){
+            throw new JmriException(ex.getMessage());
+        }
+        // don't check for integer; should check for validity here
+        return prefix + typeLetter() + curAddress;
+    }
+
+    @Override
+    @javax.annotation.Nonnull
+    @javax.annotation.CheckReturnValue
+    public String getNextValidSystemName(@Nonnull NamedBean currentBean) throws JmriException {
+        throw new jmri.JmriException("getNextValidSystemName should not have been called");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getEntryToolTip() {
+        return Bundle.getMessage("AddSensorEntryToolTip");
+    }
+
+    // listen for sensors, creating them as needed
+    @Override
+    public void reply(CanReply l) {
+        // doesn't do anything, because for now
+        // we want you to create manually
+    }
+
+    @Override
+    public void message(CanMessage l) {
+        // doesn't do anything, because
+        // messages come from us
+    }
+
+    /**
+     * No mechanism currently exists to request status updates from all layout
+     * sensors.
+     */
+    @Override
+    public void updateAll() {
+        // no current mechanisim to request status updates from all layout sensors
+    }
+
+    /**
+     * Validates to OpenLCB format.
+     * {@inheritDoc}
+     */
+    @Override
+    @Nonnull
+    public String validateSystemNameFormat(@Nonnull String name, @Nonnull java.util.Locale locale) throws jmri.NamedBean.BadSystemNameException {
+        name = super.validateSystemNameFormat(name,locale);
+        name = OlcbAddress.validateSystemNameFormat(name,locale,getSystemNamePrefix());
+        return name;
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(OlcbSensorManager.class);
+
+}
+
+
