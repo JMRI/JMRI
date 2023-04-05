ec51ad0e5e91af1b49a699575052f1537f9a4a79
Daniel <db123@bergqvist.se>
2023-04-05 18:48:39 +0200

    Update LogixNG action WebRequest

--- java/test/jmri/jmrit/logixng/actions/WebRequestTest.java
+++ java/test/jmri/jmrit/logixng/actions/WebRequestTest.java
@@ -1,23 +1,18 @@
 package jmri.jmrit.logixng.actions;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyVetoException;
 import java.util.ArrayList;
 
 import jmri.*;
 import jmri.jmrit.logixng.*;
-import jmri.jmrit.logixng.expressions.ExpressionSensor;
+import jmri.jmrit.logixng.expressions.ExpressionLight;
 import jmri.jmrit.logixng.implementation.DefaultConditionalNGScaffold;
-import jmri.jmrit.logixng.implementation.DefaultSymbolTable;
 import jmri.jmrit.logixng.util.LineEnding;
 import jmri.jmrit.logixng.util.parser.ParserException;
-import jmri.util.JUnitAppender;
 import jmri.util.JUnitUtil;
 
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -31,85 +26,11 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
 
     private LogixNG _logixNG;
     private ConditionalNG _conditionalNG;
-
     private GlobalVariable _responseCodeVariable;
     private GlobalVariable _replyVariable;
     private GlobalVariable _cookiesVariable;
 
-//    private WebRequest webRequest;
-//    private Light light;
 
-/*
-//    @Ignore
-    @Test
-    public void testWebRequest() throws JmriException {
-        webRequest.getSelectUrl().setValue("https://jmri.bergqvist.se/LogixNG_WebRequest_Test.php");
-//        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Get);
-        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Post);
-        webRequest.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-        webRequest.getParameters().add(new WebRequest.Parameter("address", SymbolTable.InitialValueType.String, "Adenravagen 2"));
-        conditionalNG.execute();
-    }
-
-//    @Ignore
-    @Test
-    public void testWebRequest2() throws JmriException {
-        MaleSocket socketWebRequest1 = conditionalNG.getFemaleSocket().getConnectedSocket();
-        socketWebRequest1.addLocalVariable("cookies", SymbolTable.InitialValueType.String, "");
-
-        conditionalNG.unregisterListeners();
-
-        WebRequest webRequest2 = new WebRequest("IQDA322", null);
-        MaleSocket socketWebRequest2 =
-                InstanceManager.getDefault(DigitalActionManager.class).registerAction(webRequest2);
-        socketWebRequest1.getChild(0).connect(socketWebRequest2);
-
-        WebRequest webRequest3 = new WebRequest("IQDA323", null);
-        MaleSocket socketWebRequest3 =
-                InstanceManager.getDefault(DigitalActionManager.class).registerAction(webRequest3);
-        socketWebRequest2.getChild(0).connect(socketWebRequest3);
-
-        webRequest2.setUseThread(false);
-        webRequest3.setUseThread(false);
-
-        conditionalNG.registerListeners();
-
-        webRequest.setLocalVariableForCookies("cookies");
-        webRequest2.setLocalVariableForCookies("cookies");
-        webRequest3.setLocalVariableForCookies("cookies");
-
-
-
-
-
-        webRequest.getSelectUrl().setValue("https://www.modulsyd.se/forum_3.3/index.php?aaaa=1");
-        webRequest2.getSelectUrl().setValue("https://www.modulsyd.se/forum_3.3/index.php?aaaa=2");
-        webRequest3.getSelectUrl().setValue("https://www.modulsyd.se/forum_3.3/index.php?aaaa=3");
-//        webRequest3.getSelectUrl().setValue("https://jmri.bergqvist.se/LogixNG_WebRequest_Test.php");
-
-
-        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Get);
-        webRequest2.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Get);
-        webRequest3.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Get);
-/*
-        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Post);
-        webRequest2.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Post);
-        webRequest3.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Post);
-        webRequest.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-        webRequest2.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-        webRequest3.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-        webRequest.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-        webRequest2.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-        webRequest3.getParameters().add(new WebRequest.Parameter("name", SymbolTable.InitialValueType.String, "Daniel123"));
-*./
-//        logixNG.setEnabled(true);
-        conditionalNG.execute();
-
-//        webRequest.setLocalVariableForCookies("cookies");
-//        conditionalNG.execute();
-//        conditionalNG.execute();
-    }
-*/
     @Override
     public ConditionalNG getConditionalNG() {
         return _conditionalNG;
@@ -171,8 +92,25 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
                 "                              ! A1%n" +
                 "                                 Log local variables ::: Use default%n" +
                 "                              ! A2%n" +
-                "                                 Socket not connected%n",
-                WEB_REQUEST_URL, WEB_REQUEST_URL);
+                "                                 Socket not connected%n" +
+                "   ConditionalNG: Test post request%n" +
+                "      ! A%n" +
+                "         If Then Else. Execute on change ::: Use default%n" +
+                "            ? If%n" +
+                "               Light TestPostRequestLight is On ::: Use default%n" +
+                "            ! Then%n" +
+                "               Web request for %s ::: Use default%n" +
+                "                  ! Execute%n" +
+                "                     Many ::: Use default%n" +
+                "                        ! A1%n" +
+                "                           Log local variables ::: Use default%n" +
+                "                        ! A2%n" +
+                "                           Socket not connected%n" +
+                "            ! Else%n" +
+                "               Socket not connected%n",
+                WEB_REQUEST_URL,
+                WEB_REQUEST_URL,
+                WEB_REQUEST_URL);
     }
 
     @Override
@@ -238,6 +176,7 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
         WebRequest webRequest = new WebRequest("IQDA103", null);
         webRequest.setUseThread(false);
         webRequest.getSelectUrl().setValue(WEB_REQUEST_URL);
+        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Get);
         webRequest.getParameters().add(new WebRequest.Parameter("action", SymbolTable.InitialValueType.String, "throw"));
         webRequest.getParameters().add(new WebRequest.Parameter("turnout", SymbolTable.InitialValueType.LocalVariable, "turnout"));
         webRequest.setLocalVariableForResponseCode("responseCode");
@@ -328,6 +267,7 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
         WebRequest webRequest = new WebRequest("IQDA203", null);
         webRequest.setUseThread(false);
         webRequest.getSelectUrl().setValue(WEB_REQUEST_URL);
+        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Get);
         webRequest.getParameters().add(new WebRequest.Parameter("action", SymbolTable.InitialValueType.String, "cookies"));
         webRequest.getParameters().add(new WebRequest.Parameter("cookie", SymbolTable.InitialValueType.LocalVariable, "sensor"));
         webRequest.setLocalVariableForResponseCode("responseCode");
@@ -351,138 +291,67 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
         maleSocket = InstanceManager.getDefault(DigitalActionManager.class).registerAction(log);
         many.getChild(0).connect(maleSocket);
     }
-/*
+
     @Test
-    public void testCookies() throws JmriException {
+    public void testPostRequest() throws JmriException {
         _responseCodeVariable.setValue(null);
         _replyVariable.setValue(null);
-        InstanceManager.getDefault(SensorManager.class).getByUserName("TestCookiesSensor").setState(Sensor.ACTIVE);
+        InstanceManager.getDefault(LightManager.class).getByUserName("TestPostRequestLight").setState(Light.ON);
         Assert.assertEquals(200, (int)_responseCodeVariable.getValue());
-        Assert.assertEquals("Turnout MiamiWest is thrown", _replyVariable.getValue());
-
-        _responseCodeVariable.setValue(null);
-        _replyVariable.setValue(null);
-        InstanceManager.getDefault(TurnoutManager.class).getByUserName("Chicago32").setState(Turnout.THROWN);
-        Assert.assertEquals(200, (int)_responseCodeVariable.getValue());
-        Assert.assertEquals("Turnout Chicago32 is thrown", _replyVariable.getValue());
-
-        _responseCodeVariable.setValue(null);
-        _replyVariable.setValue(null);
-        InstanceManager.getDefault(TurnoutManager.class).getByUserName("TorontoFirst").setState(Turnout.THROWN);
-        Assert.assertEquals(200, (int)_responseCodeVariable.getValue());
-        Assert.assertEquals("Turnout TorontoFirst is thrown", _replyVariable.getValue());
+        Assert.assertEquals("Logged in. First name: Green, last name: Tomato", _replyVariable.getValue());
     }
 
-    private void setupCookiesConditionalNG() throws SocketAlreadyConnectedException, ParserException {
+    private void setupPostRequestConditionalNG() throws SocketAlreadyConnectedException, ParserException {
 
-        InstanceManager.getDefault(SensorManager.class).newSensor("IS1", "TestCookiesSensor");
+        Light testPostRequestLight = InstanceManager.getDefault(LightManager.class).newLight("IL1", "TestPostRequestLight");
 
-        _conditionalNG = new DefaultConditionalNGScaffold("IQC2", "Test cookies");  // NOI18N;
+        _conditionalNG = new DefaultConditionalNGScaffold("IQC3", "Test post request");  // NOI18N;
         InstanceManager.getDefault(ConditionalNG_Manager.class).register(_conditionalNG);
         _logixNG.addConditionalNG(_conditionalNG);
         _conditionalNG.setRunDelayed(false);
         _conditionalNG.setEnabled(true);
 
-        IfThenElse ifThenElse = new IfThenElse("IQDA201", null);
+        IfThenElse ifThenElse = new IfThenElse("IQDA301", null);
         MaleSocket maleSocket =
                 InstanceManager.getDefault(DigitalActionManager.class).registerAction(ifThenElse);
         _conditionalNG.getChild(0).connect(maleSocket);
 
-        ExpressionSensor expressionSensor = new ExpressionSensor("IQDE201", null);
+        ExpressionLight expressionLight = new ExpressionLight("IQDE301", null);
+        expressionLight.getSelectNamedBean().setNamedBean(testPostRequestLight);
+        expressionLight.set_Is_IsNot(Is_IsNot_Enum.Is);
+        expressionLight.setStateAddressing(NamedBeanAddressing.Direct);
+        expressionLight.setBeanState(ExpressionLight.LightState.On);
         maleSocket =
-                InstanceManager.getDefault(DigitalExpressionManager.class).registerExpression(expressionSensor);
+                InstanceManager.getDefault(DigitalExpressionManager.class).registerExpression(expressionLight);
         ifThenElse.getChild(0).connect(maleSocket);
 
-        WebRequest webRequest = new WebRequest("IQDA202", null);
+        WebRequest webRequest = new WebRequest("IQDA302", null);
         webRequest.setUseThread(false);
         webRequest.getSelectUrl().setValue(WEB_REQUEST_URL);
-        webRequest.getParameters().add(new WebRequest.Parameter("action", SymbolTable.InitialValueType.String, "cookies"));
-        webRequest.getParameters().add(new WebRequest.Parameter("turnout", SymbolTable.InitialValueType.LocalVariable, "turnout"));
+        webRequest.getSelectRequestMethod().setEnum(WebRequest.RequestMethodType.Post);
+        webRequest.getParameters().add(new WebRequest.Parameter("action", SymbolTable.InitialValueType.String, "login"));
+        webRequest.getParameters().add(new WebRequest.Parameter("fname", SymbolTable.InitialValueType.String, "Green"));
+        webRequest.getParameters().add(new WebRequest.Parameter("lname", SymbolTable.InitialValueType.String, "Tomato"));
         webRequest.setLocalVariableForResponseCode("responseCode");
         webRequest.setLocalVariableForReplyContent("reply");
         webRequest.getSelectReplyType().setEnum(WebRequest.ReplyType.String);
 //        webRequest.getSelectLineEnding().setEnum(LineEnding.MacLinuxLf);
         webRequest.getSelectLineEnding().setEnum(LineEnding.Space);
-//        actionWebRequest.getSelectNamedBean().setNamedBean(light);
-//        actionWebRequest.getSelectEnum().setEnum(ActionLight.LightState.On);
         maleSocket = InstanceManager.getDefault(DigitalActionManager.class).registerAction(webRequest);
         ifThenElse.getChild(1).connect(maleSocket);
         // These are used by super class for its testing
         _base = webRequest;
         _baseMaleSocket = maleSocket;
 
-        DigitalMany many = new DigitalMany("IQDA001", null);
+        DigitalMany many = new DigitalMany("IQDA303", null);
         maleSocket =
                 InstanceManager.getDefault(DigitalActionManager.class).registerAction(many);
-//        maleSocket.addLocalVariable("turnout", SymbolTable.InitialValueType.None, null);
         webRequest.getChild(0).connect(maleSocket);
-
-//        DigitalFormula formula = new DigitalFormula(InstanceManager.getDefault(DigitalActionManager.class).getAutoSystemName(), null);
-//        formula.setFormula("reply = reply.trim()");
-//        formula.setFormula("");
-//        maleSocket = InstanceManager.getDefault(DigitalActionManager.class).registerAction(formula);
-//        many.getChild(0).connect(maleSocket);
 
         LogLocalVariables log = new LogLocalVariables(InstanceManager.getDefault(DigitalActionManager.class).getAutoSystemName(), null);
         maleSocket = InstanceManager.getDefault(DigitalActionManager.class).registerAction(log);
         many.getChild(0).connect(maleSocket);
-
-
-
-
-/*
-        IfThenElse ifThenElse = new IfThenElse("IQDA321", null);
-        maleSocket =
-                InstanceManager.getDefault(DigitalActionManager.class).registerAction(ifThenElse);
-        conditionalNG.getChild(0).connect(maleSocket);
-
-
-
-
-
-        WebRequest webRequest = new WebRequest(InstanceManager.getDefault(DigitalActionManager.class).getAutoSystemName(), null);
-        webRequest.setUseThread(false);
-        webRequest.getSelectUrl().setValue(WEB_REQUEST_URL);
-//        actionWebRequest.getSelectNamedBean().setNamedBean(light);
-//        actionWebRequest.getSelectEnum().setEnum(ActionLight.LightState.On);
-        MaleSocket socket = InstanceManager.getDefault(DigitalActionManager.class).registerAction(webRequest);
-        ifThenElse.getChild(1).connect(socket);
-
-
-
-        // These are used by super class for its testing
-        _base = webRequest;
-        _baseMaleSocket = socket;
-*./
-
-
-
-
-        // If sensor1 is active
-        // WebRequest
-
-/*
-        ConditionalNG
-            For all turnouts
-                ListenOnBeans
-                    WebRequest
-                        Digital formula: response = response.trim()
-                        Digital formula: expectedResponse = String.format("Turnout %s is thrown", turnout.getSystemName())
-                        IfThenElse
-                            LocalVariable response is expectedResponse
-
-
-        // Test cookies
-        // Test post
-
-
-*./
     }
-*/
-
-
-
-
 
 
     // The minimal setup for log4J
@@ -500,8 +369,6 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
         _category = Category.ITEM;
         _isExternal = true;
 
-//        light = InstanceManager.getDefault(LightManager.class).provide("IL1");
-//        light.setCommandedState(Light.OFF);
         _logixNG = InstanceManager.getDefault(LogixNG_Manager.class).createLogixNG("A logixNG");
 
         _responseCodeVariable = InstanceManager.getDefault(GlobalVariableManager.class).createGlobalVariable("responseCode");
@@ -509,37 +376,7 @@ public class WebRequestTest extends AbstractDigitalActionTestBase {
 
         setupThrowTurnoutsConditionalNG();
         setupCookiesConditionalNG();
-//        setupTestPostConditionalNG();
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-        webRequest = new WebRequest(InstanceManager.getDefault(DigitalActionManager.class).getAutoSystemName(), null);
-        webRequest.setUseThread(false);
-        webRequest.getSelectUrl().setValue("https://www.jmri.org/");
-//        actionWebRequest.getSelectNamedBean().setNamedBean(light);
-//        actionWebRequest.getSelectEnum().setEnum(ActionLight.LightState.On);
-        MaleSocket socket = InstanceManager.getDefault(DigitalActionManager.class).registerAction(webRequest);
-        conditionalNG.getChild(0).connect(socket);
-
-        _base = webRequest;
-        _baseMaleSocket = socket;
-
-*/
+        setupPostRequestConditionalNG();
 
         if (! _logixNG.setParentForAllChildren(new ArrayList<>())) throw new RuntimeException();
         _logixNG.activate();
