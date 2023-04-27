cf0ccc7044e5bb6ed81b96aa7fdb6c487e99e021
Steve Young <icklesteve@yahoo.com>
2023-04-27 12:48:08 +0100

    fix testing for Assert Warn Message when Error Message
    assertWarnMessage currently passes Error Messages.
    This PR fixes the tests to check for Error, not Warn log messages.

--- java/test/jmri/jmrit/logixng/expressions/ExpressionReporterTest.java
+++ java/test/jmri/jmrit/logixng/expressions/ExpressionReporterTest.java
@@ -564,7 +564,7 @@ public class ExpressionReporterTest extends AbstractDigitalExpressionTestBase {
 
         expressionReporter.getSelectNamedBean().setNamedBean("A non existent reporter");
         Assert.assertNull("reporter handle is null", expressionReporter.getSelectNamedBean().getNamedBean());
-        JUnitAppender.assertWarnMessage("Reporter \"A non existent reporter\" is not found");
+        JUnitAppender.assertErrorMessage("Reporter \"A non existent reporter\" is not found");
 
         expressionReporter.getSelectNamedBean().setNamedBean(reporter13.getSystemName());
         Assert.assertTrue("reporter is correct", reporter13 == expressionReporter.getSelectNamedBean().getNamedBean().getBean());
