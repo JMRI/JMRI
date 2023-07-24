72916615eead541f7b897cefb24183d0894c2918
Steve Young <icklesteve@yahoo.com>
2023-07-24 13:34:07 +0100

    Update DCCppMonActionTest.java

--- java/test/jmri/jmrix/dccpp/swing/mon/DCCppMonActionTest.java
+++ java/test/jmri/jmrix/dccpp/swing/mon/DCCppMonActionTest.java
@@ -1,41 +1,44 @@
 package jmri.jmrix.dccpp.swing.mon;
 
-import java.awt.GraphicsEnvironment;
-
 import jmri.util.JUnitUtil;
 
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
-import org.junit.Assume;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 /**
  * Test simple functioning of DCCppMonAction
  *
  * @author Paul Bender Copyright (C) 2016
  */
+@DisabledIfSystemProperty(named = "java.awt.headless", matches = "true" )
 public class DCCppMonActionTest {
 
-    
-
     @Test
-    public void testMemoCtor() {
-        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
+    public void testDccPpMemoCtor() {
         DCCppMonAction action = new DCCppMonAction();
         Assert.assertNotNull("exists", action);
     }
 
+    private jmri.jmrix.dccpp.DCCppSystemConnectionMemo memo = null;
+
     @BeforeEach
     public void setUp() {
         JUnitUtil.setUp();
+        JUnitUtil.resetInstanceManager();
         jmri.jmrix.dccpp.DCCppInterfaceScaffold t = new jmri.jmrix.dccpp.DCCppInterfaceScaffold(new jmri.jmrix.dccpp.DCCppCommandStation());
-        jmri.jmrix.dccpp.DCCppSystemConnectionMemo memo = new jmri.jmrix.dccpp.DCCppSystemConnectionMemo(t);
+        memo = new jmri.jmrix.dccpp.DCCppSystemConnectionMemo(t);
 
         jmri.InstanceManager.store(memo, jmri.jmrix.dccpp.DCCppSystemConnectionMemo.class);
 
     }
 
     @AfterEach
-    public void tearDown() {        JUnitUtil.clearShutDownManager(); // put in place because AbstractMRTrafficController implementing subclass was not terminated properly
+    public void tearDown() {
+        Assertions.assertNotNull(memo);
+        memo.getDCCppTrafficController().terminateThreads();
+        memo.dispose();
+        memo = null;
         JUnitUtil.tearDown();
     }
 }
