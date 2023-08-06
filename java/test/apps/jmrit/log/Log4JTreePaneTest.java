9fbfe3b4b6815aa1cc66a063bdbe39c0f759980b
Steve Young <icklesteve@yahoo.com>
2023-08-06 04:50:27 +0100

    Update Log4JTreePaneTest.java
    add edit logging level tests

--- java/test/apps/jmrit/log/Log4JTreePaneTest.java
+++ java/test/apps/jmrit/log/Log4JTreePaneTest.java
@@ -1,27 +1,23 @@
 package apps.jmrit.log;
 
-import java.awt.GraphicsEnvironment;
-
-import javax.swing.JFrame;
-
 import jmri.util.JUnitUtil;
 
-import org.junit.Assert;
-import org.junit.Assume;
 import org.junit.jupiter.api.*;
-import org.netbeans.jemmy.operators.JFrameOperator;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
+import org.netbeans.jemmy.operators.*;
 import org.slf4j.LoggerFactory;
 
 /**
- * Invokes complete set of tests in the jmri.jmrit.log tree
+ * Tests for Log4JTreePane.
  *
  * @author Bob Jacobsen Copyright 2003, 2010
+ * @author Steve Young Copyright(C) 2023
  */
+@DisabledIfSystemProperty(named = "java.awt.headless", matches = "true")
 public class Log4JTreePaneTest extends jmri.util.swing.JmriPanelTest {
 
     @Test
     public void testShow() {
-        Assume.assumeFalse(GraphicsEnvironment.isHeadless());
         LoggerFactory.getLogger("jmri.jmrix");
         LoggerFactory.getLogger("apps.foo");
         LoggerFactory.getLogger("jmri.util");
@@ -29,9 +25,49 @@ public class Log4JTreePaneTest extends jmri.util.swing.JmriPanelTest {
         new jmri.util.swing.JmriNamedPaneAction("Log4J Tree",
                 new jmri.util.swing.sdi.JmriJFrameInterface(),
                 "apps.jmrit.log.Log4JTreePane").actionPerformed(null);
-        JFrame f = JFrameOperator.waitJFrame(Bundle.getMessage("MenuItemLogTreeAction"), true, true);
-        Assert.assertNotNull(f);
-        JUnitUtil.dispose(f);
+        
+        JFrameOperator jfo = new JFrameOperator(Bundle.getMessage("MenuItemLogTreeAction"));
+        Assertions.assertNotNull(jfo);
+
+        jfo.requestClose();
+        jfo.waitClosed();
+        
+    }
+
+    @Test
+    public void testButtons(){
+        LoggerFactory.getLogger("apps.morefoo");
+        LoggerFactory.getLogger("jmri.util");
+
+        new jmri.util.swing.JmriNamedPaneAction("Log4J Tree",
+                new jmri.util.swing.sdi.JmriJFrameInterface(),
+                "apps.jmrit.log.Log4JTreePane").actionPerformed(null);
+
+        JFrameOperator jfo = new JFrameOperator(Bundle.getMessage("MenuItemLogTreeAction"));
+        Assertions.assertNotNull(jfo);
+
+        LoggerFactory.getLogger("apps.evenmorefoo");
+        JButtonOperator refreshOp = new JButtonOperator(jfo, Bundle.getMessage("ButtonRefreshCategories"));
+        refreshOp.doClick();
+
+        JTextAreaOperator jtfo = new JTextAreaOperator(jfo, 0);
+        Assertions.assertTrue(jtfo.getText().contains("apps.evenmorefoo"));
+
+        JComboBoxOperator logLevelSelect = new JComboBoxOperator(jfo, 1);
+        logLevelSelect.setSelectedItem(org.apache.logging.log4j.Level.TRACE);
+
+        new JComboBoxOperator(jfo, 0).setSelectedItem("apps.evenmorefoo");
+        JButtonOperator jbo = new JButtonOperator(jfo, Bundle.getMessage("ButtonEditLoggingLevel"));
+        jbo.doClick();
+        Assertions.assertTrue(LoggerFactory.getLogger("apps.evenmorefoo").isTraceEnabled());
+
+        logLevelSelect.setSelectedItem(org.apache.logging.log4j.Level.ERROR);
+        jbo.doClick();
+        Assertions.assertFalse(LoggerFactory.getLogger("apps.evenmorefoo").isTraceEnabled());
+
+        jfo.requestClose();
+        jfo.waitClosed();
+
     }
 
     @BeforeEach
