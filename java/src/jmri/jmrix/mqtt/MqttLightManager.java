ad5893cf81622ace4d794b99f1ad6b517c46857c
Dean Cording <dean@cording.id.au>
2023-08-28 00:14:40 +1000

    Fixed topics to be consistent

--- java/src/jmri/jmrix/mqtt/MqttLightManager.java
+++ java/src/jmri/jmrix/mqtt/MqttLightManager.java
@@ -26,9 +26,9 @@ public class MqttLightManager extends jmri.managers.AbstractLightManager {
     }
     
     @Nonnull
-    public String sendTopicPrefix = "cmd/track/light/";
+    public String sendTopicPrefix = "track/light/";
     @Nonnull
-    public String rcvTopicPrefix  = "state/track/light/";
+    public String rcvTopicPrefix  = "track/light/";
 
     /**
      * {@inheritDoc}
