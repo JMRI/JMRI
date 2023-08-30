00cf1daa0f06de1511a0c14144ca419274e386bc
Daniel <db123@bergqvist.se>
2023-08-30 15:42:58 +0200

    Update PipeListener.java

--- java/src/jmri/util/PipeListener.java
+++ java/src/jmri/util/PipeListener.java
@@ -25,21 +25,35 @@ public class PipeListener extends Thread {
     }
 
     static final int BUFFER_SIZE = 120;
-    
+
+    private long lastTime = System.currentTimeMillis();
+
+    public long time() {
+        long result = System.currentTimeMillis() - lastTime;
+        lastTime = System.currentTimeMillis();
+        return result;
+    }
+
     @Override
     public void run() {
         try {
             char[] cbuf = new char[BUFFER_SIZE];
             while (true) {
                 try {
+                    System.out.format("%d: Read%n", time());
                     int nRead = pr.read(cbuf, 0, BUFFER_SIZE);  // blocking read
+                    System.out.format("%d: Read: %d%n", time(), nRead);
                     String content = new String(Arrays.copyOf(cbuf, nRead)); // retain only filled chars
+                    System.out.format("%d: Read: %d, %s%n", time(), nRead, content);
 
                     jmri.util.ThreadingUtil.runOnGUI(() -> {
+                        System.out.format("%d: Read.append: start%n", time());
                         ta.append(content);
+                        System.out.format("%d: Read.append: end%n", time());
                     });
 
                 } catch (IOException ex) {
+                    ex.printStackTrace();
                     if (ex.getMessage().equals("Write end dead") || ex.getMessage().equals("Pipe broken")) {
                         // happens when the writer thread, possibly a script, terminates
                         synchronized (this) {
@@ -55,6 +69,7 @@ public class PipeListener extends Thread {
                 }
             }
         } catch (IOException ex) {
+            ex.printStackTrace();
             ta.append("PipeListener Exiting on IOException:" + ex);
         }
     }
