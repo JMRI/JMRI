d220fe5b4146b629b631ff847ae12d7935e4805d
Steve Young <icklesteve@yahoo.com>
2023-09-02 12:09:30 +0100

    Update JmriJOptionPane.java

--- java/src/jmri/util/swing/JmriJOptionPane.java
+++ java/src/jmri/util/swing/JmriJOptionPane.java
@@ -39,7 +39,7 @@ public class JmriJOptionPane {
 
     public static final int ERROR_MESSAGE = JOptionPane.ERROR_MESSAGE;
     public static final int INFORMATION_MESSAGE = JOptionPane.INFORMATION_MESSAGE;
-    public static final int PLAIN_MESSAGE = JmriJOptionPane.PLAIN_MESSAGE;
+    public static final int PLAIN_MESSAGE = JOptionPane.PLAIN_MESSAGE;
     public static final int QUESTION_MESSAGE = JOptionPane.QUESTION_MESSAGE;
     public static final int WARNING_MESSAGE = JOptionPane.WARNING_MESSAGE;
 
