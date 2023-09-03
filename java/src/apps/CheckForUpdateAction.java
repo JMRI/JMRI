411eb73a4fba03d518155194726b951508d02464
Steve Young <icklesteve@yahoo.com>
2023-09-03 01:28:53 +0100

    CheckForUpdate Action tweaks
    use https urls
    use try with resources

--- java/src/apps/CheckForUpdateAction.java
+++ java/src/apps/CheckForUpdateAction.java
@@ -3,14 +3,15 @@ package apps;
 import java.awt.Desktop;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URI;
-import java.net.URL;
+
+import java.net.*;
+
 import javax.annotation.Nonnull;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
@@ -18,11 +19,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+
 import jmri.util.JmriJFrame;
 import jmri.util.swing.JmriPanel;
 import jmri.util.swing.WindowInterface;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Swing action to check for more recent JMRI version. Checks a jmri.org URL for
@@ -59,12 +59,19 @@ public class CheckForUpdateAction extends jmri.util.swing.JmriAbstractAction {
         String productionrelease = "";
         String testrelease = "";
 
-        InputStream in = null;
+        String urlname = "https://www.jmri.org/releaselist";
+        String downloadURI = "https://www.jmri.org/download/";
+
+        URL url;
         try {
-            String urlname = "http://jmri.org/releaselist";
-            URL url = new URL(urlname);
-            in = url.openConnection().getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            url = new URL(urlname);
+        } catch (MalformedURLException e){
+            log.error("Unexpected failure in URL parsing", e);
+            return;
+        }
+
+        try ( InputStream in = url.openConnection().getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in)); ) {
 
             // search for releases
             String line;
@@ -76,21 +83,10 @@ public class CheckForUpdateAction extends jmri.util.swing.JmriAbstractAction {
                     testrelease = getNumber(reader);
                 }
             }
-        } catch (java.net.MalformedURLException e) {
-            log.error("Unexpected failure in URL parsing", e);
-            return;
         } catch (FileNotFoundException e) {
             log.debug("Unable to get version info from web", e);
         } catch (IOException e) {
             log.debug("Unexpected failure during reading", e);
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException e1) {
-                log.error("Exception closing input stream", e1);
-            }
         }
 
         // add content
@@ -102,27 +98,21 @@ public class CheckForUpdateAction extends jmri.util.swing.JmriAbstractAction {
         p.setLayout(new FlowLayout());
 
         JButton go = new JButton(Bundle.getMessage("ButtonDownloadPage"));
-        go.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                try {
-                    Desktop.getDesktop().browse(new URI("http://jmri.org/download"));
-                } catch (java.net.URISyntaxException e) {
-                    log.error("Invalid page requested", e);
-                } catch (java.io.IOException e) {
-                    log.error("Could no load page", e);
-                }
+        go.addActionListener((ActionEvent event) -> {
+            try {
+                Desktop.getDesktop().browse(new URI(downloadURI));
+            } catch (java.net.URISyntaxException e) {
+                log.error("Invalid page requested", e);
+            } catch (java.io.IOException e) {
+                log.error("Could no load page", e);
             }
         });
         p.add(go);
 
         JButton close = new JButton(Bundle.getMessage("ButtonClose"));
-        close.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                frame.setVisible(false);
-                frame.dispose();
-            }
+        close.addActionListener((ActionEvent event) -> {
+            frame.setVisible(false);
+            frame.dispose();
         });
         p.add(close);
 
@@ -133,8 +123,9 @@ public class CheckForUpdateAction extends jmri.util.swing.JmriAbstractAction {
         frame.setVisible(true);
     }
 
-    @Nonnull String getNumber(@Nonnull BufferedReader reader) throws java.io.IOException {
-        reader.readLine();  // skip a line
+    @Nonnull
+    String getNumber(@Nonnull BufferedReader reader) throws java.io.IOException {
+        log.debug("skipping line {}", reader.readLine()); // skip a line
         String line = reader.readLine();
         if (line == null) return "";
         return line.substring(0, line.length() - 1);  // drop trailing :
@@ -146,6 +137,6 @@ public class CheckForUpdateAction extends jmri.util.swing.JmriAbstractAction {
         throw new IllegalArgumentException("Should not be invoked");
     }
 
-    private final static Logger log = LoggerFactory.getLogger(CheckForUpdateAction.class);
+    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(CheckForUpdateAction.class);
 
 }
