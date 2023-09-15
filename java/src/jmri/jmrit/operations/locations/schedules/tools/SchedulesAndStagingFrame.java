ee4bb27721bac2a3e41067b69e74366cc17326b2
dabou <dabou@DBoudreau-2022>
2023-09-14 21:27:30 -0400

    OperationsPro add "Car Type Edit" to tools menu

--- java/src/jmri/jmrit/operations/locations/schedules/tools/SchedulesAndStagingFrame.java
+++ java/src/jmri/jmrit/operations/locations/schedules/tools/SchedulesAndStagingFrame.java
@@ -15,6 +15,7 @@ import jmri.jmrit.operations.locations.schedules.Schedule;
 import jmri.jmrit.operations.locations.schedules.ScheduleItem;
 import jmri.jmrit.operations.rollingstock.cars.CarLoads;
 import jmri.jmrit.operations.rollingstock.cars.CarTypes;
+import jmri.jmrit.operations.rollingstock.cars.tools.CarLoadEditFrameAction;
 import jmri.jmrit.operations.rollingstock.cars.tools.PrintCarLoadsAction;
 import jmri.jmrit.operations.setup.Control;
 
@@ -89,6 +90,7 @@ public class SchedulesAndStagingFrame extends OperationsFrame implements java.be
         // build menu
         JMenuBar menuBar = new JMenuBar();
         JMenu toolMenu = new JMenu(Bundle.getMessage("MenuTools"));
+        toolMenu.add(new CarLoadEditFrameAction());
         toolMenu.add(new PrintCarLoadsAction(true));
         toolMenu.add(new PrintCarLoadsAction(false));
         menuBar.add(toolMenu);
