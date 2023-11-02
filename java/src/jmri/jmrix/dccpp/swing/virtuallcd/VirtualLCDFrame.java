c7f87295b2ff7bb261d0a192d4c06e4c076b5523
mstevetodd <mstevetodd@mstevetodd.com>
2023-11-02 17:49:12 -0400

    New DCC-EX Virtual LCD display

--- java/src/jmri/jmrix/dccpp/swing/virtuallcd/VirtualLCDFrame.java
+++ java/src/jmri/jmrix/dccpp/swing/virtuallcd/VirtualLCDFrame.java
@@ -76,6 +76,6 @@ public class VirtualLCDFrame extends JmriJFrame implements DCCppListener  {
         pack();
     }
    
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(VirtualLCDFrame.class);
+//    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(VirtualLCDFrame.class);
 
 }
