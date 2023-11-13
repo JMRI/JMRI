cdab59b40013f64d142b18a06d2bf0df82d03a35
Steve Young <icklesteve@yahoo.com>
2023-11-13 19:32:25 +0000

    bidib to ju5 pt4

--- java/test/jmri/jmrix/bidib/tcpserver/NetPlainTcpBidibTest.java
+++ java/test/jmri/jmrix/bidib/tcpserver/NetPlainTcpBidibTest.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jmri.jmrix.bidib.tcpserver;
 
 import jmri.jmrix.bidib.BiDiBInterfaceScaffold;
 import jmri.jmrix.bidib.BiDiBSystemConnectionMemo;
 import jmri.jmrix.bidib.TestBiDiBTrafficController;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
 import jmri.util.JUnitUtil;
-import org.junit.Assert;
+
+import org.junit.jupiter.api.*;
 
 /**
  * Tests for the NetPlainTcpBidib class
@@ -22,24 +13,26 @@ import org.junit.Assert;
  * @author  Eckart Meyer  Copyright (C) 2023
  */
 public class NetPlainTcpBidibTest {
-    
+
     BiDiBSystemConnectionMemo memo;
-    
-    @Before
+
+    @Test
+    public void testCTor() {
+        NetPlainTcpBidib t = new NetPlainTcpBidib(memo.getBiDiBTrafficController());
+        Assertions.assertNotNull(t, "exists");
+        t.stop();
+    }
+
+    @BeforeEach
     public void setUp() {
         JUnitUtil.setUp();
         memo = new BiDiBSystemConnectionMemo();
         memo.setBiDiBTrafficController(new TestBiDiBTrafficController(new BiDiBInterfaceScaffold()));
     }
-    
-    @After
+
+    @AfterEach
     public void tearDown() {
+        JUnitUtil.tearDown();
     }
 
-    @Test
-    public void testCTor() {
-        NetPlainTcpBidib t = new NetPlainTcpBidib(memo.getBiDiBTrafficController());
-        Assert.assertNotNull("exists",t);
-        t.stop();
-    }
 }
