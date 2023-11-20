1aa39eec6164f3737600100e458a4ade288ca76c
Daniel <db123@bergqvist.se>
2023-11-20 14:55:59 +0100

    Update DefaultWebServerConfigurationTest.java

--- java/test/jmri/server/web/DefaultWebServerConfigurationTest.java
+++ java/test/jmri/server/web/DefaultWebServerConfigurationTest.java
@@ -45,7 +45,7 @@ public class DefaultWebServerConfigurationTest {
                 .as("Default file paths")
                 .satisfies(paths -> {
                     assertThat(paths).isNotNull();
-                    assertThat(paths.size()).isEqualTo(15);
+                    assertThat(paths.size()).isEqualTo(14);
                 });
     }
 
@@ -78,7 +78,7 @@ public class DefaultWebServerConfigurationTest {
 
     /**
      * Test of load method with missing/non-existent resource.
-     * 
+     *
      * @throws Exception if unable to use reflection
      */
     @Test
