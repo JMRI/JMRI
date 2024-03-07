a3957bb565f1c1097d406c113c47f1295da0a8d3
Daniel <db123@bergqvist.se>
2024-03-08 00:19:50 +0100

    Add LogixNG functions cos(), tan(), atan(), sqr() and sqrt()

--- java/test/jmri/jmrit/logixng/util/parser/functions/MathFunctionsTest.java
+++ java/test/jmri/jmrit/logixng/util/parser/functions/MathFunctionsTest.java
@@ -33,12 +33,22 @@ public class MathFunctionsTest {
     ExpressionNode expr0_34 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "0.34", 0));
     ExpressionNode expr0_95 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "0.95", 0));
     ExpressionNode expr12_34 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "12.34", 0));
-    ExpressionNode expr12 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "12", 0));
-    ExpressionNode expr23 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "23", 0));
+    ExpressionNode expr12 = new ExpressionNodeIntegerNumber(new Token(TokenType.NONE, "12", 0));
+    ExpressionNode expr23 = new ExpressionNodeIntegerNumber(new Token(TokenType.NONE, "23", 0));
     ExpressionNode exprNeg0_34 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "-0.34", 0));
     ExpressionNode exprNeg0_95 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "-0.95", 0));
     ExpressionNode exprNeg12_34 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "-12.34", 0));
 
+    ExpressionNode exprPi2 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, Double.toString(1.0/2*Math.PI), 0));
+    ExpressionNode exprPi = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, Double.toString(Math.PI), 0));
+    ExpressionNode expr3Pi2 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, Double.toString(3.0/2*Math.PI), 0));
+    ExpressionNode expr2Pi = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, Double.toString(2*Math.PI), 0));
+
+    ExpressionNode expr0 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "0", 0));
+    ExpressionNode expr90 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "90", 0));
+    ExpressionNode expr180 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "180", 0));
+    ExpressionNode expr270 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "270", 0));
+    ExpressionNode expr360 = new ExpressionNodeFloatingNumber(new Token(TokenType.NONE, "360", 0));
 
     private List<ExpressionNode> getParameterList(ExpressionNode... exprNodes) {
         List<ExpressionNode> list = new ArrayList<>();
@@ -95,10 +105,172 @@ public class MathFunctionsTest {
         // Test sin(x,"deg", 12, 23)
         Assert.assertEquals("numbers are equal", (Double)18.675418424366967, (Double)sinFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_DEG, expr12, expr23)), 0.0000001d);
 
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)sinFunction.calculate(symbolTable, getParameterList(expr0, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)23.0, (Double)sinFunction.calculate(symbolTable, getParameterList(exprPi2, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)sinFunction.calculate(symbolTable, getParameterList(exprPi, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)12.0, (Double)sinFunction.calculate(symbolTable, getParameterList(expr3Pi2, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)sinFunction.calculate(symbolTable, getParameterList(expr2Pi, expr_str_RAD, expr12, expr23)), 0.0000001d);
+
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)sinFunction.calculate(symbolTable, getParameterList(expr0, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)23.0, (Double)sinFunction.calculate(symbolTable, getParameterList(expr90, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)sinFunction.calculate(symbolTable, getParameterList(expr180, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)12.0, (Double)sinFunction.calculate(symbolTable, getParameterList(expr270, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)sinFunction.calculate(symbolTable, getParameterList(expr360, expr_str_DEG, expr12, expr23)), 0.0000001d);
+
         // Test sin()
         hasThrown.set(false);
         try {
             sinFunction.calculate(symbolTable, getParameterList());
+        } catch (WrongNumberOfParametersException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+    }
+
+    @Test
+    public void testCosFunction() throws Exception {
+        Function cosFunction = InstanceManager.getDefault(FunctionManager.class).get("cos");
+        Assert.assertEquals("strings matches", "cos", cosFunction.getName());
+
+        SymbolTable symbolTable = new DefaultSymbolTable(new DefaultConditionalNG("IQC1", null));
+
+        // Test cos(x)
+        Assert.assertEquals("numbers are equal", (Double)0.9427546655283462, (Double)cosFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.5816830894638836, (Double)cosFunction.calculate(symbolTable, getParameterList(expr0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)(0.9744873987650982), (Double)cosFunction.calculate(symbolTable, getParameterList(expr12_34)), 0.0000001d);
+
+        // Test cos(x) with a string as parameter
+        Assert.assertEquals("numbers are equal", (Double)0.9427546655283462, (Double)cosFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+
+        AtomicBoolean hasThrown = new AtomicBoolean(false);
+
+        // Test cos(x,"rad"), cos(x,"deg"), cos(x,"hello"), cos(x, true)
+        Assert.assertEquals("numbers are equal", (Double)0.9427546655283462, (Double)cosFunction.calculate(symbolTable, getParameterList(expr0_34, expr_str_RAD)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.976896613081381, (Double)cosFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_DEG)), 0.0000001d);
+        hasThrown.set(false);
+        try {
+            cosFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_HELLO));
+        } catch (CalculateException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+        hasThrown.set(false);
+        try {
+            cosFunction.calculate(symbolTable, getParameterList(expr12_34, expr_boolean_true));
+        } catch (CalculateException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+
+        // Test cos(x,"deg", 12, 23)
+        Assert.assertEquals("numbers are equal", (Double)22.872931371947594, (Double)cosFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_DEG, expr12, expr23)), 0.0000001d);
+
+        Assert.assertEquals("numbers are equal", (Double)23.0, (Double)cosFunction.calculate(symbolTable, getParameterList(expr0, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)cosFunction.calculate(symbolTable, getParameterList(exprPi2, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)12.0, (Double)cosFunction.calculate(symbolTable, getParameterList(exprPi, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)cosFunction.calculate(symbolTable, getParameterList(expr3Pi2, expr_str_RAD, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)23.0, (Double)cosFunction.calculate(symbolTable, getParameterList(expr2Pi, expr_str_RAD, expr12, expr23)), 0.0000001d);
+
+        Assert.assertEquals("numbers are equal", (Double)23.0, (Double)cosFunction.calculate(symbolTable, getParameterList(expr0, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)cosFunction.calculate(symbolTable, getParameterList(expr90, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)12.0, (Double)cosFunction.calculate(symbolTable, getParameterList(expr180, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)17.5, (Double)cosFunction.calculate(symbolTable, getParameterList(expr270, expr_str_DEG, expr12, expr23)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)23.0, (Double)cosFunction.calculate(symbolTable, getParameterList(expr360, expr_str_DEG, expr12, expr23)), 0.0000001d);
+
+        // Test cos()
+        hasThrown.set(false);
+        try {
+            cosFunction.calculate(symbolTable, getParameterList());
+        } catch (WrongNumberOfParametersException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+    }
+
+    @Test
+    public void testTanFunction() throws Exception {
+        Function tanFunction = InstanceManager.getDefault(FunctionManager.class).get("tan");
+        Assert.assertEquals("strings matches", "tan", tanFunction.getName());
+
+        SymbolTable symbolTable = new DefaultSymbolTable(new DefaultConditionalNG("IQC1", null));
+
+        // Test tan(x)
+        Assert.assertEquals("numbers are equal", (Double)0.35373687803912257, (Double)tanFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)1.398382589287699, (Double)tanFunction.calculate(symbolTable, getParameterList(expr0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)(-0.23031823627096235), (Double)tanFunction.calculate(symbolTable, getParameterList(expr12_34)), 0.0000001d);
+
+        // Test tan(x) with a string as parameter
+        Assert.assertEquals("numbers are equal", (Double)0.35373687803912257, (Double)tanFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+
+        AtomicBoolean hasThrown = new AtomicBoolean(false);
+
+        // Test tan(x,"rad"), tan(x,"deg"), tan(x,"hello"), tan(x, true)
+        Assert.assertEquals("numbers are equal", (Double)0.35373687803912257, (Double)tanFunction.calculate(symbolTable, getParameterList(expr0_34, expr_str_RAD)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.21876669233184345, (Double)tanFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_DEG)), 0.0000001d);
+        hasThrown.set(false);
+        try {
+            tanFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_HELLO));
+        } catch (CalculateException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+        hasThrown.set(false);
+        try {
+            tanFunction.calculate(symbolTable, getParameterList(expr12_34, expr_boolean_true));
+        } catch (CalculateException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+
+        // Test tan()
+        hasThrown.set(false);
+        try {
+            tanFunction.calculate(symbolTable, getParameterList());
+        } catch (WrongNumberOfParametersException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+    }
+
+    @Test
+    public void testArctanFunction() throws Exception {
+        Function atanFunction = InstanceManager.getDefault(FunctionManager.class).get("atan");
+        Assert.assertEquals("strings matches", "atan", atanFunction.getName());
+
+        SymbolTable symbolTable = new DefaultSymbolTable(new DefaultConditionalNG("IQC1", null));
+
+        // Test atan(x)
+        Assert.assertEquals("numbers are equal", (Double)0.3277385067805555, (Double)atanFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.7597627548757708, (Double)atanFunction.calculate(symbolTable, getParameterList(expr0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)1.4899357456343294, (Double)atanFunction.calculate(symbolTable, getParameterList(expr12_34)), 0.0000001d);
+
+        // Test atan(x) with a string as parameter
+        Assert.assertEquals("numbers are equal", (Double)0.3277385067805555, (Double)atanFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+
+        AtomicBoolean hasThrown = new AtomicBoolean(false);
+
+        // Test atan(x,"rad"), atan(x,"deg"), atan(x,"hello"), atan(x, true)
+        Assert.assertEquals("numbers are equal", (Double)0.3277385067805555, (Double)atanFunction.calculate(symbolTable, getParameterList(expr0_34, expr_str_RAD)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)85.36702997052444, (Double)atanFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_DEG)), 0.0000001d);
+        hasThrown.set(false);
+        try {
+            atanFunction.calculate(symbolTable, getParameterList(expr12_34, expr_str_HELLO));
+        } catch (CalculateException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+        hasThrown.set(false);
+        try {
+            atanFunction.calculate(symbolTable, getParameterList(expr12_34, expr_boolean_true));
+        } catch (CalculateException e) {
+            hasThrown.set(true);
+        }
+        Assert.assertTrue("exception is thrown", hasThrown.get());
+
+        // Test atan()
+        hasThrown.set(false);
+        try {
+            atanFunction.calculate(symbolTable, getParameterList());
         } catch (WrongNumberOfParametersException e) {
             hasThrown.set(true);
         }
@@ -157,6 +329,44 @@ public class MathFunctionsTest {
                 }
             }
         }
+    }
+
+    @Test
+    public void testSqrFunction() throws Exception {
+        Function sqrFunction = InstanceManager.getDefault(FunctionManager.class).get("sqr");
+        Assert.assertEquals("strings matches", "sqr", sqrFunction.getName());
+
+        SymbolTable symbolTable = new DefaultSymbolTable(new DefaultConditionalNG("IQC1", null));
+
+        Assert.assertEquals("numbers are equal", 144L, (long)(Long)sqrFunction.calculate(symbolTable, getParameterList(expr12)));
+        Assert.assertEquals("numbers are equal", 529, (long)(Long)sqrFunction.calculate(symbolTable, getParameterList(expr23)));
+
+        Assert.assertEquals("numbers are equal", (Double)0.1156, (Double)sqrFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.9025, (Double)sqrFunction.calculate(symbolTable, getParameterList(expr0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)152.2756, (Double)sqrFunction.calculate(symbolTable, getParameterList(expr12_34)), 0.0000001d);
+
+        Assert.assertEquals("numbers are equal", (Double)0.1156, (Double)sqrFunction.calculate(symbolTable, getParameterList(exprNeg0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.9025, (Double)sqrFunction.calculate(symbolTable, getParameterList(exprNeg0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)152.2756, (Double)sqrFunction.calculate(symbolTable, getParameterList(exprNeg12_34)), 0.0000001d);
+    }
+
+    @Test
+    public void testSqrtFunction() throws Exception {
+        Function sqrtFunction = InstanceManager.getDefault(FunctionManager.class).get("sqrt");
+        Assert.assertEquals("strings matches", "sqrt", sqrtFunction.getName());
+
+        SymbolTable symbolTable = new DefaultSymbolTable(new DefaultConditionalNG("IQC1", null));
+
+        Assert.assertEquals("numbers are equal", (Double)3.4641016151377544, (Double)sqrtFunction.calculate(symbolTable, getParameterList(expr12)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)4.795831523312719, (Double)sqrtFunction.calculate(symbolTable, getParameterList(expr23)), 0.0000001d);
+
+        Assert.assertEquals("numbers are equal", (Double)0.5830951894845301, (Double)sqrtFunction.calculate(symbolTable, getParameterList(expr0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)0.9746794344808963, (Double)sqrtFunction.calculate(symbolTable, getParameterList(expr0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", (Double)3.5128336140500593, (Double)sqrtFunction.calculate(symbolTable, getParameterList(expr12_34)), 0.0000001d);
+
+        Assert.assertEquals("numbers are equal", Double.NaN, (Double)sqrtFunction.calculate(symbolTable, getParameterList(exprNeg0_34)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", Double.NaN, (Double)sqrtFunction.calculate(symbolTable, getParameterList(exprNeg0_95)), 0.0000001d);
+        Assert.assertEquals("numbers are equal", Double.NaN, (Double)sqrtFunction.calculate(symbolTable, getParameterList(exprNeg12_34)), 0.0000001d);
     }
 
     // The minimal setup for log4J
