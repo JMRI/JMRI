b0218e1a82c87c9e9dc9d17734ae1e274ab0b2a6
Daniel <db123@bergqvist.se>
2024-03-22 13:21:47 +0100

    Let scripts access the symbol table

--- java/src/jmri/jmrit/logixng/actions/ActionScript.java
+++ java/src/jmri/jmrit/logixng/actions/ActionScript.java
@@ -265,7 +265,7 @@ public class ActionScript extends AbstractDigitalAction {
         LogixNG_ScriptBindings.addScriptBindings(bindings);
 
         SymbolTable symbolTable = getConditionalNG().getSymbolTable();
-        bindings.put("symbolTable", symbolTable);    // Give the script access to the local variable 'symbolTable'
+        bindings.put("symbolTable", symbolTable);    // Give the script access to the local variables in the symbol table
 
         ThreadingUtil.runOnLayoutWithJmriException(() -> {
             ScriptEngineSelector.Engine engine =
