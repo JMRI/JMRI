6be855c3d71011fe4bd92830f7f468c20886551e
Dean Cording <dean@cording.id.au>
2024-04-01 14:17:10 +1000

    Fixed issue with MQTT Client Id

--- java/src/jmri/jmrix/mqtt/MqttAdapter.java
+++ java/src/jmri/jmrix/mqtt/MqttAdapter.java
@@ -162,12 +162,11 @@ public class MqttAdapter extends jmri.jmrix.AbstractNetworkPortController implem
             }
 
             // generate a unique client ID based on the network ID and the system prefix of the MQTT connection.
-            String clientID = jmri.InstanceManager.getDefault(jmri.web.server.WebServerPreferences.class).getRailroadName()
-                                + getSystemPrefix() + jmri.util.node.NodeIdentity.networkIdentity();
+            String clientID = jmri.util.node.NodeIdentity.networkIdentity();
             
             // ensure that only guaranteed valid characters are included in the client ID
             clientID = clientID.replaceAll("[^A-Za-z0-9]", "");
-            
+
             // ensure the length of the client ID doesn't exceed the guaranteed acceptable length of 23
             if (clientID.length() > 23) {
                 clientID = clientID.substring(0, 23);
