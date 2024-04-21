1ad9b032613cd2160822e1b8d356fe478e2b5948
Daniel <db123@bergqvist.se>
2024-04-21 08:37:28 +0200

    Update test_stale_sources.sh

--- scripts/test_stale_sources.sh
+++ scripts/test_stale_sources.sh
@@ -5,12 +5,13 @@
 output=$(mvn -X test-compile | grep "Stale source detected:")
 if [[ $? != 0 ]]; then
     echo "mvn command failed."
+    exit 1
 elif [[ $output ]]; then
-    // If here, we have stale java sources.
-    // Print the output and return with exit code 1.
+    # If here, we have stale java sources.
+    # Print the output and return with exit code 1.
     echo $output
     exit 1
 else
-    // If here, no stale java sources was found. Exit with 0.
+    # If here, no stale java sources was found. Exit with 0.
     exit 0
 fi
