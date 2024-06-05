f06d767060242b7c9ea69e7e9520233bd229fb6a
Daniel <db123@bergqvist.se>
2024-06-05 16:27:56 +0200

    Update DataSource.java

--- java/src/jmri/jmrix/pricom/pockettester/DataSource.java
+++ java/src/jmri/jmrix/pricom/pockettester/DataSource.java
@@ -313,7 +313,7 @@ public class DataSource extends jmri.util.JmriJFrame {
             // get and open the primary port
             CommPortIdentifier portID = CommPortIdentifier.getPortIdentifier(portName);
             try {
-                activeSerialPort = (SerialPort) portID.open(appName, 2000);  // name of program, msec to wait
+                activeSerialPort = portID.open(appName, 2000);  // name of program, msec to wait
             } catch (PortInUseException p) {
                 handlePortBusy(p, portName);
                 return "Port " + p + " in use already";
