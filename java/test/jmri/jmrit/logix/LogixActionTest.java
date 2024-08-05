d1c77b84c78b9d92853dcd27cf2711e4f601323c
Daniel <db123@bergqvist.se>
2024-08-05 20:16:31 +0200

    Update LogixActionTest.java

--- java/test/jmri/jmrit/logix/LogixActionTest.java
+++ java/test/jmri/jmrit/logix/LogixActionTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class LogixActionTest {
 
+    @Disabled
     @Test
     public void testLogixAction() throws Exception {
         jmri.configurexml.ConfigXmlManager cm = new jmri.configurexml.ConfigXmlManager() {
