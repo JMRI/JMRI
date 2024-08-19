f44915f8ed5f6b7939c1384be46e5396e79a6aae
Daniel <db123@bergqvist.se>
2024-08-19 05:17:24 +0200

    Update test_decoder_XSLT_transforms.sh

--- scripts/test_decoder_XSLT_transforms.sh
+++ scripts/test_decoder_XSLT_transforms.sh
@@ -3,7 +3,14 @@
 # Check that the decoder XSLT transforms work
 
 cd xml/XSLT
-ant clean
-ant
+
+ant clean && ant
+
+result=$?
+
 cd ../..
 
+if [[ $result != 0 ]]; then
+    echo "decoder XSLT transformations failed"
+    exit 1
+fi
