395c15d7f722de261f12b2b2cecd8d742c49c3b2
Bob Jacobsen <jacobsen@mac.com>
2024-12-02 16:56:34 -0500

    parse ranges

--- java/src/jmri/jmrix/openlcb/swing/EventIdParser.java
+++ java/src/jmri/jmrix/openlcb/swing/EventIdParser.java
@@ -23,11 +23,12 @@ public class EventIdParser {
             case "01": 
                 return wellKnown(eventID);
             case "09": 
-                if (eventID.toShortString().substring(0, 2) == "09.00.09") {
+                if (eventID.toShortString().startsWith("09.00.99.FF")) {
                     return trainSearch(eventID);
                 } 
                 // deliberately falling through
-            default: //$FALL-THROUGH$
+                //$FALL-THROUGH$
+            default:
                 return "";
         }
     }
@@ -76,9 +77,11 @@ public class EventIdParser {
                 return "Firmware Upgrade Request by Hardware Switch";
 
             default:
-                // check for fastclock range
+                // check for fastclock and DCC ranges
                 if (eid.startsWith("01.01.00.00.01")) {
                     return fastClock(eventID);
+                } else if (eid.startsWith("01.01.02")) {
+                    return dccRange(eventID);
                 } else {
                     return "Well-Known "+eid;
                 }
@@ -108,9 +111,13 @@ public class EventIdParser {
         } else if (bothBytes == 0xF002) {  //
             function = "Date Rollover";
         } else if (highByteMasked < 24) {  // time
-            function = set+"time "+highByteMasked+":"+lowByte;
+            String lowString = "00"+Integer.toString(lowByte);
+            lowString = lowString.substring(lowString.length()-2);
+            function = set+"time "+highByteMasked+":"+lowString;
         } else if (highByteMasked <= 0x2C) {  // date
-            function = set+"date "+(highByteMasked-0x20)+"/"+lowByte;
+            String lowString = "00"+Integer.toString(lowByte);
+            lowString = lowString.substring(lowString.length()-2);
+            function = set+"date "+(highByteMasked-0x20)+"/"+lowString;
         } else if (highByteMasked < 0x40) {  // year
             int year = (highByteMasked*256+lowByte)-0x3000;
             function = set+"year "+year;
@@ -121,8 +128,47 @@ public class EventIdParser {
         return "Fast Clock "+clockNum+" "+function;
     }
 
+    static public String dccRange(EventID eventID) {
+        String eid = eventID.toShortString();
+        if (eid.startsWith("01.01.02.00.00.FF")) {
+            return "DCC Basic Acc Addr Activate "+(eventID.toLong()&0x7FFL);
+        } else if (eid.startsWith("01.01.02.00.00.FE")) {
+            return "DCC Basic Acc Addr Deactivate "+(eventID.toLong()&0x7FFL);
+        } else if (eid.startsWith("01.01.02.00.00.FD")) {
+            return "DCC Turnout Feedback On "+(eventID.toLong()&0x7FFL);
+        } else if (eid.startsWith("01.01.02.00.00.FC")) {
+            return "DCC Turnout Feedback Off "+(eventID.toLong()&0x7FFL);
+        } else if (eid.startsWith("01.01.02.00.00.FB")) {
+            return "DCC Sensor On "+(eventID.toLong()&0xFFFL);
+        } else if (eid.startsWith("01.01.02.00.00.FA")) {
+            return "DCC Sensor Off "+(eventID.toLong()&0xFFFL);
+        } else if (eid.startsWith("01.01.02.00.01")) {
+            return "DCC Extended Accessory "
+                +((eventID.toLong()>>8)&0x7FFL)
+                +" "+(eventID.toLong()&0xFF);
+        } else {
+            return "DCC Well-Known "+eventID;
+        }
+    }
+    
     static public String trainSearch(EventID eventID) {
         return "Train Search";
+    }
+    
+    /**
+     * Take the eventID from a range, and return
+     * the lower flag bytes as a hex dotted string.
+     */
+    static public long rangeSuffix(EventID eventID) {
+        // find the mask value
+        long eid = eventID.toLong();
+        long sampleBit = eid & 0x01;
+        long mask = 0L;
+        while ( (eid &0x01L) == sampleBit) {
+            mask = (mask <<1) | 0x01;
+            eid = eid >> 1;
+        }
+        return mask;
     }
     
     // private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(EventIdParser.class);
--- java/test/jmri/jmrix/openlcb/swing/EventIdParserTest.java
+++ java/test/jmri/jmrix/openlcb/swing/EventIdParserTest.java
@@ -36,12 +36,28 @@ public class EventIdParserTest {
 
     @Test
     public void testFastClock() {
-        var eid = new EventID("01.01.00.00.01.01.09.32");
+        var eid = new EventID("01.01.00.00.01.01.09.02");
+        Assert.assertEquals("Fast Clock 1 time 9:02", EventIdParser.parse(eid));
+        eid = new EventID("01.01.00.00.01.01.09.32");
         Assert.assertEquals("Fast Clock 1 time 9:50", EventIdParser.parse(eid));
         eid = new EventID("01.01.00.00.01.01.89.32");
         Assert.assertEquals("Fast Clock 1 Set time 9:50", EventIdParser.parse(eid));
         eid = new EventID("01.01.00.00.01.01.F0.02");
         Assert.assertEquals("Fast Clock 1 Start", EventIdParser.parse(eid));
+    }
+
+    @Test
+    public void testRangeSuffix() {
+        var eid = new EventID("00.00.00.00.00.00.FF.FF");
+        Assert.assertEquals(0xFFFF, EventIdParser.rangeSuffix(eid));
+        eid = new EventID("00.00.00.00.00.FF.00.00");
+        Assert.assertEquals(0xFFFF, EventIdParser.rangeSuffix(eid));
+
+        eid = new EventID("00.00.00.00.00.FF.80.00");
+        Assert.assertEquals(0x7FFF, EventIdParser.rangeSuffix(eid));
+
+        eid = new EventID("00.00.00.00.00.00.00.03");
+        Assert.assertEquals(0x03, EventIdParser.rangeSuffix(eid));
     }
 
     @BeforeEach
