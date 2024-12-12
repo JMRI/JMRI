f733f652d6d83b7d5923079eda5b70b3b56af0f3
Steve Young <icklesteve@yahoo.com>
2024-12-12 01:16:53 +0000

    Update MarklinMenu.java
    unused var

--- java/src/jmri/jmrix/marklin/swing/MarklinMenu.java
+++ java/src/jmri/jmrix/marklin/swing/MarklinMenu.java
@@ -46,6 +46,6 @@ public class MarklinMenu extends JMenu {
         String load;
     }
 
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(MarklinMenu.class);
+    // private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(MarklinMenu.class);
 
 }
