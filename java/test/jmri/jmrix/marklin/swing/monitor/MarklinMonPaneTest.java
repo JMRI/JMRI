9f9949404957fe260652b81a8f0a7f629e00b440
Steve Young <icklesteve@yahoo.com>
2024-12-13 19:44:48 +0000

    Update MarklinMonPaneTest.java
    only run headless

--- java/test/jmri/jmrix/marklin/swing/monitor/MarklinMonPaneTest.java
+++ java/test/jmri/jmrix/marklin/swing/monitor/MarklinMonPaneTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
  *
  * @author Paul Bender Copyright (C) 2016
  */
+@jmri.util.junit.annotations.DisabledIfHeadless
 public class MarklinMonPaneTest extends jmri.jmrix.AbstractMonPaneTestBase {
 
     @Test
