77782f244d4ddcf9b63671e0c8f5a2d95465aa94
dabou <dabou@DBoudreau-2022>
2025-01-20 09:00:26 -0500

    OperationsPro Add import routes tool

---
+++ java/src/jmri/jmrit/operations/routes/tools/ImportRoutes.java
@@ -0,0 +1,153 @@
+package jmri.jmrit.operations.routes.tools;
+
+import java.io.*;
+
+import jmri.InstanceManager;
+import jmri.jmrit.operations.locations.Location;
+import jmri.jmrit.operations.locations.LocationManager;
+import jmri.jmrit.operations.rollingstock.ImportRollingStock;
+import jmri.jmrit.operations.routes.*;
+import jmri.jmrit.operations.setup.Setup;
+import jmri.util.swing.JmriJOptionPane;
+
+/**
+ * This routine will import Routes from a CSV file into the operations database.
+ * The field order is as defined below.
+ */
+public class ImportRoutes extends ImportRollingStock {
+
+    RouteManager routeManager = InstanceManager.getDefault(RouteManager.class);
+    LocationManager locationManager = InstanceManager.getDefault(LocationManager.class);
+
+    boolean importOkay = false;
+    int lineNum = 0;
+    static final String[] BREAK = new String[0];
+
+    // each route starts with name and comment
+    protected static final int FIELD_ROUTE_NAME = 0;
+    protected static final int FIELD_EMPTY = 1;
+    protected static final int FIELD_ROUTE_COMMENT = 2;
+
+    // each route can have one or more route locations
+    protected static final int FIELD_ROUTE_LOCATION_EMPTY = 0;
+    protected static final int FIELD_ROUTE_LOCATION_NAME = 1;
+    protected static final int FIELD_ROUTE_LOCATION_DIRECTION = 2;
+    protected static final int FIELD_ROUTE_LOCATION_MOVES = 3;
+    protected static final int FIELD_ROUTE_LOCATION_RANDOM = 4;
+    protected static final int FIELD_ROUTE_LOCATION_PICKUP_ALLOWED = 5;
+    protected static final int FIELD_ROUTE_LOCATION_DROP_ALLOWED = 6;
+    protected static final int FIELD_ROUTE_LOCATION_TRAVEL = 7;
+    protected static final int FIELD_ROUTE_LOCATION_DEPARTURE_TIME = 8;
+    protected static final int FIELD_ROUTE_LOCATION_TRAIN_LENGTH = 9;
+    protected static final int FIELD_ROUTE_LOCATION_GRADE = 10;
+    protected static final int FIELD_ROUTE_LOCATION_ICON_X = 11;
+    protected static final int FIELD_ROUTE_LOCATION_ICON_Y = 12;
+    protected static final int FIELD_ROUTE_LOCATION_COMMENT = 13;
+    protected static final int FIELD_ROUTE_LOCATION_COMMENT_COLOR = 14;
+
+    @Override
+    public void run() {
+        File file = getFile();
+        if (file == null) {
+            return;
+        }
+        BufferedReader rdr = getBufferedReader(file);
+        if (rdr == null) {
+            return;
+        }
+        createStatusFrame(Bundle.getMessage("TitleImportRoutes"));
+
+        boolean headerFound = false;
+        int routesAdded = 0;
+        String[] inputLine;
+        Route route = null;
+
+        while (true) {
+            inputLine = readNextLine(rdr);
+            if (inputLine.length == 0) {
+                break;
+            }
+
+            if (!headerFound && inputLine[FIELD_ROUTE_NAME].equals(Bundle.getMessage("Route"))) {
+                headerFound = true;
+                int elementNum = 0;
+                for (String lineElement : inputLine) {
+                    log.debug("Header {} is: {}", elementNum++, lineElement);
+                }
+                continue; // skip header
+            }
+            if (inputLine.length < 1) {
+                log.debug("Skipping blank line");
+                continue;
+            }
+            if (inputLine.length == 3) {
+                log.debug("Found start of route: {}", inputLine[FIELD_ROUTE_NAME]);
+                route = routeManager.getRouteByName(inputLine[FIELD_ROUTE_NAME]);
+                if (route != null) {
+                    log.info("Route: {} already exists, will not import!", inputLine[FIELD_ROUTE_NAME]);
+                    route = null;
+                } else {
+                    routesAdded++;
+                    route = routeManager.newRoute(inputLine[FIELD_ROUTE_NAME]);
+                    log.info("Creating route: {}", inputLine[FIELD_ROUTE_NAME]);
+                    route.setComment(inputLine[FIELD_ROUTE_COMMENT]);
+                }
+            }
+            if (route != null && inputLine.length == 15) {
+                log.debug("Adding route location: {}", inputLine[FIELD_ROUTE_LOCATION_NAME]);
+                Location location = locationManager.getLocationByName(inputLine[FIELD_ROUTE_LOCATION_NAME]);
+                RouteLocation rl = route.addLocation(location);
+                rl.setTrainDirection(Setup.getDirectionInt(inputLine[FIELD_ROUTE_LOCATION_DIRECTION]));
+                rl.setMaxCarMoves(Integer.parseInt(inputLine[FIELD_ROUTE_LOCATION_MOVES]));
+                rl.setRandomControl(inputLine[FIELD_ROUTE_LOCATION_RANDOM]);
+                rl.setPickUpAllowed(inputLine[FIELD_ROUTE_LOCATION_PICKUP_ALLOWED].equals(Bundle.getMessage("yes")));
+                rl.setDropAllowed(inputLine[FIELD_ROUTE_LOCATION_DROP_ALLOWED].equals(Bundle.getMessage("yes")));
+                rl.setWait(Integer.parseInt(inputLine[FIELD_ROUTE_LOCATION_TRAVEL]) - Setup.getTravelTime());
+                rl.setDepartureTime(inputLine[FIELD_ROUTE_LOCATION_DEPARTURE_TIME]);
+                rl.setMaxTrainLength(Integer.parseInt(inputLine[FIELD_ROUTE_LOCATION_TRAIN_LENGTH]));
+                rl.setGrade(Double.parseDouble(inputLine[FIELD_ROUTE_LOCATION_GRADE]));
+                rl.setTrainIconX(Integer.parseInt(inputLine[FIELD_ROUTE_LOCATION_ICON_X]));
+                rl.setTrainIconY(Integer.parseInt(inputLine[FIELD_ROUTE_LOCATION_ICON_Y]));
+                rl.setComment(inputLine[FIELD_ROUTE_LOCATION_COMMENT]);
+                rl.setCommentTextColor(inputLine[FIELD_ROUTE_LOCATION_COMMENT_COLOR]);
+            }
+        }
+
+        if (importOkay) {
+            JmriJOptionPane.showMessageDialog(null, Bundle.getMessage("ImportRoutesAdded", routesAdded),
+                    Bundle.getMessage("SuccessfulImport"), JmriJOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JmriJOptionPane.showMessageDialog(null, Bundle.getMessage("ImportRoutesAdded", routesAdded),
+                    Bundle.getMessage("ImportFailed"), JmriJOptionPane.ERROR_MESSAGE);
+        }
+        fstatus.dispose();
+    }
+
+    private String[] readNextLine(BufferedReader rdr) {
+        String line = " ";
+        lineNumber.setText(Bundle.getMessage("LineNumber", Integer.toString(++lineNum)));
+        try {
+            line = rdr.readLine();
+        } catch (IOException e) {
+            return BREAK;
+        }
+        if (line == null) {
+            importOkay = true;
+            return BREAK;
+        }
+        if (!fstatus.isShowing()) {
+            //user canceled input!
+            return BREAK;
+        }
+        line = line.trim();
+        importLine.setText(line);
+
+        String[] inputLine = parseCommaLine(line);
+        log.debug("Import line number {} has {} elements", lineNum, inputLine.length);
+
+        return inputLine;
+    }
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ImportRoutes.class);
+
+}
