1d9f391ac0c714e50e383e7a60134e6969451089
Paul Bender <paul.bender@acm.org>
2025-01-23 21:22:31 -0500

    remove unused import

--- java/src/jmri/jmrix/lenz/XNetMessage.java
+++ java/src/jmri/jmrix/lenz/XNetMessage.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
