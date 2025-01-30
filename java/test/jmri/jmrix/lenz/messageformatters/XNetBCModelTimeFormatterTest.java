a82984bcea4f82268a2d3f7dbc4aef06f2c50d3c
Paul Bender <paul.bender@acm.org>
2025-01-29 22:57:02 -0500

    fix tests

--- java/test/jmri/jmrix/lenz/messageformatters/XNetBCModelTimeFormatterTest.java
+++ java/test/jmri/jmrix/lenz/messageformatters/XNetBCModelTimeFormatterTest.java
@@ -15,7 +15,6 @@ public class XNetBCModelTimeFormatterTest {
     @Test
     public void testFormatMessage() {
         XNetBCModelTimeFormatter formatter = new XNetBCModelTimeFormatter();
-        byte[] msg = {(byte) 0x63, (byte) 0x03, (byte) 0x95, (byte) 0x36, (byte) 0x00};
         XNetReply reply = new XNetReply("63 03 95 36 C3");
         assertThat(formatter.handlesMessage(reply)).isTrue();
         assertThat(formatter.formatMessage(reply)).isEqualTo("Fast Clock Broadcast: Day 4 time 21:54 clock is running");
