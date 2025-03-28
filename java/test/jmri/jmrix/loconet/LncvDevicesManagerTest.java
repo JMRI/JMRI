13f2b915737483af3a646e6b09ce706defe94235
Steve Young <icklesteve@yahoo.com>
2025-03-28 01:43:24 +0000

    update tests
    dispose the dm after test
    dispose tc after tests
    wait for roster thread complete

--- java/test/jmri/jmrix/loconet/LncvDevicesManagerTest.java
+++ java/test/jmri/jmrix/loconet/LncvDevicesManagerTest.java
@@ -15,39 +15,48 @@ class LncvDevicesManagerTest {
     public void testCTor() {
         LncvDevicesManager lcdm = new LncvDevicesManager(memo);
         Assertions.assertNotNull(lcdm, "LncvDeviceManager exists");
+        lcdm.dispose();
     }
 
     @Test
     void testGetDeviceList() {
         LncvDevicesManager lcdm = new LncvDevicesManager(memo);
         Assertions.assertNotNull(lcdm.getDeviceList(), "LncvDeviceManager List exists");
+        lcdm.dispose();
     }
 
     @Test
     void testGetDeviceCount() {
+        JUnitUtil.initRosterConfigManager();
         LncvDevicesManager lcdm = new LncvDevicesManager(memo);
         Assertions.assertEquals(0, lcdm.getDeviceCount(), "LncvDeviceManager List empty");
         lcdm.message(new LocoNetMessage(new int[] {0xE5, 0x0F, 0x05, 0x49, 0x4B, 0x1F, 0x11, 0x29, 0x13, 0x00, 0x00, 0x08, 0x00, 0x00, 0x4D}));
         Assertions.assertEquals(1, lcdm.getDeviceCount(), "LncvDeviceManager List added 1");
+        JUnitUtil.waitThreadTerminated(LncvDevicesManager.ROSTER_THREAD_NAME+memo.getSystemPrefix());
+        lcdm.dispose();
     }
 
     @Test
     void testGetDevice() {
         LncvDevicesManager lcdm = new LncvDevicesManager(memo);
         Assertions.assertNull(lcdm.getDevice(5000, 8), "LncvDeviceManager List exists");
+        lcdm.dispose();
     }
 
     @BeforeEach
     public void setUp() {
         JUnitUtil.setUp();
         JUnitUtil.resetInstanceManager();
-        memo = new jmri.jmrix.loconet.LocoNetSystemConnectionMemo();
-        jmri.InstanceManager.setDefault(jmri.jmrix.loconet.LocoNetSystemConnectionMemo.class, memo);
-        JUnitUtil.initRosterConfigManager();
+        memo = new LocoNetSystemConnectionMemo();
+        jmri.InstanceManager.setDefault(LocoNetSystemConnectionMemo.class, memo);
     }
 
     @AfterEach
     public void tearDown() {
+        var tc = memo.getLnTrafficController();
+        if ( tc != null ) {
+            tc.dispose();
+        }
         memo = null;
         JUnitUtil.tearDown();
     }
--- java/test/jmri/jmrix/loconet/Lnsv1DevicesManagerTest.java
+++ java/test/jmri/jmrix/loconet/Lnsv1DevicesManagerTest.java
@@ -15,26 +15,32 @@ class Lnsv1DevicesManagerTest {
     public void testCTor() {
         Lnsv1DevicesManager lsvm = new Lnsv1DevicesManager(memo);
         Assertions.assertNotNull(lsvm, "Lnsv1DeviceManager exists");
+        lsvm.dispose();
     }
 
     @Test
     void testGetDeviceList() {
         Lnsv1DevicesManager lsvm = new Lnsv1DevicesManager(memo);
         Assertions.assertNotNull(lsvm.getDeviceList(), "Lnsv1DeviceManager List exists");
+        lsvm.dispose();
     }
 
     @Test
     void testGetDeviceCount() {
+        JUnitUtil.initRosterConfigManager();
         Lnsv1DevicesManager lsvm = new Lnsv1DevicesManager(memo);
         Assertions.assertEquals(0, lsvm.getDeviceCount(), "Lnsv1DeviceManager List empty");
         lsvm.message(new LocoNetMessage(new int[] {0xE5, 0x10, 0x04, 0x50, 0x01, 0x06, 0x02, 0x13, 0x16, 0x7B, 0x00, 0x02, 0x00, 0x00, 0x00, 0x27}));
         Assertions.assertEquals(1, lsvm.getDeviceCount(), "Lnsv1DeviceManager List added 1");
+        JUnitUtil.waitThreadTerminated(Lnsv1DevicesManager.ROSTER_THREAD_NAME+memo.getSystemPrefix());
+        lsvm.dispose();
     }
 
     @Test
     void testGetDevice() {
         Lnsv1DevicesManager lsvm = new Lnsv1DevicesManager(memo);
         Assertions.assertNull(lsvm.getDevice(5000, 8), "Lnsv1DeviceManager List exists");
+        lsvm.dispose();
     }
 
     @BeforeEach
@@ -43,11 +49,14 @@ class Lnsv1DevicesManagerTest {
         JUnitUtil.resetInstanceManager();
         memo = new LocoNetSystemConnectionMemo();
         jmri.InstanceManager.setDefault(LocoNetSystemConnectionMemo.class, memo);
-        JUnitUtil.initRosterConfigManager();
     }
 
     @AfterEach
     public void tearDown() {
+        var tc = memo.getLnTrafficController();
+        if ( tc != null ) {
+            tc.dispose();
+        }
         memo = null;
         JUnitUtil.tearDown();
     }
