98b5e6cdd3f3ca58463ef2e927037538c0cd49b1
lee_f_000 <lee_f_000@Base.broadband>
2025-04-20 21:14:43 +0100

    Updated SprogNanoSerialDriver with Options for Sprogslots

--- java/src/jmri/jmrix/sprog/sprognano/SprogNanoSerialDriverAdapter.java
+++ java/src/jmri/jmrix/sprog/sprognano/SprogNanoSerialDriverAdapter.java
@@ -2,6 +2,9 @@ package jmri.jmrix.sprog.sprognano;
 
 import jmri.jmrix.sprog.SprogConstants.SprogMode;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Implements SerialPortAdapter for the Sprog system.
  * <p>
@@ -18,6 +21,9 @@ public class SprogNanoSerialDriverAdapter
 
     public SprogNanoSerialDriverAdapter() {
         super(SprogMode.OPS);
+        options.put("NumSlots", // NOI18N
+                new Option(Bundle.getMessage("MakeLabel", Bundle.getMessage("NumSlotOptions")), // NOI18N
+                        new String[]{"16", "8", "32", "48", "64"}, true));
         options.put("TrackPowerState", new Option(Bundle.getMessage("OptionTrackPowerLabel"),
                 new String[]{Bundle.getMessage("PowerStateOff"), Bundle.getMessage("PowerStateOn")},
                 true)); // first element (TrackPowerState) NOI18N
@@ -25,6 +31,24 @@ public class SprogNanoSerialDriverAdapter
         this.getSystemConnectionMemo().setUserName(Bundle.getMessage("SprogNanoCSTitle"));
     }
     
-    // private final static Logger log = LoggerFactory.getLogger(SprogNanoSerialDriverAdapter.class);
+    /**
+     * Set up all of the other objects to operate with an Sprog command station
+     * connected to this port.
+     */
+    @Override
+    public void configure() {
+        String slots = getOptionState("NumSlots");
+        try {
+            numSlots = Integer.parseInt(slots);
+        }
+        catch (NumberFormatException e) {
+            log.warn("Could not parse number of slots {}", e.getMessage());
+            numSlots = 16;
+        }
+        
+        super.configure();
+    }
+
+    private final static Logger log = LoggerFactory.getLogger(SprogNanoSerialDriverAdapter.class);
 
 }
