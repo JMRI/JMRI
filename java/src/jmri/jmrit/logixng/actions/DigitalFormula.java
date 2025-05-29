714f47bc1aea62feaced25dc4c14d68db665fe3c
dsand47 <gh@dfm.sandsys.org>
2025-05-29 15:32:35 -0500

    Add WhereUsed support to the DigitalFormula classes

--- java/src/jmri/jmrit/logixng/actions/DigitalFormula.java
+++ java/src/jmri/jmrit/logixng/actions/DigitalFormula.java
@@ -12,6 +12,8 @@ import javax.annotation.CheckForNull;
 import jmri.InstanceManager;
 import jmri.JmriException;
 import jmri.Manager;
+import jmri.NamedBean;
+import jmri.NamedBeanUsageReport;
 import jmri.jmrit.logixng.*;
 import jmri.jmrit.logixng.implementation.DefaultFemaleGenericExpressionSocket;
 import jmri.jmrit.logixng.util.parser.ParserException;
@@ -393,6 +395,18 @@ public class DigitalFormula extends AbstractDigitalAction implements FemaleSocke
     public void disposeMe() {
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public void getUsageDetail(int level, NamedBean bean, List<NamedBeanUsageReport> report, NamedBean cdl) {
+        log.debug("getUsageReport :: Action DigitalFormula: bean = {}, f = \"{}\", cdl = {}", bean, getFormula(), cdl);
+        if (bean != null) {
+            // Search for possible bean references using system and user names.  False positives are possible.
+            var formula = getFormula();
+            if (formula.contains(bean.getSystemName()) || formula.contains(bean.getUserName())) {
+                report.add(new NamedBeanUsageReport("LogixNGAction", cdl, getLongDescription()));
+            }
+        }
+    }
 
     public static class SocketData {
         public final String _socketName;
