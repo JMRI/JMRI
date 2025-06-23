ce231f84b6d58aa59793d181f7f327c4d4cc3cf5
Bob Jacobsen <jacobsen@mac.com>
2025-06-23 09:17:12 -0500

    move debug logging to trace level

--- java/src/jmri/jmrit/display/configurexml/PositionableLabelXml.java
+++ java/src/jmri/jmrit/display/configurexml/PositionableLabelXml.java
@@ -617,7 +617,7 @@ public class PositionableLabelXml extends AbstractXmlAdapter {
     String simplifyFontname(String fontname, int style) {
         var loadAndStorePreferences = InstanceManager.getDefault(jmri.configurexml.LoadAndStorePreferences.class);
         if (! loadAndStorePreferences.isExcludeFontExtensions() ) { 
-            log.error("Just for CI debugging - returning early from simplifyFontname with {}", fontname);
+            log.trace("Returning early from simplifyFontname with {}", fontname);
             return fontname;
         }
 
