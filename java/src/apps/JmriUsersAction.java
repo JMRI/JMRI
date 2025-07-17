a7ef9d013f8dd71d267d73efc33c0609ba76b218
Bob Jacobsen <jacobsen@mac.com>
2025-07-17 13:24:32 -0400

    static analysis imports

--- java/src/apps/JmriUsersAction.java
+++ java/src/apps/JmriUsersAction.java
@@ -1,15 +1,7 @@
 package apps;
 
 import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-
-import jmri.util.FileUtil;
 import jmri.util.swing.JmriPanel;
 import jmri.util.swing.WindowInterface;
 
