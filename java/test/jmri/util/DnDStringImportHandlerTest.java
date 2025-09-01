cec50386b06ccf46f4672784581247107df1077f
Steve Young <icklesteve@yahoo.com>
2025-09-01 18:43:04 +0100

    update asserts to JUnit5 util d-i
    JU4 > JU5

--- java/test/jmri/util/DnDStringImportHandlerTest.java
+++ java/test/jmri/util/DnDStringImportHandlerTest.java
@@ -1,6 +1,5 @@
 package jmri.util;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
 /**
@@ -12,7 +11,7 @@ public class DnDStringImportHandlerTest {
     @Test
     public void testCTor() {
         DnDStringImportHandler t = new DnDStringImportHandler();
-        Assert.assertNotNull("exists",t);
+        Assertions.assertNotNull( t, "exists");
     }
 
     @BeforeEach
