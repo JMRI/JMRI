e37f3bf6567a8917ad9dd4dfae542abcf55f1ada
Steve Young <icklesteve@yahoo.com>
2025-09-02 13:20:59 +0100

    update asserts to Junit5 util - usb / zeroconf
    JU4 > JU5

--- java/test/jmri/util/usb/UsbBrowserActionTest.java
+++ java/test/jmri/util/usb/UsbBrowserActionTest.java
@@ -2,7 +2,6 @@ package jmri.util.usb;
 
 import jmri.util.JUnitUtil;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
 /**
@@ -14,7 +13,7 @@ public class UsbBrowserActionTest {
     @Test
     public void testCTor() {
         UsbBrowserAction t = new UsbBrowserAction();
-        Assert.assertNotNull("exists",t);
+        Assertions.assertNotNull( t, "exists");
     }
 
     @BeforeEach
