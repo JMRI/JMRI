3842f950c36fe6ad8f19d25a6dfe17b538fc8ee1
Steve Young <icklesteve@yahoo.com>
2025-09-10 15:05:49 +0100

    update asserts to JUnit5 util - swing r-x
    JU4 > JU5

--- java/test/jmri/util/swing/TextFilterTest.java
+++ java/test/jmri/util/swing/TextFilterTest.java
@@ -5,15 +5,42 @@ import java.io.IOException;
 
 import jmri.util.JUnitUtil;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.io.TempDir;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
  * @author Randall Wood Copyright 2017
  */
 public class TextFilterTest {
+
+    @Test
+    public void testConstructor() {
+        TextFilter instance = new TextFilter();
+        assertNotNull(instance);
+    }
+
+    @Test
+    public void testAccept(@TempDir File folder) throws IOException {
+        File dir = folder;
+        File txt = new File(folder, "text.txt");
+        File xml = new File(folder, "xml.xml");
+        TextFilter instance = new TextFilter();
+        assertTrue(instance.accept(dir));
+        assertTrue(instance.accept(txt));
+        assertFalse(instance.accept(xml));
+    }
+
+    @Test
+    public void testGetDescription() {
+        TextFilter instance = new TextFilter();
+        assertEquals("Text Documents (*.txt)", instance.getDescription());
+    }
 
     @BeforeEach
     public void setUp() {
@@ -23,29 +50,6 @@ public class TextFilterTest {
     @AfterEach
     public void tearDown() {
         JUnitUtil.tearDown();
-    }
-
-    @Test
-    public void testConstructor() {
-        TextFilter instance = new TextFilter();
-        Assert.assertNotNull(instance);
-    }
-
-    @Test
-    public void testAccept(@TempDir File folder) throws IOException {
-        File dir = folder;
-        File txt = new File(folder, "text.txt");
-        File xml = new File(folder, "xml.xml");
-        TextFilter instance = new TextFilter();
-        Assert.assertTrue(instance.accept(dir));
-        Assert.assertTrue(instance.accept(txt));
-        Assert.assertFalse(instance.accept(xml));
-    }
-
-    @Test
-    public void testGetDescription() {
-        TextFilter instance = new TextFilter();
-        Assert.assertEquals("Text Documents (*.txt)", instance.getDescription());
     }
 
 }
