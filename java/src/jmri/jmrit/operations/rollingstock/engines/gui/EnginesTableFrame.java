b2c329cfe2de0379d7a212a2d6d487178c827048
dabou <dabou@DBoudreau-2022>
2025-09-18 16:23:51 -0400

    OperationsPro improve test coverage

--- java/src/jmri/jmrit/operations/rollingstock/engines/gui/EnginesTableFrame.java
+++ java/src/jmri/jmrit/operations/rollingstock/engines/gui/EnginesTableFrame.java
@@ -28,7 +28,7 @@ import jmri.util.swing.JmriJOptionPane;
 public class EnginesTableFrame extends OperationsFrame implements TableModelListener {
 
     public EnginesTableModel enginesTableModel;
-    JTable enginesTable;
+    public JTable enginesTable;
     boolean showAllLocos = true;
     JScrollPane enginesPane;
     EngineManager engineManager = InstanceManager.getDefault(EngineManager.class);
