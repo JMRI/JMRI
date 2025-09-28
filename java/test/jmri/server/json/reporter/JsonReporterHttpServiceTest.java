67194a1f0767ff5a5826d2bfa148351a492cd364
Steve Young <icklesteve@yahoo.com>
2025-09-28 13:39:07 +0100

    Update JsonReporterHttpServiceTest.java
    unused imports

--- java/test/jmri/server/json/reporter/JsonReporterHttpServiceTest.java
+++ java/test/jmri/server/json/reporter/JsonReporterHttpServiceTest.java
@@ -27,13 +27,10 @@ import jmri.util.JUnitUtil;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  *
