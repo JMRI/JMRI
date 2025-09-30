2c450378616dbefccdc948a622540b97d04330e0
Steve Young <icklesteve@yahoo.com>
2025-09-30 15:38:10 +0100

    dccpp

--- java/test/jmri/jmrix/dccpp/swing/ConfigBaseStationFrameTest.java
+++ java/test/jmri/jmrix/dccpp/swing/ConfigBaseStationFrameTest.java
@@ -1,11 +1,10 @@
 package jmri.jmrix.dccpp.swing;
 
-import java.awt.GraphicsEnvironment;
-
 import jmri.jmrix.dccpp.DCCppCommandStation;
 import jmri.jmrix.dccpp.DCCppInterfaceScaffold;
 import jmri.jmrix.dccpp.DCCppSystemConnectionMemo;
 import jmri.util.JUnitUtil;
+import jmri.util.junit.annotations.DisabledIfHeadless;
 
 import org.junit.jupiter.api.*;
 
@@ -13,7 +12,10 @@ import org.junit.jupiter.api.*;
  *
  * @author Paul Bender Copyright (C) 2017
  */
+@DisabledIfHeadless
 public class ConfigBaseStationFrameTest extends jmri.util.JmriJFrameTestBase {
+
+    private DCCppSystemConnectionMemo memo;
 
     @BeforeEach
     @Override
@@ -21,17 +23,18 @@ public class ConfigBaseStationFrameTest extends jmri.util.JmriJFrameTestBase {
         JUnitUtil.setUp();
         // infrastructure objects
         DCCppInterfaceScaffold tc = new DCCppInterfaceScaffold(new DCCppCommandStation());
-        DCCppSystemConnectionMemo memo = new DCCppSystemConnectionMemo(tc);
-        if (!GraphicsEnvironment.isHeadless()) {
-            frame = new ConfigBaseStationFrame(memo);
-        }
+        memo = new DCCppSystemConnectionMemo(tc);
+        frame = new ConfigBaseStationFrame(memo);
 
     }
 
     @AfterEach
     @Override
     public void tearDown() {
-        JUnitUtil.clearShutDownManager(); // put in place because AbstractMRTrafficController implementing subclass was not terminated properly
+        memo.getDCCppTrafficController().terminateThreads();
+        memo.dispose();
+        memo = null;
+        JUnitUtil.deregisterBlockManagerShutdownTask();
         super.tearDown();
     }
 
