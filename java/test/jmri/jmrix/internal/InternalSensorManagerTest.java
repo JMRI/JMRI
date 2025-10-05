877d15645432b2e85b4f9dccc242760b83a2429b
Steve Young <icklesteve@yahoo.com>
2025-10-05 15:42:21 +0100

    Update InternalSensorManagerTest.java
    unused import

--- java/test/jmri/jmrix/internal/InternalSensorManagerTest.java
+++ java/test/jmri/jmrix/internal/InternalSensorManagerTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
