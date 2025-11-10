ce2d0ac1ef3e32e53c0c09d2dd74a98a2b3b222f
Steve Young <icklesteve@yahoo.com>
2025-11-10 21:17:45 +0000

    Update ActionListenOnBeansTest.java
    unused import

--- java/test/jmri/jmrit/logixng/actions/ActionListenOnBeansTest.java
+++ java/test/jmri/jmrit/logixng/actions/ActionListenOnBeansTest.java
@@ -1,7 +1,6 @@
 package jmri.jmrit.logixng.actions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
