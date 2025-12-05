4c5fa2be883252482ad2ccaa8d1ca9ca57b9c6a9
Ralf Lang <ralf.lang@ralf-lang.de>
2025-12-05 10:44:01 +0100

    style: Undo multiple file reformatting
    style: Undo non-functional changes to ListedTableFrame.java
    
    style: undo non-functional changes to LayoutTurnoutView.java
    
    style: undo ws changes to ToolsMenu.java
    
    style: Undo LayoutEditor.java nonfunctional changes

--- java/src/jmri/jmrit/ToolsMenu.java
+++ java/src/jmri/jmrit/ToolsMenu.java
@@ -33,10 +33,8 @@ public class ToolsMenu extends JMenu {
     ConnectionConfig serModeProCon = null;
     ConnectionConfig opsModeProCon = null;
 
-    AbstractAction serviceAction = new jmri.jmrit.symbolicprog.tabbedframe.PaneProgAction(
-            Bundle.getMessage("MenuItemDecoderProServiceProgrammer"));
-    AbstractAction opsAction = new jmri.jmrit.symbolicprog.tabbedframe.PaneOpsProgAction(
-            Bundle.getMessage("MenuItemDecoderProOpsModeProgrammer"));
+    AbstractAction serviceAction = new jmri.jmrit.symbolicprog.tabbedframe.PaneProgAction(Bundle.getMessage("MenuItemDecoderProServiceProgrammer"));
+    AbstractAction opsAction = new jmri.jmrit.symbolicprog.tabbedframe.PaneOpsProgAction(Bundle.getMessage("MenuItemDecoderProOpsModeProgrammer"));
 
     public ToolsMenu(String name) {
         this();
@@ -57,77 +55,51 @@ public class ToolsMenu extends JMenu {
         add(programmerMenu);
 
         // disable programmer menu if there's no programmer manager
-        if (InstanceManager.getNullableDefault(jmri.AddressedProgrammerManager.class) == null &&
-                InstanceManager.getNullableDefault(jmri.GlobalProgrammerManager.class) == null) {
+        if (InstanceManager.getNullableDefault(jmri.AddressedProgrammerManager.class) == null
+                && InstanceManager.getNullableDefault(jmri.GlobalProgrammerManager.class) == null) {
             programmerMenu.setEnabled(false);
         }
 
         JMenu tableMenu = new JMenu(Bundle.getMessage("MenuTables"));
 
         ///tableMenu.add(tableMenu);    /// <=== WHY?
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemTurnoutTable"),
-                "jmri.jmrit.beantable.TurnoutTableTabAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSensorTable"),
-                "jmri.jmrit.beantable.SensorTableTabAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLightTable"),
-                "jmri.jmrit.beantable.LightTableTabAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemTurnoutTable"), "jmri.jmrit.beantable.TurnoutTableTabAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSensorTable"), "jmri.jmrit.beantable.SensorTableTabAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLightTable"), "jmri.jmrit.beantable.LightTableTabAction"));
 
         JMenu signalMenu = new JMenu(Bundle.getMessage("MenuSignals"));
-        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalTable"),
-                "jmri.jmrit.beantable.SignalHeadTableAction"));
-        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalMastTable"),
-                "jmri.jmrit.beantable.SignalMastTableAction"));
-        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalGroupTable"),
-                "jmri.jmrit.beantable.SignalGroupTableAction"));
-        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalMastLogicTable"),
-                "jmri.jmrit.beantable.SignalMastLogicTableAction"));
+        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalTable"), "jmri.jmrit.beantable.SignalHeadTableAction"));
+        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalMastTable"), "jmri.jmrit.beantable.SignalMastTableAction"));
+        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalGroupTable"), "jmri.jmrit.beantable.SignalGroupTableAction"));
+        signalMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSignalMastLogicTable"), "jmri.jmrit.beantable.SignalMastLogicTableAction"));
         tableMenu.add(signalMenu);
 
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemReporterTable"),
-                "jmri.jmrit.beantable.ReporterTableTabAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemMemoryTable"),
-                "jmri.jmrit.beantable.MemoryTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemStringIOTable"),
-                "jmri.jmrit.beantable.StringIOTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemRouteTable"),
-                "jmri.jmrit.beantable.RouteTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLRouteTable"),
-                "jmri.jmrit.beantable.LRouteTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixTable"),
-                "jmri.jmrit.beantable.LogixTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemReporterTable"), "jmri.jmrit.beantable.ReporterTableTabAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemMemoryTable"), "jmri.jmrit.beantable.MemoryTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemStringIOTable"), "jmri.jmrit.beantable.StringIOTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemRouteTable"), "jmri.jmrit.beantable.RouteTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLRouteTable"), "jmri.jmrit.beantable.LRouteTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixTable"), "jmri.jmrit.beantable.LogixTableAction"));
 
         JMenu logixNG_Menu = new JMenu(Bundle.getMessage("MenuLogixNG"));
-        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGTable"),
-                "jmri.jmrit.beantable.LogixNGTableAction"));
-        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGModuleTable"),
-                "jmri.jmrit.beantable.LogixNGModuleTableAction"));
-        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGTableTable"),
-                "jmri.jmrit.beantable.LogixNGTableTableAction"));
-        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(
-                Bundle.getMessage("MenuItemLogixNGGlobalVariableTableAction"),
-                "jmri.jmrit.beantable.LogixNGGlobalVariableTableAction"));
+        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGTable"), "jmri.jmrit.beantable.LogixNGTableAction"));
+        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGModuleTable"), "jmri.jmrit.beantable.LogixNGModuleTableAction"));
+        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGTableTable"), "jmri.jmrit.beantable.LogixNGTableTableAction"));
+        logixNG_Menu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemLogixNGGlobalVariableTableAction"), "jmri.jmrit.beantable.LogixNGGlobalVariableTableAction"));
         tableMenu.add(logixNG_Menu);
 
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemBlockTable"),
-                "jmri.jmrit.beantable.BlockTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemBlockTable"), "jmri.jmrit.beantable.BlockTableAction"));
         if (InstanceManager.getDefault(GuiLafPreferencesManager.class).isOblockEditTabbed()) { // turn on or off in prefs
-            tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemOBlockTable"),
-                    "jmri.jmrit.beantable.OBlockTableAction"));
+            tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemOBlockTable"), "jmri.jmrit.beantable.OBlockTableAction"));
         } else {
             tableMenu.add(new jmri.jmrit.beantable.OBlockTableAction(Bundle.getMessage("MenuItemOBlockTable")));
         }
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSectionTable"),
-                "jmri.jmrit.beantable.SectionTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemTransitTable"),
-                "jmri.jmrit.beantable.TransitTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemAudioTable"),
-                "jmri.jmrit.beantable.AudioTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemIdTagTable"),
-                "jmri.jmrit.beantable.IdTagTableTabAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemRailComTable"),
-                "jmri.jmrit.beantable.RailComTableAction"));
-        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemTrackTilesTable"),
-                "jmri.jmrit.beantable.TrackTileTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemSectionTable"), "jmri.jmrit.beantable.SectionTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemTransitTable"), "jmri.jmrit.beantable.TransitTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemAudioTable"), "jmri.jmrit.beantable.AudioTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemIdTagTable"), "jmri.jmrit.beantable.IdTagTableTabAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemRailComTable"), "jmri.jmrit.beantable.RailComTableAction"));
+        tableMenu.add(new jmri.jmrit.beantable.ListedTableAction(Bundle.getMessage("MenuItemTrackTilesTable"), "jmri.jmrit.beantable.TrackTileTableAction"));
         add(tableMenu);
 
         JMenu throttleMenu = new JMenu(Bundle.getMessage("MenuThrottles"));
@@ -135,20 +107,15 @@ public class ToolsMenu extends JMenu {
 
         throttleMenu.add(new jmri.jmrit.throttle.ThrottlesListAction(Bundle.getMessage("MenuItemThrottlesList")));
         throttleMenu.addSeparator();
-        throttleMenu.add(
-                new jmri.jmrit.throttle.StoreXmlThrottlesLayoutAction(Bundle.getMessage("MenuItemSaveThrottleLayout")));
-        throttleMenu.add(
-                new jmri.jmrit.throttle.LoadXmlThrottlesLayoutAction(Bundle.getMessage("MenuItemLoadThrottleLayout")));
+        throttleMenu.add(new jmri.jmrit.throttle.StoreXmlThrottlesLayoutAction(Bundle.getMessage("MenuItemSaveThrottleLayout")));
+        throttleMenu.add(new jmri.jmrit.throttle.LoadXmlThrottlesLayoutAction(Bundle.getMessage("MenuItemLoadThrottleLayout")));
         throttleMenu.addSeparator();
-        throttleMenu.add(new jmri.jmrit.throttle.StoreDefaultXmlThrottlesLayoutAction(
-                Bundle.getMessage("MenuItemSaveAsDefaultThrottleLayout")));
-        throttleMenu.add(new jmri.jmrit.throttle.LoadDefaultXmlThrottlesLayoutAction(
-                Bundle.getMessage("MenuItemLoadDefaultThrottleLayout")));
+        throttleMenu.add(new jmri.jmrit.throttle.StoreDefaultXmlThrottlesLayoutAction(Bundle.getMessage("MenuItemSaveAsDefaultThrottleLayout")));
+        throttleMenu.add(new jmri.jmrit.throttle.LoadDefaultXmlThrottlesLayoutAction(Bundle.getMessage("MenuItemLoadDefaultThrottleLayout")));
         //throttleMenu.addSeparator();
         //throttleMenu.add(new jmri.jmrit.throttle.ThrottlesPreferencesAction(Bundle.getMessage("MenuItemThrottlesPreferences"))); // now in tabbed preferences
         throttleMenu.add(new JSeparator());
-        throttleMenu
-                .add(new jmri.jmrit.withrottle.WiThrottleCreationAction(Bundle.getMessage("MenuItemStartWiThrottle")));
+        throttleMenu.add(new jmri.jmrit.withrottle.WiThrottleCreationAction(Bundle.getMessage("MenuItemStartWiThrottle")));
         add(throttleMenu);
 
         // disable the throttle menu if there is no throttle Manager
@@ -156,8 +123,7 @@ public class ToolsMenu extends JMenu {
             throttleMenu.setEnabled(false);
         }
 
-        AbstractAction consistAction =
-                new jmri.jmrit.consisttool.ConsistToolAction(Bundle.getMessage("MenuItemConsistTool"));
+        AbstractAction consistAction = new jmri.jmrit.consisttool.ConsistToolAction(Bundle.getMessage("MenuItemConsistTool"));
 
         add(consistAction);
 
@@ -220,10 +186,8 @@ public class ToolsMenu extends JMenu {
         add(new JSeparator());
         JMenu vsdMenu = new JMenu(Bundle.getMessage("MenuItemVSDecoder"));
         vsdMenu.add(new jmri.jmrit.vsdecoder.VSDecoderCreationAction(Bundle.getMessage("MenuItemVSDecoderManager")));
-        vsdMenu.add(new jmri.jmrit.vsdecoder.swing.ManageLocationsAction(
-                Bundle.getMessage("MenuItemVSDecoderLocationManager")));
-        vsdMenu.add(
-                new jmri.jmrit.vsdecoder.swing.VSDPreferencesAction(Bundle.getMessage("MenuItemVSDecoderPreferences")));
+        vsdMenu.add(new jmri.jmrit.vsdecoder.swing.ManageLocationsAction(Bundle.getMessage("MenuItemVSDecoderLocationManager")));
+        vsdMenu.add(new jmri.jmrit.vsdecoder.swing.VSDPreferencesAction(Bundle.getMessage("MenuItemVSDecoderPreferences")));
         add(vsdMenu);
 
         add(new JSeparator());
@@ -246,8 +210,7 @@ public class ToolsMenu extends JMenu {
                     }
                     updateProgrammerStatus(evt);
                 });
-        InstanceManager.getList(AddressedProgrammerManager.class)
-                .forEach(m -> m.addPropertyChangeListener(this::updateProgrammerStatus));
+        InstanceManager.getList(AddressedProgrammerManager.class).forEach(m -> m.addPropertyChangeListener(this::updateProgrammerStatus));
         InstanceManager.addPropertyChangeListener(InstanceManager.getListPropertyName(GlobalProgrammerManager.class),
                 evt -> {
                     GlobalProgrammerManager m = (GlobalProgrammerManager) evt.getNewValue();
@@ -256,8 +219,7 @@ public class ToolsMenu extends JMenu {
                     }
                     updateProgrammerStatus(evt);
                 });
-        InstanceManager.getList(GlobalProgrammerManager.class)
-                .forEach(m -> m.addPropertyChangeListener(this::updateProgrammerStatus));
+        InstanceManager.getList(GlobalProgrammerManager.class).forEach(m -> m.addPropertyChangeListener(this::updateProgrammerStatus));
 
         // add items given by ToolsMenuItem service provider
         var newItemList = new ArrayList<ToolsMenuAction>();
@@ -275,9 +237,10 @@ public class ToolsMenu extends JMenu {
     }
 
     /**
-     * Enable or disable the service mode programmer menu items for the types of
-     * programmer available. Adapted from similar named function in @link
-     * jmri.jmrit.roster.swing.RosterFrame.java
+     * Enable or disable the service mode programmer menu items for the types of programmer
+     * available.
+     *
+     * Adapted from similar named function in @link jmri.jmrit.roster.swing.RosterFrame.java
      *
      * @param evt the triggering event; if not null and if a removal of a
      *            ProgrammerManager, care will be taken not to trigger the
@@ -292,11 +255,10 @@ public class ToolsMenu extends JMenu {
 
         // Find the connection that goes with the global programmer
         // test that IM has a default GPM, or that event is not the removal of a GPM
-        if (InstanceManager.containsDefault(GlobalProgrammerManager.class) ||
-                (evt != null &&
-                        evt.getPropertyName()
-                                .equals(InstanceManager.getDefaultsPropertyName(GlobalProgrammerManager.class)) &&
-                        evt.getNewValue() == null)) {
+        if (InstanceManager.containsDefault(GlobalProgrammerManager.class)
+                || (evt != null
+                && evt.getPropertyName().equals(InstanceManager.getDefaultsPropertyName(GlobalProgrammerManager.class))
+                && evt.getNewValue() == null)) {
             gpm = InstanceManager.getNullableDefault(GlobalProgrammerManager.class);
             log.trace("found global programming manager {}", gpm);
         }
@@ -306,8 +268,7 @@ public class ToolsMenu extends JMenu {
             InstanceManager.getOptionalDefault(ConnectionConfigManager.class).ifPresent((ccm) -> {
                 for (ConnectionConfig connection : ccm) {
                     log.debug("Checking connection name {}", connection.getConnectionName());
-                    if (connection.getConnectionName() != null &&
-                            connection.getConnectionName().equals(serviceModeProgrammerName)) {
+                    if (connection.getConnectionName() != null && connection.getConnectionName().equals(serviceModeProgrammerName)) {
                         log.debug("Connection found for GlobalProgrammermanager");
                         serModeProCon = connection;
                     }
@@ -317,11 +278,10 @@ public class ToolsMenu extends JMenu {
 
         // Find the connection that goes with the addressed programmer
         // test that IM has a default APM, or that event is not the removal of an APM
-        if (InstanceManager.containsDefault(AddressedProgrammerManager.class) ||
-                (evt != null &&
-                        evt.getPropertyName()
-                                .equals(InstanceManager.getDefaultsPropertyName(AddressedProgrammerManager.class)) &&
-                        evt.getNewValue() == null)) {
+        if (InstanceManager.containsDefault(AddressedProgrammerManager.class)
+                || (evt != null
+                && evt.getPropertyName().equals(InstanceManager.getDefaultsPropertyName(AddressedProgrammerManager.class))
+                && evt.getNewValue() == null)) {
             apm = InstanceManager.getNullableDefault(AddressedProgrammerManager.class);
             log.trace("found addressed programming manager {}", gpm);
         }
@@ -331,8 +291,7 @@ public class ToolsMenu extends JMenu {
             InstanceManager.getOptionalDefault(ConnectionConfigManager.class).ifPresent((ccm) -> {
                 for (ConnectionConfig connection : ccm) {
                     log.debug("Checking connection name {}", connection.getConnectionName());
-                    if (connection.getConnectionName() != null &&
-                            connection.getConnectionName().equals(opsModeProgrammerName)) {
+                    if (connection.getConnectionName() != null && connection.getConnectionName().equals(opsModeProgrammerName)) {
                         log.debug("Connection found for AddressedProgrammermanager");
                         opsModeProCon = connection;
                     }
@@ -340,8 +299,7 @@ public class ToolsMenu extends JMenu {
             });
         }
 
-        log.trace("start global check with {}, {}, {}", serModeProCon, gpm,
-                (gpm != null ? gpm.isGlobalProgrammerAvailable() : "<none>"));
+        log.trace("start global check with {}, {}, {}", serModeProCon, gpm, (gpm != null ? gpm.isGlobalProgrammerAvailable() : "<none>"));
         if (gpm != null && gpm.isGlobalProgrammerAvailable()) {
             log.debug("service mode available");
             if (oldServMode == null) {
