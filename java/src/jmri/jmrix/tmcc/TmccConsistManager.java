f8b16ab0a4110ec031d15050dfe611e8c09ed1fb
Timothy Jump <10928142+E3t1jump@users.noreply.github.com>
2025-12-05 07:10:11 -0600

    Update TmccConsistManager.java
    commenting out log/line 152

--- java/src/jmri/jmrix/tmcc/TmccConsistManager.java
+++ java/src/jmri/jmrix/tmcc/TmccConsistManager.java
@@ -149,6 +149,6 @@ public class TmccConsistManager extends AbstractConsistManager {
 
     }
 
-    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TmccSystemConnectionMemo.class);
+//    private final static org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(TmccSystemConnectionMemo.class);
 
 }
