17b4d5b81b03fe547537144da1e6675f83091113
Bob Jacobsen <jacobsen@mac.com>
2025-12-09 16:37:07 -0500

    annotate Bundle static method structure to satisfy SpotBugs

--- java/src/jmri/jmrix/loconet/lnsvf2/Bundle.java
+++ java/src/jmri/jmrix/loconet/lnsvf2/Bundle.java
@@ -10,7 +10,8 @@ import org.slf4j.LoggerFactory;
 
 @ParametersAreNonnullByDefault
 @CheckReturnValue
-@SuppressFBWarnings(value = "NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", justification = "Desired pattern is repeated class names with package-level access to members")
+@SuppressFBWarnings(value = {"NM_SAME_SIMPLE_NAME_AS_SUPERCLASS", "HSM_HIDING_METHOD"},
+    justification = "Desired pattern is repeated class names with package-level access to members")
 
 @javax.annotation.concurrent.Immutable
 
