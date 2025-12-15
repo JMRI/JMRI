1315d966728b7ce1e0c3ce0876a85cf9d7531fce
Steve Young <icklesteve@yahoo.com>
2025-12-15 09:57:30 +0000

    update lenx MessageFormatter tests
    to extend from AbstractMessageFormatterTest
    Ensures setUp / tearDown is called.

--- java/test/jmri/jmrix/lenz/messageformatters/XNetFunctionGroup7OperateRequestMessageFormatterTest.java
+++ java/test/jmri/jmrix/lenz/messageformatters/XNetFunctionGroup7OperateRequestMessageFormatterTest.java
@@ -1,20 +1,19 @@
 package jmri.jmrix.lenz.messageformatters;
 
-
+import jmri.jmrix.AbstractMessageFormatterTest;
 import jmri.jmrix.lenz.XNetMessage;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 /**
  * Tests of XNetFunctionGroup7OperateRequestMessageFormatter class
  *
  * @Paul Bender Copyright (C) 2024
  */
-public class XNetFunctionGroup7OperateRequestMessageFormatterTest {
+public class XNetFunctionGroup7OperateRequestMessageFormatterTest extends AbstractMessageFormatterTest {
 
     @Test
     public void testFormatMessageAllOff() {
-        XNetFunctionGroup7OperateRequestMessageFormatter formatter = new XNetFunctionGroup7OperateRequestMessageFormatter();
         XNetMessage msg = XNetMessage.getFunctionGroup7OpsMsg(1234, false,false,false,false,false,false,false,false);
 
         Assertions.assertTrue(formatter.handlesMessage(msg));
@@ -23,10 +22,17 @@ public class XNetFunctionGroup7OperateRequestMessageFormatterTest {
 
     @Test
     public void testFormatMessageAllOn() {
-        XNetFunctionGroup7OperateRequestMessageFormatter formatter = new XNetFunctionGroup7OperateRequestMessageFormatter();
         XNetMessage msg = XNetMessage.getFunctionGroup7OpsMsg(1234, true,true,true,true,true,true,true,true);
 
         Assertions.assertTrue(formatter.handlesMessage(msg));
         Assertions.assertEquals("Mobile Decoder Operations Request: Set Function Group 7 for Address: 1234 F37 On; F38 On; F39 On; F40 On; F41 On; F42 On; F43 On; F44 On; ",formatter.formatMessage(msg));
     }
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp(); // setup JUnit
+        formatter = new XNetFunctionGroup7OperateRequestMessageFormatter();
+    }
+
 }
--- java/test/jmri/jmrix/lenz/messageformatters/XNetRequestMultiUnitRemoveLocoMessageFormatterTest.java
+++ java/test/jmri/jmrix/lenz/messageformatters/XNetRequestMultiUnitRemoveLocoMessageFormatterTest.java
@@ -1,22 +1,30 @@
 package jmri.jmrix.lenz.messageformatters;
 
+import jmri.jmrix.AbstractMessageFormatterTest;
 import jmri.jmrix.lenz.XNetMessage;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 
 /**
  * Tests of XNetRequestMultiUnitRemoveLocoMessageFormatter class
  *
  * @author Paul Bender Copyright (C) 2024
  */
-public class XNetRequestMultiUnitRemoveLocoMessageFormatterTest {
+public class XNetRequestMultiUnitRemoveLocoMessageFormatterTest extends AbstractMessageFormatterTest {
 
     @Test
     public void testFormatter() {
-        XNetRequestMultiUnitRemoveLocoMessageFormatter formatter = new XNetRequestMultiUnitRemoveLocoMessageFormatter();
         XNetMessage msg = XNetMessage.getRemoveLocoFromConsistMsg(42,1234);
         Assertions.assertTrue(formatter.handlesMessage(msg));
         Assertions.assertEquals( "Mobile Decoder Operations Request: Remove Locomotive: 1234 from Multi Unit Consist: 42", formatter.formatMessage(msg));
 
     }
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp(); // setup JUnit
+        formatter = new XNetRequestMultiUnitRemoveLocoMessageFormatter();
+    }
+
 }
