a3ccdb97f62b3d96e623a8b536bd2ba802abb69f
Daniel <db123@bergqvist.se>
2025-12-20 23:30:43 +0100

    Remove old code

--- java/src/jmri/util/startup/configurexml/PerformActionModelXml.java
+++ java/src/jmri/util/startup/configurexml/PerformActionModelXml.java
@@ -1,12 +1,9 @@
 package jmri.util.startup.configurexml;
 
-import java.lang.reflect.InvocationTargetException;
 
 import jmri.util.startup.PerformActionModel;
 import jmri.util.startup.StartupActionsManager;
 import jmri.InstanceManager;
-import jmri.SystemConnectionMemo;
-import jmri.jmrix.swing.SystemConnectionAction;
 
 import org.jdom2.Attribute;
 import org.jdom2.Element;
@@ -75,31 +72,7 @@ public class PerformActionModelXml extends jmri.configurexml.AbstractXmlAdapter 
             String value = child.getAttributeValue("value"); // NOI18N
             if (child.getAttributeValue("name").equals("systemPrefix") // NOI18N
                     && value != null) {
-                // handle the situation where the model expects a system prefix
-                // but was not saved with one in a pre-4.19.7 JMRI instance
-                // TODO: at some point (June 2022 release?) change entire
-                // try/catch block to just "model.setSystemPrefix(value);"
-                try {
-                    Class<?> ac = Class.forName(className);
-                    if (value.isEmpty() && SystemConnectionAction.class.isAssignableFrom(ac)) {
-                        SystemConnectionAction<?> a = (SystemConnectionAction<?>) ac.getConstructor().newInstance();
-                        InstanceManager.getList(SystemConnectionMemo.class)
-                                .forEach(memo -> a.getSystemConnectionMemoClasses().stream()
-                                .filter(mc -> memo.getClass().isAssignableFrom(mc))
-                                .forEach(mc -> model.setSystemPrefix(memo.getSystemPrefix())));
-                    } else {
-                        model.setSystemPrefix(value);
-                    }
-                } catch (ClassNotFoundException
-                        | InstantiationException
-                        | IllegalAccessException
-                        | IllegalArgumentException
-                        | InvocationTargetException
-                        | NoSuchMethodException
-                        | SecurityException ex) {
-                    // ignore to allow manager to handle later
-                    log.warn("While trying to do {}, encountered exception", className, ex);
-                }
+                model.setSystemPrefix(value);
             }
         });
         InstanceManager.getDefault(StartupActionsManager.class).addAction(model);
