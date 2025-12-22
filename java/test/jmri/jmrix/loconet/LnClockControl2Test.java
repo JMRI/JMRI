fdeb843a271f02a0fdfde86251e32d5b8931f16e
Daniel <db123@bergqvist.se>
2025-12-22 10:03:30 +0100

    Add tests

---
+++ java/test/jmri/jmrix/loconet/LnClockControl2Test.java
@@ -0,0 +1,54 @@
+package jmri.jmrix.loconet;
+
+import java.util.Date;
+
+import jmri.util.JUnitUtil;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for LnClockControlTest class.
+ *
+ * @author Bob Jacobsen (c) 2025
+ **/
+public class LnClockControl2Test {
+
+    private LnClockControl ldt;
+    private Date time;
+
+    private LocoNetInterfaceScaffold lnis;
+    private LocoNetSystemConnectionMemo memo;
+    private SlotManager slotmanager;
+
+    @Test
+    public void testCheckStartValues(){
+        assertEquals(1.0, ldt.getRate(), "initial rate");
+    }
+
+    @BeforeEach
+    public void setUp() {
+        JUnitUtil.setUp();
+        JUnitUtil.resetProfileManager();
+        JUnitUtil.initConfigureManager();
+        JUnitUtil.initMemoryManager();
+
+        // prepare an interface
+        lnis = new LocoNetInterfaceScaffold(memo);
+        slotmanager = new SlotManager(lnis);
+        memo = new LocoNetSystemConnectionMemo(lnis, slotmanager);
+
+        ldt = new LnClockControl(memo);
+        time = ldt.getTime();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        memo.dispose();
+        lnis = null;
+
+        JUnitUtil.tearDown();
+    }
+
+}
