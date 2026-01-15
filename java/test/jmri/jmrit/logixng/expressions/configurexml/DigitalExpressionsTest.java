e2a01992373d070b41393de08b4c6fdd5472ca68
Steve Young <icklesteve@yahoo.com>
2026-01-15 02:29:39 +0000

    LogixNG expressions tests update
    Tests and Assertions JU4 > JU5

--- java/test/jmri/jmrit/logixng/expressions/configurexml/DigitalExpressionsTest.java
+++ java/test/jmri/jmrit/logixng/expressions/configurexml/DigitalExpressionsTest.java
@@ -1,15 +1,17 @@
 package jmri.jmrit.logixng.expressions.configurexml;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import jmri.configurexml.JmriConfigureXmlException;
 import jmri.managers.configurexml.AbstractNamedBeanManagerConfigXML;
 import jmri.util.JUnitAppender;
 import jmri.util.JUnitUtil;
 
 import org.jdom2.Element;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Test DigitalExpressions
@@ -23,58 +25,57 @@ public class DigitalExpressionsTest {
         AbstractNamedBeanManagerConfigXML b;
 
         b = new AndXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new AntecedentXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new ExpressionLightXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new ExpressionSensorXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new ExpressionTurnoutXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new FalseXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new HoldXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new OrXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new TriggerOnceXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
 
         b = new TrueXml();
-        Assert.assertNotNull("exists", b);
+        assertNotNull( b, "exists");
         b.load((Element) null, (Object) null);
         JUnitAppender.assertMessage("Invalid method called");
     }
 
-    // The minimal setup for log4J
-    @Before
+    @BeforeEach
     public void setUp() {
         JUnitUtil.setUp();
         JUnitUtil.resetInstanceManager();
@@ -85,7 +86,7 @@ public class DigitalExpressionsTest {
         JUnitUtil.initLogixNGManager();
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         jmri.jmrit.logixng.util.LogixNG_Thread.stopAllLogixNGThreads();
         JUnitUtil.deregisterBlockManagerShutdownTask();
