c18f7202003be4773e521c1c3fd045bba724fe94
Bob Jacobsen <jacobsen@mac.com>
2026-01-23 12:39:54 -0500

    test for small mouse movement, not really an effective test

--- java/test/jmri/jmrit/display/SensorIconTest.java
+++ java/test/jmri/jmrit/display/SensorIconTest.java
@@ -1,6 +1,10 @@
 package jmri.jmrit.display;
 
+import java.awt.Component;
+
+import jmri.Sensor;
 import jmri.util.junit.annotations.DisabledIfHeadless;
+import jmri.util.swing.JmriMouseEvent;
 
 import org.junit.jupiter.api.*;
 
@@ -18,12 +22,75 @@ public class SensorIconTest extends PositionableIconTest {
         Assertions.assertNotNull( p, "SensorIcon Constructor");
     }
 
+    @Test
+    @DisabledIfHeadless
+    public void testClickStationary() {
+
+        Assertions.assertEquals(Sensor.UNKNOWN, s.getCommandedState());
+
+        Component source = si;
+        int id = 1001;
+        long when = 50000;
+        int modifiers = 0;
+        int x = 3;
+        int y = 3;
+        int clickCount = 0;
+        boolean popupTrigger = false;
+        var event = new JmriMouseEvent(source, id, when, modifiers, x, y, 
+                                            clickCount, popupTrigger);
+        
+        p.doMousePressed(event);
+        p.doMouseClicked(event);
+        p.doMouseReleased(event);
+        
+        Assertions.assertEquals(Sensor.INACTIVE, s.getCommandedState());
+
+        p.doMousePressed(event);
+        p.doMouseClicked(event);
+        p.doMouseReleased(event);
+        
+        Assertions.assertEquals(Sensor.ACTIVE, s.getCommandedState());
+    }
+
+    @Test
+    @DisabledIfHeadless
+    public void testClickMove2() {
+
+        Assertions.assertEquals(Sensor.UNKNOWN, s.getCommandedState());
+
+        Component source = si;
+        int id = 1001;
+        long when = 50000;
+        int modifiers = 0;
+        int x = 3;
+        int y = 3;
+        int clickCount = 0;
+        boolean popupTrigger = false;
+        var event = new JmriMouseEvent(source, id, when, modifiers, x, y, 
+                                            clickCount, popupTrigger);
+        
+        p.doMousePressed(event);
+        
+        event = new JmriMouseEvent(source, id, when, modifiers, x+2, y, 
+                                            clickCount, popupTrigger);
+        p.doMouseClicked(event);
+        
+        event = new JmriMouseEvent(source, id, when, modifiers, x=2, y+2, 
+                                            clickCount, popupTrigger);
+        p.doMouseReleased(event);
+        
+        Assertions.assertEquals(Sensor.INACTIVE, s.getCommandedState());
+    }
+
+    Sensor s;
+    SensorIcon si;
+    
     @Override
     @BeforeEach
     public void setUp() {
         super.setUp(); // creates editor
-        SensorIcon si = new SensorIcon(editor);
-        jmri.Sensor s = jmri.InstanceManager.sensorManagerInstance().provideSensor("IS1");
+        si = new SensorIcon(editor);
+        s = jmri.InstanceManager.sensorManagerInstance().provideSensor("IS1");
         si.setSensor(new jmri.NamedBeanHandle<>("IS1", s));
         p = si;
     }
