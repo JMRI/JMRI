6b7f735c22bd2d97c1eb5117e81dfe3755e6a57c
Philip Gladstone <philip@gladstonefamily.net>
2026-02-07 23:02:08 +0000

    FIx the crash when printing on Macos to PDF (and maybe to other printers as well). Also: * Compute the character metrics correctly so that the number of characters per line is right. * However, this means that the train roster doesn't fit at a fontsize of 7 as it prints 130 characters   and the actual width is 128 characters. We now allow printing into the right margin (but not off the edge of the paper).

--- java/src/jmri/util/davidflanagan/HardcopyWriter.java
+++ java/src/jmri/util/davidflanagan/HardcopyWriter.java
@@ -6,6 +6,7 @@ import java.awt.JobAttributes.SidesType;
 import java.awt.event.ActionEvent;
 import java.awt.font.FontRenderContext;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.Writer;
 import java.text.DateFormat;
@@ -44,6 +45,7 @@ public class HardcopyWriter extends Writer {
     protected FontMetrics headermetrics;
     protected int x0, y0;
     protected int height, width;
+    protected int width_including_right_margin;
     protected int headery;
     protected float charwidth;
     protected int lineheight;
@@ -141,20 +143,31 @@ public class HardcopyWriter extends Writer {
             if (jobAttributes.getDefaultSelection().equals(DefaultSelectionType.RANGE)) {
                 prFirst = jobAttributes.getPageRanges()[0][0];
             }
+        } else {
+            if (pageAttributes.getOrientationRequested().equals(PageAttributes.OrientationRequestedType.LANDSCAPE)) {
+                pagesizePoints = new Dimension(pagesizePoints.height, pagesizePoints.width);
+                pagesizePixels = new Dimension(pagesizePixels.height, pagesizePixels.width);
+            }
         }
 
         x0 = (int) (leftmargin * 72);
         y0 = (int) (topmargin * 72);
         width = pagesizePoints.width - (int) ((leftmargin + rightmargin) * 72);
         height = pagesizePoints.height - (int) ((topmargin + bottommargin) * 72);
+        width_including_right_margin = pagesizePoints.width - (int) (leftmargin * 72);
+
+        // Create a graphics context that we can use to get font metrics
+        Image img = new BufferedImage(pagesizePixels.width, pagesizePixels.height, BufferedImage.TYPE_INT_RGB);
+        Graphics g = img.getGraphics();
+        setupGraphics(g);
 
         // get body font and font size
         font = new Font(fontName, fontStyle, fontsize);
         metrics = frame.getFontMetrics(font);
         lineheight = metrics.getHeight();
         lineascent = metrics.getAscent();
-        Rectangle2D bounds = metrics.getStringBounds("mmmmmmmmmm", frame.getGraphics());
-        charwidth = (float) (bounds.getWidth() / 10);
+        Rectangle2D bounds = metrics.getStringBounds("m".repeat(100), g);
+        charwidth = (float) (bounds.getWidth() / 100.0);
 
         // compute lines and columns within margins
         chars_per_line = (int) (width / charwidth);
@@ -324,7 +337,7 @@ public class HardcopyWriter extends Writer {
                     continue;
                 }
                 // if no more characters will fit on the line, start new line
-                if (charoffset >= width) {
+                if (charoffset >= width_including_right_margin) {
                     newline();
                     // also start a new page if needed
                     if (page == null) {
@@ -472,8 +485,13 @@ public class HardcopyWriter extends Writer {
                 metrics = frame.getFontMetrics(font);
                 lineheight = metrics.getHeight();
                 lineascent = metrics.getAscent();
-                Rectangle2D bounds = metrics.getStringBounds("mmmmmmmmmm", frame.getGraphics());
-                charwidth = (float) (bounds.getWidth() / 10);
+
+                Image img = new BufferedImage(pagesizePixels.width, pagesizePixels.height, BufferedImage.TYPE_INT_RGB);
+                Graphics g = img.getGraphics();
+                setupGraphics(g);
+
+                Rectangle2D bounds = metrics.getStringBounds("m".repeat(100), g);
+                charwidth = (float) (bounds.getWidth() / 100.0);
 
                 // compute lines and columns within margins
                 chars_per_line = (int) (width / charwidth);
@@ -575,36 +593,17 @@ public class HardcopyWriter extends Writer {
                     JFrame f = new JFrame();
                     f.pack();
                     page = f.createImage(pagesizePixels.width, pagesizePixels.height).getGraphics();
-                    Graphics2D g2d = (Graphics2D) page;
-                    double scale = Toolkit.getDefaultToolkit().getScreenResolution() / 72.0;
-                    g2d.scale(scale, scale);
+                    if (page instanceof Graphics2D) {
+                        Graphics2D g2d = (Graphics2D) page;
+                        double scale = Toolkit.getDefaultToolkit().getScreenResolution() / 72.0;
+                        g2d.scale(scale, scale);
+                    }
                 }
             } else { // Preview
                 previewImage = previewPanel.createImage(pagesizePixels.width, pagesizePixels.height);
                 page = previewImage.getGraphics();
-                Graphics2D g2d = (Graphics2D) page;
-                double scale = Toolkit.getDefaultToolkit().getScreenResolution() / 72.0;
-                g2d.scale(scale, scale);
 
-                // Enable Antialiasing (Smooths the edges)
-                g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                        RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
-                // Enable Fractional Metrics (Improves character spacing)
-                g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
-                        RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-
-                // High Quality Rendering
-                g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
-                        RenderingHints.VALUE_RENDER_QUALITY);
-
-                // Set Interpolation for the Image (The most important for images)
-                g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                        RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-
-                // Enable Antialiasing (Smooths the edges)
-                g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                        RenderingHints.VALUE_ANTIALIAS_ON);
+                setupGraphics(page);
 
                 page.setColor(Color.white);
                 page.fillRect(0, 0, previewImage.getWidth(previewPanel), previewImage.getHeight(previewPanel));
@@ -615,7 +614,7 @@ public class HardcopyWriter extends Writer {
             page.setFont(headerfont);
             page.drawString(jobname, x0, headery);
 
-            FontRenderContext frc = ((Graphics2D) page).getFontRenderContext();
+            FontRenderContext frc = page.getFontMetrics().getFontRenderContext();
 
             String s = "- " + pagenum + " -"; // print page number centered
             Rectangle2D bounds = headerfont.getStringBounds(s, frc);
@@ -631,6 +630,34 @@ public class HardcopyWriter extends Writer {
         // set basic font
         if (page != null) {
             page.setFont(font);
+        }
+    }
+
+    private void setupGraphics(Graphics g) {
+        if (g instanceof Graphics2D) {
+            Graphics2D g2d = (Graphics2D) g;
+            double scale = Toolkit.getDefaultToolkit().getScreenResolution() / 72.0;
+            g2d.scale(scale, scale);
+
+            // Enable Antialiasing (Smooths the edges)
+            g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                    RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+            // Enable Fractional Metrics (Improves character spacing)
+            g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
+                    RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+
+            // High Quality Rendering
+            g2d.setRenderingHint(RenderingHints.KEY_RENDERING,
+                    RenderingHints.VALUE_RENDER_QUALITY);
+
+            // Set Interpolation for the Image (The most important for images)
+            g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                    RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+
+            // Enable Antialiasing (Smooths the edges)
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
         }
     }
 
@@ -847,5 +874,5 @@ public class HardcopyWriter extends Writer {
         }
     }
 
-    // private final static Logger log = LoggerFactory.getLogger(HardcopyWriter.class);
+    // private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(HardcopyWriter.class);
 }
