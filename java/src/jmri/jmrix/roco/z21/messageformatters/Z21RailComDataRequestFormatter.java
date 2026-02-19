0b5a034b945722a1f0356d702bf1a978237296d5
Paul Bender <paul.bender@acm.org>
2026-02-18 23:25:30 -0500

    Use updated instruction to poll Z21 Global Reporter; keep track of the reports that are presented ( as the Z21 itself does, per the documentation ).

--- java/src/jmri/jmrix/roco/z21/messageformatters/Z21RailComDataRequestFormatter.java
+++ java/src/jmri/jmrix/roco/z21/messageformatters/Z21RailComDataRequestFormatter.java
@@ -21,7 +21,8 @@ public class Z21RailComDataRequestFormatter implements Z21MessageFormatter {
         if(!handlesMessage(m)) {
             throw new IllegalArgumentException("Message m is not a valid Z21 RailCom Data Request Message");
         }
-        return Bundle.getMessage("Z21_RAILCOM_GETDATA");
+        int address = ((Z21Message)m).getElement(5) & 0xFF + (((Z21Message)m).getElement(6) & 0xFF) << 8;
+        return Bundle.getMessage("Z21_RAILCOM_GETDATA",address);
     }
 
 }
