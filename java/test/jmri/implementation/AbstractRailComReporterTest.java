d24769b6e274eda233512eb6fa75ec0fbf6f8930
Steve Young <icklesteve@yahoo.com>
2026-03-05 09:43:58 +0000

    Update AbstractRailComReporterTest.java
    unused import

--- java/test/jmri/implementation/AbstractRailComReporterTest.java
+++ java/test/jmri/implementation/AbstractRailComReporterTest.java
@@ -3,7 +3,6 @@ package jmri.implementation;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import jmri.IdTag;
 import jmri.InstanceManager;
