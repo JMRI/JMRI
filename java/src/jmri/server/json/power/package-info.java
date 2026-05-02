30effc71dace8ea1108d7bb0f6671520043e8120
Chad Francis <yamanote1138@gmail.com>
2026-05-01 23:12:56 -0600

    Fix localhost links and update power package JavaDoc

--- java/src/jmri/server/json/power/package-info.java
+++ java/src/jmri/server/json/power/package-info.java
@@ -5,17 +5,21 @@
  * properties in the JSON data object:
  * <dl>
  * <dt>name</dt><dd>The user name of the connection supplying power.</dd>
+ * <dt>prefix</dt><dd>The system prefix of the connection supplying power
+ * (e.g. {@code "L"} for LocoNet, {@code "D"} for DCC-EX).</dd>
  * <dt>state</dt><dd>One of
- * {@link jmri.server.json.JSON#UNKNOWN}, {@link jmri.server.json.JSON#ON}, or
- * {@link jmri.server.json.JSON#OFF}</dd>
+ * {@link jmri.server.json.JSON#UNKNOWN}, {@link jmri.server.json.JSON#ON},
+ * {@link jmri.server.json.JSON#OFF}, or
+ * {@link jmri.server.json.JSON#IDLE}</dd>
  * <dt>default</dt><dd>True if this message represents the power state for the
  * default power connection; false otherwise.</dd>
  * </dl>
  *
- * If messages requesting or setting power for this server do not contain the
- * user name of a system connection, this server returns the power state for the
- * default system connection, otherwise it returns the power state for the named
- * system connection.
+ * Requests may include an optional {@code prefix} field in the data object to
+ * target a specific system connection. If {@code prefix} is absent, the default
+ * connection is used. An unknown prefix returns an HTTP 400 error. WebSocket
+ * clients receive state-change notifications only for the connection they
+ * subscribed to; use {@code method:"list"} to subscribe to all connections.
  */
 // include empty DefaultAnnotation to avoid excessive recompilation
 @edu.umd.cs.findbugs.annotations.DefaultAnnotation(value={})
