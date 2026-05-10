db5ed59b046c9b0a77a52c5995963bc7160d20cc
Daniel <db123@bergqvist.se>
2026-05-10 06:57:57 +0200

    Update ConnectionStatusTest.java

--- java/test/jmri/jmrix/ConnectionStatusTest.java
+++ java/test/jmri/jmrix/ConnectionStatusTest.java
@@ -1,7 +1,8 @@
 package jmri.jmrix;
 
-import jmri.jmrix.loconet.LnTrafficController;
-import jmri.jmrix.loconet.LocoNetSystemConnectionMemo;
+import java.util.Comparator;
+
+import jmri.NamedBean;
 import jmri.util.JUnitUtil;
 
 import org.junit.Assert;
@@ -14,10 +15,7 @@ import org.junit.jupiter.api.*;
  */
 public class ConnectionStatusTest {
 
-    private LnTrafficController lnis1;
-    private LnTrafficController lnis2;
-    private LocoNetSystemConnectionMemo lnMemo1;
-    private LocoNetSystemConnectionMemo lnMemo2;
+    private DefaultSystemConnectionMemo _memo = null;
 
     @Test
     public void testInstance() {
@@ -26,108 +24,84 @@ public class ConnectionStatusTest {
     }
 
     @Test
-    public void test2ParamterGetState() {
+    public void testGetState() {
         ConnectionStatus cs = ConnectionStatus.instance();
-        Assert.assertEquals("connection status", ConnectionStatus.CONNECTION_UNKNOWN, cs.getConnectionState(lnMemo1));
+        Assert.assertEquals("connection status", ConnectionStatus.CONNECTION_UNKNOWN, cs.getConnectionState(_memo));
     }
 
     @Test
-    public void testAddAnd2ParameterGetState() {
+    public void testAddAndGetState() {
         ConnectionStatus cs = ConnectionStatus.instance();
-        cs.addConnection(lnMemo1);
+        cs.addConnection(_memo);
         // set the status of the new connection so we know we are not
         // retreiving a new value.
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_UP);
-        Assert.assertEquals("connection status", ConnectionStatus.CONNECTION_UP, cs.getConnectionState(lnMemo1));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_UP);
+        Assert.assertEquals("connection status", ConnectionStatus.CONNECTION_UP, cs.getConnectionState(_memo));
     }
 
     @Test
-    public void test2ParameterSetAndGetState() {
+    public void testSetAndGetState() {
         ConnectionStatus cs = ConnectionStatus.instance();
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_UP);
-        Assert.assertEquals("connection status", ConnectionStatus.CONNECTION_UP, cs.getConnectionState(lnMemo1));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_UP);
+        Assert.assertEquals("connection status", ConnectionStatus.CONNECTION_UP, cs.getConnectionState(_memo));
     }
 
     @Test
-    public void test2ParamterIsConnectionOk() {
+    public void testIsConnectionOk() {
         ConnectionStatus cs = ConnectionStatus.instance();
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_UP);
-        Assert.assertTrue("connection OK", cs.isConnectionOk(lnMemo1));
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_DOWN);
-        Assert.assertFalse("connection OK", cs.isConnectionOk(lnMemo1));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_UP);
+        Assert.assertTrue("connection OK", cs.isConnectionOk(_memo));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_DOWN);
+        Assert.assertFalse("connection OK", cs.isConnectionOk(_memo));
     }
 
     @Test
     public void testIsSystemOk() {
         ConnectionStatus cs = ConnectionStatus.instance();
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_UP);
-        Assert.assertTrue("connection OK", cs.isSystemOk("Foo"));
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_DOWN);
-        Assert.assertFalse("connection OK", cs.isSystemOk("Foo"));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_UP);
+        Assert.assertTrue("connection OK", cs.isSystemOk("Test"));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_DOWN);
+        Assert.assertFalse("connection OK", cs.isSystemOk("Test"));
     }
 
     @Test
     public void testIsUnrecognizedSystemOk() {
         ConnectionStatus cs = ConnectionStatus.instance();
-        Assert.assertTrue("connection OK", cs.isConnectionOk(lnMemo1));
+        Assert.assertTrue("connection OK", cs.isConnectionOk(_memo));
     }
 
     @Test
-    public void testGetSateForSystemName() {
+    public void testGetStateForSystemName() {
         ConnectionStatus cs = ConnectionStatus.instance();
-//        cs.addConnection(lnMemo1);
+//        cs.addConnection(_memo);
         // set the status of the new connection so we know we are not
         // retreiving a new value.
-        cs.setConnectionState(lnMemo1, ConnectionStatus.CONNECTION_UP);
-        Assert.assertTrue("connection OK", cs.isConnectionOk(lnMemo1));
-    }
-
-    @Test
-    public void testIsConnectionOkWNull() {
-        ConnectionStatus cs = ConnectionStatus.instance();
-        cs.setConnectionState(lnMemo2, ConnectionStatus.CONNECTION_UP);
-        Assert.assertTrue("connection OK", cs.isConnectionOk(lnMemo2));
-        cs.setConnectionState(lnMemo2, ConnectionStatus.CONNECTION_DOWN);
-        Assert.assertFalse("connection OK", cs.isConnectionOk(lnMemo2));
+        cs.setConnectionState(_memo, ConnectionStatus.CONNECTION_UP);
+        Assert.assertTrue("connection OK", cs.isConnectionOk(_memo));
     }
 
     @BeforeEach
     public void setUp() {
         ConnectionStatus.clearInstance();
         JUnitUtil.setUp();
-        JUnitUtil.resetInstanceManager();
-        JUnitUtil.resetProfileManager();
-        JUnitUtil.initConfigureManager();
-        JUnitUtil.initInternalTurnoutManager();
-        JUnitUtil.initInternalLightManager();
-        JUnitUtil.initInternalSensorManager();
+        JUnitUtil.initDebugCommandStation();
+        JUnitUtil.initDebugProgrammerManager();
+        _memo = new DefaultSystemConnectionMemo("T", "Test") {
+            @Override
+            protected java.util.ResourceBundle getActionModelResourceBundle() {
+                return null;
+            }
 
-        // The class under test uses LocoNet connections that it pulls from the InstanceManager.
-        lnMemo1 = new jmri.jmrix.loconet.LocoNetSystemConnectionMemo("L", "LocoNet");
-        lnis1 = new jmri.jmrix.loconet.LocoNetInterfaceScaffold(lnMemo1);
-        lnMemo1.setLnTrafficController(lnis1);
-        lnMemo1.configureCommandStation(jmri.jmrix.loconet.LnCommandStationType.COMMAND_STATION_DCS100, false, false, false, false, false);
-        lnMemo1.configureManagers();
-        jmri.InstanceManager.store(lnMemo1, jmri.jmrix.loconet.LocoNetSystemConnectionMemo.class);
-
-        // The class under test uses LocoNet connections that it pulls from the InstanceManager.
-        lnMemo2 = new jmri.jmrix.loconet.LocoNetSystemConnectionMemo("L2", "LocoNet2");
-        lnis2 = new jmri.jmrix.loconet.LocoNetInterfaceScaffold(lnMemo2);
-        lnMemo2.setLnTrafficController(lnis2);
-        lnMemo2.configureCommandStation(jmri.jmrix.loconet.LnCommandStationType.COMMAND_STATION_DCS100, false, false, false, false, false);
-        lnMemo2.configureManagers();
-        jmri.InstanceManager.store(lnMemo2, jmri.jmrix.loconet.LocoNetSystemConnectionMemo.class);
+            @Override
+            public <B extends NamedBean> Comparator<B> getNamedBeanComparator(Class<B> type) {
+                return null;
+            }
+        };
     }
 
     @AfterEach
     public void tearDown() {
-        lnMemo1.dispose();
-        lnMemo1 = null;
-        lnMemo2.dispose();
-        lnMemo2 = null;
-        lnis1 = null;
-        lnis2 = null;
-
+        _memo = null;
         JUnitUtil.tearDown();
     }
 
