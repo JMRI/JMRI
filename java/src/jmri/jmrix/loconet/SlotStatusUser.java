f7783d60e39af4c24fbad69733493f3967cbf440
Bob Jacobsen <jacobsen@mac.com>
2026-06-29 04:55:02 -0400

    update extraneously-added interface so that it compiles standalone

--- java/src/jmri/jmrix/loconet/SlotStatusUser.java
+++ java/src/jmri/jmrix/loconet/SlotStatusUser.java
@@ -4,8 +4,8 @@ package jmri.jmrix.loconet;
  * Interface for marking a class as using slot status information
  * from the loconet.SlotManager.
  *
- * Objects of this type registereed with the
- * SlotManager via {@link SlotManager#addSlotStatusUser} to indicate
+ * Objects of this type are registereed with the
+ * SlotManager to indicate
  * that status information is being used, so the {@link SlotManager}
  * should accumulate it.
  *
