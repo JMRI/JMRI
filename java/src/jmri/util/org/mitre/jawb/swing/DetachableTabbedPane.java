826085e56413f8ffd58f4aed9d7ecc5c03c7f6ab
Bob Jacobsen <rgj1927@gmail.com>
2026-07-29 11:10:26 -0400

    improvements from static checks

--- java/src/jmri/util/org/mitre/jawb/swing/DetachableTabbedPane.java
+++ java/src/jmri/util/org/mitre/jawb/swing/DetachableTabbedPane.java
@@ -392,14 +392,14 @@ public class DetachableTabbedPane extends JTabbedPane {
   @Override
   public void remove (Component comp) {
     Detachable detachable = panelToDetMap.get (comp);
-    if (detachable != null)
+    if (detachable != null) {
       log.trace("panelToDetMap of {} found {} with {}", comp, detachable, detachable.component);
       remove (detachable);
-    else
+    } else {
       super.remove(comp);
+    }
   }
   private void remove (Detachable d) {
-    log.trace("remove Detachable {} Component {}", d, d.component);
     if (d != null) {
       panelToDetMap.remove (d.component);
       shiftDetachables (false, d);
